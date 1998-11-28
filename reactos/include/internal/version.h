93263fa72246c03800af3e583b523199342b3f52
David Welch <welch@cwcom.net>
1998-11-28 23:07:08 +0000

    Updated version.h only, just a test really
    svn path=/trunk/; revision=95

--- reactos/include/internal/version.h
+++ reactos/include/internal/version.h
@@ -9,9 +9,9 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define KERNEL_VERSION   "0.0.12"
+#define KERNEL_VERSION   "0.0.13"
 #define KERNEL_MAJOR_VERSION    0
 #define KERNEL_MINOR_VERSION    0
-#define KERNEL_PATCH_LEVEL      12
+#define KERNEL_PATCH_LEVEL      13
 
 #endif
