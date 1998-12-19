5d1ddcb775c8886f55024ffb0f136b5721d153d2
David Welch <welch@cwcom.net>
1998-12-19 19:05:11 +0000

    Adding missing functions to ntdll
    svn path=/trunk/; revision=128

---
+++ reactos/lib/ntdll/stdio/vsprintf.c
@@ -0,0 +1,381 @@
+/*
+ *  linux/lib/vsprintf.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
+
+/* vsprintf.c -- Lars Wirzenius & Linus Torvalds. */
+/*
+ * Wirzenius wrote this portably, Torvalds fucked it up :-)
+ */
+
+/*
+ * Appropiated for the reactos kernel, March 1998 -- David Welch
+ */
+
+#include <stdarg.h>
+
+#include <internal/debug.h>
+#include <internal/ctype.h>
+#include <internal/string.h>
+
+unsigned long simple_strtoul(const char *cp,char **endp,unsigned int base)
+{
+	unsigned long result = 0,value;
+
+	if (!base) {
+		base = 10;
+		if (*cp == '0') {
+			base = 8;
+			cp++;
+			if ((*cp == 'x') && isxdigit(cp[1])) {
+				cp++;
+				base = 16;
+			}
+		}
+	}
+	while (isxdigit(*cp) && (value = isdigit(*cp) ? *cp-'0' : (islower(*cp)
+	    ? toupper(*cp) : *cp)-'A'+10) < base) {
+		result = result*base + value;
+		cp++;
+	}
+	if (endp)
+		*endp = (char *)cp;
+	return result;
+}
+
+/* we use this so that we can do without the ctype library */
+#define is_digit(c)	((c) >= '0' && (c) <= '9')
+
+static int skip_atoi(const char **s)
+{
+	int i=0;
+
+	while (is_digit(**s))
+		i = i*10 + *((*s)++) - '0';
+	return i;
+}
+
+#define ZEROPAD	1		/* pad with zero */
+#define SIGN	2		/* unsigned/signed long */
+#define PLUS	4		/* show plus */
+#define SPACE	8		/* space if plus */
+#define LEFT	16		/* left justified */
+#define SPECIAL	32		/* 0x */
+#define LARGE	64		/* use 'ABCDEF' instead of 'abcdef' */
+
+#define do_div(n,base) ({ \
+int __res; \
+__res = ((unsigned long) n) % (unsigned) base; \
+n = ((unsigned long) n) / (unsigned) base; \
+__res; })
+
+static char * number(char * str, long num, int base, int size, int precision
+	,int type)
+{
+	char c,sign,tmp[66];
+	const char *digits="0123456789abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	if (type & LARGE)
+		digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (type & LEFT)
+		type &= ~ZEROPAD;
+	if (base < 2 || base > 36)
+		return 0;
+	c = (type & ZEROPAD) ? '0' : ' ';
+	sign = 0;
+	if (type & SIGN) {
+		if (num < 0) {
+			sign = '-';
+			num = -num;
+			size--;
+		} else if (type & PLUS) {
+			sign = '+';
+			size--;
+		} else if (type & SPACE) {
+			sign = ' ';
+			size--;
+		}
+	}
+	if (type & SPECIAL) {
+		if (base == 16)
+			size -= 2;
+		else if (base == 8)
+			size--;
+	}
+	i = 0;
+	if (num == 0)
+		tmp[i++]='0';
+	else while (num != 0)
+		tmp[i++] = digits[do_div(num,base)];
+	if (i > precision)
+		precision = i;
+	size -= precision;
+	if (!(type&(ZEROPAD+LEFT)))
+		while(size-->0)
+			*str++ = ' ';
+	if (sign)
+		*str++ = sign;
+	if (type & SPECIAL)
+		if (base==8)
+			*str++ = '0';
+		else if (base==16) {
+			*str++ = '0';
+			*str++ = digits[33];
+		}
+	if (!(type & LEFT))
+		while (size-- > 0)
+			*str++ = c;
+	while (i < precision--)
+		*str++ = '0';
+	while (i-- > 0)
+		*str++ = tmp[i];
+	while (size-- > 0)
+		*str++ = ' ';
+	return str;
+}
+
+int vsprintf(char *buf, const char *fmt, va_list args)
+{
+	int len;
+	unsigned long num;
+	int i, base;
+	char * str;
+	const char *s;
+        const short int* sw;
+
+	int flags;		/* flags to number() */
+
+	int field_width;	/* width of output field */
+	int precision;		/* min. # of digits for integers; max
+				   number of chars for from string */
+	int qualifier;		/* 'h', 'l', or 'L' for integer fields */
+   
+    
+   
+	for (str=buf ; *fmt ; ++fmt) {
+		if (*fmt != '%') {
+			*str++ = *fmt;
+			continue;
+		}
+			
+		/* process flags */
+		flags = 0;
+		repeat:
+			++fmt;		/* this also skips first '%' */
+			switch (*fmt) {
+				case '-': flags |= LEFT; goto repeat;
+				case '+': flags |= PLUS; goto repeat;
+				case ' ': flags |= SPACE; goto repeat;
+				case '#': flags |= SPECIAL; goto repeat;
+				case '0': flags |= ZEROPAD; goto repeat;
+				}
+		
+		/* get field width */
+		field_width = -1;
+		if (is_digit(*fmt))
+			field_width = skip_atoi(&fmt);
+		else if (*fmt == '*') {
+			++fmt;
+			/* it's the next argument */
+			field_width = va_arg(args, int);
+			if (field_width < 0) {
+				field_width = -field_width;
+				flags |= LEFT;
+			}
+		}
+
+		/* get the precision */
+		precision = -1;
+		if (*fmt == '.') {
+			++fmt;	
+			if (is_digit(*fmt))
+				precision = skip_atoi(&fmt);
+			else if (*fmt == '*') {
+				++fmt;
+				/* it's the next argument */
+				precision = va_arg(args, int);
+			}
+			if (precision < 0)
+				precision = 0;
+		}
+
+		/* get the conversion qualifier */
+		qualifier = -1;
+		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
+			qualifier = *fmt;
+			++fmt;
+		}
+
+		/* default base */
+		base = 10;
+
+		switch (*fmt) {
+		case 'c':
+			if (!(flags & LEFT))
+				while (--field_width > 0)
+					*str++ = ' ';
+			*str++ = (unsigned char) va_arg(args, int);
+			while (--field_width > 0)
+				*str++ = ' ';
+			continue;
+
+		 case 'w':
+		   sw = va_arg(args,short int *);
+//		   DPRINT("L %x\n",sw);
+		   if (sw==NULL)
+		     {
+//			CHECKPOINT;
+			s = "<NULL>";
+			while ((*s)!=0)
+			  {
+			     *str++ = *s++;
+			  }
+//			CHECKPOINT;
+//			DbgPrint("str %x\n",str);
+		     }
+		   else
+		     {
+			while ((*sw)!=0)
+			  {
+			     *str++ = (char)(*sw++);
+			  }
+		     }
+//		   CHECKPOINT;
+		   continue;
+		   
+		case 's':
+			s = va_arg(args, char *);
+			if (!s)
+				s = "<NULL>";
+
+			len = strnlen(s, precision);
+
+			if (!(flags & LEFT))
+				while (len < field_width--)
+					*str++ = ' ';
+			for (i = 0; i < len; ++i)
+				*str++ = *s++;
+			while (len < field_width--)
+				*str++ = ' ';
+			continue;
+
+		case 'p':
+			if (field_width == -1) {
+				field_width = 2*sizeof(void *);
+				flags |= ZEROPAD;
+			}
+			str = number(str,
+				(unsigned long) va_arg(args, void *), 16,
+				field_width, precision, flags);
+			continue;
+
+
+		case 'n':
+			if (qualifier == 'l') {
+				long * ip = va_arg(args, long *);
+				*ip = (str - buf);
+			} else {
+				int * ip = va_arg(args, int *);
+				*ip = (str - buf);
+			}
+			continue;
+
+		/* integer number formats - set up the flags and "break" */
+		case 'o':
+			base = 8;
+			break;
+		   
+		 case 'b':
+		        base = 2;
+		        break;
+		   
+		case 'X':
+			flags |= LARGE;
+		case 'x':
+			base = 16;
+			break;
+
+		case 'd':
+		case 'i':
+			flags |= SIGN;
+		case 'u':
+			break;
+
+		default:
+			if (*fmt != '%')
+				*str++ = '%';
+			if (*fmt)
+				*str++ = *fmt;
+			else
+				--fmt;
+			continue;
+		}
+		if (qualifier == 'l')
+			num = va_arg(args, unsigned long);
+		else if (qualifier == 'h')
+			if (flags & SIGN)
+				num = va_arg(args, short);
+			else
+				num = va_arg(args, unsigned short);
+		else if (flags & SIGN)
+			num = va_arg(args, int);
+		else
+			num = va_arg(args, unsigned int);
+		str = number(str, num, base, field_width, precision, flags);
+	}
+	*str = '\0';
+	return str-buf;
+}
+
+int sprintf(char * buf, const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i=vsprintf(buf,fmt,args);
+	va_end(args);
+	return i;
+}
+
+int wsprintfA(char * buf, const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i=vsprintf(buf,fmt,args);
+	va_end(args);
+	return i;
+}
+
+int wsprintfW(unsigned short * buf, const unsigned short *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	//i=vsprintf(buf,fmt,args);
+	va_end(args);
+	return i;
+}
+
+
+unsigned short towupper(unsigned short w)
+{
+	if ( w < L'A' )
+		return w + 'A';
+	else
+		return w;
+}
+
+char iswlower(unsigned short w)
+{
+	if ( w < L'A' )
+		return 1;
+	else
+		return 0;	
+}
+		
---
+++ reactos/lib/ntdll/string/wstring.c
@@ -0,0 +1,576 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS kernel
+ * FILE:            lib/ntdll/string/wstring.c
+ * PURPOSE:         Wide string functions
+ * PROGRAMMER:      David Welch (welch@mcmail.com)
+ * UPDATE HISTORY:
+ *                  Created 22/05/98
+ *   1998/12/04  RJJ  Cleaned up and added i386 def checks
+ */
+
+/* INCLUDES *****************************************************************/
+
+#include <ddk/ntddk.h>
+#include <wstring.h>
+
+#include <internal/debug.h>
+
+wchar_t * ___wcstok = NULL;
+
+/* FUNCTIONS *****************************************************************/
+
+wchar_t * 
+wcscat(wchar_t *dest, const wchar_t *src)
+{
+  int i, j;
+   
+  for (j = 0; dest[j] != 0; j++)
+    ;
+  for (i = 0; src[i] != 0; i++)
+    {
+      dest[j + i] = src[i];
+    }
+  dest[j + i] = 0;
+
+  return dest;
+}
+
+wchar_t * 
+wcschr(const wchar_t *str, wchar_t ch)
+{
+  while ((*str) != ((wchar_t) 0))
+    {
+      if ((*str) == ch)
+        {
+          return str;
+        }
+      str++;
+    }
+
+  return NULL;
+}
+
+int 
+wcscmp(const wchar_t *cs, const wchar_t *ct)
+{
+  while (*cs != '\0' && *ct != '\0' && *cs == *ct)
+    {
+      cs++;
+      ct++;
+    }
+  return *cs - *ct;
+}
+
+wchar_t* wcscpy(wchar_t* str1, const wchar_t* str2)
+{
+   wchar_t* s = str1;
+   while ((*str2)!=0)
+     {
+	*s = *str2;
+	s++;
+	str2++;
+     }
+   return(str1);
+}
+
+#if 0
+
+size_t 
+wcscspn(const wchar_t *cs, const wchar_t *ct)
+{
+  register wchar_t * __res;
+  __asm__ __volatile__(
+    "cld\n\t"
+    "movl %4,%%edi\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "notl %%ecx\n\t"
+    "decl %%ecx\n\t"
+    "movl %%ecx,%%edx\n"
+    "1:\tlodsw\n\t"
+    "testw %%eax,%%eax\n\t"
+    "je 2f\n\t"
+    "movl %4,%%edi\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "jne 1b\n"
+    "2:\tdecl %0"
+    : "=S" (__res)
+    : "a" (0),"c" (0xffffffff),"0" (cs),"g" (ct)
+    : "eax","ecx","edx","edi");
+
+  return __res-cs;
+}
+
+#else
+
+size_t 
+wcscspn(const wchar_t *cs, const wchar_t *ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+#ifdef i386
+
+int 
+wcsicmp(const wchar_t *cs,const wchar_t *ct)
+{
+  register int __res;
+
+  __asm__ __volatile__(
+    "cld\n"
+    "1:\tmovw (%%esi), %%eax\n\t"
+    "movw  (%%edi), %%edx \n\t"
+    "cmpw $0x5A, %%eax\n\t"
+    "ja 2f\t\n"
+    "cmpw $0x40, %%eax\t\n"
+    "jbe 2f\t\n"
+    "addw $0x20, %%eax\t\n"
+    "2:\t cmpw $0x5A, %%edx\t\n"
+    "ja 3f\t\n"
+    "cmpw $0x40, %%edx\t\n"
+    "jbe 3f\t\n"
+    "addw $0x20, %%edx\t\n"
+    "3:\t inc %%esi\t\n"
+    "inc %%esi\t\n"
+    "inc %%edi\t\n"
+    "inc %%edi\t\n"
+    "cmpw %%eax, %%edx\t\n"
+    "jne 4f\n\t"  
+    "cmpw $00, %%eax\n\t"
+    "jne 1b\n\t"
+    "xorl %%eax,%%eax\n\t"
+    "jmp 5f\n"
+    "4:\tsbbl %%eax,%%eax\n\t"
+    "orw $1,%%eax\n"
+    "5:"
+    : "=a" (__res)
+    : "S" (cs),"D" (ct)
+    : "esi","edi");
+
+  return __res;
+}
+
+#else
+
+int 
+wcsicmp(const wchar_t *cs,const wchar_t *ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+size_t 
+wcslen(const wchar_t *s)
+{
+  unsigned int len = 0;
+
+  while (s[len] != 0) 
+    {
+      len++;
+    }
+
+  return len;
+}
+
+wchar_t * 
+wcsncat(wchar_t *dest, const wchar_t *src, size_t count)
+{
+  int i, j;
+   
+  for (j = 0; dest[j] != 0; j++)
+    ;
+  for (i = 0; i < count; i++)
+    {
+      dest[j + i] = src[i];
+      if (src[i] == 0)
+        {
+          return dest;
+        }
+    }
+  dest[j + i] = 0;
+
+  return dest;
+}
+
+#ifdef i386
+
+int 
+wcsncmp(const wchar_t *cs, const wchar_t *ct, size_t count)
+{
+  register int __res;
+
+  __asm__ __volatile__(
+    "cld\n"
+    "1:\tdecl %3\n\t"
+    "js 2f\n\t"
+    "lodsw\n\t"
+    "scasw\n\t"
+    "jne 3f\n\t"
+    "testw %%eax,%%eax\n\t"
+    "jne 1b\n"
+    "2:\txorl %%eax,%%eax\n\t"
+    "jmp 4f\n"
+    "3:\tsbbl %%eax,%%eax\n\t"
+    "orw $1,%%eax\n"
+    "4:"
+    : "=a" (__res)
+    : "S" (cs), "D" (ct), "c" (count)
+    : "esi","edi","ecx");
+
+  return __res;
+}
+
+#else
+
+int 
+wcsncmp(const wchar_t *cs, const wchar_t *ct, size_t count)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+wchar_t* wcsncpy(wchar_t *dest, const wchar_t *src, size_t count)
+{
+  int i;
+   
+  for (i = 0; i < count; i++)
+    {
+      dest[i] = src[i];
+      if (src[i] == 0)
+        {
+          return dest;
+        }
+    }
+  dest[i] = 0;
+
+  return dest;
+}
+
+#ifdef i386
+
+int 
+wcsnicmp(const wchar_t *cs,const wchar_t *ct, size_t count)
+{
+  register int __res;
+
+  __asm__ __volatile__(
+    "cld\n"
+    "1:\t decl %3\n\t"
+    "js 6f\n\t"
+    "movw (%%esi), %%eax\n\t"
+    "movw  (%%edi), %%edx \n\t"
+    "cmpw $0x5A, %%eax\n\t"
+    "ja 2f\t\n"
+    "cmpw $0x40, %%eax\t\n"
+    "jbe 2f\t\n"
+    "addw $0x20, %%eax\t\n"
+    "2:\t cmpw $0x5A, %%edx\t\n"
+    "ja 3f\t\n"
+    "cmpw $0x40, %%edx\t\n"
+    "jbe 3f\t\n"
+    "addw $0x20, %%edx\t\n"
+    "3:\t inc %%esi\t\n"
+    "inc %%esi\t\n"
+    "inc %%edi\t\n"
+    "inc %%edi\t\n"
+    "cmpw %%eax, %%edx\t\n"
+    "jne 4f\n\t"  
+    "cmpw $00, %%eax\n\t"
+    "jne 1b\n\t"
+    "6:xorl %%eax,%%eax\n\t"
+    "jmp 5f\n"
+    "4:\tsbbl %%eax,%%eax\n\t"
+    "orw $1,%%eax\n"
+    "5:"
+    : "=a" (__res)
+    : "S" (cs), "D" (ct), "c" (count)
+    : "esi", "edi", "ecx");
+
+  return __res;
+}
+
+#else
+
+int 
+wcsnicmp(const wchar_t *cs,const wchar_t *ct, size_t count)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+#ifdef i386
+
+size_t 
+wcsnlen(const wchar_t *s, size_t count)
+{
+  register int __res;
+  __asm__ __volatile__(
+    "movl %1,%0\n\t"
+    "jmp 2f\n"
+    "1:\tcmpw $0,(%0)\n\t"
+    "je 3f\n\t"
+    "incl %0\n"
+    "2:\tdecl %2\n\t"
+    "cmpl $-1,%2\n\t"
+    "jne 1b\n"
+    "3:\tsubl %1,%0"
+    : "=a" (__res)
+    : "c" (s), "d" (count)
+    : "edx");
+
+  return __res;
+}
+
+#else
+
+size_t 
+wcsnlen(const wchar_t *s, size_t count)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+#ifdef i386
+
+wchar_t *
+wcspbrk(const wchar_t *cs, const wchar_t *ct)
+{
+  register wchar_t * __res;
+  __asm__ __volatile__(
+    "cld\n\t"
+    "movl %4,%%edi\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "notl %%ecx\n\t"
+    "decl %%ecx\n\t"
+    "movl %%ecx,%%edx\n"
+    "1:\tlodsw\n\t"
+    "testw %%eax,%%eax\n\t"
+    "je 2f\n\t"
+    "movl %4,%%edi\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "jne 1b\n\t"
+    "decl %0\n\t"
+    "jmp 3f\n"
+    "2:\txorl %0,%0\n"
+    "3:"
+    : "=S" (__res)
+    : "a" (0), "c" (0xffffffff), "0" (cs), "g" (ct)
+    : "eax", "ecx", "edx", "edi");
+
+  return __res;
+}
+
+#else
+
+wchar_t *
+wcspbrk(const wchar_t *cs, const wchar_t *ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+wchar_t * 
+wcsrchr(const wchar_t *str, wchar_t ch)
+{
+  unsigned int len = 0;
+  while (str[len] != ((wchar_t)0))
+    {
+      len++;
+    }
+   
+  for (; len > 0; len--)
+    {
+      if (str[len-1]==ch)
+        {
+          return (wchar_t *) &str[len - 1];
+        }
+    }
+
+  return NULL;
+}
+
+#ifdef i386
+
+size_t 
+wcsspn(const wchar_t *cs, const wchar_t *ct)
+{
+  register wchar_t * __res;
+
+  __asm__ __volatile__(
+    "cld\n\t"
+    "movl %4,%%edi\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "notl %%ecx\n\t"
+    "decl %%ecx\n\t"
+    "movl %%ecx,%%edx\n"
+    "1:\tlodsw\n\t"
+    "testw %%eax,%%eax\n\t"
+    "je 2f\n\t"
+    "movl %4,%%edi\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repne\n\t"
+    "scasb\n\t"
+    "je 1b\n"
+    "2:\tdecl %0"
+    : "=S" (__res)
+    : "a" (0), "c" (0xffffffff), "0" (cs), "g" (ct)
+    : "eax", "ecx", "edx", "edi");
+
+  return __res-cs;
+}
+
+#else
+
+size_t 
+wcsspn(const wchar_t *cs, const wchar_t *ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+#ifdef i386
+
+wchar_t * 
+wcsstr(const wchar_t *cs, const wchar_t *ct)
+{
+  register wchar_t * __res;
+
+  __asm__ __volatile__(
+    "cld\n\t" \
+    "movl %4,%%edi\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "notl %%ecx\n\t"
+    "decl %%ecx\n\t"	/* NOTE! This also sets Z if searchstring='' */
+    "movl %%ecx,%%edx\n"
+    "1:\tmovl %4,%%edi\n\t"
+    "movl %%esi,%%eax\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repe\n\t"
+    "cmpsw\n\t"
+    "je 2f\n\t"		/* also works for empty string, see above */
+    "xchgl %%eax,%%esi\n\t"
+    "incl %%esi\n\t"
+    "cmpw $0,-1(%%eax)\n\t"
+    "jne 1b\n\t"
+    "xorl %%eax,%%eax\n\t"
+    "2:"
+    : "=a" (__res)
+    : "0" (0), "c" (0xffffffff), "S" (cs), "g" (ct)
+    : "ecx", "edx", "edi", "esi");
+
+  return __res;
+}
+
+#else
+
+wchar_t * 
+wcsstr(const wchar_t *cs, const wchar_t *ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+unsigned long wstrlen(PWSTR s)
+{
+    return wcslen(s);
+}
+
+#ifdef i386
+
+wchar_t * 
+wcstok(wchar_t * s,const wchar_t * ct)
+{
+  register wchar_t * __res;
+
+  __asm__ __volatile__(
+    "testl %1,%1\n\t"
+    "jne 1f\n\t"
+    "testl %0,%0\n\t"
+    "je 8f\n\t"
+    "movl %0,%1\n"
+    "1:\txorl %0,%0\n\t"
+    "movl $-1,%%ecx\n\t"
+    "xorl %%eax,%%eax\n\t"
+    "cld\n\t"
+    "movl %4,%%edi\n\t"
+    "repnz\n\t"
+    "scasw\n\t"
+    "notl %%ecx\n\t"
+    "decl %%ecx\n\t"
+    "decl %%ecx\n\t"
+    "je 7f\n\t"			/* empty delimiter-string */
+    "movl %%ecx,%%edx\n"
+    "2:\tlodsw\n\t"
+    "testw %%eax,%%eax\n\t"
+    "je 7f\n\t"
+    "movl %4,%%edi\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "je 2b\n\t"
+    "decl %1\n\t"
+    "decl %1\n\t"  
+    "cmpw $0,(%1)\n\t"
+    "je 7f\n\t"
+    "movl %1,%0\n"
+    "3:\tlodsw\n\t"
+    "testw %%eax,%%eax\n\t"
+    "je 5f\n\t"
+    "movl %4,%%edi\n\t"
+    "movl %%edx,%%ecx\n\t"
+    "repne\n\t"
+    "scasw\n\t"
+    "jne 3b\n\t"
+    "decl %1\n\t"
+    "decl %1\n\t"  
+    "decl %1\n\t"  
+    "decl %1\n\t" 
+    "cmpw $0,(%1)\n\t"
+    "je 5f\n\t"
+    "movw $0,(%1)\n\t"
+    "incl %1\n\t"
+    "incl %1\n\t" 
+    "jmp 6f\n"
+    "5:\txorl %1,%1\n"
+    "6:\tcmpw $0,(%0)\n\t"
+    "jne 7f\n\t"
+    "xorl %0,%0\n"
+    "7:\ttestl %0,%0\n\t"
+    "jne 8f\n\t"
+    "movl %0,%1\n"
+    "8:"
+    : "=b" (__res), "=S" (___wcstok)
+    : "0" (___wcstok), "1" (s), "g" (ct)
+    : "eax", "ecx", "edx", "edi", "memory");
+
+  return __res;
+}
+
+#else
+
+wchar_t * 
+wcstok(wchar_t * s,const wchar_t * ct)
+{
+UNIMPLEMENTED;
+}
+
+#endif
+
+
