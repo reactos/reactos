937e125ad7762d936db62a28866882ba69b7dc71
David Welch <welch@cwcom.net>
1998-12-19 18:44:54 +0000

    Improved shell and removed symbol list
    svn path=/trunk/; revision=123

--- reactos/apps/utils/shell/shell.c
+++ reactos/apps/utils/shell/shell.c
@@ -3,6 +3,7 @@
 
 #include <ddk/ntddk.h>
 #include <stdarg.h>
+#include <string.h>
 
 void debug_printf(char* fmt, ...)
 {
@@ -15,39 +16,161 @@ void debug_printf(char* fmt, ...)
    va_end(args);
 }
 
-void main()
-{
-   KEY_EVENT_RECORD KeyEvent[2];
-   HANDLE FileHandle;
-   DWORD Result;
-   HANDLE DefaultHeap;
-   PVOID Buffer;
+HANDLE KeyboardHandle;
 
-   NtDisplayString("Simple Shell Starting...\n");
+void ExecuteCd(char* cmdline)
+{
+   UCHAR Buffer[255];
    
-//   DefaultHeap = HeapCreate(0,1024*1024,1024*1024);
-//   Buffer = HeapAlloc(DefaultHeap,0,1024);
+   debug_printf("ExecuteCd(cmdline %s)\n",cmdline);
    
-   FileHandle = CreateFile("\\Device\\Keyboard",
+   if (cmdline[0] != '\\' &&
+       cmdline[1] != ':')
+     {
+	GetCurrentDirectoryA(255,Buffer);
+     }
+   else
+     {
+	Buffer[0] = 0;
+     }
+   debug_printf("Buffer %s\n",Buffer);
+   
+   lstrcatA(Buffer,cmdline);
+   
+   debug_printf("Buffer %s\n",Buffer);
+   
+   if (Buffer[lstrlenA(Buffer)-1] != '\\')
+     {
+	lstrcatA(Buffer,"\\");
+     }
+   debug_printf("Buffer %s\n",Buffer);
+   
+   SetCurrentDirectoryA(Buffer);
+}
+
+void ExecuteDir(char* cmdline)
+{
+}
+
+void ExecuteType(char* cmdline)
+{
+   HANDLE FileHandle;
+   char c;
+   DWORD Result;
+   
+   FileHandle = CreateFile(cmdline,
 			   FILE_GENERIC_READ,
 			   0,
 			   NULL,
 			   OPEN_EXISTING,
 			   0,
 			   NULL);
-   
-
-   debug_printf("C:\\");
-   for(;;)
+   while (ReadFile(FileHandle,
+		   &c,
+		   1,
+		   &Result,
+		   NULL))
      {
-	ReadFile(FileHandle,
+	debug_printf("%c",c);
+	c = 0;
+     }
+}
+
+void ExecuteCommand(char* line)
+{
+   char* cmd;
+   char* tail;
+   
+   if (line[1] == ':' && line[2] == 0)
+     {
+	line[2] = '\\';
+	line[3] = 0;
+	SetCurrentDirectoryA(line);
+	return;
+     }
+   
+   tail = line;
+   while ((*tail)!=' ' && (*tail)!=0)
+     {
+	tail++;	
+     }
+   if ((*tail)==' ')
+     {
+	*tail = 0;
+	tail++;
+     }
+   cmd = line;
+   
+   debug_printf("cmd '%s' tail '%s'\n",cmd,tail);
+   
+   if (cmd==NULL)
+     {
+	return;
+     }
+   if (strcmp(cmd,"cd")==0)
+     {
+	ExecuteCd(tail);
+	return;
+     }
+   if (strcmp(cmd,"dir")==0)
+     {
+	ExecuteDir(tail);
+	return;
+     }
+   if (strcmp(cmd,"type")==0)
+     {
+	ExecuteType(tail);	
+	return;
+     }
+   debug_printf("Unknown command\n");
+}
+
+void ReadLine(char* line)
+{
+   KEY_EVENT_RECORD KeyEvent;
+   DWORD Result;
+   UCHAR CurrentDir[255];
+   
+   GetCurrentDirectoryA(255,CurrentDir);
+   debug_printf(CurrentDir);
+   
+   do
+     {
+	ReadFile(KeyboardHandle,
 		 &KeyEvent,
 		 sizeof(KEY_EVENT_RECORD),
 		 &Result,
 		 NULL);
-	if (KeyEvent[0].bKeyDown)
+	if (KeyEvent.bKeyDown && KeyEvent.AsciiChar != 0)
 	  {
-	     debug_printf("%c",KeyEvent[0].AsciiChar);
+	     debug_printf("%c",KeyEvent.AsciiChar);
+	     *line = KeyEvent.AsciiChar;
+	     line++;
 	  }
+     } while (!(KeyEvent.bKeyDown && KeyEvent.AsciiChar == '\n'));
+   line--;
+   *line = 0;
+}
+
+void main()
+{
+   static char line[255];
+   
+   NtDisplayString("Shell Starting...\n");
+      
+   KeyboardHandle = CreateFile("Keyboard",
+			       FILE_GENERIC_READ,
+			       0,
+			       NULL,
+			       OPEN_EXISTING,
+			       0,
+			       NULL);
+   
+   SetCurrentDirectoryA("C:\\");
+   
+   for(;;)
+     {
+	ReadLine(line);
+	ExecuteCommand(line);
      }
 }
