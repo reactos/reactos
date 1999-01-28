a984daa5de94e5e6c0d1d9f7b8a2defbfff9e45d
David Welch <welch@cwcom.net>
1999-01-28 21:42:01 +0000

    no message
    svn path=/trunk/; revision=195

---
+++ reactos/include/internal/id.h
@@ -0,0 +1,11 @@
+/*
+ * Structure ids
+ */
+
+
+#define BASE_OBJECT_ID      (0x12345678)
+#define FILE_OBJECT_ID      (BASE_OBJECT_ID + 1)
+#define DEVICE_OBJECT_ID    (BASE_OBJECT_ID + 2)
+#define DRIVER_OBJECT_ID    (BASE_OBJECT_ID + 3)
+#define DIRECTORY_OBJECT_ID (BASE_OBJECT_ID + 4)
+#define CC1_CCB_ID          (BASE_OBJECT_ID + 5)
