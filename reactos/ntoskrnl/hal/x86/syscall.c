88987510dfd427e070c45bcf606a09ae2415e90d
David Welch <welch@cwcom.net>
1999-02-01 20:52:08 +0000

    Delete this (empty file)
    svn path=/trunk/; revision=198

--- reactos/ntoskrnl/hal/x86/syscall.c
+++
@@ -1,9 +0,0 @@
-/*
- * COPYRIGHT:            See COPYRIGHT in the top level source
- * PROJECT:              ReactOS kernel
- * FILE:                 kernel/hal/x86/syscall.c
- * PURPOSE:              Handles calls from user mode
- * PROGRAMMER:           David Welch (welch@mcmail.com)
- * REVISION HISTORY:
- *          21/07/98: Created
- */ 
