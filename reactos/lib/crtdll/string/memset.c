55d6c152b6da757d8f42b6de311f24937d183489
Rex Jolliff <rex@lvcablemodem.com>
1999-02-05 19:36:21 +0000

    more changes from Boudewijn
    svn path=/trunk/; revision=209

--- reactos/lib/crtdll/string/memset.c
+++ reactos/lib/crtdll/string/memset.c
@@ -2,9 +2,11 @@ typedef int size_t;
 
 void * memset(void *src,int val,size_t count)
 {
-	int *int_src = src;
+	char *char_src = src;
+
 	while(count>0) {
-		*int_src = val;
+		*char_src = val;
+		char_src++;
 		count--;
 	}
 	return src;
