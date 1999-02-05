3aed7d1da41ea08c50fb0a853620457b71eb8426
Rex Jolliff <rex@lvcablemodem.com>
1999-02-05 19:35:56 +0000

    incorporating changes from Boudewijn
    svn path=/trunk/; revision=208

--- reactos/lib/crtdll/string/memmove.c
+++ reactos/lib/crtdll/string/memmove.c
@@ -3,14 +3,14 @@
 
 void * memmove(void *dest,const void *src,size_t count)
 {
-	int *int_dest = dest;
-	int *int_src = src;
+	char *char_dest = dest;
+	char *char_src = src;
 
 	while(count > 0 )
 	{
-		*int_dest = *int_src;
-		int_dest++;
-		int_src++;
+		*char_dest = *char_src;
+		char_dest++;
+		char_src++;
 		count--;
 	}
 
