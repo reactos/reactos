d9625dff78861d6c50dbdd3e7d09568dd787b61d
Boudewijn Dekker <ariadne@xs4all.nl>
1999-02-25 21:02:20 +0000

    implementation of debug assert
    svn path=/trunk/; revision=266

---
+++ reactos/lib/crtdll/assert/assert.c
@@ -0,0 +1,13 @@
+/* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void _assert(const char *msg, const char *file, int line)
+{
+  /* Assertion failed at foo.c line 45: x<y */
+  fprintf(stderr, "Assertion failed at %s line %d: %s\n", file, line, msg);
+  raise(SIGABRT);
+  exit(1);
+}
