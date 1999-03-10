24160311c0ff05df61efc071d5d3d96de250f01d
Rex Jolliff <rex@lvcablemodem.com>
1999-03-10 07:18:38 +0000

    more changes to compile under mingw32
    svn path=/trunk/; revision=287

--- reactos/lib/ntdll/main/dllmain.c
+++ reactos/lib/ntdll/main/dllmain.c
@@ -1,5 +1,9 @@
 
-int DllMainCRTStartup(void)
+#include <windows.h>
+
+/* FIXME: Should this function be called DllEntryPoint?  */
+
+BOOL WINAPI STDCALL DllMainCRTStartup(HINSTANCE hDLLInst, DWORD fdwReason, LPVOID lpvReserved)
 {
   return 0;
 }
