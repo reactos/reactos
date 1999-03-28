dff0889c022dfd0fe485b1cdf6d3de5228c64bd3
Rex Jolliff <rex@lvcablemodem.com>
1999-03-28 23:12:42 +0000

    added GDI32 DLL skeleton
    svn path=/trunk/; revision=347

---
+++ reactos/lib/gdi32/main/dllmain.c
@@ -0,0 +1,16 @@
+/*
+ * dllmain.c
+ *
+ * $Revision: 1.1 $
+ * $Author: rex $
+ * $Date: 1999/03/28 23:12:42 $
+ *
+ */
+
+#include <windows.h>
+
+BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+	return TRUE;
+}
+
