3fe1c6d5f4d61a6e0f5a3985f5189bea39f3ac96
Emanuele Aliberti <ea@iol.it>
1999-04-23 05:26:37 +0000

    *.rc include
    svn path=/trunk/; revision=405

---
+++ reactos/include/reactos/resource.h
@@ -0,0 +1,15 @@
+#ifndef _INC_REACTOS_RESOURCE_H
+#define _INC_REACTOS_RESOURCE_H
+
+/* Build number as YYYYMMDD */
+
+#define RES_UINT_FILE_VERSION	19990421
+
+/* Common version strings for rc scripts */
+
+#define RES_STR_COMPANY_NAME	"ReactOS Development Team\0"
+#define RES_STR_LEGAL_COPYRIGHT	"Copyright (c) 1998, 1999 ReactOS Team\0"
+#define RES_STR_PRODUCT_NAME	"ReactOS Operating System\0"
+#define RES_STR_PRODUCT_VERSION	"post 0.0.13\0"
+
+#endif /* ndef _INC_REACTOS_RESOURCE_H */
