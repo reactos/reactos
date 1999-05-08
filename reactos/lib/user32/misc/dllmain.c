3cee782a2323c89de6db3714f69ac23cff3671e6
Emanuele Aliberti <ea@iol.it>
1999-05-08 07:09:31 +0000

    user32.dll minimal implementation
    svn path=/trunk/; revision=435

---
+++ reactos/lib/user32/misc/dllmain.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+INT
+STDCALL
+DllMain(
+	PVOID	hinstDll,
+	ULONG	dwReason,
+	PVOID	reserved
+	)
+{
+	switch (dwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+			break;
+		case DLL_THREAD_ATTACH:
+			break;
+		case DLL_THREAD_DETACH:
+			break;
+		case DLL_PROCESS_DETACH:
+			break;
+	}
+	return(1);
+}
+
