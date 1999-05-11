7e62c34fdd04f36c4dde38ea0d76ea23378129a2
Emanuele Aliberti <ea@iol.it>
1999-05-11 21:19:41 +0000

    File Manage{r|ment} IFS Utility Do-nothing implementation for coming ReactOS sysutils.
    svn path=/trunk/; revision=458

---
+++ reactos/lib/fmifs/extend.c
@@ -0,0 +1,25 @@
+/* $Id: extend.c,v 1.1 1999/05/11 21:19:41 ea Exp $
+ *
+ * COPYING:	See the top level directory
+ * PROJECT:	ReactOS 
+ * FILE:	reactos/lib/fmifs/extend.c
+ * DESCRIPTION:	File management IFS utility functions
+ * PROGRAMMER:	Emanuele Aliberti
+ * UPDATED
+ * 	1999-02-16 (Emanuele Aliberti)
+ * 		Entry points added.
+ */
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+#include <fmifs.h>
+
+/* FMIFS.5 */
+VOID
+__stdcall
+Extend(VOID)
+{
+}
+
+
+/* EOF */
---
+++ reactos/lib/fmifs/media.c
@@ -0,0 +1,26 @@
+/* $Id: media.c,v 1.1 1999/05/11 21:19:41 ea Exp $
+ *
+ * COPYING:	See the top level directory
+ * PROJECT:	ReactOS 
+ * FILE:	reactos/lib/fmifs/media.c
+ * DESCRIPTION:	fmifs.dll
+ * PROGRAMMER:	Emanuele Aliberti
+ * UPDATED
+ * 	1999-02-16 (Emanuele Aliberti)
+ * 		Entry points added.
+ */
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+#include <fmifs.h>
+
+
+/* FMIFS.9 */
+VOID
+__stdcall
+QuerySupportedMedia(VOID)
+{
+}
+
+
+/* EOF */
