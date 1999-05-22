c8055376408d4ece30b81aa8e7f780167f62a996
Rex Jolliff <rex@lvcablemodem.com>
1999-05-22 23:55:56 +0000

    added stubs and build spec for win32k.sys
    svn path=/trunk/; revision=507

--- reactos/subsys/win32k/main/dllmain.c
+++ reactos/subsys/win32k/main/dllmain.c
@@ -1,13 +1,26 @@
+/*
+ *  Entry Point for win32k.sys
+ */
 
-DllMain()
+#include <windows.h>
+#include <ddk/ntddk.h>
+
+WINBOOL STDCALL DllMain(HANDLE hInst, 
+			ULONG ul_reason_for_call,
+			LPVOID lpReserved)
 {
-  /* %%% handle GDI initialization here  */
+  DbgPrint("win32k:DllMain(hInst %x, ul_reason_for_call %d)\n",
+           hInst, 
+           ul_reason_for_call);
+
+  return TRUE;  
 }
 
+#if 0
 HDC GDICreateDC(LPCWSTR Driver,
                 LPCWSTR Device,
                 CONST PDEVMODE InitData)
 {
   /* %%% initialize device driver here on first call for display DC.  */
 }
-
+#endif
