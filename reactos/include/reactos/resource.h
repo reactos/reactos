c5e296fcf096a0f5b9e2e5a1eefb7e054a4b932e
Emanuele Aliberti <ea@iol.it>
1999-06-08 22:37:30 +0000

    Updated to 0.0.14 RES_STR_FILE_VERSION eq RES_STR_PRODUCT_VERSION
    svn path=/trunk/; revision=544

--- reactos/include/reactos/resource.h
+++ reactos/include/reactos/resource.h
@@ -5,25 +5,26 @@
 
 #define RES_UINT_FV_MAJOR	0
 #define RES_UINT_FV_MINOR	0
-#define RES_UINT_FV_REVISION	13
+#define RES_UINT_FV_REVISION	14
 /* Build number as YYYYMMDD */
-#define RES_UINT_FV_BUILD	19990516
-/* TO be removed */
-#define RES_UINT_FILE_VERSION	RES_UINT_FV_BUILD
+#define RES_UINT_FV_BUILD	19990608
 
 /* ReactOS Product Version UINTs */
 
 #define RES_UINT_PV_MAJOR	0
 #define RES_UINT_PV_MINOR	0
-#define RES_UINT_PV_REVISION	13
+#define RES_UINT_PV_REVISION	14
 /* Build number as YYYYMMDD */
-#define RES_UINT_PV_BUILD	19990516
+#define RES_UINT_PV_BUILD	19990608
 
 /* Common version strings for rc scripts */
 
 #define RES_STR_COMPANY_NAME	"ReactOS Development Team\0"
 #define RES_STR_LEGAL_COPYRIGHT	"Copyright (c) 1998, 1999 ReactOS Team\0"
 #define RES_STR_PRODUCT_NAME	"ReactOS Operating System\0"
-#define RES_STR_PRODUCT_VERSION	"post 0.0.13\0"
+#define RES_STR_PRODUCT_VERSION	"post 0.0.14\0"
+
+/* FILE_VERSION defaults to PRODUCT_VERSION */
+#define RES_STR_FILE_VERSION	RES_STR_PRODUCT_VERSION
 
 #endif /* ndef _INC_REACTOS_RESOURCE_H */
