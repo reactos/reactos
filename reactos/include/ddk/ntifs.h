fad2f584e99230f9b032bbfcd2ef189963a0c483
Emanuele Aliberti <ea@iol.it>
1999-07-04 22:22:05 +0000

    Bo's ntifs.h + ReactOS extensions (untested).
    svn path=/trunk/; revision=582

--- reactos/include/ddk/ntifs.h
+++ reactos/include/ddk/ntifs.h
@@ -1621,4 +1621,71 @@ ZwUnlockFile (
 }
 #endif
 
+/* --- BEGIN - REACTOS ONLY --- */
+
+typedef struct _BCB
+{
+	LIST_ENTRY	CacheSegmentListHead;
+	PFILE_OBJECT	FileObject;
+	KSPIN_LOCK	BcbLock;
+	
+} BCB, *PBCB;
+
+#define CACHE_SEGMENT_SIZE (0x1000)
+
+struct _MEMORY_AREA;
+
+typedef struct _CACHE_SEGMENT
+{
+	PVOID			BaseAddress;
+	struct _MEMORY_AREA	* MemoryArea;
+	BOOLEAN			Valid;
+	LIST_ENTRY		ListEntry;
+	ULONG			FileOffset;
+	KEVENT			Lock;
+	ULONG			ReferenceCount;
+	PBCB			Bcb;
+	
+} CACHE_SEGMENT, *PCACHE_SEGMENT;
+
+
+NTSTATUS
+CcFlushCachePage (
+	PCACHE_SEGMENT	CacheSeg
+	);
+
+NTSTATUS
+CcReleaseCachePage (
+	PBCB		Bcb,
+	PCACHE_SEGMENT	CacheSeg,
+	BOOLEAN		Valid
+	);
+
+NTSTATUS
+CcRequestCachePage (
+	PBCB		Bcb,
+	ULONG		FileOffset,
+	PVOID		* BaseAddress,
+	PBOOLEAN	UptoDate,
+	PCACHE_SEGMENT	* CacheSeg
+	);
+
+NTSTATUS
+CcInitializeFileCache (
+	PFILE_OBJECT	FileObject,
+	PBCB		* Bcb
+	);
+
+NTSTATUS
+CcReleaseFileCache (
+	PFILE_OBJECT	FileObject,
+	PBCB		Bcb
+	);
+
+#include <ddk/cctypes.h>
+
+#include <ddk/ccfuncs.h>
+
+/* --- END - REACTOS ONLY --- */
+
 #endif // _NTIFS_
