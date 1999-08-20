1f636015bae2b5af9dca3baf5f833997b70a62e1
Emanuele Aliberti <ea@iol.it>
1999-08-20 16:30:16 +0000

    NLS stubs and exported data.
    svn path=/trunk/; revision=622

---
+++ reactos/ntoskrnl/nls/nls.c
@@ -0,0 +1,19 @@
+/* $Id: nls.c,v 1.1 1999/08/20 16:30:16 ea Exp $
+ *
+ * reactos/ntoskrnl/nls/nls.c
+ */
+#include <ntos.h>
+
+BYTE
+NlsLeadByteInfo = 0; /* ? */
+
+USHORT
+NlsMbCodePageTag = 0;
+
+USHORT
+NlsOemLeadByteInfo = 0;
+
+USHORT
+NlsAnsiCodePage = 0;
+
+/* EOF */
