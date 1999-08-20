e9703bfa9807174eb6d5ee83a4e86477e6f1e9c3
Emanuele Aliberti <ea@iol.it>
1999-08-20 16:29:22 +0000

    FS stubs.
    svn path=/trunk/; revision=621

---
+++ reactos/ntoskrnl/fs/mcb.c
@@ -0,0 +1,259 @@
+/* $Id: mcb.c,v 1.1 1999/08/20 16:29:22 ea Exp $
+ *
+ * reactos/ntoskrnl/fs/mcb.c
+ *
+ */
+#include <ntos.h>
+
+#include <internal/debug.h>
+
+/**********************************************************************
+ * NAME							EXPORTED
+ *
+ * DESCRIPTION
+ *	
+ * ARGUMENTS
+ *
+ * RETURN VALUE
+ *
+ * NOTES
+ */
+VOID
+STDCALL
+FsRtlAddLargeMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4,
+	DWORD	Unknown5,
+	DWORD	Unknown6
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlAddMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlGetNextLargeMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlGetNextMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlInitializeLargeMcb (
+	DWORD	Unknown0,
+	DWORD	Unknown1
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlInitializeMcb (
+	DWORD	Unknown0,
+	DWORD	Unknown1
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlLookupLargeMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4,
+	DWORD	Unknown5,
+	DWORD	Unknown6,
+	DWORD	Unknown7
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlLookupLastLargeMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlLookupLastMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlLookupMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlNumberOfRunsInLargeMcb (
+	DWORD	Unknown0
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlNumberOfRunsInMcb (
+	DWORD	Unknown0
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlRemoveLargeMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlRemoveMcbEntry (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlSplitLargeMcb (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2,
+	DWORD	Unknown3,
+	DWORD	Unknown4
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlTruncateLargeMcb (
+	DWORD	Unknown0,
+	DWORD	Unknown1,
+	DWORD	Unknown2
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlTruncateMcb (
+	DWORD	Unknown0,
+	DWORD	Unknown1
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+VOID
+STDCALL
+FsRtlUninitializeLargeMcb (
+	DWORD	Unknown0
+	)
+{
+	UNIMPLEMENTED;
+}
+
+
+VOID
+STDCALL
+FsRtlUninitializeMcb (
+	DWORD	Unknown0
+	)
+{
+	UNIMPLEMENTED
+}
+
+
+/* EOF */
+
