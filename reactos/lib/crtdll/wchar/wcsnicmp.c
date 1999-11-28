e2e5efb9d7f1e158cd629059f1ef4e714cd9905f
Eric Kohl <eric.kohl@reactos.org>
1999-11-28 01:55:41 +0000

    fixed a typo
    svn path=/trunk/; revision=804

--- reactos/lib/crtdll/wchar/wcsnicmp.c
+++ reactos/lib/crtdll/wchar/wcsnicmp.c
@@ -2,7 +2,7 @@
 
 int _wcsnicmp(const wchar_t * cs,const wchar_t * ct,size_t count)
 {
-  wchar_t *save = (char *)cs;
+  wchar_t *save = (wchar_t *)cs;
   while (towlower(*cs) == towlower(*ct) && (int)(cs - save) < count)
   {
     if (*cs == 0)
@@ -11,5 +11,4 @@ int _wcsnicmp(const wchar_t * cs,const wchar_t * ct,size_t count)
     ct++;
   }
   return towlower(*cs) - towlower(*ct);
-	
 }
