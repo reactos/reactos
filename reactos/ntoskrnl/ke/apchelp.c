c40b27bbc48b556524917501baced2abe6087f12
David Welch <welch@cwcom.net>
1999-12-13 23:07:49 +0000

    Forgot to remove obsolete file
    svn path=/trunk/; revision=869

--- reactos/ntoskrnl/ke/apchelp.c
+++
@@ -1,9 +0,0 @@
-
-void KeApcProlog(void);
-   __asm__("\n\t.global _KeApcProlog\n\t"
-      "_KeApcProlog:\n\t"
-      "pusha\n\t"
-      "call _KeApcProlog2\n\t"
-      "popa\n\t"
-      "iret\n\t");
-
