f28a023c99c144b7e47954323f18c3df05294242
Eric Kohl <eric.kohl@reactos.org>
2000-02-19 17:18:34 +0000

    Fixed a bug - memmove() couldn't handle overlapping buffers
    svn path=/trunk/; revision=990

--- reactos/lib/ntdll/string/memmove.c
+++ reactos/lib/ntdll/string/memmove.c
@@ -1,18 +1,36 @@
 #include <string.h>
 
-
 void * memmove(void *dest,const void *src,size_t count)
 {
 	char *char_dest = (char *)dest;
 	char *char_src = (char *)src;
 
-	while(count > 0 )
+	if ((char_dest <= char_src) || (char_dest >= (char_src+count)))
 	{
-		*char_dest = *char_src;
-		char_dest++;
-		char_src++;
-		count--;
+		/*  non-overlapping buffers */
+		while(count > 0)
+		{
+			*char_dest = *char_src;
+			char_dest++;
+			char_src++;
+			count--;
+		}
+	}
+	else
+	{
+		/* overlaping buffers */
+		char_dest = (char *)dest + count - 1;
+		char_src = (char *)src + count - 1;
+
+		while(count > 0)
+		{
+			*char_dest = *char_src;
+			char_dest--;
+			char_src--;
+			count--;
+		}
 	}
 
 	return dest;
 }
+
