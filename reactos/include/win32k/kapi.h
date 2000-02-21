151f2f646e6c12db4551ff2fc51b7ed316fa37cb
Emanuele Aliberti <ea@iol.it>
2000-02-21 00:50:17 +0000

    WIN32K Kernel API main include file (replaces win32k/win32k.h).
    svn path=/trunk/; revision=994

---
+++ reactos/include/win32k/kapi.h
@@ -0,0 +1,21 @@
+#ifndef _WIN32K_KAPI_H
+#define _WIN32K_KAPI_H
+#include <win32k/bitmaps.h>
+#include <win32k/brush.h>
+#include <win32k/cliprgn.h>
+#include <win32k/color.h>
+#include <win32k/coord.h>
+#include <win32k/dc.h>
+#include <win32k/debug.h>
+#include <win32k/fillshap.h>
+#include <win32k/icm.h>
+#include <win32k/line.h>
+#include <win32k/metafile.h>
+#include <win32k/paint.h>
+#include <win32k/path.h>
+#include <win32k/pen.h>
+#include <win32k/print.h>
+#include <win32k/region.h>
+#include <win32k/text.h>
+#include <win32k/wingl.h>
+#endif /* ndef _WIN32K_KAPI_H */
