fbfce3bc03ceff96425c46d76ac48b43d0886d4a
David Welch <welch@cwcom.net>
2000-03-13 23:16:46 +0000

    Updated install script
    svn path=/trunk/; revision=1055

--- reactos/install.sh
+++ reactos/install.sh
@@ -2,14 +2,7 @@ mkdir -p $1/reactos
 mkdir -p $1/reactos/system32
 mkdir -p $1/reactos/system32/drivers
 mkdir -p $1/reactos/bin
-cp loaders/dos/loadros.com $1
-cp ntoskrnl/ntoskrnl.exe $1
-cp services/fs/vfat/vfatfs.sys $1
-cp services/dd/ide/ide.sys $1
-cp services/dd/keyboard/keyboard.sys $1/reactos/system32/drivers
-cp services/dd/blue/blue.sys $1/reactos/system32/drivers
-cp services/dd/vga/vgamp.sys $1/reactos/system32/drivers
-cp services/dd/vidport/vidport.sys $1/reactos/system32/drivers
+./install-system.sh $1
 cp apps/shell/shell.exe $1/reactos/system32
 cp lib/ntdll/ntdll.dll $1/reactos/system32
 cp lib/kernel32/kernel32.dll $1/reactos/system32
