1ba7343b1c4ec249d8f5f85b925fd76424fcd5b4
Jason Filby <jason.filby@gmail.com>
2000-03-17 22:14:31 +0000

    Additional modifications
    svn path=/trunk/; revision=1072

--- reactos/install-system.sh
+++ reactos/install-system.sh
@@ -8,5 +8,6 @@ cp services/fs/vfat/vfatfs.sys $1
 cp services/dd/ide/ide.sys $1
 cp services/dd/keyboard/keyboard.sys $1/reactos/system32/drivers
 cp services/dd/blue/blue.sys $1/reactos/system32/drivers
-cp services/dd/vga/vgamp.sys $1/reactos/system32/drivers
+cp services/dd/vga/miniport/vgamp.sys $1/reactos/system32/drivers
+cp services/dd/vga/display/vgaddi.dll $1/reactos/system32/drivers
 cp services/dd/vidport/vidport.sys $1/reactos/system32/drivers
