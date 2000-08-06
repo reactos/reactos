011b958ceda7ee1088334d82fa850f7bd6209154
Jason Filby <jason.filby@gmail.com>
2000-08-06 12:34:24 +0000

    new rev
    svn path=/branches/prep0016/; revision=1287

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.3 2000/01/19 07:05:04 rex Exp $
+/* $Id: version.h,v 1.3.2.1 2000/08/06 12:34:24 jfilby Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -16,11 +16,11 @@
 
 #define KERNEL_VERSION_MAJOR    	0
 #define KERNEL_VERSION_MINOR    	0
-#define KERNEL_VERSION_PATCH_LEVEL      15
+#define KERNEL_VERSION_PATCH_LEVEL      16
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
-#define KERNEL_RELEASE_DATE		20000118L
+#define KERNEL_RELEASE_DATE		20000804L
 
-#define KERNEL_VERSION                  "0.0.15"
+#define KERNEL_VERSION                  "0.0.16"
 
 #endif
 
