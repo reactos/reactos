4a5bc9edc8984e15458e513ed796d2eab2a0ea19
Eric Kohl <eric.kohl@reactos.org>
2000-10-06 22:54:41 +0000

    Minor fix
    svn path=/trunk/; revision=1383

--- reactos/ntoskrnl/ex/locale.c
+++ reactos/ntoskrnl/ex/locale.c
@@ -1,4 +1,5 @@
-/*
+/* $Id: locale.c,v 1.3 2000/10/06 22:54:41 ekohl Exp $
+ *
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS kernel
  * FILE:            ntoskrnl/ex/locale.c
@@ -17,12 +18,18 @@
 
 /* FUNCTIONS *****************************************************************/
 
-NTSTATUS STDCALL NtQueryDefaultLocale(VOID)
+NTSTATUS STDCALL
+NtQueryDefaultLocale(IN BOOLEAN UserProfile,
+		     OUT PLCID DefaultLocaleId)
 {
    UNIMPLEMENTED;
 }
 
-NTSTATUS STDCALL NtSetDefaultLocale(VOID)
+NTSTATUS STDCALL
+NtSetDefaultLocale(IN BOOLEAN UserProfile,
+		   IN LCID DefaultLocaleId)
 {
    UNIMPLEMENTED;
 }
+
+/* EOF */
