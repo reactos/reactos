1db1c12cf33bd24295c190afaacdeac48fcaef17
Eric Kohl <eric.kohl@reactos.org>
2000-10-22 02:01:42 +0000

    Fixed prototype
    svn path=/trunk/; revision=1413

--- reactos/apps/tests/gditest/gditest.c
+++ reactos/apps/tests/gditest/gditest.c
@@ -4,7 +4,7 @@
 
 #include <windows.h>
 
-extern VOID STDCALL GdiDllInitialize(PVOID hInstance, ULONG Event, PVOID Reserved);
+extern BOOL STDCALL GdiDllInitialize(PVOID hInstance, ULONG Event, PVOID Reserved);
 
 int main (void)
 {
