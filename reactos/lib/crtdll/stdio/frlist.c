5786d70e398461257f711ac236e562ae68f515ab
Eric Kohl <eric.kohl@reactos.org>
2001-01-27 20:56:44 +0000

    Fixed _iob bug
    svn path=/trunk/; revision=1574

--- reactos/lib/crtdll/stdio/frlist.c
+++ reactos/lib/crtdll/stdio/frlist.c
@@ -3,13 +3,11 @@
 //#include <crtdll/local.h>
 #include <crtdll/internal/file.h>
 
-extern FILE _crtdll_iob[5];
 
 static __file_rec __initial_file_rec = {
   0,
   5,
-{ &_crtdll_iob[0], &_crtdll_iob[1], &_crtdll_iob[2], 
-   &_crtdll_iob[3], &_crtdll_iob[4] }
+  { &_iob[0], &_iob[1], &_iob[2], &_iob[3], &_iob[4] }
 };
 
 __file_rec *__file_rec_list = &__initial_file_rec;
