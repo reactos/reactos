33267a7bd2a3dd03c522e62cc6c23761724a5491
David Welch <welch@cwcom.net>
2001-04-10 22:13:23 +0000

    More sensible dependencies scheme for configuration
    svn path=/trunk/; revision=1785

--- reactos/ntoskrnl/include/internal/config.h
+++ reactos/ntoskrnl/include/internal/config.h
@@ -1,4 +1,7 @@
-/* Automatically generated, don't edit */
+/* Automatically generated, Edit the Makefile to change configuration */
+#ifndef __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H
+#define __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H
 #define DBG
-#define KDBG
 #define UP
+#define CONFIG "DBG UP"
+#endif /* __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H */
