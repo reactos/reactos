33408fe398b5d2ebcff25970710f84a8a86155fa
David Welch <welch@cwcom.net>
2001-04-12 00:56:04 +0000

    Reduced the size of the kernel disk image by moving the initial page tables and page directory to the bss and initializing them at runtime.
    svn path=/trunk/; revision=1788

--- reactos/ntoskrnl/dbg/kdb.c
+++ reactos/ntoskrnl/dbg/kdb.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: kdb.c,v 1.1 2001/04/10 17:48:16 dwelch Exp $
+/* $Id: kdb.c,v 1.2 2001/04/12 00:56:04 dwelch Exp $
  *
  * PROJECT:         ReactOS kernel
  * FILE:            ntoskrnl/dbg/kdb.c
@@ -34,6 +34,21 @@
 
 #define NDEBUG
 #include <internal/debug.h>
+
+/* TYPES *********************************************************************/
+
+/* GLOBALS *******************************************************************/
+
+struct
+{
+  PCH Name;
+  ULONG (*Fn)(PKTRAP_FRAME Tf, PCH Args);
+} DebuggerCommands[] = {
+  {"cont", DbgContCommand},
+  {"regs", DbgRegsCommand},
+  {"bugcheck", DbgBugCheckCommand},
+  {NULL, NULL}
+};
 
 /* FUNCTIONS *****************************************************************/
 
@@ -61,35 +76,79 @@ KdbGetCommand(PCH Buffer)
 }
 
 ULONG
-KdbMainLoop(VOID)
+DbgContCommnad(PCH Args, PKTRAP_FRAME Tf)
+{
+  /* Not too difficult. */
+  return(0);
+}
+
+ULONG
+DbgRegsCommand(PCH Args, PKTRAP_FRAME Tf)
+{
+  DbgPrint("CS:EIP %.4x:%.8x, EAX %.8x EBX %.8x ECX %.8x EDX %.8x\n",
+	   Tf->Cs, Tf->Eip, Tf->Eax, Tf->Ebx, Tf->Ecx, Tf->Edx);
+  DbgPrint("ESI %.8x EDI %.8x EBP %.8x SS:ESP %.4x:%.8x\n",
+	   Tf->Esi, Tf->Edi, Tf->Ebp, Tf->Ss, Tf->Esp);
+  return(1);
+}
+
+ULONG
+DbgBugCheckCommand(PCH Args, PKTRAP_FRAME Tf)
+{
+  KeBugCheck(1);
+  return(1);
+}
+
+VOID
+KdbDoCommand(PCH CommandLine, PKTRAP_FRAME Tf)
+{
+  ULONG i;
+  PCH Next;
+  PCH s;
+
+  s = strpbrk(CommandLine, "\t ");
+  if (s != NULL)
+    {
+      *s = 0;
+      Next = s + 1;
+    }
+  else
+    {
+      Next = NULL;
+    }
+
+  for (i = 0; DebuggerCommands[i].Name != NULL; i++)
+    {
+      if (strcmp(DebuggerCommands[i], CommandLine) == 0)
+	{
+	  return(DebuggerCommands[i](Tf, Next));
+	}
+    }
+  DbgPrint("Command '%s %s' is unknown\n", CommandLine, Next);
+  return(1);
+}
+
+VOID
+KdbMainLoop(PKTRAP_FRAME Tf)
 {
   CHAR Command[256];
+  ULONG s;
 
-  for (;;)
+  do
     {
       DbgPrint("kdb:> ");
 
       KdbGetCommand(Command);
 
-      switch (Command[0])
-	{
-	  /* Continue. */
-	case 'c':
-	  return(0);
-
-	  /* Bug check the system */
-	case 'p':
-	  KeBugCheck(0);
-	  break;	  
-	}
-    }
+      s = KdbDoCommand(Command, Tf);    
+    } while (s != 0);
 }
 
 VOID
 KdbInternalEnter(PKTRAP_FRAME Tf)
 {
   __asm__ __volatile__ ("cli\n\t");
-  (VOID)KdbMainLoop();
+  (VOID)KdbMainLoop(Tf);
   __asm__ __volatile__("sti\n\t");
 }
 
