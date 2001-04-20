6b9f511801036c0772d1a0d166d8b2f72f0d2040
David Welch <welch@cwcom.net>
2001-04-20 12:59:13 +0000

    Remove config.h
    svn path=/trunk/; revision=1813

--- reactos/ntoskrnl/include/internal/config.h
+++
@@ -1,7 +0,0 @@
-/* Automatically generated, Edit the Makefile to change configuration */
-#ifndef __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H
-#define __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H
-#define DBG
-#define UP
-#define CONFIG "DBG UP"
-#endif /* __NTOSKRNL_INCLUDE_INTERNAL_CONFIG_H */
