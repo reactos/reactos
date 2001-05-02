979adb975ed81e9781a3170c32a9f8bd671d528f
Jason Filby <jason.filby@gmail.com>
2001-05-02 12:35:48 +0000

    Update to test new GDI functions
    svn path=/trunk/; revision=1863

--- reactos/apps/tests/gditest/gditest.c
+++ reactos/apps/tests/gditest/gditest.c
@@ -9,23 +9,23 @@ extern BOOL STDCALL GdiDllInitialize(HANDLE hInstance, DWORD Event, LPVOID Reser
 int main (void)
 {
   HDC  Desktop, MyDC, DC24;
-  HPEN  RedPen, GreenPen, BluePen;
+  HPEN  RedPen, GreenPen, BluePen, WhitePen;
   HBITMAP  MyBitmap, DIB24;
-  HFONT  hf;
+  HFONT  hf, tf;
   BITMAPINFOHEADER BitInf;
   BITMAPINFO BitPalInf;
 
   printf("Entering GDITest..\n");
 
-//  GdiDllInitialize (NULL, DLL_PROCESS_ATTACH, NULL);
+  GdiDllInitialize (NULL, DLL_PROCESS_ATTACH, NULL);
 
   // Set up a DC called Desktop that accesses DISPLAY
   Desktop = CreateDCA("DISPLAY", NULL, NULL, NULL);
   if (Desktop == NULL)
     return 1;
 
-  // Create a red pen and select it into the DC
-  BluePen = CreatePen(PS_SOLID, 8, 0x0000ff);
+  // Create a blue pen and select it into the DC
+  BluePen = CreatePen(PS_SOLID, 8, RGB(0, 0, 0xff));
   SelectObject(Desktop, BluePen);
 
   // Draw a shape on the DC
@@ -36,13 +36,40 @@ int main (void)
   MoveToEx(Desktop, 50, 50, NULL);
   LineTo(Desktop, 200, 50);
 
+  WhitePen = CreatePen(PS_SOLID, 3, RGB(0xff, 0xff, 0xff));
+  SelectObject(Desktop, GreenPen);
+
+  MoveToEx(Desktop, 20, 70, NULL);
+  LineTo(Desktop, 500, 70);
+  MoveToEx(Desktop, 70, 20, NULL);
+  LineTo(Desktop, 70, 150);
+
+  // Test font support
+  GreenPen = CreatePen(PS_SOLID, 3, RGB(0, 0xff, 0));
+  RedPen = CreatePen(PS_SOLID, 3, RGB(0xff, 0, 0));
+
+  hf = CreateFontA(24, 0, 0, TA_BASELINE, FW_NORMAL, FALSE, FALSE, FALSE,
+                   ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+                   DEFAULT_QUALITY, FIXED_PITCH|FF_DONTCARE, "Helmet");
+  SelectObject(Desktop, hf);
+  SetTextColor(Desktop, RGB(0xff, 0, 0));
+  TextOutA(Desktop, 70, 70, "React", 5);
+  SetTextColor(Desktop, RGB(0, 0xff, 0));
+  TextOutA(Desktop, 140, 70, "OS", 2);
+
+  tf = CreateFontA(14, 0, 0, TA_BASELINE, FW_NORMAL, FALSE, FALSE, FALSE,
+                   ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+                   DEFAULT_QUALITY, FIXED_PITCH|FF_DONTCARE, "Timmons");
+  SelectObject(Desktop, tf);
+  SetTextColor(Desktop, RGB(0xff, 0xff, 0xff));
+  TextOutA(Desktop, 70, 90, "This is a test of ReactOS text, using the FreeType 2 library!", 61);
+
   // TEST 1: Copy from the VGA into a device compatible DC, draw on it, then blt it to the VGA again
   MyDC = CreateCompatibleDC(Desktop);
   MyBitmap = CreateCompatibleBitmap(Desktop, 151, 251);
   SelectObject(MyDC, MyBitmap);
-  BitBlt(MyDC, 0, 0, 152, 251, Desktop, 50, 50, SRCCOPY);
+  BitBlt(MyDC, 0, 0, 151, 251, Desktop, 50, 50, SRCCOPY); // can we say 151, 251 since bottom corner is not inclusive?
 
-  GreenPen = CreatePen(PS_SOLID, 3, 0x00ff00);
   SelectObject(MyDC, GreenPen);
   Rectangle(MyDC, 10, 10, 50, 50);
 
@@ -64,20 +91,11 @@ int main (void)
   SelectObject(DC24, DIB24);
 
   BitBlt(DC24, 0, 0, 101, 201, MyDC, 0, 0, SRCCOPY);
-  RedPen = CreatePen(PS_SOLID, 3, 0xff0000);
   SelectObject(DC24, RedPen);
   Rectangle(DC24, 80, 90, 100, 110);
   MoveToEx(DC24, 80, 90, NULL);
   LineTo(DC24, 100, 110);
   BitBlt(Desktop, 200, 200, 110, 120, DC24, 0, 0, SRCCOPY);
-
-  // Text tests
-/*        hf = CreateFontA(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
-                        ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-                        DEFAULT_QUALITY, FIXED_PITCH|FF_DONTCARE, "System"); */
-//  TextOutA(Desktop, 200, 0, "ReactOS", 7);
-//  TextOutA(Desktop, 200, 9, "This is a test of the ReactOS GDI functionality", 47);
-//  TextOutA(Desktop, 200, 18, "The font being used is an internal static one", 45);
 
   Sleep( 10000 );
   // Free up everything
