a0fe0d14a0742e3cb3fa3ba6e8e1abfdc984c01b
Phillip Susi <phreak@iag.net>
2001-05-05 18:09:30 +0000

    Removed a bunch of junk from wine and fixed crash on loading
    svn path=/trunk/; revision=1887

--- reactos/lib/user32/misc/dllmain.c
+++ reactos/lib/user32/misc/dllmain.c
@@ -1,8 +1,5 @@
 #include <windows.h>
 
-extern HWINSTA WinStation;
-extern HDESK Desktop;
-
 WINBOOL STDCALL DllMain (HANDLE hInst, 
 			 ULONG ul_reason_for_call,
 			 LPVOID lpReserved);
@@ -27,13 +24,6 @@ DllMain(
 		case DLL_PROCESS_ATTACH:
 			//WinStation = CreateWindowStationA(NULL,0,GENERIC_ALL,NULL);
 			//Desktop = CreateDesktopA(NULL,NULL,NULL,0,0,NULL);
-			WINPOS_CreateInternalPosAtom();
-			SYSCOLOR_Init();
-        		WIDGETS_Init();
-			ICONTITLE_Init();
-			DIALOG_Init();
-			COMBO_Init();
-			MENU_Init();
 			break;
 		case DLL_THREAD_ATTACH:
 			break;
