4304c92e6eb6026709a131571711ce97fcc00e1c
Eric Kohl <eric.kohl@reactos.org>
2001-05-05 18:00:33 +0000

    Fixed silly 'off by one' bug
    svn path=/trunk/; revision=1885

--- reactos/apps/tests/mstest/msclient.c
+++ reactos/apps/tests/mstest/msclient.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	return 0;
      }
    
-   cbLength = (ULONG)strlen(lpszTestMessage);
+   cbLength = (ULONG)strlen(lpszTestMessage)+1;
    
    WriteFile(hMailslot,
 	     lpszTestMessage,
