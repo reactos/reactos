83e63dc11badef13551fc471827092cd49e7a4c4
Eric Kohl <eric.kohl@reactos.org>
2001-07-03 13:07:12 +0000

    Untangled crtdll/msvcrt header mess.
    svn path=/trunk/; revision=2029

---
+++ reactos/include/msvcrt/sys/locking.h
@@ -0,0 +1,38 @@
+/*
+ * locking.h
+ *
+ * Constants for the mode parameter of the locking function.
+ *
+ * This file is part of the Mingw32 package.
+ *
+ * Contributors:
+ *  Created by Colin Peters <colin@bird.fu.is.saga-u.ac.jp>
+ *
+ *  THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ *  This source code is offered for use in the public domain. You may
+ *  use, modify or distribute it freely.
+ *
+ *  This code is distributed in the hope that it will be useful but
+ *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ *  DISCLAMED. This includes but is not limited to warranties of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * $Revision: 1.1 $
+ * $Author: ekohl $
+ * $Date: 2001/07/03 13:07:12 $
+ *
+ */
+
+#ifndef	__STRICT_ANSI__
+
+#ifndef	_LOCKING_H_
+#define	_LOCKING_H_
+
+/*
+ * TODO: Define LK_... constants.
+ */
+
+#endif	/* Not _LOCKING_H_ */
+
+#endif	/* Not __STRICT_ANSI__ */
---
+++ reactos/include/msvcrt/sys/time.h
@@ -0,0 +1,3 @@
+
+#include <msvcrt/time.h>
+
