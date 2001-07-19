20a0131534a6ab5d07d753134cdecf0d3e52364c
Eric Kohl <eric.kohl@reactos.org>
2001-07-19 18:41:09 +0000

    Fixed bug in _makepath(). Patch by Hartmut Birr.
    svn path=/trunk/; revision=2073

--- reactos/lib/crtdll/stdlib/makepath.c
+++ reactos/lib/crtdll/stdlib/makepath.c
@@ -1,30 +1,34 @@
 #include <crtdll/stdlib.h>
 #include <crtdll/string.h>
 
-void _makepath( char *path, const char *drive, const char *dir, const char *fname, const char *ext )
+void _makepath(char *path, const char *drive, const char *dir, const char *fname, const char *ext)
 {
-	int dir_len;
-	if ( drive != NULL  && (*drive)) {
-		strcpy(path,drive);
-		strcat(path,":");
-	}
-        else
-            (*path)=0;
+  int dir_len;
 
-	if ( dir != NULL ) {
-		strcat(path,dir);
-		if ( *dir != '\\' )
-			strcat(path,"\\");
-		dir_len = strlen(dir);
-		if ( dir_len && *(dir + dir_len - 1) != '\\' ) 
-			strcat(path,"\\"); 
+  if ((drive != NULL) && (*drive))
+    {
+      strcpy(path, drive);
+      strcat(path, ":");
+    }
+  else
+    (*path)=0;
+
+  if (dir != NULL)
+    {
+      strcat(path, dir);
+      dir_len = strlen(dir);
+      if (dir_len && *(dir + dir_len - 1) != '\\')
+	strcat(path, "\\");
+    }
+
+  if (fname != NULL)
+    {
+      strcat(path, fname);
+      if (ext != NULL)
+	{
+	  if (*ext != '.')
+	    strcat(path, ".");
+	strcat(path, ext);
 	}
-	if ( fname != NULL ) {
-		strcat(path,fname);
-		if ( ext != NULL ) {
-			if ( *ext != '.')
-				strcat(path,".");
-			strcat(path,ext);
-		}
-	}
+    }
 }
--- reactos/lib/msvcrt/stdlib/makepath.c
+++ reactos/lib/msvcrt/stdlib/makepath.c
@@ -1,58 +1,34 @@
 #include <msvcrt/stdlib.h>
 #include <msvcrt/string.h>
 
-void _makepath( char *path, const char *drive, const char *dir, const char *fname, const char *ext )
+void _makepath(char *path, const char *drive, const char *dir, const char *fname, const char *ext)
 {
-	int dir_len;
-	if ( drive != NULL  && (*drive)) {
-		strcpy(path,drive);
-		strcat(path,":");
-	}
-        else
-            (*path)=0;
+  int dir_len;
 
-	if ( dir != NULL ) {
-		strcat(path,dir);
-		if ( *dir != '\\' )
-			strcat(path,"\\");
-		dir_len = strlen(dir);
-		if ( dir_len && *(dir + dir_len - 1) != '\\' ) 
-			strcat(path,"\\"); 
-	}
-	if ( fname != NULL ) {
-		strcat(path,fname);
-		if ( ext != NULL ) {
-			if ( *ext != '.')
-				strcat(path,".");
-			strcat(path,ext);
-		}
-	}
-}
+  if ((drive != NULL) && (*drive))
+    {
+      strcpy(path, drive);
+      strcat(path, ":");
+    }
+  else
+    (*path)=0;
 
-void _wmakepath( wchar_t *path, const wchar_t *drive, const wchar_t *dir, const wchar_t *fname, const wchar_t *ext )
-{
-	int dir_len;
-	if ( drive != NULL && (*drive)) {
-		wcscpy(path,drive);
-		wcscat(path,L":");
-	}
-	   else
-	   (*path)=0;
+  if (dir != NULL)
+    {
+      strcat(path, dir);
+      dir_len = strlen(dir);
+      if (dir_len && *(dir + dir_len - 1) != '\\')
+	strcat(path, "\\");
+    }
 
-	if ( dir != NULL ) {
-		wcscat(path,dir);
-		if ( *dir != L'\\' )
-			wcscat(path,L"\\");
-		dir_len = wcslen(dir);
-		if ( dir_len && *(dir + dir_len - 1) != L'\\' )
-			wcscat(path,L"\\");
+  if (fname != NULL)
+    {
+      strcat(path, fname);
+      if (ext != NULL)
+	{
+	  if (*ext != '.')
+	    strcat(path, ".");
+	strcat(path, ext);
 	}
-	if ( fname != NULL ) {
-		wcscat(path,fname);
-		if ( ext != NULL ) {
-			if ( *ext != L'.')
-				wcscat(path,L".");
-			wcscat(path,ext);
-		}
-	}
+    }
 }
