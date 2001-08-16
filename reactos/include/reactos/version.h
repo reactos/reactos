5b66798cd7a411cab87088e32a313aee04380536
Jason Filby <jason.filby@gmail.com>
2001-08-16 19:42:15 +0000

    Change versioning infor for 0.0.18
    svn path=/trunk/; revision=2181

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.5 2001/02/11 15:02:48 ekohl Exp $
+/* $Id: version.h,v 1.6 2001/08/16 19:42:15 jfilby Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -16,11 +16,11 @@
 
 #define KERNEL_VERSION_MAJOR		0
 #define KERNEL_VERSION_MINOR		0
-#define KERNEL_VERSION_PATCH_LEVEL	17
+#define KERNEL_VERSION_PATCH_LEVEL	18
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
-#define KERNEL_RELEASE_DATE		20010209L
+#define KERNEL_RELEASE_DATE		20010816L
 
-#define KERNEL_VERSION			"0.0.17"
+#define KERNEL_VERSION			"0.0.18"
 
 #endif
 
