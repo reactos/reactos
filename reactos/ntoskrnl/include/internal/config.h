be1ee02554c16fcda4958698ecb079f452561d4a
Casper Hornstrup <chorns@users.sourceforge.net>
2001-08-21 21:51:43 +0000

    Global configuration moved to include/roscfg.h
    svn path=/trunk/; revision=2187

---
+++ reactos/ntoskrnl/include/internal/config.h
@@ -0,0 +1,7 @@
+#ifndef __DEPRECATED_INTERNAL_CONFIG_H
+#define __DEPRECATED__INTERNAL_CONFIG_H
+
+/* Moved to /include/roscfg.h */
+#include <roscfg.h>
+
+#endif /* __DEPRECATED_INTERNAL_CONFIG_H */
