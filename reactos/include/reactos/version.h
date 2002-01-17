e5d9890c6c9ad57cae6d8b89a6add600403f3b55
Emanuele Aliberti <ea@iol.it>
2002-01-17 23:22:58 +0000

    KERNEL_VERSION macro removed from reactos/version.h.
    Use the dinamically generated macros in reactos/buildno.h.
    
    svn path=/trunk/; revision=2521

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.6 2001/08/16 19:42:15 jfilby Exp $
+/* $Id: version.h,v 1.7 2002/01/17 23:22:58 ea Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -8,7 +8,10 @@
  * REVISIONS:
  * 	1999-11-06 (ea)
  * 		Moved from include/internal in include/reactos
- * 		to be used by buildno.
+ *		to be used by buildno.
+ *	2002-01-17 (ea)
+ *		KERNEL_VERSION removed. Use
+ *		reactos/buildno.h:KERNEL_VERSION_STR instead.
  */
 
 #ifndef __VERSION_H
@@ -20,7 +23,6 @@
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
 #define KERNEL_RELEASE_DATE		20010816L
 
-#define KERNEL_VERSION			"0.0.18"
 
 #endif
 
