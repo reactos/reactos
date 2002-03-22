4e0441ed705b58a68a1b246d9655412ef4ef47c4
KJK::Hyperion <hackbunny@reactos.org>
2002-03-22 01:24:49 +0000

    dummy include files for compatibility with Microsoft POSIX
    svn path=/trunk/; revision=2760

---
+++ posix/include/sys/errno.h
@@ -0,0 +1,44 @@
+/* $Id: errno.h,v 1.1 2002/03/22 01:24:49 hyperion Exp $
+ */
+/*
+ * sys/errno.h
+ *
+ * dummy include file for Microsoft POSIX and Interix compatibility
+ *
+ * This file is part of the ReactOS Operating System.
+ *
+ * Contributors:
+ *  Created by KJK::Hyperion <noog@libero.it>
+ *
+ *  THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ *  This source code is offered for use in the public domain. You may
+ *  use, modify or distribute it freely.
+ *
+ *  This code is distributed in the hope that it will be useful but
+ *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ *  DISCLAMED. This includes but is not limited to warranties of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#ifndef __SYS_ERRNO_H_INCLUDED__
+#define __SYS_ERRNO_H_INCLUDED__
+
+/* INCLUDES */
+#include <errno.h>
+
+/* OBJECTS */
+
+/* TYPES */
+
+/* CONSTANTS */
+
+/* PROTOTYPES */
+
+/* MACROS */
+
+#endif /* __SYS_ERRNO_H_INCLUDED__ */
+
+/* EOF */
+
