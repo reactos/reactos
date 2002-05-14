3991d3694345a5b160b916fc777a62c7a89345ea
Eric Kohl <eric.kohl@reactos.org>
2002-05-14 23:17:12 +0000

    Silenced debug message.
    svn path=/trunk/; revision=2952

--- reactos/drivers/storage/atapi/atapi.c
+++ reactos/drivers/storage/atapi/atapi.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: atapi.c,v 1.19 2002/04/10 17:01:47 ekohl Exp $
+/* $Id: atapi.c,v 1.20 2002/05/14 23:17:12 ekohl Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS ATAPI miniport driver
@@ -672,7 +672,7 @@ AtapiInterrupt(IN PVOID DeviceExtension)
   DevExt = (PATAPI_MINIPORT_EXTENSION)DeviceExtension;
   if (DevExt->ExpectingInterrupt == FALSE)
     {
-      DPRINT1("AtapiInterrupt(): Unexpected interrupt\n");
+      DPRINT("AtapiInterrupt(): Unexpected interrupt\n");
       return(FALSE);
     }
 
