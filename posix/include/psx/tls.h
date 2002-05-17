75a810da7df6dbb34dc80ddd8f5395db70bb70eb
KJK::Hyperion <hackbunny@reactos.org>
2002-05-17 02:07:14 +0000

    TLS calls (currently unused)
    svn path=/trunk/; revision=2963

---
+++ posix/include/psx/tls.h
@@ -0,0 +1,48 @@
+/* $Id: tls.h,v 1.1 2002/05/17 02:07:14 hyperion Exp $
+ */
+/*
+ * psx/tls.h
+ *
+ * types and calls for TLS management
+ *
+ * This file is part of the ReactOS Operating System.
+ *
+ * Contributors:
+ *  Created by KJK::Hyperion <noog@libero.it>
+ *
+ *  THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ *  This source code is offered for use in the public domain. You may
+ *  use, modify or distribute it freely.
+ *
+ *  This code is distributed in the hope that it will be useful but
+ *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ *  DISCLAMED. This includes but is not limited to warranties of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#ifndef __PSX_TLS_H_INCLUDED__
+#define __PSX_TLS_H_INCLUDED__
+
+/* INCLUDES */
+
+/* OBJECTS */
+
+/* TYPES */
+typedef unsigned int __tls_index_t;
+
+/* CONSTANTS */
+
+/* PROTOTYPES */
+__tls_index_t __tls_alloc();
+int           __tls_free(__tls_index_t index);
+void *        __tls_get_val(__tls_index_t index);
+int           __tls_put_val(__tls_index_t index, void * data);
+
+/* MACROS */
+
+#endif /* __PSX_TLS_H_INCLUDED__ */
+
+/* EOF */
+
