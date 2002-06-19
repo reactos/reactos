c2b9c68cbffb14cb21bf9ead1ff96c562c595933
Robert Dickenson <robd@reactos.org>
2002-06-19 06:26:11 +0000

    Created new module for wshirda.dll
    svn path=/trunk/; revision=3122

---
+++ reactos/lib/wshirda/wshirda.c
@@ -0,0 +1,206 @@
+/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ReactOS Winsock 2 IRDA Helper DLL
+ * FILE:        wshirda.c
+ * PURPOSE:     DLL entry
+ * PROGRAMMERS: Robert D. Dickenson (robertdickenson@users.sourceforge.net)
+ * REVISIONS:
+ *   RDD 18/06-2002 Created
+ */
+#include "wshirda.h"
+
+#ifdef DBG
+
+/* See debug.h for debug/trace constants */
+DWORD DebugTraceLevel = MAX_TRACE;
+
+#endif /* DBG */
+
+/* To make the linker happy */
+VOID STDCALL KeBugCheck (ULONG	BugCheckCode) {}
+
+
+BOOL
+EXPORT
+DllMain(HANDLE hInstDll,
+        ULONG dwReason,
+        PVOID Reserved)
+{
+    WSH_DbgPrint(MIN_TRACE, ("DllMain of wshirda.dll\n"));
+
+    switch (dwReason) {
+    case DLL_PROCESS_ATTACH:
+        /* Don't need thread attach notifications
+           so disable them to improve performance */
+        DisableThreadLibraryCalls(hInstDll);
+        break;
+
+    case DLL_THREAD_ATTACH:
+        break;
+
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+INT
+WINAPI
+WSHEnumProtocols(
+    IN      LPINT lpiProtocols  OPTIONAL,
+    IN      LPWSTR lpTransportKeyName,
+    IN OUT  LPVOID lpProtocolBuffer,
+    IN OUT  LPDWORD lpdwBufferLength)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHGetProviderGuid(
+    IN  LPWSTR ProviderName,
+    OUT LPGUID ProviderGuid)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHGetSockaddrType(
+    IN  PSOCKADDR Sockaddr,
+    IN  DWORD SockaddrLength,
+    OUT PSOCKADDR_INFO SockaddrInfo)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHGetSocketInformation(
+    IN  PVOID HelperDllSocketContext,
+    IN  SOCKET SocketHandle,
+    IN  HANDLE TdiAddressObjectHandle,
+    IN  HANDLE TdiConnectionObjectHandle,
+    IN  INT Level,
+    IN  INT OptionName,
+    OUT PCHAR OptionValue,
+    OUT INT OptionLength)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHGetWSAProtocolInfo(
+    IN  LPWSTR ProviderName,
+    OUT LPWSAPROTOCOL_INFOW *ProtocolInfo,
+    OUT LPDWORD ProtocolInfoEntries)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHGetWildcardSockaddr(
+    IN  PVOID HelperDllSocketContext,
+    OUT PSOCKADDR Sockaddr,
+    OUT PINT SockaddrLength)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+DWORD
+WINAPI
+WSHGetWinsockMapping(
+    OUT PWINSOCK_MAPPING Mapping,
+    IN  DWORD MappingLength)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHIoctl(
+    IN  PVOID HelperDllSocketContext,
+    IN  SOCKET SocketHandle,
+    IN  HANDLE TdiAddressObjectHandle,
+    IN  HANDLE TdiConnectionObjectHandle,
+    IN  DWORD IoControlCode,
+    IN  LPVOID InputBuffer,
+    IN  DWORD InputBufferLength,
+    IN  LPVOID OutputBuffer,
+    IN  DWORD OutputBufferLength,
+    OUT LPDWORD NumberOfBytesReturned,
+    IN  LPWSAOVERLAPPED Overlapped,
+    IN  LPWSAOVERLAPPED_COMPLETION_ROUTINE CompletionRoutine,
+    OUT LPBOOL NeedsCompletion)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHNotify(
+    IN  PVOID HelperDllSocketContext,
+    IN  SOCKET SocketHandle,
+    IN  HANDLE TdiAddressObjectHandle,
+    IN  HANDLE TdiConnectionObjectHandle,
+    IN  DWORD NotifyEvent)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHOpenSocket(
+    IN OUT  PINT AddressFamily,
+    IN OUT  PINT SocketType,
+    IN OUT  PINT Protocol,
+    OUT     PUNICODE_STRING TransportDeviceName,
+    OUT     PVOID HelperDllSocketContext,
+    OUT     PDWORD NotificationEvents)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+INT
+WINAPI
+WSHSetSocketInformation(
+    IN  PVOID HelperDllSocketContext,
+    IN  SOCKET SocketHandle,
+    IN  HANDLE TdiAddressObjectHandle,
+    IN  HANDLE TdiConnectionObjectHandle,
+    IN  INT Level,
+    IN  INT OptionName,
+    IN  PCHAR OptionValue,
+    IN  INT OptionLength)
+{
+    UNIMPLEMENTED
+
+    return 0;
+}
+
+/* EOF */
---
+++ reactos/lib/wshirda/wshirda.h
@@ -0,0 +1,22 @@
+/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ReactOS Winsock 2 IRDA Helper DLL
+ * FILE:        include/ws2help.h
+ * PURPOSE:     WinSock 2 IRDA Helper DLL
+ */
+#ifndef __WSHIRDA_H
+#define __WSHIRDA_H
+
+#include <wsahelp.h>
+#include <windows.h>
+#include <ddk/ntddk.h>
+#include <debug.h>
+
+#define EXPORT STDCALL
+
+#define OBJ_NAME_PATH_SEPARATOR ((WCHAR)L'\\')
+
+
+#endif /* __WSHIRDA_H */
+
+/* EOF */
