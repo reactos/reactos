157460c518e1c012d441459c06cb1bfcea880e38
Robert Dickenson <robd@reactos.org>
2002-07-03 19:32:52 +0000

    New File Manager application - winfile.
    svn path=/trunk/; revision=3176

---
+++ rosapps/winfile/resource.h
@@ -0,0 +1,76 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by winfile.rc
+//
+#define ID_FILE_MENU                    0
+#define ID_DISK_MENU                    1
+#define ID_TREE_MENU                    2
+#define ID_VIEW_MENU                    3
+#define ID_OPTIONS_MENU                 4
+#define ID_SECURITY_MENU                5
+#define ID_WINDOW_MENU                  6
+#define ID_HELP_MENU                    7
+
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_WINFILE                     107
+#define IDI_SMALL                       108
+#define IDC_WINFILE                     109
+#define IDC_WINFILE_FRAME               110
+#define IDR_WINFILE_MENU                130
+#define IDD_DIALOG1                     131
+#define IDB_OPEN_FILE                   132
+#define IDB_CLOSED_FILE                 133
+#define IDB_ROOT                        134
+
+#define ID_HELP_HELPTOPICS              32771
+#define ID_VIEW_STATUSBAR               32774
+#define ID_VIEW_SPLIT                   32775
+#define ID_VIEW_REFRESH                 32776
+#define ID_EDIT_DELETE                  32778
+#define ID_EDIT_RENAME                  32779
+#define ID_EDIT_COPYKEYNAME             32781
+#define ID_EDIT_FIND                    32782
+#define ID_EDIT_FINDNEXT                32783
+#define ID_EDIT_MODIFY                  32784
+#define ID_HELP_CONTENTS                32795
+#define ID_WINDOW_CASCADE               32797
+#define ID_WINDOW_TILE                  32798
+#define ID_WINDOW_ARRANGEICONS          32799
+#define ID_OPTIONS_FONT                 32800
+#define ID_OPTIONS_AUTOREFRESH          32801
+#define ID_OPTIONS_READONLYMODE         32802
+#define ID_OPTIONS_CONFIRMONDELETE      32803
+#define ID_OPTIONS_SAVESETTINGSONEXIT   32804
+#define ID_SECURITY_PERMISSIONS         32805
+#define ID_VIEW_TREEANDDATA             32806
+#define ID_VIEW_TREEONLY                32807
+#define ID_VIEW_DATAONLY                32808
+#define ID_VIEW_DISPLAYBINARYDATA       32810
+#define ID_VIEW_REFRESHALL              32811
+#define ID_VIEW_REFRESHACTIVE           32812
+#define ID_VIEW_FINDKEY                 32813
+#define ID_TREE_EXPANDONELEVEL          32814
+#define ID_TREE_EXPANDBRANCH            32815
+#define ID_TREE_EXPANDALL               32816
+#define ID_TREE_COLLAPSEBRANCH          32817
+#define ID_REGISTRY_OPENLOCAL           32825
+
+#define IDC_LICENSE_EDIT                1029
+#define IDS_LICENSE                     32835
+
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32836
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
