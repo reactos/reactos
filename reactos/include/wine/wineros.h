8c8444aecb625ec0f6c364e28bfacd568f6b7c4a
Steven Edwards <winehacker@gmail.com>
2002-07-07 10:44:03 +0000

    Removed by mistake
    svn path=/trunk/; revision=3188

---
+++ reactos/include/wine/wineros.h
@@ -0,0 +1,10 @@
+/* Wine wrapper for ReactOS */
+#ifndef __REACTOS_WINE_WINEROS_H
+#define __REACTOS_WINE_WINEROS_H
+
+typedef unsigned short u_short;
+typedef unsigned long u_long;
+
+#define FD_SETSIZE 64
+
+#endif  /* __REACTOS_WINE_WINEROS_H */
