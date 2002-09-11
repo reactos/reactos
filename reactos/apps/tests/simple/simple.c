19a7100f393433feae8c548fad6ab75f5b37983a
Steven Edwards <winehacker@gmail.com>
2002-09-11 22:34:40 +0000

    Fixed text output for simple messagebox test
    svn path=/trunk/; revision=3487

--- reactos/apps/tests/simple/simple.c
+++ reactos/apps/tests/simple/simple.c
@@ -10,6 +10,6 @@
 int STDCALL
 WinMain (HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow)
 {
-	MessageBox (NULL, "Hello, Windows!", "Hello", MB_OK);
+	MessageBox (NULL, "Hello, ReactOS!", "Hello", MB_OK);
 	return 0;
 }
