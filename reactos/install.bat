8f5a73d23734eee8888321669dfffa717b1dcf29
Robert Dickenson <robd@reactos.org>
2002-09-24 14:47:27 +0000

    Added new targets to the copy list.
    svn path=/trunk/; revision=3547

--- reactos/install.bat
+++ reactos/install.bat
@@ -43,11 +43,14 @@ copy drivers\input\psaux\psaux.sys %ROS_INSTALL%\system32\drivers
 copy drivers\dd\blue\blue.sys %ROS_INSTALL%\system32\drivers
 copy drivers\dd\beep\beep.sys %ROS_INSTALL%\system32\drivers
 copy drivers\dd\null\null.sys %ROS_INSTALL%\system32\drivers
+copy drivers\dd\serial\serial.sys %ROS_INSTALL%\system32\drivers
+copy drivers\dd\serenum\serenum.sys %ROS_INSTALL%\system32\drivers
 copy drivers\dd\vga\miniport\vgamp.sys %ROS_INSTALL%\system32\drivers
 copy drivers\dd\vga\display\vgaddi.dll %ROS_INSTALL%\system32\drivers
 copy drivers\dd\vidport\vidport.sys %ROS_INSTALL%\system32\drivers
 copy drivers\net\afd\afd.sys %ROS_INSTALL%\system32\drivers
 copy drivers\net\dd\ne2000\ne2000.sys %ROS_INSTALL%\system32\drivers
+copy drivers\net\dd\miniport\nscirda\nscirda.sys %ROS_INSTALL%\system32\drivers
 copy drivers\net\ndis\ndis.sys %ROS_INSTALL%\system32\drivers
 copy drivers\net\packet\packet.sys %ROS_INSTALL%\system32\drivers
 copy drivers\net\tcpip\tcpip.sys %ROS_INSTALL%\system32\drivers
@@ -68,13 +71,21 @@ copy lib\advapi32\advapi32.dll %ROS_INSTALL%\system32
 copy lib\crtdll\crtdll.dll %ROS_INSTALL%\system32
 copy lib\fmifs\fmifs.dll %ROS_INSTALL%\system32
 copy lib\gdi32\gdi32.dll %ROS_INSTALL%\system32
+copy lib\iphlpapi\iphlpapi.dll %ROS_INSTALL%\system32
 copy lib\kernel32\kernel32.dll %ROS_INSTALL%\system32
+copy lib\libpcap\libpcap.dll %ROS_INSTALL%\system32
 copy lib\msafd\msafd.dll %ROS_INSTALL%\system32
 copy lib\msvcrt\msvcrt.dll %ROS_INSTALL%\system32
 copy lib\ntdll\ntdll.dll %ROS_INSTALL%\system32
+copy lib\packet\packet.dll %ROS_INSTALL%\system32
 copy lib\secur32\secur32.dll %ROS_INSTALL%\system32
+copy lib\shell32\roshel32.dll %ROS_INSTALL%\system32
+copy lib\snmpapi\snmpapi.dll %ROS_INSTALL%\system32
 copy lib\user32\user32.dll %ROS_INSTALL%\system32
 copy lib\ws2_32\ws2_32.dll %ROS_INSTALL%\system32
+copy lib\ws2help\ws2help.dll %ROS_INSTALL%\system32
+copy lib\wshirda\wshirda.dll %ROS_INSTALL%\system32
+copy lib\wsock32\wsock32.dll %ROS_INSTALL%\system32
 copy subsys\smss\smss.exe %ROS_INSTALL%\system32
 copy subsys\csrss\csrss.exe %ROS_INSTALL%\system32
 copy subsys\win32k\win32k.sys %ROS_INSTALL%\system32\drivers
@@ -108,9 +119,9 @@ copy apps\tests\winhello\winhello.exe %ROS_INSTALL%\bin
 copy apps\tests\sectest\sectest.exe %ROS_INSTALL%\bin
 copy apps\tests\isotest\isotest.exe %ROS_INSTALL%\bin
 copy apps\tests\regtest\regtest.exe %ROS_INSTALL%\bin
+copy apps\tests\restest\restest.exe %ROS_INSTALL%\bin
 copy apps\tests\tokentest\tokentest.exe %ROS_INSTALL%\bin
 copy media\fonts\helb____.ttf %ROS_INSTALL%\media\fonts
 copy media\fonts\timr____.ttf %ROS_INSTALL%\media\fonts
 copy media\nls\*.nls %ROS_INSTALL%\system32
 copy ntoskrnl\ntoskrnl.map %ROS_INSTALL%\symbols
-
