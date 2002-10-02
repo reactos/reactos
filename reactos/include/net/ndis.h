5073fe8a33d4eb553de0a891926c42fe0909ef93
guido <guido@svn.reactos.org>
2002-10-02 21:08:24 +0000

    removed duplicate type definition
    svn path=/trunk/; revision=3601

--- reactos/include/net/ndis.h
+++ reactos/include/net/ndis.h
@@ -96,15 +96,6 @@ typedef struct _DMA_CONFIGURATION_BYTE1
 } DMA_CONFIGURATION_BYTE1;
 
 
-typedef struct _CM_MCA_POS_DATA
-{
-    USHORT  AdapterId;
-    UCHAR   PosData1;
-    UCHAR   PosData2;
-    UCHAR   PosData3;
-    UCHAR   PosData4;
-} CM_MCA_POS_DATA, *PCM_MCA_POS_DATA;
-
 typedef struct _EISA_MEMORY_TYPE
 {
     UCHAR   ReadWrite:1;
