b8114825ec8a1d80e0c813b64988197f3066b85b
Hartmut Birr <osexpert@googlemail.com>
2002-10-31 07:29:07 +0000

    Merged all exec* and spawn* functions in one source file.
    svn path=/trunk/; revision=3683

--- reactos/lib/msvcrt/process/spawnv.c
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
-
-#include <msvcrt/process.h>
-#include <msvcrt/stdlib.h>
-
-int _spawnv(int nMode, const char* szPath, char* const* szaArgv)
-{
-  return _spawnve(nMode, szPath, (char * const *)szaArgv, _environ);
-}
--- reactos/lib/msvcrt/process/spawnvp.c
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
-
-#include <msvcrt/process.h>
-#include <msvcrt/stdlib.h>
-
-int _spawnvp(int nMode, const char* szPath, char* const* szaArgv)
-{
-  return spawnvpe(nMode, szPath, (char * const *)szaArgv, _environ);
-}
