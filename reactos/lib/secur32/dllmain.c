c2cdc52d2e0a125127b663c66aa1bc1edd936f24
Robert Dickenson <robd@reactos.org>
2002-11-10 13:38:14 +0000

    committing a parameter swap from Gunnars heap fix which I seem to have missed.
    svn path=/trunk/; revision=3724

--- reactos/lib/secur32/dllmain.c
+++ reactos/lib/secur32/dllmain.c
@@ -1,4 +1,4 @@
-/* $Id: dllmain.c,v 1.3 2002/09/08 10:23:08 chorns Exp $
+/* $Id: dllmain.c,v 1.4 2002/11/10 13:38:14 robd Exp $
  *
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS system libraries
@@ -24,7 +24,7 @@ WINBOOL STDCALL DllMain(HINSTANCE hInstance, ULONG Reason, PVOID Reserved)
    switch (Reason)
      {
       case DLL_PROCESS_ATTACH:
-	Secur32Heap = RtlCreateHeap(0, NULL, 4096, 0, NULL, NULL);
+	Secur32Heap = RtlCreateHeap(0, NULL, 0, 4096, NULL, NULL);
 	if (Secur32Heap == 0)
 	  {
 	     return(FALSE);
