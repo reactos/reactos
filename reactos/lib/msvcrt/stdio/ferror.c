99b937362d1dffa8df7d02ad588680c761b34ea9
Hartmut Birr <osexpert@googlemail.com>
2002-11-27 20:56:28 +0000

    Fixed the return value in ferror.
    svn path=/trunk/; revision=3802

--- reactos/lib/msvcrt/stdio/ferror.c
+++ reactos/lib/msvcrt/stdio/ferror.c
@@ -7,22 +7,9 @@
 int ferror(FILE *stream);
 #endif
 
-//int *_errno(void)
-//{
-//  return(&GetThreadData()->terrno);
-//}
-//int __set_errno(int error)
-//{
-//  PTHREADDATA ThreadData;
-//  ThreadData = GetThreadData();
-//  if (ThreadData)
-//    ThreadData->terrno = error;
-//  return(error);
-//}
 int *_errno(void);
 
 int ferror(FILE *stream)
 {
-//  return stream->_flag & _IOERR;
-  return *_errno();
+  return stream->_flag & _IOERR;
 }
