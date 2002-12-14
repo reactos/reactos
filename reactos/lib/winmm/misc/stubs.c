9894d327a48502a23699fbbbfb691087d0347396
Steven Edwards <winehacker@gmail.com>
2002-12-14 17:36:51 +0000

    Added more stubs for bochs.
    svn path=/trunk/; revision=3875

--- reactos/lib/winmm/misc/stubs.c
+++ reactos/lib/winmm/misc/stubs.c
@@ -1,6 +1,10 @@
 /* 
   The stubs here are totaly wrong so please help a brother out 
   and fix this shit. sedwards 9-24-02
+
+  Added more stubs for bochs 1.3 once again still mostly wrong
+  but bochs gets further now. 12-14-02
+
 */
 
 #include <windows.h>
@@ -15,11 +19,20 @@ waveOutReset(HWAVEOUT hWaveOut)
 	return 1;
 }
 
+
+UINT WINAPI waveOutWrite(HWAVEOUT hWaveOut, LPCSTR pszSoundA,
+			 UINT uSize)
+{
+	DbgPrint("waveOutWrite unimplemented\n");
+	return 1;
+}
+
+
 WINBOOL 
 STDCALL
 sndPlaySoundA(LPCSTR pszSoundA, UINT uFlags)
 {
-	DbgPrint("waveOutReset unimplemented\n");
+	DbgPrint("sndPlaySoundA unimplemented\n");
 	return 1;
 }
 
@@ -27,6 +40,14 @@ WINBOOL
 STDCALL
 sndPlaySoundW(LPCSTR pszSoundA, UINT uFlags)
 {
-	DbgPrint("waveOutReset unimplemented\n");
+	DbgPrint("sndPlaySoundW unimplemented\n");
+	return 1;
+}
+
+WINBOOL 
+STDCALL
+midiOutReset(HWAVEOUT hWaveOut)
+{
+	DbgPrint("midiOutReset unimplemented\n");
 	return 1;
 }
