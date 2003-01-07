3f13766f1642962dd6b06762f38a967f8a41e025
Robert Dickenson <robd@reactos.org>
2003-01-07 16:24:45 +0000

    added install batch similar to that provided in /reactos. Removed deprecated Makefile.
    svn path=/trunk/; revision=3947

--- posix/install.bat
+++ posix/install.bat
@@ -5,7 +5,6 @@ goto Install
 :NoParameter
 set ROS_INSTALL=c:\reactos
 :Install
-echo on
 echo Installing to %ROS_INSTALL%
 @echo off
 
@@ -21,4 +20,3 @@ copy lib\psxdll\psxdll.dll %ROS_INSTALL%\system32
 copy lib\psxdll\psxdll.map %ROS_INSTALL%\symbols
 copy lib\psxx\psxx.dll %ROS_INSTALL%\system32
 copy lib\psxx\psxx.map %ROS_INSTALL%\symbols
-
