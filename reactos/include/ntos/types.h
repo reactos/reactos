2cc86942716dac7d123a22851d2faa7ccca5c6ca
Hartmut Birr <osexpert@googlemail.com>
2003-01-13 00:50:37 +0000

    Fixed UINT8/PUINT8.
    svn path=/trunk/; revision=3994

--- reactos/include/ntos/types.h
+++ reactos/include/ntos/types.h
@@ -26,7 +26,7 @@
 typedef signed char         INT8, *PINT8;
 typedef signed short        INT16, *PINT16;
 
-typedef unsigned short      UINT8, *PUINT8;
+typedef unsigned char       UINT8, *PUINT8;
 typedef unsigned short      UINT16, *PUINT16;
 
 
