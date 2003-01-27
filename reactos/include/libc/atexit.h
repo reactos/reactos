6c08d81780495db69bee37705838a79a30740939
Robert Dickenson <robd@reactos.org>
2003-01-27 14:49:18 +0000

    backdoor macro fixed.
    svn path=/trunk/; revision=4075

--- reactos/include/libc/atexit.h
+++ reactos/include/libc/atexit.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 1995 DJ Delorie, see COPYING.DJ for details */
 #ifndef __dj_include_libc_atexit_h__
-#define __dj_include_libc_dosexec_h__
+#define __dj_include_libc_atexit_h__
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,4 +30,4 @@ extern struct __atexit *__atexit_ptr;
 }
 #endif
 
-#endif /* __dj_include_libc_dosexec_h__ */
+#endif /* __dj_include_libc_atexit_h__ */
