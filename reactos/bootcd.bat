155e4760a8828188a199620d3f8a9b93d9512375
Eric Kohl <eric.kohl@reactos.org>
2003-01-30 14:45:51 +0000

    Added some more files to the boot-cd. Fixed bug in txtsetup.sif.
    svn path=/trunk/; revision=4082

--- reactos/bootcd.bat
+++ reactos/bootcd.bat
@@ -34,9 +34,13 @@ copy /Y hal\halx86\hal.dll %BOOTCD_DIR%\disk\install
 copy /Y drivers\fs\vfat\vfatfs.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\fs\cdfs\cdfs.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\fs\ntfs\ntfs.sys %BOOTCD_DIR%\disk\install
-copy /Y drivers\dd\floppy\floppy.sys %BOOTCD_DIR%\disk\install
+copy /Y drivers\fs\fs_rec\fs_rec.sys %BOOTCD_DIR%\disk\install
+copy /Y drivers\dd\beep\beep.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\dd\blue\blue.sys %BOOTCD_DIR%\disk\install
+copy /Y drivers\dd\floppy\floppy.sys %BOOTCD_DIR%\disk\install
+copy /Y drivers\dd\null\null.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\input\keyboard\keyboard.sys %BOOTCD_DIR%\disk\install
+copy /Y drivers\input\psaux\psaux.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\storage\atapi\atapi.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\storage\scsiport\scsiport.sys %BOOTCD_DIR%\disk\install
 copy /Y drivers\storage\cdrom\cdrom.sys %BOOTCD_DIR%\disk\install
@@ -53,3 +57,6 @@ copy /Y subsys\csrss\csrss.exe %BOOTCD_DIR%\disk\install
 copy /Y subsys\system\services\services.exe %BOOTCD_DIR%\disk\install
 copy /Y subsys\system\shell\shell.exe %BOOTCD_DIR%\disk\install
 copy /Y subsys\system\winlogon\winlogon.exe %BOOTCD_DIR%\disk\install
+
+rem copy temporary files
+copy /Y system.hiv %BOOTCD_DIR%\disk\install
