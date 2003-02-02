a9d96a9cfc3a87439d7b5ec12ba4adacfb16ea01
KJK::Hyperion <hackbunny@reactos.org>
2003-02-02 19:24:44 +0000

    Finally solved some advapi32 nonsense:  - advapi32 doesn't import any more from itself  - smarter stubbing (look at the files modified in this commit)
    svn path=/trunk/; revision=4106

--- reactos/lib/advapi32/sec/lsa.c
+++ reactos/lib/advapi32/sec/lsa.c
@@ -1,4 +1,4 @@
-/* $Id: lsa.c,v 1.4 2002/09/08 10:22:37 chorns Exp $
+/* $Id: lsa.c,v 1.5 2003/02/02 19:24:44 hyperion Exp $
  *
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS system libraries
@@ -8,578 +8,127 @@
  * UPDATE HISTORY:
  *	19990322 EA created
  *	19990515 EA stubs
+ *      20030202 KJK compressed stubs
  */
 #include <windows.h>
 #include <ddk/ntddk.h>
 
-
-/**********************************************************************
- *	LsaAddAccountRights
- */
-INT
-STDCALL
-LsaAddAccountRights (VOID)
+/*
+LsaClearAuditLog
+LsaClose
+LsaDelete
+LsaFreeMemory
+*/
+NTSTATUS STDCALL _LsaStub1(ULONG_PTR Unknown0)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaAddPrivilegesToAccount
- */
-INT
-STDCALL
-LsaAddPrivilegesToAccount (VOID)
+/*
+LsaAddPrivilegesToAccount
+LsaDeleteTrustedDomain
+LsaEnumeratePrivilegesOfAccount
+LsaGetQuotasForAccount
+LsaGetSystemAccessAccount
+LsaGetUserName
+LsaSetQuotasForAccount
+LsaSetSystemAccessAccount
+*/
+NTSTATUS STDCALL _LsaStub2(ULONG_PTR Unknown0, ULONG_PTR Unknown1)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaClearAuditLog
- */
-INT
-STDCALL
-LsaClearAuditLog (VOID)
+/*
+LsaLookupPrivilegeName
+LsaLookupPrivilegeValue
+LsaQueryInfoTrustedDomain
+LsaQueryInformationPolicy
+LsaQuerySecurityObject
+LsaRemovePrivilegesFromAccount
+LsaRetrievePrivateData
+LsaSetInformationPolicy
+LsaSetInformationTrustedDomain
+LsaSetSecret
+LsaSetSecurityObject
+LsaStorePrivateData
+*/
+NTSTATUS STDCALL _LsaStub3
+(
+ ULONG_PTR Unknown0,
+ ULONG_PTR Unknown1,
+ ULONG_PTR Unknown2
+)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaClose
- */
-INT
-STDCALL
-LsaClose (VOID)
+/*
+LsaAddAccountRights
+LsaCreateAccount
+LsaCreateSecret
+LsaCreateTrustedDomain
+LsaEnumerateAccountRights
+LsaEnumerateAccountsWithUserRight
+LsaLookupPrivilegeDisplayName
+LsaOpenAccount
+LsaOpenPolicy
+LsaOpenSecret
+LsaOpenTrustedDomain
+LsaSetTrustedDomainInformation
+*/
+NTSTATUS STDCALL _LsaStub4
+(
+ ULONG_PTR Unknown0,
+ ULONG_PTR Unknown1,
+ ULONG_PTR Unknown2,
+ ULONG_PTR Unknown3
+)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaCreateAccount
- */
-INT
-STDCALL
-LsaCreateAccount (
-	DWORD	Unknown0,
-	DWORD	Unknown1,
-	DWORD	Unknown2,
-	DWORD	Unknown3	
-	)
+/*
+LsaEnumerateAccounts
+LsaEnumeratePrivileges
+LsaEnumerateTrustedDomains
+LsaLookupNames
+LsaLookupSids
+LsaQuerySecret
+LsaQueryTrustedDomainInfo
+LsaRemoveAccountRights
+*/
+NTSTATUS STDCALL _LsaStub5
+(
+ ULONG_PTR Unknown0,
+ ULONG_PTR Unknown1,
+ ULONG_PTR Unknown2,
+ ULONG_PTR Unknown3,
+ ULONG_PTR Unknown4
+)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaCreateSecret
- */
-INT
-STDCALL
-LsaCreateSecret (VOID)
+/*
+LsaICLookupNames
+LsaICLookupSids
+*/
+NTSTATUS STDCALL _LsaStub8
+(
+ ULONG_PTR Unknown0,
+ ULONG_PTR Unknown1,
+ ULONG_PTR Unknown2,
+ ULONG_PTR Unknown3,
+ ULONG_PTR Unknown4,
+ ULONG_PTR Unknown5,
+ ULONG_PTR Unknown6,
+ ULONG_PTR Unknown7
+)
 {
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
+ return STATUS_NOT_IMPLEMENTED;
 }
 
-
-/**********************************************************************
- *	LsaCreateTrustedDomain
- */
-INT
-STDCALL
-LsaCreateTrustedDomain (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaDelete
- */
-INT
-STDCALL
-LsaDelete (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaDeleteTrustedDomain
- */
-INT
-STDCALL
-LsaDeleteTrustedDomain (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumerateAccountRights
- */
-INT
-STDCALL
-LsaEnumerateAccountRights (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumerateAccounts
- */
-INT
-STDCALL
-LsaEnumerateAccounts (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumerateAccountsWithUserRight
- */
-INT
-STDCALL
-LsaEnumerateAccountsWithUserRight (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumeratePrivileges
- */
-INT
-STDCALL
-LsaEnumeratePrivileges (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumeratePrivilegesOfAccount
- */
-INT
-STDCALL
-LsaEnumeratePrivilegesOfAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaEnumerateTrustedDomains
- */
-INT
-STDCALL
-LsaEnumerateTrustedDomains (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaFreeMemory
- */
-INT
-STDCALL
-LsaFreeMemory (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaGetQuotasForAccount
- */
-INT
-STDCALL
-LsaGetQuotasForAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaGetSystemAccessAccount
- */
-INT
-STDCALL
-LsaGetSystemAccessAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaGetUserName
- */
-INT
-STDCALL
-LsaGetUserName (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaICLookupNames
- */
-INT
-STDCALL
-LsaICLookupNames (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaICLookupSids
- */
-INT
-STDCALL
-LsaICLookupSids (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaLookupNames
- */
-INT
-STDCALL
-LsaLookupNames (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaLookupPrivilegeDisplayName
- */
-INT
-STDCALL
-LsaLookupPrivilegeDisplayName (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaLookupPrivilegeName
- */
-INT
-STDCALL
-LsaLookupPrivilegeName (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaLookupPrivilegeValue
- */
-INT
-STDCALL
-LsaLookupPrivilegeValue (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaLookupSids
- */
-INT
-STDCALL
-LsaLookupSids (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaNtStatusToWinError
- */
-INT
-STDCALL
-LsaNtStatusToWinError (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaOpenAccount
- */
-INT
-STDCALL
-LsaOpenAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaOpenPolicy
- */
-INT
-STDCALL
-LsaOpenPolicy (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaOpenSecret
- */
-INT
-STDCALL
-LsaOpenSecret (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaOpenTrustedDomain
- */
-INT
-STDCALL
-LsaOpenTrustedDomain (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaQueryInfoTrustedDomain
- */
-INT
-STDCALL
-LsaQueryInfoTrustedDomain (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaQueryInformationPolicy
- */
-INT
-STDCALL
-LsaQueryInformationPolicy (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaQuerySecret
- */
-INT
-STDCALL
-LsaQuerySecret (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaQuerySecurityObject
- */
-INT
-STDCALL
-LsaQuerySecurityObject (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaQueryTrustedDomainInfo
- */
-INT
-STDCALL
-LsaQueryTrustedDomainInfo (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaRemoveAccountRights
- */
-INT
-STDCALL
-LsaRemoveAccountRights (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaRemovePrivilegesFromAccount
- */
-INT
-STDCALL
-LsaRemovePrivilegesFromAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaRetrievePrivateData
- */
-INT
-STDCALL
-LsaRetrievePrivateData (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetInformationPolicy
- */
-INT
-STDCALL
-LsaSetInformationPolicy (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetInformationTrustedDomain
- */
-INT
-STDCALL
-LsaSetInformationTrustedDomain (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetQuotasForAccount
- */
-INT
-STDCALL
-LsaSetQuotasForAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetSecret
- */
-INT
-STDCALL
-LsaSetSecret (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetSecurityObject
- */
-INT
-STDCALL
-LsaSetSecurityObject (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetSystemAccessAccount
- */
-INT
-STDCALL
-LsaSetSystemAccessAccount (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaSetTrustedDomainInformation
- */
-INT
-STDCALL
-LsaSetTrustedDomainInformation (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
-
-
-/**********************************************************************
- *	LsaStorePrivateData
- */
-INT
-STDCALL
-LsaStorePrivateData (VOID)
-{
-	SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-	return 0;
-}
 
 
 /* EOF */
