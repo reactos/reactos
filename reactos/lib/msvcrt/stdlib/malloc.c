e0fbe100b8edeef91cef90b6203ad718e27cfafb
Jason Filby <jason.filby@gmail.com>
2003-02-21 21:50:04 +0000

    New and delete operators
    svn path=/trunk/; revision=4188

--- reactos/lib/msvcrt/stdlib/malloc.c
+++ reactos/lib/msvcrt/stdlib/malloc.c
@@ -3,6 +3,28 @@
 
 extern HANDLE hHeap;
 
+typedef void (*MSVCRT_new_handler_func)(unsigned long size);
+static MSVCRT_new_handler_func MSVCRT_new_handler;
+
+/* ??2@YAPAXI@Z (MSVCRT.@) */
+void* MSVCRT_operator_new(unsigned long size)
+{
+  void *retval = HeapAlloc(GetProcessHeap(), 0, size);
+
+/*  FIXME: LOCK_HEAP; */
+  if(!retval && MSVCRT_new_handler)
+    (*MSVCRT_new_handler)(size);
+/*  FIXME: UNLOCK_HEAP; */
+
+  return retval;
+}
+
+/* ??3@YAXPAX@Z (MSVCRT.@) */
+void MSVCRT_operator_delete(void *mem)
+{
+  HeapFree(GetProcessHeap(), 0, mem);
+}
+
 void* malloc(size_t _size)
 {
    return HeapAlloc(hHeap, HEAP_ZERO_MEMORY, _size);
@@ -19,6 +41,6 @@ void* calloc(size_t _nmemb, size_t _size)
 }
 
 void* realloc(void* _ptr, size_t _size)
-{ 
+{
    return HeapReAlloc(hHeap, 0, _ptr, _size);
 }
