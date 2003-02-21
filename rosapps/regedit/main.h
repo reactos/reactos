9f3fb17e6e0f0f3b55eed278122ae8c4716f44c0
Robert Dickenson <robd@reactos.org>
2003-02-21 14:04:55 +0000

    minor mod to main header to attempt to fix mingw version problem.
    svn path=/trunk/; revision=4182

--- rosapps/regedit/main.h
+++ rosapps/regedit/main.h
@@ -76,7 +76,7 @@ extern TCHAR szTitle[];
 extern TCHAR szFrameClass[];
 extern TCHAR szChildClass[];
 
-#if __MINGW32_MAJOR_VERSION == 1
+#if __MINGW32_MAJOR_VERSION < 1
 typedef struct tagNMITEMACTIVATE{
     NMHDR   hdr;
     int     iItem;
