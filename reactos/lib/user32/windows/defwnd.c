19af3dbdde6d157b018953e0c4f0a4367982449a
Richard Campbell <betam4x@gmail.com>
2003-03-12 16:36:18 +0000

    *** empty log message ***
    svn path=/trunk/; revision=4295

--- reactos/lib/user32/windows/defwnd.c
+++ reactos/lib/user32/windows/defwnd.c
@@ -1,4 +1,4 @@
-/* $Id: defwnd.c,v 1.33 2003/03/12 16:27:32 rcampbell Exp $
+/* $Id: defwnd.c,v 1.34 2003/03/12 16:36:18 rcampbell Exp $
  *
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS user32.dll
@@ -319,10 +319,10 @@ static void UserDrawCaptionNC( HDC hDC, RECT *rect, HWND hWnd,
 {
     RECT r = *rect;
     char buffer[256];
-
+    /* REWRITE IN PROGRESS:  Don't touch -- rcampbell -- 03-12-2003 */
     /* Implement and Use DrawCaption() */
     SelectObject( hDC, GetSysColorBrush(active ? COLOR_ACTIVECAPTION : COLOR_INACTIVECAPTION) );
-    PatBlt(hDC,rect->left + GetSystemMetrics(SM_CYFIXEDFRAME), rect->top + 3, rect->right - (GetSystemMetrics(SM_CYFIXEDFRAME) * 2), rect->bottom - 1, PATCOPY );
+    PatBlt(hDC,rect->left + GetSystemMetrics(SM_CXFIXEDFRAME), rect->top + 3, rect->right - (GetSystemMetrics(SM_CXFIXEDFRAME) * 2), rect->bottom -  1, PATCOPY );
     
     if (style & WS_SYSMENU)
     {
