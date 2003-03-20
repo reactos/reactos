ea5140f3c04aaee5b665757b19e23e8fc5e50caa
Mark Tempel <mtempel@visi.com>
2003-03-20 03:03:39 +0000

    Moved implementation of Rectangle to here. Added Polygon
    svn path=/trunk/; revision=4360

---
+++ reactos/lib/gdi32/objects/fillshap.c
@@ -0,0 +1,28 @@
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
+#undef WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <ddk/ntddk.h>
+#include <win32k/kapi.h>
+
+BOOL
+STDCALL
+Polygon(HDC	hDC,
+	CONST POINT	*lpPoints,
+	int	nCount)
+{
+	return W32kPolygon(hDC, (CONST PPOINT)lpPoints, nCount);
+}
+
+BOOL
+STDCALL
+Rectangle(HDC  hDC,
+	int  LeftRect,
+	int  TopRect,
+	int  RightRect,
+	int  BottomRect)
+{
+   return W32kRectangle(hDC, LeftRect, TopRect, RightRect, BottomRect);
+}
