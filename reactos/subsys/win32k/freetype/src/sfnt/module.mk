1d1253f73477edfe1ecf045fcd4ae17d3bac566a
Gé van Geldorp <ge@gse.nl>
2003-04-01 08:55:51 +0000

    Changed location of FreeType to lib/freetype
    svn path=/trunk/; revision=4467

--- reactos/subsys/win32k/freetype/src/sfnt/module.mk
+++
@@ -1,7 +0,0 @@
-make_module_list: add_sfnt_module
-
-add_sfnt_module:
-	$(OPEN_DRIVER)sfnt_module_class$(CLOSE_DRIVER)
-	$(ECHO_DRIVER)sfnt      $(ECHO_DRIVER_DESC)helper module for TrueType & OpenType formats$(ECHO_DRIVER_DONE)
-
-# EOF
--- reactos/subsys/win32k/freetype/src/smooth/module.mk
+++
@@ -1,7 +0,0 @@
-make_module_list: add_smooth_renderer
-
-add_smooth_renderer:
-	$(OPEN_DRIVER)ft_smooth_renderer_class$(CLOSE_DRIVER)
-	$(ECHO_DRIVER)smooth    $(ECHO_DRIVER_DESC)anti-aliased bitmap renderer$(ECHO_DRIVER_DONE)
-
-# EOF
