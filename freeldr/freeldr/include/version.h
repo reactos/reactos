d44d6e392a95854da8a71b9fdd340a0cc8798e74
Casper Hornstrup <chorns@users.sourceforge.net>
2003-04-05 15:28:45 +0000

    Added DOS compatible Master Boot Record (MBR)
    svn path=/trunk/; revision=4497

--- freeldr/freeldr/include/version.h
+++ freeldr/freeldr/include/version.h
@@ -22,7 +22,7 @@
 
 
 /* just some stuff */
-#define VERSION			"FreeLoader v1.8.3"
+#define VERSION			"FreeLoader v1.8.4"
 #define COPYRIGHT		"Copyright (C) 1998-2003 Brian Palmer <brianp@sginet.com>"
 #define AUTHOR_EMAIL	"<brianp@sginet.com>"
 #define BY_AUTHOR		"by Brian Palmer"
@@ -36,7 +36,7 @@
 //
 #define FREELOADER_MAJOR_VERSION	1
 #define FREELOADER_MINOR_VERSION	8
-#define FREELOADER_PATCH_VERSION	3
+#define FREELOADER_PATCH_VERSION	4
 
 
 PUCHAR	GetFreeLoaderVersionString(VOID);
