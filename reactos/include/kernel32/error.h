b9269e59c82700c88de99b5fa316a109bcb09d9b
KJK::Hyperion <hackbunny@reactos.org>
2003-04-26 00:25:01 +0000

    SetLastErrorByStatus() now a macro, for binary compatibility with Windows
    svn path=/trunk/; revision=4583

--- reactos/include/kernel32/error.h
+++ reactos/include/kernel32/error.h
@@ -1,12 +1,11 @@
 #ifndef _INCLUDE_KERNEL32_H
 #define _INCLUDE_KERNEL32_H
-/* $Id: error.h,v 1.3 2002/09/08 10:22:30 chorns Exp $ */
+/* $Id: error.h,v 1.4 2003/04/26 00:25:01 hyperion Exp $ */
 #include <windows.h>
 #define NTOS_MODE_USER
 #include <ntos.h>
-DWORD
-STDCALL
-SetLastErrorByStatus (
-	NTSTATUS	Status
-	);
+
+#define SetLastErrorByStatus(__S__) \
+ ((void)SetLastError(RtlNtStatusToDosError(__S__)))
+
 #endif /* _INCLUDE_KERNEL32_H */
