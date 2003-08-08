529277d521a2ddf59ef2284d63fb1d8a1b9ea26c
Hartmut Birr <osexpert@googlemail.com>
2003-08-08 00:49:24 +0000

    - Fixed some type problems if cmd is compiled as unicode.
    svn path=/trunk/; revision=5466

--- reactos/subsys/system/cmd/del.c
+++ reactos/subsys/system/cmd/del.c
@@ -339,7 +339,7 @@ INT CommandDelete (LPTSTR cmd, LPTSTR param)
 		else
 			ConOutPrintf (_T("    %lu file%s deleted\n"),
 			              dwFiles,
-			              (dwFiles == 1) ? "" : "s");
+			              (dwFiles == 1) ? _T("") : _T("s"));
 	}
 
 	return 0;
