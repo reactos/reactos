18ebc095c1d10dcfbc6cd5b48a231ecc547a1c16
Royce Mitchell III <royce3@ev1.net>
2003-08-12 21:08:14 +0000

    fixed build error
    svn path=/trunk/; revision=5546

--- reactos/subsys/win32k/stubs/xpstubs.c
+++ reactos/subsys/win32k/stubs/xpstubs.c
@@ -3,10 +3,19 @@
  * in Windows XP and beyond ( i.e. a low priority for us right now )
  */
 
+#undef WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <ddk/ntddk.h>
+#include <stdlib.h>
+#include <win32k/bitmaps.h>
+#include <win32k/debug.h>
+#include <debug.h>
+#include <ddk/winddi.h>
+#include "../eng/objects.h"
+#include <include/error.h>
 
 #define STUB(x) void x(void) { DbgPrint("WIN32K: Stub for %s\n", #x); }
+
+typedef PVOID EVENT, *PEVENT;
 
 BOOL
 STDCALL
