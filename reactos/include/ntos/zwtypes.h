12d437f94ed3022ea3caf9a16075de0498d6a990
David Welch <welch@cwcom.net>
2003-08-20 00:01:30 +0000

    - Forgot this file - sorry.
    svn path=/trunk/; revision=5686

--- reactos/include/ntos/zwtypes.h
+++ reactos/include/ntos/zwtypes.h
@@ -3,6 +3,31 @@
 
 #ifndef __USE_W32API
 
+typedef struct _LDT_ENTRY {
+  WORD LimitLow;
+  WORD BaseLow;
+  union {
+    struct {
+      BYTE BaseMid;
+      BYTE Flags1;
+      BYTE Flags2;
+      BYTE BaseHi;
+    } Bytes;
+    struct {
+      DWORD BaseMid : 8;
+      DWORD Type : 5;
+      DWORD Dpl : 2;
+      DWORD Pres : 1;
+      DWORD LimitHi : 4;
+      DWORD Sys : 1;
+      DWORD Reserved_0 : 1;
+      DWORD Default_Big : 1;
+      DWORD Granularity : 1;
+      DWORD BaseHi : 8;
+    } Bits;
+  } HighWord;
+} LDT_ENTRY, *PLDT_ENTRY, *LPLDT_ENTRY;
+
 typedef enum _THREAD_STATE {
 	StateInitialized,
 	StateReady,
