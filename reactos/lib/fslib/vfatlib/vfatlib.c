5f04c08330d8b30ab2fefa0731cb298c88d5e54e
Eric Kohl <eric.kohl@reactos.org>
2003-08-21 15:33:48 +0000

    Added FAT16 format code.
    svn path=/trunk/; revision=5721

--- reactos/lib/fslib/vfatlib/vfatlib.c
+++ reactos/lib/fslib/vfatlib/vfatlib.c
@@ -29,14 +29,6 @@ GetShiftCount(ULONG Value)
 }
 
 
-NTSTATUS
-VfatInitialize()
-{
-  DPRINT("VfatInitialize()\n");
-
-  return STATUS_SUCCESS;
-}
-
 static NTSTATUS
 VfatWriteBootSector(IN HANDLE FileHandle,
   IN PFAT32_BOOT_SECTOR BootSector)
@@ -321,6 +313,166 @@ VfatWriteRootDirectory(IN HANDLE FileHandle,
 }
 
 
+static NTSTATUS
+VfatFormatFat32 (HANDLE  FileHandle,
+		 PPARTITION_INFORMATION  PartitionInfo,
+		 PDISK_GEOMETRY DiskGeometry,
+		 PUNICODE_STRING Label,
+		 BOOL  QuickFormat,
+		 DWORD  ClusterSize,
+		 PFMIFSCALLBACK  Callback)
+{
+  FAT32_BOOT_SECTOR BootSector;
+  ANSI_STRING VolumeLabel;
+  ULONG RootDirSectors;
+  ULONG TmpVal1;
+  ULONG TmpVal2;
+  ULONG TmpVal3;
+  NTSTATUS Status;
+
+  /* Calculate cluster size */
+  if (ClusterSize == 0)
+    {
+      if (PartitionInfo->PartitionLength.QuadPart < 8ULL * 1024ULL * 1024ULL * 1024ULL)
+	{
+	  /* Partition < 8GB ==> 4KB Cluster */
+	  ClusterSize = 4096;
+	}
+      else if (PartitionInfo->PartitionLength.QuadPart < 16ULL * 1024ULL * 1024ULL * 1024ULL)
+	{
+	  /* Partition 8GB - 16GB ==> 8KB Cluster */
+	  ClusterSize = 8192;
+	}
+      else if (PartitionInfo->PartitionLength.QuadPart < 32ULL * 1024ULL * 1024ULL * 1024ULL)
+	{
+	  /* Partition 16GB - 32GB ==> 16KB Cluster */
+	  ClusterSize = 16384;
+	}
+      else
+	{
+	  /* Partition >= 32GB ==> 32KB Cluster */
+	  ClusterSize = 32768;
+	}
+    }
+
+  memset(&BootSector, 0, sizeof(FAT32_BOOT_SECTOR));
+  memcpy(&BootSector.OEMName[0], "MSWIN4.1", 8);
+  BootSector.BytesPerSector = DiskGeometry->BytesPerSector;
+  BootSector.SectorsPerCluster = ClusterSize / BootSector.BytesPerSector;
+  BootSector.ReservedSectors = 32;
+  BootSector.FATCount = 2;
+  BootSector.RootEntries = 0;
+  BootSector.Sectors = 0;
+  BootSector.Media = 0xf8;
+  BootSector.FATSectors = 0;
+  BootSector.SectorsPerTrack = DiskGeometry->SectorsPerTrack;
+  BootSector.Heads = DiskGeometry->TracksPerCylinder;
+  BootSector.HiddenSectors = PartitionInfo->HiddenSectors;
+  BootSector.SectorsHuge = PartitionInfo->PartitionLength.QuadPart >>
+    GetShiftCount(BootSector.BytesPerSector); /* Use shifting to avoid 64-bit division */
+  BootSector.FATSectors32 = 0; /* Set later */
+  BootSector.ExtFlag = 0; /* Mirror all FATs */
+  BootSector.FSVersion = 0x0000; /* 0:0 */
+  BootSector.RootCluster = 2;
+  BootSector.FSInfoSector = 1;
+  BootSector.BootBackup = 6;
+  BootSector.Drive = 0xff; /* No BIOS boot drive available */
+  BootSector.ExtBootSignature = 0x29;
+  BootSector.VolumeID = 0x45768798; /* FIXME: */
+  if ((Label == NULL) || (Label->Buffer == NULL))
+    {
+      memcpy(&BootSector.VolumeLabel[0], "NO NAME    ", 11);
+    }
+  else
+    {
+      RtlUnicodeStringToAnsiString(&VolumeLabel, Label, TRUE);
+      memset(&BootSector.VolumeLabel[0], ' ', 11);
+      memcpy(&BootSector.VolumeLabel[0], VolumeLabel.Buffer,
+        VolumeLabel.Length < 11 ? VolumeLabel.Length : 11);
+      RtlFreeAnsiString(&VolumeLabel);
+    }
+  memcpy(&BootSector.SysType[0], "FAT32   ", 8);
+
+  RootDirSectors = ((BootSector.RootEntries * 32) +
+    (BootSector.BytesPerSector - 1)) / BootSector.BytesPerSector;
+  TmpVal1 = BootSector.SectorsHuge - (BootSector.ReservedSectors + RootDirSectors);
+  TmpVal2 = (256 * BootSector.SectorsPerCluster) + BootSector.FATCount;
+  if (TRUE /* FAT32 */)
+    {
+      TmpVal2 = 0;
+      do
+        {
+          if (TmpVal2 == 0)
+	    {
+	      TmpVal3 = 0xffffffff;
+	    }
+	  else
+	    {
+	      TmpVal3 = TmpVal2;
+	    }
+          TmpVal2 = ((TmpVal1 - TmpVal2 * BootSector.FATCount) / BootSector.SectorsPerCluster) + 2;
+	  TmpVal2 = (sizeof(ULONG) * TmpVal2 + BootSector.BytesPerSector - 1) / BootSector.BytesPerSector;
+        }
+      while (TmpVal3 > TmpVal2);
+      BootSector.FATSectors32 = TmpVal2;
+    }
+
+  Status = VfatWriteBootSector(FileHandle,
+    &BootSector);
+  if (!NT_SUCCESS(Status))
+    {
+      DPRINT("VfatWriteBootSector() failed with status 0x%.08x\n", Status);
+      return Status;
+    }
+
+  Status = VfatWriteFsInfo(FileHandle,
+    &BootSector);
+  if (!NT_SUCCESS(Status))
+    {
+      DPRINT("VfatWriteFsInfo() failed with status 0x%.08x\n", Status);
+      return Status;
+    }
+
+  /* Write first FAT copy */
+  Status = VfatWriteFAT(FileHandle,
+    0,
+    &BootSector);
+  if (!NT_SUCCESS(Status))
+    {
+      DPRINT("VfatWriteFAT() failed with status 0x%.08x\n", Status);
+      return Status;
+    }
+
+  /* Write second FAT copy */
+  Status = VfatWriteFAT(FileHandle,
+    BootSector.FATSectors32,
+    &BootSector);
+  if (!NT_SUCCESS(Status))
+    {
+      DPRINT("VfatWriteFAT() failed with status 0x%.08x.\n", Status);
+      return Status;
+    }
+
+  Status = VfatWriteRootDirectory(FileHandle,
+    &BootSector);
+  if (!NT_SUCCESS(Status))
+    {
+      DPRINT("VfatWriteRootDirectory() failed with status 0x%.08x\n", Status);
+    }
+
+  return Status;
+}
+
+
+NTSTATUS
+VfatInitialize()
+{
+  DPRINT("VfatInitialize()\n");
+
+  return STATUS_SUCCESS;
+}
+
+
 NTSTATUS
 VfatFormat(
 	PUNICODE_STRING  DriveRoot,
@@ -336,13 +488,7 @@ VfatFormat(
   HANDLE FileHandle;
   SCSI_ADDRESS ScsiAddress;
   PARTITION_INFORMATION PartitionInfo;
-  FAT32_BOOT_SECTOR BootSector;
-  ANSI_STRING VolumeLabel;
   NTSTATUS Status;
-  ULONG RootDirSectors;
-  ULONG TmpVal1;
-  ULONG TmpVal2;
-  ULONG TmpVal3;
 
   DPRINT("VfatFormat(DriveRoot '%S')\n", DriveRoot->Buffer);
 
@@ -428,146 +574,23 @@ VfatFormat(
 
   if (PartitionInfo.PartitionLength.QuadPart < 512ULL * 1024ULL * 1024ULL)
     {
-      DPRINT1("FIXME: Partition size is smaller than 512MB - use FAT16\n");
-      NtClose(FileHandle);
-      return STATUS_UNSUCCESSFUL;
-    }
-
-
-  /* Calculate cluster size */
-  if (ClusterSize == 0)
-    {
-      if (PartitionInfo.PartitionLength.QuadPart < 8ULL * 1024ULL * 1024ULL * 1024ULL)
-	{
-	  /* Partition < 8GB ==> 4KB Cluster */
-	  ClusterSize = 4096;
-	}
-      else if (PartitionInfo.PartitionLength.QuadPart < 16ULL * 1024ULL * 1024ULL * 1024ULL)
-	{
-	  /* Partition 8GB - 16GB ==> 8KB Cluster */
-	  ClusterSize = 8192;
-	}
-      else if (PartitionInfo.PartitionLength.QuadPart < 32ULL * 1024ULL * 1024ULL * 1024ULL)
-	{
-	  /* Partition 16GB - 32GB ==> 16KB Cluster */
-	  ClusterSize = 16384;
-	}
-      else
-	{
-	  /* Partition >= 32GB ==> 32KB Cluster */
-	  ClusterSize = 32768;
-	}
-    }
-
-  memset(&BootSector, 0, sizeof(FAT32_BOOT_SECTOR));
-  memcpy(&BootSector.OEMName[0], "MSWIN4.1", 8);
-  BootSector.BytesPerSector = DiskGeometry.BytesPerSector;
-  BootSector.SectorsPerCluster = ClusterSize / BootSector.BytesPerSector;
-  BootSector.ReservedSectors = 32;
-  BootSector.FATCount = 2;
-  BootSector.RootEntries = 0;
-  BootSector.Sectors = 0;
-  BootSector.Media = 0xf8;
-  BootSector.FATSectors = 0;
-  BootSector.SectorsPerTrack = DiskGeometry.SectorsPerTrack;
-  BootSector.Heads = DiskGeometry.TracksPerCylinder;
-  BootSector.HiddenSectors = DiskGeometry.SectorsPerTrack; //PartitionInfo.HiddenSectors;
-  BootSector.SectorsHuge = PartitionInfo.PartitionLength.QuadPart >>
-    GetShiftCount(BootSector.BytesPerSector); /* Use shifting to avoid 64-bit division */
-  BootSector.FATSectors32 = 0; /* Set later */
-  BootSector.ExtFlag = 0; /* Mirror all FATs */
-  BootSector.FSVersion = 0x0000; /* 0:0 */
-  BootSector.RootCluster = 2;
-  BootSector.FSInfoSector = 1;
-  BootSector.BootBackup = 6;
-  BootSector.Drive = 0xff; /* No BIOS boot drive available */
-  BootSector.ExtBootSignature = 0x29;
-  BootSector.VolumeID = 0x45768798; /* FIXME: */
-  if ((Label == NULL) || (Label->Buffer == NULL))
-    {
-      memcpy(&BootSector.VolumeLabel[0], "NO NAME    ", 11);
+      Status = Fat16Format (FileHandle,
+			    &PartitionInfo,
+			    &DiskGeometry,
+			    Label,
+			    QuickFormat,
+			    ClusterSize,
+			    Callback);
     }
   else
     {
-      RtlUnicodeStringToAnsiString(&VolumeLabel, Label, TRUE);
-      memset(&BootSector.VolumeLabel[0], ' ', 11);
-      memcpy(&BootSector.VolumeLabel[0], VolumeLabel.Buffer,
-        VolumeLabel.Length < 11 ? VolumeLabel.Length : 11);
-      RtlFreeAnsiString(&VolumeLabel);
-    }
-  memcpy(&BootSector.SysType[0], "FAT32   ", 8);
-
-  RootDirSectors = ((BootSector.RootEntries * 32) +
-    (BootSector.BytesPerSector - 1)) / BootSector.BytesPerSector;
-  TmpVal1 = BootSector.SectorsHuge - (BootSector.ReservedSectors + RootDirSectors);
-  TmpVal2 = (256 * BootSector.SectorsPerCluster) + BootSector.FATCount;
-  if (TRUE /* FAT32 */)
-    {
-      TmpVal2 = 0;
-      do
-        {
-          if (TmpVal2 == 0)
-	    {
-	      TmpVal3 = 0xffffffff;
-	    }
-	  else
-	    {
-	      TmpVal3 = TmpVal2;
-	    }
-          TmpVal2 = ((TmpVal1 - TmpVal2 * BootSector.FATCount) / BootSector.SectorsPerCluster) + 2;
-	  TmpVal2 = (sizeof(ULONG) * TmpVal2 + BootSector.BytesPerSector - 1) / BootSector.BytesPerSector;
-        }
-      while (TmpVal3 > TmpVal2);
-      BootSector.FATSectors32 = TmpVal2;
-    }
-
-  Status = VfatWriteBootSector(FileHandle,
-    &BootSector);
-  if (!NT_SUCCESS(Status))
-    {
-      DPRINT("VfatWriteBootSector() failed with status 0x%.08x\n", Status);
-      NtClose(FileHandle);
-      return Status;
-    }
-
-  Status = VfatWriteFsInfo(FileHandle,
-    &BootSector);
-  if (!NT_SUCCESS(Status))
-    {
-      DPRINT("VfatWriteFsInfo() failed with status 0x%.08x\n", Status);
-      NtClose(FileHandle);
-      return Status;
-    }
-
-  /* Write first FAT copy */
-  Status = VfatWriteFAT(FileHandle,
-    0,
-    &BootSector);
-  if (!NT_SUCCESS(Status))
-    {
-      DPRINT("VfatWriteFAT() failed with status 0x%.08x\n", Status);
-      NtClose(FileHandle);
-      return Status;
-    }
-
-  /* Write second FAT copy */
-  Status = VfatWriteFAT(FileHandle,
-    BootSector.FATSectors32,
-    &BootSector);
-  if (!NT_SUCCESS(Status))
-    {
-      DPRINT("VfatWriteFAT() failed with status 0x%.08x.\n", Status);
-      NtClose(FileHandle);
-      return Status;
-    }
-
-  Status = VfatWriteRootDirectory(FileHandle,
-    &BootSector);
-  if (!NT_SUCCESS(Status))
-    {
-      DPRINT("VfatWriteRootDirectory() failed with status 0x%.08x\n", Status);
-      NtClose(FileHandle);
-      return Status;
+      Status = VfatFormatFat32 (FileHandle,
+				&PartitionInfo,
+				&DiskGeometry,
+				Label,
+				QuickFormat,
+				ClusterSize,
+				Callback);
     }
 
   NtClose(FileHandle);
--- reactos/lib/fslib/vfatlib/vfatlib.h
+++ reactos/lib/fslib/vfatlib/vfatlib.h
@@ -11,6 +11,35 @@
 
 #define SECTORSIZE 512
 
+typedef struct _FAT16_BOOT_SECTOR
+{
+  unsigned char  magic0;                      // 0
+  unsigned char  res0;                        // 1
+  unsigned char  magic1;                      // 2
+  unsigned char  OEMName[8];                  // 3
+  unsigned short BytesPerSector;              // 11
+  unsigned char  SectorsPerCluster;           // 13
+  unsigned short ReservedSectors;             // 14
+  unsigned char  FATCount;                    // 16
+  unsigned short RootEntries;                 // 17
+  unsigned short Sectors;                     // 19
+  unsigned char  Media;                       // 21
+  unsigned short FATSectors;                  // 22
+  unsigned short SectorsPerTrack;             // 24
+  unsigned short Heads;                       // 26
+  unsigned long  HiddenSectors;               // 28
+  unsigned long  SectorsHuge;                 // 32
+  unsigned char  Drive;                       // 36
+  unsigned char  Res1;                        // 37
+  unsigned char  ExtBootSignature;            // 38
+  unsigned long  VolumeID;                    // 39
+  unsigned char  VolumeLabel[11];             // 43
+  unsigned char  SysType[8];                  // 54
+  unsigned char  Res2[446];                   // 62
+  unsigned long  Signature1;                  // 508
+} __attribute__((packed)) FAT16_BOOT_SECTOR, *PFAT16_BOOT_SECTOR;
+
+
 typedef struct _FAT32_BOOT_SECTOR
 {
   unsigned char  magic0;                      // 0
@@ -56,3 +85,13 @@ typedef struct _FAT32_FSINFO
   unsigned long  Res2[3];          // 496
   unsigned long  TrailSig;         // 508
 } __attribute__((packed)) FAT32_FSINFO, *PFAT32_FSINFO;
+
+
+NTSTATUS
+Fat16Format (HANDLE  FileHandle,
+	     PPARTITION_INFORMATION  PartitionInfo,
+	     PDISK_GEOMETRY DiskGeometry,
+	     PUNICODE_STRING Label,
+	     BOOL  QuickFormat,
+	     DWORD  ClusterSize,
+	     PFMIFSCALLBACK  Callback);
