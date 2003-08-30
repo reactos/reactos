e9170f21ca7721b911c57e9076850932253f1f7c
nj <nj@svn.reactos.org>
2003-08-30 22:14:42 +0000

    Version 0.1.3
    svn path=/trunk/; revision=5948

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.14 2003/07/02 21:10:16 gvg Exp $
+/* $Id: version.h,v 1.15 2003/08/30 22:14:42 nj Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -19,9 +19,9 @@
 
 #define KERNEL_VERSION_MAJOR		0
 #define KERNEL_VERSION_MINOR		1
-#define KERNEL_VERSION_PATCH_LEVEL	2
+#define KERNEL_VERSION_PATCH_LEVEL	3
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
-#define KERNEL_RELEASE_DATE		20030702L
+#define KERNEL_RELEASE_DATE		20030830L
 
 
 #endif
