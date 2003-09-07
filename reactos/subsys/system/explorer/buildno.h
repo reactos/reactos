61009dcbf69a80401cb704e95a4eaef98b5fb4f0
Martin Fuchs <fuchs.martin@gmail.com>
2003-09-07 22:00:02 +0000

    Version 0.1.3.7
    svn path=/trunk/; revision=6010

--- reactos/subsys/system/explorer/buildno.h
+++ reactos/subsys/system/explorer/buildno.h
@@ -1,10 +1,10 @@
 /* Do not edit - Machine generated */
 #ifndef _INC_REACTOS_BUILDNO
 #define _INC_REACTOS_BUILDNO
-#define KERNEL_VERSION_BUILD	2
-#define KERNEL_VERSION_BUILD_STR	"2"
-#define KERNEL_RELEASE_RC	"0.1.3.2\0"
-#define KERNEL_RELEASE_STR	"0.1.3.2"
+#define KERNEL_VERSION_BUILD	7
+#define KERNEL_VERSION_BUILD_STR	"7"
+#define KERNEL_RELEASE_RC	"0.1.3.7\0"
+#define KERNEL_RELEASE_STR	"0.1.3.7"
 #define KERNEL_VERSION_RC	"0.1.3\0"
 #define KERNEL_VERSION_STR	"0.1.3"
 #endif
