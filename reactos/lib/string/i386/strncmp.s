b834d75fbe4b76d145a854d4673a34745285763a
The ReactOS Team <ros-dev@reactos.org>
2003-09-20 23:37:56 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.71.12'.
    svn path=/branches/unlabeled-1.71.12/; revision=6106

--- reactos/lib/string/i386/strncmp.s
+++
@@ -1,6 +0,0 @@
-/* $Id: strncmp.s,v 1.2 2003/07/06 23:04:19 hyperion Exp $
- */
-
-#include "tcsncmp.h"
-
-/* EOF */
--- reactos/lib/string/wcscat.c
+++
@@ -1,8 +0,0 @@
-/* $Id: wcscat.c,v 1.2 2003/07/06 23:04:19 hyperion Exp $
- */
-
-#define _UNICODE
-#include <wchar.h>
-#include "tcscat.h"
-
-/* EOF */
