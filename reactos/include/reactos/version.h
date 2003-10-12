c21ca7f81c1a5e07e368f29a329dabdcfec2bd97
Gé van Geldorp <ge@gse.nl>
2003-10-12 14:01:30 +0000

    Bump version number to 0.1.4
    svn path=/trunk/; revision=6293

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.15 2003/08/30 22:14:42 nj Exp $
+/* $Id: version.h,v 1.16 2003/10/12 14:01:30 gvg Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -19,9 +19,9 @@
 
 #define KERNEL_VERSION_MAJOR		0
 #define KERNEL_VERSION_MINOR		1
-#define KERNEL_VERSION_PATCH_LEVEL	3
+#define KERNEL_VERSION_PATCH_LEVEL	4
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
-#define KERNEL_RELEASE_DATE		20030830L
+#define KERNEL_RELEASE_DATE		20031011L
 
 
 #endif
