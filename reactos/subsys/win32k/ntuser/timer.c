f09774d1ba06857cb8f61afa0673f304341ac4d5
Thomas Bluemel <thomas@reactsoft.com>
2003-10-15 19:28:57 +0000

    fixed NtUserSetSystemTimer()
    svn path=/trunk/; revision=6328

--- reactos/subsys/win32k/ntuser/timer.c
+++ reactos/subsys/win32k/ntuser/timer.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: timer.c,v 1.12 2003/10/15 13:39:09 weiden Exp $
+/* $Id: timer.c,v 1.13 2003/10/15 19:28:57 weiden Exp $
  *
  * COPYRIGHT:        See COPYING in the top level directory
  * PROJECT:          ReactOS kernel
@@ -632,7 +632,7 @@ NtUserSetSystemTimer(
  TIMERPROC lpTimerFunc
 )
 {
-  return IntSetTimer(hWnd, nIDEvent, uElapse, lpTimerFunc, FALSE);
+  return IntSetTimer(hWnd, nIDEvent, uElapse, lpTimerFunc, TRUE);
 }
 
 
