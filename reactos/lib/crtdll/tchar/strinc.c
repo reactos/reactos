e157092513949bf09e865b2f76971d8e3a06dd0a
The ReactOS Team <ros-dev@reactos.org>
2003-11-17 02:12:52 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.120.8'.
    svn path=/branches/unlabeled-1.120.8/; revision=6674

--- reactos/lib/crtdll/tchar/strinc.c
+++
@@ -1,9 +0,0 @@
-#include <msvcrt/string.h>
-
-/*
- * @implemented
- */
-char * _strinc(const char *str) 
-{ 
-	return (char *)(++str); 
-}
--- reactos/lib/string/i386/wcscmp.s
+++
@@ -1,7 +0,0 @@
-/* $Id: wcscmp.s,v 1.2 2003/07/06 23:04:19 hyperion Exp $
- */
-
-#define _UNICODE
-#include "tcscmp.h"
-
-/* EOF */
