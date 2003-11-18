3a5a7572a9d439f03988fabe1d7f1479c910cb94
nj <nj@svn.reactos.org>
2003-11-18 19:19:47 +0000

    Updated to version 0.1.5
    svn path=/branches/ros-branch-0_1_5/; revision=6688

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.16 2003/10/12 14:01:30 gvg Exp $
+/* $Id: version.h,v 1.16.2.1 2003/11/18 19:19:47 nj Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -19,9 +19,9 @@
 
 #define KERNEL_VERSION_MAJOR		0
 #define KERNEL_VERSION_MINOR		1
-#define KERNEL_VERSION_PATCH_LEVEL	4
+#define KERNEL_VERSION_PATCH_LEVEL	5
 /* Edit each time a new release is out: format is YYYYMMDD (UTC) */
-#define KERNEL_RELEASE_DATE		20031011L
+#define KERNEL_RELEASE_DATE		20031109L
 
 
 #endif
