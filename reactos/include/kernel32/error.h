3d93ac09f7429611f0fe5ec6295e44e321f6f4bb
Royce Mitchell III <royce3@ev1.net>
2003-11-18 05:09:17 +0000

    fix circular-dependency include
    svn path=/trunk/; revision=6682

--- reactos/include/kernel32/error.h
+++ reactos/include/kernel32/error.h
@@ -1,9 +1,12 @@
 #ifndef _INCLUDE_KERNEL32_H
 #define _INCLUDE_KERNEL32_H
-/* $Id: error.h,v 1.4 2003/04/26 00:25:01 hyperion Exp $ */
+/* $Id: error.h,v 1.5 2003/11/18 05:09:17 royce Exp $ */
 #include <windows.h>
 #define NTOS_MODE_USER
-#include <ntos.h>
+#ifndef _NTOS_H
+#error you must include <ntos.h> before you can include kernel32/error.h
+/*#include <ntos.h>*/
+#endif/*_NTOS_H*/
 
 #define SetLastErrorByStatus(__S__) \
  ((void)SetLastError(RtlNtStatusToDosError(__S__)))
