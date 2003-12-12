e4ca173e647ca4a8e14eb1dc065d9a16f29e852b
Gé van Geldorp <ge@gse.nl>
2003-12-12 14:57:23 +0000

    Rollback changes which were checked in by accident
    svn path=/trunk/; revision=6966

--- reactos/lib/shlwapi/reg.c
+++ reactos/lib/shlwapi/reg.c
@@ -19,11 +19,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifdef DBG
-#undef DBG
-#define DBG 1
-#endif
-
 #include <stdarg.h>
 #include <string.h>
 #ifdef __REACTOS__
