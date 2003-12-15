3a99900c9d21b92bfc35a3a9163d041939d582bc
Steven Edwards <winehacker@gmail.com>
2003-12-15 20:21:13 +0000

    Add some basic type information.
    svn path=/trunk/; revision=7081

--- reactos/include/wine/port.h
+++ reactos/include/wine/port.h
@@ -6,7 +6,18 @@
 #ifndef _ROS_WINE_PORT
 #define _ROS_WINE_PORT
 
-typedef short     INT16;
+typedef signed char      INT8, *PINT8;
+typedef signed short     INT16, *PINT16;
+typedef signed int       INT32, *PINT32;
+typedef unsigned char    UINT8, *PUINT8;
+typedef unsigned short   UINT16, *PUINT16;
+typedef unsigned int     UINT32, *PUINT32;
+typedef signed int       LONG32, *PLONG32;
+typedef unsigned int     ULONG32, *PULONG32;
+typedef unsigned int     DWORD32, *PDWORD32;
+typedef INT16     *LPINT16;
+typedef UINT16    *LPUINT16;
+
 #define HFILE_ERROR ((HFILE)-1) /* Already in winbase.h - ros is fubar */
 
 #define strncasecmp strncmp
