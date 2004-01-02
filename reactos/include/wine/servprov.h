1fc3cf8187e6622bc5f7b0e662fcb9525dcde17c
Gé van Geldorp <ge@gse.nl>
2004-01-02 21:29:50 +0000

    Allow buidling with current CVS w32api
    svn path=/trunk/; revision=7414

--- reactos/include/wine/servprov.h
+++ reactos/include/wine/servprov.h
@@ -7,6 +7,8 @@
 
 DEFINE_GUID(IID_IServiceProvider, 0x6d5140c1, 0x7436, 0x11ce, 0x80,0x34, 0x00,0xaa,0x00,0x60,0x09,0xfa);
 
+#if (__W32API_MAJOR_VERSION < 2 || __W32API_MINOR_VERSION < 5)
+
 /*** IUnknown methods ***/
 #define IServiceProvider_QueryInterface(p,a,b) (p)->lpVtbl->QueryInterface(p,a,b)
 #define IServiceProvider_AddRef(p) (p)->lpVtbl->AddRef(p)
@@ -14,5 +16,6 @@ DEFINE_GUID(IID_IServiceProvider, 0x6d5140c1, 0x7436, 0x11ce, 0x80,0x34, 0x00,0x
 /*** IServiceProvider methods ***/
 #define IServiceProvider_QueryService(p,a,b,c) (p)->lpVtbl->QueryService(p,a,b,c)
 
+#endif /* __W32API_MAJOR_VERSION < 2 || __W32API_MINOR_VERSION < 5 */
 
 #endif  /* __WINE_SERVPROV_H */
