bc0b751b2bc569b77620da9d6b7497c577f2bcc9
Filip Navara <filip.navara@gmail.com>
2004-01-13 17:18:33 +0000

    Initial version of generic framebuffer display driver.
    svn path=/trunk/; revision=7607

---
+++ reactos/drivers/video/displays/framebuf/framebuf.h
@@ -0,0 +1,140 @@
+/*
+ * ReactOS Generic Framebuffer display driver
+ *
+ * Copyright (C) 2004 Filip Navara
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef FRAMEBUF_H
+#define FRAMEBUF_H
+
+#include <windef.h>
+#include <wingdi.h>
+#include <ddk/winddi.h>
+#include <ddk/ntddvdeo.h>
+
+typedef struct _PDEV
+{
+   HANDLE hDriver;
+   HDEV hDevEng;
+   HSURF hSurfEng;
+   ULONG ModeIndex;
+   ULONG ScreenWidth;
+   ULONG ScreenHeight;
+   ULONG ScreenDelta;
+   BYTE BitsPerPixel;
+   ULONG RedMask;
+   ULONG GreenMask;
+   ULONG BlueMask;
+   BYTE PaletteShift;
+   PVOID ScreenPtr;
+   HPALETTE DefaultPalette;
+   PALETTEENTRY *PaletteEntries;
+} PDEV, *PPDEV;
+
+#define DEVICE_NAME	L"framebuf"
+#define ALLOC_TAG	TAG('F','B','U','F')
+
+DHPDEV STDCALL
+DrvEnablePDEV(
+   IN DEVMODEW *pdm,
+   IN LPWSTR pwszLogAddress,
+   IN ULONG cPat,
+   OUT HSURF *phsurfPatterns,
+   IN ULONG cjCaps,
+   OUT ULONG *pdevcaps,
+   IN ULONG cjDevInfo,
+   OUT DEVINFO *pdi,
+   IN HDEV hdev,
+   IN LPWSTR pwszDeviceName,
+   IN HANDLE hDriver);
+
+VOID STDCALL
+DrvCompletePDEV(
+   IN DHPDEV dhpdev,
+   IN HDEV hdev);
+
+VOID STDCALL
+DrvDisablePDEV(
+   IN DHPDEV dhpdev);
+
+HSURF STDCALL
+DrvEnableSurface(
+   IN DHPDEV dhpdev);
+
+VOID STDCALL
+DrvDisableSurface(
+   IN DHPDEV dhpdev);
+
+BOOL STDCALL
+DrvAssertMode(
+   IN DHPDEV dhpdev,
+   IN BOOL bEnable);
+
+ULONG STDCALL
+DrvGetModes(
+   IN HANDLE hDriver,
+   IN ULONG cjSize,
+   OUT DEVMODEW *pdm);
+
+BOOL STDCALL
+DrvSetPalette(
+   IN DHPDEV dhpdev,
+   IN PALOBJ *ppalo,
+   IN FLONG fl,
+   IN ULONG iStart,
+   IN ULONG cColors);
+
+ULONG STDCALL
+DrvSetPointerShape(
+   IN SURFOBJ *pso,
+   IN SURFOBJ *psoMask,
+   IN SURFOBJ *psoColor,
+   IN XLATEOBJ *pxlo,
+   IN LONG xHot,
+   IN LONG yHot,
+   IN LONG x,
+   IN LONG y,
+   IN RECTL *prcl,
+   IN FLONG fl);
+
+VOID STDCALL
+DrvMovePointer(
+   IN SURFOBJ *pso,
+   IN LONG x,
+   IN LONG y,
+   IN RECTL *prcl);
+
+BOOL FASTCALL
+IntInitScreenInfo(
+   PPDEV ppdev,
+   LPDEVMODEW pDevMode,
+   PGDIINFO pGdiInfo,
+   PDEVINFO pDevInfo);
+
+BOOL FASTCALL
+IntInitDefaultPalette(
+   PPDEV ppdev,
+   PDEVINFO pDevInfo);
+
+BOOL DDKAPI
+IntSetPalette(
+   IN DHPDEV dhpdev,
+   IN PPALETTEENTRY ppalent,
+   IN ULONG iStart,
+   IN ULONG cColors);
+
+#endif /* FRAMEBUF_H */
