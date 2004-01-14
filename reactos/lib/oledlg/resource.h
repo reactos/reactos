fc98d1568663d6bdf5dca257b41d02352e09315a
Steven Edwards <winehacker@gmail.com>
2004-01-14 01:55:00 +0000

    Ported oledlg.dll from WINE
    svn path=/trunk/; revision=7619

---
+++ reactos/lib/oledlg/resource.h
@@ -0,0 +1,40 @@
+/*
+ * Definitions for OLE dialog resources
+ *
+ * Copyright 2003 Ulrich Czekalla for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define IDS_RESULTOBJDESC               101
+#define IDS_RESULTFILEOBJDESC           102
+#define IDS_BROWSE                      103
+#define IDS_NOTOLEMOD                   104
+#define IDS_NOTOLEMODCAPTION            105
+
+#define UIINSERTOBJECT                  129
+
+#define IDC_OBJTYPELIST                 1000
+#define IDC_RESULT                      1001
+#define IDC_CREATENEW                   1002
+#define IDC_CREATECONTROL               1003
+#define IDC_CREATEFROMFILE              1004
+#define IDC_OBJTYPELBL                  1005
+#define IDC_RESULTDESC                  1006
+#define IDC_ADDCONTROL                  1007
+#define IDC_ASICON                      1008
+#define IDC_BROWSE                      1009
+#define IDC_FILELBL                     1010
+#define IDC_FILE                        1011
