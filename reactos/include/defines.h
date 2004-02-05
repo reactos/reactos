1b0cdbf59cb8f7ad81e39f27686047701cbf7737
Thomas Bluemel <thomas@reactsoft.com>
2004-02-05 19:14:32 +0000

    added missing definitions
    svn path=/trunk/; revision=8039

--- reactos/include/defines.h
+++ reactos/include/defines.h
@@ -3091,6 +3091,7 @@ extern "C" {
 #define PSH_RTLREADING	(2048)
 #define PSCB_INITIALIZED	(1)
 #define PSCB_PRECREATE	(2)
+#define PSCB_BUTTONPRESSED	(3)
 
 /* PSN_APPLY message */
 #define PSNRET_NOERROR	(0)
@@ -4941,6 +4942,22 @@ extern "C" {
 #define FreeProcInstance(p) (p)
 
 #define _fmemcpy memcpy
+
+/* Control Panel */
+#define WM_CPL_LAUNCH (WM_USER+1000)
+#define WM_CPL_LAUNCHED (WM_USER+1001)
+#define CPL_DYNAMIC_RES 0
+#define CPL_INIT 1
+#define CPL_GETCOUNT 2
+#define CPL_INQUIRE 3
+#define CPL_SELECT 4
+#define CPL_DBLCLK 5
+#define CPL_STOP 6
+#define CPL_EXIT 7
+#define CPL_NEWINQUIRE 8
+#define CPL_STARTWPARMSA 9
+#define CPL_STARTWPARMSW 10
+#define CPL_SETUP 200
 
 /* Used by wxwindows. */
 #define SIZEFULLSCREEN	SIZE_MAXIMIZED
