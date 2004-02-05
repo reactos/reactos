5e2e1b620bb9648a0aca83f6cc5e0b3fae136a22
Royce Mitchell III <royce3@ev1.net>
2004-02-05 16:11:19 +0000

    TEB and ICD table offsets and ICD table structure
    svn path=/trunk/; revision=8037

---
+++ reactos/lib/opengl32/icdtable.h
@@ -0,0 +1,21 @@
+// icdtable.h
+
+#ifndef OPENGL32_PRIVATE_ICDTABLE_H
+#define OPENGL32_PRIVATE_ICDTABLE_H
+
+enum icdoffsets_e
+{
+	ICDIDX_INVALID = -1,
+#define ICD_ENTRY(x) ICDIDX_##x,
+#include "icdlist.h"
+#undef ICD_ENTRY
+	ICDIDX_COUNT
+};
+
+struct ICDTable
+{
+	DWORD	num_funcs; // Normally 336 (0x150)
+	PROC	dispatch_table[812];
+};
+
+#endif//OPENGL32_PRIVATE_ICDTABLE_H
