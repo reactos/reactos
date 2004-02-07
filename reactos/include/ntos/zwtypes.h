03aba396882b09dc907df08d0e672c8b2796fd13
Filip Navara <filip.navara@gmail.com>
2004-02-07 03:56:18 +0000

    - Minor correction to FILE_FULL_EA_INFORMATION prototype
    svn path=/trunk/; revision=8071

--- reactos/include/ntos/zwtypes.h
+++ reactos/include/ntos/zwtypes.h
@@ -801,7 +801,7 @@ typedef struct _FILE_FULL_EA_INFORMATION
 	UCHAR Flags;
 	UCHAR EaNameLength;
 	USHORT EaValueLength;
-	CHAR  EaName[0];
+	CHAR  EaName[1];
 } FILE_FULL_EA_INFORMATION, *PFILE_FULL_EA_INFORMATION;
 
 
