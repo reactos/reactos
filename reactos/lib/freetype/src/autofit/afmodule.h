88b0aebe1a69851e6b88723655ac3c251fcff3bc
Filip Navara <filip.navara@gmail.com>
2004-02-07 00:11:02 +0000

    - Fixed compilation of the updated freetype
    svn path=/trunk/; revision=8068

---
+++ reactos/lib/freetype/src/autofit/afmodule.h
@@ -0,0 +1,16 @@
+#ifndef __AFMODULE_H__
+#define __AFMODULE_H__
+
+#include <ft2build.h>
+#include FT_MODULE_H
+
+
+FT_BEGIN_HEADER
+
+  FT_CALLBACK_TABLE
+  const FT_Module_Class  autofit_module_class;
+
+
+FT_END_HEADER
+
+#endif /* __AFMODULE_H__ */
---
+++ reactos/lib/freetype/src/autofit/autofit.c
@@ -0,0 +1,8 @@
+#define FT_MAKE_OPTION_SINGLE_OBJECT
+#include <ft2build.h>
+#include "afangles.c"
+#include "afglobal.c"
+#include "afhints.c"
+#include "aflatin.c"
+#include "afloader.c"
+#include "afmodule.c"
