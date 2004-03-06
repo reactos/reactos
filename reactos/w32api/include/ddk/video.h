de9089e707873af8eae01c627546391faf282639
David Welch <welch@cwcom.net>
2004-03-06 22:27:55 +0000

    - Added padding to align VIDEO_PORT_CONFIG_INFO.VdmPhysicalVideoMemoryAddress and subsequent members the same as visualc.
    svn path=/trunk/; revision=8554

--- reactos/w32api/include/ddk/video.h
+++ reactos/w32api/include/ddk/video.h
@@ -169,6 +169,7 @@ typedef struct _VIDEO_PORT_CONFIG_INFO {
   ULONG  NumEmulatorAccessEntries;
   PEMULATOR_ACCESS_ENTRY  EmulatorAccessEntries;
   ULONG_PTR  EmulatorAccessEntriesContext;
+  PVOID Reserved0;
   PHYSICAL_ADDRESS  VdmPhysicalVideoMemoryAddress;
   ULONG  VdmPhysicalVideoMemoryLength;
   ULONG  HardwareStateSize;
