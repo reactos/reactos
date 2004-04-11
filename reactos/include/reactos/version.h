45267ce58da40e36eca31193eec788881b4c0dbb
Gé van Geldorp <ge@gse.nl>
2004-04-11 09:57:18 +0000

    Bump version number
    svn path=/trunk/; revision=9080

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.20 2004/02/20 23:24:30 gvg Exp $
+/* $Id: version.h,v 1.21 2004/04/11 09:57:18 gvg Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -19,7 +19,7 @@
 
 #define KERNEL_VERSION_MAJOR		0
 #define KERNEL_VERSION_MINOR		2
-#define KERNEL_VERSION_PATCH_LEVEL	2
+#define KERNEL_VERSION_PATCH_LEVEL	3
 
 /* KERNEL_VERSION_BUILD_TYPE is L"CVS", L"RC1", L"RC2" or L"FINAL" */
 #define KERNEL_VERSION_BUILD_TYPE	L"CVS"
