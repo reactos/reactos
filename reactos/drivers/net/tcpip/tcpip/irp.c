a7d110e4d0b16195f188ba8f2e2375b5d72e7881
Art Yerkes <art.yerkes@gmail.com>
2004-05-29 04:41:08 +0000

    Added common irp handling.
    svn path=/branches/arty_20040329/; revision=9533

---
+++ reactos/drivers/net/tcpip/tcpip/irp.c
@@ -0,0 +1,29 @@
+/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ReactOS TCP/IP protocol driver
+ * FILE:        tcpip/dispatch.h
+ * PURPOSE:     TDI dispatch routines
+ * PROGRAMMERS: Casper S. Hornstrup (chorns@users.sourceforge.net)
+ * REVISIONS:
+ *   CSH 01/08-2000 Created
+ * TODO:        Validate device object in all dispatch routines
+ */
+#include <roscfg.h>
+#include <tcpip.h>
+#include <dispatch.h>
+#include <routines.h>
+#include <datagram.h>
+#include <info.h>
+
+NTSTATUS IRPFinish( PIRP Irp, NTSTATUS Status ) {
+    if( Status == STATUS_PENDING ) {
+	IoMarkIrpPending( Irp );
+    } 
+
+    Irp->IoStatus.Status = Status;
+
+    IoCompleteRequest( Irp, IO_NETWORK_INCREMENT );
+
+    return Status;
+}
+
