0d5d9a4b7d74bab748b1167acc9af38c031b723d
Casper Hornstrup <chorns@users.sourceforge.net>
2004-05-31 12:21:24 +0000

    2004-05-31  Casper S. Hornstrup  <chorns@users.sourceforge.net>
    	* include/reactos/version.h (KERNEL_VERSION_BUILD_TYPE): Replace possible
    	value FINAL with RELEASE.
    
    svn path=/trunk/; revision=9568

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.21 2004/04/11 09:57:18 gvg Exp $
+/* $Id: version.h,v 1.22 2004/05/31 12:21:24 chorns Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -21,7 +21,7 @@
 #define KERNEL_VERSION_MINOR		2
 #define KERNEL_VERSION_PATCH_LEVEL	3
 
-/* KERNEL_VERSION_BUILD_TYPE is L"CVS", L"RC1", L"RC2" or L"FINAL" */
+/* KERNEL_VERSION_BUILD_TYPE is L"CVS", L"RC1", L"RC2" or L"RELEASE" */
 #define KERNEL_VERSION_BUILD_TYPE	L"CVS"
 
 
