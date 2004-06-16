5995e56ce270f1c81097920f87d09c4fd74cd776
Filip Navara <filip.navara@gmail.com>
2004-06-16 21:49:29 +0000

    - Change build type to RC1 on release branch.
    svn path=/branches/ros-branch-0_2_3/; revision=9692

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.22 2004/05/31 12:21:24 chorns Exp $
+/* $Id: version.h,v 1.22.2.1 2004/06/16 21:49:29 navaraf Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -22,7 +22,7 @@
 #define KERNEL_VERSION_PATCH_LEVEL	3
 
 /* KERNEL_VERSION_BUILD_TYPE is L"CVS", L"RC1", L"RC2" or L"RELEASE" */
-#define KERNEL_VERSION_BUILD_TYPE	L"CVS"
+#define KERNEL_VERSION_BUILD_TYPE	L"RC1"
 
 
 #endif
