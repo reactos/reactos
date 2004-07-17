13bc74184a66db6fe4f6592a42a0a85b26a5b27f
Gregor Anich <blight@blight.eu.org>
2004-07-17 15:12:25 +0000

    Handle escape to get opengl driver info in IntEngExtEscape to avoid the infinite loop in the UNIMPLEMENTED macro (needed by opengl32.dll)
    svn path=/trunk/; revision=10176

--- reactos/subsys/win32k/objects/print.c
+++ reactos/subsys/win32k/objects/print.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: print.c,v 1.20 2004/07/14 20:48:58 navaraf Exp $ */
+/* $Id: print.c,v 1.21 2004/07/17 15:12:25 blight Exp $ */
 #include <w32k.h>
 
 INT
@@ -65,6 +65,9 @@ IntEngExtEscape(
    INT      OutSize,
    LPVOID   OutData)
 {
+   if (Escape == 0x1101) /* Get OpenGL driver name */
+      return 0; /* use default driver */
+
    UNIMPLEMENTED;
    return -1;
 }
