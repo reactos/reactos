054fcf9a85a745a8a02518b34cb55403e2cf5b95
Alex Ionescu <aionescu@gmail.com>
2004-08-05 19:03:24 +0000

    Fixed Names.
    svn path=/trunk/; revision=10405

--- reactos/lib/rtl/unicodeprefix.c
+++ reactos/lib/rtl/unicodeprefix.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: unicodeprfx.c,v 1.1 2004/08/05 18:17:37 ion Exp $
+/* $Id: unicodeprefix.c,v 1.1 2004/08/05 19:03:24 ion Exp $
  *
  * COPYRIGHT:         See COPYING in the top level directory
  * PROJECT:           ReactOS system libraries
