846acbcf70973e3cbe1fea9d5cac6b2e5e19a062
Art Yerkes <art.yerkes@gmail.com>
2004-08-08 22:07:06 +0000

    Added a (C) notice.
    svn path=/trunk/; revision=10439

--- reactos/tools/ms2ps/ms2ps.cpp
+++ reactos/tools/ms2ps/ms2ps.cpp
@@ -1,3 +1,29 @@
+/*
+ *  ReactOS kernel
+ *  Copyright (C) 2004 ReactOS Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+/* $Id: ms2ps.cpp,v 1.2 2004/08/08 22:07:06 arty Exp $
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS seh conversion tool
+ * FILE:            tools/ms2ps/ms2ps.cpp
+ * PURPOSE:         Conversion tool from msvc to pseh style seh
+ * PROGRAMMER:      Art Yerkes
+ */
+
 #include <iostream>
 #include <vector>
 #include <list>
