c4b330cce6af62cd107f5c16b87de35cd4c4006d
Thomas Bluemel <thomas@reactsoft.com>
2004-08-10 11:54:43 +0000

    split up headers
    svn path=/trunk/; revision=10469

--- reactos/include/rosrtl/string.h
+++ reactos/include/rosrtl/string.h
@@ -1,4 +1,4 @@
-/* $Id: string.h,v 1.3 2004/08/10 10:57:54 weiden Exp $
+/* $Id: string.h,v 1.4 2004/08/10 11:54:43 weiden Exp $
  */
 
 #ifndef ROSRTL_STRING_H__
@@ -45,31 +45,6 @@ extern "C"
 NTSTATUS NTAPI RosAppendUnicodeString( PUNICODE_STRING ResultFirst,
 				       PUNICODE_STRING Second,
 				       BOOL Deallocate );
-
-int
-RosLenOfStrResource(HINSTANCE hInst, UINT uID);
-int
-RosAllocAndLoadStringA(LPSTR *lpTarget, HINSTANCE hInst, UINT uID);
-int
-RosAllocAndLoadStringW(LPWSTR *lpTarget, HINSTANCE hInst, UINT uID);
-DWORD
-RosFormatStrA(LPSTR *lpTarget, LPSTR lpFormat, ...);
-DWORD
-RosFormatStrW(LPWSTR *lpTarget, LPWSTR lpFormat, ...);
-DWORD
-RosLoadAndFormatStrA(HINSTANCE hInst, UINT uID, LPSTR *lpTarget, ...);
-DWORD
-RosLoadAndFormatStrW(HINSTANCE hInst, UINT uID, LPWSTR *lpTarget, ...);
-
-#ifdef UNICODE
-# define RosFmtString RosFmtStringW
-# define RosAllocAndLoadString RosAllocAndLoadStringW
-# define RosLoadAndFormatStr RosLoadAndFormatStrW
-#else
-# define RosFmtString RosFmtStringA
-# define RosAllocAndLoadString RosAllocAndLoadStringA
-# define RosLoadAndFormatStr RosLoadAndFormatStrA
-#endif
 
 #ifdef __cplusplus
 }
