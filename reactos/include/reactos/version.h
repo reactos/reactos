509a299316a04f3691d93167eb61bfc34623ecbc
Robert Kopferl <robertk@mok.lvcm.com>
2004-08-26 20:36:11 +0000

    changed back to CVS, false br
    svn path=/trunk/; revision=10698

--- reactos/include/reactos/version.h
+++ reactos/include/reactos/version.h
@@ -1,4 +1,4 @@
-/* $Id: version.h,v 1.24 2004/08/26 20:24:40 robertk Exp $
+/* $Id: version.h,v 1.25 2004/08/26 20:36:11 robertk Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS kernel
@@ -22,7 +22,7 @@
 #define KERNEL_VERSION_PATCH_LEVEL	4
 
 /* KERNEL_VERSION_BUILD_TYPE is L"CVS", L"RC1", L"RC2" or L"RELEASE" */
-#define KERNEL_VERSION_BUILD_TYPE	L"RC1"
+#define KERNEL_VERSION_BUILD_TYPE	L"CVS"
 
 
 #endif
