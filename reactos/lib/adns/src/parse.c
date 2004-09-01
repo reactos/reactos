500044ccc860936a48b11561c0435edbacafa728
Art Yerkes <art.yerkes@gmail.com>
2004-09-01 02:33:50 +0000

    Corrected compile error due to different enum used in prototype.
    svn path=/trunk/; revision=10765

--- reactos/lib/adns/src/parse.c
+++ reactos/lib/adns/src/parse.c
@@ -115,7 +115,7 @@ adns_status adns__findlabel_next(findlabel_state *fls,
 }
 
 adns_status adns__parse_domain(adns_state ads, int serv, adns_query qu,
-			       vbuf *vb, adns_queryflags flags,
+			       vbuf *vb, parsedomain_flags flags,
 			       const byte *dgram, int dglen, int *cbyte_io, int max) {
   findlabel_state fls;
   
