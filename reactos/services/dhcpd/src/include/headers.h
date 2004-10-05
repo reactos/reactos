528acaf7b34f9bfc6c3fa4c21a329fa9e7ede528
Steven Edwards <winehacker@gmail.com>
2004-10-05 13:09:46 +0000

    Added a simple embedded GPL dhcp server for ReactOS by Carlos Manuel Duclos Vergara
    TODO:
    Make it a real Window Service
    Make it use the registry rather than dhcpd.conf
    .......
    
    svn path=/trunk/; revision=11198

---
+++ reactos/services/dhcpd/src/include/headers.h
@@ -0,0 +1,23 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#ifdef  __MINGW32__
+
+#include <winsock.h>
+
+#else
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#endif
+
+#endif
