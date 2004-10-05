7f50dbe3fe7f8a8578d720e30b011911578ce323
Gé van Geldorp <ge@gse.nl>
2004-10-05 07:43:19 +0000

    Alexandre Julliard <julliard@winehq.org> - Fixed typo in ProgramsW. - Changed the case of windows and system directories to match the   directories we create by default. - Avoid depending on the non-standard IUnknown_METHODS macro in Wine   internal headers.
    svn path=/trunk/; revision=11195

--- reactos/lib/shell32/shellfolder.h
+++ reactos/lib/shell32/shellfolder.h
@@ -40,13 +40,18 @@ typedef struct ISFHelper *LPISFHELPER;
  */
 
 #define INTERFACE ISFHelper
-#define ISFHelper_METHODS \
-	IUnknown_METHODS \
-	STDMETHOD(GetUniqueName)(THIS_ LPSTR  lpName, UINT  uLen) PURE; \
-	STDMETHOD(AddFolder)(THIS_ HWND  hwnd, LPCSTR  lpName, LPITEMIDLIST * ppidlOut) PURE; \
-	STDMETHOD(DeleteItems)(THIS_ UINT  cidl, LPCITEMIDLIST * apidl) PURE; \
-	STDMETHOD(CopyItems)(THIS_ IShellFolder * pSFFrom, UINT  cidl, LPCITEMIDLIST * apidl) PURE;
-DECLARE_INTERFACE_(ISFHelper, IUnknown) { ISFHelper_METHODS };
+DECLARE_INTERFACE_(ISFHelper,IUnknown)
+{
+    /*** IUnknown methods ***/
+    STDMETHOD_(HRESULT,QueryInterface)(THIS_ REFIID riid, void** ppvObject) PURE;
+    STDMETHOD_(ULONG,AddRef)(THIS) PURE;
+    STDMETHOD_(ULONG,Release)(THIS) PURE;
+    /*** ISFHelper methods ***/
+    STDMETHOD(GetUniqueName)(THIS_ LPSTR  lpName, UINT  uLen) PURE;
+    STDMETHOD(AddFolder)(THIS_ HWND  hwnd, LPCSTR  lpName, LPITEMIDLIST * ppidlOut) PURE;
+    STDMETHOD(DeleteItems)(THIS_ UINT  cidl, LPCITEMIDLIST * apidl) PURE;
+    STDMETHOD(CopyItems)(THIS_ IShellFolder * pSFFrom, UINT  cidl, LPCITEMIDLIST * apidl) PURE;
+};
 #undef INTERFACE
 
 #ifdef COBJMACROS
