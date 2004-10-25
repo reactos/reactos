59f2e2567ab7ce21f7a38dee12546d56c0ca7be7
Alex Ionescu <aionescu@gmail.com>
2004-10-25 01:10:20 +0000

    Fix the mess CVS did
    svn path=/branches/new_headers/; revision=11431

--- reactos/w32api/include/reactos/roscfg.h
+++
@@ -1,7 +0,0 @@
-/* Automatically generated, Edit the Makefile to change configuration */
-#ifndef __INCLUDE_CONFIG_H
-#define __INCLUDE_CONFIG_H
-#define UP
-#define PREPARE_TESTS
-#define CONFIG "UP"
-#endif /* __INCLUDE_CONFIG_H */
