b2a213a80cd810373029c019914ce3a6d3cc9d46
Eric Kohl <eric.kohl@reactos.org>
2004-10-30 12:33:51 +0000

    Add Regional Options (Intl) control panel application.
    svn path=/trunk/; revision=11473

---
+++ reactos/lib/cpl/intl/resource.h
@@ -0,0 +1,19 @@
+#ifndef __CPL_RESOURCE_H
+#define __CPL_RESOURCE_H
+
+
+#define IDC_CPLICON		1
+
+#define IDD_GENERALPAGE		100
+#define IDD_NUMBERSPAGE		101
+#define IDD_CURRENCYPAGE	102
+#define IDD_TIMEPAGE		103
+#define IDD_DATEPAGE		104
+#define IDD_LOCALEPAGE		105
+
+#define IDS_CPLNAME		1000
+#define IDS_CPLDESCRIPTION	1001
+
+#endif /* __CPL_RESOURCE_H */
+
+/* EOF */
