2296e99d5649dedb0b2479892a38604b3346ed5e
Thomas Bluemel <thomas@reactsoft.com>
2004-11-05 22:36:36 +0000

    1. export all psapi stubs 2. added missing psapi declarations to w32api
    svn path=/trunk/; revision=11545

--- reactos/lib/psapi/misc/stubs.c
+++ reactos/lib/psapi/misc/stubs.c
@@ -1,19 +1,33 @@
-/* $Id: stubs.c,v 1.7 2004/11/03 22:43:00 weiden Exp $ */
+/* $Id: stubs.c,v 1.8 2004/11/05 22:36:36 weiden Exp $ */
 #include "precomp.h"
 
 #define NDEBUG
 #include <debug.h>
 
-#if 0
 /*
  * @unimplemented
  */
 BOOL
 STDCALL
-EnumPageFiles(PENUM_PAGE_CALLBACKW pCallbackRoutine,
-              LPVOID lpContext)
+EnumPageFilesA(PENUM_PAGE_FILE_CALLBACKA pCallbackRoutine,
+               LPVOID lpContext)
 {
-  SetLastError(ERROR_INVALID_FUNCTION);
+  DPRINT1("PSAPI: EnumPageFilesA is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*
+ * @unimplemented
+ */
+BOOL
+STDCALL
+EnumPageFilesW(PENUM_PAGE_FILE_CALLBACKW pCallbackRoutine,
+               LPVOID lpContext)
+{
+  DPRINT1("PSAPI: EnumPageFilesW is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
   return FALSE;
 }
 
@@ -26,10 +40,10 @@ STDCALL
 GetPerformanceInfo(PPERFORMANCE_INFORMATION pPerformanceInformation,
                    DWORD cb)
 {
-  SetLastError(ERROR_INVALID_FUNCTION);
+  DPRINT1("PSAPI: GetPerformanceInfo is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
   return FALSE;
 }
-#endif
 
 
 /*
@@ -41,7 +55,8 @@ GetProcessMemoryInfo(HANDLE Process,
                      PPROCESS_MEMORY_COUNTERS ppsmemCounters,
                      DWORD cb)
 {
-  SetLastError(ERROR_INVALID_FUNCTION);
+  DPRINT1("PSAPI: GetProcessMemoryInfo is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
   return FALSE;
 }
 
@@ -55,8 +70,39 @@ QueryWorkingSet(HANDLE hProcess,
                 PVOID pv,
                 DWORD cb)
 {
-  SetLastError(ERROR_INVALID_FUNCTION);
+  DPRINT1("PSAPI: QueryWorkingSet is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
   return FALSE;
+}
+
+
+/*
+ * @unimplemented
+ */
+DWORD
+STDCALL
+GetProcessImageFileNameW(HANDLE hProcess,
+                         LPWSTR lpImageFileName,
+                         DWORD nSize)
+{
+  DPRINT1("PSAPI: GetProcessImageFileNameW is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return 0;
+}
+
+
+/*
+ * @unimplemented
+ */
+DWORD
+STDCALL
+GetProcessImageFileNameA(HANDLE hProcess,
+                         LPSTR lpImageFileName,
+                         DWORD nSize)
+{
+  DPRINT1("PSAPI: GetProcessImageFileNameA is UNIMPLEMENTED!\n");
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return 0;
 }
 
 /* EOF */
