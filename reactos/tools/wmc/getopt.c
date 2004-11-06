6b9538eb5407ba8b24cdfe1d66507e5f0a7687de
Steven Edwards <winehacker@gmail.com>
2004-11-06 21:06:52 +0000

    include stdlib.h, string.h and ctype.h to fix a build problem I am seeing.
    svn path=/trunk/; revision=11566

--- reactos/tools/wmc/getopt.c
+++ reactos/tools/wmc/getopt.c
@@ -40,6 +40,9 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
