7cbb0d7e6e76d040958d1b2a71919bdf2888a180
Gregor Anich <blight@blight.eu.org>
2004-12-17 13:19:44 +0000

    Remove unused defines and structures.
    svn path=/trunk/; revision=12164

--- reactos/include/ntos/kdbgsyms.h
+++ reactos/include/ntos/kdbgsyms.h
@@ -1,34 +1,10 @@
-
 #ifndef __KDBGSYMS_H
 #define __KDBGSYMS_H
 
 #include <ddk/ntddk.h>
 
-#define ST_FILENAME	0x00
-#define ST_FUNCTION	0x01
-#define ST_LINENUMBER	0x02
-
-typedef struct _SYMBOL
-{
-  struct _SYMBOL *Next;
-  /* Address relative to module base address */
-  ULONG RelativeAddress;
-  ULONG SymbolType;
-  ANSI_STRING Name;
-  ULONG LineNumber;
-} SYMBOL, *PSYMBOL;
-
-typedef struct _SYMBOL_TABLE
-{
-  ULONG SymbolCount;
-  PSYMBOL Symbols;
-} SYMBOL_TABLE, *PSYMBOL_TABLE;
-
 typedef struct _IMAGE_SYMBOL_INFO
 {
-  SYMBOL_TABLE FileNameSymbols;
-  SYMBOL_TABLE FunctionSymbols;
-  SYMBOL_TABLE LineNumberSymbols;
   ULONG_PTR ImageBase;
   ULONG_PTR ImageSize;
   PVOID FileBuffer;
@@ -38,8 +14,5 @@ typedef struct _IMAGE_SYMBOL_INFO
   ULONG SymbolStringsLength;
 } IMAGE_SYMBOL_INFO, *PIMAGE_SYMBOL_INFO;
 
-#define AreSymbolsParsed(si)((si)->FileNameSymbols.Symbols \
-	|| (si)->FunctionSymbols.Symbols \
-	|| (si)->LineNumberSymbols.Symbols)
+#endif /* __KDBGSYMS_H */
 
-#endif
