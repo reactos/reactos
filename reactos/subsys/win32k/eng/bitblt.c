e35792c4ede88a1f2d05a2a6844ab9a40ef4741c
Royce Mitchell III <royce3@ev1.net>
2004-12-18 17:36:13 +0000

    BUGFIX: EngMaskBitBlt() shouldn't BSOD if Mask is NULL
    svn path=/trunk/; revision=12186

--- reactos/subsys/win32k/eng/bitblt.c
+++ reactos/subsys/win32k/eng/bitblt.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: bitblt.c,v 1.64 2004/12/14 03:36:46 royce Exp $
+/* $Id: bitblt.c,v 1.65 2004/12/18 17:36:13 royce Exp $
  *
  * COPYRIGHT:        See COPYING in the top level directory
  * PROJECT:          ReactOS kernel
@@ -900,6 +900,9 @@ EngMaskBitBlt(SURFOBJ *DestObj,
   POINTL             Pt;
   ULONG              Direction;
   POINTL             AdjustedBrushOrigin;
+
+  if ( NULL == Mask )
+    return FALSE;
 
   if (NULL != SourcePoint)
     {
