9769f53baa030b3b3283ea4ba86540dd2475200e
Filip Navara <filip.navara@gmail.com>
2004-12-22 01:20:52 +0000

    - Wait for the shell process to finish execution.
    svn path=/trunk/; revision=12281

--- reactos/subsys/system/userinit/userinit.c
+++ reactos/subsys/system/userinit/userinit.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: userinit.c,v 1.5 2004/08/17 21:49:51 weiden Exp $
+/* $Id: userinit.c,v 1.6 2004/12/22 01:20:52 navaraf Exp $
  *
  * COPYRIGHT:   See COPYING in the top level directory
  * PROJECT:     ReactOS Userinit Logon Application
@@ -97,6 +97,7 @@ void StartShell(void)
                    &si,
                    &pi))
   {
+    WaitForSingleObject(pi.hProcess, INFINITE);
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
   }
