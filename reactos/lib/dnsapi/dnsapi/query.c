3ad6d76cf9058a75c84d295e5cce9e2aac132429
Filip Navara <filip.navara@gmail.com>
2004-12-28 12:18:08 +0000

    - Don't try to free uninitialized pointer in error case.
    svn path=/trunk/; revision=12378

--- reactos/lib/dnsapi/dnsapi/query.c
+++ reactos/lib/dnsapi/dnsapi/query.c
@@ -64,7 +64,6 @@ DNS_STATUS WINAPI DnsQuery_A
 			    adns_if_noserverwarn,
 			    0 );
     if( adns_error != adns_s_ok ) {
-      adns_finish( astate );
       return DnsIntTranslateAdnsToDNS_STATUS( adns_error );
     }
 
