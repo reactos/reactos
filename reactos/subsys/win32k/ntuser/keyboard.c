22244655d62c99ae7ee20edb451ec8781ddecd44
Gé van Geldorp <ge@gse.nl>
2005-01-03 22:00:39 +0000

    Reverse last change, as it causes problems for ReadConsole (line input)
    svn path=/trunk/; revision=12764

--- reactos/subsys/win32k/ntuser/keyboard.c
+++ reactos/subsys/win32k/ntuser/keyboard.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id: keyboard.c,v 1.35 2004/12/28 08:50:10 gvg Exp $
+/* $Id$
  *
  * COPYRIGHT:        See COPYING in the top level directory
  * PROJECT:          ReactOS kernel
@@ -342,23 +342,16 @@ int STDCALL ToUnicodeEx( UINT wVirtKey,
 			 HKL dwhkl ) {
   int ToUnicodeResult = 0;
 
-  if (0 == (lpKeyState[wVirtKey] & KS_DOWN_BIT))
-    {
-      ToUnicodeResult = 0;
-    }
-  else
-    {
-      IntLockQueueState;
-      ToUnicodeResult = ToUnicodeInner( wVirtKey,
-				        wScanCode,
-				        lpKeyState,
-				        pwszBuff,
-				        cchBuff,
-				        wFlags,
-				        PsGetWin32Thread() ? 
-				        PsGetWin32Thread()->KeyboardLayout : 0 );
-      IntUnLockQueueState;
-    }
+  IntLockQueueState;
+  ToUnicodeResult = ToUnicodeInner( wVirtKey,
+				    wScanCode,
+				    lpKeyState,
+				    pwszBuff,
+				    cchBuff,
+				    wFlags,
+				    PsGetWin32Thread() ? 
+				    PsGetWin32Thread()->KeyboardLayout : 0 );
+  IntUnLockQueueState;
 
   return ToUnicodeResult;
 }
