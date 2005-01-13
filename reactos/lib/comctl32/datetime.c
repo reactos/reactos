289a65afa7ad010e282c0d0fb56a07ce286ebc1a
Gé van Geldorp <ge@gse.nl>
2005-01-13 09:28:58 +0000

    Small Wine sync
    svn path=/trunk/; revision=13025

--- reactos/lib/comctl32/datetime.c
+++ reactos/lib/comctl32/datetime.c
@@ -1085,6 +1085,7 @@ DATETIME_WindowProc (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		     uMsg, wParam, lParam);
 	return DefWindowProcW (hwnd, uMsg, wParam, lParam);
     }
+    return 0;
 }
 
 
