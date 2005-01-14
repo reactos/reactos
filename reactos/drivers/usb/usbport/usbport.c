ee34d784d4a75948a13ee73a6d73e2055a3153aa
Mark Tempel <mtempel@visi.com>
2005-01-14 16:12:34 +0000

    Initial commit of USB support.  These are just stubs.  They build, but that is about it.  Expect heavy development in usbport, and usbuhci.
    svn path=/trunk/; revision=13044

---
+++ reactos/drivers/usb/usbport/usbport.c
@@ -0,0 +1,81 @@
+/*
+ * ReactOS USB Port driver
+ * Copyright (C) 2004 Aleksey Bragin
+ *           (C) 2005 Mark Tempel
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * STATUS:
+ *   19-Dec-2004 - just a stub for now, but with useful info by Filip
+ */
+
+/* INCLUDES *******************************************************************/
+#include <stddef.h>
+#include <windef.h>
+#include <ddk/ntddk.h>
+#include "usbport.h"
+
+/* PUBLIC AND PRIVATE FUNCTIONS ***********************************************/
+
+/*
+** Standard DriverEntry method.
+** We do nothing here.  All real work is done in USBPRORT_RegisterUSBPortDriver.
+*/
+NTSTATUS STDCALL
+DriverEntry(IN PVOID Context1, IN PVOID Context2)
+{
+	return STATUS_SUCCESS;
+}
+/*
+ * This method is used by miniports to connect set up 
+ */
+NTSTATUS STDCALL
+USBPORT_RegisterUSBPortDriver(PDRIVER_OBJECT DriverObject, DWORD Unknown1,
+    PUSB_CONTROLLER_INTERFACE Interface)
+{
+	ASSERT(KeGetCurrentIRQL() < DISPATCH_LEVEL);
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS STDCALL
+USBPORT_GetHciMn(VOID)
+{
+	return 0x10000001;
+}
+/*
+ * This method is to allow miniports to create 
+ */
+NTSTATUS STDCALL
+USBPORT_AllocateUsbControllerInterface(OUT PUSB_CONTROLLER_INTERFACE *pControllerInterface)
+{
+	ASSERT(KeGetCurrentIRQL() < DISPATCH_LEVEL);
+	ASSERT(0 != ControllerObject);
+
+	*pControllerInterface = (PUSB_CONTROLLER_INTERFACE)ExAllocatePoolWithTag(PagedPool, sizeof(USB_CONTROLLER_INTERFACE),USB_CONTROLLER_INTERFACE_TAG);
+	RtlZeroMemory(*pControllerInterface, sizeof(USB_CONTROLLER_INTERFACE));
+	
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS STDCALL
+USBPORT_FreeUsbControllerInterface(IN PUSB_CONTROLLER_INTERFACE ControllerInterface)
+{
+	ASSERT(KeGetCurrentIRQL() < DISPATCH_LEVEL);	
+
+	ExFreePool(ControllerInterface);
+	
+	return STATUS_SUCCESS;
+}
