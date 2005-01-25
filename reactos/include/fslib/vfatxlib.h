85d156faef65699eb94f134ac8f32d60c24c2e08
Hartmut Birr <osexpert@googlemail.com>
2005-01-25 22:10:39 +0000

    - Added vfatxlib.
    svn path=/trunk/; revision=13286

---
+++ reactos/include/fslib/vfatxlib.h
@@ -0,0 +1,24 @@
+/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ReactOS VFAT filesystem library
+ * FILE:        include/fslib/vfatlib.h
+ * PURPOSE:     Public definitions for vfat filesystem library
+ */
+#ifndef __VFATXLIB_H
+#define __VFATXLIB_H
+
+#include <fmifs.h>
+
+NTSTATUS
+VfatxInitialize (VOID);
+
+NTSTATUS
+VfatxCleanup (VOID);
+
+NTSTATUS
+VfatxFormat (PUNICODE_STRING DriveRoot,
+	    ULONG MediaFlag,
+	    BOOLEAN QuickFormat,
+	    PFMIFSCALLBACK Callback);
+
+#endif /*__VFATLIB_H */
