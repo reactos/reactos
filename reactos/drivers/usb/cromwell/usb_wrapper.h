d3fb293ea330c5247d44090fe9fc7019c83c06b2
Aleksey Bragin <aleksey@reactos.org>
2005-01-26 22:34:39 +0000

    This is the initial version of USB port+hub+hostcontroller drivers from a special version of Linux USB Stack. More info will be placed at ROS-Wiki.
    svn path=/trunk/; revision=13324

---
+++ reactos/drivers/usb/cromwell/usb_wrapper.h
@@ -0,0 +1,14 @@
+//#include <stdlib.h>
+//#include <ntos/types.h>
+//#include <ddk/extypes.h>
+#include <ddk/ntddk.h>
+#include <debug.h>
+
+void wait_ms(int mils);
+
+#include "linux/linux_wrapper.h"
+#define __KERNEL__
+#undef CONFIG_PCI
+#define CONFIG_PCI
+
+#include "linux/usb.h"
