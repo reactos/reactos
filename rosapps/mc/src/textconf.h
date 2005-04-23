bffc2cc145aa225a652f14f5f27232438fe8042c
Art Yerkes <art.yerkes@gmail.com>
2005-04-23 04:10:06 +0000

    Finally creating the PowerPC branch.  Time to get some local edits out of here.
    svn path=/branches/powerpc/; revision=14752

--- rosapps/mc/src/textconf.h
+++
@@ -1,7 +0,0 @@
-/* Defines what features are to be includes in the text mode edition */
-
-#define COMPUTE_FORMAT_ALLOCATIONS    1
-#define PORT_WIDGET_WANTS_HISTORY     1
-#define PORT_NEEDS_CHANGE_SCREEN_SIZE 1
-
-#define port_shutdown_extra_fds()
--- rosapps/tests/README.txt
+++
@@ -1,3 +0,0 @@
-This folder contains tests that are not merged in to the new regression system.
-
-Please DO NOT add new tests here.
