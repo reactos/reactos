8ece706e06ba2ae2154f42c015ea8062ba0612f8
Thomas Bluemel <thomas@reactsoft.com>
2005-05-11 17:03:00 +0000

    revert the changes i accidently committed
    svn path=/trunk/; revision=15220

--- rosapps/tests/hello/hello.c
+++ rosapps/tests/hello/hello.c
@@ -1,12 +1,8 @@
-#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char* argv[])
 {
-   WCHAR str[255];
-   
-   wsprintfW(str, L"%04X", 0xab34);
-   printf("%S\n", str);
+   printf("Hello world\n");
    return(0);
 }
