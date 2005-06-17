a4cb3017e6632c898eefb577148ac14eaacf63fd
Filip Navara <filip.navara@gmail.com>
2005-06-17 13:13:37 +0000

    Define KEBUGCHECK and KEBUGCHECKEX here.
    svn path=/trunk/; revision=15979

--- reactos/include/debug.h
+++ reactos/include/debug.h
@@ -93,4 +93,9 @@
 #error Unsupported architecture.
 #endif
 
+#ifndes KEBUGCHECK
+#define KEBUGCHECK(a) DbgPrint("KeBugCheck (0x%X) at %s:%i\n", a, __FILE__,__LINE__), KeBugCheck(a)
+#define KEBUGCHECKEX(a,b,c,d,e) DbgPrint("KeBugCheckEx (0x%X, 0x%X, 0x%X, 0x%X, 0x%X) at %s:%i\n", a, b, c, d, e, __FILE__,__LINE__), KeBugCheckEx(a,b,c,d,e)
+#endif
+
 #endif /* __INTERNAL_DEBUG */
