4b08e9770afc6c8e1bb8ae90767adc8e2d6c7199
Alex Ionescu <aionescu@gmail.com>
2005-06-17 17:59:11 +0000

    Oops. Thanks w3seek
    svn path=/trunk/; revision=16007

--- reactos/include/ntdll/ntdll.h
+++ reactos/include/ntdll/ntdll.h
@@ -41,6 +41,17 @@
 #define CHECKPOINT1(args...)
 #endif
 
+/* Macros expanding to the appropriate inline assembly to raise a breakpoint */
+#if defined(_M_IX86)
+#define ASM_BREAKPOINT "\nint $3\n"
+#elif defined(_M_ALPHA)
+#define ASM_BREAKPOINT "\ncall_pal bpt\n"
+#elif defined(_M_MIPS)
+#define ASM_BREAKPOINT "\nbreak\n"
+#else
+#error Unsupported architecture.
+#endif
+
 #define ROUNDUP(a,b)	((((a)+(b)-1)/(b))*(b))
 #define ROUNDDOWN(a,b)	(((a)/(b))*(b))
 
