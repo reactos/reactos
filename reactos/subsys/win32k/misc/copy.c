4bfbd2cfaf6edda5f5f0b8140b90becd5c6f19af
Thomas Bluemel <thomas@reactsoft.com>
2005-07-22 20:51:36 +0000

    probe the pointers in _MmCopyFromCaller and _MmCopyToCaller. There's no need to check the processor mode since it's always just used to copy from/to user memory
    svn path=/trunk/; revision=16690

--- reactos/subsys/win32k/misc/copy.c
+++ reactos/subsys/win32k/misc/copy.c
@@ -2,9 +2,23 @@
 
 NTSTATUS _MmCopyFromCaller( PVOID Target, PVOID Source, UINT Bytes ) {
     NTSTATUS Status = STATUS_SUCCESS;
-
+    
     _SEH_TRY {
-	RtlCopyMemory(Target,Source,Bytes);
+        ProbeForRead(Source,Bytes,1);
+        RtlCopyMemory(Target,Source,Bytes);
+    } _SEH_HANDLE {
+	Status = _SEH_GetExceptionCode();
+    } _SEH_END;
+
+    return Status;
+}
+
+NTSTATUS _MmCopyToCaller( PVOID Target, PVOID Source, UINT Bytes ) {
+    NTSTATUS Status = STATUS_SUCCESS;
+    
+    _SEH_TRY {
+        ProbeForWrite(Target,Bytes,1);
+        RtlCopyMemory(Target,Source,Bytes);
     } _SEH_HANDLE {
 	Status = _SEH_GetExceptionCode();
     } _SEH_END;
