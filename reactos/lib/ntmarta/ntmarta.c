5887e9c4c9a33e6545e745d413555f1672a6b168
Thomas Bluemel <thomas@reactsoft.com>
2005-07-26 21:17:06 +0000

    added an initial MARTA provider (only two functions stubbed)
    svn path=/trunk/; revision=16759

---
+++ reactos/lib/ntmarta/ntmarta.c
@@ -0,0 +1,89 @@
+/*
+ * ReactOS MARTA provider
+ * Copyright (C) 2004 ReactOS Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/* $Id$
+ *
+ * PROJECT:         ReactOS MARTA provider
+ * FILE:            lib/ntmarta/ntmarta.c
+ * PURPOSE:         ReactOS MARTA provider
+ * PROGRAMMER:      Thomas Weidenmueller <w3seek@reactos.com>
+ *
+ * UPDATE HISTORY:
+ *      07/26/2005  Created
+ */
+#include <ntmarta.h>
+
+#define NDEBUG
+#include <debug.h>
+
+HINSTANCE hDllInstance;
+
+/**********************************************************************
+ * AccRewriteGetHandleRights				EXPORTED
+ *
+ * @unimplemented
+ */
+DWORD STDCALL
+AccRewriteGetHandleRights(HANDLE handle,
+                          SE_OBJECT_TYPE ObjectType,
+                          SECURITY_INFORMATION SecurityInfo,
+                          PSID* ppsidOwner,
+                          PSID* ppsidGroup,
+                          PACL* ppDacl,
+                          PACL* ppSacl,
+                          PSECURITY_DESCRIPTOR* ppSecurityDescriptor)
+{
+    UNIMPLEMENTED;
+    return ERROR_CALL_NOT_IMPLEMENTED;
+}
+
+
+/**********************************************************************
+ * AccRewriteSetHandleRights				EXPORTED
+ *
+ * @unimplemented
+ */
+DWORD STDCALL
+AccRewriteSetHandleRights(HANDLE handle,
+                          SE_OBJECT_TYPE ObjectType,
+                          SECURITY_INFORMATION SecurityInfo,
+                          PSECURITY_DESCRIPTOR pSecurityDescriptor)
+{
+    UNIMPLEMENTED;
+    return ERROR_CALL_NOT_IMPLEMENTED;
+}
+
+
+BOOL STDCALL
+DllMain(IN HINSTANCE hinstDLL,
+        IN DWORD dwReason,
+        IN LPVOID lpvReserved)
+{
+    switch (dwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            hDllInstance = hinstDLL;
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
+
---
+++ reactos/lib/ntmarta/ntmarta.h
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <accctrl.h>
+
+ULONG DbgPrint(PCH Format,...);
+
+extern HINSTANCE hDllInstance;
+
+/* EOF */
