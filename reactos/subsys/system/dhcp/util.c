a5bc0e6462c41b0091aa73c985ae99414e89e57d
Alex Ionescu <aionescu@gmail.com>
2005-07-31 05:46:42 +0000

    Define NDEBUG
    svn path=/trunk/; revision=16912

--- reactos/subsys/system/dhcp/util.c
+++ reactos/subsys/system/dhcp/util.c
@@ -1,6 +1,8 @@
 #include <stdarg.h>
-#include <reactos/debug.h>
 #include "rosdhcp.h"
+
+#define NDEBUG
+#include <reactos/debug.h>
 
 char *piaddr( struct iaddr addr ) {
     struct sockaddr_in sa;
