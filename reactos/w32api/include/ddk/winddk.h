6887acc456d5d78eb2fe9369dd62732e4ffcd184
Steven Edwards <winehacker@gmail.com>
2005-08-11 17:27:13 +0000

    remove ros specific functions from public header
    svn path=/trunk/; revision=17288

--- reactos/w32api/include/ddk/winddk.h
+++ reactos/w32api/include/ddk/winddk.h
@@ -9838,16 +9838,6 @@ DbgSetDebugFilterState(
   IN ULONG  Level,
   IN BOOLEAN  State);
 
-NTOSAPI
-BOOLEAN
-DDKAPI
-KeRosPrintAddress ( PVOID address );
-
-NTOSAPI
-VOID
-DDKAPI
-KeRosDumpStackFrames ( PULONG Frame, ULONG FrameCount );
-
 #ifdef DBG
 
 #define KdPrint(_x_) DbgPrint _x_
