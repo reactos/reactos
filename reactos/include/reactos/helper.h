a650cc6d778230c34871b80d53c3eca3de5aed15
Hervé Poussineau <hpoussin@reactos.org>
2005-09-04 20:45:34 +0000

    Don't double-define KEBUGCHECK(EX)
    svn path=/trunk/; revision=17640

--- reactos/include/reactos/helper.h
+++ reactos/include/reactos/helper.h
@@ -11,8 +11,10 @@
 #define ABS_VALUE(V) (((V) < 0) ? -(V) : (V))
 #define RtlRosMin(X,Y) (((X) < (Y))? (X) : (Y))
 #define RtlRosMin3(X,Y,Z) (((X) < (Y)) ? RtlRosMin(X,Z) : RtlRosMin(Y,Z))
+#ifndef KEBUGCHECK
 #define KEBUGCHECKEX(a,b,c,d,e) DbgPrint("KeBugCheckEx at %s:%i\n",__FILE__,__LINE__), KeBugCheckEx(a,b,c,d,e)
 #define KEBUGCHECK(a) DbgPrint("KeBugCheck at %s:%i\n",__FILE__,__LINE__), KeBugCheck(a)
+#endif
 #define EXPORTED __declspec(dllexport)
 #define IMPORTED __declspec(dllimport)
 #define LIST_FOR_EACH(entry, head) \
