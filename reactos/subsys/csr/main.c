a4808fca797d54253f2a76e043b7322ae1b574cf
Emanuele Aliberti <ea@iol.it>
2005-10-02 07:42:52 +0000

    - add copyright notice - fix unmatched bracket
    svn path=/trunk/; revision=18199

--- reactos/subsys/csr/main.c
+++ reactos/subsys/csr/main.c
@@ -1,3 +1,23 @@
+/* $Id$
+ * --------------------------------------------------------------------
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.LIB. If not, write
+ * to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+ * MA 02139, USA.  
+ *
+ * --------------------------------------------------------------------
+ */
 /*
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS CSR Sub System
@@ -27,7 +47,7 @@ CsrpSetDefaultProcessHardErrorMode (VOID)
     NtSetInformationProcess(NtCurrentProcess(),
                             ProcessDefaultHardErrorMode,
                             &DefaultHardErrorMode,
-                            sizeof(DefaultHardErrorMode);
+                            sizeof(DefaultHardErrorMode));
 }
 
 int
