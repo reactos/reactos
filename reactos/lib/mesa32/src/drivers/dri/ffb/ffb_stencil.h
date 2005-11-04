be46b8647d164b92f176bde6eb8a91546c2ac19c
Gregor Anich <blight@blight.eu.org>
2005-11-04 20:35:21 +0000

    Remove unused driver sources.
    svn path=/trunk/; revision=19005

--- reactos/lib/mesa32/src/drivers/dri/ffb/ffb_stencil.h
+++
@@ -1,8 +0,0 @@
-/* $XFree86: xc/lib/GL/mesa/src/drv/ffb/ffb_stencil.h,v 1.1 2000/06/20 05:08:39 dawes Exp $ */
-
-#ifndef _FFB_STENCIL_H
-#define _FFB_STENCIL_H
-
-void ffbSetStencilFunctions(driRenderbuffer *drb, const GLvisual *vis);
-
-#endif /* !(_FFB_STENCIL_H) */
--- reactos/lib/mesa32/src/drivers/dri/radeon/radeon_sanity.h
+++
@@ -1,8 +0,0 @@
-#ifndef RADEON_SANITY_H
-#define RADEON_SANITY_H
-
-extern int radeonSanityCmdBuffer( radeonContextPtr rmesa,
-				  int nbox,
-				  drm_clip_rect_t *boxes );
-
-#endif
