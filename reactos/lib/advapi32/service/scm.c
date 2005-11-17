1f3e7f67298eb7986a6d50d787392d1d5ef09e2d
Christoph von Wittich <christoph_vw@reactos.org>
2005-11-17 13:46:07 +0000

    sorry, forgot to change one var
    svn path=/trunk/; revision=19293

--- reactos/lib/advapi32/service/scm.c
+++ reactos/lib/advapi32/service/scm.c
@@ -343,7 +343,7 @@ CreateServiceA(
         SetLastError(ERROR_NOT_ENOUGH_MEMORY);
         goto cleanup;
     }
-    MultiByteToWideChar(CP_ACP, 0, lpDependencies, -1, lpDependenciesW, len);
+    MultiByteToWideChar(CP_ACP, 0, lpDependencies, -1, lpDependenciesW, dwDependenciesLength);
 
     len = MultiByteToWideChar(CP_ACP, 0, lpServiceStartName, -1, NULL, 0);
     lpServiceStartName = HeapAlloc(GetProcessHeap(), 0, len * sizeof(WCHAR));
