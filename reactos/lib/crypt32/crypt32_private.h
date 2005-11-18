f2c46acc545337816efdecf73737ed1924afaa75
Thomas Bluemel <thomas@reactsoft.com>
2005-11-18 23:58:48 +0000

    fix the build
    svn path=/trunk/; revision=19337

--- reactos/lib/crypt32/crypt32_private.h
+++ reactos/lib/crypt32/crypt32_private.h
@@ -19,8 +19,6 @@
 #ifndef __CRYPT32_PRIVATE_H__
 #define __CRYPT32_PRIVATE_H__
 
-#define STATUS_ACCESS_VIOLATION          0xC0000005
-
 /* Returns a handle to the default crypto provider; loads it if necessary.
  * Returns NULL on failure.
  */
