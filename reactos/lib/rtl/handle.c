cfdaa3b11d3c636f07cbc86425b2ca791adb9c63
Filip Navara <filip.navara@gmail.com>
2005-11-21 21:22:13 +0000

    Be consistent and change #if DBG to #ifdef DBG.
    svn path=/trunk/; revision=19426

--- reactos/lib/rtl/handle.c
+++ reactos/lib/rtl/handle.c
@@ -125,7 +125,7 @@ BOOLEAN NTAPI
 RtlFreeHandle(PRTL_HANDLE_TABLE HandleTable,
 	      PRTL_HANDLE_TABLE_ENTRY Handle)
 {
-#if DBG
+#ifdef DBG
    /* check if handle is valid */
    if (RtlIsValidHandle(HandleTable, Handle))
      return FALSE;
