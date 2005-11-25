8cef988efb7c058cd7479b5a6c5879806d5db652
Thomas Bluemel <thomas@reactsoft.com>
2005-11-25 16:13:58 +0000

    - add a (very incomplete) devguid.h - add mmsys.cpl to the build system and bootcd
    svn path=/trunk/; revision=19559

---
+++ reactos/w32api/include/devguid.h
@@ -0,0 +1,19 @@
+/*
+ * devguid.h
+ *
+ * PnP Device Class GUIDs
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+DEFINE_GUID(GUID_DEVCLASS_CDROM, 0x4D36E965, 0xE325, 0x11CE, 0xBF, 0xC1, 0x08, 0x00, 0x2B, 0xE1, 0x03, 0x18);
+DEFINE_GUID(GUID_DEVCLASS_MEDIA, 0x4D36E96C, 0xE325, 0x11CE, 0xBF, 0xC1, 0x08, 0x00, 0x2B, 0xE1, 0x03, 0x18);
