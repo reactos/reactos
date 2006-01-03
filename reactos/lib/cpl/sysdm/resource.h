e94e037e29b981556e3de7a6f0005ae890094dd3
Ged Murphy <gedmurphy@reactos.org>
2006-01-03 21:17:28 +0000

    fix ugly tab formatting
    svn path=/trunk/; revision=20552

--- reactos/lib/cpl/sysdm/resource.h
+++ reactos/lib/cpl/sysdm/resource.h
@@ -13,17 +13,17 @@
 #define RC_LICENSE                      101
 #define RTDATA                          300
 
-#define IDI_CPLSYSTEM	                100
+#define IDI_CPLSYSTEM                   100
 #define IDI_DEVMGR                      101
 
-#define IDD_PROPPAGEGENERAL	            100
-#define IDD_PROPPAGECOMPUTER	        101
-#define IDD_PROPPAGEHARDWARE	        102
-#define IDD_PROPPAGEUSERPROFILE	        103
-#define IDD_PROPPAGEADVANCED	        104
+#define IDD_PROPPAGEGENERAL             100
+#define IDD_PROPPAGECOMPUTER            101
+#define IDD_PROPPAGEHARDWARE            102
+#define IDD_PROPPAGEUSERPROFILE         103
+#define IDD_PROPPAGEADVANCED            104
 
-#define IDS_CPLSYSTEMNAME	           1001
-#define IDS_CPLSYSTEMDESCRIPTION	   2001
+#define IDS_CPLSYSTEMNAME              1001
+#define IDS_CPLSYSTEMDESCRIPTION       2001
 
 /* controls */
 #define IDC_LICENSEMEMO                 101
@@ -32,19 +32,19 @@
 #define IDC_PROCESSORSPEED              104
 #define IDC_SYSTEMMEMORY                105
 #define IDC_DEVMGR                      106
-#define IDC_ENVVAR	                    107
-#define IDC_STAREC	                    108
-#define IDC_PERFOR	                    109
+#define IDC_ENVVAR                      107
+#define IDC_STAREC                      108
+#define IDC_PERFOR                      109
 #define IDC_ICON1                       201
 
-#define IDC_COMPUTERNAME	            202
-#define IDC_WORKGROUPDOMAIN_NAME	    203
+#define IDC_COMPUTERNAME                202
+#define IDC_WORKGROUPDOMAIN_NAME        203
 #define IDC_WORKGROUPDOMAIN	            204
-#define IDC_NETWORK_ID	                205
-#define IDC_NETWORK_PROPERTY	        206
+#define IDC_NETWORK_ID                  205
+#define IDC_NETWORK_PROPERTY            206
 #define IDC_HARDWARE_WIZARD	            207
-#define IDC_HARDWARE_PROFILE	        210
-#define IDC_HARDWARE_DRIVER_SIGN	    211
+#define IDC_HARDWARE_PROFILE            210
+#define IDC_HARDWARE_DRIVER_SIGN        211
 #define IDC_HARDWARE_DEVICE_MANAGER	    212
 
 #define IDC_USERPROFILE_LIST            213
