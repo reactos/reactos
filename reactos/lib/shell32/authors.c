3761ecf1a5afd1fd103b77edb08517332c635dd1
Klemens Friedl <frik85@gmail.com>
2006-01-08 21:09:00 +0000

    altered several year numbers from "2005" to "2006"
    svn path=/trunk/; revision=20730

--- reactos/lib/shell32/authors.c
+++ reactos/lib/shell32/authors.c
@@ -1,1 +1,1 @@
-const char * const SHELL_Authors[] = { "Copyright 1993-2005 WINE team", "Copyright 1998-2005 ReactOS Team", 0 };
+const char * const SHELL_Authors[] = { "Copyright 1993-2006 WINE team", "Copyright 1998-2006 ReactOS Team", 0 };
