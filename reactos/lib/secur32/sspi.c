a2c387cc4f8c3f760ac76a0bf02e6221c7f70e4a
Christoph von Wittich <christoph_vw@reactos.org>
2006-01-09 15:08:35 +0000

    added some stubs (Photoshop CS seems to work now)
    svn path=/trunk/; revision=20752

--- reactos/lib/secur32/sspi.c
+++ reactos/lib/secur32/sspi.c
@@ -35,3 +35,42 @@ FreeContextBuffer (
 	DPRINT1("%s() not implemented!\n", __FUNCTION__);
 	return ERROR_CALL_NOT_IMPLEMENTED;
 }
+
+SECURITY_STATUS
+WINAPI
+AcquireCredentialsHandleA ( 
+    SEC_CHAR* pszPrincipal,
+    SEC_CHAR* pszPackage,
+    ULONG fUsage,
+    PLUID pID,
+    PVOID pAuth,
+    SEC_GET_KEY_FN pGetKeyFn,
+    PVOID pvGetKeyArgument,
+    PCredHandle phCred,
+    PTimeStamp pExpires
+    )
+{
+	DPRINT1("%s() not implemented!\n", __FUNCTION__);
+	return ERROR_CALL_NOT_IMPLEMENTED;
+}
+
+SECURITY_STATUS
+WINAPI
+InitializeSecurityContextA ( 
+    PCredHandle phCred,
+    PCtxtHandle phContext,
+    SEC_CHAR* pszTarget,
+    ULONG fContextReq,
+    ULONG Reserved,
+    ULONG TargetData,
+    PSecBufferDesc pInput,
+    ULONG Reserved2,
+    PCtxtHandle phNewContext,
+    PSecBufferDesc pOut,
+    PULONG pfContextAttributes,
+    PTimeStamp pExpires
+    )
+{
+	DPRINT1("%s() not implemented!\n", __FUNCTION__);
+	return ERROR_CALL_NOT_IMPLEMENTED;
+}
