9dc1d6aa1842206687a24c0566b64500bd14e68d
Saveliy Tretiakov <saveliyt@gmail.com>
2006-04-07 13:03:59 +0000

    Thomas Weidenmueller is not author of this control panel applet. From svn log I found that former author was Johannes Anderwald (j_anderw@sbox.tugraz.at). Updated headers.
    svn path=/trunk/; revision=21505

--- reactos/dll/cpl/access/access.c
+++ reactos/dll/cpl/access/access.c
@@ -1,27 +1,10 @@
-/*
- *  ReactOS
- *  Copyright (C) 2004 ReactOS Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
 /* $Id$
  *
  * PROJECT:         ReactOS System Control Panel
+ * LICENSE:         GPL - See COPYING in the top level directory
  * FILE:            lib/cpl/system/sysdm.c
  * PURPOSE:         ReactOS System Control Panel
- * PROGRAMMER:      Thomas Weidenmueller (w3seek@users.sourceforge.net)
+ * COPYRIGHT:       Copyright 2004 Johannes Anderwald (j_anderw@sbox.tugraz.at)
  * UPDATE HISTORY:
  *      03-04-2004  Created
  */
