a9161f091399489289dcbc052f9e82b8095fc557
Saveliy Tretiakov <saveliyt@gmail.com>
2006-04-14 16:31:06 +0000

    EngCreateBitmap may return null. Add return value checks
    svn path=/trunk/; revision=21589

--- reactos/subsystems/win32/win32k/eng/engmisc.c
+++ reactos/subsystems/win32/win32k/eng/engmisc.c
@@ -68,6 +68,13 @@ IntEngEnter(PINTENG_ENTER_LEAVE EnterLeave,
     EnterLeave->OutputBitmap = EngCreateBitmap(BitmapSize, Width,
                                                DestObj->iBitmapFormat,
                                                BMF_TOPDOWN | BMF_NOZEROINIT, NULL);
+                                               
+    if (!EnterLeave->OutputBitmap)
+      {
+      DPRINT1("EngCreateBitmap() failed\n");
+      return FALSE;
+      }
+      
     *OutputObj = EngLockSurface((HSURF)EnterLeave->OutputBitmap);
 
     EnterLeave->DestRect.left = 0;
