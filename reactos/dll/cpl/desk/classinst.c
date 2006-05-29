64664422f48257bd0b869c367ffc4b4435e032a3
Hervé Poussineau <hpoussin@reactos.org>
2006-05-29 11:09:54 +0000

    Do the normal install before custom install actions. So, we should get the right .inf filename in SP_DRVINFO_DETAIL_DATA.InfFileName, as it is modified if the file is copied to %SYSTEMROOT%/Inf
    svn path=/trunk/; revision=22104

--- reactos/dll/cpl/desk/classinst.c
+++ reactos/dll/cpl/desk/classinst.c
@@ -36,7 +36,7 @@ DisplayClassInstaller(
 	if (InstallFunction != DIF_INSTALLDEVICE)
 		return ERROR_DI_DO_DEFAULT;
 
-	/* Set DI_NEEDRESTART flag */
+	/* Set DI_DONOTCALLCONFIGMG flag */
 	InstallParams.cbSize = sizeof(SP_DEVINSTALL_PARAMS);
 	result = SetupDiGetDeviceInstallParams(DeviceInfoSet, DeviceInfoData, &InstallParams);
 	if (!result)
@@ -46,13 +46,22 @@ DisplayClassInstaller(
 		goto cleanup;
 	}
 
-	InstallParams.Flags |= DI_NEEDRESTART;
+	InstallParams.Flags |= DI_DONOTCALLCONFIGMG;
 
 	result = SetupDiSetDeviceInstallParams(DeviceInfoSet, DeviceInfoData, &InstallParams);
 	if (!result)
 	{
 		rc = GetLastError();
 		DPRINT("SetupDiSetDeviceInstallParams() failed with error 0x%lx\n", rc);
+		goto cleanup;
+	}
+
+	/* Do normal install */
+	result = SetupDiInstallDevice(DeviceInfoSet, DeviceInfoData);
+	if (!result)
+	{
+		rc = GetLastError();
+		DPRINT("SetupDiGetDeviceRegistryProperty() failed with error 0x%lx\n", rc);
 		goto cleanup;
 	}
 
@@ -96,15 +105,6 @@ DisplayClassInstaller(
 		goto cleanup;
 	}
 	_tcscat(SectionName, _T(".SoftwareSettings"));
-
-	/* Do normal install */
-	result = SetupDiInstallDevice(DeviceInfoSet, DeviceInfoData);
-	if (!result)
-	{
-		rc = GetLastError();
-		DPRINT("SetupDiGetDeviceRegistryProperty() failed with error 0x%lx\n", rc);
-		goto cleanup;
-	}
 
 	/* Open driver registry key and create Settings subkey */
 	hDriverKey = SetupDiOpenDevRegKey(
