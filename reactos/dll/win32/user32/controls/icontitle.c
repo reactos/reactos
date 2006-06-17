3c7fc296281d47f01323918bd3cd69c1eb4bc3c9
James Tabor <james.tabor@reactos.org>
2006-06-17 05:02:55 +0000

    Sync with wine.
    svn path=/trunk/; revision=22378

--- reactos/dll/win32/user32/controls/icontitle.c
+++ reactos/dll/win32/user32/controls/icontitle.c
@@ -53,7 +53,7 @@ const struct builtin_class_descr ICONTITLE_builtin_class =
 HWND ICONTITLE_Create( HWND owner )
 {
     HWND hWnd;
-    HINSTANCE instance = (HINSTANCE)GetWindowLongA( owner, GWL_HINSTANCE );
+    HINSTANCE instance = (HINSTANCE)GetWindowLongPtrA( owner, GWLP_HINSTANCE );
     LONG style = WS_CLIPSIBLINGS;
 
     if (!IsWindowEnabled(owner)) style |= WS_DISABLED;
@@ -142,7 +142,7 @@ static BOOL ICONTITLE_Paint( HWND hwnd, HWND owner, HDC hDC, BOOL bActive )
     {
         if( GetWindowLongA( hwnd, GWL_STYLE ) & WS_CHILD )
 	{
-	    hBrush = (HBRUSH) GetClassLongA(hwnd, GCL_HBRBACKGROUND);
+	    hBrush = (HBRUSH) GetClassLongPtrW(hwnd, GCLP_HBRBACKGROUND);
 	    if( hBrush )
 	    {
 		INT level;
