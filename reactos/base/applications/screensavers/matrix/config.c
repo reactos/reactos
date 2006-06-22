reactos/reactos

commit e2ba765506202623d5668c1c1e58c9e3d6727c7e

Bit of fun. Add a built-in coded message for the first time the screensaver is run. If removed, it will not reappear.
svn path=/trunk/; revision=21635

reactos/base/applications/screensavers/matrix/config.c
@@ -68,7 +68,7 @@ BOOL CALLBACK ConfigDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		prevheight = GetSystemMetrics(SM_CYSCREEN) / GLYPH_HEIGHT + 1;
 
 		//Add any saved messages to the combo box
-		for(index = 0; index < g_nNumMessages; index++)
+		for(index = 0; index <= g_nNumMessages; index++)
 		{
 			if(lstrlen(g_szMessages[index]) > 0)
 				SendDlgItemMessage(hwnd, IDC_COMBO1, CB_ADDSTRING, 0, (LPARAM)g_szMessages[index]);

reactos/base/applications/screensavers/matrix/settings.c
@@ -93,6 +93,11 @@ void LoadSettings()
 			}
 		}
 	}
+	else
+	{
+		/* built-in coded message for first run */
+		lstrcpyn(g_szMessages[0], "ReactOS", MAXMSG_LENGTH);
+	}
 
 	RegCloseKey(hkey);
 	free(hugechar);

commit 2b5ee06b1a1b4ba83d35b417dfdb15f3e3796284

Fix bug introduced in r21635.
svn path=/trunk/; revision=22499

reactos/base/applications/screensavers/matrix/config.c
@@ -69,7 +69,7 @@ BOOL CALLBACK ConfigDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		prevheight = GetSystemMetrics(SM_CYSCREEN) / GLYPH_HEIGHT + 1;
 
 		//Add any saved messages to the combo box
-		for(index = 0; index <= g_nNumMessages; index++)
+		for(index = 0; index < g_nNumMessages; index++)
 		{
 			if(lstrlen(g_szMessages[index]) > 0)
 				SendDlgItemMessage(hwnd, IDC_COMBO1, CB_ADDSTRING, 0, (LPARAM)g_szMessages[index]);

reactos/base/applications/screensavers/matrix/settings.c
@@ -98,6 +98,7 @@ void LoadSettings()
 	{
 		/* built-in coded message for first run */
 		lstrcpyn(g_szMessages[0], _T("ReactOS"), MAXMSG_LENGTH);
+		++g_nNumMessages;
 	}
 
 	RegCloseKey(hkey);