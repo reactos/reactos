f5270b01058b3778e77f4741cfecd5a9b8b24666
Alex Ionescu <aionescu@gmail.com>
2006-06-30 19:01:01 +0000

    [FORMATTING] - Tabs->spaces and proper prototype formatting.
    svn path=/trunk/; revision=22725

--- reactos/ntoskrnl/io/efi.c
+++ reactos/ntoskrnl/io/efi.c
@@ -1,107 +1,91 @@
 /*
- * COPYRIGHT:       See COPYING in the top level directory
- * PROJECT:         ReactOS kernel
+ * PROJECT:         ReactOS Kernel
+ * LICENSE:         GPL - See COPYING in the top level directory
  * FILE:            ntoskrnl/io/efi.c
- * PURPOSE:         EFI Unimplemented Function Calls
- *
- * PROGRAMMERS:     Alex Ionescu (alex@relsoft.net)
+ * PURPOSE:         I/O Functions for EFI Machines
+ * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
  */
 
 /* INCLUDES *****************************************************************/
 
 #include <ntoskrnl.h>
+#define NDEBUG
 #include <internal/debug.h>
 
 /* FUNCTIONS *****************************************************************/
 
 NTSTATUS
-STDCALL
-NtAddBootEntry(
-	IN PUNICODE_STRING EntryName,
-	IN PUNICODE_STRING EntryValue
-	)
+NTAPI
+NtAddBootEntry(IN PUNICODE_STRING EntryName,
+               IN PUNICODE_STRING EntryValue)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtDeleteBootEntry(
-	IN PUNICODE_STRING EntryName,
-	IN PUNICODE_STRING EntryValue
-	)
+NTAPI
+NtDeleteBootEntry(IN PUNICODE_STRING EntryName,
+                  IN PUNICODE_STRING EntryValue)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtEnumerateBootEntries(
-    IN PVOID Buffer,
-    IN PULONG BufferLength
-	)
+NTAPI
+NtEnumerateBootEntries(IN PVOID Buffer,
+                       IN PULONG BufferLength)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtQueryBootEntryOrder(
-    IN PULONG Ids,
-    IN PULONG Count
-	)
+NTAPI
+NtQueryBootEntryOrder(IN PULONG Ids,
+                      IN PULONG Count)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtQueryBootOptions(
-    IN PBOOT_OPTIONS BootOptions,
-    IN PULONG BootOptionsLength
-	)
+NTAPI
+NtQueryBootOptions(IN PBOOT_OPTIONS BootOptions,
+                   IN PULONG BootOptionsLength)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtSetBootEntryOrder(
-    IN PULONG Ids,
-    IN PULONG Count
-	)
+NTAPI
+NtSetBootEntryOrder(IN PULONG Ids,
+                    IN PULONG Count)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtSetBootOptions(
-    IN PBOOT_OPTIONS BootOptions,
-    IN ULONG FieldsToChange
-	)
+NTAPI
+NtSetBootOptions(IN PBOOT_OPTIONS BootOptions,
+                 IN ULONG FieldsToChange)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS
-STDCALL
-NtTranslateFilePath(
-    PFILE_PATH InputFilePath,
-    ULONG OutputType,
-    PFILE_PATH OutputFilePath,
-    ULONG OutputFilePathLength
-	)
+NTAPI
+NtTranslateFilePath(PFILE_PATH InputFilePath,
+                    ULONG OutputType,
+                    PFILE_PATH OutputFilePath,
+                    ULONG OutputFilePathLength)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /* EOF */
--- reactos/ntoskrnl/io/wmi.c
+++ reactos/ntoskrnl/io/wmi.c
@@ -1,11 +1,9 @@
-/* $Id$
- *
- * COPYRIGHT:       See COPYING in the top level directory
- * PROJECT:         ReactOS kernel
+/*
+ * PROJECT:         ReactOS Kernel
+ * LICENSE:         GPL - See COPYING in the top level directory
  * FILE:            ntoskrnl/io/wmi.c
- * PURPOSE:         Windows Management Instrumentation
- *
- * PROGRAMMERS:     Alex Ionescu (alex@relsoft.net)
+ * PURPOSE:         I/O Windows Management Instrumentation (WMI) Support
+ * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
  */
 
 /* INCLUDES *****************************************************************/
@@ -20,240 +18,212 @@
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIRegistrationControl(
-    IN PDEVICE_OBJECT DeviceObject,
-    IN ULONG Action
-	)
+NTAPI
+IoWMIRegistrationControl(IN PDEVICE_OBJECT DeviceObject,
+                         IN ULONG Action)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIAllocateInstanceIds(
-    IN GUID *Guid,
-    IN ULONG InstanceCount,
-    OUT ULONG *FirstInstanceId
-    )
+NTAPI
+IoWMIAllocateInstanceIds(IN GUID *Guid,
+                         IN ULONG InstanceCount,
+                         OUT ULONG *FirstInstanceId)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMISuggestInstanceName(
-    IN PDEVICE_OBJECT PhysicalDeviceObject OPTIONAL,
-    IN PUNICODE_STRING SymbolicLinkName OPTIONAL,
-    IN BOOLEAN CombineNames,
-    OUT PUNICODE_STRING SuggestedInstanceName
-    )
+NTAPI
+IoWMISuggestInstanceName(IN PDEVICE_OBJECT PhysicalDeviceObject OPTIONAL,
+                         IN PUNICODE_STRING SymbolicLinkName OPTIONAL,
+                         IN BOOLEAN CombineNames,
+                         OUT PUNICODE_STRING SuggestedInstanceName)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIWriteEvent(
-    IN PVOID WnodeEventItem
-    )
+NTAPI
+IoWMIWriteEvent(IN PVOID WnodeEventItem)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL IoWMIOpenBlock(
-    IN GUID *DataBlockGuid,
-    IN ULONG DesiredAccess,
-    OUT PVOID *DataBlockObject
-    )
+NTAPI
+IoWMIOpenBlock(IN GUID *DataBlockGuid,
+               IN ULONG DesiredAccess,
+               OUT PVOID *DataBlockObject)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL IoWMIQueryAllData(
-    IN PVOID DataBlockObject,
-    IN OUT ULONG *InOutBufferSize,
-    OUT PVOID OutBuffer
-)
+NTAPI
+IoWMIQueryAllData(IN PVOID DataBlockObject,
+                  IN OUT ULONG *InOutBufferSize,
+                  OUT PVOID OutBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIQueryAllDataMultiple(
-    IN PVOID *DataBlockObjectList,
-    IN ULONG ObjectCount,
-    IN OUT ULONG *InOutBufferSize,
-    OUT PVOID OutBuffer
-)
+NTAPI
+IoWMIQueryAllDataMultiple(IN PVOID *DataBlockObjectList,
+                          IN ULONG ObjectCount,
+                          IN OUT ULONG *InOutBufferSize,
+                          OUT PVOID OutBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIQuerySingleInstance(
-    IN PVOID DataBlockObject,
-    IN PUNICODE_STRING InstanceName,
-    IN OUT ULONG *InOutBufferSize,
-    OUT PVOID OutBuffer
-)
+NTAPI
+IoWMIQuerySingleInstance(IN PVOID DataBlockObject,
+                         IN PUNICODE_STRING InstanceName,
+                         IN OUT ULONG *InOutBufferSize,
+                         OUT PVOID OutBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIQuerySingleInstanceMultiple(
-    IN PVOID *DataBlockObjectList,
-    IN PUNICODE_STRING InstanceNames,
-    IN ULONG ObjectCount,
-    IN OUT ULONG *InOutBufferSize,
-    OUT PVOID OutBuffer
-)
+NTAPI
+IoWMIQuerySingleInstanceMultiple(IN PVOID *DataBlockObjectList,
+                                 IN PUNICODE_STRING InstanceNames,
+                                 IN ULONG ObjectCount,
+                                 IN OUT ULONG *InOutBufferSize,
+                                 OUT PVOID OutBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMISetSingleInstance(
-    IN PVOID DataBlockObject,
-    IN PUNICODE_STRING InstanceName,
-    IN ULONG Version,
-    IN ULONG ValueBufferSize,
-    IN PVOID ValueBuffer
-    )
+NTAPI
+IoWMISetSingleInstance(IN PVOID DataBlockObject,
+                       IN PUNICODE_STRING InstanceName,
+                       IN ULONG Version,
+                       IN ULONG ValueBufferSize,
+                       IN PVOID ValueBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMISetSingleItem(
-    IN PVOID DataBlockObject,
-    IN PUNICODE_STRING InstanceName,
-    IN ULONG DataItemId,
-    IN ULONG Version,
-    IN ULONG ValueBufferSize,
-    IN PVOID ValueBuffer
-    )
+NTAPI
+IoWMISetSingleItem(IN PVOID DataBlockObject,
+                   IN PUNICODE_STRING InstanceName,
+                   IN ULONG DataItemId,
+                   IN ULONG Version,
+                   IN ULONG ValueBufferSize,
+                   IN PVOID ValueBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIExecuteMethod(
-    IN PVOID DataBlockObject,
-    IN PUNICODE_STRING InstanceName,
-    IN ULONG MethodId,
-    IN ULONG InBufferSize,
-    IN OUT PULONG OutBufferSize,
-    IN OUT PUCHAR InOutBuffer
-    )
+NTAPI
+IoWMIExecuteMethod(IN PVOID DataBlockObject,
+                   IN PUNICODE_STRING InstanceName,
+                   IN ULONG MethodId,
+                   IN ULONG InBufferSize,
+                   IN OUT PULONG OutBufferSize,
+                   IN OUT PUCHAR InOutBuffer)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMISetNotificationCallback(
-    IN PVOID Object,
-    IN WMI_NOTIFICATION_CALLBACK Callback,
-    IN PVOID Context
-    )
+NTAPI
+IoWMISetNotificationCallback(IN PVOID Object,
+                             IN WMI_NOTIFICATION_CALLBACK Callback,
+                             IN PVOID Context)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIHandleToInstanceName(
-    IN PVOID DataBlockObject,
-    IN HANDLE FileHandle,
-    OUT PUNICODE_STRING InstanceName
-    )
+NTAPI
+IoWMIHandleToInstanceName(IN PVOID DataBlockObject,
+                          IN HANDLE FileHandle,
+                          OUT PUNICODE_STRING InstanceName)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
-IoWMIDeviceObjectToInstanceName(
-    IN PVOID DataBlockObject,
-    IN PDEVICE_OBJECT DeviceObject,
-    OUT PUNICODE_STRING InstanceName
-    )
+NTAPI
+IoWMIDeviceObjectToInstanceName(IN PVOID DataBlockObject,
+                                IN PDEVICE_OBJECT DeviceObject,
+                                OUT PUNICODE_STRING InstanceName)
 {
-	UNIMPLEMENTED;
-	return STATUS_NOT_IMPLEMENTED;
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /*
  * @unimplemented
  */
 NTSTATUS
-STDCALL
+NTAPI
 NtTraceEvent(IN ULONG TraceHandle,
              IN ULONG Flags,
              IN ULONG TraceHeaderLength,
