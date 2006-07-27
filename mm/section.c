5a099e3953e8543e78dd3b3eab62fa06003d9355
Maarten Bosma <maarten@bosma.de>
2006-07-27 11:32:03 +0000

    Merge 23303
    svn path=/branches/cache_manager_rewrite/; revision=23317

--- mm/section.c
+++ mm/section.c
@@ -133,6 +133,17 @@ NTAPI
 MmGetPageEntrySectionSegment(PMM_SECTION_SEGMENT Segment,
                              ULONG Offset);
 
+PFILE_OBJECT
+NTAPI
+MmGetFileObjectForSection(IN PROS_SECTION_OBJECT Section)
+{
+    PAGED_CODE();
+    ASSERT(Section);
+
+    /* Return the file object */
+    return Section->FileObject; // Section->ControlArea->FileObject on NT
+}
+
 /* Note: Mmsp prefix denotes "Memory Manager Section Private". */
 
 /*
