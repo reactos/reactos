4ade91ae661d5a34f2d1e66a4f26abc44bd6ef5b
Alex Ionescu <aionescu@gmail.com>
2006-08-23 06:59:11 +0000

    - Kill two more hacks. IDR isn't hacked at 0xFFFFFFA on startup anymore, and the PIC is correctly set to 0xFA at startup.
    svn path=/trunk/; revision=23666

--- reactos/hal/halx86/up/halinit_up.c
+++ reactos/hal/halx86/up/halinit_up.c
@@ -21,8 +21,6 @@ VOID
 HalpInitPhase0(PROS_LOADER_PARAMETER_BLOCK LoaderBlock)
 {
   HalpInitPICs();
-  /* FIXME: Big-ass hack. First, should be 0xFFFFFFFF, second, shouldnt' be done here */
-  KeGetPcr()->IDR = 0xFFFFFFFA;
 
   /* Setup busy waiting */
   HalpCalibrateStallExecution();
