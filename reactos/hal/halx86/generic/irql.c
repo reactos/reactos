a82987f86003f0d557eb9aa9aacb1d6b67cf7f7d
Alex Ionescu <aionescu@gmail.com>
2006-08-23 06:35:33 +0000

    - Switch to ASM version of KfLowerIrql
    svn path=/trunk/; revision=23662

--- reactos/hal/halx86/generic/irql.c
+++ reactos/hal/halx86/generic/irql.c
@@ -22,33 +22,6 @@ extern UCHAR SoftIntByteTable[];
 
 /* FUNCTIONS ****************************************************************/
 
-VOID FASTCALL
-KfLowerIrql (KIRQL	NewIrql)
-{
-    ULONG Mask;
-    ULONG Flags;
-    UCHAR Pending;
-
-    Ki386SaveFlags(Flags);
-    Ki386DisableInterrupts();
-
-    if (KeGetPcr()->Irql > DISPATCH_LEVEL)
-    {
-        Mask = KeGetPcr()->IDR | KiI8259MaskTable[NewIrql];
-        WRITE_PORT_UCHAR((PUCHAR)0x21,  (UCHAR)Mask);
-        Mask >>= 8;
-        WRITE_PORT_UCHAR((PUCHAR)0xa1, (UCHAR)Mask);
-    }
-
-
-  KeGetPcr()->Irql = NewIrql;
-  Pending = SoftIntByteTable[KeGetPcr()->IRR];
-  if (Pending > NewIrql)
-    {
-              SoftIntHandlerTable[Pending]();
-    }
-  Ki386RestoreFlags(Flags);
-}
 
 VOID STDCALL HalEndSystemInterrupt (KIRQL Irql, ULONG Unknown2)
 {
