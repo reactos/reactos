c30a8d1cd466b1f1ba39a90ff30d4e171c46b176
Alex Ionescu <aionescu@gmail.com>
2006-08-23 01:23:11 +0000

    - Duplicate code between the remaining functions (KfLowerIRql and HalEndSystemInterrupt) so testing the switch to their ASM versions will be easier. - Use same table as in ASM code.
    svn path=/trunk/; revision=23660

--- reactos/hal/halx86/generic/irql.c
+++ reactos/hal/halx86/generic/irql.c
@@ -12,29 +12,22 @@
 #include <hal.h>
 #define NDEBUG
 #include <debug.h>
-#include <ndk/asm.h>
 
 /* GLOBALS ******************************************************************/
 
-UCHAR Table[8] =
-{
-    0, 0,
-    1, 1,
-    2, 2, 2, 2
-};
-
 typedef VOID (*PSW_HANDLER)(VOID);
 extern PSW_HANDLER SoftIntHandlerTable[];
+extern ULONG KiI8259MaskTable[];
+extern UCHAR SoftIntByteTable[];
 
 /* FUNCTIONS ****************************************************************/
 
-extern ULONG KiI8259MaskTable[];
-
-VOID STATIC
-HalpLowerIrql(KIRQL NewIrql)
+VOID FASTCALL
+KfLowerIrql (KIRQL	NewIrql)
 {
     ULONG Mask;
     ULONG Flags;
+    UCHAR Pending;
 
     Ki386SaveFlags(Flags);
     Ki386DisableInterrupts();
@@ -47,66 +40,42 @@ HalpLowerIrql(KIRQL NewIrql)
         WRITE_PORT_UCHAR((PUCHAR)0xa1, (UCHAR)Mask);
     }
 
-  if (NewIrql >= PROFILE_LEVEL)
-    {
-      KeGetPcr()->Irql = NewIrql;
-      Ki386RestoreFlags(Flags);
-      return;
-    }
-  if (NewIrql >= DISPATCH_LEVEL)
-    {
-      KeGetPcr()->Irql = NewIrql;
-      Ki386RestoreFlags(Flags);
-      return;
-    }
+
   KeGetPcr()->Irql = NewIrql;
-  if (Table[KeGetPcr()->IRR] > NewIrql)
+  Pending = SoftIntByteTable[KeGetPcr()->IRR];
+  if (Pending > NewIrql)
     {
-              SoftIntHandlerTable[Table[KeGetPcr()->IRR]]();
+              SoftIntHandlerTable[Pending]();
     }
   Ki386RestoreFlags(Flags);
 }
 
-/**********************************************************************
- * NAME							EXPORTED
- *	KfLowerIrql
- *
- * DESCRIPTION
- *	Restores the irq level on the current processor
- *
- * ARGUMENTS
- *	NewIrql = Irql to lower to
- *
- * RETURN VALUE
- *	None
- *
- * NOTES
- *	Uses fastcall convention
- */
-VOID FASTCALL
-KfLowerIrql (KIRQL	NewIrql)
-{
-  DPRINT("KfLowerIrql(NewIrql %d)\n", NewIrql);
-  
-  if (NewIrql > KeGetPcr()->Irql)
-    {
-      DbgPrint ("(%s:%d) NewIrql %x CurrentIrql %x\n",
-		__FILE__, __LINE__, NewIrql, KeGetPcr()->Irql);
-      KEBUGCHECK(0);
-      for(;;);
-    }
-  
-  HalpLowerIrql(NewIrql);
-}
-
-
 VOID STDCALL HalEndSystemInterrupt (KIRQL Irql, ULONG Unknown2)
-/*
- * FUNCTION: Finish a system interrupt and restore the specified irq level.
- */
 {
-    //DPRINT1("ENDING: %lx %lx\n", Irql, Unknown2);
-  HalpLowerIrql(Irql);
+    ULONG Mask;
+    ULONG Flags;
+    UCHAR Pending;
+
+    Ki386SaveFlags(Flags);
+    Ki386DisableInterrupts();
+
+    if (KeGetPcr()->Irql > DISPATCH_LEVEL)
+    {
+        Mask = KeGetPcr()->IDR | KiI8259MaskTable[Irql];
+        WRITE_PORT_UCHAR((PUCHAR)0x21,  (UCHAR)Mask);
+        Mask >>= 8;
+        WRITE_PORT_UCHAR((PUCHAR)0xa1, (UCHAR)Mask);
+    }
+
+
+  KeGetPcr()->Irql = Irql;
+  Pending = SoftIntByteTable[KeGetPcr()->IRR];
+  if (Pending > Irql)
+    {
+              SoftIntHandlerTable[Pending]();
+    }
+  Ki386RestoreFlags(Flags);
 }
+
 
 /* EOF */
