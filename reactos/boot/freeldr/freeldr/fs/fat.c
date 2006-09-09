4a7321f583c64ba4982cc3813ee5c8852c79f383
Art Yerkes <art.yerkes@gmail.com>
2006-09-09 13:33:18 +0000

    Removed workaround.  Now know the cause.
    svn path=/branches/powerpc/; revision=23989

--- reactos/boot/freeldr/freeldr/fs/fat.c
+++ reactos/boot/freeldr/freeldr/fs/fat.c
@@ -431,11 +431,7 @@ BOOLEAN FatSearchDirectoryBufferForFile(PVOID DirectoryBuffer, ULONG DirectorySi
 		//
 		if (DirEntry->FileName[0] == '\0')
 		{
-		        /* A workaround for something I see ... there are 
-			 * zero entries in the middle of the dir.  This is
-			 * probably a bug in the way the fat fs was made.
-			 */
-			continue; //return FALSE;
+		        return FALSE;
 		}
 
 		//
