d1e6b52baa78cb6a53f42c70597f92fbc5027e67
Magnus Olsen <magnus@greatlord.com>
2006-11-30 04:35:10 +0000

    forgot this file
    svn path=/trunk/; revision=24982

---
+++ reactos/dll/directx/ddraw/callbacks_surf_hel.c
@@ -0,0 +1,93 @@
+/* $Id: main.c 21434 2006-04-01 19:12:56Z greatlrd $
+ *
+ * COPYRIGHT:            See COPYING in the top level directory
+ * PROJECT:              ReactOS kernel
+ * FILE:                 lib/ddraw/ddraw.c
+ * PURPOSE:              DirectDraw Library 
+ * PROGRAMMER:           Magnus Olsen (greatlrd)
+ *
+ */
+
+#include <windows.h>
+#include "rosdraw.h"
+#include "d3dhal.h"
+#include "ddrawgdi.h"
+
+
+DWORD CALLBACK HelDdSurfAddAttachedSurface(LPDDHAL_ADDATTACHEDSURFACEDATA lpDestroySurface)
+{
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfBlt(LPDDHAL_BLTDATA lpBltData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfDestroySurface(LPDDHAL_DESTROYSURFACEDATA lpDestroySurfaceData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfFlip(LPDDHAL_FLIPDATA lpFlipData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfGetBltStatus(LPDDHAL_GETBLTSTATUSDATA lpGetBltStatusData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfGetFlipStatus(LPDDHAL_GETFLIPSTATUSDATA lpGetFlipStatusData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfLock(LPDDHAL_LOCKDATA lpLockData)
+{	
+	DX_STUB;
+}
+
+
+DWORD CALLBACK HelDdSurfreserved4(DWORD *lpPtr)
+{	
+	/* 
+	   This api is not doucment by MS So I leave it
+	   as stub. 
+     */
+
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfSetClipList(LPDDHAL_SETCLIPLISTDATA lpSetClipListData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfSetColorKey(LPDDHAL_SETCOLORKEYDATA lpSetColorKeyData)
+{	
+	DX_STUB;
+}
+
+
+DWORD CALLBACK HelDdSurfSetOverlayPosition(LPDDHAL_SETOVERLAYPOSITIONDATA lpSetOverlayPositionData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfSetPalette(LPDDHAL_SETPALETTEDATA lpSetPaletteData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfUnlock(LPDDHAL_UNLOCKDATA lpUnLockData)
+{	
+	DX_STUB;
+}
+
+DWORD CALLBACK HelDdSurfUpdateOverlay(LPDDHAL_UPDATEOVERLAYDATA lpUpDateOveryLayData)
+{	
+	DX_STUB;
+}
+
