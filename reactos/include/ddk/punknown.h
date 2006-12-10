275e3ef97e955d23cb52a37a02f505fdc4d99600
Andrew Greenwood <silverblade@reactos.org>
2006-12-10 04:04:03 +0000

    These files aren't complete yet, but this will put them at least in their right place...
    svn path=/trunk/; revision=25107

---
+++ reactos/include/ddk/punknown.h
@@ -0,0 +1,82 @@
+/*
+    ReactOS Kernel-Mode COM
+    by Andrew Greenwood
+
+    Please see COPYING in the top-level directory for license information.
+*/
+
+#ifndef _UNKNOWN_H_
+#define _UNKNOWN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <windef.h>
+#define COM_NO_WINDOWS_H
+#include <basetyps.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+
+/* ===============================================================
+    IUnknown
+*/
+
+DEFINE_GUID(
+    IID_IUnknown,
+    0x00000000, 0x0000, 0x0000, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x00, 0x00, 0x46);
+
+#undef INTERFACE
+#define INTERFACE IUnknown
+
+DECLARE_INTERFACE(IUnknown)
+{
+    STDMETHOD_(NTSTATUS, QueryInterface)( THIS_
+        IN  REFIID,
+        OUT PVOID*)
+    PURE;
+
+    STDMETHOD_(ULONG, AddRef)( THIS )
+    PURE;
+
+    STDMETHOD_(ULONG, Release)( THIS )
+    PURE;
+};
+
+#undef INTERFACE
+
+typedef IUnknown *PUNKNOWN;
+
+
+/* ===============================================================
+    IUnknown definition
+    Boilerplate code macro for use in subclassed interfaces
+*/
+
+#define DEFINE_ABSTRACT_UNKNOWN() \
+    STDMETHOD_(NTSTATUS, QueryInterface)( THIS_ \
+        REFIID InterfaceId, \
+        PVOID* Interface) \
+    PURE; \
+\
+    STDMETHOD_(ULONG, AddRef)(THIS) \
+    PURE; \
+\
+    STDMETHOD_(ULONG, Release)(THIS) \
+    PURE;
+
+
+/* ===============================================================
+    Constructor callback definition
+*/
+
+typedef HRESULT (*PFNCREATEINSTANCE)(
+    OUT PUNKNOWN* Unknown,
+    IN  REFCLSID ClassId,
+    IN  PUNKNOWN OuterUnknown,
+    IN  POOL_TYPE PoolType);
+
+#endif
