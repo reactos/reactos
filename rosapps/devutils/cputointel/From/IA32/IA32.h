b760238f4d7e382eb0be501c14e3fc340003b441
Magnus Olsen <magnus@greatlord.com>
2007-01-20 13:39:01 +0000

    sorry ia32 should be own folder
    svn path=/trunk/; revision=25544

---
+++ rosapps/devutils/cputointel/From/IA32/IA32.h
@@ -0,0 +1,17 @@
+
+#include "../../misc.h"
+
+CPU_INT IA32Brain(  CPU_BYTE *cpu_buffer,
+                     CPU_UNINT cpu_pos,
+                     CPU_UNINT cpu_size,
+                     CPU_UNINT BaseAddress,
+                     CPU_UNINT cpuarch,
+                     FILE *outfp);
+
+/* here we put the prototype for the opcode api that brain need we show a example for it */
+CPU_INT IA32_Add(FILE *out, CPU_BYTE * cpu_buffer, CPU_UNINT cpu_pos, CPU_UNINT cpu_size, CPU_UNINT BaseAddress, CPU_UNINT cpuarch);
+
+
+/* Export comment thing see m68k for example 
+ * in dummy we do not show it, for it is diffent for each cpu
+ */
