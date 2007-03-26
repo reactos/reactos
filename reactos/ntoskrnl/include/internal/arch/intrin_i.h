cd21248e889b04ec5901fa192d4e0b70bc2d2e3d
Hervé Poussineau <hpoussin@reactos.org>
2007-03-26 23:00:58 +0000

    Add missing file
    svn path=/trunk/; revision=26182

---
+++ reactos/ntoskrnl/include/internal/arch/intrin_i.h
@@ -0,0 +1,34 @@
+/*
+ *  ReactOS kernel
+ *  Copyright (C) 1998, 1999, 2000, 2001 ReactOS Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef __NTOSKRNL_INCLUDE_INTERNAL_ARCH_INTRIN_I_H
+#define __NTOSKRNL_INCLUDE_INTERNAL_ARCH_INTRIN_I_H
+
+#ifdef _M_IX86
+#include "../i386/intrin_i.h"
+#elif defined(_M_PPC)
+#include "../powerpc/intrin_i.h"
+#elif defined(_M_MIPS)
+#include "../mips/intrin_i.h"
+#else
+#error "Unknown processor"
+#endif
+
+#endif /* __NTOSKRNL_INCLUDE_INTERNAL_ARCH_INTRIN_I_H */
+
+/* EOF */
