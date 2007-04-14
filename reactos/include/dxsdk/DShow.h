0a8246f2809cc24aeab37588a1fe2dee55828a10
Magnus Olsen <magnus@greatlord.com>
2007-04-14 20:07:28 +0000

    Find some older works, dxsdk header compatible with dxsdk 2004 dec release. I will clean up sdk so dx thuse dx header belong to dxsdk will be here. later. In this folder will contain all header that is compatible with dxsdk 2004 dec. later I will upgrade this folder file to more resent dxsdk headers.
    svn path=/trunk/; revision=26347

---
+++ reactos/include/dxsdk/DShow.h
@@ -0,0 +1,57 @@
+
+#ifndef __DSHOW_INCLUDED__
+#define __DSHOW_INCLUDED__
+
+#ifdef  _MSC_VER
+  #pragma warning(disable:4100)
+  #pragma warning(disable:4201)
+  #pragma warning(disable:4511)
+  #pragma warning(disable:4512)
+  #pragma warning(disable:4514)
+  #if _MSC_VER>=1100
+    #define AM_NOVTABLE __declspec(novtable)
+  #else
+    #define AM_NOVTABLE
+  #endif
+#endif
+
+#include <windows.h>
+#include <windowsx.h>
+#include <olectl.h>
+#include <ddraw.h>
+#include <mmsystem.h>
+
+#ifndef NO_DSHOW_STRSAFE
+  #define NO_SHLWAPI_STRFCNS
+#include <strsafe.h>
+#endif
+
+#ifndef NUMELMS
+   #define NUMELMS(aa) (sizeof(aa)/sizeof((aa)[0]))
+#endif
+
+#include <strmif.h>
+#include <amvideo.h>
+#include <amaudio.h>
+#include <control.h>
+#include <evcode.h>
+#include <uuids.h>
+#include <errors.h>
+#include <edevdefs.h>
+#include <audevcod.h>
+#include <dvdevcod.h>
+
+#ifndef InterlockedExchangePointer
+  #define InterlockedExchangePointer(Target, Value) (PVOID)InterlockedExchange((PLONG)(Target), (LONG)(Value))
+#endif
+
+#ifndef OATRUE
+  #define OATRUE (-1)
+#endif
+
+#ifndef OAFALSE
+  #define OAFALSE (0)
+#endif
+
+#endif 
+
---
+++ reactos/include/dxsdk/il21dec.h
@@ -0,0 +1,68 @@
+
+
+#ifndef __IL21DEC__
+#define __IL21DEC__
+
+typedef enum _AM_LINE21_CCSTYLE
+{
+  AM_L21_CCSTYLE_None = 0,
+  AM_L21_CCSTYLE_PopOn,
+  AM_L21_CCSTYLE_PaintOn,
+  AM_L21_CCSTYLE_RollUp
+} AM_LINE21_CCSTYLE, *PAM_LINE21_CCSTYLE;
+
+typedef enum _AM_LINE21_CCSERVICE
+{
+  AM_L21_CCSERVICE_None = 0,
+  AM_L21_CCSERVICE_Caption1,
+  AM_L21_CCSERVICE_Caption2,
+  AM_L21_CCSERVICE_Text1,
+  AM_L21_CCSERVICE_Text2,
+  AM_L21_CCSERVICE_XDS,
+  AM_L21_CCSERVICE_DefChannel = 10,
+  AM_L21_CCSERVICE_Invalid
+} AM_LINE21_CCSERVICE, *PAM_LINE21_CCSERVICE;
+
+typedef enum _AM_LINE21_DRAWBGMODE
+{
+  AM_L21_DRAWBGMODE_Opaque,
+  AM_L21_DRAWBGMODE_Transparent
+} AM_LINE21_DRAWBGMODE, *PAM_LINE21_DRAWBGMODE;
+
+typedef enum _AM_LINE21_CCSTATE
+{
+  AM_L21_CCSTATE_Off = 0,
+  AM_L21_CCSTATE_On
+} AM_LINE21_CCSTATE, *PAM_LINE21_CCSTATE;
+
+typedef enum _AM_LINE21_CCLEVEL
+{
+  AM_L21_CCLEVEL_TC2 = 0
+} AM_LINE21_CCLEVEL, *PAM_LINE21_CCLEVEL;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DECLARE_INTERFACE_(IAMLine21Decoder, IUnknown)
+{
+  public:
+  STDMETHOD(GetDecoderLevel)(THIS_ AM_LINE21_CCLEVEL *lpLevel) PURE;
+  STDMETHOD(GetCurrentService)(THIS_ AM_LINE21_CCSERVICE *lpService) PURE;
+  STDMETHOD(SetCurrentService)(THIS_ AM_LINE21_CCSERVICE Service) PURE;
+  STDMETHOD(GetServiceState)(THIS_ AM_LINE21_CCSTATE *lpState) PURE;
+  STDMETHOD(SetServiceState)(THIS_ AM_LINE21_CCSTATE State) PURE;
+  STDMETHOD(GetOutputFormat)(THIS_ LPBITMAPINFOHEADER lpbmih) PURE;
+  STDMETHOD(SetOutputFormat)(THIS_ LPBITMAPINFO lpbmi) PURE;
+  STDMETHOD(GetBackgroundColor)(THIS_ DWORD *pdwPhysColor) PURE;
+  STDMETHOD(SetBackgroundColor)(THIS_ DWORD dwPhysColor) PURE;
+  STDMETHOD(GetRedrawAlways)(THIS_ LPBOOL lpbOption) PURE;
+  STDMETHOD(SetRedrawAlways)(THIS_ BOOL bOption) PURE;
+  STDMETHOD(GetDrawBackgroundMode)(THIS_ AM_LINE21_DRAWBGMODE *lpMode) PURE;
+  STDMETHOD(SetDrawBackgroundMode)(THIS_ AM_LINE21_DRAWBGMODE Mode) PURE;
+} ;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
