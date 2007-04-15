cccfc146180c50bff11e78ea1b6a2d61adea8fa6
Magnus Olsen <magnus@greatlord.com>
2007-04-15 09:08:13 +0000

    Find some older works, dxsdk header compatible with dxsdk 2004 dec release. I will clean up sdk so dx thuse dx header belong to dxsdk will be here. later. In this folder will contain all header that is compatible with dxsdk 2004 dec. later I will upgrade this folder file to more resent dxsdk headers.
    svn path=/trunk/; revision=26352

---
+++ reactos/include/dxsdk/PIXPlugin.h
@@ -0,0 +1,47 @@
+
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+#define PIX_PLUGIN_SYSTEM_VERSION 0x101
+
+    enum PIXCOUNTERDATATYPE
+{
+    PCDT_RESERVED,
+    PCDT_FLOAT,
+    PCDT_INT,
+    PCDT_INT64,
+    PCDT_STRING,
+};
+
+typedef int PIXCOUNTERID;
+
+struct PIXCOUNTERINFO
+{
+  PIXCOUNTERID counterID;
+  WCHAR* pstrName;
+  PIXCOUNTERDATATYPE pcdtDataType;
+};
+
+struct PIXPLUGININFO
+{
+  HINSTANCE hinst;
+  WCHAR* pstrPluginName;
+  int iPluginVersion;
+  int iPluginSystemVersion;
+};
+
+BOOL WINAPI PIXBeginExperiment( PIXCOUNTERID id, const WCHAR* pstrApplication );
+BOOL WINAPI PIXEndFrame( PIXCOUNTERID id, UINT iFrame, DWORD* pdwReturnBytes, BYTE** ppReturnData );
+BOOL WINAPI PIXEndExperiment( PIXCOUNTERID id );
+BOOL WINAPI PIXGetCounterInfo( DWORD* pdwReturnCounters, PIXCOUNTERINFO** ppCounterInfoList );
+BOOL WINAPI PIXGetCounterDesc( PIXCOUNTERID id, WCHAR** ppstrCounterDesc );
+BOOL WINAPI PIXGetPluginInfo( PIXPLUGININFO* pPIXPluginInfo );
+
+#ifdef __cplusplus
+};
+#endif
