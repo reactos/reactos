a7c2ad7021e9c4697f1fa7bdd89072805b32e31b
James Tabor <james.tabor@reactos.org>
2007-04-20 23:50:44 +0000

    Implement CreatePolygonRgn and partial implementation of CreatePolyPolygonRgn. Need to implement NtGdiPolyPolyDraw. It is stubbed in my tree.
    svn path=/trunk/; revision=26444

--- reactos/dll/win32/gdi32/objects/region.c
+++ reactos/dll/win32/gdi32/objects/region.c
@@ -21,3 +21,28 @@ GetClipRgn(
 	else	return 0;
 }
 
+
+HRGN
+WINAPI 
+CreatePolygonRgn( const POINT* Point, int Count, int Mode)
+{
+  return CreatePolyPolygonRgn(Point, (const INT*)&Count, 1, Mode);
+}
+
+
+HRGN
+WINAPI
+CreatePolyPolygonRgn( const POINT* Point,
+                      const INT* Count,
+                      int inPolygons,
+                      int Mode)
+{
+/*  return (HRGN) NtGdiPolyPolyDraw(  (HDC) Mode,
+                                 (PPOINT) Point,
+                                 (PULONG) Count,
+                                  (ULONG) inPolygons,
+                                          GdiPolyPolyRgn );
+*/
+}
+
+
