cdd37d19ea49ee9f2d9f6d219d228dc2284b5f8c
Ged Murphy <gedmurphy@reactos.org>
2007-04-26 11:32:13 +0000

    - add a minimal clusapi.h. part 2 of clusapi - this header will need a lot more work.
    svn path=/trunk/; revision=26514

---
+++ reactos/include/psdk/clusapi.h
@@ -0,0 +1,19 @@
+
+#ifndef _CLUSAPI_H
+#define _CLUSAPI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DWORD
+WINAPI
+GetNodeClusterState(
+    IN  LPCWSTR lpszNodeName,
+    OUT DWORD   *pdwClusterState
+    );
+
+#ifdef __cplusplus
+}
+#endif
+#endif // _CLUSAPI_H
