2dd2c5e5b5e5c9f12717db6478bf59f380936976
Dmitry Gorbachev <gorbachev@reactos.org>
2007-04-28 21:23:44 +0000

    Undo r26555 change as suggested by Aleksey & Thomas.
    svn path=/trunk/; revision=26563

--- reactos/base/applications/taskmgr/taskmgr.c
+++ reactos/base/applications/taskmgr/taskmgr.c
@@ -1067,7 +1067,7 @@ LPTSTR GetLastErrorText(LPTSTR lpszBuf, DWORD dwSize)
         lpszBuf[0] = TEXT('\0');
     } else {
         lpszTemp[lstrlen(lpszTemp)-2] = TEXT('\0');  /*remove cr and newline character */
-        wsprintf(lpszBuf, TEXT("%s (0x%x)"), lpszTemp, (int)GetLastError());
+        _stprintf(lpszBuf, TEXT("%s (0x%x)"), lpszTemp, (int)GetLastError());
     }
     if (lpszTemp) {
         LocalFree((HLOCAL)lpszTemp);
