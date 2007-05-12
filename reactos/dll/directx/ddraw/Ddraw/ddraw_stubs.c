1846501455e79e43b7dcbc2115c7da3b47ad1975
Maarten Bosma <maarten@bosma.de>
2007-05-12 16:37:17 +0000

    Reformating
    svn path=/trunk/; revision=26732

--- reactos/dll/directx/ddraw/Ddraw/ddraw_stubs.c
+++ reactos/dll/directx/ddraw/Ddraw/ddraw_stubs.c
@@ -2,7 +2,7 @@
  *
  * COPYRIGHT:            See COPYING in the top level directory
  * PROJECT:              ReactOS
- * FILE:                 
+ * FILE:                 ddraw\ddraw\ddraw_stubs.c
  * PURPOSE:              IDirectDraw7 Implementation
  * PROGRAMMER:           Magnus Olsen, Maarten Bosma
  *
@@ -11,8 +11,7 @@
 #include "../rosdraw.h"
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT
 WINAPI
@@ -21,284 +20,198 @@ Main_DirectDraw_CreateClipper(LPDIRECTDRAW7 iface,
                               LPDIRECTDRAWCLIPPER *ppClipper,
                               IUnknown *pUnkOuter)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/* */
 HRESULT WINAPI Main_DirectDraw_CreatePalette(LPDIRECTDRAW7 iface, DWORD dwFlags,
                   LPPALETTEENTRY palent, LPDIRECTDRAWPALETTE* ppPalette, LPUNKNOWN pUnkOuter)
 {
-    DX_WINDBG_trace();
-
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-
-
-
-
-/*
- * stub
- * Status not done
- */
 HRESULT WINAPI Main_DirectDraw_DuplicateSurface(LPDIRECTDRAW7 iface, LPDIRECTDRAWSURFACE7 src,
                  LPDIRECTDRAWSURFACE7* dst)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI Main_DirectDraw_EnumDisplayModes(LPDIRECTDRAW7 iface, DWORD dwFlags,
                  LPDDSURFACEDESC2 pDDSD, LPVOID context, LPDDENUMMODESCALLBACK2 callback)
 {
-
-   DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * stub
- * Status not done
- */
 HRESULT WINAPI
 Main_DirectDraw_EnumSurfaces(LPDIRECTDRAW7 iface, DWORD dwFlags,
                  LPDDSURFACEDESC2 lpDDSD2, LPVOID context,
                  LPDDENUMSURFACESCALLBACK7 callback)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_FlipToGDISurface(LPDIRECTDRAW7 iface)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_GetCaps(LPDIRECTDRAW7 iface, LPDDCAPS pDriverCaps,
             LPDDCAPS pHELCaps)
 {
-
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI Main_DirectDraw_GetDisplayMode(LPDIRECTDRAW7 iface, LPDDSURFACEDESC2 pDDSD)
 {
-  //LPDDRAWI_DIRECTDRAW_INT This = (LPDDRAWI_DIRECTDRAW_INT)iface;
-
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI
 Main_DirectDraw_GetFourCCCodes(LPDIRECTDRAW7 iface, LPDWORD pNumCodes, LPDWORD pCodes)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI
 Main_DirectDraw_GetGDISurface(LPDIRECTDRAW7 iface,
                                              LPDIRECTDRAWSURFACE7 *lplpGDIDDSSurface)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * IMPLEMENT
- * Status ok
- */
 HRESULT WINAPI
 Main_DirectDraw_GetMonitorFrequency(LPDIRECTDRAW7 iface,LPDWORD freq)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * IMPLEMENT
- * Status ok
- */
 HRESULT WINAPI
 Main_DirectDraw_GetScanLine(LPDIRECTDRAW7 iface, LPDWORD lpdwScanLine)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI
 Main_DirectDraw_GetVerticalBlankStatus(LPDIRECTDRAW7 iface, LPBOOL lpbIsInVB)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT
 WINAPI
 Main_DirectDraw_Initialize (LPDIRECTDRAW7 iface, LPGUID lpGUID)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_RestoreDisplayMode(LPDIRECTDRAW7 iface)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
-   return DD_OK;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_SetDisplayMode (LPDIRECTDRAW7 iface, DWORD dwWidth, DWORD dwHeight,
                                                                 DWORD dwBPP, DWORD dwRefreshRate, DWORD dwFlags)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_WaitForVerticalBlank(LPDIRECTDRAW7 iface, DWORD dwFlags,
                                                    HANDLE h)
 {
-
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
 /*
- * IMPLEMENT
- * Status ok
+ * Status: Implentation removed due to rewrite
  */
 HRESULT WINAPI
 Main_DirectDraw_GetAvailableVidMem(LPDIRECTDRAW7 iface, LPDDSCAPS2 ddscaps,
                    LPDWORD total, LPDWORD free)
 {
-  DX_WINDBG_trace();
-
-   DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_GetSurfaceFromDC(LPDIRECTDRAW7 iface, HDC hdc,
                                                 LPDIRECTDRAWSURFACE7 *lpDDS)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_RestoreAllSurfaces(LPDIRECTDRAW7 iface)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_TestCooperativeLevel(LPDIRECTDRAW7 iface)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_GetDeviceIdentifier(LPDIRECTDRAW7 iface,
                    LPDDDEVICEIDENTIFIER2 pDDDI, DWORD dwFlags)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_StartModeTest(LPDIRECTDRAW7 iface, LPSIZE pModes,
                   DWORD dwNumModes, DWORD dwFlags)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
 
-/*
- * Stub
- * Status todo
- */
 HRESULT WINAPI Main_DirectDraw_EvaluateMode(LPDIRECTDRAW7 iface,DWORD a,DWORD* b)
 {
-    DX_WINDBG_trace();
-    DX_STUB;
+	DX_WINDBG_trace();
+	DX_STUB;
 }
