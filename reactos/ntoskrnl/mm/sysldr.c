355f43c264a08554e933d752550e20e92edf836b
Dmitry Gorbachev <gorbachev@reactos.org>
2007-06-15 12:11:48 +0000

    Improve FIXME comment. For future generations of developers. :)
    svn path=/trunk/; revision=27187

--- reactos/ntoskrnl/mm/sysldr.c
+++ reactos/ntoskrnl/mm/sysldr.c
@@ -1767,7 +1767,7 @@ LoaderScan:
                                    NtHeader->OptionalHeader.AddressOfEntryPoint);
     LdrEntry->SizeOfImage = DriverSize;
     LdrEntry->CheckSum = NtHeader->OptionalHeader.CheckSum;
-    LdrEntry->SectionPointer = NULL; // ?
+    LdrEntry->SectionPointer = NULL; // FIXME: Bug 2151.
 
     /* Now write the DLL name */
     LdrEntry->BaseDllName.Buffer = (PVOID)(LdrEntry + 1);
