6c0a11007bbc026fdbbdcb6a6a94f34bcc9f6dab
Magnus Olsen <magnus@greatlord.com>
2007-07-08 16:09:29 +0000

    use // as comment instead for ;
    svn path=/trunk/; revision=27486

--- reactos/lib/sdk/crt/math/i386/log10_asm.s
+++ reactos/lib/sdk/crt/math/i386/log10_asm.s
@@ -18,10 +18,10 @@ _log10:
 
     push    ebp
     mov     ebp,esp
-    fld     qword ptr [ebp+8]       ; Load real from stack
-    fldlg2                          ; Load log base 10 of 2
-    fxch    st(1)                   ; Exchange st, st(1)
-    fyl2x                           ; Compute the log base 10(x)
+    fld     qword ptr [ebp+8]       // Load real from stack
+    fldlg2                          // Load log base 10 of 2
+    fxch    st(1)                   // Exchange st, st(1)
+    fyl2x                           // Compute the log base 10(x)
     pop     ebp
     ret
 
