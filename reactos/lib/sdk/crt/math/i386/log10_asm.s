aec8f7eff113473614eb6458d79b9d4dc71f6203
Magnus Olsen <magnus@greatlord.com>
2007-07-08 15:33:38 +0000

    correct fix for log10.
    svn path=/trunk/; revision=27482

--- reactos/lib/sdk/crt/math/i386/log10_asm.s
+++ reactos/lib/sdk/crt/math/i386/log10_asm.s
@@ -15,11 +15,13 @@
 /* FUNCTIONS ***************************************************************/
 
 _log10:
-        push    ebp
-        mov     ebp,esp            
-        fldlg2  
-        fld     qword ptr [ebp+8]   
-        fyl2x                    
-        pop     ebp
-        ret
+
+    push    ebp
+    mov     ebp,esp
+    fld     qword ptr [ebp+8]       ; Load real from stack
+    fldlg2                          ; Load log base 10 of 2
+    fxch    st(1)                   ; Exchange st, st(1)
+    fyl2x                           ; Compute the log base 10(x)
+    pop     ebp
+    ret
 
