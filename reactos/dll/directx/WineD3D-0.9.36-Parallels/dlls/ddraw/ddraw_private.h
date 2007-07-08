2e65336e44a39715376927e0c3889fc3c6af52d9
Magnus Olsen <magnus@greatlord.com>
2007-07-08 22:30:46 +0000

    paralles ddraw being build now.
    svn path=/branches/reactx/; revision=27528

--- reactos/dll/directx/WineD3D-0.9.36-Parallels/dlls/ddraw/ddraw_private.h
+++ reactos/dll/directx/WineD3D-0.9.36-Parallels/dlls/ddraw/ddraw_private.h
@@ -33,6 +33,14 @@
 #else
 # include <windows.h>
 #endif
+
+/* Bug in ReactOS PSDK */
+typedef struct _RGNDATA
+{
+    RGNDATAHEADER   rdh;
+    char            Buffer[1];
+} RGNDATA, *PRGNDATA, *NPRGNDATA, *LPRGNDATA;
+
 #include "ddraw.h"
 #include "ddrawi.h"
 #include "d3d.h"
