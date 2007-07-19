68f474c5afded219610e5852bf37165db236acd3
Aleksey Bragin <aleksey@reactos.org>
2007-07-19 12:14:08 +0000

    Patch by Dmitry Chapyshev <lentind@yandex.ru>: - Make downloader icon in multiple color depths. - Remove a newline character from winver's window title.
    svn path=/trunk/; revision=27725

--- rosapps/winver/winver.c
+++ rosapps/winver/winver.c
@@ -2,7 +2,7 @@
 #include <tchar.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-  static const WCHAR szROS[] = { 'R','e','a','c','t','O','S','\n',0 };
+  static const WCHAR szROS[] = { 'R','e','a','c','t','O','S',0 };
   UNREFERENCED_PARAMETER(lpCmdLine);
   UNREFERENCED_PARAMETER(nCmdShow);
   UNREFERENCED_PARAMETER(hPrevInstance);
