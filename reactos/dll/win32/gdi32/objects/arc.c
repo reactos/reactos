29241f09f4b5e8a0d9af05877dce31788a5caf63
James Tabor <james.tabor@reactos.org>
2007-08-01 19:26:55 +0000

    Move meta code from painting to arc. Including the new Pie function with ifed out.
    svn path=/trunk/; revision=28079

--- reactos/dll/win32/gdi32/objects/arc.c
+++ reactos/dll/win32/gdi32/objects/arc.c
@@ -14,6 +14,31 @@ Arc(
 	int nYEndArc
 )
 {
+#if 0
+// Handle something other than a normal dc object.
+ if (GDI_HANDLE_GET_TYPE(hDC) != GDI_OBJECT_TYPE_DC)
+ {
+    if (GDI_HANDLE_GET_TYPE(hDC) == GDI_OBJECT_TYPE_METADC)
+//    Call Wine (rewrite of) MFDRV_MetaParam8
+      return MFDRV_MetaParam8( hDC, META_ARC, a1, a2, a3, a4, a5, a6, a7, a8)
+    else
+    {
+      PLDC pLDC = GdiGetLDC(hDC);      
+      if ( !pLDC )
+      {
+         SetLastError(ERROR_INVALID_HANDLE);
+         return FALSE;
+      }
+      if (pLDC->iType == LDC_EMFLDC)
+      {
+//      Call Wine (rewrite of) EMFDRV_ArcChordPie
+        BOOL Ret = EMFDRV_ArcChordPie( hDC, a1, a2, a3, a4, a5, a6, a7, a8, EMR_ARC);
+        return Ret;
+      }
+      return FALSE;  
+    } 
+ }
+#endif
 	return NtGdiArcInternal(GdiTypeArc,
 	                        hDC,
 	                        nLeftRect,
@@ -39,6 +64,29 @@ ArcTo(
 	int  nXRadial2,
 	int  nYRadial2)
 {
+#if 0
+// Handle something other than a normal dc object.
+ if (GDI_HANDLE_GET_TYPE(hDC) != GDI_OBJECT_TYPE_DC)
+ {
+    if (GDI_HANDLE_GET_TYPE(hDC) == GDI_OBJECT_TYPE_METADC)
+      return FALSE; //No meta support for ArcTo
+    else
+    {
+      PLDC pLDC = GdiGetLDC(hDC);
+      if ( !pLDC )
+      {
+         SetLastError(ERROR_INVALID_HANDLE);
+         return FALSE;
+      }
+      if (pLDC->iType == LDC_EMFLDC)
+      {
+        BOOL Ret = EMFDRV_ArcChordPie( hDC, a1, a2, a3, a4, a5, a6, a7, a8, EMR_ARCTO);
+        return Ret;
+      }
+      return FALSE;
+    }
+ }
+#endif
 	return NtGdiArcInternal(GdiTypeArcTo,
 	                        hDC,
 	                        nLeftRect,
@@ -64,6 +112,29 @@ Chord(
 	int  nXRadial2,
 	int  nYRadial2)
 {
+#if 0
+// Handle something other than a normal dc object.
+ if (GDI_HANDLE_GET_TYPE(hDC) != GDI_OBJECT_TYPE_DC)
+ {
+    if (GDI_HANDLE_GET_TYPE(hDC) == GDI_OBJECT_TYPE_METADC)
+      return MFDRV_MetaParam8( hDC, META_CHORD, a1, a2, a3, a4, a5, a6, a7, a8)
+    else
+    {
+      PLDC pLDC = GdiGetLDC(hDC);
+      if ( !pLDC )
+      {
+         SetLastError(ERROR_INVALID_HANDLE);
+         return FALSE;
+      }
+      if (pLDC->iType == LDC_EMFLDC)
+      {
+        BOOL Ret = EMFDRV_ArcChordPie( hDC, a1, a2, a3, a4, a5, a6, a7, a8, EMR_CHORD);
+        return Ret;
+      }
+      return FALSE;
+    }
+ }
+#endif
 	return NtGdiArcInternal(GdiTypeChord,
 	                        hDC,
 	                        nLeftRect,
@@ -76,4 +147,47 @@ Chord(
 	                        nYRadial2);
 }
 
+
+#if 0
+/*
+ * @unimplemented
+ */
+BOOL
+STDCALL
+Pie(
+	HDC	hDC,
+	int	a1,
+	int	a2,
+	int	a3,
+	int	a4,
+	int	a5,
+	int	a6,
+	int	a7,
+	int	a8
+	)
+{
+// Handle something other than a normal dc object.
+ if (GDI_HANDLE_GET_TYPE(hDC) != GDI_OBJECT_TYPE_DC)
+ {
+    if (GDI_HANDLE_GET_TYPE(hDC) == GDI_OBJECT_TYPE_METADC)
+      return MFDRV_MetaParam8( hDC, META_PIE, a1, a2, a3, a4, a5, a6, a7, a8)
+    else
+    {
+      PLDC pLDC = GdiGetLDC(hDC);
+      if ( !pLDC )
+      {
+         SetLastError(ERROR_INVALID_HANDLE);
+         return FALSE;
+      }
+      if (pLDC->iType == LDC_EMFLDC)
+      {
+        BOOL Ret = EMFDRV_ArcChordPie( hDC, a1, a2, a3, a4, a5, a6, a7, a8, EMR_PIE);
+        return Ret;
+      }
+      return FALSE;
+    }
+ }
+ return NtGdiArcInternal(GdiTypePie, hDC, a1, a2, a3, a4, a5, a6, a7, a8);
+}
+#endif
 
