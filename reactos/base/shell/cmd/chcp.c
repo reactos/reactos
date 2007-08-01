953da5af6e90d17871613b59d8e9182d7024c449
Christoph von Wittich <christoph_vw@reactos.org>
2007-08-01 22:41:35 +0000

    fix a memory leak
    svn path=/trunk/; revision=28084

--- reactos/base/shell/cmd/chcp.c
+++ reactos/base/shell/cmd/chcp.c
@@ -41,6 +41,7 @@ INT CommandChcp (LPTSTR cmd, LPTSTR param)
 		/* display active code page number */
 		LoadString(CMD_ModuleHandle, STRING_CHCP_ERROR1, szMsg, RC_STRING_MAX_SIZE);
 		ConErrPrintf(szMsg, InputCodePage);
+		freep (arg);
 		return 0;
 	}
 
@@ -50,6 +51,7 @@ INT CommandChcp (LPTSTR cmd, LPTSTR param)
 		LoadString(CMD_ModuleHandle, STRING_ERROR_INVALID_PARAM_FORMAT, szMsg, RC_STRING_MAX_SIZE);
 		ConErrPrintf(szMsg, param);
 		nErrorLevel = 1;
+		freep (arg);
 		return 1;
 	}
 
