9c968b2b68885d2a3f1884c09652400207256ac6
Hervé Poussineau <hpoussin@reactos.org>
2007-09-24 11:42:04 +0000

    Remove this headers set, as they already exist in include/crt
    svn path=/trunk/; revision=29179

--- reactos/lib/3rdparty/mingw/include/mem.h
+++
@@ -1,6 +0,0 @@
-/*
- * This file is part of the Mingw32 package.
- *
- * mem.h maps to string.h
- */
-#include <string.h>
--- reactos/lib/3rdparty/mingw/include/memory.h
+++
@@ -1,7 +0,0 @@
-/*
- * This file is part of the Mingw32 package.
- *
- * memory.h maps to the standard string.h header.
- */
-
-#include	<string.h>
