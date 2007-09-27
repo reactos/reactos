b6ebc1b19ce5346e34f35c65729a7b22eb0757b4
Timo Kreuzer <timo.kreuzer@reactos.org>
2007-09-27 22:32:09 +0000

    add another small test for AddFontResource
    svn path=/trunk/; revision=29263

--- rostests/apitests/gdi32api/tests/AddFontResourceEx.c
+++ rostests/apitests/gdi32api/tests/AddFontResourceEx.c
@@ -1,12 +1,14 @@
+#define STAMP_DESIGNVECTOR (0x8000000 + 'd' + ('v' << 8))
 
 INT
 Test_AddFontResourceEx(PTESTINFO pti)
 {
 	WCHAR szFileName[MAX_PATH];
-	GetEnvironmentVariableW(L"systemroot", szFileName, MAX_PATH);
 
+	GetEnvironmentVariableW(L"systemroot", szFileName, MAX_PATH);
 	wcscat(szFileName, L"\\Fonts\\cour.ttf");
 
+	/* Test flags = 0 */
 	SetLastError(ERROR_SUCCESS);
 	TEST(AddFontResourceExW(szFileName, 0, 0) != 0);
 	TEST(GetLastError() == ERROR_SUCCESS);
@@ -14,6 +16,10 @@ Test_AddFontResourceEx(PTESTINFO pti)
 	SetLastError(ERROR_SUCCESS);
 	TEST(AddFontResourceExW(szFileName, 256, 0) == 0);
 	TEST(GetLastError() == ERROR_INVALID_PARAMETER);
+
+	/* Test invalid pointer as last parameter */
+	TEST(AddFontResourceExW(szFileName, 0, (void*)-1) != 0);
+
 
 	return APISTATUS_NORMAL;
 }
