b817f334af2eaca8b1b192e4499df72157bedb32
Johannes Anderwald <johannes.anderwald@reactos.org>
2007-10-11 17:07:58 +0000

    Jonathan Ernst <jonathan@ernstfamily.ch> - Update the address of the Free Software Foundation. Stefan Leichter <Stefan.Leichter@camline.com> - Fix the file version string of the version resource.
    svn path=/trunk/; revision=29506

--- reactos/dll/win32/shell32/version.h
+++ reactos/dll/win32/shell32/version.h
@@ -15,10 +15,14 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
 #define WINE_FILEVERSION_MAJOR         5
 #define WINE_FILEVERSION_MINOR         0
 #define WINE_FILEVERSION_BUILD      3900
 #define WINE_FILEVERSION_PLATFORMID 6975
+
+/* FIXME: when libs/wpp gets fixed to support concatenation we can remove
+ * this and define it in version.rc */
+#define WINE_FILEVERSION_STR "5.0.3900.6975"
