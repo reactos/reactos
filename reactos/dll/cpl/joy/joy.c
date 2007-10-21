60efb9002e307c1551ceb60e9332ba971fe495c8
Daniel Reimer <reimer.daniel@freenet.de>
2007-10-21 13:49:27 +0000

    Remove Trailing spaces. Add German Translations for joy and tapiui.rc. Fix a typo in en-US of tapiui.
    svn path=/trunk/; revision=29735

--- reactos/dll/cpl/joy/joy.c
+++ reactos/dll/cpl/joy/joy.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* 
+/*
  *
  * PROJECT:         ReactOS Software Control Panel
  * FILE:            dll/cpl/joy/joy.c
@@ -36,7 +36,7 @@ HWND MainDlg;
 
 /* Applets */
 
-APPLET Applets[NUM_APPLETS] = 
+APPLET Applets[NUM_APPLETS] =
 {
     {IDI_CPLSYSTEM, IDS_CPLSYSTEMNAME, IDS_CPLSYSTEMDESCRIPTION, SystemApplet}
 };
