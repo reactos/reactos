115c413a45e2dbca524cffb28fac6c7370ff4bcc
Magnus Olsen <magnus@greatlord.com>
2007-10-22 20:15:49 +0000

    adding dxgthk.sys stub we need it for dxg.sys
    svn path=/branches/reactx/; revision=29795

---
+++ reactos/drivers/directx/dxgthk/main.c
@@ -0,0 +1,19 @@
+
+/*
+ * COPYRIGHT:        See COPYING in the top level directory
+ * PROJECT:          ReactOS kernel
+ * PURPOSE:          Native driver for dxg implementation
+ * FILE:             drivers/directx/dxg/main.c
+ * PROGRAMER:        Magnus olsen (magnus@greatlord.com)
+ * REVISION HISTORY:
+ *       15/10-2007   Magnus Olsen
+ */
+
+NTSTATUS
+DriverEntry(IN PVOID Context1,
+            IN PVOID Context2)
+{
+    return 0;
+}
+
+
