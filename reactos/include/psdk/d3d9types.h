7a53ca4d32a2a8b74bb5deefe2d31ea3ec6e4036
Magnus Olsen <magnus@greatlord.com>
2007-11-25 12:08:36 +0000

    fix compiling warings for d3d9 that I got local.
    svn path=/trunk/; revision=30731

--- reactos/include/psdk/d3d9types.h
+++ reactos/include/psdk/d3d9types.h
@@ -191,9 +191,11 @@
 #define D3DVERTEXTEXTURESAMPLER2 (D3DDMAPSAMPLER+3)
 #define D3DVERTEXTEXTURESAMPLER3 (D3DDMAPSAMPLER+4)
 
+#ifndef MAKEFOURCC
 #define MAKEFOURCC(ch0, ch1, ch2, ch3)  \
     ((DWORD)(BYTE)(ch0) | ((DWORD)(BYTE)(ch1) << 8) |  \
     ((DWORD)(BYTE)(ch2) << 16) | ((DWORD)(BYTE)(ch3) << 24 ))
+#endif
 
 /* Constants used by D3DPRESENT_PARAMETERS. when creating a device or swapchain */
 
