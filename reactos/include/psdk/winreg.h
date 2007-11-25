6dcabded4e284faa64b1fd5608b05cfdb5e9c54f
Johannes Anderwald <johannes.anderwald@reactos.org>
2007-11-25 21:47:17 +0000

    - add RRF_RT_ANY constant
    svn path=/trunk/; revision=30762

--- reactos/include/psdk/winreg.h
+++ reactos/include/psdk/winreg.h
@@ -47,6 +47,7 @@ extern "C" {
 #define RRF_RT_REG_QWORD        (1 << 6)
 #define RRF_RT_DWORD            (RRF_RT_REG_BINARY | RRF_RT_REG_DWORD)
 #define RRF_RT_QWORD            (RRF_RT_REG_BINARY | RRF_RT_REG_QWORD)
+#define RRF_RT_ANY              (0x0000FFFF)
 #define RRF_NOEXPAND            (1 << 28)
 #define RRF_ZEROONFAILURE       (1 << 29)
 #endif
