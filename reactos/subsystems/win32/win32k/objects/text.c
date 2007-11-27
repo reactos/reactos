reactos/reactos

commit 755bf1c3e718c659e675a8f55e5e8c74fb0b3b95

- make code ANSI-C compatible (makes msvc more happier )
svn path=/trunk/; revision=30780

reactos/subsystems/win32/win32k/objects/text.c
@@ -173,7 +173,7 @@ IntLoadSystemFonts(VOID)
    HANDLE hDirectory;
    BYTE *DirInfoBuffer;
    PFILE_DIRECTORY_INFORMATION DirInfo;
-   BOOL bRestartScan = TRUE;
+   BOOLEAN bRestartScan = TRUE;
    NTSTATUS Status;
 
    RtlInitUnicodeString(&Directory, L"\\SystemRoot\\media\\fonts\\");
@@ -2583,7 +2583,7 @@ FTVectorToPOINTFX(FT_Vector *vec, POINTFX *pt)
    in the highest 16 bits and the decimal part in the lowest 16 bits of the FT_Fixed.
    It fails if the integer part of the float number is greater than SHORT_MAX.
 */
-static inline FT_Fixed FT_FixedFromFloat(float f)
+static __inline FT_Fixed FT_FixedFromFloat(float f)
 {
 	short value = f;
 	unsigned short fract = (f - value) * 0xFFFF;
@@ -2594,7 +2594,7 @@ static inline FT_Fixed FT_FixedFromFloat(float f)
    This function builds an FT_Fixed from a FIXED. It simply put f.value
    in the highest 16 bits and f.fract in the lowest 16 bits of the FT_Fixed.
 */
-static inline FT_Fixed FT_FixedFromFIXED(FIXED f)
+static __inline FT_Fixed FT_FixedFromFIXED(FIXED f)
 {
 	return (FT_Fixed)((long)f.value << 16 | (unsigned long)f.fract);
 }
@@ -3364,10 +3364,11 @@ DWORD
 NtGdiGetCharSet(HDC hDC)
 {
   PDC Dc;
+  PDC_ATTR Dc_Attr;
   DWORD cscp = IntGdiGetCharSet(hDC);
   // If here, update everything!  
   Dc = DC_LockDc(hDC);
-  PDC_ATTR Dc_Attr = Dc->pDc_Attr;
+  Dc_Attr = Dc->pDc_Attr;
   if (!Dc_Attr) Dc_Attr = &Dc->Dc_Attr;
   if (!Dc)
   {
@@ -3621,7 +3622,7 @@ NtGdiGetTextExtentExW(
 
   /* FIXME: Handle fl */
 
-  if (Count < 0)
+  if (!Count)
     {
       SetLastWin32Error(ERROR_INVALID_PARAMETER);
       return FALSE;

reactos/subsystems/win32/win32k/ntuser/scrollbar.c
@@ -330,11 +330,7 @@ co_IntSetScrollInfo(PWINDOW_OBJECT Window, INT nBar, LPCSCROLLINFO lpsi, BOOL bR
    }
 
    /* Make sure the page size is valid */
-   if (Info->nPage < 0)
-   {
-      Info->nPage = 0;
-   }
-   else if (Info->nMax - Info->nMin + 1 < Info->nPage)
+   if (Info->nMax - Info->nMin + 1 < Info->nPage)
    {
       Info->nPage = Info->nMax - Info->nMin + 1;
    }

reactos/subsystems/win32/win32k/ntuser/windc.c
@@ -261,10 +261,11 @@ DceReleaseDC(DCE* dce, BOOL EndPaint)
    {
      if (!(dce->DCXFlags & DCX_NORESETATTRS))
      {
+       PDC dc;
        /* make the DC clean so that SetDCState doesn't try to update the vis rgn */
        IntGdiSetHookFlags(dce->hDC, DCHF_VALIDATEVISRGN);
 
-       PDC dc = DC_LockDc ( dce->hDC );
+       dc = DC_LockDc ( dce->hDC );
        IntGdiCopyFromSaveState(dc, defaultDCstate, dce->hDC ); // Was SetDCState.
 
        dce->DCXFlags &= ~DCX_DCEBUSY;

reactos/subsystems/win32/win32k/objects/color.c
@@ -627,7 +627,7 @@ BOOL STDCALL NtGdiResizePalette(HPALETTE  hpal,
 }
 
 BOOL STDCALL NtGdiSetColorAdjustment(HDC  hDC,
-                             CONST LPCOLORADJUSTMENT  ca)
+                                     LPCOLORADJUSTMENT  ca)
 {
    UNIMPLEMENTED;
    return FALSE;

reactos/subsystems/win32/win32k/objects/coord.c
@@ -37,14 +37,16 @@
 void FASTCALL
 IntFixIsotropicMapping(PDC dc)
 {
+  ULONG xdim;
+  ULONG ydim;
   PDC_ATTR Dc_Attr = dc->pDc_Attr;
   if(!Dc_Attr) Dc_Attr = &dc->Dc_Attr;
 
-  ULONG xdim = EngMulDiv(Dc_Attr->szlViewportExt.cx,
+  xdim = EngMulDiv(Dc_Attr->szlViewportExt.cx,
                ((PGDIDEVICE)dc->pPDev)->GDIInfo.ulHorzSize,
                ((PGDIDEVICE)dc->pPDev)->GDIInfo.ulHorzRes) /
                                 Dc_Attr->szlWindowExt.cx;
-  ULONG ydim = EngMulDiv(Dc_Attr->szlViewportExt.cy,
+  ydim = EngMulDiv(Dc_Attr->szlViewportExt.cy,
                ((PGDIDEVICE)dc->pPDev)->GDIInfo.ulVertSize,
                ((PGDIDEVICE)dc->pPDev)->GDIInfo.ulVertRes) /
                                 Dc_Attr->szlWindowExt.cy;
@@ -85,8 +87,8 @@ IntGdiCombineTransform(LPXFORM XFormResult,
 }
 
 BOOL STDCALL NtGdiCombineTransform(LPXFORM  UnsafeXFormResult,
-                           CONST LPXFORM  Unsafexform1,
-                           CONST LPXFORM  Unsafexform2)
+                                   LPXFORM  Unsafexform1,
+                                   LPXFORM  Unsafexform2)
 {
   XFORM  xformTemp;
   XFORM  xform1 = {0}, xform2 = {0};
@@ -433,7 +435,7 @@ NtGdiTransformPoints( HDC hDC,
 BOOL
 STDCALL
 NtGdiModifyWorldTransform(HDC hDC,
-                          CONST LPXFORM  UnsafeXForm,
+                          LPXFORM  UnsafeXForm,
                           DWORD Mode)
 {
    PDC dc;

reactos/subsystems/win32/win32k/objects/dc.c
@@ -1103,7 +1103,7 @@ NtGdiGetDCPoint( HDC hDC, UINT iPoint, PPOINTL Point)
 {
   BOOL Ret = TRUE;
   DC *dc;
-  POINT SafePoint;
+  POINTL SafePoint;
   SIZE Size;
   NTSTATUS Status = STATUS_SUCCESS;
 
@@ -1997,7 +1997,7 @@ NtGdiSelectObject(HDC  hDC, HGDIOBJ  hGDIObj)
       else
       {
         dc->w.bitsPerPixel = BitsPerFormat(pb->SurfObj.iBitmapFormat);
-        dc->w.hPalette = ((DEVINFO)((GDIDEVICE *)dc->pPDev)->DevInfo).hpalDefault;
+        dc->w.hPalette = ((GDIDEVICE *)dc->pPDev)->DevInfo.hpalDefault;
       }
 
       /* Reselect brush and pen to regenerate the XLATEOBJs. */
@@ -2373,6 +2373,7 @@ FASTCALL
 DC_AllocateDcAttr(HDC hDC)
 {
   PVOID NewMem = NULL;
+  PDC pDC;
   HANDLE Pid = NtCurrentProcess();
   ULONG MemSize = sizeof(DC_ATTR); //PAGE_SIZE it will allocate that size
 
@@ -2399,7 +2400,7 @@ DC_AllocateDcAttr(HDC hDC)
     }
   }
   KeLeaveCriticalRegion();
-  PDC pDC = DC_LockDc(hDC);
+  pDC = DC_LockDc(hDC);
   if(NewMem)
   {
      pDC->pDc_Attr = NewMem; // Store pointer

reactos/subsystems/win32/win32k/objects/dcutil.c
@@ -134,9 +134,10 @@ BOOL
 FASTCALL
 DCU_SynchDcAttrtoUser(HDC hDC)
 {
+  BOOL Ret;
   PDC pDC = DC_LockDc ( hDC );
   if (!pDC) return FALSE;
-  BOOL Ret = DCU_SyncDcAttrtoUser(pDC);
+  Ret = DCU_SyncDcAttrtoUser(pDC);
   DC_UnlockDc( pDC );
   return Ret;
 }

reactos/subsystems/win32/win32k/objects/dibobj.c
@@ -209,7 +209,7 @@ IntSetDIBits(
   }
 
   // Destination palette obtained from the hDC
-  hDCPalette = PALETTE_LockPalette(((DEVINFO)((GDIDEVICE *)DC->pPDev)->DevInfo).hpalDefault);
+  hDCPalette = PALETTE_LockPalette(((GDIDEVICE *)DC->pPDev)->DevInfo.hpalDefault);
   if (NULL == hDCPalette)
     {
       EngUnlockSurface(SourceSurf);
@@ -219,7 +219,7 @@ IntSetDIBits(
       return 0;
     }
   DDB_Palette_Type = hDCPalette->Mode;
-  DDB_Palette = ((DEVINFO)((GDIDEVICE *)DC->pPDev)->DevInfo).hpalDefault;
+  DDB_Palette = ((GDIDEVICE *)DC->pPDev)->DevInfo.hpalDefault;
   PALETTE_UnlockPalette(hDCPalette);
 
   // Source palette obtained from the BITMAPINFO
@@ -489,9 +489,11 @@ NtGdiGetDIBitsInternal(HDC hDC,
         {
             if (Info->bmiHeader.biSize == sizeof(BITMAPCOREHEADER))
             {
+                BITMAPCOREHEADER* coreheader;
+
                 ProbeForWrite(Info, sizeof(BITMAPINFO), 1);
 
-                BITMAPCOREHEADER* coreheader = (BITMAPCOREHEADER*) Info;
+                coreheader = (BITMAPCOREHEADER*) Info;
                 coreheader->bcWidth =BitmapObj->SurfObj.sizlBitmap.cx;
                 coreheader->bcPlanes = 1;
                 coreheader->bcBitCount =  BitsPerFormat(BitmapObj->SurfObj.iBitmapFormat);

commit 19c56142c7022217b2359b66731c77b83b834218

Fix underline bug introduced in r30780
svn path=/trunk/; revision=30808

reactos/subsystems/win32/win32k/objects/text.c
@@ -3622,11 +3622,6 @@ NtGdiGetTextExtentExW(
 
   /* FIXME: Handle fl */
 
-  if (!Count)
-    {
-      SetLastWin32Error(ERROR_INVALID_PARAMETER);
-      return FALSE;
-    }
   if (0 == Count)
     {
       Size.cx = 0;