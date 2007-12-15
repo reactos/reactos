fd649cdc194b28ec41a6a07786e21503e384149b
Sylvain Petreolle <spetreolle@yahoo.fr>
2007-12-15 11:30:07 +0000

    include SDK first
    svn path=/trunk/; revision=31236

--- rostests/drivers/memtest/memtest.c
+++ rostests/drivers/memtest/memtest.c
@@ -21,11 +21,11 @@
 
 /* INCLUDES *******************************************************************/
 
-//#define NDEBUG
-#include <debug.h>
-
 #include <ddk/ntddk.h>
 #include "memtest.h"
+
+//#define NDEBUG
+#include <debug.h>
 
 HANDLE MonitorThreadHandle;
 
