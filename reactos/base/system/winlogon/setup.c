0099292d684032ad7b8ec35d837aa793700618a6
Hervé Poussineau <hpoussin@reactos.org>
2007-12-21 10:06:46 +0000

    [FORMATTING] Fix indentation
    svn path=/trunk/; revision=31366

--- reactos/base/system/winlogon/setup.c
+++ reactos/base/system/winlogon/setup.c
@@ -19,135 +19,137 @@ WINE_DEFAULT_DEBUG_CHANNEL(winlogon);
 DWORD
 GetSetupType(VOID)
 {
-  DWORD dwError;
-  HKEY hKey;
-  DWORD dwType;
-  DWORD dwSize;
-  DWORD dwSetupType;
+	DWORD dwError;
+	HKEY hKey;
+	DWORD dwType;
+	DWORD dwSize;
+	DWORD dwSetupType;
 
-  dwError = RegOpenKeyExW(HKEY_LOCAL_MACHINE,
-			 L"SYSTEM\\Setup", //TEXT("SYSTEM\\Setup"),
-			 0,
-			 KEY_QUERY_VALUE,
-			 &hKey);
-  if (dwError != ERROR_SUCCESS)
-    {
-      return 0;
-    }
+	TRACE("GetSetupType()\n");
 
-  dwSize = sizeof(DWORD);
-  dwError = RegQueryValueExW (hKey,
-			     L"SetupType", //TEXT("SetupType"),
-			     NULL,
-			     &dwType,
-			     (LPBYTE)&dwSetupType,
-			     &dwSize);
-  RegCloseKey (hKey);
-  if (dwError != ERROR_SUCCESS || dwType != REG_DWORD)
-    {
-      return 0;
-    }
+	/* Open key */
+	dwError = RegOpenKeyExW(
+		HKEY_LOCAL_MACHINE,
+		L"SYSTEM\\Setup",
+		0,
+		KEY_QUERY_VALUE,
+		&hKey);
+	if (dwError != ERROR_SUCCESS)
+		return 0;
 
-  return dwSetupType;
+	/* Read key */
+	dwSize = sizeof(DWORD);
+	dwError = RegQueryValueExW(
+		hKey,
+		L"SetupType",
+		NULL,
+		&dwType,
+		(LPBYTE)&dwSetupType,
+		&dwSize);
+
+	/* Close key, and check if returned values are correct */
+	RegCloseKey(hKey);
+	if (dwError != ERROR_SUCCESS || dwType != REG_DWORD || dwSize != sizeof(DWORD))
+		return 0;
+
+	TRACE("GetSetupType() returns %lu\n", dwSetupType);
+	return dwSetupType;
 }
-
 
 static DWORD WINAPI
-RunSetupThreadProc (IN LPVOID lpParameter)
+RunSetupThreadProc(
+	IN LPVOID lpParameter)
 {
-  PROCESS_INFORMATION ProcessInformation;
-  STARTUPINFOW StartupInfo;
-  WCHAR Shell[MAX_PATH];
-  WCHAR CommandLine[MAX_PATH];
-  BOOL Result;
-  DWORD dwError;
-  HKEY hKey;
-  DWORD dwType;
-  DWORD dwSize;
-  DWORD dwExitCode;
+	PROCESS_INFORMATION ProcessInformation;
+	STARTUPINFOW StartupInfo;
+	WCHAR Shell[MAX_PATH];
+	WCHAR CommandLine[MAX_PATH];
+	BOOL Result;
+	DWORD dwError;
+	HKEY hKey;
+	DWORD dwType;
+	DWORD dwSize;
+	DWORD dwExitCode;
 
-  TRACE ("RunSetup() called\n");
+	TRACE("RunSetup() called\n");
 
-  dwError = RegOpenKeyExW (HKEY_LOCAL_MACHINE,
-			   L"SYSTEM\\Setup",
-			   0,
-			   KEY_QUERY_VALUE,
-			   &hKey);
-  if (dwError != ERROR_SUCCESS)
-    {
-      return FALSE;
-    }
+	/* Open key */
+	dwError = RegOpenKeyExW(
+		HKEY_LOCAL_MACHINE,
+		L"SYSTEM\\Setup",
+		0,
+		KEY_QUERY_VALUE,
+		&hKey);
+	if (dwError != ERROR_SUCCESS)
+		return FALSE;
 
-  dwSize = MAX_PATH;
-  dwError = RegQueryValueExW (hKey,
-			      L"CmdLine",
-			      NULL,
-			      &dwType,
-			      (LPBYTE)Shell,
-			      &dwSize);
-  RegCloseKey (hKey);
-  if (dwError != ERROR_SUCCESS)
-    {
-      return FALSE;
-    }
+	/* Read key */
+	dwSize = (sizeof(Shell) / sizeof(Shell[0])) - 1;
+	dwError = RegQueryValueExW(
+		hKey,
+		L"CmdLine",
+		NULL,
+		&dwType,
+		(LPBYTE)Shell,
+		&dwSize);
+	RegCloseKey(hKey);
+	if (dwError != ERROR_SUCCESS)
+		return FALSE;
 
-  if (dwType == REG_EXPAND_SZ)
-    {
-      ExpandEnvironmentStringsW(Shell, CommandLine, MAX_PATH);
-    }
-  else if (dwType == REG_SZ)
-    {
-      wcscpy(CommandLine, Shell);
-    }
-  else
-    {
-      return FALSE;
-    }
+	/* Finish string */
+	Shell[dwSize / sizeof(WCHAR)] = UNICODE_NULL;
 
-  TRACE ("Should run '%S' now.\n", CommandLine);
+	/* Expand string (if applicable) */
+	if (dwType == REG_EXPAND_SZ)
+		ExpandEnvironmentStringsW(Shell, CommandLine, MAX_PATH);
+	else if (dwType == REG_SZ)
+		wcscpy(CommandLine, Shell);
+	else
+		return FALSE;
 
-  StartupInfo.cb = sizeof(StartupInfo);
-  StartupInfo.lpReserved = NULL;
-  StartupInfo.lpDesktop = NULL;
-  StartupInfo.lpTitle = NULL;
-  StartupInfo.dwFlags = 0;
-  StartupInfo.cbReserved2 = 0;
-  StartupInfo.lpReserved2 = 0;
+	TRACE("Should run '%s' now\n", debugstr_w(CommandLine));
 
-  TRACE ("Creating new setup process\n");
+	/* Start process */
+	StartupInfo.cb = sizeof(StartupInfo);
+	StartupInfo.lpReserved = NULL;
+	StartupInfo.lpDesktop = NULL;
+	StartupInfo.lpTitle = NULL;
+	StartupInfo.dwFlags = 0;
+	StartupInfo.cbReserved2 = 0;
+	StartupInfo.lpReserved2 = 0;
+	Result = CreateProcessW(
+		NULL,
+		CommandLine,
+		NULL,
+		NULL,
+		FALSE,
+		DETACHED_PROCESS,
+		NULL,
+		NULL,
+		&StartupInfo,
+		&ProcessInformation);
+	if (!Result)
+	{
+		TRACE("Failed to run setup process\n");
+		return FALSE;
+	}
 
-  Result = CreateProcessW (NULL,
-			   CommandLine,
-			   NULL,
-			   NULL,
-			   FALSE,
-			   DETACHED_PROCESS,
-			   NULL,
-			   NULL,
-			   &StartupInfo,
-			   &ProcessInformation);
-  if (!Result)
-    {
-      TRACE ("Failed to run setup process\n");
-      return FALSE;
-    }
+	/* Wait for process termination */
+	WaitForSingleObject(ProcessInformation.hProcess, INFINITE);
 
-  /* Wait for process termination */
-  WaitForSingleObject (ProcessInformation.hProcess, INFINITE);
+	GetExitCodeProcess(ProcessInformation.hProcess, &dwExitCode);
 
-  GetExitCodeProcess (ProcessInformation.hProcess, &dwExitCode);
+	/* Close handles */
+	CloseHandle(ProcessInformation.hThread);
+	CloseHandle(ProcessInformation.hProcess);
 
-  CloseHandle (ProcessInformation.hThread);
-  CloseHandle (ProcessInformation.hProcess);
+	TRACE ("RunSetup() done\n");
 
-  TRACE ("RunSetup() done.\n");
-
-  return TRUE;
+	return TRUE;
 }
 
-
 BOOL
-RunSetup (VOID)
+RunSetup(VOID)
 {
 	HANDLE hThread;
 
