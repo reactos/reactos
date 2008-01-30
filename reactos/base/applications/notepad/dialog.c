8e346ad9794a9b7fe48c796e6d657e39d1122fd5
Christoph von Wittich <christoph_vw@reactos.org>
2008-01-30 16:02:49 +0000

    use FILE_SHARE_READ | FILE_SHARE_WRITE to open files which are in use by another process See issue #3011 for more details.
    svn path=/trunk/; revision=32056

--- reactos/base/applications/notepad/dialog.c
+++ reactos/base/applications/notepad/dialog.c
@@ -289,7 +289,7 @@ void DoOpenFile(LPCTSTR szFileName)
     if (!DoCloseFile())
         return;
 
-    hFile = CreateFile(szFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+    hFile = CreateFile(szFileName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
                        OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE)
     {
