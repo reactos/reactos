f87aaee1e65d178a3086e68dfcc9d1e6247deae9
Dmitry Chapyshev <dmitry@reactos.org>
2008-02-24 10:56:35 +0000

    - Change ListBox to ListView - Added list of code pages
    svn path=/trunk/; revision=32464

--- reactos/dll/cpl/intl/advanced.c
+++ reactos/dll/cpl/intl/advanced.c
@@ -1,9 +1,172 @@
 #include <windows.h>
 #include <commctrl.h>
+#include <setupapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
 #include <cpl.h>
 
 #include "intl.h"
 #include "resource.h"
+
+typedef struct CPStruct
+{
+    WORD    Status;
+    UINT    CPage;
+    HANDLE  hCPage;
+    TCHAR   Name[MAX_PATH];
+    struct  CPStruct *NextItem;
+} CPAGE, *LPCPAGE;
+
+static LPCPAGE PCPage = NULL;
+static HINF hIntlInf;
+
+static BOOL
+GetSupportedCP(VOID)
+{
+    UINT uiCPage, Count, Number;
+    INFCONTEXT infCont;
+    LPCPAGE lpCPage;
+    HANDLE hCPage;
+
+    Count = (UINT) SetupGetLineCount(hIntlInf, _T("CodePages"));
+    if (Count <= 0) return FALSE;
+
+    for (Number = 0; Number < Count; Number++)
+    {
+        if (SetupGetLineByIndex(hIntlInf, _T("CodePages"), Number, &infCont) &&
+            SetupGetIntField(&infCont, 0, (PINT)&uiCPage))
+        {
+            if (!(hCPage = GlobalAlloc(GHND, sizeof(CPAGE)))) return FALSE;
+
+            lpCPage            = GlobalLock(hCPage);
+            lpCPage->CPage     = uiCPage;
+            lpCPage->hCPage    = hCPage;
+            lpCPage->Status    = 0;
+            (lpCPage->Name)[0] = 0;
+
+            CPINFOEX cpInfEx;
+            if (GetCPInfoEx(uiCPage, 0, &cpInfEx))
+            {
+                _tcscpy(lpCPage->Name, cpInfEx.CodePageName);
+            }
+            else if (!SetupGetStringField(&infCont, 1, lpCPage->Name, MAX_PATH, NULL))
+            {
+                GlobalUnlock(hCPage);
+                GlobalFree(hCPage);
+                continue;
+            }
+
+            TCHAR Section[MAX_PATH];
+            _stprintf(Section, TEXT("%s%d"), _T("CODEPAGE_REMOVE_"), uiCPage);
+            if ((uiCPage == GetACP()) || (uiCPage == GetOEMCP()) || 
+                (!SetupFindFirstLine(hIntlInf, Section, _T("AddReg"), &infCont)))
+            {
+                lpCPage->Status |= (0x0001 | 0x0002);
+            }
+
+            lpCPage->NextItem = PCPage;
+            PCPage = lpCPage;
+        }
+    }
+
+    return TRUE;
+}
+
+static BOOL CALLBACK
+InstalledCPProc(LPTSTR lpStr)
+{
+    LPCPAGE lpCP;
+    UINT uiCP;
+
+    lpCP = PCPage;
+    uiCP = _ttol(lpStr);
+    for (;;)
+    {
+        if (!lpCP) break;
+        if (lpCP->CPage == uiCP)
+        {
+            lpCP->Status |= 0x0001;
+            break;
+        }
+        lpCP = lpCP->NextItem;
+    }
+
+    return TRUE;
+}
+
+static VOID
+InitLangList(HWND hwndDlg)
+{
+    LPCPAGE lpCPage;
+    INT ItemIndex;
+    HWND hList;
+
+    hList = GetDlgItem(hwndDlg, IDC_CONV_TABLES);
+
+    hIntlInf = SetupOpenInfFile(_T("intl.inf"), NULL, INF_STYLE_WIN4, NULL);
+
+    if (hIntlInf == INVALID_HANDLE_VALUE) return;
+
+    if (!SetupOpenAppendInfFile(NULL, hIntlInf, NULL))
+    {
+        SetupCloseInfFile(hIntlInf);
+        hIntlInf = NULL;
+        return;
+    }
+
+    if (!GetSupportedCP()) return;
+
+    SetupCloseInfFile(hIntlInf);
+
+    if (!EnumSystemCodePages(InstalledCPProc, CP_INSTALLED)) return;
+
+    LV_COLUMN column;
+    LV_ITEM item;
+    RECT ListRect;
+
+    ZeroMemory(&column, sizeof(LV_COLUMN));
+    column.mask = LVCF_FMT|LVCF_TEXT|LVCF_WIDTH;
+    column.fmt  = LVCFMT_LEFT;
+    GetClientRect(hList, &ListRect);
+    column.cx   = ListRect.right - GetSystemMetrics(SM_CYHSCROLL);
+    (VOID) ListView_InsertColumn(hList, 0, &column);
+
+    (VOID) ListView_SetExtendedListViewStyle(hList, LVS_EX_CHECKBOXES|LVS_EX_FULLROWSELECT);
+
+    lpCPage = PCPage;
+
+    for (;;)
+    {
+        if (!lpCPage) break;
+        ZeroMemory(&item, sizeof(LV_ITEM));
+        item.mask      = LVIF_TEXT|LVIF_PARAM|LVIF_STATE;
+        item.state     = 0;
+        item.stateMask = LVIS_STATEIMAGEMASK;
+        item.pszText   = lpCPage->Name;
+        item.lParam    = (LPARAM)lpCPage;
+
+        ItemIndex = ListView_InsertItem(hList, &item);
+
+        if (ItemIndex >= 0)
+        {
+            if (lpCPage->Status & 0x0001)
+            {
+                ListView_SetItemState(hList, ItemIndex,
+                                      INDEXTOSTATEIMAGEMASK(LVIS_SELECTED),
+                                      LVIS_STATEIMAGEMASK);
+            }
+            else
+            {
+                ListView_SetItemState(hList, ItemIndex,
+                                      INDEXTOSTATEIMAGEMASK(LVIS_FOCUSED),
+                                      LVIS_STATEIMAGEMASK);
+            }
+        }
+
+        lpCPage = lpCPage->NextItem;
+    }
+}
 
 /* Property page dialog callback */
 INT_PTR CALLBACK
@@ -15,7 +178,10 @@ AdvancedPageProc(HWND hwndDlg,
     switch(uMsg)
     {
         case WM_INITDIALOG:
-            break;
+        {
+            InitLangList(hwndDlg);
+        }
+        break;
     }
 
     return FALSE;
