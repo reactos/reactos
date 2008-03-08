bec091e65875d01f2e5e402199f35f253d0ae22f
Pierre Schweitzer <pierre@reactos.org>
2008-03-08 21:45:51 +0000

    - Updated NTFS header with the "new" FSF address - Removed useless field $id in ntfs.rc
    svn path=/trunk/; revision=32615

--- reactos/drivers/filesystems/ntfs/fastio.c
+++ reactos/drivers/filesystems/ntfs/fastio.c
@@ -1,6 +1,6 @@
 /*
  *  ReactOS kernel
- *  Copyright (C) 2002 ReactOS Team
+ *  Copyright (C) 2008 ReactOS Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * COPYRIGHT:        See COPYING in the top level directory
  * PROJECT:          ReactOS kernel
