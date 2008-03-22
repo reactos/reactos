b690ccfd9871ff0a8d170bc1e20758e61a167789
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-03-22 02:23:08 +0000

    update NTDDI defines
    svn path=/trunk/; revision=32742

--- reactos/include/psdk/sdkddkver.h
+++ reactos/include/psdk/sdkddkver.h
@@ -48,9 +48,28 @@
 #define NTDDI_WINXP                         0x05010000
 #define NTDDI_WINXPSP1                      0x05010100
 #define NTDDI_WINXPSP2                      0x05010200
+#define NTDDI_WINXPSP3                      0x05010300
+#define NTDDI_WINXPSP4                      0x05010400
 #define NTDDI_WS03                          0x05020000
 #define NTDDI_WS03SP1                       0x05020100
-#define NTDDI_LONGHORN                      0x06000000
+#define NTDDI_WS03SP2                       0x05020200
+#define NTDDI_WS03SP3                       0x05020300
+#define NTDDI_WS03SP4                       0x05020400
+#define NTDDI_WIN6                          0x06000000
+#define NTDDI_WIN6SP1                       0x06000100
+#define NTDDI_WIN6SP2                       0x06000200
+#define NTDDI_WIN6SP3                       0x06000300
+#define NTDDI_WIN6SP4                       0x06000400
+#define NTDDI_LONGHORN                      NTDDI_WIN6
+#define NTDDI_VISTA                         NTDDI_WIN6
+#define NTDDI_VISTASP1                      NTDDI_WIN6SP1
+#define NTDDI_VISTASP2                      NTDDI_WIN6SP2
+#define NTDDI_VISTASP3                      NTDDI_WIN6SP3
+#define NTDDI_VISTASP4                      NTDDI_WIN6SP4
+#define NTDDI_WS08                          NTDDI_WIN6SP1
+#define NTDDI_WS08SP2                       NTDDI_WIN6SP2
+#define NTDDI_WS08SP3                       NTDDI_WIN6SP3
+#define NTDDI_WS08SP4                       NTDDI_WIN6SP4
 
 /* Macros to get the NTDDI for a given WIN32 */
 #define NTDDI_VERSION_FROM_WIN32_WINNT2(ver)    ver##0000
