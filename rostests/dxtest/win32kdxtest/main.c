6727851943586c6289455155f725f72266649820
Magnus Olsen <magnus@greatlord.com>
2008-04-19 07:21:03 +0000

    add small help text and file dump from win32kdxtest
    svn path=/trunk/; revision=33032

--- rostests/dxtest/win32kdxtest/main.c
+++ rostests/dxtest/win32kdxtest/main.c
@@ -13,6 +13,7 @@
 #include "test.h"
 
 BOOL dumping_on =FALSE;
+FILE *fs_file;
 
 /* we using d3d8thk.dll it is doing the real syscall in windows 2000
  * in ReactOS and Windows XP and higher d3d8thk.dll it linking to
@@ -32,6 +33,30 @@ int main(int argc, char **argv)
         {
             dumping_on = TRUE;
         }
+
+        if ( (stricmp(argv[1],"-help")==0) ||
+             (stricmp(argv[1],"-?")==0) ||
+             (stricmp(argv[1],"/help")==0) ||
+             (stricmp(argv[1],"/?")==0) )
+        {
+            printf("the %s support follow param \n",argv[0]);
+            printf("-dump              : It dump all data it resvie to screen \n");
+            printf("-dumpfile filename : It dump all data it resvie to file \n");
+            printf("\nrember u can only use one of them at time \n");
+            exit(1);
+        }
+    }
+
+    if (argc == 3)
+    {
+        if (stricmp(argv[1],"-dumpfile")==0)
+        {
+            /* create or over write a file in binary mode, and redirect printf to the file */ 
+            if ( (fs_file = freopen(argv[2], "wb", stdout)) != NULL) 
+            {
+                dumping_on = TRUE;
+            }
+        }
     }
 
     hDirectDrawLocal = test_NtGdiDdCreateDirectDrawObject();
@@ -45,6 +70,11 @@ int main(int argc, char **argv)
     test_NtGdiDdCanCreateSurface(hDirectDrawLocal);
 
     test_NtGdiDdDeleteDirectDrawObject(hDirectDrawLocal);
+
+    if (fs_file != NULL)
+    {
+        fclose(fs_file);
+    }
     return 0;
 }
 
