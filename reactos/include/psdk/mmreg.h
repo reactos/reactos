ba45724f37673b1575347aa54ecc4a5e63bf49b7
Johannes Anderwald <johannes.anderwald@reactos.org>
2008-05-04 13:51:59 +0000

    - add mmreg.h
    svn path=/trunk/; revision=33268

---
+++ reactos/include/psdk/mmreg.h
@@ -0,0 +1,24 @@
+#ifndef _INC_MMREG
+#define _INC_MMREG 158
+
+
+#ifndef NOMMIDS
+
+/* define manufacturer ids */
+#ifndef MM_MICROSOFT
+#define MM_MICROSOFT                  1
+#endif
+
+
+/* define WDM types */
+#define  MM_MSFT_WDMAUDIO_WAVEOUT 0x64
+#define  MM_MSFT_WDMAUDIO_WAVEIN 0x65
+#define  MM_MSFT_WDMAUDIO_MIDIOUT 0x66
+#define  MM_MSFT_WDMAUDIO_MIDIIN 0x67
+#define  MM_MSFT_WDMAUDIO_MIXER 0x68
+#define  MM_MSFT_WDMAUDIO_AUX 0x69
+
+
+#endif
+
+#endif /* _INC_MMREG */
