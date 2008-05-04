8cfcdea5fab775b47b336416c286acf100af3c4b
Colin Finck <colin@reactos.org>
2008-05-04 09:04:14 +0000

    Move console.dll properly, Part 2/2. This commit will fix build again.
    svn path=/trunk/; revision=33262

---
+++ reactos/dll/cpl/console/resource.h
@@ -0,0 +1,96 @@
+#ifndef RESOURCE_H__
+#define RESOURCE_H__
+
+#define IDC_CPLICON				40
+#define IDS_CPLNAME				41
+#define IDS_CPLDESCRIPTION		42
+#define IDS_APPLY_SHORTCUT_ALL  43
+
+#define IDD_PROPPAGEOPTIONS		100
+#define IDD_PROPPAGEFONT		101
+#define IDD_PROPPAGELAYOUT		102
+#define IDD_PROPPAGECOLORS		103
+#define IDD_APPLYOPTIONS		104
+
+//options dialog
+#define IDC_CHECK_QUICK_EDIT			200
+#define IDC_CHECK_INSERT_MODE			201
+#define IDC_CHECK_DISCARD_DUPLICATES	202
+#define IDC_EDIT_BUFFER_SIZE			203
+#define IDC_UPDOWN_BUFFER_SIZE			204
+#define IDC_EDIT_NUM_BUFFER				205
+#define IDC_UPDOWN_NUM_BUFFER			206
+#define IDC_RADIO_SMALL_CURSOR			207
+#define IDC_RADIO_MEDIUM_CURSOR			208
+#define	IDC_RADIO_LARGE_CURSOR			209
+#define	IDC_RADIO_DISPLAY_WINDOW		210
+#define IDC_RADIO_DISPLAY_FULL			211
+
+// apply dialog
+#define IDC_RADIO_APPLY_ALL				300
+#define IDC_RADIO_APPLY_CURRENT			301
+
+//font dialog
+#define IDC_CHECK_BOLD_FONTS			400
+#define IDC_STATIC_SELECT_FONT_PREVIEW  401
+#define IDC_FONT_SIZE_X                 402
+#define IDC_FONT_SIZE_Y                 403
+#define IDC_LBOX_FONTS                  404
+#define IDC_STATIC_FONT_WINDOW_PREVIEW  405
+#define IDC_LBOX_TYPE                   406
+#define IDC_GROUPBOX_FONT_NAME          407
+
+//layout dialog
+
+#define IDC_UPDOWN_SCREEN_BUFFER_WIDTH   500
+#define IDC_EDIT_SCREEN_BUFFER_WIDTH     501
+#define IDC_EDIT_SCREEN_BUFFER_HEIGHT    502
+#define IDC_UPDOWN_SCREEN_BUFFER_HEIGHT  503
+#define IDC_EDIT_WINDOW_SIZE_WIDTH       504
+#define IDC_UPDOWN_WINDOW_SIZE_WIDTH     505
+#define IDC_EDIT_WINDOW_SIZE_HEIGHT      506
+#define IDC_UPDOWN_WINDOW_SIZE_HEIGHT    507
+#define IDC_EDIT_WINDOW_POS_LEFT         508
+#define IDC_UPDOWN_WINDOW_POS_LEFT       509
+#define IDC_EDIT_WINDOW_POS_TOP          510
+#define IDC_UPDOWN_WINDOW_POS_TOP        511
+#define IDC_CHECK_SYSTEM_POS_WINDOW      512
+#define IDC_STATIC_LAYOUT_WINDOW_PREVIEW 513
+
+//color dialog
+
+#define IDC_RADIO_SCREEN_TEXT           600
+#define IDC_RADIO_SCREEN_BACKGROUND     601
+#define IDC_RADIO_POPUP_TEXT            602
+#define IDC_RADIO_POPUP_BACKGROUND      603
+#define IDC_EDIT_COLOR_RED              604
+#define IDC_UPDOWN_COLOR_RED            605
+#define IDC_EDIT_COLOR_GREEN            606
+#define IDC_UPDOWN_COLOR_GREEN          607
+#define IDC_EDIT_COLOR_BLUE             608
+#define IDC_UPDOWN_COLOR_BLUE           609
+#define IDC_STATIC_SCREEN_COLOR         610
+#define IDC_STATIC_POPUP_COLOR          611
+#define IDC_STATIC_COLOR1               612
+#define IDC_STATIC_COLOR2               613
+#define IDC_STATIC_COLOR3               614
+#define IDC_STATIC_COLOR4               615
+#define IDC_STATIC_COLOR5               616
+#define IDC_STATIC_COLOR6               617
+#define IDC_STATIC_COLOR7               618
+#define IDC_STATIC_COLOR8               619
+#define IDC_STATIC_COLOR9               620
+#define IDC_STATIC_COLOR10              621
+#define IDC_STATIC_COLOR11              622
+#define IDC_STATIC_COLOR12              623
+#define IDC_STATIC_COLOR13              624
+#define IDC_STATIC_COLOR14              625
+#define IDC_STATIC_COLOR15              626
+#define IDC_STATIC_COLOR16              627
+
+
+//string ids
+#define IDS_SCREEN_TEXT					700
+#define IDS_RASTERFONTS					701
+
+#endif
