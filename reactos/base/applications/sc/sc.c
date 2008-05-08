433491c7f4b65a8dec3d15b29b17410b50088eab
Hervé Poussineau <hpoussin@reactos.org>
2008-05-08 16:48:39 +0000

    Fix sc.exe compilation by removing hack
    svn path=/trunk/; revision=33372

--- reactos/base/applications/sc/sc.c
+++ reactos/base/applications/sc/sc.c
@@ -189,10 +189,6 @@ ScControl(LPCTSTR Server,       // remote machine name
     return 0;
 }
 
-#if defined(_UNICODE) && defined(__GNUC__)
-static
-#endif
-
 int _tmain(int argc, LPCTSTR argv[])
 {
     LPCTSTR Server = NULL;   // remote machine
@@ -236,42 +232,3 @@ int _tmain(int argc, LPCTSTR argv[])
                          argc-2);
     }
 }
-
-
-#if defined(_UNICODE) && defined(__GNUC__)
-/* HACK - MINGW HAS NO OFFICIAL SUPPORT FOR wmain()!!! */
-int main( int argc, char **argv )
-{
-    WCHAR **argvW;
-    int i, j, Ret = 1;
-
-    if ((argvW = (WCHAR**) malloc(argc * sizeof(WCHAR*))))
-    {
-        /* convert the arguments */
-        for (i = 0, j = 0; i < argc; i++)
-        {
-            if (!(argvW[i] = (WCHAR*) malloc((strlen(argv[i]) + 1) * sizeof(WCHAR))))
-            {
-                j++;
-            }
-            swprintf(argvW[i], L"%hs", argv[i]);
-        }
-
-        if (j == 0)
-        {
-            /* no error converting the parameters, call wmain() */
-            Ret = wmain(argc, (LPCTSTR *)argvW);
-        }
-
-        /* free the arguments */
-        for (i = 0; i < argc; i++)
-        {
-            if (argvW[i])
-                free(argvW[i]);
-        }
-        free(argvW);
-    }
-
-    return Ret;
-}
-#endif
