722735c75493e2ddcf6fd03519cb7895fe295ea4
Colin Finck <colin@reactos.org>
2008-05-18 10:56:31 +0000

    Check if the GetWindowsDirectory call succeeded and use PathAppend to prevent a buffer overflow, when WinDir + "\regedit.exe" > MAX_PATH
    svn path=/trunk/; revision=33571

--- reactos/base/applications/regedt32/regedt32.c
+++ reactos/base/applications/regedt32/regedt32.c
@@ -1,16 +1,18 @@
 #include <windows.h>
 #include <tchar.h>
 #include <shellapi.h>
+#include <shlwapi.h>
 
 int WINAPI _tWinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst,
                      LPTSTR lpsCmdLine, int nCmdShow)
 {
     TCHAR szPath[MAX_PATH];
 
-    GetWindowsDirectory(szPath, MAX_PATH);
-    _tcscat(szPath, _T("\\regedit.exe"));
-
-    ShellExecute(NULL, NULL, szPath, lpsCmdLine, NULL, nCmdShow);
+    if(GetWindowsDirectory(szPath, MAX_PATH))
+    {
+        PathAppend(szPath, _T("regedit.exe"));
+        ShellExecute(NULL, NULL, szPath, lpsCmdLine, NULL, nCmdShow);
+    }
 
     return 0;
 }
