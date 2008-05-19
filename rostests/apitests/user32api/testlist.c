e657a0d73aa19fc4399b88a1dcaef1a2f9adc49b
Gregor Brunmar <gbrunmar.ros@gmail.com>
2008-05-19 17:50:39 +0000

    ReactOS leads you to mysterious API calls. Added tests for RealGetWindowsClass() with some strange behaviours for later use when implementing RealGetWindowClass().
    svn path=/trunk/; revision=33599

--- rostests/apitests/user32api/testlist.c
+++ rostests/apitests/user32api/testlist.c
@@ -1,23 +1,27 @@
 #ifndef _USER32TESTLIST_H
 #define _USER32TESTLIST_H
 
+#define ARRAY_SIZE(x)   (sizeof(x)/sizeof(x[0]))
+
 #include "user32api.h"
 
 /* include the tests */
 #include "tests/ScrollDC.c"
 #include "tests/ScrollWindowEx.c"
+#include "tests/RealGetWindowClass.c"
 
 /* The List of tests */
 TESTENTRY TestList[] =
 {
-	{ L"ScrollDC", Test_ScrollDC },
-	{ L"ScrollWindowEx", Test_ScrollWindowEx }
+    { L"ScrollDC", Test_ScrollDC },
+    { L"ScrollWindowEx", Test_ScrollWindowEx },
+    { L"RealGetWindowClass", Test_RealGetWindowClass },
 };
 
 /* The function that gives us the number of tests */
 INT NumTests(void)
 {
-	return sizeof(TestList) / sizeof(TESTENTRY);
+    return ARRAY_SIZE(TestList);
 }
 
 #endif /* _USER32TESTLIST_H */
