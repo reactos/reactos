457aee023d9c3ebcde0a60736fd17437b40d98c5
Magnus Olsen <magnus@greatlord.com>
2008-06-01 15:48:51 +0000

    add all test we can do for EngCreateSemaphore, it checking some info in the internal HANDLE hsem is same as PRTL_CRITICAL_SECTION struct
    svn path=/trunk/; revision=33811

--- rostests/apitests/gdi32api/tests/EngCreateSemaphore.c
+++ rostests/apitests/gdi32api/tests/EngCreateSemaphore.c
@@ -18,7 +18,28 @@ Test_EngCreateSemaphore(PTESTINFO pti)
     RTEST (lpcrit->LockSemaphore == 0);
     RTEST (lpcrit->SpinCount == 0);
 
+    ASSERT(lpcrit->DebugInfo != NULL);
+    RTEST (lpcrit->DebugInfo->Type == 0);
+    RTEST (lpcrit->DebugInfo->CreatorBackTraceIndex == 0);
+    RTEST (lpcrit->DebugInfo->EntryCount == 0);
+    RTEST (lpcrit->DebugInfo->ContentionCount == 0);
+
     EngDeleteSemaphore(hsem);
+
+    RTEST (lpcrit->DebugInfo != NULL);
+    RTEST (lpcrit->LockCount > 0);
+    RTEST (lpcrit->RecursionCount == 0);
+    RTEST (lpcrit->OwningThread == 0);
+    RTEST (lpcrit->LockSemaphore == 0);
+    RTEST (lpcrit->SpinCount == 0);
+
+    ASSERT(lpcrit->DebugInfo != NULL);
+    // my (magnus olsen) value I getting back in vista RTEST (lpcrit->DebugInfo->Type == 0xA478);
+    RTEST (lpcrit->DebugInfo->Type != 0);
+    RTEST (lpcrit->DebugInfo->CreatorBackTraceIndex != 0);
+    RTEST (lpcrit->DebugInfo->EntryCount != 0);
+    // my (magnus olsen) value I getting back RTEST in vista (lpcrit->DebugInfo->ContentionCount == 0x20000);
+    RTEST (lpcrit->DebugInfo->ContentionCount != 0);
 
     return APISTATUS_NORMAL;
 }
