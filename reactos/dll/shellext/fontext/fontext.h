4f6a32920d4c15f00ef0701c49dd5585f46860fb
Dmitry Chapyshev <dmitry@reactos.org>
2008-06-27 17:34:08 +0000

    - Add fontext.dll
    svn path=/trunk/; revision=34135

---
+++ reactos/dll/shellext/fontext/fontext.h
@@ -0,0 +1,12 @@
+#ifndef __FONTEXT__H
+#define __FONTEXT__H
+
+#define COBJMACROS
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <shlobj.h>
+
+#include <debug.h>
+
+#endif /* __FONTEXT__H */
---
+++ reactos/dll/shellext/fontext/resource.h
@@ -0,0 +1,6 @@
+#ifndef __RESOURCE__H
+#define __RESOURCE__H
+
+#define IDS_REACTOS_FONTS_FOLDER 151
+
+#endif /* __RESOURCE__H */
