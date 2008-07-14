26fa849ea031b81651b532a99e5ede155c55ba71
ReactOS Portable Systems Group <ros-arm-bringup@svn.reactos.org>
2008-07-14 03:34:07 +0000

    - Re-organize our unimplemented function stubs according to port targets (user mode support, exceptions/SEH, driver ISRs, x86-only functionality and arch-specific Ke).
    svn path=/trunk/; revision=34494

--- reactos/ntoskrnl/ke/arm/stubs_asm.s
+++ reactos/ntoskrnl/ke/arm/stubs_asm.s
@@ -8,51 +8,53 @@ GENERATE_ARM_STUB _except_handler2
 GENERATE_ARM_STUB _except_handler3
 GENERATE_ARM_STUB _global_unwind2  
 GENERATE_ARM_STUB _local_unwind2  
-
-//
-// Exported Ke Arch-Specific APIs
-//
-GENERATE_ARM_STUB DbgBreakPointWithStatus 
-GENERATE_ARM_STUB KeConnectInterrupt 
-GENERATE_ARM_STUB KeDisconnectInterrupt 
-GENERATE_ARM_STUB KeFlushEntireTb 
-GENERATE_ARM_STUB KeGetRecommendedSharedDataAlignment 
-GENERATE_ARM_STUB KeIcacheFlushCount 
-GENERATE_ARM_STUB KeInitializeInterrupt 
-GENERATE_ARM_STUB KeInvalidateAllCaches
-GENERATE_ARM_STUB KeQueryActiveProcessors 
-GENERATE_ARM_STUB KeRaiseUserException 
-GENERATE_ARM_STUB KeSaveStateForHibernate 
-GENERATE_ARM_STUB KeSetDmaIoCoherency 
-GENERATE_ARM_STUB KeSynchronizeExecution 
-GENERATE_ARM_STUB KeUserModeCallback 
-GENERATE_ARM_STUB NtSetLdtEntries
-GENERATE_ARM_STUB NtRaiseException
-GENERATE_ARM_STUB NtCallbackReturn
-GENERATE_ARM_STUB NtContinue
-GENERATE_ARM_STUB NtVdmControl 
-GENERATE_ARM_STUB RtlCreateUserThread
-GENERATE_ARM_STUB RtlInitializeContext
-
-//
-// Internal Ke Arch-Specific Helpers
-//
-GENERATE_ARM_STUB KiInitializeUserApc
-GENERATE_ARM_STUB KeDisableInterrupts
-GENERATE_ARM_STUB KiSwapProcess
-GENERATE_ARM_STUB KeSwitchKernelStack
-
-//
-// Traps, Debugging and Execeptions
-//
-GENERATE_ARM_STUB KiPassiveRelease 
-GENERATE_ARM_STUB KiInterruptTemplate 
-GENERATE_ARM_STUB KiUnexpectedInterrupt  
 GENERATE_ARM_STUB RtlGetCallersAddress 
 GENERATE_ARM_STUB RtlUnwind 
 GENERATE_ARM_STUB RtlpGetExceptionAddress
 GENERATE_ARM_STUB RtlDispatchException
 GENERATE_ARM_STUB RtlpGetStackLimits
-
+GENERATE_ARM_STUB DbgBreakPointWithStatus 
+GENERATE_ARM_STUB KeRaiseUserException 
 GENERATE_ARM_STUB KdpGdbStubInit
-GENERATE_ARM_STUB MmDbgTranslatePhysicalAddress
+GENERATE_ARM_STUB NtRaiseException
+
+//
+// Driver ISRs
+//
+GENERATE_ARM_STUB KeConnectInterrupt 
+GENERATE_ARM_STUB KeDisconnectInterrupt 
+GENERATE_ARM_STUB KiPassiveRelease 
+GENERATE_ARM_STUB KiInterruptTemplate 
+GENERATE_ARM_STUB KiUnexpectedInterrupt  
+GENERATE_ARM_STUB KeInitializeInterrupt 
+GENERATE_ARM_STUB KeSynchronizeExecution 
+
+//
+// User Mode Support
+//
+GENERATE_ARM_STUB KiInitializeUserApc
+GENERATE_ARM_STUB KiSwapProcess
+GENERATE_ARM_STUB KeSwitchKernelStack
+GENERATE_ARM_STUB RtlCreateUserThread
+GENERATE_ARM_STUB RtlInitializeContext
+GENERATE_ARM_STUB KeUserModeCallback 
+GENERATE_ARM_STUB NtCallbackReturn
+GENERATE_ARM_STUB NtContinue
+
+//
+// Non-ARM Functionality
+//
+GENERATE_ARM_STUB NtSetLdtEntries
+GENERATE_ARM_STUB NtVdmControl 
+
+//
+// Ke Arch-Specific Helpers
+//
+GENERATE_ARM_STUB KeDisableInterrupts
+GENERATE_ARM_STUB KeFlushEntireTb 
+GENERATE_ARM_STUB KeGetRecommendedSharedDataAlignment 
+GENERATE_ARM_STUB KeIcacheFlushCount 
+GENERATE_ARM_STUB KeInvalidateAllCaches
+GENERATE_ARM_STUB KeQueryActiveProcessors 
+GENERATE_ARM_STUB KeSaveStateForHibernate 
+GENERATE_ARM_STUB KeSetDmaIoCoherency 
