9b33d2d77b20a43401496b8e4461fe6ca90e9a8d
Aleksey Bragin <aleksey@reactos.org>
2008-07-21 21:37:38 +0000

    - Rewrite the header file to achieve better compatibility. - Add project/solution for compiling with MSVC (it actually compiles and links), the project file is a tweaked rbuild-generated one.
    svn path=/branches/nwin32/; revision=34648

--- subsystems/win32/win32k/include/win32k.h
+++ subsystems/win32/win32k/include/win32k.h
@@ -3,7 +3,7 @@
  * PROJECT:         ReactOS Win32K
  * FILE:            subsystems/win32/win32k/include/win32k.h
  * PURPOSE:         Main Win32K Header
- * PROGRAMMER:      Stefan Ginsberg (stefan__100__@hotmail.com)
+ * PROGRAMMER:      Aleksey Bragin (aleksey@reactos.org)
  */
 
 /* INCLUDES ******************************************************************/
@@ -15,14 +15,14 @@
 #define NTDDI_VERSION NTDDI_WS03SP1
 #define WINVER 0x600
 
-#define _NO_COM
-
-/* DDK/NDK/SDK Headers */
-#include <ddk/ntddk.h>
-#include <ddk/ntddmou.h>
-#include <ddk/ntifs.h>
-#include <ddk/tvout.h>
-#include <ndk/ntndk.h>
+/* Initial DDK/IFS Headers */
+#ifdef _MSC_VER
+#include <excpt.h>
+#include <ntdef.h>
+#undef DECLSPEC_IMPORT
+#define DECLSPEC_IMPORT
+#endif
+#include <ntifs.h>
 
 /* Win32 Headers */
 /* FIXME: Defines in winbase.h that we need... */
@@ -40,8 +40,18 @@ typedef struct _SECURITY_ATTRIBUTES SECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
 #include <prntfont.h>
 #include <dde.h>
 #include <wincon.h>
+
+/* This set of headers is greatly incompatible */
+/* TODO: Either fix ddrawi.h + all dependencies, or create a new temporary
+         header */
 #define _NOCSECT_TYPE
-#include <ddrawi.h>
+// #include <ddrawi.h>
+typedef LPVOID LPVIDMEM;
+typedef LPVOID LPVMEMHEAP;
+typedef LPVOID LPSURFACEALIGNMENT;
+
+/* NDK Headers */
+#include <ntndk.h>
 
 /* SEH Support with PSEH */
 #include <pseh/pseh.h>
@@ -59,54 +69,9 @@ typedef struct _SECURITY_ATTRIBUTES SECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
 #include <win32k/callback.h>
 #include <win32k/ntusrtyp.h>
 #include <win32k/ntuser.h>
-#include <win32k/ntgdityp.h>
-#include <ntgdi.h>
 
-
-
+/* FIXME */
 #if 0
-/* DDK/NDK/SDK Headers */
-#include <ddk/ntddk.h>
-#include <ddk/ntddmou.h>
-#include <ddk/ntifs.h>
-#include <ddk/tvout.h>
-#include <ndk/ntndk.h>
-
-/* Win32 Headers */
-/* FIXME: Defines in winbase.h that we need... */
-typedef struct _SECURITY_ATTRIBUTES SECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
-#define WINBASEAPI
-#define STARTF_USESIZE 2
-#define STARTF_USEPOSITION 4
-#include <stdarg.h>
-#include <windef.h>
-#include <winerror.h>
-#include <wingdi.h>
-#include <winddi.h>
-#include <winuser.h>
-#include <prntfont.h>
-#include <dde.h>
-#include <wincon.h>
-#define _NOCSECT_TYPE
-#include <ddrawi.h>
-
-/* SEH Support with PSEH */
-#include <pseh/pseh.h>
-
-/* CSRSS Header */
-#include <csrss/csrss.h>
-
-/* Helper Header */
-#include <reactos/helper.h>
-
-/* Probe and capture */
-#include <reactos/probe.h>
-
-/* Public Win32K Headers */
-#include <win32k/callback.h>
-#include <win32k/ntusrtyp.h>
-#include <win32k/ntuser.h>
 #include <win32k/ntgdityp.h>
-#include <win32k/ntgdibad.h>
 #include <ntgdi.h>
 #endif
