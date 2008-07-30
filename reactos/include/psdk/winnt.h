9b55463daa38161129a9c4d9e13aa140fe2fd1e8
Hervé Poussineau <hpoussin@reactos.org>
2008-07-30 07:29:55 +0000

    unix define doesn't mean we're on a x86 compatible machine
    svn path=/trunk/; revision=34945

--- reactos/include/psdk/winnt.h
+++ reactos/include/psdk/winnt.h
@@ -2011,7 +2011,7 @@ typedef struct _ACL_SIZE_INFORMATION {
 } ACL_SIZE_INFORMATION;
 
 /* FIXME: add more machines */
-#if defined(_X86_) || defined(unix) && !defined(__PowerPC__)
+#if defined(_X86_) && !defined(__PowerPC__)
 #define SIZE_OF_80387_REGISTERS	80
 #define CONTEXT_i386	0x10000
 #define CONTEXT_i486	0x10000
