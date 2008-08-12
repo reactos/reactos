8f5e892fbfa324a2b71ff2e8d0a9edd5e84b08a4
Cameron Gutman <aicommander@gmail.com>
2008-08-12 18:34:07 +0000

    - Free the MDL on failure  - Use _SEH_YIELD
    svn path=/branches/aicom-network-fixes/; revision=35291

--- drivers/network/afd/afd/tdi.c
+++ drivers/network/afd/afd/tdi.c
@@ -864,16 +864,11 @@ NTSTATUS TdiSend
         MmProbeAndLockPages(Mdl, KernelMode, IoModifyAccess);
     } _SEH_HANDLE {
         AFD_DbgPrint(MIN_TRACE, ("MmProbeAndLockPages() failed.\n"));
+	IoFreeMdl(Mdl);
         IoFreeIrp(*Irp);
         *Irp = NULL;
-        Status = STATUS_INSUFFICIENT_RESOURCES;
+        _SEH_YIELD(return STATUS_INSUFFICIENT_RESOURCES);
     } _SEH_END;
-
-    if( !NT_SUCCESS(Status) ) {
-	IoFreeIrp(*Irp);
-	*Irp = NULL;
-	return Status;
-    }
 
     AFD_DbgPrint(MID_TRACE,("AFD>>> Got an MDL: %x\n", Mdl));
 
@@ -952,16 +947,11 @@ NTSTATUS TdiReceive(
         AFD_DbgPrint(MIN_TRACE, ("probe and lock done\n"));
     } _SEH_HANDLE {
         AFD_DbgPrint(MIN_TRACE, ("MmProbeAndLockPages() failed.\n"));
+	IoFreeMdl(Mdl);
         IoFreeIrp(*Irp);
 	*Irp = NULL;
-	Status = STATUS_INSUFFICIENT_RESOURCES;
+	_SEH_YIELD(return STATUS_INSUFFICIENT_RESOURCES);
     } _SEH_END;
-
-    if( !NT_SUCCESS(Status) ) {
-	IoFreeIrp(*Irp);
-	*Irp = NULL;
-	return Status;
-    }
 
     AFD_DbgPrint(MID_TRACE,("AFD>>> Got an MDL: %x\n", Mdl));
 
@@ -1055,6 +1045,7 @@ NTSTATUS TdiReceiveDatagram(
         MmProbeAndLockPages(Mdl, KernelMode, IoModifyAccess);
     } _SEH_HANDLE {
         AFD_DbgPrint(MIN_TRACE, ("MmProbeAndLockPages() failed.\n"));
+	IoFreeMdl(Mdl);
         IoFreeIrp(*Irp);
         *Irp = NULL;
         _SEH_YIELD(return STATUS_INSUFFICIENT_RESOURCES);
@@ -1153,6 +1144,7 @@ NTSTATUS TdiSendDatagram(
         MmProbeAndLockPages(Mdl, KernelMode, IoModifyAccess);
     } _SEH_HANDLE {
         AFD_DbgPrint(MIN_TRACE, ("MmProbeAndLockPages() failed.\n"));
+	IoFreeMdl(Mdl);
         IoFreeIrp(*Irp);
         *Irp = NULL;
         _SEH_YIELD(return STATUS_INSUFFICIENT_RESOURCES);
