001f91468e6a54fec7b1c8ef3e159ff1cf90f7c5
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-08-25 00:12:33 +0000

    - Fix comments and comment out stmxcsr, as it crashes for some reason.
    svn path=/branches/ros-amd64-bringup/; revision=35619

--- reactos/ntoskrnl/ke/amd64/cpu.c
+++ reactos/ntoskrnl/ke/amd64/cpu.c
@@ -495,14 +495,14 @@ VOID
 NTAPI
 KiRestoreProcessorControlState(PKPROCESSOR_STATE ProcessorState)
 {
-    /* Save the CR registers */
+    /* Restore the CR registers */
     __writecr0(ProcessorState->SpecialRegisters.Cr0);
 //    __writecr2(ProcessorState->SpecialRegisters.Cr2);
     __writecr3(ProcessorState->SpecialRegisters.Cr3);
     __writecr4(ProcessorState->SpecialRegisters.Cr4);
     __writecr8(ProcessorState->SpecialRegisters.Cr8);
 
-    /* Save the DR registers */
+    /* Restore the DR registers */
     __writedr(0, ProcessorState->SpecialRegisters.KernelDr0);
     __writedr(1, ProcessorState->SpecialRegisters.KernelDr1);
     __writedr(2, ProcessorState->SpecialRegisters.KernelDr2);
@@ -510,7 +510,7 @@ KiRestoreProcessorControlState(PKPROCESSOR_STATE ProcessorState)
     __writedr(6, ProcessorState->SpecialRegisters.KernelDr6);
     __writedr(7, ProcessorState->SpecialRegisters.KernelDr7);
 
-    /* Save GDT, IDT, LDT and TSS */
+    /* Restore GDT, IDT, LDT and TSS */
     __lgdt(&ProcessorState->SpecialRegisters.Gdtr.Limit);
     __lldt(&ProcessorState->SpecialRegisters.Ldtr);
     __ltr(&ProcessorState->SpecialRegisters.Tr);
@@ -522,6 +522,8 @@ KiRestoreProcessorControlState(PKPROCESSOR_STATE ProcessorState)
 //    ProcessorState->SpecialRegisters.LastBranchFromRip
 //    ProcessorState->SpecialRegisters.LastExceptionToRip
 //    ProcessorState->SpecialRegisters.LastExceptionFromRip
+
+    /* Restore MSRs */
     __writemsr(X86_MSR_GSBASE, ProcessorState->SpecialRegisters.MsrGsBase);
     __writemsr(X86_MSR_KERNEL_GSBASE, ProcessorState->SpecialRegisters.MsrGsSwap);
     __writemsr(X86_MSR_STAR, ProcessorState->SpecialRegisters.MsrStar);
@@ -556,12 +558,14 @@ KiSaveProcessorControlState(OUT PKPROCESSOR_STATE ProcessorState)
     __str(&ProcessorState->SpecialRegisters.Tr);
     __sidt(&ProcessorState->SpecialRegisters.Idtr.Limit);
 
-    __stmxcsr(&ProcessorState->SpecialRegisters.MxCsr);
+//    __stmxcsr(&ProcessorState->SpecialRegisters.MxCsr);
 //    ProcessorState->SpecialRegisters.DebugControl = 
 //    ProcessorState->SpecialRegisters.LastBranchToRip = 
 //    ProcessorState->SpecialRegisters.LastBranchFromRip = 
 //    ProcessorState->SpecialRegisters.LastExceptionToRip = 
 //    ProcessorState->SpecialRegisters.LastExceptionFromRip = 
+
+    /* Save MSRs */
     ProcessorState->SpecialRegisters.MsrGsBase = __readmsr(X86_MSR_GSBASE);
     ProcessorState->SpecialRegisters.MsrGsSwap = __readmsr(X86_MSR_KERNEL_GSBASE);
     ProcessorState->SpecialRegisters.MsrStar = __readmsr(X86_MSR_STAR);
