064cf076e995251406998d8c7fba44de94e345ee
Stefan Ginsberg <stefanginsberg@gmail.com>
2008-09-08 11:52:33 +0000

    - NtQueryInformationFile and NtSetInformationFile: Don't clear the IRP_SYNCHRONOUS_API flag if it has been set previously! This should obsolete the hack in irp.c!IopCompleteRequest. - If you still see "Not an executive event -- should not be dereferenced" after this patch then please report to me.
    svn path=/trunk/; revision=36047

--- reactos/ntoskrnl/io/iomgr/iofunc.c
+++ reactos/ntoskrnl/io/iomgr/iofunc.c
@@ -1809,10 +1809,10 @@ NtQueryInformationFile(IN HANDLE FileHandle,
     if (!NT_SUCCESS(Status)) return Status;
 
     /* Set the flags */
-    Irp->Flags = (IRP_BUFFERED_IO |
-                  IRP_DEALLOCATE_BUFFER |
-                  IRP_INPUT_OPERATION |
-                  IRP_DEFER_IO_COMPLETION);
+    Irp->Flags |= (IRP_BUFFERED_IO |
+                   IRP_DEALLOCATE_BUFFER |
+                   IRP_INPUT_OPERATION |
+                   IRP_DEFER_IO_COMPLETION);
 
     /* Set the Parameters */
     StackPtr->Parameters.QueryFile.FileInformationClass = FileInformationClass;
@@ -2405,9 +2405,9 @@ NtSetInformationFile(IN HANDLE FileHandle,
     if (!NT_SUCCESS(Status)) return Status;
 
     /* Set the flags */
-    Irp->Flags = (IRP_BUFFERED_IO |
-                  IRP_DEALLOCATE_BUFFER |
-                  IRP_DEFER_IO_COMPLETION);
+    Irp->Flags |= (IRP_BUFFERED_IO |
+                   IRP_DEALLOCATE_BUFFER |
+                   IRP_DEFER_IO_COMPLETION);
 
     /* Set the Parameters */
     StackPtr->Parameters.SetFile.FileInformationClass = FileInformationClass;
