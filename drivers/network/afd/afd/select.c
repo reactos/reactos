48c47e07e66063341444667c6708e6ae7c792b87
Cameron Gutman <aicommander@gmail.com>
2008-09-10 07:43:05 +0000

    - Return the correct status
    svn path=/branches/aicom-network-fixes/; revision=36107

--- drivers/network/afd/afd/select.c
+++ drivers/network/afd/afd/select.c
@@ -317,7 +317,7 @@ AfdEventSelect( PDEVICE_OBJECT DeviceObject, PIRP Irp,
 
     AFD_DbgPrint(MID_TRACE,("Returning %x\n", Status));
 
-    return UnlockAndMaybeComplete( FCB, STATUS_SUCCESS, Irp,
+    return UnlockAndMaybeComplete( FCB, Status, Irp,
 				   0, NULL );
 }
 
