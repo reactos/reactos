d9d2e414c765d48f36f63534a34bf6c078fe8ed3
Christoph von Wittich <christoph_vw@reactos.org>
2008-09-14 10:05:35 +0000

    update headers for shdocvw
    svn path=/trunk/; revision=36226

--- reactos/include/psdk/intshcut.h
+++ reactos/include/psdk/intshcut.h
@@ -1,82 +1,114 @@
-#ifndef _INTSHCUT_H
-#define _INTSHCUT_H
-#if __GNUC__ >=3
-#pragma GCC system_header
-#endif
+/*
+ * Copyright (C) 2007 Francois Gouget
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __WINE_INTSHCUT_H
+#define __WINE_INTSHCUT_H
 
 #include <isguids.h>
+
+#define INTSHCUTAPI
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define INTSHCUTAPI DECLSPEC_IMPORT
+
 #define E_FLAGS MAKE_SCODE(SEVERITY_ERROR,FACILITY_ITF,0x1000)
 #define IS_E_EXEC_FAILED MAKE_SCODE(SEVERITY_ERROR,FACILITY_ITF,0x2002)
 #define URL_E_INVALID_SYNTAX MAKE_SCODE(SEVERITY_ERROR,FACILITY_ITF,0x1001)
 #define URL_E_UNREGISTERED_PROTOCOL MAKE_SCODE(SEVERITY_ERROR,FACILITY_ITF,0x1002)
+
 typedef enum iurl_seturl_flags {
- IURL_SETURL_FL_GUESS_PROTOCOL=1,
- IURL_SETURL_FL_USE_DEFAULT_PROTOCOL,
- ALL_IURL_SETURL_FLAGS=(IURL_SETURL_FL_GUESS_PROTOCOL|IURL_SETURL_FL_USE_DEFAULT_PROTOCOL)
+    IURL_SETURL_FL_GUESS_PROTOCOL=1,
+    IURL_SETURL_FL_USE_DEFAULT_PROTOCOL,
+    ALL_IURL_SETURL_FLAGS=(IURL_SETURL_FL_GUESS_PROTOCOL|IURL_SETURL_FL_USE_DEFAULT_PROTOCOL)
 } IURL_SETURL_FLAGS;
+
 typedef enum iurl_invokecommand_flags {
- IURL_INVOKECOMMAND_FL_ALLOW_UI=1,
- IURL_INVOKECOMMAND_FL_USE_DEFAULT_VERB,
- ALL_IURL_INVOKECOMMAND_FLAGS=(IURL_INVOKECOMMAND_FL_ALLOW_UI|IURL_INVOKECOMMAND_FL_USE_DEFAULT_VERB)
+    IURL_INVOKECOMMAND_FL_ALLOW_UI=1,
+    IURL_INVOKECOMMAND_FL_USE_DEFAULT_VERB,
+    ALL_IURL_INVOKECOMMAND_FLAGS=(IURL_INVOKECOMMAND_FL_ALLOW_UI|IURL_INVOKECOMMAND_FL_USE_DEFAULT_VERB)
 } IURL_INVOKECOMMAND_FLAGS;
-typedef enum translateurl_in_flags {
- TRANSLATEURL_FL_GUESS_PROTOCOL=1,
- TRANSLATEURL_FL_USE_DEFAULT_PROTOCOL,
- ALL_TRANSLATEURL_FLAGS=(TRANSLATEURL_FL_GUESS_PROTOCOL|TRANSLATEURL_FL_USE_DEFAULT_PROTOCOL)
-} TRANSLATEURL_IN_FLAGS;
-typedef enum urlassociationdialog_in_flags {
- URLASSOCDLG_FL_USE_DEFAULT_NAME=1,
- URLASSOCDLG_FL_REGISTER_ASSOC,
- ALL_URLASSOCDLG_FLAGS=(URLASSOCDLG_FL_USE_DEFAULT_NAME|URLASSOCDLG_FL_REGISTER_ASSOC)
-} URLASSOCIATIONDIALOG_IN_FLAGS;
-typedef enum mimeassociationdialog_in_flags {
- MIMEASSOCDLG_FL_REGISTER_ASSOC=1,
- ALL_MIMEASSOCDLG_FLAGS=MIMEASSOCDLG_FL_REGISTER_ASSOC
-} MIMEASSOCIATIONDIALOG_IN_FLAGS;
-typedef struct urlinvokecommandinfo {
- DWORD dwcbSize;
- DWORD dwFlags;
- HWND hwndParent;
- PCSTR pcszVerb;
-} URLINVOKECOMMANDINFO,*PURLINVOKECOMMANDINFO;
-typedef const URLINVOKECOMMANDINFO CURLINVOKECOMMANDINFO;
-typedef const URLINVOKECOMMANDINFO *PCURLINVOKECOMMANDINFO;
-#define INTERFACE IUniformResourceLocator
-DECLARE_INTERFACE_(IUniformResourceLocator,IUnknown)
+
+typedef struct urlinvokecommandinfoA {
+    DWORD dwcbSize;
+    DWORD dwFlags;
+    HWND hwndParent;
+    LPCSTR pcszVerb;
+} URLINVOKECOMMANDINFOA, *PURLINVOKECOMMANDINFOA;
+typedef const URLINVOKECOMMANDINFOA CURLINVOKECOMMANDINFOA;
+typedef const URLINVOKECOMMANDINFOA *PCURLINVOKECOMMANDINFOA;
+
+typedef struct urlinvokecommandinfoW {
+    DWORD dwcbSize;
+    DWORD dwFlags;
+    HWND hwndParent;
+    LPCWSTR pcszVerb;
+} URLINVOKECOMMANDINFOW, *PURLINVOKECOMMANDINFOW;
+typedef const URLINVOKECOMMANDINFOW CURLINVOKECOMMANDINFOW;
+typedef const URLINVOKECOMMANDINFOW *PCURLINVOKECOMMANDINFOW;
+
+#define INTERFACE IUniformResourceLocatorA
+DECLARE_INTERFACE_(IUniformResourceLocatorA,IUnknown)
 {
- STDMETHOD(QueryInterface)(THIS_ REFIID,PVOID*) PURE;
- STDMETHOD_(ULONG,AddRef)(THIS) PURE;
- STDMETHOD_(ULONG,Release)(THIS) PURE;
- STDMETHOD(SetURL)(THIS_ PCSTR,DWORD) PURE;
- STDMETHOD(GetURL)(THIS_ PSTR*) PURE;
- STDMETHOD(InvokeCommand)(THIS_ PURLINVOKECOMMANDINFO) PURE;
+    /*** IUnknown methods ***/
+    STDMETHOD_(HRESULT,QueryInterface)(THIS_ REFIID riid, VOID **ppvObject) PURE;
+    STDMETHOD_(ULONG,AddRef)(THIS) PURE;
+    STDMETHOD_(ULONG,Release)(THIS) PURE;
+    /*** IUniformResourceLocatorA methods ***/
+    STDMETHOD(SetURL)(THIS_ LPCSTR pcszURL, DWORD dwInFlags) PURE;
+    STDMETHOD(GetURL)(THIS_ LPSTR *ppszURL) PURE;
+    STDMETHOD(InvokeCommand)(THIS_ PURLINVOKECOMMANDINFOA pURLCommandInfo) PURE;
 };
 #undef INTERFACE
-typedef IUniformResourceLocator *PIUniformResourceLocator;
-typedef const IUniformResourceLocator CIUniformResourceLocator;
-typedef const IUniformResourceLocator *PCIUniformResourceLocator;
 
-BOOL WINAPI InetIsOffline(DWORD);
-HRESULT WINAPI MIMEAssociationDialogA(HWND,DWORD,PCSTR,PCSTR,PSTR,UINT);
-HRESULT WINAPI MIMEAssociationDialogW(HWND,DWORD,PCWSTR,PCWSTR,PWSTR,UINT);
-HRESULT WINAPI TranslateURLA(PCSTR,DWORD,PSTR*);
-HRESULT WINAPI TranslateURLW(PCWSTR,DWORD,PWSTR*);
-HRESULT WINAPI URLAssociationDialogA(HWND,DWORD,PCSTR,PCSTR,PSTR,UINT);
-HRESULT WINAPI URLAssociationDialogW(HWND,DWORD,PCWSTR,PCWSTR,PWSTR,UINT);
-#ifdef UNICODE
-#define TranslateURL TranslateURLW
-#define MIMEAssociationDialog MIMEAssociationDialogW
-#define URLAssociationDialog URLAssociationDialogW
-#else
-#define TranslateURL TranslateURLA
-#define MIMEAssociationDialog MIMEAssociationDialogA
-#define URLAssociationDialog URLAssociationDialogA
-#endif
+#define INTERFACE IUniformResourceLocatorW
+DECLARE_INTERFACE_(IUniformResourceLocatorW,IUnknown)
+{
+    /*** IUnknown methods ***/
+    STDMETHOD_(HRESULT,QueryInterface)(THIS_ REFIID riid, VOID **ppvObject) PURE;
+    STDMETHOD_(ULONG,AddRef)(THIS) PURE;
+    STDMETHOD_(ULONG,Release)(THIS) PURE;
+    /*** IUniformResourceLocatorW methods ***/
+    STDMETHOD(SetURL)(THIS_ LPCWSTR pcszURL, DWORD dwInFlags) PURE;
+    STDMETHOD(GetURL)(THIS_ LPWSTR *ppszURL) PURE;
+    STDMETHOD(InvokeCommand)(THIS_ PURLINVOKECOMMANDINFOW pURLCommandInfo) PURE;
+};
+#undef INTERFACE
+
+DECL_WINELIB_TYPE_AW(URLINVOKECOMMANDINFO)
+DECL_WINELIB_TYPE_AW(PURLINVOKECOMMANDINFO)
+DECL_WINELIB_TYPE_AW(CURLINVOKECOMMANDINFO)
+DECL_WINELIB_TYPE_AW(PCURLINVOKECOMMANDINFO)
+
+
+typedef enum translateurl_in_flags {
+    TRANSLATEURL_FL_GUESS_PROTOCOL=1,
+    TRANSLATEURL_FL_USE_DEFAULT_PROTOCOL
+} TRANSLATEURL_IN_FLAGS;
+
+HRESULT WINAPI TranslateURLA(LPCSTR, DWORD, LPSTR *);
+HRESULT WINAPI TranslateURLW(LPCWSTR, DWORD, LPWSTR *);
+#define TranslateURL WINELIB_NAME_AW(TranslateURL)
+
+BOOL    WINAPI InetIsOffline(DWORD);
+
 #ifdef __cplusplus
 }
 #endif
-#endif
+
+#endif  /* __WINE_INTSHCUT_H */
