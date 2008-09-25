94b893abacec278705fa3aea906c438c65c6fe92
Aleksey Bragin <aleksey@reactos.org>
2008-09-25 11:24:51 +0000

    - Fix one more totally "out of ideas how to create sections" MmCreateSection usage. Not only the MaximumSize is mandatory for file-backed sections, but an allocation type must be specified (SEC_COMMIT, and it's not the same as some humble "0" passed there as a value). - Fix ReactOS's MmCreateDataFileSection to ignore 0 value in the MaximumSize (it has an additional check for MaximumSize being non-NULL, but this should be removed in future).
    svn path=/trunk/; revision=36506

--- reactos/subsystems/win32/win32k/objects/text.c
+++ reactos/subsystems/win32/win32k/objects/text.c
@@ -278,6 +278,7 @@ IntGdiAddFontResource(PUNICODE_STRING FileName, DWORD Characteristics)
    PFONT_ENTRY Entry;
    PSECTION_OBJECT SectionObject;
    ULONG ViewSize = 0;
+   LARGE_INTEGER SectionSize;
    FT_Fixed XScale, YScale;
    UNICODE_STRING FontRegPath = RTL_CONSTANT_STRING(L"\\REGISTRY\\Machine\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts");
 
@@ -294,13 +295,14 @@ IntGdiAddFontResource(PUNICODE_STRING FileName, DWORD Characteristics)
 
    if (!NT_SUCCESS(Status))
    {
-      DPRINT("Could not font file: %wZ\n", FileName);
+      DPRINT("Could not load font file: %wZ\n", FileName);
       return 0;
    }
 
+   SectionSize.QuadPart = 0LL;
    Status = MmCreateSection((PVOID)&SectionObject, SECTION_ALL_ACCESS,
-                            NULL, NULL, PAGE_READONLY,
-                            0, FileHandle, NULL);
+                            NULL, &SectionSize, PAGE_READONLY,
+                            SEC_COMMIT, FileHandle, NULL);
    if (!NT_SUCCESS(Status))
    {
       DPRINT("Could not map file: %wZ\n", FileName);
