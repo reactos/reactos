591ca1c7de3f00f3c6cef2b89f9e4087937c7dfb
Johannes Anderwald <johannes.anderwald@reactos.org>
2008-09-30 20:16:00 +0000

    - Add resources for advanced tcp/ip configuration dialog
    svn path=/trunk/; revision=36604

--- reactos/dll/win32/netcfgx/resource.h
+++ reactos/dll/win32/netcfgx/resource.h
@@ -2,8 +2,11 @@
 #define _RESOURCE_H__
 
 /* dialogs */
-#define IDD_TCPIP_BASIC_DLG 100
-#define IDD_TCPIP_ALTCF_DLG 101
+#define IDD_TCPIP_BASIC_DLG    100
+#define IDD_TCPIP_ALTCF_DLG    101
+#define IDD_TCPIP_ADVIP_DLG    102
+#define IDD_TCPIP_ADVDNS_DLG   103
+#define IDD_TCPIP_ADVOPT_DLG   104
 
 
 /* controls */
@@ -17,6 +20,41 @@
 #define IDC_DNS1            1007
 #define IDC_DNS2            1008
 #define IDC_ADVANCED        1009
+
+#define IDC_IPLIST          2000
+#define IDC_IPADD           2001
+#define IDC_IPMOD           2002
+#define IDC_IPDEL           2003
+#define IDC_GWLIST          2004
+#define IDC_GWADD           2005
+#define IDC_GWMOD           2006
+#define IDC_GWDEL           2007
+#define IDC_AUTOMETRIC      2008
+#define IDC_METRIC          2009
+
+#define IDC_DNSADDRLIST     3000
+#define IDC_DNSADDRUP       3001
+#define IDC_DNSADDRDOWN     3002
+#define IDC_DNSADDRADD      3003
+#define IDC_DNSADDRMOD      3004
+#define IDC_DNSADDRDEL      3005
+#define IDC_PRIMSUFFIX      3006
+#define IDC_TOPPRIMSUFFIX   3007
+#define IDC_SELSUFFIX       3008
+#define IDC_DNSSUFFIXLIST   3009
+#define IDC_DNSSUFFIXUP     3010
+#define IDC_DNSSUFFIXDOWN   3011
+#define IDC_DNSSUFFIXADD    3012
+#define IDC_DNSSUFFIXMOD    3013
+#define IDC_DNSSUFFIXDEL    3014
+#define IDC_SUFFIX          3015
+#define IDC_REGSUFFIX       3016
+#define IDC_USESUFFIX       3017
+
+
+#define IDC_OPTLIST         5000
+#define IDC_OPTPROP         5001
+#define IDC_OPTDESC         5002
 
 
 #define IDC_TCP_ALLOW_ALL   7000
