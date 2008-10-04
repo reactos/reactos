bb1039c37a41f810b5942e5e99b72202910b3016
Stefan Ginsberg <stefanginsberg@gmail.com>
2008-10-04 10:16:39 +0000

    - Register all Win32 callouts/procedures (just stubs for now though)
    svn path=/branches/nwin32/; revision=36640

--- subsystems/win32/win32k/gre/init.c
+++ subsystems/win32/win32k/gre/init.c
@@ -20,31 +20,8 @@
 
 NTSTATUS
 APIENTRY
-Win32kWinStaObjectParse(PWIN32_PARSEMETHOD_PARAMETERS Parameters)
-{
-    UNIMPLEMENTED;
-    return STATUS_SUCCESS;
-}
-
-VOID
-APIENTRY
-Win32kWinStaObjectDelete(PWIN32_DELETEMETHOD_PARAMETERS Parameters)
-{
-    UNIMPLEMENTED;
-}
-
-VOID
-APIENTRY
-Win32kDesktopObjectDelete(PWIN32_DELETEMETHOD_PARAMETERS Parameters)
-{
-    UNIMPLEMENTED;
-}
-
-
-NTSTATUS
-APIENTRY
-Win32kProcessCallback(struct _EPROCESS *Process,
-                      BOOLEAN Create)
+Win32kProcessCallout(PEPROCESS Process,
+                     BOOLEAN Create)
 {
     UNIMPLEMENTED;
     return STATUS_SUCCESS;
@@ -52,8 +29,120 @@ Win32kProcessCallback(struct _EPROCESS *Process,
 
 NTSTATUS
 APIENTRY
-Win32kThreadCallback(struct _ETHREAD *Thread,
-                     PSW32THREADCALLOUTTYPE Type)
+Win32kThreadCallout(PETHREAD Thread,
+                    PSW32THREADCALLOUTTYPE Type)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kGlobalAtomTableCallout(VOID)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kPowerEventCallout(PWIN32_POWEREVENT_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kPowerStateCallout(PWIN32_POWERSTATE_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kJobCallout(PWIN32_JOBCALLOUT_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kDesktopOpenProcedure(PWIN32_OPENMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kDesktopOkToCloseProcedure(PWIN32_OKAYTOCLOSEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kDesktopCloseProcedure(PWIN32_CLOSEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+VOID
+APIENTRY
+Win32kDesktopDeleteProcedure(PWIN32_DELETEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+}
+
+NTSTATUS
+APIENTRY
+Win32kWindowStationOkToCloseProcedure(PWIN32_OKAYTOCLOSEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kWindowStationCloseProcedure(PWIN32_CLOSEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+VOID
+APIENTRY
+Win32kWindowStationDeleteProcedure(PWIN32_DELETEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+}
+
+NTSTATUS
+APIENTRY
+Win32kWindowStationParseProcedure(PWIN32_PARSEMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kWindowStationOpenProcedure(PWIN32_OPENMETHOD_PARAMETERS Parameters)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+APIENTRY
+Win32kWin32DataCollectionProcedure(PEPROCESS Process,
+                                   PVOID Callback,
+                                   PVOID Context)
 {
     UNIMPLEMENTED;
     return STATUS_SUCCESS;
@@ -66,7 +155,7 @@ NTAPI
 DriverEntry(IN PDRIVER_OBJECT DriverObject,
             PUNICODE_STRING RegistryPath)
 {
-    WIN32_CALLOUTS_FPNS CalloutData = {0};
+    WIN32_CALLOUTS_FPNS CalloutData;
 
     DPRINT1("Win32k initialization: DO %p, RegPath %wZ\n", DriverObject,
         RegistryPath);
@@ -82,15 +171,27 @@ DriverEntry(IN PDRIVER_OBJECT DriverObject,
         return STATUS_UNSUCCESSFUL;
     }
 
-    /* Register Win32 callouts */
-    CalloutData.WindowStationParseProcedure = Win32kWinStaObjectParse;
-    CalloutData.WindowStationDeleteProcedure = Win32kWinStaObjectDelete;
-    CalloutData.DesktopDeleteProcedure = Win32kDesktopObjectDelete;
-    CalloutData.ProcessCallout = Win32kProcessCallback;
-    CalloutData.ThreadCallout = Win32kThreadCallback;
+    /* Set up Win32 callouts */
+    CalloutData.ProcessCallout = Win32kProcessCallout;
+    CalloutData.ThreadCallout = Win32kThreadCallout;
+    CalloutData.GlobalAtomTableCallout = Win32kGlobalAtomTableCallout;
+    CalloutData.PowerEventCallout = Win32kPowerEventCallout;
+    CalloutData.PowerStateCallout = Win32kPowerStateCallout;
+    CalloutData.JobCallout = Win32kJobCallout;
     CalloutData.BatchFlushRoutine = NtGdiFlushUserBatch;
+    CalloutData.DesktopOpenProcedure = Win32kDesktopOpenProcedure;
+    CalloutData.DesktopOkToCloseProcedure = Win32kDesktopOkToCloseProcedure;
+    CalloutData.DesktopCloseProcedure = Win32kDesktopCloseProcedure;
+    CalloutData.DesktopDeleteProcedure = Win32kDesktopDeleteProcedure;
+    CalloutData.WindowStationOkToCloseProcedure = Win32kWindowStationOkToCloseProcedure;
+    CalloutData.WindowStationCloseProcedure = Win32kWindowStationCloseProcedure;
+    CalloutData.WindowStationDeleteProcedure = Win32kWindowStationDeleteProcedure;
+    CalloutData.WindowStationParseProcedure = Win32kWindowStationParseProcedure;
+    CalloutData.WindowStationOpenProcedure = Win32kWindowStationOpenProcedure;
+    CalloutData.Win32DataCollectionProcedure = Win32kWin32DataCollectionProcedure;
 
-    PsEstablishWin32Callouts((PWIN32_CALLOUTS_FPNS)&CalloutData);
+    /* Register them */
+    PsEstablishWin32Callouts(&CalloutData);
 
     return STATUS_SUCCESS;
 }
