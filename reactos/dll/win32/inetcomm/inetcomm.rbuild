34b8a16cc7d5116c6b844316f1252769f39d8315
Dmitry Chapyshev <dmitry@reactos.org>
2008-11-08 07:14:34 +0000

    - Sync inetcomm with Wine Head - Sync imnxport.idl and mimeole.idl
    svn path=/trunk/; revision=37250

--- reactos/dll/win32/inetcomm/inetcomm.rbuild
+++ reactos/dll/win32/inetcomm/inetcomm.rbuild
@@ -8,14 +8,19 @@
 	<include base="ReactOS">include/reactos/wine</include>
 	<define name="__WINESRC__" />
 	<define name="_WIN32_WINNT">0x600</define>
+	<file>imaptransport.c</file>
 	<file>inetcomm_main.c</file>
 	<file>internettransport.c</file>
+	<file>mimeintl.c</file>
 	<file>mimeole.c</file>
+	<file>pop3transport.c</file>
 	<file>regsvr.c</file>
+	<file>smtptransport.c</file>
 	<file>inetcomm.spec</file>
 	<library>wine</library>
 	<library>uuid</library>
 	<library>ole32</library>
+	<library>oleaut32</library>
 	<library>ws2_32</library>
 	<library>user32</library>
 	<library>advapi32</library>
--- reactos/dll/win32/inetcomm/inetcomm_main.c
+++ reactos/dll/win32/inetcomm/inetcomm_main.c
@@ -37,6 +37,8 @@ WINE_DEFAULT_DEBUG_CHANNEL(inetcomm);
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
+    static IMimeInternational *international;
+
     TRACE("(%p, %d, %p)\n", hinstDLL, fdwReason, lpvReserved);
 
     switch (fdwReason)
@@ -47,8 +49,10 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
         DisableThreadLibraryCalls(hinstDLL);
         if (!InternetTransport_RegisterClass(hinstDLL))
             return FALSE;
+        MimeInternational_Construct(&international);
         break;
     case DLL_PROCESS_DETACH:
+        IMimeInternational_Release(international);
         InternetTransport_UnregisterClass(hinstDLL);
         break;
     default:
@@ -133,6 +137,8 @@ static const struct IClassFactoryVtbl cf_vtbl =
 
 static cf mime_body_cf      = { &cf_vtbl, MimeBody_create };
 static cf mime_allocator_cf = { &cf_vtbl, MimeAllocator_create };
+static cf mime_message_cf   = { &cf_vtbl, MimeMessage_create };
+static cf mime_security_cf  = { &cf_vtbl, MimeSecurity_create };
 
 /***********************************************************************
  *              DllGetClassObject (INETCOMM.@)
@@ -143,7 +149,27 @@ HRESULT WINAPI DllGetClassObject(REFCLSID rclsid, REFIID iid, LPVOID *ppv)
 
     TRACE("%s %s %p\n", debugstr_guid(rclsid), debugstr_guid(iid), ppv );
 
-    if( IsEqualCLSID( rclsid, &CLSID_IMimeBody ))
+    if (IsEqualCLSID(rclsid, &CLSID_ISMTPTransport))
+        return SMTPTransportCF_Create(iid, ppv);
+
+    if (IsEqualCLSID(rclsid, &CLSID_ISMTPTransport2))
+        return SMTPTransportCF_Create(iid, ppv);
+
+    if (IsEqualCLSID(rclsid, &CLSID_IIMAPTransport))
+        return IMAPTransportCF_Create(iid, ppv);
+
+    if (IsEqualCLSID(rclsid, &CLSID_IPOP3Transport))
+        return POP3TransportCF_Create(iid, ppv);
+
+    if ( IsEqualCLSID( rclsid, &CLSID_IMimeSecurity ))
+    {
+        cf = (IClassFactory*) &mime_security_cf.lpVtbl;
+    }
+    else if( IsEqualCLSID( rclsid, &CLSID_IMimeMessage ))
+    {
+        cf = (IClassFactory*) &mime_message_cf.lpVtbl;
+    }
+    else if( IsEqualCLSID( rclsid, &CLSID_IMimeBody ))
     {
         cf = (IClassFactory*) &mime_body_cf.lpVtbl;
     }
