346f2efabf12353f8a2aab02a4c504e629a948c6
Dmitry Chapyshev <dmitry@reactos.org>
2008-11-12 13:02:29 +0000

    - Sync uxtheme with wine head
    svn path=/trunk/; revision=37303

--- reactos/dll/win32/uxtheme/uxini.c
+++ reactos/dll/win32/uxtheme/uxini.c
@@ -68,7 +68,7 @@ PUXINI_FILE UXINI_LoadINI(HMODULE hTheme, LPCWSTR lpName) {
     TRACE("Loading resource INI %s\n", debugstr_w(lpName));
 
     if((hrsc = FindResourceW(hTheme, lpName, szTextFileResource))) {
-        if(!(lpThemesIni = (LPCWSTR)LoadResource(hTheme, hrsc))) {
+        if(!(lpThemesIni = LoadResource(hTheme, hrsc))) {
             TRACE("%s resource not found\n", debugstr_w(lpName));
             return NULL;
         }
