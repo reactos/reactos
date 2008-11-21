24660aadf94f1b68a4c0fc4909ad9534bfa2383e
Kamil Hornicek <kamil.hornicek@reactos.org>
2008-11-21 09:58:07 +0000

    - fix some checks, verified in win xp
    svn path=/branches/reactx/; revision=37503

--- reactos/dll/directx/ddraw/Surface/createsurface.c
+++ reactos/dll/directx/ddraw/Surface/createsurface.c
@@ -49,7 +49,7 @@ Internal_CreateSurface( LPDDRAWI_DIRECTDRAW_INT pDDraw, LPDDSURFACEDESC2 pDDSD,
         return CLASS_E_NOAGGREGATION;
     }
 
-    if(!pDDSD->dwFlags & DDSD_CAPS)
+    if(!(pDDSD->dwFlags & DDSD_CAPS))
     {
         return DDERR_INVALIDPARAMS;
     }
@@ -64,14 +64,14 @@ Internal_CreateSurface( LPDDRAWI_DIRECTDRAW_INT pDDraw, LPDDSURFACEDESC2 pDDSD,
         return DDERR_INVALIDCAPS;
     }
 
-    if(!(pDDSD->dwFlags & DDSD_HEIGHT) && !(pDDSD->dwFlags & DDSD_HEIGHT)
-        && !(pDDSD->ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE))
+    if((!(pDDSD->dwFlags & DDSD_HEIGHT) || !(pDDSD->dwFlags & DDSD_WIDTH))
+       && !(pDDSD->ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE))
     {
         return DDERR_INVALIDPARAMS;
     }
 
-    else if(pDDSD->dwFlags & DDSD_HEIGHT && pDDSD->dwFlags & DDSD_HEIGHT
-        && pDDSD->ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE)
+    else if(((pDDSD->dwFlags & DDSD_HEIGHT) || (pDDSD->dwFlags & DDSD_WIDTH))
+       && (pDDSD->ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE))
     {
         return DDERR_INVALIDPARAMS;
     }
