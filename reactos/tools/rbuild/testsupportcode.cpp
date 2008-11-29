8ad61b6d08395fd1aae0e17fb84457263a4fefbb
Stefan Ginsberg <stefanginsberg@gmail.com>
2008-11-29 21:01:42 +0000

    - Don't generate a function with non-standard STDCALL -- WINAPI works great, I promise.
    svn path=/trunk/; revision=37747

--- reactos/tools/rbuild/testsupportcode.cpp
+++ reactos/tools/rbuild/testsupportcode.cpp
@@ -390,7 +390,7 @@ TestSupportCode::WriteStartupFile ( Module& module )
 	s = s + sprintf ( s, "}\n" );
 	s = s + sprintf ( s, "\n" );
 	s = s + sprintf ( s, "int\n" );
-	s = s + sprintf ( s, "STDCALL\n" );
+	s = s + sprintf ( s, "WINAPI\n" );
 	s = s + sprintf ( s, "WinMain(HINSTANCE hInstance,\n" );
 	s = s + sprintf ( s, "  HINSTANCE hPrevInstance,\n" );
 	s = s + sprintf ( s, "  LPSTR lpszCmdParam,\n" );
