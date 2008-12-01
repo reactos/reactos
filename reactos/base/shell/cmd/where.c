53db82f49c5a3239b217c9d0299a1983a1077a87
Johannes Anderwald <johannes.anderwald@reactos.org>
2008-12-01 14:57:34 +0000

    - revert 37497 - use FindFirstFileW to obtain to original filename which is passed to CreateProcess - fixes bug 3875
    svn path=/trunk/; revision=37791

--- reactos/base/shell/cmd/where.c
+++ reactos/base/shell/cmd/where.c
@@ -141,10 +141,12 @@ SearchForExecutableSingle (LPCTSTR pFileName, LPTSTR pFullName, LPTSTR pPathExt,
 BOOL
 SearchForExecutable (LPCTSTR pFileName, LPTSTR pFullName)
 {
-	static TCHAR pszDefaultPathExt[] = _T(".com;.exe;.bat;.cmd");
+	static TCHAR pszDefaultPathExt[] = _T(".COM;.EXE;.BAT;.CMD");
 	LPTSTR pszPathExt, pszPath;
 	LPTSTR pCh;
 	DWORD  dwBuffer;
+	HANDLE hFile;
+	WIN32_FIND_DATAW FindData;
 	TRACE ("SearchForExecutable: \'%s\'\n", debugstr_aw(pFileName));
 
 	/* load environment varable PATHEXT */
@@ -154,20 +156,21 @@ SearchForExecutable (LPCTSTR pFileName, LPTSTR pFullName)
 	{
 		pszPathExt = (LPTSTR)cmd_realloc (pszPathExt, dwBuffer * sizeof (TCHAR));
 		GetEnvironmentVariable (_T("PATHEXT"), pszPathExt, dwBuffer);
-		_tcslwr(pszPathExt);
 	}
 	else if (0 == dwBuffer)
 	{
 		_tcscpy(pszPathExt, pszDefaultPathExt);
 	}
-	else
-	{
-		_tcslwr(pszPathExt);
-	}
 
 	/* Check if valid directly on specified path */
 	if (SearchForExecutableSingle(pFileName, pFullName, pszPathExt, NULL))
 	{
+		hFile = FindFirstFileW(pFullName, &FindData);
+		if (hFile)
+		{
+			_tcscpy(pFullName, FindData.cFileName);
+		}
+		FindClose(hFile);
 		cmd_free(pszPathExt);
 		return TRUE;
 	}
@@ -196,6 +199,12 @@ SearchForExecutable (LPCTSTR pFileName, LPTSTR pFullName)
 	{
 		if (SearchForExecutableSingle(pFileName, pFullName, pszPathExt, pCh))
 		{
+			hFile = FindFirstFileW(pFullName, &FindData);
+			if (hFile)
+			{
+				_tcscpy(pFullName, FindData.cFileName);
+			}
+			FindClose(hFile);
 			cmd_free(pszPath);
 			cmd_free(pszPathExt);
 			return TRUE;
