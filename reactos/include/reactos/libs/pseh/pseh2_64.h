9aa69b88a98c4c8eadfb4ac8ca777d250be99613
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-12-06 18:33:48 +0000

    oops, looks like I forgot to commit this file (SEH2 stubs)
    svn path=/branches/ros-amd64-bringup/; revision=37903

---
+++ reactos/include/reactos/libs/pseh/pseh2_64.h
@@ -0,0 +1,16 @@
+#ifndef KJK_PSEH2_H_
+#define KJK_PSEH2_H_
+
+
+#define _SEH2_TRY if(1)
+#define _SEH2_EXCEPT(x) else
+#define _SEH2_END
+#define _SEH2_GetExceptionCode() (0)
+#define _SEH2_YIELD(x) x
+#define _SEH2_LEAVE
+
+
+
+#endif
+
+/* EOF */
