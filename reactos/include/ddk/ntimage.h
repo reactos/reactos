77a33b8c79f826397f5ff9b685cc8f367d63cad3
Aleksey Bragin <aleksey@reactos.org>
2008-12-11 12:50:37 +0000

    - Add Dll Characteristics defines.
    svn path=/trunk/; revision=38016

--- reactos/include/ddk/ntimage.h
+++ reactos/include/ddk/ntimage.h
@@ -419,7 +419,17 @@ typedef PIMAGE_NT_HEADERS32                 PIMAGE_NT_HEADERS;
      ((NtHeader))->FileHeader.SizeOfOptionalHeader   \
     ))
 
-
+//
+// Dll Characteristics
+//
+#define IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE          0x0040
+#define IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY       0x0080
+#define IMAGE_DLLCHARACTERISTICS_NX_COMPAT             0x0100
+#define IMAGE_DLLCHARACTERISTICS_NO_ISOLATION          0x0200
+#define IMAGE_DLLCHARACTERISTICS_NO_SEH                0x0400
+#define IMAGE_DLLCHARACTERISTICS_NO_BIND               0x0800
+#define IMAGE_DLLCHARACTERISTICS_WDM_DRIVER            0x2000
+#define IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE 0x8000
 
 //
 // Directory Entry Specifiers
