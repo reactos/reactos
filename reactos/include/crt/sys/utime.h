e9d55d0638bc4a5c6963fd4a193cc2e885ccf2d7
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-12-14 23:41:51 +0000

    mbctype.h: fix broken _mbctype[] and _mbcasemap[] declarations sec_api/time_s.h: add asctime_s stdio.h: comment out defines for non-conforming swprintfs sys/utime.h: fix definition of _utime / _utime32 to be compatible to msvc while linking to msvcrt.dll tchar.h: add secure time functions fixes msvcrt_winetest
    svn path=/trunk/; revision=38088

--- reactos/include/crt/sys/utime.h
+++ reactos/include/crt/sys/utime.h
@@ -103,31 +103,32 @@ extern "C" {
   _CRTIMP int __cdecl _wutime64(const wchar_t *_Filename,struct __utimbuf64 *_Time);
 #endif
 
-#if 0
+// Do it like this to keep compatibility to MSVC while using msvcrt.dll
 #ifndef RC_INVOKED
-#ifdef _USE_32BIT_TIME_T
-__CRT_INLINE int __cdecl _utime32(const char *_Filename,struct _utimbuf *_Utimbuf) {
-  return _utime32(_Filename,(struct __utimbuf32 *)_Utimbuf);
-}
-__CRT_INLINE int __cdecl _futime(int _Desc,struct _utimbuf *_Utimbuf) {
-  return _futime32(_Desc,(struct __utimbuf32 *)_Utimbuf);
-}
-__CRT_INLINE int __cdecl _wutime(const wchar_t *_Filename,struct _utimbuf *_Utimbuf) {
-  return _wutime32(_Filename,(struct __utimbuf32 *)_Utimbuf);
-}
-#else
-__CRT_INLINE int __cdecl _utime(const char *_Filename,struct _utimbuf *_Utimbuf) {
-  return _utime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
-}
-__CRT_INLINE int __cdecl _futime(int _Desc,struct _utimbuf *_Utimbuf) {
-  return _futime64(_Desc,(struct __utimbuf64 *)_Utimbuf);
-}
-__CRT_INLINE int __cdecl _wutime(const wchar_t *_Filename,struct _utimbuf *_Utimbuf) {
-  return _wutime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
-}
-#endif
-#endif
-#endif
+ #ifdef _USE_32BIT_TIME_T
+  __CRT_INLINE int __cdecl _utime32(const char *_Filename,struct __utimbuf32 *_Utimbuf) {
+    return _utime(_Filename,(struct __timbuf *)_Utimbuf);
+  }
+  __CRT_INLINE int __cdecl _futime32(int _Desc,struct __utimbuf32 *_Utimbuf) {
+    return _futime(_Desc,(struct _utimbuf *)_Utimbuf);
+  }
+  __CRT_INLINE int __cdecl _wutime32(const wchar_t *_Filename,struct __utimbuf32 *_Utimbuf) {
+    return _wutime(_Filename,(struct _utimbuf *)_Utimbuf);
+  }
+ #else // !_USE_32BIT_TIME_T
+  #ifndef _WIN64
+  __CRT_INLINE int __cdecl _utime(const char *_Filename,struct _utimbuf *_Utimbuf) {
+    return _utime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
+  }
+  __CRT_INLINE int __cdecl _futime(int _Desc,struct _utimbuf *_Utimbuf) {
+    return _futime64(_Desc,(struct __utimbuf64 *)_Utimbuf);
+  }
+  __CRT_INLINE int __cdecl _wutime(const wchar_t *_Filename,struct _utimbuf *_Utimbuf) {
+    return _wutime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
+  }
+  #endif
+ #endif // _USE_32BIT_TIME_T
+#endif // RC_INVOKED
 
 #ifndef	NO_OLDNAMES
 __CRT_INLINE int __cdecl utime(const char *_Filename,struct utimbuf *_Utimbuf) {
