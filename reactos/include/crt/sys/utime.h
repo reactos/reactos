db8c91d651e99317f51ef9414418f0f4ad789d51
Timo Kreuzer <timo.kreuzer@reactos.org>
2008-12-14 01:43:24 +0000

    fix utime usage
    svn path=/trunk/; revision=38068

--- reactos/include/crt/sys/utime.h
+++ reactos/include/crt/sys/utime.h
@@ -91,8 +91,11 @@ extern "C" {
 #endif
 #endif
 
+  _CRTIMP int __cdecl _utime(const char *_Filename,struct _utimbuf *_Utimbuf);
   _CRTIMP int __cdecl _utime32(const char *_Filename,struct __utimbuf32 *_Time);
+  _CRTIMP int __cdecl _futime(int _Desc,struct _utimbuf *_Utimbuf);
   _CRTIMP int __cdecl _futime32(int _FileDes,struct __utimbuf32 *_Time);
+  _CRTIMP int __cdecl _wutime(const wchar_t *_Filename,struct _utimbuf *_Utimbuf);
   _CRTIMP int __cdecl _wutime32(const wchar_t *_Filename,struct __utimbuf32 *_Time);
 #if _INTEGRAL_MAX_BITS >= 64
   _CRTIMP int __cdecl _utime64(const char *_Filename,struct __utimbuf64 *_Time);
@@ -100,9 +103,10 @@ extern "C" {
   _CRTIMP int __cdecl _wutime64(const wchar_t *_Filename,struct __utimbuf64 *_Time);
 #endif
 
+#if 0
 #ifndef RC_INVOKED
 #ifdef _USE_32BIT_TIME_T
-__CRT_INLINE int __cdecl _utime(const char *_Filename,struct _utimbuf *_Utimbuf) {
+__CRT_INLINE int __cdecl _utime32(const char *_Filename,struct _utimbuf *_Utimbuf) {
   return _utime32(_Filename,(struct __utimbuf32 *)_Utimbuf);
 }
 __CRT_INLINE int __cdecl _futime(int _Desc,struct _utimbuf *_Utimbuf) {
@@ -122,18 +126,13 @@ __CRT_INLINE int __cdecl _wutime(const wchar_t *_Filename,struct _utimbuf *_Utim
   return _wutime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
 }
 #endif
+#endif
+#endif
 
 #ifndef	NO_OLDNAMES
-#ifdef _USE_32BIT_TIME_T
 __CRT_INLINE int __cdecl utime(const char *_Filename,struct utimbuf *_Utimbuf) {
-  return _utime32(_Filename,(struct __utimbuf32 *)_Utimbuf);
+  return _utime(_Filename,(struct _utimbuf *)_Utimbuf);
 }
-#else
-__CRT_INLINE int __cdecl utime(const char *_Filename,struct utimbuf *_Utimbuf) {
-  return _utime64(_Filename,(struct __utimbuf64 *)_Utimbuf);
-}
-#endif
-#endif
 #endif
 
 #ifdef __cplusplus
