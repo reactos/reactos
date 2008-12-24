e5b0a21e66b0bc8a02c5c0adca562fd93a30204c
James Tabor <james.tabor@reactos.org>
2008-12-24 22:58:21 +0000

    [sginsberg] fixed some warnings.
    svn path=/trunk/; revision=38336

--- reactos/dll/win32/wininet/internet.h
+++ reactos/dll/win32/wininet/internet.h
@@ -23,9 +23,6 @@
 #ifndef _WINE_INTERNET_H_
 #define _WINE_INTERNET_H_
 
-/* ReactOS-specific definitions */
-#define CP_UNIXCP   CP_THREAD_ACP
-
 #ifndef __WINE_CONFIG_H
 # error You must include config.h to use this header
 #endif
