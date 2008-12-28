97d06533f8c39dbd7ef82370c12958aa12b1a7dd
Christoph von Wittich <christoph_vw@reactos.org>
2008-12-28 11:31:26 +0000

    sync browseui winetest to wine 1.1.11
    svn path=/trunk/; revision=38417

--- rostests/winetests/browseui/autocomplete.c
+++ rostests/winetests/browseui/autocomplete.c
@@ -20,9 +20,11 @@
 #include <assert.h>
 #include <stdarg.h>
 
+#include <initguid.h>
 #include <windows.h>
 #include <shlobj.h>
 #include <shlwapi.h>
+#include <shlguid.h>
 
 #include "wine/test.h"
 
