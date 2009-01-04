f54bc7959ae8c9a8954234de3732d221e19b4b71
Aleksey Bragin <aleksey@reactos.org>
2009-01-04 10:12:38 +0000

    - Revert changes to NtSetVolumeInformationFile made in 38550, it should indeed use IoGetRelatedDeviceObject, not target device.
    svn path=/trunk/; revision=38552

--- reactos/ntoskrnl/io/iomgr/iofunc.c
+++ reactos/ntoskrnl/io/iomgr/iofunc.c
@@ -3307,8 +3307,7 @@ NtSetVolumeInformationFile(IN HANDLE FileHandle,
     }
 
     /* Get the device object */
-    Status = IoGetRelatedTargetDevice(FileObject, &DeviceObject);
-    if (!NT_SUCCESS(Status)) return Status;
+    DeviceObject = IoGetRelatedDeviceObject(FileObject);
 
     /* Clear File Object event */
     KeClearEvent(&FileObject->Event);
