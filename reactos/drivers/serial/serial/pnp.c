25efba5b5b86a6c2040d9b1c84205c3d2a0a2a44
Stefan Ginsberg <stefanginsberg@gmail.com>
2009-01-20 18:58:08 +0000

    - Fix leftover from 38960
    svn path=/trunk/; revision=38983

--- reactos/drivers/serial/serial/pnp.c
+++ reactos/drivers/serial/serial/pnp.c
@@ -223,7 +223,7 @@ SerialPnpStartDevice(
 	ComPortBase = ULongToPtr(DeviceExtension->BaseAddress);
 
 	/* Test if we are trying to start the serial port used for debugging */
-    DPRINT1("Comparing addresses: KdComPortInUse: %p, ComPortBase: %p\n", KdComPortInUse, ComPortBase);
+    DPRINT("Comparing addresses: KdComPortInUse: %p, ComPortBase: %p\n", KdComPortInUse, ComPortBase);
 	if (KdComPortInUse == ComPortBase)
 	{
 		INFO_(SERIAL, "Failing IRP_MN_START_DEVICE as this serial port is used for debugging\n");
