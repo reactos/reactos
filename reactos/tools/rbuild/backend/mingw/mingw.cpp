ba54ffaf1039ed9412cf24cd088e3345055fab75
Aleksey Bragin <aleksey@reactos.org>
2009-01-26 16:55:20 +0000

    - Revert 39111: "Begin moving rules out of modulehandler.cpp and into makefile include rules.mak, where they will be more readable and manageable. Currently implemented: target C compiler and target C++ compiler rules. Please do a clean build if possible. Testing with unusual output paths appreciated"
    svn path=/trunk/; revision=39112

--- reactos/tools/rbuild/backend/mingw/mingw.cpp
+++ reactos/tools/rbuild/backend/mingw/mingw.cpp
@@ -519,8 +519,6 @@ MingwBackend::GenerateProjectLFLAGS () const
 void
 MingwBackend::GenerateGlobalVariables () const
 {
-	fputs ( "include tools$(SEP)rbuild$(SEP)backend$(SEP)mingw$(SEP)rules.mak\n", fMakefile );
-
 	if ( configuration.Dependencies == FullDependencies )
 	{
 		fprintf ( fMakefile,
