0c9316bd544e8f12eba9b2d8e6ece70886374210
Michael Martin <michael.martin@reactos.org>
2009-02-03 09:09:06 +0000

    - Change MultiByteToWideChar to match windows behavior. Fix part 1 of ? for bug #3740.
    svn path=/trunk/; revision=39301

--- reactos/dll/win32/kernel32/misc/nls.c
+++ reactos/dll/win32/kernel32/misc/nls.c
@@ -523,6 +523,14 @@ IntMultiByteToWideCharCP(UINT CodePage,
         if (WideCharCount == 0)
             return MultiByteCount;
 
+        /* Fill the WideCharString buffer with what will fit: Verified on WinXP */
+        for (TempLength = (WideCharCount < MultiByteCount) ? WideCharCount : MultiByteCount;
+            TempLength > 0;
+            MultiByteString++, TempLength--)
+        {
+            *WideCharString++ = CodePageTable->MultiByteTable[(UCHAR)*MultiByteString];
+        }
+
         /* Adjust buffer size. Wine trick ;-) */
         if (WideCharCount < MultiByteCount)
         {
@@ -530,15 +538,7 @@ IntMultiByteToWideCharCP(UINT CodePage,
             SetLastError(ERROR_INSUFFICIENT_BUFFER);
             return 0;
         }
-
-        for (TempLength = MultiByteCount;
-            TempLength > 0;
-            MultiByteString++, TempLength--)
-        {
-            *WideCharString++ = CodePageTable->MultiByteTable[(UCHAR)*MultiByteString];
-        }
-
-        return MultiByteCount;
+	    return MultiByteCount;
     }
 }
 
