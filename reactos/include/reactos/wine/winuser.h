d258115b75dc0530e9e02343ae9db16d6cf10283
Christoph von Wittich <christoph_vw@reactos.org>
2009-02-08 18:52:59 +0000

    E_PROP_ID_UNSUPPORTED is already defined in vfwmsgs.h
    svn path=/trunk/; revision=39497

--- reactos/include/reactos/wine/winuser.h
+++ reactos/include/reactos/wine/winuser.h
@@ -18,14 +18,6 @@ WINUSERAPI UINT WINAPI PrivateExtractIconsA(LPCSTR,int,int,int,HICON*,UINT*,UINT
 WINUSERAPI UINT WINAPI PrivateExtractIconsW(LPCWSTR,int,int,int,HICON*,UINT*,UINT,UINT);
 
 #define WM_ALTTABACTIVE         0x0029
-
-#ifndef E_PROP_ID_UNSUPPORTED
-#define E_PROP_ID_UNSUPPORTED            ((HRESULT)0x80070490)
-#endif
-#ifndef E_PROP_SET_UNSUPPORTED
-#define E_PROP_SET_UNSUPPORTED           ((HRESULT)0x80070492)
-#endif
-
 #define WM_SETVISIBLE           0x0009
 
 #endif /* __WINE_WINUSER_H */
