871d4837211beb7a94c4e7d8ccef0818c68d12a0
Danny Götte <danny.goette@reactos.org>
2009-02-12 12:28:16 +0000

    add support for different port numbers while connecting to MySQL. patch by Mariusz Przybylski
    svn path=/branches/danny-web/; revision=39564

--- reactos.org/htdocs/roscms/connect.db.php
+++ reactos.org/htdocs/roscms/connect.db.php
@@ -21,9 +21,10 @@
 
 // Database:
 $db_host = 'localhost';
-$db_user = 'user';
-$db_pass = 'pass';
-$db_name = 'roscms';
+$db_user = 'lev-db';
+$db_pass = 'DangerGround';
+$db_name = 'realroscms';
+$db_port = 3306;
 
 // vars are unset in DBConnection class
 // !!! so be sure to use DBConnection or unset(..) yourself
--- reactos.org/htdocs/roscms/lib/DBConnection.class.php
+++ reactos.org/htdocs/roscms/lib/DBConnection.class.php
@@ -41,13 +41,14 @@ class DBConnection extends PDO
     include_once(ROSCMS_PATH.'connect.db.php');
 
     try {
-      parent::__construct('mysql:dbname='.$db_name.';host='.$db_host, $db_user, $db_pass);
+      parent::__construct('mysql:dbname='.$db_name.';host='.$db_host.';port='.$db_port, $db_user, $db_pass);
 
       // unset loaded db config
       unset($GLOBALS['db_name']);
       unset($GLOBALS['db_host']);
       unset($GLOBALS['db_user']);
       unset($GLOBALS['db_pass']);
+      unset($GLOBALS['db_port']);
 
       // show errors as warning, and use our own statement class
       $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
