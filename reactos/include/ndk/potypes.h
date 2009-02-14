a39829c012ceed85187ecf23b5abb9ce84cab378
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-02-14 01:57:28 +0000

    Add PPROCESSOR_IDLE_FUNCTION, fix PROCESSOR_POWER_STATE and move it from ndk to ntpoapi.h
    svn path=/trunk/; revision=39598

--- reactos/include/ndk/potypes.h
+++ reactos/include/ndk/potypes.h
@@ -40,57 +40,6 @@ typedef enum _SYSTEM_DOCK_STATE
 #ifndef NTOS_MODE_USER
 
 //
-// Processor Power State Data
-//
-typedef struct _PROCESSOR_POWER_STATE
-{
-    PVOID IdleFunction;
-    ULONG Idle0KernelTimeLimit;
-    ULONG Idle0LastTime;
-    PVOID IdleHandlers;
-    PVOID IdleState;
-    ULONG IdleHandlersCount;
-    ULONGLONG LastCheck;
-    PROCESSOR_IDLE_TIMES IdleTimes;
-    ULONG IdleTime1;
-    ULONG PromotionCheck;
-    ULONG IdleTime2;
-    UCHAR CurrentThrottle;
-    UCHAR ThermalThrottleLimit;
-    UCHAR CurrentThrottleIndex;
-    UCHAR ThermalThrottleIndex;
-    ULONG LastKernelUserTime;
-    ULONG PerfIdleTime;
-    ULONG DebugDelta;
-    ULONG DebugCount;
-    ULONG LastSysTime;
-    ULONG TotalIdleStateTime[3];
-    ULONG TotalIdleTransitions[3];
-    ULONGLONG PreviousC3StateTime;
-    UCHAR KneeThrottleIndex;
-    UCHAR ThrottleLimitIndex;
-    UCHAR PerfStatesCount;
-    UCHAR ProcessorMinThrottle;
-    UCHAR ProcessorMaxThrottle;
-    UCHAR LastBusyPercentage;
-    UCHAR LastC3Percentage;
-    UCHAR LastAdjustedBusyPercentage;
-    ULONG PromotionCount;
-    ULONG DemotionCount;
-    ULONG ErrorCount;
-    ULONG RetryCount;
-    ULONG Flags;
-    LARGE_INTEGER PerfCounterFrequency;
-    ULONG PerfTickCount;
-    KTIMER PerfTimer;
-    KDPC PerfDpc;
-    PROCESSOR_PERF_STATE *PerfStates;
-    PVOID PerfSetThrottle;
-    ULONG LastC3KernelUserTime;
-    ULONG Spare1[1];
-} PROCESSOR_POWER_STATE, *PPROCESSOR_POWER_STATE;
-
-//
 // Device Notification Structure
 //
 typedef struct _PO_DEVICE_NOTIFY
