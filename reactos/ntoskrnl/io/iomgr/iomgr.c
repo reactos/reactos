9b42ebfbcc4bcc6aad720ede9bafab19bfc216d9
Dmitry Gorbachev <gorbachev@reactos.org>
2009-02-23 13:17:07 +0000

    Call IopEnumerateDevice() later, after IopCreateArcNames().
    svn path=/trunk/; revision=39721

--- reactos/ntoskrnl/io/iomgr/iomgr.c
+++ reactos/ntoskrnl/io/iomgr/iomgr.c
@@ -496,9 +496,6 @@ IoInitSystem(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
     /* Call back drivers that asked for */
     IopReinitializeBootDrivers();
 
-    /* Initialize PnP root relations */
-    IopEnumerateDevice(IopRootDeviceNode->PhysicalDeviceObject);
-
     /* Check if this was a ramdisk boot */
     if (!_strnicmp(LoaderBlock->ArcBootDeviceName, "ramdisk(0)", 10))
     {
@@ -511,6 +508,9 @@ IoInitSystem(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 
     /* Mark the system boot partition */
     if (!IopMarkBootPartition(LoaderBlock)) return FALSE;
+
+    /* Initialize PnP root relations */
+    IopEnumerateDevice(IopRootDeviceNode->PhysicalDeviceObject);
 
 #ifndef _WINKD_
     /* Read KDB Data */
