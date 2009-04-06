468227f96adb1c2d78d88ff7c8c9f3e1a01afde9
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-04-06 01:21:04 +0000

    DDK update - Add devioctl.h - miniport.h: fix include guard and add more stuff - video.h: fix include guard, remove VideoDebugprint prototype - videoagp.h: fix include guard -winddi.h: fix include guard, add some defines and some function pointer types - guiddef.h: add definition of EXTERN_C
    svn path=/trunk/; revision=40383

--- reactos/include/ddk/miniport.h
+++ reactos/include/ddk/miniport.h
@@ -20,12 +20,48 @@
  *
  */
 
-#ifndef __MINIPORT_H
-#define __MINIPORT_H
+#ifndef _MINIPORT_
+#define _MINIPORT_
+
+#include <ntdef.h>
+#include <stddef.h>
+#include <basetsd.h>
+#include <sdkddkver.h>
+#include <guiddef.h>
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef enum _INTERFACE_TYPE
+{
+    InterfaceTypeUndefined = -1,
+    Internal,
+    Isa,
+    Eisa,
+    MicroChannel,
+    TurboChannel,
+    PCIBus,
+    VMEBus,
+    NuBus,
+    PCMCIABus,
+    CBus,
+    MPIBus,
+    MPSABus,
+    ProcessorInternal,
+    InternalPowerBus,
+    PNPISABus,
+    PNPBus,
+    Vmcs,
+    MaximumInterfaceType
+} INTERFACE_TYPE, *PINTERFACE_TYPE;
+
+typedef enum _KINTERRUPT_MODE
+{
+    LevelSensitive,
+    Latched
+} KINTERRUPT_MODE, *PKINTERRUPT_MODE;
 
 #define EMULATOR_READ_ACCESS              0x01
 #define EMULATOR_WRITE_ACCESS             0x02
@@ -35,7 +71,6 @@ typedef enum _EMULATOR_PORT_ACCESS_TYPE {
 	Ushort,
 	Ulong
 } EMULATOR_PORT_ACCESS_TYPE, *PEMULATOR_PORT_ACCESS_TYPE;
-
 
 typedef struct _EMULATOR_ACCESS_ENTRY {
   ULONG  BasePort;
@@ -57,6 +92,170 @@ typedef struct _VIDEO_ACCESS_RANGE {
   UCHAR  RangePassive;
 } VIDEO_ACCESS_RANGE, *PVIDEO_ACCESS_RANGE;
 #endif
+
+typedef VOID (*PINTERFACE_REFERENCE)(PVOID Context);
+typedef VOID (*PINTERFACE_DEREFERENCE)(PVOID Context);
+
+typedef struct _INTERFACE
+{
+    USHORT Size;
+    USHORT Version;
+    PVOID Context;
+    PINTERFACE_REFERENCE InterfaceReference;
+    PINTERFACE_DEREFERENCE InterfaceDereference;
+} INTERFACE, *PINTERFACE;
+
+typedef enum _IRQ_DEVICE_POLICY
+{
+    IrqPolicyMachineDefault = 0,
+    IrqPolicyAllCloseProcessors,
+    IrqPolicyOneCloseProcessor,
+    IrqPolicyAllProcessorsInMachine,
+    IrqPolicySpecifiedProcessors,
+    IrqPolicySpreadMessagesAcrossAllProcessors
+} IRQ_DEVICE_POLICY, *PIRQ_DEVICE_POLICY;
+
+typedef enum _IRQ_PRIORITY
+{
+    IrqPriorityUndefined = 0,
+    IrqPriorityLow,
+    IrqPriorityNormal,
+    IrqPriorityHigh
+} IRQ_PRIORITY, *PIRQ_PRIORITY;
+
+typedef enum _DMA_WIDTH
+{
+    Width8Bits,
+    Width16Bits,
+    Width32Bits,
+    MaximumDmaWidth
+} DMA_WIDTH, *PDMA_WIDTH;
+
+typedef enum _DMA_SPEED
+{
+    Compatible,
+    TypeA,
+    TypeB,
+    TypeC,
+    TypeF,
+    MaximumDmaSpeed
+} DMA_SPEED, *PDMA_SPEED;
+
+#pragma pack(push,4)
+typedef enum _BUS_DATA_TYPE
+{
+    ConfigurationSpaceUndefined = -1,
+    Cmos,
+    EisaConfiguration,
+    Pos,
+    CbusConfiguration,
+    PCIConfiguration,
+    VMEConfiguration,
+    NuBusConfiguration,
+    PCMCIAConfiguration,
+    MPIConfiguration,
+    MPSAConfiguration,
+    PNPISAConfiguration,
+    SgiInternalConfiguration,
+    MaximumBusDataType
+} BUS_DATA_TYPE, *PBUS_DATA_TYPE;
+#pragma pack(pop)
+
+typedef struct _IO_RESOURCE_DESCRIPTOR
+{
+    UCHAR Option;
+    UCHAR Type;
+    UCHAR ShareDisposition;
+    UCHAR Spare1;
+    USHORT Flags;
+    USHORT Spare2;
+
+    union
+    {
+        struct
+        {
+            ULONG Length;
+            ULONG Alignment;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Port;
+
+        struct
+        {
+            ULONG Length;
+            ULONG Alignment;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Memory;
+
+        struct
+        {
+            ULONG MinimumVector;
+            ULONG MaximumVector;
+            IRQ_DEVICE_POLICY AffinityPolicy;
+            IRQ_PRIORITY PriorityPolicy;
+            KAFFINITY TargetedProcessors;
+        } Interrupt;
+
+        struct
+        {
+            ULONG MinimumChannel;
+            ULONG MaximumChannel;
+        } Dma;
+
+        struct
+        {
+            ULONG Length;
+            ULONG Alignment;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Generic;
+
+        struct
+        {
+            ULONG Data[3];
+        } DevicePrivate;
+
+        struct
+        {
+            ULONG Length;
+            ULONG MinBusNumber;
+            ULONG MaxBusNumber;
+            ULONG Reserved;
+        } BusNumber;
+
+        struct
+        {
+            ULONG Priority;
+            ULONG Reserved1;
+            ULONG Reserved2;
+        } ConfigData;
+
+        struct
+        {
+            ULONG Length40;
+            ULONG Alignment40;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Memory40;
+
+        struct
+        {
+            ULONG Length48;
+            ULONG Alignment48;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Memory48;
+
+        struct
+        {
+            ULONG Length64;
+            ULONG Alignment64;
+            PHYSICAL_ADDRESS MinimumAddress;
+            PHYSICAL_ADDRESS MaximumAddress;
+        } Memory64;
+    } u;
+} IO_RESOURCE_DESCRIPTOR, *PIO_RESOURCE_DESCRIPTOR;
 
 typedef VOID DDKAPI
 (*PBANKED_SECTION_ROUTINE)(
