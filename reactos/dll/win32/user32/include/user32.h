a909ec044718b152e8edfde2468782768a2d07d1
James Tabor <james.tabor@reactos.org>
2009-04-13 23:27:52 +0000

    - Remove NtGdi calls from user32 headers.
    svn path=/trunk/; revision=40494

--- reactos/dll/win32/user32/include/user32.h
+++ reactos/dll/win32/user32/include/user32.h
@@ -43,8 +43,6 @@
 #include <pseh/pseh2.h>
 
 /* FIXME: Use ntgdi.h then cleanup... */
-HGDIOBJ WINAPI  NtGdiSelectObject(HDC  hDC, HGDIOBJ  hGDIObj);
-BOOL WINAPI NtGdiPatBlt(HDC hdcDst, INT x, INT y, INT cx, INT cy, DWORD rop4);
 LONG WINAPI GdiGetCharDimensions(HDC, LPTEXTMETRICW, LONG *);
 BOOL FASTCALL IsMetaFile(HDC);
 
