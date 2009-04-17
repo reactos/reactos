0f8d58977d2ca69c443af86aa886bdbbe4db3903
Cameron Gutman <aicommander@gmail.com>
2009-04-17 01:50:29 +0000

    - Fix another multiplication error
    svn path=/trunk/; revision=40553

--- reactos/drivers/network/ndis/ndis/control.c
+++ reactos/drivers/network/ndis/ndis/control.c
@@ -310,7 +310,7 @@ NdisWaitEvent(
   LARGE_INTEGER Timeout;
   NTSTATUS Status;
 
-  Timeout.QuadPart = MsToWait * -10000LL;
+  Timeout.QuadPart = Int32x32To64(MsToWait, -10000);
 
   Status = KeWaitForSingleObject(&Event->Event, Executive, KernelMode, TRUE, &Timeout);
 
