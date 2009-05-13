5f6dd7004ebd919ff6b3ff184225ebe5637bd195
Steven Edwards <winehacker@gmail.com>
2009-05-13 19:35:05 +0000

    minor SunOS fix
    svn path=/trunk/; revision=40911

--- reactos/tools/ssprintf.cpp
+++ reactos/tools/ssprintf.cpp
@@ -41,7 +41,12 @@ inline int iswdigit ( wchar_t c )
 }
 #endif
 
-#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__CYGWIN__)
+#if defined(__sun__)
+#include <alloca.h>
+#include <ieee.h>
+#endif
+
+#if defined(__FreeBSD__) || defined(__APPLE__) defined(__sun__) || defined(__CYGWIN__)
 # define __isnan isnan
 # define __finite finite
 # define powl __builtin_powl
