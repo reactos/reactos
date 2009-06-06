c4c37910a46d9ce0874ec6596244b4c50ae96050
Ged Murphy <gedmurphy@reactos.org>
2009-06-06 14:30:57 +0000

    Use the dll runtime library. Fixes building taskmgr with VS and probably others
    svn path=/trunk/; revision=41302

--- reactos/tools/rbuild/backend/msvc/vcprojmaker.cpp
+++ reactos/tools/rbuild/backend/msvc/vcprojmaker.cpp
@@ -410,7 +410,7 @@ MSVCBackend::_generate_vcproj ( const Module& module )
 		fprintf ( OUT, "\t\t\t\tForcedIncludeFiles=\"%s\"\r\n", "warning.h");
 		fprintf ( OUT, "\t\t\t\tMinimalRebuild=\"%s\"\r\n", speed ? "TRUE" : "FALSE" );
 		fprintf ( OUT, "\t\t\t\tBasicRuntimeChecks=\"0\"\r\n" );
-		fprintf ( OUT, "\t\t\t\tRuntimeLibrary=\"%d\"\r\n", debug ? 1 : 5 );	// 1=/MTd 5=/MT
+		fprintf ( OUT, "\t\t\t\tRuntimeLibrary=\"%d\"\r\n", debug ? 3 : 2 );	// 3=/MDd 2=/MD
 		fprintf ( OUT, "\t\t\t\tBufferSecurityCheck=\"FALSE\"\r\n" );
 		fprintf ( OUT, "\t\t\t\tEnableFunctionLevelLinking=\"FALSE\"\r\n" );
 
