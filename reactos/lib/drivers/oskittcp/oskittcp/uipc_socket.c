9722b6109542ea0ecd094f3103c0aa1bb8b81ee7
Cameron Gutman <aicommander@gmail.com>
2009-06-27 06:59:22 +0000

    - The timeout value passed to tsleep is in milliseconds and so_linger is in seconds so convert to milliseconds
    svn path=/trunk/; revision=41632

--- reactos/lib/drivers/oskittcp/oskittcp/uipc_socket.c
+++ reactos/lib/drivers/oskittcp/oskittcp/uipc_socket.c
@@ -190,7 +190,7 @@ soclose(so)
 				goto drop;
 			while (so->so_state & SS_ISCONNECTED) {
 				error = tsleep((caddr_t)&so->so_timeo,
-				    PSOCK | PCATCH, netcls, so->so_linger);
+				    PSOCK | PCATCH, netcls, so->so_linger * 1000);
 				if (error)
 					break;
 			}
