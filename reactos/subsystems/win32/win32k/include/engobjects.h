4d9e5830a126ea66f709aa8cddf1f478ef9db169
Colin Finck <colin@reactos.org>
2009-06-29 18:24:35 +0000

    SVN maintenance: Remove some svn:keywords properties
    svn path=/trunk/; revision=41694

--- reactos/subsystems/win32/win32k/include/engobjects.h
+++ reactos/subsystems/win32/win32k/include/engobjects.h
@@ -16,7 +16,6 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-/* $Id$
  *
  * COPYRIGHT:         See COPYING in the top level directory
  * PROJECT:           ReactOS kernel
