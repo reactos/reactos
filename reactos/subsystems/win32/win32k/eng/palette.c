2120170004f1fde06c1543852d5d0fb995deedb3
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-07-03 23:06:04 +0000

    [FORMATTING] palette.c: remove extended header, fix indentation, no code change
    svn path=/trunk/; revision=41760

--- reactos/subsystems/win32/win32k/eng/palette.c
+++ reactos/subsystems/win32/win32k/eng/palette.c
@@ -1,30 +1,9 @@
 /*
- *  ReactOS W32 Subsystem
- *  Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003 ReactOS Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-/* $Id$
- *
  * COPYRIGHT:         See COPYING in the top level directory
  * PROJECT:           ReactOS kernel
  * PURPOSE:           GDI Palette Functions
  * FILE:              subsys/win32k/eng/palette.c
  * PROGRAMER:         Jason Filby
- * REVISION HISTORY:
- *                 11/7/1999: Created
  */
 
 #include <w32k.h>
@@ -35,79 +14,88 @@
 //
 //
 //
-VOID FASTCALL
+VOID
+FASTCALL
 ColorCorrection(PPALETTE PalGDI, PPALETTEENTRY PaletteEntry, ULONG Colors)
 {
-   PPDEVOBJ ppdev = (PPDEVOBJ)PalGDI->hPDev;
+    PPDEVOBJ ppdev = (PPDEVOBJ)PalGDI->hPDev;
 
-   if (!ppdev) return;
+    if (!ppdev) return;
 
-   if (ppdev->flFlags & PDEV_GAMMARAMP_TABLE)
-   {
-      INT i;
-      PGAMMARAMP GammaRamp = (PGAMMARAMP)ppdev->pvGammaRamp;
-      for ( i = 0; i < Colors; i++)
-      {
-          PaletteEntry[i].peRed   += GammaRamp->Red[i];
-          PaletteEntry[i].peGreen += GammaRamp->Green[i];
-          PaletteEntry[i].peBlue  += GammaRamp->Blue[i];
-      }
-   }
-   return;
+    if (ppdev->flFlags & PDEV_GAMMARAMP_TABLE)
+    {
+        INT i;
+        PGAMMARAMP GammaRamp = (PGAMMARAMP)ppdev->pvGammaRamp;
+        for ( i = 0; i < Colors; i++)
+        {
+            PaletteEntry[i].peRed   += GammaRamp->Red[i];
+            PaletteEntry[i].peGreen += GammaRamp->Green[i];
+            PaletteEntry[i].peBlue  += GammaRamp->Blue[i];
+        }
+    }
+    return;
 }
 
 /*
  * @implemented
  */
-HPALETTE APIENTRY
-EngCreatePalette(ULONG Mode, ULONG NumColors, ULONG *Colors,
-                 ULONG Red, ULONG Green, ULONG Blue)
+HPALETTE
+APIENTRY
+EngCreatePalette(
+    ULONG Mode,
+    ULONG NumColors,
+    ULONG *Colors,
+    ULONG Red,
+    ULONG Green,
+    ULONG Blue)
 {
-   HPALETTE Palette;
+    HPALETTE Palette;
 
-   Palette = PALETTE_AllocPalette(Mode, NumColors, Colors, Red, Green, Blue);
-   if (Palette != NULL)
-   {
-      GDIOBJ_SetOwnership(Palette, NULL);
-   }
+    Palette = PALETTE_AllocPalette(Mode, NumColors, Colors, Red, Green, Blue);
+    if (Palette != NULL)
+    {
+        GDIOBJ_SetOwnership(Palette, NULL);
+    }
 
-   return Palette;
+    return Palette;
 }
 
 /*
  * @implemented
  */
-BOOL APIENTRY
+BOOL
+APIENTRY
 EngDeletePalette(IN HPALETTE Palette)
 {
-   GDIOBJ_SetOwnership(Palette, PsGetCurrentProcess());
+    GDIOBJ_SetOwnership(Palette, PsGetCurrentProcess());
 
-   return PALETTE_FreePaletteByHandle(Palette);
+    return PALETTE_FreePaletteByHandle(Palette);
 }
 
 /*
  * @implemented
  */
-ULONG APIENTRY
+ULONG
+APIENTRY
 PALOBJ_cGetColors(PALOBJ *PalObj, ULONG Start, ULONG Colors, ULONG *PaletteEntry)
 {
-   PALETTE *PalGDI;
+    PALETTE *PalGDI;
 
-   PalGDI = (PALETTE*)PalObj;
+    PalGDI = (PALETTE*)PalObj;
    /* PalGDI = (PALETTE*)AccessInternalObjectFromUserObject(PalObj); */
 
-   if (Start >= PalGDI->NumColors)
-      return 0;
+    if (Start >= PalGDI->NumColors)
+        return 0;
 
-   Colors = min(Colors, PalGDI->NumColors - Start);
+    Colors = min(Colors, PalGDI->NumColors - Start);
 
-   /* NOTE: PaletteEntry ULONGs are in the same order as PALETTEENTRY. */
-   RtlCopyMemory(PaletteEntry, PalGDI->IndexedColors + Start, sizeof(ULONG) * Colors);
+    /* NOTE: PaletteEntry ULONGs are in the same order as PALETTEENTRY. */
+    RtlCopyMemory(PaletteEntry, PalGDI->IndexedColors + Start, sizeof(ULONG) * Colors);
 
-   if (PalGDI->Mode & PAL_GAMMACORRECTION)
-      ColorCorrection(PalGDI, (PPALETTEENTRY)PaletteEntry, Colors);
+    if (PalGDI->Mode & PAL_GAMMACORRECTION)
+        ColorCorrection(PalGDI, (PPALETTEENTRY)PaletteEntry, Colors);
 
-   return Colors;
+    return Colors;
 }
 
 /* EOF */
