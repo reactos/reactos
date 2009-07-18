cf1ec1bc3bc584e3eff873075a6bcf390ff914b0
Aleksey Bragin <aleksey@reactos.org>
2009-07-18 09:26:43 +0000

    - Remove unneeded stuff.
    svn path=/branches/arwinss/; revision=42028

--- reactos/subsystems/win32/win32k/include/request.h
+++ reactos/subsystems/win32/win32k/include/request.h
@@ -21,92 +21,12 @@
 #ifndef __WINE_SERVER_REQUEST_H
 #define __WINE_SERVER_REQUEST_H
 
-#include <assert.h>
-
-#include "thread.h"
-#include "wine/server_protocol.h"
-
 /* max request length */
 #define MAX_REQUEST_LENGTH  8192
 
 /* request handler definition */
 #define DECL_HANDLER(name) \
     void req_##name( const struct name##_request *req, struct name##_reply *reply )
-
-/* request functions */
-
-#ifdef __GNUC__
-extern void fatal_protocol_error( struct thread *thread,
-                                  const char *err, ... ) __attribute__((format (printf,2,3)));
-extern void fatal_protocol_perror( struct thread *thread,
-                                   const char *err, ... ) __attribute__((format (printf,2,3)));
-extern void fatal_error( const char *err, ... )  __attribute__((noreturn,format(printf,1,2)));
-extern void fatal_perror( const char *err, ... )  __attribute__((noreturn,format(printf,1,2)));
-#else
-extern void fatal_protocol_error( struct thread *thread, const char *err, ... );
-extern void fatal_protocol_perror( struct thread *thread, const char *err, ... );
-extern void fatal_error( const char *err, ... );
-extern void fatal_perror( const char *err, ... );
-#endif
-
-extern const char *get_config_dir(void);
-extern void *set_reply_data_size( data_size_t size );
-extern int receive_fd( struct process *process );
-extern int send_client_fd( struct process *process, int fd, obj_handle_t handle );
-extern void read_request( struct thread *thread );
-extern void write_reply( struct thread *thread );
-extern unsigned int get_tick_count(void);
-extern void open_master_socket(void);
-extern void close_master_socket( timeout_t timeout );
-extern void shutdown_master_socket(void);
-extern int wait_for_lock(void);
-extern int kill_lock_owner( int sig );
-extern int server_dir_fd, config_dir_fd;
-
-extern void trace_request(void);
-extern void trace_reply( enum request req, const union generic_reply *reply );
-
-/* get the request vararg data */
-static inline const void *get_req_data(void)
-{
-    return current->req_data;
-}
-
-/* get the request vararg size */
-static inline data_size_t get_req_data_size(void)
-{
-    return current->req.request_header.request_size;
-}
-
-/* get the request vararg as unicode string */
-static inline void get_req_unicode_str( struct unicode_str *str )
-{
-    str->str = get_req_data();
-    str->len = (get_req_data_size() / sizeof(WCHAR)) * sizeof(WCHAR);
-}
-
-/* get the reply maximum vararg size */
-static inline data_size_t get_reply_max_size(void)
-{
-    return current->req.request_header.reply_size;
-}
-
-/* allocate and fill the reply data */
-static inline void *set_reply_data( const void *data, data_size_t size )
-{
-    void *ret = set_reply_data_size( size );
-    if (ret) memcpy( ret, data, size );
-    return ret;
-}
-
-/* set the reply data pointer directly (will be freed by request code) */
-static inline void set_reply_data_ptr( void *data, data_size_t size )
-{
-    assert( size <= get_reply_max_size() );
-    current->reply_size = size;
-    current->reply_data = data;
-}
-
 
 /* Everything below this line is generated automatically by tools/make_requests */
 /* ### make_requests begin ### */
