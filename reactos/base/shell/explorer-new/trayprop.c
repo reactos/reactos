54d58cdb656055df137ab3774c1883d2955a67c4
Ged Murphy <gedmurphy@reactos.org>
2009-07-23 21:35:52 +0000

    Draw the appropriate taskbar mockup image in the dialog when the user clicks the checkboxes
    svn path=/trunk/; revision=42165

--- reactos/base/shell/explorer-new/trayprop.c
+++ reactos/base/shell/explorer-new/trayprop.c
@@ -20,18 +20,142 @@
 
 #include <precomp.h>
 
+typedef struct _PROPSHEET_INFO
+{
+    HWND hTaskbarWnd;
+    HWND hStartWnd;
+    HWND hNotiWnd;
+    HWND hToolWnd;
+
+    HBITMAP hTaskbarBitmap;
+} PROPSHEET_INFO, *PPROPSHEET_INFO;
+
+
+static BOOL
+UpdateTaskbarBitmap(PPROPSHEET_INFO pPropInfo)
+{
+    HWND hwndLock, hwndHide, hwndGroup, hwndShowQL;
+    HWND hwndBitmap;
+    BOOL bLock, bHide, bGroup, bShowQL;
+    LPTSTR lpImageName = NULL;
+    RECT BitmapRect;
+    BOOL bRet = FALSE;
+
+    hwndLock = GetDlgItem(pPropInfo->hTaskbarWnd, IDC_TASKBARPROP_LOCK);
+    hwndHide = GetDlgItem(pPropInfo->hTaskbarWnd, IDC_TASKBARPROP_HIDE);
+    hwndGroup = GetDlgItem(pPropInfo->hTaskbarWnd, IDC_TASKBARPROP_GROUP);
+    hwndShowQL = GetDlgItem(pPropInfo->hTaskbarWnd, IDC_TASKBARPROP_SHOWQL);
+
+    if (hwndLock && hwndHide && hwndGroup && hwndShowQL)
+    {
+        bLock = (SendMessage(hwndLock, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        bHide = (SendMessage(hwndHide, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        bGroup = (SendMessage(hwndGroup, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        bShowQL = (SendMessage(hwndShowQL, BM_GETCHECK, 0, 0) == BST_CHECKED);
+
+        if (bHide)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_AUTOHIDE);
+        else if (bLock  && bGroup  && bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_LOCK_GROUP_QL);
+        else if (bLock  && !bGroup && !bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_LOCK_NOGROUP_NOQL);
+        else if (bLock  && bGroup  && !bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_LOCK_GROUP_NOQL);
+        else if (bLock  && !bGroup && bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_LOCK_NOGROUP_QL);
+        else if (!bLock && !bGroup && !bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_NOLOCK_NOGROUP_NOQL);
+        else if (!bLock && bGroup  && !bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_NOLOCK_GROUP_NOQL);
+        else if (!bLock && !bGroup && bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_NOLOCK_NOGROUP_QL);
+        else if (!bLock && bGroup  && bShowQL)
+            lpImageName = MAKEINTRESOURCE(IDB_TASKBARPROP_NOLOCK_GROUP_QL);
+
+        if (lpImageName)
+        {
+            if (pPropInfo->hTaskbarBitmap)
+            {
+                DeleteObject(pPropInfo->hTaskbarBitmap);
+            }
+
+            pPropInfo->hTaskbarBitmap = LoadImage(hExplorerInstance,
+                                                  lpImageName,
+                                                  IMAGE_BITMAP,
+                                                  0,
+                                                  0,
+                                                  LR_DEFAULTCOLOR);
+            if (pPropInfo->hTaskbarBitmap)
+            {
+                hwndBitmap = GetDlgItem(pPropInfo->hTaskbarWnd,
+                                        IDC_TASKBARPROP_TASKBARBITMAP);
+                if (hwndBitmap)
+                {
+                    SendMessage(hwndBitmap,
+                                STM_SETIMAGE,
+                                IMAGE_BITMAP,
+                                (LPARAM)pPropInfo->hTaskbarBitmap);
+                }
+            }
+        }
+    }
+
+    return bRet;
+}
+
+static VOID
+OnCreateTaskbarPage(HWND hwnd,
+                    PPROPSHEET_INFO pPropInfo)
+{
+    SetWindowLongPtr(hwnd,
+                     GWLP_USERDATA,
+                     (LONG_PTR)pPropInfo);
+
+    pPropInfo->hTaskbarWnd = hwnd;
+
+    // FIXME: check buttons
+
+    UpdateTaskbarBitmap(pPropInfo);
+}
+
 INT_PTR CALLBACK
 TaskbarPageProc(HWND hwndDlg,
                 UINT uMsg,
                 WPARAM wParam,
                 LPARAM lParam)
 {
+    PPROPSHEET_INFO pPropInfo;
+
+    /* Get the window context */
+    pPropInfo = (PPROPSHEET_INFO)GetWindowLongPtrW(hwndDlg,
+                                                   GWLP_USERDATA);
+    if (pPropInfo == NULL && uMsg != WM_INITDIALOG)
+    {
+        goto HandleDefaultMessage;
+    }
+
     switch (uMsg)
     {
         case WM_INITDIALOG:
+            OnCreateTaskbarPage(hwndDlg, (PPROPSHEET_INFO)lParam);
             break;
 
-        case WM_DESTROY:
+        case WM_COMMAND:
+            switch(LOWORD(wParam))
+            {
+                case IDC_TASKBARPROP_LOCK:
+                case IDC_TASKBARPROP_HIDE:
+                case IDC_TASKBARPROP_GROUP:
+                case IDC_TASKBARPROP_SHOWQL:
+                    if (HIWORD(wParam) == BN_CLICKED)
+                    {
+                        UpdateTaskbarBitmap(pPropInfo);
+
+                        /* Enable the 'Apply' button */
+                        PropSheet_Changed(GetParent(hwndDlg), hwndDlg);
+                    }
+                    break;
+            }
             break;
 
         case WM_NOTIFY:
@@ -49,6 +173,17 @@ TaskbarPageProc(HWND hwndDlg,
 
             break;
         }
+
+        case WM_DESTROY:
+            if (pPropInfo->hTaskbarBitmap)
+            {
+                DeleteObject(pPropInfo->hTaskbarBitmap);
+            }
+            break;
+
+HandleDefaultMessage:
+        default:
+            return FALSE;
     }
 
     return FALSE;
@@ -161,13 +296,17 @@ ToolbarsPageProc(HWND hwndDlg,
 
 
 static VOID
-InitPropSheetPage(PROPSHEETPAGE *psp, WORD idDlg, DLGPROC DlgProc)
+InitPropSheetPage(PROPSHEETPAGE *psp,
+                  WORD idDlg,
+                  DLGPROC DlgProc,
+                  LPARAM lParam)
 {
     ZeroMemory(psp, sizeof(PROPSHEETPAGE));
     psp->dwSize = sizeof(PROPSHEETPAGE);
     psp->dwFlags = PSP_DEFAULT;
     psp->hInstance = hExplorerInstance;
     psp->pszTemplate = MAKEINTRESOURCE(idDlg);
+    psp->lParam = lParam;
     psp->pfnDlgProc = DlgProc;
 }
 
@@ -175,9 +314,19 @@ InitPropSheetPage(PROPSHEETPAGE *psp, WORD idDlg, DLGPROC DlgProc)
 HWND
 DisplayTrayProperties(ITrayWindow *Tray)
 {
+    PPROPSHEET_INFO pPropInfo;
     PROPSHEETHEADER psh;
     PROPSHEETPAGE psp[4];
     TCHAR szCaption[256];
+    HWND hwnd = 0;
+
+    pPropInfo = (PPROPSHEET_INFO)HeapAlloc(hProcessHeap,
+                                           HEAP_ZERO_MEMORY,
+                                           sizeof(PROPSHEET_INFO));
+    if (!pPropInfo)
+    {
+        return NULL;
+    }
 
     if (!LoadString(hExplorerInstance,
                     IDS_TASKBAR_STARTMENU_PROP_CAPTION,
@@ -198,10 +347,17 @@ DisplayTrayProperties(ITrayWindow *Tray)
     psh.nStartPage = 0;
     psh.ppsp = psp;
 
-    InitPropSheetPage(&psp[0], IDD_TASKBARPROP_TASKBAR, (DLGPROC)TaskbarPageProc);
-    InitPropSheetPage(&psp[1], IDD_TASKBARPROP_STARTMENU, (DLGPROC)StartMenuPageProc);
-    InitPropSheetPage(&psp[2], IDD_TASKBARPROP_NOTIFICATION, (DLGPROC)NotificationPageProc);
-    InitPropSheetPage(&psp[3], IDD_TASKBARPROP_TOOLBARS, (DLGPROC)ToolbarsPageProc);
+    InitPropSheetPage(&psp[0], IDD_TASKBARPROP_TASKBAR, (DLGPROC)TaskbarPageProc, (LPARAM)pPropInfo);
+    InitPropSheetPage(&psp[1], IDD_TASKBARPROP_STARTMENU, (DLGPROC)StartMenuPageProc, (LPARAM)pPropInfo);
+    InitPropSheetPage(&psp[2], IDD_TASKBARPROP_NOTIFICATION, (DLGPROC)NotificationPageProc, (LPARAM)pPropInfo);
+    InitPropSheetPage(&psp[3], IDD_TASKBARPROP_TOOLBARS, (DLGPROC)ToolbarsPageProc, (LPARAM)pPropInfo);
 
-    return (HWND)PropertySheet(&psh);
+    PropertySheet(&psh);
+
+    HeapFree(hProcessHeap,
+             0,
+             pPropInfo);
+
+    // FIXME: return the HWND
+    return NULL;
 }
