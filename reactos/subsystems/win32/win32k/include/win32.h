9d2a032c20ccda59c21c611c7efac35d347f599e
James Tabor <james.tabor@reactos.org>
2009-08-12 06:54:31 +0000

    - Move W32 Process flags to the proper place.
    svn path=/trunk/; revision=42630

--- reactos/subsystems/win32/win32k/include/win32.h
+++ reactos/subsystems/win32/win32k/include/win32.h
@@ -1,6 +1,36 @@
 #ifndef __INCLUDE_NAPI_WIN32_H
 #define __INCLUDE_NAPI_WIN32_H
 
+/* W32PROCESS flags */
+#define W32PF_CONSOLEAPPLICATION      0x00000001
+#define W32PF_FORCEOFFFEEDBACK        0x00000002
+#define W32PF_STARTGLASS              0x00000004
+#define W32PF_WOW                     0x00000008
+#define W32PF_READSCREENACCESSGRANTED 0x00000010
+#define W32PF_INITIALIZED             0x00000020
+#define W32PF_APPSTARTING             0x00000040
+#define W32PF_WOW64                   0x00000080
+#define W32PF_ALLOWFOREGROUNDACTIVATE 0x00000100
+#define W32PF_OWNDCCLEANUP            0x00000200
+#define W32PF_SHOWSTARTGLASSCALLED    0x00000400
+#define W32PF_FORCEBACKGROUNDPRIORITY 0x00000800
+#define W32PF_TERMINATED              0x00001000
+#define W32PF_CLASSESREGISTERED       0x00002000
+#define W32PF_THREADCONNECTED         0x00004000
+#define W32PF_PROCESSCONNECTED        0x00008000
+#define W32PF_WAKEWOWEXEC             0x00010000
+#define W32PF_WAITFORINPUTIDLE        0x00020000
+#define W32PF_IOWINSTA                0x00040000
+#define W32PF_CONSOLEFOREGROUND       0x00080000
+#define W32PF_OLELOADED               0x00100000
+#define W32PF_SCREENSAVER             0x00200000
+#define W32PF_IDLESCREENSAVER         0x00400000
+#define W32PF_ICONTITLEREGISTERED     0x10000000
+// ReactOS
+#define W32PF_NOWINDOWGHOSTING       (0x01000000)
+#define W32PF_MANUALGUICHECK         (0x02000000)
+#define W32PF_CREATEDWINORDC         (0x04000000)
+
 extern BOOL ClientPfnInit;
 extern HINSTANCE hModClient;
 extern HANDLE hModuleWin;    // This Win32k Instance.
@@ -95,8 +125,10 @@ typedef struct _PROCESSINFO
 {
   W32PROCESS;
 
-  PCLS                pclsPrivateList;
-  PCLS                pclsPublicList;
+  PCLS pclsPrivateList;
+  PCLS pclsPublicList;
+
+  DWORD dwRegisteredClasses;
   /* ReactOS */
   LIST_ENTRY ClassList;
   LIST_ENTRY MenuListHead;
