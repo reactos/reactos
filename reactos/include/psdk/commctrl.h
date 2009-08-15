e75501547e844565736b627707f999368af60662
Stefan Ginsberg <stefanginsberg@gmail.com>
2009-08-15 20:11:04 +0000

    - Attempt to fix user32 build after 42706 -- user32 needs those undocumented TB_* too
    svn path=/trunk/; revision=42714

--- reactos/include/psdk/commctrl.h
+++ reactos/include/psdk/commctrl.h
@@ -1231,14 +1231,14 @@ static const WCHAR TOOLBARCLASSNAMEW[] = { 'T','o','o','l','b','a','r',
 #define TB_GETSTRING             WINELIB_NAME_AW(TB_GETSTRING)
 
 /* undocumented messages in Toolbar */
-#ifdef __WINESRC__
+/* #ifdef __WINESRC__ */
 #define TB_UNKWN45D              (WM_USER+93)
 #define TB_SETHOTITEM2           (WM_USER+94)
 #define TB_SETLISTGAP            (WM_USER+96)
 #define TB_GETIMAGELISTCOUNT     (WM_USER+98)
 #define TB_GETIDEALSIZE          (WM_USER+99)
 #define TB_UNKWN464              (WM_USER+100)
-#endif
+/* #endif */
 
 #define TB_GETMETRICS            (WM_USER+101)
 #define TB_SETMETRICS            (WM_USER+102)
