302ad7c74feed53732d3d4f42048218858d51e78
Michael Martin <michael.martin@reactos.org>
2009-09-21 12:49:25 +0000

    LogfWriteData: Add missing LeaveCriticalSection before returning from function. Found by Amine Khaldi
    svn path=/trunk/; revision=43102

--- reactos/base/services/eventlog/file.c
+++ reactos/base/services/eventlog/file.c
@@ -704,6 +704,7 @@ BOOL LogfWriteData(PLOGFILE LogFile, DWORD BufSize, PBYTE Buffer)
 
     if (!GetFileSizeEx(LogFile->hFile, &logFileSize))
     {
+        LeaveCriticalSection(&LogFile->cs);
         return FALSE;
     }
 
@@ -751,6 +752,7 @@ BOOL LogfWriteData(PLOGFILE LogFile, DWORD BufSize, PBYTE Buffer)
             if (RecBuf->Reserved != LOGFILE_SIGNATURE)
             {
                 DPRINT1("LogFile corrupt!\n");
+                LeaveCriticalSection(&LogFile->cs);
                 return FALSE;
             }
 
