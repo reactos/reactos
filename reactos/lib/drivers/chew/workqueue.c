27f05c27a80bd3d0bc7ab991776f10367fc081ea
Dmitry Gorbachev <gorbachev@reactos.org>
2009-10-12 03:27:45 +0000

    [CHEW] - Fix formatting, comments. - Delete an empty directory.
    svn path=/trunk/; revision=43386

--- reactos/lib/drivers/chew/workqueue.c
+++ reactos/lib/drivers/chew/workqueue.c
@@ -1,15 +1,17 @@
 /*
  * COPYRIGHT:       See COPYING in the top level directory
  * PROJECT:         ReactOS kernel
- * FILE:            drivers/lib/chew/workqueue.c
+ * FILE:            lib/drivers/chew/workqueue.c
  * PURPOSE:         Common Highlevel Executive Worker
  *
  * PROGRAMMERS:     arty (ayerkes@speakeasy.net)
  */
+
 #include <ntddk.h>
 #include <chew/chew.h>
 
 #define NDEBUG
+//#include <debug.h>
 
 #define FOURCC(w,x,y,z) (((w) << 24) | ((x) << 16) | ((y) << 8) | (z))
 #define CHEW_TAG FOURCC('C','H','E','W')
@@ -19,64 +21,73 @@ LIST_ENTRY     WorkQueue;
 KSPIN_LOCK     WorkQueueLock;
 KEVENT         WorkQueueClear;
 
-typedef struct _WORK_ITEM {
+typedef struct _WORK_ITEM
+{
     LIST_ENTRY Entry;
     PIO_WORKITEM WorkItem;
-    VOID (*Worker)( PVOID WorkerContext );
+    VOID (*Worker)(PVOID WorkerContext);
     PVOID WorkerContext;
 } WORK_ITEM, *PWORK_ITEM;
 
-VOID ChewInit( PDEVICE_OBJECT DeviceObject ) {
+VOID ChewInit(PDEVICE_OBJECT DeviceObject)
+{
     WorkQueueDevice = DeviceObject;
-    InitializeListHead( &WorkQueue );
-    KeInitializeSpinLock( &WorkQueueLock );
+    InitializeListHead(&WorkQueue);
+    KeInitializeSpinLock(&WorkQueueLock);
     KeInitializeEvent(&WorkQueueClear, NotificationEvent, TRUE);
 }
 
-VOID ChewShutdown() {
+VOID ChewShutdown(VOID)
+{
     KeWaitForSingleObject(&WorkQueueClear, Executive, KernelMode, FALSE, NULL);
 }
 
-VOID NTAPI ChewWorkItem( PDEVICE_OBJECT DeviceObject, PVOID ChewItem ) {
+VOID NTAPI ChewWorkItem(PDEVICE_OBJECT DeviceObject, PVOID ChewItem)
+{
     PWORK_ITEM WorkItem = ChewItem;
     KIRQL OldIrql;
 
-    WorkItem->Worker( WorkItem->WorkerContext );
+    WorkItem->Worker(WorkItem->WorkerContext);
 
-    IoFreeWorkItem( WorkItem->WorkItem );
+    IoFreeWorkItem(WorkItem->WorkItem);
 
     KeAcquireSpinLock(&WorkQueueLock, &OldIrql);
     RemoveEntryList(&WorkItem->Entry);
 
     if (IsListEmpty(&WorkQueue))
         KeSetEvent(&WorkQueueClear, 0, FALSE);
+
     KeReleaseSpinLock(&WorkQueueLock, OldIrql);
 
     ExFreePoolWithTag(WorkItem, CHEW_TAG);
 }
-    
-BOOLEAN ChewCreate
-( VOID (*Worker)( PVOID ), PVOID WorkerContext ) {
-    PWORK_ITEM Item;
-    Item = ExAllocatePoolWithTag
-	    ( NonPagedPool, 
-	      sizeof( WORK_ITEM ), 
-	      CHEW_TAG );
-	
-    if( Item ) {
-	Item->WorkItem = IoAllocateWorkItem( WorkQueueDevice );
-	if( !Item->WorkItem ) {
-	    ExFreePool( Item );
-	    return FALSE;
-	}
-	Item->Worker = Worker;
-	Item->WorkerContext = WorkerContext;
-	ExInterlockedInsertTailList( &WorkQueue, &Item->Entry, &WorkQueueLock );
-	KeResetEvent(&WorkQueueClear);
-	IoQueueWorkItem( Item->WorkItem, ChewWorkItem, DelayedWorkQueue, Item );
 
-	return TRUE;
-    } else {
-	return FALSE;
+BOOLEAN ChewCreate(VOID (*Worker)(PVOID), PVOID WorkerContext)
+{
+    PWORK_ITEM Item;
+    Item = ExAllocatePoolWithTag(NonPagedPool, 
+                                 sizeof(WORK_ITEM),
+                                 CHEW_TAG);
+
+    if (Item)
+    {
+        Item->WorkItem = IoAllocateWorkItem(WorkQueueDevice);
+        if (!Item->WorkItem)
+        {
+            ExFreePool(Item);
+            return FALSE;
+        }
+
+        Item->Worker = Worker;
+        Item->WorkerContext = WorkerContext;
+        ExInterlockedInsertTailList(&WorkQueue, &Item->Entry, &WorkQueueLock);
+        KeResetEvent(&WorkQueueClear);
+        IoQueueWorkItem(Item->WorkItem, ChewWorkItem, DelayedWorkQueue, Item);
+
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
     }
 }
