b40f0f355b437d734bfb8184a0c613e7e0c491e5
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-10-14 16:56:54 +0000

    [NDK] remove PORT_MAXIMUM_MESSAGE_LENGTH, it's defined in wdm.h
    svn path=/trunk/; revision=43456

--- reactos/include/ndk/lpctypes.h
+++ reactos/include/ndk/lpctypes.h
@@ -32,11 +32,6 @@ Author:
     (((ULONG)(x)+(s)-1) & ~((ULONG)(s)-1))
 
 //
-// Maximum message size that can be sent through an LPC Port without a section
-//
-#define PORT_MAXIMUM_MESSAGE_LENGTH     256
-
-//
 // Port Object Access Masks
 //
 #define PORT_CONNECT                    0x1
