c236dd31f988cc893bf57464566be44b341a2e40
Timo Kreuzer <timo.kreuzer@reactos.org>
2009-10-14 17:05:51 +0000

    Fix definition of PORT_MAXIMUM_MESSAGE_LENGTH for 64 bit
    svn path=/trunk/; revision=43460

--- reactos/include/ndk/lpctypes.h
+++ reactos/include/ndk/lpctypes.h
@@ -83,7 +83,11 @@ typedef enum _PORT_INFORMATION_CLASS
 //
 // Maximum message size that can be sent through an LPC Port without a section
 //
-#define PORT_MAXIMUM_MESSAGE_LENGTH     256
+#ifdef _WIN64
+#define PORT_MAXIMUM_MESSAGE_LENGTH 512
+#else
+#define PORT_MAXIMUM_MESSAGE_LENGTH 256
+#endif
 
 //
 // Portable LPC Types for 32/64-bit compatibility
