5b1a765b54430cd062707ab2ef0483bf8e2e09a1
Gregor Schneider <grschneider@gmail.com>
2009-10-25 18:49:08 +0000

    [win32k] Stub out page to device space conversion, fixes three more gdi32 mapping winetests
    svn path=/trunk/; revision=43749

--- reactos/subsystems/win32/win32k/objects/coord.c
+++ reactos/subsystems/win32/win32k/objects/coord.c
@@ -202,12 +202,16 @@ NtGdiGetTransform(
                 MatrixS2XForm(XForm, &dc->dclevel.mxWorldToDevice);
                 break;
 
+            case GdiPageSpaceToDeviceSpace:
+                DPRINT1("Page space -> device space is unsupported!\n");
+                break;
+
             case GdiDeviceSpaceToWorldSpace:
                 MatrixS2XForm(XForm, &dc->dclevel.mxDeviceToWorld);
                 break;
 
             default:
-                DPRINT1("Unknown or unsupported transform %lu\n", iXform);
+                DPRINT1("Unknown transform %lu\n", iXform);
                 Status = STATUS_INVALID_PARAMETER;
                 break;
         }
