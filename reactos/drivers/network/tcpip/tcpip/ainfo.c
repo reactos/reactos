5a3d389400d0673c37946027eaf301240063790b
Cameron Gutman <aicommander@gmail.com>
2009-10-26 03:19:30 +0000

    - Forgot this file
    svn path=/trunk/; revision=43769

---
+++ reactos/drivers/network/tcpip/tcpip/ainfo.c
@@ -0,0 +1,40 @@
+/*
+ * COPYRIGHT:   See COPYING in the top level directory
+ * PROJECT:     ReactOS TCP/IP protocol driver
+ * FILE:        tcpip/ainfo.c
+ * PURPOSE:     Per-socket information.
+ * PROGRAMMER:  Cameron Gutman
+ */
+
+#include "precomp.h"
+
+TDI_STATUS SetAddressFileInfo(TDIObjectID *ID,
+                              PADDRESS_FILE AddrFile,
+                              PVOID Buffer,
+                              UINT BufferSize)
+{
+    switch (ID->toi_id)
+    {
+      case AO_OPTION_TTL:
+         if (BufferSize < sizeof(UCHAR))
+             return TDI_INVALID_PARAMETER;
+
+         AddrFile->TTL = *((PUCHAR)Buffer);
+         return TDI_SUCCESS;
+
+      default:
+         DbgPrint("Unimplemented option %x\n", ID->toi_id);
+
+         return TDI_INVALID_REQUEST;
+    }
+}
+
+TDI_STATUS GetAddressFileInfo(TDIObjectID *ID,
+                              PADDRESS_FILE AddrFile,
+                              PVOID Buffer,
+                              PUINT BufferSize)
+{
+    UNIMPLEMENTED
+
+    return TDI_INVALID_REQUEST;
+}
