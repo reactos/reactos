071909e1df1c5e88eb78f51ae92749f01fe15fe5
Samuel Serapion <samuel.serapion@gmail.com>
2009-12-01 03:17:54 +0000

    fix build.
    svn path=/branches/ros-amd64-bringup/; revision=44338

--- reactos/include/psdk/winnt.h
+++ reactos/include/psdk/winnt.h
@@ -3249,7 +3249,7 @@ typedef struct _SINGLE_LIST_ENTRY {
 #define _SLIST_HEADER_
 
 #if defined(_WIN64)
-typedef struct DECLSPEC_ALIGN(16) _SLIST_ENTRY *PSLIST_ENTRY;
+typedef struct _SLIST_ENTRY *PSLIST_ENTRY;
 typedef struct DECLSPEC_ALIGN(16) _SLIST_ENTRY {
 	PSLIST_ENTRY Next;
 } SLIST_ENTRY;
