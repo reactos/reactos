628ba3758abf105cc94e5dfab482f5901b6c4951
Ged Murphy <gedmurphy@reactos.org>
2009-12-04 15:09:44 +0000

    Remove the devcpp backend.
    svn path=/trunk/; revision=44403

--- reactos/tools/rbuild/rbuild.cpp
+++ reactos/tools/rbuild/rbuild.cpp
@@ -388,6 +388,8 @@ main ( int argc, char** argv )
 		if ( RootXmlFile.length () == 0 )
 			throw MissingArgumentException ( "-r" );
 
+		DWORD start = GetTickCount();
+
 		string projectFilename ( RootXmlFile );
 
 		printf ( "Reading build files..." );
@@ -401,6 +403,9 @@ main ( int argc, char** argv )
 
 		project.ExecuteInvocations ();
 		project.GetBackend().Process();
+
+		DWORD end = GetTickCount();
+		printf("time - %lu.%d\n", (end - start) / 1000, (end - start) % 1000);
 
 		return 0;
 	}
