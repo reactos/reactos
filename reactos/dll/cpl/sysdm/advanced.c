23294457ab97826d77384f964ba5e03830b3f588
Ged Murphy <gedmurphy@reactos.org>
2009-12-09 16:34:05 +0000

    [SYSDM] - Add a setting to the system control panel applet which allows users to force reactos to report as a workstation. - This is a usermode change only, it will not alter the kernel nor anything calling kernel apis (e.g. RtlGetVersion) - This change should allow applications which don't allow installation on non-workstation version to install. See issue #5003 for more details.
    svn path=/trunk/; revision=44487

--- reactos/dll/cpl/sysdm/advanced.c
+++ reactos/dll/cpl/sysdm/advanced.c
@@ -4,13 +4,111 @@
  * FILE:        dll/cpl/sysdm/advanced.c
  * PURPOSE:     Memory, start-up and profiles settings
  * COPYRIGHT:   Copyright Thomas Weidenmueller <w3seek@reactos.org>
-                Copyright 2006 Ged Murphy <gedmurphy@gmail.com>
+                Copyright 2006 - 2009 Ged Murphy <gedmurphy@reactos.org>
  *
  */
 
 #include "precomp.h"
 
 static TCHAR BugLink[] = _T("http://www.reactos.org/bugzilla");
+static TCHAR ReportAsWorkstationKey[] = _T("SYSTEM\\CurrentControlSet\\Control\\ReactOS\\Settings\\Version");
+
+
+static VOID
+OnOK(HWND hwndDlg)
+{
+    HKEY hKey;
+    DWORD ReportAsWorkstation;
+
+    ReportAsWorkstation = (SendDlgItemMessageW(hwndDlg,
+                                               IDC_REPORTASWORKSTATION,
+                                               BM_GETCHECK,
+                                               0,
+                                               0) == BST_CHECKED);
+
+    if (RegCreateKeyEx(HKEY_LOCAL_MACHINE,
+                       ReportAsWorkstationKey,
+                       0,
+                       NULL,
+                       0,
+                       KEY_WRITE,
+                       NULL,
+                       &hKey,
+                       NULL) == ERROR_SUCCESS)
+    {
+        RegSetValueEx(hKey,
+                      _T("ReportAsWorkstation"),
+                      0,
+                      REG_DWORD,
+                      (LPBYTE)&ReportAsWorkstation,
+                      sizeof(DWORD));
+    }
+}
+
+static VOID
+OnInitSysSettingsDialog(HWND hwndDlg)
+{
+    HKEY hKey;
+    DWORD dwVal;
+    DWORD dwType = REG_DWORD;
+    DWORD cbData = sizeof(DWORD);
+
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+                     ReportAsWorkstationKey,
+                     0,
+                     KEY_READ,
+                     &hKey) == ERROR_SUCCESS)
+    {
+        if (RegQueryValueEx(hKey,
+                            _T("ReportAsWorkstation"),
+                            0,
+                            &dwType,
+                            (LPBYTE)&dwVal,
+                            &cbData) == ERROR_SUCCESS)
+        {
+            if (dwVal == TRUE)
+            {
+                // set the check box
+                SendDlgItemMessageW(hwndDlg,
+                                    IDC_REPORTASWORKSTATION,
+                                    BM_SETCHECK,
+                                    BST_CHECKED,
+                                    0);
+            }
+        }
+    }
+
+
+}
+
+INT_PTR CALLBACK
+SysSettingsDlgProc(HWND hwndDlg,
+                   UINT uMsg,
+                   WPARAM wParam,
+                   LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+
+    switch (uMsg)
+    {
+        case WM_INITDIALOG:
+            OnInitSysSettingsDialog(hwndDlg);
+            break;
+
+        case WM_COMMAND:
+            switch (LOWORD(wParam))
+            {
+                case IDOK:
+                    OnOK(hwndDlg);
+                    EndDialog(hwndDlg, 0);
+                    return TRUE;
+            }
+            break;
+    }
+
+    return FALSE;
+}
+
 
 /* Property page dialog callback */
 INT_PTR CALLBACK
@@ -49,6 +147,13 @@ AdvancedPageProc(HWND hwndDlg,
                               MAKEINTRESOURCE(IDD_STARTUPRECOVERY),
                               hwndDlg,
                               (DLGPROC)StartRecDlgProc);
+                    break;
+
+                case IDC_SYSSETTINGS:
+                    DialogBox(hApplet,
+                              MAKEINTRESOURCE(IDD_SYSSETTINGS),
+                              hwndDlg,
+                              (DLGPROC)SysSettingsDlgProc);
                     break;
 
                 case IDC_ENVVAR:
