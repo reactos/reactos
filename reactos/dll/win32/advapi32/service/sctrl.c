c0d46197775dbb559795b88e5c64738b1016a539
Gregor Schneider <grschneider@gmail.com>
2009-12-12 12:57:46 +0000

    [advapi32] Print the used name when failing to open a control pipe
    svn path=/trunk/; revision=44544

--- reactos/dll/win32/advapi32/service/sctrl.c
+++ reactos/dll/win32/advapi32/service/sctrl.c
@@ -237,7 +237,7 @@ ScConnectControlPipe(HANDLE *hPipe)
                          NULL);
     if (*hPipe == INVALID_HANDLE_VALUE)
     {
-        ERR("CreateFileW() failed (Error %lu)\n", GetLastError());
+        ERR("CreateFileW() failed for pipe %S (Error %lu)\n", NtControlPipeName, GetLastError());
         return ERROR_FAILED_SERVICE_CONTROLLER_CONNECT;
     }
 
