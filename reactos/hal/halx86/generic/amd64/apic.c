2a4563b90d179a6c9d089d433ad46069c922a7d0
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-02-08 15:50:27 +0000

    [HAL] Add a file, I forgot
    svn path=/branches/ros-amd64-bringup/; revision=45499

---
+++ reactos/hal/halx86/generic/amd64/apic.c
@@ -0,0 +1,20 @@
+/*
+ * COPYRIGHT:        See COPYING in the top level directory
+ * PROJECT:          ReactOS kernel
+ * FILE:             hal/apic.c
+ * PURPOSE:          Hardware Abstraction Layer DLL
+ * PROGRAMMER:       Timo Kreuzer
+ */
+
+#include <hal.h>
+
+//#define NDEBUG
+#include <debug.h>
+
+VOID
+NTAPI
+HalpInitializePICs(IN BOOLEAN EnableInterrupts)
+{
+    UNIMPLEMENTED;
+}
+
