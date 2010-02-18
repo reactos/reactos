de3d0b9d0c37c4d0359b3dd6c67bf4ad8aa8d411
Sir Richard <sir_richard@svn.reactos.org>
2010-02-18 17:00:09 +0000

    Patch by Daniel Zimmermann <netzimme@aim.com>: [DDK]: Add PCI_ADDRESS_MEMORY_SPACE. [HAL]: Use it in HalpAssignPCISlotResources.
    svn path=/trunk/; revision=45611

--- reactos/hal/halx86/generic/bus/pcibus.c
+++ reactos/hal/halx86/generic/bus/pcibus.c
@@ -614,7 +614,7 @@ HalpAssignPCISlotResources(IN PBUS_HANDLER BusHandler,
     {
         if (0 != PciConfig.u.type0.BaseAddresses[Address])
         {
-            if (/*PCI_BASE_ADDRESS_SPACE_MEMORY*/ 0 ==
+            if (PCI_ADDRESS_MEMORY_SPACE ==
                 (PciConfig.u.type0.BaseAddresses[Address] & 0x1))
             {
                 Descriptor->Type = CmResourceTypeMemory;
