63ed339b1860c79d7708e975384fa6be2e389fa9
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-03-28 23:32:29 +0000

    Apply MSVC compilation fix.
    svn path=/branches/ros-amd64-bringup/; revision=46538

--- reactos/include/reactos/mingw-w64/internal.h
+++ reactos/include/reactos/mingw-w64/internal.h
@@ -35,6 +35,12 @@ extern "C" {
 #define __IOINFO_TM_UTF8 1
 #define __IOINFO_TM_UTF16LE 2
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4214)
+#pragma warning(disable:4820)
+#endif
+
   typedef struct {
     intptr_t osfhnd;
     char osfile;
@@ -45,6 +51,10 @@ extern "C" {
     char unicode : 1;
     char pipech2[2];
   } ioinfo;
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 #define IOINFO_ARRAY_ELTS (1 << 5)
 
