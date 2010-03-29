e6421edfc93b6b42b8dd793c712282b530c83bb8
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-03-29 05:17:15 +0000

    Merge bdasup.h
    svn path=/trunk/; revision=46558

--- reactos/include/ddk/bdasup.h
+++ reactos/include/ddk/bdasup.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#if (NTDDI_VERSION >= NTDDI_WINXP)
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -19,8 +23,9 @@ typedef GUID *PGUID;
 
 /* Types */
 
-typedef struct _BDA_PIN_PAIRING
-{
+typedef ULONG BDA_TOPOLOGY_JOINT, *PBDA_TOPOLOGY_JOINT;
+
+typedef struct _BDA_PIN_PAIRING {
   ULONG ulInputPin;
   ULONG ulOutputPin;
   ULONG ulcMaxInputsPerOutput;
@@ -31,19 +36,22 @@ typedef struct _BDA_PIN_PAIRING
   const ULONG *pTopologyJoints;
 } BDA_PIN_PAIRING, *PBDA_PIN_PAIRING;
 
-typedef struct _BDA_FILTER_TEMPLATE
-{
+typedef struct _BDA_FILTER_TEMPLATE {
   const KSFILTER_DESCRIPTOR *pFilterDescriptor;
   ULONG ulcPinPairs;
   const BDA_PIN_PAIRING *pPinPairs;
 } BDA_FILTER_TEMPLATE, *PBDA_FILTER_TEMPLATE;
 
-
-typedef struct _KSM_PIN
-{
+typedef struct _KSM_PIN_PAIR {
   KSMETHOD Method;
-    __GNU_EXTENSION union
-    {
+  ULONG InputPinId;
+  ULONG OutputPinId;
+  ULONG Reserved;
+} KSM_PIN_PAIR, * PKSM_PIN_PAIR;
+
+typedef struct _KSM_PIN {
+  KSMETHOD Method;
+  __GNU_EXTENSION union {
     ULONG PinId;
     ULONG PinType;
   };
@@ -52,113 +60,146 @@ typedef struct _KSM_PIN
 
 /* Functions */
 
-STDMETHODIMP_(NTSTATUS) BdaCheckChanges(IN PIRP  Irp);
-STDMETHODIMP_(NTSTATUS) BdaCommitChanges(IN PIRP  Irp);
+STDMETHODIMP_(NTSTATUS)
+BdaCheckChanges(
+  IN PIRP Irp);
 
-STDMETHODIMP_(NTSTATUS) BdaCreateFilterFactory(
+STDMETHODIMP_(NTSTATUS)
+BdaCommitChanges(
+  IN PIRP Irp);
+
+STDMETHODIMP_(NTSTATUS)
+BdaCreateFilterFactory(
   IN PKSDEVICE pKSDevice,
   IN const KSFILTER_DESCRIPTOR *pFilterDescriptor,
   IN const BDA_FILTER_TEMPLATE *pBdaFilterTemplate);
 
-STDMETHODIMP_(NTSTATUS) BdaCreateFilterFactoryEx(
+STDMETHODIMP_(NTSTATUS)
+BdaCreateFilterFactoryEx(
   IN PKSDEVICE pKSDevice,
   IN const KSFILTER_DESCRIPTOR *pFilterDescriptor,
   IN const BDA_FILTER_TEMPLATE *pBdaFilterTemplate,
   OUT PKSFILTERFACTORY  *ppKSFilterFactory);
 
-STDMETHODIMP_(NTSTATUS) BdaCreatePin(
+STDMETHODIMP_(NTSTATUS)
+BdaCreatePin(
   IN PKSFILTER pKSFilter,
   IN ULONG ulPinType,
   OUT ULONG *pulPinId);
 
-STDMETHODIMP_(NTSTATUS) BdaCreateTopology(
+STDMETHODIMP_(NTSTATUS)
+BdaCreateTopology(
   IN PKSFILTER pKSFilter,
   IN ULONG InputPinId,
   IN ULONG OutputPinId);
 
-STDMETHODIMP_(NTSTATUS) BdaDeletePin(
+STDMETHODIMP_(NTSTATUS)
+BdaDeletePin(
   IN PKSFILTER pKSFilter,
   IN ULONG *pulPinId);
 
-STDMETHODIMP_(NTSTATUS) BdaFilterFactoryUpdateCacheData(
+STDMETHODIMP_(NTSTATUS)
+BdaFilterFactoryUpdateCacheData(
   IN PKSFILTERFACTORY pFilterFactory,
   IN const KSFILTER_DESCRIPTOR *pFilterDescriptor OPTIONAL);
 
-STDMETHODIMP_(NTSTATUS) BdaGetChangeState(
+STDMETHODIMP_(NTSTATUS)
+BdaGetChangeState(
   IN PIRP Irp,
   OUT BDA_CHANGE_STATE *pChangeState);
 
-STDMETHODIMP_(NTSTATUS) BdaInitFilter(
+STDMETHODIMP_(NTSTATUS)
+BdaInitFilter(
   IN PKSFILTER pKSFilter,
   IN const BDA_FILTER_TEMPLATE *pBdaFilterTemplate);
 
-STDMETHODIMP_(NTSTATUS) BdaMethodCreatePin(
+STDMETHODIMP_(NTSTATUS)
+BdaMethodCreatePin(
   IN PIRP Irp,
   IN KSMETHOD *pKSMethod,
   OUT ULONG *pulPinFactoryID);
 
-STDMETHODIMP_(NTSTATUS) BdaMethodCreateTopology(
+STDMETHODIMP_(NTSTATUS)
+BdaMethodCreateTopology(
   IN PIRP Irp,
   IN KSMETHOD *pKSMethod,
   OPTIONAL PVOID pvIgnored);
 
-STDMETHODIMP_(NTSTATUS) BdaMethodDeletePin(
+STDMETHODIMP_(NTSTATUS)
+BdaMethodDeletePin(
   IN PIRP Irp,
   IN KSMETHOD *pKSMethod,
   OPTIONAL PVOID pvIgnored);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyGetControllingPinId(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyGetControllingPinId(
   IN PIRP Irp,
   IN KSP_BDA_NODE_PIN *pProperty,
   OUT ULONG *pulControllingPinId);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyGetPinControl(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyGetPinControl(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty,
   OUT ULONG *pulProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyNodeDescriptors(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyNodeDescriptors(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty,
   OUT BDANODE_DESCRIPTOR *pNodeDescriptorProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyNodeEvents(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyNodeEvents(
   IN PIRP Irp,
   IN KSP_NODE *pKSProperty,
   OUT GUID *pguidProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyNodeMethods(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyNodeMethods(
   IN PIRP Irp,
   IN KSP_NODE *pKSProperty,
   OUT GUID *pguidProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyNodeProperties(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyNodeProperties(
   IN PIRP Irp,
   IN KSP_NODE *pKSProperty,
   OUT GUID *pguidProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyNodeTypes(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyNodeTypes(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty,
   OUT ULONG *pulProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyPinTypes(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyPinTypes(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty,
   OUT ULONG *pulProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaPropertyTemplateConnections(
+STDMETHODIMP_(NTSTATUS)
+BdaPropertyTemplateConnections(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty,
   OUT KSTOPOLOGY_CONNECTION *pConnectionProperty);
 
-STDMETHODIMP_(NTSTATUS) BdaStartChanges(IN PIRP Irp);
-STDMETHODIMP_(NTSTATUS) BdaUninitFilter(IN PKSFILTER pKSFilter);
+STDMETHODIMP_(NTSTATUS)
+BdaStartChanges(
+  IN PIRP Irp);
 
-STDMETHODIMP_(NTSTATUS) BdaValidateNodeProperty(
+STDMETHODIMP_(NTSTATUS)
+BdaUninitFilter(
+  IN PKSFILTER pKSFilter);
+
+STDMETHODIMP_(NTSTATUS)
+BdaValidateNodeProperty(
   IN PIRP Irp,
   IN KSPROPERTY *pKSProperty);
 
 #if defined(__cplusplus)
 }
 #endif
+
+#endif /* (NTDDI_VERSION >= NTDDI_WINXP) */
