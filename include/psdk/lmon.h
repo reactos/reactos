c7a0a39963f10136e8d2eb871a14a56a98a1fbe3
Amine Khaldi <amine.khaldi@reactos.org>
2010-04-16 13:42:34 +0000

    [DDK] - Move lmon.h to PSDK and apply a consistent formatting.
    svn path=/branches/header-work/; revision=46896

--- include/psdk/lmon.h
+++ include/psdk/lmon.h
@@ -1,5 +1,4 @@
 
-
 #ifdef UNICODE
 #define PORT_INFO_FF PORT_INFO_FFW
 #define PPORT_INFO_FF PPORT_INFO_FFW
@@ -10,18 +9,14 @@
 #define LPPORT_INFO_FF LPPORT_INFO_FFA
 #endif
 
-typedef struct _PORT_INFO_FFW
-{
-   LPWSTR pName;
-   DWORD cbMonitorData;
-   LPBYTE pMonitorData;
+typedef struct _PORT_INFO_FFW {
+  LPWSTR pName;
+  DWORD cbMonitorData;
+  LPBYTE pMonitorData;
 } PORT_INFO_FFW, *PPORT_INFO_FFW, *LPPORT_INFO_FFW;
 
-typedef struct _PORT_INFO_FFA
-{
-   LPSTR pName;
-   DWORD cbMonitorData;
-   LPBYTE pMonitorData;
+typedef struct _PORT_INFO_FFA {
+  LPSTR pName;
+  DWORD cbMonitorData;
+  LPBYTE pMonitorData;
 } PORT_INFO_FFA, *PPORT_INFO_FFA, *LPPORT_INFO_FFA;
-
-
