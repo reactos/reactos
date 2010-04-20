590cff7bd18dc92d414690454d4fdab624155e4b
Aleksey Bragin <aleksey@reactos.org>
2010-04-20 17:36:44 +0000

    - Add Wow64* functions declarations to winbase.h - Export Wow64* functions in kernel32, because 32bit Windows also exports them. - Sync ole32, oleaut32 with Wine-1.1.43.
    svn path=/trunk/; revision=46959

--- reactos/dll/win32/ole32/moniker.c
+++ reactos/dll/win32/ole32/moniker.c
@@ -127,6 +127,7 @@ static BOOL start_rpcss(void)
     WCHAR cmd[MAX_PATH];
     static const WCHAR rpcss[] = {'\\','r','p','c','s','s','.','e','x','e',0};
     BOOL rslt;
+    void *redir;
 
     TRACE("\n");
 
@@ -135,7 +136,9 @@ static BOOL start_rpcss(void)
     GetSystemDirectoryW( cmd, MAX_PATH - sizeof(rpcss)/sizeof(WCHAR) );
     strcatW( cmd, rpcss );
 
+    Wow64DisableWow64FsRedirection( &redir );
     rslt = CreateProcessW( cmd, cmd, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi );
+    Wow64RevertWow64FsRedirection( redir );
 
     if (rslt)
     {
