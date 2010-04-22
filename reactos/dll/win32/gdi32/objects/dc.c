14347c75ad50fead45b67b447b8bfadfe8d92f9e
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-04-22 22:28:49 +0000

    Disable gdi batch for SelectObject with fonts. Fixes font regression.
    svn path=/trunk/; revision=46998

--- reactos/dll/win32/gdi32/objects/dc.c
+++ reactos/dll/win32/gdi32/objects/dc.c
@@ -1540,7 +1540,7 @@ SelectObject(HDC hDC,
     PDC_ATTR pDc_Attr;
     HGDIOBJ hOldObj = NULL;
     UINT uType;
-    PTEB pTeb;
+//    PTEB pTeb;
 
     if(!GdiGetHandleUserData(hDC, GDI_OBJECT_TYPE_DC, (PVOID)&pDc_Attr))
     {
@@ -1582,6 +1582,7 @@ SelectObject(HDC hDC,
         case GDI_OBJECT_TYPE_FONT:
             hOldObj = pDc_Attr->hlfntNew;
             if (hOldObj == hGdiObj) return hOldObj;
+#if 0
             pDc_Attr->ulDirty_ &= ~SLOW_WIDTHS;
             pDc_Attr->ulDirty_ |= DIRTY_CHARSET;
             pDc_Attr->hlfntNew = hGdiObj;
@@ -1603,6 +1604,7 @@ SelectObject(HDC hDC,
               if (pTeb->GdiBatchCount >= GDI_BatchLimit) NtGdiFlush();
               return hOldObj;
             }
+#endif
             // default for select object font
             return NtGdiSelectFont(hDC, hGdiObj);
 
