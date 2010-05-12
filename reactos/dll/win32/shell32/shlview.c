b320b4bcb1e0d5e139cb959227b9057ec68cf369
Gregor Schneider <grschneider@gmail.com>
2010-05-12 21:11:24 +0000

    [SHELL32] Amendment to r47182: GetAsyncKeyState -> GetKeyState (thanks to Giannis), simplify
    svn path=/trunk/; revision=47185

--- reactos/dll/win32/shell32/shlview.c
+++ reactos/dll/win32/shell32/shlview.c
@@ -1511,7 +1511,7 @@ static LRESULT ShellView_OnNotify(IShellViewImpl * This, UINT CtlID, LPNMHDR lpn
 	      msg.pt = 0;*/
 
 	      LPNMLVKEYDOWN plvKeyDown = (LPNMLVKEYDOWN) lpnmh;
-          SHORT ctrl = GetAsyncKeyState(VK_CONTROL);
+          SHORT ctrl = GetKeyState(VK_CONTROL) & 0x8000;
 
               /* initiate a rename of the selected file or directory */
               if(plvKeyDown->wVKey == VK_F2)
@@ -1592,11 +1592,11 @@ static LRESULT ShellView_OnNotify(IShellViewImpl * This, UINT CtlID, LPNMHDR lpn
                 IShellBrowser_BrowseObject(lpSb, NULL, SBSP_PARENT);
             }
         }
-        else if(plvKeyDown->wVKey == 'C' && (ctrl & 0x8000))
+        else if(plvKeyDown->wVKey == 'C' && ctrl)
         {
             FIXME("Need to copy\n");
         }
-        else if(plvKeyDown->wVKey == 'V' && (ctrl & 0x8000))
+        else if(plvKeyDown->wVKey == 'V' && ctrl)
         {
             FIXME("Need to paste\n");
         }
