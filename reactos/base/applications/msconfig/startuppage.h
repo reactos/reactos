5168fe0e809e763537d9e0c9492e79f3493f4af5
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-05-31 13:45:29 +0000

    [MSCONFIG] Add header guards. They are not neccessary in this case, but better to have them, especially since it uses a compilation unit.
    svn path=/trunk/; revision=47488

--- reactos/base/applications/msconfig/startuppage.h
+++ reactos/base/applications/msconfig/startuppage.h
@@ -1,4 +1,9 @@
+#ifndef _STARTUPPAGE_H_
+#define _STARTUPPAGE_H_
+
 extern HWND hStartupPage;
 extern HWND hStartupPageListCtrl;
 
 INT_PTR CALLBACK StartupPageWndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+
+#endif
