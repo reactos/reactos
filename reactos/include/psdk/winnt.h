c25fc39e6f8e9540c15b06d9b556fd25196fde4f
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-06-04 21:50:06 +0000

    [winnt.h] Fix definition of KNONVOLATILE_CONTEXT_POINTERS for amd64
    svn path=/trunk/; revision=47578

--- reactos/include/psdk/winnt.h
+++ reactos/include/psdk/winnt.h
@@ -2300,8 +2300,8 @@ typedef struct _KNONVOLATILE_CONTEXT_POINTERS {
             PM128A Xmm13;
             PM128A Xmm14;
             PM128A Xmm15;
-        };
-    };
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
 
     union {
         PULONG64 IntegerContext[16];
@@ -2322,8 +2322,8 @@ typedef struct _KNONVOLATILE_CONTEXT_POINTERS {
             PULONG64 R13;
             PULONG64 R14;
             PULONG64 R15;
-        };
-    };
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME2;
 } KNONVOLATILE_CONTEXT_POINTERS, *PKNONVOLATILE_CONTEXT_POINTERS;
 
 #define RUNTIME_FUNCTION_INDIRECT 0x1
