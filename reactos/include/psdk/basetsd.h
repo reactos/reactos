a47afb4b46568a7158c3bf2ce85b5d411c2a417c
Amine Khaldi <amine.khaldi@reactos.org>
2010-06-10 20:31:21 +0000

    [PSDK] - Move the amd64 check before the i386 one, and also check for __i386__
    svn path=/trunk/; revision=47744

--- reactos/include/psdk/basetsd.h
+++ reactos/include/psdk/basetsd.h
@@ -26,10 +26,10 @@
 #if !defined(_X86_) && !defined(_AMD64) && !defined(_IA64_) && !defined(_ALPHA_) && \
     !defined(_ARM_) && !defined(_PPC_) && !defined(_MIPS_) && !defined(_68K_)
 
-#if defined(_M_IX86)
-#define _X86_
-#elif defined(_M_AMD64) || defined(__x86_64__)
+#if defined(_M_AMD64) || defined(__x86_64__)
 #define _AMD64_
+#elif defined(_M_IX86) || defined(__i386__)
+#define _X86_
 #elif defined(_M_IA64) || defined(__ia64__)
 #define _IA64_
 #elif defined(_M_ALPHA) || defined(__alpha__)
