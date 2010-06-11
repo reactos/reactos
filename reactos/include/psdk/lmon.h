42d943893a68cd09023c36364bf0ed316124d244
Amine Khaldi <amine.khaldi@reactos.org>
2010-06-11 13:52:46 +0000

    [PSDK] - Add some missing file-headers.
    svn path=/trunk/; revision=47755

--- reactos/include/psdk/lmon.h
+++ reactos/include/psdk/lmon.h
@@ -1,3 +1,22 @@
+/*
+ * lmon.h
+ *
+ * This file is part of the ReactOS PSDK package.
+ *
+ * Contributors:
+ *   Created by Magnus Olsen.
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
 
 #ifdef UNICODE
 #define PORT_INFO_FF PORT_INFO_FFW
