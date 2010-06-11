af25a406cf8eb43ac9a8962bbb2d34da2b5fc6be
Amine Khaldi <amine.khaldi@reactos.org>
2010-06-11 12:10:58 +0000

    [PSDK] - Copy atsmedia.h from our dxsdk to our psdk (replacing the one that's already there) - Add some missing file-headers.
    svn path=/trunk/; revision=47753

--- reactos/include/psdk/devpropdef.h
+++ reactos/include/psdk/devpropdef.h
@@ -1,3 +1,23 @@
+/*
+ * devpropdef.h
+ *
+ * This file is part of the ReactOS PSDK package.
+ *
+ * Contributors:
+ *   Created by Amine Khaldi.
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
 #ifndef _DEVPROPDEF_H_
 #define _DEVPROPDEF_H_
 
