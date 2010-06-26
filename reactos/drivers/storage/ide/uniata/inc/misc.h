e1a011f4a92f1fd813e1b1fb4bde1d4c5c8c6346
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-06-26 00:02:51 +0000

    Fix a typo
    svn path=/branches/ros-amd64-bringup/; revision=47847

--- reactos/drivers/storage/ide/uniata/inc/misc.h
+++ reactos/drivers/storage/ide/uniata/inc/misc.h
@@ -4,7 +4,7 @@
 /* The definitions look so crappy, because the code doesn't care 
    whether the source is an array or an integer */
 #define MOV_DD_SWP(a,b) ((a) = RtlUlongByteSwap(*(PULONG)&(b)))
-#define MOV_DW_SWP(a,b) ((a) = RtlUshortByteSwap(*(PULONG)&(b)))
-#define MOV_SWP_DW2DD(a,b) ((a) = RtlUshortByteSwap(*(PULONG)&(b)))
+#define MOV_DW_SWP(a,b) ((a) = RtlUshortByteSwap(*(PUSHORT)&(b)))
+#define MOV_SWP_DW2DD(a,b) ((a) = RtlUshortByteSwap(*(PUSHORT)&(b)))
 
 #endif // __CROSSNT_MISC__H__
