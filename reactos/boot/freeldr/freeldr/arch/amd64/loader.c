073976148811727971381b77cc42808a3a4ae152
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-07-27 16:21:20 +0000

    [FREELDR] Don't redefine _NTSYSTEM_
    svn path=/trunk/; revision=48313

--- reactos/boot/freeldr/freeldr/arch/amd64/loader.c
+++ reactos/boot/freeldr/freeldr/arch/amd64/loader.c
@@ -16,7 +16,6 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-#define _NTSYSTEM_
 #include <freeldr.h>
 
 #define NDEBUG
