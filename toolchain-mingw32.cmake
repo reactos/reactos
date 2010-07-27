33d934555231c173594763c7e5db580b470999f9
Sir Richard <sir_richard@svn.reactos.org>
2010-07-27 16:00:40 +0000

    [CMAKE]: Detect correct MinGW compiler on Windows vs Linux/Darwin.
    svn path=/branches/cmake-bringup/; revision=48309

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -1,12 +1,17 @@
+# Choose the right MinGW prefix
+if (CMAKE_SYSTEM_NAME STREQUAL Windows)
+set(MINGW_PREFIX "")
+else()
+set(MINGW_PREFIX "mingw32-")
+endif()
+
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_PROCESSIR i686)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER mingw32-gcc)
-SET(CMAKE_CXX_COMPILER mingw32-g++)
-
-# here is the target environment located
-#SET(CMAKE_FIND_ROOT_PATH /usr/local/RosBE )
+SET(CMAKE_C_COMPILER ${MINGW_PREFIX}gcc)
+SET(CMAKE_CXX_COMPILER ${MINGW_PREFIX}g++)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
