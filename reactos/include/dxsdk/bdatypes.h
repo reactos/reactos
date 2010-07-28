80a061f01c9891b6a57c6c7a7a778eb726f85af3
Amine Khaldi <amine.khaldi@reactos.org>
2010-07-28 12:52:04 +0000

    [DXSDK] - Add some headers to some headers.
    svn path=/trunk/; revision=48328

--- reactos/include/dxsdk/bdatypes.h
+++ reactos/include/dxsdk/bdatypes.h
@@ -1,3 +1,19 @@
+/*
+ * bdatypes.h
+ *
+ * This file is part of the ReactOS DXSDK package.
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
 
 #ifndef _BDATYPES_
 
