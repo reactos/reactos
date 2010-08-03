5abcbffcd0ea83556bdf81d7bbc365caf402f0a3
Sir Richard <sir_richard@svn.reactos.org>
2010-08-03 14:53:12 +0000

    [CMAKE]: Make simple rule for building ASM files. [CMAKE]: Enable RC support. [CMAKE]: Stop building tools we won't need.
    svn path=/branches/cmake-bringup/; revision=48427

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -15,8 +15,9 @@ SET(CMAKE_SYSTEM_PROCESSOR i686)
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER ${MINGW_PREFIX}gcc)
 SET(CMAKE_CXX_COMPILER ${MINGW_PREFIX}g++)
-
+SET(CMAKE_RC_COMPILER ${MINGW_PREFIX}windres)
 SET(CMAKE_ASM_COMPILER ${MINGW_PREFIX}gcc)
+set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> -x assembler-with-cpp -o <OBJECT> <FLAGS> <DEFINES> -D__ASM__ -c <SOURCE>")
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
