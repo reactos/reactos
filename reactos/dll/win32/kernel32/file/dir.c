e715956b8031f5b3d3a41f1767c2f08725c6c8e3
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-08-04 13:33:37 +0000

    [KERNEL32] Fix a check in ContainsPath. Patch by Gabriel Ilardi.
    See issue #5478 for more details.
    
    svn path=/trunk/; revision=48456

--- reactos/dll/win32/kernel32/file/dir.c
+++ reactos/dll/win32/kernel32/file/dir.c
@@ -972,7 +972,7 @@ ContainsPath(LPCWSTR name)
 {
     if (RtlDetermineDosPathNameType_U(name) != RtlPathTypeRelative) return TRUE;
     if (name[0] != '.') return FALSE;
-    if (name[1] == '/' || name[1] == '\\') return TRUE;
+    if (name[1] == '/' || name[1] == '\\' || name[1] == '\0') return TRUE;
     return (name[1] == '.' && (name[2] == '/' || name[2] == '\\'));
 }
 
