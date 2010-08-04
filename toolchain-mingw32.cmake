3930317e127d324cae9dc3ed4396c01f402821be
Sir Richard <sir_richard@svn.reactos.org>
2010-08-04 14:29:24 +0000

    [CMAKE]: Disable standard C libraries (WIP: Should add libgcc). [CMAKE]: Set global linker flags we need for all libraries. [CMAKE]: Set specific NTDLL linker flag. Thanks to Amine for some of his help.
    svn path=/branches/cmake-bringup/; revision=48457

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -20,6 +20,10 @@ SET(CMAKE_ASM_COMPILER ${MINGW_PREFIX}gcc)
 SET(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> -x assembler-with-cpp -o <OBJECT> <FLAGS> <DEFINES> -D__ASM__ -c <SOURCE>")
 SET(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> -O coff -I${REACTOS_SOURCE_DIR}/include/ -I${REACTOS_BINARY_DIR}/include/reactos -i <SOURCE> -o <OBJECT> <DEFINES> -DRC_INVOKED")
 
+# Use stdcall fixups, and don't link with anything by default unless we say so
+set(CMAKE_C_STANDARD_LIBRARIES "") # We should add the environment libgcc here
+SET(CMAKE_SHARED_LINKER_FLAGS "-Wl,--enable-stdcall-fixup -Wl,--kill-at -nodefaultlibs -nostdlib")
+
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
