807b0907d5fa2decb6b3dcd7b6d44ec8af3960ec
Sir Richard <sir_richard@svn.reactos.org>
2010-08-04 07:51:26 +0000

    [CMAKE]: Add full support for linking RC files with windres.
    svn path=/branches/cmake-bringup/; revision=48448

--- toolchain-mingw32.cmake
+++ toolchain-mingw32.cmake
@@ -17,9 +17,8 @@ SET(CMAKE_C_COMPILER ${MINGW_PREFIX}gcc)
 SET(CMAKE_CXX_COMPILER ${MINGW_PREFIX}g++)
 SET(CMAKE_RC_COMPILER ${MINGW_PREFIX}windres)
 SET(CMAKE_ASM_COMPILER ${MINGW_PREFIX}gcc)
-set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> -x assembler-with-cpp -o <OBJECT> <FLAGS> <DEFINES> -D__ASM__ -c <SOURCE>")
-
-set(CMAKE_STANDARD_LIBRARIES "")
+SET(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_ASM_COMPILER> -x assembler-with-cpp -o <OBJECT> <FLAGS> <DEFINES> -D__ASM__ -c <SOURCE>")
+SET(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> -O coff -I${REACTOS_SOURCE_DIR}/include/ -I${REACTOS_BINARY_DIR}/include/reactos -i <SOURCE> -o <OBJECT> <DEFINES> -DRC_INVOKED")
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
