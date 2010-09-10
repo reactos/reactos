60bb233f468cb70dd1645e3e15ca7aaa15a088d5
Aleksey Bragin <aleksey@reactos.org>
2010-09-10 08:30:13 +0000

    - Fix typos in the description of serial port defines.
    svn path=/trunk/; revision=48728

--- reactos/include/reactos/drivers/serial/ns16550.h
+++ reactos/include/reactos/drivers/serial/ns16550.h
@@ -11,7 +11,7 @@
 #pragma once
 
 /* Note: These definitions are the internal definitions used by Microsoft serial
-   driver (see src/kernel/serial.h in Windows sources). Linux uses its own, as
+   driver (see src/kernel/serial/serial.h in WDK source code). Linux uses its own, as
    do most other OS.
 */
 
