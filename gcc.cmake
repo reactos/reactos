0dbb8d3eb2e1ac5b868c776aa7780e3d114988b2
Amine Khaldi <amine.khaldi@reactos.org>
2010-09-19 00:15:45 +0000

    [CMAKE] - *cough* Separate compiler specific flags.
    svn path=/branches/cmake-bringup/; revision=48808

---
+++ gcc.cmake
@@ -0,0 +1,25 @@
+
+# Linking
+link_directories("${REACTOS_SOURCE_DIR}/importlibs" ${REACTOS_BINARY_DIR}/lib/3rdparty/mingw)
+set(CMAKE_C_LINK_EXECUTABLE "<CMAKE_C_COMPILER> <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_EXE_LINKER_FLAGS "-nodefaultlibs -nostdlib -Wl,--enable-auto-image-base -Wl,--kill-at -Wl,-T,${REACTOS_SOURCE_DIR}/global.lds")
+
+# Compiler Core
+add_definitions(-pipe -fms-extensions)
+
+set(CMAKE_C_CREATE_SHARED_LIBRARY "<CMAKE_C_COMPILER> <CMAKE_SHARED_LIBRARY_C_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
+
+# Debugging (Note: DWARF-4 on 4.5.1 when we ship)
+add_definitions(-gdwarf-2 -g2 -femit-struct-debug-detailed=none -feliminate-unused-debug-types)
+
+# Tuning
+add_definitions(-march=pentium -mtune=i686)
+
+# Warnings
+add_definitions(-Wall -Wno-char-subscripts -Wpointer-arith -Wno-multichar -Wno-error=uninitialized -Wno-unused-value -Winvalid-pch)
+
+# Optimizations
+add_definitions(-Os -fno-strict-aliasing -ftracer -momit-leaf-frame-pointer -mpreferred-stack-boundary=2 -fno-set-stack-executable -fno-optimize-sibling-calls)
+
+# C++ Flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions -fno-rtti")
---
+++ msc.cmake
@@ -0,0 +1,3 @@
+
+add_definitions(/GS- /Zl /Zi)
+add_definitions(-Dinline=__inline -D__STDC__=1)
