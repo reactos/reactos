73c1c7f232e831e6ea34ef2e9c9886c19a4a77fb
Sir Richard <sir_richard@svn.reactos.org>
2010-10-16 15:24:08 +0000

    [CLASSPNP]: Add Windows Server 2003 DDK ClassPNP sample, under the license and terms of the Windows Server 2003 DDK. [CDROM]: Add Windows Server 2003 DDK Cdrom Sample, under the license and terms of the Windows Server 2003 DDK. They both build and run fine, but PCIIDE(x) must be completed/enabled, and atapi must be rewritten to be WDM-compliant.
    svn path=/trunk/; revision=49171

---
+++ reactos/drivers/storage/class/cdrom_new/sec.c
@@ -0,0 +1,38 @@
+/*--
+
+Copyright (C) Microsoft Corporation, 1999
+
+--*/
+
+
+#include "sec.h"
+
+
+NTSTATUS
+CdRomGetRpc0Settings(
+    IN PDEVICE_OBJECT Fdo
+    )
+{
+    PCOMMON_DEVICE_EXTENSION commonExtension = Fdo->DeviceExtension;
+    PCDROM_DATA cddata = (PCDROM_DATA)(commonExtension->DriverData);
+
+    cddata->Rpc0SystemRegion = (UCHAR)(~1);        // region one
+    cddata->Rpc0SystemRegionResetCount = 0; // no resets
+
+    return STATUS_SUCCESS;
+}
+
+
+NTSTATUS
+CdRomSetRpc0Settings(
+    IN PDEVICE_OBJECT Fdo,
+    IN UCHAR NewRegion
+    )
+{
+    return STATUS_SUCCESS;
+}
+
+
+
+
+
