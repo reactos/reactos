0ef2d803bfd79bca63c5c51b81d8230debb8ab25
Sylvain Petreolle <spetreolle@yahoo.fr>
2010-10-24 15:03:07 +0000

    [CMAKE] Fix winspool loading.
    svn path=/branches/cmake-bringup/; revision=49260

---
+++ dll/win32/winspool/winspool_winspool.stubs.c
@@ -0,0 +1,82 @@
+/* File generated automatically from dll\win32\winspool\winspool.spec; do not edit! */
+/* This file can be copied, modified and distributed without restriction. */
+
+#include <stdarg.h>
+#include "windef.h"
+#include "winbase.h"
+#include "wine/config.h"
+#include "wine/exception.h"
+
+void __wine_spec_unimplemented_stub( const char *module, const char *function )
+{
+    ULONG_PTR args[2];
+
+    args[0] = (ULONG_PTR)module;
+    args[1] = (ULONG_PTR)function;
+    RaiseException( EXCEPTION_WINE_STUB, EH_NONCONTINUABLE, 2, args );
+}
+
+static const char __wine_spec_file_name[] = "winspool.drv";
+
+void __wine_stub_winspool_drv_100(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "EnumPrinterPropertySheets"); }
+void __wine_stub_winspool_drv_101(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ClusterSplOpen"); }
+void __wine_stub_winspool_drv_102(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ClusterSplClose"); }
+void __wine_stub_winspool_drv_103(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ClusterSplIsAlive"); }
+void __wine_stub_winspool_drv_104(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PerfClose"); }
+void __wine_stub_winspool_drv_105(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PerfCollect"); }
+void __wine_stub_winspool_drv_106(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PerfOpen"); }
+void __wine_stub_winspool_drv_205(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SplReadPrinter"); }
+void __wine_stub_winspool_drv_206(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "AddPerMachineConnectionA"); }
+void __wine_stub_winspool_drv_207(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "AddPerMachineConnectionW"); }
+void __wine_stub_winspool_drv_208(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DeletePerMachineConnectionA"); }
+void __wine_stub_winspool_drv_209(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DeletePerMachineConnectionW"); }
+void __wine_stub_winspool_drv_210(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "EnumPerMachineConnectionsA"); }
+void __wine_stub_winspool_drv_211(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "EnumPerMachineConnectionsW"); }
+void __wine_stub_winspool_drv_212(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "LoadPrinterDriver"); }
+void __wine_stub_winspool_drv_213(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "RefCntLoadDriver"); }
+void __wine_stub_winspool_drv_214(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "RefCntUnloadDriver"); }
+void __wine_stub_winspool_drv_215(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ForceUnloadDriver"); }
+void __wine_stub_winspool_drv_216(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PublishPrinterA"); }
+void __wine_stub_winspool_drv_217(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PublishPrinterW"); }
+void __wine_stub_winspool_drv_218(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "CallCommonPropertySheetUI"); }
+void __wine_stub_winspool_drv_219(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIQueueCreate"); }
+void __wine_stub_winspool_drv_220(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIPrinterPropPages"); }
+void __wine_stub_winspool_drv_221(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIDocumentDefaults"); }
+void __wine_stub_winspool_drv_222(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SendRecvBidiData"); }
+void __wine_stub_winspool_drv_223(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "RouterFreeBidiResponseContainer"); }
+void __wine_stub_winspool_drv_224(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ExternalConnectToLd64In32Server"); }
+void __wine_stub_winspool_drv_226(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIWebPnpEntry"); }
+void __wine_stub_winspool_drv_227(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIWebPnpPostEntry"); }
+void __wine_stub_winspool_drv_228(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUICreateInstance"); }
+void __wine_stub_winspool_drv_229(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIDocumentPropertiesWrap"); }
+void __wine_stub_winspool_drv_230(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIPrinterSetup"); }
+void __wine_stub_winspool_drv_231(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PrintUIServerPropPages"); }
+void __wine_stub_winspool_drv_232(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "AddDriverCatalog"); }
+void __wine_stub_winspool_drv_107(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ADVANCEDSETUPDIALOG"); }
+void __wine_stub_winspool_drv_133(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "AdvancedSetupDialog"); }
+void __wine_stub_winspool_drv_135(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "CloseSpoolFileHandle"); }
+void __wine_stub_winspool_drv_139(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ConvertAnsiDevModeToUnicodeDevMode"); }
+void __wine_stub_winspool_drv_140(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "ConvertUnicodeDevModeToAnsiDevMode"); }
+void __wine_stub_winspool_drv_141(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "CommitSpoolData"); }
+void __wine_stub_winspool_drv_142(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "CreatePrinterIC"); }
+void __wine_stub_winspool_drv_143(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DEVICECAPABILITIES"); }
+void __wine_stub_winspool_drv_144(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DEVICEMODE"); }
+void __wine_stub_winspool_drv_162(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DeletePrinterIC"); }
+void __wine_stub_winspool_drv_163(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DevQueryPrint"); }
+void __wine_stub_winspool_drv_170(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DeviceMode"); }
+void __wine_stub_winspool_drv_171(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "DocumentEvent"); }
+void __wine_stub_winspool_drv_175(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "EXTDEVICEMODE"); }
+void __wine_stub_winspool_drv_252(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "GetSpoolFileHandle"); }
+void __wine_stub_winspool_drv_253(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "IsValidDevmodeA"); }
+void __wine_stub_winspool_drv_254(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "IsValidDevmodeW"); }
+void __wine_stub_winspool_drv_257(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "PlayGdiScriptOnPrinterIC"); }
+void __wine_stub_winspool_drv_261(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "QueryColorProfile"); }
+void __wine_stub_winspool_drv_262(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "QuerySpoolMode"); }
+void __wine_stub_winspool_drv_263(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "QueryRemoteFonts"); }
+void __wine_stub_winspool_drv_268(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SeekPrinter"); }
+void __wine_stub_winspool_drv_269(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SetAllocFailCount"); }
+void __wine_stub_winspool_drv_280(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SplDriverUnloadComplete"); }
+void __wine_stub_winspool_drv_281(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SpoolerDevQueryPrintW"); }
+void __wine_stub_winspool_drv_283(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "SpoolerPrinterEvent"); }
+void __wine_stub_winspool_drv_284(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "StartDocDlgA"); }
+void __wine_stub_winspool_drv_285(void) { __wine_spec_unimplemented_stub(__wine_spec_file_name, "StartDocDlgW"); }
