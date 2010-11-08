b4593924da2f340e4f9b875d8f4ece8c0141abb1
Sir Richard <sir_richard@svn.reactos.org>
2010-11-08 02:37:17 +0000

    [NTOS]: KiDispatchInterrupt (the DPC handler) in C, instead of ASM.
    svn path=/trunk/; revision=49521

--- reactos/ntoskrnl/ke/i386/thrdini.c
+++ reactos/ntoskrnl/ke/i386/thrdini.c
@@ -49,6 +49,13 @@ KiSwitchThreads(
     IN PKTHREAD NewThread
 );
 
+VOID
+FASTCALL
+KiRetireDpcListInDpcStack(
+    IN PKPRCB Prcb,
+    IN PVOID DpcStack
+);
+
 /* FUNCTIONS *****************************************************************/
 
 VOID
@@ -446,5 +453,66 @@ KiSwapContextEntry(IN PKSWITCHFRAME SwitchFrame,
     _enable();
     KiSwitchThreads(OldThread, NewThread);
 }
+
+VOID
+NTAPI
+KiDispatchInterrupt(VOID)
+{
+    PKIPCR Pcr = (PKIPCR)KeGetPcr();
+    PKPRCB Prcb = &Pcr->PrcbData;
+    PVOID OldHandler;
+    PKTHREAD NewThread, OldThread;
+    
+    /* Disable interrupts */
+    _disable();
+    
+    /* Check for pending timers, pending DPCs, or pending ready threads */
+    if ((Prcb->DpcData[0].DpcQueueDepth) ||
+        (Prcb->TimerRequest) ||
+        (Prcb->DeferredReadyListHead.Next))
+    {
+        /* Switch to safe execution context */
+        OldHandler = Pcr->NtTib.ExceptionList;
+        Pcr->NtTib.ExceptionList = EXCEPTION_CHAIN_END;
+        
+        /* Retire DPCs while under the DPC stack */
+        KiRetireDpcListInDpcStack(Prcb, Prcb->DpcStack);
+        
+        /* Restore context */
+        Pcr->NtTib.ExceptionList = OldHandler;
+    }
+    
+    /* Re-enable interrupts */
+    _enable();
+    
+    /* Check for quantum end */
+    if (Prcb->QuantumEnd)
+    {
+        /* Handle quantum end */
+        Prcb->QuantumEnd = FALSE;
+        KiQuantumEnd();
+    }
+    else if (Prcb->NextThread)
+    {       
+        /* Capture current thread data */
+        OldThread = Prcb->CurrentThread;
+        NewThread = Prcb->NextThread;
+    
+        /* Set new thread data */
+        Prcb->NextThread = NULL;
+        Prcb->CurrentThread = NewThread;
+    
+        /* The thread is now running */
+        NewThread->State = Running;
+        OldThread->WaitReason = WrDispatchInt;
+        
+        /* Make the old thread ready */
+        KxQueueReadyThread(OldThread, Prcb);
+        
+        /* Swap to the new thread. FIXME: APC Bypass */
+        KiSwapContext(OldThread, NewThread);
+    }
+}
+
 
 /* EOF */
