eb84dde490c7fac250580d229825c6a51ccfb762
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-11-20 10:43:37 +0000

    [NTOSKRNL] Use constants instead of a gcc extension
    svn path=/branches/cmake-bringup/; revision=49655

--- ntoskrnl/mm/ARM3/miarm.h
+++ ntoskrnl/mm/ARM3/miarm.h
@@ -138,6 +138,14 @@ C_ASSERT(SYSTEM_PD_SIZE == PAGE_SIZE);
 #define PTE_EXECUTE_READWRITE   0x0
 #define PTE_EXECUTE_WRITECOPY   0x200
 #define PTE_PROTOTYPE           0x400
+
+//
+// State Flags
+//
+#define PTE_VALID               0x1
+#define PTE_ACCESSED            0x20
+#define PTE_DIRTY               0x40
+
 //
 // Cache flags
 //
