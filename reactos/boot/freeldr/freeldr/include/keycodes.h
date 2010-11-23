74e7e1825ef04b48a380cc59314427fc770669da
Sir Richard <sir_richard@svn.reactos.org>
2010-11-23 17:21:52 +0000

    [FREELDR]: Add more keycodes.
    svn path=/trunk/; revision=49749

--- reactos/boot/freeldr/freeldr/include/keycodes.h
+++ reactos/boot/freeldr/freeldr/include/keycodes.h
@@ -24,11 +24,14 @@
 #define	KEY_ENTER		0x0D
 #define KEY_BACKSPACE	0x08
 #define KEY_SPACE		0x20
+#define KEY_LEFTSHIFT   0x2A
+#define KEY_HOME        0x47
 #define KEY_UP			0x48
 #define KEY_DOWN		0x50
 #define KEY_LEFT		0x4B
 #define KEY_RIGHT		0x4D
 #define KEY_ESC			0x1B
+#define KEY_CAPS_LOCK	0x3A
 #define KEY_F1			0x3B
 #define KEY_F2			0x3C
 #define KEY_F3			0x3D
@@ -38,4 +41,7 @@
 #define KEY_F7			0x41
 #define KEY_F8			0x42
 #define KEY_F9			0x43
-#define KEY_F10			0x44
+#define KEY_F10         0x44
+#define KEY_KEYPAD_PLUS	0x4e
+#define KEY_END         0x4f
+#define KEY_SEND        0xE7
