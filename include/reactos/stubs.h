7b68fbc7bbfb6924835b8cbd74513131325235cf
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-12-01 21:39:38 +0000

    [CMAKE] Convert __wine_spec_unimplemented_stub to a macro
    svn path=/branches/cmake-bringup/; revision=49888

--- include/reactos/stubs.h
+++ include/reactos/stubs.h
@@ -7,11 +7,10 @@
 
 #include <debug.h>
 
-void __wine_spec_unimplemented_stub(const char *module, const char *function )
-{
-    ULONG_PTR args[2];
-
-    args[0] = (ULONG_PTR)module;
-    args[1] = (ULONG_PTR)function;
-    RaiseException( EXCEPTION_WINE_STUB, EH_NONCONTINUABLE, 2, args );
+#define __wine_spec_unimplemented_stub(module, function) \
+{ \
+    ULONG_PTR args[2]; \
+    args[0] = (ULONG_PTR)module; \
+    args[1] = (ULONG_PTR)function; \
+    RaiseException( EXCEPTION_WINE_STUB, EH_NONCONTINUABLE, 2, args ); \
 }
