91b7e707a904714f72d42c202f8793ea3c9d85c0
Timo Kreuzer <timo.kreuzer@reactos.org>
2010-12-04 12:54:17 +0000

    [CMAKE] "don't assign 1 to a BOOL variable"
    svn path=/branches/cmake-bringup/; revision=49947

--- config.cmake
+++ config.cmake
@@ -21,29 +21,29 @@ set (OPTIMIZE "1" CACHE STRING
   4 = -O2
   5 = -O3")
 
-set(DBG 1 CACHE BOOL
+set(DBG TRUE CACHE BOOL
 "Whether to compile for debugging.")
 
-set(KDBG 1 CACHE BOOL
+set(KDBG TRUE CACHE BOOL
 "Whether to compile in the integrated kernel debugger.")
 
-set(GDB 0 CACHE BOOL
+set(GDB FALSE CACHE BOOL
 "Whether to compile for debugging with GDB.
 If you don't use GDB, don't	enable this.")
 
-set(_WINKD_ 0 CACHE BOOL
+set(_WINKD_ FALSE CACHE BOOL
 "Whether to compile with the KD protocol.")
 
-set(_ELF_ 0 CACHE BOOL
+set(_ELF_ FALSE CACHE BOOL
 "Whether to compile support for ELF files.
 Do not enable unless you know what you're doing.")
 
-set(NSWPAT 0 CACHE BOOL
+set(NSWPAT FALSE CACHE BOOL
 "Whether to compile apps/libs with features covered software patents or not.
 If you live in a country where software patents are valid/apply, don't
 enable this (except they/you purchased a license from the patent owner).
 This settings is disabled (0) by default.")
 
-set(BUILD_MP 1 CACHE BOOL
+set(BUILD_MP TRUE CACHE BOOL
 "Whether to compile the multi processor versions for ntoskrnl and hal.")
 
