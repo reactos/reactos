d2a291cfadf629ef260110fb608c16922573fabc
Magnus Olsen <magnus@greatlord.com>
2010-12-11 13:09:06 +0000

    update edd_directx_global
    svn path=/branches/reactx/; revision=50010

--- reactos/include/reactos/drivers/directx/directxint.h
+++ reactos/include/reactos/drivers/directx/directxint.h
@@ -145,7 +145,7 @@ typedef struct _EDD_DIRECTDRAW_GLOBAL
 /* 0x298 */    DD_CALLBACKS ddCallbacks;                               // 0x298 <-- verified to match Windows XP, ddCallbacks
 /* 0x2C4 */    DD_SURFACECALLBACKS ddSurfaceCallbacks;                 // 0x2C4 <-- verified to match Windows XP, ddSurfaceCallbacks
 /* 0x304 */    DD_PALETTECALLBACKS ddPaletteCallbacks;                 // 0x304 <-- verified to match Windows XP, ddPaletteCallbacks
-/* 0x314 */    ULONG unk_314[46];
+/* 0x314 */    D3DNTHAL_GLOBALDRIVERDATA d3dNtGlobalDriverData;        // 0x304 <-- verified to match Windows XP, d3dNtGlobalDriverData
 /* 0x3D4 */    D3DNTHAL_CALLBACKS d3dNtHalCallbacks;
 /* 0x460 */    ULONG unk_460[9];
 /* 0x47C */    D3DNTHAL_CALLBACKS2 d3dNtHalCallbacks2;
