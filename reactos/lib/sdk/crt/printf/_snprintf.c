d50bc6057e83f8d2f08152bcd561d6914adff267
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-01-01 13:50:07 +0000

    [CRT] Fix bugs in new printf implementation: use buffer size in _snprintf and check for the right failure return value of streamout_char.
    svn path=/trunk/; revision=50251

--- reactos/lib/sdk/crt/printf/_snprintf.c
+++ reactos/lib/sdk/crt/printf/_snprintf.c
@@ -23,7 +23,7 @@ _snprintf(char *buffer, size_t count, const char *format, ...)
     stream._base = buffer;
     stream._ptr = stream._base;
     stream._charbuf = 0;
-    stream._bufsiz = (unsigned long)-1;
+    stream._bufsiz = count;
     stream._cnt = stream._bufsiz;
     stream._flag = 0;
     stream._tmpfname = 0;
@@ -31,7 +31,7 @@ _snprintf(char *buffer, size_t count, const char *format, ...)
     va_start(argptr, format);
     result = streamout(&stream, format, argptr);
     va_end(argptr);
-    
+
     *stream._ptr = '\0';
     return result;
 }
