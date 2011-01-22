83fb618aba253e5f1025b074d26b10ae7a5ef0bb
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-01-22 16:26:37 +0000

    [CMAKE] Don't set lib pathes in msc toolchain file. Fixes amd64
    svn path=/branches/cmake-bringup/; revision=50464

--- toolchain-msvc.cmake
+++ toolchain-msvc.cmake
@@ -1,25 +1,4 @@
 
-if(NOT ARCH)
-  set(ARCH i386)
-endif()
-
-# WDK support
-string(REPLACE * ${ARCH} ATL_LIB_PATH $ENV{ATL_LIB_PATH})
-string(REPLACE * ${ARCH} CRT_LIB_PATH $ENV{CRT_LIB_PATH})
-string(REPLACE * ${ARCH} DDK_LIB_PATH $ENV{DDK_LIB_PATH})
-string(REPLACE * ${ARCH} KMDF_LIB_PATH $ENV{KMDF_LIB_PATH})
-string(REPLACE * ${ARCH} MFC_LIB_PATH $ENV{MFC_LIB_PATH})
-string(REPLACE * ${ARCH} SDK_LIB_PATH $ENV{SDK_LIB_PATH})
-
-link_directories(
-#${ATL_LIB_PATH}
-                 ${CRT_LIB_PATH}
-                 ${DDK_LIB_PATH}
-#                 ${IFSKIT_LIB_PATH}
-#                 ${KMDF_LIB_PATH}
-#                 ${MFC_LIB_PATH}
-                 ${SDK_LIB_PATH})
-
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR i686)
@@ -34,7 +13,7 @@ SET(CMAKE_IDL_COMPILER midl)
 SET(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> <DEFINES> /I${REACTOS_SOURCE_DIR}/include/psdk /I${REACTOS_BINARY_DIR}/include/psdk /I${REACTOS_SOURCE_DIR}/include /I${REACTOS_SOURCE_DIR}/include/reactos /I${REACTOS_BINARY_DIR}/include/reactos /I${REACTOS_SOURCE_DIR}/include/reactos/wine /I${REACTOS_SOURCE_DIR}/include/crt /I${REACTOS_SOURCE_DIR}/include/crt/mingw32 /fo <OBJECT> <SOURCE>")
 SET(CMAKE_IDL_COMPILE_OBJECT "<CMAKE_IDL_COMPILER> <FLAGS> <DEFINES> /win32 /Dstrict_context_handle= /h <OBJECT> <SOURCE>")
 
-SET(CMAKE_ASM_COMPILE_OBJECT 
+SET(CMAKE_ASM_COMPILE_OBJECT
     "<CMAKE_C_COMPILER> /nologo /X /I${REACTOS_SOURCE_DIR}/include/asm /I${REACTOS_BINARY_DIR}/include/asm <FLAGS> <DEFINES> /D__ASM__ /D_USE_ML /EP /c <SOURCE> > <OBJECT>.tmp"
     "<CMAKE_ASM_COMPILER> /nologo /Fo<OBJECT> /c /Ta <OBJECT>.tmp")
 
