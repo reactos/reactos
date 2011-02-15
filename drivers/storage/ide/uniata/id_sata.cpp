8e32abe55ce19dff25d7298bb8d0de44b0a62f81
Giannis Adamopoulos <gadamopoulos@reactos.org>
2011-02-15 17:01:38 +0000

    [uniata] - Fix some compilation issues with msc
    svn path=/branches/cmake-bringup/; revision=50711

--- drivers/storage/ide/uniata/id_sata.cpp
+++ drivers/storage/ide/uniata/id_sata.cpp
@@ -237,7 +237,8 @@ UniataSataEvent(
     return FALSE;
 } // end UniataSataEvent()
 
-ULONG
+ULONG 
+NTAPI
 UniataSataReadPort4(
     IN PHW_CHANNEL chan,
     IN ULONG io_port_ndx,
@@ -302,6 +303,7 @@ UniataSataReadPort4(
 } // end UniataSataReadPort4()
 
 VOID
+NTAPI
 UniataSataWritePort4(
     IN PHW_CHANNEL chan,
     IN ULONG io_port_ndx,
