a2453433f1dee7587bcb0cc421693558e4655235
Johannes Anderwald <johannes.anderwald@reactos.org>
2011-03-10 12:41:50 +0000

    [MSAFD] - Correct calculating PollBufferSize - Found by DPH
    svn path=/trunk/; revision=51007

--- reactos/dll/win32/msafd/misc/dllmain.c
+++ reactos/dll/win32/msafd/misc/dllmain.c
@@ -907,7 +907,7 @@ WSPSelect(IN int nfds,
     }
 
     PollInfo->HandleCount = j;
-    PollBufferSize = ((PCHAR)&PollInfo->Handles[j+1]) - ((PCHAR)PollInfo);
+	PollBufferSize = sizeof(AFD_POLL_INFO) + (PollInfo->HandleCount > 1 ? sizeof(HANDLE) * ( PollInfo->HandleCount - 1) : 0);
 
     /* Send IOCTL */
     Status = NtDeviceIoControlFile((HANDLE)PollInfo->Handles[0].Handle,
