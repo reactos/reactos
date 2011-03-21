01ecf02e26cfef98698c80acf26ad50a9c517e8e
James Tabor <james.tabor@reactos.org>
2011-03-21 02:20:24 +0000

    [Win32k] - Setup RectInRegion for internal use.
    svn path=/trunk/; revision=51102

--- reactos/subsystems/win32/win32k/objects/rect.c
+++ reactos/subsystems/win32/win32k/objects/rect.c
@@ -16,7 +16,6 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-/* $Id$ */
 
 #include <win32k.h>
 
