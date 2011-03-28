cabc5ed079cdea8abe463a360fde15b7b6e3cb8b
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-03-28 11:59:37 +0000

    [CMAKE/ROSSYM] Implement some dummy functions for MSVC. ntoskrnl links now.
    svn path=/branches/cmake-bringup/; revision=51182

--- lib/rossym/dummy.c
+++ lib/rossym/dummy.c
@@ -1,1 +1,32 @@
-/* This file is intentionally left empty */
+
+#include <ntddk.h>
+#include <reactos/rossym.h>
+
+#define NDEBUG
+#include <debug.h>
+
+BOOLEAN
+RosSymCreateFromFile(PVOID FileContext, PROSSYM_INFO *RosSymInfo)
+{
+    return FALSE;
+}
+
+VOID
+RosSymDelete(PROSSYM_INFO RosSymInfo)
+{
+}
+
+BOOLEAN
+RosSymGetAddressInformation(PROSSYM_INFO RosSymInfo,
+                            ULONG_PTR RelativeAddress,
+                            ULONG *LineNumber,
+                            char *FileName,
+                            char *FunctionName)
+{
+    return FALSE;
+}
+
+VOID
+RosSymInitKernelMode(VOID)
+{
+}
