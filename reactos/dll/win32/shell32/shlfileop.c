ed27eba84377638a4e7e993d56c4881a1228b53d
Olaf Siejka <caemyr@openmailbox.org>
2011-03-29 21:41:14 +0000

    [SHELL32] - Fix compile warning (uninitialized use). Based on patch by Edijs Kolesnikovics <terminedijs at yahoo dot com> slightly modified by Aleksiej See issue #6058 for more details.
    svn path=/trunk/; revision=51196

--- reactos/dll/win32/shell32/shlfileop.c
+++ reactos/dll/win32/shell32/shlfileop.c
@@ -584,7 +584,7 @@ BOOL
 QueueFile(
     FILE_OPERATION_CONTEXT * Context)
 {
-    FILE_ENTRY * from, *to;
+    FILE_ENTRY * from, *too = NULL;
     BOOL bRet = FALSE;
 
     if (Context->Index >= Context->from->dwNumFiles)
