dc2b96f14f8dc5b5c9265ad860a597fb8fb70fa6
Gabriel Ilardi <gabrielilardi@hotmail.it>
2011-04-19 09:15:05 +0000

    [winetests] Add new lines to test results for kernel32/dosdev.c
    svn path=/trunk/; revision=51400

--- rostests/winetests/kernel32/dosdev.c
+++ rostests/winetests/kernel32/dosdev.c
@@ -33,13 +33,13 @@ static void test_DefineDosDeviceA1(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp");
-    ok(Result, "Failed to subst drive using lowercase drive letter");
+    ok(Result, "Failed to subst drive using lowercase drive letter\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp");
-    ok(Result, "Failed to remove subst drive using lowercase drive letter");
+    ok(Result, "Failed to remove subst drive using lowercase drive letter\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 static void test_DefineDosDeviceA2(void)
@@ -50,13 +50,13 @@ static void test_DefineDosDeviceA2(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp");
-    ok(!Result, "Subst drive using trailing path seperator");
+    ok(!Result, "Subst drive using trailing path seperator\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp");
-    ok(!Result, "Subst drive using trailing path seperator");
+    ok(!Result, "Subst drive using trailing path seperator\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present when it should not be created in the first place");
+    ok(!Result, "Subst drive is present when it should not be created in the first place\n");
 }
 
 static void test_DefineDosDeviceA3(void)
@@ -67,13 +67,13 @@ static void test_DefineDosDeviceA3(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp");
-    ok(Result, "Failed to subst drive using non-DOS drive name");
+    ok(Result, "Failed to subst drive using non-DOS drive name\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp");
-    ok(Result, "Failed to subst drive using non-DOS drive name");
+    ok(Result, "Failed to subst drive using non-DOS drive name\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 static void test_DefineDosDeviceA4(void)
@@ -84,13 +84,13 @@ static void test_DefineDosDeviceA4(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION, Drive, NULL);
-    ok(Result, "Failed to remove subst drive using NULL Target name");
+    ok(Result, "Failed to remove subst drive using NULL Target name\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 static void test_DefineDosDeviceA5(void)
@@ -101,31 +101,31 @@ static void test_DefineDosDeviceA5(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp1");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp2");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp3");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp1");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp3") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp3") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp2");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp3") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp3") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp3");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 static void test_DefineDosDeviceA6(void)
@@ -136,31 +136,31 @@ static void test_DefineDosDeviceA6(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp1");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp2");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp3");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp3");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp2") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp2") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp2");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp1") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp1") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp1");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 static void test_DefineDosDeviceA7(void)
@@ -171,49 +171,49 @@ static void test_DefineDosDeviceA7(void)
     BOOL Result;
 
     Result = DefineDosDeviceA(0, Drive, "C:\\temp1");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp2");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp3");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp4");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
     Result = DefineDosDeviceA(0, Drive, "C:\\temp5");
-    ok(Result, "Failed to subst drive");
+    ok(Result, "Failed to subst drive\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp2");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp5") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp5") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp5");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp4") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp4") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp1");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
-        ok((_stricmp(Target, "\\??\\C:\\temp4") == 0), "Subst drive is not pointing to correct target");
+        ok((_stricmp(Target, "\\??\\C:\\temp4") == 0), "Subst drive is not pointing to correct target\n");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp3");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(Result, "Failed to query subst drive");
+    ok(Result, "Failed to query subst drive\n");
     if (Result)
         ok((_stricmp(Target, "\\??\\C:\\temp4") == 0), "Subst drive is not pointing to correct target");
 
     Result = DefineDosDeviceA(DDD_REMOVE_DEFINITION | DDD_EXACT_MATCH_ON_REMOVE, Drive, "C:\\temp4");
-    ok(Result, "Failed to remove subst drive");
+    ok(Result, "Failed to remove subst drive\n");
 
     Result = QueryDosDeviceA(Drive, Target, MAX_PATH);
-    ok(!Result, "Subst drive is present even after remove attempt");
+    ok(!Result, "Subst drive is present even after remove attempt\n");
 }
 
 START_TEST(dosdev)
