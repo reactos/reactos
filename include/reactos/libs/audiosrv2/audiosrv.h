51726a97851b8ec806d417cace676e1307ae8df3
Neeraj Yadav <neer4j.iit.delhi@gmail.com>
2011-04-22 17:27:40 +0000

    Add Audio Server Headers
    svn path=/branches/nyadav-audio-branch/; revision=51433

---
+++ include/reactos/libs/audiosrv2/audiosrv.h
@@ -0,0 +1,47 @@
+#include <windows.h>
+#include <memory.h>
+
+#ifndef _PORTINTERFACE_H
+#define _PORTINTERFACE_H
+
+#define MIXER getmixerengine()
+
+typedef struct PortStream
+{
+	int volume;
+	double freq;
+	int channels;
+	int bitspersample;
+	DWORD channelmask;
+	HANDLE thread;
+	struct PortStream * next;
+} PortStream;
+
+typedef struct MixerEngine
+{
+	int mastervolume;
+	int mute;
+	char dead;
+	double masterfreq;
+	int masterchannels;
+	DWORD masterchannelmask;
+	int masterbitspersample;
+	int workingbuffer;
+	PSHORT masterdoublebuf[2];
+	HANDLE mixerthread;
+	HANDLE playerthread;
+	HANDLE EventPool[2];//0=Played,1=Ready
+	PortStream * portstream;
+} MixerEngine;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport) MixerEngine * getmixerengine();
+
+#ifdef __cplusplus 
+}
+#endif
+
+#endif
