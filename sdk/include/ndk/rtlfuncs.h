62912d95fc1c9f6c1199047c680d4d02d6584064
Samuel Serapion <samuel.serapion@gmail.com>
2011-05-24 07:17:44 +0000

    12 [NDK]
    add forgotten function definition. please merge to trunk.
    
    svn path=/branches/sspi-bringup/; revision=51875

--- sdk/include/ndk/rtlfuncs.h
+++ sdk/include/ndk/rtlfuncs.h
@@ -1882,6 +1882,14 @@ RtlUnicodeStringToOemString(
 NTSYSAPI
 NTSTATUS
 NTAPI
+RtlUnicodeStringToCountedOemString(
+  IN OUT POEM_STRING DestinationString,
+  IN PCUNICODE_STRING SourceString,
+  IN BOOLEAN AllocateDestinationString);
+
+NTSYSAPI
+NTSTATUS
+NTAPI
 RtlUpcaseUnicodeToOemN(
     PCHAR OemString,
     ULONG OemSize,
