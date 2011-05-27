bda4efccd45367cb88e92209c84bc554fca4e3a4
Neeraj Yadav <neer4j.iit.delhi@gmail.com>
2011-05-27 09:40:19 +0000

    [AUDSRV] Add a client to test audio server
    svn path=/branches/nyadav-audio-branch/; revision=51954

---
+++ drivers/wdm/audio/backpln/audclient/audclient.c
@@ -0,0 +1,12 @@
+#define WIN32_NO_STATUS
+#define _KSDDK_
+
+#include <audsrvapi.h>
+#include <stdio.h>
+int
+__cdecl
+wmain(int argc, char* argv[])
+{
+	printf("ReactOS Audio Mixer Sample Client.\n");
+    return 0;
+}
