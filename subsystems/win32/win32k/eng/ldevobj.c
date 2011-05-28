12c06258d0365803302a79ad49661797ac7818f0
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-05-28 16:12:40 +0000

    [WIN32K] - Fix EngMapFontFileFD (return adress and size) - Fix memory leak in EngUnloadImage
    svn path=/branches/GSoC_2011/GdiFontDriver/; revision=51977

--- subsystems/win32/win32k/eng/ldevobj.c
+++ subsystems/win32/win32k/eng/ldevobj.c
@@ -101,6 +101,7 @@ LDEVOBJ_vFreeLDEV(PLDEVOBJ pldev)
 {
     /* Make sure we don't have a driver loaded */
     ASSERT(pldev && pldev->pGdiDriverInfo == NULL);
+    ASSERT(pldev->cRefs == 0);
 
     /* Free the memory */
     ExFreePoolWithTag(pldev, GDITAG_LDEV);
@@ -475,8 +476,9 @@ EngUnloadImage(
         if (pldev->pldevNext)
             pldev->pldevNext->pldevPrev = pldev->pldevPrev;
 
-        /* Unload the image */
+        /* Unload the image and free the LDEV */
         LDEVOBJ_vUnloadImage(pldev);
+        LDEVOBJ_vFreeLDEV(pldev);
     }
 
     /* Unlock loader */
