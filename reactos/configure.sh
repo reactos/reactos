e39c778ae1c527ebe2e84ff9af326035132fc562
Sylvain Petreolle <spetreolle@yahoo.fr>
2011-06-11 17:03:03 +0000

    [CMAKE] configure : Remove reactos cache. Fix echo syntax.
    svn path=/trunk/; revision=52192

--- reactos/configure.sh
+++ reactos/configure.sh
@@ -10,7 +10,6 @@ ARCH=$ROS_ARCH
 REACTOS_SOURCE_DIR=$(cd `dirname $0` && pwd)
 REACTOS_OUTPUT_PATH=output-$BUILD_ENVIRONMENT-$ARCH
 
-echo ARCH $ARCH ROS $ROS_ARCH
 if [ "$REACTOS_SOURCE_DIR" == "$PWD" ]
 then
   echo Creating directories in $REACTOS_OUTPUT_PATH
@@ -31,8 +30,14 @@ fi
 REACTOS_BUILD_TOOLS_DIR="$PWD"
 cmake -G "Unix Makefiles" -DARCH=$ARCH "$REACTOS_SOURCE_DIR"
 
+echo Preparing reactos...
 cd ../reactos
+if [ -f CMakeCache.txt ]
+then
+  rm -f CMakeCache.txt
+fi
+
 cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain-mingw32.cmake -DARCH=$ARCH -DREACTOS_BUILD_TOOLS_DIR="$REACTOS_BUILD_TOOLS_DIR" "$REACTOS_SOURCE_DIR"
 
-echo Configure script complete! Enter directories and execute appropriate build commands(ex: make, makex, etc...).
+echo Configure script complete! Enter directories and execute appropriate build commands\(ex: make, makex, etc...\).
 
