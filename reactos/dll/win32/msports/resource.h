f17bf3f6a664c532797f1bfa53a70202e77043e1
Eric Kohl <eric.kohl@reactos.org>
2011-06-13 09:27:23 +0000

    [MSPORTS] Finish the serial port settings dialog.
    svn path=/trunk/; revision=52208

--- reactos/dll/win32/msports/resource.h
+++ reactos/dll/win32/msports/resource.h
@@ -1,3 +1,9 @@
 
 #define IDD_SERIALSETTINGS                200
-#define IDC_SERIALBITSPERSECOND           201
+#define IDC_SERIAL_BITSPERSECOND          201
+#define IDC_SERIAL_DATABITS               202
+#define IDC_SERIAL_PARITY                 203
+#define IDC_SERIAL_STOPBITS               204
+#define IDC_SERIAL_FLOWCONTROL            205
+#define IDC_SERIAL_ADVANCED               206
+#define IDC_SERIAL_RESTORE                207
