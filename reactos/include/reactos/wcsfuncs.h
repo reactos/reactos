6fc52906e5e5c16c24ace013b05ebcabf9d5fdd2
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-06-15 11:42:37 +0000

    forgot to commit this file
    svn path=/trunk/; revision=52243

---
+++ reactos/include/reactos/wcsfuncs.h
@@ -0,0 +1,23 @@
+/*
+  PROJECT:    ReactOS
+  LICENSE:    GPL v2 or any later version
+  FILE:       include/host/wcsfuncs.h
+  PURPOSE:    Header for the "host_wcsfuncs" static library
+  COPYRIGHT:  Copyright 2008 Colin Finck <mail@colinfinck.de>
+*/
+
+#ifndef _HOST_WCSFUNCS_H
+#define _HOST_WCSFUNCS_H
+
+/* Map str*W functions to wcs* function */
+
+#define isspaceW iswspace
+#define strchrW  wcschr
+#define strcmpiW _wcsicmp
+#define strcpyW  wcscpy
+#define strlenW  wcslen
+#define strncmpW wcsncmp
+#define strtolW  wcstol
+#define strtoulW wcstoul
+
+#endif
