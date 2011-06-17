08542a70e0e7e46e4b4647cbc1cab31323691dca
Sir Richard <sir_richard@svn.reactos.org>
2011-06-17 12:54:05 +0000

    Patch by Anton Yarotsky: [SACDRV]: Implement memory manager. [SACDRV]: Define debugging macros.
    svn path=/trunk/; revision=52311

--- reactos/drivers/sac/driver/sacdrv.h
+++ reactos/drivers/sac/driver/sacdrv.h
@@ -9,6 +9,46 @@
 /* INCLUDES *******************************************************************/
 #include <ntddk.h>
 
+#define SAC_DBG_ENTRY_EXIT		0x01
+#define SAC_DBG_MM				0x1000
+
+#define SAC_DBG(x, ...)						\
+	if (SACDebug & x)						\
+	{										\
+		DbgPrint("SAC %s: ", __FUNCTION__);	\
+		DbgPrint(__VA_ARGS__);				\
+	}
+
+//Rcp? - sacdrv.sys - SAC Driver (Headless)
+//RcpA - sacdrv.sys -     Internal memory mgr alloc block
+//RcpI - sacdrv.sys -     Internal memory mgr initial heap block
+//RcpS - sacdrv.sys -     Security related block
+#define GENERIC_TAG				'?pcR'
+#define ALLOC_BLOCK_TAG			'ApcR'
+#define INITIAL_BLOCK_TAG		'IpcR'
+#define SECURITY_BLOCK_TAG		'SpcR'
+#define FREE_POOL_TAG			'FpcR'
+
+#define LOCAL_MEMORY_SIGNATURE 	'SSEL'
+#define GLOBAL_MEMORY_SIGNATURE	'DAEH'
+
+#define SAC_MEMORY_LIST_SIZE	(1 * 1024 * 1024)
+
+typedef struct _SAC_MEMORY_ENTRY
+{
+	ULONG Signature;
+	ULONG Tag;
+	ULONG Size;
+} SAC_MEMORY_ENTRY, *PSAC_MEMORY_ENTRY;
+
+typedef struct _SAC_MEMORY_LIST
+{
+	ULONG Signature;
+	PSAC_MEMORY_ENTRY LocalDescriptor;
+	ULONG Size;
+	struct _SAC_MEMORY_LIST* Next;
+} SAC_MEMORY_LIST, *PSAC_MEMORY_LIST;
+
 typedef enum _SAC_CHANNEL_TYPE
 {
 	VtUtf8,
@@ -116,3 +156,6 @@ typedef struct _SAC_CHANNEL_ATTRIBUTES
 	PKEVENT RedrawEvent;
 	GUID ChannelId;	
 } SAC_CHANNEL_ATTRIBUTES, *PSAC_CHANNEL_ATTRIBUTES;
+
+extern ULONG SACDebug;
+
