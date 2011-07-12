eb23bde45832cb95f2b0ce35f44f5a7505376322
Giannis Adamopoulos <gadamopoulos@reactos.org>
2011-07-12 21:25:51 +0000

    [apihooksrv] - Commit a test utility that can install user api hooks and give for the first time real themes support. This is a temporary test program till the same functionality is integrated to winlogon. To use it someone has to activate themes with themecfg and then run "apihook uxtheme.dll". Also note that it is possible to use this tool in windows for testing purposes
    svn path=/branches/GSoC_2011/ThemesSupport/; revision=52665

---
+++ dll/win32/uxtheme/apihooksrv/apihooksrv.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+typedef BOOL (WINAPI * PTHEMESHOOKINSTALL) ();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    HMODULE hmodRosTheme;
+    PTHEMESHOOKINSTALL fnInstall, fnRemove;
+    BOOL ret;
+    DWORD info = BSM_APPLICATIONS;
+
+    if ( argc != 2 )
+    {
+        return 0;
+    }
+
+
+      //hmodRosTheme = LoadLibrary(L"C:\\Reactos\\src\\themes-priv\\output-MinGW-i386\\reactos\\dll\\win32\\uxtheme\\rostheme.dll");
+    hmodRosTheme = LoadLibrary(argv[1]);
+    if(!hmodRosTheme)
+    {
+        printf("failed to load rostheme\n");
+        return 0;
+    }
+
+    fnInstall = GetProcAddress(hmodRosTheme, (LPCSTR)34);
+    fnRemove = GetProcAddress(hmodRosTheme, (LPCSTR)35);
+
+    if(!fnInstall)
+    {
+        printf("failed to get ThemeHooksInstall\n");
+        return 0;
+    }
+
+    if(!fnRemove)
+    {
+        printf("failed to get ThemeHooksRemove\n");
+        return 0;
+    }
+
+    ret = fnInstall();
+    printf("ThemeHooksInstall returned %d\n", ret);
+    
+    BroadcastSystemMessage(BSF_POSTMESSAGE, &info,WM_SETFOCUS,0,0);
+
+    Sleep(36000);
+
+    ret = fnRemove();
+    printf("ThemeHooksRemove returned %d\n", ret);
+
+    BroadcastSystemMessage(BSF_POSTMESSAGE, &info,WM_SETFOCUS,0,0);
+
+	return 0;
+}
+
