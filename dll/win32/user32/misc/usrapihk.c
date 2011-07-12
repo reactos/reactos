aeebfa7e0e9f775367f51c1c08fb23dec10739e3
Giannis Adamopoulos <gadamopoulos@reactos.org>
2011-07-12 09:14:24 +0000

    [user32] - Implement UnregisterUserApiHook - Fix a warning
    svn path=/branches/GSoC_2011/ThemesSupport/; revision=52651

--- dll/win32/user32/misc/usrapihk.c
+++ dll/win32/user32/misc/usrapihk.c
@@ -259,7 +259,7 @@ InitUserApiHook(HINSTANCE hInstance, USERAPIHOOKPROC pfn)
 
   RtlEnterCriticalSection(&gcsUserApiHook);
 
-  if (!pfn(uahLoadInit,(ULONG_PTR)&uah) ||  // Swap data, User32 to and Uxtheme from!
+  if (!pfn(uahLoadInit,&uah) ||  // Swap data, User32 to and Uxtheme from!
        uah.ForceResetUserApiHook != (FARPROC)ForceResetUserApiHook ||
        uah.size <= 0 )
   {
@@ -391,7 +391,5 @@ BOOL WINAPI RegisterUserApiHook(PUSERAPIHOOKINFO puah)
  */
 BOOL WINAPI UnregisterUserApiHook(VOID)
 {
-  // Direct call to Win32k! Here only as a prototype.....
-  UNIMPLEMENTED;
-  return FALSE;
+    return NtUserUnregisterUserApiHook();
 }
