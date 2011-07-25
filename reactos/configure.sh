3b813c314ddab1207470ea250589ad81e0a72134
Amine Khaldi <amine.khaldi@reactos.org>
2011-07-25 19:08:52 +0000

    [CMAKE] * Add PCH variable (disabled by default) to configure scripts. * Add ccache variable to unix configure script.
    svn path=/trunk/; revision=52866

--- reactos/configure.sh
+++ reactos/configure.sh
@@ -37,7 +37,7 @@ then
   rm -f CMakeCache.txt
 fi
 
-cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain-gcc.cmake -DARCH=$ARCH -DREACTOS_BUILD_TOOLS_DIR="$REACTOS_BUILD_TOOLS_DIR" "$REACTOS_SOURCE_DIR"
+cmake -G "Unix Makefiles" -DENABLE_CCACHE=0 -DPCH=0 -DCMAKE_TOOLCHAIN_FILE=toolchain-gcc.cmake -DARCH=$ARCH -DREACTOS_BUILD_TOOLS_DIR="$REACTOS_BUILD_TOOLS_DIR" "$REACTOS_SOURCE_DIR"
 
 echo Configure script complete! Enter directories and execute appropriate build commands\(ex: make, makex, etc...\).
 
