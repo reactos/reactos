5d382481442ad7c62eec7797ed50ed1ad39ab6a1
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-08-10 16:11:59 +0000

    [ROSSYM_NEW] Add RosSymAggregate and RosSymFreeAggregate stubs for MSVC
    svn path=/trunk/; revision=53164

--- reactos/lib/rossym_new/dummy.c
+++ reactos/lib/rossym_new/dummy.c
@@ -8,17 +8,20 @@
 BOOLEAN
 RosSymCreateFromFile(PVOID FileContext, PROSSYM_INFO *RosSymInfo)
 {
+    UNIMPLEMENTED;
     return FALSE;
 }
 
 VOID
 RosSymDelete(PROSSYM_INFO RosSymInfo)
 {
+    UNIMPLEMENTED;
 }
 
 VOID
 RosSymFreeInfo(PROSSYM_LINEINFO LineInfo)
 {
+    UNIMPLEMENTED;
 }
 
 BOOLEAN
@@ -26,15 +29,32 @@ RosSymGetAddressInformation(PROSSYM_INFO RosSymInfo,
                             ULONG_PTR RelativeAddress,
                             PROSSYM_LINEINFO RosSymLineInfo)
 {
+    UNIMPLEMENTED;
     return FALSE;
 }
 
 VOID
 RosSymInit(PROSSYM_CALLBACKS Callbacks)
 {
+    UNIMPLEMENTED;
 }
 
 VOID
 RosSymInitKernelMode(VOID)
 {
+    UNIMPLEMENTED;
 }
+
+BOOLEAN
+RosSymAggregate(PROSSYM_INFO RosSymInfo, PCHAR Type, PROSSYM_AGGREGATE Aggregate)
+{
+    UNIMPLEMENTED;
+    return FALSE;
+}
+
+VOID
+RosSymFreeAggregate(PROSSYM_AGGREGATE Aggregate)
+{
+    UNIMPLEMENTED;
+}
+
