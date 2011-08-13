9ace1afb580b156e79f4e744e312039f4fbd56be
Matthias Kupfer <mkupfer@reactos.org>
2011-08-13 18:57:33 +0000

    move clean the screen to a later point
    svn path=/trunk/; revision=53208

--- reactos/ntoskrnl/ex/init.c
+++ reactos/ntoskrnl/ex/init.c
@@ -1918,9 +1918,6 @@ Phase1InitializationDiscard(IN PVOID Context)
     /* Update progress bar */
     InbvUpdateProgressBar(100);
 
-    /* Clean the screen */
-    if (InbvBootDriverInstalled) FinalizeBootLogo();
-
     /* Allow strings to be displayed */
     InbvEnableDisplayString(TRUE);
 
@@ -1958,6 +1955,9 @@ Phase1InitializationDiscard(IN PVOID Context)
                         (PVOID*)&ProcessParameters,
                         &Size,
                         MEM_RELEASE);
+
+    /* Clean the screen */
+    if (InbvBootDriverInstalled) FinalizeBootLogo();
 
     /* Increase init phase */
     ExpInitializationPhase++;
