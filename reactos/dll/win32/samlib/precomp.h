b27131c18048229294995d1e5398676f0028384c
Amine Khaldi <amine.khaldi@reactos.org>
2011-08-14 16:59:43 +0000

    [SAMLIB] * Include NDK headers on-demand. * Introduce a PCH.
    svn path=/trunk/; revision=53233

---
+++ reactos/dll/win32/samlib/precomp.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+#include <windows.h>
+#include <winerror.h>
+#define NTOS_MODE_USER
+#include <ndk/rtlfuncs.h>
+
+#include "debug.h"
