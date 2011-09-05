f627019d71b1002814b2ac4adf5998a89b7ebd17
Timo Kreuzer <timo.kreuzer@reactos.org>
2011-09-05 15:20:07 +0000

    [HAL] - Use functions instead of macros for irq<->vector<->irql translation, this will be neccessary, once apic support is there - Disable mp hal in rbuild for now, its broken anyway
    svn path=/trunk/; revision=53600

--- reactos/hal/halx86/include/halirq.h
+++ reactos/hal/halx86/include/halirq.h
@@ -4,29 +4,19 @@
 
 #pragma once
 
-#ifdef CONFIG_SMP
+UCHAR
+FASTCALL
+HalpIrqToVector(UCHAR Irq);
 
-#define FIRST_DEVICE_VECTOR	(0x30)
-#define FIRST_SYSTEM_VECTOR	(0xef)
+KIRQL
+FASTCALL
+HalpVectorToIrql(UCHAR Vector);
 
-#define IRQ_BASE		FIRST_DEVICE_VECTOR
-#define	NR_IRQS			(FIRST_SYSTEM_VECTOR - FIRST_DEVICE_VECTOR)
+UCHAR
+FASTCALL
+HalpVectorToIrq(UCHAR Vector);
 
-/*
- * FIXME:
- *   This does not work if we have more than 24 IRQs (ie. more than one I/O APIC)
- */
-#define VECTOR2IRQ(vector)	(23 - (vector - IRQ_BASE) / 8)
-#define VECTOR2IRQL(vector)	(PROFILE_LEVEL - VECTOR2IRQ(vector))
-#define IRQ2VECTOR(irq)		(((23 - (irq)) * 8) + FIRST_DEVICE_VECTOR)
+#define VECTOR2IRQ(vector)	HalpVectorToIrq(vector)
+#define VECTOR2IRQL(vector)	HalpVectorToIrql(vector)
+#define IRQ2VECTOR(irq)		HalpIrqToVector(irq)
 
-#else
-
-#define IRQ_BASE		(0x30)
-#define	NR_IRQS			(16)
-
-#define VECTOR2IRQ(vector)	((vector) - IRQ_BASE)
-#define VECTOR2IRQL(vector)	(PROFILE_LEVEL - VECTOR2IRQ(vector))
-#define IRQ2VECTOR(irq)		((irq) + IRQ_BASE)
-
-#endif
