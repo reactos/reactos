b63c511bbfebd65e465c2284a780d734be792c46
Rafal Harabien <rafalh@reactos.org>
2011-10-09 20:41:09 +0000

    [WIN32K] - Formatting... No code change.
    svn path=/trunk/; revision=54066

--- reactos/subsystems/win32/win32k/ntuser/hotkey.c
+++ reactos/subsystems/win32/win32k/ntuser/hotkey.c
@@ -28,9 +28,9 @@ NTSTATUS
 NTAPI
 InitHotkeyImpl(VOID)
 {
-   InitializeListHead(&gHotkeyList);
+    InitializeListHead(&gHotkeyList);
 
-   return STATUS_SUCCESS;
+    return STATUS_SUCCESS;
 }
 
 #if 0 //not used
@@ -38,7 +38,7 @@ NTSTATUS FASTCALL
 CleanupHotKeys(VOID)
 {
 
-   return STATUS_SUCCESS;
+    return STATUS_SUCCESS;
 }
 #endif
 
@@ -53,7 +53,7 @@ UINT FASTCALL
 IntGetModifiers(PBYTE pKeyState)
 {
     UINT fModifiers = 0;
-    
+
     if (IS_KEY_DOWN(pKeyState, VK_SHIFT))
         fModifiers |= MOD_SHIFT;
 
@@ -72,48 +72,48 @@ IntGetModifiers(PBYTE pKeyState)
 VOID FASTCALL
 UnregisterWindowHotKeys(PWND Window)
 {
-   PHOT_KEY_ITEM HotKeyItem, tmp;
+    PHOT_KEY_ITEM HotKeyItem, tmp;
 
-   LIST_FOR_EACH_SAFE(HotKeyItem, tmp, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if (HotKeyItem->hWnd == Window->head.h)
-      {
-         RemoveEntryList (&HotKeyItem->ListEntry);
-         ExFreePool (HotKeyItem);
-      }
-   }
+    LIST_FOR_EACH_SAFE(HotKeyItem, tmp, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (HotKeyItem->hWnd == Window->head.h)
+        {
+            RemoveEntryList(&HotKeyItem->ListEntry);
+            ExFreePool(HotKeyItem);
+        }
+    }
 }
 
 VOID FASTCALL
 UnregisterThreadHotKeys(struct _ETHREAD *Thread)
 {
-   PHOT_KEY_ITEM HotKeyItem, tmp;
+    PHOT_KEY_ITEM HotKeyItem, tmp;
 
-   LIST_FOR_EACH_SAFE(HotKeyItem, tmp, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if (HotKeyItem->Thread == Thread)
-      {
-         RemoveEntryList (&HotKeyItem->ListEntry);
-         ExFreePool (HotKeyItem);
-      }
-   }
+    LIST_FOR_EACH_SAFE(HotKeyItem, tmp, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (HotKeyItem->Thread == Thread)
+        {
+            RemoveEntryList(&HotKeyItem->ListEntry);
+            ExFreePool(HotKeyItem);
+        }
+    }
 
 }
 
 PHOT_KEY_ITEM FASTCALL
 IsHotKey(UINT fsModifiers, WORD wVk)
 {
-   PHOT_KEY_ITEM pHotKeyItem;
+    PHOT_KEY_ITEM pHotKeyItem;
 
-   LIST_FOR_EACH(pHotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if (pHotKeyItem->fsModifiers == fsModifiers && pHotKeyItem->vk == wVk)
-      {
-         return pHotKeyItem;
-      }
-   }
+    LIST_FOR_EACH(pHotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (pHotKeyItem->fsModifiers == fsModifiers && pHotKeyItem->vk == wVk)
+        {
+            return pHotKeyItem;
+        }
+    }
 
-   return NULL;
+    return NULL;
 }
 
 /*
@@ -177,106 +177,106 @@ co_UserProcessHotKeys(WORD wVk, BOOL bIsDown)
 // Get/SetHotKey message support.
 //
 UINT FASTCALL
-DefWndGetHotKey( HWND hwnd )
+DefWndGetHotKey(HWND hwnd)
 {
-   PHOT_KEY_ITEM HotKeyItem;
+    PHOT_KEY_ITEM HotKeyItem;
 
-   ERR("DefWndGetHotKey\n");
+    ERR("DefWndGetHotKey\n");
 
-   if (IsListEmpty(&gHotkeyList)) return 0;
+    if (IsListEmpty(&gHotkeyList)) return 0;
 
-   LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if ( HotKeyItem->hWnd == hwnd &&
-           HotKeyItem->id == IDHOT_REACTOS )
-      {
-         return MAKELONG(HotKeyItem->vk, HotKeyItem->fsModifiers);
-      }
-   }
-   return 0;
+    LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (HotKeyItem->hWnd == hwnd &&
+            HotKeyItem->id == IDHOT_REACTOS)
+        {
+            return MAKELONG(HotKeyItem->vk, HotKeyItem->fsModifiers);
+        }
+    }
+    return 0;
 }
 
-INT FASTCALL 
-DefWndSetHotKey( PWND pWnd, WPARAM wParam )
+INT FASTCALL
+DefWndSetHotKey(PWND pWnd, WPARAM wParam)
 {
-   UINT fsModifiers, vk;
-   PHOT_KEY_ITEM HotKeyItem;
-   HWND hWnd;
-   BOOL HaveSameWnd = FALSE;
-   INT Ret = 1;
+    UINT fsModifiers, vk;
+    PHOT_KEY_ITEM HotKeyItem;
+    HWND hWnd;
+    BOOL HaveSameWnd = FALSE;
+    INT Ret = 1;
 
-   ERR("DefWndSetHotKey wParam 0x%x\n", wParam);
+    ERR("DefWndSetHotKey wParam 0x%x\n", wParam);
 
-   // A hot key cannot be associated with a child window.
-   if (pWnd->style & WS_CHILD) return 0;
+    // A hot key cannot be associated with a child window.
+    if (pWnd->style & WS_CHILD) return 0;
 
-   // VK_ESCAPE, VK_SPACE, and VK_TAB are invalid hot keys.
-   if ( LOWORD(wParam) == VK_ESCAPE ||
+    // VK_ESCAPE, VK_SPACE, and VK_TAB are invalid hot keys.
+    if (LOWORD(wParam) == VK_ESCAPE ||
         LOWORD(wParam) == VK_SPACE ||
-        LOWORD(wParam) == VK_TAB ) return -1;
+        LOWORD(wParam) == VK_TAB) return -1;
 
-   vk = LOWORD(wParam);
-   fsModifiers = HIWORD(wParam);   
-   hWnd = UserHMGetHandle(pWnd);
+    vk = LOWORD(wParam);
+    fsModifiers = HIWORD(wParam);
+    hWnd = UserHMGetHandle(pWnd);
 
-   if (wParam)
-   {
-      LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-      {
-         if ( HotKeyItem->fsModifiers == fsModifiers &&
-              HotKeyItem->vk == vk &&
-              HotKeyItem->id == IDHOT_REACTOS )
-         {
-            if (HotKeyItem->hWnd != hWnd)
-               Ret = 2; // Another window already has the same hot key.
+    if (wParam)
+    {
+        LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+        {
+            if (HotKeyItem->fsModifiers == fsModifiers &&
+                HotKeyItem->vk == vk &&
+                HotKeyItem->id == IDHOT_REACTOS)
+            {
+                if (HotKeyItem->hWnd != hWnd)
+                    Ret = 2; // Another window already has the same hot key.
+                break;
+            }
+        }
+    }
+
+    LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (HotKeyItem->hWnd == hWnd &&
+            HotKeyItem->id == IDHOT_REACTOS)
+        {
+            HaveSameWnd = TRUE;
             break;
-         }
-      }
-   }
+        }
+    }
 
-   LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if ( HotKeyItem->hWnd == hWnd &&
-           HotKeyItem->id == IDHOT_REACTOS )
-      {
-         HaveSameWnd = TRUE;
-         break;
-      }
-   }
+    if (HaveSameWnd)
+    {
+        if (wParam == 0)
+        {   // Setting wParam to NULL removes the hot key associated with a window.
+            UnregisterWindowHotKeys(pWnd);
+        }
+        else
+        {   /* A window can only have one hot key. If the window already has a hot key
+               associated with it, the new hot key replaces the old one. */
+            HotKeyItem->fsModifiers = fsModifiers;
+            HotKeyItem->vk = vk;
+        }
+    }
+    else //
+    {
+        if (wParam == 0)
+            return 1; // Do nothing, exit.
 
-   if (HaveSameWnd)
-   {
-      if (wParam == 0)
-      { // Setting wParam to NULL removes the hot key associated with a window.
-         UnregisterWindowHotKeys(pWnd);
-      }
-      else
-      { /* A window can only have one hot key. If the window already has a hot key
-           associated with it, the new hot key replaces the old one. */
-         HotKeyItem->fsModifiers = fsModifiers;
-         HotKeyItem->vk = vk;
-      }
-   }
-   else // 
-   {
-      if (wParam == 0)
-         return 1; // Do nothing, exit.
+        HotKeyItem = ExAllocatePoolWithTag(PagedPool, sizeof(HOT_KEY_ITEM), USERTAG_HOTKEY);
+        if (HotKeyItem == NULL)
+        {
+            return 0;
+        }
 
-      HotKeyItem = ExAllocatePoolWithTag (PagedPool, sizeof(HOT_KEY_ITEM), USERTAG_HOTKEY);
-      if (HotKeyItem == NULL)
-      {
-        return 0;
-      }
+        HotKeyItem->Thread = pWnd->head.pti->pEThread;
+        HotKeyItem->hWnd = hWnd;
+        HotKeyItem->id = IDHOT_REACTOS; // Don't care, these hot keys are unrelated to the hot keys set by RegisterHotKey.
+        HotKeyItem->fsModifiers = fsModifiers;
+        HotKeyItem->vk = vk;
 
-      HotKeyItem->Thread = pWnd->head.pti->pEThread;
-      HotKeyItem->hWnd = hWnd;
-      HotKeyItem->id = IDHOT_REACTOS; // Don't care, these hot keys are unrelated to the hot keys set by RegisterHotKey.
-      HotKeyItem->fsModifiers = fsModifiers;
-      HotKeyItem->vk = vk;
-
-      InsertHeadList (&gHotkeyList, &HotKeyItem->ListEntry);
-   }
-   return Ret;
+        InsertHeadList(&gHotkeyList, &HotKeyItem->ListEntry);
+    }
+    return Ret;
 }
 
 /* SYSCALLS *****************************************************************/
@@ -288,88 +288,88 @@ NtUserRegisterHotKey(HWND hWnd,
                      UINT fsModifiers,
                      UINT vk)
 {
-   PHOT_KEY_ITEM HotKeyItem;
-   PWND Window;
-   PETHREAD HotKeyThread;
-   DECLARE_RETURN(BOOL);
+    PHOT_KEY_ITEM HotKeyItem;
+    PWND Window;
+    PETHREAD HotKeyThread;
+    DECLARE_RETURN(BOOL);
 
-   TRACE("Enter NtUserRegisterHotKey\n");
-   UserEnterExclusive();
+    TRACE("Enter NtUserRegisterHotKey\n");
+    UserEnterExclusive();
 
-   if (hWnd == NULL)
-   {
-      HotKeyThread = PsGetCurrentThread();
-   }
-   else
-   {
-      if(!(Window = UserGetWindowObject(hWnd)))
-      {
-         RETURN( FALSE);
-      }
-      HotKeyThread = Window->head.pti->pEThread;
-   }
+    if (hWnd == NULL)
+    {
+        HotKeyThread = PsGetCurrentThread();
+    }
+    else
+    {
+        if(!(Window = UserGetWindowObject(hWnd)))
+        {
+            RETURN( FALSE);
+        }
+        HotKeyThread = Window->head.pti->pEThread;
+    }
 
-   /* Check for existing hotkey */
-   if (IsHotKey(fsModifiers, vk))
-   {
-      RETURN( FALSE);
-   }
+    /* Check for existing hotkey */
+    if (IsHotKey(fsModifiers, vk))
+    {
+        RETURN( FALSE);
+    }
 
-   HotKeyItem = ExAllocatePoolWithTag(PagedPool, sizeof(HOT_KEY_ITEM), USERTAG_HOTKEY);
-   if (HotKeyItem == NULL)
-   {
-      RETURN( FALSE);
-   }
+    HotKeyItem = ExAllocatePoolWithTag(PagedPool, sizeof(HOT_KEY_ITEM), USERTAG_HOTKEY);
+    if (HotKeyItem == NULL)
+    {
+        RETURN( FALSE);
+    }
 
-   HotKeyItem->Thread = HotKeyThread;
-   HotKeyItem->hWnd = hWnd;
-   HotKeyItem->id = id;
-   HotKeyItem->fsModifiers = fsModifiers;
-   HotKeyItem->vk = vk;
+    HotKeyItem->Thread = HotKeyThread;
+    HotKeyItem->hWnd = hWnd;
+    HotKeyItem->id = id;
+    HotKeyItem->fsModifiers = fsModifiers;
+    HotKeyItem->vk = vk;
 
-   InsertHeadList (&gHotkeyList, &HotKeyItem->ListEntry);
+    InsertHeadList(&gHotkeyList, &HotKeyItem->ListEntry);
 
-   RETURN( TRUE);
+    RETURN( TRUE);
 
 CLEANUP:
-   TRACE("Leave NtUserRegisterHotKey, ret=%i\n",_ret_);
-   UserLeave();
-   END_CLEANUP;
+    TRACE("Leave NtUserRegisterHotKey, ret=%i\n", _ret_);
+    UserLeave();
+    END_CLEANUP;
 }
 
 
 BOOL APIENTRY
 NtUserUnregisterHotKey(HWND hWnd, int id)
 {
-   PHOT_KEY_ITEM HotKeyItem;
-   PWND Window;
-   DECLARE_RETURN(BOOL);
+    PHOT_KEY_ITEM HotKeyItem;
+    PWND Window;
+    DECLARE_RETURN(BOOL);
 
-   TRACE("Enter NtUserUnregisterHotKey\n");
-   UserEnterExclusive();
+    TRACE("Enter NtUserUnregisterHotKey\n");
+    UserEnterExclusive();
 
-   if(!(Window = UserGetWindowObject(hWnd)))
-   {
-      RETURN( FALSE);
-   }
+    if(!(Window = UserGetWindowObject(hWnd)))
+    {
+        RETURN( FALSE);
+    }
 
-   LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
-   {
-      if (HotKeyItem->hWnd == hWnd && HotKeyItem->id == id)
-      {
-         RemoveEntryList (&HotKeyItem->ListEntry);
-         ExFreePoolWithTag(HotKeyItem, USERTAG_HOTKEY);
+    LIST_FOR_EACH(HotKeyItem, &gHotkeyList, HOT_KEY_ITEM, ListEntry)
+    {
+        if (HotKeyItem->hWnd == hWnd && HotKeyItem->id == id)
+        {
+            RemoveEntryList(&HotKeyItem->ListEntry);
+            ExFreePoolWithTag(HotKeyItem, USERTAG_HOTKEY);
 
-         RETURN( TRUE);
-      }
-   }
+            RETURN( TRUE);
+        }
+    }
 
-   RETURN( FALSE);
+    RETURN( FALSE);
 
 CLEANUP:
-   TRACE("Leave NtUserUnregisterHotKey, ret=%i\n",_ret_);
-   UserLeave();
-   END_CLEANUP;
+    TRACE("Leave NtUserUnregisterHotKey, ret=%i\n", _ret_);
+    UserLeave();
+    END_CLEANUP;
 }
 
 /* EOF */
