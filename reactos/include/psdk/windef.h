8bdd44546a79964578e0a8219215ade19e6ed6d3
Jérôme Gardou <jerome.gardou@reactos.org>
2011-11-19 16:13:31 +0000

    [WINDEF.H] - you never use enough brackets
    svn path=/trunk/; revision=54442

--- reactos/include/psdk/windef.h
+++ reactos/include/psdk/windef.h
@@ -246,8 +246,8 @@ typedef LONG HRESULT;
 #endif
 #endif
 
-#define MAKEWORD(a,b) ((WORD)(((BYTE)((DWORD_PTR)(a) & 0xff)) | ((WORD)((BYTE)((DWORD_PTR)(b) & 0xff))) << 8))
-#define MAKELONG(a,b) ((LONG)(((WORD)((DWORD_PTR)(a) & 0xffff)) | ((DWORD)((WORD)((DWORD_PTR)(b) & 0xffff))) << 16))
+#define MAKEWORD(a,b) ((WORD)(((BYTE)((DWORD_PTR)(a) & 0xff)) | (((WORD)((BYTE)((DWORD_PTR)(b) & 0xff))) << 8)))
+#define MAKELONG(a,b) ((LONG)(((WORD)((DWORD_PTR)(a) & 0xffff)) | (((DWORD)((WORD)((DWORD_PTR)(b) & 0xffff))) << 16)))
 #define LOWORD(l) ((WORD)((DWORD_PTR)(l) & 0xffff))
 #define HIWORD(l) ((WORD)((DWORD_PTR)(l) >> 16))
 #define LOBYTE(w) ((BYTE)((DWORD_PTR)(w) & 0xff))
