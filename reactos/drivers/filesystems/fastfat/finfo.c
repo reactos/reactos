5de2462b2a80d060192bbe91dec2cdb64f4407fc
Pierre Schweitzer <pierre@reactos.org>
2011-12-31 00:09:57 +0000

    [FASTFAT] Return appropriate status when requesting extended attributes on FAT32
    svn path=/trunk/; revision=54788

--- reactos/drivers/filesystems/fastfat/finfo.c
+++ reactos/drivers/filesystems/fastfat/finfo.c
@@ -517,8 +517,9 @@ VfatGetEaInformation(PFILE_OBJECT FileObject,
     {
         /* FIXME */
         DPRINT1("VFAT: FileEaInformation not implemented!\n");
+        return STATUS_SUCCESS;
     }
-    return STATUS_SUCCESS;
+    return STATUS_EAS_NOT_SUPPORTED;
 }
 
 
