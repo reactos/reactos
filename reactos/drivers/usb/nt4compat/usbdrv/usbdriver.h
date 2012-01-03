da309e313f77fe8203c5424684712d3722bbe097
Timo Kreuzer <timo.kreuzer@reactos.org>
2012-01-03 20:23:29 +0000

    [USBDRV] remove hacks to fix build
    svn path=/trunk/; revision=54827

--- reactos/drivers/usb/nt4compat/usbdrv/usbdriver.h
+++ reactos/drivers/usb/nt4compat/usbdrv/usbdriver.h
@@ -1,20 +1,6 @@
 //#define NTDDI_VERSION   NTDDI_WIN2K
 //#define _WIN32_WINNT    _WIN32_WINNT_WIN2K
 
-// hacks for WDK
-#define __field_bcount_part_opt(x,y)
-#define __field_bcount_part(x,y)
-#define __field_bcount_opt(x)
-#define __in_range(a,b)
-#define __volatile
-#define __struct_bcount(x)
-#define __inexpressible_readableTo(x)
-#define __deref_volatile
-#ifndef __field_ecount
-#    define __field_ecount(x)
-#endif
-
-
 #include <ntddk.h>
 #include <stdio.h>
 
