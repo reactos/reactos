ac19035f70008c670bc34b391ce28c67ce4874a2
Amine Khaldi <amine.khaldi@reactos.org>
2012-01-14 17:31:20 +0000

    [CMAKE] * Set DBG to false in Release build type. * Set _WINKD_ to false in Release build type for MSVC builds.
    svn path=/trunk/; revision=54963

--- reactos/cmake/config.cmake
+++ reactos/cmake/config.cmake
@@ -25,14 +25,23 @@ set(GDB FALSE CACHE BOOL
 "Whether to compile for debugging with GDB.
 If you don't use GDB, don't	enable this.")
 
-set(DBG TRUE CACHE BOOL
+if(${CMAKE_BUILD_TYPE} MATCHES Release)
+    set(DBG FALSE CACHE BOOL
 "Whether to compile for debugging.")
+else()
+    set(DBG TRUE CACHE BOOL
+"Whether to compile for debugging.")
+endif()
 
 if(MSVC)
     set(KDBG FALSE CACHE BOOL
 "Whether to compile in the integrated kernel debugger.")
-    set(_WINKD_ TRUE CACHE BOOL
-"Whether to compile with the KD protocol.")
+    if(${CMAKE_BUILD_TYPE} MATCHES Release)
+        set(_WINKD_ FALSE CACHE BOOL "Whether to compile with the KD protocol.")
+    else()
+        set(_WINKD_ TRUE CACHE BOOL "Whether to compile with the KD protocol.")
+    endif()
+    
 else()
     set(KDBG TRUE CACHE BOOL
 "Whether to compile in the integrated kernel debugger.")
