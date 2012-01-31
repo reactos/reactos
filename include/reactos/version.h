483bd4cbffd8688ae9d4ab0b5903b3c69ec4d9f3
Ziliang Guo <drakekaizer666@gmail.com>
2012-01-31 01:41:32 +0000

    Really update the release version this time.
    svn path=/branches/ros-branch-0_3_14/; revision=55344

--- include/reactos/version.h
+++ include/reactos/version.h
@@ -18,8 +18,8 @@
 #define __VERSION_H
 
 #define KERNEL_VERSION_MAJOR        0
-#define KERNEL_VERSION_MINOR        4
-#define KERNEL_VERSION_PATCH_LEVEL  0
+#define KERNEL_VERSION_MINOR        3
+#define KERNEL_VERSION_PATCH_LEVEL  14
 
 #define COPYRIGHT_YEAR              "2012"
 
