c73fd0768f318473f88fac1e98dfad18ae7a0372
Timo Kreuzer <timo.kreuzer@reactos.org>
2012-05-17 18:56:00 +0000

    [GDI32_APITEST] - Create a global 4bpp dib section for tests - More tests for GetDIBits
    svn path=/trunk/; revision=56606

--- rostests/apitests/gdi32/init.c
+++ rostests/apitests/gdi32/init.c
@@ -5,6 +5,9 @@
 HBITMAP ghbmpDIB32;
 HDC ghdcDIB32;
 PULONG pulDIB32Bits;
+HBITMAP ghbmpDIB4;
+HDC ghdcDIB4;
+PULONG pulDIB4Bits;
 HPALETTE ghpal;
 
 struct
@@ -31,10 +34,16 @@ BOOL InitStuff(void)
 {
     BITMAPINFO bmi32 =
         {{sizeof(BITMAPINFOHEADER), 4, -4, 1, 32, BI_RGB, 0, 1, 1, 0, 0}};
+    BITMAPINFO bmi4 =
+        {{sizeof(BITMAPINFOHEADER), 4, -4, 1, 4, BI_RGB, 0, 1, 1, 0, 0}};
 
     ghdcDIB32 = CreateCompatibleDC(0);
+    ghdcDIB4 = CreateCompatibleDC(0);
 
     ghbmpDIB32 = CreateDIBSection(ghdcDIB32, &bmi32, DIB_PAL_COLORS, (PVOID*)&pulDIB32Bits, 0, 0 );
+    if (!ghbmpDIB32) return FALSE;
+
+    ghbmpDIB4 = CreateDIBSection(ghdcDIB4, &bmi4, DIB_PAL_COLORS, (PVOID*)&pulDIB4Bits, 0, 0 );
     if (!ghbmpDIB32) return FALSE;
 
     SelectObject(ghdcDIB32, ghbmpDIB32);
