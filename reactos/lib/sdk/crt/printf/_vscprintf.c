9c54cdba317195cb056be6841596b72b9fbe43a8
Jérôme Gardou <jerome.gardou@reactos.org>
2012-07-21 20:05:18 +0000

    [OOPS] - Forgot some files
    svn path=/trunk/; revision=56929

---
+++ reactos/lib/sdk/crt/printf/_vscprintf.c
@@ -0,0 +1,28 @@
+/*
+ * COPYRIGHT:       GNU GPL, see COPYING in the top level directory
+ * PROJECT:         ReactOS crt library
+ * FILE:            lib/sdk/crt/printf/_vscprintf.c
+ * PURPOSE:         Implementation of _vscprintf
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _cdecl streamout(FILE *stream, const char *format, va_list argptr);
+
+int _vscprintf(
+   const char *format,
+   va_list argptr
+)
+{
+    int ret;
+    FILE* nulfile = fopen("nul", "w");
+    if(nulfile == NULL)
+    {
+        /* This should never happen... */
+        return -1;
+    }
+    ret = streamout(nulfile, format, argptr);
+    fclose(nulfile);
+    return ret;
+}
