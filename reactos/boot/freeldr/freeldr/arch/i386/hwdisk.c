e78aab93b00dadcf4e0b06cc3f7bf65c346a54a2
Hervé Poussineau <hpoussin@reactos.org>
2012-07-30 06:12:58 +0000

    [FREELDR] i386: support drives bigger than ~4GB See issue #6669 for more details.
    svn path=/trunk/; revision=56993

--- reactos/boot/freeldr/freeldr/arch/i386/hwdisk.c
+++ reactos/boot/freeldr/freeldr/arch/i386/hwdisk.c
@@ -150,8 +150,7 @@ static LONG DiskSeek(ULONG FileId, LARGE_INTEGER* Position, SEEKMODE SeekMode)
     if (Position->LowPart & (Context->SectorSize - 1))
         return EINVAL;
 
-    /* FIXME: take HighPart into account */
-    Context->SectorNumber = Position->LowPart / Context->SectorSize;
+    Context->SectorNumber = (ULONG)(Position->QuadPart / Context->SectorSize);
     return ESUCCESS;
 }
 
