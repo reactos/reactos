44356bb7735c9bda58330ed033ce8345ad78ba56
Cameron Gutman <aicommander@gmail.com>
2012-08-26 20:13:22 +0000

    [AFD] - Fix returning uninitialized data from FIONREAD when the socket is not bound or connected
    svn path=/trunk/; revision=57168

--- reactos/drivers/network/afd/afd/info.c
+++ reactos/drivers/network/afd/afd/info.c
@@ -52,11 +52,7 @@ AfdGetInfo( PDEVICE_OBJECT DeviceObject, PIRP Irp,
         break;
 
     case AFD_INFO_RECEIVE_CONTENT_SIZE:
-        /* Only touch InfoReq if a socket has been set up.
-           Behaviour was verified under WinXP SP2. */
-        if(FCB->AddressFile.Object || FCB->Connection.Object)
-            InfoReq->Information.Ulong = FCB->Recv.Content - FCB->Recv.BytesUsed;
-
+        InfoReq->Information.Ulong = FCB->Recv.Content - FCB->Recv.BytesUsed;
         break;
 
         case AFD_INFO_SENDS_IN_PROGRESS:
