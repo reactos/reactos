5666d93992022a8e0898abfb3f4fb75ffbca10d9
Timo Kreuzer <timo.kreuzer@reactos.org>
2012-08-31 17:20:56 +0000

    [CRT_APITEST] Fix broken test.
    svn path=/trunk/; revision=57208

--- rostests/apitests/crt/_vscwprintf.c
+++ rostests/apitests/crt/_vscwprintf.c
@@ -16,13 +16,14 @@ static void call_varargs(int expected_ret, LPCWSTR formatString, ...)
     /* Test the basic functionality */
     va_start(args, formatString);
     ret = _vscwprintf(formatString, args);
-    ok(expected_ret == ret, "Test failed: expected %i, got %i.\n", expected_ret, ret);
+    ok(expected_ret == ret, "expected %i, got %i.\n", expected_ret, ret);
 }
 
 START_TEST(_vscwprintf)
 {
     /* Lesson of the day: don't mix wide and ansi char */
-    call_varargs(19, L"%s world!", "hello");
+    /* Lesson of the week: don't ignore the lesson of the day */
+    call_varargs(12, L"%hs world!", "hello");
     call_varargs(12, L"%s world!", L"hello");
     call_varargs(17, L"Jack ate %u pies", 100);
     /* Do not test NULL argument. That is verified to SEGV on a */
