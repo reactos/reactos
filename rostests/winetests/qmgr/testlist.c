8681ae792aaddf51b75d9e62de06dafb1265f298
Thomas Faber <thomas.faber@reactos.org>
2012-09-20 15:41:32 +0000

    [QMGR_WINETESTS] - Skip all qmgr tests because they cause timeouts on testbot. This should hopefully be fixed soon and they can be re-enabled. CORE-6536
    svn path=/trunk/; revision=57353

--- rostests/winetests/qmgr/testlist.c
+++ rostests/winetests/qmgr/testlist.c
@@ -6,18 +6,27 @@
 #define STANDALONE
 #include "wine/test.h"
 
-extern void func_qmgr(void);
-extern void func_job(void);
-extern void func_file(void);
-extern void func_enum_jobs(void);
 extern void func_enum_files(void);
+extern void func_enum_jobs(void);
+extern void func_file(void);
+extern void func_job(void);
+extern void func_qmgr(void);
+
+#ifdef __REACTOS__
+void func_skipped(void) { skip("qmgr tests skipped due to CORE-6536\n"); }
+#endif
 
 const struct test winetest_testlist[] =
 {
+/* Skipped because of testbot timeouts. See CORE-6536. */
+#ifdef __REACTOS__
+    { "skipped", func_skipped },
+#else
+    { "enum_files", func_enum_files },
+    { "enum_jobs", func_enum_jobs },
+    { "file", func_file },
+    { "job", func_job },
     { "qmgr", func_qmgr },
-	{ "job", func_job },
-	{ "file", func_file },
-	{ "enum_jobs", func_enum_jobs },
-	{ "enum_files", func_enum_files },
+#endif
     { 0, 0 }
 };
