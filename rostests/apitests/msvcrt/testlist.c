6dacc020db15a6dc9cebf7a968101a73c68e1c00
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2012-10-09 20:56:11 +0000

    [MSVCRT:APITEST] - Add a test which allows testing the command line argument lpCmdLine in WinMain function (GUI programs).
    svn path=/trunk/; revision=57524

--- rostests/apitests/msvcrt/testlist.c
+++ rostests/apitests/msvcrt/testlist.c
@@ -5,14 +5,15 @@
 #define STANDALONE
 #include "wine/test.h"
 
+extern void func_CommandLine(void);
 extern void func_ieee(void);
 extern void func_splitpath(void);
 
 const struct test winetest_testlist[] =
 {
+    { "CommandLine", func_CommandLine },
     { "ieee", func_ieee },
     { "splitpath", func_splitpath },
 
     { 0, 0 }
 };
-
