3ab390e1300f871feee8d5880edbaecf776197ff
Jérôme Gardou <jerome.gardou@reactos.org>
2012-10-25 13:05:30 +0000

    [CONFIGURE_SH] - Add the ability to use the ninja generator with unix configure script
    svn path=/trunk/; revision=57614

--- reactos/configure.sh
+++ reactos/configure.sh
@@ -9,6 +9,11 @@ BUILD_ENVIRONMENT=MinGW
 ARCH=$ROS_ARCH
 REACTOS_SOURCE_DIR=$(cd `dirname $0` && pwd)
 REACTOS_OUTPUT_PATH=output-$BUILD_ENVIRONMENT-$ARCH
+if [ "$1" = "ninja" ]; then
+CMAKE_GENERATOR="Ninja"
+else
+CMAKE_GENERATOR="Unix Makefiles"
+fi
 
 if [ "$REACTOS_SOURCE_DIR" = "$PWD" ]; then
   echo Creating directories in $REACTOS_OUTPUT_PATH
@@ -23,12 +28,12 @@ cd host-tools
 rm -f CMakeCache.txt
 
 REACTOS_BUILD_TOOLS_DIR="$PWD"
-cmake -G "Unix Makefiles" -DARCH=$ARCH "$REACTOS_SOURCE_DIR"
+cmake -G "$CMAKE_GENERATOR" -DARCH=$ARCH "$REACTOS_SOURCE_DIR"
 
 echo Preparing reactos...
 cd ../reactos
 rm -f CMakeCache.txt
 
-cmake -G "Unix Makefiles" -DENABLE_CCACHE=0 -DPCH=0 -DCMAKE_TOOLCHAIN_FILE=toolchain-gcc.cmake -DARCH=$ARCH -DREACTOS_BUILD_TOOLS_DIR="$REACTOS_BUILD_TOOLS_DIR" "$REACTOS_SOURCE_DIR"
+cmake -G "$CMAKE_GENERATOR" -DENABLE_CCACHE=0 -DPCH=0 -DCMAKE_TOOLCHAIN_FILE=toolchain-gcc.cmake -DARCH=$ARCH -DREACTOS_BUILD_TOOLS_DIR="$REACTOS_BUILD_TOOLS_DIR" "$REACTOS_SOURCE_DIR"
 
 echo Configure script complete! Enter directories and execute appropriate build commands\(ex: make, makex, etc...\).
