3bddc1bdba391d938e4202f2b57ff3e1c56fb0d9
Jérôme Gardou <jerome.gardou@reactos.org>
2012-12-13 16:00:44 +0000

    [OLE32]  - Fix compilation with newer GCC
    svn path=/trunk/; revision=57905

--- reactos/dll/win32/ole32/stg_prop.c
+++ reactos/dll/win32/ole32/stg_prop.c
@@ -1035,12 +1035,12 @@ static HRESULT PropertyStorage_ReadDictionary(PropertyStorage_impl *This,
 }
 
 #ifdef __i386__
-#define __thiscall __stdcall
+#define __thiscall_wrapper __stdcall
 #else
-#define __thiscall __cdecl
+#define __thiscall_wrapper __cdecl
 #endif
 
-static void* __thiscall Allocate_CoTaskMemAlloc(void *userdata, ULONG size)
+static void* __thiscall_wrapper Allocate_CoTaskMemAlloc(void *userdata, ULONG size)
 {
     return CoTaskMemAlloc(size);
 }
@@ -1049,7 +1049,7 @@ static void* __thiscall Allocate_CoTaskMemAlloc(void *userdata, ULONG size)
  * end of the buffer.
  */
 static HRESULT PropertyStorage_ReadProperty(PROPVARIANT *prop, const BYTE *data,
-    UINT codepage, void* (__thiscall *allocate)(void *userdata, ULONG size), void *allocate_data)
+    UINT codepage, void* (__thiscall_wrapper *allocate)(void *userdata, ULONG size), void *allocate_data)
 {
     HRESULT hr = S_OK;
 
@@ -2738,13 +2738,13 @@ end:
                    "jmp *(4*(" #num "))(%eax)" )
 
 DEFINE_STDCALL_WRAPPER(0,Allocate_PMemoryAllocator,8)
-extern void* __thiscall Allocate_PMemoryAllocator(void *this, ULONG cbSize);
+extern void* __stdcall Allocate_PMemoryAllocator(void *this, ULONG cbSize);
 
 #else
 
-static void* __thiscall Allocate_PMemoryAllocator(void *this, ULONG cbSize)
+static void* __cdecl Allocate_PMemoryAllocator(void *this, ULONG cbSize)
 {
-    void* (__thiscall *fn)(void*,ULONG) = **(void***)this;
+    void* (__cdecl *fn)(void*,ULONG) = **(void***)this;
     return fn(this, cbSize);
 }
 
