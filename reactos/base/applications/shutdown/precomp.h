45d0b41d19bdd450ba9e0e8715f15a0fcfd7295c
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2012-12-14 23:29:22 +0000

    [SHUTDOWN] Improve functionnality of the shutdown utility :
    * Added extra arguments based upon Windows 7.
    * Added options to CommandLineOptions function.
    * Simplify the PrintResourceString() function.
    * Removed PrintUsage() since PrintResourceString() does a more flexible job at printing strings out.
    * Added clear error messages when there is a problem. This makes it a bit easier to determine if there is a problem.
    * Convert all the C++ comments to C.
    * Since each version of Windows has a different upper limit for the length of the comment, a work around solution is used that first checks the length of the comment based upon the version of Windows that the user is currently using.
    * Added support for hibernation mode, but the function SetSuspendState() is only a stub right now.
    * In cases where the shutdown/restart operation is done remotely, now the proper permission is granted if the user has the proper permission.
    * Added the REASON structure, which contains all the reason code combinations required for giving the proper reason code to the computer.
    * Added bare bones basic support for the GUI option (/i). However, until the required features are added to ReactOS, the GUI won't do anything.
    * Convert the program to full UNICODE support.
    
    CORE-6613 #resolve #comment Committed in r57916. Thanks :)
    
    svn path=/trunk/; revision=57916

--- reactos/base/applications/shutdown/precomp.h
+++ reactos/base/applications/shutdown/precomp.h
@@ -1,16 +1,59 @@
 #ifndef __SHUTDOWN_PRECOMP_H
 #define __SHUTDOWN_PRECOMP_H
 
+/* INCLUDES ******************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <windows.h>
 #include <tchar.h>
-#include <reason.h> //shutdown codes
+#include <powrprof.h>
+// #include <reason.h> // Shutdown codes
 #include "resource.h"
 
-// misc.c
-INT AllocAndLoadString(OUT LPTSTR *lpTarget,
-                       IN HINSTANCE hInst,
-                       IN UINT uID);
+/* DEFINES *******************************************************************/
+#define MAX_MESSAGE_SIZE    512
+#define MAX_MAJOR_CODE      256
+#define MAX_MINOR_CODE      65536
+#define MAX_TIMEOUT         315360000
+#define MAX_BUFFER_SIZE     5024
+
+/* Reason Code List */
+typedef struct _REASON
+{
+    LPWSTR prefix;
+    int major;
+    int minor;
+    DWORD flag;
+} REASON, *PREASON;
+
+/* Used to determine how to shutdown the system. */
+struct CommandLineOptions
+{
+    BOOL abort;
+    BOOL force;
+    BOOL logoff;
+    BOOL restart;
+    BOOL shutdown;
+    BOOL document_reason;
+    BOOL hibernate;
+    DWORD shutdown_delay;
+    LPWSTR remote_system;
+    LPWSTR message;
+    DWORD reason;
+    BOOL show_gui;
+};
+
+extern const DWORD defaultReason;
+
+/* PROTOTYPES *****************************************************************/
+
+/* misc.c */
+BOOL CheckCommentLength(LPCWSTR);
+DWORD ParseReasonCode(LPCWSTR);
+VOID DisplayError(DWORD dwError);
+
+/* gui.c */
+BOOL ShutdownGuiMain(struct CommandLineOptions opts);
 
 #endif
