afc06cbf845f6c5dfbda9870043694b15b7fdb21
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2012-12-16 14:37:45 +0000

    [WIn32k] Fix the fix for Safe Mode (vide revision r57923).
    svn path=/trunk/; revision=57925

--- reactos/win32ss/user/ntuser/sysparams.c
+++ reactos/win32ss/user/ntuser/sysparams.c
@@ -25,18 +25,16 @@ BOOL g_PaintDesktopVersion = FALSE;
 #define METRIC2REG(met) (-((((met) * 1440)- 0) / dpi))
 
 #define REQ_INTERACTIVE_WINSTA(err) \
-    if (GetW32ProcessInfo()->prpwinsta) \
+    if (GetW32ProcessInfo()->prpwinsta != InputWindowStation) \
     { \
-        if (GetW32ProcessInfo()->prpwinsta != InputWindowStation) \
+        if (GetW32ProcessInfo()->prpwinsta == NULL) \
+        { \
+            ERR("NtUserSystemParametersInfo called without active window station, and it requires an interactive one\n"); \
+        } \
+        else \
         { \
             ERR("NtUserSystemParametersInfo requires interactive window station (current is %wZ)\n", &GetW32ProcessInfo()->prpwinsta->Name); \
-            EngSetLastError(err); \
-            return 0; \
         } \
-    } \
-    else \
-    { \
-        ERR("NtUserSystemParametersInfo called without active window station, and it requires an interactive one.\n"); \
         EngSetLastError(err); \
         return 0; \
     }
