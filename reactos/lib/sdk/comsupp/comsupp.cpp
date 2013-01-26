9f6d7cdd67442ca307693ff67b26176d9e716d97
Thomas Faber <thomas.faber@reactos.org>
2013-01-26 13:33:15 +0000

    [CRT] - Import C++ compiler support headers from mingw-w64 (with some fixes) - Implement the required parts of the comsupp library
    svn path=/trunk/; revision=58223

---
+++ reactos/lib/sdk/comsupp/comsupp.cpp
@@ -0,0 +1,47 @@
+/*
+ * PROJECT:         ReactOS crt library
+ * LICENSE:         GPLv2+ - See COPYING in the top level directory
+ * PURPOSE:         Compiler support for COM
+ * PROGRAMMER:      Thomas Faber
+ */
+
+#include <comdef.h>
+#include <comutil.h>
+
+/* comdef.h */
+typedef void WINAPI COM_ERROR_HANDLER(HRESULT, IErrorInfo *);
+static COM_ERROR_HANDLER *com_error_handler;
+
+void WINAPI _com_raise_error(HRESULT hr, IErrorInfo *perrinfo)
+{
+    throw _com_error(hr, perrinfo);
+}
+
+void WINAPI _set_com_error_handler(COM_ERROR_HANDLER *phandler)
+{
+    com_error_handler = phandler;
+}
+
+void WINAPI _com_issue_error(HRESULT hr)
+{
+    com_error_handler(hr, NULL);
+}
+
+void WINAPI _com_issue_errorex(HRESULT hr, IUnknown *punk, REFIID riid)
+{
+    void *pv;
+    IErrorInfo *perrinfo = NULL;
+
+    if (SUCCEEDED(punk->QueryInterface(riid, &pv)))
+    {
+        ISupportErrorInfo *pserrinfo = static_cast<ISupportErrorInfo *>(pv);
+        if (pserrinfo->InterfaceSupportsErrorInfo(riid) == S_OK)
+            (void)GetErrorInfo(0, &perrinfo);
+        pserrinfo->Release();
+    }
+
+    com_error_handler(hr, perrinfo);
+}
+
+/* comutil.h */
+_variant_t vtMissing(DISP_E_PARAMNOTFOUND, VT_ERROR);
