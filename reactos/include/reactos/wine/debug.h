f0410e5cad2b1207b504f791de08b846d049553e
Timo Kreuzer <timo.kreuzer@reactos.org>
2013-02-17 14:33:01 +0000

    [WINE] Silence GCC 4.7.2 warnings
    svn path=/trunk/; revision=58332

--- reactos/include/reactos/wine/debug.h
+++ reactos/include/reactos/wine/debug.h
@@ -245,9 +245,9 @@ static __inline const char *wine_dbgstr_longlong( ULONGLONG ll )
 #define WINE_ERR_ON(ch)            __WINE_IS_DEBUG_ON(_ERR,&__wine_dbch_##ch)
 
 #define WINE_DECLARE_DEBUG_CHANNEL(ch) \
-    static struct __wine_debug_channel __wine_dbch_##ch = { ~0, #ch }
+    static struct __wine_debug_channel __wine_dbch_##ch = { (unsigned char)~0, #ch }
 #define WINE_DEFAULT_DEBUG_CHANNEL(ch) \
-    static struct __wine_debug_channel __wine_dbch_##ch = { ~0, #ch }; \
+    static struct __wine_debug_channel __wine_dbch_##ch = { (unsigned char)~0, #ch }; \
     static struct __wine_debug_channel * const __wine_dbch___default = &__wine_dbch_##ch
 
 #define WINE_DPRINTF               wine_dbg_printf
