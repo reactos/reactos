e4b7869ead1064b714811f00ecd8c1ba52f6fa4e
Amine Khaldi <amine.khaldi@reactos.org>
2013-03-14 16:53:56 +0000

    [CRT] * Annotate direct.h.
    svn path=/trunk/; revision=58496

--- reactos/include/crt/direct.h
+++ reactos/include/crt/direct.h
@@ -25,40 +25,174 @@ extern "C" {
   };
 #endif
 
-  _CRTIMP char *__cdecl _getcwd(char *_DstBuf,int _SizeInBytes);
-  _CRTIMP char *__cdecl _getdcwd(int _Drive,char *_DstBuf,int _SizeInBytes);
-  char *__cdecl _getdcwd_nolock(int _Drive,char *_DstBuf,int _SizeInBytes);
-  _CRTIMP int __cdecl _chdir(const char *_Path);
-  _CRTIMP int __cdecl _mkdir(const char *_Path);
-  _CRTIMP int __cdecl _rmdir(const char *_Path);
-  _CRTIMP int __cdecl _chdrive(int _Drive);
-  _CRTIMP int __cdecl _getdrive(void);
-  _CRTIMP unsigned long __cdecl _getdrives(void);
+  _Check_return_
+  _Ret_opt_z_
+  _CRTIMP
+  char*
+  __cdecl
+  _getcwd(
+    _Out_writes_opt_(_SizeInBytes) char *_DstBuf,
+    _In_ int _SizeInBytes);
+
+  _Check_return_
+  _Ret_opt_z_
+  _CRTIMP
+  char*
+  __cdecl
+  _getdcwd(
+    _In_ int _Drive,
+    _Out_writes_opt_(_SizeInBytes) char *_DstBuf,
+    _In_ int _SizeInBytes);
+
+  _Check_return_
+  _Ret_opt_z_
+  char*
+  __cdecl
+  _getdcwd_nolock(
+    _In_ int _Drive,
+    _Out_writes_opt_(_SizeInBytes) char *_DstBuf,
+    _In_ int _SizeInBytes);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _chdir(
+    _In_z_ const char *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _mkdir(
+    _In_z_ const char *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _rmdir(
+    _In_z_ const char *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _chdrive(
+    _In_ int _Drive);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _getdrive(void);
+
+  _Check_return_
+  _CRTIMP
+  unsigned long
+  __cdecl
+  _getdrives(void);
 
 #ifndef _GETDISKFREE_DEFINED
 #define _GETDISKFREE_DEFINED
-  _CRTIMP unsigned __cdecl _getdiskfree(unsigned _Drive,struct _diskfree_t *_DiskFree);
+  _Check_return_
+  _CRTIMP
+  unsigned
+  __cdecl
+  _getdiskfree(
+    _In_ unsigned _Drive,
+    _Out_ struct _diskfree_t *_DiskFree);
 #endif
 
 #ifndef _WDIRECT_DEFINED
 #define _WDIRECT_DEFINED
-  _CRTIMP wchar_t *__cdecl _wgetcwd(wchar_t *_DstBuf,int _SizeInWords);
-  _CRTIMP wchar_t *__cdecl _wgetdcwd(int _Drive,wchar_t *_DstBuf,int _SizeInWords);
-  wchar_t *__cdecl _wgetdcwd_nolock(int _Drive,wchar_t *_DstBuf,int _SizeInWords);
-  _CRTIMP int __cdecl _wchdir(const wchar_t *_Path);
-  _CRTIMP int __cdecl _wmkdir(const wchar_t *_Path);
-  _CRTIMP int __cdecl _wrmdir(const wchar_t *_Path);
-#endif
 
-#ifndef	NO_OLDNAMES
+  _Check_return_
+  _Ret_opt_z_
+  _CRTIMP
+  wchar_t*
+  __cdecl
+  _wgetcwd(
+    _Out_writes_opt_(_SizeInWords) wchar_t *_DstBuf,
+    _In_ int _SizeInWords);
+
+  _Check_return_
+  _Ret_opt_z_
+  _CRTIMP
+  wchar_t*
+  __cdecl
+  _wgetdcwd(
+    _In_ int _Drive,
+    _Out_writes_opt_(_SizeInWords) wchar_t *_DstBuf,
+    _In_ int _SizeInWords);
+
+  _Check_return_
+  _Ret_opt_z_
+  wchar_t*
+  __cdecl
+  _wgetdcwd_nolock(
+    _In_ int _Drive,
+    _Out_writes_opt_(_SizeInWords) wchar_t *_DstBuf,
+    _In_ int _SizeInWords);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _wchdir(
+    _In_z_ const wchar_t *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _wmkdir(
+    _In_z_ const wchar_t *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  _wrmdir(
+    _In_z_ const wchar_t *_Path);
+
+#endif /* _WDIRECT_DEFINED */
+
+#ifndef NO_OLDNAMES
 
 #define diskfree_t _diskfree_t
 
-  _CRTIMP char *__cdecl getcwd(char *_DstBuf,int _SizeInBytes);
-  _CRTIMP int __cdecl chdir(const char *_Path);
-  _CRTIMP int __cdecl mkdir(const char *_Path);
-  _CRTIMP int __cdecl rmdir(const char *_Path);
-#endif
+  _Check_return_
+  _Ret_opt_z_
+  _CRTIMP
+  char*
+  __cdecl
+  getcwd(
+    _Out_writes_opt_(_SizeInBytes) char *_DstBuf,
+    _In_ int _SizeInBytes);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  chdir(
+    _In_z_ const char *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  mkdir(
+    _In_z_ const char *_Path);
+
+  _Check_return_
+  _CRTIMP
+  int
+  __cdecl
+  rmdir(
+    _In_z_ const char *_Path);
+
+#endif /* NO_OLDNAMES */
 
 #ifdef __cplusplus
 }
