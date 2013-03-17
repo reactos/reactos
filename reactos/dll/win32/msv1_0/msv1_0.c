e2af519ad3e1de2eaa92cb920b532fab55f60835
Eric Kohl <eric.kohl@reactos.org>
2013-03-17 13:55:51 +0000

    [MSV1_0] - Add LsaApLogonUserEx and LsaApLogonUserEx2 stubs. - Delay import samsrv and lsasrv import libs. - Start the implementation of the logon process.
    svn path=/trunk/; revision=58537

--- reactos/dll/win32/msv1_0/msv1_0.c
+++ reactos/dll/win32/msv1_0/msv1_0.c
@@ -20,6 +20,55 @@ LSA_DISPATCH_TABLE DispatchTable;
 
 /* FUNCTIONS ***************************************************************/
 
+static
+NTSTATUS
+GetDomainSid(PRPC_SID *Sid)
+{
+    LSAPR_HANDLE PolicyHandle = NULL;
+    PLSAPR_POLICY_INFORMATION PolicyInfo = NULL;
+    ULONG Length = 0;
+    NTSTATUS Status;
+
+    Status = LsaIOpenPolicyTrusted(&PolicyHandle);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("LsaIOpenPolicyTrusted() failed (Status 0x%08lx)\n", Status);
+        return Status;
+    }
+
+    Status = LsarQueryInformationPolicy(PolicyHandle,
+                                        PolicyAccountDomainInformation,
+                                        &PolicyInfo);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("LsarQueryInformationPolicy() failed (Status 0x%08lx)\n", Status);
+        goto done;
+    }
+
+    Length = RtlLengthSid(PolicyInfo->PolicyAccountDomainInfo.Sid);
+
+    *Sid = RtlAllocateHeap(RtlGetProcessHeap(), 0, Length);
+    if (*Sid == NULL)
+    {
+        ERR("Failed to allocate SID\n");
+        Status = STATUS_INSUFFICIENT_RESOURCES;
+        goto done;
+    }
+
+    memcpy(*Sid, PolicyInfo->PolicyAccountDomainInfo.Sid, Length);
+
+done:
+    if (PolicyInfo != NULL)
+        LsaIFree_LSAPR_POLICY_INFORMATION(PolicyAccountDomainInformation,
+                                          PolicyInfo);
+
+    if (PolicyHandle != NULL)
+        LsarClose(&PolicyHandle);
+
+    return Status;
+}
+
+
 /*
  * @unimplemented
  */
@@ -95,6 +144,10 @@ LsaApInitializePackage(IN ULONG AuthenticationPackageId,
     /* Get the dispatch table entries */
     DispatchTable.AllocateLsaHeap = LsaDispatchTable->AllocateLsaHeap;
     DispatchTable.FreeLsaHeap = LsaDispatchTable->FreeLsaHeap;
+    DispatchTable.AllocateClientBuffer = LsaDispatchTable->AllocateClientBuffer;
+    DispatchTable.FreeClientBuffer = LsaDispatchTable->FreeClientBuffer;
+    DispatchTable.CopyToClientBuffer = LsaDispatchTable->CopyToClientBuffer;
+    DispatchTable.CopyFromClientBuffer = LsaDispatchTable->CopyFromClientBuffer;
 
 
     /* Return the package name */
@@ -149,7 +202,186 @@ LsaApLogonUser(IN PLSA_CLIENT_REQUEST ClientRequest,
                OUT PLSA_UNICODE_STRING *AccountName,
                OUT PLSA_UNICODE_STRING *AuthenticatingAuthority)
 {
+    PMSV1_0_INTERACTIVE_LOGON LogonInfo;
+
+    SAMPR_HANDLE ServerHandle = NULL;
+    SAMPR_HANDLE DomainHandle = NULL;
+    PRPC_SID AccountDomainSid = NULL;
+    RPC_UNICODE_STRING Names[1];
+    SAMPR_ULONG_ARRAY RelativeIds = {0, NULL};
+    SAMPR_ULONG_ARRAY Use = {0, NULL};
+    NTSTATUS Status;
+
     TRACE("()\n");
+
+    TRACE("LogonType: %lu\n", LogonType);
+    TRACE("AuthenticationInformation: %p\n", AuthenticationInformation);
+    TRACE("AuthenticationInformationLength: %lu\n", AuthenticationInformationLength);
+
+
+    *ProfileBuffer = NULL;
+    *ProfileBufferLength = 0;
+    *SubStatus = STATUS_SUCCESS;
+
+    if (LogonType == Interactive ||
+        LogonType == Batch ||
+        LogonType == Service)
+    {
+        ULONG_PTR PtrOffset;
+
+        LogonInfo = (PMSV1_0_INTERACTIVE_LOGON)AuthenticationInformation;
+
+        /* Fix-up pointers in the authentication info */
+        PtrOffset = (ULONG_PTR)AuthenticationInformation - (ULONG_PTR)ClientAuthenticationBase;
+
+        LogonInfo->LogonDomainName.Buffer = (PWSTR)((ULONG_PTR)LogonInfo->LogonDomainName.Buffer + PtrOffset);
+        LogonInfo->UserName.Buffer = (PWSTR)((ULONG_PTR)LogonInfo->UserName.Buffer + PtrOffset);
+        LogonInfo->Password.Buffer = (PWSTR)((ULONG_PTR)LogonInfo->Password.Buffer + PtrOffset);
+
+        TRACE("Domain: %S\n", LogonInfo->LogonDomainName.Buffer);
+        TRACE("User: %S\n", LogonInfo->UserName.Buffer);
+        TRACE("Password: %S\n", LogonInfo->Password.Buffer);
+    }
+    else
+    {
+        FIXME("LogonType %lu is not supported yet!\n", LogonType);
+        return STATUS_NOT_IMPLEMENTED;
+    }
+
+    Status = GetDomainSid(&AccountDomainSid);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("GetDomainSid() failed (Status 0x%08lx)\n", Status);
+        return Status;
+    }
+
+    /* Connect to the SAM server */
+    Status = SamIConnect(NULL,
+                         &ServerHandle,
+                         SAM_SERVER_CONNECT | SAM_SERVER_LOOKUP_DOMAIN,
+                         TRUE);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("SamIConnect() failed (Status 0x%08lx)\n", Status);
+        goto done;
+    }
+
+    /* Open the account domain */
+    Status = SamrOpenDomain(ServerHandle,
+                            DOMAIN_LOOKUP,
+                            AccountDomainSid,
+                            &DomainHandle);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("SamrOpenDomain failed (Status %08lx)\n", Status);
+        goto done;
+    }
+
+    Names[0].Length = LogonInfo->UserName.Length;
+    Names[0].MaximumLength = LogonInfo->UserName.MaximumLength;
+    Names[0].Buffer = LogonInfo->UserName.Buffer;
+
+    /* Try to get the RID for the user name */
+    Status = SamrLookupNamesInDomain(DomainHandle,
+                                     1,
+                                     Names,
+                                     &RelativeIds,
+                                     &Use);
+    if (!NT_SUCCESS(Status))
+    {
+        TRACE("SamrLookupNamesInDomain failed (Status %08lx)\n", Status);
+        Status = STATUS_NO_SUCH_USER;
+        goto done;
+    }
+
+    /* Fail, if it is not a user account */
+    if (Use.Element[0] != SidTypeUser)
+    {
+        TRACE("Account is not a user account!\n");
+        Status = STATUS_NO_SUCH_USER;
+        goto done;
+    }
+
+
+
+done:
+    SamIFree_SAMPR_ULONG_ARRAY(&RelativeIds);
+    SamIFree_SAMPR_ULONG_ARRAY(&Use);
+
+    if (DomainHandle != NULL)
+        SamrCloseHandle(&DomainHandle);
+
+    if (ServerHandle != NULL)
+        SamrCloseHandle(&ServerHandle);
+
+    if (AccountDomainSid != NULL)
+        RtlFreeHeap(RtlGetProcessHeap(), 0, AccountDomainSid);
+
+    TRACE("LsaApLogonUser done (Status %08lx)\n", Status);
+
+    return Status;
+}
+
+
+/*
+ * @unimplemented
+ */
+NTSTATUS
+NTAPI
+LsaApLogonUserEx(IN PLSA_CLIENT_REQUEST ClientRequest,
+                 IN SECURITY_LOGON_TYPE LogonType,
+                 IN PVOID AuthenticationInformation,
+                 IN PVOID ClientAuthenticationBase,
+                 IN ULONG AuthenticationInformationLength,
+                 OUT PVOID *ProfileBuffer,
+                 OUT PULONG ProfileBufferLength,
+                 OUT PLUID LogonId,
+                 OUT PNTSTATUS SubStatus,
+                 OUT PLSA_TOKEN_INFORMATION_TYPE TokenInformationType,
+                 OUT PVOID *TokenInformation,
+                 OUT PUNICODE_STRING *AccountName,
+                 OUT PUNICODE_STRING *AuthenticatingAuthority,
+                 OUT PUNICODE_STRING *MachineName)
+{
+    TRACE("()\n");
+
+    TRACE("LogonType: %lu\n", LogonType);
+    TRACE("AuthenticationInformation: %p\n", AuthenticationInformation);
+    TRACE("AuthenticationInformationLength: %lu\n", AuthenticationInformationLength);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+/*
+ * @unimplemented
+ */
+NTSTATUS
+NTAPI
+LsaApLogonUserEx2(IN PLSA_CLIENT_REQUEST ClientRequest,
+                  IN SECURITY_LOGON_TYPE LogonType,
+                  IN PVOID ProtocolSubmitBuffer,
+                  IN PVOID ClientBufferBase,
+                  IN ULONG SubmitBufferSize,
+                  OUT PVOID *ProfileBuffer,
+                  OUT PULONG ProfileBufferSize,
+                  OUT PLUID LogonId,
+                  OUT PNTSTATUS SubStatus,
+                  OUT PLSA_TOKEN_INFORMATION_TYPE TokenInformationType,
+                  OUT PVOID *TokenInformation,
+                  OUT PUNICODE_STRING *AccountName,
+                  OUT PUNICODE_STRING *AuthenticatingAuthority,
+                  OUT PUNICODE_STRING *MachineName,
+                  OUT PSECPKG_PRIMARY_CRED PrimaryCredentials,
+                  OUT PSECPKG_SUPPLEMENTAL_CRED_ARRAY *SupplementalCredentials)
+{
+    TRACE("()\n");
+
+    TRACE("LogonType: %lu\n", LogonType);
+    TRACE("ProtocolSubmitBuffer: %p\n", ProtocolSubmitBuffer);
+    TRACE("SubmitBufferSize: %lu\n", SubmitBufferSize);
+
+
     return STATUS_NOT_IMPLEMENTED;
 }
 
