58c8c13693eff10c74eda785a9f529b06cd4e5e4
Amine Khaldi <amine.khaldi@reactos.org>
2013-04-13 23:28:46 +0000

    [PSDK] * Import audioclient.idl and mmdeviceapi.idl from Wine and add them to the generated PSDK headers. * Add missing WAVE_FORMAT_IEEE_FLOAT definition to mmreg.h. * Import audiosessiontypes.h, devpkey.h and propkey.h from Wine.
    svn path=/trunk/; revision=58751

--- reactos/include/psdk/mmreg.h
+++ reactos/include/psdk/mmreg.h
@@ -92,6 +92,7 @@ typedef const struct _WAVEFORMATEX *LPCWAVEFORMATEX;
 /* WAVE form wFormatTag IDs */
 #define  WAVE_FORMAT_UNKNOWN			0x0000	/*  Microsoft Corporation  */
 #define  WAVE_FORMAT_ADPCM			0x0002	/*  Microsoft Corporation  */
+#define  WAVE_FORMAT_IEEE_FLOAT			0x0003	/*  Microsoft Corporation  */
 #define  WAVE_FORMAT_IBM_CVSD			0x0005	/*  IBM Corporation  */
 #define  WAVE_FORMAT_ALAW			0x0006	/*  Microsoft Corporation  */
 #define  WAVE_FORMAT_MULAW			0x0007	/*  Microsoft Corporation  */
