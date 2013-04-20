44de93b3cfff49a8427ad760c830f7625c96d37e
Eric Kohl <eric.kohl@reactos.org>
2013-04-20 19:21:37 +0000

    [SAMSRV] Add missing header file.
    svn path=/trunk/; revision=58793

---
+++ reactos/dll/win32/samsrv/resources.h
@@ -0,0 +1,27 @@
+/*
+ * PROJECT:     Local Security Authority Server DLL
+ * LICENSE:     GPL - See COPYING in the top level directory
+ * FILE:        dll/win32/samsrv/resource.h
+ * PURPOSE:     Resource IDs
+ * COPYRIGHT:   Copyright 2013 Eric Kohl
+ */
+
+#pragma once
+
+#define IDS_DOMAIN_BUILTIN_NAME           100
+
+#define IDS_ALIAS_ADMINISTRATORS_NAME     110
+#define IDS_ALIAS_ADMINISTRATORS_COMMENT  111
+#define IDS_ALIAS_GUESTS_NAME             112
+#define IDS_ALIAS_GUESTS_COMMENT          113
+#define IDS_ALIAS_POWER_USERS_NAME        114
+#define IDS_ALIAS_POWER_USERS_COMMENT     115
+#define IDS_ALIAS_USERS_NAME              116
+#define IDS_ALIAS_USERS_COMMENT           117
+
+#define IDS_USER_ADMINISTRATOR_NAME       120
+#define IDS_USER_ADMINISTRATOR_COMMENT    121
+#define IDS_USER_GUEST_NAME               122
+#define IDS_USER_GUEST_COMMENT            123
+
+/* EOF */
