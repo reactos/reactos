3e16da2604f75f4ef8d0ed53fd274c7968c1a99e
Thomas Faber <thomas.faber@reactos.org>
2013-04-21 19:00:10 +0000

    [KMTESTS] - Add a kernel32 test (I know right) to check how FindFirstFile* modifies wildcards ROSTESTS-104 #resolve
    svn path=/trunk/; revision=58813

--- rostests/kmtests/kmtest/testlist.c
+++ rostests/kmtests/kmtest/testlist.c
@@ -8,6 +8,7 @@
 #include <kmt_test.h>
 
 KMT_TESTFUNC Test_Example;
+KMT_TESTFUNC Test_FindFile;
 KMT_TESTFUNC Test_IoDeviceObject;
 KMT_TESTFUNC Test_RtlAvlTree;
 KMT_TESTFUNC Test_RtlException;
@@ -19,6 +20,7 @@ KMT_TESTFUNC Test_RtlUnicodeString;
 const KMT_TEST TestList[] =
 {
     { "Example",            Test_Example },
+    { "FindFile",           Test_FindFile },
     { "IoDeviceObject",     Test_IoDeviceObject },
     { "RtlAvlTree",         Test_RtlAvlTree },
     { "RtlException",       Test_RtlException },
