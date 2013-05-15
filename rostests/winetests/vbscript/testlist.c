0af75befc5e7a9a3bb7d03a06cdee4571e1f9731
Amine Khaldi <amine.khaldi@reactos.org>
2013-05-15 17:33:58 +0000

    [VBSCRIPT_WINETEST] * Import from Wine 1.5.26.
    svn path=/trunk/; revision=59018

---
+++ rostests/winetests/vbscript/testlist.c
@@ -0,0 +1,16 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_createobj(void);
+extern void func_run(void);
+extern void func_vbscript(void);
+
+const struct test winetest_testlist[] =
+{
+    { "createobj", func_createobj },
+    { "run", func_run },
+    { "vbscript", func_vbscript },
+    { 0, 0 }
+};
