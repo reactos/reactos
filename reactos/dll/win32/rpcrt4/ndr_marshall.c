bbf97892df1125d4778909d794850dbd2ee6fcc4
Eric Kohl <eric.kohl@reactos.org>
2013-05-18 19:19:27 +0000

    [RPCRT4] Fix a bug that corrupted in-parameters when a remote function is called. If the in-parameter is an array of UNICODE_STRINGS, the bug terminates all strings at the length of the first string. For example, when LsaLookupNames is called in order to retrieve the SIDs of the "Guest" and "Administrator" users, the remote function received the strings "Guest" and "Admin".
    The patch will be sent to the WINE project after a similar bug for out-parameters has been fixed too.
    
    svn path=/trunk/; revision=59038

--- reactos/dll/win32/rpcrt4/ndr_marshall.c
+++ reactos/dll/win32/rpcrt4/ndr_marshall.c
@@ -1210,7 +1210,7 @@ static unsigned char * EmbeddedPointerMarshall(PMIDL_STUB_MESSAGE pStubMsg,
         unsigned char *bufptr = bufbase + *(const SHORT*)&info[2];
         unsigned char *saved_memory = pStubMsg->Memory;
 
-        pStubMsg->Memory = pMemory;
+        pStubMsg->Memory = membase;
         PointerMarshall(pStubMsg, bufptr, *(unsigned char**)memptr, info+4);
         pStubMsg->Memory = saved_memory;
       }
@@ -1362,7 +1362,7 @@ static void EmbeddedPointerBufferSize(PMIDL_STUB_MESSAGE pStubMsg,
         unsigned char *memptr = membase + *(const SHORT*)&info[0];
         unsigned char *saved_memory = pStubMsg->Memory;
 
-        pStubMsg->Memory = pMemory;
+        pStubMsg->Memory = membase;
         PointerBufferSize(pStubMsg, *(unsigned char**)memptr, info+4);
         pStubMsg->Memory = saved_memory;
       }
