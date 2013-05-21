816b8786c89b31301ae03e62ba4bb185b99bcbd5
Amine Khaldi <amine.khaldi@reactos.org>
2013-05-21 19:56:35 +0000

    [WININET_WINETEST] * Improve the headers inclusion for urlcache.c.
    svn path=/trunk/; revision=59063

--- rostests/winetests/wininet/urlcache.c
+++ rostests/winetests/wininet/urlcache.c
@@ -22,6 +22,10 @@
 //#include <stdio.h>
 //#include <stdlib.h>
 
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
 #include <windef.h>
 #include <winbase.h>
 #include <wininet.h>
