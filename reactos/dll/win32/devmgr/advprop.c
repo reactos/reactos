cd06cd06376a55a6641d5d88612a81f1be552605
Christoph von Wittich <christoph_vw@reactos.org>
2013-05-28 17:18:41 +0000

    [DEVMGR] reboot instead of shutdown after driver update
    svn path=/trunk/; revision=59096

--- reactos/dll/win32/devmgr/advprop.c
+++ reactos/dll/win32/devmgr/advprop.c
@@ -597,7 +597,7 @@ AdvProcDriverDlgProc(IN HWND hwndDlg,
                                         }
 
                                         /* Finally shut down the system */
-                                        if(!ExitWindowsEx(EWX_POWEROFF, SHTDN_REASON_MAJOR_OTHER | SHTDN_REASON_MINOR_OTHER | SHTDN_REASON_FLAG_PLANNED))
+                                        if(!ExitWindowsEx(EWX_REBOOT, SHTDN_REASON_MAJOR_OTHER | SHTDN_REASON_MINOR_OTHER | SHTDN_REASON_FLAG_PLANNED))
                                         {
                                             DPRINT("ExitWindowsEx failed\n");
                                             break;
