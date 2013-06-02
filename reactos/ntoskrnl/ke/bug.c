a29545474941626cc9e9ea82f1f6eda90d5c3954
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2013-06-02 18:41:40 +0000

    We use Jira now, as our bug tracker. Patch by André Guibert de Bruet CORE-7219 #resolve #comment Committed in r59149, thanks ;)
    svn path=/trunk/; revision=59149

--- reactos/ntoskrnl/ke/bug.c
+++ reactos/ntoskrnl/ke/bug.c
@@ -300,7 +300,7 @@ KeRosDumpTriageForBugZillaReport(VOID)
     extern ULONG KeI386MachineType, MxcsrFeatureMask;
     extern BOOLEAN Ke386Pae, Ke386NoExecute;
 
-    DbgPrint("ReactOS has crashed! Please go to http://www.reactos.org/bugzilla/enter_bug.cgi to file a bug!\n");
+    DbgPrint("ReactOS has crashed! Please go to http://jira.reactos.org/ to file a bug!\n");
     DbgPrint("\nHardware Information\n");
     DbgPrint("Processor Architecture: %d\n"
              "Feature Bits: %d\n"
