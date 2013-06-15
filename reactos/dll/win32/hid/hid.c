32a23aa9ab701cd9cfba3b068272c3fc0f80b2fd
Johannes Anderwald <johannes.anderwald@reactos.org>
2013-06-15 06:24:03 +0000

    [HID] - Fix function signatures - Fix build
    svn path=/trunk/; revision=59218

--- reactos/dll/win32/hid/hid.c
+++ reactos/dll/win32/hid/hid.c
@@ -126,7 +126,7 @@ HIDAPI
 NTSTATUS WINAPI
 HidP_GetButtonCaps(IN HIDP_REPORT_TYPE ReportType,
                    OUT PHIDP_BUTTON_CAPS ButtonCaps,
-                   IN OUT PULONG ButtonCapsLength,
+                   IN OUT PUSHORT ButtonCapsLength,
                    IN PHIDP_PREPARSED_DATA PreparsedData)
 {
   return HidP_GetSpecificButtonCaps(ReportType, 0, 0, 0, ButtonCaps,
@@ -343,7 +343,7 @@ HIDAPI
 NTSTATUS WINAPI
 HidP_GetValueCaps(IN HIDP_REPORT_TYPE ReportType,
                   OUT PHIDP_VALUE_CAPS ValueCaps,
-                  IN OUT PULONG ValueCapsLength,
+                  IN OUT PUSHORT ValueCapsLength,
                   IN PHIDP_PREPARSED_DATA PreparsedData)
 {
   return HidP_GetSpecificValueCaps(ReportType, 0, 0, 0, ValueCaps,
