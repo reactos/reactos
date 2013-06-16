2146622d0f97775f169a650661e1ed45c687818e
Johannes Anderwald <johannes.anderwald@reactos.org>
2013-06-16 09:17:29 +0000

    [VIDEOPRT] - Check if there is a dma adapter CORE-7181 #comment please retest
    svn path=/trunk/; revision=59224

--- reactos/win32ss/drivers/videoprt/dma.c
+++ reactos/win32ss/drivers/videoprt/dma.c
@@ -229,8 +229,11 @@ VideoPortGetCommonBuffer(IN PVOID HwDeviceExtension,
         return NULL;
     }
 
-    /* sanity check */
-    ASSERT(!IsListEmpty(&DeviceExtension->DmaAdapterList));
+    if (IsListEmpty(&DeviceExtension->DmaAdapterList))
+    {
+        /* no adapter available */
+        return NULL;
+    }
 
     /* grab first dma adapter */
     VpDmaAdapter = (PVIP_DMA_ADAPTER)CONTAINING_RECORD(DeviceExtension->DmaAdapterList.Flink, VIP_DMA_ADAPTER, Entry);
@@ -239,7 +242,6 @@ VideoPortGetCommonBuffer(IN PVOID HwDeviceExtension,
     ASSERT(VpDmaAdapter->HwDeviceExtension == HwDeviceExtension);
     ASSERT(VpDmaAdapter->Adapter != NULL);
     ASSERT(VpDmaAdapter->MapRegisters != 0);
-
 
     /* allocate common buffer */
     Result = VideoPortAllocateCommonBuffer(HwDeviceExtension, (PVP_DMA_ADAPTER)VpDmaAdapter, DesiredLength, LogicalAddress, CacheEnabled, NULL);
