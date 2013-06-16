4f0b8d3db0ecd44a6dcdc9177b4c3e84a60a92a0
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2013-06-16 22:01:41 +0000

    Create a branch for Aleksandar Andrejevic for his work on NTVDM. See http://jira.reactos.org/browse/CORE-7250 for more details.
    svn path=/branches/ntvdm/; revision=59241

--- rostests/apitests/crt/strcpy.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#include <wine/test.h>
-
-START_TEST(strcpy)
-{
-}
-
--- rostests/apitests/user32/wsprintf.c
+++
@@ -1,4 +0,0 @@
-#define sprintf wsprintfA
-#define func_sprintf func_wsprintf
-#define TEST_USER32 1
-#include "../crt/sprintf.c"
