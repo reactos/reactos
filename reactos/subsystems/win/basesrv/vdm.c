d1f2547346d19bb2080afff1b7c4f8728a340fe7
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2013-08-18 19:46:22 +0000

    [BASESRV] Add vdm.c for Virtual DOS Machines management.
    svn path=/trunk/; revision=59774

---
+++ reactos/subsystems/win/basesrv/vdm.c
@@ -0,0 +1,16 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS Base API Server DLL
+ * FILE:            subsystems/win/basesrv/vdm.c
+ * PURPOSE:         Virtual DOS Machines Management
+ * PROGRAMMERS:     Hermes Belusca-Maito (hermes.belusca@sfr.fr)
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include "basesrv.h"
+
+#define NDEBUG
+#include <debug.h>
+
+/* EOF */
