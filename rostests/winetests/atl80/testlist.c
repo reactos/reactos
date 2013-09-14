6c5cf5c2868422ed990ef6bee94c6a66df609373
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-14 17:43:11 +0000

    [ATL80_WINETEST] * Import from Wine 1.7.1.
    svn path=/trunk/; revision=60108

---
+++ rostests/winetests/atl80/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_atl(void);
+
+const struct test winetest_testlist[] =
+{
+    { "atl", func_atl },
+    { 0, 0 }
+};
