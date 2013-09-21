8c161e012443b8d51b83aa1c81f09d0d81d065aa
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-21 13:13:56 +0000

    [MPR_WINETEST] * Import from Wine 1.7.1. CORE-7469
    svn path=/trunk/; revision=60277

---
+++ rostests/winetests/mpr/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_mpr(void);
+
+const struct test winetest_testlist[] =
+{
+    { "mpr", func_mpr },
+    { 0, 0 }
+};
