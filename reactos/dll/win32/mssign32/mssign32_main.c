f6a6c78b98847dd3ae5bafe80be2eb97a347ad42
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-23 10:39:45 +0000

    [MSSIGN32] * Sync with Wine 1.7.1. CORE-7469
    svn path=/trunk/; revision=60332

--- reactos/dll/win32/mssign32/mssign32_main.c
+++ reactos/dll/win32/mssign32/mssign32_main.c
@@ -41,8 +41,6 @@ BOOL WINAPI DllMain( HINSTANCE hinst, DWORD reason, LPVOID lpv )
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls( hinst );
         break;
-    case DLL_PROCESS_DETACH:
-        break;
     }
     return TRUE;
 }
