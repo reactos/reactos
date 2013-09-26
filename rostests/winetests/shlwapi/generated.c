6efe0d39c0b338f3faef9b31d6b00edf58a3c348
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-26 16:52:35 +0000

    [SHLWAPI_WINETEST] * Speedup build.
    svn path=/trunk/; revision=60381

--- rostests/winetests/shlwapi/generated.c
+++ rostests/winetests/shlwapi/generated.c
@@ -9,9 +9,11 @@
 #define _INC_WINDOWS
 #define COM_NO_WINDOWS_H
 
+#ifndef __REACTOS__
 #define WINVER 0x0501
 #define _WIN32_IE 0x0501
 #define _WIN32_WINNT 0x0501
+#endif
 
 #define WINE_NOWINSOCK
 
