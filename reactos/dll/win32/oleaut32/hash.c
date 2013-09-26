17427e7caecd7eef61d2f919143dced667f2e238
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-26 13:59:38 +0000

    [OLEAUT32] * Sync with Wine 1.7.1. CORE-7469
    svn path=/trunk/; revision=60356

--- reactos/dll/win32/oleaut32/hash.c
+++ reactos/dll/win32/oleaut32/hash.c
@@ -544,13 +544,13 @@ ULONG WINAPI LHashValOfNameSysA( SYSKIND skind, LCID lcid, LPCSTR lpStr)
   case LANG_SWEDISH:    case LANG_SYRIAC:     case LANG_TAMIL:
   case LANG_TATAR:      case LANG_TELUGU:     case LANG_THAI:
   case LANG_UKRAINIAN:  case LANG_URDU:       case LANG_UZBEK:
-  case LANG_VIETNAMESE: case LANG_GAELIC:     case LANG_MALTESE:
+  case LANG_VIETNAMESE: case LANG_SCOTTISH_GAELIC: case LANG_MALTESE:
   case LANG_MAORI:      case LANG_ROMANSH:    case LANG_IRISH:
   case LANG_SAMI:       case LANG_UPPER_SORBIAN: case LANG_SUTU:
   case LANG_TSONGA:     case LANG_TSWANA:     case LANG_VENDA:
   case LANG_XHOSA:      case LANG_ZULU:       case LANG_ESPERANTO:
   case LANG_WALON:      case LANG_CORNISH:    case LANG_WELSH:
-  case LANG_BRETON:
+  case LANG_BRETON:     case LANG_MANX_GAELIC:
     nOffset = 16;
     pnLookup = Lookup_16;
     break;
