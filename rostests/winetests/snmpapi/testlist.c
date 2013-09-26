3a319b5be13e2f3db6868560520f64dee8429273
Amine Khaldi <amine.khaldi@reactos.org>
2013-09-26 16:56:01 +0000

    [SNMPAPI_WINETEST] * Speedup build.
    svn path=/trunk/; revision=60384

--- rostests/winetests/snmpapi/testlist.c
+++ rostests/winetests/snmpapi/testlist.c
@@ -1,10 +1,7 @@
 /* Automatically generated file; DO NOT EDIT!! */
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
 #define STANDALONE
-#include "wine/test.h"
+#include <wine/test.h>
 
 extern void func_util(void);
 
