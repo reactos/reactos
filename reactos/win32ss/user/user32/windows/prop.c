769ec79f16358778ae193658b664786f155e0675
James Tabor <james.tabor@reactos.org>
2013-10-15 22:14:48 +0000

    [Win32ss] - Prevent a potential kernel exception.
    svn path=/trunk/; revision=60682

--- reactos/win32ss/user/user32/windows/prop.c
+++ reactos/win32ss/user/user32/windows/prop.c
@@ -53,6 +53,7 @@ IntGetProp(HWND hWnd, ATOM Atom)
   for (i = 0; i < pWnd->PropListItems; i++ )
   {
       Property = CONTAINING_RECORD(ListEntry, PROPERTY, PropListEntry);
+      if (!Property) break;
       if (Property->Atom == Atom)
       {
          return(Property);
