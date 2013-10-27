b8fa6d1de4bbee37bf3ca53321aecb784b592131
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2013-10-27 22:08:41 +0000

    [NTVDM]: Introduce the SEG_OFF_TO_PTR macro for code simplification purposes.
    svn path=/branches/ntvdm/; revision=60774

--- subsystems/ntvdm/ntvdm.h
+++ subsystems/ntvdm/ntvdm.h
@@ -25,7 +25,10 @@
 #define MAX_SEGMENT 0xFFFF
 #define MAX_OFFSET 0xFFFF
 #define MAX_ADDRESS TO_LINEAR(MAX_SEGMENT, MAX_OFFSET)
-#define FAR_POINTER(x) ((ULONG_PTR)BaseAddress + TO_LINEAR(HIWORD(x), LOWORD(x)))
+
+#define FAR_POINTER(x)              ((ULONG_PTR)BaseAddress + TO_LINEAR(HIWORD(x), LOWORD(x)))
+#define SEG_OFF_TO_PTR(seg, off)    ((ULONG_PTR)BaseAddress + TO_LINEAR((seg), (off)))
+
 #define STEPS_PER_CYCLE 256
 
 /* FUNCTIONS ******************************************************************/
