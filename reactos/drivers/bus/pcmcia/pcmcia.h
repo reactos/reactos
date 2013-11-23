e5769b0d386dd27fbbc6be43316184d222c2f189
Eric Kohl <eric.kohl@reactos.org>
2013-11-23 18:36:05 +0000

    [FORMATTING] Fix indentation. No code changes!
    svn path=/trunk/; revision=61084

--- reactos/drivers/bus/pcmcia/pcmcia.h
+++ reactos/drivers/bus/pcmcia/pcmcia.h
@@ -4,32 +4,36 @@
 #include <stdio.h>
 #include <ntddk.h>
 
-typedef enum {
-  dsStopped,
-  dsStarted,
-  dsPaused,
-  dsRemoved,
-  dsSurpriseRemoved
+typedef enum
+{
+    dsStopped,
+    dsStarted,
+    dsPaused,
+    dsRemoved,
+    dsSurpriseRemoved
 } PCMCIA_DEVICE_STATE;
 
-typedef struct _PCMCIA_COMMON_EXTENSION {
-  PDEVICE_OBJECT Self;
-  BOOLEAN IsFDO;
-  POWER_SEQUENCE PowerSequence;
-  PCMCIA_DEVICE_STATE State;
-  DEVICE_POWER_STATE DevicePowerState;
-  SYSTEM_POWER_STATE SystemPowerState;
+typedef struct _PCMCIA_COMMON_EXTENSION
+{
+    PDEVICE_OBJECT Self;
+    BOOLEAN IsFDO;
+    POWER_SEQUENCE PowerSequence;
+    PCMCIA_DEVICE_STATE State;
+    DEVICE_POWER_STATE DevicePowerState;
+    SYSTEM_POWER_STATE SystemPowerState;
 } PCMCIA_COMMON_EXTENSION, *PPCMCIA_COMMON_EXTENSION;
 
-typedef struct _PCMCIA_PDO_EXTENSION {
-  PCMCIA_COMMON_EXTENSION Common;
+typedef struct _PCMCIA_PDO_EXTENSION
+{
+    PCMCIA_COMMON_EXTENSION Common;
 } PCMCIA_PDO_EXTENSION, *PPCMCIA_PDO_EXTENSION;
 
-typedef struct _PCMCIA_FDO_EXTENSION {
-  PCMCIA_COMMON_EXTENSION Common;
-  PDEVICE_OBJECT Ldo;
-  LIST_ENTRY ChildDeviceList;
-  KSPIN_LOCK Lock;
+typedef struct _PCMCIA_FDO_EXTENSION
+{
+    PCMCIA_COMMON_EXTENSION Common;
+    PDEVICE_OBJECT Ldo;
+    LIST_ENTRY ChildDeviceList;
+    KSPIN_LOCK Lock;
 } PCMCIA_FDO_EXTENSION, *PPCMCIA_FDO_EXTENSION;
 
 /* pdo.c */
