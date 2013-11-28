72f576f3c4e0f27f6910939ba6a6f617da3377e4
Amine Khaldi <amine.khaldi@reactos.org>
2013-11-28 22:04:03 +0000

    [APPLICATIONS] * Improve resource files. CORE-7637
    svn path=/trunk/; revision=61122

--- reactos/base/applications/sndrec32/resource.h
+++ reactos/base/applications/sndrec32/resource.h
@@ -1,61 +1,50 @@
 #pragma once
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by rsrc.rc
-//
-#define IDC_MYICON                      2
-#define IDB_BITMAP2_STOP_DIS            8
-#define IDD_REACTOS_SNDREC32_DIALOG     102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
-#define IDI_REACTOS_SNDREC32            107
-#define IDI_REACTOS_SNDREC32LL          107
-#define IDI_SNDREC32                    107
-#define IDI_SMALL                       108
-#define IDC_REACTOS_SNDREC32            109
-#define IDR_MAINFRAME                   128
-#define IDB_BITMAP1                     132
-#define IDB_BITMAP_REC                  132
-#define IDB_BITMAP2_START               133
-#define IDB_BITMAP2_END                 134
-#define IDB_BITMAP2_PLAY                135
-#define IDB_BITMAP2_STOP                136
-#define IDB_BITMAP2_REC                 137
-#define IDI_ICON1                       138
-#define IDR_MENU1                       139
-#define IDI_ICON2                       140
-#define IDB_BITMAP2_END_DIS             141
-#define IDB_BITMAP2_PLAY_DIS            142
-#define IDB_BITMAP2_REC_DIS             143
-#define IDB_BITMAP2_START_DIS           144
-#define IDS_STRPOS                      400
-#define IDS_STRDUR                      401
-#define IDS_STRBUF                      402
-#define IDS_STRFMT                      403
-#define IDS_STRMONO                     404
-#define IDS_STRSTEREO                   405
-#define IDS_STRING406                   406
-#define IDS_STRCHAN                     406
-#define ID_FILE_NEW                     32771
-#define ID_FILE_OPEN                    32772
-#define ID_FILE_SAVE                    32773
-#define ID_FILE_SAVEAS                  32774
-#define ID_FILE_EXIT                    32775
-#define ID__ABOUT                       32776
-#define ID_EXIT                         32777
-#define ID_NEW                          32778
-#define IDC_STATIC                      -1
+#define IDC_MYICON           2
+#define IDB_BITMAP2_STOP_DIS 8
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        145
-#define _APS_NEXT_COMMAND_VALUE         32779
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
-#endif
-#endif
+#define IDD_REACTOS_SNDREC32_DIALOG 102
+#define IDS_APP_TITLE               103
+#define IDD_ABOUTBOX                103
+#define IDM_ABOUT                   104
+#define IDI_REACTOS_SNDREC32        107
+#define IDI_REACTOS_SNDREC32LL      107
+#define IDI_SNDREC32                107
+#define IDI_SMALL                   108
+#define IDC_REACTOS_SNDREC32        109
+
+#define IDR_MAINFRAME         128
+#define IDB_BITMAP1           132
+#define IDB_BITMAP_REC        132
+#define IDB_BITMAP2_START     133
+#define IDB_BITMAP2_END       134
+#define IDB_BITMAP2_PLAY      135
+#define IDB_BITMAP2_STOP      136
+#define IDB_BITMAP2_REC       137
+#define IDI_ICON1             138
+#define IDR_MENU1             139
+#define IDI_ICON2             140
+#define IDB_BITMAP2_END_DIS   141
+#define IDB_BITMAP2_PLAY_DIS  142
+#define IDB_BITMAP2_REC_DIS   143
+#define IDB_BITMAP2_START_DIS 144
+
+#define IDS_STRPOS    400
+#define IDS_STRDUR    401
+#define IDS_STRBUF    402
+#define IDS_STRFMT    403
+#define IDS_STRMONO   404
+#define IDS_STRSTEREO 405
+#define IDS_STRING406 406
+#define IDS_STRCHAN   406
+
+#define ID_FILE_NEW    32771
+#define ID_FILE_OPEN   32772
+#define ID_FILE_SAVE   32773
+#define ID_FILE_SAVEAS 32774
+#define ID_FILE_EXIT   32775
+#define ID__ABOUT      32776
+#define ID_EXIT        32777
+#define ID_NEW         32778
+
+#define IDC_STATIC -1
