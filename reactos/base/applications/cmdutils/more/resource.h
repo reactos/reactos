c5e13cf799b9faf4c4c7381641a62de44c2daedf
Amine Khaldi <amine.khaldi@reactos.org>
2013-11-28 21:52:17 +0000

    [CMDUTILS] * Improve resource files. CORE-7637
    svn path=/trunk/; revision=61117

--- reactos/base/applications/cmdutils/more/resource.h
+++ reactos/base/applications/cmdutils/more/resource.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define IDS_USAGE           100
-#define IDS_CONTINUE        101
-#define IDS_FILE_ACCESS     102
+#define IDS_USAGE       100
+#define IDS_CONTINUE    101
+#define IDS_FILE_ACCESS 102
