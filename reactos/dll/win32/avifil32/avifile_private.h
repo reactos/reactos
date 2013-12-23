90c20950e88e245a86c4950c857634fcac6e46a0
Amine Khaldi <amine.khaldi@reactos.org>
2013-12-23 14:44:50 +0000

    [AVIFIL32] * Move some inclusions to the main header. * Extract the resource related definitions into resource.h and include it in both the main header and the resource file. * Set the default debugging channel globally through the main header. * Remove inclusions and definitions that already exist in the main header/resource file. CORE-7716
    svn path=/trunk/; revision=61347

--- reactos/dll/win32/avifil32/avifile_private.h
+++ reactos/dll/win32/avifil32/avifile_private.h
@@ -19,8 +19,29 @@
 #ifndef __AVIFILE_PRIVATE_H
 #define __AVIFILE_PRIVATE_H
 
-//#include "windef.h"
-//#include "winuser.h"
+#include <assert.h>
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
+#define COBJMACROS
+
+#include <initguid.h>
+
+#include <windef.h>
+#include <winbase.h>
+#include <wingdi.h>
+#include <vfw.h>
+
+#include <wine/unicode.h>
+
+#include <wine/debug.h>
+WINE_DEFAULT_DEBUG_CHANNEL(avifile);
+
+#include "extrachunk.h"
+#include "resource.h"
 
 #ifndef MAX_AVISTREAMS
 #define MAX_AVISTREAMS 8
@@ -35,22 +56,6 @@
 #define DIBPTR(lp)      ((LPBYTE)(lp) + (lp)->biSize + \
                          (lp)->biClrUsed * sizeof(RGBQUAD))
 #endif
-
-#define IDD_SAVEOPTIONS      0x0100
-#define IDC_INTERLEAVE       0x0110
-#define IDC_INTERLEAVEEVERY  0x0111
-#define IDC_STREAM           0x0112
-#define IDC_OPTIONS          0x0113
-#define IDC_FORMATTEXT       0x0114
-
-#define IDS_WAVESTREAMFORMAT 0x0100
-#define IDS_WAVEFILETYPE     0x0101
-#define IDS_ALLMULTIMEDIA    0x0184
-#define IDS_ALLFILES         0x0185
-#define IDS_VIDEO            0x0189
-#define IDS_AUDIO            0x0190
-#define IDS_AVIFILETYPE      0x0192
-#define IDS_UNCOMPRESSED     0x0193
 
 DEFINE_AVIGUID(CLSID_ICMStream, 0x00020001, 0, 0);
 DEFINE_AVIGUID(CLSID_WAVFile,   0x00020003, 0, 0);
--- reactos/dll/win32/avifil32/icmstream.c
+++ reactos/dll/win32/avifil32/icmstream.c
@@ -16,26 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
-#include <assert.h>
-#include <stdarg.h>
-
-#include <windef.h>
-#include <winbase.h>
-#include <wingdi.h>
-//#include "winuser.h"
-//#include "winerror.h"
-//#include "mmsystem.h"
-#include <vfw.h>
-
 #include "avifile_private.h"
-
-#include <wine/debug.h>
-
-WINE_DEFAULT_DEBUG_CHANNEL(avifile);
 
 #define MAX_FRAMESIZE       (16 * 1024 * 1024)
 #define MAX_FRAMESIZE_DIFF  512
