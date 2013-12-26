4e381684041412ab7ad8217408801b98737aa1f1
Amine Khaldi <amine.khaldi@reactos.org>
2013-12-26 19:11:46 +0000

    [PROPSYS] * Move some inclusions to the main header. * Set the default debugging channel globally through the main header. * Remove inclusions and definitions that already exist in the main header. CORE-7716
    svn path=/trunk/; revision=61431

--- reactos/dll/win32/propsys/propsys_main.c
+++ reactos/dll/win32/propsys/propsys_main.c
@@ -19,26 +19,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
-#define COBJMACROS
-#include <config.h>
-
-#include <stdarg.h>
-
-#include <windef.h>
-#include <winbase.h>
-#include <objbase.h>
-#include <rpcproxy.h>
-#include <propsys.h>
-#include <wine/debug.h>
-#include <wine/unicode.h>
-
 #include "propsys_private.h"
 
-WINE_DEFAULT_DEBUG_CHANNEL(propsys);
+#include <rpcproxy.h>
 
 static HINSTANCE propsys_hInstance;
 
