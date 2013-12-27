41db8a3768ef43286ea2585ebe65076d33d8c7bc
Amine Khaldi <amine.khaldi@reactos.org>
2013-12-27 22:58:57 +0000

    * Fix MSVC build.
    svn path=/trunk/; revision=61452

--- reactos/include/crt/intrin.h
+++ reactos/include/crt/intrin.h
@@ -1,10 +1,12 @@
 
 #pragma once
 
+#ifndef RC_INVOKED
 #ifdef __MINGW32__
 #  include "mingw32/intrin.h"
 #elif defined(_MSC_VER)
 #  include "msc/intrin.h"
 #else
 #  error Please implement intrinsics for your target compiler
+#endif
 #endif
