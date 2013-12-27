ca9b838eb0d551a65a84cbb6778245dc3ba6266d
Amine Khaldi <amine.khaldi@reactos.org>
2013-12-27 14:01:01 +0000

    [TAPI32] * Create a main header and move some inclusions to it. * Set the default debugging channel globally through the main header. * Remove inclusions and definitions that already exist in the main header. CORE-7716
    svn path=/trunk/; revision=61447

--- reactos/dll/win32/tapi32/assisted.c
+++ reactos/dll/win32/tapi32/assisted.c
@@ -19,25 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
-#include <config.h>
-//#include "wine/port.h"
-
-//#include <stdarg.h>
-#include <stdio.h>
-#include <windef.h>
-#include <winbase.h>
-//#include "wingdi.h"
-#include <winreg.h>
-#include <objbase.h>
-#include <tapi.h>
-#include <wine/unicode.h>
-#include <wine/debug.h>
-
-WINE_DEFAULT_DEBUG_CHANNEL(tapi);
+#include "precomp.h"
 
 /***********************************************************************
  *      tapiGetLocationInfoW (TAPI32.@)
--- reactos/dll/win32/tapi32/phone.c
+++ reactos/dll/win32/tapi32/phone.c
@@ -18,17 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
-#include <stdarg.h>
-
-#include <windef.h>
-#include <winbase.h>
-#include <objbase.h>
-#include <tapi.h>
-#include <wine/debug.h>
+#include "precomp.h"
 
 /*
  * Additional TSPI functions:
@@ -38,7 +28,6 @@
  * - TSPI_ProviderEnumDevices
  * - TSPI_ProviderConfig
 */
-WINE_DEFAULT_DEBUG_CHANNEL(tapi);
 
 /***********************************************************************
  *		phoneClose (TAPI32.@)
