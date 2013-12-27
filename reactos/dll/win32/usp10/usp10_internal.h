833e0020bab1e45ad1eda09f2d6fb0b7451cab80
Amine Khaldi <amine.khaldi@reactos.org>
2013-12-27 23:54:47 +0000

    [USP10] * Move some inclusions to the main header. * Remove inclusions and definitions that already exist in the main header. CORE-7716
    svn path=/trunk/; revision=61455

--- reactos/dll/win32/usp10/usp10_internal.h
+++ reactos/dll/win32/usp10/usp10_internal.h
@@ -18,6 +18,23 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  *
  */
+
+#include <config.h>
+
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
+#include <windef.h>
+#include <winbase.h>
+#include <wingdi.h>
+#include <usp10.h>
+
+#include <wine/debug.h>
+#include <wine/unicode.h>
+
 #define MS_MAKE_TAG( _x1, _x2, _x3, _x4 ) \
           ( ( (ULONG)_x4 << 24 ) |     \
             ( (ULONG)_x3 << 16 ) |     \
