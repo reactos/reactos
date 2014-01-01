127665896401304d3df6f99a83369bb53a2883ea
Amine Khaldi <amine.khaldi@reactos.org>
2014-01-01 21:54:58 +0000

    [WINMM] * Move some inclusions to the main header. * Remove inclusions and definitions that already exist in the main header. CORE-7716
    svn path=/trunk/; revision=61480

--- reactos/dll/win32/winmm/lolvldrv.c
+++ reactos/dll/win32/winmm/lolvldrv.c
@@ -21,19 +21,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <config.h>
-//#include "wine/port.h"
-
-//#include <string.h>
-//#include <stdarg.h>
-//#include <stdio.h>
-#include <assert.h>
-//#include "windef.h"
-//#include "winbase.h"
-//#include "winreg.h"
 #include "winemm.h"
-#include <wine/debug.h>
-#include <wine/exception.h>
 
 WINE_DEFAULT_DEBUG_CHANNEL(winmm);
 
