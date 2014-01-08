842cb217758bea9fae1ad6d7b8af070fda059f40
Timo Kreuzer <timo.kreuzer@reactos.org>
2014-01-08 00:01:11 +0000

    [SMLIB] Fix "annotation" and use a better variable name.
    svn path=/trunk/; revision=61573

--- reactos/lib/smlib/smclient.c
+++ reactos/lib/smlib/smclient.c
@@ -67,11 +67,11 @@ NTAPI
 SmConnectToSm(IN PUNICODE_STRING SbApiPortName,
               IN HANDLE SbApiPort,
               IN ULONG ImageType,
-              IN HANDLE SmApiPort)
+              OUT PHANDLE SmApiPort)
 {
     NTSTATUS Status;
     SB_CONNECTION_INFO ConnectInfo;
-    UNICODE_STRING DestinationString;
+    UNICODE_STRING PortName;
     SECURITY_QUALITY_OF_SERVICE SecurityQos;
     ULONG ConnectInfoLength = sizeof(ConnectInfo);
 
@@ -81,7 +81,7 @@ SmConnectToSm(IN PUNICODE_STRING SbApiPortName,
     SecurityQos.EffectiveOnly = TRUE;
 
     /* Set the SM API port name */
-    RtlInitUnicodeString(&DestinationString, L"\\SmApiPort");
+    RtlInitUnicodeString(&PortName, L"\\SmApiPort");
 
     /* Check if this is a client connecting to SMSS, or SMSS to itself */
     if (SbApiPortName)
@@ -108,7 +108,7 @@ SmConnectToSm(IN PUNICODE_STRING SbApiPortName,
 
     /* Connect to SMSS and exchange connection information */
     Status = NtConnectPort(SmApiPort,
-                           &DestinationString,
+                           &PortName,
                            &SecurityQos,
                            NULL,
                            NULL,
