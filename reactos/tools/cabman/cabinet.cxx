20fe3bef9ef50261f119479eb69f62d87128bd99
Thomas Faber <thomas.faber@reactos.org>
2014-01-17 16:07:32 +0000

    [CABMAN] - Fix a memory leak
    svn path=/trunk/; revision=61652

--- reactos/tools/cabman/cabinet.cxx
+++ reactos/tools/cabman/cabinet.cxx
@@ -634,22 +634,26 @@ bool CCabinet::SetCabinetReservedFile(char* FileName)
 {
     FILEHANDLE FileHandle;
     ULONG BytesRead;
+    char* ConvertedFileName;
 
+    ConvertedFileName = ConvertPath(FileName, true);
 #if defined(_WIN32)
-    FileHandle = CreateFile(ConvertPath(FileName, true),  // Open this file
+    FileHandle = CreateFile(ConvertedFileName,  // Open this file
         GENERIC_READ,                    // Open for reading
         FILE_SHARE_READ,                 // Share for reading
         NULL,                            // No security
         OPEN_EXISTING,                   // Existing file only
         FILE_ATTRIBUTE_NORMAL,           // Normal file
         NULL);                           // No attribute template
+    free(ConvertedFileName);
     if (FileHandle == INVALID_HANDLE_VALUE)
     {
         DPRINT(MID_TRACE, ("Cannot open cabinet reserved file.\n"));
         return false;
     }
 #else /* !_WIN32 */
-    FileHandle = fopen(ConvertPath(FileName, true), "rb");
+    FileHandle = fopen(ConvertedFileName, "rb");
+    free(ConvertedFileName);
     if (FileHandle == NULL)
     {
         DPRINT(MID_TRACE, ("Cannot open cabinet reserved file.\n"));
