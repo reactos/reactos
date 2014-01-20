3f75b028f83157bdcbcca83ce235dca6caf7e23d
Amine Khaldi <amine.khaldi@reactos.org>
2014-01-20 13:16:55 +0000

    [ATL100] * Create a main header and move some inclusions to it. CORE-7716
    svn path=/trunk/; revision=61737

--- reactos/dll/win32/atl100/registrar.c
+++ reactos/dll/win32/atl100/registrar.c
@@ -16,22 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
-#define COBJMACROS
-
-#include <stdarg.h>
-#include <windef.h>
-#include <winbase.h>
-#include <winreg.h>
-#include <objbase.h>
-
-#include <atlbase.h>
-
-#include <wine/debug.h>
-#include <wine/unicode.h>
+#include "precomp.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(atl);
 
