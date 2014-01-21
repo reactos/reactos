cabd4e530ebd437e58835ce5c673337ec3b364a1
Amine Khaldi <amine.khaldi@reactos.org>
2014-01-21 17:59:36 +0000

    [COMDLG32] * Remove one time inclusions from the main header and put them back where they belong. * Improve header inclusions. CORE-7716
    svn path=/trunk/; revision=61746

--- reactos/dll/win32/comdlg32/cdlg.h
+++ reactos/dll/win32/comdlg32/cdlg.h
@@ -21,9 +21,8 @@
 #ifndef _WINE_DLL_CDLG_H
 #define _WINE_DLL_CDLG_H
 
-#include <config.h>
+#include <wine/config.h>
 
-#include <stdarg.h>
 #include <stdio.h>
 
 #define WIN32_NO_STATUS
@@ -38,18 +37,15 @@
 #include <winbase.h>
 #include <wingdi.h>
 #include <winuser.h>
-#include <winnls.h>
 #include <wincon.h>
 #include <winternl.h>
-#include <commctrl.h>
 #include <objbase.h>
 #include <commdlg.h>
 #include <shlobj.h>
-#include <shellapi.h>
 #include <dlgs.h>
 #include <cderr.h>
 
-// RegGetValueW is supported by Win2k3 SP1 but headers need Win Vista
+/* RegGetValueW is supported by Win2k3 SP1 but headers need Win Vista */
 #undef _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
 #include <winreg.h>
