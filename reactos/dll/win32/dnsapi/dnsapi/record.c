20a454e74167c51dd0ab2aed9b94bdbd0d3793ff
Amine Khaldi <amine.khaldi@reactos.org>
2014-01-23 11:16:46 +0000

    [DNSAPI] * Improve header inclusions. CORE-7716
    svn path=/trunk/; revision=61762

--- reactos/dll/win32/dnsapi/dnsapi/record.c
+++ reactos/dll/win32/dnsapi/dnsapi/record.c
@@ -17,6 +17,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
+
 #include "precomp.h"
 
 #define NDEBUG
