aa6b0a87fe43ce723b6afb39c4716db022966b8f
Amine Khaldi <amine.khaldi@reactos.org>
2014-01-25 15:49:57 +0000

    [SECUR32] * Remove one time inclusions from the main header and put them back where they belong. CORE-7716
    svn path=/trunk/; revision=61807

--- reactos/dll/win32/secur32/precomp.h
+++ reactos/dll/win32/secur32/precomp.h
@@ -6,8 +6,6 @@
  * PROGRAMMER:      Alex Ionescu (alex@relsoft.net)
  */
 
-/* INCLUDES ******************************************************************/
-
 #include <stdarg.h>
 
 /* SDK/DDK/NDK Headers. */
@@ -16,10 +14,8 @@
 #define COM_NO_WINDOWS_H
 #include <windef.h>
 #include <winbase.h>
-#include <winnls.h>
 #define NTOS_MODE_USER
 #include <ndk/rtlfuncs.h>
-#include <lsass/lsass.h>
 
 #include <secext.h>
 #include <security.h>
