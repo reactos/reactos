05dbef656f553df980ed8921533275619b02e458
Eric Kohl <eric.kohl@reactos.org>
2014-01-29 21:54:49 +0000

    [MSGINA] Create a connection to the LSA upon initialization and use it to change passwords. Logon will use the connection later.
    svn path=/trunk/; revision=61888

--- reactos/dll/win32/msgina/msgina.c
+++ reactos/dll/win32/msgina/msgina.c
@@ -254,6 +254,46 @@ GetRegistrySettings(PGINA_CONTEXT pgContext)
 }
 
 
+static
+BOOL
+ConnectToLsa(
+    PGINA_CONTEXT pgContext)
+{
+    LSA_STRING LogonProcessName;
+    LSA_STRING PackageName;
+    LSA_OPERATIONAL_MODE SecurityMode = 0;
+    NTSTATUS Status;
+
+    /* Connect to the LSA server */
+    RtlInitAnsiString((PANSI_STRING)&LogonProcessName,
+                      "MSGINA");
+
+    Status = LsaRegisterLogonProcess(&LogonProcessName,
+                                     &pgContext->LsaHandle,
+                                     &SecurityMode);
+    if (!NT_SUCCESS(Status))
+    {
+        ERR("LsaRegisterLogonProcess failed (Status 0x%08lx)\n", Status);
+        return FALSE;
+    }
+
+    /* Get the authentication package */
+    RtlInitAnsiString((PANSI_STRING)&PackageName,
+                      MSV1_0_PACKAGE_NAME);
+
+    Status = LsaLookupAuthenticationPackage(pgContext->LsaHandle,
+                                            &PackageName,
+                                            &pgContext->AuthenticationPackage);
+    if (!NT_SUCCESS(Status))
+    {
+        ERR("LsaLookupAuthenticationPackage failed (Status 0x%08lx)\n", Status);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
 /*
  * @implemented
  */
@@ -279,6 +319,13 @@ WlxInitialize(
     if (!GetRegistrySettings(pgContext))
     {
         WARN("GetRegistrySettings() failed\n");
+        LocalFree(pgContext);
+        return FALSE;
+    }
+
+    if (!ConnectToLsa(pgContext))
+    {
+        WARN("ConnectToLsa() failed\n");
         LocalFree(pgContext);
         return FALSE;
     }
