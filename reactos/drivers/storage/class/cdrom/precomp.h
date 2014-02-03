e35722df395729f53fdbee21cb2133771d51003f
Amine Khaldi <amine.khaldi@reactos.org>
2014-02-03 10:35:35 +0000

    [CDROM] * Remove one time inclusions from the main header and put them back where they belong. * Improve header inclusions. CORE-7716
    svn path=/trunk/; revision=61943

--- reactos/drivers/storage/class/cdrom/precomp.h
+++ reactos/drivers/storage/class/cdrom/precomp.h
@@ -1,7 +1,2 @@
-#include <ntddk.h>
-#include <scsi.h>
-#include <ntddscsi.h>
-#include <ntdddisk.h>
-#include <ntddcdrm.h>
-#include <include/class2.h>
-#include <stdio.h>
+#include <ntdef.h>
+#include <ntstatus.h>
