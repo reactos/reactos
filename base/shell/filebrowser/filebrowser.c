3408d41c6039d04cf6bb736c485adb71c467bb4b
Giannis Adamopoulos <gadamopoulos@reactos.org>
2014-02-04 10:49:39 +0000

    [filebrowser] - Add an experimental module called FileBrowser.exe. This program works standalone in windows xp and windows 2003 and opens a new file browser window in its own process. If our browseui.dll is in the same folder, filebrowser will use the implementation of our file browser in windows. That means that standalone it works only in xp and 2003 but with our own browseui it will work on all windows versions (but doesn't work on all for now). This can be used for both testing the implementation of our file browser in windows and as a host process for new file browser windows for explorer_new.exe + rshell.dll
    svn path=/branches/shell-experiments/; revision=61974

---
+++ base/shell/filebrowser/filebrowser.c
@@ -0,0 +1,44 @@
+/*
+ * ReactOS Explorer
+ *
+ * Copyright 2014 Giannis Adamopoulos
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <ShObjIdl.h>
+#include <ShlObj.h>
+
+typedef HRESULT (WINAPI *SH_OPEN_NEW_FRAME)(LPITEMIDLIST pidl, IUnknown *paramC, long param10, long param14);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    HMODULE hBrowseui = LoadLibraryW(L"browseui.dll");
+    if (hBrowseui)
+    {
+        SH_OPEN_NEW_FRAME SHOpenNewFrame = (SH_OPEN_NEW_FRAME)GetProcAddress(hBrowseui, (LPCSTR)103);
+        LPITEMIDLIST pidlDrives;
+        HRESULT hRet = SHGetSpecialFolderLocation(NULL, CSIDL_DRIVES, &pidlDrives);
+        SHOpenNewFrame((LPITEMIDLIST)pidlDrives, NULL, 0, 0);
+    }
+
+    Sleep(3000);
+
+    return 0;
+}
+
