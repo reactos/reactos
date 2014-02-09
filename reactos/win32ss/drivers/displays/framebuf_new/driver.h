9224dcf894a8ee5fe21c64c023b49ab3a99804b2
Amine Khaldi <amine.khaldi@reactos.org>
2014-02-09 22:07:47 +0000

    [FRAMEBUF_NEW] * Add header guards to the main header. * Improve header inclusions. CORE-7716
    svn path=/trunk/; revision=62084

--- reactos/win32ss/drivers/displays/framebuf_new/driver.h
+++ reactos/win32ss/drivers/displays/framebuf_new/driver.h
@@ -6,16 +6,17 @@
  * PROGRAMMERS:     Copyright (c) 1992-1995 Microsoft Corporation
  *                  ReactOS Portable Systems Group
  */
- 
+
+#ifndef _FRAMEBUF_NEW_PCH_
+#define _FRAMEBUF_NEW_PCH_
+
 //#define DBG 1
-#include "stddef.h"
 #include <stdarg.h>
-#include <string.h>
-#include "windef.h"
-#include "wingdi.h"
-#include "winddi.h"
-#include "devioctl.h"
-#include "ntddvdeo.h"
+#include <windef.h>
+#include <wingdi.h>
+#include <winddi.h>
+#include <devioctl.h>
+#include <ntddvdeo.h>
 #include "debug.h"
 
 typedef struct  _PDEV
@@ -74,3 +75,5 @@ VOID NTAPI vDisableSURF(PPDEV);
 #define ALLOC_TAG               'bfDD'        // Four byte tag (characters in
                                               // reverse order) used for memory
                                               // allocations
+
+#endif /* _FRAMEBUF_NEW_PCH_ */
