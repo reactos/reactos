90e1c4d5bc5ff81fa0068be33398f031e1a7c1af
James Tabor <james.tabor@reactos.org>
2014-04-14 06:54:41 +0000

    [Win32k] - Switch menu allocation to desktop stack.
    svn path=/trunk/; revision=62748

--- reactos/win32ss/user/user32/misc/misc.c
+++ reactos/win32ss/user/user32/misc/misc.c
@@ -311,7 +311,7 @@ static const BOOL g_ObjectHeapTypeShared[TYPE_CTYPES] =
 {
     FALSE, /* TYPE_FREE (not used) */
     FALSE, /* TYPE_WINDOW */
-    TRUE,  /* TYPE_MENU  FALSE */
+    FALSE, /* TYPE_MENU */
     TRUE,  /* TYPE_CURSOR */
     TRUE,  /* TYPE_SETWINDOWPOS */
     FALSE, /* TYPE_HOOK */
@@ -324,7 +324,7 @@ static const BOOL g_ObjectHeapTypeShared[TYPE_CTYPES] =
     TRUE,  /* TYPE_MONITOR */
     FALSE, /* TYPE_KBDLAYOUT */
     FALSE, /* TYPE_KBDFILE */
-    TRUE,   /* TYPE_WINEVENTHOOK */
+    TRUE,  /* TYPE_WINEVENTHOOK */
     FALSE, /* TYPE_TIMER */
     FALSE, /* TYPE_INPUTCONTEXT */
     FALSE, /* TYPE_HIDDATA */
