6a788d310448f52704ed484742ce14146e18c46c
Amine Khaldi <amine.khaldi@reactos.org>
2014-04-22 11:36:32 +0000

    [BCRYPT_WINETEST] * Import from Wine 1.7.17. CORE-8080
    svn path=/trunk/; revision=62873

---
+++ rostests/winetests/bcrypt/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_bcrypt(void);
+
+const struct test winetest_testlist[] =
+{
+    { "bcrypt", func_bcrypt },
+    { 0, 0 }
+};
