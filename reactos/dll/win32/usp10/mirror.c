2a9d643eeb3bd378cd2bc9c075f44ece84caf101
Amine Khaldi <amine.khaldi@reactos.org>
2014-04-23 15:03:26 +0000

    [USP10] * Sync with Wine 1.7.17. CORE-8080
    svn path=/trunk/; revision=62926

--- reactos/dll/win32/usp10/mirror.c
+++ reactos/dll/win32/usp10/mirror.c
@@ -1,5 +1,5 @@
 /* Unicode BiDi mirroring */
-/* generated from http://www.unicode.org/Public/6.2.0/ucd/BidiMirroring.txt */
+/* generated from http://www.unicode.org/Public/6.3.0/ucd/BidiMirroring.txt */
 /* DO NOT EDIT!! */
 
 #include <windef.h>
