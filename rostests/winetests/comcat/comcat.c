3bcaa79636b0db439a2f5e8296f855258cf0130f
Amine Khaldi <amine.khaldi@reactos.org>
2014-04-30 12:01:03 +0000

    [COMCAT_WINETEST] * Sync with Wine 1.7.17. * Remove redundant definition. CORE-8080
    svn path=/trunk/; revision=63053

--- rostests/winetests/comcat/comcat.c
+++ rostests/winetests/comcat/comcat.c
@@ -70,15 +70,15 @@ static void do_enum(void)
 	GUID the_cat[1];
 	GUID wanted_guid;
 	ULONG fetched = -1;
-	
-	static WCHAR szCatID[] = {
+
+        static const WCHAR szCatID[] = {
 			'{',
 			'd','e','a','d','c','a','f','e',
 			'-','0','0','0','0','-','0','0','0','0',
 			'-','0','0','0','0',
 			'-','0','0','0','0','0','0','0','0','0','0','0','0',
 			'}',0};
-	static WCHAR szGuid[] = {
+        static const WCHAR szGuid[] = {
 			'{',
 			'd','e','a','d','c','a','f','e','-',
 			'b','e','e','d','-',
