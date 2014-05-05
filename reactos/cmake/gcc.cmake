108e081fe5eaaa82e69111a54ed6ce840807dbfa
Pierre Schweitzer <pierre@reactos.org>
2014-05-05 21:06:13 +0000

    [CMAKE] This was not meant to be committed. Sorry.
    svn path=/trunk/; revision=63171

--- reactos/cmake/gcc.cmake
+++ reactos/cmake/gcc.cmake
@@ -37,7 +37,7 @@ if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
 endif()
 
 if(DBG)
-    add_compile_flags_language("-Wold-style-declaration -Wdeclaration-after-statement -Wshadow" "C")
+    add_compile_flags_language("-Wold-style-declaration -Wdeclaration-after-statement" "C")
 endif()
 add_compile_flags_language("-fno-rtti -fno-exceptions" "CXX")
 
