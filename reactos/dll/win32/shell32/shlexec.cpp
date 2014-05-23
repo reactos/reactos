b3a2dbfc9a70150c6d5793e3af7930117f02c56e
Christoph von Wittich <christoph_vw@reactos.org>
2014-05-23 17:14:39 +0000

    [shell32] revert one line of r63414
    svn path=/trunk/; revision=63423

--- reactos/dll/win32/shell32/shlexec.cpp
+++ reactos/dll/win32/shell32/shlexec.cpp
@@ -459,7 +459,7 @@ static UINT_PTR SHELL_ExecuteW(const WCHAR *lpCmd, WCHAR *env, BOOL shWait,
     startup.dwFlags = STARTF_USESHOWWINDOW;
     startup.wShowWindow = psei->nShow;
     dwCreationFlags = CREATE_UNICODE_ENVIRONMENT;
-    if (!(psei->fMask & SEE_MASK_NO_CONSOLE))
+    if (psei->fMask & SEE_MASK_NO_CONSOLE)
         dwCreationFlags |= CREATE_NEW_CONSOLE;    
     startup.lpTitle = (LPWSTR)(psei->fMask & (SEE_MASK_HASLINKNAME | SEE_MASK_HASTITLE) ? psei->lpClass : NULL);
 
