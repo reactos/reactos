36d73c2c626869ad9bfc6aa972739b0e17f0436d
Aleksandar Andrejevic <aandrejevic@reactos.org>
2014-06-03 22:34:49 +0000

    [NTVDM] Fix the command line NULL-termination.
    svn path=/trunk/; revision=63538

--- reactos/subsystems/ntvdm/dos/dos32krnl/dos.c
+++ reactos/subsystems/ntvdm/dos/dos32krnl/dos.c
@@ -915,6 +915,7 @@ DWORD DosLoadExecutable(IN DOS_EXEC_TYPE LoadType,
     PIMAGE_DOS_HEADER Header;
     PDWORD RelocationTable;
     PWORD RelocWord;
+    LPSTR CmdLinePtr = (LPSTR)CommandLine;
 
     DPRINT1("DosLoadExecutable(%d, %s, %s, %s, 0x%08X, 0x%08X)\n",
             LoadType,
@@ -931,8 +932,8 @@ DWORD DosLoadExecutable(IN DOS_EXEC_TYPE LoadType,
     }
 
     /* NULL-terminate the command line by removing the return carriage character */
-    while (*CommandLine && *CommandLine != '\r') CommandLine++;
-    *(LPSTR)CommandLine = '\0';
+    while (*CmdLinePtr && *CmdLinePtr != '\r') CmdLinePtr++;
+    *CmdLinePtr = '\0';
 
     /* Open a handle to the executable */
     FileHandle = CreateFileA(ExecutablePath,
