60de098b286b97519f51f1d48e298fa89b53f023
Amine Khaldi <amine.khaldi@reactos.org>
2014-06-08 12:08:16 +0000

    * Use SHDeleteKeyW explicitly here.
    svn path=/trunk/; revision=63551

--- reactos/base/applications/cmdutils/reg/reg.c
+++ reactos/base/applications/cmdutils/reg/reg.c
@@ -294,7 +294,7 @@ static int reg_delete(WCHAR *key_name, WCHAR *value_name, BOOL value_empty,
     /* Delete subtree only if no /v* option is given */
     if (!value_name && !value_empty && !value_all)
     {
-        if (SHDeleteKey(root, p) != ERROR_SUCCESS)
+        if (SHDeleteKeyW(root, p) != ERROR_SUCCESS)
         {
             reg_message(STRING_CANNOT_FIND);
             return 1;
