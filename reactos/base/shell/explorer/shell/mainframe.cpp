5e52a82ae06b9b518b10afd2c0013ff1052f2a14
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2014-06-13 23:36:29 +0000

    [EXPLORER]: Remove some painting hack from revision 36668 .
    svn path=/trunk/; revision=63596

--- reactos/base/shell/explorer/shell/mainframe.cpp
+++ reactos/base/shell/explorer/shell/mainframe.cpp
@@ -332,15 +332,8 @@ bool MainFrameBase::ProcessMessage(UINT nmsg, WPARAM wparam, LPARAM lparam, LRES
 		break;
 
 	  case WM_SIZE: {
-#ifdef __REACTOS__	///@todo Work around to display rebar in ROS (with flickering) as long as the control isn't fixed
-		int height = SendMessage(_hwndrebar, RB_GETBARHEIGHT, 0, 0);
-		MoveWindow(_hwndrebar, 0, 0, LOWORD(lparam), height, TRUE);
-#else
- 		resize_frame(LOWORD(lparam), HIWORD(lparam));
- 		SendMessage(_hwndrebar, WM_SIZE, 0, 0);
-#endif
-
-
+		resize_frame(LOWORD(lparam), HIWORD(lparam));
+		SendMessage(_hwndrebar, WM_SIZE, 0, 0);
 		break;}	// do not pass message to DefFrameProc
 
 	  case WM_GETMINMAXINFO: {
@@ -769,13 +762,12 @@ MDIMainFrame::MDIMainFrame(HWND hwnd)
 		extraBtns.iBitmap = 8;
 		SendMessage(_hextrabar, TB_INSERTBUTTON, INT_MAX, (LPARAM)&extraBtns);
 	}
-#ifndef __REACTOS__ // don't insert reg button for ROS. Regedit should be used.
+
 	 // insert Registry button
 	extraBtns.iString = SendMessage(_hextrabar, TB_ADDSTRING, 0, (LPARAM)TEXT("Reg.\0"));
 	extraBtns.idCommand = ID_DRIVE_REGISTRY;
 	extraBtns.iBitmap = 9;
 	SendMessage(_hextrabar, TB_INSERTBUTTON, INT_MAX, (LPARAM)&extraBtns);
-#endif
 
 #ifdef _DEBUG
 	 // insert FAT direct file system access button
