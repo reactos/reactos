92e130d1cc204e322fec41d325ed4c38d10e6572
Jérôme Gardou <jerome.gardou@reactos.org>
2014-07-08 22:44:44 +0000

    [SETUPAPI_APITEST]  - Try restoring UTF8 character which got lost in translation.
    svn path=/trunk/; revision=63701

--- rostests/apitests/setupapi/devclass.c
+++ rostests/apitests/setupapi/devclass.c
@@ -1,7 +1,7 @@
 /*
  * SetupAPI device class-related functions tests
  *
- * Copyright 2006 Herv� Poussineau
+ * Copyright 2006 Hervé Poussineau
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public Licence as
