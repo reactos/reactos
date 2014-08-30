57108b787236f01e0b82f58e286e3cb3d9818de2
Jérôme Gardou <jerome.gardou@reactos.org>
2014-08-30 21:33:21 +0000

    [SCHANNEL]  - Import TLS/SSL support on top of GNUTLS from wine Won't work until we ship the gnutls DLLs
    svn path=/trunk/; revision=63996

--- reactos/dll/win32/schannel/precomp.h
+++ reactos/dll/win32/schannel/precomp.h
@@ -10,11 +10,20 @@
 #define WIN32_NO_STATUS
 #include <windef.h>
 #include <winbase.h>
+#include <winnls.h>
+#include <winreg.h>
 #include <sspi.h>
 #include <ntsecapi.h>
 #include <ntsecpkg.h>
 
+#include <schannel.h>
+
+#include <wine/list.h>
+#include "schannel_priv.h"
+
 #include <wine/debug.h>
+#include <wine/unicode.h>
+
 WINE_DEFAULT_DEBUG_CHANNEL(schannel);
 
 #endif /* _SCHANNEL_PCH_ */
