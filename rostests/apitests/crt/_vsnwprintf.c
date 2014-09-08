5d48b37adfd63434dab93e1b6ba7fbebd4a297fb
Christoph von Wittich <christoph_vw@reactos.org>
2014-09-08 07:00:00 +0000

    [apitest/crt] apply the same buffer size fix to _vsnwprintf test
    svn path=/trunk/; revision=64082

--- rostests/apitests/crt/_vsnwprintf.c
+++ rostests/apitests/crt/_vsnwprintf.c
@@ -19,7 +19,7 @@ static void call_varargs(wchar_t* buf, size_t buf_size, int expected_ret, LPCWST
     int ret;
     /* Test the basic functionality */
     va_start(args, formatString);
-    ret = _vsnwprintf(buf, 255, formatString, args);
+    ret = _vsnwprintf(buf, buf_size, formatString, args);
     ok(expected_ret == ret, "Test failed: expected %i, got %i.\n", expected_ret, ret);
 }
 
