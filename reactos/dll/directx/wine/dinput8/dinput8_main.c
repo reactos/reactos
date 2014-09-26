98c5be0de95a7c4b390dca5677d7e5108a9ebb7f
Amine Khaldi <amine.khaldi@reactos.org>
2014-09-26 09:07:14 +0000

    [DINPUT8] * Sync with Wine 1.7.27. CORE-8540
    svn path=/trunk/; revision=64299

--- reactos/dll/directx/wine/dinput8/dinput8_main.c
+++ reactos/dll/directx/wine/dinput8/dinput8_main.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <config.h>
+#include <wine/config.h>
 //#include <assert.h>
 //#include <stdarg.h>
 //#include <string.h>
