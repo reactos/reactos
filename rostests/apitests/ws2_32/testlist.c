4a66664ebaba95b87e096d0c907dcae5a60766b1
Sylvain Petreolle <spetreolle@yahoo.fr>
2014-10-03 09:00:15 +0000

    [WS2_32_APITEST] Add WSAStartup-dependent tests.
    svn path=/trunk/; revision=64480

--- rostests/apitests/ws2_32/testlist.c
+++ rostests/apitests/ws2_32/testlist.c
@@ -7,14 +7,15 @@ extern void func_getaddrinfo(void);
 extern void func_ioctlsocket(void);
 extern void func_recv(void);
 extern void func_WSAStartup(void);
+extern void func_nostartup(void);
 
 const struct test winetest_testlist[] =
 {
     { "getaddrinfo", func_getaddrinfo },
     { "ioctlsocket", func_ioctlsocket },
+    { "nostartup", func_nostartup },
     { "recv", func_recv },
     { "WSAStartup", func_WSAStartup },
-
     { 0, 0 }
 };
 
