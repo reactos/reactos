50e22a432f07ab351e2038f5b4eb193fb03f3055
Timo Kreuzer <timo.kreuzer@reactos.org>
2014-10-19 13:17:49 +0000

    [USER32] Fix uninitialized variable use/warnings in - RegisterClassExWOWW (found by MSVC runtime checks) - IntDrawState (CID-514399) - IntCallWindowProcA (CID-514389) - IntCallWindowProcW (CID-414390) - SendMessageA (CID-514392) - SendMessageW (CID-514393) - SendMessageTimeoutA (CID-514441) - SendMessageCallbackA (CID-514442) - SendNotifyMessageW (CID-514443) - SendNotifyMessageA (CID-514444) - User32CallWindowProcFromKernel (CID-514445) - DesktopWndProcA (CID-716765)
    svn path=/trunk/; revision=64822

--- reactos/win32ss/user/user32/windows/draw.c
+++ reactos/win32ss/user/user32/windows/draw.c
@@ -1272,6 +1272,10 @@ IntDrawState(HDC hdc, HBRUSH hbr, DRAWSTATEPROC func, LPARAM lp, WPARAM wp,
 
             case DST_COMPLEX: /* cx and cy must be set in this mode */
                 return FALSE;
+
+            default:
+                ERR("Invalid opcode: %u\n", opcode);
+                return FALSE;
         }
 
         if(!cx)
@@ -1552,7 +1556,7 @@ FillRect(HDC hDC, CONST RECT *lprc, HBRUSH hbr)
         /* Handle system colors */
         if (hbr <= (HBRUSH)(COLOR_MENUBAR + 1))
             hbr = GetSysColorBrush(PtrToUlong(hbr) - 1);
-        
+
         prevhbr = SelectObject(hDC, hbr);
         if (prevhbr == NULL)
             return (INT)FALSE;
