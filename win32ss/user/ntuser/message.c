a5597dfcc189b827231ba794a6065a10512972ad
Giannis Adamopoulos <gadamopoulos@reactos.org>
2014-10-27 23:49:06 +0000

    [win32k] - Revert a recent change in trunk that broke the new shell. Its a shame to see the new shell behave like crap because of win32k
    svn path=/branches/shell-experiments/; revision=65057

--- win32ss/user/ntuser/message.c
+++ win32ss/user/ntuser/message.c
@@ -851,6 +851,17 @@ co_IntPeekMessage( PMSG Msg,
             return TRUE;
         }
 
+        if ((ProcessMask & QS_MOUSE) &&
+            co_MsqPeekMouseMove( pti,
+                                 RemoveMessages,
+                                 Window,
+                                 MsgFilterMin,
+                                 MsgFilterMax,
+                                 Msg ))
+        {
+            return TRUE;
+        }
+
         /* Check for hardware events. */
         if ((ProcessMask & QS_INPUT) &&
             co_MsqPeekHardwareMessage( pti,
@@ -860,17 +871,6 @@ co_IntPeekMessage( PMSG Msg,
                                        MsgFilterMax,
                                        ProcessMask,
                                        Msg))
-        {
-            return TRUE;
-        }
-
-        if ((ProcessMask & QS_MOUSE) &&
-            co_MsqPeekMouseMove( pti,
-                                 RemoveMessages,
-                                 Window,
-                                 MsgFilterMin,
-                                 MsgFilterMax,
-                                 Msg ))
         {
             return TRUE;
         }
