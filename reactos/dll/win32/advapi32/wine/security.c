45c3c5fa2b2fff96522b6fda090fdd11ec3b1272
Amine Khaldi <amine.khaldi@reactos.org>
2014-11-15 11:57:24 +0000

    [ADVAPI32] * Update CreateProcessWithTokenW(). CORE-8540
    svn path=/trunk/; revision=65405

--- reactos/dll/win32/advapi32/wine/security.c
+++ reactos/dll/win32/advapi32/wine/security.c
@@ -3328,20 +3328,18 @@ CreateProcessWithLogonW(LPCWSTR lpUsername,
     return FALSE;
 }
 
-BOOL
-WINAPI
-CreateProcessWithTokenW(IN HANDLE hToken,
-                        IN DWORD dwLogonFlags,
-                        IN LPCWSTR lpApplicationName OPTIONAL,
-                        IN OUT LPWSTR lpCommandLine OPTIONAL,
-                        IN DWORD dwCreationFlags,
-                        IN LPVOID lpEnvironment OPTIONAL,
-                        IN LPCWSTR lpCurrentDirectory OPTIONAL,
-                        IN LPSTARTUPINFOW lpStartupInfo,
-                        OUT LPPROCESS_INFORMATION lpProcessInfo)
+BOOL WINAPI CreateProcessWithTokenW(HANDLE token, DWORD logon_flags, LPCWSTR application_name, LPWSTR command_line,
+        DWORD creation_flags, void *environment, LPCWSTR current_directory, STARTUPINFOW *startup_info,
+        PROCESS_INFORMATION *process_information )
 {
-    UNIMPLEMENTED;
-    return FALSE;
+    FIXME("%p 0x%08x %s %s 0x%08x %p %s %p %p - semi-stub\n", token,
+          logon_flags, debugstr_w(application_name), debugstr_w(command_line),
+          creation_flags, environment, debugstr_w(current_directory),
+          startup_info, process_information);
+
+    /* FIXME: check if handles should be inherited */
+    return CreateProcessW( application_name, command_line, NULL, NULL, FALSE, creation_flags, environment,
+                           current_directory, startup_info, process_information );
 }
 
 /*
