861d2619718439d0c66a51c333a7802f1f5d3aa0
Amine Khaldi <amine.khaldi@reactos.org>
2014-11-25 16:17:58 +0000

    [EXPLORER][EXPLORER_NEW] * Revert the renames (r65476 => r65478). * We'll do these after we merge.
    svn path=/branches/shell-experiments/; revision=65483

---
+++ base/shell/explorer-new/explorer.cpp
@@ -0,0 +1,239 @@
+/*
+ * ReactOS Explorer
+ *
+ * Copyright 2006 - 2007 Thomas Weidenmueller <w3seek@reactos.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "precomp.h"
+#include <shlwapi_undoc.h>
+
+DWORD WINAPI _WinList_Init(void);
+void WINAPI _ShellDDEInit(BOOL bInit);
+
+HINSTANCE hExplorerInstance;
+HMODULE hUser32;
+HANDLE hProcessHeap;
+HKEY hkExplorer = NULL;
+DRAWCAPTEMP DrawCapTemp = NULL;
+
+class CExplorerModule : public CComModule
+{
+public:
+};
+
+BEGIN_OBJECT_MAP(ObjectMap)
+END_OBJECT_MAP()
+
+CExplorerModule                             gModule;
+CAtlWinModule                               gWinModule;
+
+void *operator new (size_t, void *buf)
+{
+    return buf;
+}
+
+static VOID InitializeAtlModule(HINSTANCE hInstance, BOOL bInitialize)
+{
+    if (bInitialize)
+    {
+        /* HACK - the global constructors don't run, so I placement new them here */
+        new (&gModule) CExplorerModule;
+        new (&gWinModule) CAtlWinModule;
+        new (&_AtlBaseModule) CAtlBaseModule;
+        new (&_AtlComModule) CAtlComModule;
+
+        gModule.Init(ObjectMap, hInstance, NULL);
+    }
+    else
+    {
+        gModule.Term();
+    }
+}
+
+#if !WIN7_COMPAT_MODE
+static BOOL
+SetShellReadyEvent(IN LPCTSTR lpEventName)
+{
+    HANDLE hEvent;
+
+    hEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, lpEventName);
+    if (hEvent != NULL)
+    {
+        SetEvent(hEvent);
+
+        CloseHandle(hEvent);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static VOID
+HideMinimizedWindows(IN BOOL bHide)
+{
+    MINIMIZEDMETRICS mm;
+
+    mm.cbSize = sizeof(mm);
+    if (!SystemParametersInfo(SPI_GETMINIMIZEDMETRICS, sizeof(mm), &mm, 0))
+    {
+        ERR("SystemParametersInfo failed with %lu\n", GetLastError());
+        return;
+    }
+    if (bHide)
+        mm.iArrange |= ARW_HIDE;
+    else
+        mm.iArrange &= ~ARW_HIDE;
+    if (!SystemParametersInfo(SPI_SETMINIMIZEDMETRICS, sizeof(mm), &mm, 0))
+        ERR("SystemParametersInfo failed with %lu\n", GetLastError());
+}
+
+static INT 
+StartWithCommandLine(IN HINSTANCE hInstance)
+{
+    BOOL b = FALSE;
+    EXPLORER_CMDLINE_PARSE_RESULTS parseResults = { 0 };
+
+    if (SHExplorerParseCmdLine(&parseResults))
+        b = SHCreateFromDesktop(&parseResults);
+
+    if (parseResults.strPath)
+        SHFree(parseResults.strPath);
+
+    if (parseResults.pidlPath)
+        ILFree(parseResults.pidlPath);
+
+    if (parseResults.pidlRoot)
+        ILFree(parseResults.pidlRoot);
+
+    return b;
+}
+#endif
+
+static INT 
+StartWithDesktop(IN HINSTANCE hInstance)
+{
+    InitializeAtlModule(hInstance, TRUE);
+
+    if (RegOpenKey(HKEY_CURRENT_USER,
+        L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer",
+        &hkExplorer) != ERROR_SUCCESS)
+    {
+        WCHAR Message[256];
+        LoadString(hInstance, IDS_STARTUP_ERROR, Message, 256);
+        MessageBox(NULL, Message, NULL, MB_ICONERROR);
+        return 1;
+    }
+
+    hExplorerInstance = hInstance;
+    hProcessHeap = GetProcessHeap();
+    LoadAdvancedSettings();
+
+    hUser32 = GetModuleHandle(TEXT("USER32.DLL"));
+    if (hUser32 != NULL)
+    {
+        DrawCapTemp = (DRAWCAPTEMP) GetProcAddress(hUser32, PROC_NAME_DRAWCAPTIONTEMP);
+    }
+
+    InitCommonControls();
+    OleInitialize(NULL);
+
+#if !WIN7_COMPAT_MODE
+    ProcessStartupItems();
+
+    /* Initialize shell dde support */
+    _ShellDDEInit(TRUE);
+#endif
+
+    /* Initialize shell icons */
+    FileIconInit(TRUE);
+
+    /* Initialize CLSID_ShellWindows class */
+    _WinList_Init();
+
+    CComPtr<ITrayWindow> Tray;
+    CreateTrayWindow(&Tray);
+
+#if !WIN7_COMPAT_MODE
+    /* This not only hides the minimized window captions in the bottom
+    left screen corner, but is also needed in order to receive
+    HSHELL_* notification messages (which are required for taskbar
+    buttons to work right) */
+    HideMinimizedWindows(TRUE);
+
+    HANDLE hShellDesktop = NULL;
+    if (Tray != NULL)
+        hShellDesktop = DesktopCreateWindow(Tray);
+
+    /* WinXP: Notify msgina to hide the welcome screen */
+    if (!SetShellReadyEvent(TEXT("msgina: ShellReadyEvent")))
+        SetShellReadyEvent(TEXT("Global\\msgina: ShellReadyEvent"));
+#endif
+
+    if (Tray != NULL)
+    {
+#if !WIN7_COMPAT_MODE
+        RegisterHotKey(NULL, IDHK_RUN, MOD_WIN, 'R');
+#endif
+        TrayMessageLoop(Tray);
+#if !WIN7_COMPAT_MODE
+        HideMinimizedWindows(FALSE);
+#endif
+    }
+
+#if !WIN7_COMPAT_MODE
+    if (hShellDesktop != NULL)
+        DesktopDestroyShellWindow(hShellDesktop);
+#endif
+
+    OleUninitialize();
+
+    RegCloseKey(hkExplorer);
+    hkExplorer = NULL;
+
+    InitializeAtlModule(hInstance, FALSE);
+
+    return 0;
+}
+
+INT WINAPI
+_tWinMain(IN HINSTANCE hInstance,
+          IN HINSTANCE hPrevInstance,
+          IN LPTSTR lpCmdLine,
+          IN INT nCmdShow)
+{
+#if !WIN7_COMPAT_MODE
+    BOOL CreateShellDesktop = FALSE;
+
+    TRACE("Explorer starting... Commandline: %S\n", lpCmdLine);
+
+    /*
+    * Set our shutdown parameters: we want to shutdown the very last,
+    * but before any TaskMgr instance (which has a shutdown level of 1).
+    */
+    SetProcessShutdownParameters(2, 0);
+
+    if (GetShellWindow() == NULL)
+        CreateShellDesktop = TRUE;
+
+    if (!CreateShellDesktop)
+    {
+        return StartWithCommandLine(hInstance);
+    }
+#endif
+
+    return StartWithDesktop(hInstance);
+}
---
+++ base/shell/explorer-new/resource.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#define IDC_STATIC -1
+
+#define IDI_START 101
+
+#define IDS_START             101
+#define IDS_PROPERTIES        102
+#define IDS_OPEN_ALL_USERS    103
+#define IDS_EXPLORE_ALL_USERS 104
+#define IDS_STARTUP_ERROR     105
+
+#define IDC_STARTBTN 304
+
+#define IDM_TRAYWND   101
+#define IDM_STARTMENU 102
+
+#define IDB_STARTMENU 101
+
+#define ID_SHELL_CMD_FIRST              0xF
+#define ID_SHELL_CMD_LAST               0x7FEF
+#define ID_SHELL_CMD_PROPERTIES         (ID_SHELL_CMD_LAST + 1)
+#define ID_SHELL_CMD_OPEN_ALL_USERS     (ID_SHELL_CMD_LAST + 2)
+#define ID_SHELL_CMD_EXPLORE_ALL_USERS  (ID_SHELL_CMD_LAST + 3)
+
+#define ID_LOCKTASKBAR (ID_SHELL_CMD_LAST + 4)
+
+#define ID_SHELL_CMD_OPEN_TASKMGR (ID_SHELL_CMD_LAST + 5)
+#define ID_SHELL_CMD_UNDO_ACTION  (ID_SHELL_CMD_LAST + 6)
+#define ID_SHELL_CMD_SHOW_DESKTOP (ID_SHELL_CMD_LAST + 7)
+#define ID_SHELL_CMD_TILE_WND_V   (ID_SHELL_CMD_LAST + 8)
+#define ID_SHELL_CMD_TILE_WND_H   (ID_SHELL_CMD_LAST + 9)
+#define ID_SHELL_CMD_CASCADE_WND  (ID_SHELL_CMD_LAST + 10)
+#define ID_SHELL_CMD_CUST_NOTIF   (ID_SHELL_CMD_LAST + 11)
+#define ID_SHELL_CMD_ADJUST_DAT   (ID_SHELL_CMD_LAST + 12)
+
+/* NOTE: The following constants may *NOT* be changed because
+         they're hardcoded and need to be the exact values
+         in order to get the start menu to work! */
+#define IDM_PROGRAMS                504
+#define IDM_FAVORITES               507
+#define IDM_DOCUMENTS               501
+#define IDM_SETTINGS                508
+#define IDM_CONTROLPANEL            505
+#define IDM_SECURITY                5001
+#define IDM_NETWORKCONNECTIONS      557
+#define IDM_PRINTERSANDFAXES        510
+#define IDM_TASKBARANDSTARTMENU     413
+#define IDM_SEARCH                  520
+#define IDM_HELPANDSUPPORT          503
+#define IDM_RUN                     401
+#define IDM_SYNCHRONIZE             553
+#define IDM_LOGOFF                  402
+#define IDM_DISCONNECT              5000
+#define IDM_UNDOCKCOMPUTER          410
+#define IDM_SHUTDOWN                506
+#define IDM_LASTSTARTMENU_SEPARATOR 450
+
+/* Taskbar resources */
+#define IDD_TASKBARPAGE      2000
+#define IDC_TASKBAR_BITMAP   2001
+#define IDD_STARTMENUPAGE    2001
+#define IDD_NOTIFICATIONPAGE 2002
+#define IDD_TOOLBARSPAGE     2003
+
+/* Taskbar properties, taskbar */
+#define IDD_TASKBARPROP_TASKBAR       2000
+#define IDC_TASKBARPROP_TASKBARBITMAP 2001
+#define IDC_TASKBARPROP_LOCK          2002
+#define IDC_TASKBARPROP_HIDE          2003
+#define IDC_TASKBARPROP_ONTOP         2004
+#define IDC_TASKBARPROP_GROUP         2005
+#define IDC_TASKBARPROP_SHOWQL        2006
+#define IDC_TASKBARPROP_WNDPREV       2007
+
+#define IDB_TASKBARPROP_AUTOHIDE            2050
+#define IDB_TASKBARPROP_LOCK_GROUP_NOQL     2051
+#define IDB_TASKBARPROP_LOCK_GROUP_QL       2052
+#define IDB_TASKBARPROP_LOCK_NOGROUP_NOQL   2053
+#define IDB_TASKBARPROP_LOCK_NOGROUP_QL     2054
+#define IDB_TASKBARPROP_NOLOCK_GROUP_NOQL   2055
+#define IDB_TASKBARPROP_NOLOCK_NOGROUP_NOQL 2056
+#define IDB_TASKBARPROP_NOLOCK_NOGROUP_QL   2057
+#define IDB_TASKBARPROP_NOLOCK_GROUP_QL     2058
+#define IDC_TASKBARPROP_SECONDS             2059
+
+/* Taskbar properties, startmenu*/
+#define IDD_TASKBARPROP_STARTMENU            2100
+#define IDC_TASKBARPROP_STARTMENU            2101
+#define IDC_TASKBARPROP_STARTMENUCUST        2102
+#define IDC_TASKBARPROP_STARTMENUCLASSIC     2103
+#define IDC_TASKBARPROP_STARTMENUCLASSICCUST 2104
+#define IDC_TASKBARPROP_RECENTFILES          2105
+#define IDC_TASKBARPROP_RECENTFOLDERS        2106
+
+/* Taskbar properties, notification */
+#define IDD_TASKBARPROP_NOTIFICATION       2200
+#define IDC_TASKBARPROP_NOTIFICATIONBITMAP 2201
+#define IDC_TASKBARPROP_HIDEICONS          2202
+#define IDC_TASKBARPROP_ICONCUST           2203
+#define IDC_TASKBARPROP_CLOCK              2204
+#define IDC_TASKBARPROP_VOLUME             2205
+#define IDC_TASKBARPROP_NETWORK            2206
+#define IDC_TASKBARPROP_POWER              2207
+#define IDD_TASKBARPROP_ADVANCED           2208
+
+/* Taskbar properties, toolbars */
+#define IDD_TASKBARPROP_TOOLBARS     2300
+#define IDC_TASKBARPROP_DESKBANDLIST 2301
+
+#define IDS_TASKBAR_STARTMENU_PROP_CAPTION 2200
