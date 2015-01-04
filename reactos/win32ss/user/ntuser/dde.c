f06c2230de5d05cd48d28576435362843d5b7b6c
James Tabor <james.tabor@reactos.org>
2015-01-04 18:12:41 +0000

    [Win32k] - Move DDE stuff into its own file.
    svn path=/trunk/; revision=65975

---
+++ reactos/win32ss/user/ntuser/dde.c
@@ -0,0 +1,54 @@
+
+#include <win32k.h>
+//DBG_DEFAULT_CHANNEL(UserMisc);
+
+
+BOOL
+APIENTRY
+NtUserDdeGetQualityOfService(
+   IN HWND hwndClient,
+   IN HWND hWndServer,
+   OUT PSECURITY_QUALITY_OF_SERVICE pqosPrev)
+{
+   STUB
+
+   return 0;
+}
+
+DWORD
+APIENTRY
+NtUserDdeInitialize(
+   DWORD Unknown0,
+   DWORD Unknown1,
+   DWORD Unknown2,
+   DWORD Unknown3,
+   DWORD Unknown4)
+{
+   STUB
+
+   return 0;
+}
+
+BOOL
+APIENTRY
+NtUserDdeSetQualityOfService(
+   IN  HWND hwndClient,
+   IN  PSECURITY_QUALITY_OF_SERVICE pqosNew,
+   OUT PSECURITY_QUALITY_OF_SERVICE pqosPrev)
+{
+   STUB
+
+   return 0;
+}
+
+BOOL
+APIENTRY
+NtUserImpersonateDdeClientWindow(
+   HWND hWndClient,
+   HWND hWndServer)
+{
+   STUB
+
+   return 0;
+}
+
