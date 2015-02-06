94c27d3aeb1c2fa2d52845a2ed3e40ac9e947a1e
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2015-02-06 23:29:09 +0000

    [WINSRV]: Remove a long standing hack (added 4 years ago) that is now useless. CORE-5138 #comment Hack from r51460 finally removed.
    svn path=/trunk/; revision=66185

--- reactos/win32ss/user/winsrv/init.c
+++ reactos/win32ss/user/winsrv/init.c
@@ -9,33 +9,12 @@
 
 /* INCLUDES *******************************************************************/
 
-/* PSDK Headers */
-#include <stdarg.h>
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-#include <windef.h>
-#include <winuser.h>
+#include "winsrv.h"
 
 #define NDEBUG
 #include <debug.h>
 
-
 /* ENTRY-POINT ****************************************************************/
-
-/*** HACK from win32csr... ***/
-static HHOOK hhk = NULL;
-
-LRESULT
-CALLBACK
-KeyboardHookProc(int nCode,
-                 WPARAM wParam,
-                 LPARAM lParam)
-{
-    DPRINT1("KeyboardHookProc Processing!\n");
-    return CallNextHookEx(hhk, nCode, wParam, lParam);
-}
-/*** END - HACK from win32csr... ***/
 
 BOOL
 WINAPI
@@ -46,21 +25,6 @@ DllMain(IN HINSTANCE hInstanceDll,
     UNREFERENCED_PARAMETER(hInstanceDll);
     UNREFERENCED_PARAMETER(dwReason);
     UNREFERENCED_PARAMETER(lpReserved);
-
-    if (DLL_PROCESS_ATTACH == dwReason)
-    {
-        DPRINT1("WINSRV - HACK: Use keyboard hook hack\n");
-/*** HACK from win32csr... ***/
-//
-// HACK HACK HACK ReactOS to BOOT! Initialization BUG ALERT! See bug 5655.
-//
-        hhk = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, NULL, 0);
-// BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT!
-//  BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT!
-//   BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT! BUG ALERT!
-
-/*** END - HACK from win32csr... ***/
-    }
 
     return TRUE;
 }
