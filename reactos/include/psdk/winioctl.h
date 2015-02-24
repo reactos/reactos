3d2fd7db25be50f292bc32c36fc4b98dfcc10eaa
Christoph von Wittich <christoph_vw@reactos.org>
2015-02-24 11:04:02 +0000

    [PSDK] add missing members to DISK_PERFORMANCE struct
    svn path=/trunk/; revision=66439

--- reactos/include/psdk/winioctl.h
+++ reactos/include/psdk/winioctl.h
@@ -367,9 +367,14 @@ typedef struct _DISK_PERFORMANCE {
     LARGE_INTEGER BytesWritten;
     LARGE_INTEGER ReadTime;
     LARGE_INTEGER WriteTime;
+    LARGE_INTEGER IdleTime;
     DWORD ReadCount;
     DWORD WriteCount;
     DWORD QueueDepth;
+    DWORD SplitCount;
+    LARGE_INTEGER QueryTime;
+    DWORD   StorageDeviceNumber;
+    WCHAR   StorageManagerName[8];
 } DISK_PERFORMANCE, *PDISK_PERFORMANCE;
 typedef struct _DISK_RECORD {
   LARGE_INTEGER ByteOffset;
