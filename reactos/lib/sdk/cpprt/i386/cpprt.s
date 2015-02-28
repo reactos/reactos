5dfd2fc11e823dc0b34bf1dbb7d2b91b3ff2f34d
Thomas Faber <thomas.faber@reactos.org>
2015-02-28 21:03:04 +0000

    [CPPRT] - Implement __CxxFrameHandler3 as a wrapper around __CxxFrameHandler that translates VC8-style function exception descriptions to VC7-style. This is necessary to make C++ exceptions work on Windows Server 2003, because msvcrt!__CxxFrameHandler does not support VC8 descriptors there. CORE-9290 #resolve
    svn path=/trunk/; revision=66503

--- reactos/lib/sdk/cpprt/i386/cpprt.s
+++ reactos/lib/sdk/cpprt/i386/cpprt.s
@@ -7,13 +7,27 @@ EXTERN &orig:&type
 ALIAS <&alias> = <&orig>
 ENDM
 
+EXTERN _CxxHandleV8Frame@20 : PROC
+PUBLIC ___CxxFrameHandler3
+___CxxFrameHandler3:
+    push eax
+    push dword ptr [esp + 20]
+    push dword ptr [esp + 20]
+    push dword ptr [esp + 20]
+    push dword ptr [esp + 20]
+    call _CxxHandleV8Frame@20
+    ret
+
+EXTERN ___CxxFrameHandler : PROC
+PUBLIC _CallCxxFrameHandler
+_CallCxxFrameHandler:
+    mov eax, dword ptr [esp + 20]
+    jmp ___CxxFrameHandler
+
 ; void __stdcall `eh vector constructor iterator'(void *,unsigned int,int,void (__thiscall*)(void *),void (__thiscall*)(void *))
 DEFINE_ALIAS ??_L@YGXPAXIHP6EX0@Z1@Z, ?MSVCRTEX_eh_vector_constructor_iterator@@YGXPAXIHP6EX0@Z1@Z
 
 ; void __stdcall `eh vector destructor iterator'(void *,unsigned int,int,void (__thiscall*)(void *))
 DEFINE_ALIAS ??_M@YGXPAXIHP6EX0@Z@Z, ?MSVCRTEX_eh_vector_destructor_iterator@@YGXPAXIHP6EX0@Z@Z
-
-; These are the same
-DEFINE_ALIAS ___CxxFrameHandler3, ___CxxFrameHandler
 
 END
