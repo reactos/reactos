34babdb8d6a6bea81916473a764aa41f0ff2a2c0
Amine Khaldi <amine.khaldi@reactos.org>
2015-03-19 19:45:48 +0000

    [CLT2015] Grab r66812, r66814 and co (Rapps improvements).
    svn path=/branches/ReactOS-0.3.17-CLT2015/; revision=66817

--- reactos/base/applications/rapps/rapps.h
+++ reactos/base/applications/rapps/rapps.h
@@ -20,6 +20,7 @@
 #include <shlwapi.h>
 #include <stdio.h>
 #include <strsafe.h>
+#include <ndk/rtlfuncs.h>
 
 #include <rappsmsg.h>
 
@@ -80,6 +81,14 @@ typedef struct
     WCHAR szUrlSite[MAX_PATH];
     WCHAR szUrlDownload[MAX_PATH];
     WCHAR szCDPath[MAX_PATH];
+
+    /* caching mechanism related entries */
+    WCHAR cFileName[MAX_PATH];
+    FILETIME ftCacheStamp;
+    LIST_ENTRY List;
+
+    /* optional integrity checks */
+    BYTE MD5Checksum[16];
 
 } APPLICATION_INFO, *PAPPLICATION_INFO;
 
@@ -166,9 +175,8 @@ VOID InitLogs(VOID);
 VOID FreeLogs(VOID);
 BOOL WriteLogMessage(WORD wType, DWORD dwEventID, LPWSTR lpMsg);
 
-/* parser.c */
-INT ParserGetString(LPCWSTR section, LPCWSTR entry, LPWSTR buffer, UINT len, LPCWSTR filename);
-UINT ParserGetInt(LPCWSTR section, LPCWSTR entry, LPCWSTR filename);
+UINT ParserGetString(LPCWSTR lpKeyName, LPWSTR lpReturnedString, UINT nSize, LPCWSTR lpFileName);
+UINT ParserGetInt(LPCWSTR lpKeyName, LPCWSTR lpFileName);
 
 /* richedit.c */
 extern HWND hRichEdit;
