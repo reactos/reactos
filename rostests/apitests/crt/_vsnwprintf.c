7d5157727619a01bd118383ba0111328129f4753
Timo Kreuzer <timo.kreuzer@reactos.org>
2015-04-04 13:42:41 +0000

    [CRT_APITEST] Disable a broken test.
    svn path=/trunk/; revision=67044

--- rostests/apitests/crt/_vsnwprintf.c
+++ rostests/apitests/crt/_vsnwprintf.c
@@ -28,34 +28,34 @@ START_TEST(_vsnwprintf)
     wchar_t buffer[255];
 
     /* Test basic functionality */
-    call_varargs(buffer, 255, 10, L"%s world!", "hello");
+    //call_varargs(buffer, 255, 10, L"%s world!", "hello"); // this test is broken
     call_varargs(buffer, 255, 12, L"%s world!", L"hello");
     call_varargs(buffer, 255, 11, L"%u cookies", 100);
     /* This is how WINE implements _vcsprintf, and they are obviously wrong */
     StartSeh()
 #if defined(TEST_CRTDLL)
         call_varargs(NULL, INT_MAX, -1, L"%s it really work?", L"does");
-#else 
+#else
         call_varargs(NULL, INT_MAX, 20, L"%s it really work?", L"does");
 #endif
 
 #if defined(TEST_USER32)
     EndSeh(STATUS_ACCESS_VIOLATION);
-#else 
+#else
     EndSeh(STATUS_SUCCESS);
 #endif
 
 #if defined(TEST_USER32)/* NTDLL doesn't use/set errno */
     ok(errno == EINVAL, "Expected EINVAL, got %u\n", errno);
-#else 
+#else
     ok(errno == 0, "Expected 0, got %u\n", errno);
 #endif
-    
+
     /* This one is no better */
     StartSeh()
 #if defined(TEST_CRTDLL)
         call_varargs(NULL, 0, -1, L"%s it really work?", L"does");
-#else 
+#else
         call_varargs(NULL, 0, 20, L"%s it really work?", L"does");
 #endif
     EndSeh(STATUS_SUCCESS);
