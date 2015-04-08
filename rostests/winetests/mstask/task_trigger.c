a7321a085f031c3427e1013199dccab7b3f3d16f
Amine Khaldi <amine.khaldi@reactos.org>
2015-04-08 17:14:16 +0000

    [MSTASK_WINETEST] Sync with Wine Staging 1.7.37. CORE-9246
    svn path=/trunk/; revision=67091

--- rostests/winetests/mstask/task_trigger.c
+++ rostests/winetests/mstask/task_trigger.c
@@ -282,7 +282,6 @@ static void test_SetTrigger_GetTrigger(void)
     normal_trigger_state.wStartHour = 24;
     hres = ITaskTrigger_SetTrigger(test_trigger, &normal_trigger_state);
     ok(hres == E_INVALIDARG, "Expected E_INVALIDARG: 0x%08x\n", hres);
-    normal_trigger_state.wStartHour = 3;
     normal_trigger_state.wStartHour = 60;
     hres = ITaskTrigger_SetTrigger(test_trigger, &normal_trigger_state);
     ok(hres == E_INVALIDARG, "Expected E_INVALIDARG: 0x%08x\n", hres);
