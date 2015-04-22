3c53bca528fb0f33db3c609418242e7edf99c62d
Amine Khaldi <amine.khaldi@reactos.org>
2015-04-22 12:44:36 +0000

    [URLMON_WINETEST] Skip test_http_protocol() and test_https_protocol() because of timeouts. ROSTESTS-165
    svn path=/trunk/; revision=67348

--- rostests/winetests/urlmon/protocol.c
+++ rostests/winetests/urlmon/protocol.c
@@ -3772,11 +3772,22 @@ START_TEST(protocol)
     thread_id = GetCurrentThreadId();
 
     test_file_protocol();
-    test_http_protocol();
+
+    if (!winetest_interactive)
+        skip("ROSTESTS-165: Skipping test_http_protocol() because of timeouts.\n");
+    else
+        test_http_protocol();
+
     if(pCreateUri)
-        test_https_protocol();
+    {
+        if (!winetest_interactive)
+            skip("ROSTESTS-165: Skipping test_https_protocol() because of timeouts.\n");
+        else
+            test_https_protocol();
+    }
     else
         win_skip("Skipping https tests on too old platform\n");
+
     test_ftp_protocol();
     test_gopher_protocol();
     test_mk_protocol();
