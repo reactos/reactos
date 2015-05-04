d5fd7aaa19c0b56c193242e54cfd22091c09c4c8
Colin Finck <colin@reactos.org>
2015-05-04 13:21:13 +0000

    - Add my proposed directory tree for the components involved in printing. - Remove some Wine-imported components which are either entirely not usable for us or only implement so few functions that it's easier to completely reimplement them. - Move "localui" (Wine-imported) and "spoolsv" (ReactOS stub) to the new directories.
    svn path=/branches/colins-printing-for-freedom/; revision=67543

--- reactos/dll/win32/localspl/resource.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/* ## Resource-ID ## */
-#define IDS_LOCALPORT       500
-#define IDS_LOCALMONITOR    507
-
-/* ## Reserved memorysize for the strings (in WCHAR) ## */
-#define IDS_LOCALMONITOR_MAXLEN 64
-#define IDS_LOCALPORT_MAXLEN 32
