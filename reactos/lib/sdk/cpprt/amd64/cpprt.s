a990c60f4d91c6aac8addda02ad3b5573cf6ff38
Daniel Reimer <reimer.daniel@freenet.de>
2015-05-12 19:16:03 +0000

    [C++] Add amd64 version of r64555. Suggested and reviewed by Thomas Faber
    svn path=/trunk/; revision=67700

--- reactos/lib/sdk/cpprt/amd64/cpprt.s
+++ reactos/lib/sdk/cpprt/amd64/cpprt.s
@@ -7,13 +7,19 @@ EXTERN &orig:&type
 ALIAS <&alias> = <&orig>
 ENDM
 
-; void __cdecl `eh vector constructor iterator'(void *,unsigned __int64,int,void (__cdecl*)(void *),void (__cdecl*)(void *))
+; void __cdecl `eh vector constructor iterator'(void * __ptr64,unsigned __int64,int,void (__cdecl*)(void * __ptr64),void (__cdecl*)(void * __ptr64))
 DEFINE_ALIAS ??_L@YAXPEAX_KHP6AX0@Z2@Z, ?MSVCRTEX_eh_vector_constructor_iterator@@YAXPEAX_KHP6AX0@Z2@Z
 
-; void __cdecl `eh vector destructor iterator'(void *,unsigned __int64,int,void (__cdecl*)(void *))
+; void __cdecl `eh vector destructor iterator'(void * __ptr64,unsigned __int64,int,void (__cdecl*)(void * __ptr64))
 DEFINE_ALIAS ??_M@YAXPEAX_KHP6AX0@Z@Z, ?MSVCRTEX_eh_vector_destructor_iterator@@YAXPEAX_KHP6AX0@Z@Z
 
 ; These are the same
 DEFINE_ALIAS __CxxFrameHandler3, __CxxFrameHandler
+
+; void __cdecl operator delete(void * __ptr64,struct std::nothrow_t const & __ptr64)
+DEFINE_ALIAS ??3@YAXPEAXAEBUnothrow_t@std@@@Z, ??3@YAXPEAX@Z
+
+; void __cdecl operator delete[](void * __ptr64,struct std::nothrow_t const & __ptr64)
+DEFINE_ALIAS ??_V@YAXPEAXAEBUnothrow_t@std@@@Z, ??3@YAXPEAX@Z
 
 END
