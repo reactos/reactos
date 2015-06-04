d0d90ef6dab5f81c3fc4eaf7129e14a415f4b402
Daniel Reimer <reimer.daniel@freenet.de>
2015-06-04 14:23:08 +0000

    [RAPPS] update cab maker scripts. POSIX toy is untested!
    svn path=/trunk/; revision=68013

--- reactos/base/applications/rapps/CreateCabFile.bat
+++ reactos/base/applications/rapps/CreateCabFile.bat
@@ -1,10 +1,12 @@
 @echo off
 
+cd ..\..\..\media
+
 mkdir rapps\utf16
 
 for %%f in (rapps\*.txt) do (
-     ..\..\..\output-MinGW-i386\host-tools\tools\utf16le.exe "rapps\%%~nf.txt" "rapps\utf16\%%~nf.txt"
+     ..\output-MinGW-i386\host-tools\tools\utf16le.exe "rapps\%%~nf.txt" "rapps\utf16\%%~nf.txt"
 )
 
-..\..\..\output-MinGW-i386\host-tools\tools\cabman\cabman.exe -M mszip -S rappmgr.cab rapps\utf16\*.txt
+..\output-MinGW-i386\host-tools\tools\cabman\cabman.exe -M mszip -S rappmgr.cab rapps\utf16\*.txt
 rmdir /s /q rapps\utf16
--- reactos/base/applications/rapps/CreateCabFile.sh
+++ reactos/base/applications/rapps/CreateCabFile.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-cd rapps
+cd ../../../media/rapps
 mkdir utf16
 for i in $(find -type f); do
-  ../../../host-tools/tools/utf16le $i utf16/$i
+  ../../host-tools/tools/utf16le $i utf16/$i
 done
 cd ..
 ../../host-tools/tools/cabman/cabman -M mszip -S rappmgr.cab rapps/utf16/*.txt
