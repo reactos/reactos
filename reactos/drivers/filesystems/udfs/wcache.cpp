c5c93fc98ba082b1fa9dc4e3088cd073d69aaf14
Amine Khaldi <amine.khaldi@reactos.org>
2015-06-08 14:24:47 +0000

    [UDFS] Explicitly mention the license (GPLv2).
    svn path=/trunk/; revision=68075

--- reactos/drivers/filesystems/udfs/wcache.cpp
+++ reactos/drivers/filesystems/udfs/wcache.cpp
@@ -1,6 +1,7 @@
 ////////////////////////////////////////////////////////////////////
 // Copyright (C) Alexander Telyatnikov, Ivan Keliukh, Yegor Anchishkin, SKIF Software, 1999-2013. Kiev, Ukraine
 // All rights reserved
+// This file was released under the GPLv2 on June 2015.
 ////////////////////////////////////////////////////////////////////
 #include "udffs.h"
 
