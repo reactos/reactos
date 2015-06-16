5e1ecf69f11a7d454c56efef8cc27db1a21fd410
Ged Murphy <gedmurphy@reactos.org>
2015-06-16 12:22:14 +0000

    [DEVMGMT] The new device manager will be located in devmgr.dll
    svn path=/trunk/; revision=68163

--- reactos/base/applications/mscutils/devmgmt_new/devmgmt.cpp
+++ reactos/base/applications/mscutils/devmgmt_new/devmgmt.cpp
@@ -1,9 +1,7 @@
-#include "stdafx.h"
-#include "devmgmt.h"
-#include "MainWindow.h"
-
-HINSTANCE g_hInstance = NULL;
-HANDLE ProcessHeap = NULL;
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cfgmgr32.h>
+#include <dll\devmgr\devmgr.h>
 
 int WINAPI
 wWinMain(HINSTANCE hThisInstance,
@@ -11,47 +9,13 @@ wWinMain(HINSTANCE hThisInstance,
          LPWSTR lpCmdLine,
          int nCmdShow)
 {
-    CMainWindow MainWindow;
-    INITCOMMONCONTROLSEX icex;
-    HANDLE hMutex;
-    CAtlString szAppName;
-
-    int Ret = 1;
-
-    /* Check if the app is already running */
-    hMutex = CreateMutexW(NULL, TRUE, L"devmgmt_mutex2");
-    if (hMutex == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+    if (!DeviceManager_ExecuteW(NULL,
+                                hThisInstance,
+                                NULL,
+                                nCmdShow))
     {
-        /* Cleanup and exit */
-        //if (hMutex) CloseHandle(hMutex);
-        //return 0;
+        return GetLastError();
     }
 
-    /* Store the global values */
-    g_hInstance = hThisInstance;
-    ProcessHeap = GetProcessHeap();
-
-    /* Initialize common controls */
-    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
-    icex.dwICC = ICC_BAR_CLASSES | ICC_COOL_CLASSES;
-    InitCommonControlsEx(&icex);
-
-    /* Load the application name */
-    if (szAppName.LoadStringW(g_hInstance, IDS_APPNAME))
-    {
-        /* Initialize the main window */
-        if (MainWindow.Initialize(szAppName, nCmdShow))
-        {
-            /* Run the application */
-            Ret = MainWindow.Run();
-
-            /* Uninitialize the main window */
-            MainWindow.Uninitialize();
-        }
-    }
-
-    /* Delete the app mutex */
-    CloseHandle(hMutex);
-
-    return Ret;
+    return 0;
 }
--- reactos/base/applications/mscutils/devmgmt_new/devmgmt.h
+++ reactos/base/applications/mscutils/devmgmt_new/devmgmt.h
@@ -1,9 +1,1 @@
 #pragma once
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#pragma once
-#include "resource.h"
-
-extern HINSTANCE g_hInstance;
-extern HANDLE ProcessHeap;
-
