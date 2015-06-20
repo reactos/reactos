9046d69084df42239013bc6689bd38d742fc1a3f
Thomas Faber <thomas.faber@reactos.org>
2015-06-20 09:07:29 +0000

    [SHELL32_APITEST] - Add a test showing that repeated calls to SHGetDesktopFolder return pointers to the same object, and that its CreateViewObject method instead creates new objects. CORE-9839
    svn path=/trunk/; revision=68200

--- rostests/apitests/shell32/testlist.c
+++ rostests/apitests/shell32/testlist.c
@@ -3,11 +3,12 @@
 #define STANDALONE
 #include <wine/test.h>
 
+extern void func_CShellDesktop(void);
 extern void func_menu(void);
 
 const struct test winetest_testlist[] =
 {
+    { "CShellDesktop", func_CShellDesktop },
     { "menu", func_menu },
-    
     { 0, 0 }
 };
