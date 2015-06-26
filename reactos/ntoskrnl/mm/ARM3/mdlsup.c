09da0e30113c4ef52f4b83cba34ec5829471a59f
Thomas Faber <thomas.faber@reactos.org>
2015-06-26 10:40:49 +0000

    [NTOS:MM] - Mark the page as being on the standby list before dereferencing it in MmFreePagesFromMdl. Since the PFN is marked as deleted, MiDecrementReferenceCount will correctly insert it into the free list, but this is required to satisfy MiDecrementReferenceCount's sanity check. CORE-9877 #resolve
    svn path=/trunk/; revision=68273

--- reactos/ntoskrnl/mm/ARM3/mdlsup.c
+++ reactos/ntoskrnl/mm/ARM3/mdlsup.c
@@ -270,6 +270,7 @@ MmFreePagesFromMdl(IN PMDL Mdl)
         //
         Pfn1->u3.e1.StartOfAllocation = 0;
         Pfn1->u3.e1.EndOfAllocation = 0;
+        Pfn1->u3.e1.PageLocation = StandbyPageList;
         Pfn1->u2.ShareCount = 0;
 
         //
