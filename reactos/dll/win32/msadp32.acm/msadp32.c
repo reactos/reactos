47a6c45fd65bbd14c63c1702611ce4d8b1fb50c9
Amine Khaldi <amine.khaldi@reactos.org>
2015-07-19 22:49:34 +0000

    [MSADP32.ACM] Sync with Wine Staging 1.7.47. CORE-9924
    svn path=/trunk/; revision=68455

--- reactos/dll/win32/msadp32.acm/msadp32.c
+++ reactos/dll/win32/msadp32.acm/msadp32.c
@@ -374,8 +374,8 @@ static	LRESULT ADPCM_DriverDetails(PACMDRIVERDETAILSW add)
 {
     add->fccType = ACMDRIVERDETAILS_FCCTYPE_AUDIOCODEC;
     add->fccComp = ACMDRIVERDETAILS_FCCCOMP_UNDEFINED;
-    add->wMid = 0xFF;
-    add->wPid = 0x00;
+    add->wMid = MM_MICROSOFT;
+    add->wPid = MM_MSFT_ACM_MSADPCM;
     add->vdwACM = 0x01000000;
     add->vdwDriver = 0x01000000;
     add->fdwSupport = ACMDRIVERDETAILS_SUPPORTF_CODEC;
