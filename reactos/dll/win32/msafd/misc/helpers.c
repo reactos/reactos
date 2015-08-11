2bcae4a0cb9cd14dac998a5c3de0c422857f1805
Christoph von Wittich <christoph_vw@reactos.org>
2015-08-11 09:05:45 +0000

    [MSAFD] addendum to 68673
    svn path=/trunk/; revision=68680

--- reactos/dll/win32/msafd/misc/helpers.c
+++ reactos/dll/win32/msafd/misc/helpers.c
@@ -40,8 +40,8 @@ SockGetTdiName(
     PLIST_ENTRY	        Helpers;
     INT                 Status;
 
-    TRACE("AddressFamily %p, SocketType %p, Protocol %p, Group %u, Flags %lx, HelperDllContext %p, HeplperDllData %p, Events %p\n",
-        AddressFamily, SocketType, Protocol, Group, Flags, HelperDllContext, HelperDllData, Events);
+    TRACE("AddressFamily %p, SocketType %p, Protocol %p, Group %u, Flags %lx, TransportName %p, HelperDllContext %p, HelperDllData %p, Events %p\n",
+        AddressFamily, SocketType, Protocol, Group, Flags, TransportName, HelperDllContext, HelperDllData, Events);
 
     /* Check in our Current Loaded Helpers */
     for (Helpers = SockHelpersListHead.Flink;
