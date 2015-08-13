0303c3abbe0c8e6494cbcefb8386897acbecf1d8
Pierre Schweitzer <pierre@reactos.org>
2015-08-13 09:38:46 +0000

    [SETUPAPI_WINETESTS] Partly sync with Wine 1.7.47: - Import tests for SetupLogError* functions
    CORE-9924
    
    svn path=/trunk/; revision=68706

--- rostests/winetests/setupapi/misc.c
+++ rostests/winetests/setupapi/misc.c
@@ -776,6 +776,46 @@ static void test_defaultcallback(void)
     SetupTermDefaultQueueCallback(ctxt);
 }
 
+static void test_SetupLogError(void)
+{
+    BOOL ret;
+    DWORD error;
+
+    SetLastError(0xdeadbeef);
+    ret = SetupLogErrorA("Test without opening\r\n", LogSevInformation);
+    error = GetLastError();
+    ok(!ret, "SetupLogError succeeded\n");
+    ok(error == ERROR_FILE_INVALID, "got wrong error: %d\n", error);
+
+    SetLastError(0xdeadbeef);
+    ret = SetupOpenLog(FALSE);
+    if (!ret && GetLastError() == ERROR_ACCESS_DENIED)
+    {
+        win_skip("SetupOpenLog() failed on insufficient permissions\n");
+        return;
+    }
+    ok(ret, "SetupOpenLog failed, error %d\n", GetLastError());
+
+    SetLastError(0xdeadbeef);
+    ret = SetupLogErrorA("Test with wrong log severity\r\n", LogSevMaximum);
+    error = GetLastError();
+    ok(!ret, "SetupLogError succeeded\n");
+    ok(error == 0xdeadbeef, "got wrong error: %d\n", error);
+    ret = SetupLogErrorA("Test without EOL", LogSevInformation);
+    ok(ret, "SetupLogError failed\n");
+
+    SetLastError(0xdeadbeef);
+    ret = SetupLogErrorA(NULL, LogSevInformation);
+    ok(ret || broken(!ret && GetLastError() == ERROR_INVALID_PARAMETER /* Win Vista+ */),
+        "SetupLogError failed: %08x\n", GetLastError());
+
+    SetLastError(0xdeadbeef);
+    ret = SetupOpenLog(FALSE);
+    ok(ret, "SetupOpenLog failed, error %d\n", GetLastError());
+
+    SetupCloseLog();
+}
+
 START_TEST(misc)
 {
     HMODULE hsetupapi = GetModuleHandleA("setupapi.dll");
@@ -807,4 +847,6 @@ START_TEST(misc)
         win_skip("SetupUninstallOEMInfA is not available\n");
 
     test_defaultcallback();
+
+    test_SetupLogError();
 }
