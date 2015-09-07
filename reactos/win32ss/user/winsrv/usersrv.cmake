4544bdcfd0831b7f3ef722b7e86947cd0dbd12c2
Amine Khaldi <amine.khaldi@reactos.org>
2015-09-07 11:13:09 +0000

    [CMAKE] Add even more missing dependencies on xdk. CORE-9586
    svn path=/trunk/; revision=69087

--- reactos/win32ss/user/winsrv/usersrv.cmake
+++ reactos/win32ss/user/winsrv/usersrv.cmake
@@ -10,6 +10,7 @@ list(APPEND USERSRV_SOURCE
     usersrv/usersrv.h)
 
 add_library(usersrv ${USERSRV_SOURCE})
+add_dependencies(usersrv xdk)
 add_pch(usersrv usersrv/usersrv.h USERSRV_SOURCE)
 #add_object_library(usersrv ${USERSRV_SOURCE})
 list(APPEND USERSRV_IMPORT_LIBS basesrv)
