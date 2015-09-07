525668eff1150c39a5e007ce839e02e13a820806
Thomas Faber <thomas.faber@reactos.org>
2015-09-07 09:46:43 +0000

    [XDK][DDK][PSDK] - Generate XDK headers during build. To make changes to wdm.h, ntddk.h, ntifs.h, devioctl.h, ntdef.h or winnt.h, edit the appropriate files in include/xdk (as you should already have been doing for a long time). CORE-9586 #resolve
    svn path=/trunk/; revision=69083

--- reactos/include/xdk/generate.bat
+++
@@ -1,8 +0,0 @@
-
-
-hpp.exe wdm.template.h ..\ddk\wdm.h
-hpp.exe ntddk.template.h ..\ddk\ntddk.h
-hpp.exe ntifs.template.h ..\ddk\ntifs.h
-hpp.exe devioctl.template.h ..\psdk\devioctl.h
-hpp.exe ntdef.template.h ..\psdk\ntdef.h
-hpp.exe winnt.template.h ..\psdk\winnt.h
