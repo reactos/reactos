40d3c1924207f25d712569a7fb51110980ef34b4
Pierre Schweitzer <pierre@reactos.org>
2015-09-20 17:51:41 +0000

    [NTDLL_APITESTS] Add more tests
    svn path=/trunk/; revision=69307

--- rostests/apitests/ntdll/RtlGenerate8dot3Name.c
+++ rostests/apitests/ntdll/RtlGenerate8dot3Name.c
@@ -22,6 +22,8 @@ RtlGenerate8dot3Name(
 PWSTR Names[] = { L"Menu Démarrer", L"Sélecteur de configuration clavier.lnk", L"éèàùç.txt", L"éèàùçeeauc.txt", L"Long file name.txt", L"Long file name", L"Longfilename.txt", L"Longfilename" };
 PWSTR ShortNames1[] = { L"MENUDM~1", L"SLECTE~1.LNK", L"5C2D~1.TXT", L"EEAUC~1.TXT", L"LONGFI~1.TXT", L"LONGFI~1", L"LONGFI~1.TXT", L"LONGFI~1" };
 PWSTR ShortNames2[] = { L"MENUDM~2", L"SLECTE~2.LNK", L"5C2D~2.TXT", L"EEAUC~2.TXT", L"LONGFI~2.TXT", L"LONGFI~2", L"LONGFI~2.TXT", L"LONGFI~2" };
+PWSTR ExShortNames1[] = { L"MENUDÉ~1", L"SÉLECT~1.LNK", L"ÉÈÀÙÇ~1.TXT", L"ÉÈÀÙÇE~1.TXT", L"LONGFI~1.TXT", L"LONGFI~1", L"LONGFI~1.TXT", L"LONGFI~1" };
+PWSTR ExShortNames2[] = { L"MENUDÉ~2", L"SÉLECT~2.LNK", L"ÉÈÀÙÇ~2.TXT", L"ÉÈÀÙÇE~2.TXT", L"LONGFI~2.TXT", L"LONGFI~2", L"LONGFI~2.TXT", L"LONGFI~2" };
 
 START_TEST(RtlGenerate8dot3Name)
 {
@@ -46,6 +48,17 @@ START_TEST(RtlGenerate8dot3Name)
         ShortName.Length = 0;
         RtlGenerate8dot3Name(&LongName, FALSE, &Context, &ShortName);
         RtlInitUnicodeString(&Expected, ShortNames2[i]);
+        ok(RtlEqualUnicodeString(&Expected, &ShortName, FALSE), "Generated: %.*S. Expected: %.*S\n", ShortName.Length, ShortName.Buffer, Expected.Length, Expected.Buffer);
+
+        RtlZeroMemory(&Context, sizeof(GENERATE_NAME_CONTEXT));
+        ShortName.Length = 0;
+        RtlGenerate8dot3Name(&LongName, TRUE, &Context, &ShortName);
+        RtlInitUnicodeString(&Expected, ExShortNames1[i]);
+        ok(RtlEqualUnicodeString(&Expected, &ShortName, FALSE), "Generated: %.*S. Expected: %.*S\n", ShortName.Length, ShortName.Buffer, Expected.Length, Expected.Buffer);
+
+        ShortName.Length = 0;
+        RtlGenerate8dot3Name(&LongName, TRUE, &Context, &ShortName);
+        RtlInitUnicodeString(&Expected, ExShortNames2[i]);
         ok(RtlEqualUnicodeString(&Expected, &ShortName, FALSE), "Generated: %.*S. Expected: %.*S\n", ShortName.Length, ShortName.Buffer, Expected.Length, Expected.Buffer);
     }
 }
