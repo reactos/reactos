2ca65776873d47eea82fe9261f53308f75e33449
Pierre Schweitzer <pierre@reactos.org>
2015-09-29 17:30:54 +0000

    [MOUNTMGR] Get rid of a hack, thanks to r69413
    svn path=/trunk/; revision=69414

--- reactos/drivers/filters/mountmgr/mntmgr.h
+++ reactos/drivers/filters/mountmgr/mntmgr.h
@@ -16,12 +16,6 @@
 #endif
 
 /* Enter FIXME */
-#ifdef IsEqualGUID
-#undef IsEqualGUID
-#endif
-
-#define IsEqualGUID(rguid1, rguid2) (RtlCompareMemory(rguid1, rguid2, sizeof(GUID)) == sizeof(GUID))
-
 #define FILE_READ_PROPERTIES  0x00000008
 #define FILE_WRITE_PROPERTIES 0x00000010
 /* Leave FIXME */
