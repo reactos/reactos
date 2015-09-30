36196c1275a34331c560e43c18d6969ef573357e
Ged Murphy <gedmurphy@reactos.org>
2015-09-30 19:55:52 +0000

    [DEVMGR] Add typedef for CStringAtlW. Fixes msvc build
    svn path=/trunk/; revision=69419

--- reactos/lib/atl/atlstr.h
+++ reactos/lib/atl/atlstr.h
@@ -97,45 +97,26 @@ private:
     }
 };
 
-//
-//template class <typename _BaseType = char, class StringIterator = ChTraitsOS<_BaseType>>
-//class StrTraitATL :
-//    public StringIterator
-//{
-//public:
-//    static HINSTANCE FindStringResourceInstance(_In_ UINT nID) throw()
-//    {
-//        return AtlFindStringResourceInstance(nID);
-//    }
-//
-//    static IAtlStringMgr* GetDefaultManager() throw()
-//    {
-//        return CAtlStringMgr::GetInstance();
-//    }
-//};
-//
-//
-//template< typename _CharType = wchar_t>
-//class ChTraitsOS :
-//    public ChTraitsBase<_CharType>
-//{
-//protected:
-//
-//public:
-//
-//};
-//
-//#ifndef _ATL_CSTRING_NO_CRT
-//    typedef CStringT<wchar_t, StrTraitATL<wchar_t, ChTraitsCRT<wchar_t>>> CAtlStringW;
-//#else
-//    typedef CStringT<wchar_t, StrTraitATL<wchar_t>> CAtlStringW;
-//#endif
-//
-//#ifndef _AFX
-//    typedef CAtlStringW CStringW;
-//#endif
+
+template <typename _BaseType = char, class StringIterator = ChTraitsOS<_BaseType>>
+class StrTraitATL :
+    public StringIterator
+{
+public:
+    static HINSTANCE FindStringResourceInstance(_In_ UINT nID) throw()
+    {
+        return AtlFindStringResourceInstance(nID);
+    }
+
+    static IAtlStringMgr* GetDefaultManager() throw()
+    {
+        return CAtlStringMgr::GetInstance();
+    }
+};
 
 
-} //namespace ATL
+typedef CStringT<wchar_t, StrTraitATL<wchar_t, ChTraitsCRT<wchar_t>>> CAtlStringW;
+
+}
 
 #endif
