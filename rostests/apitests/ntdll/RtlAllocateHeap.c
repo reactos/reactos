24420c2ba13147e65c0758d1da5f7a6baa4cdc88
Pierre Schweitzer <pierre@reactos.org>
2015-10-11 21:49:16 +0000

    [NTDLL_APITESTS] Extend the RtlAllocateHeap() test
    svn path=/trunk/; revision=69506

--- rostests/apitests/ntdll/RtlAllocateHeap.c
+++ rostests/apitests/ntdll/RtlAllocateHeap.c
@@ -15,11 +15,12 @@ PVOID Buffers[0x100];
 START_TEST(RtlAllocateHeap)
 {
     USHORT i;
+    HANDLE hHeap;
     BOOLEAN Aligned = TRUE;
+    RTL_HEAP_PARAMETERS Parameters = {0};
 
     for (i = 0; i < 0x100; ++i)
     {
-        SetLastError(0xdeadbeef);
         Buffers[i] = RtlAllocateHeap(RtlGetProcessHeap(), HEAP_CREATE_ALIGN_16, (i % 16 ) + 1);
         ASSERT(Buffers[i] != NULL);
         if (!((ULONG_PTR)Buffers[i] & 0x2))
@@ -32,6 +33,33 @@ START_TEST(RtlAllocateHeap)
     {
         RtlFreeHeap(RtlGetProcessHeap(), 0, Buffers[i]);
     }
+
+    ok(Aligned  == FALSE, "No unaligned address returned\n");
+
+    Aligned = TRUE;
+    Parameters.Length = sizeof(Parameters);
+    hHeap = RtlCreateHeap(HEAP_CREATE_ALIGN_16, NULL, 0, 0, NULL, &Parameters);
+    if (hHeap == NULL)
+    {
+        return;
+    }
+
+    for (i = 0; i < 0x100; ++i)
+    {
+        Buffers[i] = RtlAllocateHeap(RtlGetProcessHeap(), 0, (i % 16 ) + 1);
+        ASSERT(Buffers[i] != NULL);
+        if (!((ULONG_PTR)Buffers[i] & 0x2))
+        {
+            Aligned = FALSE;
+        }
+    }
+
+    for (i = 0; i < 0x100; ++i)
+    {
+        RtlFreeHeap(RtlGetProcessHeap(), 0, Buffers[i]);
+    }
+
+    RtlDestroyHeap(hHeap);
 
     ok(Aligned  == FALSE, "No unaligned address returned\n");
 }
