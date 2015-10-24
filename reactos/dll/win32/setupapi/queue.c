d3a401609e57c0e009078b5a8597c885fc6fd577
Pierre Schweitzer <pierre@reactos.org>
2015-10-24 10:14:21 +0000

    [SETUPAPI] Partly backport Wine commit: 4d796458d0ed517d45adc57a1aedaf1c3bdde232 "Avoid memory leaks (coverity)." by André Hentschel
    CID 716469
    
    svn path=/trunk/; revision=69672

--- reactos/dll/win32/setupapi/queue.c
+++ reactos/dll/win32/setupapi/queue.c
@@ -1216,7 +1216,11 @@ BOOL WINAPI SetupInstallFileW( HINF hinf, PINFCONTEXT inf_context, PCWSTR source
             SetLastError( ERROR_NOT_ENOUGH_MEMORY );
             return FALSE;
         }
-        if (!SetupGetStringFieldW( inf_context, 1, inf_source, len, NULL )) return FALSE;
+        if (!SetupGetStringFieldW( inf_context, 1, inf_source, len, NULL ))
+        {
+            HeapFree( GetProcessHeap(), 0, inf_source );
+            return FALSE;
+        }
         source = inf_source;
     }
     else if (!source)
