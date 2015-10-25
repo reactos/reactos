465a34000b2e4cdb3b52681a02da3d44810cd358
Amine Khaldi <amine.khaldi@reactos.org>
2015-10-25 11:11:34 +0000

    [CRT] Remove second semicolon. Spotted by Michael Fritscher. CORE-10390
    svn path=/trunk/; revision=69691

--- reactos/lib/sdk/crt/string/mbstowcs_nt.c
+++ reactos/lib/sdk/crt/string/mbstowcs_nt.c
@@ -60,7 +60,7 @@ size_t mbstowcs (wchar_t *wcstr, const char *mbstr, size_t count)
 	if (!NT_SUCCESS(Status))
 		return -1;
 
-	return (size_t)(Size / sizeof(wchar_t));;
+	return (size_t)(Size / sizeof(wchar_t));
 }
 
 /* EOF */
