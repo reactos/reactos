6445100f0d53351df4036f0c50045fdca1f283c2
Pierre Schweitzer <pierre@reactos.org>
2015-10-25 09:28:57 +0000

    [CRT] Fix NTDLL implementation of mbstowcs() and wcstombs() so that they return length in caracters and not in bytes.
    This fixes last failing *to* CRT apitests
    
    CORE-10390
    
    svn path=/trunk/; revision=69683

--- reactos/lib/sdk/crt/string/mbstowcs_nt.c
+++ reactos/lib/sdk/crt/string/mbstowcs_nt.c
@@ -49,7 +49,7 @@ size_t mbstowcs (wchar_t *wcstr, const char *mbstr, size_t count)
 		                           mbstr,
 		                           Length);
 
-		return (size_t)Size;
+		return (size_t)(Size / sizeof(wchar_t));
 	}
 
 	Status = RtlMultiByteToUnicodeN (wcstr,
@@ -60,7 +60,7 @@ size_t mbstowcs (wchar_t *wcstr, const char *mbstr, size_t count)
 	if (!NT_SUCCESS(Status))
 		return -1;
 
-	return (size_t)Size;
+	return (size_t)(Size / sizeof(wchar_t));;
 }
 
 /* EOF */
