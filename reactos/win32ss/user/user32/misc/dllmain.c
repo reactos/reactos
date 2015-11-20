1b33a3c6aae04f4bcf9d9bf954f089699a17e32c
Thomas Faber <thomas.faber@reactos.org>
2015-11-20 18:04:50 +0000

    [USER32] - Keep an extra reference to user32 around while we're not using imm32 CORE-10540
    svn path=/trunk/; revision=69965

--- reactos/win32ss/user/user32/misc/dllmain.c
+++ reactos/win32ss/user/user32/misc/dllmain.c
@@ -526,7 +526,12 @@ DllMain(
 
             if (!gfServerProcess)
             {
-               //InitializeImmEntryTable();
+#if WIN32K_ISNT_BROKEN
+               InitializeImmEntryTable();
+#else
+               /* imm32 takes a refcount and prevents us from unloading */
+               LoadLibraryW(L"user32");
+#endif
                //
                // Wine is stub and throws an exception so save this for real Imm32.dll testing!!!!
                //
