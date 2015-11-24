0e6c0cd8777a60609af4bd189c8260dd204180f1
Amine Khaldi <amine.khaldi@reactos.org>
2015-11-24 10:52:32 +0000

    [DINPUT_WINETEST] Sync with Wine Staging 1.7.55. CORE-10536
    svn path=/trunk/; revision=70087

--- rostests/winetests/dinput/mouse.c
+++ rostests/winetests/dinput/mouse.c
@@ -97,6 +97,7 @@ static void test_acquire(IDirectInputA *pDI, HWND hwnd)
     DIPROPDWORD di_op;
     DIDEVICEOBJECTDATA mouse_state;
     DWORD cnt;
+    MSG msg;
     int i;
 
     if (! SetForegroundWindow(hwnd))
@@ -134,6 +135,8 @@ static void test_acquire(IDirectInputA *pDI, HWND hwnd)
      * loose mouse input */
     hwnd2 = CreateWindowA("static", "Temporary", WS_VISIBLE, 10, 210, 200, 200, NULL, NULL, NULL,
                           NULL);
+    ok(hwnd2 != NULL, "CreateWindowA failed with %u\n", GetLastError());
+    while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE)) DispatchMessageA(&msg);
 
     hr = IDirectInputDevice_GetDeviceState(pMouse, sizeof(m_state), &m_state);
     ok(hr == DIERR_NOTACQUIRED, "GetDeviceState() should have failed: %08x\n", hr);
