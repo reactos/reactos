7ad90a65b2a2cbb35f22349ea55c67a4db649298
Amine Khaldi <amine.khaldi@reactos.org>
2015-11-26 22:46:03 +0000

    [WORDPAD] Sync with Wine Staging 1.7.55. CORE-10536
    svn path=/trunk/; revision=70123

--- reactos/base/applications/wordpad/registry.c
+++ reactos/base/applications/wordpad/registry.c
@@ -156,9 +156,9 @@ static void format_filelist_filename(LPWSTR file, LPWSTR out)
 {
     LPWSTR pos_basename;
     LPWSTR truncpos1, truncpos2;
-    WCHAR myDocs[MAX_PATH];
+    WCHAR myDocs[MAX_STRING_LEN];
 
-    SHGetFolderPathW(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, myDocs);
+    SHGetFolderPathW(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, (LPWSTR)&myDocs);
     pos_basename = file_basename(file);
     truncpos1 = NULL;
     truncpos2 = NULL;
