0d30715ea307217c250ed13207e8cc36f696c2cc
Amine Khaldi <amine.khaldi@reactos.org>
2015-11-30 20:08:57 +0000

    [PSDK] Attempt to improve the whole iphlpapi related headers situation. CORE-7685 CORE-10536
    svn path=/trunk/; revision=70220

--- reactos/include/psdk/iprtrmib.h
+++ reactos/include/psdk/iprtrmib.h
@@ -13,298 +13,20 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 #ifndef WINE_IPRTRMIB_H__
 #define WINE_IPRTRMIB_H__
 
-#define MAX_INTERFACE_NAME_LEN 256
-
+#include <mprapi.h>
 #include <ipifcons.h>
 
-#define MAXLEN_IFDESCR 256
-#define MAXLEN_PHYSADDR 8
+#include <ipmib.h>
+#include <tcpmib.h>
+#include <udpmib.h>
 
-//It should be 16 according to Lei Shen blog (http://www.mychinaworks.com/blog/lshen/2008/04/16/220/
-#define TCPIP_OWNING_MODULE_SIZE 16
-
-typedef struct _MIB_IFROW
+typedef enum _TCP_TABLE_CLASS
 {
-    WCHAR wszName[MAX_INTERFACE_NAME_LEN];
-    DWORD dwIndex;
-    DWORD dwType;
-    DWORD dwMtu;
-    DWORD dwSpeed;
-    DWORD dwPhysAddrLen;
-    BYTE  bPhysAddr[MAXLEN_PHYSADDR];
-    DWORD dwAdminStatus;
-    DWORD dwOperStatus;
-    DWORD dwLastChange;
-    DWORD dwInOctets;
-    DWORD dwInUcastPkts;
-    DWORD dwInNUcastPkts;
-    DWORD dwInDiscards;
-    DWORD dwInErrors;
-    DWORD dwInUnknownProtos;
-    DWORD dwOutOctets;
-    DWORD dwOutUcastPkts;
-    DWORD dwOutNUcastPkts;
-    DWORD dwOutDiscards;
-    DWORD dwOutErrors;
-    DWORD dwOutQLen;
-    DWORD dwDescrLen;
-    BYTE  bDescr[MAXLEN_IFDESCR];
-} MIB_IFROW,*PMIB_IFROW;
-
-typedef struct _MIB_IFTABLE
-{
-    DWORD     dwNumEntries;
-    MIB_IFROW table[1];
-} MIB_IFTABLE, *PMIB_IFTABLE;
-
-typedef struct _MIBICMPSTATS
-{
-    DWORD dwMsgs;
-    DWORD dwErrors;
-    DWORD dwDestUnreachs;
-    DWORD dwTimeExcds;
-    DWORD dwParmProbs;
-    DWORD dwSrcQuenchs;
-    DWORD dwRedirects;
-    DWORD dwEchos;
-    DWORD dwEchoReps;
-    DWORD dwTimestamps;
-    DWORD dwTimestampReps;
-    DWORD dwAddrMasks;
-    DWORD dwAddrMaskReps;
-} MIBICMPSTATS;
-
-typedef    struct _MIBICMPINFO
-{
-    MIBICMPSTATS icmpInStats;
-    MIBICMPSTATS icmpOutStats;
-} MIBICMPINFO;
-
-typedef struct _MIB_ICMP
-{
-    MIBICMPINFO stats;
-} MIB_ICMP,*PMIB_ICMP;
-
-typedef struct _MIB_UDPSTATS
-{
-    DWORD dwInDatagrams;
-    DWORD dwNoPorts;
-    DWORD dwInErrors;
-    DWORD dwOutDatagrams;
-    DWORD dwNumAddrs;
-} MIB_UDPSTATS,*PMIB_UDPSTATS;
-
-typedef struct _MIB_UDPROW
-{
-    DWORD dwLocalAddr;
-    DWORD dwLocalPort;
-} MIB_UDPROW, *PMIB_UDPROW;
-
-typedef struct _MIB_UDPTABLE
-{
-    DWORD      dwNumEntries;
-    MIB_UDPROW table[1];
-} MIB_UDPTABLE, *PMIB_UDPTABLE;
-
-typedef struct _MIB_TCPSTATS
-{
-    DWORD dwRtoAlgorithm;
-    DWORD dwRtoMin;
-    DWORD dwRtoMax;
-    DWORD dwMaxConn;
-    DWORD dwActiveOpens;
-    DWORD dwPassiveOpens;
-    DWORD dwAttemptFails;
-    DWORD dwEstabResets;
-    DWORD dwCurrEstab;
-    DWORD dwInSegs;
-    DWORD dwOutSegs;
-    DWORD dwRetransSegs;
-    DWORD dwInErrs;
-    DWORD dwOutRsts;
-    DWORD dwNumConns;
-} MIB_TCPSTATS, *PMIB_TCPSTATS;
-
-typedef struct _MIBICMPSTATS_EX
-{
-    DWORD       dwMsgs;
-    DWORD       dwErrors;
-    DWORD       rgdwTypeCount[256];
-} MIBICMPSTATS_EX, *PMIBICMPSTATS_EX;
-
-typedef struct _MIB_ICMP_EX
-{
-    MIBICMPSTATS_EX icmpInStats;
-    MIBICMPSTATS_EX icmpOutStats;
-} MIB_ICMP_EX,*PMIB_ICMP_EX;
-
-typedef struct _MIB_TCPROW
-{
-    DWORD dwState;
-    DWORD dwLocalAddr;
-    DWORD dwLocalPort;
-    DWORD dwRemoteAddr;
-    DWORD dwRemotePort;
-} MIB_TCPROW, *PMIB_TCPROW;
-
-#define MIB_TCP_STATE_CLOSED            1
-#define MIB_TCP_STATE_LISTEN            2
-#define MIB_TCP_STATE_SYN_SENT          3
-#define MIB_TCP_STATE_SYN_RCVD          4
-#define MIB_TCP_STATE_ESTAB             5
-#define MIB_TCP_STATE_FIN_WAIT1         6
-#define MIB_TCP_STATE_FIN_WAIT2         7
-#define MIB_TCP_STATE_CLOSE_WAIT        8
-#define MIB_TCP_STATE_CLOSING           9
-#define MIB_TCP_STATE_LAST_ACK         10
-#define MIB_TCP_STATE_TIME_WAIT        11
-#define MIB_TCP_STATE_DELETE_TCB       12
-
-typedef struct _MIB_TCPTABLE
-{
-    DWORD      dwNumEntries;
-    MIB_TCPROW table[1];
-} MIB_TCPTABLE, *PMIB_TCPTABLE;
-
-typedef struct _MIB_IPSTATS
-{
-    DWORD dwForwarding;
-    DWORD dwDefaultTTL;
-    DWORD dwInReceives;
-    DWORD dwInHdrErrors;
-    DWORD dwInAddrErrors;
-    DWORD dwForwDatagrams;
-    DWORD dwInUnknownProtos;
-    DWORD dwInDiscards;
-    DWORD dwInDelivers;
-    DWORD dwOutRequests;
-    DWORD dwRoutingDiscards;
-    DWORD dwOutDiscards;
-    DWORD dwOutNoRoutes;
-    DWORD dwReasmTimeout;
-    DWORD dwReasmReqds;
-    DWORD dwReasmOks;
-    DWORD dwReasmFails;
-    DWORD dwFragOks;
-    DWORD dwFragFails;
-    DWORD dwFragCreates;
-    DWORD dwNumIf;
-    DWORD dwNumAddr;
-    DWORD dwNumRoutes;
-} MIB_IPSTATS, *PMIB_IPSTATS;
-
-typedef struct _MIB_IPADDRROW
-{
-    DWORD        dwAddr;
-    DWORD        dwIndex;
-    DWORD        dwMask;
-    DWORD        dwBCastAddr;
-    DWORD        dwReasmSize;
-    unsigned short    unused1;
-    unsigned short    wType;
-} MIB_IPADDRROW, *PMIB_IPADDRROW;
-
-typedef struct _MIB_IPADDRTABLE
-{
-    DWORD         dwNumEntries;
-    MIB_IPADDRROW table[1];
-} MIB_IPADDRTABLE, *PMIB_IPADDRTABLE;
-
-
-typedef struct _MIB_IPFORWARDNUMBER
-{
-    DWORD      dwValue;
-}MIB_IPFORWARDNUMBER,*PMIB_IPFORWARDNUMBER;
-
-typedef struct _MIB_IPFORWARDROW
-{
-    DWORD dwForwardDest;
-    DWORD dwForwardMask;
-    DWORD dwForwardPolicy;
-    DWORD dwForwardNextHop;
-    DWORD dwForwardIfIndex;
-    DWORD dwForwardType;
-    DWORD dwForwardProto;
-    DWORD dwForwardAge;
-    DWORD dwForwardNextHopAS;
-    DWORD dwForwardMetric1;
-    DWORD dwForwardMetric2;
-    DWORD dwForwardMetric3;
-    DWORD dwForwardMetric4;
-    DWORD dwForwardMetric5;
-}MIB_IPFORWARDROW, *PMIB_IPFORWARDROW;
-
-#define MIB_IPROUTE_TYPE_OTHER      1
-#define MIB_IPROUTE_TYPE_INVALID    2
-#define MIB_IPROUTE_TYPE_DIRECT     3
-#define MIB_IPROUTE_TYPE_INDIRECT   4
-
-#define MIB_IPPROTO_OTHER             1
-#define MIB_IPPROTO_LOCAL             2
-#define MIB_IPPROTO_NETMGMT           3
-#define MIB_IPPROTO_ICMP              4
-#define MIB_IPPROTO_EGP               5
-#define MIB_IPPROTO_GGP               6
-#define MIB_IPPROTO_HELLO             7
-#define MIB_IPPROTO_RIP               8
-#define MIB_IPPROTO_IS_IS             9
-#define MIB_IPPROTO_ES_IS             10
-#define MIB_IPPROTO_CISCO             11
-#define MIB_IPPROTO_BBN               12
-#define MIB_IPPROTO_OSPF              13
-#define MIB_IPPROTO_BGP               14
-
-#define MIB_IPPROTO_NT_AUTOSTATIC     10002
-#define MIB_IPPROTO_NT_STATIC         10006
-#define MIB_IPPROTO_NT_STATIC_NON_DOD 10007
-
-typedef struct _MIB_IPFORWARDTABLE
-{
-    DWORD            dwNumEntries;
-    MIB_IPFORWARDROW table[1];
-} MIB_IPFORWARDTABLE, *PMIB_IPFORWARDTABLE;
-
-typedef struct _MIB_IPNETROW
-{
-    DWORD dwIndex;
-    DWORD dwPhysAddrLen;
-    BYTE  bPhysAddr[MAXLEN_PHYSADDR];
-    DWORD dwAddr;
-    DWORD dwType;
-} MIB_IPNETROW, *PMIB_IPNETROW;
-
-#define    MIB_IPNET_TYPE_OTHER        1
-#define    MIB_IPNET_TYPE_INVALID        2
-#define    MIB_IPNET_TYPE_DYNAMIC        3
-#define    MIB_IPNET_TYPE_STATIC        4
-
-typedef struct _MIB_IPNETTABLE
-{
-    DWORD        dwNumEntries;
-    MIB_IPNETROW table[1];
-} MIB_IPNETTABLE, *PMIB_IPNETTABLE;
-
-typedef struct _MIB_TCPROW_OWNER_MODULE {
-  DWORD         dwState;
-  DWORD         dwLocalAddr;
-  DWORD         dwLocalPort;
-  DWORD         dwRemoteAddr;
-  DWORD         dwRemotePort;
-  DWORD         dwOwningPid;
-  LARGE_INTEGER liCreateTimestamp;
-  ULONGLONG     OwningModuleInfo[TCPIP_OWNING_MODULE_SIZE];
-} MIB_TCPROW_OWNER_MODULE, *PMIB_TCPROW_OWNER_MODULE;
-
-typedef enum  {
-  TCPIP_OWNER_MODULE_INFO_BASIC 
-} TCPIP_OWNER_MODULE_INFO_CLASS, *PTCPIP_OWNER_MODULE_INFO_CLASS;
-
-typedef enum {
     TCP_TABLE_BASIC_LISTENER,
     TCP_TABLE_BASIC_CONNECTIONS,
     TCP_TABLE_BASIC_ALL,
@@ -316,19 +38,17 @@ typedef enum {
     TCP_TABLE_OWNER_MODULE_ALL
 } TCP_TABLE_CLASS, *PTCP_TABLE_CLASS;
 
-typedef struct _MIB_TCPROW_OWNER_PID {
-  DWORD dwState;
-  DWORD dwLocalAddr;
-  DWORD dwLocalPort;
-  DWORD dwRemoteAddr;
-  DWORD dwRemotePort;
-  DWORD dwOwningPid;
-} MIB_TCPROW_OWNER_PID, *PMIB_TCPROW_OWNER_PID;
+typedef enum _UDP_TABLE_CLASS
+{
+    UDP_TABLE_BASIC,
+    UDP_TABLE_OWNER_PID,
+    UDP_TABLE_OWNER_MODULE
+} UDP_TABLE_CLASS, *PUDP_TABLE_CLASS;
 
-typedef struct {
-  DWORD                dwNumEntries;
-  MIB_TCPROW_OWNER_PID table[1];
-} MIB_TCPTABLE_OWNER_PID, *PMIB_TCPTABLE_OWNER_PID;
-
+/* ReactOS */
+typedef enum _TCPIP_OWNER_MODULE_INFO_CLASS
+{
+    TCPIP_OWNER_MODULE_INFO_BASIC
+} TCPIP_OWNER_MODULE_INFO_CLASS, *PTCPIP_OWNER_MODULE_INFO_CLASS;
 
 #endif /* WINE_IPRTRMIB_H__ */
--- reactos/include/psdk/iptypes.h
+++ reactos/include/psdk/iptypes.h
@@ -1,206 +1,315 @@
-#ifndef _IPTYPES_H
-#define _IPTYPES_H
+/* WINE iptypes.h
+ * Copyright (C) 2003 Juan Lang
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 
-#include <sys/types.h>
+#ifndef WINE_IPTYPES_H_
+#define WINE_IPTYPES_H_
+
+#include <time.h>
 #include <ifdef.h>
+#include <nldef.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#define DEFAULT_MINIMUM_ENTITIES 32
-#define MAX_ADAPTER_ADDRESS_LENGTH 8
-#define MAX_ADAPTER_DESCRIPTION_LENGTH 128
-#define MAX_ADAPTER_NAME_LENGTH 256
-#define MAX_DOMAIN_NAME_LEN 128
-#define MAX_HOSTNAME_LEN 128
-#define MAX_SCOPE_ID_LEN 256
-#define BROADCAST_NODETYPE 1
-#define PEER_TO_PEER_NODETYPE 2
-#define MIXED_NODETYPE 4
-#define HYBRID_NODETYPE 8
-#define IF_OTHER_ADAPTERTYPE 0
-#define IF_ETHERNET_ADAPTERTYPE 1
-#define IF_TOKEN_RING_ADAPTERTYPE 2
-#define IF_FDDI_ADAPTERTYPE 3
-#define IF_PPP_ADAPTERTYPE 4
-#define IF_LOOPBACK_ADAPTERTYPE 5
+#define MAX_ADAPTER_DESCRIPTION_LENGTH  128
+#define MAX_ADAPTER_NAME_LENGTH         256
+#define MAX_ADAPTER_ADDRESS_LENGTH      8
+#define MAX_HOSTNAME_LEN                128
+#define MAX_DOMAIN_NAME_LEN             128
+#define MAX_SCOPE_ID_LEN                256
+#define MAX_DHCPV6_DUID_LENGTH          130
+#define MAX_DNS_SUFFIX_STRING_LENGTH    256
+
+#define BROADCAST_NODETYPE              1
+#define PEER_TO_PEER_NODETYPE           2
+#define MIXED_NODETYPE                  4
+#define HYBRID_NODETYPE                 8
+
 typedef struct {
-  char String[16];
+    char String[4 * 4];
 } IP_ADDRESS_STRING, *PIP_ADDRESS_STRING, IP_MASK_STRING, *PIP_MASK_STRING;
+
 typedef struct _IP_ADDR_STRING {
-  struct _IP_ADDR_STRING* Next;
-  IP_ADDRESS_STRING IpAddress;
-  IP_MASK_STRING IpMask;
-  DWORD Context;
+    struct _IP_ADDR_STRING* Next;
+    IP_ADDRESS_STRING IpAddress;
+    IP_MASK_STRING IpMask;
+    DWORD Context;
 } IP_ADDR_STRING, *PIP_ADDR_STRING;
+
 typedef struct _IP_ADAPTER_INFO {
-  struct _IP_ADAPTER_INFO* Next;
-  DWORD ComboIndex;
-  char AdapterName[MAX_ADAPTER_NAME_LENGTH+4];
-  char Description[MAX_ADAPTER_DESCRIPTION_LENGTH+4];
-  UINT AddressLength;
-  BYTE Address[MAX_ADAPTER_ADDRESS_LENGTH];
-  DWORD Index;
-  UINT Type;
-  UINT DhcpEnabled;
-  PIP_ADDR_STRING CurrentIpAddress;
-  IP_ADDR_STRING IpAddressList;
-  IP_ADDR_STRING GatewayList;
-  IP_ADDR_STRING DhcpServer;
-  BOOL HaveWins;
-  IP_ADDR_STRING PrimaryWinsServer;
-  IP_ADDR_STRING SecondaryWinsServer;
-  time_t LeaseObtained;
-  time_t LeaseExpires;
+    struct _IP_ADAPTER_INFO* Next;
+    DWORD ComboIndex;
+    char AdapterName[MAX_ADAPTER_NAME_LENGTH + 4];
+    char Description[MAX_ADAPTER_DESCRIPTION_LENGTH + 4];
+    UINT AddressLength;
+    BYTE Address[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD Index;
+    UINT Type;
+    UINT DhcpEnabled;
+    PIP_ADDR_STRING CurrentIpAddress;
+    IP_ADDR_STRING IpAddressList;
+    IP_ADDR_STRING GatewayList;
+    IP_ADDR_STRING DhcpServer;
+    BOOL HaveWins;
+    IP_ADDR_STRING PrimaryWinsServer;
+    IP_ADDR_STRING SecondaryWinsServer;
+    time_t LeaseObtained;
+    time_t LeaseExpires;
 } IP_ADAPTER_INFO, *PIP_ADAPTER_INFO;
+
 typedef struct _IP_PER_ADAPTER_INFO {
-  UINT AutoconfigEnabled;
-  UINT AutoconfigActive;
-  PIP_ADDR_STRING CurrentDnsServer;
-  IP_ADDR_STRING DnsServerList;
+    UINT AutoconfigEnabled;
+    UINT AutoconfigActive;
+    PIP_ADDR_STRING CurrentDnsServer;
+    IP_ADDR_STRING DnsServerList;
 } IP_PER_ADAPTER_INFO, *PIP_PER_ADAPTER_INFO;
+
 typedef struct {
-  ULONG Index;
-  ULONG MediaType;
-  UCHAR ConnectionType;
-  UCHAR AccessType;
-  GUID DeviceGuid;
-  GUID InterfaceGuid;
-} IP_INTERFACE_NAME_INFO, *PIP_INTERFACE_NAME_INFO;
-typedef struct _FIXED_INFO {
-  char HostName[MAX_HOSTNAME_LEN+4] ;
-  char DomainName[MAX_DOMAIN_NAME_LEN+4];
-  PIP_ADDR_STRING CurrentDnsServer;
-  IP_ADDR_STRING DnsServerList;
-  UINT NodeType;
-  char ScopeId[MAX_SCOPE_ID_LEN+4];
-  UINT EnableRouting;
-  UINT EnableProxy;
-  UINT EnableDns;
+    char HostName[MAX_HOSTNAME_LEN + 4] ;
+    char DomainName[MAX_DOMAIN_NAME_LEN + 4];
+    PIP_ADDR_STRING CurrentDnsServer;
+    IP_ADDR_STRING DnsServerList;
+    UINT NodeType;
+    char ScopeId[MAX_SCOPE_ID_LEN + 4];
+    UINT EnableRouting;
+    UINT EnableProxy;
+    UINT EnableDns;
 } FIXED_INFO, *PFIXED_INFO;
+
+typedef NL_PREFIX_ORIGIN IP_PREFIX_ORIGIN;
+typedef NL_SUFFIX_ORIGIN IP_SUFFIX_ORIGIN;
+typedef NL_DAD_STATE IP_DAD_STATE;
+
+/* ReactOS */
+#if (NTDDI_VERSION >= NTDDI_WIN2KSP1)
+typedef struct _IP_INTERFACE_NAME_INFO {
+    ULONG Index;
+    ULONG MediaType;
+    UCHAR ConnectionType;
+    UCHAR AccessType;
+    GUID DeviceGuid;
+    GUID InterfaceGuid;
+} IP_INTERFACE_NAME_INFO, *PIP_INTERFACE_NAME_INFO;
+#endif
+
 #ifdef _WINSOCK2API_
-typedef enum {
-  IpPrefixOriginOther = 0,
-  IpPrefixOriginManual,
-  IpPrefixOriginWellKnown,
-  IpPrefixOriginDhcp,
-  IpPrefixOriginRouterAdvertisement,
-} IP_PREFIX_ORIGIN;
-typedef enum {
-  IpSuffixOriginOther = 0,
-  IpSuffixOriginManual,
-  IpSuffixOriginWellKnown,
-  IpSuffixOriginDhcp,
-  IpSuffixOriginLinkLayerAddress,
-  IpSuffixOriginRandom,
-} IP_SUFFIX_ORIGIN;
-typedef enum {
-  IpDadStateInvalid = 0,
-  IpDadStateTentative,
-  IpDadStateDuplicate,
-  IpDadStateDeprecated,
-  IpDadStatePreferred,
-} IP_DAD_STATE;
+
 typedef struct _IP_ADAPTER_UNICAST_ADDRESS {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD Flags;
-    };
-  };
-  struct _IP_ADAPTER_UNICAST_ADDRESS *Next;
-  SOCKET_ADDRESS Address;
-  IP_PREFIX_ORIGIN PrefixOrigin;
-  IP_SUFFIX_ORIGIN SuffixOrigin;
-  IP_DAD_STATE DadState;
-  ULONG ValidLifetime;
-  ULONG PreferredLifetime;
-  ULONG LeaseLifetime;
+    union {
+        struct {
+            ULONG Length;
+            DWORD Flags;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_UNICAST_ADDRESS *Next;
+    SOCKET_ADDRESS                      Address;
+    IP_PREFIX_ORIGIN                    PrefixOrigin;
+    IP_SUFFIX_ORIGIN                    SuffixOrigin;
+    IP_DAD_STATE                        DadState;
+    ULONG                               ValidLifetime;
+    ULONG                               PreferredLifetime;
+    ULONG                               LeaseLifetime;
 } IP_ADAPTER_UNICAST_ADDRESS, *PIP_ADAPTER_UNICAST_ADDRESS;
+
 typedef struct _IP_ADAPTER_ANYCAST_ADDRESS {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD Flags;
-    };
-  };
-  struct _IP_ADAPTER_ANYCAST_ADDRESS *Next;
-  SOCKET_ADDRESS Address;
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Flags;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_ANYCAST_ADDRESS *Next;
+    SOCKET_ADDRESS                      Address;
 } IP_ADAPTER_ANYCAST_ADDRESS, *PIP_ADAPTER_ANYCAST_ADDRESS;
+
 typedef struct _IP_ADAPTER_MULTICAST_ADDRESS {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD Flags;
-    };
-  };
-  struct _IP_ADAPTER_MULTICAST_ADDRESS *Next;
-  SOCKET_ADDRESS Address;
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Flags;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_MULTICAST_ADDRESS *Next;
+    SOCKET_ADDRESS                       Address;
 } IP_ADAPTER_MULTICAST_ADDRESS, *PIP_ADAPTER_MULTICAST_ADDRESS;
+
 typedef struct _IP_ADAPTER_DNS_SERVER_ADDRESS {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD Reserved;
-    };
-  };
-  struct _IP_ADAPTER_DNS_SERVER_ADDRESS *Next;
-  SOCKET_ADDRESS Address;
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Reserved;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_DNS_SERVER_ADDRESS *Next;
+    SOCKET_ADDRESS                         Address;
 } IP_ADAPTER_DNS_SERVER_ADDRESS, *PIP_ADAPTER_DNS_SERVER_ADDRESS;
+
 typedef struct _IP_ADAPTER_PREFIX {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD Flags;
-    };
-  };
-  struct _IP_ADAPTER_PREFIX *Next;
-  SOCKET_ADDRESS Address;
-  ULONG PrefixLength;
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Flags;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_PREFIX *Next;
+    SOCKET_ADDRESS             Address;
+    ULONG                      PrefixLength;
 } IP_ADAPTER_PREFIX, *PIP_ADAPTER_PREFIX;
-typedef struct _IP_ADAPTER_ADDRESSES {
-  union {
-    ULONGLONG Alignment;
-    struct {
-      ULONG Length;
-      DWORD IfIndex;
-    } DUMMYSTRUCTNAME;
-  } DUMMYUNIONNAME;
-  struct _IP_ADAPTER_ADDRESSES *Next;
-  PCHAR AdapterName;
-  PIP_ADAPTER_UNICAST_ADDRESS FirstUnicastAddress;
-  PIP_ADAPTER_ANYCAST_ADDRESS FirstAnycastAddress;
-  PIP_ADAPTER_MULTICAST_ADDRESS FirstMulticastAddress;
-  PIP_ADAPTER_DNS_SERVER_ADDRESS FirstDnsServerAddress;
-  PWCHAR DnsSuffix;
-  PWCHAR Description;
-  PWCHAR FriendlyName;
-  BYTE PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
-  DWORD PhysicalAddressLength;
-  DWORD Flags;
-  DWORD Mtu;
-  DWORD IfType;
-  IF_OPER_STATUS OperStatus;
-  DWORD Ipv6IfIndex;
-  DWORD ZoneIndices[16];
-  PIP_ADAPTER_PREFIX FirstPrefix;
-} IP_ADAPTER_ADDRESSES, *PIP_ADAPTER_ADDRESSES;
 
-#define GAA_FLAG_SKIP_UNICAST 0x00000001
-#define GAA_FLAG_SKIP_ANYCAST 0x00000002
-#define GAA_FLAG_SKIP_MULTICAST 0x00000004
-#define GAA_FLAG_SKIP_DNS_SERVER 0x00000008
-#define GAA_FLAG_INCLUDE_PREFIX 0x00000010
-#define GAA_FLAG_SKIP_FRIENDLY_NAME 0x00000020
+typedef struct _IP_ADAPTER_WINS_SERVER_ADDRESS_LH {
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Reserved;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_WINS_SERVER_ADDRESS_LH *Next;
+    SOCKET_ADDRESS Address;
+} IP_ADAPTER_WINS_SERVER_ADDRESS_LH, *PIP_ADAPTER_WINS_SERVER_ADDRESS_LH;
+typedef IP_ADAPTER_WINS_SERVER_ADDRESS_LH IP_ADAPTER_WINS_SERVER_ADDRESS;
+typedef IP_ADAPTER_WINS_SERVER_ADDRESS_LH *PIP_ADAPTER_WINS_SERVER_ADDRESS;
 
+typedef struct _IP_ADAPTER_GATEWAY_ADDRESS_LH {
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD Reserved;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_GATEWAY_ADDRESS_LH *Next;
+    SOCKET_ADDRESS Address;
+} IP_ADAPTER_GATEWAY_ADDRESS_LH, *PIP_ADAPTER_GATEWAY_ADDRESS_LH;
+typedef IP_ADAPTER_GATEWAY_ADDRESS_LH IP_ADAPTER_GATEWAY_ADDRESS;
+typedef IP_ADAPTER_GATEWAY_ADDRESS_LH *PIP_ADAPTER_GATEWAY_ADDRESS;
+
+typedef struct _IP_ADAPTER_DNS_SUFFIX {
+    struct _IP_ADAPTER_DNS_SUFFIX *Next;
+    WCHAR String[MAX_DNS_SUFFIX_STRING_LENGTH];
+} IP_ADAPTER_DNS_SUFFIX, *PIP_ADAPTER_DNS_SUFFIX;
+
+#define IP_ADAPTER_DDNS_ENABLED               0x1
+#define IP_ADAPTER_REGISTER_ADAPTER_SUFFIX    0x2
+#define IP_ADAPTER_DHCP_ENABLED               0x4
+#define IP_ADAPTER_RECEIVE_ONLY               0x8
+#define IP_ADAPTER_NO_MULTICAST               0x10
+#define IP_ADAPTER_IPV6_OTHER_STATEFUL_CONFIG 0x20
+#define IP_ADAPTER_NETBIOS_OVER_TCPIP_ENABLED 0x40
+#define IP_ADAPTER_IPV4_ENABLED               0x80
+#define IP_ADAPTER_IPV6_ENABLED               0x100
+#define IP_ADAPTER_IPV6_MANAGE_ADDRESS_CONFIG 0x200
+
+typedef struct _IP_ADAPTER_ADDRESSES_LH {
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD IfIndex;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_ADDRESSES_LH *Next;
+    PCHAR                           AdapterName;
+    PIP_ADAPTER_UNICAST_ADDRESS     FirstUnicastAddress;
+    PIP_ADAPTER_ANYCAST_ADDRESS     FirstAnycastAddress;
+    PIP_ADAPTER_MULTICAST_ADDRESS   FirstMulticastAddress;
+    PIP_ADAPTER_DNS_SERVER_ADDRESS  FirstDnsServerAddress;
+    PWCHAR                          DnsSuffix;
+    PWCHAR                          Description;
+    PWCHAR                          FriendlyName;
+    BYTE                            PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD                           PhysicalAddressLength;
+    DWORD                           Flags;
+    DWORD                           Mtu;
+    DWORD                           IfType;
+    IF_OPER_STATUS                  OperStatus;
+    DWORD                           Ipv6IfIndex;
+    DWORD                           ZoneIndices[16];
+    PIP_ADAPTER_PREFIX              FirstPrefix;
+    ULONG64                         TransmitLinkSpeed;
+    ULONG64                         ReceiveLinkSpeed;
+    PIP_ADAPTER_WINS_SERVER_ADDRESS_LH FirstWinsServerAddress;
+    PIP_ADAPTER_GATEWAY_ADDRESS_LH  FirstGatewayAddress;
+    ULONG                           Ipv4Metric;
+    ULONG                           Ipv6Metric;
+    IF_LUID                         Luid;
+    SOCKET_ADDRESS                  Dhcpv4Server;
+    NET_IF_COMPARTMENT_ID           CompartmentId;
+    NET_IF_NETWORK_GUID             NetworkGuid;
+    NET_IF_CONNECTION_TYPE          ConnectionType;
+    TUNNEL_TYPE                     TunnelType;
+    SOCKET_ADDRESS                  Dhcpv6Server;
+    BYTE                            Dhcpv6ClientDuid[MAX_DHCPV6_DUID_LENGTH];
+    ULONG                           Dhcpv6ClientDuidLength;
+    ULONG                           Dhcpv6Iaid;
+    PIP_ADAPTER_DNS_SUFFIX          FirstDnsSuffix;
+} IP_ADAPTER_ADDRESSES_LH, *PIP_ADAPTER_ADDRESSES_LH;
+
+typedef struct _IP_ADAPTER_ADDRESSES_XP {
+    union {
+        ULONGLONG Alignment;
+        struct {
+            ULONG Length;
+            DWORD IfIndex;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    struct _IP_ADAPTER_ADDRESSES_XP *Next;
+    PCHAR                           AdapterName;
+    PIP_ADAPTER_UNICAST_ADDRESS     FirstUnicastAddress;
+    PIP_ADAPTER_ANYCAST_ADDRESS     FirstAnycastAddress;
+    PIP_ADAPTER_MULTICAST_ADDRESS   FirstMulticastAddress;
+    PIP_ADAPTER_DNS_SERVER_ADDRESS  FirstDnsServerAddress;
+    PWCHAR                          DnsSuffix;
+    PWCHAR                          Description;
+    PWCHAR                          FriendlyName;
+    BYTE                            PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD                           PhysicalAddressLength;
+    DWORD                           Flags;
+    DWORD                           Mtu;
+    DWORD                           IfType;
+    IF_OPER_STATUS                  OperStatus;
+    DWORD                           Ipv6IfIndex;
+    DWORD                           ZoneIndices[16];
+    PIP_ADAPTER_PREFIX              FirstPrefix;
+} IP_ADAPTER_ADDRESSES_XP, *PIP_ADAPTER_ADDRESSES_XP;
+
+/* ReactOS */
+#if (NTDDI_VERSION >= NTDDI_VISTA)
+typedef IP_ADAPTER_ADDRESSES_LH IP_ADAPTER_ADDRESSES;
+typedef IP_ADAPTER_ADDRESSES_LH *PIP_ADAPTER_ADDRESSES;
+#else
+typedef IP_ADAPTER_ADDRESSES_XP IP_ADAPTER_ADDRESSES;
+typedef IP_ADAPTER_ADDRESSES_XP *PIP_ADAPTER_ADDRESSES;
 #endif
 
-#ifdef __cplusplus
-}
-#endif
-#endif /* _IPTYPES_H */
+#define GAA_FLAG_SKIP_UNICAST                0x00000001
+#define GAA_FLAG_SKIP_ANYCAST                0x00000002
+#define GAA_FLAG_SKIP_MULTICAST              0x00000004
+#define GAA_FLAG_SKIP_DNS_SERVER             0x00000008
+#define GAA_FLAG_INCLUDE_PREFIX              0x00000010
+#define GAA_FLAG_SKIP_FRIENDLY_NAME          0x00000020
+#define GAA_FLAG_INCLUDE_WINS_INFO           0x00000040
+#define GAA_FLAG_INCLUDE_ALL_GATEWAYS        0x00000080
+#define GAA_FLAG_INCLUDE_ALL_INTERFACES      0x00000100
+#define GAA_FLAG_INCLUDE_ALL_COMPARTMENTS    0x00000200
+#define GAA_FLAG_INCLUDE_TUNNEL_BINDINGORDER 0x00000400
+
+#endif /* _WINSOCK2API_ */
+
+#endif /* WINE_IPTYPES_H_*/
