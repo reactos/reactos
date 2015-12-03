5389fc942ec34a798ffb8e1ac843924c9cf23df4
Daniel Reimer <reimer.daniel@freenet.de>
2015-12-03 20:26:56 +0000

    [RAPPS] rapps-database does not update automatically because of broken build scripts by reactosfanboy CORE-10633 #resolve #comment Fixed by removing the copies completely and updating the ones in rapps. Not the best solution, but it works for the update task.
    svn path=/trunk/; revision=70249

--- reactos/base/applications/rapps_new/CreateCabFile.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-cd rapps
-mkdir utf16
-for i in $(find -type f); do
-  ../../../host-tools/tools/utf16le $i utf16/$i
-done
-cd ..
-../../host-tools/tools/cabman/cabman -M mszip -S rappmgr.cab rapps/utf16/*.txt
-rm -r rapps/uft16
