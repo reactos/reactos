747395cee4edd42c7a40da61ed986f1c50950c11
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2015-12-12 21:21:28 +0000

    [CLIPBRD]: SVN failure again: for some reason this file was not added to the code when applying the patch. See r70331. CORE-10679
    svn path=/trunk/; revision=70332

---
+++ reactos/base/applications/clipbrd/scrollutils.h
@@ -0,0 +1,21 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS Clipboard Viewer
+ * FILE:            base/applications/clipbrd/scrollutils.h
+ * PURPOSE:         Scrolling releated helper functions.
+ * PROGRAMMERS:     Ricardo Hanke
+ */
+
+typedef struct _SCROLLSTATE
+{
+    int CurrentX;
+    int CurrentY;
+    int MaxX;
+    int MaxY;
+} SCROLLSTATE, *LPSCROLLSTATE;
+
+void HandleKeyboardScrollEvents(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void HandleHorizontalScrollEvents(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LPSCROLLSTATE state);
+void HandleVerticalScrollEvents(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LPSCROLLSTATE state);
+void UpdateWindowScrollState(HWND hWnd, HBITMAP hBmp, LPSCROLLSTATE lpState);
+BOOL ScrollBlt(PAINTSTRUCT ps, HBITMAP hBmp, SCROLLSTATE state);
