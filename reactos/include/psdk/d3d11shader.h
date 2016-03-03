5d4b069a8bdb2883e9974d53e60794f22178fbdd
Amine Khaldi <amine.khaldi@reactos.org>
2016-03-03 13:25:26 +0000

    [PSDK] Update d3d11shader.h. CORE-10912
    svn path=/trunk/; revision=70873

--- reactos/include/psdk/d3d11shader.h
+++ reactos/include/psdk/d3d11shader.h
@@ -35,7 +35,7 @@ typedef D3D_TESSELLATOR_OUTPUT_PRIMITIVE D3D11_TESSELLATOR_OUTPUT_PRIMITIVE;
 typedef struct _D3D11_SHADER_DESC
 {
     UINT Version;
-    LPCSTR Creator;
+    const char *Creator;
     UINT Flags;
     UINT ConstantBuffers;
     UINT BoundResources;
@@ -76,11 +76,11 @@ typedef struct _D3D11_SHADER_DESC
 
 typedef struct _D3D11_SHADER_VARIABLE_DESC
 {
-    LPCSTR Name;
+    const char *Name;
     UINT StartOffset;
     UINT Size;
     UINT uFlags;
-    LPVOID DefaultValue;
+    void *DefaultValue;
     UINT StartTexture;
     UINT TextureSize;
     UINT StartSampler;
@@ -96,12 +96,12 @@ typedef struct _D3D11_SHADER_TYPE_DESC
     UINT Elements;
     UINT Members;
     UINT Offset;
-    LPCSTR Name;
+    const char *Name;
 } D3D11_SHADER_TYPE_DESC;
 
 typedef struct _D3D11_SHADER_BUFFER_DESC
 {
-    LPCSTR Name;
+    const char *Name;
     D3D_CBUFFER_TYPE Type;
     UINT Variables;
     UINT Size;
@@ -110,7 +110,7 @@ typedef struct _D3D11_SHADER_BUFFER_DESC
 
 typedef struct _D3D11_SHADER_INPUT_BIND_DESC
 {
-    LPCSTR Name;
+    const char *Name;
     D3D_SHADER_INPUT_TYPE Type;
     UINT BindPoint;
     UINT BindCount;
@@ -122,7 +122,7 @@ typedef struct _D3D11_SHADER_INPUT_BIND_DESC
 
 typedef struct _D3D11_SIGNATURE_PARAMETER_DESC
 {
-    LPCSTR SemanticName;
+    const char *SemanticName;
     UINT SemanticIndex;
     UINT Register;
     D3D_NAME SystemValueType;
@@ -139,8 +139,8 @@ DECLARE_INTERFACE(ID3D11ShaderReflectionType)
 {
     STDMETHOD(GetDesc)(THIS_ D3D11_SHADER_TYPE_DESC *desc) PURE;
     STDMETHOD_(struct ID3D11ShaderReflectionType *, GetMemberTypeByIndex)(THIS_ UINT index) PURE;
-    STDMETHOD_(struct ID3D11ShaderReflectionType *, GetMemberTypeByName)(THIS_ LPCSTR name) PURE;
-    STDMETHOD_(LPCSTR, GetMemberTypeName)(THIS_ UINT index) PURE;
+    STDMETHOD_(struct ID3D11ShaderReflectionType *, GetMemberTypeByName)(THIS_ const char *name) PURE;
+    STDMETHOD_(const char *, GetMemberTypeName)(THIS_ UINT index) PURE;
     STDMETHOD(IsEqual)(THIS_ struct ID3D11ShaderReflectionType *type) PURE;
     STDMETHOD_(struct ID3D11ShaderReflectionType *, GetSubType)(THIS) PURE;
     STDMETHOD_(struct ID3D11ShaderReflectionType *, GetBaseClass)(THIS) PURE;
@@ -170,7 +170,7 @@ DECLARE_INTERFACE(ID3D11ShaderReflectionConstantBuffer)
 {
     STDMETHOD(GetDesc)(THIS_ D3D11_SHADER_BUFFER_DESC *desc) PURE;
     STDMETHOD_(struct ID3D11ShaderReflectionVariable *, GetVariableByIndex)(THIS_ UINT index) PURE;
-    STDMETHOD_(struct ID3D11ShaderReflectionVariable *, GetVariableByName)(THIS_ LPCSTR name) PURE;
+    STDMETHOD_(struct ID3D11ShaderReflectionVariable *, GetVariableByName)(THIS_ const char *name) PURE;
 };
 #undef INTERFACE
 
@@ -180,19 +180,19 @@ DEFINE_GUID(IID_ID3D11ShaderReflection, 0x0a233719, 0x3960, 0x4578, 0x9d, 0x7c, 
 DECLARE_INTERFACE_(ID3D11ShaderReflection, IUnknown)
 {
     /* IUnknown methods */
-    STDMETHOD(QueryInterface)(THIS_ REFIID riid, LPVOID *object) PURE;
+    STDMETHOD(QueryInterface)(THIS_ REFIID riid, void **out) PURE;
     STDMETHOD_(ULONG, AddRef)(THIS) PURE;
     STDMETHOD_(ULONG, Release)(THIS) PURE;
     /* ID3D11ShaderReflection methods */
     STDMETHOD(GetDesc)(THIS_ D3D11_SHADER_DESC *desc) PURE;
     STDMETHOD_(struct ID3D11ShaderReflectionConstantBuffer *, GetConstantBufferByIndex)(THIS_ UINT index) PURE;
-    STDMETHOD_(struct ID3D11ShaderReflectionConstantBuffer *, GetConstantBufferByName)(THIS_ LPCSTR name) PURE;
+    STDMETHOD_(struct ID3D11ShaderReflectionConstantBuffer *, GetConstantBufferByName)(THIS_ const char *name) PURE;
     STDMETHOD(GetResourceBindingDesc)(THIS_ UINT index, D3D11_SHADER_INPUT_BIND_DESC *desc) PURE;
     STDMETHOD(GetInputParameterDesc)(THIS_ UINT index, D3D11_SIGNATURE_PARAMETER_DESC *desc) PURE;
     STDMETHOD(GetOutputParameterDesc)(THIS_ UINT index, D3D11_SIGNATURE_PARAMETER_DESC *desc) PURE;
     STDMETHOD(GetPatchConstantParameterDesc)(THIS_ UINT index, D3D11_SIGNATURE_PARAMETER_DESC *desc) PURE;
-    STDMETHOD_(struct ID3D11ShaderReflectionVariable *, GetVariableByName)(THIS_ LPCSTR name) PURE;
-    STDMETHOD(GetResourceBindingDescByName)(THIS_ LPCSTR name, D3D11_SHADER_INPUT_BIND_DESC *desc) PURE;
+    STDMETHOD_(struct ID3D11ShaderReflectionVariable *, GetVariableByName)(THIS_ const char *name) PURE;
+    STDMETHOD(GetResourceBindingDescByName)(THIS_ const char *name, D3D11_SHADER_INPUT_BIND_DESC *desc) PURE;
     STDMETHOD_(UINT, GetMovInstructionCount)(THIS) PURE;
     STDMETHOD_(UINT, GetMovcInstructionCount)(THIS) PURE;
     STDMETHOD_(UINT, GetConversionInstructionCount)(THIS) PURE;
