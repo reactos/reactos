c3677ca74033c8497bf56fb85d3749c361a39ae3
Amine Khaldi <amine.khaldi@reactos.org>
2016-03-03 13:36:26 +0000

    [MAPI32] Sync with Wine Staging 1.9.4. CORE-10912
    svn path=/trunk/; revision=70874

--- reactos/dll/win32/mapi32/imalloc.c
+++ reactos/dll/win32/mapi32/imalloc.c
@@ -98,7 +98,7 @@ static ULONG WINAPI IMAPIMalloc_fnRelease(LPMALLOC iface)
  */
 static LPVOID WINAPI IMAPIMalloc_fnAlloc(LPMALLOC iface, SIZE_T cb)
 {
-    TRACE("(%p)->(%d)\n", iface, cb);
+    TRACE("(%p)->(%ld)\n", iface, cb);
 
     return LocalAlloc(LMEM_FIXED, cb);
 }
@@ -108,7 +108,7 @@ static LPVOID WINAPI IMAPIMalloc_fnAlloc(LPMALLOC iface, SIZE_T cb)
  */
 static LPVOID WINAPI IMAPIMalloc_fnRealloc(LPMALLOC iface, LPVOID pv, SIZE_T cb)
 {
-    TRACE("(%p)->(%p, %d)\n", iface, pv, cb);
+    TRACE("(%p)->(%p, %ld)\n", iface, pv, cb);
 
     if (!pv)
         return LocalAlloc(LMEM_FIXED, cb);
