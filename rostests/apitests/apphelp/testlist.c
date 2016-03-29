001dc229d5e34bcf1ed9ddf3cef78f02d907f7e9
Thomas Faber <thomas.faber@reactos.org>
2016-03-29 08:55:40 +0000

    [APPHELP_APITEST] - Add tests for the apphelp SDB compatibility layer. Patch by Mark Jansen. CORE-10367
    svn path=/trunk/; revision=71077

---
+++ rostests/apitests/apphelp/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_layerapi(void);
+
+const struct test winetest_testlist[] =
+{
+    { "layerapi", func_layerapi },
+    { 0, 0 }
+};
