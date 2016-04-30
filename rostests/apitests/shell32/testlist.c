c1a1365490ad61f4776b8af077a3a410b61d4c21
Thomas Faber <thomas.faber@reactos.org>
2016-04-30 15:06:57 +0000

    [SHELL32_APITEST] - Add a test for SHParseDisplayName CORE-10434
    svn path=/trunk/; revision=71216

--- rostests/apitests/shell32/testlist.c
+++ rostests/apitests/shell32/testlist.c
@@ -6,11 +6,13 @@
 extern void func_CMyComputer(void);
 extern void func_CShellDesktop(void);
 extern void func_menu(void);
+extern void func_SHParseDisplayName(void);
 
 const struct test winetest_testlist[] =
 {
     { "CMyComputer", func_CMyComputer },
     { "CShellDesktop", func_CShellDesktop },
     { "menu", func_menu },
+    { "SHParseDisplayName", func_SHParseDisplayName },
     { 0, 0 }
 };
