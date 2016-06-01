c7a10e3a12509862384c185bcfaf11bcd720a959
Giannis Adamopoulos <gadamopoulos@reactos.org>
2016-06-01 20:13:27 +0000

    [BROWSEUI] - Remove the proxy implementation of CExplorerBand. Add IDropTarget and IDropSource interfaces.
    svn path=/trunk/; revision=71491

--- reactos/dll/win32/browseui/explorerband.h
+++ reactos/dll/win32/browseui/explorerband.h
@@ -1,121 +1,11 @@
 #pragma once
 
-#if 0
-// Is this a CToolsBand that implements a toolbar?
-class CToolBand :
-    public CComObjectRootEx<CComMultiThreadModelNoCS>,
-    public IDeskBand,
-    public IObjectWithSite,
-    public IInputObject,
-    public IPersistStream,
-    public IOleCommandTarget,
-    public IServiceProvider
-{
-public:
-    // *** IOleWindow methods ***
-    virtual HRESULT STDMETHODCALLTYPE GetWindow(HWND *lphwnd);
-    virtual HRESULT STDMETHODCALLTYPE ContextSensitiveHelp(BOOL fEnterMode);
-
-    // *** IDockingWindow methods ***
-    virtual HRESULT STDMETHODCALLTYPE CloseDW(DWORD dwReserved);
-    virtual HRESULT STDMETHODCALLTYPE ResizeBorderDW(const RECT *prcBorder, IUnknown *punkToolbarSite, BOOL fReserved);
-    virtual HRESULT STDMETHODCALLTYPE ShowDW(BOOL fShow);
-
-    // *** IDeskBand methods ***
-    virtual HRESULT STDMETHODCALLTYPE GetBandInfo(DWORD dwBandID, DWORD dwViewMode, DESKBANDINFO *pdbi);
-
-    // *** IObjectWithSite methods ***
-    virtual HRESULT STDMETHODCALLTYPE SetSite(IUnknown *pUnkSite);
-    virtual HRESULT STDMETHODCALLTYPE GetSite(REFIID riid, void **ppvSite);
-
-    // *** IOleCommandTarget methods ***
-    virtual HRESULT STDMETHODCALLTYPE QueryStatus(const GUID *pguidCmdGroup, ULONG cCmds, OLECMD prgCmds [], OLECMDTEXT *pCmdText);
-    virtual HRESULT STDMETHODCALLTYPE Exec(const GUID *pguidCmdGroup, DWORD nCmdID, DWORD nCmdexecopt, VARIANT *pvaIn, VARIANT *pvaOut);
-
-    // *** IServiceProvider methods ***
-    virtual HRESULT STDMETHODCALLTYPE QueryService(REFGUID guidService, REFIID riid, void **ppvObject);
-
-    // *** IInputObject methods ***
-    virtual HRESULT STDMETHODCALLTYPE UIActivateIO(BOOL fActivate, LPMSG lpMsg);
-    virtual HRESULT STDMETHODCALLTYPE HasFocusIO();
-    virtual HRESULT STDMETHODCALLTYPE TranslateAcceleratorIO(LPMSG lpMsg);
-
-    // *** IPersist methods ***
-    virtual HRESULT STDMETHODCALLTYPE GetClassID(CLSID *pClassID);
-
-    // *** IPersistStream methods ***
-    virtual HRESULT STDMETHODCALLTYPE IsDirty();
-    virtual HRESULT STDMETHODCALLTYPE Load(IStream *pStm);
-    virtual HRESULT STDMETHODCALLTYPE Save(IStream *pStm, BOOL fClearDirty);
-    virtual HRESULT STDMETHODCALLTYPE GetSizeMax(ULARGE_INTEGER *pcbSize);
-
-    BEGIN_COM_MAP(CToolBand)
-        COM_INTERFACE_ENTRY2_IID(IID_IOleWindow, IOleWindow, IDeskBand)
-        COM_INTERFACE_ENTRY2_IID(IID_IDockingWindow, IDockingWindow, IDeskBand)
-        COM_INTERFACE_ENTRY_IID(IID_IDeskBand, IDeskBand)
-        COM_INTERFACE_ENTRY_IID(IID_IObjectWithSite, IObjectWithSite)
-        COM_INTERFACE_ENTRY_IID(IID_IOleCommandTarget, IOleCommandTarget)
-        COM_INTERFACE_ENTRY_IID(IID_IServiceProvider, IServiceProvider)
-        COM_INTERFACE_ENTRY_IID(IID_IInputObject, IInputObject)
-        COM_INTERFACE_ENTRY2_IID(IID_IPersist, IPersist, IPersistStream)
-        COM_INTERFACE_ENTRY_IID(IID_IPersistStream, IPersistStream)
-    END_COM_MAP()
-};
-
-class CNSCBand :
-    public CToolBand,
-    public IDeskBand,
-    public IObjectWithSite,
-    public IInputObject,
-    public IPersistStream,
-    public IOleCommandTarget,
-    public IServiceProvider,
-    public IBandNavigate,
-    public IWinEventHandler,
-    public INamespaceProxy
-{
-public:
-    // *** IWinEventHandler methods ***
-    virtual HRESULT STDMETHODCALLTYPE OnWinEvent(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT *theResult);
-    virtual HRESULT STDMETHODCALLTYPE IsWindowOwner(HWND hWnd);
-
-    // *** IBandNavigate methods ***
-    virtual HRESULT STDMETHODCALLTYPE Select(long paramC);
-
-    // *** INamespaceProxy ***
-    virtual HRESULT STDMETHODCALLTYPE GetNavigateTarget(long paramC, long param10, long param14);
-    virtual HRESULT STDMETHODCALLTYPE Invoke(long paramC);
-    virtual HRESULT STDMETHODCALLTYPE OnSelectionChanged(long paramC);
-    virtual HRESULT STDMETHODCALLTYPE RefreshFlags(long paramC, long param10, long param14);
-    virtual HRESULT STDMETHODCALLTYPE CacheItem(long paramC);
-
-    BEGIN_COM_MAP(CNSCBand)
-        COM_INTERFACE_ENTRY_IID(IID_IWinEventHandler, IWinEventHandler)
-        COM_INTERFACE_ENTRY_IID(IID_IBandNavigate, IBandNavigate)
-        COM_INTERFACE_ENTRY_IID(IID_INamespaceProxy, INamespaceProxy)
-#if 0
-        COM_INTERFACE_ENTRY_CHAIN(CToolBand)
-#else
-        COM_INTERFACE_ENTRY2_IID(IID_IOleWindow, IOleWindow, IDeskBand)
-        COM_INTERFACE_ENTRY2_IID(IID_IDockingWindow, IDockingWindow, IDeskBand)
-        COM_INTERFACE_ENTRY_IID(IID_IInputObject, IInputObject)
-        COM_INTERFACE_ENTRY_IID(IID_IOleCommandTarget, IOleCommandTarget)
-        COM_INTERFACE_ENTRY_IID(IID_IServiceProvider, IServiceProvider)
-        COM_INTERFACE_ENTRY_IID(IID_IPersistStream, IPersistStream)
-        COM_INTERFACE_ENTRY2_IID(IID_IPersist, IPersist, IPersistStream)
-        COM_INTERFACE_ENTRY_IID(IID_IObjectWithSite, IObjectWithSite)
-#endif
-    END_COM_MAP()
-};
-#endif
+#define WM_USER_SHELLEVENT WM_USER+88
+#define WM_USER_FOLDEREVENT WM_USER+88
 
 class CExplorerBand :
     public CComCoClass<CExplorerBand, &CLSID_ExplorerBand>,
-#if 0
-    public CNSCBand,
-#else
     public CComObjectRootEx<CComMultiThreadModelNoCS>,
-#endif
     public IDeskBand,
     public IObjectWithSite,
     public IInputObject,
@@ -124,22 +14,22 @@ class CExplorerBand :
     public IServiceProvider,
     public IBandNavigate,
     public IWinEventHandler,
-    public INamespaceProxy
+    public INamespaceProxy,
+    public IDispatch,
+    public IDropSource,
+    public IDropTarget,
+    public CWindowImpl<CExplorerBand, CWindow, CControlWinTraits>
 {
-    CComPtr<IUnknown> m_internalBand;
 
-    CComPtr<IDeskBand> m_internalDeskBand;
-    CComPtr<IObjectWithSite> m_internalObjectWithSite;
-    CComPtr<IInputObject> m_internalInputObject;
-    CComPtr<IPersistStream> m_internalPersistStream;
-    CComPtr<IOleCommandTarget> m_internalOleCommandTarget;
-    CComPtr<IServiceProvider> m_internalServiceProvider;
-    CComPtr<IBandNavigate> m_internalBandNavigate;
-    CComPtr<IWinEventHandler> m_internalWinEventHandler;
-    CComPtr<INamespaceProxy> m_internalNamespaceProxy;
-    CComPtr<IDispatch> m_internalDispatch;
+private:
+    class NodeInfo
+    {
+    public:
+        LPITEMIDLIST absolutePidl;
+        LPITEMIDLIST relativePidl;
+        BOOL expanded;
+    };
 
-    bool m_OnWinEventShown;
 public:
     CExplorerBand();
     virtual ~CExplorerBand();
@@ -200,6 +90,16 @@ public:
     virtual HRESULT STDMETHODCALLTYPE GetTypeInfo(UINT iTInfo, LCID lcid, ITypeInfo **ppTInfo);
     virtual HRESULT STDMETHODCALLTYPE GetIDsOfNames(REFIID riid, LPOLESTR *rgszNames, UINT cNames, LCID lcid, DISPID *rgDispId);
     virtual HRESULT STDMETHODCALLTYPE Invoke(DISPID dispIdMember, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS *pDispParams, VARIANT *pVarResult, EXCEPINFO *pExcepInfo, UINT *puArgErr);
+    
+    // *** IDropTarget methods ***
+    virtual HRESULT STDMETHODCALLTYPE DragEnter(IDataObject *pObj, DWORD glfKeyState, POINTL pt, DWORD *pdwEffect);
+    virtual HRESULT STDMETHODCALLTYPE DragOver(DWORD glfKeyState, POINTL pt, DWORD *pdwEffect);
+    virtual HRESULT STDMETHODCALLTYPE DragLeave();
+    virtual HRESULT STDMETHODCALLTYPE Drop(IDataObject *pObj, DWORD glfKeyState, POINTL pt, DWORD *pdwEffect);
+
+    // *** IDropSource methods ***
+    virtual HRESULT STDMETHODCALLTYPE QueryContinueDrag(BOOL fEscapePressed, DWORD grfKeyState);
+    virtual HRESULT STDMETHODCALLTYPE GiveFeedback(DWORD dwEffect);
 
     DECLARE_REGISTRY_RESOURCEID(IDR_EXPLORERBAND)
     DECLARE_NOT_AGGREGATABLE(CExplorerBand)
@@ -207,7 +107,7 @@ public:
     DECLARE_PROTECT_FINAL_CONSTRUCT()
 
     BEGIN_COM_MAP(CExplorerBand)
-        COM_INTERFACE_ENTRY_IID(IID_IDispatch, IDeskBand)
+        COM_INTERFACE_ENTRY_IID(IID_IDispatch, IDispatch)
         COM_INTERFACE_ENTRY_IID(IID_IWinEventHandler, IWinEventHandler)
         COM_INTERFACE_ENTRY_IID(IID_IBandNavigate, IBandNavigate)
         COM_INTERFACE_ENTRY_IID(IID_INamespaceProxy, INamespaceProxy)
@@ -221,6 +121,9 @@ public:
         COM_INTERFACE_ENTRY2_IID(IID_IPersist, IPersist, IPersistStream)
         COM_INTERFACE_ENTRY_IID(IID_IPersistStream, IPersistStream)
     END_COM_MAP()
+
+    BEGIN_MSG_MAP(CExplorerBand)
+    END_MSG_MAP()
 };
 
 extern "C"
