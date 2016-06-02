add8172c718714c9a21e0fd66f22f9d3c3e233d2
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2016-06-02 21:38:47 +0000

    [SNDREC32]: Use a sunken edge for the waveform viewer.
    svn path=/trunk/; revision=71503

--- reactos/base/applications/sndrec32/sndrec32.cpp
+++ reactos/base/applications/sndrec32/sndrec32.cpp
@@ -317,17 +317,18 @@ InitInstance_wave(HWND f,
 
     hInst = hInstance;
 
-    hWnd = CreateWindow(TEXT("sndrec32_wave"),
-                        TEXT(""),
-                        WS_DLGFRAME | WS_VISIBLE | WS_CHILD,
-                        WAVEBAR_X,
-                        WAVEBAR_Y,
-                        WAVEBAR_CX,
-                        WAVEBAR_CY,
-                        f,
-                        (HMENU)8,
-                        hInstance,
-                        0);
+    hWnd = CreateWindowEx(WS_EX_STATICEDGE,
+                          TEXT("sndrec32_wave"),
+                          TEXT(""),
+                          WS_VISIBLE | WS_CHILD,
+                          WAVEBAR_X,
+                          WAVEBAR_Y,
+                          WAVEBAR_CX,
+                          WAVEBAR_CY,
+                          f,
+                          (HMENU)8,
+                          hInstance,
+                          0);
 
     if (!hWnd )
     {
@@ -363,6 +364,7 @@ WndProc_wave(HWND hWnd,
         case WM_CREATE:
             GetClientRect(hWnd, &cli);
             break;
+
         case WM_PAINT:
             /* Initialize hdc objects */
             hdc = BeginPaint(hWnd, &ps);
@@ -420,6 +422,7 @@ WndProc_wave(HWND hWnd,
 
         case WM_USER:
             break;
+
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
     }
