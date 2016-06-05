28ad1238095b09dc3bb912ca4f96c2e20f978ea8
Amine Khaldi <amine.khaldi@reactos.org>
2016-06-05 19:29:11 +0000

    [MCIAVI32] Sync with Wine Staging 1.9.11. CORE-11368
    svn path=/trunk/; revision=71560

--- reactos/dll/win32/mciavi32/mmoutput.c
+++ reactos/dll/win32/mciavi32/mmoutput.c
@@ -226,14 +226,16 @@ static BOOL	MCIAVI_AddFrame(WINE_MCIAVI* wma, LPMMCKINFO mmck,
 	      alb->numAudioBlocks, mmck->cksize);
 	if (wma->lpWaveFormat) {
 	    if (alb->numAudioBlocks >= alb->numAudioAllocated) {
-		alb->numAudioAllocated += 32;
-		if (!wma->lpAudioIndex)
-		    wma->lpAudioIndex = HeapAlloc(GetProcessHeap(), 0,
-						  alb->numAudioAllocated * sizeof(struct MMIOPos));
-		else
-		    wma->lpAudioIndex = HeapReAlloc(GetProcessHeap(), 0, wma->lpAudioIndex,
-						    alb->numAudioAllocated * sizeof(struct MMIOPos));
-		if (!wma->lpAudioIndex) return FALSE;
+                DWORD newsize = alb->numAudioAllocated + 32;
+                struct MMIOPos* newindex;
+
+                if (!wma->lpAudioIndex)
+                    newindex = HeapAlloc(GetProcessHeap(), 0, newsize * sizeof(struct MMIOPos));
+                else
+                    newindex = HeapReAlloc(GetProcessHeap(), 0, wma->lpAudioIndex, newsize * sizeof(struct MMIOPos));
+                if (!newindex) return FALSE;
+                alb->numAudioAllocated = newsize;
+                wma->lpAudioIndex = newindex;
 	    }
 	    wma->lpAudioIndex[alb->numAudioBlocks].dwOffset = mmck->dwDataOffset;
 	    wma->lpAudioIndex[alb->numAudioBlocks].dwSize = mmck->cksize;
