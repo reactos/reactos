85b40f7b30c20e699f362f06276b9fd5ab29e1cd
Timo Kreuzer <timo.kreuzer@reactos.org>
2016-06-17 03:02:23 +0300

    [WINESYNC] ucrtbase: Implement _calloc_base.
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id fdc57d497bb305e90680c3b450fa172042fd79cd by Alex Henrie <alexhenrie24@gmail.com>

--- sdk/lib/crt/wine/heap.c
+++ sdk/lib/crt/wine/heap.c
@@ -430,6 +430,14 @@ void* CDECL MSVCRT_calloc(MSVCRT_size_t count, MSVCRT_size_t size)
 }
 
 /*********************************************************************
+ *		_calloc_base (UCRTBASE.@)
+ */
+void* CDECL _calloc_base(MSVCRT_size_t count, MSVCRT_size_t size)
+{
+  return MSVCRT_calloc(count, size);
+}
+
+/*********************************************************************
  *		free (MSVCRT.@)
  */
 void CDECL MSVCRT_free(void* ptr)
