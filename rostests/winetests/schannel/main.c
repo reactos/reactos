6e323c186991e679107556e5cdb7112e648dc0e8
Amine Khaldi <amine.khaldi@reactos.org>
2016-07-03 11:46:08 +0000

    [SCHANNEL_WINETEST] Sync with Wine Staging 1.9.11. CORE-11368
    svn path=/trunk/; revision=71782

--- rostests/winetests/schannel/main.c
+++ rostests/winetests/schannel/main.c
@@ -43,7 +43,9 @@
     QueryMetaData)
 #define SECPKG_FUNCTION_TABLE_SIZE_5 FIELD_OFFSET(SECPKG_FUNCTION_TABLE, \
     ValidateTargetInfo)
-#define SECPKG_FUNCTION_TABLE_SIZE_6 sizeof(SECPKG_FUNCTION_TABLE)
+#define SECPKG_FUNCTION_TABLE_SIZE_6 FIELD_OFFSET(SECPKG_FUNCTION_TABLE, \
+    PostLogonUser)
+#define SECPKG_FUNCTION_TABLE_SIZE_7 sizeof(SECPKG_FUNCTION_TABLE)
 
 #define LSA_BASE_CAPS ( \
     SECPKG_FLAG_INTEGRITY         | \
@@ -152,6 +154,8 @@ static PSECPKG_FUNCTION_TABLE getNextSecPkgTable(PSECPKG_FUNCTION_TABLE pTable,
         size = SECPKG_FUNCTION_TABLE_SIZE_5;
     else if (Version == SECPKG_INTERFACE_VERSION_6)
         size = SECPKG_FUNCTION_TABLE_SIZE_6;
+    else if (Version == SECPKG_INTERFACE_VERSION_7)
+        size = SECPKG_FUNCTION_TABLE_SIZE_7;
     else {
         ok(FALSE, "Unknown package version 0x%x\n", Version);
         return NULL;
