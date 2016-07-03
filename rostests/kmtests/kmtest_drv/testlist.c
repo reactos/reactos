31ac33cd4383cc6fa601ce1b1748a8b98009a1cd
Thomas Faber <thomas.faber@reactos.org>
2016-07-03 09:23:37 +0000

    [KMTESTS] - Addendum to r71770 CORE-11509
    svn path=/trunk/; revision=71773

--- rostests/kmtests/kmtest_drv/testlist.c
+++ rostests/kmtests/kmtest_drv/testlist.c
@@ -54,6 +54,7 @@ KMT_TESTFUNC Test_NpfsReadWrite;
 KMT_TESTFUNC Test_NpfsVolumeInfo;
 KMT_TESTFUNC Test_ObHandle;
 KMT_TESTFUNC Test_ObReference;
+KMT_TESTFUNC Test_ObSymbolicLink;
 KMT_TESTFUNC Test_ObType;
 KMT_TESTFUNC Test_ObTypeClean;
 KMT_TESTFUNC Test_ObTypeNoClean;
@@ -122,6 +123,7 @@ const KMT_TEST TestList[] =
     { "NpfsVolumeInfo",                     Test_NpfsVolumeInfo },
     { "ObHandle",                           Test_ObHandle },
     { "ObReference",                        Test_ObReference },
+    { "ObSymbolicLink",                     Test_ObSymbolicLink },
     { "ObType",                             Test_ObType },
     { "-ObTypeClean",                       Test_ObTypeClean },
     { "-ObTypeNoClean",                     Test_ObTypeNoClean },
