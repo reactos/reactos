705846cb258168f9cd319eebaa98c9ff8d1273a5
Ged Murphy <gedmurphy@reactos.org>
2016-08-09 21:23:40 +0000

    [FLTLIB_APITEST] - Create a skeleton test project for fltlib.dll - Not yet part of the build
    svn path=/trunk/; revision=72173

---
+++ rostests/apitests/fltlib/load.c
@@ -0,0 +1,44 @@
+/*
+* FltLib related functions tests
+*
+* Copyright 2016 Ged Murphy (ged.murphy@reactos.org)
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public Licence as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this library; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+#include <apitest.h>
+#include <stdio.h>
+#include <assert.h>
+#include <winuser.h>
+#include <winreg.h>
+#include <winsvc.h>
+
+//
+// Tests for:
+//      FilterLoad
+//      FilterUnload
+//
+
+
+static void test_FIXME(void)
+{
+
+}
+
+
+START_TEST(load)
+{
+    test_FIXME();
+}
