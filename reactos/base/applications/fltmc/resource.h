0b589c107d19989788d17c044c16e048304deda7
Ged Murphy <gedmurphy@reactos.org>
2016-08-09 21:33:53 +0000

    [FLTMC] - Start to implement fltMC - It currently supports loading, unloading and listing loaded filter driver.
    svn path=/trunk/; revision=72174

---
+++ reactos/base/applications/fltmc/resource.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define IDS_USAGE                   0
+#define IDS_USAGE_LOAD              1
+#define IDS_USAGE_UNLOAD            2
+#define IDS_USAGE_FILTERS           3
+
+#define IDS_DISPLAY_FILTERS1        10
+#define IDS_DISPLAY_FILTERS2        11
+
+#define IDS_ERROR_PRIV              20
+#define IDS_ERROR_FILTERS           21
+#define IDS_ERROR_LOAD              22
+#define IDS_ERROR_UNLOAD            23
