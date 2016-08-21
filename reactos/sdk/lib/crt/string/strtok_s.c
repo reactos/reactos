761db77da53d3d5257a6e43a17411a63bce0e37c
Amine Khaldi <amine.khaldi@reactos.org>
2016-08-21 16:30:06 +0000

    [CDMAKE][CTR] Fix strtok_s for empty strings. Part of Wine commit 4fa616c by Bernhard Übelacker.
    svn path=/trunk/; revision=72406

--- reactos/sdk/lib/crt/string/strtok_s.c
+++ reactos/sdk/lib/crt/string/strtok_s.c
@@ -18,7 +18,10 @@ char * CDECL strtok_s(char *str, const char *delim, char **ctx)
     while(*str && strchr(delim, *str))
         str++;
     if(!*str)
+    {
+        *ctx = str;
         return NULL;
+    }
 
     *ctx = str+1;
     while(**ctx && !strchr(delim, **ctx))
--- reactos/sdk/tools/cdmake/cdmake.c
+++ reactos/sdk/tools/cdmake/cdmake.c
@@ -1930,7 +1930,10 @@ char* strtok_s(char *str, const char *delim, char **ctx)
     while (*str && strchr(delim, *str))
         str++;
     if (!*str)
+    {
+        *ctx = str;
         return NULL;
+    }
 
     *ctx = str + 1;
     while (**ctx && !strchr(delim, **ctx))
