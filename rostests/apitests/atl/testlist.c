639a2c725d7b3317fdbad27bc6a7eb5f28e423a8
Mark Jansen <mark.jansen@reactos.org>
2016-09-15 19:41:03 +0000

    [ATL][ATL_APITEST] Implemente CSimpleArray + CSimpleMap. Based on a patch from Katayama Hirofumi MZ. CORE-11946 Most of the code is from Katayama Hirofumi MZ, the placement new / delete is written by me, based on a hint from Giannis.
    svn path=/trunk/; revision=72688

--- rostests/apitests/atl/testlist.c
+++ rostests/apitests/atl/testlist.c
@@ -6,6 +6,8 @@ extern void func_CComBSTR(void);
 extern void func_CComHeapPtr(void);
 extern void func_CImage(void);
 extern void func_CRegKey(void);
+extern void func_CSimpleArray(void);
+extern void func_CSimpleMap(void);
 extern void func_CString(void);
 
 const struct test winetest_testlist[] =
@@ -15,6 +17,8 @@ const struct test winetest_testlist[] =
     { "CComHeapPtr", func_CComHeapPtr },
     { "CImage", func_CImage },
     { "CRegKey", func_CRegKey },
+    { "CSimpleArray", func_CSimpleArray },
+    { "CSimpleMap", func_CSimpleMap },
     { "CString", func_CString },
     { 0, 0 }
 };
