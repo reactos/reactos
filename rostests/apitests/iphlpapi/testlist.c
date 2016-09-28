d0e5925de20fa9f5e504af171bcee59bbc7806ce
Amine Khaldi <amine.khaldi@reactos.org>
2016-09-28 14:27:48 +0000

    [IPHLPAPI_APITEST] Add some tests for GetNetworkParams. By Peter Hater. ROSTESTS-244
    svn path=/trunk/; revision=72836

--- rostests/apitests/iphlpapi/testlist.c
+++ rostests/apitests/iphlpapi/testlist.c
@@ -3,11 +3,13 @@
 #define STANDALONE
 #include <apitest.h>
 
+extern void func_GetNetworkParams(void);
 extern void func_icmp(void);
 extern void func_SendARP(void);
 
 const struct test winetest_testlist[] =
 {
+    { "GetNetworkParams",     func_GetNetworkParams },
     { "icmp",                 func_icmp },
     { "SendARP",              func_SendARP },
 
