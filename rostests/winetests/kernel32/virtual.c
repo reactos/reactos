eb0d4a0a1677c1f89be98cd278cd58323d8abd08
Jérôme Gardou <jerome.gardou@reactos.org>
2016-10-31 11:51:50 +0000

    [KERNEL32_WINETEST]  - Guard page and ATL thunks tests work fine. Enable them. ROSTESTS-155 #resolve
    svn path=/trunk/; revision=73089

--- rostests/winetests/kernel32/virtual.c
+++ rostests/winetests/kernel32/virtual.c
@@ -4166,19 +4166,10 @@ START_TEST(virtual)
 #if defined(__i386__) || defined(__x86_64__)
     test_stack_commit();
 #endif
-#ifdef __i386__
-    if (!winetest_interactive)
-    {
-        skip("ROSTESTS-155: Skipping virtual guard page tests due to Mm assertion failure.\n");
-    }
-    else
-    {
-        test_guard_page();
-        /* The following tests should be executed as a last step, and in exactly this
-         * order, since ATL thunk emulation cannot be enabled anymore on Windows. */
-        test_atl_thunk_emulation( MEM_EXECUTE_OPTION_ENABLE );
-        test_atl_thunk_emulation( MEM_EXECUTE_OPTION_DISABLE );
-        test_atl_thunk_emulation( MEM_EXECUTE_OPTION_DISABLE | MEM_EXECUTE_OPTION_DISABLE_THUNK_EMULATION );
-    }
-#endif
+    test_guard_page();
+    /* The following tests should be executed as a last step, and in exactly this
+     * order, since ATL thunk emulation cannot be enabled anymore on Windows. */
+    test_atl_thunk_emulation( MEM_EXECUTE_OPTION_ENABLE );
+    test_atl_thunk_emulation( MEM_EXECUTE_OPTION_DISABLE );
+    test_atl_thunk_emulation( MEM_EXECUTE_OPTION_DISABLE | MEM_EXECUTE_OPTION_DISABLE_THUNK_EMULATION );
 }
