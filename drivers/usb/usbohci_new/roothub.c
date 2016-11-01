6c3077402e0c4bb056f1e3c572ab8fd2d089dd87
Vadim Galyant <vgal@rambler.ru>
2016-11-02 05:50:53 +0800

    [USBOHCI] Implementation OHCI_RH_ClearFeaturePortSuspendChange().

--- drivers/usb/usbohci_new/roothub.c
+++ drivers/usb/usbohci_new/roothub.c
@@ -401,7 +401,20 @@ NTAPI
 OHCI_RH_ClearFeaturePortSuspendChange(IN PVOID ohciExtension,
                                       IN USHORT Port)
 {
-    DPRINT("OHCI_RH_ClearFeaturePortSuspendChange: UNIMPLEMENTED. FIXME\n");
+    POHCI_EXTENSION OhciExtension;
+    POHCI_OPERATIONAL_REGISTERS OperationalRegs;
+
+    OhciExtension = (POHCI_EXTENSION)ohciExtension;
+
+    DPRINT("OHCI_RH_ClearFeaturePortSuspendChange: OhciExtension - %p, Port - %x\n",
+           OhciExtension,
+           Port);
+
+    OperationalRegs = OhciExtension->OperationalRegs;
+
+    WRITE_REGISTER_ULONG(&OperationalRegs->HcRhPortStatus[Port-1].AsULONG,
+                         0x40000);
+
     return 0;
 }
 
