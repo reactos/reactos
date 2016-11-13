536ef6886081d3f41a02ac387f1f5c57510797bb
vgalnt <vgal@rambler.ru>
2016-11-14 05:49:13 +0800

    [USBEHCI] Implementation EHCI_RH_ClearFeaturePortEnableChange().

--- drivers/usb/usbehci_new/roothub.c
+++ drivers/usb/usbehci_new/roothub.c
@@ -446,7 +446,24 @@ NTAPI
 EHCI_RH_ClearFeaturePortEnableChange(IN PVOID ehciExtension,
                                      IN USHORT Port)
 {
-    DPRINT("EHCI_RH_ClearFeaturePortEnableChange: UNIMPLEMENTED. FIXME\n");
+    PEHCI_EXTENSION EhciExtension;
+    PULONG PortStatusReg;
+    EHCI_PORT_STATUS_CONTROL PortSC;
+
+    DPRINT("EHCI_RH_ClearFeaturePortEnableChange: Port - %p\n", Port);
+
+    EhciExtension = (PEHCI_EXTENSION)ehciExtension;
+
+    PortStatusReg = (EhciExtension->OperationalRegs + EHCI_PORTSC) + (Port - 1);
+
+    PortSC.AsULONG = READ_REGISTER_ULONG(PortStatusReg);
+
+    PortSC.ConnectStatusChange = 0;
+    PortSC.OverCurrentChange = 0;
+    PortSC.PortEnableDisableChange = 1;
+
+    WRITE_REGISTER_ULONG(PortStatusReg, PortSC.AsULONG);
+
     return 0;
 }
 
