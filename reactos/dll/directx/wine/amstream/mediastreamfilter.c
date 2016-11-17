e5a3cf92da102df1128668cac28ab0238e5cd2a4
Amine Khaldi <amine.khaldi@reactos.org>
2016-11-17 21:43:18 +0000

    [AMSTREAM] Sync with Wine Staging 1.9.23. CORE-12409
    svn path=/trunk/; revision=73249

--- reactos/dll/directx/wine/amstream/mediastreamfilter.c
+++ reactos/dll/directx/wine/amstream/mediastreamfilter.c
@@ -348,7 +348,7 @@ static HRESULT WINAPI MediaStreamFilterImpl_AddMediaStream(IMediaStreamFilter* i
     if (!pins)
         return E_OUTOFMEMORY;
     This->pins = pins;
-    info.pFilter = (IBaseFilter*)&This->filter;
+    info.pFilter = &This->filter.IBaseFilter_iface;
     info.dir = PINDIR_INPUT;
     hr = IAMMediaStream_GetInformation(pAMMediaStream, &purpose_id, NULL);
     if (FAILED(hr))
@@ -362,11 +362,11 @@ static HRESULT WINAPI MediaStreamFilterImpl_AddMediaStream(IMediaStreamFilter* i
         return hr;
 
     pin = (MediaStreamFilter_InputPin*)This->pins[This->nb_streams];
-    pin->pin.pin.pinInfo.pFilter = (LPVOID)This;
+    pin->pin.pin.pinInfo.pFilter = &This->filter.IBaseFilter_iface;
     This->streams[This->nb_streams] = (IMediaStream*)pAMMediaStream;
     This->nb_streams++;
 
-    IMediaStream_AddRef((IMediaStream*)pAMMediaStream);
+    IAMMediaStream_AddRef(pAMMediaStream);
 
     return S_OK;
 }
@@ -510,7 +510,7 @@ HRESULT MediaStreamFilter_create(IUnknown *pUnkOuter, void **ppObj)
 
     BaseFilter_Init(&object->filter, (IBaseFilterVtbl*)&MediaStreamFilter_Vtbl, &CLSID_MediaStreamFilter, (DWORD_PTR)(__FILE__ ": MediaStreamFilterImpl.csFilter"), &BaseFuncTable);
 
-    *ppObj = object;
+    *ppObj = &object->filter.IBaseFilter_iface;
 
     return S_OK;
 }
