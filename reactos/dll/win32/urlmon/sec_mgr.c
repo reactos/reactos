01cc187f9ef350a8fdfb5f2d5da3c6ae29b441d1
Amine Khaldi <amine.khaldi@reactos.org>
2016-11-17 23:10:44 +0000

    [URLMON] Sync with Wine Staging 1.9.23. CORE-12409
    svn path=/trunk/; revision=73294

--- reactos/dll/win32/urlmon/sec_mgr.c
+++ reactos/dll/win32/urlmon/sec_mgr.c
@@ -1820,7 +1820,7 @@ HRESULT ZoneMgrImpl_Construct(IUnknown *pUnkOuter, LPVOID *ppobj)
     TRACE("(%p %p)\n", pUnkOuter, ppobj);
     ret->IInternetZoneManagerEx2_iface.lpVtbl = &ZoneMgrImplVtbl;
     ret->ref = 1;
-    *ppobj = (IInternetZoneManagerEx*)ret;
+    *ppobj = &ret->IInternetZoneManagerEx2_iface;
 
     URLMON_LockModule();
 
