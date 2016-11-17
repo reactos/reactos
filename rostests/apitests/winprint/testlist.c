5fd422387080ffd8029b3199cf68e00089cdfd26
Colin Finck <colin@reactos.org>
2016-11-17 21:24:07 +0000

    [WINPRINT_APITEST] Load all functions to test dynamically from either winprint.dll in the Print Processor Directory (for ReactOS and NT6) or localspl.dll in the system path (for NT5). Should fix winprint_apitest on ReactOS and Windows Server 2003.
    svn path=/trunk/; revision=73247

--- rostests/apitests/winprint/testlist.c
+++ rostests/apitests/winprint/testlist.c
@@ -2,16 +2,13 @@
  * PROJECT:     ReactOS Standard Print Processor API Tests
  * LICENSE:     GNU GPLv2 or any later version as published by the Free Software Foundation
  * PURPOSE:     Test list
- * COPYRIGHT:   Copyright 2015 Colin Finck <colin@reactos.org>
+ * COPYRIGHT:   Copyright 2015-2016 Colin Finck <colin@reactos.org>
  */
 
 /*
  * These tests are developed and tested against the Windows Server 2003 counterpart of winprint.
  * While ReactOS implements the Standard Print Processor in a separate module winprint.dll,
  * Windows Server 2003 puts it into the Local Print Spooler localspl.dll.
- *
- * To test against Windows, simply run these tests under Windows Server 2003, but copy its
- * localspl.dll to winprint.dll in advance.
  *
  * winspool.drv also provides functions that go into winprint.dll, but as these tests show,
  * they behave slightly different in terms of error codes due to the involved RPC and routing.
