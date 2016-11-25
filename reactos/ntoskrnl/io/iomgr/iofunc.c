7837d0dd2b23f5ddba610ea00d6785451fdee835
Pierre Schweitzer <pierre@reactos.org>
2016-11-25 22:39:49 +0000

    [NTOSKRNL] Revert r73376.
    CORE-12238
    
    svn path=/trunk/; revision=73378

--- reactos/ntoskrnl/io/iomgr/iofunc.c
+++ reactos/ntoskrnl/io/iomgr/iofunc.c
@@ -2741,7 +2741,10 @@ NtReadFile(IN HANDLE FileHandle,
 
     /* Now set the deferred read flags */
     Irp->Flags |= (IRP_READ_OPERATION | IRP_DEFER_IO_COMPLETION);
+#if 0
+    /* FIXME: VFAT SUCKS */
     if (FileObject->Flags & FO_NO_INTERMEDIATE_BUFFERING) Irp->Flags |= IRP_NOCACHE;
+#endif
 
     /* Perform the call */
     return IopPerformSynchronousRequest(DeviceObject,
@@ -3751,7 +3754,10 @@ NtWriteFile(IN HANDLE FileHandle,
 
     /* Now set the deferred read flags */
     Irp->Flags |= (IRP_WRITE_OPERATION | IRP_DEFER_IO_COMPLETION);
+#if 0
+    /* FIXME: VFAT SUCKS */
     if (FileObject->Flags & FO_NO_INTERMEDIATE_BUFFERING) Irp->Flags |= IRP_NOCACHE;
+#endif
 
     /* Perform the call */
     return IopPerformSynchronousRequest(DeviceObject,
