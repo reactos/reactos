89c3520c8618c634507fd93551d0d0da9cf7e1a8
Thomas Faber <thomas.faber@reactos.org>
2016-11-26 18:33:08 +0000

    [PSDK][XDK] - Automatically define __ROS_LONG64__ on LP64 systems. This makes no difference for ROS but makes things easier for downstream users of our headers.  Patch by Cat Zimmermann.
    svn path=/trunk/; revision=73383

--- reactos/sdk/include/xdk/winnt.template.h
+++ reactos/sdk/include/xdk/winnt.template.h
@@ -28,9 +28,8 @@
 #error Compiler too old!
 #endif
 
-/* HACK for wine code */
+#if defined(__LP64__) || (!defined(_M_AMD64) && defined(__WINESRC__))
 #if !defined(__ROS_LONG64__)
-#ifdef __WINESRC__
 #define __ROS_LONG64__
 #endif
 #endif
