11e9d57a3047f6a31ee9da84a1f89220dec60a1e
Mark Jansen <mark.jansen@reactos.org>
2016-12-22 20:51:38 +0000

    [BOOTDATA] Update AHKAppTests.cmd to check all drives for the AHK-Tests. CORE-12597 #resolve
    svn path=/trunk/; revision=73477

--- reactos/boot/bootdata/bootcdregtest/AHKAppTests.cmd
+++ reactos/boot/bootdata/bootcdregtest/AHKAppTests.cmd
@@ -1,11 +1,14 @@
 @ECHO OFF
 
-if not exist "D:\AHK-Tests" (
+set DRIVE=
+for %%X in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (if exist %%X:\AHK-Tests set DRIVE=%%X)
+
+if not defined DRIVE (
     dbgprint "AHK Application testing suite not present, skipping."
     exit /b 0
 )
 
-xcopy /Y /H /E D:\AHK-Tests\*.* C:\ReactOS\bin
+xcopy /Y /H /E %DRIVE%:\AHK-Tests\*.* %SystemRoot%\bin
 REM Download Amine's rosautotest from svn
-dwnl http://svn.reactos.org/amine/rosautotest.exe C:\ReactOS\system32\rosautotest.exe
+dwnl http://svn.reactos.org/amine/rosautotest.exe %SystemRoot%\system32\rosautotest.exe
 dbgprint "....AHK Application testing suite added."
