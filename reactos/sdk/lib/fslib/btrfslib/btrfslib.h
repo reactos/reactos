21f976c9ff996597ca8907269d115d2f1ba0238d
Pierre Schweitzer <pierre@reactos.org>
2017-01-09 20:56:55 +0000

    [BTRFSLIB] Drop that lib. It's been superseeded by r73513
    CORE-12617
    
    svn path=/trunk/; revision=73514

--- reactos/sdk/lib/fslib/btrfslib/btrfslib.h
+++
@@ -1,8 +0,0 @@
-/*
- * COPYRIGHT:   See COPYING in the top level directory
- * PROJECT:     ReactOS BTRFS FS library
- * FILE:        lib/fslib/btrfslib/btrfslib.h
- */
-#define NTOS_MODE_USER
-#include <ndk/umtypes.h>
-#include <fmifs/fmifs.h>
