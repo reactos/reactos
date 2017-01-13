cd7c1882ed729920e70b64051e20d7508773f6a0
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-01-13 21:55:25 +0000

    [SHLWAPI_APITEST] - Add tests for PathIsUNCServer and PathIsUNCServerShare, by Jared Smudde. Thanks! ROSTESTS-258 #resolve - Simplify the code: Since those functions (as well as PathIsUNC) are public and exported by name, we can directly load the apitest with shlwapi and call the APIs directly (no need for explicit LoadLibrary & GetProcAddress).
    svn path=/trunk/; revision=73544

--- rostests/apitests/shlwapi/PathIsUNCServer.c
+++ rostests/apitests/shlwapi/PathIsUNCServer.c
@@ -16,43 +16,29 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-/* Documentation: https://msdn.microsoft.com/en-us/library/windows/desktop/bb773712(v=vs.85).aspx */
+/* Documentation: https://msdn.microsoft.com/en-us/library/windows/desktop/bb773722(v=vs.85).aspx */
 
 #include <apitest.h>
 #include <shlwapi.h>
 
 #define DO_TEST(exp, str) \
 do { \
-    BOOL ret = PathIsUNCW((str)); \
+    BOOL ret = PathIsUNCServerW((str)); \
     ok(ret == (exp), "Expected %s to be %d, was %d\n", wine_dbgstr_w((str)), (exp), ret); \
 } while (0)
 
-START_TEST(isuncpath)
+START_TEST(isuncpathserver)
 {
-    DO_TEST(TRUE, L"\\\\path1\\path2");
-    DO_TEST(TRUE, L"\\\\path1");
-    DO_TEST(FALSE, L"reactos\\path4\\path5");
+    DO_TEST(TRUE, L"\\\\server");
     DO_TEST(TRUE, L"\\\\");
-    DO_TEST(TRUE, L"\\\\?\\UNC\\path1\\path2");
-    DO_TEST(TRUE, L"\\\\?\\UNC\\path1");
-    DO_TEST(TRUE, L"\\\\?\\UNC\\");
-    DO_TEST(FALSE, L"\\path1");
-    DO_TEST(FALSE, L"path1");
+    DO_TEST(FALSE, L"\\\\server\\folder");
+    DO_TEST(FALSE, L"reactos\\some\\folder");
+    DO_TEST(FALSE, L"////server//share");
     DO_TEST(FALSE, L"c:\\path1");
-
-    /* MSDN says FALSE but the test shows TRUE on Windows 2003, but returns FALSE on Windows 7 */
-    DO_TEST(TRUE, L"\\\\?\\c:\\path1");
-
-    DO_TEST(TRUE, L"\\\\path1\\");
-    DO_TEST(FALSE, L"//");
-    DO_TEST(FALSE, L"////path1");
-    DO_TEST(FALSE, L"////path1//path2");
-    DO_TEST(FALSE, L"reactos//path3//path4");
-    DO_TEST(TRUE, L"\\\\reactos\\?");
-    DO_TEST(TRUE, L"\\\\reactos\\\\");
     DO_TEST(FALSE, (wchar_t*)NULL);
+    DO_TEST(FALSE, L"");
     DO_TEST(FALSE, L" ");
 
     /* The test shows TRUE on Windows 2003, but returns FALSE on Windows 7 */
-    DO_TEST(TRUE, L"\\\\?\\");
+    DO_TEST(TRUE, L"\\\\?");
 }
--- rostests/apitests/shlwapi/testlist.c
+++ rostests/apitests/shlwapi/testlist.c
@@ -2,11 +2,15 @@
 #include <apitest.h>
 
 extern void func_isuncpath(void);
+extern void func_isuncpathserver(void);
+extern void func_isuncpathservershare(void);
 extern void func_PathUnExpandEnvStrings(void);
 
 const struct test winetest_testlist[] =
 {
     { "PathIsUNC", func_isuncpath },
+    { "PathIsUNCServer", func_isuncpathserver },
+    { "PathIsUNCServerShare", func_isuncpathservershare },
     { "PathUnExpandEnvStrings", func_PathUnExpandEnvStrings },
     { 0, 0 }
 };
