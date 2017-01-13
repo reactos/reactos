a20fc53659834f8bb3dfd193159f5b6a156dd704
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-01-13 14:51:25 +0000

    [SHLWAPI_APITEST]: Commit few tests for PathUnExpandEnvStrings.
    svn path=/trunk/; revision=73536

--- rostests/apitests/shlwapi/testlist.c
+++ rostests/apitests/shlwapi/testlist.c
@@ -2,9 +2,11 @@
 #include <apitest.h>
 
 extern void func_isuncpath(void);
+extern void func_PathUnExpandEnvStrings(void);
 
 const struct test winetest_testlist[] =
 {
     { "PathIsUNC", func_isuncpath },
+    { "PathUnExpandEnvStrings", func_PathUnExpandEnvStrings },
     { 0, 0 }
 };
