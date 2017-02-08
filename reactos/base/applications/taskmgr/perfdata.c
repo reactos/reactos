42f43a36a59e9062a3f27462bffb7e6bf41e6cbe
Kamil Hornicek <kamil.hornicek@reactos.org>
2017-02-08 22:52:46 +0000

    [TASKMGR] - ellipsis is a pointer not an array. CID 1321855
    svn path=/trunk/; revision=73760

--- reactos/base/applications/taskmgr/perfdata.c
+++ reactos/base/applications/taskmgr/perfdata.c
@@ -572,7 +572,7 @@ BOOL PerfDataGetCommandLine(ULONG Index, LPWSTR lpCommandLine, ULONG nMaxCount)
         {
             /* Found it. Use it, and add some ellipsis at the very end to make it cute */
             wcsncpy(lpCommandLine, cache->str, CMD_LINE_MIN(nMaxCount, cache->len));
-            wcscpy(lpCommandLine + CMD_LINE_MIN(nMaxCount, cache->len) - sizeof(ellipsis)/sizeof(WCHAR), ellipsis);
+            wcscpy(lpCommandLine + CMD_LINE_MIN(nMaxCount, cache->len) - wcslen(ellipsis), ellipsis);
             return TRUE;
         }
 
