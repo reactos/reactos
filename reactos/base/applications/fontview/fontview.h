3f81e26f346fc37825a9393e35f025e9ff6ded84
Mark Jansen <mark.jansen@reactos.org>
2017-02-08 17:56:22 +0000

    [FONTVIEW] Add support for font collections + show some extra font properties. Patch by Katayama Hirofumi MZ. CORE-6621 Some minor changes by me
    svn path=/trunk/; revision=73755

--- reactos/base/applications/fontview/fontview.h
+++ reactos/base/applications/fontview/fontview.h
@@ -8,11 +8,16 @@
 #define BUTTON_POS_Y 8
 #define BUTTON_WIDTH 72
 #define BUTTON_HEIGHT 21
+#define BUTTON_PADDING 8
 
 #define IDC_INSTALL 1001
 #define IDC_PRINT 1002
 #define IDC_DISPLAY 1003
+#define IDC_PREV 1004
+#define IDC_NEXT 1005
 
 LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
 
 BOOL LoadFont(LPWSTR lpCmdLine);
+
+extern INT g_FontIndex;
