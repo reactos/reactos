4634a076c461c482e3e55b905c7d4013fb9cae3a
Amine Khaldi <amine.khaldi@reactos.org>
2017-02-26 17:20:48 +0000

    [HLINK] Sync with Wine Staging 2.2. CORE-12823
    f6b1e12 hlink: Properly return interface pointers.
    4709902 hlink: Add missing parameter to TRACE() message.
    
    svn path=/trunk/; revision=73946

--- reactos/dll/win32/hlink/hlink_main.c
+++ reactos/dll/win32/hlink/hlink_main.c
@@ -516,14 +516,14 @@ static HRESULT WINAPI HLinkCF_fnQueryInterface ( LPCLASSFACTORY iface,
 {
     CFImpl *This = impl_from_IClassFactory(iface);
 
-    TRACE("(%p)->(%s)\n",This,debugstr_guid(riid));
+    TRACE("(%p)->(%s, %p)\n", This, debugstr_guid(riid), ppvObj);
 
     *ppvObj = NULL;
 
     if (IsEqualIID(riid, &IID_IUnknown) ||
         IsEqualIID(riid, &IID_IClassFactory))
     {
-        *ppvObj = This;
+        *ppvObj = &This->IClassFactory_iface;
         return S_OK;
     }
 
