ece00ea85f7bc02be87b5e29e464d213223c864b
Pierre Schweitzer <pierre@reactos.org>
2017-02-26 11:27:24 +0000

    [NTOSKRNL] Spotted with kmtest:IoFilesystem: these checks are not performed in free builds for kernel callers and are delegated to the FSD.
    svn path=/trunk/; revision=73920

--- reactos/ntoskrnl/io/iomgr/iofunc.c
+++ reactos/ntoskrnl/io/iomgr/iofunc.c
@@ -2115,6 +2115,7 @@ NtQueryInformationFile(IN HANDLE FileHandle,
         }
         _SEH2_END;
     }
+#if DBG
     else
     {
         /* Validate the information class */
@@ -2132,6 +2133,7 @@ NtQueryInformationFile(IN HANDLE FileHandle,
             return STATUS_INFO_LENGTH_MISMATCH;
         }
     }
+#endif
 
     /* Reference the Handle */
     Status = ObReferenceObjectByHandle(FileHandle,
