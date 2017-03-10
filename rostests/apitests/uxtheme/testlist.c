0feb059c77660a603d5f25687b51bd3fba933004
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-03-10 17:56:18 +0000

    [UXTHEME] -Add tests for SetWindowTheme to test the behavior that was changed in r74038.
    svn path=/trunk/; revision=74144

--- rostests/apitests/uxtheme/testlist.c
+++ rostests/apitests/uxtheme/testlist.c
@@ -3,12 +3,14 @@
 #define STANDALONE
 #include <apitest.h>
 
-extern void func_DrawThemeParentBackground(void);
 extern void func_CloseThemeData(void);
+extern void func_DrawThemeParentBackground(void);
+extern void func_SetWindowTheme(void);
 
 const struct test winetest_testlist[] =
 {
-    { "DrawThemeParentBackground", func_DrawThemeParentBackground },
     { "CloseThemeData", func_CloseThemeData },
+    { "DrawThemeParentBackground", func_DrawThemeParentBackground },
+    { "SetWindowTheme", func_SetWindowTheme },
     { 0, 0 }
 };
