2a507824c6e8b5b74fa382a0b89c36ef5ba2d3b0
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-03-31 17:17:35 +0000

    [WIN32K] -Update the prototype of NtUserPaintMenuBar here as well.
    svn path=/trunk/; revision=74250

--- reactos/win32ss/user/ntuser/ntstubs.c
+++ reactos/win32ss/user/ntuser/ntstubs.c
@@ -879,12 +879,12 @@ NtUserCalcMenuBar(
 DWORD
 APIENTRY
 NtUserPaintMenuBar(
-    DWORD dwUnknown1,
-    DWORD dwUnknown2,
-    DWORD dwUnknown3,
-    DWORD dwUnknown4,
-    DWORD dwUnknown5,
-    DWORD dwUnknown6)
+    HWND hWnd,
+    HDC hDC,
+    ULONG left,
+    ULONG right,
+    ULONG top,
+    BOOL bActive)
 {
     STUB;
     return 0;
