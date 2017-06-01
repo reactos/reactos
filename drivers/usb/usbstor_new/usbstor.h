b89500054ed30a028a8b9ebce09fb7e8e93894cb
Vadim Galyant <vgal@rambler.ru>
2017-06-01 16:00:35 +0800

    [USBSTOR] Constants for Inquiry.DeviceType field (correspond constants from scsi.h).

--- drivers/usb/usbstor_new/usbstor.h
+++ drivers/usb/usbstor_new/usbstor.h
@@ -53,6 +53,19 @@
 #define USB_PROTOCOL_KARMA        0xF5    /* Rio Karma */
 #define USB_PROTOCOL_VENDOR       0xFF    /* Use vendor specific */
 
+/* Correspond constants from scsi.h - Inquiry defines, DeviceType field. */
+
+#define USBSTOR_SCSI_DEVICE_TYPE_DIRECT         0x00  /* disks */
+#define USBSTOR_SCSI_DEVICE_TYPE_SEQUENTIAL     0x01  /* tapes */
+#define USBSTOR_SCSI_DEVICE_TYPE_PRINTER        0x02  /* printers */
+#define USBSTOR_SCSI_DEVICE_TYPE_PROCESSOR      0x03  /* scanners, printers, etc */
+#define USBSTOR_SCSI_DEVICE_TYPE_WORM           0x04  /* worms */
+#define USBSTOR_SCSI_DEVICE_TYPE_CDROM          0x05  /* cdroms */
+#define USBSTOR_SCSI_DEVICE_TYPE_SCANNER        0x06  /* scanners */
+#define USBSTOR_SCSI_DEVICE_TYPE_OPTICAL        0x07  /* optical disks */
+#define USBSTOR_SCSI_DEVICE_TYPE_MEDIA_CHANGER  0x08  /* jukebox */
+#define USBSTOR_SCSI_DEVICE_TYPE_UNKNOWN        0x1F 
+
 #define HTONS(n) (((((unsigned short)(n) & 0xFF)) << 8) | (((unsigned short)(n) & 0xFF00) >> 8))
 #define NTOHS(n) (((((unsigned short)(n) & 0xFF)) << 8) | (((unsigned short)(n) & 0xFF00) >> 8))
 
