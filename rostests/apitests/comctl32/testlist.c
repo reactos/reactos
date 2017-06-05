0cfb73fce3300299ad6e6a85825baea8a8760945
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-06-05 14:25:37 +0000

    [COMCTL32_APITEST] -Add tests for CCM_GETVERSION and CCM_SETVERSION for the v5 and v6 toolbar control.
    svn path=/trunk/; revision=74923

--- rostests/apitests/comctl32/testlist.c
+++ rostests/apitests/comctl32/testlist.c
@@ -2,9 +2,11 @@
 #include <apitest.h>
 
 extern void func_button(void);
+extern void func_toolbar(void);
 
 const struct test winetest_testlist[] =
 {
     { "buttonv6", func_button },
+    { "toolbarv6", func_toolbar },
     { 0, 0 }
 };
---
+++ rostests/apitests/comctl32/toolbar.c
@@ -0,0 +1,123 @@
+/*
+ * PROJECT:         ReactOS api tests
+ * LICENSE:         GPL - See COPYING in the top level directory
+ * PURPOSE:         Test for toolbar window class v6
+ * PROGRAMMERS:     Giannis Adamopoulos
+ */
+
+#include "wine/test.h"
+#include <stdio.h>
+#include <windows.h>
+#include <commctrl.h>
+#include <uxtheme.h>
+#include <undocuser.h>
+#include <msgtrace.h>
+#include <user32testhelpers.h>
+
+HANDLE _CreateV5ActCtx()
+{
+    ACTCTXW ActCtx = {sizeof(ACTCTX)};
+    WCHAR buffer[MAX_PATH] , *separator;
+
+    ok (GetModuleFileNameW(NULL, buffer, MAX_PATH), "GetModuleFileName failed\n");
+    separator = wcsrchr(buffer, L'\\');
+    if (separator)
+        wcscpy(separator + 1, L"comctl32v5.manifest");
+
+    ActCtx.lpSource = buffer;
+
+    return CreateActCtxW(&ActCtx);;
+}
+
+
+void TestVersionMessage()
+{
+    HWND hwnd;
+    int version;
+
+    hwnd = CreateWindowExW(0, TOOLBARCLASSNAMEW, L"Test", 0, 0, 0, 0, 0, 0, 0, 0, NULL);
+    ok(hwnd != NULL, "CreateWindowEx failed\n");
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 5, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 7, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 4, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 6, "Got %d, expected 6\n", version);
+
+    DestroyWindow(hwnd);
+}
+
+void TestV5VersionMessage()
+{
+    HWND hwnd;
+    int version;
+
+    hwnd = CreateWindowExW(0, TOOLBARCLASSNAMEW, L"Test", 0, 0, 0, 0, 0, 0, 0, 0, NULL);
+    ok(hwnd != NULL, "CreateWindowEx failed\n");
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 0, "Got %d, expected 0\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 6, 0);
+    ok(version == -1, "Got %d, expected -1\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 7, 0);
+    ok(version == -1, "Got %d, expected -1\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 5, 0);
+    ok(version == 0, "Got %d, expected -1\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 5, "Got %d, expected 5\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 4, 0);
+    ok(version == 5, "Got %d, expected -1\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 4, "Got %d, expected 5\n", version);
+
+    version = SendMessageW(hwnd, CCM_SETVERSION, 3, 0);
+    ok(version == 4, "Got %d, expected -1\n", version);
+
+    version = SendMessageW(hwnd, CCM_GETVERSION, 0, 0);
+    ok(version == 3, "Got %d, expected 5\n", version);
+
+    DestroyWindow(hwnd);
+}
+
+START_TEST(toolbar)
+{
+    HANDLE hV5ActCtx;
+
+    LoadLibraryW(L"comctl32.dll");
+
+    TestVersionMessage();
+
+    hV5ActCtx = _CreateV5ActCtx();
+    ok (hV5ActCtx != INVALID_HANDLE_VALUE, "");
+    if (hV5ActCtx)
+    {
+        ULONG_PTR cookie;
+        BOOL bActivated = ActivateActCtx(hV5ActCtx, &cookie);
+        if (bActivated)
+        {
+            TestV5VersionMessage();
+            DeactivateActCtx(0, cookie);
+        }
+    }
+}
