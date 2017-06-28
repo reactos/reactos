8d29499c63dc1347598e4ab5656696a77a8fcbc2
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-06-28 22:40:07 +0000

    [BOOTDATA]: Experiment with registry upgrade. [USETUP]: Fix the name of the txtsetup.sif section to look for when performing a registry upgrade.
    svn path=/branches/setup_improvements/; revision=75226

--- base/setup/usetup/interface/usetup.c
+++ base/setup/usetup/interface/usetup.c
@@ -4128,7 +4128,7 @@ DoUpdate:
          * we only update the hives.
          */
 
-        Success = SetupFindFirstLineW(SetupInf, L"HiveInfs.Update", NULL, &InfContext);
+        Success = SetupFindFirstLineW(SetupInf, L"HiveInfs.Upgrade", NULL, &InfContext);
         if (!Success)
         {
             /* Nothing to do for update! */
