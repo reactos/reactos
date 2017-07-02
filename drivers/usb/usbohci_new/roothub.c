83b68707dcce6a17280c8df702d0bbf890f4926c
Vadim Galyant <vgal@rambler.ru>
2017-07-03 00:11:51 +0800

    [USBOHCI] Correcting OHCI_RH_ClearFeaturePortConnectChange().

--- drivers/usb/usbohci_new/roothub.c
+++ drivers/usb/usbohci_new/roothub.c
@@ -397,6 +397,8 @@ OHCI_RH_ClearFeaturePortConnectChange(IN PVOID ohciExtension,
 {
     POHCI_EXTENSION OhciExtension;
     POHCI_OPERATIONAL_REGISTERS OperationalRegs;
+    PULONG PortStatusReg;
+    OHCI_REG_RH_PORT_STATUS PortStatus;
 
     OhciExtension = ohciExtension;
 
@@ -405,9 +407,12 @@ OHCI_RH_ClearFeaturePortConnectChange(IN PVOID ohciExtension,
            Port);
 
     OperationalRegs = OhciExtension->OperationalRegs;
+    PortStatusReg = (PULONG)&OperationalRegs->HcRhPortStatus[Port-1];
 
-    WRITE_REGISTER_ULONG(&OperationalRegs->HcRhPortStatus[Port-1].AsULONG,
-                         0x10000);
+    PortStatus.AsULONG = 0;
+    PortStatus.ConnectStatusChange = 1;
+
+    WRITE_REGISTER_ULONG(PortStatusReg, PortStatus.AsULONG);
 
     return MP_STATUS_SUCCESS;
 }
