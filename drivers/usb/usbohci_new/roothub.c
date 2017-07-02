e2b085ef68b6c67c0033723c250e971f42efe514
Vadim Galyant <vgal@rambler.ru>
2017-07-03 00:13:37 +0800

    [USBOHCI] Correcting OHCI_RH_ClearFeaturePortSuspendChange().

--- drivers/usb/usbohci_new/roothub.c
+++ drivers/usb/usbohci_new/roothub.c
@@ -451,6 +451,8 @@ OHCI_RH_ClearFeaturePortSuspendChange(IN PVOID ohciExtension,
 {
     POHCI_EXTENSION OhciExtension;
     POHCI_OPERATIONAL_REGISTERS OperationalRegs;
+    PULONG PortStatusReg;
+    OHCI_REG_RH_PORT_STATUS PortStatus;
 
     OhciExtension = ohciExtension;
 
@@ -459,9 +461,12 @@ OHCI_RH_ClearFeaturePortSuspendChange(IN PVOID ohciExtension,
            Port);
 
     OperationalRegs = OhciExtension->OperationalRegs;
+    PortStatusReg = (PULONG)&OperationalRegs->HcRhPortStatus[Port-1];
 
-    WRITE_REGISTER_ULONG(&OperationalRegs->HcRhPortStatus[Port-1].AsULONG,
-                         0x40000);
+    PortStatus.AsULONG = 0;
+    PortStatus.PortSuspendStatusChange = 1;
+
+    WRITE_REGISTER_ULONG(PortStatusReg, PortStatus.AsULONG);
 
     return MP_STATUS_SUCCESS;
 }
