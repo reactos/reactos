8ae850109a9ed79666ea4a5743e0af8638062d2c
Vadim Galyant <vgal@rambler.ru>
2017-07-03 00:06:42 +0800

    [USBOHCI] Correcting OHCI_RH_SetFeaturePortEnable().

--- drivers/usb/usbohci_new/roothub.c
+++ drivers/usb/usbohci_new/roothub.c
@@ -235,6 +235,8 @@ OHCI_RH_SetFeaturePortEnable(IN PVOID ohciExtension,
 {
     POHCI_EXTENSION OhciExtension;
     POHCI_OPERATIONAL_REGISTERS OperationalRegs;
+    PULONG PortStatusReg;
+    OHCI_REG_RH_PORT_STATUS PortStatus;
 
     OhciExtension = ohciExtension;
 
@@ -243,9 +245,12 @@ OHCI_RH_SetFeaturePortEnable(IN PVOID ohciExtension,
            Port);
 
     OperationalRegs = OhciExtension->OperationalRegs;
+    PortStatusReg = (PULONG)&OperationalRegs->HcRhPortStatus[Port-1];
 
-    WRITE_REGISTER_ULONG(&OperationalRegs->HcRhPortStatus[Port-1].AsULONG,
-                         2);
+    PortStatus.AsULONG = 0;
+    PortStatus.SetPortEnable = 1;
+
+    WRITE_REGISTER_ULONG(PortStatusReg, PortStatus.AsULONG);
 
     return MP_STATUS_SUCCESS;
 }
