7af2f717bc6ecb99c151193117d990c291bf08aa
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-08-28 14:46:49 +0000

    [SETUPLIB][USETUP] Move the settings code from usetup to setuplib.
    This code is moved ther since it'll be needed for 1st-stage GUI
    installer as well.
    
    The code dealing with keyboard layouts is however not moved yet,
    since it uses MUI* functions from usetup.
    I'll try to think how to include that in setuplib...
    
    svn path=/branches/setup_improvements/; revision=75701

--- base/setup/usetup/settings.h
+++ base/setup/usetup/settings.h
@@ -27,29 +27,6 @@
 #pragma once
 
 PGENERIC_LIST
-CreateComputerTypeList(
-    HINF InfFile);
-
-PGENERIC_LIST
-CreateDisplayDriverList(
-    HINF InfFile);
-
-BOOLEAN
-ProcessComputerFiles(
-    HINF InfFile,
-    PGENERIC_LIST List,
-    PWCHAR *AdditionalSectionName);
-
-BOOLEAN
-ProcessDisplayRegistry(
-    HINF InfFile,
-    PGENERIC_LIST List);
-
-PGENERIC_LIST
-CreateKeyboardDriverList(
-    HINF InfFile);
-
-PGENERIC_LIST
 CreateKeyboardLayoutList(
     HINF InfFile,
     WCHAR *DefaultKBLayout);
@@ -63,23 +40,11 @@ ULONG
 GetDefaultLanguageIndex(VOID);
 
 BOOLEAN
-ProcessLocaleRegistry(
-    PGENERIC_LIST List);
-
-BOOLEAN
 ProcessKeyboardLayoutRegistry(
     PGENERIC_LIST List);
 
 BOOLEAN
 ProcessKeyboardLayoutFiles(
     PGENERIC_LIST List);
-
-BOOLEAN
-SetGeoID(
-    PWCHAR Id);
-
-BOOLEAN
-SetDefaultPagefile(
-    WCHAR Drive);
 
 /* EOF */
