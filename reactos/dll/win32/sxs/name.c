9aead4c3e94c5880c25aa8be7893b756d3c9d4bf
Amine Khaldi <amine.khaldi@reactos.org>
2017-09-17 22:57:49 +0000

    [SXS] Sync with Wine Staging 2.16. CORE-13762
    3c4f6ed sxs: Use standard wine_dbgstr_longlong.
    
    svn path=/trunk/; revision=75900

--- reactos/dll/win32/sxs/name.c
+++ reactos/dll/win32/sxs/name.c
@@ -160,9 +160,9 @@ static HRESULT WINAPI name_Reserved(
     DWORD cbReserved,
     LPVOID *ppReserved )
 {
-    FIXME("%p, %s, %p, %p, %s, %x%08x, %p, %d, %p\n", iface,
+    FIXME("%p, %s, %p, %p, %s, %s, %p, %d, %p\n", iface,
           debugstr_guid(riid), pUnkReserved1, pUnkReserved2,
-          debugstr_w(szReserved), (DWORD)(llReserved >> 32), (DWORD)llReserved,
+          debugstr_w(szReserved), wine_dbgstr_longlong(llReserved),
           pvReserved, cbReserved, ppReserved);
     return E_NOTIMPL;
 }
