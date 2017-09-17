be8fcdf98bb29128ac5a5a7b9e7874309f492bae
Amine Khaldi <amine.khaldi@reactos.org>
2017-09-17 12:33:34 +0000

    [CRYPT32_WINETEST] Sync with Wine Staging 2.16. CORE-13762
    svn path=/trunk/; revision=75866

--- rostests/winetests/crypt32/encode.c
+++ rostests/winetests/crypt32/encode.c
@@ -476,7 +476,7 @@ static void testTimeEncoding(DWORD dwEncoding, LPCSTR structType,
 
 static const char *printSystemTime(const SYSTEMTIME *st)
 {
-    static char buf[25];
+    static char buf[64];
 
     sprintf(buf, "%02d-%02d-%04d %02d:%02d:%02d.%03d", st->wMonth, st->wDay,
      st->wYear, st->wHour, st->wMinute, st->wSecond, st->wMilliseconds);
@@ -485,7 +485,7 @@ static const char *printSystemTime(const SYSTEMTIME *st)
 
 static const char *printFileTime(const FILETIME *ft)
 {
-    static char buf[25];
+    static char buf[64];
     SYSTEMTIME st;
 
     FileTimeToSystemTime(ft, &st);
