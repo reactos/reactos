6b45a088f31786ada9ca2128cc936659a523d2a1
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-09-25 23:31:44 +0000

    [TIMEOUT]: Implement the TIMEOUT utility (found on Win2k3 and upwards). This is an improved "pause" command, with elapsed time count display. Based from a patch by Lee Schröder, with modifications by myself. CORE-10044 #resolve
    svn path=/trunk/; revision=75968

---
+++ reactos/base/applications/cmdutils/timeout/resource.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define IDS_USAGE                       0
+#define IDS_ERROR_OUT_OF_RANGE          1
+#define IDS_ERROR_INVALID_HANDLE_VALUE  2
+#define IDS_ERROR_READ_INPUT            3
+#define IDS_ERROR_NO_TIMER_VALUE        4
+#define IDS_ERROR_ONE_TIME              5
+#define IDS_NOBREAK_INPUT               6
+#define IDS_USER_INPUT                  7
+#define IDS_NOBREAK_INPUT_COUNT         8
+#define IDS_USER_INPUT_COUNT            9
