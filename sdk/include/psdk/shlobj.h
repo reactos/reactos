9d09b6c1fe55690151008cf5742bb920e661fa2a
Amine Khaldi <amine.khaldi@reactos.org>
2017-10-19 12:35:55 +0100

    [PSDK] Add missing KNOWN_FOLDER_FLAG and ILIsEmpty(). CORE-13762

--- sdk/include/psdk/shlobj.h
+++ sdk/include/psdk/shlobj.h
@@ -84,6 +84,21 @@ typedef struct
 DECLARE_HANDLE(HPSXA);
 #endif
 
+typedef enum
+{
+    KF_FLAG_DEFAULT                     = 0x00000000,
+    KF_FLAG_SIMPLE_IDLIST               = 0x00000100,
+    KF_FLAG_NOT_PARENT_RELATIVE         = 0x00000200,
+    KF_FLAG_DEFAULT_PATH                = 0x00000400,
+    KF_FLAG_INIT                        = 0x00000800,
+    KF_FLAG_NO_ALIAS                    = 0x00001000,
+    KF_FLAG_DONT_UNEXPAND               = 0x00002000,
+    KF_FLAG_DONT_VERIFY                 = 0x00004000,
+    KF_FLAG_CREATE                      = 0x00008000,
+    KF_FLAG_NO_APPCONTAINER_REDIRECTION = 0x00010000,
+    KF_FLAG_ALIAS_ONLY                  = 0x80000000
+} KNOWN_FOLDER_FLAG;
+
 typedef int GPFIDL_FLAGS;
 
 UINT
@@ -2161,6 +2176,10 @@ HRESULT      WINAPI ILLoadFromStream(_In_ LPSTREAM, _Inout_ LPITEMIDLIST*);
 BOOL         WINAPI ILRemoveLastID(_Inout_opt_ LPITEMIDLIST);
 HRESULT      WINAPI ILSaveToStream(_In_ LPSTREAM, _In_ LPCITEMIDLIST);
 
+static inline BOOL ILIsEmpty(_In_opt_ LPCITEMIDLIST pidl)
+{
+    return !(pidl && pidl->mkid.cb);
+}
 
 #include <poppack.h>
 
