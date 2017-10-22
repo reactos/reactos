a3cc3fbd45daebf1423d6eb3cc00005351bd0b45
Timo Kreuzer <timo.kreuzer@reactos.org>
2017-10-22 10:57:40 +0200

    [NTOS:EX] Don't dereference ExpCritSecOutOfMemoryEvent when it was never referenced.

--- ntoskrnl/ex/keyedevt.c
+++ ntoskrnl/ex/keyedevt.c
@@ -457,8 +457,11 @@ NtWaitForKeyedEvent(
     /* Do the wait */
     Status = ExpWaitForKeyedEvent(KeyedEvent, Key, Alertable, Timeout);
 
-    /* Dereference the keyed event */
-    ObDereferenceObject(KeyedEvent);
+    if (Handle != NULL)
+    {
+        /* Dereference the keyed event */
+        ObDereferenceObject(KeyedEvent);
+    }
 
     /* Return the status */
     return Status;
@@ -523,8 +526,11 @@ NtReleaseKeyedEvent(
     /* Do the wait */
     Status = ExpReleaseKeyedEvent(KeyedEvent, Key, Alertable, Timeout);
 
-    /* Dereference the keyed event */
-    ObDereferenceObject(KeyedEvent);
+    if (Handle != NULL)
+    {
+        /* Dereference the keyed event */
+        ObDereferenceObject(KeyedEvent);
+    }
 
     /* Return the status */
     return Status;
