7149a707206a59c38dbe56053a960728ef8ff56c
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2017-10-23 18:19:25 +0200

    [I8042PRT] keyboard.c: Revert "Style nit: Add a few blank lines", per review. CORE-13048

--- drivers/input/i8042prt/keyboard.c
+++ drivers/input/i8042prt/keyboard.c
@@ -344,10 +344,8 @@ i8042KbdDpcRoutine(
 	}
 
 	i8042PacketDpc(PortDeviceExtension);
-
 	if (!DeviceExtension->KeyComplete)
 		return;
-
 	/* We got the interrupt as it was being enabled, too bad */
 	if (!PortDeviceExtension->HighestDIRQLInterrupt)
 		return;
@@ -365,7 +363,6 @@ i8042KbdDpcRoutine(
 		return;
 
 	INFO_(I8042PRT, "Sending %lu key(s)\n", KeysInBufferCopy);
-
 	(*(PSERVICE_CALLBACK_ROUTINE)DeviceExtension->KeyboardData.ClassService)(
 		DeviceExtension->KeyboardData.ClassDeviceObject,
 		DeviceExtension->KeyboardBuffer,
@@ -376,9 +373,7 @@ i8042KbdDpcRoutine(
 	ASSERT(KeGetCurrentIrql() == Irql);
 
 	Irql = KeAcquireInterruptSpinLock(PortDeviceExtension->HighestDIRQLInterrupt);
-
 	DeviceExtension->KeysInBuffer -= KeysTransferred;
-
 	KeReleaseInterruptSpinLock(PortDeviceExtension->HighestDIRQLInterrupt, Irql);
 }
 
