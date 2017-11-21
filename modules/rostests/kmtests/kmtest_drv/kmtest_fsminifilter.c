adccb6e7e5d8c72b7180ee2cc70d64f8898693c6
gedmurphy <gedmurphy@reactos.org>
2017-11-21 16:03:17 +0000

    test commit

--- modules/rostests/kmtests/kmtest_drv/kmtest_fsminifilter.c
+++ modules/rostests/kmtests/kmtest_drv/kmtest_fsminifilter.c
@@ -37,7 +37,6 @@ DRIVER_INITIALIZE DriverEntry;
 /* Globals */
 static PDRIVER_OBJECT TestDriverObject;
 static PDEVICE_OBJECT KmtestDeviceObject;
-
 static FILTER_DATA FilterData;
 static PFLT_OPERATION_REGISTRATION Callbacks = NULL;
 static PFLT_CONTEXT_REGISTRATION Contexts = NULL;
