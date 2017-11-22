b4d691ba969d796acebfed01649498bc437914b0
Giannis Adamopoulos <gadamopoulos@reactos.org>
2017-11-22 11:14:49 +0200

    [BUTTONVISTEST] Add visual tests for buttons

---
+++ modules/rostests/win32/comctl32/buttonvistest/resource.h
@@ -0,0 +1,38 @@
+#define IDI_WIN32PROJECT1               107
+#define IDI_SMALL                       108
+#define IDS_APP_TITLE                   103
+#define IDC_WIN32PROJECT1               109
+#define IDB_BITMAP1                     130
+#define IDB_BITMAP2                     131
+#define IDC_MENU                        140
+
+#define IDM_NULL_WIN            200
+#define IDM_RED_WIN             201
+#define IDM_GREEN_WIN           202
+#define IDM_BLUE_WIN            203
+#define IDM_YELLOW_WIN          204
+#define IDM_CYAN_WIN            205
+
+#define IDM_NULL_STATIC         206
+#define IDM_RED_STATIC          207
+#define IDM_GREEN_STATIC        208
+#define IDM_BLUE_STATIC         209
+#define IDM_YELLOW_STATIC       210
+#define IDM_CYAN_STATIC         211
+
+#define IDM_NULL_BTN            212
+#define IDM_RED_BTN             213
+#define IDM_GREEN_BTN           214
+#define IDM_BLUE_BTN            215
+#define IDM_YELLOW_BTN          216
+#define IDM_CYAN_BTN            217
+
+#define IDM_NULL_PRINTCLNT      218
+#define IDM_RED_PRINTCLNT       219
+#define IDM_GREEN_PRINTCLNT     220
+#define IDM_BLUE_PRINTCLNT      221
+#define IDM_YELLOW_PRINTCLNT    222
+#define IDM_CYAN_PRINTCLNT      223
+
+#define IDM_SKIP_ERASE 250
+#define IDM_SKIP_PAINT 251
