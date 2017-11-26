2a3392b97d295909960b4da6c5bf70711b12420f
Amine Khaldi <amine.khaldi@reactos.org>
2017-11-26 13:24:50 +0100

    [GLU32] Add a PCH.

---
+++ dll/opengl/glu32/precomp.h
@@ -0,0 +1,51 @@
+#ifndef _GLU32_PRECOMP_H_
+#define _GLU32_PRECOMP_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+#include "gluos.h"
+#include "glrenderer.h"
+
+#include <GL/gl.h>
+
+#include "arc.h"
+#include "arcsorter.h"
+#include "backend.h"
+#include "basiccrveval.h"
+#include "basicsurfeval.h"
+#include "bezierarc.h"
+#include "bezierEval.h"
+#include "curve.h"
+#include "curvelist.h"
+#include "displaylist.h"
+#include "flist.h"
+#include "glimports.h"
+#include "mapdesc.h"
+#include "monoTriangulation.h"
+#include "myassert.h"
+#include "mymath.h"
+#include "nurbsconsts.h"
+#include "nurbstess.h"
+#include "partitionX.h"
+#include "patchlist.h"
+#include "polyDBG.h"
+#include "polyUtil.h"
+#include "quicksort.h"
+#include "quilt.h"
+#include "renderhints.h"
+#include "sampleComp.h"
+#include "sampleCompTop.h"
+#include "sampleCompBot.h"
+#include "sampleCompRight.h"
+#include "searchTree.h"
+#include "simplemath.h"
+#include "subdivider.h"
+#include "trimvertex.h"
+#include "trimvertpool.h"
+#include "varray.h"
+#include "zlassert.h"
+
+#endif /* _GLU32_PRECOMP_H_ */
