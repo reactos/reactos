2cbc32e5e466bf89a9c8580d7c4377a37c5ac757
Amine Khaldi <amine.khaldi@reactos.org>
2017-11-26 13:26:39 +0100

    [NTOBJSHEX] Improve the existing PCH and make use of it.

--- dll/shellext/ntobjshex/precomp.h
+++ dll/shellext/ntobjshex/precomp.h
@@ -1,11 +1,11 @@
-#pragma once
+#ifndef _NTOBJSHEX_PRECOMP_H_
+#define _NTOBJSHEX_PRECOMP_H_
 
-#define WIN32_NO_STATUS
 #include <stdarg.h>
 #include <tchar.h>
 
-#define COBJMACROS
 #define WIN32_NO_STATUS
+#define COBJMACROS
 #define _INC_WINDOWS
 #define COM_NO_WINDOWS_H
 #define NTOS_MODE_USER
@@ -13,26 +13,15 @@
 #include <windef.h>
 #include <winbase.h>
 #include <winreg.h>
-#include <winuser.h>
-#include <wincon.h>
-#include <ddeml.h>
-#include <shlguid_undoc.h>
 #include <shlwapi.h>
 #include <shlobj.h>
-#include <shlobj_undoc.h>
-#include <shlwapi_undoc.h>
-#include <tchar.h>
 #include <strsafe.h>
 #include <atlbase.h>
 #include <atlcom.h>
-#include <atlwin.h>
 #include <undocshell.h>
 #include <shellutils.h>
 
-#include <shellapi.h>
-
 #include <wine/debug.h>
-#include <wine/unicode.h>
 
 #include "resource.h"
 
@@ -59,7 +48,6 @@ DEFINE_GUID(CLSID_NtObjectFolder,
 
 WINE_DEFAULT_DEBUG_CHANNEL(ntobjshex);
 
-#include <rtlfuncs.h>
 #include <ntquery.h>
 #include "util.h"
 
@@ -68,3 +56,5 @@ WINE_DEFAULT_DEBUG_CHANNEL(ntobjshex);
 #include "foldercommon.h"
 #include "ntobjfolder.h"
 #include "regfolder.h"
+
+#endif /* _NTOBJSHEX_PRECOMP_H_ */
