cba274350e6d17a8356357fadb8ccb13d0c7531f
Mark Jansen <mark.jansen@reactos.org>
2017-12-16 17:22:48 +0100

    [BROWSEUI_APITEST] Add tests for IACLCustomMRU. CORE-9281

--- modules/rostests/apitests/browseui/testlist.c
+++ modules/rostests/apitests/browseui/testlist.c
@@ -4,11 +4,13 @@
 #include <apitest.h>
 
 extern void func_ACListISF(void);
+extern void func_IACLCustomMRU(void);
 extern void func_SHExplorerParseCmdLine(void);
 
 const struct test winetest_testlist[] =
 {
     { "ACListISF", func_ACListISF },
+    { "IACLCustomMRU", func_IACLCustomMRU },
     { "SHExplorerParseCmdLine", func_SHExplorerParseCmdLine },
 
     { 0, 0 }
