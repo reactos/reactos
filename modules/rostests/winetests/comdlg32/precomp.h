0622ce1775a1a2fc45a6b5bebf1a1ce22a68eb7d
Amine Khaldi <amine.khaldi@reactos.org>
2017-12-17 12:28:41 +0100

    [COMDLG32_WINETEST] Add a PCH.

---
+++ modules/rostests/winetests/comdlg32/precomp.h
@@ -0,0 +1,18 @@
+#ifndef _COMDLG32_WINETEST_PRECOMP_H_
+#define _COMDLG32_WINETEST_PRECOMP_H_
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+#define COBJMACROS
+#define CONST_VTABLE
+
+#include <wine/test.h>
+
+#include <wingdi.h>
+#include <objbase.h>
+#include <cderr.h>
+#include <commdlg.h>
+#include <dlgs.h>
+
+#endif /* !_COMDLG32_WINETEST_PRECOMP_H_ */
