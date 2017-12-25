reactos/reactos

commit 3c8a3a6e7e00e48787f1246cfe9b85fb9624af6c

[NET] CompareInfo => CompareUserInfo. #179

base/applications/network/net/cmdUser.c
@@ -13,7 +13,7 @@
 
 static
 int
-CompareInfo(const void *a,
+CompareUserInfo(const void *a,
             const void *b)
 {
     return _wcsicmp(((PUSER_INFO_0)a)->usri0_name,
@@ -62,7 +62,7 @@ EnumerateUsers(VOID)
         qsort(pBuffer,
               dwRead,
               sizeof(PUSER_INFO_0),
-              CompareInfo);
+              CompareUserInfo);
 
         for (i = 0; i < dwRead; i++)
         {

commit 0e9d9ce905500144a40116691834ddc71a0c9f9f

[NET] Fix a style issue I caused by 3c8a3a6

base/applications/network/net/cmdUser.c
@@ -10,11 +10,9 @@
 
 #include "net.h"
 
-
 static
 int
-CompareUserInfo(const void *a,
-            const void *b)
+CompareUserInfo(const void *a, const void *b)
 {
     return _wcsicmp(((PUSER_INFO_0)a)->usri0_name,
                     ((PUSER_INFO_0)b)->usri0_name);