f779c123f224a710290f63de2432513299a4201a
Ged Murphy <gedmurphy@reactos.org>
2017-12-31 13:09:09 +0000

    [TRACERT] Rewrite the tracert utility.
    - Rewrite the tracert utility to use the new ICMP APIs instead of manually crafting raw packets.
    - Still WIP but it should now work at least as well as the old one (which didn't actually work on modern versions of windows)

---
+++ base/applications/network/tracert/resource.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define IDS_USAGE                   100
+#define IDS_INVALID_OPTION          101
+#define IDS_TRACE_INFO              102
+#define IDS_TRACE_COMPLETE          103
+#define IDS_UNABLE_RESOLVE          104
+#define IDS_GEN_FAILURE             105
+
+#define IDS_HOP_COUNT               107
+#define IDS_HOP_TIME                108
+#define IDS_HOP_ZERO                109
+#define IDS_TIMEOUT                 110
+#define IDS_HOP_RES_INFO            111
+#define IDS_HOP_IP_INFO             112
+#define IDS_REQ_TIMED_OUT           113
