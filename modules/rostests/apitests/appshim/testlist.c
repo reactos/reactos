f4f4466ffd1efece8c6b7d42fe18986a143ed55c
Mark Jansen <mark.jansen@reactos.org>
2018-01-08 22:58:42 +0100

    [APPSHIM_APITEST] Add test for shims in AcLayers

--- modules/rostests/apitests/appshim/testlist.c
+++ modules/rostests/apitests/appshim/testlist.c
@@ -5,12 +5,14 @@
 
 extern void func_dispmode(void);
 extern void func_genral_hooks(void);
+extern void func_layer_hooks(void);
 extern void func_versionlie(void);
 
 const struct test winetest_testlist[] =
 {
     { "dispmode", func_dispmode },
     { "genral_hooks", func_genral_hooks },
+    { "layer_hooks", func_layer_hooks },
     { "versionlie", func_versionlie },
     { 0, 0 }
 };
