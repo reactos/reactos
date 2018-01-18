7998a3f086378e0c27196574afb550fc678ec150
Amine Khaldi <amine.khaldi@reactos.org>
2018-01-19 00:31:47 +0100

    [HNETCFG_WINETEST] Import from Wine 3.0. CORE-14225

---
+++ modules/rostests/winetests/hnetcfg/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_policy(void);
+
+const struct test winetest_testlist[] =
+{
+    { "policy", func_policy },
+    { 0, 0 }
+};
