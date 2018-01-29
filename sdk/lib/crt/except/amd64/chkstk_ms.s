aff33c855dbffbc0ef2e96e041103a26ec54814b
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-01-29 18:37:09 +0100

    [CRT] Use chkstk_ms.s on x64 builds

--- sdk/lib/crt/except/amd64/chkstk_ms.s
+++ sdk/lib/crt/except/amd64/chkstk_ms.s
@@ -15,14 +15,17 @@
 /* CODE **********************************************************************/
 .code64
 
-PUBLIC ___chkstk_ms
+PUBLIC __chkstk
+PUBLIC __alloca_probe
 
-    //cfi_startproc()
-___chkstk_ms:
+__alloca_probe:
+.PROC __chkstk
+
     push rcx                    /* save temps */
-    //cfi_push(%rcx)
+    .pushreg rcx
     push rax
-    //cfi_push(%rax)
+    .pushreg rax
+    .endprolog
 
     cmp rax, PAGE_SIZE          /* > 4k ?*/
     lea rcx, [rsp + 24]         /* point past return addr */
@@ -41,11 +44,10 @@ l_LessThanAPage:
     or byte ptr [rcx], 0        /* less than 4k, just peek here */
 
     pop rax
-    //cfi_pop(%rax)
     pop rcx
-    //cfi_pop(%rcx)
     ret
-    //cfi_endproc()
+
+.ENDP
 
 END
 /* EOF */
