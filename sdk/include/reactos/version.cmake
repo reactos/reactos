166e6c4f399e0564602f8bc70ea2810b560685b4
Timo Kreuzer <timo.kreuzer@reactos.org>
2018-02-13 20:36:08 +0100

    [CMAKE] Zap builddir.h.cmake and instead define macros globally This changes __RELFILE__ to always be relative and not rely on a GCC extension, since __FILE__ is *always* relative to the root.

--- sdk/include/reactos/version.cmake
+++ sdk/include/reactos/version.cmake
@@ -53,4 +53,3 @@ endif()
 
 configure_file(sdk/include/reactos/version.h.cmake ${REACTOS_BINARY_DIR}/sdk/include/reactos/version.h)
 configure_file(sdk/include/reactos/buildno.h.cmake ${REACTOS_BINARY_DIR}/sdk/include/reactos/buildno.h)
-configure_file(sdk/include/reactos/builddir.h.cmake ${REACTOS_BINARY_DIR}/sdk/include/reactos/builddir.h)
