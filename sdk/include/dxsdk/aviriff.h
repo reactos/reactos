7446d8c8a2b990189746f7b30d15895cf556214c
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-08 13:44:37 +0100

    [DXSDK] Add a header guard to aviriff.h.

--- sdk/include/dxsdk/aviriff.h
+++ sdk/include/dxsdk/aviriff.h
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#pragma once
+
 /* All definitions here are packed structures of on-disk formats */
 #include <pshpack2.h>
 
