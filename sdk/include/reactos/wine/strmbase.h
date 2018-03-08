fbbe3b385641f1fd47e60704c840531478a732e3
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-08 13:07:26 +0100

    [INCLUDE/WINE] Add a header guard to strmbase.h.

--- sdk/include/reactos/wine/strmbase.h
+++ sdk/include/reactos/wine/strmbase.h
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#pragma once
+
 #include "wine/list.h"
 
 HRESULT WINAPI CopyMediaType(AM_MEDIA_TYPE * pDest, const AM_MEDIA_TYPE *pSrc);
