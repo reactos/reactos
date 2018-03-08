d3e7f8dd2d12407ae4a94d53def6f25f238a4d9a
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-08 13:43:36 +0100

    [QEDIT] Sync with Wine Staging 3.3. CORE-14434

--- dll/directx/wine/qedit/qedit_private.h
+++ dll/directx/wine/qedit/qedit_private.h
@@ -22,22 +22,15 @@
 
 #include <stdarg.h>
 
-#define WIN32_LEAN_AND_MEAN
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
 #define COBJMACROS
 
-#include <windef.h>
-#include <winbase.h>
-#include <objbase.h>
-#include <dshow.h>
-#define __WINE_DDRAW_H /* ROS HACK */
-#include <qedit.h>
-
-#include <wine/debug.h>
-WINE_DEFAULT_DEBUG_CHANNEL(qedit);
+#include "windef.h"
+#include "winbase.h"
+#include "wtypes.h"
+#include "wingdi.h"
+#include "winuser.h"
+#include "dshow.h"
+#include "qedit.h"
 
 HRESULT AMTimeline_create(IUnknown *pUnkOuter, LPVOID *ppObj) DECLSPEC_HIDDEN;
 HRESULT MediaDet_create(IUnknown *pUnkOuter, LPVOID *ppObj) DECLSPEC_HIDDEN;
