7342d6751c456df1dbc32b2c81d78faf78c8c9d3
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-09 13:01:37 +0100

    [CRYPTDLG] Sync with Wine Staging 3.3. CORE-14434

--- dll/win32/cryptdlg/main.c
+++ dll/win32/cryptdlg/main.c
@@ -16,27 +16,25 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
-
 #define NONAMELESSUNION
 
-#include <config.h>
+#include "config.h"
 
 #include <stdarg.h>
 
-#include <windef.h>
-#include <winbase.h>
-//#include "winnls.h"
-#include <winreg.h>
-#include <wincrypt.h>
-//#include "wintrust.h"
-#include <winuser.h>
-//#include "objbase.h"
-#include <cryptdlg.h>
-#include <cryptuiapi.h>
+#include "windef.h"
+#include "winbase.h"
+#include "winnls.h"
+#include "winreg.h"
+#include "wincrypt.h"
+#include "wintrust.h"
+#include "winuser.h"
+#include "objbase.h"
+#include "cryptdlg.h"
+#include "cryptuiapi.h"
 #include "cryptres.h"
-#include <wine/unicode.h>
-#include <wine/debug.h>
+#include "wine/unicode.h"
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(cryptdlg);
 
