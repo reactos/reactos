feea1bc5be337f08bce5f12a90f6623916cdc40e
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-09 12:40:09 +0100

    [ACTXPRXY] Sync with Wine Staging 3.3. CORE-14434

---
+++ dll/win32/actxprxy/precomp.h
@@ -0,0 +1,17 @@
+
+#ifndef _ACTXPRXY_PRECOMP_H_
+#define _ACTXPRXY_PRECOMP_H_
+
+#define __midl_proxy
+#define WIN32_NO_STATUS
+#define WIN32_LEAN_AND_MEAN
+#define DECLSPEC_HIDDEN
+
+#include <objbase.h>
+#include <rpcproxy.h>
+#include <rpc.h>
+#include <rpcndr.h>
+#include <windows.h>
+#include <ole2.h>
+
+#endif /* !_ACTXPRXY_PRECOMP_H_ */
