7e88971d050a63bac4971e72d9f9a8e13c67a319
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-15 12:20:55 +0100

    [HLINK] Sync with Wine Staging 3.3. CORE-14434

--- dll/win32/hlink/hlink_private.h
+++ dll/win32/hlink/hlink_private.h
@@ -16,45 +16,24 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#ifndef _HLINK_PRIVATE_H
-#define _HLINK_PRIVATE_H
+#pragma once
 
 #include <stdarg.h>
 
-#define WIN32_NO_STATUS
-#define _INC_WINDOWS
-#define COM_NO_WINDOWS_H
-
 #define COBJMACROS
 
-#include <windef.h>
-#include <winbase.h>
-#include <ole2.h>
-#include <hlink.h>
-#include <hlguids.h>
+#include "winerror.h"
+#include "windef.h"
+#include "winbase.h"
+#include "winuser.h"
+#include "ole2.h"
+#include "hlink.h"
 
-#include <wine/unicode.h>
-
-#include <wine/debug.h>
-WINE_DEFAULT_DEBUG_CHANNEL(hlink);
+#include "wine/heap.h"
+#include "wine/unicode.h"
 
 extern HRESULT HLink_Constructor(IUnknown*,REFIID,void**) DECLSPEC_HIDDEN;
 extern HRESULT HLinkBrowseContext_Constructor(IUnknown*,REFIID,void**) DECLSPEC_HIDDEN;
-
-static inline void* __WINE_ALLOC_SIZE(1) heap_alloc(size_t size)
-{
-    return HeapAlloc(GetProcessHeap(), 0, size);
-}
-
-static inline void* __WINE_ALLOC_SIZE(1) heap_alloc_zero(size_t size)
-{
-    return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
-}
-
-static inline BOOL heap_free(void *mem)
-{
-    return HeapFree(GetProcessHeap(), 0, mem);
-}
 
 static inline LPWSTR hlink_strdupW(LPCWSTR str)
 {
@@ -85,5 +64,3 @@ static inline LPWSTR hlink_co_strdupW(LPCWSTR str)
 
     return ret;
 }
-
-#endif /* _HLINK_PRIVATE_H */
---
+++ dll/win32/hlink/precomp.h
@@ -0,0 +1,15 @@
+
+#ifndef _HLINK_PRECOMP_H
+#define _HLINK_PRECOMP_H
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
+#include "hlink_private.h"
+
+#include <hlguids.h>
+
+#include <wine/debug.h>
+
+#endif /* !_HLINK_PRECOMP_H */
