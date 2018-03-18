c994d6438c218338bf373373fcc0217d688f4c7b
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-18 12:31:18 +0100

    [MSGSM32.ACM] Sync with Wine Staging 3.3. CORE-14434

--- dll/win32/msgsm32.acm/msgsm32.c
+++ dll/win32/msgsm32.acm/msgsm32.c
@@ -20,14 +20,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define WIN32_NO_STATUS
+#include "config.h"
+#include <wine/port.h>
 
-#include <config.h>
-//#include <wine/port.h>
-
-//#include <assert.h>
+#include <assert.h>
 #include <stdarg.h>
-//#include <string.h>
+#include <string.h>
 
 #ifdef HAVE_GSM_GSM_H
 #include <gsm/gsm.h>
@@ -35,17 +33,17 @@
 #include <gsm.h>
 #endif
 
-#include <windef.h>
-#include <winbase.h>
-#include <wingdi.h>
-#include <winuser.h>
-#include <winnls.h>
-//#include "mmsystem.h"
-//#include "mmreg.h"
-//#include "msacm.h"
-#include <msacmdrv.h>
-//#include "wine/library.h"
-#include <wine/debug.h>
+#include "windef.h"
+#include "winbase.h"
+#include "wingdi.h"
+#include "winuser.h"
+#include "winnls.h"
+#include "mmsystem.h"
+#include "mmreg.h"
+#include "msacm.h"
+#include "msacmdrv.h"
+#include "wine/library.h"
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(gsm);
 
