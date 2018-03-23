51fd824e2183aa5b8c704068d708b1ad72bfda7f
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-23 12:31:40 +0100

    [TAPI32] Sync with Wine Staging 3.3. CORE-14434

--- dll/win32/tapi32/line.c
+++ dll/win32/tapi32/line.c
@@ -18,7 +18,22 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "precomp.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "windef.h"
+#include "winbase.h"
+#include "wingdi.h"
+#include "winreg.h"
+#include "winnls.h"
+#include "winerror.h"
+#include "objbase.h"
+#include "tapi.h"
+#include "wine/debug.h"
+#include "wine/unicode.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(tapi);
 
 /* registry keys */
 static const char szCountrylistKey[] =
