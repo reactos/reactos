302c4ae0ca06b7dd6b8c9ab01f10f6820267047c
Amine Khaldi <amine.khaldi@reactos.org>
2018-03-23 12:32:12 +0100

    [TRAFFIC] Sync with Wine Staging 3.3. CORE-14434

--- dll/win32/traffic/traffic_main.c
+++ dll/win32/traffic/traffic_main.c
@@ -17,14 +17,12 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-#define WIN32_NO_STATUS
-
 #include <stdarg.h>
 
-#include <windef.h>
-#include <winbase.h>
-#include <traffic.h>
-#include <wine/debug.h>
+#include "windef.h"
+#include "winbase.h"
+#include "traffic.h"
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(traffic);
 
