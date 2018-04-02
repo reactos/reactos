3031de8eeda1cb368cc037018f30a5d5d5ec2654
Eric Kohl <eric.kohl@reactos.org>
2018-04-02 23:13:50 +0200

    [NET] Implement the NET CONFIG WORKSTATION command

--- base/applications/network/net/cmdConfig.c
+++ base/applications/network/net/cmdConfig.c
@@ -36,10 +36,13 @@ DisplayServerConfig(VOID)
     ConPrintf(StdOut, L"%s\n\n", ServerInfo->sv102_comment);
 
     PrintPaddedResourceString(IDS_CONFIG_SERVER_VERSION, nPaddedLength);
-    ConPuts(StdOut, L"...\n");
+    ConPrintf(StdOut, L"%lu.%lu\n",
+              ServerInfo->sv102_version_major,
+              ServerInfo->sv102_version_minor);
 
     ConResPuts(StdOut, IDS_CONFIG_SERVER_ACTIVE);
-    for (i = 0; i < dwTotal; i++)
+    ConPuts(StdOut, L"\n");
+    for (i = 0; i < dwRead; i++)
     {
         ConPrintf(StdOut, L"      %s (%s)\n",
                   &TransportInfo[i].svti0_transportname[8],
@@ -78,6 +81,75 @@ static
 INT
 DisplayWorkstationConfig(VOID)
 {
+    PWKSTA_INFO_100 WorkstationInfo = NULL;
+    PWKSTA_USER_INFO_1 UserInfo = NULL;
+    PWKSTA_TRANSPORT_INFO_0 TransportInfo = NULL;
+    DWORD dwRead = 0, dwTotal = 0, i;
+    INT nPaddedLength = 38;
+    NET_API_STATUS Status;
+
+    Status = NetWkstaGetInfo(NULL, 100, (PBYTE*)&WorkstationInfo);
+    if (Status != NERR_Success)
+        goto done;
+
+    Status = NetWkstaUserGetInfo(NULL, 1, (PBYTE*)&UserInfo);
+    if (Status != NERR_Success)
+        goto done;
+
+    Status = NetWkstaTransportEnum(NULL,
+                                   0,
+                                   (PBYTE*)&TransportInfo,
+                                   MAX_PREFERRED_LENGTH,
+                                   &dwRead,
+                                   &dwTotal,
+                                   NULL);
+    if (Status != NERR_Success)
+        goto done;
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_NAME, nPaddedLength);
+    ConPrintf(StdOut, L"\\\\%s\n", WorkstationInfo->wki100_computername);
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_FULLNAME, nPaddedLength);
+    ConPuts(StdOut, L"...\n");
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_USERNAME, nPaddedLength);
+    ConPrintf(StdOut, L"%s\n", UserInfo->wkui1_username);
+
+    ConPuts(StdOut, L"\n");
+
+    ConResPuts(StdOut, IDS_CONFIG_WORKSTATION_ACTIVE);
+    ConPuts(StdOut, L"\n");
+    for (i = 0; i < dwRead; i++)
+    {
+        ConPrintf(StdOut, L"      %s (%s)\n",
+                  &TransportInfo[i].wkti0_transport_name[8],
+                  TransportInfo[i].wkti0_transport_address);
+    }
+    ConPuts(StdOut, L"\n");
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_VERSION, nPaddedLength);
+    ConPrintf(StdOut, L"%lu.%lu\n",
+              WorkstationInfo->wki100_ver_major,
+              WorkstationInfo->wki100_ver_minor);
+
+    ConPuts(StdOut, L"\n");
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_DOMAIN, nPaddedLength);
+    ConPrintf(StdOut, L"%s\n", WorkstationInfo->wki100_langroup);
+
+    PrintPaddedResourceString(IDS_CONFIG_WORKSTATION_LOGON, nPaddedLength);
+    ConPrintf(StdOut, L"%s\n", UserInfo->wkui1_logon_domain);
+
+done:
+    if (TransportInfo != NULL)
+        NetApiBufferFree(TransportInfo);
+
+    if (UserInfo != NULL)
+        NetApiBufferFree(UserInfo);
+
+    if (WorkstationInfo != NULL)
+        NetApiBufferFree(WorkstationInfo);
+
     return 0;
 }
 
