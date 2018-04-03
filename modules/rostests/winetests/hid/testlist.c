845faec4036eb102b41fc078e59c46a2c7fb6bbb
Amine Khaldi <amine.khaldi@reactos.org>
2018-04-03 13:32:05 +0100

    [HID_WINETEST] Import from Wine Staging 3.3. CORE-14434

---
+++ modules/rostests/winetests/hid/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_device(void);
+
+const struct test winetest_testlist[] =
+{
+    { "device", func_device },
+    { 0, 0 }
+};
