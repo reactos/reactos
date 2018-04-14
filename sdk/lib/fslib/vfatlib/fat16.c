38791299ca9727d4b639b25bcf5aeca5716d1826
Pierre Schweitzer <pierre@reactos.org>
2018-04-14 12:05:32 +0200

    [VFATLIB] Simplify in-memory boot sector copy. So that, coverity is happy again. NFC
    CID 1434230, 1434233, 1434238, 1434243, 1434246, 1434272

--- sdk/lib/fslib/vfatlib/fat16.c
+++ sdk/lib/fslib/vfatlib/fat16.c
@@ -38,8 +38,7 @@ Fat16WriteBootSector(IN HANDLE FileHandle,
     RtlZeroMemory(NewBootSector, BootSector->BytesPerSector);
 
     /* Copy FAT16 BPB to new bootsector */
-    memcpy(&NewBootSector->Jump[0],
-           &BootSector->Jump[0],
+    memcpy(NewBootSector, BootSector,
            FIELD_OFFSET(FAT16_BOOT_SECTOR, Res2) - FIELD_OFFSET(FAT16_BOOT_SECTOR, Jump));
            /* FAT16 BPB length (up to (not including) Res2) */
 
