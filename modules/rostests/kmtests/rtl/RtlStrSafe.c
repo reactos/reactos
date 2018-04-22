13360e08a28787b6437fe9debe7a7838e22b9243
Hernán Di Pietro <indiocolifa@hotmail.com>
2018-04-22 02:52:38 -0300

    First commit: Skeleton for test implementation + Addition to COMMON SOURCES in kmtests Cmake lists.

---
+++ modules/rostests/kmtests/rtl/RtlStrSafe.c
@@ -0,0 +1,15 @@
+/*
+ * PROJECT:         ReactOS kernel-mode tests
+ * LICENSE:         GPLv2+ - See COPYING in the top level directory
+ * PURPOSE:         Test for ntstrsafe.h functions
+ * PROGRAMMER:      Hernán Di Pietro <hernan.di.pietro@gmail.com>
+ */
+
+#define KMT_EMULATE_KERNEL
+#include <kmt_test.h>
+#include <ntstrsafe.h>
+
+START_TEST(RtlStrSafe)
+{
+   
+}
