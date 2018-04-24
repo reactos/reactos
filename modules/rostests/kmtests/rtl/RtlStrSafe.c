8e2df51809fdb7b62fd4af28935c7cf9f87aab4b
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2018-04-23 23:15:49 -0300

    [KMTESTS:RTL] First Test.

--- modules/rostests/kmtests/rtl/RtlStrSafe.c
+++ modules/rostests/kmtests/rtl/RtlStrSafe.c
@@ -8,8 +8,40 @@
 #define KMT_EMULATE_KERNEL
 #include <kmt_test.h>
 #include <ntstrsafe.h>
+#include <ntdef.h>
+#include <ndk/rtlfuncs.h>
+
+static void Test_RtlUnicodeStringPrintf()
+{
+    WCHAR Buffer[1024] = { 0 };
+    UNICODE_STRING UsString;
+    WCHAR FormatStringInts[] = L"%d %d %d";
+    
+    UsString.Buffer = Buffer;
+    UsString.Length = 0;
+    UsString.MaximumLength = sizeof(Buffer);
+    
+    const WCHAR Result[] = L"1 2 3";
+    ok_eq_bool(RtlUnicodeStringPrintf(&UsString, FormatStringInts, 1, 2, 3), STATUS_SUCCESS);
+    ok_eq_uint(UsString.Length, sizeof(Result));
+    ok_eq_uint(UsString.MaximumLength, sizeof(Buffer));
+    ok_eq_wstr(UsString.Buffer, Result);
+    
+
+}
+
+static void Test_RtlUnicodeStringPrintfEx()
+{
+
+}
+
+static void Test_RtlUnicodeStringValidate()
+{
+    
+}
 
 START_TEST(RtlStrSafe)
 {
-   
+    Test_RtlUnicodeStringPrintf();
+    Test_RtlUnicodeStringPrintfEx();
 }
