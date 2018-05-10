493fe6530dc9ad2209598ffdf93721aff3288666
Baruch Rutman <peterooch@gmail.com>
2018-05-10 11:45:21 +0300

    [LPK] More changes
    More memory management
    Reworked minor logic
    Removed trailing whitespaces

--- dll/win32/lpk/lpk.c
+++ dll/win32/lpk/lpk.c
@@ -2,7 +2,7 @@
  * PROJECT:     ReactOS LPK
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * PURPOSE:     Language Pack DLL.
- * COPYRIGHT:   Copyright 2018 Baruch Rutman & the Wine Project 
+ * PROGRAMMERS: Baruch Rutman (peterooch at gmail dot com)
  */
 
 #include "ros_lpk.h"
@@ -22,31 +22,28 @@ LpkExtTextOut(
     UINT uCount,
     const INT *lpDx,
     INT unknown)
-{   
+{
     WORD *glyphs = NULL;
     INT cGlyphs;
-    
+
     UNREFERENCED_PARAMETER(unknown);
- 
+
     fuOptions |= ETO_IGNORELANGUAGE;
-    
+
     /* Check if the string requires complex script processing */
     if (ScriptIsComplex(lpString, uCount, SIC_COMPLEX) == S_OK)
     {
         BIDI_Reorder(hdc, lpString, uCount, GCP_REORDER, WINE_GCPW_FORCE_LTR,
                      NULL, uCount, NULL, &glyphs, &cGlyphs);
 
-        if (glyphs)
-        {
-            fuOptions |= ETO_GLYPH_INDEX;
-        }
+        fuOptions |= ETO_GLYPH_INDEX;
 
         if (uCount > cGlyphs)
         cGlyphs = uCount;
 
         return ExtTextOutW(hdc, x, y, fuOptions, lprc, (LPWSTR)glyphs, cGlyphs, lpDx);
     }
-    
+
     return ExtTextOutW(hdc, x, y, fuOptions, lprc, lpString, uCount, lpDx);
 }
 
@@ -64,31 +61,41 @@ LpkGetCharacterPlacement(
     DWORD dwFlags,
     DWORD dwUnused)
 {
+    LPWSTR lpOutString;
     WORD *glyphs = NULL;
     UINT *lpOrder = NULL;
-    LPWSTR lpOutString;
+    UINT nSet;
     INT cGlyphs;
 
     UNREFERENCED_PARAMETER(dwUnused);
-    
-    /* Sanity checks */
-    if ( !(dwFlags & GCP_REORDER))
+
+    /* Sanity check */
+    if (!(dwFlags & GCP_REORDER))
        return FALSE;
 
     lpOutString = HeapAlloc(GetProcessHeap(), 0, uCount * sizeof(WCHAR));
 
-    BIDI_Reorder(hdc, lpString, uCount, dwFlags, WINE_GCPW_FORCE_LTR, lpOutString,
-                 lpResults->nGlyphs, lpOrder, &glyphs, &cGlyphs);
+    nSet = (UINT)uCount;
+    if (nSet > lpResults->nGlyphs)
+        nSet = lpResults->nGlyphs;
 
-    if (lpResults->lpOutString)
+    BIDI_Reorder(hdc, lpString, uCount, dwFlags, WINE_GCPW_FORCE_LTR, lpOutString,
+                 nSet, lpOrder, &glyphs, &cGlyphs);
+
+    lpResults->nGlyphs = (UINT)cGlyphs;
+
+    if (lpResults->lpOutString && lpOutString)
         wcscpy(lpResults->lpOutString, lpOutString);
 
     if (lpResults->lpOrder)
         memcpy(lpResults->lpOrder, lpOrder, sizeof(lpOrder));
-    
+
     if (lpResults->lpGlyphs)
         wcscpy(lpResults->lpGlyphs, glyphs);
-    
+
+    HeapFree(GetProcessHeap(), 0, lpOutString);
+    HeapFree(GetProcessHeap(), 0, lpOrder);
+    HeapFree(GetProcessHeap(), 0, glyphs);
+
     return TRUE;
 }
-
