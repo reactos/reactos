29c160dca9c74632eb9e2cc17b4ff6426d0248a4
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-05-18 06:24:30 +0900

    fix DeferWindowPos calls

--- dll/win32/shell32/dialogs/folder_options.cpp
+++ dll/win32/shell32/dialogs/folder_options.cpp
@@ -1624,15 +1624,15 @@ NewExtDlg_OnAdvanced(HWND hwndDlg, NEWEXT_DIALOG *pNewExt)
     if (hDWP)
         DeferWindowPos(hDWP, GetDlgItem(hwndDlg, IDOK), NULL,
                        rc1.left, rc1.top, rc1.right - rc1.left, rc1.bottom - rc1.top,
-                       SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOMOVE);
+                       SWP_NOACTIVATE | SWP_NOZORDER);
     if (hDWP)
         DeferWindowPos(hDWP, GetDlgItem(hwndDlg, IDCANCEL), NULL,
                        rc2.left, rc2.top, rc2.right - rc2.left, rc2.bottom - rc2.top,
-                       SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOMOVE);
+                       SWP_NOACTIVATE | SWP_NOZORDER);
     if (hDWP)
         DeferWindowPos(hDWP, hwndDlg, NULL,
                        rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top,
-                       SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOMOVE);
+                       SWP_NOACTIVATE | SWP_NOZORDER);
 
     if (hDWP)
         EndDeferWindowPos(hDWP);
