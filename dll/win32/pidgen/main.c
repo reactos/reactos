452324662809497607cf46fba15fc222f4ed3407
Amine Khaldi <amine.khaldi@reactos.org>
2018-06-04 03:46:44 +0100

    [PIDGEN] Sync with Wine Staging 3.9. CORE-14656

--- dll/win32/pidgen/main.c
+++ dll/win32/pidgen/main.c
@@ -43,3 +43,10 @@ BOOL WINAPI DllMain( HINSTANCE hinst, DWORD reason, LPVOID reserved )
     }
     return TRUE;
 }
+
+int WINAPI PIDGenSimpA(LPCSTR str, int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
+{
+    FIXME("%s,%d,%d,%d,%d,%d,%d,%d,%d\n", debugstr_a(str), p1, p2, p3, p4, p5, p6, p7, p8);
+
+    return 0;
+}
