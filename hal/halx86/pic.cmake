95e85de7d6f1830eb36ca0ef2c185129e8684a7e
Vadim Galyant <vgal@rambler.ru>
2018-08-09 04:26:27 +0900

    [HALX86] Move files from "UP" dir to "PIC" and "MINIHAL" dirs, correcting CMakeLists files.

--- hal/halx86/pic.cmake
+++ hal/halx86/pic.cmake
@@ -2,13 +2,13 @@
 list(APPEND HAL_PIC_ASM_SOURCE
     generic/systimer.S
     generic/trap.S
-    up/pic.S)
+    pic/pic.S)
 
 list(APPEND HAL_PIC_SOURCE
     generic/profil.c
     generic/timer.c
-    up/halinit_up.c
-    up/pic.c)
+    pic/halinit_pic.c
+    pic/pic.c)
 
 add_asm_files(lib_hal_pic_asm ${HAL_PIC_ASM_SOURCE})
 add_object_library(lib_hal_pic ${HAL_PIC_SOURCE} ${lib_hal_pic_asm})
