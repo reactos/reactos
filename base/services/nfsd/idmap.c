67017fb1adc5479ab5aa145af198e5a11e18616f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-08-22 15:39:45 +0900

    improve idmap.c

--- base/services/nfsd/idmap.c
+++ base/services/nfsd/idmap.c
@@ -73,6 +73,12 @@ struct idmap_lookup {
     const void *value;
 };
 
+
+#ifndef __REACTOS__
+/* configuration */
+static const char CONFIG_FILENAME[] = "C:\\ReactOS\\System32\\drivers\\etc\\ms-nfs41-idmap.conf";
+#endif
+
 struct idmap_config {
     /* ldap server information */
     char hostname[NFS41_HOSTNAME_LEN+1];
@@ -357,7 +363,9 @@ static int config_init(
     struct idmap_config *config)
 {
     int status;
+#ifdef __REACTOS__
     char config_path[MAX_PATH];
+#endif
 
     /* load default values */
     status = config_defaults(config);
@@ -366,13 +374,29 @@ static int config_init(
         goto out;
     }
 
-    GetSystemDirectoryA(config_path, ARRAYSIZE(config_path));
-    strcat(config_path, "\\drivers\\etc\\ms-nfs41-idmap.conf");
+#ifdef __REACTOS__
+    if (GetSystemDirectoryA(config_path, ARRAYSIZE(config_path)))
+    {
+        StringCchCatA(config_path, ARRAYSIZE(config_path), "\\drivers\\etc\\ms-nfs41-idmap.conf");
+    }
+    else
+    {
+        StringCchCopyA(config_path, ARRAYSIZE(config_path), "C:\\ReactOS\\system32\\drivers\\etc\\ms-nfs41-idmap.conf");
+    }
+#endif
 
     /* load configuration from file */
+#ifdef __REACTOS__
     status = config_load(config, config_path);
+#else
+    status = config_load(config, CONFIG_FILENAME);
+#endif
     if (status) {
+#ifdef __REACTOS__
         eprintf("config_load('%s') failed with %d\n", config_path, status);
+#else
+        eprintf("config_load('%s') failed with %d\n", CONFIG_FILENAME, status);
+#endif
         goto out;
     }
 out:
