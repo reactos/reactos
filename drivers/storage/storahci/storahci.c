c0bba59bf36abc948357896d735260d5b5e93981
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-08-27 13:22:32 +0900

    [DRIVERS] fix MSVC C4700 error of storahci

--- drivers/storage/storahci/storahci.c
+++ drivers/storage/storahci/storahci.c
@@ -1503,11 +1503,6 @@ AhciActivatePort (
     // Bits in this field shall only be set to ‘1’ by software when PxCMD.ST is set to ‘1’
     cmd.Status = StorPortReadRegisterUlong(AdapterExtension, &PortExtension->Port->CMD);
 
-    if (cmd.ST == 0) // PxCMD.ST == 0
-    {
-        return;
-    }
-
     // get the lowest set bit
     tmp = QueueSlots & (QueueSlots - 1);
 
