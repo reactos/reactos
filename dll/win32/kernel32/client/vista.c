87ec3bcf2677e62a30159a176f4093ba32973365
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2018-09-12 04:06:42 +0200

    [KERNEL32] Fix 2 'unused-but-set-variable' when compiling as NT6+
    {{
    .../dll/win32/kernel32/client/vista.c: In function 'SleepConditionVariableCS':
    .../dll/win32/kernel32/client/vista.c:44:20: error: variable 'TimeOutPtr' set but not used [-Werror=unused-but-set-variable]
    .../dll/win32/kernel32/client/vista.c: In function 'SleepConditionVariableSRW':
    .../dll/win32/kernel32/client/vista.c:79:20: error: variable 'TimeOutPtr' set but not used [-Werror=unused-but-set-variable]
    }}
    
    CORE-12596

--- dll/win32/kernel32/client/vista.c
+++ dll/win32/kernel32/client/vista.c
@@ -40,6 +40,7 @@ SleepConditionVariableCS(IN OUT PCONDITION_VARIABLE ConditionVariable,
                          IN DWORD dwMilliseconds)
 {
     NTSTATUS Status = STATUS_SUCCESS;
+#if 0
     LARGE_INTEGER TimeOut;
     PLARGE_INTEGER TimeOutPtr = NULL;
 
@@ -49,7 +50,6 @@ SleepConditionVariableCS(IN OUT PCONDITION_VARIABLE ConditionVariable,
         TimeOutPtr = &TimeOut;
     }
 
-#if 0
     Status = RtlSleepConditionVariableCS((PRTL_CONDITION_VARIABLE)ConditionVariable,
                                          (PRTL_CRITICAL_SECTION)CriticalSection,
                                          TimeOutPtr);
@@ -75,6 +75,7 @@ SleepConditionVariableSRW(IN OUT PCONDITION_VARIABLE ConditionVariable,
                           IN ULONG Flags)
 {
     NTSTATUS Status = STATUS_SUCCESS;
+#if 0
     LARGE_INTEGER TimeOut;
     PLARGE_INTEGER TimeOutPtr = NULL;
 
@@ -84,7 +85,6 @@ SleepConditionVariableSRW(IN OUT PCONDITION_VARIABLE ConditionVariable,
         TimeOutPtr = &TimeOut;
     }
 
-#if 0
     Status = RtlSleepConditionVariableSRW((PRTL_CONDITION_VARIABLE)ConditionVariable,
                                           (PRTL_SRWLOCK)SRWLock,
                                           TimeOutPtr,
