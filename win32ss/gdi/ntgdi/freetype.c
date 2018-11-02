de622c4b25640c9142b89cbc55fd86a0bbb97449
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-11-02 16:44:35 +0900

    add check (cbBuffer < sizeof(UNICODE_NULL))

--- win32ss/gdi/ntgdi/freetype.c
+++ win32ss/gdi/ntgdi/freetype.c
@@ -745,6 +745,10 @@ static VOID
 IntUnicodeStringToBuffer(LPWSTR pszBuffer, USHORT cbBuffer, const UNICODE_STRING *pString)
 {
     USHORT cbLength = pString->Length;
+
+    if (cbBuffer < sizeof(UNICODE_NULL))
+        return;
+
     if (cbLength + sizeof(UNICODE_NULL) > cbBuffer)
         cbLength = cbBuffer - sizeof(UNICODE_NULL);
 
