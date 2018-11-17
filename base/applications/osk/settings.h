34c812365f81686c99f0c2ad2602da712617183e
Bișoc George <fraizeraust99@gmail.com>
2018-11-17 21:27:33 +0100

    [OSK] Implement the welcome box (#1007)
    In Windows XP, both in On-Screen Keyboard and in Magnify there is a welcome box which is shown upon startup of the application. This feature is already implemented for Magnify in ReactOS which however in On-Screen Keyboard is still unimplemented.

---
+++ base/applications/osk/settings.h
@@ -0,0 +1,15 @@
+﻿/*
+ * PROJECT:         ReactOS On-Screen Keyboard
+ * LICENSE:         GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
+ * PURPOSE:         Settings header file with function prototypes
+ * COPYRIGHT:       Copyright 2018 Bișoc George (fraizeraust99 at gmail dot com)
+ */
+
+#ifndef SETTINGS_OSK_H
+#define SETTINGS_OSK_H
+
+BOOL LoadDataFromRegistry(VOID);
+BOOL SaveDataToRegistry(VOID);
+INT_PTR CALLBACK OSK_WarningProc(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lParam);
+
+#endif // SETTINGS_OSK_H
