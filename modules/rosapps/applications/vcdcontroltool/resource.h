10ce523a2c81e7f648627b3ccbda6f678b12ad66
Pierre Schweitzer <pierre@reactos.org>
2018-12-11 14:24:49 +0100

    [VCDCONTROLTOOL] Add a GUI app to manage virtual CD-ROM devices
    This is just an open sourced version of the tool provided by MS along
    with the driver.
    It will avoid users fighting with the CLI tool (that was doing the job
    though ;-)).
    
    Looks like that: https://twitter.com/HeisSpiter/status/1072482763348107264
    
    Nota 1: it doesn't have an icon
    Nota 2: code may be ugly, I'm not a umode guy! ;-)

---
+++ modules/rosapps/applications/vcdcontroltool/resource.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define IDD_MAINWINDOW    200
+#define IDD_MOUNTWINDOW   201
+#define IDD_DRIVERWINDOW  202
+
+#define IDC_STATIC        -1
+#define IDC_MAINOK        1001
+#define IDC_MAINDEVICES   1002
+#define IDC_MAINSECTORS   1003
+#define IDC_MAINSIZE      1004
+#define IDC_MAINFREE      1005
+#define IDC_MAINTOTAL     1006
+#define IDC_MAINADD       1007
+#define IDC_MAINREMOVE    1008
+#define IDC_MAINMOUNT     1009
+#define IDC_MAINEJECT     1010
+#define IDC_MAINREMOUNT   1011
+#define IDC_MAINCONTROL   1012
+#define IDC_MOUNTOK       1013
+#define IDC_MOUNTCANCEL   1014
+#define IDC_MOUNTIMAGE    1015
+#define IDC_MOUNTUDF      1016
+#define IDC_MOUNTJOLIET   1017
+#define IDC_MOUNTPERSIST  1018
+#define IDC_DRIVEROK      1019
+#define IDC_DRIVERINSTALL 1020
+#define IDC_DRIVERSTART   1021
+#define IDC_DRIVERSTOP    1022
+#define IDC_DRIVERREMOVE  1023
+#define IDC_DRIVERINFO    1024
+
+#define IDS_DRIVE         2000
+#define IDS_MAPPEDIMAGE   2001
+#define IDS_NOMOUNTED     2002
+#define IDS_NONE          2003
+#define IDS_FILTER        2004
