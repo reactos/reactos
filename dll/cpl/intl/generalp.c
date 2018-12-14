eee5e42c1c2e496c5d794e6a8c35899084fd8c22
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-12-14 17:01:19 +0900

    improve

--- dll/cpl/intl/generalp.c
+++ dll/cpl/intl/generalp.c
@@ -1332,12 +1332,12 @@ PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
     {
         case PSCB_INITIALIZED:
         {
-            hIcon = LoadIconW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDC_CPLICON));
+            hIcon = LoadIconW(hApplet, MAKEINTRESOURCEW(IDC_CPLICON));
             SendMessageW(hwndDlg, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-            return TRUE;
+            break;
         }
     }
-    return TRUE;
+    return 0;
 }
 
 INT_PTR
