2c5b9ca518590b9daff7fc1130139ebf8e3ad99f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-12-14 18:00:22 +0900

    comments

--- base/shell/explorer/trayprop.cpp
+++ base/shell/explorer/trayprop.cpp
@@ -248,6 +248,7 @@ public:
 static int CALLBACK
 PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
 {
+    // NOTE: This callback is needed to set large icon correctly.
     HICON hIcon;
     switch (uMsg)
     {
