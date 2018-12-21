f42a6c16f2565e2c73ab5c8e3c4ade20098c92c8
Pierre Schweitzer <pierre@reactos.org>
2018-12-21 19:05:07 +0100

    [IPHLPAPI_APITEST] Add tests for GetExtendedUdpTable

--- modules/rostests/apitests/iphlpapi/testlist.c
+++ modules/rostests/apitests/iphlpapi/testlist.c
@@ -4,6 +4,7 @@
 #include <apitest.h>
 
 extern void func_GetExtendedTcpTable(void);
+extern void func_GetExtendedUdpTable(void);
 extern void func_GetInterfaceName(void);
 extern void func_GetNetworkParams(void);
 extern void func_GetOwnerModuleFromTcpEntry(void);
@@ -13,6 +14,7 @@ extern void func_SendARP(void);
 const struct test winetest_testlist[] =
 {
     { "GetExtendedTcpTable",        func_GetExtendedTcpTable },
+    { "GetExtendedUdpTable",        func_GetExtendedUdpTable },
     { "GetInterfaceName",           func_GetInterfaceName },
     { "GetNetworkParams",           func_GetNetworkParams },
     { "GetOwnerModuleFromTcpEntry", func_GetOwnerModuleFromTcpEntry },
