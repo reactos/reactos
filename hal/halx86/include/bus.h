reactos/reactos

commit 71fefa32db013317842fd9224b63bcf5f72f2e32

[NDK][NTOS] Add global definition of INIT_FUNCTION/INIT_SECTION (#779)
* Add an NDK header to define INIT_FUNCTION/INIT_SECTION globally
* Use _declspec(allocate(x)) and _declspec(code_seg(x)) on MSVC versions that support it
* Use INIT_FUNCTION on functions only and INIT_SECTION on data only (required by MSVC)
* Place INIT_FUNCTION before the return type (required by MSVC)
* Make sure declarations and implementations share the same modifiers (required by MSVC)
* Add a global linker option to suppress warnings about defined but unused INIT section
* Merge INIT section into .text in freeldr

hal/halx86/include/bus.h
@@ -282,6 +282,7 @@ extern PCI_CONFIG_HANDLER PCIConfigHandler;
 extern PCI_CONFIG_HANDLER PCIConfigHandlerType1;
 extern PCI_CONFIG_HANDLER PCIConfigHandlerType2;
 
+INIT_FUNCTION
 PPCI_REGISTRY_INFO_INTERNAL
 NTAPI
 HalpQueryPciRegistryInfo(
@@ -425,12 +426,14 @@ HalpSetCmosData(
     IN ULONG Length
 );
 
+INIT_FUNCTION
 VOID
 NTAPI
 HalpInitializePciBus(
     VOID
 );
 
+INIT_FUNCTION
 VOID
 NTAPI
 HalpInitializePciStubs(
@@ -470,6 +473,7 @@ HalpFindBusAddressTranslation(
     IN BOOLEAN NextBus
 );
 
+INIT_FUNCTION
 VOID
 NTAPI
 HalpRegisterPciDebuggingDeviceInfo(
@@ -606,6 +610,7 @@ HalpTranslateIsaBusAddress(
     OUT PPHYSICAL_ADDRESS TranslatedAddress
 );
 
+INIT_FUNCTION
 ULONG
 NTAPI
 HalpGetSystemInterruptVector(

hal/halx86/legacy/bus/sysbus.c
@@ -139,6 +139,7 @@ HalpGetRootInterruptVector(IN ULONG BusInterruptLevel,
     return SystemVector;
 }
 
+INIT_FUNCTION
 ULONG
 NTAPI
 HalpGetSystemInterruptVector(IN PBUS_HANDLER BusHandler,

commit 8275445abdaa46d9a6ca8a856101834c377e2937

[HALX86] Removed reduntant INIT_FUNCTION attribute from HalpGetSystemInterruptVector. This fixes livecd crashes started since 71fefa32db013317842fd9224b63bcf5f72f2e32

hal/halx86/include/bus.h
@@ -610,7 +610,6 @@ HalpTranslateIsaBusAddress(
     OUT PPHYSICAL_ADDRESS TranslatedAddress
 );
 
-INIT_FUNCTION
 ULONG
 NTAPI
 HalpGetSystemInterruptVector(

hal/halx86/legacy/bus/sysbus.c
@@ -139,7 +139,6 @@ HalpGetRootInterruptVector(IN ULONG BusInterruptLevel,
     return SystemVector;
 }
 
-INIT_FUNCTION
 ULONG
 NTAPI
 HalpGetSystemInterruptVector(IN PBUS_HANDLER BusHandler,