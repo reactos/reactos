c3cbd1963b67d004e2d5b3b4d22cb749ab00c5f3
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-01-14 21:48:21 +0900

    fix XFORMOBJ_iSetXform 3

--- win32ss/gdi/ntgdi/xformobj.c
+++ win32ss/gdi/ntgdi/xformobj.c
@@ -150,17 +150,19 @@ XFORMOBJ_iSetXform(
     IN const XFORML *pxform)
 {
     PMATRIX pmx = XFORMOBJ_pmx(pxo);
-    FLOATOBJ ef1, ef2;
+    FLOATOBJ ef1, ef2, ef3, ef4;
 
     /* Check parameters */
     if (!pxo || !pxform) return DDI_ERROR;
 
     /* eM11 * eM22 - eM12 * eM21 != 0 */
     FLOATOBJ_SetFloat(&ef1, pxform->eM11);
-    FLOATOBJ_MulFloat(&ef1, pxform->eM22);
-    FLOATOBJ_SetFloat(&ef2, pxform->eM12);
-    FLOATOBJ_MulFloat(&ef2, pxform->eM21);
-    if (FLOATOBJ_Equal(&ef1, &ef2))
+    FLOATOBJ_SetFloat(&ef2, pxform->eM22);
+    FLOATOBJ_Mul(&ef1, &ef2);
+    FLOATOBJ_SetFloat(&ef3, pxform->eM12);
+    FLOATOBJ_SetFloat(&ef4, pxform->eM21);
+    FLOATOBJ_Mul(&ef3, &ef4);
+    if (FLOATOBJ_Equal(&ef1, &ef3))
         return DDI_ERROR;
 
     /* Copy members */
