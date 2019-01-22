e32d5a8b763715fcee79d6be3168f496b68ff372
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-01-22 20:59:38 +0900

    [APITESTS][WIN32KNT_APITEST] Improve NtGdiCreateCompatibleDC testcase (#1281)
    Use ok, ok_int and ok_ptr macros instead of obsolete TEST macros.

--- modules/rostests/apitests/win32nt/ntgdi/NtGdiCreateCompatibleDC.c
+++ modules/rostests/apitests/win32nt/ntgdi/NtGdiCreateCompatibleDC.c
@@ -14,21 +14,20 @@ START_TEST(NtGdiCreateCompatibleDC)
 
 	/* Test if aa NULL DC is accepted */
 	hDC = NtGdiCreateCompatibleDC(NULL);
-	TEST(hDC != NULL);
+	ok(hDC != NULL, "hDC was NULL.\n");
 
 	/* We select a nwe palette. Note: SelectObject doesn't work with palettes! */
 	hObj = SelectPalette(hDC, GetStockObject(DEFAULT_PALETTE), 0);
 	/* The old palette should be GetStockObject(DEFAULT_PALETTE) */
-	TEST(hObj == GetStockObject(DEFAULT_PALETTE));
+	ok_ptr(hObj, GetStockObject(DEFAULT_PALETTE));
 
 	/* The default bitmap should be GetStockObject(21) */
 	hObj = SelectObject(hDC, GetStockObject(21));
-	TEST(hObj == GetStockObject(21));
+	ok_ptr(hObj, GetStockObject(21));
 
 	/* The default pen should be GetStockObject(BLACK_PEN) */
 	hObj = SelectObject(hDC, GetStockObject(WHITE_PEN));
-	TEST(hObj == GetStockObject(BLACK_PEN));
+	ok_ptr(hObj, GetStockObject(BLACK_PEN));
 
-	TEST(NtGdiDeleteObjectApp(hDC) != 0);
+	ok(NtGdiDeleteObjectApp(hDC) != 0, "NtGdiDeleteObjectApp(hDC) was zero.\n");
 }
-
