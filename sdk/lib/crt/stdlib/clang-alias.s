dc77e7d37904fc9f3170379481fa2ff185e7d555
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-02-03 23:36:49 +0100

    [CRT] Use alias names for rot functions when compiling with Clang-CL
    Clang-CL does not support #pragma function like CL. The alternative to this approach is to disable all intrinsics for this single source file, but that is not trivial to do, as it can only be done by removing a compiler switch, which we have no CMake support for. Therefore this solution is simpler and as good.

---
+++ sdk/lib/crt/stdlib/clang-alias.s
@@ -0,0 +1,22 @@
+#include <asm.inc>
+
+.code
+
+MACRO(DEFINE_ALIAS, alias, orig, type)
+EXTERN &orig:&type
+ALIAS <&alias> = <&orig>
+ENDM
+
+#ifdef _M_X64
+DEFINE_ALIAS _rotl, __function_rotl
+DEFINE_ALIAS _rotr, __function_rotr
+DEFINE_ALIAS _lrotl, __function_lrotl
+DEFINE_ALIAS _lrotr, __function_lrotr
+#else
+DEFINE_ALIAS __rotl, ___function_rotl
+DEFINE_ALIAS __rotr, ___function_rotr
+DEFINE_ALIAS __lrotl, ___function_lrotl
+DEFINE_ALIAS __lrotr, ___function_lrotr
+#endif
+
+END
