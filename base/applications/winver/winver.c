3747699735bfaf5175e62425e84d90651897b009
Carlo-Bramini <carlo_bramini@users.sourceforge.net>
2019-03-19 21:23:32 +0100

    [WINVER] Fix WINVER theming. CORE-13343 (#1428)
    Use and initialize common controls for theming support,
    and include a manifest as well.

--- base/applications/winver/winver.c
+++ base/applications/winver/winver.c
@@ -7,14 +7,23 @@
 #include <stdarg.h>
 #include <windef.h>
 #include <winbase.h>
+#include <winuser.h>
+#include <commctrl.h>
 #include <shellapi.h>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
+    INITCOMMONCONTROLSEX iccx;
+
     UNREFERENCED_PARAMETER(hInstance);
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nCmdShow);
+
+    /* Initialize common controls */
+    iccx.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    iccx.dwICC = ICC_STANDARD_CLASSES | ICC_WIN95_CLASSES;
+    InitCommonControlsEx(&iccx);
 
     return ShellAboutW(NULL, L"ReactOS", NULL, NULL);
 }
