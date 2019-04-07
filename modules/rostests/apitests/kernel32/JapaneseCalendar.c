f74f41e18e0fa87d229d34dcec2d747efd899bec
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-04-07 19:30:26 +0900

    Reiwa characters more added

--- modules/rostests/apitests/kernel32/JapaneseCalendar.c
+++ modules/rostests/apitests/kernel32/JapaneseCalendar.c
@@ -39,7 +39,9 @@ START_TEST(JapaneseCalendar)
     static const WCHAR s_szHeisei31[] = {0x5E73, 0x6210, '3', '1', 0};  // L"\u5E73\u6210" L"31"
     static const WCHAR s_szOneCharHeisei1[] = {0x337B, 0};   // L"\u337B"
     static const WCHAR s_szOneCharHeisei2[] = {0x5E73, 0};   // L"\u5E73"
-    static const WCHAR s_szOneCharReiwa[] = {0x32FF, 0};   // L"\u32FF"
+    static const WCHAR s_szOneCharReiwa1[] = {0x32FF, 0};   // L"\u32FF"
+    static const WCHAR s_szOneCharReiwa2[] = {0x4EE4, 0};   // L"\u4EE4"
+    static const WCHAR s_szOneCharReiwa3[] = {0xF9A8, 0};   // L"\uF9A8"
     static const WCHAR s_szWareki[] = {0x548C, 0x66A6, 0};  // L"\u548C\u66A6"
     static const WCHAR s_szNen[] = {0x5E74, 0};             // L"\u5E74"
 
@@ -196,7 +198,9 @@ START_TEST(JapaneseCalendar)
         ok(/* WinXP */ (szTextW[0] == 0x7F && szTextW[1] == 0) ||
            /* Win10? */ lstrcmpiW(szTextW, s_szOneCharHeisei1) == 0 ||
            /* Win10? */ lstrcmpiW(szTextW, s_szOneCharHeisei2) == 0 ||
-           /* Win10? */ lstrcmpiW(szTextW, s_szOneCharReiwa) == 0,
+           /* Win10? */ lstrcmpiW(szTextW, s_szOneCharReiwa1) == 0 ||
+           /* Win10? */ lstrcmpiW(szTextW, s_szOneCharReiwa2) == 0 ||
+           /* Win10? */ lstrcmpiW(szTextW, s_szOneCharReiwa3) == 0,
            "szTextW: %04X %04X %04X\n", szTextW[0], szTextW[1], szTextW[2]);
 
         szTextW[0] = 0x7F;
