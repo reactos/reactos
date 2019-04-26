9aa8e7db173a4364e9e7ddc78076355011cd9ddb
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2019-04-26 02:32:06 +0200

    [ROSAUTOTEST] wmain(): Output "[ROSAUTOTEST] " in two cases
    'System uptime' and all CSimpleException.

--- modules/rostests/rosautotest/main.cpp
+++ modules/rostests/rosautotest/main.cpp
@@ -61,7 +61,7 @@ wmain(int argc, wchar_t* argv[])
 
         ss << endl
            << endl
-           << "System uptime " << setprecision(2) << fixed;
+           << "[ROSAUTOTEST] System uptime " << setprecision(2) << fixed;
         ss << ((float)GetTickCount()/1000) << " seconds" << endl;
         StringOut(ss.str());
         
@@ -91,8 +91,11 @@ wmain(int argc, wchar_t* argv[])
     }
     catch(CSimpleException& e)
     {
+        stringstream ss;
+
         // e.GetMessage() must include ending '\n'.
-        StringOut(e.GetMessage());
+        ss << "[ROSAUTOTEST] " << e.GetMessage();
+        StringOut(ss.str());
     }
     catch(CFatalException& e)
     {
