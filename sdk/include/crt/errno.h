81bdf4e36023aa9cd7d71ccdb9a6d1d09666406f
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-04-28 11:28:24 +0200

    [CRT] Add missing posix error codes
    These are required by latest mingw-w64 stl and we are mixing this with our crt headers.
    Define _CRT_NO_POSIX_ERROR_CODES in all modules that define their own constants.

--- sdk/include/crt/errno.h
+++ sdk/include/crt/errno.h
@@ -57,6 +57,50 @@ extern "C" {
 #define ENOSYS 40
 #define ENOTEMPTY 41
 
+#ifndef _CRT_NO_POSIX_ERROR_CODES
+  #define EADDRINUSE 100
+  #define EADDRNOTAVAIL 101
+  #define EAFNOSUPPORT 102
+  #define EALREADY 103
+  #define EBADMSG 104
+  #define ECANCELED 105
+  #define ECONNABORTED 106
+  #define ECONNREFUSED 107
+  #define ECONNRESET 108
+  #define EDESTADDRREQ 109
+  #define EHOSTUNREACH 110
+  #define EIDRM 111
+  #define EINPROGRESS 112
+  #define EISCONN 113
+  #define ELOOP 114
+  #define EMSGSIZE 115
+  #define ENETDOWN 116
+  #define ENETRESET 117
+  #define ENETUNREACH 118
+  #define ENOBUFS 119
+  #define ENODATA 120
+  #define ENOLINK 121
+  #define ENOMSG 122
+  #define ENOPROTOOPT 123
+  #define ENOSR 124
+  #define ENOSTR 125
+  #define ENOTCONN 126
+  #define ENOTRECOVERABLE 127
+  #define ENOTSOCK 128
+  #define ENOTSUP 129
+  #define EOPNOTSUPP 130
+  #define EOTHER 131
+  #define EOVERFLOW 132
+  #define EOWNERDEAD 133
+  #define EPROTO 134
+  #define EPROTONOSUPPORT 135
+  #define EPROTOTYPE 136
+  #define ETIME 137
+  #define ETIMEDOUT 138
+  #define ETXTBSY 139
+  #define EWOULDBLOCK 140
+#endif
+
 #ifndef RC_INVOKED
 #if !defined(_SECURECRT_ERRCODE_VALUES_DEFINED)
 #define _SECURECRT_ERRCODE_VALUES_DEFINED
