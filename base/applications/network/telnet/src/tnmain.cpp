194180b1be60d959c7e14a92dd2ef6cbe1061354
Timo Kreuzer <timo.kreuzer@reactos.org>
2019-04-28 17:22:34 +0200

    [TELNET] Add missing curly braces to for-statement
    Fixes GCC 8 warning:
    base/applications/network/telnet/src/tnmain.cpp:171:8: error: this 'for' clause does not guard... [-Werror=misleading-indentation]
            for (j = cursor; j >= 0; j--)
            ^~~
    base/applications/network/telnet/src/tnmain.cpp:174:9: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'for'
             for (k = --j; k >= 0; k--)
             ^~~

--- base/applications/network/telnet/src/tnmain.cpp
+++ base/applications/network/telnet/src/tnmain.cpp
@@ -168,14 +168,18 @@ struct cmdHistory * cfgets (char *buf, unsigned int length, struct cmdHistory *c
 						if(InputRecord.Event.KeyEvent.dwControlKeyState &
 							(LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED)) {
 							int j, k;
-							for (j = cursor; j >= 0; j--)
-								if (buf[j-1] != ' ')
-									break;
-								for (k = --j; k >= 0; k--)
-									if (buf[k] == ' ' || k == 0) {
-										cursor = !k ? k : ++k;
-										break;
-									}
+                            for (j = cursor; j >= 0; j--)
+                            {
+                                if (buf[j - 1] != ' ')
+                                    break;
+                                for (k = --j; k >= 0; k--)
+                                {
+                                    if (buf[k] == ' ' || k == 0) {
+                                        cursor = !k ? k : ++k;
+                                        break;
+                                    }
+                                }
+                            }
 						} else
 							cursor--;
 						MustRefresh = 1;
