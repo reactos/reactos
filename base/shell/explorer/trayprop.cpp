0e0e491b74e92a1ea84ca615f60bb4830a6b0cff
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2019-08-13 10:53:30 +0100

    [BUGFIX] Properties dialog box position (Jira CORE 9008)

--- base/shell/explorer/trayprop.cpp
+++ base/shell/explorer/trayprop.cpp
@@ -269,149 +269,6 @@ public:
         return PSNRET_NOERROR;
     }
 };
- 
- /**
-*    Autor:      Jose Carlos Jesus 
-*    email:      zecarlos1957@hotmail.com
-*    Project:    ReactOS  
-*    date:       12-08-2019   
-*    input - 
-*               LPARAM lParam : Pointer to Resource Dialog TEMPLATE 
-*               DWORD id :       Resource ID from object to be extracted
-*                                  id = 0 -> Dialog rectangle
-*                                  id = # -> Object rectangle
-*               LPVOID data:    Pointer to buffer that receive data
-*
-*    Output    return pointer to RECT object or NULL if ID not found
-*                     If object not found, show a Dialog with Title, ClassName, ResourceID,
-*                     and Rectangle structure from all objects inside this Resource
-*
-*      (NOTE) For now, just work with DLGTEMPLATEEX resource type
-*/
-
-WORD *TEMPLATE_ExtractRect(LPARAM lParam, DWORD id, LPVOID data)
-{
-    WCHAR buf[1024];
-    WCHAR *aux = buf;
-    WORD _id;
-    WORD len;
-    RECT rc;
-    WORD *ptr = reinterpret_cast<WORD*>(lParam);
-    DWORD signature = *((DWORD*)ptr);
-
-    if(signature != 0xffff0001)
-        return NULL;
-
-    ((*ptr == 1) && (*(ptr + 1) == 0xffff)) ? ptr += 7 : ptr += 4;
-    BOOL font = ((*ptr++ & DS_SETFONT) != 0) ;
-    WORD nItems = *ptr++;
-
-    rc.left = *ptr;
-    rc.top = *(ptr + 1);
-    rc.right = *(ptr + 2);
-    rc.bottom = *(ptr + 3);
-
-    if(id == 0)
-    {
-        ((RECT *)data)->left = *ptr;
-        ((RECT *)data)->top = *(ptr + 1);
-        ((RECT *)data)->right = *(ptr + 2);
-        ((RECT *)data)->bottom = *(ptr + 3);
-        return ptr;
-    }
-
-    ptr += 4;
-
-    if (*ptr == 0)               /// No menu id
-        ptr++;           
-    else if (*ptr == 0xffff)     /// menu # id
-        ptr += 2;        
-    else 
-        ptr += (lstrlenW((WCHAR*)ptr) + 1);
-
-    if (*ptr == 0)               /// Default Dialog ClassName id
-        ptr++;  
-    else if(*ptr == 0xffff)      /// ClassName # id
-        ptr += 2;   
-    else                         /// Unicode ClassName
-        ptr += (lstrlenW((WCHAR*)ptr) + 1);
-
-    if (*ptr == 0)               /// No WindowTitle
-        ptr++;
-    else                         /// Unicode WindowTitle
-    {
-        lstrcpyW(aux, (WCHAR*)ptr);
-        int n = lstrlenW((WCHAR*)ptr);
-        wsprintfW(aux + n,L" Rect(%d, %d, %d, %d)\n", rc.left, rc.top, rc.right, rc.bottom);
-        aux += lstrlenW(aux);
-        ptr += (n + 1);
-    }
-
-    if (font)
-    {
-        ptr += 3;
-        wsprintfW(aux, L"%s\n", (WCHAR*)ptr);
-        len = lstrlenW((WCHAR*)ptr);
-        aux += (len + 1);
-        ptr += (len + 1);
-    }
-
-    for (WORD i = 0; i < nItems; i++)
-    {
-        if ((DWORD)ptr % 4) ptr++;  /// aligned DWORD
-
-        ptr += 6;
-
-        rc.left = *ptr;
-        rc.top = *(ptr + 1);
-        rc.right = *(ptr + 2);
-        rc.bottom = *(ptr + 3);
-
-        _id = *(ptr + 4);
-        if (id == _id)
-        {
-            ((RECT*)data)->left = rc.left;
-            ((RECT*)data)->top = rc.top;
-            ((RECT*)data)->right = rc.right;
-            ((RECT*)data)->bottom = rc.bottom;
-            return ptr;
-        }
-        ptr += 6;
-        if (*ptr == 0xffff)
-        {
-            wsprintfW(aux, L" Class #0x%x ", *(ptr + 1));
-            aux += lstrlenW(aux);
-            ptr += 2;
-        }
-        else
-        {
-            wsprintfW(aux, L" Class %s ",  ptr);
-            aux += lstrlenW(aux);
-            ptr += (lstrlenW((WCHAR*)ptr) + 1);
-        }
-
-        if(*ptr == 0xffff)
-        {
-            wsprintfW(aux, L" #0x%x _ID %d Rect(%d, %d, %d, %d)\n", *(ptr+1), _id,rc.left,rc.top, rc.right, rc.bottom);
-            aux += lstrlenW(aux);
-            ptr += 2;
-        }
-        else
-        {
-            wsprintfW(aux,L" %s ID. %d Rect(%d, %d, %d, %d)\n",(WCHAR*)ptr, _id,rc.left,rc.top, rc.right, rc.bottom);
-            aux += lstrlenW(aux);
-            ptr += (lstrlenW((WCHAR*)ptr )+ 1);
-        }
-        if (*ptr)
-            ptr += *ptr;
-        else
-            ptr++;
-   }
-   MessageBoxW(0, buf, L"ID not found", MB_OK|MB_ICONEXCLAMATION);
-   return NULL;
-}
-
-#define IDC_TABCONTROL 12320
 
 static int CALLBACK
 PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
@@ -420,43 +277,20 @@ PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
     HICON hIcon;
     switch (uMsg)
     {
-        case PSCB_PRECREATE:
-        {
-            RECT rc, aux;
-            HRSRC hRes;
-            LPVOID Template;
-            APPBARDATA tbData;
-
-            if ((hRes = FindResourceW(NULL, MAKEINTRESOURCE(IDD_TASKBARPROP_TASKBAR),
-                                     (LPWSTR)RT_DIALOG)) != NULL)
-                Template = LoadResource(NULL, hRes);
-            else
-                return 0;
-
-            WORD *rcObject = TEMPLATE_ExtractRect(lParam, 0, &rc);
-            if (rcObject == NULL) return FALSE;
-
-            WORD oldH = rc.bottom;
-            TEMPLATE_ExtractRect((LPARAM)Template, 0, &rc);
-            TEMPLATE_ExtractRect(lParam, IDC_TABCONTROL, &aux);
-            AdjustWindowRect(&rc, WS_POPUPWINDOW, 0); 
-            
-            tbData.cbSize = sizeof(APPBARDATA);
-            tbData.hWnd = FindWindowW(L"Shell_TrayWnd", NULL);
-            SHAppBarMessage(ABM_GETTASKBARPOS, &tbData);
-
-            WORD tbHeight = tbData.rc.bottom - tbData.rc.top;
-            tbHeight = (tbHeight * 8) / HIWORD(GetDialogBaseUnits());
-
-            *(rcObject + 2) = rc.right;
-            *(rcObject + 3) = rc.bottom + (oldH - aux.bottom) + tbHeight;
-
-            return 0;
-        }
         case PSCB_INITIALIZED:
         {
+            RECT pos;
+            RECT rcWork;
             hIcon = LoadIconW(hExplorerInstance, MAKEINTRESOURCEW(IDI_STARTMENU));
             SendMessageW(hwndDlg, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+ 
+            GetWindowRect(hwndDlg,&pos);
+            SystemParametersInfo(SPI_GETWORKAREA, 0, &rcWork, 0);
+            if (pos.bottom > rcWork.bottom) 
+                pos.top -= (pos.bottom - rcWork.bottom + 2);
+            if (pos.right > rcWork.right)
+                pos.left -= (pos.right - rcWork.right + 2);
+            SetWindowPos(hwndDlg, 0, pos.left, pos.top, 0, 0, SWP_NOSIZE);
             break;
         }
     }
