ff1a0da8e59f21ad63f893aa8c521016551e6500
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-08-15 13:52:03 +0900

    [WININET_APITEST] Add Redirection testcase

--- modules/rostests/apitests/wininet/testlist.c
+++ modules/rostests/apitests/wininet/testlist.c
@@ -4,10 +4,12 @@
 #include <apitest.h>
 
 extern void func_InternetOpen(void);
+extern void func_Redirection(void);
 
 const struct test winetest_testlist[] =
 {
     { "InternetOpen", func_InternetOpen },
+    { "Redirection", func_Redirection },
 
     { 0, 0 }
 };
