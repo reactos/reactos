7464241adabfbdfec5a058697632a8d3938479fb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-08-15 17:50:57 +0900

    [WININET_APITEST] Add Download testcase (#1832)
    CORE-16310

--- modules/rostests/apitests/wininet/testlist.c
+++ modules/rostests/apitests/wininet/testlist.c
@@ -3,10 +3,12 @@
 #define STANDALONE
 #include <apitest.h>
 
+extern void func_Download(void);
 extern void func_InternetOpen(void);
 
 const struct test winetest_testlist[] =
 {
+    { "Download", func_Download },
     { "InternetOpen", func_InternetOpen },
 
     { 0, 0 }
