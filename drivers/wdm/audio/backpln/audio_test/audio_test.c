wf61694/5

[REACTOS] Fix MSVC printf format warnings

drivers/wdm/audio/backpln/audio_test/audio_test.c
@@ -110,7 +110,7 @@ TestKs()
     // Allocate a KS Pin Connection Request Structure
     //
     Length = sizeof(KSPIN_CONNECT) + sizeof(KSDATAFORMAT_WAVEFORMATEX);
-	printf("Length %ld KSPIN %u DATAFORMAT %u\n", Length, sizeof(KSPIN_CONNECT), sizeof(KSDATAFORMAT_WAVEFORMATEX));
+	printf("Length %ld KSPIN %zu DATAFORMAT %zu\n", Length, sizeof(KSPIN_CONNECT), sizeof(KSDATAFORMAT_WAVEFORMATEX));
     PinConnect = (PKSPIN_CONNECT)HeapAlloc(GetProcessHeap(), 0, Length);
     DataFormat = (PKSDATAFORMAT_WAVEFORMATEX)(PinConnect + 1);
 

reactos/reactos

drivers/wdm/audio/backpln/audio_test/audio_test.c
@@ -110,7 +110,7 @@ TestKs()
     // Allocate a KS Pin Connection Request Structure
     //
     Length = sizeof(KSPIN_CONNECT) + sizeof(KSDATAFORMAT_WAVEFORMATEX);
-	printf("Length %ld KSPIN %u DATAFORMAT %u\n", Length, sizeof(KSPIN_CONNECT), sizeof(KSDATAFORMAT_WAVEFORMATEX));
+	printf("Length %ld KSPIN %Iu DATAFORMAT %Iu\n", Length, sizeof(KSPIN_CONNECT), sizeof(KSDATAFORMAT_WAVEFORMATEX));
     PinConnect = (PKSPIN_CONNECT)HeapAlloc(GetProcessHeap(), 0, Length);
     DataFormat = (PKSDATAFORMAT_WAVEFORMATEX)(PinConnect + 1);
 