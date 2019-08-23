3b9508e98fc03708f9f46cd784684db9e68fbafd
Brock Mammen <brockmammen@gmail.com>
2019-08-23 15:51:43 -0500

    [SHELLFIND] Fix copyright headers

--- dll/win32/browseui/shellfind/CFindFolder.cpp
+++ dll/win32/browseui/shellfind/CFindFolder.cpp
@@ -1,3 +1,10 @@
+/*
+ * PROJECT:     ReactOS Search Shell Extension
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Search results folder
+ * COPYRIGHT:   Copyright 2019 Brock Mammen
+ */
+
 #include "CFindFolder.h"
 #include <exdispid.h>
 
--- dll/win32/browseui/shellfind/CFindFolder.h
+++ dll/win32/browseui/shellfind/CFindFolder.h
@@ -1,3 +1,10 @@
+/*
+ * PROJECT:     ReactOS Search Shell Extension
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Search results folder
+ * COPYRIGHT:   Copyright 2019 Brock Mammen
+ */
+
 #pragma once
 
 #include "shellfind.h"
