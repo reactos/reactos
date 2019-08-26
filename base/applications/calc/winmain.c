055c5a9bb7c54b05233aa4f5612d975a38b41208
Carlo Bramini <30959007+carlo-bramini@users.noreply.github.com>
2019-08-26 16:14:01 +0200

    [CALC] Enable support for HTMLHELP (#1877)
    Nowadays ReactOS includes support for htmlhelp.h and a working implementation of HHCTRL.OCX. Although current imported HHCTRL.OCX suffers of this issue:
    
    https://bugs.winehq.org/show_bug.cgi?id=47379
    See also CORE-15019
    
    + Revert 'intptr_t' to 'size_t'.

--- base/applications/calc/winmain.c
+++ base/applications/calc/winmain.c
@@ -894,7 +894,7 @@ static void delete_stat_item(int n)
 
 static char *ReadConversion(const char *formula)
 {
-    intptr_t len = strlen(formula);
+    size_t len = strlen(formula);
     char *str = (char *)malloc(len+3);
 
     if (str == NULL)
