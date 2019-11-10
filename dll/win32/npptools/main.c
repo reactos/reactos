1a3e41e7692f050b90a4d6b9b257e2509cde78ea
Amine Khaldi <amine.khaldi@reactos.org>
2019-11-10 14:08:38 +0100

    [NPPTOOLS] Sync with Wine Staging 4.18. CORE-16441

--- dll/win32/npptools/main.c
+++ dll/win32/npptools/main.c
@@ -16,7 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-#include "config.h"
 
 #include <stdarg.h>
 
