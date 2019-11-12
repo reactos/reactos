c0f340bca567c5ae6ab4906459b62e2ef26f226f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-11-12 16:47:36 +0900

    [SHELL32] Implement 'Open file location' of shortcut files (#2028)
    "Open file location" is a feature to open the location of the target of a shortcut file.
    Ideally we should probably use SHOpenFolderAndSelectItems here, but that is not 100% implemented in ros yet... CORE-12770

--- dll/win32/shell32/shresdef.h
+++ dll/win32/shell32/shresdef.h
@@ -227,6 +227,8 @@
 #define IDS_EJECT                339
 #define IDS_DISCONNECT           340
 
+#define IDS_OPENFILELOCATION     341
+
 #define IDS_MENU_EMPTY           34561
 
 /* Note: those strings are referenced from the registry */
