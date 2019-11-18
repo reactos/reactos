d0295f590754717aa12219acc5b7819ce4b98f15
Oleg Dubinskiy <oleg.dubinskij2013@yandex.ua>
2019-11-18 18:02:46 +0200

    Improvements based on suggestions

--- dll/win32/rpcrt4/rpc_async.c
+++ dll/win32/rpcrt4/rpc_async.c
@@ -159,22 +159,23 @@ RPC_STATUS WINAPI RpcAsyncCancelCall(PRPC_ASYNC_STATE pAsync, BOOL fAbortCall)
     return RPC_S_INVALID_ASYNC_HANDLE;
 }
 
+#ifdef __REACTOS__
 /***********************************************************************
  *           RpcGetAuthorizationContextForClient [RPCRT4.@]
  * 
- * Returns the Authz context for an RPC client.
+ * Called by RpcFreeAuthorizationContext to return the Authz context.
  * 
  * PARAMS
- *  ClientBinding       [I] Binding handle on the server that represents
- *                          a binding to a client.
- *  ImpersonateOnReturn [I] Directs the function to impersonate the client on return.
- *  Reserved1           [I] Reserved. Must be null.
- *  pExpirationTime     [I] Pointer to the expiration date and time of the token.
- *  Reserved2           [I] Reserved. Must be a LUID structure with each member set to zero.
- *  Reserved3           [I] Reserved. Must be zero.
- *  Reserved4           [I] Reserved. Must be null.
- *  pAuthzClientContext [I] Pointer to an AUTHZ_CLIENT_CONTEXT_HANDLE structure
- *                          that can be passed directly to Authz functions.
+ *  ClientBinding        [I] Binding handle, represents a binding to a client on the server.
+ *  ImpersonateOnReturn  [I] Directs this function to be represented the client on return.
+ *  Reserved1            [I] Reserved, equal to null.
+ *  expiration_time      [I] Points to the exact date and time when the token expires.
+ *  Reserved2            [I] Reserved, equal to a LUID structure which has a members,
+ *                           each of them is set to zero.
+ *  Reserved3            [I] Reserved, equal to zero.
+ *  Reserved4            [I] Reserved, equal to null.
+ *  authz_client_context [I] Points to an AUTHZ_CLIENT_CONTEXT_HANDLE structure
+ *                           that has direct pass to Authz functions.
  * 
  * RETURNS
  *  Success: RPC_S_OK.
@@ -185,20 +186,22 @@ WINAPI
 RpcGetAuthorizationContextForClient(RPC_BINDING_HANDLE ClientBinding,
                                     BOOL ImpersonateOnReturn,
                                     void * Reserved1,
-                                    PLARGE_INTEGER pExpirationTime,
+                                    PLARGE_INTEGER expiration_time,
                                     LUID Reserved2,
                                     DWORD Reserved3,
                                     PVOID Reserved4,
                                     PVOID *authz_client_context)
 {
-    FIXME("(%p, %s, %p, %p, %p, %d, %p, %p): stub\n",
+    FIXME("(%p, %s, %p, %p, (%u, %d), %u, %p, %p): stub\n",
           ClientBinding,
           ImpersonateOnReturn,
           Reserved1,
-          pExpirationTime,
-          Reserved2,
+          expiration_time,
+          Reserved2.HighPart,
+          Reserved2.LowPart,
           Reserved3,
           Reserved4,
           authz_client_context);
     return RPC_S_NO_CONTEXT_AVAILABLE;
 }
+#endif
