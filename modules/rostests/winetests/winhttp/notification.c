68490c16133faded90c9bcbe064c2a0e2e72bad1
Victor Perevertkin <victor.perevertkin@reactos.org>
2019-12-20 02:52:30 +0300

    [WINHTTP_WINETEST] Restore test_persistent_connection execution This reverts commit a22031d ROSTESTS-295

--- modules/rostests/winetests/winhttp/notification.c
+++ modules/rostests/winetests/winhttp/notification.c
@@ -1037,18 +1037,7 @@ START_TEST (notification)
         return;
     }
 
-#ifdef __REACTOS__
-if (!winetest_interactive)
-{
-    skip("Skipping test_persistent_connection due to hang. See ROSTESTS-295.\n");
-}
-else
-{
     test_persistent_connection( si.port );
-}
-#else
-    test_persistent_connection( si.port );
-#endif
 
     /* send the basic request again to shutdown the server thread */
     test_basic_request( si.port, NULL, quitW );
