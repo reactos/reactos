1ac7a8d419d257e370745d1afc36f51b72e52ab5
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2019-12-21 14:45:45 +0000

    [SHELL][EXPLORER] Check/uncheck options on IDC_CLASSICSTART_SETTINGS

--- base/shell/explorer/startmnucust.cpp
+++ base/shell/explorer/startmnucust.cpp
@@ -49,7 +49,7 @@ VOID OnAdvancedStartMenuItems()
     }
 }
 
-VOID OnClearRecentItems()
+VOID OnClearRecentItems(HWND hwnd)
 {
    WCHAR szPath[MAX_PATH], szFile[MAX_PATH];
    WIN32_FIND_DATAW info;
@@ -67,16 +67,208 @@ VOID OnClearRecentItems()
         while (FindNextFileW(hPath, &info));
         FindClose(hPath);
         /* FIXME: Disable the button*/
+        EnableWindow(GetDlgItem(hwnd, IDC_CLASSICSTART_CLEAR), FALSE);
     }
+}
+
+
+static struct
+{
+    PCWSTR OptionName;
+    BOOL IsDirty;
+}
+
+ClassicOption[] =
+{
+    {L"Start_EnableDragDrop", FALSE},       // REG_DWORD
+    {L"Start_ShowRun", FALSE},              // REG_DWORD
+    {L"StartMenuScrollPrograms", FALSE},    // REG_SZ
+    {L"CascadeMyPictures", FALSE},          // REG_SZ
+    {L"CascadePrinters", FALSE},            // REG_SZ
+    {L"CascadeNetworkConnections", FALSE},  // REG_SZ
+    {L"CascadeMyDocuments", FALSE},         // REG_SZ
+    {L"CascadeControlPanel", FALSE},        // REG_SZ
+    {L"StartMenuFavorites", FALSE},         // REG_DWORD
+    {L"StartMenuAdminToolsRoot", FALSE},    // REG_SZ
+    {L"ShowSmallIcones", FALSE},            // REG_DWORD
+    {L"StartCustomMenus", FALSE},           // REG_DWORD
+    {NULL, FALSE}     // Last.IsDirty stores all current checkBtn state
+};
+
+DWORD LoadUserConfData()
+{
+    HKEY hKey;
+    int iItem;
+    DWORD dwSize;
+    DWORD dwType;
+    DWORD dwStatus = 0;
+    WCHAR chBuffer[16];
+
+    if (RegOpenKeyExW(HKEY_CURRENT_USER,
+                     L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",
+                     0, KEY_QUERY_VALUE,
+                     &hKey) != ERROR_SUCCESS)
+         return 0;
+    for (iItem = 0; ClassicOption[iItem].OptionName != NULL; iItem++)
+    {
+        if (((IDS_DRAG_DROP + iItem) > IDS_SHOW_RUN) &&
+            ((IDS_DRAG_DROP + iItem) < IDS_SHOW_SMALL_ICO) &&
+            ((IDS_DRAG_DROP + iItem) != IDS_SHOW_FAVORITES))
+            dwSize = (lstrlen(L"YES") + 1) * sizeof(WCHAR);
+        else
+            dwSize = sizeof(REG_DWORD);
+
+        if (RegQueryValueExW(hKey,
+                             ClassicOption[iItem].OptionName,
+                             NULL,
+                             &dwType,
+                             (LPBYTE) &chBuffer,
+                             &dwSize) == ERROR_SUCCESS)
+        {
+            if (dwType == REG_SZ)
+            {
+                BOOL stat = ((lstrcmp(chBuffer, L"YES") == 0) || (lstrcmp(chBuffer, L"yes") == 0)) ? TRUE : FALSE;
+                if (stat)
+                    dwStatus |= (1 << iItem);
+                else
+                    dwStatus &= ~(1 << iItem);
+            }
+            else if (dwType == REG_DWORD && dwSize == sizeof(REG_DWORD))
+            {
+                DWORD regValue = *(reinterpret_cast<DWORD*>(chBuffer));
+                if (regValue)
+                    dwStatus |= (1 << iItem);
+                else
+                    dwStatus &= ~(1 << iItem);
+            }
+        }
+    }
+           //  Save Current User options
+    ClassicOption[iItem].IsDirty = dwStatus;
+    RegCloseKey(hKey);
+    return dwStatus;
+}
+
+DWORD UpLoadUserConfData(DWORD &dwUserData)
+{
+    HKEY hKey;
+    DWORD iItem = 0;
+    DWORD nItems = 0;
+    DWORD dwRes;
+
+    while (ClassicOption[nItems].OptionName != NULL)
+        nItems++;
+
+   if (RegCreateKeyExW(HKEY_CURRENT_USER,
+                       L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",
+                       0,
+                       NULL,
+                       REG_OPTION_NON_VOLATILE,
+                       KEY_SET_VALUE,
+                       NULL,
+                       &hKey,
+                       &dwRes) == ERROR_SUCCESS)
+    {
+        DWORD dwQueryData = ClassicOption[nItems].IsDirty;
+
+        while (iItem < nItems)
+        {
+            if (((dwQueryData >> iItem) & 0x01) != ((dwUserData >> iItem) & 0x01))
+            {
+                if (((IDS_DRAG_DROP + iItem) > IDS_SHOW_RUN) &&
+                    ((IDS_DRAG_DROP + iItem) < IDS_SHOW_SMALL_ICO) &&
+                    ((IDS_DRAG_DROP + iItem) != IDS_SHOW_FAVORITES))
+                {
+                    const wchar_t *sKey = ((dwQueryData >> iItem) & 0x01) ? L"YES" : L"NO";
+                    RegSetValueExW(hKey, ClassicOption[iItem].OptionName, NULL, REG_SZ, (LPBYTE) sKey, (lstrlen(sKey)+1)*sizeof(WCHAR));
+                }
+                else
+                {
+                    DWORD regValue = ((dwQueryData >> iItem) & 0x01) ? 1 : 0;
+                    RegSetValueExW(hKey, ClassicOption[iItem].OptionName, NULL, REG_DWORD, (LPBYTE) &regValue, sizeof(REG_DWORD));
+                }
+            }
+            iItem++;
+        }
+        return dwQueryData;
+    }
+    return 0;
+}
+
+HWND InitClassicListView(HWND hwnd)
+{
+    RECT rcClient;
+    HWND hListView = GetDlgItem(hwnd, IDC_CLASSICSTART_SETTINGS);
+    ListView_SetExtendedListViewStyle(hListView, LVS_EX_CHECKBOXES | LVS_EX_FULLROWSELECT);
+    GetClientRect(hListView, &rcClient);
+    WCHAR chBuffer[80];
+    int id = IDS_DRAG_DROP;
+    int nItems = 0;
+
+    while (ClassicOption[nItems].OptionName != NULL)
+        nItems++;
+    DWORD dwOptions = ClassicOption[nItems].IsDirty;
+
+    LVCOLUMNW lvc = {0};
+    lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+    lvc.pszText = NULL ;
+    lvc.cx = rcClient.right;
+    SendMessage(hListView, LVM_INSERTCOLUMN, 0, (LPARAM)&lvc);
+
+    for (int iItem = 0; iItem < nItems; iItem++)
+    {
+        LVITEM hitem = {0};
+        hitem.mask = LVIF_TEXT;
+        hitem.iItem = iItem;
+        LoadStringW(NULL, id++, chBuffer, 79);
+        hitem.pszText = chBuffer;
+        hitem.cchTextMax = (lstrlen(chBuffer)) * sizeof(WCHAR);
+        SendMessage(hListView, LVM_INSERTITEM, 0, (LPARAM)&hitem);
+
+        BOOL bStatus = ((dwOptions >> iItem) & 0x01);
+        ListView_SetCheckState(hListView, iItem, bStatus);
+    }
+    return hListView;
+}
+
+void UpdateClassicOptions(HWND hListView)
+{
+   int nItemCount = ListView_GetItemCount(hListView);
+    DWORD dwStatus = ClassicOption[nItemCount].IsDirty;
+
+    for (int iItem = 0; iItem < nItemCount; iItem++)
+    {
+        if (ClassicOption[iItem].IsDirty == TRUE)
+        {
+            BOOL bState = ListView_GetCheckState(hListView, iItem);
+            if (bState)
+                dwStatus |= (1 << iItem);
+            else
+                dwStatus &= ~(1 << iItem);
+            ClassicOption[iItem].IsDirty = FALSE;
+        }
+    }
+    ClassicOption[nItemCount].IsDirty = dwStatus;
+
 }
 
 INT_PTR CALLBACK CustomizeClassicProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 {
-    switch (Message)
+    static HWND hListView = NULL;
+
+    switch(Message)
     {
         case WM_INITDIALOG:
             /* FIXME: Properly initialize the dialog (check whether 'clear' button must be disabled, for example) */
+            hListView = InitClassicListView(hwnd);
             return TRUE;
+        case WM_NOTIFY:
+            if (((LPNMHDR)lParam)->idFrom == IDC_CLASSICSTART_SETTINGS && ((LPNMHDR)lParam)->code == NM_CLICK)
+            {
+                int item = ((LPNMITEMACTIVATE)lParam)->iItem;
+                ClassicOption[item].IsDirty = TRUE;
+            }
+            break;
         case WM_COMMAND:
             switch (LOWORD(wParam))
             {
@@ -90,23 +282,33 @@ INT_PTR CALLBACK CustomizeClassicProc(HWND hwnd, UINT Message, WPARAM wParam, LP
                     OnAdvancedStartMenuItems();
                     break;
                 case IDC_CLASSICSTART_CLEAR:
-                    OnClearRecentItems();
+                    OnClearRecentItems(hwnd);
                     break;
                 case IDOK:
+                    UpdateClassicOptions(hListView);
                     EndDialog(hwnd, IDOK);
                     break;
                 case IDCANCEL:
+                    for (int i = 0; ClassicOption[i].OptionName != NULL; i++)
+                        if (ClassicOption[i].IsDirty == TRUE)
+                            ClassicOption[i].IsDirty = FALSE;
                     EndDialog(hwnd, IDCANCEL);
                     break;
             }
             break;
+        case WM_DESTROY:
+            SendMessage(hListView, LVM_DELETEALLITEMS, 0, 0);
+            return FALSE;
         default:
             return FALSE;
     }
     return TRUE;
 }
 
-VOID ShowCustomizeClassic(HINSTANCE hInst, HWND hExplorer)
+
+
+
+INT ShowCustomizeClassic(HINSTANCE hInst, HWND hExplorer)
 {
-    DialogBoxW(hInst, MAKEINTRESOURCEW(IDD_CLASSICSTART_CUSTOMIZE), hExplorer, CustomizeClassicProc);
+    return DialogBoxW(hInst, MAKEINTRESOURCEW(IDD_CLASSICSTART_CUSTOMIZE), hExplorer, CustomizeClassicProc);
 }
