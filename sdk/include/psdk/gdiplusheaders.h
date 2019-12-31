7e08a55efd07675488049d4660c859ac47d07d37
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-12-31 10:35:42 +0900

    add comment

--- sdk/include/psdk/gdiplusheaders.h
+++ sdk/include/psdk/gdiplusheaders.h
@@ -1404,6 +1404,7 @@ class CustomLineCap : public GdiplusBase
     }
 
   private:
+    // CustomLineCap is not copyable
     CustomLineCap(const CustomLineCap &);
     CustomLineCap &
     operator=(const CustomLineCap &);
