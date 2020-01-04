ba8e4c483fdb491137cfa6d665c12dd4a476ba4c
winesync <ros-dev@reactos.org>
2020-01-04 02:09:33 +0100

    [WINESYNC]d3dx9_36: Improve D3DXSaveTextureToFile to save simple texture to dds file.
    wine-staging patch by Christian Costa <titan.costa@gmail.com>

--- dll/directx/wine/d3dx9_36/d3dx9_private.h
+++ dll/directx/wine/d3dx9_36/d3dx9_private.h
@@ -125,6 +125,8 @@ HRESULT lock_surface(IDirect3DSurface9 *surface, D3DLOCKED_RECT *lock,
         IDirect3DSurface9 **temp_surface, BOOL write) DECLSPEC_HIDDEN;
 HRESULT unlock_surface(IDirect3DSurface9 *surface, D3DLOCKED_RECT *lock,
         IDirect3DSurface9 *temp_surface, BOOL update) DECLSPEC_HIDDEN;
+HRESULT save_dds_texture_to_memory(ID3DXBuffer **dst_buffer, IDirect3DBaseTexture9 *src_texture,
+    const PALETTEENTRY *src_palette) DECLSPEC_HIDDEN;
 
 unsigned short float_32_to_16(const float in) DECLSPEC_HIDDEN;
 float float_16_to_32(const unsigned short in) DECLSPEC_HIDDEN;
