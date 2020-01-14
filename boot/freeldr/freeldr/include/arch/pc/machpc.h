df411e18b61624e7da4b5369f375910dfd14f09d
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2020-01-14 04:01:59 +0100

    [FREELDR] Generalize the cache to all the geometry data + INT 13h extensions supported status.
    The data structure used is "PC_DISK_DRIVE".
    
    - Adapt the associated functions;
    - Make DiskGetExtendedDriveParameters() private to pcdisk.c
    
    - Introduce PcDiskDriveNumberToDrive(), that is similar to the XBOX
      function XboxDiskDriveNumberToDeviceUnit(), that retrieves a valid
      pointer to the cached disk corresponding to the given BIOS DriveNumber.
      If needed the cached data gets initialized.
    
    - Make XboxDiskDriveNumberToDeviceUnit() simpler by just returning the
      pointer to the corresponding drive, of NULL if there is none.

--- boot/freeldr/freeldr/include/arch/pc/machpc.h
+++ boot/freeldr/freeldr/include/arch/pc/machpc.h
@@ -60,7 +60,7 @@ extern ULONG FrldrBootPartition;
 
 LONG DiskReportError(BOOLEAN bShowError);
 BOOLEAN DiskResetController(UCHAR DriveNumber);
-BOOLEAN DiskGetExtendedDriveParameters(UCHAR DriveNumber, PVOID Buffer, USHORT BufferSize);
+// BOOLEAN DiskGetExtendedDriveParameters(UCHAR DriveNumber, PVOID Buffer, USHORT BufferSize);
 
 BOOLEAN PcDiskReadLogicalSectors(UCHAR DriveNumber, ULONGLONG SectorNumber, ULONG SectorCount, PVOID Buffer);
 BOOLEAN PcDiskGetDriveGeometry(UCHAR DriveNumber, PGEOMETRY DriveGeometry);
