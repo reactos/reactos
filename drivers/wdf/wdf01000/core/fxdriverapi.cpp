2166217e81e149cc787686f7dde4c29b19beb040
Max Korostil <mrmks04@yandex.ru>
2020-01-15 23:03:46 +0300

    Implement WdfDriverCreate function

--- drivers/wdf/wdf01000/core/fxdriverapi.cpp
+++ drivers/wdf/wdf01000/core/fxdriverapi.cpp
@@ -1,5 +1,9 @@
 #include "wdf.h"
-#include "common/mxgeneral.h"
+#include "common/fxglobals.h"
+#include "common/fxvalidatefunctions.h"
+#include "common/dbgtrace.h"
+#include "common/fxdriver.h"
+#include "common/fxverifier.h"
 
 //
 // extern the whole file
@@ -24,15 +28,275 @@ WDFEXPORT(WdfDriverCreate)(
     WDFDRIVER* Driver
     )
 {
-    UNREFERENCED_PARAMETER(DriverGlobals);
-    UNREFERENCED_PARAMETER(DriverObject);
-    UNREFERENCED_PARAMETER(RegistryPath);
-    UNREFERENCED_PARAMETER(DriverAttributes);
-    UNREFERENCED_PARAMETER(DriverConfig);
-    UNREFERENCED_PARAMETER(Driver);
+    DDI_ENTRY();
 
-    WDFNOTIMPLEMENTED();
-    return STATUS_UNSUCCESSFUL;
+    PFX_DRIVER_GLOBALS pFxDriverGlobals;
+    FxDriver *pDriver;
+    NTSTATUS status;
+    WDFDRIVER hDriver;
+    const LONG validFlags = WdfDriverInitNonPnpDriver |
+                            WdfDriverInitNoDispatchOverride;
+
+    hDriver = NULL;
+    pFxDriverGlobals = GetFxDriverGlobals(DriverGlobals);
+
+    FxPointerNotNull(pFxDriverGlobals, DriverObject);
+    FxPointerNotNull(pFxDriverGlobals, DriverConfig);
+
+    //
+    // Validate the size of the input Driver Config structure.  The size changed
+    // after v1.1.  The size is the same for v1.1 and v1.0, verify that.
+    //
+    WDFCASSERT(sizeof(WDF_DRIVER_CONFIG_V1_0) == sizeof(WDF_DRIVER_CONFIG_V1_1));
+
+    if (DriverConfig->Size != sizeof(WDF_DRIVER_CONFIG)
+        &&
+        DriverConfig->Size != sizeof(WDF_DRIVER_CONFIG_V1_1))
+    {
+
+        status = STATUS_INFO_LENGTH_MISMATCH;
+        DoTraceLevelMessage(
+            pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDRIVER,
+            "WDF_DRIVER_CONFIG got Size %d, expected v1.1 size %d or cur ver size %d, %!STATUS!",
+            DriverConfig->Size,
+            sizeof(WDF_DRIVER_CONFIG_V1_1), sizeof(WDF_DRIVER_CONFIG), status);
+
+        return status;
+    }
+
+    //
+    // Validate the DriverInitFlags value in the Driver Config.
+    //
+    if ((DriverConfig->DriverInitFlags & ~validFlags) != 0)
+    {
+        status = STATUS_INVALID_PARAMETER;
+        DoTraceLevelMessage(
+            pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDRIVER,
+            "DriverInitFlags 0x%x invalid, valid flags are 0x%x, %!STATUS!",
+            DriverConfig->DriverInitFlags, validFlags, status);
+        return status;
+    }    
+
+    status = FxVerifierCheckIrqlLevel(pFxDriverGlobals, PASSIVE_LEVEL);
+    if (!NT_SUCCESS(status))
+    {
+        return status;
+    }
+    
+    //
+    // Driver and Public.Driver are set once WdfDriverCreate returns successfully.
+    // If they are set, that means this DDI has already been called for this
+    // client.  Return an error if this occurrs.
+    //
+    if (pFxDriverGlobals->Driver != NULL ||
+        pFxDriverGlobals->Public.Driver != NULL)
+    {
+        status = STATUS_DRIVER_INTERNAL_ERROR;
+
+        DoTraceLevelMessage(pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDRIVER,
+                            "WdfDriverCreate can only be called one time per "
+                            "WDM PDRIVER_OBJECT %p, %!STATUS!",
+                            DriverObject, status);
+
+        return status;
+    }
+
+    if (Driver != NULL)
+    {
+        *Driver = NULL;
+    }
+
+    //
+    // Initializing the tag requires initializing the driver name first.
+    //
+    FxDriver::_InitializeDriverName(pFxDriverGlobals, RegistryPath);
+
+    //
+    // Initialize the tag before any allocation so that we can use the correct
+    // tag value when allocating on behalf of the driver (for all allocations,
+    // including FxDriver).
+    //
+    // Use the client's driver tag value if they specified one.  First check
+    // to make sure the size of the structure is the new size (vs the old size
+    // in v1.1).
+    //
+    // ' kdD' - was the default tag in many DDKs, don't allow its use.
+    //
+    if (DriverConfig->Size == sizeof(WDF_DRIVER_CONFIG) &&
+        DriverConfig->DriverPoolTag != 0x0 &&
+        DriverConfig->DriverPoolTag != ' kdD')
+    {
+        //
+        // Copy directly using the driver's value
+        //
+        pFxDriverGlobals->Tag = DriverConfig->DriverPoolTag;
+        pFxDriverGlobals->Public.DriverTag = DriverConfig->DriverPoolTag;
+    }
+    else
+    {
+        //
+        // Derive the value from the driver's service name
+        //
+        FxDriver::_InitializeTag(pFxDriverGlobals, DriverConfig);
+    }
+
+    //
+    // Check to see if this is an NT4 style device driver.  If so, fail if they
+    // specified an AddDevice routine.  If no dispatch override is set,
+    // do not do any checking at all.
+    //
+    if (DriverConfig->DriverInitFlags & WdfDriverInitNoDispatchOverride)
+    {
+        DO_NOTHING();
+    }
+    else if ((DriverConfig->DriverInitFlags & WdfDriverInitNonPnpDriver) &&
+             DriverConfig->EvtDriverDeviceAdd != NULL)
+    {
+
+         DoTraceLevelMessage(pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDRIVER,
+                            "Invalid Driver flags or EvtDriverDeviceAdd callback already added"
+                              "STATUS_INVALID_PARAMETER");
+
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    status = FxValidateObjectAttributes(pFxDriverGlobals, DriverAttributes,
+                                        (FX_VALIDATE_OPTION_PARENT_NOT_ALLOWED |
+                                        FX_VALIDATE_OPTION_EXECUTION_LEVEL_ALLOWED |
+                                        FX_VALIDATE_OPTION_SYNCHRONIZATION_SCOPE_ALLOWED)
+                                        );
+
+    if (!NT_SUCCESS(status))
+    {
+        return status;
+    }
+
+    //
+    // FxDriver stores the driver wide configuration
+    //
+    FxInitialize(pFxDriverGlobals, DriverObject, RegistryPath, DriverConfig);
+
+    //
+    // FxDriver stores the driver wide configuration
+    //
+    pDriver = new(pFxDriverGlobals, DriverAttributes)
+        FxDriver(DriverObject, DriverConfig, pFxDriverGlobals);
+
+    if (pDriver != NULL)
+    {
+
+        if (NT_SUCCESS(status))
+        {
+
+            status = pDriver->Initialize(RegistryPath, DriverConfig, DriverAttributes);
+
+            if (NT_SUCCESS(status))
+            {
+                status = pDriver->Commit(DriverAttributes, (WDFOBJECT*)&hDriver, FALSE);
+            }
+        }
+    }
+    else
+    {
+        status =  STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    //
+    // Only return a valid handle on success.  Upon error, release any memory
+    // and do not rely on any other function (like the driver unload routine) to
+    // be called.
+    //
+    if (NT_SUCCESS(status))
+    {
+        //
+        // **** Note ****
+        // Do not introduce failures after this point without ensuring
+        // FxObject::DeleteFromFailedCreate has a chance to clear out any
+        // assigned callbacks on the object.
+        //
+
+        //
+        // Store the WDFDRIVER and FxDriver* globally so the driver can retrieve
+        // it anytime.
+        //
+        pFxDriverGlobals->Driver = pDriver;
+        pFxDriverGlobals->Public.Driver = hDriver;
+
+        //
+        // Record the flags so that diagnostics knows what type of driver it is.
+        //
+        pFxDriverGlobals->Public.DriverFlags |= DriverConfig->DriverInitFlags;
+
+        if (DriverConfig->DriverInitFlags &
+                (WdfDriverInitNoDispatchOverride | WdfDriverInitNonPnpDriver))
+        {
+            //
+            // If there is no dispatch override or if it is an NT4 legacy style
+            // driver then we will not displace unload in the stub if one was not
+            // specified.
+            //
+            if (DriverConfig->EvtDriverUnload != NULL)
+            {
+                pFxDriverGlobals->Public.DisplaceDriverUnload = TRUE;
+            }
+            else
+            {
+                pFxDriverGlobals->Public.DisplaceDriverUnload = FALSE;
+            }
+        }
+        else
+        {
+            pFxDriverGlobals->Public.DisplaceDriverUnload = TRUE;
+        }
+
+        if (Driver != NULL)
+        {
+            *Driver = hDriver;
+        }
+
+        //
+        // TODO: check is this code need
+        //
+        /*if (FX_TELEMETRY_ENABLED(g_TelemetryProvider, pFxDriverGlobals))
+        {
+            FxAutoString imageName;
+
+#if (FX_CORE_MODE == FX_CORE_KERNEL_MODE)
+        const PWCHAR pVersionStr =
+            FX_MAKE_WSTR(__WDF_MAJOR_VERSION_STRING) L"."
+            FX_MAKE_WSTR(__WDF_MINOR_VERSION_STRING) L"."
+            FX_MAKE_WSTR(__WDF_BUILD_NUMBER) ;
+#else // USER_MODE
+        const PWCHAR pVersionStr =
+            FX_MAKE_WSTR(__WUDF_MAJOR_VERSION_STRING) L"."
+            FX_MAKE_WSTR(__WUDF_MINOR_VERSION_STRING) L"."
+            FX_MAKE_WSTR(__WUDF_SERVICE_VERSION) ;
+#endif
+
+            //
+            // GetImageName can fail if the registry cannot be accessed. This
+            // can happen during system shutdown. Since the image name is only
+            // used for telemetry the failure can be ignored.
+            //
+            (VOID) GetImageName(pFxDriverGlobals, &imageName.m_UnicodeString);
+
+            WDF_CENSUS_EVT_WRITE_DRIVER_LOAD(g_TelemetryProvider,
+                                    pFxDriverGlobals,
+                                    imageName.m_UnicodeString.Buffer,
+                                    pVersionStr);
+        }*/
+    }
+    else
+    {
+        if (pDriver != NULL)
+        {
+            pDriver->DeleteFromFailedCreate();
+        }
+
+        FxDestroy(pFxDriverGlobals);
+    }
+
+    return status;
 }
 
 _Must_inspect_result_
