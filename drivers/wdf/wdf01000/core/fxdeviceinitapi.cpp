53ae63fe22468797ca1b0476747f79b35c10e706
Max Korostil <mrmks04@yandex.ru>
2020-01-18 03:44:10 +0300

    Implemented WdfDeviceInitSetPnpPowerEventCallbacks function

--- drivers/wdf/wdf01000/core/fxdeviceinitapi.cpp
+++ drivers/wdf/wdf01000/core/fxdeviceinitapi.cpp
@@ -1,4 +1,7 @@
-#include "wdf.h"
+#include "common/fxglobals.h"
+#include "common/fxdevice.h"
+#include "common/fxdeviceinit.h"
+
 
 extern "C" {
 
@@ -13,7 +16,58 @@ WDFEXPORT(WdfDeviceInitSetPnpPowerEventCallbacks)(
     PWDF_PNPPOWER_EVENT_CALLBACKS PnpPowerEventCallbacks
     )
 {
-    WDFNOTIMPLEMENTED();
+    DDI_ENTRY();
+        
+    PFX_DRIVER_GLOBALS pFxDriverGlobals;
+
+    FxPointerNotNull(GetFxDriverGlobals(DriverGlobals), DeviceInit);
+    pFxDriverGlobals = DeviceInit->DriverGlobals;
+
+    FxPointerNotNull(pFxDriverGlobals, PnpPowerEventCallbacks);
+
+    if (PnpPowerEventCallbacks->Size != sizeof(WDF_PNPPOWER_EVENT_CALLBACKS) &&
+        PnpPowerEventCallbacks->Size != sizeof(WDF_PNPPOWER_EVENT_CALLBACKS_V1_9))
+    {
+
+        DoTraceLevelMessage(
+            pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDEVICE,
+            "PnpPowerEventCallbacks size %d is invalid, exptected %d",
+            PnpPowerEventCallbacks->Size, sizeof(WDF_PNPPOWER_EVENT_CALLBACKS)
+            );
+
+        FxVerifierDbgBreakPoint(pFxDriverGlobals);
+
+        return;
+    }
+
+    //
+    // Make sure only one of the callbacks EvtDeviceUsageNotification or 
+    // EvtDeviceUsageNotificationEx is provided by driver for >V1.9.
+    //
+    /*if (PnpPowerEventCallbacks->Size > sizeof(WDF_PNPPOWER_EVENT_CALLBACKS_V1_9) &&
+        PnpPowerEventCallbacks->EvtDeviceUsageNotification != NULL &&
+        PnpPowerEventCallbacks->EvtDeviceUsageNotificationEx != NULL)
+    {
+
+        DoTraceLevelMessage(
+            pFxDriverGlobals, TRACE_LEVEL_ERROR, TRACINGDEVICE,
+            "Driver can provide either EvtDeviceUsageNotification or "
+            "EvtDeviceUsageNotificationEx callback but not both");
+
+        FxVerifierDbgBreakPoint(pFxDriverGlobals);
+
+        return;
+    }*/
+
+    //
+    // Driver's PnpPowerEventCallbacks structure may be from a previous 
+    // version and therefore may be different in size than the current version 
+    // that framework is using. Therefore, copy only PnpPowerEventCallbacks->Size
+    // bytes and not sizeof(PnpPowerEventCallbacks) bytes.
+    //
+    RtlCopyMemory(&DeviceInit->PnpPower.PnpPowerEventCallbacks,
+                  PnpPowerEventCallbacks,
+                  PnpPowerEventCallbacks->Size);
 }
 
 __drv_maxIRQL(DISPATCH_LEVEL)
---
+++ drivers/wdf/wdf01000/include/wdf19.h
@@ -0,0 +1,46 @@
+#ifndef _WDF19_H_
+#define _WDF19_H_
+
+#include <ntddk.h>
+
+typedef struct _WDF_PNPPOWER_EVENT_CALLBACKS_V1_9 {
+    // 
+    // Size of this structure in bytes
+    // 
+    ULONG Size;
+
+    PFN_WDF_DEVICE_D0_ENTRY                 EvtDeviceD0Entry;
+
+    PFN_WDF_DEVICE_D0_ENTRY_POST_INTERRUPTS_ENABLED EvtDeviceD0EntryPostInterruptsEnabled;
+
+    PFN_WDF_DEVICE_D0_EXIT                  EvtDeviceD0Exit;
+
+    PFN_WDF_DEVICE_D0_EXIT_PRE_INTERRUPTS_DISABLED EvtDeviceD0ExitPreInterruptsDisabled;
+
+    PFN_WDF_DEVICE_PREPARE_HARDWARE         EvtDevicePrepareHardware;
+
+    PFN_WDF_DEVICE_RELEASE_HARDWARE         EvtDeviceReleaseHardware;
+
+    PFN_WDF_DEVICE_SELF_MANAGED_IO_CLEANUP  EvtDeviceSelfManagedIoCleanup;
+
+    PFN_WDF_DEVICE_SELF_MANAGED_IO_FLUSH    EvtDeviceSelfManagedIoFlush;
+
+    PFN_WDF_DEVICE_SELF_MANAGED_IO_INIT     EvtDeviceSelfManagedIoInit;
+
+    PFN_WDF_DEVICE_SELF_MANAGED_IO_SUSPEND  EvtDeviceSelfManagedIoSuspend;
+
+    PFN_WDF_DEVICE_SELF_MANAGED_IO_RESTART  EvtDeviceSelfManagedIoRestart;
+
+    PFN_WDF_DEVICE_SURPRISE_REMOVAL         EvtDeviceSurpriseRemoval;
+
+    PFN_WDF_DEVICE_QUERY_REMOVE             EvtDeviceQueryRemove;
+
+    PFN_WDF_DEVICE_QUERY_STOP               EvtDeviceQueryStop;
+
+    PFN_WDF_DEVICE_USAGE_NOTIFICATION       EvtDeviceUsageNotification;
+
+    PFN_WDF_DEVICE_RELATIONS_QUERY          EvtDeviceRelationsQuery;
+
+} WDF_PNPPOWER_EVENT_CALLBACKS_V1_9, *PWDF_PNPPOWER_EVENT_CALLBACKS_V1_9;
+
+#endif //_WDF19_H_
