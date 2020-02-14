131db8e3eefb185e7f2632d0fe9453f9a10932fb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-02-14 22:43:10 +0900

    small fixes

--- modules/rostests/apitests/shell32/ShellExecuteW.cpp
+++ modules/rostests/apitests/shell32/ShellExecuteW.cpp
@@ -9,6 +9,7 @@
 #include "shelltest.h"
 #include <stdio.h>
 #include <winbase.h>
+#include <shlwapi.h>
 
 #define WAIT_SLEEP 700
 
@@ -19,18 +20,24 @@ START_TEST(ShellExecuteW)
     INT ret;
     HINSTANCE hInstance;
     HWND hWnd;
-    WCHAR WinDir[MAX_PATH], SysDir[MAX_PATH];
+    WCHAR WinDir[MAX_PATH], SysDir[MAX_PATH], SysDrive[MAX_PATH];
 
-    if (!GetWindowsDirectoryW(WinDir, MAX_PATH))
+    if (!GetWindowsDirectoryW(WinDir, _countof(WinDir)))
     {
         skip("GetWindowsDirectoryW failed\n");
         return;
     }
-    if (!GetSystemDirectoryW(SysDir, MAX_PATH))
+    if (!GetSystemDirectoryW(SysDir, _countof(SysDir)))
     {
         skip("GetSystemDirectoryW failed\n");
         return;
     }
+    if (!GetEnvironmentVariableW(L"SystemDrive", SysDrive, _countof(SysDrive)))
+    {
+        skip("GetEnvironmentVariableW('SystemDrive') failed\n");
+        return;
+    }
+    PathAddBackslashW(SysDrive);
 
     // TEST #1: Open Control Panel
     hInstance = ShellExecuteW(NULL, L"open", L"rundll32.exe", L"shell32.dll,Control_RunDLL desk.cpl",
@@ -85,8 +92,8 @@ START_TEST(ShellExecuteW)
         PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
     }
 
-    // TEST #5: Open "C:\\"
-    hInstance = ShellExecuteW(NULL, L"explore", L"C:\\", NULL, NULL, SW_SHOWNORMAL);
+    // TEST #5: Open %SystemDrive%
+    hInstance = ShellExecuteW(NULL, L"explore", SysDrive, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #5: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #5 ret: %d.\n", ret);
@@ -94,12 +101,12 @@ START_TEST(ShellExecuteW)
     {
         Sleep(WAIT_SLEEP);
         // Terminate Window
-        hWnd = FindWindowW(L"ExploreWClass", L"C:\\");
+        hWnd = FindWindowW(L"ExploreWClass", SysDrive);
         PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
     }
 
-    // TEST #6: Open Explorer Search on "C:\"
-    hInstance = ShellExecuteW(NULL, L"find", L"C:\\", NULL, NULL, SW_SHOWNORMAL);
+    // TEST #6: Open Explorer Search on %SYSTEMDRIVE%
+    hInstance = ShellExecuteW(NULL, L"find", SysDrive, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #6: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #6 ret: %d.\n", ret);
