11ecf5c969bffb4f245bd84e86e25a6ec70509c9
Thomas Faber <thomas.faber@reactos.org>
2020-02-24 10:48:24 +0100

    [CMAKE] Enable C4101 (unreferenced local variable) for MSVC builds.
    This is already an error in GCC builds, so will make it harder for the
    MSVC crowd to accidentally break GCC build.

--- sdk/cmake/msvc.cmake
+++ sdk/cmake/msvc.cmake
@@ -95,6 +95,7 @@ add_compile_flags("/wd4018")
 # - TODO: C4090: different 'modifier' qualifiers (for C programs only;
 #          for C++ programs, the compiler error C2440 is issued)
 # - C4098: void function returning a value
+# - C4101: unreferenced local variable
 # - C4113: parameter lists differ
 # - C4129: unrecognized escape sequence
 # - C4133: incompatible types - from '<x> *' to '<y> *'
@@ -108,7 +109,7 @@ add_compile_flags("/wd4018")
 # - C4700: uninitialized variable usage
 # - C4715: 'function': not all control paths return a value
 # - C4716: function must return a value
-add_compile_flags("/we4013 /we4020 /we4022 /we4028 /we4047 /we4098 /we4113 /we4129 /we4133 /we4163 /we4229 /we4311 /we4312 /we4313 /we4477 /we4603 /we4700 /we4715 /we4716")
+add_compile_flags("/we4013 /we4020 /we4022 /we4028 /we4047 /we4098 /we4101 /we4113 /we4129 /we4133 /we4163 /we4229 /we4311 /we4312 /we4313 /we4477 /we4603 /we4700 /we4715 /we4716")
 
 # - C4189: local variable initialized but not referenced
 # Not in Release mode and not with MSVC 2010
