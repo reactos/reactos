1e5d33ba253be0e71c2b6618541d2bb09f050b98
Joachim Henze <Joachim.Henze@reactos.org>
2020-03-05 02:32:33 +0100

    [0.4.13][WIN32SS] Improve many apps not starting with active taskbar pane (#2396)
    Many thanks to the patches author JIRA-user "blueghost"
    tibor.fuzi@gmail.com
    
    The patch fixes regression CORE-16703, which was unhidden by explorer commit
    8ad0eae6879029219ce3b62b554869a9d33aefa9 == SVN r74968
    although the root-cause was missing functionality in WIN32SS.
    
    The patch also fixes CORE-16705 and CORE-16706.
    Only a few applications still show issues with the taskbar panes,
    we have strong indications that other patches in master during
    0.4.14-dev'ing already improved the situation further.
    
    This patch (#2396) to date has not been committed to master yet,
    but I guess that will happen soon.
    
    TestBot results on 0.4.13-RC-48-g818e5bc:
    VBox https://reactos.org/testman/compare.php?ids=71645,71670 LGTM
     KVM https://reactos.org/testman/compare.php?ids=71646,71671 LGTM

--- win32ss/user/ntuser/winpos.c
+++ win32ss/user/ntuser/winpos.c
@@ -1908,7 +1908,11 @@ co_WinPosSetWindowPos(
            Window->spwndOwner == NULL &&
            (!(Window->ExStyle & WS_EX_TOOLWINDOW) ||
             (Window->ExStyle & WS_EX_APPWINDOW)))
-         co_IntShellHookNotify(HSHELL_WINDOWCREATED, (WPARAM)Window->head.h, 0);
+            {
+               co_IntShellHookNotify(HSHELL_WINDOWCREATED, (WPARAM)Window->head.h, 0);
+               if (!(WinPos.flags & SWP_NOACTIVATE))
+                  UpdateShellHook(Window);
+            }
 
       Window->style |= WS_VISIBLE; //IntSetStyle( Window, WS_VISIBLE, 0 );
       Window->head.pti->cVisWindows++;
