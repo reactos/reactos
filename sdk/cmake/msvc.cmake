93beefacbefdb3eba2429375393b982d4cdda867
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2020-03-06 22:55:12 +0100

    [CMAKE][FREELDR] Take the global defines into account when compiling ASM files.
    Now the SARCH_xxx can be used in ASM files too.
    Dedicated to Dmitry Borisov ;)

--- sdk/cmake/msvc.cmake
+++ sdk/cmake/msvc.cmake
@@ -486,6 +486,9 @@ function(CreateBootSectorTarget _target_name _asm_file _binary_file _base_addres
     set(_object_file ${_binary_file}.obj)
     set(_temp_file ${_binary_file}.tmp)
 
+    get_defines(_defines)
+    get_includes(_includes)
+
     if(USE_CLANG_CL)
         set(_no_std_includes_flag "-nostdinc")
     else()
@@ -494,7 +497,7 @@ function(CreateBootSectorTarget _target_name _asm_file _binary_file _base_addres
 
     add_custom_command(
         OUTPUT ${_temp_file}
-        COMMAND ${CMAKE_C_COMPILER} /nologo ${_no_std_includes_flag} /I${REACTOS_SOURCE_DIR}/sdk/include/asm /I${REACTOS_BINARY_DIR}/sdk/include/asm /I${REACTOS_SOURCE_DIR}/boot/freeldr /D__ASM__ /D_USE_ML /EP /c ${_asm_file} > ${_temp_file}
+        COMMAND ${CMAKE_C_COMPILER} /nologo ${_no_std_includes_flag} /I${REACTOS_SOURCE_DIR}/sdk/include/asm /I${REACTOS_BINARY_DIR}/sdk/include/asm ${_includes} ${_defines} /D__ASM__ /D_USE_ML /EP /c ${_asm_file} > ${_temp_file}
         DEPENDS ${_asm_file})
 
     if(ARCH STREQUAL "arm")
