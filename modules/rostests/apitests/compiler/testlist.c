e02626c9d629204140805cce12cb761411313fe9
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-03-12 19:20:55 +0100

    [COMPILER_APITEST] Include MS SEH tests

--- modules/rostests/apitests/compiler/testlist.c
+++ modules/rostests/apitests/compiler/testlist.c
@@ -1,11 +1,13 @@
 #define STANDALONE
 #include <apitest.h>
 
+extern void func_ms_seh(void);
 extern void func_pseh(void);
 extern void func_pseh_cpp(void);
 
 const struct test winetest_testlist[] =
 {
+    { "ms-seh", func_ms_seh },
     { "pseh", func_pseh },
     { "pseh_cpp", func_pseh_cpp },
     { 0, 0 }
