4f628f6b167c0e1936eaa501efac2eeef69533b8
jimtabor <james.tabor@reactos.org>
2020-03-21 19:13:55 -0500

    [0.4.13][EXPLORER|SHELL3232] Fix CORE-14439 part 2 of 2 - Fix Build
    Missed a file. See CORE-14439.
    
    cherry picked from commit 0.4.14-dev-1210-g
    78098f99e0838d78e9638422fccfaaeb7e52a13e
    
    [0.4.13][EXPLORER|SHELL3232] Fix CORE-14439 part 1 of 2
    
    Prevent ros from halting when Miranda IM 0.10.21 snap-to-border-feature is used.
    
    Fix SHAppBarMessage (not perfect) by adapting wine code. See CORE-14439.
    More work is required. Kept the original code for reference.
    Example : SetWindowPos should be called via Post Message.
    Code commit is to wake up other developers.
    
    cherry picked from commit 0.4.14-dev-1209-g
    10d1afea18db0ef06942239867cbc090e2330b90

--- sdk/include/reactos/undocshell.h
+++ sdk/include/reactos/undocshell.h
@@ -676,6 +676,11 @@ BOOL WINAPI GUIDFromStringW(
 #define TRAYCMD_SEARCH_FILES        41093
 #define TRAYCMD_SEARCH_COMPUTERS    41094
 
+// Explorer Tray Application Bar Data Message Commands
+#define TABDMC_APPBAR     0
+#define TABDMC_NOTIFY     1
+#define TABDMC_LOADINPROC 2
+
 void WINAPI ShellDDEInit(BOOL bInit);
 DWORD WINAPI WinList_Init(void);
 
