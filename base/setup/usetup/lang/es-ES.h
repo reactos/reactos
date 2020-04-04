da1303de305d977d07fcdb600f30769d05df6838
Julen Urizar Compains <julenuri@hotmail.com>
2020-04-04 22:31:19 +0200

    Undo the changes

--- base/setup/usetup/lang/es-ES.h
+++ base/setup/usetup/lang/es-ES.h
@@ -1026,7 +1026,7 @@ static MUI_ENTRY esESSelectPartitionEntries[] =
         8,
         15,
         "\x07  Pulse P para crear una Partici\242n Primaria.",
-//        "\x07  Pulse C para crear una Nueva Partici\242n.",
+//        "\x07  Pulse C para crear una Nueva Partici¢n.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
