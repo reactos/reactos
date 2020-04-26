73064a760203f335aa0ce7e2001de0e54ebdcb3e
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2020-04-26 19:10:48 +0200

    [WIN32SS] Fix formatting only.

--- win32ss/user/ntuser/guicheck.c
+++ win32ss/user/ntuser/guicheck.c
@@ -21,72 +21,72 @@ static LONG NrGuiAppsRunning = 0;
 static BOOL FASTCALL
 co_AddGuiApp(PPROCESSINFO W32Data)
 {
-   W32Data->W32PF_flags |= W32PF_CREATEDWINORDC;
-   if (InterlockedIncrement(&NrGuiAppsRunning) == 1)
-   {
-      BOOL Initialized;
+    W32Data->W32PF_flags |= W32PF_CREATEDWINORDC;
+    if (InterlockedIncrement(&NrGuiAppsRunning) == 1)
+    {
+        BOOL Initialized;
 
-      Initialized = co_IntInitializeDesktopGraphics();
+        Initialized = co_IntInitializeDesktopGraphics();
 
-      if (!Initialized)
-      {
-         W32Data->W32PF_flags &= ~W32PF_CREATEDWINORDC;
-         InterlockedDecrement(&NrGuiAppsRunning);
-         return FALSE;
-      }
-   }
-   return TRUE;
+        if (!Initialized)
+        {
+            W32Data->W32PF_flags &= ~W32PF_CREATEDWINORDC;
+            InterlockedDecrement(&NrGuiAppsRunning);
+            return FALSE;
+        }
+    }
+    return TRUE;
 }
 
 static void FASTCALL
 RemoveGuiApp(PPROCESSINFO W32Data)
 {
-   W32Data->W32PF_flags &= ~W32PF_CREATEDWINORDC;
-   if (InterlockedDecrement(&NrGuiAppsRunning) == 0)
-   {
-      IntEndDesktopGraphics();
-   }
+    W32Data->W32PF_flags &= ~W32PF_CREATEDWINORDC;
+    if (InterlockedDecrement(&NrGuiAppsRunning) == 0)
+    {
+        IntEndDesktopGraphics();
+    }
 }
 
 BOOL FASTCALL
 co_IntGraphicsCheck(BOOL Create)
 {
-   PPROCESSINFO W32Data;
+    PPROCESSINFO W32Data;
 
-   W32Data = PsGetCurrentProcessWin32Process();
-   if (Create)
-   {
-      if (! (W32Data->W32PF_flags & W32PF_CREATEDWINORDC) && ! (W32Data->W32PF_flags & W32PF_MANUALGUICHECK))
-      {
-         return co_AddGuiApp(W32Data);
-      }
-   }
-   else
-   {
-      if ((W32Data->W32PF_flags & W32PF_CREATEDWINORDC) && ! (W32Data->W32PF_flags & W32PF_MANUALGUICHECK))
-      {
-         RemoveGuiApp(W32Data);
-      }
-   }
+    W32Data = PsGetCurrentProcessWin32Process();
+    if (Create)
+    {
+        if (!(W32Data->W32PF_flags & W32PF_CREATEDWINORDC) && !(W32Data->W32PF_flags & W32PF_MANUALGUICHECK))
+        {
+            return co_AddGuiApp(W32Data);
+        }
+    }
+    else
+    {
+        if ((W32Data->W32PF_flags & W32PF_CREATEDWINORDC) && !(W32Data->W32PF_flags & W32PF_MANUALGUICHECK))
+        {
+            RemoveGuiApp(W32Data);
+        }
+    }
 
-   return TRUE;
+    return TRUE;
 }
 
 VOID
 FASTCALL
 co_IntUserManualGuiCheck(BOOL Create)
 {
-   PPROCESSINFO W32Data = (PPROCESSINFO)PsGetCurrentProcessWin32Process();
-   W32Data->W32PF_flags |= W32PF_MANUALGUICHECK;
+    PPROCESSINFO W32Data = (PPROCESSINFO)PsGetCurrentProcessWin32Process();
+    W32Data->W32PF_flags |= W32PF_MANUALGUICHECK;
 
-   if (Create)
-   {
-       co_AddGuiApp(W32Data);
-   }
-   else
-   {
-       RemoveGuiApp(W32Data);
-   }
+    if (Create)
+    {
+        co_AddGuiApp(W32Data);
+    }
+    else
+    {
+        RemoveGuiApp(W32Data);
+    }
 }
 
 /* EOF */
