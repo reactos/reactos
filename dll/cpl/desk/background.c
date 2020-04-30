440157ebbe903471885ae97ce7380e432152c92c
Jared Smudde <computerwhiz02@hotmail.com>
2020-04-30 00:13:56 -0500

    Use CSIDL_MYPICTURES instead of environment variable concoction.

--- dll/cpl/desk/background.c
+++ dll/cpl/desk/background.c
@@ -635,6 +635,7 @@ OnBrowseButton(HWND hwndDlg, PBACKGROUND_DATA pData)
     OPENFILENAME ofn;
     TCHAR filename[MAX_PATH];
     TCHAR fileTitle[256];
+	TCHAR initialDir[MAX_PATH];
     LPTSTR filter;
     LPTSTR extensions;
     BackgroundItem *backgroundItem = NULL;
@@ -652,6 +653,7 @@ OnBrowseButton(HWND hwndDlg, PBACKGROUND_DATA pData)
 
     hwndBackgroundList = GetDlgItem(hwndDlg, IDC_BACKGROUND_LIST);
     himl = ListView_GetImageList(hwndBackgroundList, LVSIL_SMALL);
+	SHGetFolderPathW(NULL, CSIDL_MYPICTURES, NULL, 0, initialDir);
 
     ZeroMemory(&ofn, sizeof(OPENFILENAME));
 
@@ -706,7 +708,7 @@ OnBrowseButton(HWND hwndDlg, PBACKGROUND_DATA pData)
     ofn.nFilterIndex = 0;
     ofn.lpstrFileTitle = fileTitle;
     ofn.nMaxFileTitle = 256;
-    ofn.lpstrInitialDir = L"%USERPROFILE%\\My Documents\\My Pictures";
+    ofn.lpstrInitialDir = initialDir;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY | OFN_EXPLORER;
 
     success = GetOpenFileName(&ofn);
