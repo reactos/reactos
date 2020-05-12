e04d55c6ecad9b34bd899d7983d5f081838315bc
Kyle Katarn <contact@kcsoftwares.com>
2020-05-12 23:10:09 +0200

    fix (extern missing)

--- base/applications/regedit/childwnd.c
+++ base/applications/regedit/childwnd.c
@@ -228,7 +228,7 @@ LRESULT CALLBACK AddressBarProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
     return CallWindowProcW(oldwndproc, hwnd, uMsg, wParam, lParam);
 }
 
-VOID
+extern VOID
 UpdateAddress(HTREEITEM hItem, HKEY hRootKey, LPCWSTR pszPath)
 {
     LPCWSTR keyPath, rootName;
