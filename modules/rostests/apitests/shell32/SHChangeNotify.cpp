fc0c96206c55701e5c3854e59f0846ee73739be2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-05-16 20:47:05 +0900

    split SHChangeNotify.h

--- modules/rostests/apitests/shell32/SHChangeNotify.cpp
+++ modules/rostests/apitests/shell32/SHChangeNotify.cpp
@@ -8,36 +8,13 @@
 #include "shelltest.h"
 #include <shlwapi.h>
 #include <stdio.h>
-
-#define WM_SHELL_NOTIFY (WM_USER + 100)
-#define WM_GET_NOTIFY_FLAGS (WM_USER + 101)
-#define WM_CLEAR_FLAGS (WM_USER + 102)
-#define WM_SET_PATHS (WM_USER + 103)
+#include "SHChangeNotify.h"
 
 #define DONT_SEND 0x24242424
-
-static WCHAR s_dir1[MAX_PATH];  // "%TEMP%\\WatchDir1"
-static WCHAR s_dir2[MAX_PATH];  // "%TEMP%\\WatchDir1\\Dir2"
-static WCHAR s_dir3[MAX_PATH];  // "%TEMP%\\WatchDir1\\Dir3"
-static WCHAR s_file1[MAX_PATH]; // "%TEMP%\\WatchDir1\\File1.txt"
-static WCHAR s_file2[MAX_PATH]; // "%TEMP%\\WatchDir1\\File2.txt"
 
 static HWND s_hwnd = NULL;
 static const WCHAR s_szName[] = L"SHChangeNotify testcase";
 static WCHAR s_szSubProgram[MAX_PATH];
-
-typedef enum TYPE
-{
-    TYPE_RENAMEITEM,
-    TYPE_CREATE,
-    TYPE_DELETE,
-    TYPE_MKDIR,
-    TYPE_RMDIR,
-    TYPE_UPDATEDIR,
-    TYPE_UPDATEITEM,
-    TYPE_RENAMEFOLDER,
-    TYPE_FREESPACE
-} TYPE;
 
 typedef void (*ACTION)(void);
 
@@ -238,25 +215,7 @@ DoTestEntry(const TEST_ENTRY *entry)
 static BOOL
 DoInit(void)
 {
-    WCHAR szTemp[MAX_PATH], szPath[MAX_PATH];
-
-    GetTempPathW(_countof(szTemp), szTemp);
-    GetLongPathNameW(szTemp, szPath, _countof(szPath));
-
-    lstrcpyW(s_dir1, szPath);
-    PathAppendW(s_dir1, L"WatchDir1");
-
-    lstrcpyW(s_dir2, s_dir1);
-    PathAppendW(s_dir2, L"Dir2");
-
-    lstrcpyW(s_dir3, s_dir1);
-    PathAppendW(s_dir3, L"Dir3");
-
-    lstrcpyW(s_file1, s_dir1);
-    PathAppendW(s_file1, L"File1.txt");
-
-    lstrcpyW(s_file2, s_dir1);
-    PathAppendW(s_file2, L"File2.txt");
+    DoInitPaths();
 
     CreateDirectoryW(s_dir1, NULL);
 
