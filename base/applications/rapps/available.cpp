26968b22cd52944f77a4a2f87115d8d277c49e89
赫 杨 <1160386205@qq.com>
2020-05-30 21:22:56 +0800

    always use rappmgr.cab as file name when downloading db. ignore the URL

--- base/applications/rapps/available.cpp
+++ base/applications/rapps/available.cpp
@@ -264,6 +264,7 @@ AvailableStrings::AvailableStrings()
     {
         szAppsPath = szPath;
         PathAppendW(szAppsPath.GetBuffer(MAX_PATH), L"rapps");
+        PathAddBackslashW(szAppsPath.GetBuffer());
         szAppsPath.ReleaseBuffer();
 
         szCabName = L"rappmgr.cab";
