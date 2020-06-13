b84071ee20f6dfc5447c354194aece142d016782
Victor Perevertkin <victor.perevertkin@reactos.org>
2020-06-13 06:03:24 +0300

    [SDK] Introduce FDPRINT macro for filtered debug output with custom level

---
+++ drivers/usb/usbstor/debug.h
@@ -0,0 +1,18 @@
+/*
+ * PROJECT:     ReactOS Universal Serial Bus Bulk Storage Driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Debug helpers
+ * COPYRIGHT:   2020 Victor Perevertkin (victor.perevertkin@reactos.org)
+ */
+
+#define NDEBUG
+#include <debug.h>
+
+#define DBGLVL_DISK (1 << 31)
+#define DBGLVL_PNP (1 << 30)
+
+#define ERR(fmt, ...) ERR__(DPFLTR_USBSTOR_ID, fmt, __VA_ARGS__)
+#define WARN(fmt, ...) WARN__(DPFLTR_USBSTOR_ID, fmt, __VA_ARGS__)
+#define TRACE(fmt, ...) TRACE__(DPFLTR_USBSTOR_ID, fmt, __VA_ARGS__)
+#define INFO(fmt, ...) INFO__(DPFLTR_USBSTOR_ID, fmt, __VA_ARGS__)
+#define FDPRINT(lvl, fmt, ...) FDPRINT__(DPFLTR_USBSTOR_ID, lvl, __VA_ARGS__)
