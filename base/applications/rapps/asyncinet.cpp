f467aa7edb057ba6eb97553ee118c598592f0f6b
杨赫 <1160386205@qq.com>
2020-06-14 19:25:10 +0800

    correct my e-mail address

--- base/applications/rapps/asyncinet.cpp
+++ base/applications/rapps/asyncinet.cpp
@@ -3,7 +3,7 @@
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * FILE:        base/applications/rapps/asyncinet.cpp
  * PURPOSE:     Async Internet operation using WinINet
- * COPYRIGHT:   Copyright 2020 He Yang            (1163086205@qq.com)
+ * COPYRIGHT:   Copyright 2020 He Yang            (1160386205@qq.com)
  */
 
 
