91c9cc771fa97280aace646dc7faefe12b18a103
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-06-14 13:49:22 +0200

    [WIN32KNT_APITEST] Tweak all '#include <win32nt.h>'

--- modules/rostests/apitests/win32nt/ntuser/NtUserCallHwndOpt.c
+++ modules/rostests/apitests/win32nt/ntuser/NtUserCallHwndOpt.c
@@ -5,7 +5,7 @@
  * PROGRAMMERS:
  */
 
-#include <win32nt.h>
+#include "../win32nt.h"
 
 void
 Test_HwndOptRoutine_SetProgmanWindow(void) /* 0x4a */
--- modules/rostests/apitests/win32nt/ntuser/NtUserGetIconInfo.c
+++ modules/rostests/apitests/win32nt/ntuser/NtUserGetIconInfo.c
@@ -5,7 +5,7 @@
  * PROGRAMMERS:
  */
 
-#include <win32nt.h>
+#include "../win32nt.h"
 #include "../resource.h"
 
 START_TEST(NtUserGetIconInfo)
