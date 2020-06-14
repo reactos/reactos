0a5482f7901f9f69e7a68a0ffff33a35eae96e34
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-06-14 13:49:22 +0200

    [ACCESS] GeneralPageProc().WM_INITDIALOG: Do not change iTimeOutMSec
    Partly revert 416d630 (0.4.15-dev-6974).
    CORE-19303

--- dll/cpl/access/general.c
+++ dll/cpl/access/general.c
@@ -196,8 +196,8 @@ GeneralPageProc(HWND hwndDlg,
                            IDC_RESET_BOX,
                            pGlobalData->accessTimeout.dwFlags & ATF_TIMEOUTON ? BST_CHECKED : BST_UNCHECKED);
             FillResetComboBox(GetDlgItem(hwndDlg, IDC_RESET_COMBO));
-            pGlobalData->accessTimeout.iTimeOutMSec = max(FIVE_MINS_IN_MS, pGlobalData->accessTimeout.iTimeOutMSec);
-            iCurSel = (pGlobalData->accessTimeout.iTimeOutMSec / FIVE_MINS_IN_MS) - 1;
+            iCurSel = pGlobalData->accessTimeout.iTimeOutMSec >= FIVE_MINS_IN_MS
+                      ? (pGlobalData->accessTimeout.iTimeOutMSec / FIVE_MINS_IN_MS) - 1 : 0;
             SendDlgItemMessage(hwndDlg, IDC_RESET_COMBO, CB_SETCURSEL, iCurSel, 0);
             EnableWindow(GetDlgItem(hwndDlg, IDC_RESET_COMBO),
                          pGlobalData->accessTimeout.dwFlags & ATF_TIMEOUTON ? TRUE : FALSE);
