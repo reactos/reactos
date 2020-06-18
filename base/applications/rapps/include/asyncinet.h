441c2d219096b9e27c382e7332d4864e4f069939
He Yang <1160386205@qq.com>
2020-06-18 13:02:00 +0800

    Update base/applications/rapps/include/asyncinet.h
    Co-authored-by: Mark Jansen <mark.jansen@reactos.org>

--- base/applications/rapps/include/asyncinet.h
+++ base/applications/rapps/include/asyncinet.h
@@ -8,7 +8,7 @@ enum ASYNC_EVENT
 {
     ASYNCINET_DATA,           // wParam is the Data retrieved from the internet, lParam is the length of Data
 
-    ASYNCINET_COMPLETE,       // wParam and lParam is not used.
+    ASYNCINET_COMPLETE,       // wParam and lParam are not used.
                               // when receiving this, AsyncInet will be free soon and should not used anymore
 
     ASYNCINET_CANCELLED,      // wParam and lParam is not used.
