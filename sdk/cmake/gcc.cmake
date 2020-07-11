e6f48a9720539c5bd274ee5e0dfb014365eeab37
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-07-12 00:15:59 +0200

    [CMAKE] Add a TODO about an obsolete 'CMAKE_CXX_COMPILER_VERSION' check
    following upgrade to RosBE 2.2.0 support and GCC 8.4.
    
    CORE-16798

--- sdk/cmake/gcc.cmake
+++ sdk/cmake/gcc.cmake
@@ -253,6 +253,7 @@ set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -Wl,--enable-auto-image-base,--disable-aut
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS_INIT} -Wl,--disable-stdcall-fixup")
 set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS_INIT} -Wl,--disable-stdcall-fixup")
 
+# TODO: Now-dead block, after upgrade to GCC 8.4. (CORE-16798)
 if((CMAKE_C_COMPILER_ID STREQUAL "GNU") AND
    (NOT CMAKE_BUILD_TYPE STREQUAL "Release") AND
    (NOT CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7.0))
