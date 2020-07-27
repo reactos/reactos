7988a2ac54da88240b632d00c7597a7f6da13db9
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2020-07-27 19:29:59 +0200

    [CMD_ROSTEST] Add more tests for the command echoer and new tests for the parser.

--- modules/rostests/win32/cmd/test_echoer_parser.cmd
+++ modules/rostests/win32/cmd/test_echoer_parser.cmd
@@ -251,6 +251,457 @@ if 1==1 (
 )
 
 
+
+:: Parsing FOR, IF and REM commands.
+::
+:: Note: wrong syntaxes for FOR, IF, REM or parsing operators
+:: are considered syntactic errors and batch running stops.
+:: For all other commands, execution continues.
+::
+
+@echo on
+
+echo --------- Parsing FOR, IF and REM commands ---------
+
+fOr@space@@space@@space@@tab@@tab@  /d@space@@space@@tab@ %%d iN     (*)@space@@space@@tab@ do   eCHo %%d
+:for /asdf
+:: for /d %%d in (*) do echo %%d
+:: for /d %%d in (*) do echo %%~fd
+for /d %%d in (*) do echo %%~ed
+for /d %%d in (*) do echo %~d0:
+fOr@tab@@space@@space@@space@@space@@space@@space@@space@@space@@tab@/l@space@@tab@ %%c@space@@space@@tab@ iN@tab@  (1,1,5)@space@@space@@tab@   Do@space@@space@@tab@@space@@tab@ echo@tab@@space@@space@@space@@space@@tab@@tab@%%c
+
+
+iF@space@@space@@tab@   457@space@@tab@@space@@tab@neQ@space@@space@@tab@  458@space@@tab@  (@space@@tab@ echo@space@@tab@ %~d0 )  eLSe    (@space@@space@@tab@ echo $~d0@space@@tab@ )
+
+::: if 457 nea 458 (echo %~d0) eLSe (echo $~dp)
+:if 457 nea 458 (echo yo) else (echo ya)
+:if 457 leq 458 (rem/? /d)
+:: if 457 leq 458 ( if 457 nea 458 (echo hi) else (echo yo) )
+
+iF@space@@space@@tab@  "2147483647"@space@@space@@tab@ gEq@space@@tab@@space@@tab@ "2147483648"@space@@tab@  (Echo Larger)@space@@space@@tab@    Else@space@@tab@  (:Echo Smaller)
+ecHO sMaLlEr)
+
+iF@space@@space@@tab@eRrOrlevel 0 echo hi!
+
+
+:: rem@tab@@space@@tab@ /df@space@@space@@tab@ /d
+
+rEM     /v@space@@tab@/d
+REm@space@@space@@space@@tab@@tab@  /d
+rEm REM2 /d
+reM@space@@tab@ /d@space@@tab@ >@space@@tab@ NUL
+:reM /?/d
+
+:: These commands, even commented out via the parser comment colon,
+:: cause parsing errors even when being commented out.
+:: Replace the two '%' by a single one to see the effects.
+::
+:echo %~f0 %%~dp %~p1
+:echo %%~d0 $~dp
+:echo %%~dp:
+:echo %%~b0
+
+
+
+::
+:: Parsing random commands
+::
+
+echo --------- Parsing random commands ---------
+
+:: Parsing these commands should fail (when being un-commmented).
+:@a ( b & |
+: a ( b & |
+
+:: If goto fails, batch stops. (Done OK in ROS' cmd)
+:goto /asdf whatever
+
+dir > NUL &(b)
+
+
+setlocal enabledelayedexpansion
+
+echo %~dp0
+
+set SOMEVAR=C:\ReAcToS
+rem %SOMEVAR% |
+
+Set "_var=first"
+Set "_var=second" & Rem %_var% !_var!
+Set "_var=third" & Echo %_var% !_var!
+
+endlocal
+
+
+
+::
+:: Parsing line continuations, either from parenthesized blocks
+:: or via the escape caret.
+:: It may be informative to manually run this test under CMD
+:: with cmd!fDumpTokens and cmd!fDumpParse flags enabled.
+::
+
+echo --------- Parsing line continuations ---------
+
+(
+a & b
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ '&'[a, CRLF[b, CRLF[c,d] ] ] ]
+::
+
+
+(
+a && b
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF['&&'[a,b], CRLF[c,d]] ]
+::
+
+
+(a & b)
+
+(
+a & b
+)
+
+
+(
+a & b
+c & d
+)
+
+
+(a & ^
+b
+c & d
+)
+::
+:: Parsed as:
+:: '('[ '&'[a, CRLF[b, '&'[c,d] ] ]
+::
+
+
+(
+a & b
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ '&'[a, CRLF[b, CRLF[c,d] ] ] ]
+::
+
+
+(
+a && b
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF['&&'[a,b], CRLF[c,d]] ]
+::
+
+
+(
+a || b
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF['||'[a,b], CRLF[c,d]] ]
+::
+
+
+(
+a
+b & c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF[a, '&'[b, CRLF[c,d]]] ]
+::
+
+
+(
+a
+b && c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF[a, CRLF['&&'[b,c], d]] ]
+::
+
+
+(
+a
+b
+c && d
+)
+::
+:: Parsed as:
+:: '('[ CRLF[a, CRLF[b, '&&'[c,d]]] ]
+::
+
+
+(
+a
+b
+c & d
+)
+::
+:: Parsed as:
+:: '('[ CRLF[a, CRLF[b, '&'[c,d]]] ]
+::
+
+
+(
+a
+b
+
+c
+d
+)
+::
+:: Parsed as:
+:: '('[ CRLF[a, CRLF[b, CRLF[c,d] ] ] ]
+::
+
+
+
+REM foo^
+bar^
+baz trol^
+
+
+if 1==1 (echo a) else (echo b)
+
+if 1==1 (echo a
+) else (echo b)
+
+if 1==1 (echo a) else (
+echo b)
+
+if 1==1 (
+echo a
+) else (
+echo b
+)
+
+
+REM if 1==1 (^
+REM echo a
+REM ) ^
+REM else
+REM (^
+REM echo b
+REM )
+
+
+REM if 1==1 (^
+REM echo a
+REM ) ^
+REM else^
+REM (^
+REM echo b
+REM )
+
+
+REM if 1==1 (^
+REM echo a
+REM ) ^
+REM else^
+REM (
+REM echo b
+REM )
+
+
+if 1==1 (^
+echo a
+)
+
+
+if 1==1 (^
+@echo a
+)
+
+
+
+(foo ^
+bar
+)
+
+(foo ^
+&& bar
+baz
+)
+
+(foo ^
+ && bar
+baz
+)
+
+REM (foo^
+REM && bar
+REM baz
+REM )
+
+(foo^
+bar
+baz
+)
+
+
+(foo &^
+& bar
+baz
+)
+
+
+(^
+foo
+bar
+)
+
+
+
+(
+
+
+foo ^
+bar
+)
+
+
+
+(
+
+
+foo ^
+&& bar
+baz
+)
+
+
+
+(
+
+
+foo ^
+ && bar
+baz
+)
+
+
+
+
+REM (
+REM
+REM
+REM foo^
+REM && bar
+REM baz
+REM )
+
+
+
+(
+
+
+foo^
+bar
+baz
+)
+
+
+
+(
+
+
+foo &^
+& bar
+baz
+)
+
+
+
+(
+
+
+^
+foo
+bar
+)
+
+
+
+::
+:: Tests for Character Escape and Line Continuation
+::
+
+(^"!pc::^=^!^")
+
+(^
+"!pc::^=^!^")
+
+(
+^"!pc::^=^!^")
+
+
+(^"!pc::^=^
+% New line %
+!^")
+
+REM  @   2>&1    (   (  dir)   )    &&     lol
+
+
+echo & ^
+&&lol
+
+
+REM echo & ^
+REM ^
+REM &&lol
+
+
+trol ^
+&&lol
+
+
+trol^
+&&lol
+
+
+REM (echo hi)^
+REM &&lol
+
+
+rem trol(^
+line
+@rem trol2""^
+line2
+:trol3^
+line3
+
+echo trol^
+line
+
+:echo trol^
+line
+
+
+
 @echo off
 
 ::
