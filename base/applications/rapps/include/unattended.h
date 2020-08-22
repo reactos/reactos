f9b1e471277b1c497129c90cd61c692336153d41
赫杨 <1160386205@qq.com>
2020-08-22 23:00:31 +0800

    [RAPPS] Help command is changed from /HELP to /?

--- base/applications/rapps/include/unattended.h
+++ base/applications/rapps/include/unattended.h
@@ -2,8 +2,8 @@
 
 #define CMD_KEY_INSTALL L"INSTALL"
 #define CMD_KEY_SETUP L"SETUP"
-#define CMD_KEY_HELP L"HELP"
+#define CMD_KEY_HELP L"?"
 
-const WCHAR UsageString[] = L"RAPPS [/INSTALL packagename] [/SETUP filename]";
+const WCHAR UsageString[] = L"RAPPS [/?] [/INSTALL packagename] [/SETUP filename]";
 
 BOOL ParseCmdAndExecute(LPWSTR lpCmdLine, BOOL bIsFirstLaunch, int nCmdShow);
