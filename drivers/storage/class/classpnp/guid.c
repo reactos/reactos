c57350e295190c74a1e7e72c4ae224b7216ffe40
Victor Perevertkin <victor.perevertkin@reactos.org>
2020-08-28 05:44:08 +0300

    [CLASSPNP][DISK_NEW][CDROM_NEW] Remove the drivers Due to WDK/DDK licensing issues
    CORE-17129

--- drivers/storage/class/classpnp/guid.c
+++
@@ -1,7 +0,0 @@
-/* DO NOT USE THE PRECOMPILED HEADER FOR THIS FILE! */
-
-#include <ntdef.h>
-#include <initguid.h>
-#include <ioevent.h>
-
-/* NO CODE HERE, THIS IS JUST REQUIRED FOR THE GUID DEFINITIONS */
