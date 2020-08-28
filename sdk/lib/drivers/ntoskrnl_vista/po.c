5e242c556646f8905485afbb5b9fd9fc8da6af5a
Victor Perevertkin <victor.perevertkin@reactos.org>
2020-08-28 15:28:45 +0300

    [NTOSKRNL_VISTA] Implement/stubplement more Etw, Io, Ke, Po functions CORE-17129

---
+++ sdk/lib/drivers/ntoskrnl_vista/po.c
@@ -0,0 +1,43 @@
+/*
+ * PROJECT:     ReactOS Kernel - Vista+ APIs
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Po functions of Vista+
+ * COPYRIGHT:   2020 Victor Perevertkin (victor.perevertkin@reactos.org)
+ */
+
+#include <ntdef.h>
+#include <ntifs.h>
+
+NTKERNELAPI
+NTSTATUS
+NTAPI
+PoRegisterPowerSettingCallback(
+    _In_opt_ PDEVICE_OBJECT DeviceObject,
+    _In_ LPCGUID SettingGuid,
+    _In_ PPOWER_SETTING_CALLBACK Callback,
+    _In_opt_ PVOID Context,
+    _Outptr_opt_ PVOID *Handle)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+_IRQL_requires_max_(APC_LEVEL)
+NTKERNELAPI
+NTSTATUS
+NTAPI
+PoUnregisterPowerSettingCallback(
+    _Inout_ PVOID Handle)
+{
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+NTKERNELAPI
+BOOLEAN
+NTAPI
+PoQueryWatchdogTime(
+    _In_ PDEVICE_OBJECT Pdo,
+    _Out_ PULONG SecondsRemaining)
+{
+    return FALSE;
+}
