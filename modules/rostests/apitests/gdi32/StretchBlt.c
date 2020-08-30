70311db3d53535a2d3afb9634c4a839e61430309
Doug Lyons <douglyons@douglyons.com>
2020-08-30 09:09:33 -0500

    Revise per Pull Request comments and remove unused section.

--- modules/rostests/apitests/gdi32/StretchBlt.c
+++ modules/rostests/apitests/gdi32/StretchBlt.c
@@ -1,8 +1,8 @@
 /*
- * PROJECT:         ReactOS API tests
- * LICENSE:         GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
- * PURPOSE:         Tests for the StretchBlt API
- * COPYRIGHT:       Copyright 2020 Doug Lyons <douglyons at douglyons dot com>
+ * PROJECT:     ReactOS API tests
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Tests for the StretchBlt API
+ * COPYRIGHT:   Copyright 2020 Doug Lyons (douglyons at douglyons dot com)
  */
 
 #include <stdarg.h>
@@ -55,78 +55,72 @@ static void test_StretchBlt(void)
     SelectObject(hdcDst, oldDst);
     DeleteObject(bmpDst);
 
-    memset(&biDst, 0, sizeof(BITMAPINFO));              // Clear our Bitmap Into to all zeroes
+    memset(&biDst, 0, sizeof(BITMAPINFO));
 
-    biDst.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);  // Set our Size to the header size
-    biDst.bmiHeader.biWidth = 2;                        // Set our Width to 2
-    biDst.bmiHeader.biHeight = 2;                       // Set our Height to 2 that's positive (bottom-up)
-    biDst.bmiHeader.biPlanes = 1;                       // Set out planes to 1 (1 required by Windows)
-    biDst.bmiHeader.biBitCount = 32;                    // Set out BitCount to 32 (Full Color)
-    biDst.bmiHeader.biCompression = BI_RGB;             // Set our Compression to BI_RBG (uncompressed)
+    biDst.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    biDst.bmiHeader.biWidth = 2;
+    biDst.bmiHeader.biHeight = 2;    // Set our Height to positive so we are bottom-up
+    biDst.bmiHeader.biPlanes = 1;
+    biDst.bmiHeader.biBitCount = 32; // Set out BitCount to 32 which is Full Color
+    biDst.bmiHeader.biCompression = BI_RGB;
 
-    memcpy(&biSrc, &biDst, sizeof(BITMAPINFO));         // Put same Destination params into the Source
+    memcpy(&biSrc, &biDst, sizeof(BITMAPINFO)); // Put same Destination params into the Source
 
     bmpSrc = CreateDIBSection(hdcSrc, &biSrc, DIB_RGB_COLORS, (void**)&srcBuffer, NULL, 0);
     oldSrc = SelectObject(hdcSrc, bmpSrc);
     bmpDst = CreateDIBSection(hdcDst, &biDst, DIB_RGB_COLORS, (void**)&dstBuffer, NULL, 0);
     oldDst = SelectObject(hdcDst, bmpDst);
 
-    srcBuffer[0] = 255;             // BLUE// stored beginning bottom left
-    srcBuffer[1] = 255*256;         // GREEN
-    srcBuffer[2] = 255*256*256;     // RED
-    srcBuffer[3] = 127*256*256*256; // BLACK
+    srcBuffer[0] = 0x000000FF; // BLUE - stored beginning bottom left
+    srcBuffer[1] = 0x0000FF00; // GREEN
+    srcBuffer[2] = 0x00FF0000; // RED
+    srcBuffer[3] = 0xFF000000; // BLACK - 0xFF for alpha channel is easy to recognize when printed in hex format
 
     /* Flip Left to Right on Source */
-    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 1, 0, -2, 2, SRCCOPY );
-    expected[0] = 255*256;
-    expected[1] = 255;
-    expected[2] = 127*256*256*256;
-    expected[3] = 255*256*256;
+    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 1, 0, -2, 2, SRCCOPY);
+    expected[0] = 0x0000FF00;
+    expected[1] = 0x000000FF;
+    expected[2] = 0xFF000000;
+    expected[3] = 0x00FF0000;
 
     ok(expected[1] == dstBuffer[1], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[1], dstBuffer[1] );
+        expected[1], dstBuffer[1]);
 
     ok(expected[3] == dstBuffer[3], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[3], dstBuffer[3] );
+        expected[3], dstBuffer[3]);
 
-    /* Flip Top to Bottom  on Source */
-    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 0, 1, 2, -2, SRCCOPY );
-    expected[0] = 255*256*256;
-    expected[1] = 127*256*256*256;
-    expected[2] = 255;
-    expected[3] = 255*256;
+    /* Flip Top to Bottom on Source */
+    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 0, 1, 2, -2, SRCCOPY);
+    expected[0] = 0x00FF0000;
+    expected[1] = 0xFF000000;
+    expected[2] = 0x000000FF;
+    expected[3] = 0x0000FF00;
 
     ok(expected[0] == dstBuffer[0], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[0], dstBuffer[0] );
+        expected[0], dstBuffer[0]);
 
     ok(expected[1] == dstBuffer[1], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[1], dstBuffer[1] );
+        expected[1], dstBuffer[1]);
 
-    /* Flip Left to Right and Top To Bottom on Destination */
-    StretchBlt(hdcDst, 1, 1, -2, -2, hdcSrc, 0, 0, 2, 2, SRCCOPY );
-    expected[0] = 127*256*256*256;
-    expected[1] = 255*256*256;
-    expected[2] = 255*256;
-    expected[3] = 255;
-
-    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 1, 1, -2, -2, SRCCOPY );
-    expected[0] = 127*256*256*256;
-    expected[1] = 255*256*256;
-    expected[2] = 255*256;
-    expected[3] = 255;
+    /* Flip Left to Right and Top to Bottom on Source */
+    StretchBlt(hdcDst, 0, 0, 2, 2, hdcSrc, 1, 1, -2, -2, SRCCOPY);
+    expected[0] = 0xFF000000;
+    expected[1] = 0x00FF0000;
+    expected[2] = 0x0000FF00;
+    expected[3] = 0x000000FF;
 
     ok(expected[1] == dstBuffer[1], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[1], dstBuffer[1] );
+        expected[1], dstBuffer[1]);
 
-    /* Flip Left to Right and Top To Bottom on both Source and Destination  */
+    /* Flip Left to Right and Top to Bottom on both Source and Destination */
     StretchBlt(hdcDst, 1, 1, -2, -2, hdcSrc, 1, 1, -2, -2, SRCCOPY );
-    expected[0] = 127*256*256*256;
-    expected[1] = 255*256*256;
-    expected[2] = 255*256*256;
-    expected[3] = 255;
+    expected[0] = 0xFF000000;
+    expected[1] = 0x00FF0000;
+    expected[2] = 0x00FF0000;
+    expected[3] = 0x000000FF;
 
     ok(expected[1] == dstBuffer[1], "StretchBlt expected { %08X } got { %08X }\n",
-        expected[1], dstBuffer[1] );
+        expected[1], dstBuffer[1]);
 
     DeleteDC(hdcSrc);
 
