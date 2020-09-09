3c1f6631e62150c41d18e255079189b0313bff18
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-09-09 19:18:27 +0200

    [MSVCRT] Add vista exports

--- sdk/lib/crt/crt.cmake
+++ sdk/lib/crt/crt.cmake
@@ -292,7 +292,7 @@ list(APPEND CRT_SOURCE
     string/strtod.c
     string/strtoi64.c
     string/strtok.c
-    #string/strtok_s.c
+    string/strtok_s.c
     string/strtol.c
     string/strtoul.c
     string/strtoull.c
