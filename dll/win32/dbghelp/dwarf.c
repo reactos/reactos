237aa3a9c13f6e9a3104b4242972382ca7afdc11
winesync <ros-dev@reactos.org>
2020-09-11 19:00:46 +0200

    [WINESYNC] dbghelp: Import zlib inflate code.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id d483eb21cff1f2b44f048195106d87a414dd66b8 by Jacek Caban <jacek@codeweavers.com>

--- dll/win32/dbghelp/dwarf.c
+++ dll/win32/dbghelp/dwarf.c
@@ -24,29 +24,13 @@
 
 #define NONAMELESSUNION
 
-#include "config.h"
-
 #include <sys/types.h>
-#include <fcntl.h>
-#ifdef HAVE_SYS_STAT_H
-# include <sys/stat.h>
-#endif
-#ifdef HAVE_SYS_MMAN_H
-#include <sys/mman.h>
-#endif
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef HAVE_UNISTD_H
-# include <unistd.h>
-#endif
 #include <stdio.h>
 #include <assert.h>
 #include <stdarg.h>
-
-#ifdef HAVE_ZLIB
-#include <zlib.h>
-#endif
 
 #include "windef.h"
 #include "winternl.h"
@@ -57,6 +41,7 @@
 
 #include "dbghelp_private.h"
 #include "image_private.h"
+#include "zlib.h"
 
 #include "wine/debug.h"
 
