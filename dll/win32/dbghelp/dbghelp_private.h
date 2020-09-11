c6c3e1a2a556a14896ee0704464254ca1d2268e6
winesync <ros-dev@reactos.org>
2020-09-11 15:55:33 +0200

    [WINESYNC] dbghelp: Support full memory dumps.
    Add support of flag MinidumpWithFullMemory in function
    MinidumpWriteDump. A Memory64ListStream is added to the minidump
    streams and all memory regions of the process with MEM_COMMIT state
    are written to the last part of the minidump file.
    
    Signed-off-by: Eric Bissonnette <ebisso.dev@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id b90bbcbe750f6af0ce13f2577984f4818e05a013 by Eric Bissonnette <ebisso.dev@gmail.com>

--- dll/win32/dbghelp/dbghelp_private.h
+++ dll/win32/dbghelp/dbghelp_private.h
@@ -500,6 +500,12 @@ struct dump_memory
     ULONG                               rva;
 };
 
+struct dump_memory64
+{
+    ULONG64                             base;
+    ULONG64                             size;
+};
+
 struct dump_module
 {
     unsigned                            is_elf;
@@ -538,6 +544,9 @@ struct dump_context
     struct dump_memory*                 mem;
     unsigned                            num_mem;
     unsigned                            alloc_mem;
+    struct dump_memory64*               mem64;
+    unsigned                            num_mem64;
+    unsigned                            alloc_mem64;
     /* callback information */
     MINIDUMP_CALLBACK_INFORMATION*      cb;
 };
