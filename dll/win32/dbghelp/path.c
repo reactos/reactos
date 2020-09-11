989a748f8157cf4d78baf550cfc91e3443676ba0
winesync <ros-dev@reactos.org>
2020-09-11 15:53:03 +0200

    [WINESYNC] dbghelp: Search for debug files in module path too.
    Signed-off-by: Andreas Maier <andy1.m@gmx.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 6030ee5f6f64a2ebe6df2e505e2588eb300222c2 by Andreas Maier <andy1.m@gmx.de>

--- dll/win32/dbghelp/path.c
+++ dll/win32/dbghelp/path.c
@@ -49,6 +49,15 @@ static inline const WCHAR* file_nameW(const WCHAR* str)
     return p + 1;
 }
 
+static inline void file_pathW(const WCHAR *src, WCHAR *dst)
+{
+    int len;
+
+    for (len = strlenW(src) - 1; (len > 0) && (!is_sepW(src[len])); len--);
+    memcpy( dst, src, len * sizeof(WCHAR) );
+    dst[len] = 0;
+}
+
 /******************************************************************
  *		FindDebugInfoFile (DBGHELP.@)
  *
@@ -612,9 +621,9 @@ static BOOL CALLBACK module_find_cb(PCWSTR buffer, PVOID user)
     return mf->matched == 2;
 }
 
-BOOL path_find_symbol_file(const struct process* pcs, PCSTR full_path,
-                           const GUID* guid, DWORD dw1, DWORD dw2, PSTR buffer,
-                           BOOL* is_unmatched)
+BOOL path_find_symbol_file(const struct process* pcs, const struct module* module,
+                           PCSTR full_path, const GUID* guid, DWORD dw1, DWORD dw2,
+                           PSTR buffer, BOOL* is_unmatched)
 {
     struct module_find  mf;
     WCHAR               full_pathW[MAX_PATH];
@@ -640,6 +649,23 @@ BOOL path_find_symbol_file(const struct process* pcs, PCSTR full_path,
     if (module_find_cb(full_pathW, &mf))
     {
         WideCharToMultiByte(CP_ACP, 0, full_pathW, -1, buffer, MAX_PATH, NULL, NULL);
+        return TRUE;
+    }
+
+    /* FIXME: Use Environment-Variables (see MS docs)
+                 _NT_SYMBOL_PATH and _NT_ALT_SYMBOL_PATH
+       FIXME: Implement "Standard Path Elements" (Path) ... (see MS docs)
+              do a search for (every?) path-element like this ...
+              <path>
+              <path>\dll
+              <path>\symbols\dll
+              (dll may be exe, or sys depending on the file extension)   */
+
+    /* 2. check module-path */
+    file_pathW(module->module.LoadedImageName, tmp);
+    if (do_searchW(filename, tmp, FALSE, module_find_cb, &mf))
+    {
+        WideCharToMultiByte(CP_ACP, 0, tmp, -1, buffer, MAX_PATH, NULL, NULL);
         return TRUE;
     }
 
