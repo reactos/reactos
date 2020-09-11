dccc9f3770a1b8e27e0df77b3be6049ce7f47da9
winesync <ros-dev@reactos.org>
2020-09-11 13:04:09 +0200

    [WINESYNC] dbghelp: Read the ELF program header corresponding to the target's architecture.
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id bd620ffb443eaa2dba997dc5597cb37c44d757b7 by Zebediah Figura <zfigura@codeweavers.com>

--- dll/win32/dbghelp/image_private.h
+++ dll/win32/dbghelp/image_private.h
@@ -61,12 +61,10 @@ typedef struct section              macho_section;
 #if defined(__ELF__) && !defined(DBGHELP_STATIC_LIB)
 
 #ifdef _WIN64
-#define         Elf_Phdr        Elf64_Phdr
 #define         Elf_Dyn         Elf64_Dyn
 #define         Elf_Sym         Elf64_Sym
 #define         Elf_auxv_t      Elf64_auxv_t
 #else
-#define         Elf_Phdr        Elf32_Phdr
 #define         Elf_Dyn         Elf32_Dyn
 #define         Elf_Sym         Elf32_Sym
 #define         Elf_auxv_t      Elf32_auxv_t
