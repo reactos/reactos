02b8fa3759d0dd199e1811d313a842eda4782af2
winesync <ros-dev@reactos.org>
2020-09-11 19:04:30 +0200

    [WINESYNC] dbghelp: Don't use PATH to search for libraries.
    It should be used only for the main executable, for which we get full
    path anyway.
    
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 5bc58410f28aefea3c3f41e056a372aff4dceac7 by Jacek Caban <jacek@codeweavers.com>

--- dll/win32/dbghelp/elf_module.c
+++ dll/win32/dbghelp/elf_module.c
@@ -1435,8 +1435,7 @@ static BOOL elf_search_and_load_file(struct process* pcs, const WCHAR* filename,
         load_elf.dyn_addr    = dyn_addr;
         load_elf.elf_info    = elf_info;
 
-        ret = search_unix_path(filename, getenv("PATH"), elf_load_file_cb, &load_elf)
-            || search_unix_path(filename, getenv("LD_LIBRARY_PATH"), elf_load_file_cb, &load_elf)
+        ret = search_unix_path(filename, getenv("LD_LIBRARY_PATH"), elf_load_file_cb, &load_elf)
             || search_dll_path(filename, elf_load_file_cb, &load_elf);
     }
 
