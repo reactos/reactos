f9bc55610e2ba23a5bd1b3d3e84c61cf335aab5a
winesync <ros-dev@reactos.org>
2020-09-11 16:03:20 +0200

    [WINESYNC] dbghelp: Support S_HEAPALLOCSITE entry.
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 48a74277f5f9e0c87d8c01457236a89d94cb71d3 by Alistair Leslie-Hughes <leslie_alistair@hotmail.com>

--- dll/win32/dbghelp/msc.c
+++ dll/win32/dbghelp/msc.c
@@ -2011,6 +2011,11 @@ static BOOL codeview_snarf(const struct msc_debug_info* msc_dbg, const BYTE* roo
         case S_ALIGN_V1:
             TRACE("S-Align V1\n");
             break;
+        case S_HEAPALLOCSITE:
+            TRACE("heap site: offset=0x%08x at sect_idx 0x%04x, inst_len 0x%08x, index 0x%08x\n",
+                    sym->heap_alloc_site.offset, sym->heap_alloc_site.sect_idx,
+                    sym->heap_alloc_site.inst_len, sym->heap_alloc_site.index);
+            break;
 
         /* the symbols we can safely ignore for now */
         case S_TRAMPOLINE:
