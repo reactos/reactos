2489e7dbe11b99ba63be4d228aac2e3e3940d8ad
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-09-13 18:55:17 +0200

    [KERNEL32_VISTA] Move vista.c from kernel32 to kernel32_vista
    - Remove duplicated functions
    - Use AreFileApisANSI() instead of global variable bIsFileApiAnsi in shared kernel32_shared

--- dll/win32/kernel32/kernel32_vista/k32_vista.h
+++ dll/win32/kernel32/kernel32_vista/k32_vista.h
@@ -6,7 +6,18 @@
 #include <windef.h>
 #include <winbase.h>
 
+/* Redefine NTDDI_VERSION to 2K3 SP1 to get correct NDK definitions */
+#undef NTDDI_VERSION
+#define NTDDI_VERSION NTDDI_WS03SP1
+
+#include <ndk/iofuncs.h>
 #include <ndk/kefuncs.h>
+#include <ndk/obfuncs.h>
 #include <ndk/psfuncs.h>
 #include <ndk/rtlfuncs.h>
 
+/* CSRSS Headers */
+#include <win/base.h>
+
+/* Internal Kernel32 Header */
+#include "../include/kernel32.h"
