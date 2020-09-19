a406f848f1aa33d2883ebc0ebf0a341ca7365169
Kyle Katarn <contact@kcsoftwares.com>
2020-09-19 16:29:30 +0200

    Update win32ss/user/ntuser/scrollbar.c
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- win32ss/user/ntuser/scrollbar.c
+++ win32ss/user/ntuser/scrollbar.c
@@ -597,8 +597,7 @@ co_IntSetScrollInfo(PWND Window, INT nBar, LPCSCROLLINFO lpsi, BOOL bRedraw)
    if (lpsi->fMask & (SIF_RANGE | SIF_PAGE | SIF_DISABLENOSCROLL))
    {
       new_flags = Window->pSBInfo->WSBflags;
-      /* Cast Info->nPage to int to prevent wraparound/underflow. Fixes CORE-16491 */
-      if (Info->nMin >= (int)(Info->nMax - max((int)Info->nPage - 1, 0)))
+      if (Info->nMin + (int)max(Info->nPage, 1) > Info->nMax)
       {
          /* Hide or disable scroll-bar */
          if (lpsi->fMask & SIF_DISABLENOSCROLL)
