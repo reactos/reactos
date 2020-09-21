3d9993c20af29773aa1cffb0f56999fedd28133c
winesync <ros-dev@reactos.org>
2020-09-21 22:40:27 +0200

    [WINESYNC] d3dx9: Merge the d3dx_effect_SetMatrix() helper.
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id b1621b2c28c57af09064d69e6cbe8573db88b555 by Michael Stefaniuc <mstefani@winehq.org>

--- dll/directx/wine/d3dx9_36/effect.c
+++ dll/directx/wine/d3dx9_36/effect.c
@@ -1530,39 +1530,6 @@ static HRESULT d3dx9_base_effect_get_vector_array(struct d3dx9_base_effect *base
     return D3DERR_INVALIDCALL;
 }
 
-static HRESULT d3dx9_base_effect_set_matrix(struct d3dx9_base_effect *base,
-        D3DXHANDLE parameter, const D3DXMATRIX *matrix)
-{
-    struct d3dx_parameter *param = get_valid_parameter(base, parameter);
-
-    if (param && !param->element_count)
-    {
-        TRACE("Class %s\n", debug_d3dxparameter_class(param->class));
-
-        switch (param->class)
-        {
-            case D3DXPC_MATRIX_ROWS:
-                set_matrix(param, matrix);
-                set_dirty(param);
-                return D3D_OK;
-
-            case D3DXPC_SCALAR:
-            case D3DXPC_VECTOR:
-            case D3DXPC_OBJECT:
-            case D3DXPC_STRUCT:
-                break;
-
-            default:
-                FIXME("Unhandled class %s\n", debug_d3dxparameter_class(param->class));
-                break;
-        }
-    }
-
-    WARN("Parameter not found.\n");
-
-    return D3DERR_INVALIDCALL;
-}
-
 static HRESULT d3dx9_base_effect_set_matrix_array(struct d3dx9_base_effect *base,
         D3DXHANDLE parameter, const D3DXMATRIX *matrix, UINT count)
 {
@@ -3450,10 +3417,36 @@ static HRESULT WINAPI d3dx_effect_GetVectorArray(ID3DXEffect *iface, D3DXHANDLE 
 static HRESULT WINAPI d3dx_effect_SetMatrix(ID3DXEffect *iface, D3DXHANDLE parameter, const D3DXMATRIX *matrix)
 {
     struct d3dx_effect *effect = impl_from_ID3DXEffect(iface);
+    struct d3dx_parameter *param = get_valid_parameter(&effect->base_effect, parameter);
 
     TRACE("iface %p, parameter %p, matrix %p.\n", iface, parameter, matrix);
 
-    return d3dx9_base_effect_set_matrix(&effect->base_effect, parameter, matrix);
+    if (param && !param->element_count)
+    {
+        TRACE("Class %s.\n", debug_d3dxparameter_class(param->class));
+
+        switch (param->class)
+        {
+            case D3DXPC_MATRIX_ROWS:
+                set_matrix(param, matrix);
+                set_dirty(param);
+                return D3D_OK;
+
+            case D3DXPC_SCALAR:
+            case D3DXPC_VECTOR:
+            case D3DXPC_OBJECT:
+            case D3DXPC_STRUCT:
+                break;
+
+            default:
+                FIXME("Unhandled class %s.\n", debug_d3dxparameter_class(param->class));
+                break;
+        }
+    }
+
+    WARN("Parameter not found.\n");
+
+    return D3DERR_INVALIDCALL;
 }
 
 static HRESULT WINAPI d3dx_effect_GetMatrix(ID3DXEffect *iface, D3DXHANDLE parameter, D3DXMATRIX *matrix)
