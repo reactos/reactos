07f9f7965a4d40556d2e7717b0d03861f62cdd25
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-09-24 07:49:16 +0200

    [CPPRT][CRT] Move c++ exception handlers to crt

--- sdk/lib/cpprt/amd64/cpprt.s
+++ sdk/lib/cpprt/amd64/cpprt.s
@@ -19,9 +19,6 @@ DEFINE_ALIAS ??_M@YAXPEAX_K1P6AX0@Z@Z, ?MSVCRTEX_eh_vector_destructor_iterator@@
 ; void __cdecl `eh vector destructor iterator'(void * __ptr64,unsigned __int64,int,void (__cdecl*)(void * __ptr64))
 DEFINE_ALIAS ??_M@YAXPEAX_KHP6AX0@Z@Z, ?MSVCRTEX_eh_vector_destructor_iterator@@YAXPEAX_KHP6AX0@Z@Z
 
-; These are the same
-DEFINE_ALIAS __CxxFrameHandler3, __CxxFrameHandler
-
 ; void __cdecl operator delete(void * __ptr64,struct std::nothrow_t const & __ptr64)
 DEFINE_ALIAS ??3@YAXPEAXAEBUnothrow_t@std@@@Z, ??3@YAXPEAX@Z
 
