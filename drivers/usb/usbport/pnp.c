e693d5ffc08e945e0d3d3c4b7172d3f2b79e5f0e
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-10-05 15:43:47 +0200

    [USBPORT] Make unload work

--- drivers/usb/usbport/pnp.c
+++ drivers/usb/usbport/pnp.c
@@ -490,7 +490,6 @@ NTAPI
 USBPORT_StopDevice(IN PDEVICE_OBJECT FdoDevice)
 {
     DPRINT1("USBPORT_StopDevice: UNIMPLEMENTED. FIXME\n");
-    DbgBreakPoint();
     return STATUS_SUCCESS;
 }
 
@@ -901,6 +900,7 @@ USBPORT_StartDevice(IN PDEVICE_OBJECT FdoDevice,
     return Status;
 
 ExitWithError:
+    USBPORT_StopWorkerThread(FdoDevice);
     USBPORT_StopDevice(FdoDevice);
 
     DPRINT1("USBPORT_StartDevice: ExitWithError Status - %lx\n", Status);
