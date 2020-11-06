f9bd7516058ce1b174fb978bd228a664eff0d077
Jérôme Gardou <jerome.gardou@reactos.org>
2020-11-06 17:27:32 +0100

    [CRT] Fix __forceinline definition for clang

--- sdk/include/crt/_mingw.h
+++ sdk/include/crt/_mingw.h
@@ -193,7 +193,7 @@ allow GCC to optimize away some EH unwind code, at least in DW2 case.  */
 # ifdef __cplusplus
 #  define __forceinline inline __attribute__((__always_inline__))
 # else
-#  if ( __MINGW_GNUC_PREREQ(4, 3)  &&  __STDC_VERSION__ >= 199901L)
+#  if (( __MINGW_GNUC_PREREQ(4, 3)  &&  __STDC_VERSION__ >= 199901L) || defined(__clang__))
 #   define __forceinline extern inline __attribute__((__always_inline__,__gnu_inline__))
 #  else
 #   define __forceinline extern __inline__ __attribute__((__always_inline__))
