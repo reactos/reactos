bd0445dbf21ec930d235d30b4d8397b5348e37c7
Jérôme Gardou <jerome.gardou@reactos.org>
2020-11-16 09:24:58 +0100

    [NTOS/FSRTL] Implement FsRtlAcquireToCreateMappedSection using filter callbacks

--- ntoskrnl/include/internal/fsrtl.h
+++ ntoskrnl/include/internal/fsrtl.h
@@ -140,3 +140,11 @@ FsRtlInitSystem(
 //
 extern PERESOURCE FsRtlPagingIoResources;
 extern PAGED_LOOKASIDE_LIST FsRtlFileLockLookasideList;
+
+//
+// File locking routine
+//
+NTSTATUS
+NTAPI
+FsRtlAcquireToCreateMappedSection(_In_ PFILE_OBJECT FileObject,
+                                  _In_ ULONG SectionPageProtection);
