a7d28d5204969dfc02abc290cca7988617718e8d
winesync <ros-dev@reactos.org>
2020-12-08 18:22:04 +0100

    [WINESYNC] wininet: Strip filename if no path is set in cookie.
    The order of the stored cookies doesn't match in /testC, so
    be a bit less strict in the test.
    
    wine-staging patch by Michael Müller <michael@fds-team.de>

--- modules/rostests/winetests/wininet/http.c
+++ modules/rostests/winetests/wininet/http.c
@@ -2364,7 +2364,7 @@ static DWORD CALLBACK server_thread(LPVOID param)
         }
         if (strstr(buffer, "/testC"))
         {
-            if (strstr(buffer, "Cookie: cookie=biscuit"))
+            if (strstr(buffer, "cookie=biscuit"))
                 send(c, okmsg, sizeof okmsg-1, 0);
             else
                 send(c, notokmsg, sizeof notokmsg-1, 0);
@@ -3352,7 +3352,7 @@ static void test_header_override(int port)
     ret = HttpSendRequestA(req, NULL, 0, NULL, 0);
     ok(ret, "HttpSendRequest failed\n");
 
-    test_status_code_todo(req, 200);
+    test_status_code(req, 200);
 
     InternetCloseHandle(req);
     req = HttpOpenRequestA(con, NULL, "/test_cookie_check_host_override", NULL, NULL, NULL, INTERNET_FLAG_KEEP_CONNECTION, 0);
@@ -3361,7 +3361,7 @@ static void test_header_override(int port)
     ret = HttpSendRequestA(req, NULL, 0, NULL, 0);
     ok(ret, "HttpSendRequest failed\n");
 
-    test_status_code_todo(req, 200);
+    test_status_code(req, 200);
 
     InternetCloseHandle(req);
     InternetSetCookieA("http://test.local", "foo", "bar");
