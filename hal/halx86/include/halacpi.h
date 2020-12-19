9792dc8b409d12bd88b946c18b219e7e16b50255
Vadim Galyant <vgal@rambler.ru>
2020-12-19 08:48:59 +0400

    [HAL] Add ACPI table (MADT) definitions.

--- hal/halx86/include/halacpi.h
+++ hal/halx86/include/halacpi.h
@@ -1,5 +1,126 @@
 #pragma once
 
+/* ACPI Specification constants */
+
+/* Names within the namespace are 4 bytes long */
+#define ACPI_NAMESEG_SIZE               4     // Fixed by ACPI spec
+
+/* Sizes for ACPI table headers */
+#define ACPI_OEM_ID_SIZE                6
+#define ACPI_OEM_TABLE_ID_SIZE          8
+
+/* Master ACPI Table Header.
+   This common header is used by all ACPI tables except the RSDP and FACS.
+*/
+typedef struct _ACPI_TABLE_HEADER
+{
+    CHAR Signature[ACPI_NAMESEG_SIZE];        // ASCII table signature
+    ULONG Length;                             // Length of table in bytes, including this header
+    UCHAR Revision;                           // ACPI Specification minor version number
+    UCHAR Checksum;                           // To make sum of entire table == 0
+    CHAR OemId[ACPI_OEM_ID_SIZE];             // ASCII OEM identification
+    CHAR OemTableId[ACPI_OEM_TABLE_ID_SIZE];  // ASCII OEM table identification
+    ULONG OemRevision;                        // OEM revision number
+    CHAR AslCompilerId[ACPI_NAMESEG_SIZE];    // ASCII ASL compiler vendor ID
+    ULONG AslCompilerRevision;                // ASL compiler version
+
+} ACPI_TABLE_HEADER, *PACPI_TABLE_HEADER;
+
+/* Common subtable headers */
+
+/* Generic subtable header (used in MADT, SRAT, etc.) */
+typedef struct _ACPI_SUBTABLE_HEADER
+{
+    UCHAR Type;
+    UCHAR Length;
+
+} ACPI_SUBTABLE_HEADER, *PACPI_SUBTABLE_HEADER;
+
+/* Values for MADT subtable type in ACPI_SUBTABLE_HEADER */
+enum AcpiMadtType
+{
+    ACPI_MADT_TYPE_LOCAL_APIC               = 0,
+    ACPI_MADT_TYPE_IO_APIC                  = 1,
+    ACPI_MADT_TYPE_INTERRUPT_OVERRIDE       = 2,
+    ACPI_MADT_TYPE_NMI_SOURCE               = 3,
+    ACPI_MADT_TYPE_LOCAL_APIC_NMI           = 4,
+    ACPI_MADT_TYPE_LOCAL_APIC_OVERRIDE      = 5,
+    ACPI_MADT_TYPE_IO_SAPIC                 = 6,
+    ACPI_MADT_TYPE_LOCAL_SAPIC              = 7,
+    ACPI_MADT_TYPE_INTERRUPT_SOURCE         = 8,
+    ACPI_MADT_TYPE_LOCAL_X2APIC             = 9,
+    ACPI_MADT_TYPE_LOCAL_X2APIC_NMI         = 10,
+    ACPI_MADT_TYPE_GENERIC_INTERRUPT        = 11,
+    ACPI_MADT_TYPE_GENERIC_DISTRIBUTOR      = 12,
+    ACPI_MADT_TYPE_GENERIC_MSI_FRAME        = 13,
+    ACPI_MADT_TYPE_GENERIC_REDISTRIBUTOR    = 14,
+    ACPI_MADT_TYPE_GENERIC_TRANSLATOR       = 15,
+    ACPI_MADT_TYPE_RESERVED                 = 16 // 16 and greater are reserved
+};
+
+/* MADT Subtables, correspond to Type in ACPI_SUBTABLE_HEADER */
+
+/* 0: Processor Local APIC */
+typedef struct _ACPI_MADT_LOCAL_APIC
+{
+    ACPI_SUBTABLE_HEADER Header;
+    UCHAR ProcessorId;          // ACPI processor id
+    UCHAR Id;                   // Processor's local APIC id
+    ULONG LapicFlags;
+
+} ACPI_MADT_LOCAL_APIC, *PACPI_MADT_LOCAL_APIC;
+
+/* 1: IO APIC */
+typedef struct _ACPI_MADT_IO_APIC
+{
+    ACPI_SUBTABLE_HEADER Header;
+    UCHAR Id;                   // I/O APIC ID
+    UCHAR Reserved;             // Reserved - must be zero
+    ULONG Address;              // APIC physical address
+    ULONG GlobalIrqBase;        // Global system interrupt where INTI lines start
+
+} ACPI_MADT_IO_APIC, *PACPI_MADT_IO_APIC;
+
+/* 2: Interrupt Override */
+#include <pshpack1.h>
+typedef struct _ACPI_MADT_INTERRUPT_OVERRIDE
+{
+    ACPI_SUBTABLE_HEADER Header;
+    UCHAR Bus;                  // 0 - ISA
+    UCHAR SourceIrq;            // Interrupt source (IRQ)
+    ULONG GlobalIrq;            // Global system interrupt
+    USHORT IntiFlags;
+
+} ACPI_MADT_INTERRUPT_OVERRIDE, *PACPI_MADT_INTERRUPT_OVERRIDE;
+#include <poppack.h>
+
+/* 3: NMI Source */
+typedef struct _ACPI_MADT_NMI_SOURCE
+{
+    ACPI_SUBTABLE_HEADER Header;
+    USHORT IntiFlags;
+    ULONG GlobalIrq;            // Global system interrupt
+
+} ACPI_MADT_NMI_SOURCE, *PACPI_MADT_NMI_SOURCE;
+
+/* 4: Local APIC NMI */
+typedef struct _ACPI_MADT_LOCAL_APIC_NMI
+{
+    ACPI_SUBTABLE_HEADER Header;
+    UCHAR ProcessorId;          // ACPI processor id
+    USHORT IntiFlags;
+    UCHAR Lint;                 // LINTn to which NMI is connected
+} ACPI_MADT_LOCAL_APIC_NMI;
+
+/* MADT - Multiple APIC Description Table */
+typedef struct _ACPI_TABLE_MADT
+{
+    ACPI_TABLE_HEADER Header;   // Common ACPI table header
+    ULONG Address;              // Physical address of local APIC
+    ULONG Flags;
+
+} ACPI_TABLE_MADT, *PACPI_TABLE_MADT;
+
 //
 // Internal HAL structure
 //
