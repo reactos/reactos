98698dea9a267496bbc3ae3de6a0f70a95cfa441
Vadim Galyant <vgal@rambler.ru>
2020-12-20 00:54:14 +0400

    [HAL] Implement HalpMapNtToHwProcessorId().

--- hal/halx86/apic/halinit.c
+++ hal/halx86/apic/halinit.c
@@ -29,6 +29,7 @@ PKPCR HalpProcessorPCR[MAX_CPUS];
 HALP_MP_INFO_TABLE HalpMpInfoTable;
 PLOCAL_APIC HalpProcLocalApicTable = NULL;
 UCHAR HalpMaxProcsPerCluster = 0;
+BOOLEAN HalpForceApicPhysicalDestinationMode = FALSE;
 
 /* FUNCTIONS ****************************************************************/
 
@@ -58,8 +59,16 @@ UCHAR
 NTAPI
 HalpMapNtToHwProcessorId(UCHAR Number)
 {
-    // FIXME UNIMPLIMENTED;
-    ASSERT(FALSE);
+    ASSERT(HalpForceApicPhysicalDestinationMode == FALSE);
+
+    if (!HalpMaxProcsPerCluster)
+    {
+        ASSERT(Number < 8);
+        return (1 << Number);
+    }
+
+    // FIXME
+    DbgBreakPoint();
     return 0;
 }
 
