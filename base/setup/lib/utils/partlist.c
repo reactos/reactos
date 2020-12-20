ea47455687c7713b4487188d4d0185fa6e66a7eb
Joachim Henze <Joachim.Henze@reactos.org>
2020-12-21 00:21:49 +0100

    [0.4.11][SETUP] Silence some noisy DPRINT1
    picked from 0.4.12-dev-260-g 0150bb964a411474d6cbd60ee45421bb6281deae

--- base/setup/lib/utils/partlist.c
+++ base/setup/lib/utils/partlist.c
@@ -2972,7 +2972,7 @@ CheckActiveSystemPartition(
         return;
     }
 
-    DPRINT1("We are here (1)!\n");
+    DPRINT("We are here (1)!\n");
 
     List->SystemPartition = NULL;
     List->OriginalSystemPartition = NULL;
@@ -3010,7 +3010,7 @@ CheckActiveSystemPartition(
         DPRINT1("NewDisk TRUE but first partition is used?\n");
     }
 
-    DPRINT1("We are here (2)!\n");
+    DPRINT("We are here (2)!\n");
 
     /*
      * The disk is not new, check if any partition is initialized;
@@ -3053,7 +3053,7 @@ CheckActiveSystemPartition(
     List->SystemPartition = NULL;
     List->OriginalSystemPartition = NULL;
 
-    DPRINT1("We are here (3)!\n");
+    DPRINT("We are here (3)!\n");
 
     /* The disk is not new, scan all partitions to find the (active) system partition */
     ListEntry = DiskEntry->PrimaryPartListHead.Flink;
