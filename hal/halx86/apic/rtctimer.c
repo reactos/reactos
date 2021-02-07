a08eb92223953a6562635d5daa1bea0eb2d95c4e
Vadim Galyant <vgal@rambler.ru>
2021-02-07 21:44:31 +0400

    [HAL] Implement HalpClockInterruptStubHandler().

--- hal/halx86/apic/rtctimer.c
+++ hal/halx86/apic/rtctimer.c
@@ -348,8 +348,27 @@ VOID
 FASTCALL
 HalpClockInterruptStubHandler(_In_ PKTRAP_FRAME TrapFrame)
 {
-    DPRINT1("HalpClockInterruptStubHandler: TrapFrame %X. DbgBreakPoint()\n", TrapFrame);
-    DbgBreakPoint();
+    UCHAR CmosData;
+
+    /* Enter trap */
+    KiEnterInterruptTrap(TrapFrame);
+
+    /* Read register C, so that the next interrupt can happen */
+    CmosData = HalpReadCmos(RTC_REGISTER_C);
+    CmosData = HalpReadCmos(RTC_REGISTER_C);
+
+    while (CmosData & RTC_REG_C_IRQ)
+    {
+        CmosData = HalpReadCmos(RTC_REGISTER_C);
+    }
+
+    ApicWrite(APIC_EOI, 0);
+
+  #ifdef __REACTOS__
+    KiEoiHelper(TrapFrame);
+  #else
+    #error FIXME call Kei386EoiHelper()
+  #endif
 }
 #endif
 
