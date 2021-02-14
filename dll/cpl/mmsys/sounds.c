8b6e5e87c3a94192f09156cf7d82936e41f7e31c
Jared Smudde <computerwhiz02@hotmail.com>
2021-02-14 14:40:30 -0600

    [MMSYS] Add the OFN_EXPLORER flag to the Browse for sound dialog. (#3460)

--- dll/cpl/mmsys/sounds.c
+++ dll/cpl/mmsys/sounds.c
@@ -1151,7 +1151,7 @@ SoundsDlgProc(HWND hwndDlg,
                     LoadStringW(hApplet, IDS_BROWSE_FOR_SOUND, szTitle, _countof(szTitle));
                     ofn.lpstrTitle = szTitle;
                     ofn.lpstrInitialDir = L"%SystemRoot%\\Media";
-                    ofn.Flags = OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+                    ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
 
                     if (GetOpenFileNameW(&ofn) != FALSE)
                     {
