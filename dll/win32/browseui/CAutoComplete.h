6bfb76b68a7f8fe12b4fa4503b6225297108dd0c
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-02-23 06:07:09 +0900

    [BROWSEUI] Refactoring CAutoComplete Part 1 (#3472)
    - Expand tabs to spaces in CAutoComplete.h.
    - Add m_ prefix to every member variable.
    CORE-9281

--- dll/win32/browseui/CAutoComplete.h
+++ dll/win32/browseui/CAutoComplete.h
@@ -1,8 +1,9 @@
 /*
- *	AutoComplete interfaces implementation.
+ *  AutoComplete interfaces implementation.
  *
- *	Copyright 2004	Maxime Bellengé <maxime.bellenge@laposte.net>
- *	Copyright 2009  Andrew Hill
+ *  Copyright 2004  Maxime Bellengé <maxime.bellenge@laposte.net>
+ *  Copyright 2009  Andrew Hill
+ *  Copyright 2021  Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,48 +24,48 @@
 #define _AUTOCOMPLETE_H_
 
 class CAutoComplete :
-	public CComCoClass<CAutoComplete, &CLSID_AutoComplete>,
-	public CComObjectRootEx<CComMultiThreadModelNoCS>,
-	public IAutoComplete2,
-	public IAutoCompleteDropDown,
-	public IEnumString
+    public CComCoClass<CAutoComplete, &CLSID_AutoComplete>,
+    public CComObjectRootEx<CComMultiThreadModelNoCS>,
+    public IAutoComplete2,
+    public IAutoCompleteDropDown,
+    public IEnumString
 {
 private:
-	BOOL					enabled;
-	BOOL					initialized;
-	HWND					hwndEdit;
-	HWND					hwndListBox;
-	WNDPROC					wpOrigEditProc;
-	WNDPROC					wpOrigLBoxProc;
-	WCHAR					*txtbackup;
-	WCHAR					*quickComplete;
-	CComPtr<IEnumString>	enumstr;
-	AUTOCOMPLETEOPTIONS		options;
+    BOOL                    m_enabled;
+    BOOL                    m_initialized;
+    HWND                    m_hwndEdit;
+    HWND                    m_hwndListBox;
+    WNDPROC                 m_wpOrigEditProc;
+    WNDPROC                 m_wpOrigLBoxProc;
+    LPWSTR                  m_txtbackup;        // HeapAlloc'ed
+    LPWSTR                  m_quickComplete;    // HeapAlloc'ed
+    CComPtr<IEnumString>    m_enumstr;
+    AUTOCOMPLETEOPTIONS     m_options;
 public:
 
-	CAutoComplete();
-	~CAutoComplete();
+    CAutoComplete();
+    ~CAutoComplete();
 
-	static LRESULT APIENTRY ACEditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-	static LRESULT APIENTRY ACLBoxSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    static LRESULT APIENTRY ACEditSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    static LRESULT APIENTRY ACLBoxSubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-	void CreateListbox();
+    void CreateListbox();
 
-	// IAutoComplete2
-	virtual HRESULT WINAPI Enable(BOOL fEnable);
-	virtual HRESULT WINAPI Init(HWND hwndEdit, IUnknown *punkACL, LPCOLESTR pwzsRegKeyPath, LPCOLESTR pwszQuickComplete);
-	virtual HRESULT WINAPI GetOptions(DWORD *pdwFlag);
-	virtual HRESULT WINAPI SetOptions(DWORD dwFlag);
+    // IAutoComplete2
+    virtual HRESULT WINAPI Enable(BOOL fEnable);
+    virtual HRESULT WINAPI Init(HWND hwndEdit, IUnknown *punkACL, LPCOLESTR pwzsRegKeyPath, LPCOLESTR pwszQuickComplete);
+    virtual HRESULT WINAPI GetOptions(DWORD *pdwFlag);
+    virtual HRESULT WINAPI SetOptions(DWORD dwFlag);
 
-	// IAutoCompleteDropDown
-	virtual HRESULT STDMETHODCALLTYPE GetDropDownStatus(DWORD *pdwFlags, LPWSTR *ppwszString);
-	virtual HRESULT STDMETHODCALLTYPE ResetEnumerator();
+    // IAutoCompleteDropDown
+    virtual HRESULT STDMETHODCALLTYPE GetDropDownStatus(DWORD *pdwFlags, LPWSTR *ppwszString);
+    virtual HRESULT STDMETHODCALLTYPE ResetEnumerator();
 
-	// IEnumString methods
-	virtual HRESULT STDMETHODCALLTYPE Next(ULONG celt, LPOLESTR *rgelt, ULONG *pceltFetched);
-	virtual HRESULT STDMETHODCALLTYPE Skip(ULONG celt);
-	virtual HRESULT STDMETHODCALLTYPE Reset();
-	virtual HRESULT STDMETHODCALLTYPE Clone(IEnumString **ppenum);
+    // IEnumString methods
+    virtual HRESULT STDMETHODCALLTYPE Next(ULONG celt, LPOLESTR *rgelt, ULONG *pceltFetched);
+    virtual HRESULT STDMETHODCALLTYPE Skip(ULONG celt);
+    virtual HRESULT STDMETHODCALLTYPE Reset();
+    virtual HRESULT STDMETHODCALLTYPE Clone(IEnumString **ppenum);
 
 DECLARE_REGISTRY_RESOURCEID(IDR_AUTOCOMPLETE)
 DECLARE_NOT_AGGREGATABLE(CAutoComplete)
@@ -72,10 +73,10 @@ DECLARE_NOT_AGGREGATABLE(CAutoComplete)
 DECLARE_PROTECT_FINAL_CONSTRUCT()
 
 BEGIN_COM_MAP(CAutoComplete)
-	COM_INTERFACE_ENTRY_IID(IID_IAutoComplete, IAutoComplete)
-	COM_INTERFACE_ENTRY_IID(IID_IAutoComplete2, IAutoComplete2)
-	COM_INTERFACE_ENTRY_IID(IID_IAutoCompleteDropDown, IAutoCompleteDropDown)
-	COM_INTERFACE_ENTRY_IID(IID_IEnumString, IEnumString)
+    COM_INTERFACE_ENTRY_IID(IID_IAutoComplete, IAutoComplete)
+    COM_INTERFACE_ENTRY_IID(IID_IAutoComplete2, IAutoComplete2)
+    COM_INTERFACE_ENTRY_IID(IID_IAutoCompleteDropDown, IAutoCompleteDropDown)
+    COM_INTERFACE_ENTRY_IID(IID_IEnumString, IEnumString)
 END_COM_MAP()
 };
 
