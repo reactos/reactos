a93e065ebce692289cec49af0e686122be479fbf
Jérôme Gardou <jerome.gardou@reactos.org>
2021-02-23 11:07:35 +0100

    [NTOS:MM] Do not chunk-read file when we are the modified-page-writer thread

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -1200,8 +1200,10 @@ MmMakeSegmentResident(
     if (!NT_SUCCESS(Status))
         return Status;
 
-    /* If the file is not random access, read a 64K Chunk. */
-    if (!FlagOn(FileObject->Flags, FO_RANDOM_ACCESS))
+    /* If the file is not random access and we are not the page out thread
+     * read a 64K Chunk. */
+    if (((ULONG_PTR)IoGetTopLevelIrp() != FSRTL_MOD_WRITE_TOP_LEVEL_IRP)
+        && !FlagOn(FileObject->Flags, FO_RANDOM_ACCESS))
     {
         RangeStart = Offset - (Offset % _64K);
         if (RangeEnd % _64K)
