59928be88eda3bbc1929d0f94b71736cf6245d7a
Jérôme Gardou <jerome.gardou@reactos.org>
2021-02-23 10:48:41 +0100

    [NTOS:MM] Reset PFN usage as soon as we are locking the PFN DB when tracing PFNs

--- ntoskrnl/include/internal/mm.h
+++ ntoskrnl/include/internal/mm.h
@@ -959,7 +959,16 @@ FORCEINLINE
 KIRQL
 MiAcquirePfnLock(VOID)
 {
+#if MI_TRACE_PFNS
+    KIRQL ret = KeAcquireQueuedSpinLock(LockQueuePfnLock);
+
+    MI_SET_USAGE(MI_USAGE_NOT_SET);
+    MI_SET_PROCESS2("Not set");
+
+    return ret;
+#else
     return KeAcquireQueuedSpinLock(LockQueuePfnLock);
+#endif
 }
 
 FORCEINLINE
@@ -974,11 +983,12 @@ FORCEINLINE
 VOID
 MiAcquirePfnLockAtDpcLevel(VOID)
 {
-    PKSPIN_LOCK_QUEUE LockQueue;
-
     ASSERT(KeGetCurrentIrql() >= DISPATCH_LEVEL);
-    LockQueue = &KeGetCurrentPrcb()->LockQueue[LockQueuePfnLock];
-    KeAcquireQueuedSpinLockAtDpcLevel(LockQueue);
+    KeAcquireQueuedSpinLockAtDpcLevel(&KeGetCurrentPrcb()->LockQueue[LockQueuePfnLock]);
+#if MI_TRACE_PFNS
+    MI_SET_USAGE(MI_USAGE_NOT_SET);
+    MI_SET_PROCESS2("Not set");
+#endif
 }
 
 FORCEINLINE
