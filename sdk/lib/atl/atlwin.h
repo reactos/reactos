4f83032a1290fed0c5f11b7a21d1dea0d0570755
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-03-11 17:18:43 +0900

    [ATL] Split CWindowImplBaseT::WindowProc asserion
    CORE-9281

--- sdk/lib/atl/atlwin.h
+++ sdk/lib/atl/atlwin.h
@@ -1578,7 +1578,9 @@ public:
         BOOL handled;
         LONG_PTR saveWindowProc;
 
-        ATLASSERT(pThis != NULL && (pThis->m_dwState & WINSTATE_DESTROYED) == 0 && pThis->m_hWnd != NULL);
+        ATLASSERT(pThis != NULL);
+        ATLASSERT((pThis->m_dwState & WINSTATE_DESTROYED) == 0);
+        ATLASSERT(pThis->m_hWnd != NULL);
         if (pThis == NULL || (pThis->m_dwState & WINSTATE_DESTROYED) != 0 || pThis->m_hWnd == NULL)
             return 0;
 
