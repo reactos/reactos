79bc62f206ef78ef232ab6758f4e4bda30ca9473
Jérôme Gardou <jerome.gardou@reactos.org>
2021-04-09 16:52:53 +0200

    [COMPILER_APITEST] Disable SEH tests for GCC amd64 build

--- modules/rostests/apitests/compiler/testlist.c
+++ modules/rostests/apitests/compiler/testlist.c
@@ -7,7 +7,9 @@ extern void func_pseh_cpp(void);
 
 const struct test winetest_testlist[] =
 {
+#if !(defined(__GNUC__) && defined(_M_AMD64))
     { "ms-seh", func_ms_seh },
+#endif
     { "pseh", func_pseh },
     { "pseh_cpp", func_pseh_cpp },
     { 0, 0 }
