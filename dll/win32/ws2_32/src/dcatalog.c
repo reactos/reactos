af9df932748d3aaa2f334e0f430d35723fa46651
Hervé Poussineau <hpoussin@reactos.org>
2021-04-17 23:06:53 +0200

    [WS2_32] Create registry keys Protocol_Catalog9/NameSpace_Catalog5 if needed
    This fixes networking in LiveCD and installation in 2nd stage.
    CORE-17514

--- dll/win32/ws2_32/src/dcatalog.c
+++ dll/win32/ws2_32/src/dcatalog.c
@@ -59,34 +59,44 @@ WsTcOpen(IN PTCATALOG Catalog,
                                 &RegType,
                                 NULL,
                                 &RegSize);
-    if (ErrorCode != ERROR_SUCCESS)
+    if (ErrorCode == ERROR_FILE_NOT_FOUND)
     {
-        DPRINT1("Failed to get protocol catalog name: %d.\n", ErrorCode);
-        return FALSE;
+        static const CHAR DefaultCatalogName[] = "Protocol_Catalog9";
+        RegSize = sizeof(DefaultCatalogName);
+        CatalogKeyName = HeapAlloc(WsSockHeap, 0, RegSize);
+        memcpy(CatalogKeyName, DefaultCatalogName, RegSize);
     }
-
-    if (RegType != REG_SZ)
+    else
     {
-        DPRINT1("Protocol catalog name is not a string (Type %d).\n", RegType);
-        return FALSE;
+        if (ErrorCode != ERROR_SUCCESS)
+        {
+            DPRINT1("Failed to get protocol catalog name: %d.\n", ErrorCode);
+            return FALSE;
+        }
+
+        if (RegType != REG_SZ)
+        {
+            DPRINT1("Protocol catalog name is not a string (Type %d).\n", RegType);
+            return FALSE;
+        }
+
+        CatalogKeyName = HeapAlloc(WsSockHeap, 0, RegSize);
+
+        /* Read the catalog name */
+        ErrorCode = RegQueryValueEx(ParentKey,
+                                    "Current_Protocol_Catalog",
+                                    0,
+                                    &RegType,
+                                    (LPBYTE)CatalogKeyName,
+                                    &RegSize);
+
+        /* Open the Catalog Key */
+        ErrorCode = RegOpenKeyEx(ParentKey,
+                                 CatalogKeyName,
+                                 0,
+                                 MAXIMUM_ALLOWED,
+                                 &CatalogKey);
     }
-
-    CatalogKeyName = HeapAlloc(WsSockHeap, 0, RegSize);
-
-    /* Read the catalog name */
-    ErrorCode = RegQueryValueEx(ParentKey,
-                                "Current_Protocol_Catalog",
-                                0,
-                                &RegType,
-                                (LPBYTE)CatalogKeyName,
-                                &RegSize);
-
-    /* Open the Catalog Key */
-    ErrorCode = RegOpenKeyEx(ParentKey,
-                             CatalogKeyName,
-                             0,
-                             MAXIMUM_ALLOWED,
-                             &CatalogKey);
 
     /* If we didn't find the key, create it */
     if (ErrorCode == ERROR_SUCCESS)
