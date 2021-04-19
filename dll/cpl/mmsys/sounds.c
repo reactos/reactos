9d31a0d204f565045c80235386fe45efeaf592d2
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2021-04-19 19:52:31 +0300

    [MMSYS] Fix clang compilation warnings.
      This commit fixes following warnings:
        - ../dll/cpl/mmsys/sounds.c:938:32: warning: address of array 'pLabelContext->szValue' will always evaluate to 'true' [-Wpointer-bool-conversion]
                      if (pLabelContext->szValue && _tcslen(pLabelContext->szValue) > 0)
                          ~~~~~~~~~~~~~~~^~~~~~~ ~~

--- dll/cpl/mmsys/sounds.c
+++ dll/cpl/mmsys/sounds.c
@@ -935,7 +935,7 @@ ShowSoundScheme(PGLOBAL_DATA pGlobalData, HWND hwndDlg)
             tvItem.item.state = TVIS_EXPANDED;
             tvItem.item.stateMask = TVIS_EXPANDED;
             tvItem.item.pszText = pLabelMap->szDesc;
-            if (pLabelContext->szValue && _tcslen(pLabelContext->szValue) > 0)
+            if (_tcslen(pLabelContext->szValue) > 0)
             {
                 tvItem.item.iImage = IMAGE_SOUND_ASSIGNED;
                 tvItem.item.iSelectedImage = IMAGE_SOUND_ASSIGNED;
