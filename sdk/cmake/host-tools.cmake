37bc01f42b6d940c763b0d346f44971c7abe08d9
Jérôme Gardou <jerome.gardou@reactos.org>
2021-04-22 10:37:50 +0200

    [CMAKE] Introduce a GCC plugin for helping with amd64 SEH implementation
    \#pragma REACTOS SEH(except)
    \#pragma REACTOS SEH(finally)
    What it does is counting the number of SEH __try blocks and emit the proper assembly statements at function prologue
    It also checks for mixing C++ & SEH exception handling, which wouldn't work

--- sdk/cmake/host-tools.cmake
+++ sdk/cmake/host-tools.cmake
@@ -6,6 +6,13 @@ function(setup_host_tools)
     if(NOT MSVC)
         list(APPEND HOST_TOOLS rsym pefixup)
     endif()
+    if ((ARCH STREQUAL "amd64") AND (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
+        execute_process(
+            COMMAND ${CMAKE_C_COMPILER} --print-file-name=plugin
+            OUTPUT_VARIABLE GCC_PLUGIN_DIR)
+        string(STRIP ${GCC_PLUGIN_DIR} GCC_PLUGIN_DIR)
+        list(APPEND HOST_MODULES gcc_plugin_seh)
+    endif()
     list(TRANSFORM HOST_TOOLS PREPEND "${REACTOS_BINARY_DIR}/host-tools/bin/" OUTPUT_VARIABLE HOST_TOOLS_OUTPUT)
     if (CMAKE_HOST_WIN32)
         list(TRANSFORM HOST_TOOLS_OUTPUT APPEND ".exe")
@@ -13,13 +20,21 @@ function(setup_host_tools)
             set(HOST_EXTRA_DIR "$(ConfigurationName)/")
         endif()
         set(HOST_EXE_SUFFIX ".exe")
+        set(HOST_MODULE_SUFFIX ".dll")
+    else()
+        set(HOST_MODULE_SUFFIX ".so")
     endif()
 
     ExternalProject_Add(host-tools
         SOURCE_DIR ${REACTOS_SOURCE_DIR}
         PREFIX ${REACTOS_BINARY_DIR}/host-tools
         BINARY_DIR ${REACTOS_BINARY_DIR}/host-tools/bin
-        CMAKE_ARGS -UCMAKE_TOOLCHAIN_FILE -DARCH:STRING=${ARCH} -DCMAKE_INSTALL_PREFIX=${REACTOS_BINARY_DIR}/host-tools -DTOOLS_FOLDER=${REACTOS_BINARY_DIR}/host-tools/bin
+        CMAKE_ARGS
+            -UCMAKE_TOOLCHAIN_FILE
+            -DARCH:STRING=${ARCH}
+            -DCMAKE_INSTALL_PREFIX=${REACTOS_BINARY_DIR}/host-tools
+            -DTOOLS_FOLDER=${REACTOS_BINARY_DIR}/host-tools/bin
+            -DGCC_PLUGIN_DIR=${GCC_PLUGIN_DIR}
         BUILD_ALWAYS TRUE
         INSTALL_COMMAND ${CMAKE_COMMAND} -E true
         BUILD_BYPRODUCTS ${HOST_TOOLS_OUTPUT}
@@ -31,5 +46,11 @@ function(setup_host_tools)
         add_executable(native-${_tool} IMPORTED)
         set_target_properties(native-${_tool} PROPERTIES IMPORTED_LOCATION ${INSTALL_DIR}/bin/${HOST_EXTRA_DIR}${_tool}${HOST_EXE_SUFFIX})
         add_dependencies(native-${_tool} host-tools ${INSTALL_DIR}/bin/${HOST_EXTRA_DIR}${_tool}${HOST_EXE_SUFFIX})
+    endforeach()
+
+    foreach(_module ${HOST_MODULES})
+        add_library(native-${_module} MODULE IMPORTED)
+        set_target_properties(native-${_module} PROPERTIES IMPORTED_LOCATION ${INSTALL_DIR}/bin/${HOST_EXTRA_DIR}${_module}${HOST_MODULE_SUFFIX})
+        add_dependencies(native-${_module} host-tools ${INSTALL_DIR}/bin/${HOST_EXTRA_DIR}${_module}${HOST_MODULE_SUFFIX})
     endforeach()
 endfunction()
