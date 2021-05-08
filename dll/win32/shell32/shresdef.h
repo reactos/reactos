2636cff09fdb70bfe63c52ea9d2d24dbfc1e337f
Catalin Gabriel Draghita <69764719+jeffbox12@users.noreply.github.com>
2021-05-08 08:54:32 +0200

    [SHELL32] Show Product version on About dialog bmp (#3621)
    The About dialog banner will now show the correspondent Product version selected on install.
    This will make it easier to tell which edition of ReactOS is installed.

--- dll/win32/shell32/shresdef.h
+++ dll/win32/shell32/shresdef.h
@@ -28,6 +28,8 @@
 
 /* Bitmaps */
 #define IDB_REACTOS                 131
+#define IDB_REACTOS_WORKSTATION     400
+#define IDB_REACTOS_SERVER          401
 #define IDB_LINEBAR                 138
 #define IDB_SHELL_IEXPLORE_LG       204
 #define IDB_SHELL_IEXPLORE_LG_HOT   205
