44e89c1d3d2e583113474deada572a925e4c69d0
Jérôme Gardou <jerome.gardou@reactos.org>
2021-05-20 11:48:02 +0200

    [NTOS:MM] Fix another instance of reentrant spinlock acquisition

--- ntoskrnl/mm/freelist.c
+++ ntoskrnl/mm/freelist.c
@@ -537,20 +537,19 @@ ULONG
 NTAPI
 MmGetReferenceCountPage(PFN_NUMBER Pfn)
 {
-    KIRQL oldIrql;
     ULONG RCount;
     PMMPFN Pfn1;
 
+    MI_ASSERT_PFN_LOCK_HELD();
+
     DPRINT("MmGetReferenceCountPage(PhysicalAddress %x)\n", Pfn << PAGE_SHIFT);
 
-    oldIrql = MiAcquirePfnLock();
     Pfn1 = MiGetPfnEntry(Pfn);
     ASSERT(Pfn1);
     ASSERT_IS_ROS_PFN(Pfn1);
 
     RCount = Pfn1->u3.e2.ReferenceCount;
 
-    MiReleasePfnLock(oldIrql);
     return(RCount);
 }
 
