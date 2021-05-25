6b4f45eead5ea7185ab542363b1a05f9cd48e3e1
Vadim Galyant <vgal@rambler.ru>
2021-05-25 16:10:38 +0400

    [LIB:ARBITER] Implement ArbInitializeArbiterInstance().

--- ntoskrnl/io/pnpmgr/arbs.c
+++ ntoskrnl/io/pnpmgr/arbs.c
@@ -29,11 +29,62 @@ extern ARBITER_INSTANCE IopRootPortArbiter;
 
 NTSTATUS
 NTAPI
+IopBusNumberUnpackRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _Out_ PULONGLONG OutMinimumAddress,
+    _Out_ PULONGLONG OutMaximumAddress,
+    _Out_ PULONG OutLength,
+    _Out_ PULONG OutAlignment)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopBusNumberPackResource(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _In_ ULONGLONG Start,
+    _Out_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopBusNumberUnpackResource(
+    _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR Descriptor,
+    _Out_ PULONGLONG Start,
+    _Out_ PULONG Length)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+LONG
+NTAPI
+IopBusNumberScoreRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+#define ARB_MAX_BUS_NUMBER 0xFF
+
+NTSTATUS
+NTAPI
 IopBusNumberInitialize(VOID)
 {
     NTSTATUS Status;
 
     DPRINT("IopRootBusNumberArbiter %p\n", &IopRootBusNumberArbiter);
+
+    IopRootBusNumberArbiter.UnpackRequirement = IopBusNumberUnpackRequirement;
+    IopRootBusNumberArbiter.PackResource = IopBusNumberPackResource;
+    IopRootBusNumberArbiter.UnpackResource = IopBusNumberUnpackResource;
+    IopRootBusNumberArbiter.ScoreRequirement = IopBusNumberScoreRequirement;
 
     Status = ArbInitializeArbiterInstance(&IopRootBusNumberArbiter,
                                           NULL,
@@ -41,10 +92,79 @@ IopBusNumberInitialize(VOID)
                                           L"RootBusNumber",
                                           L"Root",
                                           NULL);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("IopBusNumberInitialize: Status %p\n", Status);
+        ASSERT(FALSE);
+        return Status;
+    }
+
+    Status = RtlAddRange(IopRootBusNumberArbiter.Allocation,
+                         (ULONGLONG)(ARB_MAX_BUS_NUMBER + 1),
+                         (ULONGLONG)(-1),
+                         0,
+                         0,
+                         NULL,
+                         NULL);
+
     return Status;
 }
 
 /* Irq arbiter */
+
+NTSTATUS
+NTAPI
+IopIrqUnpackRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _Out_ PULONGLONG OutMinimumVector,
+    _Out_ PULONGLONG OutMaximumVector,
+    _Out_ PULONG OutParam1,
+    _Out_ PULONG OutParam2)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopIrqPackResource(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _In_ ULONGLONG Start,
+    _Out_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopIrqUnpackResource(
+    _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor,
+    _Out_ PULONGLONG Start,
+    _Out_ PULONG OutLength)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+LONG
+NTAPI
+IopIrqScoreRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+NTSTATUS
+NTAPI
+IopIrqTranslateOrdering(
+    _Out_ PIO_RESOURCE_DESCRIPTOR OutIoDescriptor,
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
 
 NTSTATUS
 NTAPI
@@ -54,16 +174,73 @@ IopIrqInitialize(VOID)
 
     DPRINT("IopRootIrqArbiter %p\n", &IopRootIrqArbiter);
 
+    IopRootIrqArbiter.UnpackRequirement = IopIrqUnpackRequirement;
+    IopRootIrqArbiter.PackResource = IopIrqPackResource;
+    IopRootIrqArbiter.UnpackResource = IopIrqUnpackResource;
+    IopRootIrqArbiter.ScoreRequirement = IopIrqScoreRequirement;
+
     Status = ArbInitializeArbiterInstance(&IopRootIrqArbiter,
                                           NULL,
                                           CmResourceTypeInterrupt,
                                           L"RootIRQ",
                                           L"Root",
-                                          NULL);
+                                          IopIrqTranslateOrdering);
     return Status;
 }
 
 /* Dma arbiter */
+
+NTSTATUS
+NTAPI
+IopDmaUnpackRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _Out_ PULONGLONG OutMinimumChannel,
+    _Out_ PULONGLONG OutMaximumChannel,
+    _Out_ PULONG OutParam1,
+    _Out_ PULONG OutParam2)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopDmaPackResource(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _In_ ULONGLONG Start,
+    _Out_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopDmaUnpackResource(
+    _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor,
+    _Out_ PULONGLONG Start,
+    _Out_ PULONG OutLength)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+LONG
+NTAPI
+IopDmaScoreRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+NTSTATUS
+NTAPI
+IopDmaOverrideConflict()
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
 
 NTSTATUS
 NTAPI
@@ -72,6 +249,13 @@ IopDmaInitialize(VOID)
     NTSTATUS Status;
 
     DPRINT("IopRootDmaArbiter %p\n", &IopRootDmaArbiter);
+
+    IopRootDmaArbiter.UnpackRequirement = IopDmaUnpackRequirement;
+    IopRootDmaArbiter.PackResource = IopDmaPackResource;
+    IopRootDmaArbiter.UnpackResource = IopDmaUnpackResource;
+    IopRootDmaArbiter.ScoreRequirement = IopDmaScoreRequirement;
+
+    IopRootDmaArbiter.OverrideConflict = IopDmaOverrideConflict;
 
     Status = ArbInitializeArbiterInstance(&IopRootDmaArbiter,
                                           NULL,
@@ -82,7 +266,73 @@ IopDmaInitialize(VOID)
     return Status;
 }
 
+/* Common for Memory and Port arbiters */
+
+NTSTATUS
+NTAPI
+IopGenericUnpackRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _Out_ PULONGLONG OutMinimumAddress,
+    _Out_ PULONGLONG OutMaximumAddress,
+    _Out_ PULONG OutLength,
+    _Out_ PULONG OutAlignment)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopGenericPackResource(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
+    _In_ ULONGLONG Start,
+    _Out_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+IopGenericUnpackResource(
+    _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor,
+    _Out_ PULONGLONG Start,
+    _Out_ PULONG OutLength)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+LONG
+NTAPI
+IopGenericScoreRequirement(
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return 0;
+}
+
+NTSTATUS
+NTAPI
+IopGenericTranslateOrdering(
+    _Out_ PIO_RESOURCE_DESCRIPTOR OutIoDescriptor,
+    _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 /* Memory arbiter */
+
+BOOLEAN
+NTAPI
+IopMemFindSuitableRange(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PARBITER_ALLOCATION_STATE State)
+{
+    UNIMPLEMENTED;
+    return FALSE;
+}
 
 NTSTATUS
 NTAPI
@@ -92,16 +342,66 @@ IopMemInitialize(VOID)
 
     DPRINT("IopRootMemArbiter %p\n", &IopRootMemArbiter);
 
+    IopRootMemArbiter.UnpackRequirement = IopGenericUnpackRequirement;
+    IopRootMemArbiter.PackResource = IopGenericPackResource;
+    IopRootMemArbiter.UnpackResource = IopGenericUnpackResource;
+    IopRootMemArbiter.ScoreRequirement = IopGenericScoreRequirement;
+
+    IopRootMemArbiter.FindSuitableRange = IopMemFindSuitableRange;
+
     Status = ArbInitializeArbiterInstance(&IopRootMemArbiter,
                                           NULL,
                                           CmResourceTypeMemory,
                                           L"RootMemory",
                                           L"Root",
-                                          NULL);
+                                          IopGenericTranslateOrdering);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("IopMemInitialize: Status %p\n", Status);
+        ASSERT(FALSE);
+        return Status;
+    }
+
+    Status = RtlAddRange(IopRootMemArbiter.Allocation,
+                         0ull,
+                         (ULONGLONG)(PAGE_SIZE - 1),
+                         0,
+                         0,
+                         NULL,
+                         NULL);
+
     return Status;
 }
 
 /* Port arbiter */
+
+BOOLEAN
+NTAPI
+IopPortFindSuitableRange(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PARBITER_ALLOCATION_STATE State)
+{
+    UNIMPLEMENTED;
+    return FALSE;
+}
+
+VOID
+NTAPI
+IopPortAddAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+}
+
+VOID
+NTAPI
+IopPortBacktrackAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+}
 
 NTSTATUS
 NTAPI
@@ -111,12 +411,21 @@ IopPortInitialize(VOID)
 
     DPRINT("IopRootPortArbiter %p\n", &IopRootPortArbiter);
 
+    IopRootPortArbiter.UnpackRequirement = IopGenericUnpackRequirement;
+    IopRootPortArbiter.PackResource = IopGenericPackResource;
+    IopRootPortArbiter.UnpackResource = IopGenericUnpackResource;
+    IopRootPortArbiter.ScoreRequirement = IopGenericScoreRequirement;
+
+    IopRootPortArbiter.FindSuitableRange = IopPortFindSuitableRange;
+    IopRootPortArbiter.AddAllocation = IopPortAddAllocation;
+    IopRootPortArbiter.BacktrackAllocation = IopPortBacktrackAllocation;
+
     Status = ArbInitializeArbiterInstance(&IopRootPortArbiter,
                                           NULL,
                                           CmResourceTypePort,
                                           L"RootPort",
                                           L"Root",
-                                          NULL);
+                                          IopGenericTranslateOrdering);
     return Status;
 }
 
--- sdk/lib/drivers/arbiter/arbiter.c
+++ sdk/lib/drivers/arbiter/arbiter.c
@@ -10,7 +10,7 @@
 
 #include <ntifs.h>
 #include <ndk/rtltypes.h>
-
+#include <ndk/rtlfuncs.h>
 #include "arbiter.h"
 
 #define NDEBUG
@@ -24,6 +24,202 @@
 
 NTSTATUS
 NTAPI
+ArbTestAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PLIST_ENTRY ArbitrationList)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbRetestAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PLIST_ENTRY ArbitrationList)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbCommitAllocation(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbRollbackAllocation(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+/*  Not correct yet, FIXME! */
+NTSTATUS
+NTAPI
+ArbAddReserved(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbPreprocessEntry(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
+ArbAllocateEntry(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+BOOLEAN
+NTAPI
+ArbGetNextAllocationRange(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+    return FALSE;
+}
+
+BOOLEAN
+NTAPI
+ArbFindSuitableRange(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+    return FALSE;
+}
+
+VOID
+NTAPI
+ArbAddAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+}
+
+VOID
+NTAPI
+ArbBacktrackAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _Inout_ PARBITER_ALLOCATION_STATE ArbState)
+{
+    UNIMPLEMENTED;
+}
+
+/*  Not correct yet, FIXME! */
+NTSTATUS
+NTAPI
+ArbOverrideConflict(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbBootAllocation(
+    _In_ PARBITER_INSTANCE Arbiter,
+    _In_ PLIST_ENTRY ArbitrationList)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+/*  Not correct yet, FIXME! */
+NTSTATUS
+NTAPI
+ArbQueryConflict(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+/*  Not correct yet, FIXME! */
+NTSTATUS
+NTAPI
+ArbStartArbiter(
+    _In_ PARBITER_INSTANCE Arbiter)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbAddOrdering(
+    _Out_ PARBITER_ORDERING_LIST OrderList,
+    _In_ ULONGLONG MinimumAddress,
+    _In_ ULONGLONG MaximumAddress)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbPruneOrdering(
+    _Out_ PARBITER_ORDERING_LIST OrderingList,
+    _In_ ULONGLONG MinimumAddress,
+    _In_ ULONGLONG MaximumAddress)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbInitializeOrderingList(
+    _Out_ PARBITER_ORDERING_LIST OrderList)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+VOID
+NTAPI
+ArbFreeOrderingList(
+    _Out_ PARBITER_ORDERING_LIST OrderList)
+{
+    UNIMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ArbBuildAssignmentOrdering(
+    _Inout_ PARBITER_INSTANCE ArbInstance,
+    _In_ PCWSTR OrderName,
+    _In_ PCWSTR ReservedOrderName,
+    _In_ PARB_TRANSLATE_ORDERING TranslateOrderingFunction)
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS
+NTAPI
 ArbInitializeArbiterInstance(
     _Inout_ PARBITER_INSTANCE Arbiter,
     _In_ PDEVICE_OBJECT BusDeviceObject,
@@ -34,10 +230,135 @@ ArbInitializeArbiterInstance(
 {
     NTSTATUS Status;
 
-    DPRINT("ArbInitializeArbiterInstance: Initializing %S Arbiter\n", ArbiterName);
-    UNIMPLEMENTED;
+    DPRINT("ArbInitializeArbiterInstance: '%S'\n", ArbiterName);
 
-    Status = STATUS_SUCCESS;
+    ASSERT(Arbiter->UnpackRequirement != NULL);
+    ASSERT(Arbiter->PackResource != NULL);
+    ASSERT(Arbiter->UnpackResource != NULL);
+
+    ASSERT(Arbiter->MutexEvent == NULL &&
+           Arbiter->Allocation == NULL &&
+           Arbiter->PossibleAllocation == NULL &&
+           Arbiter->AllocationStack == NULL);
+
+    Arbiter->Signature = 'sbrA';
+    Arbiter->BusDeviceObject = BusDeviceObject;
+
+    Arbiter->MutexEvent = ExAllocatePoolWithTag(NonPagedPool, sizeof(KEVENT), 'MbrA');
+    if (!Arbiter->MutexEvent)
+    {
+        DPRINT1("ArbInitializeArbiterInstance: STATUS_INSUFFICIENT_RESOURCES\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    KeInitializeEvent(Arbiter->MutexEvent, SynchronizationEvent, TRUE);
+
+    Arbiter->AllocationStack = ExAllocatePoolWithTag(PagedPool, PAGE_SIZE, 'AbrA');
+    if (!Arbiter->AllocationStack)
+    {
+        DPRINT1("ArbInitializeArbiterInstance: STATUS_INSUFFICIENT_RESOURCES\n");
+        ExFreePoolWithTag(Arbiter->MutexEvent, 'MbrA');
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    Arbiter->AllocationStackMaxSize = PAGE_SIZE;
+
+    Arbiter->Allocation = ExAllocatePoolWithTag(PagedPool, sizeof(RTL_RANGE_LIST), 'RbrA');
+    if (!Arbiter->Allocation)
+    {
+        DPRINT1("ArbInitializeArbiterInstance: STATUS_INSUFFICIENT_RESOURCES\n");
+        ExFreePoolWithTag(Arbiter->AllocationStack, 'AbrA');
+        ExFreePoolWithTag(Arbiter->MutexEvent, 'MbrA');
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    Arbiter->PossibleAllocation = ExAllocatePoolWithTag(PagedPool, sizeof(RTL_RANGE_LIST), 'RbrA');
+    if (!Arbiter->PossibleAllocation)
+    {
+        DPRINT1("ArbInitializeArbiterInstance: STATUS_INSUFFICIENT_RESOURCES\n");
+        ExFreePoolWithTag(Arbiter->Allocation, 'RbrA');
+        ExFreePoolWithTag(Arbiter->AllocationStack, 'AbrA');
+        ExFreePoolWithTag(Arbiter->MutexEvent, 'MbrA');
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    RtlInitializeRangeList(Arbiter->Allocation);
+    RtlInitializeRangeList(Arbiter->PossibleAllocation);
+
+    Arbiter->Name = ArbiterName;
+    Arbiter->ResourceType = ResourceType;
+    Arbiter->TransactionInProgress = FALSE;
+
+    if (!Arbiter->TestAllocation)
+        Arbiter->TestAllocation = ArbTestAllocation;
+
+    if (!Arbiter->RetestAllocation)
+        Arbiter->RetestAllocation = ArbRetestAllocation;
+
+    if (!Arbiter->CommitAllocation)
+        Arbiter->CommitAllocation = ArbCommitAllocation;
+
+    if (!Arbiter->RollbackAllocation)
+        Arbiter->RollbackAllocation = ArbRollbackAllocation;
+
+    if (!Arbiter->AddReserved)
+        Arbiter->AddReserved = ArbAddReserved;
+
+    if (!Arbiter->PreprocessEntry)
+        Arbiter->PreprocessEntry = ArbPreprocessEntry;
+
+    if (!Arbiter->AllocateEntry)
+        Arbiter->AllocateEntry = ArbAllocateEntry;
+
+    if (!Arbiter->GetNextAllocationRange)
+        Arbiter->GetNextAllocationRange = ArbGetNextAllocationRange;
+
+    if (!Arbiter->FindSuitableRange)
+        Arbiter->FindSuitableRange = ArbFindSuitableRange;
+
+    if (!Arbiter->AddAllocation)
+        Arbiter->AddAllocation = ArbAddAllocation;
+
+    if (!Arbiter->BacktrackAllocation)
+        Arbiter->BacktrackAllocation = ArbBacktrackAllocation;
+
+    if (!Arbiter->OverrideConflict)
+        Arbiter->OverrideConflict = ArbOverrideConflict;
+
+    if (!Arbiter->BootAllocation)
+        Arbiter->BootAllocation = ArbBootAllocation;
+
+    if (!Arbiter->QueryConflict)
+        Arbiter->QueryConflict = ArbQueryConflict;
+
+    if (!Arbiter->StartArbiter)
+        Arbiter->StartArbiter = ArbStartArbiter;
+
+    Status = ArbBuildAssignmentOrdering(Arbiter, OrderName, OrderName, TranslateOrderingFunction);
+    if (NT_SUCCESS(Status))
+    {
+        return STATUS_SUCCESS;
+    }
+
+    DPRINT1("ArbInitializeArbiterInstance: Status %X\n", Status);
+
+    if (Arbiter->MutexEvent)
+    {
+        ExFreePoolWithTag(Arbiter->MutexEvent, 'MbrA');
+    }
+    if (Arbiter->Allocation)
+    {
+        ExFreePoolWithTag(Arbiter->Allocation, 'RbrA');
+    }
+    if (Arbiter->PossibleAllocation)
+    {
+        ExFreePoolWithTag(Arbiter->PossibleAllocation, 'RbrA');
+    }
+    if (Arbiter->AllocationStack)
+    {
+        ExFreePoolWithTag(Arbiter->AllocationStack, 'AbrA');
+    }
+
     return Status;
 }
 
