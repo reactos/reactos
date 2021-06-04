e266d6b0b813e6092ac763c88c01a6757696af05
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-06-04 16:00:35 +0900

    rename variables

--- modules/rostests/apitests/shell32/FindExecutable.cpp
+++ modules/rostests/apitests/shell32/FindExecutable.cpp
@@ -9,21 +9,21 @@
 #include <stdio.h>
 #include <shlwapi.h>
 
-static char s_szSubProgram[MAX_PATH];
+static char s_sub_program[MAX_PATH];
 
 static BOOL
 GetSubProgramPath(void)
 {
-    GetModuleFileNameA(NULL, s_szSubProgram, _countof(s_szSubProgram));
-    PathRemoveFileSpecA(s_szSubProgram);
-    PathAppendA(s_szSubProgram, "shell32_apitest_sub.exe");
+    GetModuleFileNameA(NULL, s_sub_program, _countof(s_sub_program));
+    PathRemoveFileSpecA(s_sub_program);
+    PathAppendA(s_sub_program, "shell32_apitest_sub.exe");
 
-    if (!PathFileExistsA(s_szSubProgram))
+    if (!PathFileExistsA(s_sub_program))
     {
-        PathRemoveFileSpecA(s_szSubProgram);
-        PathAppendA(s_szSubProgram, "testdata\\shell32_apitest_sub.exe");
+        PathRemoveFileSpecA(s_sub_program);
+        PathAppendA(s_sub_program, "testdata\\shell32_apitest_sub.exe");
 
-        if (!PathFileExistsA(s_szSubProgram))
+        if (!PathFileExistsA(s_sub_program))
         {
             return FALSE;
         }
@@ -41,32 +41,32 @@ typedef struct TEST_ENTRY
     LPCSTR result;
 } TEST_ENTRY;
 
-static char s_szWinDir[MAX_PATH];
-static char s_szWinNotepad[MAX_PATH];
-static char s_szSysNotepad[MAX_PATH];
-static char s_szWinTestExe[MAX_PATH];
-static char s_szSysTestExe[MAX_PATH];
-static char s_szWinBatFile[MAX_PATH];
-static char s_szSysBatFile[MAX_PATH];
+static char s_win_dir[MAX_PATH];
+static char s_win_notepad[MAX_PATH];
+static char s_sys_notepad[MAX_PATH];
+static char s_win_test_exe[MAX_PATH];
+static char s_sys_test_exe[MAX_PATH];
+static char s_win_bat_file[MAX_PATH];
+static char s_sys_bat_file[MAX_PATH];
 
 #define DIR_0 NULL
 #define DIR_1 "."
 #define DIR_2 ".."
-#define DIR_3 s_szWinDir
+#define DIR_3 s_win_dir
 #define DIR_4 "(invalid)"
 
 static const TEST_ENTRY s_entries[] =
 {
-    { __LINE__, TRUE, "notepad", DIR_0, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad", DIR_1, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad", DIR_2, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad", DIR_3, s_szWinNotepad },
-    { __LINE__, TRUE, "notepad", DIR_4, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad.exe", DIR_0, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad.exe", DIR_1, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad.exe", DIR_2, s_szSysNotepad },
-    { __LINE__, TRUE, "notepad.exe", DIR_3, s_szWinNotepad },
-    { __LINE__, TRUE, "notepad.exe", DIR_4, s_szSysNotepad },
+    { __LINE__, TRUE, "notepad", DIR_0, s_sys_notepad },
+    { __LINE__, TRUE, "notepad", DIR_1, s_sys_notepad },
+    { __LINE__, TRUE, "notepad", DIR_2, s_sys_notepad },
+    { __LINE__, TRUE, "notepad", DIR_3, s_win_notepad },
+    { __LINE__, TRUE, "notepad", DIR_4, s_sys_notepad },
+    { __LINE__, TRUE, "notepad.exe", DIR_0, s_sys_notepad },
+    { __LINE__, TRUE, "notepad.exe", DIR_1, s_sys_notepad },
+    { __LINE__, TRUE, "notepad.exe", DIR_2, s_sys_notepad },
+    { __LINE__, TRUE, "notepad.exe", DIR_3, s_win_notepad },
+    { __LINE__, TRUE, "notepad.exe", DIR_4, s_sys_notepad },
     { __LINE__, FALSE, "notepad.bat", DIR_0, "" },
     { __LINE__, FALSE, "notepad.bat", DIR_1, "" },
     { __LINE__, FALSE, "notepad.bat", DIR_2, "" },
@@ -82,22 +82,22 @@ static const TEST_ENTRY s_entries[] =
     { __LINE__, FALSE, "..\\notepad.exe", DIR_2, "" },
     { __LINE__, FALSE, "..\\notepad.exe", DIR_3, "" },
     { __LINE__, FALSE, "..\\notepad.exe", DIR_4, "" },
-    { __LINE__, TRUE, "test program", DIR_0, s_szSysTestExe },
-    { __LINE__, TRUE, "test program", DIR_1, s_szSysTestExe },
-    { __LINE__, TRUE, "test program", DIR_2, s_szSysTestExe },
-    { __LINE__, TRUE, "test program", DIR_3, s_szWinTestExe },
-    { __LINE__, TRUE, "test program", DIR_4, s_szSysTestExe },
-    { __LINE__, TRUE, "test program.exe", DIR_0, s_szSysTestExe },
-    { __LINE__, TRUE, "test program.exe", DIR_1, s_szSysTestExe },
-    { __LINE__, TRUE, "test program.exe", DIR_2, s_szSysTestExe },
-    { __LINE__, TRUE, "test program.exe", DIR_3, s_szWinTestExe },
-    { __LINE__, TRUE, "test program.exe", DIR_4, s_szSysTestExe },
-    { __LINE__, TRUE, "test program.bat", DIR_0, s_szSysBatFile },
-    { __LINE__, TRUE, "test program.bat", DIR_1, s_szSysBatFile },
-    { __LINE__, TRUE, "test program.bat", DIR_2, s_szSysBatFile },
-    { __LINE__, TRUE, "test program.bat", DIR_3, s_szWinBatFile },
-    { __LINE__, TRUE, "test program.bat", DIR_4, s_szSysBatFile },
-    { __LINE__, TRUE, "shell32_apitest_sub.exe", DIR_0, s_szSubProgram },
+    { __LINE__, TRUE, "test program", DIR_0, s_sys_test_exe },
+    { __LINE__, TRUE, "test program", DIR_1, s_sys_test_exe },
+    { __LINE__, TRUE, "test program", DIR_2, s_sys_test_exe },
+    { __LINE__, TRUE, "test program", DIR_3, s_win_test_exe },
+    { __LINE__, TRUE, "test program", DIR_4, s_sys_test_exe },
+    { __LINE__, TRUE, "test program.exe", DIR_0, s_sys_test_exe },
+    { __LINE__, TRUE, "test program.exe", DIR_1, s_sys_test_exe },
+    { __LINE__, TRUE, "test program.exe", DIR_2, s_sys_test_exe },
+    { __LINE__, TRUE, "test program.exe", DIR_3, s_win_test_exe },
+    { __LINE__, TRUE, "test program.exe", DIR_4, s_sys_test_exe },
+    { __LINE__, TRUE, "test program.bat", DIR_0, s_sys_bat_file },
+    { __LINE__, TRUE, "test program.bat", DIR_1, s_sys_bat_file },
+    { __LINE__, TRUE, "test program.bat", DIR_2, s_sys_bat_file },
+    { __LINE__, TRUE, "test program.bat", DIR_3, s_win_bat_file },
+    { __LINE__, TRUE, "test program.bat", DIR_4, s_sys_bat_file },
+    { __LINE__, TRUE, "shell32_apitest_sub.exe", DIR_0, s_sub_program },
     { __LINE__, TRUE, "shell32_apitest_sub.exe", DIR_1, "shell32_apitest_sub.exe" },
     { __LINE__, FALSE, "shell32_apitest_sub.exe", DIR_2, "" },
     { __LINE__, FALSE, "shell32_apitest_sub.exe", DIR_3, "" },
@@ -125,44 +125,44 @@ START_TEST(FindExecutable)
         skip("shell32_apitest_sub.exe not found\n");
     }
 
-    GetWindowsDirectoryA(s_szWinDir, _countof(s_szWinDir));
+    GetWindowsDirectoryA(s_win_dir, _countof(s_win_dir));
 
-    GetWindowsDirectoryA(s_szWinNotepad, _countof(s_szWinNotepad));
-    PathAppendA(s_szWinNotepad, "notepad.exe");
+    GetWindowsDirectoryA(s_win_notepad, _countof(s_win_notepad));
+    PathAppendA(s_win_notepad, "notepad.exe");
 
-    GetSystemDirectoryA(s_szSysNotepad, _countof(s_szSysNotepad));
-    PathAppendA(s_szSysNotepad, "notepad.exe");
+    GetSystemDirectoryA(s_sys_notepad, _countof(s_sys_notepad));
+    PathAppendA(s_sys_notepad, "notepad.exe");
 
-    GetWindowsDirectoryA(s_szWinTestExe, _countof(s_szWinTestExe));
-    PathAppendA(s_szWinTestExe, "test program.exe");
-    BOOL ret = CopyFileA(s_szSubProgram, s_szWinTestExe, FALSE);
+    GetWindowsDirectoryA(s_win_test_exe, _countof(s_win_test_exe));
+    PathAppendA(s_win_test_exe, "test program.exe");
+    BOOL ret = CopyFileA(s_sub_program, s_win_test_exe, FALSE);
     if (!ret)
     {
         skip("Please retry with admin rights\n");
         return;
     }
 
-    GetSystemDirectoryA(s_szSysTestExe, _countof(s_szSysTestExe));
-    PathAppendA(s_szSysTestExe, "test program.exe");
-    ok_int(CopyFileA(s_szSubProgram, s_szSysTestExe, FALSE), TRUE);
+    GetSystemDirectoryA(s_sys_test_exe, _countof(s_sys_test_exe));
+    PathAppendA(s_sys_test_exe, "test program.exe");
+    ok_int(CopyFileA(s_sub_program, s_sys_test_exe, FALSE), TRUE);
 
-    GetWindowsDirectoryA(s_szWinBatFile, _countof(s_szWinBatFile));
-    PathAppendA(s_szWinBatFile, "test program.bat");
-    fclose(fopen(s_szWinBatFile, "wb"));
-    ok_int(PathFileExistsA(s_szWinBatFile), TRUE);
+    GetWindowsDirectoryA(s_win_bat_file, _countof(s_win_bat_file));
+    PathAppendA(s_win_bat_file, "test program.bat");
+    fclose(fopen(s_win_bat_file, "wb"));
+    ok_int(PathFileExistsA(s_win_bat_file), TRUE);
 
-    GetSystemDirectoryA(s_szSysBatFile, _countof(s_szSysBatFile));
-    PathAppendA(s_szSysBatFile, "test program.bat");
-    fclose(fopen(s_szSysBatFile, "wb"));
-    ok_int(PathFileExistsA(s_szSysBatFile), TRUE);
+    GetSystemDirectoryA(s_sys_bat_file, _countof(s_sys_bat_file));
+    PathAppendA(s_sys_bat_file, "test program.bat");
+    fclose(fopen(s_sys_bat_file, "wb"));
+    ok_int(PathFileExistsA(s_sys_bat_file), TRUE);
 
     for (UINT iTest = 0; iTest < _countof(s_entries); ++iTest)
     {
         DoTestEntry(&s_entries[iTest]);
     }
 
-    DeleteFileA(s_szWinTestExe);
-    DeleteFileA(s_szSysTestExe);
-    DeleteFileA(s_szWinBatFile);
-    DeleteFileA(s_szSysBatFile);
+    DeleteFileA(s_win_test_exe);
+    DeleteFileA(s_sys_test_exe);
+    DeleteFileA(s_win_bat_file);
+    DeleteFileA(s_sys_bat_file);
 }
