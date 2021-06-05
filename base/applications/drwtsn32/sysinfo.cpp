1f44552d36f41397991d4594edada88cd062a107
Mark Jansen <mark.jansen@reactos.org>
2021-06-05 22:13:22 +0200

    [DRWTSN32] Implement arm context reading
    CORE-17605 CORE-17604

--- base/applications/drwtsn32/sysinfo.cpp
+++ base/applications/drwtsn32/sysinfo.cpp
@@ -2,7 +2,7 @@
  * PROJECT:     Dr. Watson crash reporter
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * PURPOSE:     Output system info
- * COPYRIGHT:   Copyright 2017 Mark Jansen (mark.jansen@reactos.org)
+ * COPYRIGHT:   Copyright 2017 Mark Jansen <mark.jansen@reactos.org>
  */
 
 #include "precomp.h"
