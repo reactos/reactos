ada2f7dd17ead6965949ce0b37eee5ed95acad08
Joachim Henze <Joachim.Henze@reactos.org>
2021-06-20 15:39:24 +0200

    [0.4.12][LSASRV] LsapLogonUser(): Fix 'AuthenticatingAuthority' copypasta (#2839)
    Reported by contributor 'qarmin', found by static code analysis.
    CORE-17074
    
    The glitch once was introduced 7 years ago by SVN r58933 == git 7934e35cdf55b07334f04d86d9c7717b0cb41c0b
    There is no real-world-issue known to date caused by this bug.
    
    fix cherry picked from commit 0.4.15-dev-303-g 99d0a348aa4417672ad07720404765b4b2dbe9f5

--- dll/win32/lsasrv/authpackage.c
+++ dll/win32/lsasrv/authpackage.c
@@ -1779,7 +1779,7 @@ done:
     /* Free the authentication authority */
     if (AuthenticatingAuthority != NULL)
     {
-        if (AuthenticatingAuthority != NULL)
+        if (AuthenticatingAuthority->Buffer != NULL)
             LsapFreeHeap(AuthenticatingAuthority->Buffer);
 
         LsapFreeHeap(AuthenticatingAuthority);
