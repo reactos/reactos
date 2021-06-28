1c7f3476a0c7febaa573e2e35ecbbc5efb354e01
Victor Perevertkin <victor.perevertkin@reactos.org>
2021-06-28 17:54:09 +0300

    [CMAKE] Use BUILD_MP CMake option correctly

--- sdk/cmake/config.cmake
+++ sdk/cmake/config.cmake
@@ -97,8 +97,7 @@ else()
     set(_WINKD_ FALSE CACHE BOOL "Whether to compile with the KD protocol.")
 endif()
 
-cmake_dependent_option(BUILD_MP "Whether to build the multiprocessor versions of NTOSKRNL and HAL." ON
-                       "ARCH STREQUAL i386" OFF)
+option(BUILD_MP "Whether to build the multiprocessor versions of NTOSKRNL and HAL." ON)
 
 cmake_dependent_option(ISAPNP_ENABLE "Whether to enable the ISA PnP support." ON
                        "ARCH STREQUAL i386 AND NOT SARCH STREQUAL xbox" OFF)
