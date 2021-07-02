39296a09b1719c625477bdc4d0934f7733a1d756
Justin Miller <Justinmiller100@gmail.com>
2021-07-02 08:50:40 -0700

    [HALX86] Implemented x86 AP bootstub

--- hal/halx86/smp.cmake
+++ hal/halx86/smp.cmake
@@ -6,6 +6,19 @@ list(APPEND HAL_SMP_SOURCE
     smp/mpmemory.c
     smp/ipi.c)
 
-add_library(lib_hal_smp OBJECT ${HAL_SMP_SOURCE})
+if(ARCH STREQUAL "i386")
+    list(APPEND HAL_SMP_ASM_SOURCE
+        smp/i386/apentry.S
+        smp/i386/apspinup.S)
+endif()
+
+if(ARCH STREQUAL "amd64")
+    list(APPEND HAL_SMP_ASM_SOURCE
+        smp/amd64/apentry.S
+        smp/amd64/apspinup.S)
+endif()
+
+add_asm_files(lib_hal_smp_asm ${HAL_SMP_ASM_SOURCE})
+add_library(lib_hal_smp OBJECT ${HAL_SMP_SOURCE} ${lib_hal_smp_asm})
 add_dependencies(lib_hal_smp bugcodes xdk)
 target_compile_definitions(lib_hal_smp PRIVATE CONFIG_SMP)
