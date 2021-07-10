4fcd28b19be4406441e4065869e401ff421f0c98
Justin Miller <justinmiller100@gmail.com>
2021-07-10 16:39:44 -0700

    [HALX86] Organize the way we copy over a bit

--- hal/halx86/smp/mpmemory.c
+++ hal/halx86/smp/mpmemory.c
@@ -20,6 +20,7 @@ extern PVOID APEntry;
 extern PVOID APEntryEnd;
 extern PVOID APSpinup;
 extern PVOID APSpinupEnd;
+extern UINT16 APJumpOffset;
 
 /* FUNCTIONS *****************************************************************/
 
@@ -27,9 +28,15 @@ VOID
 HalpInitializeAPStub(PVOID APStubLocation)
 {
     PVOID APStubSecondPhaseLoc;
+    PVOID APJumppLoc;
+
+    /* Get the locations used to copy over */
+    APJumppLoc = (PUSHORT)((ULONG_PTR)APStubLocation + (ULONG_PTR)&APJumpOffset - (ULONG_PTR)&APEntry); 
+    APStubSecondPhaseLoc = (PVOID)((ULONG_PTR)APStubLocation + ((ULONG_PTR)&APEntryEnd  - (ULONG_PTR)&APEntry));
+    APJumpOffset = (UINT16)(((ULONG_PTR)APStubLocation * 4) + ((ULONG_PTR)&APEntryEnd  - (ULONG_PTR)&APEntry));
 
     /* Copy over the bootstub for specific AP */
-    APStubSecondPhaseLoc = (PVOID)((ULONG_PTR)APStubLocation + ((ULONG_PTR)&APEntryEnd  - (ULONG_PTR)&APEntry));
     RtlCopyMemory(APStubLocation, &APEntry,  ((ULONG_PTR)&APEntryEnd - (ULONG_PTR)&APEntry));
     RtlCopyMemory(APStubSecondPhaseLoc, &APSpinup,  ((ULONG_PTR)&APSpinupEnd - (ULONG_PTR)&APSpinup));
+    RtlCopyMemory(APJumppLoc, &APJumpOffset,  sizeof(APJumpOffset));
 }
