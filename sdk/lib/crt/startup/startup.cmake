8c4aff39a891f67cac045eff9a02f93a150663fe
Timo Kreuzer <timo.kreuzer@reactos.org>
2021-07-17 14:53:17 +0200

    [CRT] Refactor CMake files

---
+++ sdk/lib/crt/startup/startup.cmake
@@ -0,0 +1,29 @@
+
+list(APPEND CRT_STARTUP_SOURCE
+    startup/crtexe.c
+    startup/wcrtexe.c
+    startup/crt_handler.c
+    startup/crtdll.c
+    startup/_newmode.c
+    startup/wildcard.c
+    startup/tlssup.c
+    startup/mingw_helpers.c
+    startup/natstart.c
+    startup/charmax.c
+    #startup/merr.c
+    startup/atonexit.c
+    #startup/txtmode.c
+    startup/pesect.c
+    startup/tlsmcrt.c
+    startup/tlsthrd.c
+    startup/tlsmthread.c
+    startup/cinitexe.c
+    startup/gs_support.c
+    startup/dll_argv.c
+    startup/dllargv.c
+    startup/wdllargv.c
+    startup/crt0_c.c
+    startup/crt0_w.c
+    startup/dllentry.c
+    startup/reactos.c
+)
