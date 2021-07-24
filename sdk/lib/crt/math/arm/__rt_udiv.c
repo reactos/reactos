f1e2c6cbd0577a20f5938af67cb5f49b4dde380a
Timo Kreuzer <timo.kreuzer@reactos.org>
2021-07-24 11:41:06 +0200

    [CRT/arm] Update file headers to new format and change license to MIT
    * Add Raman Masanin to copyrights
    * Add some comments
    * Return long long from __rt_sdiv (no functional difference, just for clarity)

--- sdk/lib/crt/math/arm/__rt_udiv.c
+++ sdk/lib/crt/math/arm/__rt_udiv.c
@@ -1,16 +1,18 @@
 /*
- * COPYRIGHT:       BSD - See COPYING.ARM in the top level directory
- * PROJECT:         ReactOS CRT library
- * PURPOSE:         Implementation of __rt_udiv
- * PROGRAMMER:      Timo Kreuzer
- * REFERENCE:       http://research.microsoft.com/en-us/um/redmond/projects/invisible/src/crt/md/arm/_div10.s.htm
- *                  http://research.microsoft.com/en-us/um/redmond/projects/invisible/src/crt/md/arm/_udiv.c.htm
+ * PROJECT:     ReactOS CRT library
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Implementation of __rt_udiv
+ * COPYRIGHT:   Copyright 2015 Timo Kreuzer <timo.kreuzer@reactos.org>
+ *              Copyright 2021 Raman Masanin <36927roma@gmail.com>
  */
 
 #define __rt_div_worker __rt_udiv_worker
 
 #include "__rt_div_worker.h"
 
+ /*
+  * Returns quotient in R0, remainder in R1
+  */
 unsigned long long
 __rt_udiv(
     unsigned int divisor,
@@ -22,4 +24,3 @@ __rt_udiv(
 
     return result.raw_data;
 }
-
