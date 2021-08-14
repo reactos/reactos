4fb72c205ca9236ac67307414f417886c2622b7d
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-08-14 14:04:11 +0900

    [IMM32_APITEST] Add ImmIsUIMessage testcase

--- modules/rostests/apitests/imm32/testlist.c
+++ modules/rostests/apitests/imm32/testlist.c
@@ -4,10 +4,12 @@
 
 extern void func_clientimc(void);
 extern void func_imcc(void);
+extern void func_ImmIsUIMessage(void);
 
 const struct test winetest_testlist[] =
 {
     { "clientimc", func_clientimc },
     { "imcc", func_imcc },
+    { "ImmIsUIMessage", func_ImmIsUIMessage },
     { 0, 0 }
 };
