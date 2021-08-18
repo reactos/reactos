724b91ffb08144641b91941cf8f58fa8ac0decfc
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-08-18 19:44:19 +0900

    fix build

--- dll/win32/imm32/imm.c
+++ dll/win32/imm32/imm.c
@@ -31,6 +31,7 @@
 #include <ndk/rtlfuncs.h>
 #include "../../../win32ss/include/ntuser.h"
 #include "../../../win32ss/include/ntwin32.h"
+#include <undocuser.h>
 #include <imm32_undoc.h>
 #include <strsafe.h>
 
