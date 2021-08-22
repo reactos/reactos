43a5b675953f7296a792e0cff95fd4b566a3e75c
Justin miller <justinmiller100@gmail.com>
2021-08-22 00:52:50 -0700

    [NTOS:KE] Fix up KeStartAllProcessors + Fix GDT/IDT Code

--- ntoskrnl/ke/mp.c
+++ ntoskrnl/ke/mp.c
@@ -8,27 +8,19 @@
 /* INCLUDES *****************************************************************/
 
 #include <ntoskrnl.h>
+#include <reactos/mp.h>
 #define NDEBUG
 #include <debug.h>
 
 /* GLOBALS *******************************************************************/
-#define DPL_SYSTEM  0
 
 ULONG ProcessorCount = 0;
-
-typedef struct _APINFO
-{
-    KIPCR Pcr;
-    KTHREAD Thread;
-    KGDTENTRY Gdt[128];
-    KIDTENTRY Idt;
-    KTSS Tss;
-} APINFO, *PAPINFO;
-
-VOID
-NTAPI
-KxInitAPProcessorState(
-    _Out_ PKPROCESSOR_STATE ProcessorState);
+PAPINFO APInfo;
+KDESCRIPTOR BSPGdt, BSPIdt;
+KPROCESSOR_STATE ProcessorState;
+PVOID KernelStack, DPCStack, PageTableLoc;
+extern LOADER_PARAMETER_BLOCK *KeLoaderBlock;
+PHYSICAL_ADDRESS GdtPhysicalLoc, IdtPhysicalLoc, PageTablePhysicalLoc;
 
 /* FUNCTIONS *****************************************************************/
 
@@ -37,26 +29,27 @@ VOID
 NTAPI
 KeStartAllProcessors()
 {
-    PVOID KernelStack;
-    PVOID DPCStack;
-    PAPINFO APInfo;
-    KPROCESSOR_STATE ProcessorState;
-    KDESCRIPTOR Gdt, Idt;
-   // PVOID NewGdt, NewIdt;
-    extern LOADER_PARAMETER_BLOCK *KeLoaderBlock;
-    do 
+    while (TRUE)
     {
         SIZE_T APInfoSize = sizeof(APINFO);
         APInfo = ExAllocatePool(NonPagedPool, APInfoSize);
-        ASSERT(APInfo);
-        ProcessorCount++;
+       
+        /* Load the GDT */
+        _sgdt(&BSPGdt.Limit);
+        __sidt(&BSPIdt.Limit);
+        GdtPhysicalLoc = MmGetPhysicalAddress((PVOID)&APInfo->Gdt);
+        IdtPhysicalLoc = MmGetPhysicalAddress((PVOID)&APInfo->Idt);
+        RtlCopyMemory(&APInfo->Gdt, (PVOID)BSPGdt.Base, BSPGdt.Limit + 1);
+        RtlCopyMemory(&APInfo->Idt, (PVOID)BSPIdt.Base, BSPIdt.Limit + 1);
 
-        _sgdt(&Gdt.Limit);
-        __sidt(&Idt.Limit);
-
+        /* Create the stacks */
         KernelStack = MmCreateKernelStack(FALSE, 0);
-        ASSERT(KernelStack);
         DPCStack = MmCreateKernelStack(FALSE, 0);
+
+        /* Check for sucess and increment processorcount */
+        ASSERT(APInfo);
+        ASSERT(KernelStack);
+        ProcessorCount++;
 
         /* Initalize Architecture specific segments */
     #ifdef _M_AMD64
@@ -68,18 +61,18 @@ KeStartAllProcessors()
         ProcessorState = APInfo->Pcr.Prcb.ProcessorState;
 
         /* Prep a new loaderblock for AP */
-        KeLoaderBlock.KernelStack = (ULONG_PTR)KernelStack;
-        KeLoaderBlock.Prcb = (ULONG_PTR)&APInfo->Pcr.Prcb;
-        KeLoaderBlock.Thread = (ULONG_PTR)&APInfo->Pcr.Prcb.IdleThread;
+        KeLoaderBlock->KernelStack = (ULONG_PTR)KernelStack;
+        KeLoaderBlock->Prcb = (ULONG_PTR)&APInfo->Pcr.Prcb;
+        KeLoaderBlock->Thread = (ULONG_PTR)&APInfo->Pcr.Prcb.IdleThread;
     #elif _M_IX86
         /* Fully initalize AP's TSS */
-        Ki386InitializeTss(&APInfo->Tss, &APInfo->Idt, &APInfo->Gdt[0]);
+        Ki386InitializeTss(&APInfo->Tss, &APInfo->Idt[0], &APInfo->Gdt[0]);
 
         /* Initalize a new PCR for the specific AP */
         KiInitializePcr(ProcessorCount,
                         &APInfo->Pcr,
-                        &APInfo->Idt, 
-                        (PKGDTENTRY)&APInfo->Gdt, 
+                        &APInfo->Idt[0], 
+                        &APInfo->Gdt[0], 
                         &APInfo->Tss,
                         &APInfo->Thread,
                         DPCStack);
@@ -89,21 +82,35 @@ KeStartAllProcessors()
         KeLoaderBlock->KernelStack = (ULONG_PTR)KernelStack;
         KeLoaderBlock->Prcb = (ULONG_PTR)&APInfo->Pcr.Prcb;
         KeLoaderBlock->Thread = (ULONG_PTR)&APInfo->Pcr.Prcb->IdleThread;
-        ProcessorState.ContextFrame.Esp = (ULONG_PTR)KernelStack;
+
+        /* Modify GDT */
+        KiSetGdtEntry(KiGetGdtEntry(&APInfo->Gdt, KGDT_TSS), (ULONG_PTR)&APInfo->Tss,
+            0x78-1, 0x09, 0, 0);
+        KiSetGdtEntry(KiGetGdtEntry(&APInfo->Gdt, KGDT_R0_PCR), (ULONG_PTR)&APInfo->Pcr,
+            MM_PAGE_SIZE - 1, TYPE_DATA, 0, 2);
+        KiSetGdtEntry(KiGetGdtEntry(&APInfo->Gdt, KGDT_DF_TSS), (ULONG_PTR)&APInfo->TssDoubleFault,
+            0xFFFF, 0x09, 0, 2);
+        KiSetGdtEntry(KiGetGdtEntry(&APInfo->Gdt, KGDT_NMI_TSS), (ULONG_PTR)&APInfo->TssNMI,
+            0xFFFF, TYPE_CODE, 0, 2);
+
     #endif
 
         /* Prep ProcessorState then start the AP */
         KxInitAPProcessorState(&ProcessorState);
+        if (!HalStartNextProcessor(KeLoaderBlock, &ProcessorState))
+        {
+            break;
+        }
+        while (KeLoaderBlock->Prcb)
+        {
+            KeMemoryBarrier();
+            DPRINT("KeStartAllProcessors: Waiting for initalization confirmation from supposive CPU: #%X\n", ProcessorCount);
+            YieldProcessor();
+        }
+    }
 
-        /* Initalize GDT and IDT */
-        //The following notes and code needs to be cleaned and change before commiting to PR
-
-        
-        /* Load TSR */
-       // ProcessorState.SpecialRegisters.Tr = APTss;
-    } while (HalStartNextProcessor(KeLoaderBlock, &ProcessorState));
     ProcessorCount--;
-    /* Started means the AP itself is online, this doesn't mean it's seen by the kernel */
+    /* Started means the AP itself is online, doesn't quite mean it's seen by the kernel */
     DPRINT1("HalStartNextProcessor: Sucessful AP startup count is %X\n", ProcessorCount);
     /* We have finished starting processors, Time to cleanup! */
     MmDeleteKernelStack(KernelStack, FALSE);
@@ -136,16 +143,101 @@ KxInitAPProcessorState(
         ProcessorState->SpecialRegisters.Cr4 = __readcr4();
 
         /* Prepare Segment Registers */
-        ProcessorState->ContextFrame.SegCs = KGDT_R0_CODE;
         ProcessorState->ContextFrame.SegSs = KGDT_R0_DATA;
         ProcessorState->ContextFrame.SegDs = KGDT_R0_DATA;
         ProcessorState->ContextFrame.SegEs = KGDT_R0_DATA; // This is vital for rep stosd.
         /* Clear GS */
         ProcessorState->ContextFrame.SegGs = 0;
-        /* Set FS to PCR */
-        ProcessorState->ContextFrame.SegFs = KGDT_R0_PCR;
 
+         Set Special ProcessorState Values */
+        ProcessorState.ContextFrame.SegFs = (ULONG_PTR)&APInfo->Pcr;
+        ProcessorState.ContextFrame.Esp = (ULONG_PTR)KernelStack;
+        ProcessorState.ContextFrame.Eax = (ULONG_PTR)PageTableLoc;
+
+        /* Setup GDT Ptrs for AP */
+        ProcessorState.SpecialRegisters.Gdtr.Base = GdtPhysicalLoc.QuadPart;
+        ProcessorState.SpecialRegisters.Gdtr.Limit = BSPGdt.Limit;
+        ProcessorState.SpecialRegisters.Idtr.Base = IdtPhysicalLoc.QuadPart;
+        ProcessorState.SpecialRegisters.Idtr.Limit = BSPIdt.Limit;
 
         ProcessorState->ContextFrame.Eip = (ULONG_PTR)KiSystemStartup;
+        DPRINT1("The location of kisystemstartup is %X\n", (ULONG_PTR)KiSystemStartup);
     }
 #endif
+
+/* GDT Functions, TODO: Find a way to share these between Freeldr and here */
+
+FORCEINLINE
+PKGDTENTRY
+KiGetGdtEntry(
+    IN PVOID pGdt,
+    IN USHORT Selector)
+{
+    return (PKGDTENTRY)((ULONG_PTR)pGdt + (Selector & ~RPL_MASK));
+}
+
+FORCEINLINE
+VOID
+KiSetGdtDescriptorBase(
+    IN OUT PKGDTENTRY Entry,
+    IN ULONG32 Base)
+{
+    Entry->BaseLow = (USHORT)(Base & 0xffff);
+    Entry->HighWord.Bytes.BaseMid = (UCHAR)((Base >> 16) & 0xff);
+    Entry->HighWord.Bytes.BaseHi  = (UCHAR)((Base >> 24) & 0xff);
+    // Entry->BaseUpper = (ULONG)(Base >> 32);
+}
+
+FORCEINLINE
+VOID
+KiSetGdtDescriptorLimit(
+    IN OUT PKGDTENTRY Entry,
+    IN ULONG Limit)
+{
+    if (Limit < 0x100000)
+    {
+        Entry->HighWord.Bits.Granularity = 0;
+    }
+    else
+    {
+        Limit >>= 12;
+        Entry->HighWord.Bits.Granularity = 1;
+    }
+    Entry->LimitLow = (USHORT)(Limit & 0xffff);
+    Entry->HighWord.Bits.LimitHi = ((Limit >> 16) & 0x0f);
+}
+
+VOID
+KiSetGdtEntryEx(
+    IN OUT PKGDTENTRY Entry,
+    IN ULONG32 Base,
+    IN ULONG Limit,
+    IN UCHAR Type,
+    IN UCHAR Dpl,
+    IN BOOLEAN Granularity,
+    IN UCHAR SegMode) // 0: 16-bit, 1: 32-bit, 2: 64-bit
+{
+    KiSetGdtDescriptorBase(Entry, Base);
+    KiSetGdtDescriptorLimit(Entry, Limit);
+    Entry->HighWord.Bits.Type = (Type & 0x1f);
+    Entry->HighWord.Bits.Dpl  = (Dpl & 0x3);
+    Entry->HighWord.Bits.Pres = (Type != 0); // Present, must be 1 when the GDT entry is valid.
+    Entry->HighWord.Bits.Sys  = 0;           // System
+    Entry->HighWord.Bits.Reserved_0  = 0;    // LongMode = !!(SegMode & 1);
+    Entry->HighWord.Bits.Default_Big = !!(SegMode & 2);
+    Entry->HighWord.Bits.Granularity |= !!Granularity; // The flag may have been already set by KiSetGdtDescriptorLimit().
+    // Entry->MustBeZero = 0;
+}
+
+FORCEINLINE
+VOID
+KiSetGdtEntry(
+    IN OUT PKGDTENTRY Entry,
+    IN ULONG32 Base,
+    IN ULONG Limit,
+    IN UCHAR Type,
+    IN UCHAR Dpl,
+    IN UCHAR SegMode) // 0: 16-bit, 1: 32-bit, 2: 64-bit
+{
+    KiSetGdtEntryEx(Entry, Base, Limit, Type, Dpl, FALSE, SegMode);
+}
---
+++ sdk/include/reactos/mp.h
@@ -0,0 +1,29 @@
+/*
+ * PROJECT:     ReactOS Kernel
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Headerfile for the multiprocessor setup functions
+ * COPYRIGHT:   Copyright 2021 Justin Miller <justinmiller100@gmail.com>
+ */
+
+#pragma once
+
+typedef struct _APINFO
+{
+    KIPCR Pcr;
+    KTHREAD Thread;
+    KGDTENTRY Gdt[128];
+    KIDTENTRY Idt[128];
+    KTSS Tss;
+    KTSS TssDoubleFault;
+    KTSS TssNMI;
+    UCHAR DECLSPEC_ALIGN(PAGE_SIZE) NMIStackData[KERNEL_STACK_SIZE]; // See DOUBLE_FAULT_STACK_SIZE
+} APINFO, *PAPINFO;
+
+/* GDT Definetions */
+
+VOID
+NTAPI
+KxInitAPProcessorState(
+    _Out_ PKPROCESSOR_STATE ProcessorState);
+
+#define DPL_SYSTEM  0
