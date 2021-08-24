reactos/reactos

commit 6158207c318bccaf1767d1a5c1545de45beb113d

[BOOTDATA] Do not include caroots.inf in registry.inf. CORE-12020
This splits registry.inf roughly in half, which massively reduces heap usage
in 1st stage. DPH and low-memory scenarios benefit from this.

boot/bootdata/CMakeLists.txt
@@ -5,8 +5,13 @@ add_subdirectory(packages)
 
 add_cd_file(FILE ${CMAKE_CURRENT_SOURCE_DIR}/txtsetup.sif DESTINATION reactos NO_CAB FOR bootcd regtest)
 
+add_custom_target(converted_caroots_inf DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf)
+add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf
+                   COMMAND native-utf16le "${CMAKE_CURRENT_SOURCE_DIR}/caroots.inf" "${CMAKE_CURRENT_BINARY_DIR}/caroots.inf"
+                   DEPENDS native-utf16le ${CMAKE_CURRENT_SOURCE_DIR}/caroots.inf)
+add_cd_file(TARGET converted_caroots_inf FILE ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf DESTINATION reactos NO_CAB FOR all)
+
 add_registry_inf(
-    caroots.inf
     hivecls.inf
     hivedef.inf
     hivesft.inf

boot/bootdata/txtsetup.sif
@@ -523,6 +523,7 @@ Default = "XT-, AT- or extended keyboard (83-105 keys)"
 0000048F = kbdeo.dll
 
 [HiveInfs.Install]
+AddReg=caroots.inf,AddReg
 AddReg=registry.inf,AddReg
 
 ; EOF

commit f67731633d67295153366c59697fd110c4b1999e

[CMAKE][BOOTDATA] Fix root CA certificates for Live CD (#3930)
This fixes regression introduced in 6158207c318.

CORE-17739 CORE-17735

boot/bootdata/CMakeLists.txt
@@ -9,7 +9,7 @@ add_custom_target(converted_caroots_inf DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/caro
 add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf
                    COMMAND native-utf16le "${CMAKE_CURRENT_SOURCE_DIR}/caroots.inf" "${CMAKE_CURRENT_BINARY_DIR}/caroots.inf"
                    DEPENDS native-utf16le ${CMAKE_CURRENT_SOURCE_DIR}/caroots.inf)
-add_cd_file(TARGET converted_caroots_inf FILE ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf DESTINATION reactos NO_CAB FOR all)
+add_cd_file(TARGET converted_caroots_inf FILE ${CMAKE_CURRENT_BINARY_DIR}/caroots.inf DESTINATION reactos NO_CAB FOR bootcd regtest)
 
 add_registry_inf(
     hivecls.inf

sdk/cmake/CMakeMacros.cmake
@@ -794,7 +794,8 @@ function(create_registry_hives)
     # LiveCD hives
     list(APPEND _livecd_inf_files
         ${_registry_inf}
-        ${CMAKE_SOURCE_DIR}/boot/bootdata/livecd.inf)
+        ${CMAKE_SOURCE_DIR}/boot/bootdata/livecd.inf
+        ${CMAKE_SOURCE_DIR}/boot/bootdata/caroots.inf)
     if(SARCH STREQUAL "xbox")
         list(APPEND _livecd_inf_files
             ${CMAKE_SOURCE_DIR}/boot/bootdata/hiveinst_xbox.inf)