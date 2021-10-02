9ad46bc9eade362c2461ab4341bfcb37bea879bb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2021-10-02 11:35:33 +0900

    alphabet ordering

--- modules/rostests/apitests/imm32/testlist.c
+++ modules/rostests/apitests/imm32/testlist.c
@@ -5,15 +5,15 @@
 extern void func_clientimc(void);
 extern void func_himc(void);
 extern void func_imcc(void);
-extern void func_ImmIsUIMessage(void);
 extern void func_ImmGetImeInfoEx(void);
+extern void func_ImmIsUIMessage(void);
 
 const struct test winetest_testlist[] =
 {
     { "clientimc", func_clientimc },
     { "himc", func_himc },
     { "imcc", func_imcc },
-    { "ImmIsUIMessage", func_ImmIsUIMessage },
     { "ImmGetImeInfoEx", func_ImmGetImeInfoEx },
+    { "ImmIsUIMessage", func_ImmIsUIMessage },
     { 0, 0 }
 };
