30010a5aa750b64528ebb2dca85ecfd7c3484f13
Serge Gautherie <32623169+SergeGautherie@users.noreply.github.com>
2021-12-03 03:14:44 +0100

    [FREELDR] debug.h: Add an explicit VOID parameter type (#4133)
    Addendum to 98c17d3.

--- boot/freeldr/freeldr/include/debug.h
+++ boot/freeldr/freeldr/include/debug.h
@@ -44,7 +44,7 @@
     ULONG   DbgPrint(const char *Format, ...);
     VOID    DbgPrint2(ULONG Mask, ULONG Level, const char *File, ULONG Line, char *Format, ...);
     VOID    DebugDumpBuffer(ULONG Mask, PVOID Buffer, ULONG Length);
-    VOID    DebugDisableScreenPort();
+    VOID    DebugDisableScreenPort(VOID);
     VOID    DbgParseDebugChannels(PCHAR Value);
 
     #define ERR_LEVEL      0x1
