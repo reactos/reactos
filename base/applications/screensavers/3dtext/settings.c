59a132a254446e11240432a61b03784809f9f016
Doug Lyons <douglyons@douglyons.com>
2021-12-03 21:19:58 -0600

    Re-write settings.c to make it easier to understand and have better functionality

--- base/applications/screensavers/3dtext/settings.c
+++ base/applications/screensavers/3dtext/settings.c
@@ -18,39 +18,102 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/* This program can actually be entered from two very different contexts.
+ * One is that it can be entered when a normal user is signed into Windows.
+ * This is the case when it is called from the Desk CPL in the ScreenSaver
+ * Tab. In this context, we will be able to open a registry key for the
+ * HKEY_CURRENT_USER and use it to find the Display String to use.
+ * The other case is when is is run when the screen saver timeout occurs.
+ * Now we are running without a normal logged in user and if we try and open
+ * HKEY_CURRENT_USER, we will get a Windows Error #2 - File not Found.
+ * In this case, we will have to use the values in HKEY_USER\.Default to
+ * find the Display String to use.
+*/
+
 #include "3dtext.h"
 
 #include <winreg.h>
+#include <strsafe.h>
 
-TCHAR m_Text[MAX_PATH] = _T("ReactOS Rocks!");
+WCHAR m_Text[MAX_PATH] = L"ReactOS Rocks!";
 
 VOID LoadSettings(VOID)
 {
-	HKEY hkey;
-	DWORD len = MAX_PATH * sizeof(TCHAR);
+    HKEY hkey;
+    DWORD len = MAX_PATH * sizeof(WCHAR);
+    DWORD dwRet;
 
-	if (RegCreateKeyEx(HKEY_CURRENT_USER, _T("Software\\Microsoft\\ScreenSavers\\Text3D"), 0,
-		_T(""), 0, KEY_READ, NULL, &hkey, NULL) == ERROR_SUCCESS)
+    dwRet = RegOpenKeyExW(HKEY_CURRENT_USER,
+                          L"Software\\Microsoft\\ScreenSavers\\Text3D",
+                          0,
+                          KEY_READ,
+                          &hkey);
+
+    if (dwRet == ERROR_SUCCESS)
     {
-        RegQueryValueEx(hkey, _T("DisplayString"), NULL, NULL, (LPBYTE)m_Text, &len);
-        RegCloseKey(hkey);
+        dwRet = RegQueryValueExW(hkey,
+                                 L"DisplayString",
+                                 NULL,
+                                 NULL,
+                                 (LPBYTE)m_Text,
+                                 &len);
+
+        if( dwRet == ERROR_SUCCESS )
+        {
+            /* This makes sure that the settings are saved in two places. */
+            SaveSettings();
+        }
+    }
+    else
+    {
+        dwRet = RegOpenKeyExW(HKEY_USERS,
+                              L".Default\\Software\\Microsoft\\ScreenSavers\\Text3D",
+                              0,
+                              KEY_READ,
+                              &hkey);
+
+        if( dwRet == ERROR_SUCCESS )
+        {
+            dwRet = RegQueryValueExW(hkey,
+                                     L"DisplayString",
+                                     NULL,
+                                     NULL,
+                                     (LPBYTE)m_Text,
+                                     &len);
+
+            if( dwRet == ERROR_SUCCESS )
+            {
+                /* This makes sure that the settings are saved in two places. */
+                SaveSettings();
+            }
+        }
+        else
+        {
+            /* Save Default Settings in two places. */
+            SaveSettings();
+        }
     }
 }
 
 VOID SaveSettings(VOID)
 {
-	HKEY hkey;
+    HKEY hkey;
+    size_t CbDestLength = 0;
 
-	if (RegCreateKeyEx(HKEY_CURRENT_USER, _T("Software\\Microsoft\\ScreenSavers\\Text3D"), 0,
-		_T(""), 0, KEY_WRITE, NULL, &hkey, NULL) == ERROR_SUCCESS)
+    if (RegCreateKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\ScreenSavers\\Text3D", 0,
+    L"", 0, KEY_WRITE, NULL, &hkey, NULL) == ERROR_SUCCESS)
     {
-        RegSetValueEx(hkey, _T("DisplayString"), 0, REG_SZ, (LPBYTE)m_Text, (_tcslen(m_Text) + 1) * sizeof(TCHAR));
+        StringCbLengthW(m_Text, sizeof(m_Text), &CbDestLength);
+        RegSetValueExW(hkey, L"DisplayString", 0, REG_SZ, (LPBYTE)m_Text, CbDestLength + sizeof(WCHAR));
         RegCloseKey(hkey);
     }
-    if (RegCreateKeyEx(HKEY_USERS, _T(".DEFAULT\\Software\\Microsoft\\ScreenSavers\\Text3D"), 0,
-        _T(""), 0, KEY_WRITE, NULL, &hkey, NULL) == ERROR_SUCCESS)
+
+    CbDestLength = 0;
+    if (RegCreateKeyExW(HKEY_USERS, L".DEFAULT\\Software\\Microsoft\\ScreenSavers\\Text3D", 0,
+        L"", 0, KEY_WRITE, NULL, &hkey, NULL) == ERROR_SUCCESS)
     {
-        RegSetValueEx(hkey, _T("DisplayString"), 0, REG_SZ, (LPBYTE)m_Text, (_tcslen(m_Text) + 1) * sizeof(TCHAR));
+        StringCbLengthW(m_Text, sizeof(m_Text), &CbDestLength);
+        RegSetValueExW(hkey, L"DisplayString", 0, REG_SZ, (LPBYTE)m_Text, CbDestLength + sizeof(WCHAR));
         RegCloseKey(hkey);
     }
 }
