11d11f93eeaa7f426b938c4cd937732d1efadf7c
Thomas Faber <thomas.faber@reactos.org>
2021-12-11 15:44:21 -0500

    [NTDLL_APITEST] Add tests for NtCreateProfile and NtStartProfile.

--- modules/rostests/apitests/ntdll/testlist.c
+++ modules/rostests/apitests/ntdll/testlist.c
@@ -19,6 +19,7 @@ extern void func_NtCompareTokens(void);
 extern void func_NtContinue(void);
 extern void func_NtCreateFile(void);
 extern void func_NtCreateKey(void);
+extern void func_NtCreateProfile(void);
 extern void func_NtCreateThread(void);
 extern void func_NtDeleteKey(void);
 extern void func_NtDuplicateObject(void);
@@ -53,6 +54,7 @@ extern void func_NtSetInformationThread(void);
 extern void func_NtSetInformationToken(void);
 extern void func_NtSetValueKey(void);
 extern void func_NtSetVolumeInformationFile(void);
+extern void func_NtStartProfile(void);
 extern void func_NtSystemInformation(void);
 extern void func_NtUnloadDriver(void);
 extern void func_NtWriteFile(void);
@@ -124,6 +126,7 @@ const struct test winetest_testlist[] =
     { "NtContinue",                     func_NtContinue },
     { "NtCreateFile",                   func_NtCreateFile },
     { "NtCreateKey",                    func_NtCreateKey },
+    { "NtCreateProfile",                func_NtCreateProfile },
     { "NtCreateThread",                 func_NtCreateThread },
     { "NtDeleteKey",                    func_NtDeleteKey },
     { "NtDuplicateObject",              func_NtDuplicateObject },
@@ -158,6 +161,7 @@ const struct test winetest_testlist[] =
     { "NtSetInformationToken",          func_NtSetInformationToken },
     { "NtSetValueKey",                  func_NtSetValueKey},
     { "NtSetVolumeInformationFile",     func_NtSetVolumeInformationFile },
+    { "NtStartProfile",                 func_NtStartProfile },
     { "NtSystemInformation",            func_NtSystemInformation },
     { "NtUnloadDriver",                 func_NtUnloadDriver },
     { "NtWriteFile",                    func_NtWriteFile },
