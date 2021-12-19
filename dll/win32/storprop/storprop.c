9a7bd386b9b38fa5917331502a9d3de92b199b9f
Eric Kohl <eric.kohl@reactos.org>
2021-12-20 00:08:06 +0100

    [STORPROP] Add missing stubs and move some functions around

--- dll/win32/storprop/storprop.c
+++ dll/win32/storprop/storprop.c
@@ -5,86 +5,13 @@
  * COPYRIGHT:   2020 Eric Kohl (eric.kohl@reactos.org)
  */
 
-#define WIN32_NO_STATUS
-#include <stdarg.h>
-#include <windef.h>
-#include <winbase.h>
-#include <winreg.h>
-#include <winuser.h>
-#include <commctrl.h>
-#include <setupapi.h>
+#include "precomp.h"
 
 #define NDEBUG
 #include <debug.h>
 
 HINSTANCE hInstance = NULL;
 
-/*
- * @unimplemented
- */
-DWORD
-WINAPI
-DiskClassInstaller(
-    _In_ DI_FUNCTION InstallFunction,
-    _In_ HDEVINFO DeviceInfoSet,
-    _In_ PSP_DEVINFO_DATA DeviceInfoData OPTIONAL)
-{
-    DPRINT("DiskClassInstaller(%u %p %p)\n",
-           InstallFunction, DeviceInfoSet, DeviceInfoData);
-
-    if (InstallFunction == DIF_ADDPROPERTYPAGE_ADVANCED)
-    {
-        return ERROR_SUCCESS;
-    }
-
-    return ERROR_DI_DO_DEFAULT;
-}
-
-/*
- * @unimplemented
- */
-DWORD
-WINAPI
-DvdClassInstaller(
-    _In_ DI_FUNCTION InstallFunction,
-    _In_ HDEVINFO DeviceInfoSet,
-    _In_ PSP_DEVINFO_DATA DeviceInfoData OPTIONAL)
-{
-    DPRINT("DvdClassInstaller(%u %p %p)\n",
-           InstallFunction, DeviceInfoSet, DeviceInfoData);
-
-    return ERROR_DI_DO_DEFAULT;
-}
-
-/*
- * @unimplemented
- */
-BOOL
-WINAPI
-DvdPropPageProvider(
-    _In_ PSP_PROPSHEETPAGE_REQUEST lpPropSheetPageRequest,
-    _In_ LPFNADDPROPSHEETPAGE lpfnAddPropSheetPageProc,
-    _In_ LPARAM lParam)
-{
-    DPRINT("DvdPropPageProvider(%p %p %lx)\n",
-           lpPropSheetPageRequest, lpfnAddPropSheetPageProc, lParam);
-    return FALSE;
-}
-
-/*
- * @unimplemented
- */
-BOOL
-WINAPI
-IdePropPageProvider(
-    _In_ PSP_PROPSHEETPAGE_REQUEST lpPropSheetPageRequest,
-    _In_ LPFNADDPROPSHEETPAGE lpfnAddPropSheetPageProc,
-    _In_ LPARAM lParam)
-{
-    DPRINT("IdePropPageProvider(%p %p %lx)\n",
-           lpPropSheetPageRequest, lpfnAddPropSheetPageProc, lParam);
-    return FALSE;
-}
 
 /*
  * @implemented
@@ -100,6 +27,7 @@ VolumePropPageProvider(
            lpPropSheetPageRequest, lpfnAddPropSheetPageProc, lParam);
     return FALSE;
 }
+
 
 BOOL
 WINAPI
