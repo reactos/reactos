9592656ea578b9bbcdd2a5bab6e19e5509893963
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-01-01 08:20:02 +0900

    add validation

--- win32ss/user/ntuser/ntstubs.c
+++ win32ss/user/ntuser/ntstubs.c
@@ -363,7 +363,8 @@ BOOL APIENTRY UserUpdateInputContext(PIMC pIMC, DWORD dwType, DWORD_PTR dwValue)
             break;
 
         case UIC_IMEWINDOW:
-            pIMC->hImeWnd = (HWND)dwValue;
+            if (ValidateHwndNoErr((HWND)dwValue))
+                pIMC->hImeWnd = (HWND)dwValue;
             break;
 
         default:
