4b94449b39e8a9bb7bd379ae2bf7d2f61af00787
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-01-09 10:18:15 +0900

    revert history.h

--- base/applications/mspaint/history.h
+++ base/applications/mspaint/history.h
@@ -25,10 +25,10 @@ private:
     int redoSteps;
 public:
     ImageModel();
-    void CopyPrevious();
-    void Undo();
-    void Redo();
-    void ResetToPrevious();
+    void CopyPrevious(void);
+    void Undo(void);
+    void Redo(void);
+    void ResetToPrevious(void);
     void ClearHistory(void);
     void Insert(HBITMAP hbm);
     void Crop(int nWidth, int nHeight, int nOffsetX = 0, int nOffsetY = 0);
