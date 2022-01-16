dbff9b0183f1dba8d440d47038eb6fcd201d2d94
winesync <ros-dev@reactos.org>
2022-01-16 20:10:05 +0100

    [WINESYNC] reg/tests: Try deleting strings containing escaped null characters.
    Signed-off-by: Hugh McMaster <hugh.mcmaster@outlook.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id aa98e3f6da78135ecffaa50bb78ad906dfd4570d by Hugh McMaster <hugh.mcmaster@outlook.com>

--- modules/rostests/winetests/reg/delete.c
+++ modules/rostests/winetests/reg/delete.c
@@ -100,6 +100,22 @@ static void test_delete(void)
     ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
     verify_reg_nonexist(hkey, "\\foo\\bar");
 
+    add_value(hkey, "string\\01", REG_SZ, "Value", 6);
+    add_value(hkey, "string2", REG_SZ, "foo\\0bar", 9);
+    add_value(hkey, "\\0", REG_SZ, "Value", 6);
+
+    run_reg_exe("reg delete HKCU\\" KEY_BASE " /v string\\01 /f", &r);
+    ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
+    verify_reg_nonexist(hkey, "string\\01");
+
+    run_reg_exe("reg delete HKCU\\" KEY_BASE " /v string2 /f", &r);
+    ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
+    verify_reg_nonexist(hkey, "string2");
+
+    run_reg_exe("reg delete HKCU\\" KEY_BASE " /v \\0 /f", &r);
+    ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
+    verify_reg_nonexist(hkey, "\\0");
+
     close_key(hkey);
 
     run_reg_exe("reg delete HKCU\\" KEY_BASE " /f", &r);
