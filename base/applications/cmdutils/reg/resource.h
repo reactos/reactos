34aed2639af060cc356a793c2961887e88146336
winesync <ros-dev@reactos.org>
2022-01-17 18:42:07 +0100

    [WINESYNC] reg: Group resource IDs by source location.
    Signed-off-by: Hugh McMaster <hugh.mcmaster@outlook.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 8f997a8b9b75320449711d3b05241b5f1f580ac3 by Hugh McMaster <hugh.mcmaster@outlook.com>

--- base/applications/cmdutils/reg/resource.h
+++ base/applications/cmdutils/reg/resource.h
@@ -22,45 +22,59 @@
 
 #include <windef.h>
 
-/* Translation IDs. */
-#define STRING_USAGE                  101
-#define STRING_ADD_USAGE              102
-#define STRING_DELETE_USAGE           103
-#define STRING_QUERY_USAGE            104
-#define STRING_SUCCESS                105
-#define STRING_INVALID_KEY            106
-#define STRING_INVALID_CMDLINE        107
-#define STRING_NO_REMOTE              108
-#define STRING_VALUE_NONEXIST         109
-#define STRING_UNSUPPORTED_TYPE       110
-#define STRING_MISSING_NUMBER         111
-#define STRING_MISSING_HEXDATA        112
-#define STRING_UNHANDLED_TYPE         113
-#define STRING_OVERWRITE_VALUE        114
-#define STRING_YESNO                  115
-#define STRING_YES                    116
-#define STRING_NO                     117
-#define STRING_CANCELLED              118
-#define STRING_DEFAULT_VALUE          119
-#define STRING_DELETE_VALUE           120
-#define STRING_DELETE_VALUEALL        121
-#define STRING_DELETE_SUBKEY          122
-#define STRING_INVALID_STRING         123
-#define STRING_VALUEALL_FAILED        124
-#define STRING_MATCHES_FOUND          126
-#define STRING_INVALID_SYNTAX         127
-#define STRING_INVALID_OPTION         128
-#define STRING_REG_HELP               129
-#define STRING_FUNC_HELP              130
-#define STRING_VALUE_NOT_SET          131
-#define STRING_IMPORT_USAGE           132
-#define STRING_FILE_NOT_FOUND         133
-#define STRING_ESCAPE_SEQUENCE        135
-#define STRING_EXPORT_USAGE           136
-#define STRING_INVALID_SYSTEM_KEY     137
-#define STRING_OVERWRITE_FILE         138
-#define STRING_KEY_NONEXIST           139
-#define STRING_KEY_IMPORT_FAILED      140
-#define STRING_REG_VIEW_USAGE         141
-#define STRING_ACCESS_DENIED          142
-#define STRING_COPY_USAGE             143
+/* Translation IDs */
+
+/* Shared */
+#define STRING_YES                    100
+#define STRING_NO                     101
+#define STRING_YESNO                  103
+#define STRING_INVALID_SYNTAX         105
+#define STRING_FUNC_HELP              106
+#define STRING_ACCESS_DENIED          107
+#define STRING_SUCCESS                108
+#define STRING_CANCELLED              109
+#define STRING_KEY_NONEXIST           110
+#define STRING_VALUE_NONEXIST         111
+#define STRING_DEFAULT_VALUE          112
+
+/* reg.c */
+#define STRING_REG_HELP               150
+#define STRING_USAGE                  151
+#define STRING_ADD_USAGE              152
+#define STRING_COPY_USAGE             153
+#define STRING_DELETE_USAGE           154
+#define STRING_EXPORT_USAGE           155
+#define STRING_IMPORT_USAGE           156
+#define STRING_QUERY_USAGE            157
+#define STRING_REG_VIEW_USAGE         164
+#define STRING_INVALID_KEY            165
+#define STRING_NO_REMOTE              166
+#define STRING_INVALID_SYSTEM_KEY     167
+#define STRING_INVALID_OPTION         168
+
+/* add.c */
+#define STRING_MISSING_NUMBER         200
+#define STRING_MISSING_HEXDATA        201
+#define STRING_INVALID_STRING         202
+#define STRING_UNHANDLED_TYPE         203
+#define STRING_UNSUPPORTED_TYPE       204
+#define STRING_OVERWRITE_VALUE        205
+#define STRING_INVALID_CMDLINE        206
+
+/* delete.c */
+#define STRING_DELETE_VALUE           300
+#define STRING_DELETE_VALUEALL        301
+#define STRING_DELETE_SUBKEY          302
+#define STRING_VALUEALL_FAILED        303
+
+/* export.c */
+#define STRING_OVERWRITE_FILE         350
+
+/* import.c */
+#define STRING_ESCAPE_SEQUENCE        400
+#define STRING_KEY_IMPORT_FAILED      401
+#define STRING_FILE_NOT_FOUND         402
+
+/* query.c */
+#define STRING_VALUE_NOT_SET          450
+#define STRING_MATCHES_FOUND          451
