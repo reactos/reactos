83fb76e7455a6bba77af8bce8e569b508c3c4fde
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-01-20 11:16:01 +0900

    fix file header

--- sdk/lib/atl/atltrace.h
+++ sdk/lib/atl/atltrace.h
@@ -1,5 +1,5 @@
 /*
- * PROJECT:     ReactOS Kernel
+ * PROJECT:     ReactOS ATL
  * LICENSE:     LGPL-2.0-or-later (https://spdx.org/licenses/LGPL-2.0-or-later)
  * PURPOSE:     Providing ATLTRACE macro
  * COPYRIGHT:   Copyright 2022 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
