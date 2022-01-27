88c8b9227760dcbfc8d20d60b6fbbe5181f1951a
Joachim Henze <Joachim.Henze@reactos.org>
2022-01-27 20:17:10 +0100

    [0.4.12][NTOSKRNL] Mute noisy DPRINT 'SectionObject has ImageSection' during shutdown CORE-18029
    All releases/0.4.8 until and including releases/0.4.14 were affected by the following logspam
    during shutdown:
    (../ntoskrnl/mm/section.c:4839) SectionObject has ImageSection
    triggered 2 times for bootcd, but triggered 83 times for livecd.
    
    Today I found out by chance, that we started to trigger it with the switch from CDFS to CDFS_NEW with
    0.4.8-dev-164-g ec6b3ecbe48ad51c366563196cc93a6ae74e8b69
    
    We 'suffered' from that logspam for many years now and it did never help us in any way.
    And during 0.4.15-dev'ing with the MM rewrite, the logging was entirely removed in the NTOSKRNL.

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -4817,10 +4817,7 @@ MmFlushImageSection (IN PSECTION_OBJECT_POINTERS SectionObjectPointer,
 #endif
 
         if (SectionObjectPointer->ImageSectionObject)
-        {
-            DPRINT1("SectionObject has ImageSection\n");
             return FALSE;
-        }
 
 #ifdef NEWCC
         CcpLock();
