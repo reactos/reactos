3b8bfa0bd2138074a4a3c7220e4d71e44b7b43ef
Joachim Henze <Joachim.Henze@reactos.org>
2022-01-30 22:20:06 +0100

    [0.4.13][WIN32SS] Fix Mask for Draw Frame States CORE-8808
    Fixes drawing of caption bar buttons (min/max/close in Mozilla Firefox titlebar when its menubar is disabled).
    See CORE-8808.
    
    The fix is a partial port of 0.4.14-dev-1314-g acf135d16d2aede3c12edc4ef47033baf5540751
    I left out all changes to the logging and took only the essence of this patch.

--- win32ss/user/ntuser/draw.c
+++ win32ss/user/ntuser/draw.c
@@ -757,7 +757,7 @@ BOOL FASTCALL UITOOLS95_DFC_ButtonCheckRadio(HDC dc, LPRECT r, UINT uFlags, BOOL
 /* Ported from WINE20020904 */
 BOOL FASTCALL UITOOLS95_DrawFrameButton(HDC hdc, LPRECT rc, UINT uState)
 {
-    switch(uState & 0xff)
+    switch(uState & 0x1f)
     {
         case DFCS_BUTTONPUSH:
             return UITOOLS95_DFC_ButtonPush(hdc, rc, uState);
@@ -787,7 +787,7 @@ BOOL FASTCALL UITOOLS95_DrawFrameCaption(HDC dc, LPRECT r, UINT uFlags)
     RECT myr;
     INT bkmode;
     WCHAR Symbol;
-    switch(uFlags & 0xff)
+    switch(uFlags & 0xf)
     {
         case DFCS_CAPTIONCLOSE:
 		Symbol = 'r';
@@ -854,7 +854,7 @@ BOOL FASTCALL UITOOLS95_DrawFrameScroll(HDC dc, LPRECT r, UINT uFlags)
     RECT myr;
     INT bkmode;
     WCHAR Symbol;
-    switch(uFlags & 0xff)
+    switch(uFlags & 0x1f)
     {
         case DFCS_SCROLLCOMBOBOX:
         case DFCS_SCROLLDOWN:
@@ -957,7 +957,7 @@ BOOL FASTCALL UITOOLS95_DrawFrameMenu(HDC dc, LPRECT r, UINT uFlags)
     RECT myr;
     INT cxy;
     cxy = UITOOLS_MakeSquareRect(r, &myr);
-    switch(uFlags & 0xff)
+    switch(uFlags & 0x1f)
     {
         case DFCS_MENUARROWUP:
             Symbol = '5';
@@ -1000,8 +1000,8 @@ BOOL FASTCALL UITOOLS95_DrawFrameMenu(HDC dc, LPRECT r, UINT uFlags)
     /* save font */
     hOldFont = NtGdiSelectFont(dc, hFont);
 
-    if ((uFlags & 0xff) == DFCS_MENUARROWUP ||
-        (uFlags & 0xff) == DFCS_MENUARROWDOWN )
+    if ((uFlags & 0x1f) == DFCS_MENUARROWUP ||
+        (uFlags & 0x1f) == DFCS_MENUARROWDOWN )
     {
 #if 0
        if (uFlags & DFCS_INACTIVE)
