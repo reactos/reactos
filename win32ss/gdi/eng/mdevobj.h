9db63ad59552fd58d94934ebb158b18177da468a
Hervé Poussineau <hpoussin@reactos.org>
2022-03-20 15:45:19 +0100

    [WIN32SS] Add functions related to MDEV (creation, destruction...)

--- win32ss/gdi/eng/mdevobj.h
+++ win32ss/gdi/eng/mdevobj.h
@@ -7,11 +7,40 @@ typedef struct _PDEVOBJ *PPDEVOBJ;
 
 typedef struct _MDEVOBJ
 {
+    ULONG cDev;
     PPDEVOBJ ppdevGlobal;
+    struct
+    {
+        PPDEVOBJ ppdev;
+    } dev[10]; /* FIXME: max number of displays. Needs dynamic allocation */
 } MDEVOBJ, *PMDEVOBJ;
 
 /* Globals ********************************************************************/
 
 extern PMDEVOBJ gpmdev; /* FIXME: should be stored in gpDispInfo->pmdev */
+
+/* Function prototypes ********************************************************/
+
+VOID
+MDEVOBJ_vEnable(
+    _Inout_ PMDEVOBJ pmdev);
+
+BOOL
+MDEVOBJ_bDisable(
+    _Inout_ PMDEVOBJ pmdev);
+
+/* Create a new MDEV:
+ * - pustrDeviceName: name of the device to put in MDEV. If NULL, will put all graphics devices in MDEV
+ * - pdm: settings associated to pustrDeviceName. Unused if pustrDeviceName is NULL.
+ * Return value: the new MDEV (or NULL in case of error)
+ */
+PMDEVOBJ
+MDEVOBJ_Create(
+    _In_opt_ PUNICODE_STRING pustrDeviceName,
+    _In_opt_ PDEVMODEW pdm);
+
+VOID
+MDEVOBJ_vDestroy(
+    _Inout_ PMDEVOBJ pmdev);
 
 #endif /* !__WIN32K_MDEVOBJ_H */
