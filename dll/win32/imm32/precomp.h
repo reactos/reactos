9401f6cd77815b2d057f10eeed7994be213859db
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-03-31 08:20:52 +0900

    add comment: // Win: pImmHeap

--- dll/win32/imm32/precomp.h
+++ dll/win32/imm32/precomp.h
@@ -75,7 +75,7 @@ extern PIMEDPI g_pImeDpiList;
 extern PSERVERINFO gpsi;
 extern SHAREDINFO g_SharedInfo;
 extern BYTE g_bClientRegd;
-extern HANDLE ghImmHeap;
+extern HANDLE ghImmHeap; // Win: pImmHeap
 
 BOOL Imm32GetSystemLibraryPath(LPWSTR pszPath, DWORD cchPath, LPCWSTR pszFileName);
 VOID APIENTRY LogFontAnsiToWide(const LOGFONTA *plfA, LPLOGFONTW plfW);
