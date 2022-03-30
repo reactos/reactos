58076d8beab6cab35ef5a6cc9f490bfd4f393be6
Raymond Czerny <chip@raymisoft.de>
2022-03-30 20:08:08 +0200

    [PSDK] Language-id set to neutral in the version information path.

--- sdk/include/psdk/versionhelpers.h
+++ sdk/include/psdk/versionhelpers.h
@@ -176,7 +176,7 @@ IsReactOS()
                 VerInfo  = HeapAlloc(GetProcessHeap(), 0, dwSize);
                 GetFileVersionInfoW(szFullPath, 0L, dwSize, VerInfo);
                 bProduct = VerQueryValueW(VerInfo,
-                                          L"\\StringFileInfo\\040904B0\\ProductName",
+                                          L"\\StringFileInfo\\000004B0\\ProductName",
                                           (LPVOID)&lszValue,
                                           &cchVer);
                 if (bProduct)
