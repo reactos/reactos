3cf4f6d5c0e74f3f2c792fc235afa50dbfad227b
Raymond Czerny <chip@raymisoft.de>
2022-05-06 04:57:34 +0200

    Revert "[PSDK] Use of UNICODE independent function calls."
    This reverts commit 69adb958efce71af95c353922427895364871ca7.

--- sdk/include/psdk/versionhelpers.h
+++ sdk/include/psdk/versionhelpers.h
@@ -26,7 +26,6 @@
 
 #include <specstrings.h>
 #include <winreg.h>
-#include <string.h>
 
 #ifdef __cplusplus
 #define VERSIONHELPERAPI inline bool
@@ -154,30 +153,25 @@ IsReactOS()
     HKEY hKey;
     BOOL bResult = FALSE;
 
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-                     L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",
-                     0,
-                     KEY_QUERY_VALUE,
-                     &hKey) == ERROR_SUCCESS)
+    if (RegOpenKeyExW(HKEY_LOCAL_MACHINE,
+                      L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",
+                      0,
+                      KEY_QUERY_VALUE,
+                      &hKey) == ERROR_SUCCESS)
     {
-        TCHAR szProductName[30];
+        WCHAR szProductName[30];
         DWORD dwLength = sizeof(szProductName);
 
-        if (RegGetValue(hKey,
-                        NULL,
-                        TEXT("ProductName"),
-                        RRF_RT_REG_SZ | RRF_ZEROONFAILURE,
-                        NULL,
-                        szProductName,
-                        &dwLength) == ERROR_SUCCESS)
+        if (RegGetValueW(hKey,
+                         NULL,
+                         L"ProductName",
+                         RRF_RT_REG_SZ | RRF_ZEROONFAILURE,
+                         NULL,
+                         szProductName,
+                         &dwLength) == ERROR_SUCCESS)
         {
-#ifdef UNICODE
             bResult = (wcscmp(L"ReactOS", szProductName) == 0);
-#else
-            bResult = (strcmp("ReactOS", szProductName) == 0);
-#endif
         }
-        RegCloseKey(hKey);
     }
     return bResult;
 }
