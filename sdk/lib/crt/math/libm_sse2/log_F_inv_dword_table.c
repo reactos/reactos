898ca65dbd13a328dc9edc585caa62df43dc4952
Timo Kreuzer <timo.kreuzer@reactos.org>
2022-06-18 12:16:29 +0200

    [LIBM] Convert tables from asm to C

---
+++ sdk/lib/crt/math/libm_sse2/log_F_inv_dword_table.c
@@ -0,0 +1,164 @@
+/*
+ * MIT License
+ * -----------
+ *
+ * Copyright (c) 2002-2019 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this Software and associated documentaon files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **
+ ** Defines __log_F_inv_dword
+ ** Used in log10f and logf
+ **
+ */
+
+#include <crtdefs.h>
+
+const unsigned int _CRT_ALIGN(16) __log_F_inv_dword[] =
+{
+    0x40000000,
+    0x3ffe03f8,
+    0x3ffc0fc1,
+    0x3ffa232d,
+    0x3ff83e10,
+    0x3ff6603e,
+    0x3ff4898d,
+    0x3ff2b9d6,
+    0x3ff0f0f1,
+    0x3fef2eb7,
+    0x3fed7304,
+    0x3febbdb3,
+    0x3fea0ea1,
+    0x3fe865ac,
+    0x3fe6c2b4,
+    0x3fe52598,
+    0x3fe38e39,
+    0x3fe1fc78,
+    0x3fe07038,
+    0x3fdee95c,
+    0x3fdd67c9,
+    0x3fdbeb62,
+    0x3fda740e,
+    0x3fd901b2,
+    0x3fd79436,
+    0x3fd62b81,
+    0x3fd4c77b,
+    0x3fd3680d,
+    0x3fd20d21,
+    0x3fd0b6a0,
+    0x3fcf6475,
+    0x3fce168a,
+    0x3fcccccd,
+    0x3fcb8728,
+    0x3fca4588,
+    0x3fc907da,
+    0x3fc7ce0c,
+    0x3fc6980c,
+    0x3fc565c8,
+    0x3fc43730,
+    0x3fc30c31,
+    0x3fc1e4bc,
+    0x3fc0c0c1,
+    0x3fbfa030,
+    0x3fbe82fa,
+    0x3fbd6910,
+    0x3fbc5264,
+    0x3fbb3ee7,
+    0x3fba2e8c,
+    0x3fb92144,
+    0x3fb81703,
+    0x3fb70fbb,
+    0x3fb60b61,
+    0x3fb509e7,
+    0x3fb40b41,
+    0x3fb30f63,
+    0x3fb21643,
+    0x3fb11fd4,
+    0x3fb02c0b,
+    0x3faf3ade,
+    0x3fae4c41,
+    0x3fad602b,
+    0x3fac7692,
+    0x3fab8f6a,
+    0x3faaaaab,
+    0x3fa9c84a,
+    0x3fa8e83f,
+    0x3fa80a81,
+    0x3fa72f05,
+    0x3fa655c4,
+    0x3fa57eb5,
+    0x3fa4a9cf,
+    0x3fa3d70a,
+    0x3fa3065e,
+    0x3fa237c3,
+    0x3fa16b31,
+    0x3fa0a0a1,
+    0x3f9fd80a,
+    0x3f9f1166,
+    0x3f9e4cad,
+    0x3f9d89d9,
+    0x3f9cc8e1,
+    0x3f9c09c1,
+    0x3f9b4c70,
+    0x3f9a90e8,
+    0x3f99d723,
+    0x3f991f1a,
+    0x3f9868c8,
+    0x3f97b426,
+    0x3f97012e,
+    0x3f964fda,
+    0x3f95a025,
+    0x3f94f209,
+    0x3f944581,
+    0x3f939a86,
+    0x3f92f114,
+    0x3f924925,
+    0x3f91a2b4,
+    0x3f90fdbc,
+    0x3f905a38,
+    0x3f8fb824,
+    0x3f8f177a,
+    0x3f8e7835,
+    0x3f8dda52,
+    0x3f8d3dcb,
+    0x3f8ca29c,
+    0x3f8c08c1,
+    0x3f8b7034,
+    0x3f8ad8f3,
+    0x3f8a42f8,
+    0x3f89ae41,
+    0x3f891ac7,
+    0x3f888889,
+    0x3f87f781,
+    0x3f8767ab,
+    0x3f86d905,
+    0x3f864b8a,
+    0x3f85bf37,
+    0x3f853408,
+    0x3f84a9fa,
+    0x3f842108,
+    0x3f839930,
+    0x3f83126f,
+    0x3f828cc0,
+    0x3f820821,
+    0x3f81848e,
+    0x3f810204,
+    0x3f808081,
+    0x3f800000,
+};
