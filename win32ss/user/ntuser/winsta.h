eb8e3b14a09e8673cdb2c1aa35524a5a5551be77
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-07-31 07:56:34 +0900

    Update win32ss/user/ntuser/winsta.h
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- win32ss/user/ntuser/winsta.h
+++ win32ss/user/ntuser/winsta.h
@@ -38,6 +38,7 @@ typedef struct _WINSTATION_OBJECT
     LUID           luidUser;
     PVOID          psidUser;
 
+    /* ReactOS-specific */
     struct _DESKTOP* ActiveDesktop;
     HANDLE         ShellWindow;
     HANDLE         ShellListView;
