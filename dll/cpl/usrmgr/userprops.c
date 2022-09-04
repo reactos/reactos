bbede4dd89f5e3913239bf0eefa179766b6a2a16
Kyle Katarn <contact@kcsoftwares.com>
2022-09-04 10:07:10 +0200

    Squashed commit of the following:
    commit b42ddce11a7f9e60f434f2abcace59c5c938f21e
    Author: Kyle Katarn <contact@kcsoftwares.com>
    Date:   Sat Sep 3 23:30:57 2022 +0200
    
        [NETSHELL] Fix "ghost" network activity when opening Network Status page (#4662)
    
        Opening Network Status page creates a systematic "ghost" network activity,
        activating RX+TX activity icon both on property page and tray, while no Rx/Tx.
        This is due to pContext->dw[In|Out]Octets being initialized to 0 and compared
        to refreshed interface data.
    
        To circumvent this, copy refreshed interface data to the context on first update.
    
    commit 7dc58ccb0c390b48c65d6dae9ae592f6b17d3cea
    Author: Sam Watson <multisniperism@gmail.com>
    Date:   Sun Sep 4 09:18:36 2022 +1200
    
        [THEMES] Improve text colors of Modern Dark theme (#4655)
    
        - Update colors for all different font sizes.
        - Update caption color for other fonts too.
    
    commit 22f0c3a84a24b75d7ebe54913ae51185fb0c28a2
    Author: Kyle Katarn <contact@kcsoftwares.com>
    Date:   Sat Sep 3 23:12:29 2022 +0200
    
        [TRANSLATION] French translation update (#4638)
    
        Update of various French translation files for several modules:
        CALC, LABEL, DXDIAG, MPLAY32, RAPPS, SHUTDOWN, SNDVOL32, EXPLORER, WINLOGON;
        CONSOLE.CPL, MMSYS.CPL; NETCFGX.DLL, SHELL32.DLL, SHLWAPI.DLL, SYSSETUP.DLL
    
    commit 7ed0284e8eb25b5512d551bc9256cdfc945c0351
    Author: Kyle Katarn <contact@kcsoftwares.com>
    Date:   Sat Sep 3 22:56:33 2022 +0200
    
        [NTOS:EX] Fix returned number of handles for Idle System Process (#4661)
    
        PsIdleProcess and PsInitialSystemProcess share the same handle table. This
        leads ObGetProcessHandleCount() to report the same number of handles
        when called on those system processes, when being enumerated by
        NtQuerySystemInformation(SystemProcessInformation).
    
        Instead, just return 0 for the handle count of the Idle process in SystemProcessInformation.
        This is not done in ObGetProcessHandleCount(), since a separate
        NtQueryInformationProcess(ProcessHandleCount) for the idle process should return
        a non-zero value.
    
        CORE-16577
    
    commit de5af76811bbd318cdef46e193f306833e3fa740
    Author: Kyle Katarn <112266950+KRosUser@users.noreply.github.com>
    Date:   Sat Sep 3 17:29:26 2022 +0200
    
        [TASKMGR] Fix overlapping controls (French only) (#4658)
    
        Controls are sized with 0 pixel gap, which is OK in general but show an overlap upon selection.
        Add the extra 1 pixel gap.
    
    commit f8c28b977363444ad31124a6794347c53532b43f
    Author: Kyle Katarn <112266950+KRosUser@users.noreply.github.com>
    Date:   Sat Sep 3 17:27:50 2022 +0200
    
        [TASKMGR] Fix UI on SwitchTo for multiple application (#4652)
    
    commit 6b2eeb0a298a89158eb2fa6dd66b8445088f0830
    Author: Kyle Katarn <112266950+KRosUser@users.noreply.github.com>
    Date:   Sat Sep 3 17:22:14 2022 +0200
    
        [CONSOLE.CPL] Use translated resource rather than hardcoded text (#4647)
    
    commit f07741e6316f0ae677c11e1db008b8c84e6cfe5b
    Author: Kyle Katarn <112266950+KRosUser@users.noreply.github.com>
    Date:   Sat Sep 3 17:19:41 2022 +0200
    
        [USRMGR] Replace old (leftover) debug MessageBox with Wine trace macros (#4640)
    
        Remove leftover of debug code. CORE-18276

--- dll/cpl/usrmgr/userprops.c
+++ dll/cpl/usrmgr/userprops.c
@@ -167,7 +167,7 @@ SetUserProfileData(HWND hwndDlg,
     status = NetUserSetInfo(NULL, pUserData->szUserName, 3, (LPBYTE)pUserInfo, &dwIndex);
     if (status != NERR_Success)
     {
-        DebugPrintf(_T("Status: %lu  Index: %lu"), status, dwIndex);
+        ERR("NetUserSetInfo failed. Status: %lu  Index: %lu", status, dwIndex);
     }
 
     if (pszProfilePath)
@@ -476,7 +476,7 @@ AddSelectedGroupsToUser(HWND hwndDlg,
                                                  (LPBYTE)&memberInfo, 1);
                 if (status == NERR_Success)
                 {
-                    DebugPrintf(_TEXT("Selected group: %s"), szGroupName);
+                    TRACE("Selected group: %s", dbgstrx(szGroupName));
                     bResult = TRUE;
                 }
                 else
@@ -784,7 +784,7 @@ SetUserGeneralData(HWND hwndDlg,
     status = NetUserSetInfo(NULL, pUserData->szUserName, 3, (LPBYTE)pUserInfo, &dwIndex);
     if (status != NERR_Success)
     {
-        DebugPrintf(_T("Status: %lu  Index: %lu"), status, dwIndex);
+        ERR("NetUserSetInfo failed. Status: %lu  Index: %lu", status, dwIndex);
     }
 
     if (pszFullName)
