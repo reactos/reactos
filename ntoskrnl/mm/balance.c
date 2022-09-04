e118dddfbe30a9e56603e18161d8f1a078564f14
Kyle Katarn <contact@kcsoftwares.com>
2022-09-04 11:11:25 +0200

    Fix static inline

--- ntoskrnl/mm/balance.c
+++ ntoskrnl/mm/balance.c
@@ -38,23 +38,6 @@ static LONG PageOutThreadActive;
 
 /* FUNCTIONS ****************************************************************/
 
-static inline VOID UpdateTotalCommittedPages(VOID)
-{
-    /*
-     *   Add up all the used "Committed" memory + pagefile.
-     *   Not sure this is right. 8^\
-     *   MmTotalCommittedPages should be adjusted consistently with
-    *    other counters at different places.
-     */
-
-    MmTotalCommittedPages = MiMemoryConsumers[MC_SYSTEM].PagesUsed +
-                            MiMemoryConsumers[MC_USER].PagesUsed +
-                            MiUsedSwapPages;
-
-    if (MmTotalCommittedPages > MmPeakCommitment)
-        MmPeakCommitment = MmTotalCommittedPages;
-}
-
 CODE_SEG("INIT")
 VOID
 NTAPI
