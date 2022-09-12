51dd0523c7255806f2f433beea4f2d8cea6dce3f
Hervé Poussineau <hpoussin@reactos.org>
2022-09-12 08:13:17 +0200

    [SHELL32] Fill link path before initializing the dialog
    This fixes the Target type, Target location and Target fields
    in shortcut properties window.
    
    CORE-15229

--- dll/win32/shell32/CShellLink.cpp
+++ dll/win32/shell32/CShellLink.cpp
@@ -2735,7 +2735,12 @@ LPWSTR SH_GetTargetTypeByPath(LPCWSTR lpcwFullPath)
 
 BOOL CShellLink::OnInitDialog(HWND hwndDlg, HWND hwndFocus, LPARAM lParam)
 {
+    WCHAR buffer[MAX_PATH];
+
     TRACE("CShellLink::OnInitDialog(hwnd %p hwndFocus %p lParam %p)\n", hwndDlg, hwndFocus, lParam);
+
+    if (m_pPidl && SHGetPathFromIDListW(m_pPidl, buffer))
+        m_sPath = buffer;
 
     TRACE("m_sArgs: %S sComponent: %S m_sDescription: %S m_sIcoPath: %S m_sPath: %S m_sPathRel: %S sProduct: %S m_sWorkDir: %S\n", m_sArgs, sComponent, m_sDescription,
           m_sIcoPath, m_sPath, m_sPathRel, sProduct, m_sWorkDir);
