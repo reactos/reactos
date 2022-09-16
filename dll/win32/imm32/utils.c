db00a7522757ae4e5a084611528d42c076337921
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-09-16 17:35:05 +0900

    [IMM32] Don't allow invalid 'IME File' values
    Improve security. CORE-11700

--- dll/win32/imm32/utils.c
+++ dll/win32/imm32/utils.c
@@ -908,7 +908,8 @@ UINT APIENTRY Imm32GetImeLayout(PREG_IME pLayouts, UINT cLayouts)
 
         RegCloseKey(hkeyIME);
 
-        if (!szImeFileName[0])
+        /* We don't allow the invalid "IME File" values for security reason */
+        if (!szImeFileName[0] || wcschr(szImeFileName, L'\\') != NULL)
             break;
 
         Imm32StrToUInt(szImeKey, &Value, 16);
