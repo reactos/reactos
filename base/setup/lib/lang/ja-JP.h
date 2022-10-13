26408144a16c13c81d4971052060ddfc89cb69d8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-10-14 07:17:24 +0900

    [INF][INTL.INF][SETUPLIB] We don't have preinstalled IMEs (#4774)
    Don't write the preinstalled IME keyboard layouts in Chinese, Japanese and Korean (CJK). CORE-18506

--- base/setup/lib/lang/ja-JP.h
+++ base/setup/lib/lang/ja-JP.h
@@ -2,7 +2,7 @@
 
 MUI_LAYOUTS jaJPLayouts[] =
 {
-//    { L"0411", L"e0010411" },
+//    { L"0411", L"E0010411" }, // We don't have preinstalled IMEs.
     { L"0411", L"00000411" },
     { NULL, NULL }
 };
