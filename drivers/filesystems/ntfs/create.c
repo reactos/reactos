reactos/reactos

commit 5579428b4f1361481dae3ea0b901c0a915401148

[NTFS] - Add some utility functions and improve some comments. Improve NtfsAddFilenameToDirectory(). +PrintAllVCNs() - Diagnostic function which prints VCN of every node in an index allocation. +GetAllocationOffsetFromVCN() - Calculates location of an index buffer from the node's VCN. +GetInfoClassName() - Gets a string representation of an info class enumeration, to speed up development of unimplemented classes. -NtfsSetInformation() & NtfsQueryInformation() - Use GetInfoClassName to report unhandled information classes. -CompareTreeKeys() - Add a comment and clarify some comments. -NtfsAddFilenameToDirectory() - Don't try to update the size of Index Root on disk if the attribute length hasn't changed.
svn path=/branches/GSoC_2016/NTFS/; revision=75424

drivers/filesystems/ntfs/finfo.c
@@ -289,6 +289,128 @@ NtfsGetSteamInformation(PNTFS_FCB Fcb,
     return Status;
 }
 
+// Convert enum value to friendly name
+const PCSTR
+GetInfoClassName(FILE_INFORMATION_CLASS infoClass)
+{
+    const PCSTR fileInfoClassNames[] = { "???????",
+        "FileDirectoryInformation",
+        "FileFullDirectoryInformation",
+        "FileBothDirectoryInformation",
+        "FileBasicInformation",
+        "FileStandardInformation",
+        "FileInternalInformation",
+        "FileEaInformation",
+        "FileAccessInformation",
+        "FileNameInformation",
+        "FileRenameInformation",
+        "FileLinkInformation",
+        "FileNamesInformation",
+        "FileDispositionInformation",
+        "FilePositionInformation",
+        "FileFullEaInformation",
+        "FileModeInformation",
+        "FileAlignmentInformation",
+        "FileAllInformation",
+        "FileAllocationInformation",
+        "FileEndOfFileInformation",
+        "FileAlternateNameInformation",
+        "FileStreamInformation",
+        "FilePipeInformation",
+        "FilePipeLocalInformation",
+        "FilePipeRemoteInformation",
+        "FileMailslotQueryInformation",
+        "FileMailslotSetInformation",
+        "FileCompressionInformation",
+        "FileObjectIdInformation",
+        "FileCompletionInformation",
+        "FileMoveClusterInformation",
+        "FileQuotaInformation",
+        "FileReparsePointInformation",
+        "FileNetworkOpenInformation",
+        "FileAttributeTagInformation",
+        "FileTrackingInformation",
+        "FileIdBothDirectoryInformation",
+        "FileIdFullDirectoryInformation",
+        "FileValidDataLengthInformation",
+        "FileShortNameInformation",
+        "FileIoCompletionNotificationInformation",
+        "FileIoStatusBlockRangeInformation",
+        "FileIoPriorityHintInformation",
+        "FileSfioReserveInformation",
+        "FileSfioVolumeInformation",
+        "FileHardLinkInformation",
+        "FileProcessIdsUsingFileInformation",
+        "FileNormalizedNameInformation",
+        "FileNetworkPhysicalNameInformation",
+        "FileIdGlobalTxDirectoryInformation",
+        "FileIsRemoteDeviceInformation",
+        "FileAttributeCacheInformation",
+        "FileNumaNodeInformation",
+        "FileStandardLinkInformation",
+        "FileRemoteProtocolInformation",
+        "FileReplaceCompletionInformation",
+        "FileMaximumInformation",
+        "FileDirectoryInformation",
+        "FileFullDirectoryInformation",
+        "FileBothDirectoryInformation",
+        "FileBasicInformation",
+        "FileStandardInformation",
+        "FileInternalInformation",
+        "FileEaInformation",
+        "FileAccessInformation",
+        "FileNameInformation",
+        "FileRenameInformation",
+        "FileLinkInformation",
+        "FileNamesInformation",
+        "FileDispositionInformation",
+        "FilePositionInformation",
+        "FileFullEaInformation",
+        "FileModeInformation",
+        "FileAlignmentInformation",
+        "FileAllInformation",
+        "FileAllocationInformation",
+        "FileEndOfFileInformation",
+        "FileAlternateNameInformation",
+        "FileStreamInformation",
+        "FilePipeInformation",
+        "FilePipeLocalInformation",
+        "FilePipeRemoteInformation",
+        "FileMailslotQueryInformation",
+        "FileMailslotSetInformation",
+        "FileCompressionInformation",
+        "FileObjectIdInformation",
+        "FileCompletionInformation",
+        "FileMoveClusterInformation",
+        "FileQuotaInformation",
+        "FileReparsePointInformation",
+        "FileNetworkOpenInformation",
+        "FileAttributeTagInformation",
+        "FileTrackingInformation",
+        "FileIdBothDirectoryInformation",
+        "FileIdFullDirectoryInformation",
+        "FileValidDataLengthInformation",
+        "FileShortNameInformation",
+        "FileIoCompletionNotificationInformation",
+        "FileIoStatusBlockRangeInformation",
+        "FileIoPriorityHintInformation",
+        "FileSfioReserveInformation",
+        "FileSfioVolumeInformation",
+        "FileHardLinkInformation",
+        "FileProcessIdsUsingFileInformation",
+        "FileNormalizedNameInformation",
+        "FileNetworkPhysicalNameInformation",
+        "FileIdGlobalTxDirectoryInformation",
+        "FileIsRemoteDeviceInformation",
+        "FileAttributeCacheInformation",
+        "FileNumaNodeInformation",
+        "FileStandardLinkInformation",
+        "FileRemoteProtocolInformation",
+        "FileReplaceCompletionInformation",
+        "FileMaximumInformation" };
+    return fileInfoClassNames[infoClass];
+}
+
 /*
  * FUNCTION: Retrieve the specified file information
  */
@@ -376,12 +498,12 @@ NtfsQueryInformation(PNTFS_IRP_CONTEXT IrpContext)
 
         case FileAlternateNameInformation:
         case FileAllInformation:
-            DPRINT1("Unimplemented information class %u\n", FileInformationClass);
+            DPRINT1("Unimplemented information class: %s\n", GetInfoClassName(FileInformationClass));
             Status = STATUS_NOT_IMPLEMENTED;
             break;
 
         default:
-            DPRINT1("Unimplemented information class %u\n", FileInformationClass);
+            DPRINT1("Unimplemented information class: %s\n", GetInfoClassName(FileInformationClass));
             Status = STATUS_INVALID_PARAMETER;
     }
 
@@ -645,7 +767,7 @@ NtfsSetInformation(PNTFS_IRP_CONTEXT IrpContext)
         // TODO: all other information classes
 
         default:
-            DPRINT1("FIXME: Unimplemented information class %u\n", FileInformationClass);
+            DPRINT1("FIXME: Unimplemented information class: %s\n", GetInfoClassName(FileInformationClass));
             Status = STATUS_NOT_IMPLEMENTED;
     }
 

drivers/filesystems/ntfs/btree.c
@@ -32,6 +32,47 @@
 
 /* FUNCTIONS ****************************************************************/
 
+// TEMP FUNCTION for diagnostic purposes.
+// Prints VCN of every node in an index allocation
+VOID
+PrintAllVCNs(PDEVICE_EXTENSION Vcb,
+             PNTFS_ATTR_CONTEXT IndexAllocationContext,
+             ULONG NodeSize)
+{
+    ULONGLONG CurrentOffset = 0;
+    PINDEX_BUFFER CurrentNode, Buffer;
+    ULONGLONG BufferSize = AttributeDataLength(&IndexAllocationContext->Record);
+    ULONGLONG i;
+    int Count = 0;
+
+    Buffer = ExAllocatePoolWithTag(NonPagedPool, BufferSize, TAG_NTFS);
+
+    ULONG BytesRead = ReadAttribute(Vcb, IndexAllocationContext, 0, (PCHAR)Buffer, BufferSize);
+
+    ASSERT(BytesRead = BufferSize);
+
+    CurrentNode = Buffer;
+
+    // loop through all the nodes
+    for (i = 0; i < BufferSize; i += NodeSize)
+    {
+        NTSTATUS Status = FixupUpdateSequenceArray(Vcb, &CurrentNode->Ntfs);
+        if (!NT_SUCCESS(Status))
+        {
+            DPRINT1("ERROR: Fixing fixup failed!\n");
+            continue;
+        }
+
+        DPRINT1("Node #%d, VCN: %I64u\n", Count, CurrentNode->VCN);
+
+        CurrentNode = (PINDEX_BUFFER)((ULONG_PTR)CurrentNode + NodeSize);
+        CurrentOffset += NodeSize;
+        Count++;
+    }
+
+    ExFreePoolWithTag(Buffer, TAG_NTFS);
+}
+
 /**
 * @name CompareTreeKeys
 * @implemented
@@ -62,6 +103,7 @@ CompareTreeKeys(PB_TREE_KEY Key1, PB_TREE_KEY Key2, BOOLEAN CaseSensitive)
     UNICODE_STRING Key1Name, Key2Name;
     LONG Comparison;
 
+    // Key1 must not be the final key (AKA the dummy key)
     ASSERT(!(Key1->IndexEntry->Flags & NTFS_INDEX_ENTRY_END));
 
     // If Key2 is the "dummy key", key 1 will always come first
@@ -89,7 +131,7 @@ CompareTreeKeys(PB_TREE_KEY Key1, PB_TREE_KEY Key2, BOOLEAN CaseSensitive)
         // Compare the names of the same length
         Comparison = RtlCompareUnicodeString(&Key1Name, &Key2Name, !CaseSensitive);
 
-        // If the truncated files are the same length, the shorter one comes first
+        // If the truncated names are the same length, the shorter one comes first
         if (Comparison == 0)
             return -1;
     }
@@ -102,7 +144,7 @@ CompareTreeKeys(PB_TREE_KEY Key1, PB_TREE_KEY Key2, BOOLEAN CaseSensitive)
         // Compare the names of the same length
         Comparison = RtlCompareUnicodeString(&Key1Name, &Key2Name, !CaseSensitive);
 
-        // If the truncated files are the same length, the shorter one comes first
+        // If the truncated names are the same length, the shorter one comes first
         if (Comparison == 0)
             return 1;
     }

drivers/filesystems/ntfs/mft.c
@@ -2018,25 +2018,29 @@ NtfsAddFilenameToDirectory(PDEVICE_EXTENSION DeviceExt,
     // we must create an index allocation and index bitmap (TODO). Also TODO: support file records with
     // $ATTRIBUTE_LIST's.
     AttributeLength = NewIndexRoot->Header.AllocatedSize + FIELD_OFFSET(INDEX_ROOT_ATTRIBUTE, Header);
-    DestinationAttribute = (PNTFS_ATTR_RECORD)((ULONG_PTR)ParentFileRecord + IndexRootOffset);
-
-    // Find the attribute (or attribute-end marker) after the index root
-    NextAttribute = (PNTFS_ATTR_RECORD)((ULONG_PTR)DestinationAttribute + DestinationAttribute->Length);
-    if (NextAttribute->Type != AttributeEnd)
+    
+    if (AttributeLength != IndexRootContext->Record.Resident.ValueLength)
     {
-        DPRINT1("FIXME: For now, only resizing index root at the end of a file record is supported!\n");
-        ExFreePoolWithTag(NewIndexRoot, TAG_NTFS);
-        ReleaseAttributeContext(IndexRootContext);
-        ExFreePoolWithTag(I30IndexRoot, TAG_NTFS);
-        ExFreePoolWithTag(ParentFileRecord, TAG_NTFS);
-        return STATUS_NOT_IMPLEMENTED;
-    }
+        DestinationAttribute = (PNTFS_ATTR_RECORD)((ULONG_PTR)ParentFileRecord + IndexRootOffset);
 
-    // Update the length of the attribute in the file record of the parent directory
-    InternalSetResidentAttributeLength(IndexRootContext,
-                                       ParentFileRecord,
-                                       IndexRootOffset,
-                                       AttributeLength);
+        // Find the attribute (or attribute-end marker) after the index root
+        NextAttribute = (PNTFS_ATTR_RECORD)((ULONG_PTR)DestinationAttribute + DestinationAttribute->Length);
+        if (NextAttribute->Type != AttributeEnd)
+        {
+            DPRINT1("FIXME: For now, only resizing index root at the end of a file record is supported!\n");
+            ExFreePoolWithTag(NewIndexRoot, TAG_NTFS);
+            ReleaseAttributeContext(IndexRootContext);
+            ExFreePoolWithTag(I30IndexRoot, TAG_NTFS);
+            ExFreePoolWithTag(ParentFileRecord, TAG_NTFS);
+            return STATUS_NOT_IMPLEMENTED;
+        }
+
+        // Update the length of the attribute in the file record of the parent directory
+        InternalSetResidentAttributeLength(IndexRootContext,
+                                           ParentFileRecord,
+                                           IndexRootOffset,
+                                           AttributeLength);
+    }
 
     NT_ASSERT(ParentFileRecord->BytesInUse <= DeviceExt->NtfsInfo.BytesPerFileRecord);
 

drivers/filesystems/ntfs/ntfs.h
@@ -208,6 +208,9 @@ typedef enum
 #define INDEX_ROOT_SMALL 0x0
 #define INDEX_ROOT_LARGE 0x1
 
+#define INDEX_NODE_SMALL 0x0
+#define INDEX_NODE_LARGE 0x1
+
 #define NTFS_INDEX_ENTRY_NODE            1
 #define NTFS_INDEX_ENTRY_END            2
 
@@ -701,7 +704,20 @@ VOID
 DestroyBTree(PB_TREE Tree);
 
 VOID
+DestroyBTreeNode(PB_TREE_FILENAME_NODE Node);
+
+VOID
 DumpBTree(PB_TREE Tree);
+
+VOID
+DumpBTreeNode(PB_TREE_FILENAME_NODE Node,
+              ULONG Number,
+              ULONG Depth);
+
+ULONGLONG
+GetAllocationOffsetFromVCN(PDEVICE_EXTENSION DeviceExt,
+                           ULONG IndexBufferSize,
+                           ULONGLONG Vcn);
 
 NTSTATUS
 NtfsInsertKey(ULONGLONG FileReference,

commit 677de9af0482b4ae596749bb345874540da9df5d

[0.4.7][NTFS] DPRINT1->DPRINT, EOL-WS
First part with the typo in the assert was introduced by 5579428b4f1361481dae3ea0b901c0a915401148
which was merged into ros by a merge-task during 0.4.8-dev'ing.
releases/0.4.7 didn't have that code yet.
fix picked from 0.4.15-dev-5035-g f308c6a2bc9cc8678385a55fab6956867595d71d
I still mention 'PrintAllVCNs(): Fix a typo in an ASSERT() (#4682)' here to keep my commits
grouped together in that PR.
----------
[NTFS] Do not print debug message for every function call by default
That 2nd part was picked from 0.4.13-dev-450-g 155243494059037a5cccd3b1c57f0b77ca897c95
----------
And for all the files that I had to touch for the 2nd part in the older branches,
I ported back also some EOL-whitespace-fixes within like they were done during 0.4.15-dev'ing.

drivers/filesystems/ntfs/create.c
@@ -111,7 +111,7 @@ NtfsMoonWalkID(PDEVICE_EXTENSION DeviceExt,
     WCHAR FullPath[MAX_PATH];
     ULONG WritePosition = MAX_PATH - 1;
 
-    DPRINT1("NtfsMoonWalkID(%p, %I64x, %p)\n", DeviceExt, Id, OutPath);
+    DPRINT("NtfsMoonWalkID(%p, %I64x, %p)\n", DeviceExt, Id, OutPath);
 
     RtlZeroMemory(FullPath, sizeof(FullPath));
     MftRecord = ExAllocatePoolWithTag(NonPagedPool,
@@ -183,7 +183,7 @@ NtfsOpenFileById(PDEVICE_EXTENSION DeviceExt,
     PNTFS_FCB FCB;
     PFILE_RECORD_HEADER MftRecord;
 
-    DPRINT1("NtfsOpenFileById(%p, %p, %I64x, %p)\n", DeviceExt, FileObject, MftId, FoundFCB);
+    DPRINT("NtfsOpenFileById(%p, %p, %I64x, %p)\n", DeviceExt, FileObject, MftId, FoundFCB);
 
     ASSERT(MftId < 0x10);
     if (MftId > 0xb) /* No entries are used yet beyond this */
@@ -253,7 +253,7 @@ NtfsOpenFile(PDEVICE_EXTENSION DeviceExt,
     NTSTATUS Status;
     PWSTR AbsFileName = NULL;
 
-    DPRINT1("NtfsOpenFile(%p, %p, %S, %p)\n", DeviceExt, FileObject, FileName, FoundFCB);
+    DPRINT("NtfsOpenFile(%p, %p, %S, %p)\n", DeviceExt, FileObject, FileName, FoundFCB);
 
     *FoundFCB = NULL;
 
@@ -292,7 +292,7 @@ NtfsOpenFile(PDEVICE_EXTENSION DeviceExt,
                            ParentFcb);
         }
 
-        if (!NT_SUCCESS (Status))
+        if (!NT_SUCCESS(Status))
         {
             DPRINT("Could not make a new FCB, status: %x\n", Status);
 
@@ -335,11 +335,11 @@ NtfsCreateFile(PDEVICE_OBJECT DeviceObject,
     NTSTATUS Status;
     UNICODE_STRING FullPath;
 
-    DPRINT1("NtfsCreateFile(%p, %p) called\n", DeviceObject, Irp);
+    DPRINT("NtfsCreateFile(%p, %p) called\n", DeviceObject, Irp);
 
     DeviceExt = DeviceObject->DeviceExtension;
     ASSERT(DeviceExt);
-    Stack = IoGetCurrentIrpStackLocation (Irp);
+    Stack = IoGetCurrentIrpStackLocation(Irp);
     ASSERT(Stack);
 
     RequestedDisposition = ((Stack->Parameters.Create.Options >> 24) & 0xff);

drivers/filesystems/ntfs/dirctl.c
@@ -279,7 +279,7 @@ NtfsQueryDirectory(PNTFS_IRP_CONTEXT IrpContext)
     ULONGLONG MFTRecord, OldMFTRecord = 0;
     UNICODE_STRING Pattern;
 
-    DPRINT1("NtfsQueryDirectory() called\n");
+    DPRINT("NtfsQueryDirectory() called\n");
 
     ASSERT(IrpContext);
     Irp = IrpContext->Irp;
@@ -493,7 +493,7 @@ NtfsDirectoryControl(PNTFS_IRP_CONTEXT IrpContext)
 {
     NTSTATUS Status = STATUS_UNSUCCESSFUL;
 
-    DPRINT1("NtfsDirectoryControl() called\n");
+    DPRINT("NtfsDirectoryControl() called\n");
 
     switch (IrpContext->MinorFunction)
     {

drivers/filesystems/ntfs/fcb.c
@@ -140,7 +140,7 @@ NtfsFCBIsReparsePoint(PNTFS_FCB Fcb)
 BOOLEAN
 NtfsFCBIsCompressed(PNTFS_FCB Fcb)
 {
-    return ((Fcb->Entry.FileAttributes & NTFS_FILE_TYPE_COMPRESSED) == NTFS_FILE_TYPE_COMPRESSED); 
+    return ((Fcb->Entry.FileAttributes & NTFS_FILE_TYPE_COMPRESSED) == NTFS_FILE_TYPE_COMPRESSED);
 }
 
 BOOLEAN
@@ -391,7 +391,7 @@ NtfsMakeFCBFromDirEntry(PNTFS_VCB Vcb,
     PNTFS_FCB rcFCB;
     ULONGLONG Size, AllocatedSize;
 
-    DPRINT1("NtfsMakeFCBFromDirEntry(%p, %p, %wZ, %p, %p, %p)\n", Vcb, DirectoryFCB, Name, Stream, Record, fileFCB);
+    DPRINT("NtfsMakeFCBFromDirEntry(%p, %p, %wZ, %p, %p, %p)\n", Vcb, DirectoryFCB, Name, Stream, Record, fileFCB);
 
     FileName = GetBestFileNameFromRecord(Vcb, Record);
     if (!FileName)
@@ -517,7 +517,7 @@ NtfsDirFindFile(PNTFS_VCB Vcb,
     PNTFS_ATTR_CONTEXT DataContext;
     USHORT Length = 0;
 
-    DPRINT1("NtfsDirFindFile(%p, %p, %S, %p)\n", Vcb, DirectoryFcb, FileToFind, FoundFCB);
+    DPRINT("NtfsDirFindFile(%p, %p, %S, %p)\n", Vcb, DirectoryFcb, FileToFind, FoundFCB);
 
     *FoundFCB = NULL;
     RtlInitUnicodeString(&File, FileToFind);
@@ -609,7 +609,7 @@ NtfsGetFCBForFile(PNTFS_VCB Vcb,
 
 #if 1
     /* Trivial case, open of the root directory on volume */
-    if (pFileName [0] == L'\0' || wcscmp(pFileName, L"\\") == 0)
+    if (pFileName[0] == L'\0' || wcscmp(pFileName, L"\\") == 0)
     {
         DPRINT("returning root FCB\n");
 
@@ -716,7 +716,7 @@ NtfsGetFCBForFile(PNTFS_VCB Vcb,
 NTSTATUS
 NtfsReadFCBAttribute(PNTFS_VCB Vcb,
                      PNTFS_FCB pFCB,
-                     ULONG Type, 
+                     ULONG Type,
                      PCWSTR Name,
                      ULONG NameLength,
                      PVOID * Data)

drivers/filesystems/ntfs/finfo.c
@@ -46,7 +46,7 @@ NtfsGetStandardInformation(PNTFS_FCB Fcb,
 {
     UNREFERENCED_PARAMETER(DeviceObject);
 
-    DPRINT1("NtfsGetStandardInformation(%p, %p, %p, %p)\n", Fcb, DeviceObject, StandardInfo, BufferLength);
+    DPRINT("NtfsGetStandardInformation(%p, %p, %p, %p)\n", Fcb, DeviceObject, StandardInfo, BufferLength);
 
     if (*BufferLength < sizeof(FILE_STANDARD_INFORMATION))
         return STATUS_BUFFER_TOO_SMALL;
@@ -102,7 +102,7 @@ NtfsGetBasicInformation(PFILE_OBJECT FileObject,
 {
     PFILENAME_ATTRIBUTE FileName = &Fcb->Entry;
 
-    DPRINT1("NtfsGetBasicInformation(%p, %p, %p, %p, %p)\n", FileObject, Fcb, DeviceObject, BasicInfo, BufferLength);
+    DPRINT("NtfsGetBasicInformation(%p, %p, %p, %p, %p)\n", FileObject, Fcb, DeviceObject, BasicInfo, BufferLength);
 
     if (*BufferLength < sizeof(FILE_BASIC_INFORMATION))
         return STATUS_BUFFER_TOO_SMALL;
@@ -136,7 +136,7 @@ NtfsGetNameInformation(PFILE_OBJECT FileObject,
     UNREFERENCED_PARAMETER(FileObject);
     UNREFERENCED_PARAMETER(DeviceObject);
 
-    DPRINT1("NtfsGetNameInformation(%p, %p, %p, %p, %p)\n", FileObject, Fcb, DeviceObject, NameInfo, BufferLength);
+    DPRINT("NtfsGetNameInformation(%p, %p, %p, %p, %p)\n", FileObject, Fcb, DeviceObject, NameInfo, BufferLength);
 
     ASSERT(NameInfo != NULL);
     ASSERT(Fcb != NULL);
@@ -200,7 +200,7 @@ NtfsGetNetworkOpenInformation(PNTFS_FCB Fcb,
 {
     PFILENAME_ATTRIBUTE FileName = &Fcb->Entry;
 
-    DPRINT1("NtfsGetNetworkOpenInformation(%p, %p, %p, %p)\n", Fcb, DeviceExt, NetworkInfo, BufferLength);
+    DPRINT("NtfsGetNetworkOpenInformation(%p, %p, %p, %p)\n", Fcb, DeviceExt, NetworkInfo, BufferLength);
 
     if (*BufferLength < sizeof(FILE_NETWORK_OPEN_INFORMATION))
         return STATUS_BUFFER_TOO_SMALL;

drivers/filesystems/ntfs/fsctl.c
@@ -22,7 +22,7 @@
  * PURPOSE:          NTFS filesystem driver
  * PROGRAMMER:       Eric Kohl
  *                   Valentin Verkhovsky
- *                   Pierre Schweitzer 
+ *                   Pierre Schweitzer
  */
 
 /* INCLUDES *****************************************************************/
@@ -50,7 +50,7 @@ NtfsHasFileSystem(PDEVICE_OBJECT DeviceToMount)
     PBOOT_SECTOR BootSector;
     NTSTATUS Status;
 
-    DPRINT1("NtfsHasFileSystem() called\n");
+    DPRINT("NtfsHasFileSystem() called\n");
 
     Size = sizeof(DISK_GEOMETRY);
     Status = NtfsDeviceIoControl(DeviceToMount,
@@ -147,7 +147,7 @@ NtfsHasFileSystem(PDEVICE_OBJECT DeviceToMount)
 
     /* Check cluster size */
     ClusterSize = BootSector->BPB.BytesPerSector * BootSector->BPB.SectorsPerCluster;
-    if (ClusterSize != 512 && ClusterSize != 1024 && 
+    if (ClusterSize != 512 && ClusterSize != 1024 &&
         ClusterSize != 2048 && ClusterSize != 4096 &&
         ClusterSize != 8192 && ClusterSize != 16384 &&
         ClusterSize != 32768 && ClusterSize != 65536)
@@ -413,7 +413,7 @@ NtfsMountVolume(PDEVICE_OBJECT DeviceObject,
     PNTFS_VCB Vcb = NULL;
     NTSTATUS Status;
 
-    DPRINT1("NtfsMountVolume() called\n");
+    DPRINT("NtfsMountVolume() called\n");
 
     if (DeviceObject != NtfsGlobalData->DeviceObject)
     {
@@ -722,7 +722,7 @@ GetVolumeBitmap(PDEVICE_EXTENSION DeviceExt,
     ULONGLONG ToCopy;
     BOOLEAN Overflow = FALSE;
 
-    DPRINT1("GetVolumeBitmap(%p, %p)\n", DeviceExt, Irp);
+    DPRINT("GetVolumeBitmap(%p, %p)\n", DeviceExt, Irp);
 
     Stack = IoGetCurrentIrpStackLocation(Irp);
 
@@ -891,7 +891,7 @@ NtfsUserFsRequest(PDEVICE_OBJECT DeviceObject,
     PIO_STACK_LOCATION Stack;
     PDEVICE_EXTENSION DeviceExt;
 
-    DPRINT1("NtfsUserFsRequest(%p, %p)\n", DeviceObject, Irp);
+    DPRINT("NtfsUserFsRequest(%p, %p)\n", DeviceObject, Irp);
 
     Stack = IoGetCurrentIrpStackLocation(Irp);
     DeviceExt = DeviceObject->DeviceExtension;
@@ -953,7 +953,7 @@ NtfsFileSystemControl(PNTFS_IRP_CONTEXT IrpContext)
     PIRP Irp;
     PDEVICE_OBJECT DeviceObject;
 
-    DPRINT1("NtfsFileSystemControl() called\n");
+    DPRINT("NtfsFileSystemControl() called\n");
 
     DeviceObject = IrpContext->DeviceObject;
     Irp = IrpContext->Irp;

drivers/filesystems/ntfs/rw.c
@@ -59,7 +59,7 @@ NtfsReadFile(PDEVICE_EXTENSION DeviceExt,
     PCHAR ReadBuffer = (PCHAR)Buffer;
     ULONGLONG StreamSize;
 
-    DPRINT1("NtfsReadFile(%p, %p, %p, %u, %u, %x, %p)\n", DeviceExt, FileObject, Buffer, Length, ReadOffset, IrpFlags, LengthRead);
+    DPRINT("NtfsReadFile(%p, %p, %p, %u, %u, %x, %p)\n", DeviceExt, FileObject, Buffer, Length, ReadOffset, IrpFlags, LengthRead);
 
     *LengthRead = 0;
 
@@ -164,7 +164,7 @@ NtfsReadFile(PDEVICE_EXTENSION DeviceExt,
         AllocatedBuffer = TRUE;
     }
 
-    DPRINT1("Effective read: %lu at %lu for stream '%S'\n", RealLength, RealReadOffset, Fcb->Stream);
+    DPRINT("Effective read: %lu at %lu for stream '%S'\n", RealLength, RealReadOffset, Fcb->Stream);
     RealLengthRead = ReadAttribute(DeviceExt, DataContext, RealReadOffset, (PCHAR)ReadBuffer, RealLength);
     if (RealLengthRead == 0)
     {
@@ -183,7 +183,7 @@ NtfsReadFile(PDEVICE_EXTENSION DeviceExt,
 
     *LengthRead = ToRead;
 
-    DPRINT1("%lu got read\n", *LengthRead);
+    DPRINT("%lu got read\n", *LengthRead);
 
     if (AllocatedBuffer)
     {

drivers/filesystems/ntfs/volinfo.c
@@ -45,7 +45,7 @@ NtfsGetFreeClusters(PDEVICE_EXTENSION DeviceExt)
     ULONG Read = 0;
     RTL_BITMAP Bitmap;
 
-    DPRINT1("NtfsGetFreeClusters(%p)\n", DeviceExt);
+    DPRINT("NtfsGetFreeClusters(%p)\n", DeviceExt);
 
     BitmapRecord = ExAllocatePoolWithTag(NonPagedPool,
                                          DeviceExt->NtfsInfo.BytesPerFileRecord,