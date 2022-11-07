59f0ed43b96c7af7eb4abedae8e6024c87779dc1
Stanislav Motylkov <x86corez@gmail.com>
2022-11-07 20:30:02 +0300

    [USER32] Coding style fixes only

--- win32ss/user/user32/windows/text.c
+++ win32ss/user/user32/windows/text.c
@@ -1,28 +1,8 @@
 /*
- *  ReactOS kernel
- *  Copyright (C) 1998, 1999, 2000, 2001 ReactOS Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-/*
- * PROJECT:         ReactOS user32.dll
- * FILE:            win32ss/user/user32/windows/text.c
- * PURPOSE:         Input
- * PROGRAMMER:      Casper S. Hornstrup (chorns@users.sourceforge.net)
- * UPDATE HISTORY:
- *      09-05-2001  CSH  Created
+ * PROJECT:     ReactOS user32.dll
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Text processing functions
+ * COPYRIGHT:   Copyright 2001-2004 Casper S. Hornstrup <chorns@users.sourceforge.net>
  */
 
 #include <user32.h>
@@ -122,7 +102,7 @@ LPWSTR
 WINAPI
 CharPrevW(LPCWSTR start, LPCWSTR x)
 {
-    if (x > start) return (LPWSTR)(x-1);
+    if (x > start) return (LPWSTR)(x - 1);
     else return (LPWSTR)x;
 }
 
@@ -168,7 +148,8 @@ LPSTR
 WINAPI
 CharPrevA(LPCSTR start, LPCSTR ptr)
 {
-    while (*start && (start < ptr)) {
+    while (*start && (start < ptr))
+    {
         LPCSTR next = CharNextA(start);
         if (next >= ptr) break;
         start = next;
@@ -179,11 +160,13 @@ CharPrevA(LPCSTR start, LPCSTR ptr)
 /*
  * @implemented
  */
-LPSTR WINAPI CharPrevExA( WORD codepage, LPCSTR start, LPCSTR ptr, DWORD flags )
+LPSTR
+WINAPI
+CharPrevExA(WORD codepage, LPCSTR start, LPCSTR ptr, DWORD flags)
 {
     while (*start && (start < ptr))
     {
-        LPCSTR next = CharNextExA( codepage, start, flags );
+        LPCSTR next = CharNextExA(codepage, start, flags);
         if (next > ptr) break;
         start = next;
     }
