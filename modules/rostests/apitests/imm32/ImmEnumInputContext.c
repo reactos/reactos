328caf1b689a8c57cdea9c26c2464044dae4a0cb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-11-13 17:25:01 +0900

    fix copyright text

--- modules/rostests/apitests/imm32/ImmEnumInputContext.c
+++ modules/rostests/apitests/imm32/ImmEnumInputContext.c
@@ -2,7 +2,7 @@
  * PROJECT:     ReactOS api tests
  * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
  * PURPOSE:     Test for ImmEnumInputContext
- * COPYRIGHT:   Copyright 2021 Katayama Hirofumi MZ (katayama.hirofumi.mz@gmail.com)
+ * COPYRIGHT:   Copyright 2022 Katayama Hirofumi MZ (katayama.hirofumi.mz@gmail.com)
  */
 
 #include "precomp.h"
