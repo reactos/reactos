d3a9245593f3b9acd55eed22ff9b6b4ce569c200
Rexx J. Larsson <joellarssonpriv@gmail.com>
2022-12-30 21:54:06 +0100

    added ifndef

--- dll/win32/msi/dialog.c
+++ dll/win32/msi/dialog.c
@@ -3160,7 +3160,11 @@ static void msi_dialog_vcl_add_columns( msi_dialog *dialog, msi_control *control
          */
         if ( !wcsncmp( num, L"-", 1 ) || !str_is_number( num ) ) {
             msi_free( num );
+            #ifndef __REACTOS__
+            return;
+            #else
             continue;
+            #endif
         }
 
         ZeroMemory( &lvc, sizeof(lvc) );
