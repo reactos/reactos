0eac50a978ad4619a149587733d01e9b63c3df63
Jose Carlos Jesus <zecarlos1957@hotmail.com>
2023-01-03 19:28:18 +0000

    [SHLWAPI] Prevent calling a NULL point
    Take advantage of compiler executing instructions from right to lef

--- dll/win32/shlwapi/path.c
+++ dll/win32/shlwapi/path.c
@@ -1353,7 +1353,7 @@ BOOL WINAPI PathFindOnPathExW(LPWSTR lpszFile,LPCWSTR *lppszOtherDirs,DWORD dwWh
 
   TRACE("(%s,%p,%08x)\n", debugstr_w(lpszFile), lppszOtherDirs, dwWhich);
 
-  if (!lpszFile || !PathIsFileSpecW(lpszFile))
+  if (!PathIsFileSpecW(lpszFile) || !lpszFile)
     return FALSE;
 
   /* Search provided directories first */
