76fe489a7c7b3f106b9d83783afdeef6e4a43640
Oleg Dubinskiy <oleg.dubinskij30@gmail.com>
2023-01-12 01:09:45 +0100

    [HDAUDBUS_NEW] Reenable Work Item, as we want to use this driver as normal HD Audio Bus driver

--- drivers/wdm/audio/hdaudbus_new/hdac_controller.cpp
+++ drivers/wdm/audio/hdaudbus_new/hdac_controller.cpp
@@ -235,7 +235,7 @@ void hdac_bus_update_rirb(PFDO_CONTEXT fdoCtx) {
 
 			SklHdAudBusPrint(DEBUG_LEVEL_ERROR, DBG_IOCTL,
 				"Attempting to queue workitem for %d\n", addr);
-			//WdfWorkItemEnqueue(fdoCtx->unsolWork);
+			WdfWorkItemEnqueue(fdoCtx->unsolWork);
 		}
 		else if (fdoCtx->rirb.cmds[addr]) {
 			fdoCtx->rirb.res[addr] = res;
