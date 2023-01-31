092ef62627d719da342c4158db9e3218faf3b9f6
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-31 11:10:46 +0900

    improve

--- modules/rostests/apitests/shell32/CommandLineToArgvW.cpp
+++ modules/rostests/apitests/shell32/CommandLineToArgvW.cpp
@@ -9,31 +9,28 @@
 #include <atlstr.h>
 
 static VOID
-DoEntry(INT lineno, LPCWSTR cmdline, INT argc_minus_1, ...)
+DoEntry(INT lineno, LPCWSTR cmdline, INT expected_argc, ...)
 {
     va_list va;
-    va_start(va, argc_minus_1);
+    va_start(va, expected_argc);
 
     INT real_argc;
-    LPWSTR* real_argv = CommandLineToArgvW(cmdline, &real_argc);
-    INT real_argc_minus_1 = real_argc - 1;
+    LPWSTR *real_argv = CommandLineToArgvW(cmdline, &real_argc);
 
-    INT m = min(argc_minus_1, real_argc_minus_1);
-    for (INT i = 0; i < m; ++i)
+    INT common = min(expected_argc, real_argc);
+    for (INT i = 1; i < common; ++i)
     {
-        LPCWSTR arg1 = va_arg(va, LPCWSTR);
-        LPCWSTR arg2 = real_argv[i + 1];
-        CStringA str1 = wine_dbgstr_w(arg1);
-        CStringA str2 = wine_dbgstr_w(arg2);
+        CStringA str1 = wine_dbgstr_w(va_arg(va, LPCWSTR));
+        CStringA str2 = wine_dbgstr_w(real_argv[i]);
         ok(str1 == str2, "Line %d: %s != %s\n", lineno, (LPCSTR)str1, (LPCSTR)str2);
     }
 
-    INT diff = abs(argc_minus_1 - real_argc_minus_1);
+    INT diff = abs(expected_argc - real_argc);
     while (diff-- > 0)
     {
-        ok(argc_minus_1 == real_argc_minus_1,
-           "Line %d: argc_minus_1 %d and real_argc_minus_1 %d are different\n",
-           lineno, argc_minus_1, real_argc_minus_1);
+        ok(expected_argc == real_argc,
+           "Line %d: expected_argc %d and real_argc %d are different\n",
+           lineno, expected_argc, real_argc);
     }
 
     LocalFree(real_argv);
@@ -42,25 +39,25 @@ DoEntry(INT lineno, LPCWSTR cmdline, INT argc_minus_1, ...)
 
 START_TEST(CommandLineToArgvW)
 {
-    DoEntry(__LINE__, L"", 0);
-    DoEntry(__LINE__, L"test.exe", 0);
-    DoEntry(__LINE__, L"test.exe ", 0);
-    DoEntry(__LINE__, L"test.exe\t", 0);
-    DoEntry(__LINE__, L"test.exe\r", 0);
-    DoEntry(__LINE__, L"test.exe\n", 0);
-    DoEntry(__LINE__, L"\"This is a test.exe\"", 0);
-    DoEntry(__LINE__, L"\"This is a test.exe\" ", 0);
-    DoEntry(__LINE__, L"\"This is a test.exe\"\t", 0);
-    DoEntry(__LINE__, L"\"This is a test.exe\"\r", 1, L"\r");
-    DoEntry(__LINE__, L"\"This is a test.exe\"\n", 1, L"\n");
-    DoEntry(__LINE__, L"test.exe a", 1, L"a");
-    DoEntry(__LINE__, L"test.exe\ta", 1, L"a");
-    DoEntry(__LINE__, L"test.exe\ra", 1, L"a");
-    DoEntry(__LINE__, L"test.exe\na", 1, L"a");
-    DoEntry(__LINE__, L"test.exe a b c", 3, L"a", L"b", L"c");
-    DoEntry(__LINE__, L"test.exe a b \"c", 3, L"a", L"b", L"c");
-    DoEntry(__LINE__, L"test.exe \"a b\" \"c d\"", 2, L"a b", L"c d");
-    DoEntry(__LINE__, L"test.exe \"a \" d\"", 2, L"a ", L"d");
-    DoEntry(__LINE__, L"test.exe \"0 1\"\" 2", 2, L"0 1\"", L"2");
-    DoEntry(__LINE__, L"test.exe \"0 1\"\"\" 2", 1, L"0 1\" 2");
+    DoEntry(__LINE__, L"", 1);
+    DoEntry(__LINE__, L"test.exe", 1);
+    DoEntry(__LINE__, L"test.exe ", 1);
+    DoEntry(__LINE__, L"test.exe\t", 1);
+    DoEntry(__LINE__, L"test.exe\r", 1);
+    DoEntry(__LINE__, L"test.exe\n", 1);
+    DoEntry(__LINE__, L"\"This is a test.exe\"", 1);
+    DoEntry(__LINE__, L"\"This is a test.exe\" ", 1);
+    DoEntry(__LINE__, L"\"This is a test.exe\"\t", 1);
+    DoEntry(__LINE__, L"\"This is a test.exe\"\r", 2, L"\r");
+    DoEntry(__LINE__, L"\"This is a test.exe\"\n", 2, L"\n");
+    DoEntry(__LINE__, L"test.exe a", 2, L"a");
+    DoEntry(__LINE__, L"test.exe\ta", 2, L"a");
+    DoEntry(__LINE__, L"test.exe\ra", 2, L"a");
+    DoEntry(__LINE__, L"test.exe\na", 2, L"a");
+    DoEntry(__LINE__, L"test.exe a b c", 4, L"a", L"b", L"c");
+    DoEntry(__LINE__, L"test.exe a b \"c", 4, L"a", L"b", L"c");
+    DoEntry(__LINE__, L"test.exe \"a b\" \"c d\"", 3, L"a b", L"c d");
+    DoEntry(__LINE__, L"test.exe \"a \" d\"", 3, L"a ", L"d");
+    DoEntry(__LINE__, L"test.exe \"0 1\"\" 2", 3, L"0 1\"", L"2");
+    DoEntry(__LINE__, L"test.exe \"0 1\"\"\" 2", 2, L"0 1\" 2");
 }
