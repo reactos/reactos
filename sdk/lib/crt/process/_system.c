1829990a8b3c72bd0d9f5cf93dd142247a2dcd66
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-02-01 18:42:02 +0900

    [MSVCRT] Implement _wsystem

--- sdk/lib/crt/process/_system.c
+++ sdk/lib/crt/process/_system.c
@@ -4,6 +4,7 @@
  * FILE:        lib/sdk/crt/process/_system.c
  * PURPOSE:     Excutes a shell command
  * PROGRAMER:   Ariadne
+ *              Katayama Hirofumi MZ
  * UPDATE HISTORY:
  *              04/03/99: Created
  */
@@ -118,6 +119,99 @@ int system(const char *command)
 
 int CDECL _wsystem(const wchar_t* cmd)
 {
-    FIXME("_wsystem stub\n");
-    return -1;
+    wchar_t *szCmdLine = NULL;
+    wchar_t *szComSpec = NULL;
+
+    PROCESS_INFORMATION ProcessInformation;
+    STARTUPINFOW StartupInfo;
+    wchar_t *s;
+    BOOL result;
+
+    int nStatus;
+
+    szComSpec = _wgetenv(L"COMSPEC");
+
+    // system should return 0 if cmd is null and the shell is found
+
+    if (cmd == NULL)
+    {
+        if (szComSpec == NULL)
+            return 0;
+        else
+            return 1;
+    }
+
+    if (szComSpec == NULL)
+        return -1;
+
+    // should return 127 or 0 ( MS ) if the shell is not found
+    // _set_errno(ENOENT);
+
+    if (szComSpec == NULL)
+    {
+        szComSpec = L"cmd.exe";
+    }
+
+    /* split the path from shell command */
+    s = max(wcsrchr(szComSpec, L'\\'), wcsrchr(szComSpec, L'/'));
+    if (s == NULL)
+        s = szComSpec;
+    else
+        s++;
+
+    szCmdLine = malloc((wcslen(s) + 4 + wcslen(cmd) + 1) * sizeof(wchar_t));
+    if (szCmdLine == NULL)
+    {
+        _set_errno(ENOMEM);
+        return -1;
+    }
+
+    wcscpy(szCmdLine, s);
+    s = wcsrchr(szCmdLine, L'.');
+    if (s)
+        *s = 0;
+    wcscat(szCmdLine, L" /C ");
+    wcscat(szCmdLine, cmd);
+
+    //command file has invalid format ENOEXEC
+
+    memset(&StartupInfo, 0, sizeof(StartupInfo));
+    StartupInfo.cb = sizeof(StartupInfo);
+    StartupInfo.lpReserved= NULL;
+    StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+    StartupInfo.wShowWindow = SW_SHOWDEFAULT;
+    StartupInfo.lpReserved2 = NULL;
+    StartupInfo.cbReserved2 = 0;
+
+    // According to ansi standards the new process should ignore  SIGINT and SIGQUIT
+    // In order to disable ctr-c the process is created with CREATE_NEW_PROCESS_GROUP,
+    // thus SetConsoleCtrlHandler(NULL,TRUE) is made on behalf of the new process.
+
+    //SIGCHILD should be blocked aswell
+
+    result = CreateProcessW(szComSpec,
+                            szCmdLine,
+                            NULL,
+                            NULL,
+                            TRUE,
+                            CREATE_NEW_PROCESS_GROUP,
+                            NULL,
+                            NULL,
+                            &StartupInfo,
+                            &ProcessInformation);
+    free(szCmdLine);
+
+    if (!result)
+    {
+        _dosmaperr(GetLastError());
+        return -1;
+    }
+
+    CloseHandle(ProcessInformation.hThread);
+
+    // system should wait untill the calling process is finished
+    _cwait(&nStatus, (intptr_t)ProcessInformation.hProcess, 0);
+    CloseHandle(ProcessInformation.hProcess);
+
+    return nStatus;
 }
