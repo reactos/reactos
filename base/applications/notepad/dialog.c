e53f3e3ed514aaca1e25088bc5cd321dadc93443
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-02-17 17:49:24 +0900

    simplify and improve

--- base/applications/notepad/dialog.c
+++ base/applications/notepad/dialog.c
@@ -411,12 +411,15 @@ BOOL DoCloseFile(VOID)
                 break;
 
             case IDCANCEL:
-                return FALSE;
-
             default:
                 return FALSE;
         }
     }
+
+    SetWindowText(Globals.hEdit, empty_str);
+    SendMessage(Globals.hEdit, EM_EMPTYUNDOBUFFER, 0, 0);
+    Globals.iEoln = EOLN_CRLF;
+    Globals.encFile = ENCODING_UTF8;
 
     SetFileName(empty_str);
     UpdateWindowCaption(TRUE);
@@ -482,10 +485,6 @@ VOID DIALOG_FileNew(VOID)
     /* Close any files and prompt to save changes */
     if (DoCloseFile())
     {
-        SetWindowText(Globals.hEdit, empty_str);
-        SendMessage(Globals.hEdit, EM_EMPTYUNDOBUFFER, 0, 0);
-        Globals.iEoln = EOLN_CRLF;
-        Globals.encFile = ENCODING_UTF8;
         SetFocus(Globals.hEdit);
         NOTEPAD_EnableSearchMenu();
         DIALOG_StatusBarUpdateAll();
