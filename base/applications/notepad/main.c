99d5895d3e3a25ea0090f9849502c097f3c3b275
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-02-25 11:40:08 +0900

    improve

--- base/applications/notepad/main.c
+++ base/applications/notepad/main.c
@@ -651,6 +651,12 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE prev, LPTSTR cmdline, int sh
 
     while (GetMessage(&msg, 0, 0, 0))
     {
+        if (msg.hwnd != Globals.hMainWnd &&
+            msg.message == WM_KEYDOWN && msg.wParam == VK_F3)
+        {
+            msg.hwnd = Globals.hMainWnd;
+        }
+
         if (!IsDialogMessage(Globals.hFindReplaceDlg, &msg) &&
             !TranslateAccelerator(Globals.hMainWnd, hAccel, &msg))
         {
