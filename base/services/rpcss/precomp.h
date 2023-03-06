79919f213c1553f9800a30f2c4973bc77f7f5702
Joachim Henze <Joachim.Henze@reactos.org>
2023-03-06 11:56:12 +0100

    [0.4.8][WMISVC][SRVSVC][WKSSVC][DHCPSVC][RPCSS] Start/Stop behavior
    by partially porting back:
    0.4.15-dev-xxx [WMISVC] Fix DPRINT that lacks arguments
    0.4.13-dev-684-g eab73ad1a4613c79af7f8eed45d3b4497ec08dfe [SRVSVC] Make the server service stoppable
    0.4.13-dev-683-g 4073a8c58db8441489f142b65f088dfd28c9c2f7 [WKSSVC] Make the workstation service stoppable
    0.4.13-dev-680-g eb532bc64159f62ca766efa1bb8a2d199dcf95e9 [WMISVC] Accept stop and shutdown commands when the service is running
    0.4.12-dev-1063-g 15a828c86d1d68a9dabaaa77bb6bb9d5e7104f42 [DHCPCSVC] Eliminate unused code and associated parameters
    0.4.9-dev-547-g 444338ae2e4ea1cdd5a9b8bc2f6a2481496d9b3c [RPCSS] Sync with Wine Staging 3.3. CORE-14434
    partially 0.4.9-dev-351-g 9febc82acf317ea16ddb04cda88e24051bacd8c4 [DHCPCSVC] Add the ability to stop the DHCP client (first part of CORE-14390)

---
+++ base/services/rpcss/precomp.h
@@ -0,0 +1,22 @@
+
+#ifndef _RPCSS_PRECOMP_H_
+#define _RPCSS_PRECOMP_H_
+
+#include <stdarg.h>
+
+#define WIN32_NO_STATUS
+#define _INC_WINDOWS
+#define COM_NO_WINDOWS_H
+
+#define NONAMELESSUNION
+#define NONAMELESSSTRUCT
+
+#include <windef.h>
+#include <winbase.h>
+
+#include <irot_s.h>
+#include <epm_s.h>
+
+#include <wine/list.h>
+
+#endif /* !_RPCSS_PRECOMP_H_ */
