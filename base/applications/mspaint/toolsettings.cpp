6ad8be547a607943d8d97af6d740cfb2b3f5057b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-03-18 14:54:44 +0900

    s/arc/rects/

--- base/applications/mspaint/toolsettings.cpp
+++ base/applications/mspaint/toolsettings.cpp
@@ -16,19 +16,19 @@
 #define CY_TRANS_ICON 30
 #define BOX_MARGIN 2
 
-static const INT s_AirRadius[4] = { 5, 8, 3, 12 };
+static const BYTE s_AirRadius[4] = { 5, 8, 3, 12 };
 
 /* FUNCTIONS ********************************************************/
 
 static INT
-getSplitRects(RECT *arc, INT cColumns, INT cRows, LPCRECT prc, LPPOINT ppt)
+getSplitRects(RECT *rects, INT cColumns, INT cRows, LPCRECT prc, LPPOINT ppt)
 {
     INT cx = prc->right - prc->left, cy = prc->bottom - prc->top;
     for (INT i = 0, iRow = 0; iRow < cRows; ++iRow)
     {
         for (INT iColumn = 0; iColumn < cColumns; ++iColumn)
         {
-            RECT& rc = arc[i];
+            RECT& rc = rects[i];
             rc.left = prc->left + (iColumn * cx / cColumns);
             rc.top = prc->top + (iRow * cy / cRows);
             rc.right = prc->left + ((iColumn + 1) * cx / cColumns);
@@ -41,9 +41,9 @@ getSplitRects(RECT *arc, INT cColumns, INT cRows, LPCRECT prc, LPPOINT ppt)
     return -1;
 }
 
-static inline INT getTransRects(RECT arc[2], LPCRECT prc, LPPOINT ppt = NULL)
+static inline INT getTransRects(RECT rects[2], LPCRECT prc, LPPOINT ppt = NULL)
 {
-    return getSplitRects(arc, 1, 2, prc, ppt);
+    return getSplitRects(rects, 1, 2, prc, ppt);
 }
 
 VOID CToolSettingsWindow::drawTrans(HDC hdc, LPCRECT prc)
@@ -58,22 +58,22 @@ VOID CToolSettingsWindow::drawTrans(HDC hdc, LPCRECT prc)
                  CX_TRANS_ICON, CY_TRANS_ICON, 0, NULL, DI_NORMAL);
 }
 
-static inline INT getRubberRects(RECT arc[4], LPCRECT prc, LPPOINT ppt = NULL)
+static inline INT getRubberRects(RECT rects[4], LPCRECT prc, LPPOINT ppt = NULL)
 {
-    return getSplitRects(arc, 1, 4, prc, ppt);
+    return getSplitRects(rects, 1, 4, prc, ppt);
 }
 
 VOID CToolSettingsWindow::drawRubber(HDC hdc, LPCRECT prc)
 {
-    RECT arc[4], rcRubber;
-    getRubberRects(arc, prc);
+    RECT rects[4], rcRubber;
+    getRubberRects(rects, prc);
     INT xCenter = (prc->left + prc->right) / 2;
     for (INT i = 0; i < 4; i++)
     {
         INT iColor, radius = i + 2;
         if (toolsModel.GetRubberRadius() == radius)
         {
-            ::FillRect(hdc, &arc[i], ::GetSysColorBrush(COLOR_HIGHLIGHT));
+            ::FillRect(hdc, &rects[i], ::GetSysColorBrush(COLOR_HIGHLIGHT));
             iColor = COLOR_HIGHLIGHTTEXT;
         }
         else
@@ -81,7 +81,7 @@ VOID CToolSettingsWindow::drawRubber(HDC hdc, LPCRECT prc)
             iColor = COLOR_WINDOWTEXT;
         }
 
-        INT yCenter = (arc[i].top + arc[i].bottom) / 2;
+        INT yCenter = (rects[i].top + rects[i].bottom) / 2;
         rcRubber.left = xCenter - radius;
         rcRubber.top = yCenter - radius;
         rcRubber.right = rcRubber.left + radius * 2;
@@ -90,21 +90,21 @@ VOID CToolSettingsWindow::drawRubber(HDC hdc, LPCRECT prc)
     }
 }
 
-static inline INT getBrushRects(RECT arc[12], LPCRECT prc, LPPOINT ppt = NULL)
+static inline INT getBrushRects(RECT rects[12], LPCRECT prc, LPPOINT ppt = NULL)
 {
-    return getSplitRects(arc, 3, 4, prc, ppt);
+    return getSplitRects(rects, 3, 4, prc, ppt);
 }
 
 VOID CToolSettingsWindow::drawBrush(HDC hdc, LPCRECT prc)
 {
-    RECT arc[12];
-    getBrushRects(arc, prc);
+    RECT rects[12];
+    getBrushRects(rects, prc);
 
-    ::FillRect(hdc, &arc[toolsModel.GetBrushStyle()], ::GetSysColorBrush(COLOR_HIGHLIGHT));
+    ::FillRect(hdc, &rects[toolsModel.GetBrushStyle()], ::GetSysColorBrush(COLOR_HIGHLIGHT));
 
     for (INT i = 0; i < 12; i++)
     {
-        RECT rcItem = arc[i];
+        RECT rcItem = rects[i];
         INT x = (rcItem.left + rcItem.right) / 2, y = (rcItem.top + rcItem.bottom) / 2;
         INT iColor;
         if (i == toolsModel.GetBrushStyle())
@@ -115,26 +115,26 @@ VOID CToolSettingsWindow::drawBrush(HDC hdc, LPCRECT prc)
     }
 }
 
-static inline INT getLineRects(RECT arc[5], LPCRECT prc, LPPOINT ppt = NULL)
+static inline INT getLineRects(RECT rects[5], LPCRECT prc, LPPOINT ppt = NULL)
 {
-    return getSplitRects(arc, 1, 5, prc, ppt);
+    return getSplitRects(rects, 1, 5, prc, ppt);
 }
 
 VOID CToolSettingsWindow::drawLine(HDC hdc, LPCRECT prc)
 {
-    RECT arc[5];
-    getLineRects(arc, prc);
+    RECT rects[5];
+    getLineRects(rects, prc);
 
     for (INT i = 0; i < 5; i++)
     {
         INT penWidth = i + 1;
-        RECT rcLine = arc[i];
+        RECT rcLine = rects[i];
         ::InflateRect(&rcLine, -2, 0);
         rcLine.top = (rcLine.top + rcLine.bottom - penWidth) / 2;
         rcLine.bottom = rcLine.top + penWidth;
         if (toolsModel.GetLineWidth() == penWidth)
         {
-            ::FillRect(hdc, &arc[i], ::GetSysColorBrush(COLOR_HIGHLIGHT));
+            ::FillRect(hdc, &rects[i], ::GetSysColorBrush(COLOR_HIGHLIGHT));
             ::FillRect(hdc, &rcLine, ::GetSysColorBrush(COLOR_HIGHLIGHTTEXT));
         }
         else
@@ -144,23 +144,23 @@ VOID CToolSettingsWindow::drawLine(HDC hdc, LPCRECT prc)
     }
 }
 
-static INT getAirBrushRects(RECT arc[4], LPCRECT prc, LPPOINT ppt = NULL)
+static INT getAirBrushRects(RECT rects[4], LPCRECT prc, LPPOINT ppt = NULL)
 {
     INT cx = (prc->right - prc->left), cy = (prc->bottom - prc->top);
 
-    arc[0] = arc[1] = arc[2] = arc[3] = *prc;
+    rects[0] = rects[1] = rects[2] = rects[3] = *prc;
 
-    arc[0].right = arc[1].left = prc->left + cx * 3 / 8;
-    arc[0].bottom = arc[1].bottom = prc->top + cy / 2;
+    rects[0].right = rects[1].left = prc->left + cx * 3 / 8;
+    rects[0].bottom = rects[1].bottom = prc->top + cy / 2;
 
-    arc[2].top = arc[3].top = prc->top + cy / 2;
-    arc[2].right = arc[3].left = prc->left + cx * 2 / 8;
+    rects[2].top = rects[3].top = prc->top + cy / 2;
+    rects[2].right = rects[3].left = prc->left + cx * 2 / 8;
 
     if (ppt)
     {
         for (INT i = 0; i < 4; ++i)
         {
-            if (::PtInRect(&arc[i], *ppt))
+            if (::PtInRect(&rects[i], *ppt))
                 return i;
         }
     }
@@ -169,13 +169,13 @@ static INT getAirBrushRects(RECT arc[4], LPCRECT prc, LPPOINT ppt = NULL)
 
 VOID CToolSettingsWindow::drawAirBrush(HDC hdc, LPCRECT prc)
 {
-    RECT arc[4];
-    getAirBrushRects(arc, prc);
+    RECT rects[4];
+    getAirBrushRects(rects, prc);
 
     srand(0);
-    for (INT i = 0; i < 4; ++i)
+    for (size_t i = 0; i < 4; ++i)
     {
-        RECT& rc = arc[i];
+        RECT& rc = rects[i];
         INT x = (rc.left + rc.right) / 2;
         INT y = (rc.top + rc.bottom) / 2;
         BOOL bHigh = (s_AirRadius[i] == toolsModel.GetAirBrushWidth());
@@ -191,19 +191,19 @@ VOID CToolSettingsWindow::drawAirBrush(HDC hdc, LPCRECT prc)
     }
 }
 
-static inline INT getBoxRects(RECT arc[3], LPCRECT prc, LPPOINT ppt = NULL)
+static inline INT getBoxRects(RECT rects[3], LPCRECT prc, LPPOINT ppt = NULL)
 {
-    return getSplitRects(arc, 1, 3, prc, ppt);
+    return getSplitRects(rects, 1, 3, prc, ppt);
 }
 
 VOID CToolSettingsWindow::drawBox(HDC hdc, LPCRECT prc)
 {
-    RECT arc[3];
-    getBoxRects(arc, prc);
+    RECT rects[3];
+    getBoxRects(rects, prc);
 
     for (INT iItem = 0; iItem < 3; ++iItem)
     {
-        RECT& rcItem = arc[iItem];
+        RECT& rcItem = rects[iItem];
 
         if (toolsModel.GetShapeStyle() == iItem)
             ::FillRect(hdc, &rcItem, ::GetSysColorBrush(COLOR_HIGHLIGHT));
@@ -331,7 +331,7 @@ LRESULT CToolSettingsWindow::OnLButtonDown(UINT nMsg, WPARAM wParam, LPARAM lPar
 {
     POINT pt = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
 
-    RECT rcClient, arc[12];
+    RECT rcClient, rects[12];
     GetClientRect(&rcClient);
 
     RECT rect1 = { 0, 0, rcClient.right, rcClient.bottom / 2 };
@@ -345,28 +345,28 @@ LRESULT CToolSettingsWindow::OnLButtonDown(UINT nMsg, WPARAM wParam, LPARAM lPar
         case TOOL_FREESEL:
         case TOOL_RECTSEL:
         case TOOL_TEXT:
-            iItem = getTransRects(arc, &rect1, &pt);
+            iItem = getTransRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetBackgroundTransparent(iItem);
             break;
         case TOOL_RUBBER:
-            iItem = getRubberRects(arc, &rect1, &pt);
+            iItem = getRubberRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetRubberRadius(iItem + 2);
             break;
         case TOOL_BRUSH:
-            iItem = getBrushRects(arc, &rect1, &pt);
+            iItem = getBrushRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetBrushStyle(iItem);
             break;
         case TOOL_AIRBRUSH:
-            iItem = getAirBrushRects(arc, &rect1, &pt);
+            iItem = getAirBrushRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetAirBrushWidth(s_AirRadius[iItem]);
             break;
         case TOOL_LINE:
         case TOOL_BEZIER:
-            iItem = getLineRects(arc, &rect1, &pt);
+            iItem = getLineRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetLineWidth(iItem + 1);
             break;
@@ -374,11 +374,11 @@ LRESULT CToolSettingsWindow::OnLButtonDown(UINT nMsg, WPARAM wParam, LPARAM lPar
         case TOOL_SHAPE:
         case TOOL_ELLIPSE:
         case TOOL_RRECT:
-            iItem = getBoxRects(arc, &rect1, &pt);
+            iItem = getBoxRects(rects, &rect1, &pt);
             if (iItem != -1)
                 toolsModel.SetShapeStyle(iItem);
 
-            iItem = getLineRects(arc, &rect2, &pt);
+            iItem = getLineRects(rects, &rect2, &pt);
             if (iItem != -1)
                 toolsModel.SetLineWidth(iItem + 1);
             break;
