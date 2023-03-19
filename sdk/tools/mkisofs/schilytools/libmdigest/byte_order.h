b08c9aa2ac39807ae0b86e25f086af0026a52bbf
Adam Słaboń <asaillen@protonmail.com>
2023-03-19 13:58:27 +0100

    [MKISOFS] Fix MSVC build on ARM hosts
    Check also for MSVC target machine macros on ARM

--- sdk/tools/mkisofs/schilytools/libmdigest/byte_order.h
+++ sdk/tools/mkisofs/schilytools/libmdigest/byte_order.h
@@ -61,7 +61,8 @@ extern "C" {
 		__BYTE_ORDER == __LITTLE_ENDIAN) || \
 	defined(CPU_IA32) || defined(CPU_X64) || \
 	defined(__ia64) || defined(__ia64__) || defined(__alpha__) || defined(_M_ALPHA) || \
-	defined(vax) || defined(MIPSEL) || defined(_ARM_) || defined(__arm64__)
+	defined(vax) || defined(MIPSEL) || \
+	defined(_ARM_) || defined(__arm64__) || defined(_M_ARM) || defined(_M_ARM64)
 #define	CPU_LITTLE_ENDIAN
 #define	IS_BIG_ENDIAN 0
 #define	IS_LITTLE_ENDIAN 1
