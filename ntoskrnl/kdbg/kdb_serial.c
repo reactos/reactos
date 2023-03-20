a03c6840ea7397e971f2f15d713480e213458ceb
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2023-03-20 17:49:15 +0100

    [NTOS:KDBG] Isolate terminal and input-related routines from the rest of KDBG. (#5188)
    This is done in preparation for moving all this functionality in a
    separate KDTERM "KD Terminal Driver" DLL.
    
    Additionally:
    
    - Flush the terminal input before sending ANSI escape sequences.
    
    - Use a specific KdbpSendCommandSerial() function to send these sequences,
      instead of KdpDprintf()! Otherwise, we send them to **ALL** the display
      providers, including for example dmesg. Replaying the listing with
      dmesg would then cause the terminal to misbehave later.
      For example, it would send the answer of a "Query Device Attributes"
      command, as the response to a query for terminal size...

--- ntoskrnl/kdbg/kdb_serial.c
+++ ntoskrnl/kdbg/kdb_serial.c
@@ -1,19 +1,24 @@
 /*
- * COPYRIGHT:       See COPYING in the top level directory
- * PROJECT:         ReactOS kernel
- * FILE:            ntoskrnl/kdbg/kdb_serial.c
- * PURPOSE:         Serial driver
- *
- * PROGRAMMERS:     Victor Kirhenshtein (sauros@iname.com)
- *                  Jason Filby (jasonfilby@yahoo.com)
- *                  arty
+ * PROJECT:     ReactOS KDBG Kernel Debugger Terminal Driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Serial driver
+ * COPYRIGHT:   Copyright 2004 Art Yerkes <ayerkes@speakeasy.net>
+ *              Copyright 2005 Gregor Anich <blight@blight.eu.org>
  */
 
-/* INCLUDES ****************************************************************/
+/* INCLUDES ******************************************************************/
 
 #include <ntoskrnl.h>
-#define NDEBUG
-#include <debug.h>
+
+/* FUNCTIONS *****************************************************************/
+
+VOID
+KdbpSendCommandSerial(
+    _In_ PCSTR Command)
+{
+    while (*Command)
+        KdPortPutByteEx(&SerialPortInfo, *Command++);
+}
 
 CHAR
 KdbpTryGetCharSerial(ULONG Retry)
