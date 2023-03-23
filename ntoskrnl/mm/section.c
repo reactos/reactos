4ff9c6ffedb99a573bc44dbb5b9985824e820d21
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-03-23 16:36:30 +0900

    Update ntoskrnl/mm/section.c

--- ntoskrnl/mm/section.c
+++ ntoskrnl/mm/section.c
@@ -3637,7 +3637,7 @@ MiRosUnmapViewOfSection(IN PEPROCESS Process,
         PMM_SECTION_SEGMENT Segment = MemoryArea->SectionData.Segment;
         PMMVAD Vad = &MemoryArea->VadNode;
         PFILE_OBJECT FileObject;
-        SIZE_T ViewSize = PAGE_SIZE + ((Vad->EndingVpn - Vad->StartingVpn) << PAGE_SHIFT);
+        SIZE_T ViewSize;
         LARGE_INTEGER ViewOffset;
         ViewOffset.QuadPart = MemoryArea->SectionData.ViewOffset;
         
