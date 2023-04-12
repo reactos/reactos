db032899db02cd07d8a2cde2296617515a0f1175
Mark Jansen <mark.jansen@reactos.org>
2023-04-12 23:27:08 +0200

    [CLEANMGR] Add basic cleanmgr implementation
    CORE-18941

---
+++ base/applications/cleanmgr/cleanmgr/resource.h
@@ -0,0 +1,30 @@
+/*
+ * PROJECT:     ReactOS Disk Cleanup
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Resource definitions
+ * COPYRIGHT:   Copyright 2023-2025 Mark Jansen <mark.jansen@reactos.org>
+ */
+
+#define IDC_STATIC                      -1
+
+#define IDI_CLEANMGR                    100
+
+#define IDD_PROPERTIES_MAIN             200
+#define IDC_DISKICON                    201
+#define IDC_TOTAL_CLEANABLE             202
+#define IDC_HANDLERLIST                 203
+#define IDC_SELECTED_GAINED             204
+#define IDC_DESCRIPTION                 205
+#define IDC_DETAILS                     206
+
+#define IDD_SELECTDRIVE                 220
+#define IDC_DRIVES                      221
+
+#define IDS_PROPERTIES_MAIN_TITLE       1000
+#define IDS_TOTAL_CLEANABLE_CAPTION     1001
+#define IDS_DISK_CLEANUP                1002
+#define IDS_CONFIRM_DELETE              1003
+#define IDS_CALCULATING                 1004
+#define IDS_SCANNING                    1005
+#define IDS_CLEANING_CAPTION            1006
+#define IDS_CLEANING                    1007
