2bef103f2b4b12e69a3e14175a0970c9a709c7c7
Timo Kreuzer <timo.kreuzer@reactos.org>
2023-05-20 12:02:03 +0300

    [RTL_APITEST] Move RtlIntSafe test from kmtest to rtl_apitest

--- modules/rostests/apitests/rtl/testlist.c
+++ modules/rostests/apitests/rtl/testlist.c
@@ -4,9 +4,12 @@
 #include <apitest.h>
 
 extern void func_RtlCaptureContext(void);
+extern void func_RtlIntSafe(void);
 
 const struct test winetest_testlist[] =
 {
+    { "RtlIntSafe",               func_RtlIntSafe },
+
 #ifdef _M_AMD64
     { "RtlCaptureContext",        func_RtlCaptureContext },
 #endif
