bf6af0f52eba23b2ca6677813792baa80c6d1f82
Thomas Faber <thomas.faber@reactos.org>
2023-05-27 22:29:16 -0400

    [NTOS:SE] Mark output parameters as such.

--- ntoskrnl/se/access.c
+++ ntoskrnl/se/access.c
@@ -23,10 +23,10 @@
  * @param[in] Process
  * Valid process object where subject context is to be captured.
  *
- * @param[in,out] AccessState
+ * @param[out] AccessState
  * An initialized returned parameter to an access state.
  *
- * @param[in] AuxData
+ * @param[out] AuxData
  * Auxiliary security data for access state.
  *
  * @param[in] Access
@@ -43,8 +43,8 @@ NTAPI
 SeCreateAccessStateEx(
     _In_ PETHREAD Thread,
     _In_ PEPROCESS Process,
-    _Inout_ PACCESS_STATE AccessState,
-    _In_ PAUX_ACCESS_DATA AuxData,
+    _Out_ PACCESS_STATE AccessState,
+    _Out_ __drv_aliasesMem PAUX_ACCESS_DATA AuxData,
     _In_ ACCESS_MASK Access,
     _In_ PGENERIC_MAPPING GenericMapping)
 {
@@ -101,10 +101,10 @@ SeCreateAccessStateEx(
  * @brief
  * Creates an access state.
  *
- * @param[in,out] AccessState
+ * @param[out] AccessState
  * An initialized returned parameter to an access state.
  *
- * @param[in] AuxData
+ * @param[out] AuxData
  * Auxiliary security data for access state.
  *
  * @param[in] Access
@@ -119,8 +119,8 @@ SeCreateAccessStateEx(
 NTSTATUS
 NTAPI
 SeCreateAccessState(
-    _Inout_ PACCESS_STATE AccessState,
-    _In_ PAUX_ACCESS_DATA AuxData,
+    _Out_ PACCESS_STATE AccessState,
+    _Out_ __drv_aliasesMem PAUX_ACCESS_DATA AuxData,
     _In_ ACCESS_MASK Access,
     _In_ PGENERIC_MAPPING GenericMapping)
 {
