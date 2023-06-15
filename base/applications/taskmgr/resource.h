f70807988629fd69f1cf11047132d7436ed5ec5b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-06-15 14:36:12 +0900

    improve 2

--- base/applications/taskmgr/resource.h
+++ base/applications/taskmgr/resource.h
@@ -1,3 +1,10 @@
+/*
+ * PROJECT:     ReactOS Task Manager
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
+ * PURPOSE:     The resource IDs
+ * COPYRIGHT:   Copyright (C) 1999-2001 Brian Palmer <brianp@reactos.org>
+ */
+
 #pragma once
 
 #define IDS_IDLE_PROCESS              102
