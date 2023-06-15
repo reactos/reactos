78af67602db9357d31280a2f6abb0a6cd44d3806
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2023-06-15 11:35:35 +0200

    [SCSIPORT] Simplify storage of device type identifiers.
    Addendum to commits 87d9dfae0 (r74595) and a97c6e0aa .

--- drivers/storage/port/scsiport/scsitypes.h
+++ drivers/storage/port/scsiport/scsitypes.h
@@ -1,47 +1,43 @@
 
-// see https://docs.microsoft.com/en-us/windows-hardware/drivers/install/identifiers-for-scsi-devices
+// See https://docs.microsoft.com/en-us/windows-hardware/drivers/install/identifiers-for-scsi-devices
 // and https://docs.microsoft.com/en-us/windows-hardware/drivers/install/identifiers-for-ide-devices
+
+struct
+{
+    PCSTR  DeviceType;
+    PCSTR  GenericType;
+    PCWSTR PeripheralType;
+} DeviceTypeIds[] =
+{
+    {"Disk",        "GenDisk",          L"DiskPeripheral"               }, // DIRECT_ACCESS_DEVICE
+    {"Sequential",  "",                 L"TapePeripheral"               }, // SEQUENTIAL_ACCESS_DEVICE
+    {"Printer",     "GenPrinter",       L"PrinterPeripheral"            }, // PRINTER_DEVICE
+    {"Processor",   "",                 L"OtherPeripheral"              }, // PROCESSOR_DEVICE
+    {"Worm",        "GenWorm",          L"WormPeripheral"               }, // WRITE_ONCE_READ_MULTIPLE_DEVICE
+    {"CdRom",       "GenCdRom",         L"CdRomPeripheral"              }, // READ_ONLY_DIRECT_ACCESS_DEVICE
+    {"Scanner",     "GenScanner",       L"ScannerPeripheral"            }, // SCANNER_DEVICE
+    {"Optical",     "GenOptical",       L"OpticalDiskPeripheral"        }, // OPTICAL_DEVICE
+    {"Changer",     "ScsiChanger",      L"MediumChangerPeripheral"      }, // MEDIUM_CHANGER
+    {"Net",         "ScsiNet",          L"CommunicationsPeripheral"     }, // COMMUNICATION_DEVICE
+    {"ASCIT8",      "ScsiASCIT8",       L"ASCPrePressGraphicsPeripheral"}, // 10
+    {"ASCIT8",      "ScsiASCIT8",       L"ASCPrePressGraphicsPeripheral"}, // 11
+    {"Array",       "ScsiArray",        L"ArrayPeripheral"              }, // ARRAY_CONTROLLER_DEVICE
+    {"Enclosure",   "ScsiEnclosure",    L"EnclosurePeripheral"          }, // SCSI_ENCLOSURE_DEVICE
+    {"RBC",         "ScsiRBC",          L"RBCPeripheral"                }, // REDUCED_BLOCK_DEVICE
+    {"CardReader",  "ScsiCardReader",   L"CardReaderPeripheral"         }, // OPTICAL_CARD_READER_WRITER_DEVICE
+    {"Bridge",      "ScsiBridge",       L"BridgePeripheral"             }, // BRIDGE_CONTROLLER_DEVICE
+    {"Other",       "ScsiOther",        L"OtherPeripheral"              }
+};
 
 FORCEINLINE
 PCSTR
 GetDeviceType(
     _In_ PINQUIRYDATA InquiryData)
 {
-    switch (InquiryData->DeviceType)
-    {
-        case DIRECT_ACCESS_DEVICE:
-            return "Disk";
-        case SEQUENTIAL_ACCESS_DEVICE:
-            return "Sequential";
-        case PRINTER_DEVICE:
-            return "Printer";
-        case PROCESSOR_DEVICE:
-            return "Processor";
-        case WRITE_ONCE_READ_MULTIPLE_DEVICE:
-            return "Worm";
-        case READ_ONLY_DIRECT_ACCESS_DEVICE:
-            return "CdRom";
-        case SCANNER_DEVICE:
-            return "Scanner";
-        case OPTICAL_DEVICE:
-            return "Optical";
-        case MEDIUM_CHANGER:
-            return "Changer";
-        case COMMUNICATION_DEVICE:
-            return "Net";
-        case ARRAY_CONTROLLER_DEVICE:
-            return "Array";
-        case SCSI_ENCLOSURE_DEVICE:
-            return "Enclosure";
-        case REDUCED_BLOCK_DEVICE:
-            return "RBC";
-        case OPTICAL_CARD_READER_WRITER_DEVICE:
-            return "CardReader";
-        case BRIDGE_CONTROLLER_DEVICE:
-            return "Bridge";
-        default:
-            return "Other";
-    }
+    if (InquiryData->DeviceType < RTL_NUMBER_OF(DeviceTypeIds))
+        return DeviceTypeIds[InquiryData->DeviceType].DeviceType;
+    else
+        return DeviceTypeIds[RTL_NUMBER_OF(DeviceTypeIds)-1].DeviceType;
 }
 
 FORCEINLINE
@@ -49,37 +45,10 @@ PCSTR
 GetGenericType(
     _In_ PINQUIRYDATA InquiryData)
 {
-    switch (InquiryData->DeviceType)
-    {
-        case DIRECT_ACCESS_DEVICE:
-            return "GenDisk";
-        case PRINTER_DEVICE:
-            return "GenPrinter";
-        case WRITE_ONCE_READ_MULTIPLE_DEVICE:
-            return "GenWorm";
-        case READ_ONLY_DIRECT_ACCESS_DEVICE:
-            return "GenCdRom";
-        case SCANNER_DEVICE:
-            return "GenScanner";
-        case OPTICAL_DEVICE:
-            return "GenOptical";
-        case MEDIUM_CHANGER:
-            return "ScsiChanger";
-        case COMMUNICATION_DEVICE:
-            return "ScsiNet";
-        case ARRAY_CONTROLLER_DEVICE:
-            return "ScsiArray";
-        case SCSI_ENCLOSURE_DEVICE:
-            return "ScsiEnclosure";
-        case REDUCED_BLOCK_DEVICE:
-            return "ScsiRBC";
-        case OPTICAL_CARD_READER_WRITER_DEVICE:
-            return "ScsiCardReader";
-        case BRIDGE_CONTROLLER_DEVICE:
-            return "ScsiBridge";
-        default:
-            return "ScsiOther";
-    }
+    if (InquiryData->DeviceType < RTL_NUMBER_OF(DeviceTypeIds))
+        return DeviceTypeIds[InquiryData->DeviceType].GenericType;
+    else
+        return DeviceTypeIds[RTL_NUMBER_OF(DeviceTypeIds)-1].GenericType;
 }
 
 FORCEINLINE
@@ -87,38 +56,8 @@ PCWSTR
 GetPeripheralTypeW(
     _In_ PINQUIRYDATA InquiryData)
 {
-    switch (InquiryData->DeviceType)
-    {
-        case DIRECT_ACCESS_DEVICE:
-            return L"DiskPeripheral";
-        case SEQUENTIAL_ACCESS_DEVICE:
-            return L"TapePeripheral";
-        case PRINTER_DEVICE:
-            return L"PrinterPeripheral";
-        // case 3: "ProcessorPeripheral", classified as 'other': fall back to default case.
-        case WRITE_ONCE_READ_MULTIPLE_DEVICE:
-            return L"WormPeripheral";
-        case READ_ONLY_DIRECT_ACCESS_DEVICE:
-            return L"CdRomPeripheral";
-        case SCANNER_DEVICE:
-            return L"ScannerPeripheral";
-        case OPTICAL_DEVICE:
-            return L"OpticalDiskPeripheral";
-        case MEDIUM_CHANGER:
-            return L"MediumChangerPeripheral";
-        case COMMUNICATION_DEVICE:
-            return L"CommunicationsPeripheral";
-        case ARRAY_CONTROLLER_DEVICE:
-            return L"ArrayPeripheral";
-        case SCSI_ENCLOSURE_DEVICE:
-            return L"EnclosurePeripheral";
-        case REDUCED_BLOCK_DEVICE:
-            return L"RBCPeripheral";
-        case OPTICAL_CARD_READER_WRITER_DEVICE:
-            return L"CardReaderPeripheral";
-        case BRIDGE_CONTROLLER_DEVICE:
-            return L"BridgePeripheral";
-        default:
-            return L"OtherPeripheral";
-    }
+    if (InquiryData->DeviceType < RTL_NUMBER_OF(DeviceTypeIds))
+        return DeviceTypeIds[InquiryData->DeviceType].PeripheralType;
+    else
+        return DeviceTypeIds[RTL_NUMBER_OF(DeviceTypeIds)-1].PeripheralType;
 }
