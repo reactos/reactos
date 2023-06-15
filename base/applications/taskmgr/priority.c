9f1f2e02a8b668c40e48afc7b5e6bbd266da462b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-06-15 14:34:07 +0900

    improve

--- base/applications/taskmgr/priority.c
+++ base/applications/taskmgr/priority.c
@@ -1,7 +1,7 @@
 /*
  * PROJECT:     ReactOS Task Manager
  * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
- * PURPOSE:     Setting priority
+ * PURPOSE:     Setting priority of process
  * COPYRIGHT:   Copyright (C) 1999-2001 Brian Palmer <brianp@reactos.org>
  *              Copyright (C) 2005 Klemens Friedl <frik85@reactos.at>
  */
