reactos/reactos

commit 61012eb544ebb12ffe878376af8c700ed6577cf0

[XBOXVMP] Fix in VideoPortGetAccessRanges call: missing Slot parameter.
Contrary to what is (badly) written on MSDN, this parameter is
*mandatory* when the ranges looked for are on a PCI adapter.

Detected when testing with MS Windows' videoprt.sys on XBOX emulator;
thanks to Simone Lombardo for assistance!

win32ss/drivers/miniport/xboxvmp/xboxvmp.c
@@ -61,18 +61,20 @@ XboxVmpFindAdapter(
     OUT PUCHAR Again)
 {
     PXBOXVMP_DEVICE_EXTENSION XboxVmpDeviceExtension;
-    VIDEO_ACCESS_RANGE AccessRanges[3];
     VP_STATUS Status;
+    VIDEO_ACCESS_RANGE AccessRanges[2];
     USHORT VendorId = 0x10DE; /* NVIDIA Corporation */
     USHORT DeviceId = 0x02A0; /* NV2A XGPU */
+    ULONG Slot = 0;
 
     TRACE_(IHVVIDEO, "XboxVmpFindAdapter\n");
 
     XboxVmpDeviceExtension = (PXBOXVMP_DEVICE_EXTENSION)HwDeviceExtension;
 
-    Status = VideoPortGetAccessRanges(HwDeviceExtension, 0, NULL, 3, AccessRanges,
-                                      &VendorId, &DeviceId, NULL);
-
+    VideoPortZeroMemory(&AccessRanges, sizeof(AccessRanges));
+    Status = VideoPortGetAccessRanges(HwDeviceExtension, 0, NULL,
+                                      RTL_NUMBER_OF(AccessRanges), AccessRanges,
+                                      &VendorId, &DeviceId, &Slot);
     if (Status == NO_ERROR)
     {
         XboxVmpDeviceExtension->PhysControlStart = AccessRanges[0].RangeStart;

commit 61c77d1f87118b27588554ff14ccb69a7910608b

[XBOXVMP] Real OG XBOX NVidia GPU exposes 3 access ranges, so retrieve them. CORE-19011
Fix a "bug" I introduced in commit 61012eb54.
VideoPortGetAccessRanges() expects the caller to specify at least the
total number of ranges the hardware exposes, otherwise it fails with
ERROR_MORE_DATA. (Tested also with the help of Windows' videoprt.sys.)

On real original XBOX there are three:
```
01:00.0 VGA compatible controller [0300]: NVIDIA Corporation NV2A [XGPU] [10de:02a0] (rev a1)
    Subsystem: Unknown [0000:0000]
    Flags: bus master, 66MHz, medium devsel, latency 0, IRQ 03
    Memory at fd000000 (32-bit, non-prefetchable) [size=16M]
    Memory at f0000000 (32-bit, prefetchable) [size=128M]
    Memory at 00000000 (32-bit, prefetchable) [size=512K]
```

Thanks to Stanislav and Daniel for tests on the real XBOX.

win32ss/drivers/miniport/xboxvmp/xboxvmp.c
@@ -62,7 +62,8 @@ XboxVmpFindAdapter(
 {
     PXBOXVMP_DEVICE_EXTENSION XboxVmpDeviceExtension;
     VP_STATUS Status;
-    VIDEO_ACCESS_RANGE AccessRanges[2];
+    /* 3 access ranges: for MMIO, VRAM, and Indirect memory access IO ports */
+    VIDEO_ACCESS_RANGE AccessRanges[3];
     USHORT VendorId = 0x10DE; /* NVIDIA Corporation */
     USHORT DeviceId = 0x02A0; /* NV2A XGPU */
     ULONG Slot = 0;