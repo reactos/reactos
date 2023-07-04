04119126c400f04f2028bd87eabdbee755e054dd
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-07-04 09:48:28 +0900

    add tests

--- modules/rostests/apitests/advapi32/IsTextUnicode.c
+++ modules/rostests/apitests/advapi32/IsTextUnicode.c
@@ -128,6 +128,9 @@ START_TEST(IsTextUnicode)
     };
 
     const char japanese_with_lead[] = "ABC" "\x83\x40" "D";
+    const char japanese_sjis[] = "\x93\xFA\x96\x7B\x8C\xEA\x31\x32\x33\x93\xFA\x96\x7B\x8C\xEA";
+    const char japanese_utf8[] = "\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E\x31\x32\x33\xE6\x97\xA5"
+                                 "\xE6\x9C\xAC\xE8\xAA\x9E";
     const char simplfied_chinese_with_lead[] = "ABC" "\xC5\xC5" "D";
     const char korean_with_lead[] = "ABC" "\xBF\xAD" "D";
     const char traditional_chinese_with_lead[] = "ABC" "\xB1\xC1" "D";
@@ -151,6 +154,24 @@ START_TEST(IsTextUnicode)
     Result = IS_TEXT_UNICODE_DBCS_LEADBYTE;
     ok(!IsTextUnicode(japanese_with_lead, sizeof(japanese_with_lead), &Result), "IsTextUnicode() returned TRUE, expected FALSE\n");
     ok(Result == IS_TEXT_UNICODE_DBCS_LEADBYTE, "Result returned 0x%x, expected 0x%x\n", Result, IS_TEXT_UNICODE_DBCS_LEADBYTE);
+
+    Result = IS_TEXT_UNICODE_STATISTICS | IS_TEXT_UNICODE_REVERSE_STATISTICS;
+    ok(!IsTextUnicode(japanese_sjis, sizeof(japanese_sjis) - 1, &Result), "IsTextUnicode() returned TRUE, expected FALSE\n");
+    ok(Result == 0, "Result returned 0x%x, expected 0x%x\n", Result, 0);
+
+    Result = IS_TEXT_UNICODE_STATISTICS | IS_TEXT_UNICODE_REVERSE_STATISTICS | IS_TEXT_UNICODE_DBCS_LEADBYTE;
+    ok(!IsTextUnicode(japanese_sjis, sizeof(japanese_sjis) - 1, &Result), "IsTextUnicode() returned TRUE, expected FALSE\n");
+    ok(Result == (IS_TEXT_UNICODE_DBCS_LEADBYTE | IS_TEXT_UNICODE_REVERSE_STATISTICS),
+       "Result returned 0x%x, expected 0x%x\n", Result, IS_TEXT_UNICODE_DBCS_LEADBYTE | IS_TEXT_UNICODE_REVERSE_STATISTICS);
+
+    Result = IS_TEXT_UNICODE_STATISTICS | IS_TEXT_UNICODE_REVERSE_STATISTICS;
+    ok(!IsTextUnicode(japanese_utf8, sizeof(japanese_utf8) - 1, &Result), "IsTextUnicode() returned TRUE, expected FALSE\n");
+    ok(Result == 0, "Result returned 0x%x, expected 0x%x\n", Result, 0);
+
+    Result = IS_TEXT_UNICODE_STATISTICS | IS_TEXT_UNICODE_REVERSE_STATISTICS | IS_TEXT_UNICODE_DBCS_LEADBYTE;
+    ok(!IsTextUnicode(japanese_utf8, sizeof(japanese_utf8) - 1, &Result), "IsTextUnicode() returned TRUE, expected FALSE\n");
+    ok(Result == (IS_TEXT_UNICODE_DBCS_LEADBYTE | IS_TEXT_UNICODE_STATISTICS),
+       "Result returned 0x%x, expected 0x%x\n", Result, IS_TEXT_UNICODE_DBCS_LEADBYTE | IS_TEXT_UNICODE_STATISTICS);
 
     /* Simplified Chinese */
     SetupLocale(936, 936, -1);
