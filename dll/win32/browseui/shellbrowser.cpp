22fbff044e91c7d8371268adb951334e0e8154cd
Carl J. Bialorucki <cbialo2@outlook.com>
2023-07-20 13:25:52 -0600

    Revert changes from unrelated work

--- dll/win32/browseui/shellbrowser.cpp
+++ dll/win32/browseui/shellbrowser.cpp
@@ -806,22 +806,14 @@ HRESULT CShellBrowser::BrowseToPIDL(LPCITEMIDLIST pidl, long flags)
     CComPtr<IShellFolder>                   newFolder;
     FOLDERSETTINGS                          newFolderSettings;
     HRESULT                                 hResult;
-    CLSID                                   clsid;
-    BOOL                                    HasIconViewType;
 
     // called by shell view to browse to new folder
     // also called by explorer band to navigate to new folder
     hResult = SHBindToFolder(pidl, &newFolder);
     if (FAILED_UNEXPECTEDLY(hResult))
         return hResult;
-    IUnknown_GetClassID(newFolder, &clsid);
-    HasIconViewType = clsid == CLSID_MyComputer || clsid == CLSID_ControlPanel ||
-                      clsid == CLSID_ShellDesktop;
 
-    if (HasIconViewType)
-        newFolderSettings.ViewMode = FVM_ICON;
-    else
-        newFolderSettings.ViewMode = FVM_DETAILS;
+    newFolderSettings.ViewMode = FVM_ICON;
     newFolderSettings.fFlags = 0;
     hResult = BrowseToPath(newFolder, pidl, &newFolderSettings, flags);
     if (FAILED_UNEXPECTEDLY(hResult))
