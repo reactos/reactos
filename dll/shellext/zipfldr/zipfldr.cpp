fd1771c534b56485844352629144be843d7d5072
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-07-21 11:09:16 +0900

    use wcstol

--- dll/shellext/zipfldr/zipfldr.cpp
+++ dll/shellext/zipfldr/zipfldr.cpp
@@ -106,7 +106,7 @@ UINT GetZipCodePage(BOOL bUnZip)
         return nDefaultCodePage;
 
     szValue[_countof(szValue) - 1] = UNICODE_NULL;
-    return wcstoul(szValue, NULL, 0);
+    return wcstol(szValue, NULL, 0);
 }
 
 EXTERN_C
