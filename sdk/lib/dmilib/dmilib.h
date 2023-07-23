69331dcee10e6caae44b67aa34a4f7b5eada2ed1
Joachim Henze <Joachim.Henze@reactos.org>
2023-07-23 20:06:49 +0200

    [DMILIB][UDMIHELP] Favor pragma once

--- sdk/lib/dmilib/dmilib.h
+++ sdk/lib/dmilib/dmilib.h
@@ -1,13 +1,11 @@
 /*
  * PROJECT:     ReactOS DMI/SMBIOS Library
  * LICENSE:     GPL - See COPYING in the top level directory
- * FILE:        dmilib.h
  * PURPOSE:     SMBIOS table parsing functions
  * PROGRAMMERS: Timo Kreuzer (timo.kreuzer@reactos.org)
  */
 
-#ifndef DMILIB_H
-#define DMILIB_H
+#pragma once
 
 enum _ID_STRINGS
 {
@@ -35,5 +33,3 @@ ParseSMBiosTables(
     _In_reads_bytes_(TableSize) PVOID SMBiosTables,
     _In_ ULONG TableSize,
     _Inout_updates_(ID_STRINGS_MAX) PCHAR * Strings);
-
-#endif /* DMILIB_H */
--- sdk/lib/udmihelp/udmihelp.h
+++ sdk/lib/udmihelp/udmihelp.h
@@ -5,8 +5,7 @@
  * COPYRIGHT:   Copyright 2018 Stanislav Motylkov
  */
 
-#ifndef UDMIHELP_H
-#define UDMIHELP_H
+#pragma once
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,4 +36,3 @@ FreeSMBiosData(
 } // extern "C"
 #endif
 
-#endif /* UDMIHELP_H */
