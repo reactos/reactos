97b7f9d0408d75b29db9f0eae3fad2cc80fa413c
David L Bean <d_bean@hotmail.com>
2023-07-28 17:32:20 -0400

    Update dll/win32/kernel32/client/proc.c
    Co-authored-by: Joachim Henze <joachim.henze@reactos.org>

--- dll/win32/kernel32/client/proc.c
+++ dll/win32/kernel32/client/proc.c
@@ -4579,7 +4579,7 @@ StartScan:
 													    DPRINT("CreateProcessInternalW: handlevalue used as socket is: %lx\n", handleInfo.HandleValue);
     													DPRINT("CreateProcessInternalW: CatalogId returned is %x\n", SharedSockets[sockindex].dwCatalogEntryId);
 	    												DPRINT("CreateProcessInternalW: SocketType returned is %x\n", SharedSockets[sockindex].iSocketType );
-		    											DPRINT("CreateProcessInternalW: Protocol returned is %x\n", SharedSockets[sockindex].iProtocol );
+		    											DPRINT("CreateProcessInternalW: Protocol returned is %x\n", SharedSockets[sockindex].iProtocol);
 			    										DPRINT("CreateProcessInternalW: dwProviderReserved returned is %x\n", SharedSockets[sockindex].dwProviderReserved);
 				    									// DuplicateSocket populates these for us, we can use dwProviderReserved item to carry handle value and be our flag that socket is inherited.
 					    								SharedSockets[sockindex].dwProviderReserved = handleInfo.HandleValue;
