828aafb7ebc0d7272c6f1611060712bb82d55030
Jesus Sanz del Rey <jesussanz2003@gmail.com>
2023-07-29 05:27:20 +0200

    Change license header to new format

--- base/shell/explorer/taskswnd.cpp
+++ base/shell/explorer/taskswnd.cpp
@@ -1,21 +1,9 @@
 /*
- * ReactOS Explorer
- *
- * Copyright 2006 - 2007 Thomas Weidenmueller <w3seek@reactos.org>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * PROJECT:     ReactOS Explorer
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.0-or-later)
+ * PURPOSE:     Handles all taskbar related stuff like taskbar grouping and taskbar buttons
+ * COPYRIGHT:   Copyright 2006 - 2007 Thomas Weidenmueller <w3seek@reactos.org>
+ *              Copyright 2023 Jesús Sanz del Rey <jesussanz2003@gmail.com>
  */
 
 #include "precomp.h"
