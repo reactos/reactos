c08d95464e55a17734dd10731d5b1eb9e4a936b8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-08-04 12:53:13 +0900

    [SHELL32][SHELL32_APITEST][SDK] Implement SheRemoveQuoteA/W

--- sdk/include/reactos/undocshell.h
+++ sdk/include/reactos/undocshell.h
@@ -651,6 +651,9 @@ BOOL WINAPI GUIDFromStringW(
     _Out_  LPGUID pguid
     );
 
+LPSTR WINAPI SheRemoveQuotesA(LPSTR psz);
+LPWSTR WINAPI SheRemoveQuotesW(LPWSTR psz);
+
 /*****************************************************************************
  * Shell32 resources
  */
