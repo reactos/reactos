2d1821b753f12c9b4c0c2ca8037671f05157ee96
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2023-08-13 21:47:01 +0200

    [NTUSER][USER32] Remove 1+3 remnant defines
    IS_SYSTEM_MENU():
    "moved" to ntuser on 6dfa71c (r68904).
    
    IS_SYSTEM_POPUP():
    uselessly added on 6fc29cc (r8195).
    
    IS_BITMAP_ITEM():
    uses removed on 07b6ddc (r23221).
    uselessly copied to ntuser on 6dfa71c (r68904).

--- win32ss/user/user32/windows/menu.c
+++ win32ss/user/user32/windows/menu.c
@@ -42,14 +42,6 @@ WINE_DEFAULT_DEBUG_CHANNEL(menu);
 #define IS_STRING_ITEM(flags) (MF_STRING == MENU_ITEM_TYPE(flags))
 #define IS_MAGIC_BITMAP(id) ((id) && ((INT_PTR)(id) < 12) && ((INT_PTR)(id) >= -1))
 
-#define IS_SYSTEM_MENU(MenuInfo)  \
-	(0 == ((MenuInfo)->fFlags & MNF_POPUP) && 0 != ((MenuInfo)->fFlags & MNF_SYSMENU))
-
-#define IS_SYSTEM_POPUP(MenuInfo) \
-	(0 != ((MenuInfo)->fFlags & MNF_POPUP) && 0 != ((MenuInfo)->fFlags & MNF_SYSMENU))
-
-#define IS_BITMAP_ITEM(flags) (MF_BITMAP == MENU_ITEM_TYPE(flags))
-
 /*********************************************************************
  * PopupMenu class descriptor
  */
