5bd337905fce48f42ff0952f0a678e0ebd0fb9df
Stanislav Motylkov <x86corez@gmail.com>
2023-08-17 14:12:21 +0300

    Encoding

--- modules/rostests/tests/combotst/combotst.c
+++ modules/rostests/tests/combotst/combotst.c
@@ -7,7 +7,7 @@
 * or make this more complex. Also Fonts are not heavily used.
 
 * This source code is in the PUBLIC DOMAIN and has NO WARRANTY.
-* by Waldo Alvarez CaÃ±izares <wac at ghost.matcom.uh.cu>, started July 11, 2003. */
+* by Waldo Alvarez Cañizares <wac at ghost.matcom.uh.cu>, started July 11, 2003. */
 
 //#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
