050a9b0316630a959d78a6ce5f82258c82342221
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2023-08-17 23:15:55 +0200

    [KERNEL32_VISTA] Improve SetFileInformationByHandle()
    Follow-up to 455f330 (0.4.15-dev-6969).

--- dll/win32/kernel32/kernel32_vista/SetFileInformationByHandle.c
+++ dll/win32/kernel32/kernel32_vista/SetFileInformationByHandle.c
@@ -11,37 +11,66 @@
 
 #include <k32_vista.h>
 
-/* Quick and dirty table for conversion */
-FILE_INFORMATION_CLASS ConvertToFileInfo[MaximumFileInfoByHandleClass] =
+/* Quick and dirty table for conversion
+   from FILE_INFO_BY_HANDLE_CLASS to FILE_INFORMATION_CLASS */
+static
+FILE_INFORMATION_CLASS ConvertInfoToInformation[] =
 {
-    FileBasicInformation, FileStandardInformation, FileNameInformation, FileRenameInformation,
-    FileDispositionInformation, FileAllocationInformation, FileEndOfFileInformation, FileStreamInformation,
-    FileCompressionInformation, FileAttributeTagInformation, FileIdBothDirectoryInformation, (FILE_INFORMATION_CLASS)-1,
-    FileIoPriorityHintInformation, FileRemoteProtocolInformation
+    FileBasicInformation,
+    FileStandardInformation,
+    FileNameInformation,
+    FileRenameInformation,
+    FileDispositionInformation,
+    FileAllocationInformation,
+    FileEndOfFileInformation,
+    FileStreamInformation,
+    FileCompressionInformation,
+    FileAttributeTagInformation,
+    FileIdBothDirectoryInformation,
+    0, // No match for FileIdBothDirectoryRestartInfo
+    FileIoPriorityHintInformation, // Vista || ReactOS
+    FileRemoteProtocolInformation, // Win7 || ReactOS
+    FileFullDirectoryInformation,
+    0, // No match for FileFullDirectoryRestartInfo
+#if (NTDDI_VERSION >= NTDDI_WIN8) || defined(__REACTOS__)
+    0, // No match for FileStorageInfo (Or is it FileStorageReserveIdInformation?)
+    FileAlignmentInformation,
+    FileIdInformation, // Win8 || ReactOS
+    FileIdExtdDirectoryInformation, // Win8 || ReactOS
+    0, // No match for FileIdExtdDirectoryRestartInfo
+#endif
+#if (NTDDI_VERSION >= NTDDI_WIN10_RS1) || defined(__REACTOS__)
+    FileDispositionInformationEx, // Win10rs1 || ReactOS
+    FileRenameInformationEx, // Win10rs1 || ReactOS
+#endif
+#if (NTDDI_VERSION >= NTDDI_WIN10_19H1) || defined(__REACTOS__)
+    FileCaseSensitiveInformation, // Win10rs4 || ReactOS
+    FileNormalizedNameInformation, // Vista || ReactOS
+#endif
 };
+C_ASSERT(_countof(ConvertInfoToInformation) == MaximumFileInfoByHandleClass);
 
 /* Quick implementation, still going farther than Wine implementation */
 BOOL
 WINAPI
-SetFileInformationByHandle(HANDLE hFile,
-                           FILE_INFO_BY_HANDLE_CLASS FileInformationClass,
-                           LPVOID lpFileInformation,
-                           DWORD dwBufferSize)
+SetFileInformationByHandle(
+    _In_ HANDLE hFile,
+    _In_range_(0, MaximumFileInfoByHandleClass - 1) FILE_INFO_BY_HANDLE_CLASS FileInfoClass,
+    _In_ LPVOID lpFileInformation,
+    _In_ DWORD dwBufferSize)
 {
-    NTSTATUS Status;
+    FILE_INFORMATION_CLASS FileInformationClass;
     IO_STATUS_BLOCK IoStatusBlock;
-    FILE_INFORMATION_CLASS FileInfoClass;
-
-    FileInfoClass = (FILE_INFORMATION_CLASS)-1;
+    NTSTATUS Status;
 
     /* Attempt to convert the class */
-    if (FileInformationClass < MaximumFileInfoByHandleClass)
-    {
-        FileInfoClass = ConvertToFileInfo[FileInformationClass];
-    }
+    if (FileInfoClass >= 0 && FileInfoClass < MaximumFileInfoByHandleClass)
+        FileInformationClass = ConvertInfoToInformation[FileInfoClass];
+    else
+        FileInformationClass = 0;
 
     /* If wrong, bail out */
-    if (FileInfoClass == -1)
+    if (FileInformationClass == 0)
     {
         SetLastError(ERROR_INVALID_PARAMETER);
         return FALSE;
@@ -49,8 +78,7 @@ SetFileInformationByHandle(HANDLE hFile,
 
     /* And set the information */
     Status = NtSetInformationFile(hFile, &IoStatusBlock, lpFileInformation,
-                                  dwBufferSize, FileInfoClass);
-
+                                  dwBufferSize, FileInformationClass);
     if (!NT_SUCCESS(Status))
     {
         BaseSetLastNTError(Status);
