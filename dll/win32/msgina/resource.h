4d7c92d22b7f14640e668a8f6f2559126389de48
Carl J. Bialorucki <cbialo2@outlook.com>
2023-08-29 07:36:17 -0600

    Update dll/win32/msgina/resource.h
    Co-authored-by: Stanislav Motylkov <x86corez@gmail.com>

--- dll/win32/msgina/resource.h
+++ dll/win32/msgina/resource.h
@@ -123,6 +123,6 @@
 #define IDS_SHUTDOWN_SLEEP_DESC       50008
 #define IDS_SHUTDOWN_HIBERNATE_DESC   50009
 
-/* Emergency Restart*/
+/* Emergency Restart */
 #define IDS_EMERGENCY_RESTART_TITLE   50010
 #define IDS_EMERGENCY_RESTART         50011
