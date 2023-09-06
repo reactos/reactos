d79131889cb5aec7d54aa1f4b80626a826d4e243
Doug Lyons <douglyons@douglyons.com>
2023-09-06 11:48:54 -0500

    Changes based on reviewer comments

--- modules/rostests/winetests/winhttp/winhttp.c
+++ modules/rostests/winetests/winhttp/winhttp.c
@@ -4276,12 +4276,9 @@ static void test_IWinHttpRequest(int port)
 
     hr = IWinHttpRequest_get_ResponseText( req, &response );
     ok( hr == S_OK, "got %08x\n", hr );
-#if __REACTOS__
-    if (SysStringByteLen(response)) // Returned non-zero length response
-        ok( !memcmp(response, data_start, sizeof(data_start)), "got %s\n",
-            wine_dbgstr_wn(response, min(SysStringByteLen(response) / sizeof(WCHAR), 32)) );
-    else
-        ok(FALSE, "got zero (0) length response\n");
+#ifdef __REACTOS__
+    ok( !memcmp(response, data_start, sizeof(data_start)), "got %s\n",
+        wine_dbgstr_wn(response, min(SysStringLen(response), 32)) );
 #else
     ok( !memcmp(response, data_start, sizeof(data_start)), "got %s\n", wine_dbgstr_wn(response, 32) );
 #endif
