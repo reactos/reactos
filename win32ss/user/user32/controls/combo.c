ea51302b8f280c93476994055b005cfa40e1597a
Joachim Henze <Joachim.Henze@reactos.org>
2023-09-10 21:17:33 +0200

    [USER32] Sync comctl32 combo.c code in context of CORE-17883

--- win32ss/user/user32/controls/combo.c
+++ win32ss/user/user32/controls/combo.c
@@ -1074,8 +1074,7 @@ static void CBDropDown( LPHEADCOMBO lphc )
 
 
    if( !(lphc->wState & CBF_NOREDRAW) )
-     RedrawWindow( lphc->self, NULL, 0, RDW_INVALIDATE |
-			   RDW_ERASE | RDW_UPDATENOW | RDW_NOCHILDREN );
+     RedrawWindow( lphc->self, NULL, 0, RDW_INVALIDATE | RDW_ERASE | RDW_UPDATENOW );
 
    EnableWindow( lphc->hWndLBox, TRUE );
    if (GetCapture() != lphc->self)
