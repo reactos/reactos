3a9c5b5c10477ab92cda5dfc6362624dc71b11d7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-09-10 17:31:18 +0900

    fix _lock_file for NULL

--- sdk/lib/crt/stdio/file.c
+++ sdk/lib/crt/stdio/file.c
@@ -1195,7 +1195,7 @@ void CDECL _lock_file(FILE *file)
     if(file>=_iob && file<_iob+_IOB_ENTRIES)
         _lock(_STREAM_LOCKS+(file-_iob));
     /* ReactOS: string streams dont need to be locked */
-    else if(!(file->_flag & _IOSTRG))
+    else if(file && !(file->_flag & _IOSTRG))
         EnterCriticalSection(&((file_crit*)file)->crit);
 }
 
