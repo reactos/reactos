49881e5afa1bd1e38900a45fcbb337db131f2058
winesync <ros-dev@reactos.org>
2023-09-12 12:11:26 -0700

    [WINESYNC] d3d9: Handle stateblocks in d3d9_device_SetVertexShaderConstantI().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id ceef84084e214b0febf10fec681528404a840a73 by Zebediah Figura <z.figura12@gmail.com>

--- dll/directx/wine/d3d9/device.c
+++ dll/directx/wine/d3d9/device.c
@@ -3515,8 +3515,11 @@ static HRESULT WINAPI d3d9_device_SetVertexShaderConstantI(IDirect3DDevice9Ex *i
     TRACE("iface %p, reg_idx %u, data %p, count %u.\n", iface, reg_idx, data, count);
 
     wined3d_mutex_lock();
-    hr = wined3d_device_set_vs_consts_i(device->wined3d_device,
+    hr = wined3d_stateblock_set_vs_consts_i(device->update_state,
             reg_idx, count, (const struct wined3d_ivec4 *)data);
+    if (SUCCEEDED(hr) && !device->recording)
+        hr = wined3d_device_set_vs_consts_i(device->wined3d_device,
+                reg_idx, count, (const struct wined3d_ivec4 *)data);
     wined3d_mutex_unlock();
 
     return hr;
