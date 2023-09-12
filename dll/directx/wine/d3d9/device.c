25a93f4a493235e53bccd43660cdb7588f0246ac
winesync <ros-dev@reactos.org>
2023-09-12 11:42:38 -0700

    [WINESYNC] d3d9: Do not add map access indiscriminately in d3d9_device_create_surface().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 84f64598441ea59f83cc49ac471119312cdbd4db by Henri Verbeet <hverbeet@codeweavers.com>

--- dll/directx/wine/d3d9/device.c
+++ dll/directx/wine/d3d9/device.c
@@ -1399,8 +1399,7 @@ static HRESULT d3d9_device_create_surface(struct d3d9_device *device, UINT width
     if (pool == D3DPOOL_SCRATCH)
         desc.usage |= WINED3DUSAGE_SCRATCH;
     desc.bind_flags = wined3d_bind_flags_from_d3d9_usage(usage);
-    desc.access = wined3daccess_from_d3dpool(pool, usage)
-            | WINED3D_RESOURCE_ACCESS_MAP_R | WINED3D_RESOURCE_ACCESS_MAP_W;
+    desc.access = wined3daccess_from_d3dpool(pool, usage);
     desc.width = width;
     desc.height = height;
     desc.depth = 1;
