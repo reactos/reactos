aaf15c49840e7788e51103beb2b6d8fd55e6b4b4
winesync <ros-dev@reactos.org>
2023-09-12 11:42:44 -0700

    [WINESYNC] d3d9: Disallow creating managed 2D textures on d3d9ex devices.
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id d511b67da3a0fc4869c2ddfd7a5308f9de107655 by Henri Verbeet <hverbeet@codeweavers.com>

--- dll/directx/wine/d3d9/texture.c
+++ dll/directx/wine/d3d9/texture.c
@@ -1301,6 +1301,12 @@ HRESULT texture_init(struct d3d9_texture *texture, struct d3d9_device *device,
     DWORD flags = 0;
     HRESULT hr;
 
+    if (pool == D3DPOOL_MANAGED && device->d3d_parent->extended)
+    {
+        WARN("Managed resources are not supported by d3d9ex devices.\n");
+        return D3DERR_INVALIDCALL;
+    }
+
     texture->IDirect3DBaseTexture9_iface.lpVtbl = (const IDirect3DBaseTexture9Vtbl *)&d3d9_texture_2d_vtbl;
     d3d9_resource_init(&texture->resource);
     list_init(&texture->rtv_list);
