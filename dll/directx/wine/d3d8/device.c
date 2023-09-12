ef709a0af8d4f8d1539e8ca2398e047b7e75d689
winesync <ros-dev@reactos.org>
2023-09-12 12:11:26 -0700

    [WINESYNC] d3d8: Handle stateblocks in d3d8_device_SetPixelShader().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 041181f1dcf83425718fd97581343379a5ce058c by Zebediah Figura <z.figura12@gmail.com>

--- dll/directx/wine/d3d8/device.c
+++ dll/directx/wine/d3d8/device.c
@@ -3144,7 +3144,9 @@ static HRESULT WINAPI d3d8_device_SetPixelShader(IDirect3DDevice8 *iface, DWORD 
 
     if (!shader)
     {
-        wined3d_device_set_pixel_shader(device->wined3d_device, NULL);
+        wined3d_stateblock_set_pixel_shader(device->update_state, NULL);
+        if (!device->recording)
+            wined3d_device_set_pixel_shader(device->wined3d_device, NULL);
         wined3d_mutex_unlock();
         return D3D_OK;
     }
@@ -3157,7 +3159,9 @@ static HRESULT WINAPI d3d8_device_SetPixelShader(IDirect3DDevice8 *iface, DWORD 
     }
 
     TRACE("Setting shader %p.\n", shader_impl);
-    wined3d_device_set_pixel_shader(device->wined3d_device, shader_impl->wined3d_shader);
+    wined3d_stateblock_set_pixel_shader(device->update_state, shader_impl->wined3d_shader);
+    if (!device->recording)
+        wined3d_device_set_pixel_shader(device->wined3d_device, shader_impl->wined3d_shader);
     wined3d_mutex_unlock();
 
     return D3D_OK;
