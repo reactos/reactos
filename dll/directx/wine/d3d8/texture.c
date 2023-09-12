85afd26478935fb10f85070b2180792c3a0d715b
winesync <ros-dev@reactos.org>
2023-09-12 11:42:16 -0700

    [WINESYNC] d3d8: Do not specify WINED3D_TEXTURE_CREATE_MAPPABLE in texture_init().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id cfebf9e260d045774a9a42a0836b369a3cb65d88 by Henri Verbeet <hverbeet@codeweavers.com>

--- dll/directx/wine/d3d8/texture.c
+++ dll/directx/wine/d3d8/texture.c
@@ -1117,9 +1117,6 @@ HRESULT texture_init(struct d3d8_texture *texture, struct d3d8_device *device,
     desc.depth = 1;
     desc.size = 0;
 
-    if (pool != D3DPOOL_DEFAULT || (usage & D3DUSAGE_DYNAMIC))
-        flags |= WINED3D_TEXTURE_CREATE_MAPPABLE;
-
     if (!levels)
         levels = wined3d_log2i(max(width, height)) + 1;
 
