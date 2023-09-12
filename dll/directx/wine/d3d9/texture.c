87635215eda5cacac254127a206dc3fed596e44e
winesync <ros-dev@reactos.org>
2023-09-12 11:42:36 -0700

    [WINESYNC] d3d9: Do not add map access indiscriminately in texture_init().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 949dbbd31f450178c90ea8267097a975b77c3219 by Henri Verbeet <hverbeet@codeweavers.com>

--- dll/directx/wine/d3d9/texture.c
+++ dll/directx/wine/d3d9/texture.c
@@ -1314,8 +1314,7 @@ HRESULT texture_init(struct d3d9_texture *texture, struct d3d9_device *device,
     if (pool == D3DPOOL_SCRATCH)
         desc.usage |= WINED3DUSAGE_SCRATCH;
     desc.bind_flags = wined3d_bind_flags_from_d3d9_usage(usage) | WINED3D_BIND_SHADER_RESOURCE;
-    desc.access = wined3daccess_from_d3dpool(pool, usage)
-            | WINED3D_RESOURCE_ACCESS_MAP_R | WINED3D_RESOURCE_ACCESS_MAP_W;
+    desc.access = wined3daccess_from_d3dpool(pool, usage);
     desc.width = width;
     desc.height = height;
     desc.depth = 1;
