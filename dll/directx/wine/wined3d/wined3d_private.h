f6af73386c32f43bda54789d8c09d7bf08f269d6
winesync <ros-dev@reactos.org>
2023-09-14 09:23:18 -0700

    [WINESYNC] wined3d: Derive format component counts from the component info.
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 19a532ca0abbd288340f05e032794bb0a7f16120 by Henri Verbeet <hverbeet@codeweavers.com>

--- dll/directx/wine/wined3d/wined3d_private.h
+++ dll/directx/wine/wined3d/wined3d_private.h
@@ -4386,6 +4386,7 @@ struct wined3d_format
     enum wined3d_format_id id;
 
     D3DDDIFORMAT ddi_format;
+    unsigned int component_count;
     DWORD red_size;
     DWORD green_size;
     DWORD blue_size;
@@ -4403,7 +4404,6 @@ struct wined3d_format
     UINT block_byte_count;
 
     enum wined3d_ffp_emit_idx emit_idx;
-    GLint component_count;
     GLenum gl_vtx_type;
     GLint gl_vtx_format;
 
