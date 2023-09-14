48d1f6319115ed1ed6fe9a5f949b9261b6c71de6
winesync <ros-dev@reactos.org>
2023-09-14 16:06:43 +0200

    [WINESYNC] setupapi: Move prototypes not in Windows's setupapi.h to setupapi_private.h.
    And delete the ones we don't need.
    
    wine commit id 1172e66e5b7fa96decf89f8866e71b77e5773ec7 by Alex Henrie <alexhenrie24@gmail.com>
    
    + ReactOS: Private exports prototypes are added in sdk setupapi_undoc.h
    
    Co-authored-by: Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>

--- sdk/include/reactos/setupapi_undoc.h
+++ sdk/include/reactos/setupapi_undoc.h
@@ -33,6 +33,50 @@ WINSETUPAPI BOOL   WINAPI pSetupStringTableSetExtraData(HSTRING_TABLE, DWORD, LP
 WINSETUPAPI LPWSTR WINAPI pSetupStringTableStringFromId(HSTRING_TABLE, DWORD);
 WINSETUPAPI BOOL   WINAPI pSetupStringTableStringFromIdEx(HSTRING_TABLE, DWORD, LPWSTR, LPDWORD);
 
+
+WINSETUPAPI LONG WINAPI AddTagToGroupOrderList(PCWSTR lpGroupName, DWORD dwUnknown2, DWORD dwUnknown3); // Not exported
+WINSETUPAPI VOID WINAPI AssertFail(LPSTR, UINT, LPSTR);                                                 // Not exported
+WINSETUPAPI DWORD WINAPI CaptureStringArg(PCWSTR lpSrc, PWSTR* lpDst);                                  // Not exported
+WINSETUPAPI BOOL WINAPI DelayedMove(PCWSTR lpExistingFileName, PCWSTR lpNewFileName);                   // Not exported
+WINSETUPAPI BOOL WINAPI DoesUserHavePrivilege(PCWSTR lpPrivilegeName);
+WINSETUPAPI PWSTR WINAPI DuplicateString(PCWSTR lpSrc);                                                 // Not exported
+WINSETUPAPI BOOL WINAPI EnablePrivilege(PCWSTR lpPrivilegeName, BOOL bEnable);                          // Not exported
+
+WINSETUPAPI BOOL WINAPI FileExists(PCWSTR lpFileName, PWIN32_FIND_DATAW lpFileFindData);                // Not exported
+WINSETUPAPI DWORD WINAPI GetSetFileTimestamp(PCWSTR, PFILETIME, PFILETIME, PFILETIME, BOOLEAN);         // Not exported
+
+WINSETUPAPI BOOL WINAPI IsUserAdmin(VOID);
+WINSETUPAPI PWSTR WINAPI MultiByteToUnicode(PCSTR lpMultiByteStr, UINT uCodePage);                      // Not exported
+WINSETUPAPI VOID WINAPI MyFree(PVOID lpMem);
+WINSETUPAPI PVOID WINAPI MyMalloc(DWORD dwSize);
+WINSETUPAPI PVOID WINAPI MyRealloc(PVOID lpSrc, DWORD dwSize);
+WINSETUPAPI DWORD WINAPI OpenAndMapForRead(PCWSTR, PDWORD, PHANDLE, PHANDLE, PVOID*);                   // Not exported
+WINSETUPAPI LONG WINAPI QueryRegistryValue(HKEY, PCWSTR, PBYTE*, PDWORD, PDWORD);                       // Not exported
+/* RetreiveFileSecurity is not a typo, as per Microsoft's dlls */
+WINSETUPAPI DWORD WINAPI RetreiveFileSecurity(PCWSTR, PSECURITY_DESCRIPTOR*);                           // Not exported
+WINSETUPAPI DWORD WINAPI StampFileSecurity(PCWSTR, PSECURITY_DESCRIPTOR);                               // Not exported
+WINSETUPAPI DWORD WINAPI TakeOwnershipOfFile(PCWSTR);                                                   // Not exported
+WINSETUPAPI PSTR WINAPI UnicodeToMultiByte(PCWSTR lpUnicodeStr, UINT uCodePage);
+WINSETUPAPI BOOL WINAPI UnmapAndCloseFile(HANDLE, HANDLE, PVOID);                                       // Not exported
+
+
+WINSETUPAPI DWORD WINAPI pSetupCaptureAndConvertAnsiArg(PCSTR, PWSTR*);
+WINSETUPAPI VOID WINAPI pSetupCenterWindowRelativeToParent(HWND);
+WINSETUPAPI BOOL WINAPI pSetupConcatenatePaths(LPWSTR, LPCWSTR, DWORD, LPDWORD);
+WINSETUPAPI PWSTR WINAPI pSetupDuplicateString(PCWSTR);
+WINSETUPAPI BOOL WINAPI pSetupEnablePrivilege(PCWSTR, BOOL);
+WINSETUPAPI PWSTR WINAPI pSetupGetFileTitle(PCWSTR);
+WINSETUPAPI BOOL WINAPI pSetupGetVersionInfoFromImage(LPWSTR, PULARGE_INTEGER, LPWORD);
+WINSETUPAPI DWORD WINAPI pSetupGuidFromString(PCWSTR, LPGUID);
+WINSETUPAPI BOOL WINAPI pSetupIsGuidNull(LPGUID);
+WINSETUPAPI BOOL WINAPI pSetupIsUserAdmin(VOID);
+WINSETUPAPI PWSTR WINAPI pSetupMultiByteToUnicode(PCSTR, UINT);
+WINSETUPAPI DWORD WINAPI pSetupOpenAndMapForRead(PCWSTR, PDWORD, PHANDLE, PHANDLE, PVOID*);
+WINSETUPAPI DWORD WINAPI pSetupStringFromGuid(LPGUID, PWSTR, DWORD);
+
+WINSETUPAPI PSTR WINAPI pSetupUnicodeToMultiByte(PCWSTR lpUnicodeStr, UINT uCodePage);
+WINSETUPAPI BOOL WINAPI pSetupUnmapAndCloseFile(HANDLE, HANDLE, PVOID);
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
