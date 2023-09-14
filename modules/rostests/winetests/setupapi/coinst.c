5081550642df238d7abe6bc9541c882564c0449d
winesync <ros-dev@reactos.org>
2023-09-14 19:55:52 +0200

    [WINESYNC] setupapi: Implement custom class installers in SetupDiCallClassInstaller().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id fce26e60cc2400f3f3da27c221dce95593dd400e by Zebediah Figura <z.figura12@gmail.com>

---
+++ modules/rostests/winetests/setupapi/coinst.c
@@ -0,0 +1,51 @@
+/*
+ * Test co-installer and class installer DLL
+ *
+ * Copyright 2018 Zebediah Figura
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+#include "windef.h"
+#include "winbase.h"
+#include "winuser.h"
+#include "winreg.h"
+#include "setupapi.h"
+
+unsigned int callback_count;
+DI_FUNCTION last_message;
+
+DWORD WINAPI class_success(DI_FUNCTION function, HDEVINFO set, SP_DEVINFO_DATA *device)
+{
+    callback_count++;
+    last_message = function;
+    return ERROR_SUCCESS;
+}
+
+DWORD WINAPI ClassInstall(DI_FUNCTION function, HDEVINFO set, SP_DEVINFO_DATA *device)
+{
+    return class_success(function, set, device);
+}
+
+DWORD WINAPI class_default(DI_FUNCTION function, HDEVINFO set, SP_DEVINFO_DATA *device)
+{
+    return ERROR_DI_DO_DEFAULT;
+}
+
+DWORD WINAPI class_error(DI_FUNCTION function, HDEVINFO set, SP_DEVINFO_DATA *device)
+{
+    return 0xdeadbeef;
+}
---
+++ modules/rostests/winetests/setupapi/coinst.spec
@@ -0,0 +1,6 @@
+@ stdcall class_success(long ptr ptr)
+@ stdcall ClassInstall(long ptr ptr)
+@ stdcall class_default(long ptr ptr)
+@ stdcall class_error(long ptr ptr)
+@ extern callback_count
+@ extern last_message
