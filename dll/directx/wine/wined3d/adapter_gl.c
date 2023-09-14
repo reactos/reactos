973450ea73b36cc6806cdff1ce25a455b2bc6efc
winesync <ros-dev@reactos.org>
2023-09-14 10:54:18 -0700

    [WINESYNC] wined3d: Use real values for memory accounting on NVIDIA cards.
    FIXME: Reimplement wined3d_device_get_available_texture_mem
           without using the context on the main thread.
    
    wine-staging patch by Michael Müller <michael@fds-team.de>

--- dll/directx/wine/wined3d/adapter_gl.c
+++ dll/directx/wine/wined3d/adapter_gl.c
@@ -228,6 +228,7 @@ static const struct wined3d_extension_map gl_extension_map[] =
     {"GL_NV_vertex_program2",               NV_VERTEX_PROGRAM2            },
     {"GL_NV_vertex_program2_option",        NV_VERTEX_PROGRAM2_OPTION     },
     {"GL_NV_vertex_program3",               NV_VERTEX_PROGRAM3            },
+    {"GL_NVX_gpu_memory_info",              NVX_GPU_MEMORY_INFO           },
 };
 
 static const struct wined3d_extension_map wgl_extension_map[] =
@@ -1056,6 +1057,17 @@ static const struct wined3d_gpu_description *query_gpu_description(const struct 
 
         TRACE("Card reports vendor PCI ID 0x%04x, device PCI ID 0x%04x, 0x%s bytes of video memory.\n",
                 vendor, device, wine_dbgstr_longlong(*vram_bytes));
+
+        gpu_description = wined3d_get_gpu_description(vendor, device);
+    }
+    else if (gl_info->supported[NVX_GPU_MEMORY_INFO])
+    {
+        GLint vram_kb;
+        gl_info->gl_ops.gl.p_glGetIntegerv(GL_GPU_MEMORY_INFO_DEDICATED_VIDMEM_NVX, &vram_kb);
+
+        *vram_bytes = (UINT64)vram_kb * 1024;
+        TRACE("Got 0x%s as video memory from NVX_GPU_MEMORY_INFO extension.\n",
+                wine_dbgstr_longlong(*vram_bytes));
 
         gpu_description = wined3d_get_gpu_description(vendor, device);
     }
