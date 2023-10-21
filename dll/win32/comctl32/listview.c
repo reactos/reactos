855008d97b2c7e1a1b72ad268b68d83c739ebc6d
Jesús Sanz del Rey <jesussanz2003@gmail.com>
2023-10-21 18:51:54 +0200

    [COMCTL32] Invalidate LVIS_CUT selected items too (#4218)
    This fixes icons that should be drawn transparently not showing as such until window gets repainted.
    For example, hidden shell folders that are not drawn transparently until the filebrowser window is resized...
    
    CORE-16722

--- dll/win32/comctl32/listview.c
+++ dll/win32/comctl32/listview.c
@@ -2276,7 +2276,11 @@ static void LISTVIEW_InvalidateSelectedItems(const LISTVIEW_INFO *infoPtr)
     iterator_frameditems(&i, infoPtr, &infoPtr->rcList); 
     while(iterator_next(&i))
     {
+#ifdef __REACTOS__
+	if (LISTVIEW_GetItemState(infoPtr, i.nItem, LVIS_SELECTED | LVIS_CUT))
+#else
 	if (LISTVIEW_GetItemState(infoPtr, i.nItem, LVIS_SELECTED))
+#endif
 	    LISTVIEW_InvalidateItem(infoPtr, i.nItem);
     }
     iterator_destroy(&i);
