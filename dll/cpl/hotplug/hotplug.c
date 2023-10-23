ea4424c61b72186cc87d806379521c55e9b98df3
Thamatip Chitpong <thamatip.chitpong@reactos.org>
2023-10-23 21:16:00 +0700

    [HOTPLUG] Add device removal function

--- dll/cpl/hotplug/hotplug.c
+++ dll/cpl/hotplug/hotplug.c
@@ -407,6 +407,23 @@ ShowDeviceProperties(
     HeapFree(GetProcessHeap(), 0, pszDevId);
 }
 
+static
+VOID
+SafeRemoveDevice(
+    _In_ DEVINST DevInst)
+{
+    PNP_VETO_TYPE VetoType = PNP_VetoTypeUnknown;
+    CONFIGRET cr;
+
+    cr = CM_Request_Device_EjectW(DevInst, &VetoType, NULL, 0, 0);
+    if (cr != CR_SUCCESS && VetoType == PNP_VetoTypeUnknown)
+    {
+        WCHAR szError[64];
+        swprintf(szError, L"Failed to remove device (0x%x)", cr);
+        MessageBoxW(NULL, szError, NULL, MB_ICONEXCLAMATION | MB_OK);
+    }
+}
+
 INT_PTR
 CALLBACK
 SafeRemovalDlgProc(
@@ -503,6 +520,14 @@ SafeRemovalDlgProc(
                 {
                     HWND hwndDevTree = GetDlgItem(hwndDlg, IDC_SAFE_REMOVE_DEVICE_TREE);
                     ShowDeviceProperties(hwndDlg, GetSelectedDeviceInst(hwndDevTree));
+                    break;
+                }
+
+                case IDC_SAFE_REMOVE_STOP:
+                case IDM_STOP:
+                {
+                    HWND hwndDevTree = GetDlgItem(hwndDlg, IDC_SAFE_REMOVE_DEVICE_TREE);
+                    SafeRemoveDevice(GetSelectedDeviceInst(hwndDevTree));
                     break;
                 }
             }
