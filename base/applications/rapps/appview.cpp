5f8567f1677cc3b51866fd59138deb78e6a9e6af
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-11-08 08:46:27 +0900

    fix as (HICON)1

--- base/applications/rapps/appview.cpp
+++ base/applications/rapps/appview.cpp
@@ -1300,7 +1300,7 @@ CAppsListView::AddApplication(CAppInfo *AppInfo, BOOL InitialCheckState)
         }
 
         /* Use the default icon if none were found in the file, or if it is not supported (returned 1) */
-        if (!hIcon || (hIcon == (HICON)(UINT_PTR)1))
+        if (!hIcon || (hIcon == (HICON)1))
         {
             /* Load default icon */
             hIcon = LoadIconW(hInst, MAKEINTRESOURCEW(IDI_MAIN));
