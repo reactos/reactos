7aa8d4b9199c7a799e49bca934a56e26fdbb69fa
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-11-14 08:08:11 +0900

    fix 2

--- dll/cpl/access/general.c
+++ dll/cpl/access/general.c
@@ -196,7 +196,7 @@ GeneralPageProc(HWND hwndDlg,
                            IDC_RESET_BOX,
                            pGlobalData->accessTimeout.dwFlags & ATF_TIMEOUTON ? BST_CHECKED : BST_UNCHECKED);
             FillResetComboBox(GetDlgItem(hwndDlg, IDC_RESET_COMBO));
-            pGlobalData->accessTimeout.iTimeOutMSec = max(pGlobalData->accessTimeout.iTimeOutMSec, FIVE_MINS_IN_MS);
+            pGlobalData->accessTimeout.iTimeOutMSec = max(FIVE_MINS_IN_MS, pGlobalData->accessTimeout.iTimeOutMSec);
             iCurSel = (pGlobalData->accessTimeout.iTimeOutMSec / FIVE_MINS_IN_MS) - 1;
             SendDlgItemMessage(hwndDlg, IDC_RESET_COMBO, CB_SETCURSEL, iCurSel, 0);
             EnableWindow(GetDlgItem(hwndDlg, IDC_RESET_COMBO),
