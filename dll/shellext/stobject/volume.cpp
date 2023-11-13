f5f02478ffbcaf9b80d1a962bd44a0fd198f9eeb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-11-14 08:31:51 +0900

    [STOBJECT] Remove #if 0 and #endif in Volume_IsMute

--- dll/shellext/stobject/volume.cpp
+++ dll/shellext/stobject/volume.cpp
@@ -111,7 +111,6 @@ static HRESULT __stdcall Volume_FindMixerControl(CSysTray * pSysTray)
 
 HRESULT Volume_IsMute()
 {
-#if 0
     MIXERCONTROLDETAILS mixerControlDetails;
 
     if (g_mixerId != (UINT)-1 && g_muteControlID != (DWORD)-1)
@@ -130,7 +129,7 @@ HRESULT Volume_IsMute()
 
         g_IsMute = detailsResult != 0;
     }
-#endif
+
     return S_OK;
 }
 
