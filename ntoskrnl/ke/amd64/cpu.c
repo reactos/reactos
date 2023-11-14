e3de830c5b29c07adf30b6f4b4ee4f795d0c1dfe
Timo Kreuzer <timo.kreuzer@reactos.org>
2023-11-14 19:53:05 +0200

    [NTOS:KE/x64] Don't call KiGetCpuVendor from KiGetCacheInformation
    Use the field in the PRCB instead.

--- ntoskrnl/ke/amd64/cpu.c
+++ ntoskrnl/ke/amd64/cpu.c
@@ -427,7 +427,6 @@ NTAPI
 KiGetCacheInformation(VOID)
 {
     PKIPCR Pcr = (PKIPCR)KeGetPcr();
-    ULONG Vendor;
     ULONG CacheRequests = 0, i;
     ULONG CurrentRegister;
     UCHAR RegisterByte;
@@ -437,12 +436,8 @@ KiGetCacheInformation(VOID)
     /* Set default L2 size */
     Pcr->SecondLevelCacheSize = 0;
 
-    /* Get the Vendor ID and make sure we support CPUID */
-    Vendor = KiGetCpuVendor();
-    if (!Vendor) return;
-
     /* Check the Vendor ID */
-    switch (Vendor)
+    switch (Pcr->Prcb.CpuVendor)
     {
         /* Handle Intel case */
         case CPU_INTEL:
