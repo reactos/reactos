294eb31cfd00571f09449859a4177b0271f91635
Timo Kreuzer <timo.kreuzer@reactos.org>
2023-11-14 16:51:04 +0200

    [NTOS:KE/x64] Handle extended processor state on context switch

--- ntoskrnl/ke/amd64/thrdini.c
+++ ntoskrnl/ke/amd64/thrdini.c
@@ -167,8 +167,20 @@ KiSwapContextResume(
     PKPROCESS OldProcess, NewProcess;
 
     /* Setup ring 0 stack pointer */
-    Pcr->TssBase->Rsp0 = (ULONG64)NewThread->InitialStack; // FIXME: NPX save area?
+    Pcr->TssBase->Rsp0 = (ULONG64)NewThread->InitialStack;
     Pcr->Prcb.RspBase = Pcr->TssBase->Rsp0;
+
+    /* Save old thread's extended state */
+    if (OldThread->NpxState != 0)
+    {
+        KiSaveXState(OldThread->StateSaveArea, OldThread->NpxState);
+    }
+
+    /* Load new thread's extended state */
+    if (NewThread->NpxState != 0)
+    {
+        KiRestoreXState(NewThread->StateSaveArea, NewThread->NpxState);
+    }
 
     /* Now we are the new thread. Check if it's in a new process */
     OldProcess = OldThread->ApcState.Process;
