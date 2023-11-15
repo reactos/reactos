bd0a5498b04cf77e611cb6526fd88459be2a7b19
Ratin Gao <ratin@knsoft.org>
2023-11-16 03:59:32 +0800

    [NTDLL:LDR] Little fixes about SAL annotation and behavior (#5793)
    - Uniform function declarations in .c and .h with fixes and SAL2 improvements.
    - Use LDR_[UN]LOCK_LOADER_LOCK_FLAG_XXX correctly.
    - Fix incorrect UsedSize calculation in LdrQueryProcessModuleInformationEx (caught by ThFabba).
    - Whatever the callback request stop the enumeration or not in LdrEnumerateLoadedModules, the following operations should be the same.
    - Fix 2 incorrect DPRINT1 that printed incorrect parameter.
    - Return error if RtlAllocateHeap failed in LdrpGetProcedureAddress, and add comments about NT6.2 new changes.

--- sdk/include/ndk/ldrfuncs.h
+++ sdk/include/ndk/ldrfuncs.h
@@ -97,8 +97,8 @@ LdrGetProcedureAddress(
 ULONG
 NTAPI
 LdrRelocateImage(
-    _In_ PVOID NewBase,
-    _In_ PCCH LoaderName,
+    _In_ PVOID BaseAddress,
+    _In_opt_ PCSTR LoaderName,
     _In_ ULONG Success,
     _In_ ULONG Conflict,
     _In_ ULONG Invalid
@@ -139,9 +139,9 @@ LdrProcessRelocationBlockLongLong(
 NTSTATUS
 NTAPI
 LdrEnumerateLoadedModules(
-    _In_ BOOLEAN ReservedFlag,
+    _Reserved_ ULONG ReservedFlag,
     _In_ PLDR_ENUM_CALLBACK EnumProc,
-    _In_ PVOID Context
+    _In_opt_ PVOID Context
 );
 
 #endif
