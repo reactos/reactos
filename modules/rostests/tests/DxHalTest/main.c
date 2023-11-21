b0e1a6d831453b1ac5a01c19e1396c1da17d6aa1
Andrew Shelkovenko <diakin@yandex.ru>
2023-11-21 06:42:28 +0300

    Update modules/rostests/tests/DxHalTest/main.c
    Co-authored-by: Carl J. Bialorucki <cbialo2@outlook.com>

--- modules/rostests/tests/DxHalTest/main.c
+++ modules/rostests/tests/DxHalTest/main.c
@@ -106,7 +106,7 @@ int WINAPI WinMain (HINSTANCE hInst, HINSTANCE hPrevInst,
     printf("This code may require more comments to know how stuff works.\n");
 	printf("Best regards, Magnus Olsen magnus@greatlord.com or greatlord@reactos.org\n");
     printf("Copyright 2006 by Magnus Olsen\n\n");
-    printf("This demo shows how to start dx draw hal and create a primary surface,\n");
+    printf("This demo shows how to start DirectDraw, creating a primary surface\n");
 	printf("and an overlay surface and blt to the primary surface.\n");
 
 
