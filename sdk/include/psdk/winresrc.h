3e83562aa7e0116e9345d1b02cb829efcfb64071
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2023-11-23 15:21:05 +0100

    [PSDK] Sync winresrc.h with wine-8.20
    It is also compatible with MS PSDK file.
    
    NOTE: I have observed that at least one .rh file (commctrl.rh) we have
    from Wine, is not yet fully compatible with its MS PSDK counterpart
    (for example it misses the TBSTYLE_* defines).

--- sdk/include/psdk/winresrc.h
+++ sdk/include/psdk/winresrc.h
@@ -1,11 +1,44 @@
+/*
+ * Copyright (C) 2002 Alexandre Julliard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
 #ifndef _WINRESRC_
 #define _WINRESRC_
 
-#define WIN32_LEAN_AND_MEAN
+#ifndef WINVER
+#define WINVER 0x0500
+#endif
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0501
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0410
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0500
+#endif
+
 #include <winuser.rh>
-#include <winnt.rh>
 #include <commctrl.rh>
 #include <dde.rh>
+#include <winnt.rh>
 #include <dlgs.h>
 #include <winver.h>
 
