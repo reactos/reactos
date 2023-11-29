078568faa0915b4d01d15454c93d0cd9a83c1567
Whindmar Saksit <whindsaks@proton.me>
2023-11-29 08:25:34 +0100

    [SHELL32] Support protocols and EditFlags in File Types dialog

--- dll/win32/shell32/precomp.h
+++ dll/win32/shell32/precomp.h
@@ -153,4 +153,13 @@ public:
     END_MSG_MAP()
 };
 
+// SHExtractIconsW is a forward, use this function instead inside shell32
+static inline HICON
+SHELL32_SHExtractIcon(LPCWSTR File, int Index, int cx, int cy)
+{
+    HICON hIco;
+    int r = PrivateExtractIconsW(File, Index, cx, cy, &hIco, NULL, 1, 0);
+    return r > 0 ? hIco : NULL;
+}
+
 #endif /* _PRECOMP_H__ */
