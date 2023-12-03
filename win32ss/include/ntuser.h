0fa9fa9b043f10541246c68a3930d2bafaaf4ddf
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-03 12:31:50 +0900

    improve 2

--- win32ss/include/ntuser.h
+++ win32ss/include/ntuser.h
@@ -305,7 +305,7 @@ typedef struct _CALLBACKWND
 #define CI_IMMACTIVATE       0x00000040 /* IMM/IME (Asian input) */
 #define CI_CTFCOINIT         0x00000080 /* Did CTF CoInitialize? */
 #define CI_CTFTIM            0x00000100 /* CTF Thread Input Manager (TIM) */
-#define CI_CICERO_STARTED    0x00000200 /* Is CTF thread started in the thread? */
+#define CI_CICERO_STARTED    0x00000200 /* Is Cicero started in the thread? */
 #define CI_TSFDISABLED       0x00000400 /* TSF (Text Services Framework a.k.a. Cicero) */
 #define CI_AIMMACTIVATED     0x00000800 /* Active IMM (AIMM) */
 
