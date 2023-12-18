465ccc36711734f097e40aba7a04e70070353d0f
Ratin Gao <ratin@knsoft.org>
2023-12-19 01:35:30 +0800

    [NTDLL][ADVAPI32][ADVAPI32_APITEST][CRYPTLIB] Add hash API exports for NT6+ and apitests with small fixes
    - For NT6.0+, NTDLL exports those hash APIs that ADVAPI32 thunks forward to
    - Fix undocumented context structure layout and content
    - Add apitests to verify both of hash result and structure content

--- sdk/lib/cryptlib/md5.c
+++ sdk/lib/cryptlib/md5.c
@@ -152,6 +152,7 @@ VOID NTAPI MD5Final( MD5_CTX *ctx )
     MD5Transform( ctx->buf, (unsigned int *)ctx->in );
     byteReverse( (unsigned char *)ctx->buf, 4 );
     memcpy( ctx->digest, ctx->buf, 16 );
+    memset(ctx->in, 0, sizeof(ctx->in));
 }
 
 /* The four core functions - F1 is optimized somewhat */
