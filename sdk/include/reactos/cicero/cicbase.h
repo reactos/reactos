1f378a2691e8891238e539017954e1795e4cd911
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-21 18:25:14 +0900

    delete cicpath.h

--- sdk/include/reactos/cicero/cicbase.h
+++ sdk/include/reactos/cicero/cicbase.h
@@ -7,7 +7,9 @@
 
 #pragma once
 
-#include <ndk/pstypes.h> /* for PROCESSINFOCLASS */
+#ifndef __cplusplus
+    #error Cicero needs C++.
+#endif
 
 #include "cicpath.h"
 
@@ -34,22 +36,20 @@ static inline void cicMemFree(LPVOID ptr)
         LocalFree(ptr);
 }
 
-#ifdef __cplusplus
-    inline void* __cdecl operator new(size_t size) noexcept
-    {
-        return cicMemAllocClear(size);
-    }
+inline void* __cdecl operator new(size_t size) noexcept
+{
+    return cicMemAllocClear(size);
+}
 
-    inline void __cdecl operator delete(void* ptr) noexcept
-    {
-        cicMemFree(ptr);
-    }
+inline void __cdecl operator delete(void* ptr) noexcept
+{
+    cicMemFree(ptr);
+}
 
-    inline void __cdecl operator delete(void* ptr, size_t size) noexcept
-    {
-        cicMemFree(ptr);
-    }
-#endif // __cplusplus
+inline void __cdecl operator delete(void* ptr, size_t size) noexcept
+{
+    cicMemFree(ptr);
+}
 
 // FIXME: Use msutb.dll and header
 static inline void ClosePopupTipbar(void)
@@ -100,6 +100,46 @@ cicGetOSInfo(VOID)
     return dwOsInfo;
 }
 
+struct CicSystemModulePath
+{
+    WCHAR m_szPath[MAX_PATH];
+    SIZE_T m_cchPath;
+
+    CicSystemModulePath()
+    {
+        m_szPath[0] = UNICODE_NULL;
+        m_cchPath = 0;
+    }
+
+    BOOL Init(_In_ LPCWSTR pszFileName, _In_ BOOL bSysWinDir);
+};
+
+// Get an instance handle that is already loaded
+static inline HINSTANCE
+GetSystemModuleHandle(
+    _In_ LPCWSTR pszFileName,
+    _In_ BOOL bSysWinDir)
+{
+    CicSystemModulePath ModPath;
+    if (!ModPath.Init(pszFileName, bSysWinDir))
+        return NULL;
+    return GetModuleHandleW(ModPath.m_szPath);
+}
+
+// Load a system library
+static inline HINSTANCE
+LoadSystemLibrary(
+    _In_ LPCWSTR pszFileName,
+    _In_ BOOL bSysWinDir)
+{
+    CicSystemModulePath ModPath;
+    if (!ModPath.Init(pszFileName, bSysWinDir))
+        return NULL;
+    return ::LoadLibraryW(ModPath.m_szPath);
+}
+
+#include <ndk/pstypes.h> /* for PROCESSINFOCLASS */
+
 /* ntdll!NtQueryInformationProcess */
 typedef NTSTATUS (WINAPI *FN_NtQueryInformationProcess)(HANDLE, PROCESSINFOCLASS, PVOID, ULONG, PULONG);
 
@@ -129,4 +169,46 @@ static inline BOOL cicIsWow64(VOID)
         return FALSE;
 
     return !!Value;
+}
+
+inline BOOL
+CicSystemModulePath::Init(
+    _In_ LPCWSTR pszFileName,
+    _In_ BOOL bSysWinDir)
+{
+    SIZE_T cchPath;
+    if (bSysWinDir)
+    {
+        // Usually C:\Windows or C:\ReactOS
+        cchPath = ::GetSystemWindowsDirectory(m_szPath, _countof(m_szPath));
+    }
+    else
+    {
+        // Usually C:\Windows\system32 or C:\ReactOS\system32
+        cchPath = ::GetSystemDirectoryW(m_szPath, _countof(m_szPath));
+    }
+
+    m_szPath[_countof(m_szPath) - 1] = UNICODE_NULL; // Avoid buffer overrun
+
+    if ((cchPath == 0) || (cchPath > _countof(m_szPath) - 2))
+        goto Failure;
+
+    // Add backslash if necessary
+    if ((cchPath > 0) && (m_szPath[cchPath - 1] != L'\\'))
+    {
+        m_szPath[cchPath + 0] = L'\\';
+        m_szPath[cchPath + 1] = UNICODE_NULL;
+    }
+
+    // Append pszFileName
+    if (FAILED(StringCchCatW(m_szPath, _countof(m_szPath), pszFileName)))
+        goto Failure;
+
+    m_cchPath = wcslen(m_szPath);
+    return TRUE;
+
+Failure:
+    m_szPath[0] = UNICODE_NULL;
+    m_cchPath = 0;
+    return FALSE;
 }
