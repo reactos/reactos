825636f15de31e6de2a66fde7f9b048275dbe698
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-23 18:23:20 +0900

    fix comment 4

--- dll/win32/msutb/precomp.h
+++ dll/win32/msutb/precomp.h
@@ -1,7 +1,7 @@
 /*
  * PROJECT:     ReactOS msutb.dll
  * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
- * PURPOSE:     Text Input Panel Bar (Tipbar) for on-screen keyboard and handwriting recognition
+ * PURPOSE:     Language Bar (Tipbar)
  * COPYRIGHT:   Copyright 2023 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
  */
 
