4d448134dec85dadc4add100811d6d48474ed1bf
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-01-01 09:15:03 +0900

    [COMCTL32] EDIT & ListView & TreeView: Fix region leaks

--- dll/win32/comctl32/listview.c
+++ dll/win32/comctl32/listview.c
@@ -10817,6 +10817,7 @@ static BOOL LISTVIEW_NCPaint(const LISTVIEW_INFO *infoPtr, HRGN region)
 
     /* Call default proc to get the scrollbars etc. painted */
     DefWindowProcW (infoPtr->hwndSelf, WM_NCPAINT, (WPARAM)cliprgn, 0);
+    DeleteObject(cliprgn);
 
     return FALSE;
 }
