121de8e23b277870884b1716482308a71e479eae
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-01-03 18:49:16 +0900

    [MSCTFIME] Implement CCaret class

--- dll/ime/msctfime/msctfime.cpp
+++ dll/ime/msctfime/msctfime.cpp
@@ -3487,6 +3487,134 @@ CtfImeThreadDetach(VOID)
 }
 
 /***********************************************************************
+ *      CCaret
+ */
+class CCaret
+{
+protected:
+    HWND m_hWnd;
+    UINT m_uCaretBlinkTimerID;
+    POINT m_pt;
+    SIZE m_size;
+    BOOL m_bCaretBlinking;
+    BOOL m_bCaretVisible;
+
+public:
+    enum { TIMER_ID = 0x4F83AF91 };
+    CCaret();
+    virtual ~CCaret();
+
+    void CreateCaret(HWND hWnd, SIZE size);
+    void DestroyCaret();
+    void HideCaret();
+    void InvertCaret();
+    void OnTimer();
+    void SetCaretPos(POINT pt);
+    void SetBlinking(BOOL bBlinking);
+};
+
+/**
+ * @implemented
+ */
+CCaret::CCaret()
+{
+    m_bCaretBlinking = FALSE;
+    m_bCaretVisible = FALSE;
+    m_uCaretBlinkTimerID = 0;
+    m_pt.x = m_pt.y = 0;
+    m_size.cx = m_size.cy = 0;
+}
+
+/**
+ * @implemented
+ */
+CCaret::~CCaret()
+{
+    HideCaret();
+    ::KillTimer(m_hWnd, m_uCaretBlinkTimerID);
+    m_uCaretBlinkTimerID = 0;
+}
+
+/**
+ * @implemented
+ */
+void CCaret::CreateCaret(HWND hWnd, SIZE size)
+{
+    m_hWnd = hWnd;
+    m_size = size;
+    if (::IsWindow(hWnd))
+        m_uCaretBlinkTimerID = ::SetTimer(m_hWnd, TIMER_ID, ::GetCaretBlinkTime(), NULL);
+}
+
+/**
+ * @implemented
+ */
+void CCaret::DestroyCaret()
+{
+    HideCaret();
+    ::KillTimer(m_hWnd, m_uCaretBlinkTimerID);
+    m_uCaretBlinkTimerID = 0;
+}
+
+/**
+ * @implemented
+ */
+void CCaret::HideCaret()
+{
+    if (m_bCaretVisible)
+    {
+        m_bCaretVisible = FALSE;
+        InvertCaret();
+    }
+    m_bCaretBlinking = FALSE;
+}
+
+/**
+ * @implemented
+ */
+void CCaret::InvertCaret()
+{
+    HDC hDC = ::GetDC(m_hWnd);
+    ::PatBlt(hDC, m_pt.x, m_pt.y, m_size.cx, m_size.cy, DSTINVERT);
+    ::ReleaseDC(m_hWnd, hDC);
+}
+
+/**
+ * @implemented
+ */
+void CCaret::OnTimer()
+{
+    if (m_bCaretBlinking)
+    {
+        m_bCaretVisible = !m_bCaretVisible;
+        InvertCaret();
+    }
+}
+
+/**
+ * @implemented
+ */
+void CCaret::SetCaretPos(POINT pt)
+{
+    BOOL bCaretVisible = m_bCaretVisible;
+    if (bCaretVisible)
+        InvertCaret();
+
+    m_pt = pt;
+
+    if (bCaretVisible)
+        InvertCaret();
+}
+
+/**
+ * @implemented
+ */
+void CCaret::SetBlinking(BOOL bBlinking)
+{
+    m_bCaretBlinking = bBlinking;
+}
+
+/***********************************************************************
  *      UIComposition
  */
 struct UIComposition
