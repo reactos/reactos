56f9b09bcd7d2c05eebc61c318f365ea4ef50426
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2024-01-04 21:44:37 +0100

    [SETUPAPI] Prepare for wine sync -- __REACTOS__-specific code

--- dll/win32/setupapi/install.c
+++ dll/win32/setupapi/install.c
@@ -1392,6 +1392,8 @@ BOOL WINAPI SetupInstallFromInfSectionW( HWND owner, HINF hinf, PCWSTR section, 
 {
     BOOL ret;
     int i;
+
+#ifdef __REACTOS__
     struct needs_callback_info needs_info;
 
     /* Parse 'Include' and 'Needs' directives */
@@ -1407,6 +1409,7 @@ BOOL WINAPI SetupInstallFromInfSectionW( HWND owner, HINF hinf, PCWSTR section, 
     needs_info.devinfo = devinfo;
     needs_info.devinfo_data = devinfo_data;
     iterate_section_fields( hinf, section, Needs, needs_callback, &needs_info);
+#endif
 
     if (flags & SPINST_FILES)
     {
