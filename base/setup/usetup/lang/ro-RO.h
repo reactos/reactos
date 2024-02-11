8ceb3e6d33b707c00b58de3d4d7c498ec4c12a9e
Andrei Miloiu <miloiuandrei@gmail.com>
2024-02-12 00:14:33 +0200

    [USETUP] Update Romanian (ro-RO) translation (#6474)

--- base/setup/usetup/lang/ro-RO.h
+++ base/setup/usetup/lang/ro-RO.h
@@ -2088,7 +2088,7 @@ MUI_ERROR roROErrorEntries[] =
     },
     {
         // ERROR_UPDATE_LOCALESETTINGS,
-        "E\272ec la instituirea sistemului de localizare.\n",
+        "Instalatoprul nu a putut seta loca\376ia sistemului.\n",
         "ENTER = Repornire calculator"
     },
     {
@@ -2099,7 +2099,7 @@ MUI_ERROR roROErrorEntries[] =
     },
     {
         // ERROR_UPDATE_GEOID,
-        "E\272ec la instituirea de geo id.",
+        "Instalatorul nu a putut seta geo id.",
         "ENTER = Repornire calculator"
     },
     {
