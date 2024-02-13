64a330730280c5756ae8e7c0e6ffab8ebaa1c8b5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-02-13 16:01:12 +0900

    improve 2

--- modules/rostests/apitests/imm32/precomp.h
+++ modules/rostests/apitests/imm32/precomp.h
@@ -5,12 +5,12 @@
 #include <windows.h>
 #include <imm.h>
 #include <immdev.h>
+#include <imm32_undoc.h>
 #include <pseh/pseh2.h>
 #include <ndk/umtypes.h>
 #include <ndk/pstypes.h>
 #include "../../../win32ss/include/ntuser.h"
 #include <undocuser.h>
-#include <imm32_undoc.h>
 #include <ndk/rtlfuncs.h>
 #include <wine/test.h>
 #include <stdio.h>
