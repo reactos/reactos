a3548bb00b3149ea075b717d81e1ec9b922814b0
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-02-21 09:45:56 +0900

    [MSCTFIME][SDK] Implement CDefCompFrameWindow

--- dll/ime/msctfime/ui.h
+++ dll/ime/msctfime/ui.h
@@ -7,6 +7,15 @@
 
 #pragma once
 
+class CUIFGripper;
+    class CDefCompFrameGripper;
+class CUIFToolbarButton;
+    class CCompFinalizeButton;
+class CUIFWindow;
+    class CCompFrameWindow;
+        class CCompButtonFrameWindow;
+        class CDefCompFrameWindow;
+
 /***********************************************************************/
 
 extern UINT WM_MSIME_SERVICE;
@@ -22,6 +31,92 @@ extern UINT WM_MSIME_KEYMAP;
 
 BOOL IsMsImeMessage(_In_ UINT uMsg);
 BOOL RegisterMSIMEMessage(VOID);
+
+/***********************************************************************/
+
+class CDefCompFrameGripper : public CUIFGripper
+{
+public:
+    CDefCompFrameWindow *m_pDefCompFrameWindow;
+
+    CDefCompFrameGripper(CDefCompFrameWindow *pDefCompFrameWindow, LPCRECT prc, DWORD style);
+};
+
+/***********************************************************************/
+
+class CCompFrameWindow;
+
+class CCompFinalizeButton : public CUIFToolbarButton
+{
+public:
+    CCompFrameWindow *m_pCompFrameWindow;
+
+    CCompFinalizeButton(
+        CCompFrameWindow *pParent,
+        DWORD nObjectID,
+        LPCRECT prc,
+        DWORD style,
+        DWORD dwButtonFlags,
+        LPCWSTR pszText);
+    ~CCompFinalizeButton() override;
+
+    STDMETHOD_(void, OnLeftClick)() override;
+};
+
+/***********************************************************************/
+
+class CCompFrameWindow : public CUIFWindow
+{
+public:
+    HIMC m_hIMC;
+
+    CCompFrameWindow(HIMC hIMC, DWORD style);
+};
+
+/***********************************************************************/
+
+class CCompButtonFrameWindow : public CCompFrameWindow
+{
+public:
+    MARGINS m_Margins;
+    CCompFinalizeButton *m_pFinalizeButton;
+
+    CCompButtonFrameWindow(HIMC hIMC, DWORD style);
+
+    void Init();
+    void MoveShow(LONG x, LONG y, BOOL bShow);
+
+    STDMETHOD_(void, OnCreate)(HWND hWnd) override;
+};
+
+/***********************************************************************/
+
+class CDefCompFrameWindow : public CCompFrameWindow
+{
+public:
+    HWND m_hwndDefCompFrame;
+    CDefCompFrameGripper *m_pGripper;
+    CCompFinalizeButton *m_pFinalizeButton;
+    MARGINS m_Margins;
+
+public:
+    CDefCompFrameWindow(HIMC hIMC, DWORD style);
+    ~CDefCompFrameWindow() override;
+
+    void Init();
+    INT GetGripperWidth();
+    void MyScreenToClient(LPPOINT ppt, LPRECT prc);
+    void SetCompStrRect(INT nWidth, INT nHeight, BOOL bShow);
+
+    void LoadPosition();
+    void SavePosition();
+
+    STDMETHOD_(void, OnCreate)(HWND hWnd) override;
+    STDMETHOD_(BOOL, OnSetCursor)(UINT uMsg, LONG x, LONG y) override;
+    STDMETHOD_(LRESULT, OnWindowPosChanged)(HWND hWnd, UINT uMsg, WPARAM wParam,
+                                            LPARAM lParam) override;
+    STDMETHOD_(void, HandleMouseMsg)(UINT uMsg, LONG x, LONG y) override;
+};
 
 /***********************************************************************/
 
