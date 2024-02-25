5eb27e3f4a1c61c475e988a1b282bf62c65009d5
Joachim Henze <Joachim.Henze@reactos.org>
2024-02-25 21:22:35 +0100

    using DBG_UNREFERENCED_LOCAL_VARIABLE(iLargeImage); allows to keep the assert. As suggested by Stasm.
    Personally I think the assert is overkill here, but if it makes others happy,
    then I do it like that. Better than the compilers whining definitely.

--- dll/win32/shell32/dialogs/filetypes.cpp
+++ dll/win32/shell32/dialogs/filetypes.cpp
@@ -660,11 +660,13 @@ FileTypesDlg_InsertToLV(HWND hListView, LPCWSTR szName, INT iItem, LPCWSTR szFil
     }
 
     // add icon to imagelist
-    INT iSmallImage = -1;
+    INT iLargeImage = -1, iSmallImage = -1;
     if (Entry->hIconLarge && Entry->hIconSmall)
     {
-        ImageList_AddIcon(himlLarge, Entry->hIconLarge);
+        iLargeImage = ImageList_AddIcon(himlLarge, Entry->hIconLarge);
         iSmallImage = ImageList_AddIcon(himlSmall, Entry->hIconSmall);
+        ASSERT(iLargeImage == iSmallImage);
+        DBG_UNREFERENCED_LOCAL_VARIABLE(iLargeImage);
     }
 
     // Do not add excluded entries
@@ -1037,8 +1039,10 @@ EditTypeDlg_UpdateEntryIcon(HWND hwndDlg, PEDITTYPE_DIALOG pEditType,
     HIMAGELIST himlLarge = ListView_GetImageList(hListView, LVSIL_NORMAL);
     HIMAGELIST himlSmall = ListView_GetImageList(hListView, LVSIL_SMALL);
 
-    ImageList_AddIcon(himlLarge, pEntry->hIconLarge);
+    INT iLargeImage = ImageList_AddIcon(himlLarge, pEntry->hIconLarge);
     INT iSmallImage = ImageList_AddIcon(himlSmall, pEntry->hIconSmall);
+    ASSERT(iLargeImage == iSmallImage);
+    DBG_UNREFERENCED_LOCAL_VARIABLE(iLargeImage);
 
     INT iItem = ListView_GetNextItem(hListView, -1, LVNI_SELECTED);
     if (iItem != -1)
