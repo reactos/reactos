37f4571b124febe839210bae1062cc3f18fd2ec8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-03-03 21:35:11 +0900

    ok_ntstatus, ok_ptr, add tests

--- modules/rostests/apitests/ntdll/LdrLoadDll.c
+++ modules/rostests/apitests/ntdll/LdrLoadDll.c
@@ -37,98 +37,126 @@ START_TEST(LdrLoadDll)
     StartSeh()
         Status = LdrLoadDll(NULL, NULL, &DllName, NULL);
     EndSeh(STATUS_ACCESS_VIOLATION);
-    ok_long(Status, 0xDEADFACE);
+    ok_ntstatus(Status, 0xDEADFACE);
 
     Status = 0xDEADFACE;
     BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(NULL, NULL, &DllName, &BaseAddress);
     EndSeh(STATUS_ACCESS_VIOLATION);
-    ok_long(Status, 0xDEADFACE);
+    ok_ntstatus(Status, 0xDEADFACE);
     ok_ptr(BaseAddress, InvalidPointer);
 
     Status = 0xDEADFACE;
     StartSeh()
         Status = LdrLoadDll(L"", NULL, &DllName, NULL);
     EndSeh(STATUS_ACCESS_VIOLATION);
-    ok_long(Status, 0xDEADFACE);
+    ok_ntstatus(Status, 0xDEADFACE);
 
     Status = 0xDEADFACE;
     BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(L"", NULL, &DllName, &BaseAddress);
     EndSeh(STATUS_ACCESS_VIOLATION);
-    ok_long(Status, 0xDEADFACE);
+    ok_ntstatus(Status, 0xDEADFACE);
     ok_ptr(BaseAddress, InvalidPointer);
 
     RtlInitUnicodeString(&DllName, L"advapi32.dll");
 
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(NULL, NULL, &DllName, NULL);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_ACCESS_VIOLATION);
+    ok_ntstatus(Status, 0xDEADFACE);
+    ok_ptr(BaseAddress, InvalidPointer);
 
     RtlInitUnicodeString(&DllName, L"advapi32.dll");
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         BaseAddress = InvalidPointer;
         Status = LdrLoadDll(NULL, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         ok(BaseAddress != NULL && BaseAddress != InvalidPointer, "BaseAddress = %p\n", BaseAddress);
         if (NT_SUCCESS(Status))
         {
             BaseAddress2 = InvalidPointer;
             Status = LdrLoadDll(NULL, NULL, &DllName, &BaseAddress2);
-            ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
-            ok(BaseAddress2 == BaseAddress, "BaseAddress2 = %p, expected %p\n", BaseAddress2, BaseAddress);
+            ok_ntstatus(Status, STATUS_SUCCESS);
+            ok_ptr(BaseAddress2, BaseAddress);
             LdrUnloadDll(BaseAddress);
         }
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     RtlInitUnicodeString(&DllName, L"advapi32.dll");
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(L"\\SystemRoot\\System32", NULL, &DllName, NULL);
     EndSeh(STATUS_ACCESS_VIOLATION);
+    ok_ntstatus(Status, 0xDEADFACE);
+    ok_ptr(BaseAddress, InvalidPointer);
 
     /* Test with only backslashes in path; no file extension */
     StringCchPrintfW(szPath, _countof(szPath), L"%s\\advapi32", szSysDir);
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     /* Test with only backslashes in path; with file extension */
     StringCchPrintfW(szPath, _countof(szPath), L"%s\\advapi32.dll", szSysDir);
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     StringCchPrintfW(szPath, _countof(szPath), L"%s/advapi32", szSysDir);
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     /* Test with one forward slash in path; with file extension */
     StringCchPrintfW(szPath, _countof(szPath), L"%s/advapi32.dll", szSysDir);
 
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     /* Test with only forward slashes in path; no file extension */
     StringCchPrintfW(szPath, _countof(szPath), L"%s\\advapi32", szSysDir);
@@ -139,12 +167,16 @@ START_TEST(LdrLoadDll)
     }
 
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 
     /* Test with only forward slashes in path; with file extension */
     StringCchPrintfW(szPath, _countof(szPath), L"%s\\advapi32.dll", szSysDir);
@@ -155,10 +187,14 @@ START_TEST(LdrLoadDll)
     }
 
     RtlInitUnicodeString(&DllName, szPath);
+    Status = 0xDEADFACE;
+    BaseAddress = InvalidPointer;
     StartSeh()
         Status = LdrLoadDll(szWinDir, NULL, &DllName, &BaseAddress);
-        ok(Status == STATUS_SUCCESS, "Status = %lx\n", Status);
+        ok_ntstatus(Status, STATUS_SUCCESS);
         if (NT_SUCCESS(Status))
             LdrUnloadDll(BaseAddress);
     EndSeh(STATUS_SUCCESS);
+    ok_ntstatus(Status, STATUS_SUCCESS);
+    ok(BaseAddress != NULL, "BaseAddress was NULL\n");
 }
