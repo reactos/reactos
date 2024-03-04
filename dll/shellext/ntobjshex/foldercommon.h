a1a4b36a068e66ff8ea43cbc08a2621917b64f2c
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-03-04 22:49:39 +0900

    Update dll/shellext/ntobjshex/foldercommon.h

--- dll/shellext/ntobjshex/foldercommon.h
+++ dll/shellext/ntobjshex/foldercommon.h
@@ -42,7 +42,7 @@ public:
         return E_NOTIMPL;
     }
 
-    STDMETHODIMP Initialize(IShellView* psv)
+    virtual STDMETHODIMP Initialize(IShellView* psv)
     {
         m_View = psv;
         return S_OK;
