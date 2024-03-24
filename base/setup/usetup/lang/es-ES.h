54df31ccacbe45201615ad48239b51dcfffab688
Julen Urizar Compains <julenuri@hotmail.com>
2024-03-24 10:30:56 +0100

    [TRANSLATION] Update Spanish (es-ES) translation (#6449)
    - [REACTOS] es-ES: Fixes and typos
    - [USETUP] es-ES: Fixes and typos
    - [HOTPLUG] es-ES: Fixes and typos
    - [OPENGLCFG] Add Spanish (es-ES) translation
    - [NETSHELL] es-ES: Fixes and typos
    - [SHELL32] es-ES: Fixes and typos
    - [SYSSETUP] es-ES: Fixes and typos
    
    Reviewed-by: Ismael Ferreras Morezuelas <swyterzone@gmail.com>

--- base/setup/usetup/lang/es-ES.h
+++ base/setup/usetup/lang/es-ES.h
@@ -1,7 +1,15 @@
 // This file is converted by code7bit.
 // code7bit: https://github.com/katahiromz/code7bit
 // To revert conversion, please execute "code7bit -r <file>".
-/* Translated by ??? and Ismael Ferreras Morezuelas (Swyter) revised in 2020 by Julen Urizar Compains*/
+/*
+ * PROJECT:     ReactOS Setup
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Spanish resource file
+ * TRANSLATORS: Copyright 2007 Gabriel Ilardi <gabrielilardi@hotmail.it>
+ *              Copyright 2007 Marc Piulachs <marc.piulachs@live.com>
+ *              Copyright 2015 Ismael Ferreras Morezuelas <swyterzone+ros@gmail.com>
+ *              Copyright 2020-2024 Julen Urizar Compains <julenuri@hotmail.com>
+ */
 
 #pragma once
 
@@ -69,7 +77,7 @@ static MUI_ENTRY esESLanguagePageEntries[] =
     {
         8,
         11,
-        "   Luego, pulse INTRO.",
+        "   Despu\202s, pulse INTRO.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -114,7 +122,7 @@ static MUI_ENTRY esESWelcomePageEntries[] =
     {
         6,
         11,
-        "Este es el primer paso de instalaci\242n, que copiar\240 ReactOS en",
+        "\220ste es el primer paso de la instalaci\242n, que copiar\240 ReactOS en",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -195,42 +203,42 @@ static MUI_ENTRY esESIntroPageEntries[] =
     {
         6,
         8,
-        "Situaci\242n de la Versi\242n de ReactOS",
+        "Aviso sobre la Versi\242n de ReactOS",
         TEXT_STYLE_HIGHLIGHT,
         TEXT_ID_STATIC
     },
     {
         6,
         11,
-        "ReactOS est\240 en fase Alpha, lo que significa que a\243n no est\240",
+        "ReactOS est\240 en fase alfa, lo que significa que a\243n no es",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         12,
-        "completo y est\240 bajo intenso desarrollo. Se recomienda usarlo solo",
+        "perfecto y est\240 bajo intenso desarrollo. Se recomienda usarlo",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         13,
-        "para hacer pruebas y testeos y no como un SO de uso diario.",
+        "solo para hacer pruebas y testeos y no como un sistema de uso diario.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         15,
-        "Haz copia de tus datos o prueba en un ordenador secundario",
+        "Realice una copia de tus datos importantes o pruebe en un ordenador",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         16,
-        "en caso de que uses ReactOS en hardware real.",
+        "secundario en caso de que vayas a usar ReactOS en hardware real.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -373,7 +381,7 @@ static MUI_ENTRY esESLicensePageEntries[] =
     {
         8,
         24,
-        "Este es un software libre; lea el c\242digo para las condiciones de copia.",
+        "\220ste es un software libre; lea el c\242digo para las condiciones de copia.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -841,7 +849,7 @@ static MUI_ENTRY esESDisplayPageEntries[] =
     {
         8,
         10,
-         "\x07  Pulse ARRIBA y ABAJO para modificar la resoluci\303\263n.",
+         "\x07  Pulse ARRIBA y ABAJO para modificar la resoluci\242n.",
          TEXT_STYLE_NORMAL,
          TEXT_ID_STATIC
     },
@@ -952,7 +960,7 @@ static MUI_ENTRY esESSelectPartitionEntries[] =
     {
         6,
         9,
-        "disponible en el disco para nuevas particiones.",
+        "disponible en los discos duros para nuevas particiones.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -1332,21 +1340,21 @@ static MUI_ENTRY esESInstallDirectoryEntries[] =
     {
         6,
         14,
-        "Para cambiar el directorio sugerido, Pulse RETROCESO para eliminar",
+        "Para cambiar el directorio sugerido, pulse las flechas o presione",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         15,
-        "car\240cteres y escriba el directorio donde quiere instalar",
+        "RETROCESO para eliminar caracteres y escriba el directorio",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         6,
         16,
-        "ReactOS.",
+        "donde quiere instalar ReactOS.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -1422,35 +1430,35 @@ static MUI_ENTRY esESBootLoaderSelectPageEntries[] =
     {
         6,
         8,
-        "Please select where Setup should install the bootloader:",
+        "Seleccione por favor d\242nde se instalar\240 el sector de arranque:",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         8,
         12,
-        "Instalar el cargador de arranque en el disco duro (MBR y VBR).",
+        "Instalar el sector de arranque en el disco duro (MBR y VBR).",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         8,
         13,
-        "Instalar el cargador de arranque en el disco duro (s\242lo VBR).",
+        "Instalar el sector de arranque en el disco duro (s\242lo VBR).",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         8,
         14,
-        "Instalar el cargador de inicio en un disquete.",
+        "Instalar el sector de arranque en un disquete.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         8,
         15,
-        "Omitir la instalaci\242n del cargador de arranque.",
+        "Omitir la instalaci\242n del sector de arranque.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
@@ -1481,14 +1489,14 @@ static MUI_ENTRY etESBootLoaderInstallPageEntries[] =
     {
         6,
         8,
-        "En este paso, el instalador crear\240 el cargador de arranque.",
+        "En este paso, el instalador crear\240 el sector de arranque.",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
     {
         0,
         0,
-        "Instalando sector de arranque en el disco, espere por favor...",
+        "Instalando el sector de arranque en el disco, espere por favor...",
         TEXT_TYPE_STATUS | TEXT_PADDING_BIG,
         TEXT_ID_STATIC
     },
@@ -1512,7 +1520,7 @@ static MUI_ENTRY esESBootLoaderRemovableDiskPageEntries[] =
     {
         6,
         8,
-        "El instalador no pudo instalar el cargador de arranque en el disco",
+        "El instalador no pudo instalar el sector de arranque en el disco",
         TEXT_STYLE_NORMAL,
         TEXT_ID_STATIC
     },
