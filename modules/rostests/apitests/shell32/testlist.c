31ce37da2ace6cf1c3fc35c1dd6ce138263f65f7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-04-12 21:29:07 +0900

    [SHELL32][SHELL32_APITEST][SDK] Implement GUIDFromStringA (#6734)
    Implementing missing features...
    JIRA issue: CORE-19278
    - Add GUIDFromString testcase.
    - Implement GUIDFromStringA function.
    - Add NULL check to GUIDFromStringW.
    - Add GUIDFromStringA prototype to "undocshell.h".

--- modules/rostests/apitests/shell32/testlist.c
+++ modules/rostests/apitests/shell32/testlist.c
@@ -17,6 +17,7 @@ extern void func_DragDrop(void);
 extern void func_ExtractIconEx(void);
 extern void func_FindExecutable(void);
 extern void func_GetDisplayNameOf(void);
+extern void func_GUIDFromString(void);
 extern void func_Int64ToString(void);
 extern void func_IShellFolderViewCB(void);
 extern void func_menu(void);
@@ -57,6 +58,7 @@ const struct test winetest_testlist[] =
     { "ExtractIconEx", func_ExtractIconEx },
     { "FindExecutable", func_FindExecutable },
     { "GetDisplayNameOf", func_GetDisplayNameOf },
+    { "GUIDFromString", func_GUIDFromString },
     { "Int64ToString", func_Int64ToString },
     { "IShellFolderViewCB", func_IShellFolderViewCB },
     { "menu", func_menu },
