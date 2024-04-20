9c06f916a57a749d61fac23970a6722b08f3f378
winesync <ros-dev@reactos.org>
2024-04-20 00:55:39 -0700

    [WINESYNC] user32: Fix itemData passed in ListBox WM_MEASUREITEM message.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 3c5c9b157771828419cb5d7e831d46813ede041f by Piotr Caban <piotr@codeweavers.com>

--- win32ss/user/user32/controls/listbox.c
+++ win32ss/user/user32/controls/listbox.c
@@ -1561,7 +1561,7 @@ static LRESULT LISTBOX_InsertItem( LB_DESCR *descr, INT index,
         RtlMoveMemory( item + 1, item,
                        (descr->nb_items - index) * sizeof(LB_ITEMDATA) );
     item->str      = str;
-    item->data     = data;
+    item->data     = HAS_STRINGS(descr) ? 0 : data;
     item->height   = 0;
     item->selected = FALSE;
     descr->nb_items++;
@@ -1576,7 +1576,7 @@ static LRESULT LISTBOX_InsertItem( LB_DESCR *descr, INT index,
         mis.CtlType    = ODT_LISTBOX;
         mis.CtlID      = id;
         mis.itemID     = index;
-        mis.itemData   = descr->items[index].data;
+        mis.itemData   = data;
         mis.itemHeight = descr->item_height;
         SendMessageW( descr->owner, WM_MEASUREITEM, id, (LPARAM)&mis );
         item->height = mis.itemHeight ? mis.itemHeight : 1;
@@ -1617,7 +1617,6 @@ static LRESULT LISTBOX_InsertItem( LB_DESCR *descr, INT index,
 static LRESULT LISTBOX_InsertString( LB_DESCR *descr, INT index, LPCWSTR str )
 {
     LPWSTR new_str = NULL;
-    ULONG_PTR data = 0;
     LRESULT ret;
 
     if (HAS_STRINGS(descr))
@@ -1631,10 +1630,9 @@ static LRESULT LISTBOX_InsertString( LB_DESCR *descr, INT index, LPCWSTR str )
         }
         strcpyW(new_str, str);
     }
-    else data = (ULONG_PTR)str;
 
     if (index == -1) index = descr->nb_items;
-    if ((ret = LISTBOX_InsertItem( descr, index, new_str, data )) != 0)
+    if ((ret = LISTBOX_InsertItem( descr, index, new_str, (ULONG_PTR)str )) != 0)
     {
         HeapFree( GetProcessHeap(), 0, new_str );
         return ret;
