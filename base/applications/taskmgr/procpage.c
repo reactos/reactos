a06b237e8e64e8b2af6591998429787a34ae192e
Thamatip Chitpong <thamatip.chitpong@reactos.org>
2024-04-22 21:04:23 +0700

    [TASKMGR] Process page: Allow using "Open File Location" functionality without running Explorer shell
    If Explorer shell is not available, use ReactOS's alternative file browser instead.

--- base/applications/taskmgr/procpage.c
+++ base/applications/taskmgr/procpage.c
@@ -1248,8 +1248,11 @@ void ProcessPage_OnOpenFileLocation(void)
 
     StringCchPrintfW(pszCmdLine, dwLength, szCmdFormat, pszExePath);
 
-    /* Call the shell to open the file location and select it */
-    ShellExecuteW(NULL, L"open", L"explorer.exe", pszCmdLine, NULL, SW_SHOWNORMAL);
+    /* Call the shell to open the file location and select it. If Explorer shell
+     * is not available, use ReactOS's alternative file browser instead. */
+    ShellExecuteW(NULL, L"open",
+                  GetShellWindow() ? L"explorer.exe" : L"filebrowser.exe",
+                  pszCmdLine, NULL, SW_SHOWNORMAL);
 
 Cleanup:
     HeapFree(GetProcessHeap(), 0, pszCmdLine);
