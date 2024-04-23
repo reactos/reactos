798ca0394a1cfd4f562c769dc5bfb9732a386dd3
winesync <ros-dev@reactos.org>
2024-04-23 12:00:12 +0300

    [WINESYNC] ntdll: Check the namespace in RtlQueryActivationContextApplicationSettings.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id e46259fb823f55aa331381fe56916d20049d52f2 by Alexandre Julliard <julliard@winehq.org>

--- sdk/lib/rtl/actctx.c
+++ sdk/lib/rtl/actctx.c
@@ -1060,6 +1060,7 @@ static void free_entity_array(struct entity_array *array)
         case ACTIVATION_CONTEXT_SECTION_APPLICATION_SETTINGS:
             RtlFreeHeap(GetProcessHeap(), 0, entity->u.settings.name);
             RtlFreeHeap(GetProcessHeap(), 0, entity->u.settings.value);
+            RtlFreeHeap(GetProcessHeap(), 0, entity->u.settings.ns);
             break;
         default:
             FIXME("Unknown entity kind %d\n", entity->kind);
@@ -5115,7 +5116,7 @@ static NTSTATUS find_guid(ACTIVATION_CONTEXT* actctx, ULONG section_kind,
     return STATUS_SUCCESS;
 }
 
-static const WCHAR *find_app_settings( ACTIVATION_CONTEXT *actctx, const WCHAR *settings )
+static const WCHAR *find_app_settings( ACTIVATION_CONTEXT *actctx, const WCHAR *settings, const WCHAR *ns )
 {
     unsigned int i, j;
 
@@ -5126,7 +5127,8 @@ static const WCHAR *find_app_settings( ACTIVATION_CONTEXT *actctx, const WCHAR *
         {
             struct entity *entity = &assembly->entities.base[j];
             if (entity->kind == ACTIVATION_CONTEXT_SECTION_APPLICATION_SETTINGS &&
-                !strcmpW( entity->u.settings.name, settings ))
+                !strcmpW( entity->u.settings.name, settings ) &&
+                !strcmpW( entity->u.settings.ns, ns ))
                 return entity->u.settings.value;
         }
     }
@@ -5979,7 +5981,6 @@ NTSTATUS WINAPI RtlQueryActivationContextApplicationSettings( DWORD flags, HANDL
                                                               const WCHAR *settings, WCHAR *buffer,
                                                               SIZE_T size, SIZE_T *written )
 {
-    static const WCHAR namespaceW[] = {'h','t','t','p',':','/','/','s','c','h','e','m','a','s','.','m','i','c','r','o','s','o','f','t','.','c','o','m','/','S','M','I','/','2','0',0};
     ACTIVATION_CONTEXT *actctx = check_actctx( handle );
     const WCHAR *res;
 
@@ -5989,16 +5990,20 @@ NTSTATUS WINAPI RtlQueryActivationContextApplicationSettings( DWORD flags, HANDL
         return STATUS_INVALID_PARAMETER;
     }
 
-    if (ns && strncmpW( ns, namespaceW, strlenW(namespaceW) ))
+    if (ns)
     {
-        WARN( "unknown namespace %S\n", ns );
-        return STATUS_INVALID_PARAMETER;
+        if (strcmpW( ns, windowsSettings2005NSW ) &&
+            strcmpW( ns, windowsSettings2011NSW ) &&
+            strcmpW( ns, windowsSettings2016NSW ) &&
+            strcmpW( ns, windowsSettings2017NSW ))
+            return STATUS_INVALID_PARAMETER;
     }
+    else ns = windowsSettings2005NSW;
 
     if (!handle) handle = process_actctx;
     if (!(actctx = check_actctx( handle ))) return STATUS_INVALID_PARAMETER;
 
-    if (!(res = find_app_settings( actctx, settings ))) return STATUS_SXS_KEY_NOT_FOUND;
+    if (!(res = find_app_settings( actctx, settings, ns ))) return STATUS_SXS_KEY_NOT_FOUND;
 
     if (written) *written = strlenW(res) + 1;
     if (size < strlenW(res)) return STATUS_BUFFER_TOO_SMALL;
