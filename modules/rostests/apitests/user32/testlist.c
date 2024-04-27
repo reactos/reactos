ffd060295d83c2ac8899591c4c10e7be85cfcd77
Doug Lyons <douglyons@douglyons.com>
2024-04-27 13:30:51 -0500

    [USER32_APITEST] Test MSVC rc.exe compiled 32-bpp BI_BITFIELD bmp (#6755)
    * Cleanup and Changes based on reviewer comments
    * For failure, show returned and expected values
    
    CORE-17005

--- modules/rostests/apitests/user32/testlist.c
+++ modules/rostests/apitests/user32/testlist.c
@@ -32,6 +32,7 @@ extern void func_InitializeLpkHooks(void);
 extern void func_KbdLayout(void);
 extern void func_keybd_event(void);
 extern void func_LoadImage(void);
+extern void func_LoadImageGCC(void);
 extern void func_LookupIconIdFromDirectoryEx(void);
 extern void func_MessageStateAnalyzer(void);
 extern void func_NextDlgItem(void);
@@ -91,6 +92,7 @@ const struct test winetest_testlist[] =
     { "KbdLayout", func_KbdLayout },
     { "keybd_event", func_keybd_event },
     { "LoadImage", func_LoadImage },
+    { "LoadImageGCC", func_LoadImageGCC },
     { "LookupIconIdFromDirectoryEx", func_LookupIconIdFromDirectoryEx },
     { "MessageStateAnalyzer", func_MessageStateAnalyzer },
     { "NextDlgItem", func_NextDlgItem },
