dcf9eb060a9900e01c762c8a2fcec10e9d119518
Marcin Jabłoński <nnxcomputing@gmail.com>
2024-04-27 02:28:46 +0200

    [SHELL32] Fix Control_RunDLLW (#5400)
    This commit makes Control_RunDLLW pass all but one tests from rostests (the one test that fails is the first one, but it only fails if the path to the test program contains a space).
    
    - Rework string parsing in the Control_DoLaunch routine
    - Do not send the CPL_STARTWPARMSW message, if no extra parameters were specified (fixes the failing Got NULL lParam2! and some CPL_STARTWPARMSW: expected -1 got %d tests)
    - Do not resolve invalid dialog names to index zero, unless the name is empty (fixes some of the failing CPL_DBLCLK: expected -1 got %d tests)
    - Handle quotes in the second part of wszCmd
    
    CORE-8981

--- dll/win32/syssetup/wizard.c
+++ dll/win32/syssetup/wizard.c
@@ -2467,7 +2467,7 @@ FinishDlgProc(HWND hwndDlg,
             if (!SetupData->UnattendSetup || !SetupData->DisableGeckoInst)
             {
                 /* Run the Wine Gecko prompt */
-                Control_RunDLLW(hwndDlg, 0, L"appwiz.cpl install_gecko", SW_SHOW);
+                Control_RunDLLW(hwndDlg, 0, L"appwiz.cpl,,install_gecko", SW_SHOW);
             }
 
             /* Set title font */
