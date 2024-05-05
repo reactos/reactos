f4a9f9cde33985a44c79e2b1ef37628cb426c3f4
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-05-05 16:15:30 +0300

    [CRT][NTDLL][MSVCRT] Add _swprintf implementation

--- sdk/lib/crt/printf/printf.cmake
+++ sdk/lib/crt/printf/printf.cmake
@@ -2,6 +2,7 @@
 list(APPEND LIBCNTPR_PRINTF_SOURCE
     printf/_snprintf.c
     printf/_snwprintf.c
+    printf/_swprintf.c
     printf/_vscprintf.c
     printf/_vscwprintf.c
     printf/_vsnprintf.c
