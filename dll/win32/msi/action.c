7af99cecc749117a14da439a5c23f01be6fba101
winesync <ros-dev@reactos.org>
2024-05-17 16:57:45 -0400

    [WINESYNC] msi: Install global assemblies before running deferred custom actions.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56493
    
    wine commit id 7f0ca9c284d15d03f14a8fefefac834990d622cd by Hans Leidekker <hans@codeweavers.com>

--- dll/win32/msi/action.c
+++ dll/win32/msi/action.c
@@ -5162,16 +5162,6 @@ static UINT ACTION_InstallFinalize(MSIPACKAGE *package)
     MSIFILE *file;
     MSIFILEPATCH *patch;
 
-    /* first do the same as an InstallExecute */
-    rc = execute_script(package, SCRIPT_INSTALL);
-    if (rc != ERROR_SUCCESS)
-        return rc;
-
-    /* then handle commit actions */
-    rc = execute_script(package, SCRIPT_COMMIT);
-    if (rc != ERROR_SUCCESS)
-        return rc;
-
     /* install global assemblies */
     LIST_FOR_EACH_ENTRY( file, &package->files, MSIFILE, entry )
     {
@@ -5209,6 +5199,16 @@ static UINT ACTION_InstallFinalize(MSIPACKAGE *package)
             return rc;
         }
     }
+
+    /* first do the same as an InstallExecute */
+    rc = execute_script(package, SCRIPT_INSTALL);
+    if (rc != ERROR_SUCCESS)
+        return rc;
+
+    /* then handle commit actions */
+    rc = execute_script(package, SCRIPT_COMMIT);
+    if (rc != ERROR_SUCCESS)
+        return rc;
 
     if (is_full_uninstall(package))
         rc = ACTION_UnpublishProduct(package);
