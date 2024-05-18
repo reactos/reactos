cffa11c597514cf69130cc2434c4fd06a37675a7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-05-18 22:23:34 +0900

    file header

--- dll/win32/shell32/wine/brsfolder.cpp
+++ dll/win32/shell32/wine/brsfolder.cpp
@@ -1,20 +1,12 @@
 /*
- * Copyright 1999 Juergen Schmied
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- *
+ * PROJECT:     ReactOS shell32
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
+ * PURPOSE:     SHBrowseForFolderA/W functions
+ * COPYRIGHT:   Copyright 1999 Juergen Schmied
+ *              Copyright 2024 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
+ */
+
+/*
  * FIXME:
  *  - many memory leaks
  *  - many flags unimplemented
