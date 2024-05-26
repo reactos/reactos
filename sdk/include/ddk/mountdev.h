e47d08fc5a85fae5d551ce2a7723d040768de5f3
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2024-05-26 18:46:20 +0200

    [DDK] mountdev.h, mountmgr.h: Minor cleanup (#6957)
    - Add missing file info headers.
    - Make the headers C++ friendly.

--- sdk/include/ddk/mountdev.h
+++ sdk/include/ddk/mountdev.h
@@ -1,7 +1,37 @@
+/*
+ * mountmgr.h
+ *
+ * Mount Manager mounted devices interface
+ *
+ * This file is part of the ReactOS DDK package.
+ *
+ * Contributors:
+ *   Magnus Olsen <greatlord@reactos.org>
+ *   Amine Khaldi <amine.khaldi@reactos.org>
+ *   Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
+ *
+ * THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ * This source code is offered for use in the public domain. You may
+ * use, modify or distribute it freely.
+ *
+ * This code is distributed in the hope that it will be useful but
+ * WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ * DISCLAIMED. This includes but is not limited to warranties of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
 #ifndef _MOUNTDEV_
 #define _MOUNTDEV_
 
+#pragma once
+
 #include <mountmgr.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #if (NTDDI_VERSION >= NTDDI_WIN2K)
 
@@ -43,5 +73,8 @@ typedef struct _MOUNTDEV_UNIQUE_ID_CHANGE_NOTIFY_OUTPUT {
   USHORT NewUniqueIdLength;
 } MOUNTDEV_UNIQUE_ID_CHANGE_NOTIFY_OUTPUT, *PMOUNTDEV_UNIQUE_ID_CHANGE_NOTIFY_OUTPUT;
 
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* _MOUNTDEV_ */
