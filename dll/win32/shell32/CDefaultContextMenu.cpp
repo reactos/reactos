8e816a9d34195e1a0b9ee49060e2ce6f8a6c3a1f
Joachim Henze <Joachim.Henze@reactos.org>
2024-05-27 03:26:50 +0200

    [0.4.13][SHELL32][BOOTDATA] RC & Improve folder-options (2024-05-27)
    Ports back:
    partially 0.4.15-dev-8109-g d7be7486c97912931ddccfba24da5b2e96be0975 [SHELL32] RC: Fix some FIXMEs (#6861), leaving out the comments
    0.4.15-dev-8106-g a549a9d8e403180f42e84e9c3e7979d60e62c268 [SHELL32] ro-RO.rc Improvement (#6844)
    0.4.15-dev-8021-g 7ce96fd3642f8be57247250acc20354d1a47b8ba [SHELL32] Fix keyboard navigation in file deletion dialog CORE-19564 (#6833)
    partially 0.4.15-dev-7877-g e85ef799fe90c9cd82193537ebf453e3aee24fb9 [SHELL32] Update Romanian (ro-RO) translation (PR6657)
    0.4.15-dev-7842-g 558ab78fc5f70e5e71c109aa9fd88418042b430a [SHELL32] *.rc: Strip all unneeded WS_DISABLED in dlg style (#6675)
    0.4.15-dev-7834-g 00c4b3d99f5b21bdbd402d6c6dd232f721491590 [SHELL32] *.rc: Anonymize the "LIST2" control in IDD_AUTOPLAY1 (#6672)
    0.4.15-dev-7260-g 1061ed4d2839024f5aa67f9ccf63eef6640f0d93 [SHELL32] *.rc 640x480-ify IDD_FOLDER_OPTIONS_* (#6165) CORE-1762
    0.4.15-dev-3170-g e34a799f0c158bd23d0e98347e4f1b85c0f6549d [BOOTDATA] Fix "DllName" casing.
    0.4.14-dev-1487-g c0d5e4fd6bd0b27c13825a61863a0acb9c583fe1 [SHELL32] CORE-16895 RC IDD_FOLDER_OPTIONS_FILETYPES All langs
    0.4.14-dev-69-g 5d7ae44059399f633f956c8010c8944af9003746 [SHELL32] Demote an ERR to WARN (which was a tiny part of PR1907) which actually started to spam with zipfldr-introducation already
    
    This backport focuses mainly on porting back the updates dimensions for folder-options-dlgs.
    
    Delete FCIDM_SHVIEW_COPYTO & FCIDM_SHVIEW_MOVETO (fixes accelerator collisions in es-ES) and
    IDS_ADVANCED_NET_CRAWLER & IDS_ADVANCED_WEB_VIEW_BARRICADE and IDS_EFI_FILE
    because all of that is unimplemented in the older versions, and I do not intend to port any of that back ever.
    
    Binary size of shell32.dll shrinks for all branches:
    shell32.dll master          RosBEWin2.2.2 GCC8.4.0 dbg x86              10.631.168 (0.4.15-dev-8027-g565a4b3)
    shell32.dll releases/0.4.14 RosBEWin2.2.2 GCC8.4.0 dbg x86  9.201.664 -> 9.188.352
    shell32.dll releases/0.4.14 RosBEWin2.1.6 GCC4.7.2 dbg x86  9.230.336 -> 9.216.512
    shell32.dll releases/0.4.13 RosBEWin2.1.6 GCC4.7.2 dbg x86  9.086.976 -> 9.073.664
    shell32.dll releases/0.4.12 RosBEWin2.1.6 GCC4.7.2 dbg x86  9.031.680 -> 9.018.368
    shell32.dll releases/0.4.11 RosBEWin2.1.6 GCC4.7.2 dbg x86  8.954.880 -> 8.942.592
    shell32.dll releases/0.4.10 RosBEWin2.1.6 GCC4.7.2 dbg x86  8.945.152 -> 8.932.352
    shell32.dll releases/0.4. 9 RosBEWin2.1.6 GCC4.7.2 dbg x86  8.618.496 -> 8.607.744
    shell32.dll releases/0.4. 8 RosBEWin2.1.6 GCC4.7.2 dbg x86  8.536.576 -> 8.528.896
    shell32.dll releases/0.4. 7 RosBEWin2.1.6 GCC4.7.2 dbg x86  8.466.944 -> 8.459.776

--- dll/win32/shell32/CDefaultContextMenu.cpp
+++ dll/win32/shell32/CDefaultContextMenu.cpp
@@ -1,7 +1,6 @@
 /*
  * PROJECT:     shell32
  * LICENSE:     GPL - See COPYING in the top level directory
- * FILE:        dll/win32/shell32/shv_item_new.c
  * PURPOSE:     provides default context menu implementation
  * PROGRAMMERS: Johannes Anderwald (johannes.anderwald@reactos.org)
  */
@@ -384,7 +383,7 @@ CDefaultContextMenu::LoadDynamicContextMenuHandler(HKEY hKey, const CLSID *pclsi
     hr = pExtInit->Initialize(m_pidlFolder, m_pDataObj, hKey);
     if (FAILED(hr))
     {
-        ERR("IShellExtInit::Initialize failed.clsid %s hr 0x%x\n", wine_dbgstr_guid(pclsid), hr);
+        WARN("IShellExtInit::Initialize failed.clsid %s hr 0x%x\n", wine_dbgstr_guid(pclsid), hr);
         return hr;
     }
 
