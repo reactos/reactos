1d40392148871bb887b4d94e3ea178b6afe3b7a4
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-06-05 19:44:45 +0900

    fix

--- base/setup/lib/muifonts.h
+++ base/setup/lib/muifonts.h
@@ -35,29 +35,6 @@ MUI_SUBFONT LatinFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
@@ -90,29 +67,6 @@ MUI_SUBFONT CyrillicFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
@@ -145,29 +99,6 @@ MUI_SUBFONT GreekFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
@@ -200,7 +131,17 @@ MUI_SUBFONT HebrewFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
+    { NULL, NULL }
+};
+
+WCHAR CSF_LocalName0[] = {0x5B8B, 0x4F53, 0};                   /* SimSun */
+WCHAR CSF_LocalName1[] = {0x4E2D, 0x6613, 0x5B8B, 0x4F53, 0};   /* SimSun */
+WCHAR CSF_LocalName2[] = {0x65B0, 0x5B8B, 0x4F53, 0};           /* NSimSun */
+WCHAR CSF_LocalName3[] = {0xFC4B, 0x91CC, 0};                   /* SimHei */
+WCHAR CSF_LocalName4[] = {'M', 'S', 0x5B8B, 0x4F53, 0};         /* MS Song */
+MUI_SUBFONT ChineseSimplifiedFonts[] =
+{
+    /* The beginning of Far East Asian common */
     { L"Batang",                 L"Droid Sans Fallback" },
     { L"BatangChe",              L"Droid Sans Fallback" },
     { L"DLCMingBold",            L"Droid Sans Fallback" },
@@ -222,17 +163,7 @@ MUI_SUBFONT HebrewFonts[] =
     { L"PMingLiU",               L"Droid Sans Fallback" },
     { L"SimHei",                 L"Droid Sans Fallback" },
     { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
-    { NULL, NULL }
-};
-
-WCHAR CSF_LocalName0[] = {0x5B8B, 0x4F53, 0};                   /* SimSun */
-WCHAR CSF_LocalName1[] = {0x4E2D, 0x6613, 0x5B8B, 0x4F53, 0};   /* SimSun */
-WCHAR CSF_LocalName2[] = {0x65B0, 0x5B8B, 0x4F53, 0};           /* NSimSun */
-WCHAR CSF_LocalName3[] = {0xFC4B, 0x91CC, 0};                   /* SimHei */
-WCHAR CSF_LocalName4[] = {'M', 'S', 0x5B8B, 0x4F53, 0};         /* MS Song */
-MUI_SUBFONT ChineseSimplifiedFonts[] =
-{
+    /* The end of Far East Asian common */
     { L"Arial Baltic,186",       L"Arial,186" },
     { L"Arial CE,238",           L"Arial,238" },
     { L"Arial CYR,204",          L"Arial,204" },
@@ -270,7 +201,16 @@ MUI_SUBFONT ChineseSimplifiedFonts[] =
     { CSF_LocalName2,            L"Droid Sans Fallback" },
     { CSF_LocalName3,            L"Droid Sans Fallback" },
     { CSF_LocalName4,            L"Droid Sans Fallback" },
-    /* The beginning of FE Asian fonts */
+    { NULL, NULL }
+};
+
+WCHAR CTF_LocalName0[] = {0x7D30, 0x660E, 0x9AD4, 0};                       /* MingLiU */
+WCHAR CTF_LocalName1[] = {0x65B0, 0x7D30, 0x660E, 0x9AD4, 0};               /* PMingLiU */
+WCHAR CTF_LocalName2[] = {0x83EF, 0x5EB7, 0x4E2D, 0x660E, 0x9AD4, 0};       /* DLCMingMedium */
+WCHAR CTF_LocalName3[] = {0x83EF, 0x5EB7, 0x7C97, 0x660E, 0x9AD4, 0};       /* DLCMingBold */
+MUI_SUBFONT ChineseTraditionalFonts[] =
+{
+    /* The beginning of Far East Asian common */
     { L"Batang",                 L"Droid Sans Fallback" },
     { L"BatangChe",              L"Droid Sans Fallback" },
     { L"DLCMingBold",            L"Droid Sans Fallback" },
@@ -292,16 +232,7 @@ MUI_SUBFONT ChineseSimplifiedFonts[] =
     { L"PMingLiU",               L"Droid Sans Fallback" },
     { L"SimHei",                 L"Droid Sans Fallback" },
     { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
-    { NULL, NULL }
-};
-
-WCHAR CTF_LocalName0[] = {0x7D30, 0x660E, 0x9AD4, 0};                       /* MingLiU */
-WCHAR CTF_LocalName1[] = {0x65B0, 0x7D30, 0x660E, 0x9AD4, 0};               /* PMingLiU */
-WCHAR CTF_LocalName2[] = {0x83EF, 0x5EB7, 0x4E2D, 0x660E, 0x9AD4, 0};       /* DLCMingMedium */
-WCHAR CTF_LocalName3[] = {0x83EF, 0x5EB7, 0x7C97, 0x660E, 0x9AD4, 0};       /* DLCMingBold */
-MUI_SUBFONT ChineseTraditionalFonts[] =
-{
+    /* The end of Far East Asian common */
     { L"Arial Baltic,186",       L"Arial,186" },
     { L"Arial CE,238",           L"Arial,238" },
     { L"Arial CYR,204",          L"Arial,204" },
@@ -338,7 +269,16 @@ MUI_SUBFONT ChineseTraditionalFonts[] =
     { CTF_LocalName1,            L"Droid Sans Fallback" },
     { CTF_LocalName2,            L"Droid Sans Fallback" },
     { CTF_LocalName3,            L"Droid Sans Fallback" },
-    /* The beginning of FE Asian fonts */
+    { NULL, NULL }
+};
+
+WCHAR JF_LocalName0[] = {0xFF2D, 0xFF33, ' ', 0x660E, 0x671D, 0};                           /* MS Mincho */
+WCHAR JF_LocalName1[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x660E, 0x671D, 0};                   /* MS PMincho */
+WCHAR JF_LocalName2[] = {0xFF2D, 0xFF33, ' ', 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};           /* MS Gothic */
+WCHAR JF_LocalName3[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};   /* MS PGothic */
+MUI_SUBFONT JapaneseFonts[] =
+{
+    /* The beginning of Far East Asian common */
     { L"Batang",                 L"Droid Sans Fallback" },
     { L"BatangChe",              L"Droid Sans Fallback" },
     { L"DLCMingBold",            L"Droid Sans Fallback" },
@@ -360,16 +300,7 @@ MUI_SUBFONT ChineseTraditionalFonts[] =
     { L"PMingLiU",               L"Droid Sans Fallback" },
     { L"SimHei",                 L"Droid Sans Fallback" },
     { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
-    { NULL, NULL }
-};
-
-WCHAR JF_LocalName0[] = {0xFF2D, 0xFF33, ' ', 0x660E, 0x671D, 0};                           /* MS Mincho */
-WCHAR JF_LocalName1[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x660E, 0x671D, 0};                   /* MS PMincho */
-WCHAR JF_LocalName2[] = {0xFF2D, 0xFF33, ' ', 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};           /* MS Gothic */
-WCHAR JF_LocalName3[] = {0xFF2D, 0xFF33, ' ', 0xFF30, 0x30B4, 0x30B7, 0x30C3, 0x30AF, 0};   /* MS PGothic */
-MUI_SUBFONT JapaneseFonts[] =
-{
+    /* The end of Far East Asian common */
     { L"Arial Baltic,186",       L"Arial,186" },
     { L"Arial CE,238",           L"Arial,238" },
     { L"Arial CYR,204",          L"Arial,204" },
@@ -406,7 +337,18 @@ MUI_SUBFONT JapaneseFonts[] =
     { JF_LocalName1,             L"Droid Sans Fallback" },
     { JF_LocalName2,             L"Droid Sans Fallback" },
     { JF_LocalName3,             L"Droid Sans Fallback" },
-    /* The beginning of FE Asian fonts */
+    { NULL, NULL }
+};
+
+WCHAR KF_LocalName0[] = {0xBC14, 0xD0D5, 0};            /* Batang */
+WCHAR KF_LocalName1[] = {0xBC14, 0xD0D5, 0xCCB4, 0};    /* BatangChe */
+WCHAR KF_LocalName2[] = {0xAD81, 0xC11C, 0};            /* Gungsuh */
+WCHAR KF_LocalName3[] = {0xAD81, 0xC11C, 0xCCB4, 0};    /* GungsuhChe */
+WCHAR KF_LocalName4[] = {0xAD74, 0xB9BC, 0};            /* Gulim */
+WCHAR KF_LocalName5[] = {0xAD74, 0xB9BC, 0xCCB4, 0};    /* GulimChe */
+MUI_SUBFONT KoreanFonts[] =
+{
+    /* The beginning of Far East Asian common */
     { L"Batang",                 L"Droid Sans Fallback" },
     { L"BatangChe",              L"Droid Sans Fallback" },
     { L"DLCMingBold",            L"Droid Sans Fallback" },
@@ -420,26 +362,15 @@ MUI_SUBFONT JapaneseFonts[] =
     { L"MS PGothic",             L"Droid Sans Fallback" },
     { L"MS PMincho",             L"Droid Sans Fallback" },
     { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
     { L"MS UI Gothic",           L"Droid Sans Fallback" },
+    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
     { L"Ming Light",             L"Droid Sans Fallback" },
     { L"MingLiU",                L"Droid Sans Fallback" },
     { L"NSimSun",                L"Droid Sans Fallback" },
     { L"PMingLiU",               L"Droid Sans Fallback" },
     { L"SimHei",                 L"Droid Sans Fallback" },
     { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
-    { NULL, NULL }
-};
-
-WCHAR KF_LocalName0[] = {0xBC14, 0xD0D5, 0};            /* Batang */
-WCHAR KF_LocalName1[] = {0xBC14, 0xD0D5, 0xCCB4, 0};    /* BatangChe */
-WCHAR KF_LocalName2[] = {0xAD81, 0xC11C, 0};            /* Gungsuh */
-WCHAR KF_LocalName3[] = {0xAD81, 0xC11C, 0xCCB4, 0};    /* GungsuhChe */
-WCHAR KF_LocalName4[] = {0xAD74, 0xB9BC, 0};            /* Gulim */
-WCHAR KF_LocalName5[] = {0xAD74, 0xB9BC, 0xCCB4, 0};    /* GulimChe */
-MUI_SUBFONT KoreanFonts[] =
-{
+    /* The end of Far East Asian common */
     { L"Arial Baltic,186",       L"Arial,186" },
     { L"Arial CE,238",           L"Arial,238" },
     { L"Arial CYR,204",          L"Arial,204" },
@@ -478,29 +409,6 @@ MUI_SUBFONT KoreanFonts[] =
     { KF_LocalName3,             L"Droid Sans Fallback" },
     { KF_LocalName4,             L"Droid Sans Fallback" },
     { KF_LocalName5,             L"Droid Sans Fallback" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
@@ -529,29 +437,6 @@ MUI_SUBFONT UnicodeFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
@@ -587,29 +472,6 @@ MUI_SUBFONT HindiFonts[] =
     { L"Times New Roman Greek,161",    L"Times New Roman,161" },
     { L"Times New Roman TUR,162",      L"Times New Roman,162" },
     { L"Tms Rmn",                L"Times New Roman" },
-    /* The beginning of FE Asian fonts */
-    { L"Batang",                 L"Droid Sans Fallback" },
-    { L"BatangChe",              L"Droid Sans Fallback" },
-    { L"DLCMingBold",            L"Droid Sans Fallback" },
-    { L"DLCMingMedium",          L"Droid Sans Fallback" },
-    { L"Gulim",                  L"Droid Sans Fallback" },
-    { L"GulimChe",               L"Droid Sans Fallback" },
-    { L"Gungsuh",                L"Droid Sans Fallback" },
-    { L"GungsuhChe",             L"Droid Sans Fallback" },
-    { L"MS Gothic",              L"Droid Sans Fallback" },
-    { L"MS Mincho",              L"Droid Sans Fallback" },
-    { L"MS PGothic",             L"Droid Sans Fallback" },
-    { L"MS PMincho",             L"Droid Sans Fallback" },
-    { L"MS Song",                L"Droid Sans Fallback" },
-    { L"MS UI Gothic",           L"Droid Sans Fallback" },
-    { L"MS UI Gothic 2",         L"Droid Sans Fallback" },
-    { L"Ming Light",             L"Droid Sans Fallback" },
-    { L"MingLiU",                L"Droid Sans Fallback" },
-    { L"NSimSun",                L"Droid Sans Fallback" },
-    { L"PMingLiU",               L"Droid Sans Fallback" },
-    { L"SimHei",                 L"Droid Sans Fallback" },
-    { L"SimSun",                 L"Droid Sans Fallback" },
-    /* The end of FE Asian fonts */
     { NULL, NULL }
 };
 
