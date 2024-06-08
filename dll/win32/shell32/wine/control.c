ba25776fc7010c5267fcfa7424ee8328a5ac5c51
Thamatip Chitpong <thamatip.chitpong@reactos.org>
2024-06-08 23:15:16 +0700

    [SHELL32] Control_DoWindow: Add file browser fallback
    If Explorer shell is not available, use ReactOS's alternative file browser instead.
    CORE-19648

--- dll/win32/shell32/wine/control.c
+++ dll/win32/shell32/wine/control.c
@@ -732,9 +732,10 @@ static	void	Control_DoWindow(CPanel* panel, HWND hWnd, HINSTANCE hInst)
 #else
 static	void	Control_DoWindow(CPanel* panel, HWND hWnd, HINSTANCE hInst)
 {
+    /* NOTE: If Explorer shell is not available, use ReactOS's alternative file browser instead */
     ShellExecuteW(NULL,
                   L"open",
-                  L"explorer.exe",
+                  GetShellWindow() ? L"explorer.exe" : L"filebrowser.exe",
                   L"::{20D04FE0-3AEA-1069-A2D8-08002B30309D}\\::{21EC2020-3AEA-1069-A2DD-08002B30309D}",
                   NULL,
                   SW_SHOWDEFAULT);
