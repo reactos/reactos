f57601d14bb252ac95f0d0ab19d33978016d4fa3
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2024-06-26 13:46:48 +0200

    [SETUPLIB][USETUP] FS utilities: Use the ISO C and C++ conformant name: _wcsicmp
    Stop using non-conforming wcsicmp.
    See also PR #6925 by Timo Kreuzer.

--- base/setup/lib/fsutil.c
+++ base/setup/lib/fsutil.c
@@ -207,7 +207,7 @@ GetFileSystemByName(
     {
         Item = CONTAINING_RECORD(ListEntry, FILE_SYSTEM_ITEM, ListEntry);
         if (Item->FileSystemName &&
-            (wcsicmp(FileSystemName, Item->FileSystemName) == 0))
+            (_wcsicmp(FileSystemName, Item->FileSystemName) == 0))
         {
             return Item;
         }
@@ -225,7 +225,7 @@ GetFileSystemByName(
     while (Count--)
     {
         if (FileSystems->FileSystemName &&
-            (wcsicmp(FileSystemName, FileSystems->FileSystemName) == 0))
+            (_wcsicmp(FileSystemName, FileSystems->FileSystemName) == 0))
         {
             return FileSystems;
         }
@@ -332,9 +332,9 @@ FormatFileSystem_UStr(
     }
 
     /* Set the BackwardCompatible flag in case we format with older FAT12/16 */
-    if (wcsicmp(FileSystemName, L"FAT") == 0)
+    if (_wcsicmp(FileSystemName, L"FAT") == 0)
         BackwardCompatible = TRUE;
-    // else if (wcsicmp(FileSystemName, L"FAT32") == 0)
+    // else if (_wcsicmp(FileSystemName, L"FAT32") == 0)
         // BackwardCompatible = FALSE;
 
     /* Convert the FMIFS MediaFlag to a NT MediaType */
@@ -849,7 +849,7 @@ FormatPartition(
      * Adjust the file system name in case of FAT vs. FAT32, according to
      * the type of partition returned by FileSystemToMBRPartitionType().
      */
-    if (wcsicmp(FileSystemName, L"FAT") == 0)
+    if (_wcsicmp(FileSystemName, L"FAT") == 0)
     {
         if ((PartitionType == PARTITION_FAT32) ||
             (PartitionType == PARTITION_FAT32_XINT13))
