9c26ecdd4712896ccaf83e5945d38241332a6038
Joachim Henze <joachim.henze@reactos.org>
2024-06-30 13:02:36 +0200

    invest another line
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- base/applications/regedit/framewnd.c
+++ base/applications/regedit/framewnd.c
@@ -1464,7 +1464,8 @@ static BOOL _CmdWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return result;
 }
 
-/* PURPOSE: Processes messages for the main frame window
+/*
+ * PURPOSE: Processes messages for the main frame window
  *
  * WM_COMMAND - process the application menu
  * WM_DESTROY - post a quit message and return
