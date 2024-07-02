a452db5d6a53e098372437ef549632d4ad1dab65
Joachim Henze <Joachim.Henze@reactos.org>
2024-07-02 23:47:17 +0200

    [0.4.14][RAPPS] Strip certificate pinning, tweak RC+formatting
    This fixes CORE-19666 '[RAPPS] Let's encrypt certificate (expired?), first seen 2024-06-30'.
    This is fixed for now only in the releases, but not on master yet. Most likely master will receive a certificate update instead.
    
    We had the same expiring happening 3 years ago,
    back then I teeth-gnashingly applied more code to rapps in the older branches
    to allow it to still connect even when the certificate expired, see 0.4.14-RC-62-g ad8521d0239486c25fc6550c59847548ba93c192
    which was better than before where the access was blocked entirely.
    But still even with that ugly message-box it still does not feel sustainable for me for the older branches.
    I don't want to touch that every 3 years just because of that BS.
    Let's simply get rid of that stuff for the older branches. That type of expiring certs is nothing but
    running within an endless hamster-wheel.
    
    All the changes to strip certificate pinning are located within:
    CMakelists.txt, loaddlg.cpp, resource.h, *.rc (stripping of 2 IDS_UNABLE_TO_QUERY_CERT & IDS_MISMATCH_CERT_INFO)
    
    The rest of this backport is unrelated: either small translation improvements, or
    formatting-improvements within the code (only whitespace, no functional change anywhere).
    
    size of rapps.exe shrinks on all rls-branches:
    master          RosBEWin2.2.2 GCC8.4.0 dbg x86            1.677.824 (0.4.15-dev-8260-gedf6b80)
    releases/0.4.14 RosBEWin2.2.2 GCC8.4.0 dbg x86  944.128 ->  924.160
    releases/0.4.14 RosBEWin2.1.6 GCC4.7.2 dbg x86  959.488 ->  942.592
    releases/0.4.13 RosBEWin2.1.6 GCC4.7.2 dbg x86  883.200 ->  866.816
    releases/0.4.12 RosBEWin2.1.6 GCC4.7.2 dbg x86  888.320 ->  875.008
    releases/0.4.11 RosBEWin2.1.6 GCC4.7.2 dbg x86  873.472 ->  860.672
    releases/0.4.10 RosBEWin2.1.6 GCC4.7.2 dbg x86  865.792 ->  852.992
    releases/0.4. 9 RosBEWin2.1.6 GCC4.7.2 dbg x86  865.280 ->  845.824
    releases/0.4. 8 RosBEWin2.1.6 GCC4.7.2 dbg x86  865.280 ->  845.824
    releases/0.4. 7 RosBEWin2.1.6 GCC4.7.2 dbg x86  865.280 ->  845.824

--- base/applications/rapps/loaddlg.cpp
+++ base/applications/rapps/loaddlg.cpp
@@ -1,33 +1,32 @@
 /*
  * PROJECT:     ReactOS Applications Manager
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
- * FILE:        base/applications/rapps/loaddlg.cpp
  * PURPOSE:     Displaying a download dialog
- * COPYRIGHT:   Copyright 2001 John R. Sheets             (for CodeWeavers)
- *              Copyright 2004 Mike McCormack             (for CodeWeavers)
- *              Copyright 2005 Ge van Geldorp             (gvg@reactos.org)
- *              Copyright 2009 Dmitry Chapyshev           (dmitry@reactos.org)
+ * COPYRIGHT:   Copyright 2001 John R. Sheets (for CodeWeavers)
+ *              Copyright 2004 Mike McCormack (for CodeWeavers)
+ *              Copyright 2005 Ge van Geldorp (gvg@reactos.org)
+ *              Copyright 2009 Dmitry Chapyshev (dmitry@reactos.org)
  *              Copyright 2015 Ismael Ferreras Morezuelas (swyterzone+ros@gmail.com)
- *              Copyright 2017 Alexander Shaposhnikov     (sanchaez@reactos.org)
+ *              Copyright 2017 Alexander Shaposhnikov (sanchaez@reactos.org)
  */
 
- /*
-  * Based on Wine dlls/shdocvw/shdocvw_main.c
-  *
-  * This library is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU Lesser General Public
-  * License as published by the Free Software Foundation; either
-  * version 2.1 of the License, or (at your option) any later version.
-  *
-  * This library is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  * Lesser General Public License for more details.
-  *
-  * You should have received a copy of the GNU Lesser General Public
-  * License along with this library; if not, write to the Free Software
-  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-  */
+/*
+ * Based on Wine dlls/shdocvw/shdocvw_main.c
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #include "rapps.h"
 
 #include <shlobj_undoc.h>
@@ -47,12 +46,6 @@
 #include "rosui.h"
 #include "dialogs.h"
 #include "misc.h"
-
-#ifdef USE_CERT_PINNING
-#define CERT_ISSUER_INFO_OLD "US\r\nLet's Encrypt\r\nLet's Encrypt Authority X3"
-#define CERT_ISSUER_INFO_NEW "US\r\nLet's Encrypt\r\nR3"
-#define CERT_SUBJECT_INFO "rapps.reactos.org"
-#endif
 
 enum DownloadStatus
 {
@@ -101,9 +94,7 @@ struct DownloadParam
     LPCWSTR szCaption;
 };
 
-
-class CDownloaderProgress
-    : public CWindowImpl<CDownloaderProgress, CWindow, CControlWinTraits>
+class CDownloaderProgress : public CWindowImpl<CDownloaderProgress, CWindow, CControlWinTraits>
 {
     ATL::CStringW m_szProgressText;
 
@@ -136,7 +127,7 @@ public:
         {
             /* total size is known */
             WCHAR szProgressMax[100];
-            UINT uiPercentage = ((ULONGLONG) ulProgress * 100) / ulProgressMax;
+            UINT uiPercentage = ((ULONGLONG)ulProgress * 100) / ulProgressMax;
 
             /* send the current progress to the progress bar */
             if (!IsWindow()) return;
@@ -146,10 +137,7 @@ public:
             StrFormatByteSizeW(ulProgressMax, szProgressMax, _countof(szProgressMax));
 
             /* generate the text on progress bar */
-            ProgressText.Format(L"%u%% \x2014 %ls / %ls",
-                                uiPercentage,
-                                szProgress,
-                                szProgressMax);
+            ProgressText.Format(L"%u%% \x2014 %ls / %ls", uiPercentage, szProgress, szProgressMax);
         }
         else
         {
@@ -163,12 +151,12 @@ public:
 
         /* and finally display it */
         if (!IsWindow()) return;
-        SendMessage(WM_SETTEXT, 0, (LPARAM) ProgressText.GetString());
+        SendMessage(WM_SETTEXT, 0, (LPARAM)ProgressText.GetString());
     }
 
     LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled)
     {
-        PAINTSTRUCT  ps;
+        PAINTSTRUCT ps;
         HDC hDC = BeginPaint(&ps), hdcMem;
         HBITMAP hbmMem;
         HANDLE hOld;
@@ -188,16 +176,14 @@ public:
         hOld = SelectObject(hdcMem, hbmMem);
 
         /* call the original draw code and redirect it to our memory buffer */
-        DefWindowProc(uMsg, (WPARAM) hdcMem, lParam);
+        DefWindowProc(uMsg, (WPARAM)hdcMem, lParam);
 
         /* draw our nifty progress text over it */
         SelectFont(hdcMem, GetStockFont(DEFAULT_GUI_FONT));
-        DrawShadowText(hdcMem, m_szProgressText.GetString(), m_szProgressText.GetLength(),
-                       &myRect,
-                       DT_CENTER | DT_VCENTER | DT_NOPREFIX | DT_SINGLELINE,
-                       GetSysColor(COLOR_CAPTIONTEXT),
-                       GetSysColor(COLOR_3DSHADOW),
-                       1, 1);
+        DrawShadowText(
+            hdcMem, m_szProgressText.GetString(), m_szProgressText.GetLength(), &myRect,
+            DT_CENTER | DT_VCENTER | DT_NOPREFIX | DT_SINGLELINE, GetSysColor(COLOR_CAPTIONTEXT),
+            GetSysColor(COLOR_3DSHADOW), 1, 1);
 
         /* transfer the off-screen DC to the screen */
         BitBlt(hDC, 0, 0, win_width, win_height, hdcMem, 0, 0, SRCCOPY);
@@ -221,14 +207,13 @@ public:
     }
 
     BEGIN_MSG_MAP(CDownloaderProgress)
-        MESSAGE_HANDLER(WM_ERASEBKGND, OnPaint)
-        MESSAGE_HANDLER(WM_PAINT, OnPaint)
-        MESSAGE_HANDLER(WM_SETTEXT, OnSetText)
+    MESSAGE_HANDLER(WM_ERASEBKGND, OnPaint)
+    MESSAGE_HANDLER(WM_PAINT, OnPaint)
+    MESSAGE_HANDLER(WM_SETTEXT, OnSetText)
     END_MSG_MAP()
 };
 
-class CDowloadingAppsListView
-    : public CListView
+class CDowloadingAppsListView : public CListView
 {
 public:
     HWND Create(HWND hwndParent)
@@ -275,8 +260,7 @@ public:
     VOID AddRow(INT RowIndex, LPCWSTR szAppName, const DownloadStatus Status)
     {
         ATL::CStringW szStatus = LoadStatusString(Status);
-        AddItem(RowIndex,
-                const_cast<LPWSTR>(szAppName));
+        AddItem(RowIndex, const_cast<LPWSTR>(szAppName));
         SetDownloadStatus(RowIndex, Status);
     }
 
@@ -293,50 +277,6 @@ public:
         return (InsertColumn(Index, &Column) == -1) ? FALSE : TRUE;
     }
 };
-
-#ifdef USE_CERT_PINNING
-typedef CHeapPtr<char, CLocalAllocator> CLocalPtr;
-
-static BOOL CertGetSubjectAndIssuer(HINTERNET hFile, CLocalPtr& subjectInfo, CLocalPtr& issuerInfo)
-{
-    DWORD certInfoLength;
-    INTERNET_CERTIFICATE_INFOA certInfo;
-    DWORD size, flags;
-
-    size = sizeof(flags);
-    if (!InternetQueryOptionA(hFile, INTERNET_OPTION_SECURITY_FLAGS, &flags, &size))
-    {
-        return FALSE;
-    }
-
-    if (!flags & SECURITY_FLAG_SECURE)
-    {
-        return FALSE;
-    }
-
-    /* Despite what the header indicates, the implementation of INTERNET_CERTIFICATE_INFO is not Unicode-aware. */
-    certInfoLength = sizeof(certInfo);
-    if (!InternetQueryOptionA(hFile,
-                              INTERNET_OPTION_SECURITY_CERTIFICATE_STRUCT,
-                              &certInfo,
-                              &certInfoLength))
-    {
-        return FALSE;
-    }
-
-    subjectInfo.Attach(certInfo.lpszSubjectInfo);
-    issuerInfo.Attach(certInfo.lpszIssuerInfo);
-
-    if (certInfo.lpszProtocolName)
-        LocalFree(certInfo.lpszProtocolName);
-    if (certInfo.lpszSignatureAlgName)
-        LocalFree(certInfo.lpszSignatureAlgName);
-    if (certInfo.lpszEncryptionAlgName)
-        LocalFree(certInfo.lpszEncryptionAlgName);
-
-    return certInfo.lpszSubjectInfo && certInfo.lpszIssuerInfo;
-}
-#endif
 
 inline VOID MessageBox_LoadString(HWND hMainWnd, INT StringID)
 {
@@ -364,13 +304,12 @@ public:
     static VOID LaunchDownloadDialog(BOOL);
 };
 
-
 // CDownloadManager
-ATL::CSimpleArray<DownloadInfo>         CDownloadManager::AppsToInstallList;
-CDowloadingAppsListView                 CDownloadManager::DownloadsListView;
-CDownloaderProgress                     CDownloadManager::ProgressBar;
-BOOL                                    CDownloadManager::bCancelled = FALSE;
-BOOL                                    CDownloadManager::bModal = FALSE;
+ATL::CSimpleArray<DownloadInfo> CDownloadManager::AppsToInstallList;
+CDowloadingAppsListView CDownloadManager::DownloadsListView;
+CDownloaderProgress CDownloadManager::ProgressBar;
+BOOL CDownloadManager::bCancelled = FALSE;
+BOOL CDownloadManager::bModal = FALSE;
 
 VOID CDownloadManager::Add(DownloadInfo info)
 {
@@ -397,13 +336,13 @@ INT_PTR CALLBACK CDownloadManager::DownloadDlgProc(HWND Dlg, UINT uMsg, WPARAM w
 
         bCancelled = FALSE;
 
-        hIconBg = (HICON) GetClassLongPtrW(hMainWnd, GCLP_HICON);
-        hIconSm = (HICON) GetClassLongPtrW(hMainWnd, GCLP_HICONSM);
+        hIconBg = (HICON)GetClassLongPtrW(hMainWnd, GCLP_HICON);
+        hIconSm = (HICON)GetClassLongPtrW(hMainWnd, GCLP_HICONSM);
 
         if (hIconBg && hIconSm)
         {
-            SendMessageW(Dlg, WM_SETICON, ICON_BIG, (LPARAM) hIconBg);
-            SendMessageW(Dlg, WM_SETICON, ICON_SMALL, (LPARAM) hIconSm);
+            SendMessageW(Dlg, WM_SETICON, ICON_BIG, (LPARAM)hIconBg);
+            SendMessageW(Dlg, WM_SETICON, ICON_SMALL, (LPARAM)hIconSm);
         }
 
         SetWindowLongW(Dlg, GWLP_USERDATA, 0);
@@ -438,8 +377,7 @@ INT_PTR CALLBACK CDownloadManager::DownloadDlgProc(HWND Dlg, UINT uMsg, WPARAM w
         // Start download process
         DownloadParam *param = new DownloadParam(Dlg, AppsToInstallList, szCaption);
         unsigned int ThreadId;
-        HANDLE Thread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, (void *) param, 0, &ThreadId);
-
+        HANDLE Thread = (HANDLE)_beginthreadex(NULL, 0, ThreadFunc, (void *)param, 0, &ThreadId);
         if (!Thread)
         {
             return FALSE;
@@ -509,7 +447,7 @@ VOID CDownloadManager::UpdateProgress(
         }
 
         /* paste it into our dialog and don't do it again in this instance */
-        SendMessageW(Item, WM_SETTEXT, 0, (LPARAM) buf.GetString());
+        SendMessageW(Item, WM_SETTEXT, 0, (LPARAM)buf.GetString());
         UrlHasBeenCopied = TRUE;
     }
 }
@@ -578,13 +516,9 @@ unsigned int WINAPI CDownloadManager::ThreadFunc(LPVOID param)
 
         // Change caption to show the currently downloaded app
         if (!bCab)
-        {
             szNewCaption.Format(szCaption, InfoArray[iAppId].szName.GetString());
-        }
         else
-        {
             szNewCaption.LoadStringW(IDS_DL_DIALOG_DB_DOWNLOAD_DISP);
-        }
 
         if (!IsWindow(hDlg)) goto end;
         SetWindowTextW(hDlg, szNewCaption.GetString());
@@ -657,10 +591,6 @@ unsigned int WINAPI CDownloadManager::ThreadFunc(LPVOID param)
         urlComponents.dwStructSize = sizeof(urlComponents);
 
         urlLength = InfoArray[iAppId].szUrl.GetLength();
-        urlComponents.dwSchemeLength = urlLength + 1;
-        urlComponents.lpszScheme = (LPWSTR) malloc(urlComponents.dwSchemeLength * sizeof(WCHAR));
-        urlComponents.dwHostNameLength = urlLength + 1;
-        urlComponents.lpszHostName = (LPWSTR) malloc(urlComponents.dwHostNameLength * sizeof(WCHAR));
 
         if (!InternetCrackUrlW(InfoArray[iAppId].szUrl, urlLength + 1, ICU_DECODE | ICU_ESCAPE, &urlComponents))
             goto end;
@@ -669,9 +599,7 @@ unsigned int WINAPI CDownloadManager::ThreadFunc(LPVOID param)
 
         if (urlComponents.nScheme == INTERNET_SCHEME_HTTP || urlComponents.nScheme == INTERNET_SCHEME_HTTPS)
         {
-            hFile = InternetOpenUrlW(hOpen, InfoArray[iAppId].szUrl.GetString(), NULL, 0,
-                                     dwUrlConnectFlags,
-                                     0);
+            hFile = InternetOpenUrlW(hOpen, InfoArray[iAppId].szUrl.GetString(), NULL, 0, dwUrlConnectFlags, 0);
             if (!hFile)
             {
                 MessageBox_LoadString(hMainWnd, IDS_UNABLE_TO_DOWNLOAD2);
@@ -719,43 +647,6 @@ unsigned int WINAPI CDownloadManager::ThreadFunc(LPVOID param)
                 ProgressBar.SetMarquee(TRUE);
             }
         }
-
-        free(urlComponents.lpszScheme);
-        free(urlComponents.lpszHostName);
-
-#ifdef USE_CERT_PINNING
-        // are we using HTTPS to download the RAPPS update package? check if the certificate is original
-        if ((urlComponents.nScheme == INTERNET_SCHEME_HTTPS) &&
-            (wcscmp(InfoArray[iAppId].szUrl, APPLICATION_DATABASE_URL) == 0))
-        {
-            CLocalPtr subjectName, issuerName;
-            CStringA szMsgText;
-            bool bAskQuestion = false;
-            if (!CertGetSubjectAndIssuer(hFile, subjectName, issuerName))
-            {
-                szMsgText.LoadStringW(IDS_UNABLE_TO_QUERY_CERT);
-                bAskQuestion = true;
-            }
-            else
-            {
-                if (strcmp(subjectName, CERT_SUBJECT_INFO) ||
-                    (strcmp(issuerName, CERT_ISSUER_INFO_OLD) &&
-                    strcmp(issuerName, CERT_ISSUER_INFO_NEW)))
-                {
-                    szMsgText.Format(IDS_MISMATCH_CERT_INFO, (char*)subjectName, (const char*)issuerName);
-                    bAskQuestion = true;
-                }
-            }
-
-            if (bAskQuestion)
-            {
-                if (MessageBoxA(hMainWnd, szMsgText.GetString(), NULL, MB_YESNO | MB_ICONERROR) != IDYES)
-                {
-                    goto end;
-                }
-            }
-        }
-#endif
 
         hOut = CreateFileW(Path.GetString(), GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, 0, NULL);
 
@@ -810,7 +701,7 @@ unsigned int WINAPI CDownloadManager::ThreadFunc(LPVOID param)
 
             if (!IsWindow(hDlg)) goto end;
             SetWindowTextW(hDlg, szMsgText.GetString());
-            SendMessageW(GetDlgItem(hDlg, IDC_DOWNLOAD_STATUS), WM_SETTEXT, 0, (LPARAM) Path.GetString());
+            SendMessageW(GetDlgItem(hDlg, IDC_DOWNLOAD_STATUS), WM_SETTEXT, 0, (LPARAM)Path.GetString());
 
             // this may take a while, depending on the file size
             if (!VerifyInteg(InfoArray[iAppId].szSHA1.GetString(), Path.GetString()))
@@ -887,21 +778,14 @@ VOID CDownloadManager::LaunchDownloadDialog(BOOL bIsModal)
     CDownloadManager::bModal = bIsModal;
     if (bIsModal)
     {
-        DialogBoxW(hInst,
-                   MAKEINTRESOURCEW(IDD_DOWNLOAD_DIALOG),
-                   hMainWnd,
-                   DownloadDlgProc);
+        DialogBoxW(hInst, MAKEINTRESOURCEW(IDD_DOWNLOAD_DIALOG), hMainWnd, DownloadDlgProc);
     }
     else
     {
-        CreateDialogW(hInst,
-                      MAKEINTRESOURCEW(IDD_DOWNLOAD_DIALOG),
-                      hMainWnd,
-                      DownloadDlgProc);
+        CreateDialogW(hInst, MAKEINTRESOURCEW(IDD_DOWNLOAD_DIALOG), hMainWnd, DownloadDlgProc);
     }
 }
 // CDownloadManager
-
 
 BOOL DownloadListOfApplications(const ATL::CSimpleArray<CAvailableApplicationInfo>& AppsList, BOOL bIsModal)
 {
@@ -936,4 +820,3 @@ VOID DownloadApplicationsDB(LPCWSTR lpUrl)
     DatabaseDLInfo.szName.LoadStringW(IDS_DL_DIALOG_DB_DISP);
     CDownloadManager::Download(DatabaseDLInfo, TRUE);
 }
-
