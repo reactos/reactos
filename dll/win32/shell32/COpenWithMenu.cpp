b41332349ab557ffd032cd995a4feb0bd05ade41
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-07-03 23:01:50 +0900

    [SHELL32] Refresh UI on assoc change (SHOpenWithDialog) (#7076)
    Fix file association UX.
    JIRA issue: CORE-19670
    - Check the return value of
      shell32!SHOpenWithDialog function.
    - If changed, then update the UI display
      and internal data.
    - Use OAIF_FORCE_REGISTRATION flag.

--- dll/win32/shell32/COpenWithMenu.cpp
+++ dll/win32/shell32/COpenWithMenu.cpp
@@ -1049,8 +1049,12 @@ VOID COpenWithDialog::Accept()
     if (pApp)
     {
         /* Set programm as default handler */
-        if (SendDlgItemMessage(m_hDialog, 14003, BM_GETCHECK, 0, 0) == BST_CHECKED)
+        if (IsDlgButtonChecked(m_hDialog, 14003) == BST_CHECKED)
+        {
             m_pAppList->SetDefaultHandler(pApp, m_pInfo->pcszFile);
+            // FIXME: Update DefaultIcon registry
+            SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_FLUSHNOWAIT, NULL, NULL);
+        }
 
         /* Execute program */
         if (m_pInfo->oaifInFlags & OAIF_EXEC)
