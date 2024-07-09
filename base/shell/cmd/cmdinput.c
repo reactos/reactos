e5001a09fada75b38776c5c3630b97c0e1ae0439
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-07-09 22:06:45 +0900

    big fix

--- base/shell/cmd/cmdinput.c
+++ base/shell/cmd/cmdinput.c
@@ -108,8 +108,8 @@
  * to know the differences between those two settings.
  * Values 0x00, 0x0D (carriage return) and 0x20 (space) disable completion.
  */
-TCHAR AutoCompletionChar = _T('\t'); // Default is 0x20
-TCHAR PathCompletionChar = _T('\t'); // Default is 0x20
+TCHAR AutoCompletionChar = 0x20; // Disabled by default
+TCHAR PathCompletionChar = 0x20; // Disabled by default
 
 
 SHORT maxx;
