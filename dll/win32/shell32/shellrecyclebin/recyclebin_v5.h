22b913928f4a1d13120b0ec7a1c6889d37ec0e3a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-07-29 08:14:02 +0900

    [SHELL32] RecycleBin5Enum: Make it C++ (#7173)
    Modernize code.
    JIRA issue: CORE-19595
    Rewrite RecycleBin5Enum in C++.

--- dll/win32/shell32/shellrecyclebin/recyclebin_v5.h
+++ dll/win32/shell32/shellrecyclebin/recyclebin_v5.h
@@ -80,13 +80,14 @@ DECLARE_INTERFACE_(IRecycleBin5, IUnknown)
     (This)->lpVtbl->OnClosing(This, prb5el)
 #endif
 
+EXTERN_C
 HRESULT
 RecycleBin5Enum_Constructor(
-    IN IRecycleBin5 *prb,
-    IN HANDLE hInfo,
-    IN HANDLE hInfoMapped,
-    IN LPCWSTR szPrefix,
-    OUT IUnknown **ppUnknown);
+    _In_ IRecycleBin5 *prb,
+    _In_ HANDLE hInfo,
+    _In_ HANDLE hInfoMapped,
+    _In_ LPCWSTR szPrefix,
+    _Out_ IUnknown **ppUnknown);
 
 #ifdef __cplusplus
 }
