fc6b3c570628d415b6af1b91c7ac6ee2bcda60de
Doug Lyons <douglyons@douglyons.com>
2024-08-08 23:56:53 -0500

    Fix right popup menu over edge again and add some documentation.

--- win32ss/user/ntuser/menu.c
+++ win32ss/user/ntuser/menu.c
@@ -2988,18 +2988,22 @@ static BOOL FASTCALL MENU_ShowPopup(PWND pwndOwner, PMENU menu, UINT id, UINT fl
     DebugPoint(x, y, RGB(0, 0, 255));
 #endif
     monitor = UserMonitorFromPoint( ptx, MONITOR_DEFAULTTONEAREST );
+    TRACE("RECT is (%d,%d)-(%d,%d)\n", monitor->rcMonitor.left,
+          monitor->rcMonitor.top, monitor->rcMonitor.right, monitor->rcMonitor.bottom);
+    /* The above shows 'RECT is (0,0)-(800,600)' for an 800x600 pixel screen.
+     * So the x and y values in this case start at 0 and end at 799 and 599. */
 
     /* We are off the right side of the screen */
     if (x + width > monitor->rcMonitor.right)
     {
-        /* Position menu at right edge of screen */
-        x = monitor->rcMonitor.right - width + 1;
+        /* Position menu at right edge of the screen */
+        x = monitor->rcMonitor.right - width;
     }
 
     /* We are off the left side of the screen */
     if (x < monitor->rcMonitor.left)
     {
-        /* Position menu at left edge of screen */
+        /* Position menu at left edge of the screen */
         x = 0;
 
         if (x < monitor->rcMonitor.left || x >= monitor->rcMonitor.right || bIsPopup)
