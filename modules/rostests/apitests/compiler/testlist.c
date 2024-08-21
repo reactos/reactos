6fd6e9c306e35bac0225292854fd4c481797f567
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-08-21 22:40:58 +0300

    [COMPILER_APITEST] Add tests for float to int conversion
    These tests come with a VS solution, because that is the only way to test against a known good system, as the required runtime functions (like _ftol) are statically linked from the VS runtime library.

--- modules/rostests/apitests/compiler/testlist.c
+++ modules/rostests/apitests/compiler/testlist.c
@@ -1,16 +1,20 @@
 #define STANDALONE
 #include <apitest.h>
 
+extern void func_floatconv(void);
 extern void func_ms_seh(void);
 extern void func_pseh(void);
 extern void func_pseh_cpp(void);
 
 const struct test winetest_testlist[] =
 {
+    { "floatconv", func_floatconv },
+#ifndef __VS_PROJECT__
 #if !(defined(__GNUC__) && defined(_M_AMD64))
     { "ms-seh", func_ms_seh },
 #endif
     { "pseh", func_pseh },
     { "pseh_cpp", func_pseh_cpp },
+#endif /* __VS_PROJECT__ */
     { 0, 0 }
 };
