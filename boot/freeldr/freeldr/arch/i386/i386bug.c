d437f4c397ae026c27d4c3a02926c21ab002cc3f
Joachim Henze <Joachim.Henze@reactos.org>
2024-08-23 22:05:58 +0200

    [FREELDR] Print the arch + buildnumber on those crash-screens

--- boot/freeldr/freeldr/arch/i386/i386bug.c
+++ boot/freeldr/freeldr/arch/i386/i386bug.c
@@ -1,6 +1,7 @@
 
 #include <freeldr.h>
 
+#include <reactos/buildno.h>
 #include <debug.h>
 
 typedef struct _FRAME
@@ -115,9 +116,11 @@ i386PrintExceptionText(ULONG TrapIndex, PKTRAP_FRAME TrapFrame, PKSPECIAL_REGIST
     i386_ScreenPosX = 0;
     i386_ScreenPosY = 0;
 
-    PrintText("An error occured in " VERSION "\n"
+    PrintText("Error in %s %s\n"
               "Report this error on the ReactOS Bug Tracker: https://jira.reactos.org\n\n"
-              "0x%02lx: %s\n\n", TrapIndex, i386ExceptionDescriptionText[TrapIndex]);
+              "0x%02lx: %s\n\n",
+              KERNEL_VERSION_STR, KERNEL_VERSION_BUILD_STR,
+              TrapIndex, i386ExceptionDescriptionText[TrapIndex]);
 
 #ifdef _M_IX86
     PrintText("EAX: %.8lx        ESP: %.8lx        CR0: %.8lx        DR0: %.8lx\n",
