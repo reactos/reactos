a8aab6dd5e8d2288a9d108d4de2431197b6a94da
Justin Miller <justin.miller@reactos.org>
2024-09-10 23:13:34 -0700

    [BOOT][NTOS:KE] Remove ARMv6 code and enable ntkrnlmp

--- ntoskrnl/ntos.cmake
+++ ntoskrnl/ntos.cmake
@@ -392,6 +392,10 @@ elseif(ARCH STREQUAL "arm")
         ${REACTOS_SOURCE_DIR}/ntoskrnl/mm/ARM3/arm/init.c
         ${REACTOS_SOURCE_DIR}/ntoskrnl/ps/arm/psctx.c
         ${REACTOS_SOURCE_DIR}/ntoskrnl/rtl/arm/rtlexcpt.c)
+    if(BUILD_MP)
+        list(APPEND SOURCE
+            ${REACTOS_SOURCE_DIR}/ntoskrnl/ke/arm/mproc.c)
+    endif()
 endif()
 
 if(NOT _WINKD_)
