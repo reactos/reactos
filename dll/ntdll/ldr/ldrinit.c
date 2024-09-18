1c9af252aa09cff2cdcbf7aa6850f87d16b421fa
Shane Fournier <shanefournier@yandex.com>
2024-09-17 20:46:32 -0400

    [TLS] Corrected an oversight regarding the TLS directory, as well as another involving the freeing of old TLS vectors.

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -1295,8 +1295,8 @@ LdrpInitializeTls()
 
         /* Check if we have a directory */
         if (!TlsDirectory) continue;
-        /* Todo: change usage of TLS directory characteristics so it is not treated as an index anymore */
-        if (TlsDirectory->SizeOfZeroFill & 0x2) continue;
+        /* Tentatively this flag should be named LDRP_TLS_LOADED  */
+        if (LdrEntry->Flags & 0x10) continue;
 
         /* Check if the image has TLS */
         if (!LdrpImageHasTls) LdrpImageHasTls = TRUE;
@@ -1316,6 +1316,7 @@ LdrpInitializeTls()
         /* Lock the DLL and mark it for TLS Usage */
         LdrEntry->LoadCount = -1;
         LdrEntry->TlsIndex = -1;
+        LdrEntry->Flags |= 0x10;
 
         /* Save the cached TLS data */
         TlsData->TlsDirectory = *TlsDirectory;
@@ -1324,7 +1325,6 @@ LdrpInitializeTls()
         /* Update the index */
         *(PLONG)TlsData->TlsDirectory.AddressOfIndex = LdrpNumberOfTlsEntries;
         TlsData->TlsDirectory.Characteristics = LdrpNumberOfTlsEntries++;
-        TlsData->TlsDirectory.SizeOfZeroFill |= 0x2;
     }
 
     /* Done setting up TLS, allocate entries */
@@ -1499,7 +1499,6 @@ LdrpFreeTls(VOID)
                         OldTlsVectorDataEntry);
         }
     }
-    Teb->SystemReserved[0] = NULL;
 }
 
 NTSTATUS
