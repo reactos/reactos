e0d4716d8f2206179fc1057ac82b81ac08fd0ed3
Shane Fournier <shanefournier@yandex.com>
2024-09-20 14:34:39 -0400

    [TLS] corrected an issue where some TLS alloc calls failed, causing issues in some system components. These calls are unnecessary.

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -1273,6 +1273,7 @@ LdrpInitializeTls()
     PIMAGE_TLS_DIRECTORY TlsDirectory;
     PLDRP_TLS_DATA TlsData;
     ULONG Size;
+    BOOL AllocateTls = FALSE;
 
     /* Initialize the TLS List */
     if (!LdrpImageHasTls)
@@ -1300,6 +1301,7 @@ LdrpInitializeTls()
 
         /* Check if the image has TLS */
         if (!LdrpImageHasTls) LdrpImageHasTls = TRUE;
+        AllocateTls = TRUE;
 
         /* Show debug message */
         if (ShowSnaps)
@@ -1328,7 +1330,10 @@ LdrpInitializeTls()
     }
 
     /* Done setting up TLS, allocate entries */
-    return LdrpAllocateTls();
+    if (AllocateTls)
+        return LdrpAllocateTls();
+    else
+        return STATUS_SUCCESS;
 }
 
 NTSTATUS
@@ -1346,7 +1351,7 @@ LdrpAllocateTls(VOID)
     /* Check if we have any entries */
     if (!LdrpNumberOfTlsEntries)
         return STATUS_SUCCESS;
-
+    /* Check if there are new entries to add to the vector */
     if (LdrpNumberOfTlsEntries == (ULONG)Teb->UserReserved[0])
         return STATUS_SUCCESS;
 
