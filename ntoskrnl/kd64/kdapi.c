1db1bceaa728ab13c58e15da866d5a602208987b
Hervé Poussineau <hpoussin@reactos.org>
2024-09-22 21:40:23 +0200

    [NTOS:KD64] Implement KdSystemDebugControl: SysDbgCheckLowMemory

--- ntoskrnl/kd64/kdapi.c
+++ ntoskrnl/kd64/kdapi.c
@@ -2571,8 +2571,7 @@ KdSystemDebugControl(
             break;
 
         case SysDbgCheckLowMemory:
-            UNIMPLEMENTED;
-            Status = STATUS_NOT_IMPLEMENTED;
+            Status = KdpSysCheckLowMemory(0);
             break;
 
         default:
