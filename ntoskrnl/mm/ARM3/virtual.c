030c1fe57682cbcff653949b0d3d18e4dc250598
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-09-23 15:14:56 +0300

    HACK HACK HACK: disable an ASSERT
    We need proper IPIs to flush the TLB

--- ntoskrnl/mm/ARM3/virtual.c
+++ ntoskrnl/mm/ARM3/virtual.c
@@ -2423,7 +2423,7 @@ MiProtectVirtualMemory(IN PEPROCESS Process,
                     MI_WRITE_INVALID_PTE(PointerPte, PteContents);
 #ifdef CONFIG_SMP
                     // FIXME: Should invalidate entry in every CPU TLB
-                    ASSERT(KeNumberProcessors == 1);
+                    //ASSERT(KeNumberProcessors == 1);
 #endif
                     KeInvalidateTlbEntry(MiPteToAddress(PointerPte));
 
