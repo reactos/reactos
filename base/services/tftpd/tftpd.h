08db0e1acccdb56cb4b4176b50aeaf97ac063183
Mahir Gül <mahirgul@gmail.com>
2024-09-27 10:17:23 +0200

    Update tftpd.h
    I removed that i think it is unnecessary
    
    #if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
    #define MYLWORD unsigned long long

--- base/services/tftpd/tftpd.h
+++ base/services/tftpd/tftpd.h
@@ -21,12 +21,7 @@
 #define MYBYTE unsigned char
 #define MYWORD unsigned short
 #define MYDWORD unsigned int
-
-#if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__) ) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
-#define MYLWORD unsigned long long
-#else
 #define MYLWORD unsigned int
-#endif
 
 #ifdef _MSC_VER
    #define strcasecmp _stricmp
