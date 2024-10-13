e2024204563b1a8c6d8c216daff1355b5bae519d
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-10-13 10:31:24 +0200

    [VCRUNTIME] Allow disabling GCC attributes for SAL

--- sdk/include/vcruntime/gcc_sal.h
+++ sdk/include/vcruntime/gcc_sal.h
@@ -16,6 +16,7 @@
 #define __has_attribute(__x) 0
 #endif
 
+#ifndef _GCC_NO_SAL_ATTRIIBUTES
 #if __has_attribute(warn_unused_result)
 # undef _Must_inspect_result_
 /* FIXME: Not really equivalent */
@@ -24,3 +25,4 @@
 /* This one is 1:1 equivalent */
 # define _Check_return_ __attribute__((__warn_unused_result__))
 #endif
+#endif // _GCC_NO_SAL_ATTRIIBUTES
