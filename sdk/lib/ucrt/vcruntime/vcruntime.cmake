5dcd1d1714663fcc7db6fb4dab48ac16e6c0c5d5
Timo Kreuzer <timo.kreuzer@reactos.org>
2024-10-22 09:05:04 +0300

    [UCRT:VCRUNTIME] Implement __security_init_cookie and __security_check_cookie

---
+++ sdk/lib/ucrt/vcruntime/vcruntime.cmake
@@ -0,0 +1,14 @@
+
+list(APPEND UCRT_VCRUNTIME_SOURCES
+    vcruntime/__security_init_cookie.c
+)
+
+if(${ARCH} STREQUAL "i386")
+    list(APPEND UCRT_VCRUNTIME_SOURCES
+        vcruntime/i386/__security_check_cookie.s
+    )
+elseif(${ARCH} STREQUAL "amd64")
+    list(APPEND UCRT_VCRUNTIME_SOURCES
+        vcruntime/amd64/__security_check_cookie.s
+    )
+endif()
