630aa7f215ae4f904341b64ded702e4152e991d1
Stanislav Motylkov <x86corez@gmail.com>
2024-12-30 16:07:32 +0300

    Update dll/win32/shell32/brfolder.cpp

--- dll/win32/shell32/brfolder.cpp
+++ dll/win32/shell32/brfolder.cpp
@@ -408,7 +408,7 @@ BrFolder_Expand(
     ULONG ulFetched;
     while (S_OK == pEnum->Next(1, &pidlTemp, &ulFetched))
     {
-        /* We need to ignore the return value of BrFolder_InsertItem to process next folders */
+        /* Ignore return value of BrFolder_InsertItem to avoid incomplete folder listing */
         BrFolder_InsertItem(info, lpsf, pidlTemp, pidlFull, hParent);
         pidlTemp.Free(); // Finally, free the pidl that the shell gave us...
     }
