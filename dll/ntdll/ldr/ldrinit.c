ee2659369415607c41e3cd9700272a7885b3252c
Shane Fournier <shanefournier@yandex.com>
2025-01-02 16:36:51 -0500

    [NTDLL] Some small cleanup in Ldr, including defining LDRP_TLS_LOADED

--- dll/ntdll/ldr/ldrinit.c
+++ dll/ntdll/ldr/ldrinit.c
@@ -1266,7 +1266,7 @@ LdrShutdownThread(VOID)
 
 NTSTATUS
 NTAPI
-LdrpInitializeTls()
+LdrpInitializeTls(VOID)
 {
     PLIST_ENTRY NextEntry, ListHead;
     PLDR_DATA_TABLE_ENTRY LdrEntry;
@@ -1296,8 +1296,7 @@ LdrpInitializeTls()
 
         /* Check if we have a directory */
         if (!TlsDirectory) continue;
-        /* Tentatively this flag should be named LDRP_TLS_LOADED  */
-        if (LdrEntry->Flags & 0x10) continue;
+        if (LdrEntry->Flags & LDRP_TLS_LOADED) continue;
 
         /* Check if the image has TLS */
         if (!LdrpImageHasTls) LdrpImageHasTls = TRUE;
@@ -1318,7 +1317,7 @@ LdrpInitializeTls()
         /* Lock the DLL and mark it for TLS Usage */
         LdrEntry->LoadCount = -1;
         LdrEntry->TlsIndex = -1;
-        LdrEntry->Flags |= 0x10;
+        LdrEntry->Flags |= LDRP_TLS_LOADED;
 
         /* Save the cached TLS data */
         TlsData->TlsDirectory = *TlsDirectory;
