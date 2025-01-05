2f14a10393afbac00de8bb56519c0241866bdebf
Julen Urizar Compains <julenuri@hotmail.com>
2025-01-05 03:22:03 +0100

    [ROSTESTS] Commit some suggestions

--- modules/rostests/apitests/kernel32/Pipes.c
+++ modules/rostests/apitests/kernel32/Pipes.c
@@ -7,12 +7,12 @@
 
 #include "precomp.h"
 
-#define LP TEXT("\\\\.\\pipe\\rostest_pipe")
+static const char* g_PipeName = "\\\\.\\pipe\\rostest_pipe";
 
 #define MINBUFFERSIZE 1
 #define MAXBUFFERSIZE 255
 
-static DWORD dReadBufferSize;
+static DWORD g_dwReadBufferSize;
 
 DWORD
 WINAPI
@@ -25,20 +25,21 @@ PipeWriter(
     BOOL Success;
     PCSTR pszInMsg = "Test";
 
-    hPipe = CreateFile(LP, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+    hPipe = CreateFile(g_PipeName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
             NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     ok(hPipe != INVALID_HANDLE_VALUE, "CreateFile failed, results might not be accurate\n");
     if (hPipe != INVALID_HANDLE_VALUE)
     {
-        Sleep(0);
+        Sleep(1);
+        cbWritten = 0xdeadbeef;
         Success = WriteFile(hPipe, &pszInMsg, 4, &cbWritten, NULL);
         dwLastError = GetLastError();
 
         ok(Success, "Pipe's WriteFile returned FALSE, instead of expected TRUE\n");
-        if(dwLastError != 0)
+        ok(dwLastError == 0, "dwLastError was 0x%lx\n", dwLastError);
             trace("Last Error = 0x%lX\n",dwLastError);
 
-        ok(cbWritten > 0,"Pipe's Writefile has lpNumberOfBytesWritten <= 0\n");
+        ok(cbWritten > 0,"Invalid cbWritten: 0x%lx\n", cbWritten);
 
         CloseHandle(hPipe);
     }
@@ -58,7 +59,7 @@ PipeReader(
     CHAR szOutMsg[MAXBUFFERSIZE];
 
     hPipe = CreateNamedPipeA( 
-        LP,
+        g_PipeName,
         PIPE_ACCESS_DUPLEX,
         PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE,
         1,
@@ -74,24 +75,25 @@ PipeReader(
         ok(hThread != INVALID_HANDLE_VALUE, "CreateThread failed\n");
         if (hThread != INVALID_HANDLE_VALUE)
         {
-            Sleep(0);
-            Success = ReadFile(hPipe, &szOutMsg, dReadBufferSize, &cbRead, NULL);
+            Sleep(1);
+            cbRead = 0xdeadbeef;
+            Success = ReadFile(hPipe, &szOutMsg, g_dwReadBufferSize, &cbRead, NULL);
             dwLastError = GetLastError();
 
-            if(dReadBufferSize == MINBUFFERSIZE)
+            if(g_dwReadBufferSize == MINBUFFERSIZE)
             {
                 ok(!Success, "Pipe's ReadFile returned TRUE, instead of expected FALSE\n");
             }
             else
             {
                 ok(Success, "Pipe's ReadFile returned FALSE, instead of expected TRUE\n");
-                if(dwLastError != 0)
+                ok(dwLastError == 0, "dwLastError was 0x%lx\n", dwLastError);
                     trace("Last Error = 0x%lX\n",dwLastError);
             }
 
-            ok(cbRead > 0,"Pipe's Readfile has lpNumberOfBytesRead <= 0\n");
+            ok(cbRead > 0,"Invalid cbRead: 0x%lx\n", cbRead);
 
-            if(dReadBufferSize == MINBUFFERSIZE)
+            if(g_dwReadBufferSize == MINBUFFERSIZE)
                 ok(dwLastError == ERROR_MORE_DATA, "Pipe's ReadFile last error is unexpected\n");
 
             WaitForSingleObject(hThread, INFINITE);
@@ -110,7 +112,7 @@ StartTestCORE17376(
 
     trace("adReadBufferSize = %ld - START\n", adReadBufferSize);
 
-    dReadBufferSize = adReadBufferSize;
+    g_dwReadBufferSize = adReadBufferSize;
 
     hThread = CreateThread(NULL,0, PipeReader, NULL, 0, NULL);
     ok(hThread != INVALID_HANDLE_VALUE, "CreateThread failed\n");
