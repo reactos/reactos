ad91512fa17965fbf2f91dba7250b59f53f1e56a
Julen Urizar Compains <julenuri@hotmail.com>
2025-01-07 07:56:59 +0100

    [ROSTESTS] Apply some ChatGPT proposed optimizations, currently working
    [ROSTESTS] Apply minor format suggestions.
    
    From Serge.
    
    Co-authored-by: Serge Gautherie <32623169+SergeGautherie@users.noreply.github.com>
    
    [KERNEL32][ROSTESTS] Apply some Serge suggestions: Remove checkError, using stack allocation for outMsg, adding e-mails to PROGRAMMER
    
    [KERNEL32][ROSTESTS] Serge suggestions: Merge definition and initialization of hPipe in a single line
    
    [KERNEL32] [ROSTESTS] Minor change in modules/rostests/apitests/kernel32/Pipes.c
    
    Co-authored-by: Serge Gautherie <32623169+SergeGautherie@users.noreply.github.com>
    
    [KERNEL32] [ROSTESTS] Apply hbelusca suggestion to Pipes.c
    
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- modules/rostests/apitests/kernel32/Pipes.c
+++ modules/rostests/apitests/kernel32/Pipes.c
@@ -2,7 +2,8 @@
  * PROJECT:         ReactOS api tests
  * LICENSE:         GNU GPLv2 only as published by the Free Software Foundation
  * PURPOSE:         Test for pipe (CORE-17376)
- * PROGRAMMER:      Simone Mario Lombardo
+ * PROGRAMMER:      Simone Mario Lombardo <me@simonelombardo.com>
+ *                  Julen Urizar Compains <julenuri@hotmail.com>
  */
 
 #include "precomp.h"
@@ -11,109 +12,99 @@ static const char* g_PipeName = "\\\\.\\pipe\\rostest_pipe";
 
 #define MINBUFFERSIZE 1
 #define MAXBUFFERSIZE 255
+#define TEST_MESSAGE "Test"
+#define TEST_MESSAGE_SIZE 4
 
 static DWORD g_dwReadBufferSize;
 
 DWORD WINAPI PipeWriter(_In_ PVOID Param)
 {
-    DWORD cbWritten;
-    HANDLE hPipe = (HANDLE) Param;
-    DWORD dwLastError;
-    BOOL Success;
-    PCSTR pszInMsg = "Test";
+    HANDLE hPipe = (HANDLE)Param;
+    DWORD cbWritten = 0;
+    BOOL Success = WriteFile(hPipe, TEST_MESSAGE, TEST_MESSAGE_SIZE, &cbWritten, NULL);
 
-    cbWritten = 0xdeadbeef;
-    Success = WriteFile(hPipe, &pszInMsg, 4, &cbWritten, NULL);
-    dwLastError = GetLastError();
-
-    ok(Success, "Pipe's WriteFile returned FALSE, instead of expected TRUE\n");
-    ok(dwLastError == 0, "dwLastError was 0x%lx\n", dwLastError);
-    trace("Last Error = 0x%lX\n",dwLastError);
-    ok(cbWritten == 4,"Invalid cbWritten: 0x%lx\n", cbWritten);
+    ok(Success, "WriteFile() failed, last error = 0x%lx\n", GetLastError());
+    ok_int(cbWritten, TEST_MESSAGE_SIZE);
 
     return 0;
 }
 
 DWORD WINAPI PipeReader(_In_ PVOID Param)
 {
-    DWORD cbRead;
-    DWORD dwLastError;
-    BOOL Success;
-    CHAR szOutMsg[MAXBUFFERSIZE];
-    HANDLE hPipe = (HANDLE) Param;
+    HANDLE hPipe = (HANDLE)Param;
+    CHAR outMsg[MAXBUFFERSIZE];
 
-    cbRead = 0xdeadbeef;
-    Success = ReadFile(hPipe, &szOutMsg, g_dwReadBufferSize, &cbRead, NULL);
-    dwLastError = GetLastError();
-
-    if(g_dwReadBufferSize == MINBUFFERSIZE)
-    {
-        ok(!Success, "Pipe's ReadFile returned TRUE, instead of expected FALSE\n");
-    }
+    DWORD cbRead = 0;
+    BOOL Success = ReadFile(hPipe, outMsg, g_dwReadBufferSize, &cbRead, NULL);
+    
+    if (g_dwReadBufferSize == MINBUFFERSIZE)
+        ok(!Success, "ReadFile() unexpectedly succeeded\n");
     else
-    {
-        ok(Success, "Pipe's ReadFile returned FALSE, instead of expected TRUE\n");
-        ok(dwLastError == 0, "dwLastError was 0x%lx\n", dwLastError);
-        trace("Last Error = 0x%lX\n",dwLastError);
-    }
+        ok(Success, "ReadFile() failed, last error = 0x%lx\n", GetLastError());
 
-    ok(cbRead > 0,"Invalid cbRead: 0x%lx\n", cbRead);
+    ok(cbRead != 0, "cbRead == 0\n");
 
-    if(g_dwReadBufferSize == MINBUFFERSIZE)
-        ok(dwLastError == ERROR_MORE_DATA, "Pipe's ReadFile last error is unexpected\n");
+    if (g_dwReadBufferSize == MINBUFFERSIZE)
+        ok_hex(GetLastError(), ERROR_MORE_DATA);
 
     return 0;
 }
 
 VOID StartTestCORE17376(_In_ DWORD adReadBufferSize)
 {
-    HANDLE hPipeReader;
-    HANDLE hPipeWriter;
-    HANDLE hThreadReader;
-    HANDLE hThreadWriter;
+    HANDLE hPipeReader, hPipeWriter, hThreadReader, hThreadWriter;
     trace("adReadBufferSize = %ld - START\n", adReadBufferSize);
 
     g_dwReadBufferSize = adReadBufferSize;
 
-    hPipeReader = CreateNamedPipeA( 
+    hPipeReader = CreateNamedPipeA(
         g_PipeName,
         PIPE_ACCESS_DUPLEX,
-        PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE,
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE,
         1,
         MAXBUFFERSIZE,
         MAXBUFFERSIZE,
         0,
         NULL);
     ok(hPipeReader != INVALID_HANDLE_VALUE, "CreateNamedPipeA failed\n");
-    hThreadReader = CreateThread(NULL,0, PipeReader, hPipeReader, 0, NULL);
 
-    if (hPipeReader != INVALID_HANDLE_VALUE)
+    if (hPipeReader == INVALID_HANDLE_VALUE)
+        return;
+
+    hThreadReader = CreateThread(NULL, 0, PipeReader, hPipeReader, 0, NULL);
+    ok(hThreadReader != INVALID_HANDLE_VALUE, "CreateThread failed\n");
+
+    hPipeWriter = CreateFile(
+        g_PipeName,
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    ok(hPipeWriter != INVALID_HANDLE_VALUE, "CreateFile failed\n");
+
+    if (hPipeWriter != INVALID_HANDLE_VALUE)
     {
-        ok(hThreadReader != INVALID_HANDLE_VALUE, "CreateThread failed\n");
+        hThreadWriter = CreateThread(NULL, 0, PipeWriter, hPipeWriter, 0, NULL);
+        ok(hThreadWriter != INVALID_HANDLE_VALUE, "CreateThread failed\n");
 
-        hPipeWriter = CreateFile(g_PipeName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
-        NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    ok(hPipeWriter != INVALID_HANDLE_VALUE, "CreateFile failed, results might not be accurate\n");
-
-        if (hPipeWriter != INVALID_HANDLE_VALUE)
+        if (hThreadWriter != INVALID_HANDLE_VALUE)
         {
-            hThreadWriter = CreateThread(NULL,0, PipeWriter, hPipeWriter, 0, NULL);
-            ok(hThreadWriter != INVALID_HANDLE_VALUE, "CreateThread failed\n");
-            if (hThreadWriter != INVALID_HANDLE_VALUE)
-            {
-                WaitForSingleObject(hThreadWriter, INFINITE);
-                CloseHandle(hThreadWriter);
-                if (hThreadReader != INVALID_HANDLE_VALUE)
-                {
-                    WaitForSingleObject(hThreadReader, INFINITE);
-                    CloseHandle(hThreadReader);
-                }
-            }
-            CloseHandle(hPipeWriter);
+            WaitForSingleObject(hThreadWriter, INFINITE);
+            CloseHandle(hThreadWriter);
         }
-        CloseHandle(hPipeReader);
+        CloseHandle(hPipeWriter);
     }
-        trace("adReadBufferSize = %ld - COMPLETED\n", adReadBufferSize);
+
+    if (hThreadReader != INVALID_HANDLE_VALUE)
+    {
+        WaitForSingleObject(hThreadReader, INFINITE);
+        CloseHandle(hThreadReader);
+    }
+    CloseHandle(hPipeReader);
+
+    trace("adReadBufferSize = %ld - COMPLETED\n", adReadBufferSize);
 }
 
 START_TEST(Pipes)
@@ -121,4 +112,3 @@ START_TEST(Pipes)
     StartTestCORE17376(MINBUFFERSIZE);
     StartTestCORE17376(MAXBUFFERSIZE);
 }
-
