eedf39860e0bbca8104407a01d7389d7b80fed6e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-01-16 19:28:29 +0900

    add apitest

---
+++ modules/rostests/apitests/shell32/SHGetUnreadMailCountW.cpp
@@ -0,0 +1,62 @@
+/*
+ * PROJECT:     ReactOS API tests
+ * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
+ * PURPOSE:     Test for SHGetUnreadMailCountW
+ * COPYRIGHT:   Copyright 2025 Katayama Hirofumi MZ (katayama.hirofumi.mz@gmail.com)
+ */
+
+#include "shelltest.h"
+
+static VOID SetUnreadMailInfo(PDWORD pdwDisposition)
+{
+    HKEY hKey;
+    LSTATUS error = RegCreateKeyExW(
+        HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\UnreadMail\\example.com",
+        0, NULL, 0, KEY_WRITE, NULL, &hKey, pdwDisposition);
+    ok_long(error, ERROR_SUCCESS);
+
+    DWORD dwCount = 1;
+    error = SHSetValueW(hKey, NULL, L"MessageCount", REG_DWORD, &dwCount, sizeof(dwCount));
+    ok_long(error, ERROR_SUCCESS);
+
+    FILETIME FileTime;
+    GetSystemTimeAsFileTime(&FileTime);
+    error = SHSetValueW(hKey, NULL, L"TimeStamp", REG_BINARY, &FileTime, sizeof(FileTime));
+    ok_long(error, ERROR_SUCCESS);
+
+    LPCWSTR pszApp = L"MyMailerApp";
+    DWORD cbValue = (lstrlenW(pszApp) + 1) * sizeof(WCHAR);
+    error = SHSetValueW(hKey, NULL, L"Application", REG_SZ, pszApp, cbValue);
+    ok_long(error, ERROR_SUCCESS);
+
+    RegCloseKey(hKey);
+}
+
+START_TEST(SHGetUnreadMailCountW)
+{
+    HRESULT hr;
+
+    DWORD dwDisposition;
+    SetUnreadMailInfo(&dwDisposition);
+
+    hr = SHGetUnreadMailCountW(NULL, L"example.com", NULL, NULL, NULL, 0);
+    ok_hex(hr, S_OK);
+
+    FILETIME FileTime;
+    hr = SHGetUnreadMailCountW(NULL, L"example.com", NULL, &FileTime, NULL, 0);
+    ok_hex(hr, S_OK);
+
+    DWORD dwCount = 0;
+    hr = SHGetUnreadMailCountW(NULL, L"example.com", &dwCount, NULL, NULL, 0);
+    ok_hex(hr, S_OK);
+    ok_long(dwCount, 1);
+
+    hr = SHGetUnreadMailCountW(NULL, NULL, &dwCount, NULL, NULL, 0);
+    ok_hex(hr, S_OK);
+
+    if (dwDisposition == REG_CREATED_NEW_KEY)
+    {
+        RegDeleteKeyW(HKEY_CURRENT_USER,
+                      L"Software\\Microsoft\\Windows\\CurrentVersion\\UnreadMail\\example.com");
+    }
+}
