52198fa7e6b2259b953970fce378adb542db5f7a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-01-19 10:37:43 +0900

    improve tests

--- modules/rostests/apitests/shell32/SHSetUnreadMailCountW.cpp
+++ modules/rostests/apitests/shell32/SHSetUnreadMailCountW.cpp
@@ -18,7 +18,7 @@ START_TEST(SHSetUnreadMailCountW)
     ok_long(error, ERROR_SUCCESS);
     RegCloseKey(hKey);
 
-    HRESULT hr = SHSetUnreadMailCountW(L"example.com", 0, L"ThisIsMyMailerApp");
+    HRESULT hr = SHSetUnreadMailCountW(L"example.com", 1, L"MyMailerApp");
     ok_hex(hr, S_OK);
 
     error = RegOpenKeyExW(HKEY_CURRENT_USER,
@@ -31,13 +31,24 @@ START_TEST(SHSetUnreadMailCountW)
     DWORD dwValue, cbValue = sizeof(dwValue);
     error = RegQueryValueExW(hKey, L"MessageCount", NULL, NULL, (PBYTE)&dwValue, &cbValue);
     ok_long(error, ERROR_SUCCESS);
+    ok_long(dwValue, 1);
+
+    FILETIME FileTime;
+    cbValue = sizeof(FileTime);
+    error = RegQueryValueExW(hKey, L"TimeStamp", NULL, NULL, (PBYTE)&FileTime, &cbValue);
+    ok_long(error, ERROR_SUCCESS);
+    ok(FileTime.dwHighDateTime != 0, "FileTime.dwHighDateTime was zero\n");
 
     WCHAR szValue[MAX_PATH];
     cbValue = sizeof(szValue);
     error = RegQueryValueExW(hKey, L"Application", NULL, NULL, (PBYTE)szValue, &cbValue);
     ok_long(error, ERROR_SUCCESS);
+    ok_wstr(szValue, L"MyMailerApp");
 
     RegCloseKey(hKey);
+
+    hr = SHSetUnreadMailCountW(L"example.com", 0, L"MyMailerApp");
+    ok_hex(hr, S_OK);
 
     if (dwDisposition == REG_CREATED_NEW_KEY)
     {
