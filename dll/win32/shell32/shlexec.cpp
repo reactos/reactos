5353bb3e84e849230938a7aea9c6aa679cfd1ed8
Stanislav Motylkov <x86corez@gmail.com>
2025-01-20 12:56:58 +0300

    Update dll/win32/shell32/shlexec.cpp

--- dll/win32/shell32/shlexec.cpp
+++ dll/win32/shell32/shlexec.cpp
@@ -802,14 +802,14 @@ static UINT SHELL_FindExecutable(LPCWSTR lpPath, LPCWSTR lpFile, LPCWSTR lpVerb,
         xlpFile[0] = '\0';
     }
 
-  /* Clear any trailing periods */
-  for (SIZE_T i = wcslen(xlpFile);i > 0; i--)
-  {
-      if (i > 0 && xlpFile[i - 1] == '.')
-          xlpFile[i - 1] = 0;
-      else
-          break;
-   }
+    /* Clear any trailing periods */
+    for (SIZE_T i = wcslen(xlpFile); i > 0; i--)
+    {
+        if (i > 0 && xlpFile[i - 1] == '.')
+            xlpFile[i - 1] = 0;
+        else
+            break;
+    }
 
     attribs = GetFileAttributesW(lpFile);
     if (attribs != INVALID_FILE_ATTRIBUTES && (attribs & FILE_ATTRIBUTE_DIRECTORY))
