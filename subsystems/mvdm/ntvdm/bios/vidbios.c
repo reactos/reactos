f281ff7232c395ad3632973207bc416c56a448b1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-01-26 09:15:17 +0900

    Update subsystems/mvdm/ntvdm/bios/vidbios.c
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- subsystems/mvdm/ntvdm/bios/vidbios.c
+++ subsystems/mvdm/ntvdm/bios/vidbios.c
@@ -2348,7 +2348,7 @@ static VOID VidBiosSetCursorShape(WORD CursorStartEnd)
      * =========
      * Contrary to what is mentioned in lots of literature out there, e.g. in:
      * https://web.archive.org/web/20180507104824/http://webpages.charter.net/danrollins/techhelp/0072.HTM
-     * http://www.bioscentral.com/misc/bda.htm (DEAD_LINK)
+     * https://web.archive.org/web/20240119203029/http://www.bioscentral.com/misc/bda.htm
      * and in other various places, bit 0 of Bda->VGAOptions is 0 when
      * cursor emulation is ENABLED, and is 1 when it is DISABLED.
      *
