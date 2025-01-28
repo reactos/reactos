82da1390eca91109dbc720ab8cfa958ad33122a4
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2025-01-28 17:19:07 +0100

    Revert "[KMTESTS] Temporarily disable the debugger disable/reenable tests (#7424)"
    This reverts commit 40ea0811982ca7faaf4d243128e793a986d29649.

--- modules/rostests/kmtests/ntos_kd/NtSystemDebugControl.c
+++ modules/rostests/kmtests/ntos_kd/NtSystemDebugControl.c
@@ -180,11 +180,6 @@ START_TEST(NtSystemDebugControl)
      * the next tests are going to change its state */
     WasDebuggerEnabled = SharedUserData->KdDebuggerEnabled;
 
-//
-// FIXME: Re-enable ONCE our KDBG and KDCOM dlls support disabling and re-enabling.
-//
-    DBG_UNREFERENCED_LOCAL_VARIABLE(WasDebuggerEnabled);
-#if 0
     /* Try to disable or enable the debugger, depending on its original state */
     if (WasDebuggerEnabled)
         Command = SysDbgDisableKernelDebugger; // 22
@@ -224,8 +219,6 @@ START_TEST(NtSystemDebugControl)
         ok_eq_hex_test(Command, Status, STATUS_SUCCESS);
     else
         ok_eq_hex_test(Command, Status, STATUS_DEBUGGER_INACTIVE);
-#endif
-// END FIXME
 
 
     /* Supported commands */
