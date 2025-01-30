063c818c729bd2eeb560c691fdbf1297c94a4d24
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-01-30 12:42:59 +0200

    [MSVCRT_WINETEST] Sync to wine-10.0

--- modules/rostests/winetests/msvcrt/dir.c
+++ modules/rostests/winetests/msvcrt/dir.c
@@ -138,36 +138,6 @@ static void test_makepath(void)
     }
 }
 
-static const WCHAR expected0[] = {'\0','X','X','X','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected1[] = {'\0','X','X','X','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected2[] = {'\0',':','X','X','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected3[] = {'\0',':','d','X','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected4[] = {'\0',':','d','\\','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected5[] = {'\0',':','d','\\','f','X','X','X','X','X','X','X','X'};
-static const WCHAR expected6[] = {'\0',':','d','\\','f','i','X','X','X','X','X','X','X'};
-static const WCHAR expected7[] = {'\0',':','d','\\','f','i','l','X','X','X','X','X','X'};
-static const WCHAR expected8[] = {'\0',':','d','\\','f','i','l','e','X','X','X','X','X'};
-static const WCHAR expected9[] = {'\0',':','d','\\','f','i','l','e','.','X','X','X','X'};
-static const WCHAR expected10[] = {'\0',':','d','\\','f','i','l','e','.','e','X','X','X'};
-static const WCHAR expected11[] = {'\0',':','d','\\','f','i','l','e','.','e','x','X','X'};
-
-static const WCHAR expected12[] = {'\0',':','X','X','X','X','X','X','X','X'};
-static const WCHAR expected13[] = {'\0',':','d','X','X','X','X','X','X','X'};
-static const WCHAR expected14[] = {'\0',':','d','i','X','X','X','X','X','X'};
-static const WCHAR expected15[] = {'\0',':','d','i','r','X','X','X','X','X'};
-static const WCHAR expected16[] = {'\0',':','d','i','r','\\','X','X','X','X'};
-
-static const WCHAR expected17[] = {'\0','o','o'};
-static const WCHAR expected18[] = {'\0','o','o','\0','X'};
-static const WCHAR expected19[] = {'\0','o','o','\0'};
-static const WCHAR expected20[] = {'\0','o','o','\0','X','X','X','X','X'};
-static const WCHAR expected21[] = {'\0','o','o','\\','f','i','l','X','X'};
-static const WCHAR expected22[] = {'\0','o','o','\0','X','X','X','X','X','X','X','X','X'};
-static const WCHAR expected23[] = {'\0','o','o','\\','f','i','l','X','X','X','X','X','X'};
-static const WCHAR expected24[] = {'\0','o','o','\\','f','i','l','e','.','e','x','X','X'};
-static const WCHAR expected25[] = {'\0','o','o','\0','X','X','X','X'};
-static const WCHAR expected26[] = {'\0','o','o','.','e','x','X','X'};
-
 typedef struct
 {
     const char* buffer;
@@ -184,35 +154,35 @@ typedef struct
 static const makepath_s_case makepath_s_cases[] =
 {
     /* Behavior with directory parameter containing backslash. */
-    {NULL, 1, "c:", "d\\", "file", "ext", "\0XXXXXXXXXXXX", expected0, 13},
-    {NULL, 2, "c:", "d\\", "file", "ext", "\0XXXXXXXXXXXX", expected1, 13},
-    {NULL, 3, "c:", "d\\", "file", "ext", "\0:XXXXXXXXXXX", expected2, 13},
-    {NULL, 4, "c:", "d\\", "file", "ext", "\0:dXXXXXXXXXX", expected3, 13},
-    {NULL, 5, "c:", "d\\", "file", "ext", "\0:d\\XXXXXXXXX", expected4, 13},
-    {NULL, 6, "c:", "d\\", "file", "ext", "\0:d\\fXXXXXXXX", expected5, 13},
-    {NULL, 7, "c:", "d\\", "file", "ext", "\0:d\\fiXXXXXXX", expected6, 13},
-    {NULL, 8, "c:", "d\\", "file", "ext", "\0:d\\filXXXXXX", expected7, 13},
-    {NULL, 9, "c:", "d\\", "file", "ext", "\0:d\\fileXXXXX", expected8, 13},
-    {NULL, 10, "c:", "d\\", "file", "ext", "\0:d\\file.XXXX", expected9, 13},
-    {NULL, 11, "c:", "d\\", "file", "ext", "\0:d\\file.eXXX", expected10, 13},
-    {NULL, 12, "c:", "d\\", "file", "ext", "\0:d\\file.exXX", expected11, 13},
+    {NULL, 1, "c:", "d\\", "file", "ext", "\0XXXXXXXXXXXX", L"\0XXXXXXXXXXXX", 13},
+    {NULL, 2, "c:", "d\\", "file", "ext", "\0XXXXXXXXXXXX", L"\0XXXXXXXXXXXX", 13},
+    {NULL, 3, "c:", "d\\", "file", "ext", "\0:XXXXXXXXXXX", L"\0:XXXXXXXXXXX", 13},
+    {NULL, 4, "c:", "d\\", "file", "ext", "\0:dXXXXXXXXXX", L"\0:dXXXXXXXXXX", 13},
+    {NULL, 5, "c:", "d\\", "file", "ext", "\0:d\\XXXXXXXXX", L"\0:d\\XXXXXXXXX", 13},
+    {NULL, 6, "c:", "d\\", "file", "ext", "\0:d\\fXXXXXXXX", L"\0:d\\fXXXXXXXX", 13},
+    {NULL, 7, "c:", "d\\", "file", "ext", "\0:d\\fiXXXXXXX", L"\0:d\\fiXXXXXXX", 13},
+    {NULL, 8, "c:", "d\\", "file", "ext", "\0:d\\filXXXXXX", L"\0:d\\filXXXXXX", 13},
+    {NULL, 9, "c:", "d\\", "file", "ext", "\0:d\\fileXXXXX", L"\0:d\\fileXXXXX", 13},
+    {NULL, 10, "c:", "d\\", "file", "ext", "\0:d\\file.XXXX", L"\0:d\\file.XXXX", 13},
+    {NULL, 11, "c:", "d\\", "file", "ext", "\0:d\\file.eXXX", L"\0:d\\file.eXXX", 13},
+    {NULL, 12, "c:", "d\\", "file", "ext", "\0:d\\file.exXX", L"\0:d\\file.exXX", 13},
     /* Behavior with directory parameter lacking backslash. */
-    {NULL, 3, "c:", "dir", "f", "ext", "\0:XXXXXXXX", expected12, 10},
-    {NULL, 4, "c:", "dir", "f", "ext", "\0:dXXXXXXX", expected13, 10},
-    {NULL, 5, "c:", "dir", "f", "ext", "\0:diXXXXXX", expected14, 10},
-    {NULL, 6, "c:", "dir", "f", "ext", "\0:dirXXXXX", expected15, 10},
-    {NULL, 7, "c:", "dir", "f", "ext", "\0:dir\\XXXX", expected16, 10},
+    {NULL, 3, "c:", "dir", "f", "ext", "\0:XXXXXXXX", L"\0:XXXXXXXX", 10},
+    {NULL, 4, "c:", "dir", "f", "ext", "\0:dXXXXXXX", L"\0:dXXXXXXX", 10},
+    {NULL, 5, "c:", "dir", "f", "ext", "\0:diXXXXXX", L"\0:diXXXXXX", 10},
+    {NULL, 6, "c:", "dir", "f", "ext", "\0:dirXXXXX", L"\0:dirXXXXX", 10},
+    {NULL, 7, "c:", "dir", "f", "ext", "\0:dir\\XXXX", L"\0:dir\\XXXX", 10},
     /* Behavior with overlapped buffer. */
-    {"foo", 2, USE_BUFF, NULL, NULL, NULL, "\0oo", expected17, 3},
-    {"foo", 4, NULL, USE_BUFF, NULL, NULL, "\0oo\0X", expected18, 5},
-    {"foo", 3, NULL, NULL, USE_BUFF, NULL, "\0oo\0", expected19, 4},
-    {"foo", 4, NULL, USE_BUFF, "file", NULL, "\0oo\0XXXXX", expected20, 9},
-    {"foo", 8, NULL, USE_BUFF, "file", NULL, "\0oo\\filXX", expected21, 9},
-    {"foo", 4, NULL, USE_BUFF, "file", "ext", "\0oo\0XXXXXXXXX", expected22, 13},
-    {"foo", 8, NULL, USE_BUFF, "file", "ext", "\0oo\\filXXXXXX", expected23, 13},
-    {"foo", 12, NULL, USE_BUFF, "file", "ext", "\0oo\\file.exXX", expected24, 13},
-    {"foo", 4, NULL, NULL, USE_BUFF, "ext", "\0oo\0XXXX", expected25, 8},
-    {"foo", 7, NULL, NULL, USE_BUFF, "ext", "\0oo.exXX", expected26, 8},
+    {"foo", 2, USE_BUFF, NULL, NULL, NULL, "\0oo", L"\0oo", 3},
+    {"foo", 4, NULL, USE_BUFF, NULL, NULL, "\0oo\0X", L"\0oo\0X", 5},
+    {"foo", 3, NULL, NULL, USE_BUFF, NULL, "\0oo\0", L"\0oo", 4},
+    {"foo", 4, NULL, USE_BUFF, "file", NULL, "\0oo\0XXXXX", L"\0oo\0XXXXX", 9},
+    {"foo", 8, NULL, USE_BUFF, "file", NULL, "\0oo\\filXX", L"\0oo\\filXX", 9},
+    {"foo", 4, NULL, USE_BUFF, "file", "ext", "\0oo\0XXXXXXXXX", L"\0oo\0XXXXXXXXX", 13},
+    {"foo", 8, NULL, USE_BUFF, "file", "ext", "\0oo\\filXXXXXX", L"\0oo\\filXXXXXX", 13},
+    {"foo", 12, NULL, USE_BUFF, "file", "ext", "\0oo\\file.exXX", L"\0oo\\file.exXX", 13},
+    {"foo", 4, NULL, NULL, USE_BUFF, "ext", "\0oo\0XXXX", L"\0oo\0XXXX", 8},
+    {"foo", 7, NULL, NULL, USE_BUFF, "ext", "\0oo.exXX", L"\0oo.exXX", 8},
 };
 
 static void test_makepath_s(void)
@@ -438,10 +408,6 @@ static void test_searchenv(void)
         "\\search_env_test\\dir3longer\\3.dat"
     };
 
-    const WCHAR env_w[] = {'T','E','S','T','_','P','A','T','H',0};
-    const WCHAR dat1_w[] = {'1','.','d','a','t',0};
-    const WCHAR dat3_w[] = {'3','.','d','a','t',0};
-
     char env1[4*MAX_PATH], env2[4*MAX_PATH], tmppath[MAX_PATH], path[2*MAX_PATH];
     char result[MAX_PATH], exp[2*MAX_PATH];
     WCHAR result_w[MAX_PATH];
@@ -510,13 +476,13 @@ static void test_searchenv(void)
     }
 
     memset(result_w, 'x', sizeof(result_w));
-    _wsearchenv(dat1_w, env_w, result_w);
+    _wsearchenv(L"1.dat", L"TEST_PATH", result_w);
     WideCharToMultiByte(CP_ACP, 0, result_w, -1, result, MAX_PATH, NULL, NULL);
     ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
 
     if (p_wsearchenv_s) {
         memset(result_w, 'x', sizeof(result_w));
-        i = p_wsearchenv_s(dat1_w, env_w, result_w, MAX_PATH);
+        i = p_wsearchenv_s(L"1.dat", L"TEST_PATH", result_w, MAX_PATH);
         ok(!i, "wsearchenv_s returned %d\n", i);
         ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
     }
@@ -535,13 +501,13 @@ static void test_searchenv(void)
     }
 
     memset(result_w, 'x', sizeof(result_w));
-    _wsearchenv(dat3_w, env_w, result_w);
+    _wsearchenv(L"3.dat", L"TEST_PATH", result_w);
     WideCharToMultiByte(CP_ACP, 0, result_w, -1, result, MAX_PATH, NULL, NULL);
     ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
 
     if (p_wsearchenv_s) {
         memset(result_w, 'x', sizeof(result_w));
-        i = p_wsearchenv_s(dat3_w, env_w, result_w, MAX_PATH);
+        i = p_wsearchenv_s(L"3.dat", L"TEST_PATH", result_w, MAX_PATH);
         ok(!i, "wsearchenv_s returned %d\n", i);
         ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
     }
@@ -561,13 +527,13 @@ static void test_searchenv(void)
     }
 
     memset(result_w, 'x', sizeof(result_w));
-    _wsearchenv(dat1_w, env_w, result_w);
+    _wsearchenv(L"1.dat", L"TEST_PATH", result_w);
     WideCharToMultiByte(CP_ACP, 0, result_w, -1, result, MAX_PATH, NULL, NULL);
     ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
 
     if (p_wsearchenv_s) {
         memset(result_w, 'x', sizeof(result_w));
-        i = p_wsearchenv_s(dat1_w, env_w, result_w, MAX_PATH);
+        i = p_wsearchenv_s(L"1.dat", L"TEST_PATH", result_w, MAX_PATH);
         ok(!i, "wsearchenv_s returned %d\n", i);
         ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
     }
@@ -586,16 +552,30 @@ static void test_searchenv(void)
     }
 
     memset(result_w, 'x', sizeof(result_w));
-    _wsearchenv(dat3_w, env_w, result_w);
+    _wsearchenv(L"3.dat", L"TEST_PATH", result_w);
     WideCharToMultiByte(CP_ACP, 0, result_w, -1, result, MAX_PATH, NULL, NULL);
     ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
 
     if (p_wsearchenv_s) {
         memset(result_w, 'x', sizeof(result_w));
-        i = p_wsearchenv_s(dat3_w, env_w, result_w, MAX_PATH);
+        i = p_wsearchenv_s(L"3.dat", L"TEST_PATH", result_w, MAX_PATH);
         ok(!i, "wsearchenv_s returned %d\n", i);
         ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
     }
+
+    strcpy(env1, "TEST_PATH=");
+    strcat(env1, tmppath);
+    strcat(env1, "\"\\search_env_test\\\"d\"i\"r\"1");
+    putenv(env1);
+    strcpy(exp, tmppath);
+    strcat(exp, files[0]);
+    _searchenv("1.dat", "TEST_PATH", result);
+    ok(!strcmp(result, exp), "got %s, expected '%s'\n", result, exp);
+
+    strcat(env1, ";");
+    putenv(env1);
+    _searchenv("1.dat", "TEST_PATH", result);
+    ok(!result[0], "got %s, expected ''\n", result);
 
     putenv("TEST_PATH=");
 
