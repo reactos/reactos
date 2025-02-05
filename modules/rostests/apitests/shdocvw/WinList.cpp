7a17a6d034aa7da5323b5ba014e4a74c25738d2f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-02-05 18:50:33 +0900

    [SHDOCVW_APITEST] Add WinList testcase

---
+++ modules/rostests/apitests/shdocvw/WinList.cpp
@@ -0,0 +1,79 @@
+/*
+ * PROJECT:     ReactOS api tests
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
+ * PURPOSE:     Tests for shdocvw!WinList_*
+ * COPYRIGHT:   Copyright 2025 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
+ */
+
+#include <apitest.h>
+#include <shlwapi.h>
+#include <shlobj.h>
+#include <shlobj_undoc.h>
+#include <shlguid_undoc.h>
+#include <shlwapi_undoc.h>
+#include <versionhelpers.h>
+
+typedef BOOL (WINAPI *FN_WinList_Init)(VOID);
+typedef VOID (WINAPI *FN_WinList_Terminate)(VOID);
+typedef IShellWindows* (WINAPI *FN_WinList_GetShellWindows)(BOOL);
+
+static FN_WinList_Init g_pWinList_Init = NULL;
+static FN_WinList_Terminate g_pWinList_Terminate = NULL;
+static FN_WinList_GetShellWindows g_pWinList_GetShellWindows = NULL;
+
+static VOID
+TEST_WinList(VOID)
+{
+    BOOL bInited = g_pWinList_Init && g_pWinList_Init();
+    ok_int(bInited, FALSE); // WinList_Init should fail because this process is not explorer.exe
+
+    IShellWindows *pShellWindows = g_pWinList_GetShellWindows(FALSE);
+    ok(pShellWindows != NULL, "pShellWindows was null\n");
+
+    if (pShellWindows)
+    {
+        LONG nCount = -1;
+        HRESULT hr = pShellWindows->get_Count(&nCount);
+        ok_long(hr, S_OK);
+        ok(nCount >= 0, "nCount was %ld\n", nCount);
+
+        pShellWindows->Release();
+    }
+    else
+    {
+        ok_int(TRUE, FALSE);
+        ok_int(TRUE, FALSE);
+    }
+
+    if (bInited && g_pWinList_Terminate)
+        g_pWinList_Terminate();
+}
+
+START_TEST(WinList)
+{
+    HRESULT hrCoInit = CoInitialize(NULL);
+
+    HINSTANCE hSHDOCVW = LoadLibraryW(L"shdocvw.dll");
+    if (!hSHDOCVW)
+    {
+        skip("shdocvw.dll not loaded\n");
+    }
+    else
+    {
+        g_pWinList_Init = (FN_WinList_Init)GetProcAddress(hSHDOCVW, MAKEINTRESOURCEA(110));
+        g_pWinList_Terminate = (FN_WinList_Terminate)GetProcAddress(hSHDOCVW, MAKEINTRESOURCEA(111));
+        g_pWinList_GetShellWindows = (FN_WinList_GetShellWindows)GetProcAddress(hSHDOCVW, MAKEINTRESOURCEA(179));
+        if (!g_pWinList_Init || !g_pWinList_Terminate || !g_pWinList_GetShellWindows)
+        {
+            skip("Some WinList_* functions not found: %p %p %p\n",
+                 g_pWinList_Init, g_pWinList_Terminate, g_pWinList_GetShellWindows);
+        }
+        else
+        {
+            TEST_WinList();
+        }
+    }
+
+    if (SUCCEEDED(hrCoInit))
+        CoUninitialize();
+}
