e57e41eaf7d8be11777463f26b6ffe02b9a85a2a
FutureLizard205 <miguelbarraalmeida@gmail.com>
2025-02-22 23:40:41 +0000

    Use "#ifndef __REACTOS__" instead of just deleting code

--- dll/win32/comctl32/listview.c
+++ dll/win32/comctl32/listview.c
@@ -3725,20 +3725,51 @@ static void LISTVIEW_SetGroupSelection(LISTVIEW_INFO *infoPtr, INT nItem)
     item.state = LVIS_SELECTED; 
     item.stateMask = LVIS_SELECTED;
 
-	if (infoPtr->nSelectionMark == -1)
-	{
-	    infoPtr->nSelectionMark = nItem;
-	    ranges_additem(selection, nItem);
-	}
-	else
-	{
-	    RANGE sel;
-	    
-	    sel.lower = min(infoPtr->nSelectionMark, nItem);
-	    sel.upper = max(infoPtr->nSelectionMark, nItem) + 1;
-	    ranges_add(selection, sel);
-	}
-	
+#ifndef __REACTOS__
+    if ((infoPtr->uView == LV_VIEW_LIST) || (infoPtr->uView == LV_VIEW_DETAILS))
+    {
+#endif
+    if (infoPtr->nSelectionMark == -1)
+    {
+        infoPtr->nSelectionMark = nItem;
+        ranges_additem(selection, nItem);
+    }
+    else
+    {
+        RANGE sel;
+        
+        sel.lower = min(infoPtr->nSelectionMark, nItem);
+        sel.upper = max(infoPtr->nSelectionMark, nItem) + 1;
+        ranges_add(selection, sel);
+    }
+#ifndef __REACTOS__
+    }
+    else
+    {
+    RECT rcItem, rcSel, rcSelMark;
+    POINT ptItem;
+    
+    rcItem.left = LVIR_BOUNDS;
+    if (!LISTVIEW_GetItemRect(infoPtr, nItem, &rcItem)) {
+        ranges_destroy (selection);
+        return;
+    }
+    rcSelMark.left = LVIR_BOUNDS;
+    if (!LISTVIEW_GetItemRect(infoPtr, infoPtr->nSelectionMark, &rcSelMark)) {
+        ranges_destroy (selection);
+        return;
+    }
+    UnionRect(&rcSel, &rcItem, &rcSelMark);
+    iterator_frameditems(&i, infoPtr, &rcSel);
+    while(iterator_next(&i))
+    {
+        LISTVIEW_GetItemPosition(infoPtr, i.nItem, &ptItem);
+        if (PtInRect(&rcSel, ptItem)) ranges_additem(selection, i.nItem);
+    }
+    iterator_destroy(&i);
+    }
+#endif
+
     /* disable per item notifications on LVS_OWNERDATA style
        FIXME: single LVN_ODSTATECHANGED should be used */
     old_mask = infoPtr->notify_mask & NOTIFY_MASK_ITEM_CHANGE;
