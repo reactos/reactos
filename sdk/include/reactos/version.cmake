c7cb60617c5ea78c6f1216d2ab65ea867e1cb212
jpb <jakub_baranski@outlook.com>
2025-02-28 21:12:31 +0000

    [VERSION] Rework build tag

--- sdk/include/reactos/version.cmake
+++ sdk/include/reactos/version.cmake
@@ -1,7 +1,7 @@
 option(NO_REACTOS_BUILDNO "If true, disables the generation of buildno.h and version.h for each configure" OFF)
 
 if (NOT NO_REACTOS_BUILDNO)
-    string(TIMESTAMP KERNEL_VERSION_BUILD %Y%m%d UTC)
+    string(TIMESTAMP KERNEL_VERSION_BUILD %Y%m%d-%H%M UTC)
 else()
     set(KERNEL_VERSION_BUILD "custom")
 endif()
@@ -9,7 +9,7 @@ endif()
 set(KERNEL_VERSION_MAJOR "6")
 set(KERNEL_VERSION_MINOR "0")
 set(KERNEL_VERSION_PATCH_LEVEL "6000")
-set(KERNEL_VERSION_REVISION "10")
+set(KERNEL_VERSION_REVISION "51")
 set(COPYRIGHT_YEAR "2025")
 
 # KERNEL_VERSION_BUILD_TYPE is "dev" for Git builds
@@ -28,35 +28,8 @@ if(NOT KERNEL_VERSION_BUILD_TYPE STREQUAL "")
 endif()
 
 # Get Git revision through "git describe"
-set(COMMIT_HASH "unknown-hash")
-set(REVISION "unknown-revision")
-
-if((EXISTS "${REACTOS_SOURCE_DIR}/.git") AND (NOT NO_REACTOS_BUILDNO))
-    find_package(Git)
-    if(GIT_FOUND)
-        execute_process(
-            COMMAND "${GIT_EXECUTABLE}" rev-parse HEAD
-            WORKING_DIRECTORY ${REACTOS_SOURCE_DIR}
-            OUTPUT_VARIABLE GIT_COMMIT_HASH
-            RESULT_VARIABLE GIT_CALL_RESULT
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        if(GIT_CALL_RESULT EQUAL 0)
-            set(COMMIT_HASH "${GIT_COMMIT_HASH}")
-        endif()
-
-        execute_process(
-            COMMAND "${GIT_EXECUTABLE}" describe --abbrev=7 --long --always
-            WORKING_DIRECTORY ${REACTOS_SOURCE_DIR}
-            OUTPUT_VARIABLE GIT_DESCRIBE_REVISION
-            RESULT_VARIABLE GIT_CALL_RESULT
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-        if(GIT_CALL_RESULT EQUAL 0)
-            set(REVISION "${GIT_DESCRIBE_REVISION}")
-        endif()
-    endif()
-endif()
+set(COMMIT_HASH "")
+set(REVISION "")
 
 configure_file(sdk/include/reactos/version.h.cmake ${REACTOS_BINARY_DIR}/sdk/include/reactos/version.h)
 configure_file(sdk/include/reactos/buildno.h.cmake ${REACTOS_BINARY_DIR}/sdk/include/reactos/buildno.h)
