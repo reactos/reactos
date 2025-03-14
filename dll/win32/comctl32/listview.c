8c3cea3743473242cdd18efbada90381f17addfd
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-15 07:52:35 +0900

    fix comctl32 listview

--- dll/win32/comctl32/listview.c
+++ dll/win32/comctl32/listview.c
@@ -12003,23 +12003,42 @@ LISTVIEW_WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 #ifdef __REACTOS__
   case WM_SETTINGCHANGE: /* Same as WM_WININICHANGE */
-    if (wParam == SPI_SETICONTITLELOGFONT)
     {
-      BOOL bWasSameFont = (infoPtr->hDefaultFont == infoPtr->hFont);
+      BOOL bSetFont = FALSE;
       LOGFONTW logFont;
-      SystemParametersInfoW(SPI_GETICONTITLELOGFONT, 0, &logFont, 0);
 
-      if (infoPtr->hDefaultFont)
-        DeleteObject(infoPtr->hDefaultFont);
-      infoPtr->hDefaultFont = CreateFontIndirectW(&logFont);
+      if (wParam == SPI_SETICONMETRICS || (!wParam && !lParam))
+      {
+        ICONMETRICS metrics;
+        SystemParametersInfoW(SPI_GETICONMETRICS, 0, &metrics, 0);
+        LISTVIEW_SetIconSpacing(infoPtr, metrics.iHorzSpacing, metrics.iVertSpacing);
+        logFont = metrics.lfFont;
+        bSetFont = TRUE;
+      }
 
-      if (bWasSameFont || !infoPtr->hFont)
-        infoPtr->hFont = infoPtr->hDefaultFont;
+      if (wParam == SPI_SETICONTITLELOGFONT || (!wParam && lParam))
+      {
+        SystemParametersInfoW(SPI_GETICONTITLELOGFONT, 0, &logFont, 0);
+        bSetFont = TRUE;
+      }
 
-      LISTVIEW_SaveTextMetrics(infoPtr);
-      LISTVIEW_UpdateItemSize(infoPtr);
-      LISTVIEW_UpdateScroll(infoPtr);
-      LISTVIEW_InvalidateRect(infoPtr, NULL);
+      if (bSetFont)
+      {
+        BOOL bDefaultOrNullFont = (infoPtr->hDefaultFont == infoPtr->hFont || !infoPtr->hFont);
+
+        if (infoPtr->hDefaultFont)
+          DeleteObject(infoPtr->hDefaultFont);
+        infoPtr->hDefaultFont = CreateFontIndirectW(&logFont);
+
+        if (bDefaultOrNullFont)
+          infoPtr->hFont = infoPtr->hDefaultFont;
+
+        LISTVIEW_SaveTextMetrics(infoPtr);
+        LISTVIEW_UpdateItemSize(infoPtr);
+        LISTVIEW_UpdateScroll(infoPtr);
+        LISTVIEW_InvalidateRect(infoPtr, NULL);
+      }
+
       return 0;
     }
 #else
