96aa1eee179b7c0225adb3c642a30d8d3e6904a1
Whindmar Saksit <whindsaks@proton.me>
2025-03-15 16:31:22 +0100

    [LNKTOOL][SHELL32] Add utility to create/dump .lnk files (#7745)
    Unlike mkshelllink, this uses the native COM interface to create the link.

--- sdk/include/psdk/shlwapi.h
+++ sdk/include/psdk/shlwapi.h
@@ -25,6 +25,15 @@
 #include <objbase.h>
 #include <shtypes.h>
 
+#ifndef _SHLWAPI_
+#define LWSTDAPI_(type)  EXTERN_C DECLSPEC_IMPORT type WINAPI
+#define LWSTDAPIV_(type) EXTERN_C DECLSPEC_IMPORT type STDAPIVCALLTYPE
+#else
+#define LWSTDAPI_(type)  type WINAPI
+#define LWSTDAPIV_(type) type STDAPIVCALLTYPE
+#endif
+#define LWSTDAPI         LWSTDAPI_(HRESULT)
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* defined(__cplusplus) */
@@ -1896,6 +1905,14 @@ SHCreateStreamOnFileEx(
   _Outptr_ struct IStream**);
 
 HRESULT WINAPI SHCreateStreamWrapper(LPBYTE,DWORD,DWORD,struct IStream**);
+
+#ifndef _SHLWAPI_
+LWSTDAPI IStream_Reset(_In_ struct IStream*);
+#if !defined(IStream_Read) && defined(__cplusplus)
+LWSTDAPI IStream_Read(_In_ struct IStream*, _Out_ void*, _In_ ULONG);
+LWSTDAPI IStream_Write(_In_ struct IStream*, _In_ const void*, _In_ ULONG);
+#endif
+#endif
 
 #endif /* NO_SHLWAPI_STREAM */
 
