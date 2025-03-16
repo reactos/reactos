3092045e1e7e8a43f44a140afc078fad4d46c0d5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-16 20:19:35 +0900

    add 'ReactOS Help Center' banner

--- base/applications/notepad/help/res/notepad.js
+++ base/applications/notepad/help/res/notepad.js
@@ -3,6 +3,7 @@ function resolve_images(common_res_url) {
   window.onload = (function() {
     // Common resources
     var common_roslogo_url = common_res_url + "roslogo-32x32.png";
+    document.getElementById("top_banner").src = common_res_url + "rosbanner.png";
     document.getElementById("top_script").href = common_res_url + "common.js";
     document.getElementById("top_stylesheet").href = common_res_url + "common.css";
     document.getElementById("top_favicon").href = common_roslogo_url;
