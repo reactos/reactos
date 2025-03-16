ec78cc363f7da3f78667c5afcfe24926b168f216
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-16 17:08:30 +0900

    add notepad.js

---
+++ base/applications/notepad/help/res/notepad.js
@@ -0,0 +1,15 @@
+function resolve_images(common_res_url) {
+  document.addEventListener("DOMContentLoaded", function() {
+    // Common resources
+    var common_roslogo_url = common_res_url + "roslogo-32x32.png";
+    document.getElementById("top_script").src = common_res_url + "common.js";
+    document.getElementById("top_stylesheet").href = common_res_url + "common.css";
+    document.getElementById("top_favicon").href = common_roslogo_url;
+
+    // TODO: Embed and/or reference image(s).
+    // To embed image data, please use this: https://dopiaza.org/goodies/data-uri-generator/
+    var app_icon_url = "./res/notepad-32x32.png";
+    document.getElementById("app_icon").src = app_icon_url;
+    document.getElementById("roslogo").src = common_roslogo_url;
+  });
+}
