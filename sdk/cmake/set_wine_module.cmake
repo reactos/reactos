f0152c26053658bf4aad94a8a921a2cf713ab4ec
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-20 07:04:28 +0900

    fixing 6

--- sdk/cmake/set_wine_module.cmake
+++ sdk/cmake/set_wine_module.cmake
@@ -9,7 +9,7 @@ endfunction()
 
 # FIXME: Replace this call with set_wine_module
 function(set_wine_module_FIXME_1 TARGET)
-    target_compile_definitions(${TARGET} PRIVATE -D__WINESRC__)
+    target_compile_definitions(${TARGET} PRIVATE __WINESRC__)
 endfunction()
 
 # FIXME: Replace this call with set_wine_module
@@ -17,5 +17,5 @@ function(set_wine_module_FIXME_2 TARGET)
     include_directories(BEFORE
         ${REACTOS_SOURCE_DIR}/sdk/include/reactos/wine
         ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
-    target_compile_definitions(${TARGET} PRIVATE -D__WINESRC__)
+    target_compile_definitions(${TARGET} PRIVATE __WINESRC__)
 endfunction()
