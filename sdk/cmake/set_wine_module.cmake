1f45b1605caec97b737f2b18b68a77b4011d9ae7
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-19 13:49:08 +0900

    [DLL:WIN32][CMAKE] Introduce set_wine_module and delete some -D__WINESRC__

---
+++ sdk/cmake/set_wine_module.cmake
@@ -0,0 +1,7 @@
+
+function(set_wine_module TARGET)
+    target_include_directories(${TARGET} BEFORE PRIVATE
+        sdk/include/reactos/wine
+        ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
+    target_compile_definitions(${TARGET} PRIVATE __WINESRC__) # FIXME: CORE-5743: Delete this line
+endfunction()
