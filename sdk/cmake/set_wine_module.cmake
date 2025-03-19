881a82d765c93a8e3f0eed6693e1f93a9d12194c
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-19 14:34:24 +0900

    fix fusion

--- sdk/cmake/set_wine_module.cmake
+++ sdk/cmake/set_wine_module.cmake
@@ -1,6 +1,6 @@
 
 function(set_wine_module TARGET)
-    target_include_directories(${TARGET} BEFORE PRIVATE
+    target_include_directories(${TARGET} SYSTEM BEFORE PRIVATE
         sdk/include/reactos/wine
         ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
     target_compile_definitions(${TARGET} PRIVATE __WINESRC__) # FIXME: CORE-5743: Delete this line
