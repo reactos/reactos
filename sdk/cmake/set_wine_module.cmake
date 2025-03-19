4597c6f45f654b3683e8a12198dbd2121c04195e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-19 14:49:19 +0900

    REACTOS_SOURCE_DIR

--- sdk/cmake/set_wine_module.cmake
+++ sdk/cmake/set_wine_module.cmake
@@ -1,7 +1,7 @@
 
 function(set_wine_module TARGET)
     target_include_directories(${TARGET} SYSTEM BEFORE PRIVATE
-        sdk/include/reactos/wine
+        ${REACTOS_SOURCE_DIR}/sdk/include/reactos/wine
         ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
     target_compile_definitions(${TARGET} PRIVATE __WINESRC__) # FIXME: CORE-5743: Delete this line
 endfunction()
