1c26767f992ee58d0cbdccb9062cbfd08ee6e9ed
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-21 18:46:22 +0900

    Update dll/win32/browseui/shellbrowser.cpp

--- dll/win32/browseui/shellbrowser.cpp
+++ dll/win32/browseui/shellbrowser.cpp
@@ -3946,7 +3946,6 @@ LRESULT CShellBrowser::OnAddToFavorites(WORD wNotifyCode, WORD wID, HWND hWndCtl
     WCHAR szPath[MAX_PATH];
     SHGetSpecialFolderPathW(m_hWnd, szPath, CSIDL_FAVORITES, TRUE);
     PathAppendW(szPath, fileInfo.szDisplayName);
-    PathRemoveExtensionW(szPath);
     PathAddExtensionW(szPath, L".lnk");
 
     CreateShortcut(szPath, fCurrentDirectoryPIDL, NULL);
