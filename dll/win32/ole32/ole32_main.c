31bc4339977f671df196f85b6cf61f5bbf2de372
winesync <ros-dev@reactos.org>
2025-03-24 20:32:03 +0100

    [WINESYNC] ole32: Move IsValidInterface() to another file.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id eb316eda1dceab1d4c1a1335353da79e0c346c26 by Nikolay Sivov <nsivov@codeweavers.com>

--- dll/win32/ole32/ole32_main.c
+++ dll/win32/ole32/ole32_main.c
@@ -175,3 +175,22 @@ HRESULT WINAPI CoGetCallState(int unknown, PULONG unknown2)
     FIXME("%d, %p\n", unknown, unknown2);
     return E_NOTIMPL;
 }
+
+/******************************************************************************
+ *		IsValidInterface	[OLE32.@]
+ *
+ * Determines whether a pointer is a valid interface.
+ *
+ * PARAMS
+ *  punk [I] Interface to be tested.
+ *
+ * RETURNS
+ *  TRUE, if the passed pointer is a valid interface, or FALSE otherwise.
+ */
+BOOL WINAPI IsValidInterface(LPUNKNOWN punk)
+{
+	return !(IsBadReadPtr(punk,4) ||
+                 IsBadReadPtr(punk->lpVtbl,4) ||
+                 IsBadReadPtr(punk->lpVtbl->QueryInterface,9) ||
+                 IsBadCodePtr((FARPROC)punk->lpVtbl->QueryInterface));
+}
