52812654a31e14e2b8db401d0bcfe053b53157fd
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-30 08:45:48 +0900

    fix arm longjmp

--- sdk/lib/crt/setjmp/arm/setjmp.s
+++ sdk/lib/crt/setjmp/arm/setjmp.s
@@ -55,8 +55,11 @@
     vld1.64 {d6}, [r0]!
     vld1.64 {d7}, [r0]!
 
-    /* Return 1 */
-    mov r0, #1
+    /* Check if r1 is zero */
+    cmp r1, #0
+    /* if r1 is zero, then set r0 to 1, else r0 = r1 */
+    moveq r0, #1
+    movne r0, r1
     bx lr
     LEAF_END longjmp
 
