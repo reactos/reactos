cac49aed1110a8dffba87374d2e5dbdefa1f54e1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-30 05:55:55 +0900

    Update sdk/lib/crt/setjmp/amd64/setjmp.s
    Co-authored-by: Serge Gautherie <32623169+SergeGautherie@users.noreply.github.com>

--- sdk/lib/crt/setjmp/amd64/setjmp.s
+++ sdk/lib/crt/setjmp/amd64/setjmp.s
@@ -124,7 +124,7 @@ FUNC _setjmpex
     movdqu [rcx + JUMP_BUFFER_Xmm15], xmm15     /* Store xmm15 */
     mov rsp, rbp                                /* Restore original rsp */
     pop rbp                                     /* Restore original rbp */
-    xor eax, eax                                /* Return 0 */
+    xor eax, eax                                /* Return 0 on first (_setjmpex) return */
 LABEL2:
     ret
 ENDFUNC
