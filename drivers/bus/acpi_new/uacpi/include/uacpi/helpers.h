cad427d0ce9af7dd50ae225b27a2cfd4364dddb5
Justin Miller <justin.miller@reactos.org>
2025-03-30 19:17:49 +0200

    [ACPI_NEW] Minimal to build
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>
    Co-authored-by: Dmitry Borisov <di.sean@protonmail.com>

--- drivers/bus/acpi_new/uacpi/include/uacpi/helpers.h
+++ drivers/bus/acpi_new/uacpi/include/uacpi/helpers.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#if __REACTOS__
+#define UACPI_STATIC_ASSERT _STATIC_ASSERT
+#else
 #ifdef __cplusplus
 #define UACPI_STATIC_ASSERT static_assert
 #else
 #define UACPI_STATIC_ASSERT _Static_assert
 #endif
+#endif
 
+#ifdef __REACTOS__
+#define UACPI_BUILD_BUG_ON_WITH_MSG(expr, msg) UACPI_STATIC_ASSERT(!(expr))
+#else
 #define UACPI_BUILD_BUG_ON_WITH_MSG(expr, msg) UACPI_STATIC_ASSERT(!(expr), msg)
+#endif
 
 #define UACPI_BUILD_BUG_ON(expr) \
     UACPI_BUILD_BUG_ON_WITH_MSG(expr, "BUILD BUG: " #expr " evaluated to true")
