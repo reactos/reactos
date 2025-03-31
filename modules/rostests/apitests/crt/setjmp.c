e0fbde844916477209dbee405f5e41b2fe28b218
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-03-31 21:09:11 +0900

    improve

--- modules/rostests/apitests/crt/setjmp.c
+++ modules/rostests/apitests/crt/setjmp.c
@@ -11,49 +11,52 @@
 
 static jmp_buf g_jmp_buf;
 
+static INT s_check_points[18] = { 0 };
+
+#define CHECK_POINT(number) s_check_points[number] = __LINE__
+
 static void TEST_setjmp_simple(void)
 {
     memset(&g_jmp_buf, 0xCC, sizeof(g_jmp_buf));
 
     if (setjmp(g_jmp_buf) == 0)
     {
-        ok(TRUE, "Yes, come here\n");
+        CHECK_POINT(0);
     }
     else
     {
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(1);
     }
 }
 
 static void TEST_setjmp_return_check(void)
 {
     volatile int x = 1001, value;
-
     memset(&g_jmp_buf, 0xCC, sizeof(g_jmp_buf));
     value = setjmp(g_jmp_buf);
 
     if (value == 0)
     {
-        ok(TRUE, "Yes, come here\n");
+        CHECK_POINT(2);
         longjmp(g_jmp_buf, 999);
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(3);
     }
     else if (value == 999)
     {
-        ok(TRUE, "Yes, come here\n");
+        CHECK_POINT(4);
         ok_int(x, 1001);
     }
     else
     {
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(5);
     }
 }
 
 static void TEST_longjmp(int value)
 {
-    ok(TRUE, "Yes, come here\n");
+    CHECK_POINT(6);
     longjmp(g_jmp_buf, value);
-    ok(FALSE, "No, never come here\n");
+    CHECK_POINT(7);
 }
 
 static void TEST_setjmp_longjmp_integration(void)
@@ -65,19 +68,17 @@ static void TEST_setjmp_longjmp_integration(void)
 
     if (value == 0)
     {
-        ok(TRUE, "Yes, come here\n");
-
+        CHECK_POINT(8);
         TEST_longjmp(0xBEEFCAFE);
-
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(9);
     }
     else if (value == 0xBEEFCAFE)
     {
-        ok(TRUE, "Yes, come here\n");
+        CHECK_POINT(10);
     }
     else
     {
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(11);
     }
 }
 
@@ -93,40 +94,63 @@ static void TEST_setjmp_zero_longjmp_check(void)
     {
         if (went_zero)
         {
-            ok(FALSE, "No, never come here\n");
-            skip("Further tests skipped\n");
+            CHECK_POINT(12);
             return;
         }
         went_zero = TRUE;
 
-        ok(TRUE, "Yes, come here\n");
+        CHECK_POINT(13);
 
         TEST_longjmp(0); /* giving zero should go to one */
 
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(14);
     }
     else if (value == 1)
     {
         if (went_zero)
         {
-            ok(TRUE, "Yes, come here\n");
+            CHECK_POINT(15);
         }
         else
         {
-            ok(FALSE, "No, never come here\n");
+            CHECK_POINT(16);
         }
     }
     else
     {
-        ok(FALSE, "No, never come here\n");
+        CHECK_POINT(17);
     }
 }
 
 START_TEST(setjmp)
 {
+    ZeroMemory(&s_check_points, sizeof(s_check_points));
+
+    /* FIXME: These tests are insufficiant */
     TEST_setjmp_simple();
     TEST_setjmp_return_check();
     TEST_setjmp_longjmp_integration();
     TEST_setjmp_zero_longjmp_check();
-    /* FIXME: These tests are insufficiant */
+
+#define DO_COME(number)    ok(s_check_points[number],  "Line %d: Didn't reach\n",    s_check_points[number])
+#define NEVER_COME(number) ok(!s_check_points[number], "Line %d: Wrongly reached\n", s_check_points[number])
+
+    DO_COME(0);
+    NEVER_COME(1);
+    DO_COME(2);
+    NEVER_COME(3);
+    DO_COME(4);
+    NEVER_COME(5);
+    DO_COME(6);
+    NEVER_COME(7);
+    DO_COME(8);
+    NEVER_COME(9);
+    DO_COME(10);
+    NEVER_COME(11);
+    NEVER_COME(12);
+    DO_COME(13);
+    NEVER_COME(14);
+    DO_COME(15);
+    NEVER_COME(16);
+    NEVER_COME(17);
 }
