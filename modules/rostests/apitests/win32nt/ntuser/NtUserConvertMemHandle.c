25de1d212a72b7dabbe5065c9e53457f49432304
Max Korostil <mrmks04@yandex.ru>
2025-04-09 22:11:14 +0300

    Add successful test.

--- modules/rostests/apitests/win32nt/ntuser/NtUserConvertMemHandle.c
+++ modules/rostests/apitests/win32nt/ntuser/NtUserConvertMemHandle.c
@@ -11,7 +11,11 @@
 START_TEST(NtUserConvertMemHandle)
 {
     HANDLE hMem;
+    CHAR data[] = {1, 2, 3, 4};
  
     hMem = NtUserConvertMemHandle((PVOID)(UINT_PTR)0xDEADBEEF, 0xFFFF);
     ok_hdl(hMem, NULL);
+
+    hMem = NtUserConvertMemHandle(data, sizeof(data));
+    ok(hMem != NULL, "hMem is NULL\n");
 }
