911ff791c6b1b57665596c977971f6362f1688d9
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-04-13 12:28:12 +0900

    Update dll/win32/comctl32/pager.c
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- dll/win32/comctl32/pager.c
+++ dll/win32/comctl32/pager.c
@@ -430,7 +430,7 @@ PAGER_RecalcSize(PAGER_INFO *infoPtr)
     if (!infoPtr->m_bProcessingReCalcSize)
     {
         infoPtr->m_bProcessingReCalcSize = TRUE;
-        /* NOTE: Posting a recalc message to ourselves, not actually a edit control message */
+        /* NOTE: Posting a recalc message to ourselves, not actually an edit control message */
         PostMessageW(infoPtr->hwndSelf, EM_FMTLINES, 0, 0);
     }
     return DefWindowProcW(infoPtr->hwndSelf, PGM_RECALCSIZE, 0, 0);
