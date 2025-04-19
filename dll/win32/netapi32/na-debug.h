15046b226b8a856f2aa1d3f54b5cf00c5d426116
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-04-19 19:01:22 +0900

    [NETAPI32] Make NETAPI32 non-Wine module

---
+++ dll/win32/netapi32/na-debug.h
@@ -0,0 +1,136 @@
+/*
+ * PROJECT:     ReactOS NETAPI32
+ * LICENSE:     LGPL-2.1-or-later (https://spdx.org/licenses/LGPL-2.1-or-later)
+ * PURPOSE:     Debugging NETAPI32
+ * COPYRIGHT:   Copyright 2025 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
+ */
+
+#pragma once
+
+#include <winnls.h>
+#include <stdlib.h>
+#include <objbase.h>
+
+typedef enum tagDEBUGCHANNEL
+{
+    DbgCh_netapi32 = 0,
+    DbgCh_netbios,
+} DEBUGCHANNEL;
+
+#if DBG
+    #ifndef __RELFILE__
+        #define __RELFILE__ __FILE__
+    #endif
+
+    #define ERR_LEVEL   0x1
+    #define TRACE_LEVEL 0x8
+
+    #define WINE_DEFAULT_DEBUG_CHANNEL(x) static int DbgDefaultChannel = DbgCh_##x;
+    #define DBG_IS_CHANNEL_ENABLED(ch) IntIsDebugChannelEnabled(ch)
+
+    #define DBG_PRINT(ch, level, tag, fmt, ...) (void)( \
+        (((level) == ERR_LEVEL) || DBG_IS_CHANNEL_ENABLED(ch)) ? \
+        (DbgPrint("(%s:%d) %s" fmt, __RELFILE__, __LINE__, (tag), ##__VA_ARGS__), FALSE) : TRUE \
+    )
+
+    #define ERR(fmt, ...)   DBG_PRINT(DbgDefaultChannel, ERR_LEVEL,   "err: ",   fmt, ##__VA_ARGS__)
+    #define WARN(fmt, ...)  DBG_PRINT(DbgDefaultChannel, ERR_LEVEL,   "warn: ",  fmt, ##__VA_ARGS__)
+    #define FIXME(fmt, ...) DBG_PRINT(DbgDefaultChannel, ERR_LEVEL,   "fixme: ", fmt, ##__VA_ARGS__)
+    #define TRACE(fmt, ...) DBG_PRINT(DbgDefaultChannel, TRACE_LEVEL, "",        fmt, ##__VA_ARGS__)
+
+    #define UNIMPLEMENTED FIXME("%s is UNIMPLEMENTED\n", __FUNCTION__)
+#else
+    #define WINE_DEFAULT_DEBUG_CHANNEL(x)
+    #define DBG_IS_CHANNEL_ENABLED(ch,level)
+    #define DBG_PRINT(ch,level)
+    #define ERR(fmt, ...)
+    #define WARN(fmt, ...)
+    #define FIXME(fmt, ...)
+    #define TRACE(fmt, ...)
+    #define UNIMPLEMENTED
+#endif
+
+#if DBG
+static inline BOOL
+IntIsDebugChannelEnabled(DEBUGCHANNEL channel)
+{
+    CHAR szValue[MAX_PATH], *pch0, *pch;
+
+    if (!GetEnvironmentVariableA("DEBUGCHANNEL", szValue, _countof(szValue)))
+        return FALSE;
+
+    for (pch0 = szValue;; pch0 = pch + 1)
+    {
+        pch = strchr(pch0, ',');
+        if (pch)
+            *pch = ANSI_NULL;
+        if (channel == DbgCh_netapi32 && _stricmp(pch0, "netapi32") == 0)
+            return TRUE;
+        if (channel == DbgCh_netbios && _stricmp(pch0, "netbios") == 0)
+            return TRUE;
+        if (!pch)
+            return FALSE;
+    }
+}
+#endif /* DBG */
+
+#define NUM_BUFS 4
+
+static inline LPCSTR
+debugstr_a(PCSTR pszA)
+{
+    static CHAR s_bufs[NUM_BUFS][MAX_PATH];
+    static SIZE_T s_index = 0;
+    CHAR buf[MAX_PATH], *ptr;
+
+    if (!pszA)
+        return "(null)";
+
+    buf[0] = '"';
+    lstrcpynA(&buf[1], pszA, _countof(buf) - 2);
+    lstrcatA(buf, "\"");
+
+    ptr = lstrcpynA(s_bufs[s_index], buf, _countof(s_bufs[s_index]));
+    s_index = (s_index + 1) % _countof(s_bufs);
+    return ptr;
+}
+
+static inline LPCSTR
+debugstr_w(PCWSTR pszW)
+{
+    static CHAR s_bufs[NUM_BUFS][MAX_PATH];
+    static SIZE_T s_index = 0;
+    CHAR buf[MAX_PATH], *ptr;
+
+    if (!pszW)
+        return "(null)";
+
+    buf[0] = '"';
+    WideCharToMultiByte(CP_ACP, 0, pszW, -1, &buf[1], _countof(buf) - 2, NULL, NULL);
+    buf[_countof(buf) - 2] = ANSI_NULL;
+    lstrcatA(buf, "\"");
+
+    ptr = lstrcpynA(s_bufs[s_index], buf, _countof(s_bufs[s_index]));
+    s_index = (s_index + 1) % _countof(s_bufs);
+    return ptr;
+}
+
+static inline LPCSTR
+debugstr_guid(const GUID *guid)
+{
+    static CHAR s_bufs[NUM_BUFS][50];
+    WCHAR szW[50];
+    static SIZE_T s_index = 0;
+    CHAR buf[MAX_PATH], *ptr;
+
+    if (!guid)
+        return "(null)";
+
+    StringFromGUID2(guid, szW, _countof(szW));
+    WideCharToMultiByte(CP_ACP, 0, szW, -1, buf, _countof(s_bufs[0]), NULL, NULL);
+    s_bufs[s_index][_countof(s_bufs[0]) - 1] = ANSI_NULL;
+
+    ptr = s_bufs[s_index];
+    s_index = (s_index + 1) % _countof(s_bufs);
+    return ptr;
+}
--- dll/win32/netapi32/netapi32.h
+++ dll/win32/netapi32/netapi32.h
@@ -1,7 +1,9 @@
 #ifndef __WINE_NETAPI32_H__
 #define __WINE_NETAPI32_H__
 
+#ifndef __REACTOS__
 #include <wine/config.h>
+#endif
 
 #include <limits.h>
 #include <stdarg.h>
@@ -19,8 +21,13 @@
 #include <nb30.h>
 #include <iphlpapi.h>
 
+#ifdef __REACTOS__
+#include "na-debug.h"
+#define strcmpiW lstrcmpiW
+#else
 #include <wine/debug.h>
 #include <wine/unicode.h>
+#endif
 
 #define NTOS_MODE_USER
 #include <ndk/rtlfuncs.h>
