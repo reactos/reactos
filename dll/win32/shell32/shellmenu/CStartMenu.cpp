c5c19cc4cec74cfde0fdac0efe06cb63015d01e2
Mohammad Amin Mollazadeh <madamin@pm.me>
2025-04-26 21:17:06 +0330

    [SHELL32] chore: remove fixme for CORE-19477

--- dll/win32/shell32/shellmenu/CStartMenu.cpp
+++ dll/win32/shell32/shellmenu/CStartMenu.cpp
@@ -371,9 +371,6 @@ private:
 
         TRACE("csidl: 0x%X\n", csidl);
 
-        if (csidl == CSIDL_CONTROLS || csidl == CSIDL_NETWORK || csidl == CSIDL_PRINTERS)
-            FIXME("This CSIDL %d wrongly opens My Computer. CORE-19477\n", csidl);
-
         CComHeapPtr<ITEMIDLIST> pidl;
         SHGetSpecialFolderLocation(NULL, csidl, &pidl);
 
