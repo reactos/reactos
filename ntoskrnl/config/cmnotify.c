f4f2d90cdf00f71f8716e9328d127038e86be6c2
Mohammad Amin Mollazadeh <madamin@pm.me>
2025-04-28 17:43:34 +0330

    [NTOS:CM] chore: refactor to subroutines

--- ntoskrnl/config/cmnotify.c
+++ ntoskrnl/config/cmnotify.c
@@ -12,12 +12,41 @@
 #define NDEBUG
 #include "debug.h"
 
-/* FUNCTIONS *****************************************************************/
+/* INTERNAL FUNCTIONS ********************************************************/
 
 VOID
 NTAPI
-CmpReportNotifyToPostBlocks(_In_ PCM_NOTIFY_BLOCK NotifyBlock,
-                            _In_ ULONG Filter)
+CmpFlushPostBlock(_In_ PCM_POST_BLOCK PostBlock,
+                  _In_ ULONG Filter)
+{
+    /* Don't flush if it doesn't match our filter
+     * Using REG_LEGAL_CHANGE_FILTER to flush all PostBlocks regardless to their filter
+     */
+    if ((Filter != REG_LEGAL_CHANGE_FILTER) && (PostBlock->Filter & Filter) != Filter)
+        return;
+        
+    /* Signal the event */
+    KeSetEvent(PostBlock->Event, 1, FALSE);
+
+    /* FIXME: Handle WorkQueueItem */
+
+    /* FIXME: Handle ApcRoutine */
+
+    /* Cleanup resources */
+    ObDereferenceObject(PostBlock->Event);
+    ZwClose(PostBlock->EventHandle);
+
+    /* Remove post block entry */
+    RemoveEntryList(&(PostBlock->NotifyList));
+
+    /* Free the memory */
+    ExFreePoolWithTag(PostBlock, TAG_CM);
+}
+
+VOID
+NTAPI
+CmpFlushAllPostBlocks(_In_ PCM_NOTIFY_BLOCK NotifyBlock,
+                      _In_ ULONG Filter)
 {
     PLIST_ENTRY ListHead, NextEntry;
     PCM_POST_BLOCK PostBlock;
@@ -30,32 +59,90 @@ CmpReportNotifyToPostBlocks(_In_ PCM_NOTIFY_BLOCK NotifyBlock,
     NextEntry = ListHead->Flink;
     while (NextEntry != ListHead)
     {
-        /* Get the post block */
+        /* Get PostBlock for current entry */
         PostBlock = CONTAINING_RECORD(NextEntry, CM_POST_BLOCK, NotifyList);
-
-        /* Check the filter */
-        if ((PostBlock->Filter & Filter) != Filter)
-        {
-            /* The listener doesn't want this type of notification, skip it */
-            NextEntry = NextEntry->Flink;
-            continue;
-        }
-
-        /* Signal the event */
-        KeSetEvent(PostBlock->Event, 1, FALSE);
-
-        /* FIXME: Support for ApcRoutine */
-
-        /* Release the Post Block */
+        /* Navigate to the next, since the previous entry might get freed */
         NextEntry = NextEntry->Flink;
-        /* Free the event object */
-        ObDereferenceObject(PostBlock->Event);
-        ZwClose(PostBlock->EventHandle);
-        /* Remove post block entry */
-        RemoveEntryList(&(PostBlock->NotifyList));
-        /* Free the memory */
-        ExFreePoolWithTag(PostBlock, TAG_CM);
+
+        /* Flush it */
+        CmpFlushPostBlock(PostBlock, Filter);
     }
+}
+
+/* FUNCTIONS *****************************************************************/
+
+NTSTATUS
+NTAPI
+CmpInsertNewPostBlock(_In_      PCM_NOTIFY_BLOCK NotifyBlock,
+                      _In_      ULONG Filter,
+                      _In_opt_  HANDLE EventHandle,
+                      _In_opt_  PKEVENT EventObject,
+                      _Out_     PCM_POST_BLOCK *PostBlockOut)
+{
+    NTSTATUS Status;
+    HANDLE LocalEventHandle;
+    PKEVENT LocalEventObject;
+    PCM_POST_BLOCK PostBlock;
+
+    /* EventHandle should be provided when an EventObject is provided */
+    if (EventHandle == NULL && EventObject != NULL)
+        return STATUS_INVALID_PARAMETER;
+
+    if (EventHandle == NULL && EventObject == NULL)
+    {
+        /* Allocate an event object if it's not provided by the caller */
+        Status = CmpCreateEvent(NotificationEvent, &LocalEventHandle, &LocalEventObject);
+        if (!NT_SUCCESS(Status))
+            return Status;
+    }
+    else if (EventHandle != NULL && EventObject == NULL)
+    {
+        /* Open the event handle if the event object is not provided by the caller */
+        LocalEventHandle = EventHandle;
+        
+        Status = ObReferenceObjectByHandle(EventHandle,
+                                           EVENT_MODIFY_STATE,
+                                           ExEventObjectType,
+                                           KernelMode,
+                                           (PVOID*)&LocalEventObject,
+                                           NULL);
+        if (!NT_SUCCESS(Status))
+            return Status;
+    }
+    else
+    {
+        /* The only reminaing state is, both EventHandle and EventObjects are non-null */
+        LocalEventHandle = EventHandle;
+        LocalEventObject = EventObject;
+    }
+
+    /* Allocate memory */
+    PostBlock = ExAllocatePoolWithTag(NonPagedPool, sizeof(CM_POST_BLOCK), TAG_CM);
+    if (!PostBlock)
+    {
+        /* Free the event resources if we own them */
+        if (LocalEventObject != EventObject)
+            ObDereferenceObject(LocalEventObject);
+        if (LocalEventHandle != EventHandle)
+            ZwClose(LocalEventHandle);
+
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+    RtlZeroMemory(PostBlock, sizeof(CM_POST_BLOCK));
+
+    /* Initialize list heads */
+    InitializeListHead(&(PostBlock->NotifyList));
+
+    /* Fill fields */
+    PostBlock->Filter = Filter;
+    PostBlock->EventHandle = LocalEventHandle;
+    PostBlock->Event = LocalEventObject;
+
+    /* Insert to NotifyBlock */
+    InsertHeadList(&(PostBlock->NotifyList), &(NotifyBlock->PostList));
+
+    *PostBlockOut = PostBlock;
+    return STATUS_SUCCESS;
 }
 
 VOID
@@ -89,7 +176,7 @@ CmpReportNotify(IN PCM_KEY_CONTROL_BLOCK Kcb,
         if ((NotifyBlock->Filter & Filter) != Filter) goto SkipEntry;
 
         /* Report the notification to PostBlocks linked to this NotifyBlock */
-        CmpReportNotifyToPostBlocks(NotifyBlock, Filter);
+        CmpFlushAllPostBlocks(NotifyBlock, Filter);
 
 SkipEntry:
         /* Navigate to next entry */
@@ -102,42 +189,21 @@ NTAPI
 CmpFlushNotify(IN PCM_KEY_BODY KeyBody,
                IN BOOLEAN LockHeld)
 {
-    PLIST_ENTRY ListHead, NextEntry;
-    PCM_POST_BLOCK PostBlock;
-
     /* Lock the KCB so no one would try to make changes
      * to NotifyBlock while we are releasing its resources
      */
     if (!LockHeld)
         CmpAcquireKcbLockExclusive(KeyBody->KeyControlBlock);
 
-    /* Enumerate PostBlocks */
-    ListHead = &(KeyBody->NotifyBlock->PostList);
-    NextEntry = ListHead->Flink;
-    while (NextEntry != ListHead)
-    {
-        /* Get the post block */
-        PostBlock = CONTAINING_RECORD(NextEntry, CM_POST_BLOCK, NotifyList);
+    /* Flush PostBlocks */
+    CmpFlushAllPostBlocks(KeyBody->NotifyBlock, REG_LEGAL_CHANGE_FILTER);
 
-        /* Signal the event */
-        KeSetEvent(PostBlock->Event, 1, FALSE);
-
-        /* Free the event object */
-        ObDereferenceObject(PostBlock->Event);
-        ZwClose(PostBlock->EventHandle);
-        /* Remove post block entry */
-        RemoveEntryList(&(PostBlock->NotifyList));
-        /* Free the memory */
-        ExFreePoolWithTag(PostBlock, TAG_CM);
-
-        NextEntry = ListHead->Flink;
-    }
-
-    /* Finally, free the NotifyBlock */
+    /* Free the NotifyBlock */
     RemoveEntryList(&(KeyBody->NotifyBlock->HiveList));
     ExFreePoolWithTag(KeyBody->NotifyBlock, TAG_CM);
     KeyBody->NotifyBlock = NULL;
 
+    /* Unlock the Kcb if we locked it previously */
     if (!LockHeld)
         CmpReleaseKcbLock(KeyBody->KeyControlBlock);
 }
