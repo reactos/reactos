bfa63932ce644b0e1c6ae0e7f1cd12f0467190ba
Eric Kohl <eric.kohl@reactos.org>
2025-04-28 21:14:20 +0200

    [MOUCLASS] Activate and deactivate the device interface for mouse devices

--- drivers/input/mouclass/mouclass.c
+++ drivers/input/mouclass/mouclass.c
@@ -840,6 +840,7 @@ ClassPnp(
 			}
 			else
 				DeviceExtension->FileHandle = NULL;
+			IoSetDeviceInterfaceState(&DeviceExtension->InterfaceName, TRUE);
 			Irp->IoStatus.Status = Status;
 			IoCompleteRequest(Irp, IO_NO_INCREMENT);
 			return Status;
@@ -854,6 +855,7 @@ ClassPnp(
 			break;
 
         case IRP_MN_REMOVE_DEVICE:
+			IoSetDeviceInterfaceState(&DeviceExtension->InterfaceName, FALSE);
             if (DeviceExtension->FileHandle)
 			{
 				ZwClose(DeviceExtension->FileHandle);
