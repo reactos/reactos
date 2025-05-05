45b928f8bc01f484c8a5168ed4ec1ba6794efc53
Dmitry Borisov <di.sean@protonmail.com>
2025-05-06 02:42:11 +0600

    [ACPI] Remove section assignment from IOCTL handlers (#7829)
    Fix a bug introduced in 5070e8960a.
    
    CORE-17256
    
    - These handlers must be memory resident. Otherwise,
      this would cause a page fault that needs to be resolved.
      Low-level storage drivers could evaluate ACPI control
      methods while the device's IRP queue has been paused.
    
    - Minor code enhancements.

--- drivers/bus/acpi/include/acpisys.h
+++ drivers/bus/acpi/include/acpisys.h
@@ -73,13 +73,6 @@ typedef struct _FDO_DEVICE_DATA
 
 } FDO_DEVICE_DATA, *PFDO_DEVICE_DATA;
 
-typedef struct _EVAL_WORKITEM_DATA
-{
-    PPDO_DEVICE_DATA DeviceData;
-    PIRP Irp;
-    WORK_QUEUE_ITEM WorkQueueItem;
-} EVAL_WORKITEM_DATA, *PEVAL_WORKITEM_DATA;
-
 #define FDO_FROM_PDO(pdoData) \
           ((PFDO_DEVICE_DATA) (pdoData)->ParentFdo->DeviceExtension)
 
@@ -100,10 +93,6 @@ NTSTATUS
 ACPIEnumerateDevices(
   PFDO_DEVICE_DATA DeviceExtension);
 
-CODE_SEG("PAGE")
-WORKER_THREAD_ROUTINE Bus_PDO_EvalMethodWorker;
-
-CODE_SEG("PAGE")
 NTSTATUS
 NTAPI
 Bus_PDO_EvalMethod(
