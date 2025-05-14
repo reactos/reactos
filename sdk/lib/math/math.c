82f42ffa24365dc071360eae121f5f803d6b54bb
Vakkas Emre Yıldız <72315833+vakkas1234@users.noreply.github.com>
2025-05-14 18:44:27 +0300

    [sdk/lib/math] Add new static math library
    This commit introduces a new static library under sdk/lib/math.
    
    The library currently provides basic arithmetic functions:
    - Add
    Header file is exposed publicly for use in other modules.

---
+++ sdk/lib/math/math.c
@@ -0,0 +1,6 @@
+
+#include "math.h"
+int Add(int a,int b)
+{
+    return a+b;
+}
---
+++ sdk/lib/math/math.h
@@ -0,0 +1,7 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+
+int Add(int a, int b);
+int Sub(int a, int b);
+
+#endif // _MATH_H_
