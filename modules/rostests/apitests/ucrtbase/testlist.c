43546cb5c2a109a60c195a7e01ac15ffef6c44c0
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-05-16 15:46:26 +0300

    [UCRTBASE_APITEST] Add ucrtbase_apitest
    First test is ceil.

---
+++ modules/rostests/apitests/ucrtbase/testlist.c
@@ -0,0 +1,14 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_ceil(void);
+
+
+const struct test winetest_testlist[] =
+{
+    { "ceil", func_ceil },
+
+    { 0, 0 }
+};
