a42b3c36e44359e905d5aabab3917f3181787e78
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-05-16 16:21:25 +0300

    [UCRTBASE_APITEST] Add tests for round/roundf

--- modules/rostests/apitests/ucrtbase/testlist.c
+++ modules/rostests/apitests/ucrtbase/testlist.c
@@ -4,11 +4,13 @@
 #include <wine/test.h>
 
 extern void func_ceil(void);
+extern void func_round(void);
 
 
 const struct test winetest_testlist[] =
 {
     { "ceil", func_ceil },
+    { "round", func_round },
 
     { 0, 0 }
 };
