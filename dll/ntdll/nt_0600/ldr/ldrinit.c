bb00c5c6ffd98e8966f1de5d5f8ba95f5756180d
Justin Miller <justin.miller@reactos.org>
2025-05-29 00:37:09 -0700

    [NTDLL] Stub out the Ldr Win7 Update/Win8 APIs

--- dll/ntdll/nt_0600/ldr/ldrinit.c
+++ dll/ntdll/nt_0600/ldr/ldrinit.c
@@ -1,3 +1,37 @@
 #include "ntdll_vista.h"
 
 PVOID LdrpHeap;
+#include <debug.h>
+
+
+/* These APIs are very commonly used in modern apps + needed for kernelbase, But these stubs will work for now. */
+NTSTATUS WINAPI LdrGetDllDirectory( UNICODE_STRING *dir )
+{
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+
+NTSTATUS WINAPI LdrSetDllDirectory( const UNICODE_STRING *dir )
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS WINAPI LdrAddDllDirectory( const UNICODE_STRING *dir, void **cookie )
+{
+    UNIMPLEMENTED;
+    return STATUS_UNSUCCESSFUL;
+}
+
+NTSTATUS WINAPI LdrRemoveDllDirectory( void *cookie )
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS WINAPI LdrSetDefaultDllDirectories( ULONG flags )
+{
+    UNIMPLEMENTED;
+    return STATUS_SUCCESS;
+}
