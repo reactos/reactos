586989350bb894b8e2034b172abb17c929bd26e5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-06-08 17:21:24 +0900

    [KBSWITCH] Support System Pen Icon (#8080)
    ## Purpose
    Supporting System Pen icon and IME menus for
    East-Asian users.
    JIRA issue: CORE-20142
    
    ## Overview
    The East-Asian system has the system pen icon
    in taskbar additionally. The system pen icon
    shows the current IME status.
    
    If the user clicked the IME system pen icon,
    the IME menu will open. The IME system pen icon
    can be customized by the IME module by posting
    indicator messages.
    
    ## Proposed changes
    - Add default pen icon resources.
    - Add base/applications/kbswitch/imemenu.c to
      handle IME menus.
    - Add code for adding, modifying and deleting
      the System Pen icon.
    - Modify indicdll.spec.
    - Fix popup menu alignment.

--- base/applications/kbswitch/kbswitch.h
+++ base/applications/kbswitch/kbswitch.h
@@ -13,16 +13,25 @@
 #include <ime/indicml.h> /* INDICATOR_CLASS, INDICM_... */
 
 #include "resource.h"
+#include "indicdll/resource.h"
 
 #define CCH_LAYOUT_ID       8   // Character Count of a layout ID like "00000409"
 #define CCH_ULONG_DEC       10  // Maximum Character Count of a ULONG in decimal
 
+// Far East Language IDs
+#define LANGID_CHINESE_SIMPLIFIED MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)
+#define LANGID_CHINESE_TRADITIONAL MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_TRADITIONAL)
+#define LANGID_JAPANESE MAKELANGID(LANG_JAPANESE, SUBLANG_DEFAULT)
+#define LANGID_KOREAN MAKELANGID(LANG_KOREAN, SUBLANG_DEFAULT)
+
 #define WM_LANG_CHANGED    (WM_USER + 10200)
 #define WM_WINDOW_ACTIVATE (WM_USER + 10300)
 
-typedef BOOL (APIENTRY *FN_KbSwitchSetHooks)(BOOL bDoHook);
-
-const TCHAR szKbSwitcherName[] = INDICATOR_CLASS;
+#define IME_STATUS_NO_IME 0
+#define IME_STATUS_IME_CLOSED 1
+#define IME_STATUS_IME_OPEN 2
+#define IME_STATUS_IME_NATIVE 4
+#define IME_STATUS_IME_FULLSHAPE 8
 
 static inline BOOL
 IsWndClassName(_In_opt_ HWND hwndTarget, PCTSTR pszName)
