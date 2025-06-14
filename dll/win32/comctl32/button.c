d200752e96e7050a339824437e5afcdb8bbf4ab8
winesync <ros-dev@reactos.org>
2025-06-14 16:54:55 +0300

    [WINESYNC] comctl32/button: Implement BCM_GETSPLITINFO.
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    
    wine commit id 4e08a7e5d700a5796ccd4873a8c715748d09c5e8 by Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dll/win32/comctl32/button.c
+++ dll/win32/comctl32/button.c
@@ -757,6 +757,9 @@ static LRESULT CALLBACK BUTTON_WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, L
         infoPtr->hwnd = hWnd;
         infoPtr->parent = cs->hwndParent;
         infoPtr->style = cs->style;
+        infoPtr->split_style = BCSS_STRETCH;
+        infoPtr->glyph = (HIMAGELIST)0x36;  /* Marlett down arrow char code */
+        infoPtr->glyph_size.cx = get_default_glyph_size(infoPtr);
 #ifdef __REACTOS__
         SetRect(&infoPtr->text_margin, 1,1,1,1);
 #endif
@@ -1274,6 +1277,22 @@ static LRESULT CALLBACK BUTTON_WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, L
             infoPtr->glyph_size.cx = get_default_glyph_size(infoPtr);
 
         /* Windows doesn't invalidate or redraw it, so we don't, either */
+        return TRUE;
+    }
+
+    case BCM_GETSPLITINFO:
+    {
+        BUTTON_SPLITINFO *info = (BUTTON_SPLITINFO*)lParam;
+
+        if (!info) return FALSE;
+
+        if (info->mask & BCSIF_STYLE)
+            info->uSplitStyle = infoPtr->split_style;
+        if (info->mask & (BCSIF_GLYPH | BCSIF_IMAGE))
+            info->himlGlyph = infoPtr->glyph;
+        if (info->mask & BCSIF_SIZE)
+            info->size = infoPtr->glyph_size;
+
         return TRUE;
     }
 
