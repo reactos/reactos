36011902f2f9c38f1112ad066b2413c175c47795
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-06-18 13:13:32 +0300

    [CRT] Remove obsolete CRT files

--- sdk/lib/crt/time/difftime32.c
+++
@@ -1,9 +0,0 @@
-/*
- * COPYRIGHT:   LGPL, See LGPL.txt in the top level directory
- * PROJECT:     ReactOS CRT library
- * FILE:        lib/sdk/crt/time/difftime32.c
- * PURPOSE:     Implementation of _difftime32
- * PROGRAMERS:  Timo Kreuzer
- */
-#define _USE_EXPLICIT_32BIT_TIME
-#include "difftime.c"
