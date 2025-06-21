d2eb6a9e26f64d71259c469072864443801b372e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-06-21 11:34:16 +0900

    [VCRUNTIME] Attempt to fix MSVC ARM build

---
+++ sdk/lib/vcruntime/arm/interlocked.s
@@ -0,0 +1,23 @@
+#include <asm.inc>
+
+.code64
+
+PUBLIC _InterlockedCompareExchangePointer
+    ret
+END
+
+PUBLIC _InterlockedExchangePointer
+    ret
+END
+
+PUBLIC _InterlockedExchangeAdd
+    ret
+END
+
+PUBLIC _InterlockedIncrement
+    ret
+END
+
+PUBLIC _InterlockedDecrement
+    ret
+END
