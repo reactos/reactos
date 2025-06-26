f7c36c6d942ec3821b10ca6d5009e3df0f0bc63a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-06-26 18:20:49 +0900

    [SHELL32_APITEST] Certainly close newly-opened windows (#8171)
    It's frustrating when windows open
    during a test and remain after the test
    is over.
    JIRA issue: ROSTESTS-402
    - Delete closewnd.cpp. Enhance
      closewnd.h.
    - Modify ShellExecCmdLine,
      ShellExec_RunDLL, ShellExecuteEx,
      and ShellExecuteW testcases.
    - Certainly close the newly-opened
      windows.

--- modules/rostests/apitests/shell32/ShellExecuteW.cpp
+++ modules/rostests/apitests/shell32/ShellExecuteW.cpp
@@ -10,17 +10,21 @@
 #include <stdio.h>
 #include <winbase.h>
 #include <shlwapi.h>
+#include "closewnd.h"
 
 #define WAIT_SLEEP 700
 
 // ShellExecuteW(handle, "open", <path_to_executable>, <parameters>, NULL, SW_SHOWNORMAL);
 
+static WINDOW_LIST s_List1, s_List2;
+
 START_TEST(ShellExecuteW)
 {
     INT ret;
     HINSTANCE hInstance;
-    HWND hWnd;
     WCHAR WinDir[MAX_PATH], SysDir[MAX_PATH], SysDrive[MAX_PATH];
+
+    GetWindowList(&s_List1);
 
     if (!GetWindowsDirectoryW(WinDir, _countof(WinDir)))
     {
@@ -47,104 +51,57 @@ START_TEST(ShellExecuteW)
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #1: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #1 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(NULL, L"Display Properties");
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #2: Open Notepad
     hInstance = ShellExecuteW(NULL, L"open", L"notepad.exe", NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #2: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #2 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"Notepad", L"Untitled - Notepad");
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #3: Open Windows folder
     hInstance = ShellExecuteW(NULL, NULL, WinDir, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #3: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #3 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"CabinetWClass", WinDir);
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #4: Open system32 folder
     hInstance = ShellExecuteW(NULL, L"open", SysDir, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #4: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #4 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"CabinetWClass", SysDir);
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #5: Open %SystemDrive%
     hInstance = ShellExecuteW(NULL, L"explore", SysDrive, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #5: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #5 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"ExploreWClass", SysDrive);
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #6: Open Explorer Search on %SYSTEMDRIVE%
     hInstance = ShellExecuteW(NULL, L"find", SysDrive, NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #6: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #6 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"CabinetWClass", L"Search Results");
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #7: Open My Documents ("::{450d8fba-ad25-11d0-98a8-0800361b1103}")
     hInstance = ShellExecuteW(NULL, NULL, L"::{450d8fba-ad25-11d0-98a8-0800361b1103}", NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #7: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #7 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"CabinetWClass", NULL);
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
 
     // TEST #8: Open My Documents ("shell:::{450d8fba-ad25-11d0-98a8-0800361b1103}")
     hInstance = ShellExecuteW(NULL, L"open", L"shell:::{450d8fba-ad25-11d0-98a8-0800361b1103}", NULL, NULL, SW_SHOWNORMAL);
     ret = (INT)(UINT_PTR)hInstance;
     ok(ret > 31, "TEST #8: ret:%d, LastError: %ld\n", ret, GetLastError());
     trace("TEST #8 ret: %d.\n", ret);
-    if (hInstance)
-    {
-        Sleep(WAIT_SLEEP);
-        // Terminate Window
-        hWnd = FindWindowW(L"CabinetWClass", NULL);
-        PostMessage(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0);
-    }
+
+    // Execution can be asynchronous; you have to wait for it to finish.
+    Sleep(2000);
+
+    // Close newly-opened window(s)
+    GetWindowList(&s_List2);
+    CloseNewWindows(&s_List1, &s_List2);
+    FreeWindowList(&s_List1);
+    FreeWindowList(&s_List2);
 }
 
 // Windows Server 2003 and Windows XP SP3 return values (Win 7 returns 42 in all cases)
--- modules/rostests/apitests/shell32/closewnd.h
+++ modules/rostests/apitests/shell32/closewnd.h
@@ -2,7 +2,7 @@
  * PROJECT:     ReactOS API tests
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * PURPOSE:     Close windows after tests
- * COPYRIGHT:   Copyright 2024 Katayama Hirofumi MZ (katayama.hirofumi.mz@gmail.com)
+ * COPYRIGHT:   Copyright 2024-2025 Katayama Hirofumi MZ (katayama.hirofumi.mz@gmail.com)
  */
 
 #pragma once
@@ -13,8 +13,122 @@ typedef struct WINDOW_LIST
     HWND *m_phWnds;
 } WINDOW_LIST, *PWINDOW_LIST;
 
-void GetWindowList(PWINDOW_LIST pList);
-void GetWindowListForClose(PWINDOW_LIST pList);
-HWND FindNewWindow(PWINDOW_LIST List1, PWINDOW_LIST List2);
-void CloseNewWindows(PWINDOW_LIST List1, PWINDOW_LIST List2);
-void FreeWindowList(PWINDOW_LIST pList);
+static inline VOID FreeWindowList(PWINDOW_LIST pList)
+{
+    free(pList->m_phWnds);
+    pList->m_phWnds = NULL;
+    pList->m_chWnds = 0;
+}
+
+static inline BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
+{
+    if (!IsWindowVisible(hwnd))
+        return TRUE;
+
+    PWINDOW_LIST pList = (PWINDOW_LIST)lParam;
+    SIZE_T cb = (pList->m_chWnds + 1) * sizeof(HWND);
+    HWND *phWnds = (HWND *)realloc(pList->m_phWnds, cb);
+    if (!phWnds)
+        return FALSE;
+    phWnds[pList->m_chWnds++] = hwnd;
+    pList->m_phWnds = phWnds;
+    return TRUE;
+}
+
+static inline VOID GetWindowList(PWINDOW_LIST pList)
+{
+    pList->m_phWnds = NULL;
+    pList->m_chWnds = 0;
+    EnumWindows(EnumWindowsProc, (LPARAM)pList);
+}
+
+static inline HWND FindInWindowList(const WINDOW_LIST &list, HWND hWnd)
+{
+    for (SIZE_T i = 0; i < list.m_chWnds; ++i)
+    {
+        if (list.m_phWnds[i] == hWnd)
+            return hWnd;
+    }
+    return NULL;
+}
+
+static inline VOID CloseNewWindows(PWINDOW_LIST List1, PWINDOW_LIST List2)
+{
+    for (SIZE_T i = 0; i < List2->m_chWnds; ++i)
+    {
+        HWND hWnd = List2->m_phWnds[i];
+        if (!IsWindowVisible(hWnd) || FindInWindowList(*List1, hWnd))
+            continue;
+
+        if (!PostMessageW(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0))
+        {
+            DWORD_PTR result;
+            if (!SendMessageTimeoutW(hWnd, WM_SYSCOMMAND, SC_CLOSE, 0, SMTO_ABORTIFHUNG, 3000, &result))
+            {
+                SwitchToThisWindow(hWnd, TRUE);
+
+                // SwitchToThisWindow may take time
+                Sleep(500);
+
+                // Alt+F4
+                INPUT inputs[4];
+                ZeroMemory(&inputs, sizeof(inputs));
+                inputs[0].type = inputs[1].type = inputs[2].type = inputs[3].type = INPUT_KEYBOARD;
+                inputs[0].ki.wVk = inputs[3].ki.wVk = VK_LMENU;
+                inputs[1].ki.wVk = inputs[2].ki.wVk = VK_F4;
+                inputs[2].ki.dwFlags = inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+                SendInput(_countof(inputs), inputs, sizeof(INPUT));
+
+                // Closing a window may take time
+                Sleep(1000);
+
+                if (IsWindowVisible(hWnd))
+                {
+                    CHAR szClass[64];
+                    GetClassNameA(hWnd, szClass, _countof(szClass));
+                    trace("Unable to close window %p (%s)\n", hWnd, szClass);
+                }
+            }
+        }
+    }
+}
+
+static inline HWND FindNewWindow(PWINDOW_LIST List1, PWINDOW_LIST List2)
+{
+    for (SIZE_T i2 = 0; i2 < List2->m_chWnds; ++i2)
+    {
+        HWND hWnd = List2->m_phWnds[i2];
+        if (!IsWindowEnabled(hWnd) || !IsWindowVisible(hWnd))
+            continue;
+
+        BOOL bFoundInList1 = FALSE;
+        for (SIZE_T i1 = 0; i1 < List1->m_chWnds; ++i1)
+        {
+            if (hWnd == List1->m_phWnds[i1])
+            {
+                bFoundInList1 = TRUE;
+                break;
+            }
+        }
+
+        if (!bFoundInList1)
+            return hWnd;
+    }
+    return NULL;
+}
+
+static inline BOOL CALLBACK CountWindowsProc(HWND hwnd, LPARAM lParam)
+{
+    if (!IsWindowVisible(hwnd))
+        return TRUE;
+
+    *(PINT)lParam += 1;
+    return TRUE;
+}
+
+static inline INT GetWindowCount(VOID)
+{
+    INT nCount = 0;
+    EnumWindows(CountWindowsProc, (LPARAM)&nCount);
+    return nCount;
+}
