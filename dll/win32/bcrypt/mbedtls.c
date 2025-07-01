3df7f38ed1c630ba69a11b4fd350b9981da57f84
Peter Hater <7element@mail.bg>
2025-07-01 13:11:54 +0000

    [BCRYPT] Implement symetric encryption/decryption

--- dll/win32/bcrypt/mbedtls.c
+++ dll/win32/bcrypt/mbedtls.c
@@ -18,6 +18,18 @@ MAKE_FUNCPTR(mbedtls_md_hmac_update);
 MAKE_FUNCPTR(mbedtls_md_hmac_starts);
 MAKE_FUNCPTR(mbedtls_md_hmac_finish);
 MAKE_FUNCPTR(mbedtls_md_free);
+MAKE_FUNCPTR(mbedtls_cipher_init);
+MAKE_FUNCPTR(mbedtls_cipher_setup);
+MAKE_FUNCPTR(mbedtls_cipher_info_from_type);
+MAKE_FUNCPTR(mbedtls_cipher_setkey);
+MAKE_FUNCPTR(mbedtls_cipher_set_iv);
+MAKE_FUNCPTR(mbedtls_cipher_set_padding_mode);
+MAKE_FUNCPTR(mbedtls_cipher_update);
+MAKE_FUNCPTR(mbedtls_cipher_update_ad);
+MAKE_FUNCPTR(mbedtls_cipher_check_tag);
+MAKE_FUNCPTR(mbedtls_cipher_write_tag);
+MAKE_FUNCPTR(mbedtls_cipher_finish);
+MAKE_FUNCPTR(mbedtls_cipher_free);
 #undef MAKE_FUNCPTR
 
 #define mbedtls_md_init                 pmbedtls_md_init
@@ -31,6 +43,18 @@ MAKE_FUNCPTR(mbedtls_md_free);
 #define mbedtls_md_hmac_starts          pmbedtls_md_hmac_starts
 #define mbedtls_md_hmac_finish          pmbedtls_md_hmac_finish
 #define mbedtls_md_free                 pmbedtls_md_free
+#define mbedtls_cipher_init             pmbedtls_cipher_init
+#define mbedtls_cipher_setup            pmbedtls_cipher_setup
+#define mbedtls_cipher_info_from_type   pmbedtls_cipher_info_from_type
+#define mbedtls_cipher_setkey           pmbedtls_cipher_setkey
+#define mbedtls_cipher_set_iv           pmbedtls_cipher_set_iv
+#define mbedtls_cipher_set_padding_mode pmbedtls_cipher_set_padding_mode
+#define mbedtls_cipher_update           pmbedtls_cipher_update
+#define mbedtls_cipher_update_ad        pmbedtls_cipher_update_ad
+#define mbedtls_cipher_check_tag        pmbedtls_cipher_check_tag
+#define mbedtls_cipher_write_tag        pmbedtls_cipher_write_tag
+#define mbedtls_cipher_finish           pmbedtls_cipher_finish
+#define mbedtls_cipher_free             pmbedtls_cipher_free
 
 NTSTATUS process_attach( void *args )
 {
@@ -58,6 +82,18 @@ NTSTATUS process_attach( void *args )
     LOAD_FUNCPTR(mbedtls_md_hmac_starts)
     LOAD_FUNCPTR(mbedtls_md_hmac_finish)
     LOAD_FUNCPTR(mbedtls_md_free);
+    LOAD_FUNCPTR(mbedtls_cipher_init)
+    LOAD_FUNCPTR(mbedtls_cipher_setup)
+    LOAD_FUNCPTR(mbedtls_cipher_info_from_type)
+    LOAD_FUNCPTR(mbedtls_cipher_setkey)
+    LOAD_FUNCPTR(mbedtls_cipher_set_iv)
+    LOAD_FUNCPTR(mbedtls_cipher_set_padding_mode);
+    LOAD_FUNCPTR(mbedtls_cipher_update);
+    LOAD_FUNCPTR(mbedtls_cipher_update_ad);
+    LOAD_FUNCPTR(mbedtls_cipher_check_tag);
+    LOAD_FUNCPTR(mbedtls_cipher_write_tag);
+    LOAD_FUNCPTR(mbedtls_cipher_finish);
+    LOAD_FUNCPTR(mbedtls_cipher_free);
 #undef LOAD_FUNCPTR
 
     return STATUS_SUCCESS;
@@ -181,34 +217,246 @@ NTSTATUS hash_get_size( struct hash *hash, ULONG *output )
     return STATUS_SUCCESS;
 }
 
-NTSTATUS key_symmetric_set_auth_data( void *args )
+typedef struct {
+	unsigned char *data;
+	unsigned int size;
+} mbedtls_datum_t;
+
+union key_data
 {
-    return STATUS_NOT_IMPLEMENTED;
+    mbedtls_cipher_context_t cipher_ctx;
+    struct
+    {
+        mbedtls_pk_context  privkey;
+        mbedtls_pk_context  pubkey;
+        mbedtls_dhm_context dh_params;
+    } a;
+};
+C_ASSERT( sizeof(union key_data) <= sizeof(((struct key *)0)->private) );
+
+static union key_data *key_data( struct key *key )
+{
+    return (union key_data *)key->private;
+}
+
+mbedtls_cipher_id_t get_mbedtls_cipher( const struct key *key )
+{
+    switch (key->alg_id)
+    {
+    case ALG_ID_3DES:
+        WARN( "handle block size\n" );
+        switch (key->u.s.mode)
+        {
+        case CHAIN_MODE_ECB:
+            return MBEDTLS_CIPHER_DES_EDE3_ECB;
+        case CHAIN_MODE_CBC:
+            return MBEDTLS_CIPHER_DES_EDE3_CBC;
+        default:
+            break;
+        }
+        FIXME( "3DES mode %u with key length %u not supported\n", key->u.s.mode, key->u.s.secret_len );
+        return MBEDTLS_CIPHER_NONE;
+
+    case ALG_ID_AES:
+        WARN( "handle block size\n" );
+        switch (key->u.s.mode)
+        {
+        case CHAIN_MODE_GCM:
+            if (key->u.s.secret_len == 16) return MBEDTLS_CIPHER_AES_128_GCM;
+            if (key->u.s.secret_len == 24) return MBEDTLS_CIPHER_AES_192_GCM;
+            if (key->u.s.secret_len == 32) return MBEDTLS_CIPHER_AES_256_GCM;
+            break;
+        case CHAIN_MODE_ECB: /* can be emulated with CBC + empty IV */
+        case CHAIN_MODE_CBC:
+            if (key->u.s.secret_len == 16) return MBEDTLS_CIPHER_AES_128_CBC;
+            if (key->u.s.secret_len == 24) return MBEDTLS_CIPHER_AES_192_CBC;
+            if (key->u.s.secret_len == 32) return MBEDTLS_CIPHER_AES_256_CBC;
+            break;
+        case CHAIN_MODE_CFB:
+            if (key->u.s.secret_len == 16) return MBEDTLS_CIPHER_AES_128_CFB8;
+            if (key->u.s.secret_len == 24) return MBEDTLS_CIPHER_AES_192_CFB8;
+            if (key->u.s.secret_len == 32) return MBEDTLS_CIPHER_AES_256_CFB8;
+            break;
+        default:
+            break;
+        }
+        FIXME( "AES mode %u with key length %u not supported\n", key->u.s.mode, key->u.s.secret_len );
+        return MBEDTLS_CIPHER_NONE;
+
+    default:
+        FIXME( "algorithm %u not supported\n", key->alg_id );
+        return MBEDTLS_CIPHER_NONE;
+    }
+    return MBEDTLS_CIPHER_NONE;
 }
 
 NTSTATUS key_symmetric_vector_reset( void *args )
 {
-    return STATUS_NOT_IMPLEMENTED;
+    struct key *key = args;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if (key_data(key)->cipher_ctx.cipher_info != NULL && key->u.s.vector != NULL && key->u.s.vector_len)
+    {
+        if (mbedtls_cipher_set_iv( &key_data(key)->cipher_ctx, key->u.s.vector, key->u.s.vector_len )) return STATUS_INTERNAL_ERROR;
+        return STATUS_SUCCESS;
+    }
+    TRACE( "invalidating cipher handle\n" );
+    mbedtls_cipher_free( &key_data(key)->cipher_ctx );
+    key_data(key)->cipher_ctx.cipher_info = NULL;
+    return STATUS_SUCCESS;
 }
 
-NTSTATUS key_symmetric_encrypt_internal( void *args )
+NTSTATUS init_cipher_handle( struct key *key, const mbedtls_operation_t operation )
 {
-    return STATUS_NOT_IMPLEMENTED;
+    mbedtls_cipher_type_t cipher;
+    const mbedtls_cipher_info_t *info;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if (key_data(key)->cipher_ctx.cipher_info != NULL)
+    {
+        if (mbedtls_cipher_get_operation( &key_data(key)->cipher_ctx ) == operation) return STATUS_SUCCESS;
+        mbedtls_cipher_free( &key_data(key)->cipher_ctx );
+        key_data(key)->cipher_ctx.cipher_info = NULL;
+    }
+    cipher = get_mbedtls_cipher( key );
+    if (cipher == MBEDTLS_CIPHER_NONE) return STATUS_NOT_SUPPORTED;
+
+    if ((info = mbedtls_cipher_info_from_type( cipher )) == NULL)
+    {
+        return STATUS_INTERNAL_ERROR;
+    }
+    mbedtls_cipher_init( &key_data(key)->cipher_ctx );
+    if (mbedtls_cipher_setup( &key_data(key)->cipher_ctx, info ))
+    {
+        mbedtls_cipher_free( &key_data(key)->cipher_ctx );
+        return STATUS_INTERNAL_ERROR;
+    }
+    mbedtls_cipher_set_padding_mode( &key_data(key)->cipher_ctx, MBEDTLS_PADDING_NONE );
+    if (key->u.s.secret != NULL && key->u.s.secret_len > 0) mbedtls_cipher_setkey( &key_data(key)->cipher_ctx, key->u.s.secret, /*mbedtls_cipher_get_key_bitlen( &key_data(key)->cipher_ctx )*/key->u.s.secret_len*8, operation );
+    if (key->u.s.vector != NULL && key->u.s.vector_len > 0) mbedtls_cipher_set_iv( &key_data(key)->cipher_ctx, key->u.s.vector, key->u.s.vector_len );
+
+    return STATUS_SUCCESS;
 }
 
-NTSTATUS key_symmetric_decrypt_internal( void *args )
+NTSTATUS key_symmetric_set_auth_data( void *args )
 {
-    return STATUS_NOT_IMPLEMENTED;
+    struct key_symmetric_set_auth_data_params *auth_params = args;
+    NTSTATUS status;
+    int ret;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if ((status = init_cipher_handle( auth_params->key, auth_params->encrypt ? MBEDTLS_ENCRYPT : MBEDTLS_DECRYPT ))) return status;
+
+    ret = mbedtls_cipher_update_ad( &key_data(auth_params->key)->cipher_ctx, auth_params->auth_data, auth_params->len );
+    if (ret) return STATUS_INTERNAL_ERROR;
+    return STATUS_SUCCESS;
 }
 
 NTSTATUS key_symmetric_get_tag( void *args )
 {
-    return STATUS_NOT_IMPLEMENTED;
+    struct key_symmetric_get_tag_params *tag_params = args;
+    mbedtls_operation_t operation;
+    int ret;
+    NTSTATUS status;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    operation = mbedtls_cipher_get_operation( &key_data(tag_params->key)->cipher_ctx );
+    if ((status = init_cipher_handle( tag_params->key, operation ))) return status;
+
+    switch (operation) {
+        case MBEDTLS_ENCRYPT:
+            ret = mbedtls_cipher_write_tag( &key_data(tag_params->key)->cipher_ctx, tag_params->tag, tag_params->len );
+            if (ret) status = STATUS_INTERNAL_ERROR;
+            break;
+        case MBEDTLS_DECRYPT:
+            ret = mbedtls_cipher_check_tag( &key_data(tag_params->key)->cipher_ctx, tag_params->tag, tag_params->len );
+            if (ret)
+            {
+                if (ret == MBEDTLS_ERR_CIPHER_AUTH_FAILED)
+                    status = STATUS_AUTH_TAG_MISMATCH;
+                else
+                    status = STATUS_INTERNAL_ERROR;
+            }
+            break;
+        default:
+            FIXME( "AES operation %u not supported\n", operation );
+            status = STATUS_INTERNAL_ERROR;
+            break;
+    }
+
+    return status;
+}
+
+NTSTATUS key_symmetric_encrypt_internal( void *args )
+{
+    const struct key_symmetric_encrypt_params *params = args;
+    NTSTATUS status;
+    int ret;
+    size_t output_len = 0;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if ((status = init_cipher_handle( params->key, MBEDTLS_ENCRYPT ))) return status;
+
+    ret = mbedtls_cipher_update( &key_data(params->key)->cipher_ctx,
+                                    params->input, params->input_len,
+                                    params->output, &output_len);
+    if (!output_len)
+        ret = mbedtls_cipher_finish( &key_data(params->key)->cipher_ctx,
+                                    params->output, &output_len);
+    if (ret) return STATUS_INTERNAL_ERROR;
+
+    if (params->output_len != output_len) return STATUS_INTERNAL_ERROR;
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS key_symmetric_decrypt_internal( void *args )
+{
+    const struct key_symmetric_decrypt_params *params = args;
+    NTSTATUS status;
+    int ret;
+    size_t output_len = 0;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if ((status = init_cipher_handle( params->key, MBEDTLS_DECRYPT ))) return status;
+
+    ret = mbedtls_cipher_update( &key_data(params->key)->cipher_ctx,
+                                    params->input, params->input_len,
+                                    params->output, &output_len);
+    if (!output_len)
+        ret = mbedtls_cipher_finish( &key_data(params->key)->cipher_ctx,
+                                    params->output, &output_len);
+    if (ret) return STATUS_INTERNAL_ERROR;
+
+    if (params->output_len != output_len) return STATUS_INTERNAL_ERROR;
+
+    return STATUS_SUCCESS;
 }
 
 NTSTATUS key_symmetric_destroy( void *args )
 {
-    return STATUS_NOT_IMPLEMENTED;
+    struct key *key = args;
+
+#ifndef __REACTOS__
+    if (!libmbedtls_handle) return STATUS_INTERNAL_ERROR;
+#endif
+    if (key_data(key)->cipher_ctx.cipher_info != NULL) {
+        mbedtls_cipher_free( &key_data(key)->cipher_ctx );
+        key_data(key)->cipher_ctx.cipher_info = NULL;
+    }
+    return STATUS_SUCCESS;
 }
 
 NTSTATUS key_asymmetric_generate( void *args )
