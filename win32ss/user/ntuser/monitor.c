f506f8b8063db1e29ca94def64151bf854ecc98a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-07-14 17:25:59 +0900

    Update win32ss/user/ntuser/monitor.c

--- win32ss/user/ntuser/monitor.c
+++ win32ss/user/ntuser/monitor.c
@@ -970,7 +970,7 @@ NtUserMonitorFromWindow(
 {
     PWND pWnd;
     HMONITOR hMonitor = NULL;
-    RECTL Rect = { 0, 0, 0, 0 };
+    RECTL Rect = {0, 0, 0, 0};
 
     TRACE("Enter NtUserMonitorFromWindow\n");
 
