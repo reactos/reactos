624c3fd847868d26b9e3f3e1ada7eab188ca8648
George Bișoc <george.bisoc@reactos.org>
2025-07-14 22:52:59 +0200

    [SDK] Introduce the PO GUID library
    This library aims to include all the public and global power manager related GUID identifiers into one shared library, of which they can be accessed across multiple modules of ReactOS, e.g. the kernel, ACPI, PCI, et al.
    
    This patch is needed for the development of the Power Manager (#5719) to continue.
    
    CORE-18969

---
+++ sdk/lib/poguid/poguid.c
@@ -0,0 +1,14 @@
+/*
+ * PROJECT:     ReactOS SDK
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Power Manager GUID library
+ * COPYRIGHT:   Copyright 2011 Timo Kreuzer <timo.kreuzer@reactos.org>
+ */
+
+/* INCLUDES *****************************************************************/
+
+#include <wdm.h>
+#include <initguid.h>
+#include <poclass.h>
+
+/* EOF */
