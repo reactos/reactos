c9842e5aad5d45039dd0dfea2889a5a6d25ca1d4
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2025-07-29 13:57:12 -0600

    Move /sdk/include/reactos/wine to /sdk/include/wine, reorder global includes, remove unneeded includes (#8258)
    - Move sdk\include\reactos\wine to sdk\include\wine
    - Reorder the directories in include_directories() to be closer to alphabetical. This should make it easier to determine what global include directories can be removed in the future.

--- sdk/cmake/set_wine_module.cmake
+++ sdk/cmake/set_wine_module.cmake
@@ -3,14 +3,14 @@
 
 function(set_wine_module TARGET)
     include_directories(BEFORE
-        ${REACTOS_SOURCE_DIR}/sdk/include/reactos/wine
-        ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
+        ${REACTOS_SOURCE_DIR}/sdk/include/wine
+        ${REACTOS_BINARY_DIR}/sdk/include/wine)
 endfunction()
 
 # FIXME: Replace this call with set_wine_module
 function(set_wine_module_FIXME TARGET)
     include_directories(BEFORE
-        ${REACTOS_SOURCE_DIR}/sdk/include/reactos/wine
-        ${REACTOS_BINARY_DIR}/sdk/include/reactos/wine)
+        ${REACTOS_SOURCE_DIR}/sdk/include/wine
+        ${REACTOS_BINARY_DIR}/sdk/include/wine)
     target_compile_definitions(${TARGET} PRIVATE __WINESRC__)
 endfunction()
