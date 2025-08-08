reactos/reactos

commit 3842b59f75e0793188fc94fb2c670930a04d7fbe

[TCPIP] Retrieve the interface medium type from NDIS
Send OID_GEN_PHYSICAL_MEDIUM and OID_GEN_MEDIA_SUPPORTED query requests to NDIS.

This will enable us to distinguish wired and wireless ethernet adapters.

drivers/network/tcpip/tcpip/iinfo.c
@@ -24,6 +24,7 @@ TDI_STATUS InfoTdiQueryGetInterfaceMIB(TDIEntityID ID,
     PLAN_ADAPTER IF;
     PCHAR IFDescr;
     ULONG Size;
+    ULONG Medium;
     NDIS_STATUS NdisStatus;
 
     if (!Interface)
@@ -44,7 +45,7 @@ TDI_STATUS InfoTdiQueryGetInterfaceMIB(TDIEntityID ID,
     OutData->if_index = Interface->Index;
     /* viz: tcpip keeps those indices */
     OutData->if_type = Interface ==
-        Loopback ? MIB_IF_TYPE_LOOPBACK : MIB_IF_TYPE_ETHERNET;
+        Loopback ? MIB_IF_TYPE_LOOPBACK : MIB_IF_TYPE_OTHER;
     OutData->if_mtu = Interface->MTU;
     TI_DbgPrint(DEBUG_INFO,
 		("Getting interface speed\n"));
@@ -56,11 +57,102 @@ TDI_STATUS InfoTdiQueryGetInterfaceMIB(TDIEntityID ID,
     IFDescr = (PCHAR)&OutData->if_descr[0];
 
     if( IF ) {
-	GetInterfaceSpeed( Interface, (PUINT)&OutData->if_speed );
-	TI_DbgPrint(DEBUG_INFO,
-		    ("IF Speed = %d * 100bps\n", OutData->if_speed));
-	memcpy(OutData->if_physaddr, Interface->Address, Interface->AddressLength);
-	TI_DbgPrint(DEBUG_INFO, ("Got HWAddr\n"));
+        if (OutData->if_type == MIB_IF_TYPE_OTHER)
+        {
+            NdisStatus = NDISCall(IF,
+                                  NdisRequestQueryInformation,
+                                  OID_GEN_PHYSICAL_MEDIUM,
+                                  &Medium,
+                                  sizeof(ULONG));
+            if (NdisStatus == NDIS_STATUS_SUCCESS)
+            {
+                switch (Medium)
+                {
+#if 0
+                    case NdisPhysicalMediumUnspecified:
+#endif
+                    case NdisPhysicalMediumWirelessLan:
+                        OutData->if_type = IF_TYPE_IEEE80211;
+                        break;
+#if 0
+                    case NdisPhysicalMediumCableModem:
+                    case NdisPhysicalMediumPhoneLine:
+                    case NdisPhysicalMediumPowerLine:
+                    case NdisPhysicalMediumDSL:
+                    case NdisPhysicalMediumFibreChannel:
+                    case NdisPhysicalMedium1394:
+                    case NdisPhysicalMediumWirelessWan:
+#endif
+                    case NdisPhysicalMediumNative802_11:
+                        OutData->if_type = IF_TYPE_IEEE80211;
+                        break;
+#if 0
+                    case NdisPhysicalMediumBluetooth:
+                    case NdisPhysicalMediumInfiniband:
+                    case NdisPhysicalMediumWiMax:
+                    case NdisPhysicalMediumUWB:
+#endif
+                    case NdisPhysicalMedium802_3:
+                        OutData->if_type = MIB_IF_TYPE_ETHERNET;
+                        break;
+
+                    case NdisPhysicalMedium802_5:
+                        OutData->if_type = MIB_IF_TYPE_TOKENRING;
+                        break;
+#if 0
+                    case NdisPhysicalMediumIrda:
+                    case NdisPhysicalMediumWiredWAN:
+                    case NdisPhysicalMediumWiredCoWan:
+                    case NdisPhysicalMediumOther:
+#endif
+                }
+            }
+        }
+
+        if (OutData->if_type == MIB_IF_TYPE_OTHER)
+        {
+            NdisStatus = NDISCall(IF,
+                                  NdisRequestQueryInformation,
+                                  OID_GEN_MEDIA_SUPPORTED,
+                                  &Medium,
+                                  sizeof(ULONG));
+            if (NdisStatus == NDIS_STATUS_SUCCESS)
+            {
+                switch (Medium)
+                {
+                    case NdisMedium802_3:
+                        OutData->if_type = MIB_IF_TYPE_ETHERNET;
+                        break;
+
+                    case NdisMedium802_5:
+                        OutData->if_type = MIB_IF_TYPE_TOKENRING;
+                        break;
+
+                    case NdisMediumFddi:
+                        OutData->if_type = MIB_IF_TYPE_FDDI;
+                        break;
+#if 0
+                    case NdisMediumWan,
+                    case NdisMediumLocalTalk:
+                    case NdisMediumDix:
+                    case NdisMediumArcnetRaw:
+                    case NdisMediumArcnet878_2:
+                    case NdisMediumAtm:
+                    case NdisMediumWirelessWan:
+                    case NdisMediumIrda:
+                    case NdisMediumBpc:
+                    case NdisMediumCoWan:
+                    case NdisMedium1394:
+#endif
+                }
+            }
+        }
+
+        GetInterfaceSpeed( Interface, (PUINT)&OutData->if_speed );
+        TI_DbgPrint(DEBUG_INFO,
+            ("IF Speed = %d * 100bps\n", OutData->if_speed));
+        memcpy(OutData->if_physaddr, Interface->Address, Interface->AddressLength);
+        TI_DbgPrint(DEBUG_INFO, ("Got HWAddr\n"));
 
         memcpy(&OutData->if_inoctets, &Interface->Stats, sizeof(SEND_RECV_STATS));
 

commit 62f9d1ad46b33c7ec777a276a8c2a78480602298

[TCPIP] Return MIB_IF_TYPE_ETHERNET for WLAN adapter types when compiling ReactOS as NT 5.2 (#8300)
Split Windows Server 2003 and Windows Vista behaviour in two different cases and make them used separately: when ReactOS target version is NT 5.2, use the 1st case, otherwise use the 2nd one instead.
Also, handle NdisPhysicalMediumNative802_11 case only for NT 6.0 and newer, since that's actually supported starting from NDIS 6.0, according to MSDN:
https://learn.microsoft.com/en-us/windows-hardware/drivers/network/oid-gen-physical-medium
while for NDIS 5.1 and newer it's:
https://learn.microsoft.com/en-us/previous-versions/windows/hardware/network/ff560255(v=vs.85)
This fixes:
1. A critical regression with unavailable networking in ReactOS when 3rd-party NT5-compatible Ethernet/Wi-Fi drivers are used (both in VM and on real hardware) caused by commit 3842b59f75e0793188fc94fb2c670930a04d7fbe from @EricKohl.
2. Failure to open Network Properties and Network Status from the Network tray icon (caused by the same commit too).
MSDN tells us this is done so for Windows XP and Server 2003 as well, and IF_TYPE_IEEE80211 is returned only in Windows Vista and newer, so the following Microsoft documentation is actually correct in this case:
https://learn.microsoft.com/en-us/windows/win32/api/iptypes/ns-iptypes-ip_adapter_info
(see the information about "IP_ADAPTER_INFO.Type" member below on that page)
!!!Please note that the behaviour of Vista+ case (e. g., when compiling ReactOS targeted as NT 6.0 and newer) remains unchanged after this fix (networking still may not work), so it still requires the proper support of 3rd-party NT6+ compatible network miniport drivers (by handling this case via the Vista/NDIS6-style way) for NT6 target!!!
CORE-20244

drivers/network/tcpip/tcpip/iinfo.c
@@ -72,7 +72,12 @@ TDI_STATUS InfoTdiQueryGetInterfaceMIB(TDIEntityID ID,
                     case NdisPhysicalMediumUnspecified:
 #endif
                     case NdisPhysicalMediumWirelessLan:
+                        /* Win2003: MIB_IF_TYPE_ETHERNET; Vista+: IF_TYPE_IEEE80211 */
+#if (_WIN32_WINNT >= _WIN32_WINNT_VISTA)
                         OutData->if_type = IF_TYPE_IEEE80211;
+#else
+                        OutData->if_type = MIB_IF_TYPE_ETHERNET;
+#endif
                         break;
 #if 0
                     case NdisPhysicalMediumCableModem:
@@ -83,9 +88,12 @@ TDI_STATUS InfoTdiQueryGetInterfaceMIB(TDIEntityID ID,
                     case NdisPhysicalMedium1394:
                     case NdisPhysicalMediumWirelessWan:
 #endif
+#if (_WIN32_WINNT >= _WIN32_WINNT_VISTA)
+                    /* NDIS 6.0+ (Vista+) */
                     case NdisPhysicalMediumNative802_11:
                         OutData->if_type = IF_TYPE_IEEE80211;
                         break;
+#endif
 #if 0
                     case NdisPhysicalMediumBluetooth:
                     case NdisPhysicalMediumInfiniband: