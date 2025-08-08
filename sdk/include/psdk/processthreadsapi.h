f471e91e8cf20afa8e364dd8801c801d35e0b601
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2025-08-08 17:58:40 -0600

    psdk and wine header changes needed for iphlpapi, shlwapi, advapi32, and kernel32 winetest syncs

--- sdk/include/psdk/processthreadsapi.h
+++ sdk/include/psdk/processthreadsapi.h
@@ -143,6 +143,23 @@ GetCurrentThreadEffectiveToken(
 
 #endif // (_WIN32_WINNT >= 0x602) || defined(__REACTOS__)
 
+typedef enum _PROCESS_INFORMATION_CLASS
+{
+    ProcessMemoryPriority,
+    ProcessMemoryExhaustionInfo,
+    ProcessAppMemoryInfo,
+    ProcessInPrivateInfo,
+    ProcessPowerThrottling,
+    ProcessReservedValue1,
+    ProcessTelemetryCoverageInfo,
+    ProcessProtectionLevelInfo,
+    ProcessLeapSecondInfo,
+    ProcessMachineTypeInfo,
+    ProcessOverrideSubsequentPrefetchParameter,
+    ProcessMaxOverridePrefetchParameter,
+    ProcessInformationClassMax
+} PROCESS_INFORMATION_CLASS;
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
