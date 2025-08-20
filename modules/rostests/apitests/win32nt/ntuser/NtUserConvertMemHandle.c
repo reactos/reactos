639956b7cba5ae7ed66ea37f05ac3fda5e50bd94
Max Korostil <mrmks04@yandex.ru>
2025-08-21 00:23:30 +0300

    Change success test.

--- modules/rostests/apitests/win32nt/ntuser/NtUserConvertMemHandle.c
+++ modules/rostests/apitests/win32nt/ntuser/NtUserConvertMemHandle.c
@@ -8,15 +8,126 @@
 #include "../win32nt.h"
 
 
+HGLOBAL createGlobalMemory(const CHAR* pString, DWORD stringLength)
+{
+    HGLOBAL hGlobalBuffer = NULL;
+    CHAR* pLockedBuffer = NULL;
+
+    hGlobalBuffer = GlobalAlloc(GMEM_DDESHARE, stringLength);
+    if (hGlobalBuffer == NULL)
+    {
+        return hGlobalBuffer;
+    }
+
+    pLockedBuffer = (CHAR*)GlobalLock(hGlobalBuffer);
+    if (pLockedBuffer)
+    {
+        memcpy(pLockedBuffer, pString, stringLength);
+    }
+    
+    GlobalUnlock(hGlobalBuffer);
+    
+    return hGlobalBuffer;
+}
+
+HANDLE setClipboardData(UINT uFormat, HANDLE hGlobalMem)
+{
+    DWORD dwSize = 0;
+    PVOID pMem = NULL;
+    HANDLE hRet = NULL;
+    HANDLE hMem = NULL;
+    SETCLIPBDATA scd = {FALSE, FALSE};
+   
+    // Get global memory
+    pMem = GlobalLock(hGlobalMem);
+    dwSize = GlobalSize(hGlobalMem);
+ 
+    hMem = NtUserConvertMemHandle(pMem, dwSize);
+    GlobalUnlock(hGlobalMem);
+    if (hMem == NULL)
+    {
+        return hMem;
+    }
+
+    scd.fGlobalHandle = TRUE;
+    hRet = NtUserSetClipboardData(uFormat, hMem, &scd);
+
+    return hRet;
+}
+
+HANDLE getClipboardData(UINT uFormat)
+{
+    HANDLE hData = NULL;
+    HANDLE hGlobal = NULL;
+    PVOID pData = NULL;
+    DWORD cbData = 0;
+    GETCLIPBDATA gcd;
+
+    hData = NtUserGetClipboardData(uFormat, &gcd);
+    if (gcd.fGlobalHandle)
+    {
+        NtUserCreateLocalMemHandle(hData, NULL, 0, &cbData);
+        hGlobal = GlobalAlloc(GMEM_DDESHARE | GMEM_MOVEABLE, cbData);
+
+        if (hGlobal == NULL)
+        {
+            return hGlobal;
+        }
+
+        pData = GlobalLock(hGlobal);
+        NtUserCreateLocalMemHandle(hData, pData, cbData, NULL);
+        GlobalUnlock(hGlobal);
+    }
+
+    return hGlobal;
+}
+
 START_TEST(NtUserConvertMemHandle)
 {
     HANDLE hMem;
-    CHAR data[] = {1, 2, 3, 4};
- 
+    CONST CHAR testString[] = "Test string";
+    HGLOBAL  hGlobal = NULL;
+    
     hMem = NtUserConvertMemHandle((PVOID)(UINT_PTR)0xDEADBEEF, 0xFFFF);
     ok_hdl(hMem, NULL);
 
-    hMem = NtUserConvertMemHandle(data, sizeof(data));
-    ok(hMem != NULL, "hMem is NULL\n");
-    // FIXME: Clean up hMem.
+    // Alloc global memory
+    hGlobal = createGlobalMemory(testString, sizeof(testString));
+    if (hGlobal == NULL)
+    {
+        skip("hGlobal is NULL\n");
+    }
+    else
+    {
+        HANDLE hMem = NULL;
+        HANDLE hRet = NULL;
+
+        OpenClipboard(NULL);
+        hRet = setClipboardData(CF_TEXT, hGlobal);
+        CloseClipboard();
+
+        if (hRet == NULL)
+        {
+            skip("Set clipboard data failed\n");
+            goto cleanup;
+        }
+
+        OpenClipboard(NULL);
+        hMem = getClipboardData(CF_TEXT);
+        CloseClipboard();
+
+        if (hMem)
+        {
+            PVOID pData = GlobalLock(hMem);
+            ok_long(memcmp(pData, testString, sizeof(testString)), 0);
+            GlobalUnlock(hMem);
+        }
+        else
+        {
+            skip("Get clipboard data failed\n");
+        }
+
+        cleanup:
+        GlobalFree(hGlobal);
+    }
 }
