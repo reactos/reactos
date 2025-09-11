3725034620612436ca83de747c00a77d2cabd04e
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-09-11 16:11:57 +0300

    [CRT_APITEST] Fix tests for _sn(w)printf
    - Dynamically load function from the appropriate DLL
    - Fix a test for crtdll

--- modules/rostests/apitests/crt/_snwprintf.c
+++ modules/rostests/apitests/crt/_snwprintf.c
@@ -1,3 +1,6 @@
 #define _UNICODE
+#include <wchar.h>
 #define func__sntprintf func__snwprintf
+typedef int (__cdecl *PFN_sntprintf)(wchar_t *_Dest, size_t _Count, const wchar_t *_Format, ...);
+#define str_sntprintf "_snwprintf"
 #include "_sntprintf.h"
