a56a1d7e188f271403bd602874f25d01421effb5
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2025-09-25 15:56:05 -0500

    Formatting changes

--- sdk/include/psdk/iptypes.h
+++ sdk/include/psdk/iptypes.h
@@ -32,38 +32,38 @@ typedef struct {
 } IP_ADDRESS_STRING, *PIP_ADDRESS_STRING, IP_MASK_STRING, *PIP_MASK_STRING;
 
 typedef struct _IP_ADDR_STRING {
-    struct _IP_ADDR_STRING* Next;
-    IP_ADDRESS_STRING       IpAddress;
-    IP_MASK_STRING          IpMask;
-    DWORD                   Context;
+    struct _IP_ADDR_STRING *Next;
+    IP_ADDRESS_STRING IpAddress;
+    IP_MASK_STRING IpMask;
+    DWORD Context;
 } IP_ADDR_STRING, *PIP_ADDR_STRING;
 
 typedef struct _IP_ADAPTER_INFO {
-    struct _IP_ADAPTER_INFO* Next;
-    DWORD                    ComboIndex;
-    char                     AdapterName[MAX_ADAPTER_NAME_LENGTH + 4];
-    char                     Description[MAX_ADAPTER_DESCRIPTION_LENGTH + 4];
-    UINT                     AddressLength;
-    BYTE                     Address[MAX_ADAPTER_ADDRESS_LENGTH];
-    DWORD                    Index;
-    UINT                     Type;
-    UINT                     DhcpEnabled;
-    PIP_ADDR_STRING          CurrentIpAddress;
-    IP_ADDR_STRING           IpAddressList;
-    IP_ADDR_STRING           GatewayList;
-    IP_ADDR_STRING           DhcpServer;
-    BOOL                     HaveWins;
-    IP_ADDR_STRING           PrimaryWinsServer;
-    IP_ADDR_STRING           SecondaryWinsServer;
-    time_t                   LeaseObtained;
-    time_t                   LeaseExpires;
+    struct _IP_ADAPTER_INFO *Next;
+    DWORD ComboIndex;
+    char AdapterName[MAX_ADAPTER_NAME_LENGTH + 4];
+    char Description[MAX_ADAPTER_DESCRIPTION_LENGTH + 4];
+    UINT AddressLength;
+    BYTE Address[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD Index;
+    UINT Type;
+    UINT DhcpEnabled;
+    PIP_ADDR_STRING CurrentIpAddress;
+    IP_ADDR_STRING IpAddressList;
+    IP_ADDR_STRING GatewayList;
+    IP_ADDR_STRING DhcpServer;
+    BOOL HaveWins;
+    IP_ADDR_STRING PrimaryWinsServer;
+    IP_ADDR_STRING SecondaryWinsServer;
+    time_t LeaseObtained;
+    time_t LeaseExpires;
 } IP_ADAPTER_INFO, *PIP_ADAPTER_INFO;
 
 typedef struct _IP_PER_ADAPTER_INFO_W2KSP1 {
-    UINT            AutoconfigEnabled;
-    UINT            AutoconfigActive;
+    UINT AutoconfigEnabled;
+    UINT AutoconfigActive;
     PIP_ADDR_STRING CurrentDnsServer;
-    IP_ADDR_STRING  DnsServerList;
+    IP_ADDR_STRING DnsServerList;
 } IP_PER_ADAPTER_INFO_W2KSP1, *PIP_PER_ADAPTER_INFO_W2KSP1;
 #if (NTDDI_VERSION >= NTDDI_WIN2KSP1)
 typedef IP_PER_ADAPTER_INFO_W2KSP1 IP_PER_ADAPTER_INFO;
@@ -71,15 +71,15 @@ typedef IP_PER_ADAPTER_INFO_W2KSP1 *PIP_PER_ADAPTER_INFO;
 #endif
 
 typedef struct {
-    char            HostName[MAX_HOSTNAME_LEN + 4] ;
-    char            DomainName[MAX_DOMAIN_NAME_LEN + 4];
+    char HostName[MAX_HOSTNAME_LEN + 4] ;
+    char DomainName[MAX_DOMAIN_NAME_LEN + 4];
     PIP_ADDR_STRING CurrentDnsServer;
-    IP_ADDR_STRING  DnsServerList;
-    UINT            NodeType;
-    char            ScopeId[MAX_SCOPE_ID_LEN + 4];
-    UINT            EnableRouting;
-    UINT            EnableProxy;
-    UINT            EnableDns;
+    IP_ADDR_STRING DnsServerList;
+    UINT NodeType;
+    char ScopeId[MAX_SCOPE_ID_LEN + 4];
+    UINT EnableRouting;
+    UINT EnableProxy;
+    UINT EnableDns;
 } FIXED_INFO_W2KSP1, *PFIXED_INFO_W2KSP1;
 #if (NTDDI_VERSION >= NTDDI_WIN2KSP1)
 typedef FIXED_INFO_W2KSP1 FIXED_INFO;
@@ -106,17 +106,17 @@ typedef NL_DAD_STATE IP_DAD_STATE;
 #define IP_ADAPTER_IPV6_ENABLED               0x0100
 #define IP_ADAPTER_IPV6_MANAGE_ADDRESS_CONFIG 0x0200
 
-#define GAA_FLAG_SKIP_UNICAST                0x0001
-#define GAA_FLAG_SKIP_ANYCAST                0x0002
-#define GAA_FLAG_SKIP_MULTICAST              0x0004
-#define GAA_FLAG_SKIP_DNS_SERVER             0x0008
-#define GAA_FLAG_INCLUDE_PREFIX              0x0010
-#define GAA_FLAG_SKIP_FRIENDLY_NAME          0x0020
-#define GAA_FLAG_INCLUDE_WINS_INFO           0x0040
-#define GAA_FLAG_INCLUDE_GATEWAYS            0x0080
-#define GAA_FLAG_INCLUDE_ALL_INTERFACES      0x0100
-#define GAA_FLAG_INCLUDE_ALL_COMPARTMENTS    0x0200
-#define GAA_FLAG_INCLUDE_TUNNEL_BINDINGORDER 0x0400
+#define GAA_FLAG_SKIP_UNICAST                 0x0001
+#define GAA_FLAG_SKIP_ANYCAST                 0x0002
+#define GAA_FLAG_SKIP_MULTICAST               0x0004
+#define GAA_FLAG_SKIP_DNS_SERVER              0x0008
+#define GAA_FLAG_INCLUDE_PREFIX               0x0010
+#define GAA_FLAG_SKIP_FRIENDLY_NAME           0x0020
+#define GAA_FLAG_INCLUDE_WINS_INFO            0x0040
+#define GAA_FLAG_INCLUDE_GATEWAYS             0x0080
+#define GAA_FLAG_INCLUDE_ALL_INTERFACES       0x0100
+#define GAA_FLAG_INCLUDE_ALL_COMPARTMENTS     0x0200
+#define GAA_FLAG_INCLUDE_TUNNEL_BINDINGORDER  0x0400
 
 typedef struct _IP_ADAPTER_UNICAST_ADDRESS_LH {
     union {
@@ -125,15 +125,15 @@ typedef struct _IP_ADAPTER_UNICAST_ADDRESS_LH {
             DWORD Flags;
         };
     };
-    struct _IP_ADAPTER_UNICAST_ADDRESS_LH   *Next;
-    SOCKET_ADDRESS                          Address;
-    IP_PREFIX_ORIGIN                        PrefixOrigin;
-    IP_SUFFIX_ORIGIN                        SuffixOrigin;
-    IP_DAD_STATE                            DadState;
-    ULONG                                   ValidLifetime;
-    ULONG                                   PreferredLifetime;
-    ULONG                                   LeaseLifetime;
-    UINT8                                   OnLinkPrefixLength;
+    struct _IP_ADAPTER_UNICAST_ADDRESS_LH *Next;
+    SOCKET_ADDRESS Address;
+    IP_PREFIX_ORIGIN PrefixOrigin;
+    IP_SUFFIX_ORIGIN SuffixOrigin;
+    IP_DAD_STATE DadState;
+    ULONG ValidLifetime;
+    ULONG PreferredLifetime;
+    ULONG LeaseLifetime;
+    UINT8 OnLinkPrefixLength;
 } IP_ADAPTER_UNICAST_ADDRESS_LH, *PIP_ADAPTER_UNICAST_ADDRESS_LH;
 
 typedef struct _IP_ADAPTER_UNICAST_ADDRESS_XP {
@@ -143,14 +143,14 @@ typedef struct _IP_ADAPTER_UNICAST_ADDRESS_XP {
             DWORD Flags;
         };
     };
-    struct _IP_ADAPTER_UNICAST_ADDRESS_XP   *Next;
-    SOCKET_ADDRESS                          Address;
-    IP_PREFIX_ORIGIN                        PrefixOrigin;
-    IP_SUFFIX_ORIGIN                        SuffixOrigin;
-    IP_DAD_STATE                            DadState;
-    ULONG                                   ValidLifetime;
-    ULONG                                   PreferredLifetime;
-    ULONG                                   LeaseLifetime;
+    struct _IP_ADAPTER_UNICAST_ADDRESS_XP *Next;
+    SOCKET_ADDRESS Address;
+    IP_PREFIX_ORIGIN PrefixOrigin;
+    IP_SUFFIX_ORIGIN SuffixOrigin;
+    IP_DAD_STATE DadState;
+    ULONG ValidLifetime;
+    ULONG PreferredLifetime;
+    ULONG LeaseLifetime;
 } IP_ADAPTER_UNICAST_ADDRESS_XP, *PIP_ADAPTER_UNICAST_ADDRESS_XP;
 
 #if defined(__REACTOS__) || (NTDDI_VERSION >= NTDDI_VISTA)
@@ -170,7 +170,7 @@ typedef struct _IP_ADAPTER_ANYCAST_ADDRESS_XP {
         };
     };
     struct _IP_ADAPTER_ANYCAST_ADDRESS *Next;
-    SOCKET_ADDRESS                     Address;
+    SOCKET_ADDRESS Address;
 } IP_ADAPTER_ANYCAST_ADDRESS_XP, *PIP_ADAPTER_ANYCAST_ADDRESS_XP;
 #if (NTDDI_VERSION >= NTDDI_WINXP)
 typedef IP_ADAPTER_ANYCAST_ADDRESS_XP IP_ADAPTER_ANYCAST_ADDRESS;
@@ -186,7 +186,7 @@ typedef struct _IP_ADAPTER_MULTICAST_ADDRESS_XP {
         };
     };
     struct _IP_ADAPTER_MULTICAST_ADDRESS *Next;
-    SOCKET_ADDRESS                       Address;
+    SOCKET_ADDRESS Address;
 } IP_ADAPTER_MULTICAST_ADDRESS_XP, *PIP_ADAPTER_MULTICAST_ADDRESS_XP;
 #if (NTDDI_VERSION >= NTDDI_WINXP)
 typedef IP_ADAPTER_MULTICAST_ADDRESS_XP IP_ADAPTER_MULTICAST_ADDRESS;
@@ -218,7 +218,7 @@ typedef struct _IP_ADAPTER_WINS_SERVER_ADDRESS_LH {
         };
     };
     struct _IP_ADAPTER_WINS_SERVER_ADDRESS_LH *Next;
-    SOCKET_ADDRESS                            Address;
+    SOCKET_ADDRESS Address;
 } IP_ADAPTER_WINS_SERVER_ADDRESS_LH, *PIP_ADAPTER_WINS_SERVER_ADDRESS_LH;
 #if (NTDDI_VERSION >= NTDDI_VISTA)
 typedef IP_ADAPTER_WINS_SERVER_ADDRESS_LH IP_ADAPTER_WINS_SERVER_ADDRESS;
@@ -250,8 +250,8 @@ typedef struct _IP_ADAPTER_PREFIX_XP {
         };
     };
     struct _IP_ADAPTER_PREFIX_XP *Next;
-    SOCKET_ADDRESS               Address;
-    ULONG                        PrefixLength;
+    SOCKET_ADDRESS Address;
+    ULONG PrefixLength;
 } IP_ADAPTER_PREFIX_XP, *PIP_ADAPTER_PREFIX_XP;
 #if (NTDDI_VERSION >= NTDDI_WINXP)
 typedef IP_ADAPTER_PREFIX_XP IP_ADAPTER_PREFIX;
@@ -271,56 +271,56 @@ typedef struct _IP_ADAPTER_ADDRESSES_LH {
             DWORD IfIndex;
         };
     };
-    struct _IP_ADAPTER_ADDRESSES_LH    *Next;
-    PCHAR                              AdapterName;
-    PIP_ADAPTER_UNICAST_ADDRESS_LH     FirstUnicastAddress;
-    PIP_ADAPTER_ANYCAST_ADDRESS_XP     FirstAnycastAddress;
-    PIP_ADAPTER_MULTICAST_ADDRESS_XP   FirstMulticastAddress;
-    PIP_ADAPTER_DNS_SERVER_ADDRESS_XP  FirstDnsServerAddress;
-    PWCHAR                             DnsSuffix;
-    PWCHAR                             Description;
-    PWCHAR                             FriendlyName;
-    BYTE                               PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
-    DWORD                              PhysicalAddressLength;
+    struct _IP_ADAPTER_ADDRESSES_LH *Next;
+    PCHAR AdapterName;
+    PIP_ADAPTER_UNICAST_ADDRESS_LH FirstUnicastAddress;
+    PIP_ADAPTER_ANYCAST_ADDRESS_XP FirstAnycastAddress;
+    PIP_ADAPTER_MULTICAST_ADDRESS_XP FirstMulticastAddress;
+    PIP_ADAPTER_DNS_SERVER_ADDRESS_XP FirstDnsServerAddress;
+    PWCHAR DnsSuffix;
+    PWCHAR Description;
+    PWCHAR FriendlyName;
+    BYTE PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD PhysicalAddressLength;
     union {
-        DWORD                          Flags;
+        DWORD Flags;
         struct {
-            DWORD                      DdnsEnabled : 1;
-            DWORD                      RegisterAdapterSuffix : 1;
-            DWORD                      Dhcpv4Enabled : 1;
-            DWORD                      ReceiveOnly : 1;
-            DWORD                      NoMulticast : 1;
-            DWORD                      Ipv6OtherStatefulConfig : 1;
-            DWORD                      NetbiosOverTcpipEnabled : 1;
-            DWORD                      Ipv4Enabled : 1;
-            DWORD                      Ipv6Enabled : 1;
-            DWORD                      Ipv6ManagedAddressConfigurationSupported : 1;
+            DWORD DdnsEnabled : 1;
+            DWORD RegisterAdapterSuffix : 1;
+            DWORD Dhcpv4Enabled : 1;
+            DWORD ReceiveOnly : 1;
+            DWORD NoMulticast : 1;
+            DWORD Ipv6OtherStatefulConfig : 1;
+            DWORD NetbiosOverTcpipEnabled : 1;
+            DWORD Ipv4Enabled : 1;
+            DWORD Ipv6Enabled : 1;
+            DWORD Ipv6ManagedAddressConfigurationSupported : 1;
         };
     };
-    DWORD                              Mtu;
-    DWORD                              IfType;
-    IF_OPER_STATUS                     OperStatus;
-    DWORD                              Ipv6IfIndex;
-    DWORD                              ZoneIndices[16];
-    PIP_ADAPTER_PREFIX_XP              FirstPrefix;
-    ULONG64                            TransmitLinkSpeed;
-    ULONG64                            ReceiveLinkSpeed;
+    DWORD Mtu;
+    DWORD IfType;
+    IF_OPER_STATUS OperStatus;
+    DWORD Ipv6IfIndex;
+    DWORD ZoneIndices[16];
+    PIP_ADAPTER_PREFIX_XP FirstPrefix;
+    ULONG64 TransmitLinkSpeed;
+    ULONG64 ReceiveLinkSpeed;
     PIP_ADAPTER_WINS_SERVER_ADDRESS_LH FirstWinsServerAddress;
-    PIP_ADAPTER_GATEWAY_ADDRESS_LH     FirstGatewayAddress;
-    ULONG                              Ipv4Metric;
-    ULONG                              Ipv6Metric;
-    IF_LUID                            Luid;
-    SOCKET_ADDRESS                     Dhcpv4Server;
-    NET_IF_COMPARTMENT_ID              CompartmentId;
-    NET_IF_NETWORK_GUID                NetworkGuid;
-    NET_IF_CONNECTION_TYPE             ConnectionType;
-    TUNNEL_TYPE                        TunnelType;
-    SOCKET_ADDRESS                     Dhcpv6Server;
-    BYTE                               Dhcpv6ClientDuid[MAX_DHCPV6_DUID_LENGTH];
-    ULONG                              Dhcpv6ClientDuidLength;
-    ULONG                              Dhcpv6Iaid;
+    PIP_ADAPTER_GATEWAY_ADDRESS_LH FirstGatewayAddress;
+    ULONG Ipv4Metric;
+    ULONG Ipv6Metric;
+    IF_LUID Luid;
+    SOCKET_ADDRESS Dhcpv4Server;
+    NET_IF_COMPARTMENT_ID CompartmentId;
+    NET_IF_NETWORK_GUID NetworkGuid;
+    NET_IF_CONNECTION_TYPE ConnectionType;
+    TUNNEL_TYPE TunnelType;
+    SOCKET_ADDRESS Dhcpv6Server;
+    BYTE Dhcpv6ClientDuid[MAX_DHCPV6_DUID_LENGTH];
+    ULONG Dhcpv6ClientDuidLength;
+    ULONG Dhcpv6Iaid;
 #if (NTDDI_VERSION >= NTDDI_VISTASP1)
-    PIP_ADAPTER_DNS_SUFFIX             FirstDnsSuffix;
+    PIP_ADAPTER_DNS_SUFFIX FirstDnsSuffix;
 #endif
 } IP_ADAPTER_ADDRESSES_LH, *PIP_ADAPTER_ADDRESSES_LH;
 
@@ -333,23 +333,23 @@ typedef struct _IP_ADAPTER_ADDRESSES_XP {
         };
     };
     struct _IP_ADAPTER_ADDRESSES_XP *Next;
-    PCHAR                           AdapterName;
-    PIP_ADAPTER_UNICAST_ADDRESS     FirstUnicastAddress;
-    PIP_ADAPTER_ANYCAST_ADDRESS     FirstAnycastAddress;
-    PIP_ADAPTER_MULTICAST_ADDRESS   FirstMulticastAddress;
-    PIP_ADAPTER_DNS_SERVER_ADDRESS  FirstDnsServerAddress;
-    PWCHAR                          DnsSuffix;
-    PWCHAR                          Description;
-    PWCHAR                          FriendlyName;
-    BYTE                            PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
-    DWORD                           PhysicalAddressLength;
-    DWORD                           Flags;
-    DWORD                           Mtu;
-    DWORD                           IfType;
-    IF_OPER_STATUS                  OperStatus;
-    DWORD                           Ipv6IfIndex;
-    DWORD                           ZoneIndices[16];
-    PIP_ADAPTER_PREFIX              FirstPrefix;
+    PCHAR AdapterName;
+    PIP_ADAPTER_UNICAST_ADDRESS FirstUnicastAddress;
+    PIP_ADAPTER_ANYCAST_ADDRESS FirstAnycastAddress;
+    PIP_ADAPTER_MULTICAST_ADDRESS FirstMulticastAddress;
+    PIP_ADAPTER_DNS_SERVER_ADDRESS FirstDnsServerAddress;
+    PWCHAR DnsSuffix;
+    PWCHAR Description;
+    PWCHAR FriendlyName;
+    BYTE PhysicalAddress[MAX_ADAPTER_ADDRESS_LENGTH];
+    DWORD PhysicalAddressLength;
+    DWORD Flags;
+    DWORD Mtu;
+    DWORD IfType;
+    IF_OPER_STATUS OperStatus;
+    DWORD Ipv6IfIndex;
+    DWORD ZoneIndices[16];
+    PIP_ADAPTER_PREFIX FirstPrefix;
 } IP_ADAPTER_ADDRESSES_XP, *PIP_ADAPTER_ADDRESSES_XP;
 
 #if (NTDDI_VERSION >= NTDDI_VISTA)
@@ -370,8 +370,8 @@ typedef struct ip_interface_name_info_w2ksp1 {
     ULONG MediaType;
     UCHAR ConnectionType;
     UCHAR AccessType;
-    GUID  DeviceGuid;
-    GUID  InterfaceGuid;
+    GUID DeviceGuid;
+    GUID InterfaceGuid;
 } IP_INTERFACE_NAME_INFO_W2KSP1, *PIP_INTERFACE_NAME_INFO_W2KSP1;
 #if (NTDDI_VERSION >= NTDDI_WIN2KSP1)
 typedef IP_INTERFACE_NAME_INFO_W2KSP1 IP_INTERFACE_NAME_INFO;
