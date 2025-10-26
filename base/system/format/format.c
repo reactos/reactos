d1b1292429f90750d10fd1566ddae612ac17fa71
Serge Gautherie <32623169+SergeGautherie@users.noreply.github.com>
2025-10-26 16:42:01 +0100

    [FMIFS][FORMAT][SDK] Improve/Fix QueryDeviceInformation() and related code (#7475)
    - FMIFS: QueryDeviceInformation(): Use more specific types and improve documentation.
      Follow-up to commit 4838d7bd56.
    
    - FORMAT: wmain():
      * Zero the correct variable. Addendum to commit c5a9f22d4e.
      * No need to zero the whole volumeName array. Follow-up to commit 358fecdcf0.

--- base/system/format/format.c
+++ base/system/format/format.c
@@ -352,11 +352,11 @@ static VOID Usage(LPWSTR ProgramName)
 int wmain(int argc, WCHAR *argv[])
 {
     int badArg;
-    DEVICE_INFORMATION DeviceInformation = {0};
+    DEVICE_INFORMATION DeviceInformation;
     FMIFS_MEDIA_FLAG media = FMIFS_HARDDISK;
     DWORD driveType;
     WCHAR fileSystem[1024];
-    WCHAR volumeName[1024] = {0};
+    WCHAR volumeName[1024];
     WCHAR input[1024];
     DWORD serialNumber;
     ULARGE_INTEGER totalNumberOfBytes, totalNumberOfFreeBytes;
@@ -465,6 +465,8 @@ int wmain(int argc, WCHAR *argv[])
         dwError = GetLastError();
         if (dwError == ERROR_UNRECOGNIZED_VOLUME)
         {
+            // Unformatted volume
+            volumeName[0] = UNICODE_NULL;
             wcscpy(fileSystem, L"RAW");
         }
         else
@@ -477,9 +479,11 @@ int wmain(int argc, WCHAR *argv[])
 
     ConResPrintf(StdOut, STRING_FILESYSTEM, fileSystem);
 
-    if (QueryDeviceInformation(RootDirectory,
-                               &DeviceInformation,
-                               sizeof(DeviceInformation)))
+    if (!QueryDeviceInformation(RootDirectory, &DeviceInformation, sizeof(DeviceInformation)))
+    {
+        totalNumberOfBytes.QuadPart = 0;
+    }
+    else
     {
         totalNumberOfBytes.QuadPart = DeviceInformation.SectorSize *
                                       DeviceInformation.SectorCount.QuadPart;
