69a26e085dccc68325b1659900792ed7e0c10a79
菜叶片(ItsSunshineXD) <61444298+CNMrSunshine@users.noreply.github.com>
2025-10-26 21:59:14 +0800

    Refactor session path handling for NULL checks

--- dll/win32/kernel32/client/session.c
+++ dll/win32/kernel32/client/session.c
@@ -37,44 +37,54 @@ DosPathToSessionPathW(IN DWORD SessionID,
                       OUT LPWSTR *OutPath)
 {
     DWORD LastError = 0;
-    if ((InPath == 0x0) || IsBadReadPtr(InPath, 2), IsBadWritePtr(OutPath, 8)) {
+    if (InPath == NULL)
+    {
         LastError = ERROR_INVALID_PARAMETER;
     }
-    else {
+    else
+    {
         size_t InPathLength = wcslen(InPath);
         size_t OutPathLength = 0;
         LPWSTR SessionPath = 0;
         if (BaseStaticServerData->LUIDDeviceMapsEnabled == FALSE) {
-            if (SessionID == 0) {
+            if (SessionID == 0)
+            {
                 OutPathLength += wcslen(L"GLOBALROOT\\DosDevices\\");
                 OutPathLength += InPathLength;
             }
-            else {
+            else
+            {
                 OutPathLength += wcslen(L"GLOBALROOT\\Sessions\\");
                 OutPathLength += 10; // Length of SessionID(char)
                 OutPathLength += wcslen(L"\\DosDevices\\");
                 OutPathLength += InPathLength;
             }
         }
-        else {
+        else
+        {
             OutPathLength += InPathLength;
         }
         OutPathLength += 1; // \0 termination
         SessionPath = (LPWSTR)LocalAlloc(LMEM_FIXED, OutPathLength * sizeof(WCHAR));
-        if (SessionPath != 0) {
-            if (BaseStaticServerData->LUIDDeviceMapsEnabled == TRUE) {
-                swprintf_s(SessionPath, OutPathLength, L"%ws", InPath);
+        if (SessionPath != 0)
+        {
+            if (BaseStaticServerData->LUIDDeviceMapsEnabled == TRUE)
+            {
+                RtlStringCchPrintfW(SessionPath, OutPathLength, L"%ws", InPath);
             }
-            else if (SessionID == 0) {  // GLOBALROOT\DosDevices\$InPath
-                swprintf_s(SessionPath, OutPathLength, L"GLOBALROOT\\DosDevices\\%ws", InPath);
+            else if (SessionID == 0)
+            {  // GLOBALROOT\DosDevices\$InPath
+                RtlStringCchPrintfW(SessionPath, OutPathLength, L"GLOBALROOT\\DosDevices\\%ws", InPath);
             }
-            else { // GLOBALROOT\Sessions\$SessionID\DosDevices\$InPath
-                swprintf_s(SessionPath, OutPathLength, L"GLOBALROOT\\Sessions\\%u\\DosDevices\\%ws", SessionID, InPath);
+            else
+            { // GLOBALROOT\Sessions\$SessionID\DosDevices\$InPath
+                RtlStringCchPrintfW(SessionPath, OutPathLength, L"GLOBALROOT\\Sessions\\%u\\DosDevices\\%ws", SessionID, InPath);
             }
             *OutPath = SessionPath;
             return 1;
         }
-        else {
+        else
+        {
             LastError = ERROR_NOT_ENOUGH_MEMORY;
         }
     }
@@ -92,27 +102,32 @@ DosPathToSessionPathA(IN DWORD SessionID,
                       OUT LPSTR *OutPath)
 {
     DWORD LastError = 0;
-    if ((InPath == 0) || IsBadReadPtr(InPath, 2) || IsBadWritePtr(OutPath, 8)) {
+    if (InPath == NULL)
+    {
         LastError = ERROR_INVALID_PARAMETER;
     }
-    else { // AnsiPath: InPath(ANSI) -> UnicodePath: InPath(Unicode) -> wcOutPath: OutPath(wide-char) -> UnicodePath: OutPath(Unicode) -> AnsiPath: OutPath(ANSI)
+    else
+    { // AnsiPath: InPath(ANSI) -> UnicodePath: InPath(Unicode) -> wcOutPath: OutPath(wide-char) -> UnicodePath: OutPath(Unicode) -> AnsiPath: OutPath(ANSI)
         STRING AnsiPath = { 0 };
         RtlInitAnsiString(&AnsiPath, InPath);
         UNICODE_STRING UnicodePath;
         NTSTATUS status = RtlAnsiStringToUnicodeString(&UnicodePath, &AnsiPath, TRUE); // UnicodePath = InPath(Unicode)
-        if (status < 0) {
+        if (status < 0)
+        {
             BaseSetLastNTError(status);
             return 0;
         }
-        LPWSTR wcOutPath = 0; // OutPath(wide-char)
-        if (!DosPathToSessionPathW(SessionID, UnicodePath.Buffer, &wcOutPath)) {
+        LPWSTR wcOutPath = NULL; // OutPath(wide-char)
+        if (!DosPathToSessionPathW(SessionID, UnicodePath.Buffer, &wcOutPath))
+        {
             RtlFreeUnicodeString(&UnicodePath);
             return 0;
         }
         RtlFreeUnicodeString(&UnicodePath);
         RtlInitUnicodeString(&UnicodePath, wcOutPath); // UnicodePath = OutPath(Unicode)
         status = RtlUnicodeStringToAnsiString(&AnsiPath, &UnicodePath, TRUE);
-        if (status < 0) {
+        if (status < 0)
+        {
             BaseSetLastNTError(status);
             LocalFree(wcOutPath);
             return 0;
@@ -120,14 +135,16 @@ DosPathToSessionPathA(IN DWORD SessionID,
         size_t AnsiOutPathLen = strlen(AnsiPath.Buffer);
         AnsiOutPathLen += 1;
         char *SessionPath = LocalAlloc(LMEM_FIXED, AnsiOutPathLen * sizeof(CHAR));
-        if (SessionPath != 0) {
-            strcpy_s(SessionPath, AnsiOutPathLen, AnsiPath.Buffer);
+        if (SessionPath != 0)
+        {
+            RtlStringCchCopyA(SessionPath, AnsiOutPathLen, AnsiPath.Buffer);
             *OutPath = SessionPath;
             LocalFree(wcOutPath);
             RtlFreeAnsiString(&AnsiPath);
             return 1;
         }
-        else {
+        else
+        {
             LocalFree(wcOutPath);
             RtlFreeAnsiString(&AnsiPath);
             LastError = ERROR_NOT_ENOUGH_MEMORY;
