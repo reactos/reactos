2cd16778c63c35f37b9e42d42d0a6d795aa8e5fd
菜叶片(ItsSunshineXD) <61444298+CNMrSunshine@users.noreply.github.com>
2025-10-27 10:10:17 +0800

    Fixed coding style issue

--- dll/win32/kernel32/client/session.c
+++ dll/win32/kernel32/client/session.c
@@ -5,8 +5,6 @@
  * PURPOSE:         Session Support APIs
  * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
  *                  Jiayu Ge (yepian@stu2025.jnu.edu.cn)
- * UPDATE HISTORY:
- *                  Updated 10/26/2025
  */
 
 /* INCLUDES *******************************************************************/
@@ -15,15 +13,6 @@
 
 #define NDEBUG
 #include <debug.h>
-
-/* GLOBALS ********************************************************************/
-
-PBASE_STATIC_SERVER_DATA BaseStaticServerData;
-
-#define LMEM_FIXED 0x0
-
-#define ERROR_INVALID_PARAMETER 0x57
-#define ERROR_NOT_ENOUGH_MEMORY 0x8
 
 /* FUNCTIONS ******************************************************************/
 
@@ -36,7 +25,7 @@ DosPathToSessionPathW(IN DWORD SessionID,
                       IN LPWSTR InPath,
                       OUT LPWSTR *OutPath)
 {
-    DWORD LastError = 0;
+    DWORD LastError = ERROR_SUCCESS;
     if (InPath == NULL)
     {
         LastError = ERROR_INVALID_PARAMETER;
@@ -45,8 +34,9 @@ DosPathToSessionPathW(IN DWORD SessionID,
     {
         size_t InPathLength = wcslen(InPath);
         size_t OutPathLength = 0;
-        LPWSTR SessionPath = 0;
-        if (BaseStaticServerData->LUIDDeviceMapsEnabled == FALSE) {
+        LPWSTR SessionPath = NULL;
+        if (!BaseStaticServerData->LUIDDeviceMapsEnabled)
+        {
             if (SessionID == 0)
             {
                 OutPathLength += wcslen(L"GLOBALROOT\\DosDevices\\");
@@ -66,18 +56,20 @@ DosPathToSessionPathW(IN DWORD SessionID,
         }
         OutPathLength += 1; // \0 termination
         SessionPath = (LPWSTR)LocalAlloc(LMEM_FIXED, OutPathLength * sizeof(WCHAR));
-        if (SessionPath != 0)
+        if (!SessionPath)
         {
-            if (BaseStaticServerData->LUIDDeviceMapsEnabled == TRUE)
+            if (BaseStaticServerData->LUIDDeviceMapsEnabled)
             {
                 RtlStringCchPrintfW(SessionPath, OutPathLength, L"%ws", InPath);
             }
             else if (SessionID == 0)
-            {  // GLOBALROOT\DosDevices\$InPath
+            { 
+                // GLOBALROOT\DosDevices\$InPath
                 RtlStringCchPrintfW(SessionPath, OutPathLength, L"GLOBALROOT\\DosDevices\\%ws", InPath);
             }
             else
-            { // GLOBALROOT\Sessions\$SessionID\DosDevices\$InPath
+            {
+                // GLOBALROOT\Sessions\$SessionID\DosDevices\$InPath
                 RtlStringCchPrintfW(SessionPath, OutPathLength, L"GLOBALROOT\\Sessions\\%u\\DosDevices\\%ws", SessionID, InPath);
             }
             *OutPath = SessionPath;
@@ -101,7 +93,7 @@ DosPathToSessionPathA(IN DWORD SessionID,
                       IN LPSTR InPath,
                       OUT LPSTR *OutPath)
 {
-    DWORD LastError = 0;
+    DWORD LastError = ERROR_SUCCESS;
     if (InPath == NULL)
     {
         LastError = ERROR_INVALID_PARAMETER;
@@ -112,7 +104,7 @@ DosPathToSessionPathA(IN DWORD SessionID,
         RtlInitAnsiString(&AnsiPath, InPath);
         UNICODE_STRING UnicodePath;
         NTSTATUS status = RtlAnsiStringToUnicodeString(&UnicodePath, &AnsiPath, TRUE); // UnicodePath = InPath(Unicode)
-        if (status < 0)
+        if (!NT_SUCCESS(status))
         {
             BaseSetLastNTError(status);
             return 0;
@@ -126,7 +118,7 @@ DosPathToSessionPathA(IN DWORD SessionID,
         RtlFreeUnicodeString(&UnicodePath);
         RtlInitUnicodeString(&UnicodePath, wcOutPath); // UnicodePath = OutPath(Unicode)
         status = RtlUnicodeStringToAnsiString(&AnsiPath, &UnicodePath, TRUE);
-        if (status < 0)
+        if (!NT_SUCCESS(status))
         {
             BaseSetLastNTError(status);
             LocalFree(wcOutPath);
@@ -135,7 +127,7 @@ DosPathToSessionPathA(IN DWORD SessionID,
         size_t AnsiOutPathLen = strlen(AnsiPath.Buffer);
         AnsiOutPathLen += 1;
         char *SessionPath = LocalAlloc(LMEM_FIXED, AnsiOutPathLen * sizeof(CHAR));
-        if (SessionPath != 0)
+        if (SessionPath != NULL)
         {
             RtlStringCchCopyA(SessionPath, AnsiOutPathLen, AnsiPath.Buffer);
             *OutPath = SessionPath;
