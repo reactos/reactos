426152a713d9ef00e0f5fd70ca7dbbd825923a65
Johannes Thoma <johannes@johannesthoma.com>
2025-11-07 11:55:47 +0000

    [NETIO] Changed license from GPL to MIT
    The netio.c and related files are now licensed under the
    (more permissive) MIT license. This applies to the files
    in the drivers/network/netio directory (which are written
    solely by me). It does NOT apply to the files in
    drivers/network/tdihelpers since they are written by
    somebody else.
    
    Have fun with it :)

--- drivers/network/netio/netio.c
+++ drivers/network/netio/netio.c
@@ -1,11 +1,21 @@
 /*
  * PROJECT:     NetIO driver
- * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
  * PURPOSE:     NT6 compatible NETIO.SYS driver: A BSD sockets like kernel
  *              internal interface for networking (TCP/IP, UDP/IP).
  * COPYRIGHT:   Copyright 2023-2025 Johannes Khoshnazar-Thoma
  *              <johannes@johannesthoma.com>
  */
+
+/*
+Copyright © 2025 Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 /*
  * [NETIO] NETIO.SYS driver
