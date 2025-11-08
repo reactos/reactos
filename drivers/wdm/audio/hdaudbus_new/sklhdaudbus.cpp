5520794a4c080d64cffe253f79abc3f0aa1e7a2f
Oleg Dubinskiy <oleg.dubinskiy@reactos.org>
2025-11-08 18:42:00 +0100

    [HDAUDBUS_NEW] Import new SkyLake/KabyLake/ApolloLake HD audio bus driver from https://github.com/coolstar/sklhdaudbus/tree/master (BSD-3-Clause License) [DOC] 3rd Party Files.txt: Add 'HD Audio Driver for Windows 10 / 11 with Skylake -> Raptor Lake DSP support' CORE-15350, CORE-18776

---
+++ drivers/wdm/audio/hdaudbus_new/sklhdaudbus.cpp
@@ -0,0 +1,60 @@
+#include "driver.h"
+
+NTSTATUS
+NTAPI
+SklHdAudBusEvtDeviceAdd(
+	_In_ WDFDRIVER Driver,
+	_Inout_ PWDFDEVICE_INIT DeviceInit
+)
+{
+	UNREFERENCED_PARAMETER(Driver);
+	NTSTATUS status;
+
+	//
+	// Initialize all the properties specific to the device.
+	// Framework has default values for the one that are not
+	// set explicitly here. So please read the doc and make sure
+	// you are okay with the defaults.
+	//
+	WdfDeviceInitSetDeviceType(DeviceInit, FILE_DEVICE_BUS_EXTENDER);
+
+	status = Fdo_Create(DeviceInit);
+	return status;
+}
+
+extern "C" NTSTATUS
+NTAPI
+DriverEntry(
+__in PDRIVER_OBJECT  DriverObject,
+__in PUNICODE_STRING RegistryPath
+)
+{
+	WDF_DRIVER_CONFIG config;
+	NTSTATUS status;
+	WDFDRIVER wdfDriver;
+
+	SklHdAudBusPrint(DEBUG_LEVEL_INFO, DBG_INIT,
+		"Driver Entry\n");
+
+	//
+	//  Default to NonPagedPoolNx for non paged pool allocations where supported.
+	//
+
+	ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
+
+	WDF_DRIVER_CONFIG_INIT(&config, SklHdAudBusEvtDeviceAdd);
+
+	status = WdfDriverCreate(DriverObject,
+		RegistryPath,
+		WDF_NO_OBJECT_ATTRIBUTES,
+		&config,
+		&wdfDriver
+	);
+	if (!NT_SUCCESS(status))
+	{
+		SklHdAudBusPrint(DEBUG_LEVEL_ERROR, DBG_INIT,
+			"WdfDriverCreate failed %x\n", status);
+	}
+
+	return status;
+}
