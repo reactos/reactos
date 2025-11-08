7c2d923007b6717bbb00338829e53acbfcfa2fcb
Oleg Dubinskiy <oleg.dubinskiy@reactos.org>
2025-11-08 18:42:00 +0100

    [HDAUDBUS_NEW] Import new SkyLake/KabyLake/ApolloLake HD audio bus driver from https://github.com/coolstar/sklhdaudbus/tree/master (BSD-3-Clause License) [DOC] 3rd Party Files.txt: Add 'HD Audio Driver for Windows 10 / 11 with Skylake -> Raptor Lake DSP support' CORE-15350, CORE-18776

---
+++ drivers/wdm/audio/hdaudbus_new/hdac_stream.h
@@ -0,0 +1,6 @@
+UINT16 hdac_format(PHDAC_STREAM stream);
+
+void hdac_stream_start(PHDAC_STREAM stream);
+void hdac_stream_stop(PHDAC_STREAM stream);
+void hdac_stream_reset(PHDAC_STREAM stream);
+void hdac_stream_setup(PHDAC_STREAM stream);
