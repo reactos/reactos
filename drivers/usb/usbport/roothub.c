6e2f3dcbb8516d579e2038cd225bf796ef5a7640
Justin Miller <justin.miller@reactos.org>
2025-11-08 12:47:55 -0800

    [USBPORT] CORE-17052 Handle port status better and CreateDevice MaxPacketSize

--- drivers/usb/usbport/roothub.c
+++ drivers/usb/usbport/roothub.c
@@ -190,6 +190,13 @@ USBPORT_RootHubClassCommand(IN PDEVICE_OBJECT FdoDevice,
                 return RHStatus;
             }
 
+            if (Port == 0 ||
+                Port > PdoExtension->RootHubDescriptors->Descriptor.bNumberOfPorts)
+            {
+                DPRINT1("USBPORT_RootHubClassCommand: CLEAR_FEATURE invalid port %u\n", Port);
+                return RHStatus;
+            }
+
             switch (Feature)
             {
                 case FEATURE_PORT_ENABLE:
@@ -248,6 +255,13 @@ USBPORT_RootHubClassCommand(IN PDEVICE_OBJECT FdoDevice,
             }
 
             Feature = SetupPacket->wValue.W;
+
+            if (Port == 0 ||
+                Port > PdoExtension->RootHubDescriptors->Descriptor.bNumberOfPorts)
+            {
+                DPRINT1("USBPORT_RootHubClassCommand: SET_FEATURE invalid port %u\n", Port);
+                return RHStatus;
+            }
 
             switch (Feature)
             {
