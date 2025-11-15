ed78af53011919c79dbceb11f01396d31668d80c
Mikhail Tyukin <mishakeys20@gmail.com>
2025-11-15 11:14:12 -0500

    [OLE32] Sync to wine-10.0

--- modules/rostests/winetests/ole32/testlib.c
+++ modules/rostests/winetests/ole32/testlib.c
@@ -1,5 +1,7 @@
 /*
- * Copyright 2007 Google (Dan Hipschman)
+ * Test library
+ *
+ * Copyright 2019 Nikolay Sivov
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,13 +18,24 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#pragma makedep proxy
-#pragma makedep register
+#if 0
+#pragma makedep testdll
+#endif
 
-#include "oleidl.idl"
+#include <stdio.h>
+#include <windows.h>
+#include <initguid.h>
 
-[
-    threading(both),
-    uuid(00000320-0000-0000-c000-000000000046)
-]
-coclass PSFactoryBuffer { interface IPSFactoryBuffer; }
+DEFINE_GUID(CLSID_WineOOPTest, 0x5201163f, 0x8164, 0x4fd0, 0xa1, 0xa2, 0x5d, 0x5a, 0x36, 0x54, 0xd3, 0xbd);
+DEFINE_GUID(IID_Testiface7, 0x82222222, 0x1234, 0x1234, 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0);
+
+HRESULT WINAPI DllGetClassObject(REFCLSID clsid, REFIID riid, void **obj)
+{
+    if (IsEqualGUID(clsid, &CLSID_WineOOPTest))
+        return 0x80001234;
+
+    if (IsEqualGUID(clsid, &IID_Testiface7))
+        return 0x80001235;
+
+    return E_NOTIMPL;
+}
