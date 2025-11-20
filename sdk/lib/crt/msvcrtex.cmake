eba2bbd177e64b072d2a9ed01db4819db36fb546
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-11-20 16:57:33 +0200

    [CRT] Remove ftol2_sse

--- sdk/lib/crt/msvcrtex.cmake
+++ sdk/lib/crt/msvcrtex.cmake
@@ -93,15 +93,6 @@ add_asm_files(msvcrtex_asm ${MSVCRTEX_ASM_SOURCE})
 add_library(msvcrtex OBJECT ${MSVCRTEX_SOURCE} ${msvcrtex_asm})
 target_compile_definitions(msvcrtex PRIVATE _DLL _MSVCRTEX_)
 
-if(MSVC AND (ARCH STREQUAL "i386"))
-    # user32.dll needs this as a stand-alone object file
-    add_asm_files(ftol2_asm math/i386/ftol2_asm.s)
-    add_library(ftol2_sse OBJECT ${ftol2_asm})
-    target_compile_definitions(ftol2_sse PRIVATE $<TARGET_PROPERTY:msvcrtex,COMPILE_DEFINITIONS>)
-    set_target_properties(ftol2_sse PROPERTIES LINKER_LANGUAGE C)
-endif()
-
-
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "Clang")
     target_compile_options(msvcrtex PRIVATE $<$<COMPILE_LANGUAGE:C>:-Wno-main>)
     if(LTCG)
