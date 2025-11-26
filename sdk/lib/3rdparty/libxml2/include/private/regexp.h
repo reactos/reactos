eee19b9865d1519a53e2bdfee74cb5f612c0dd15
Timo Kreuzer <timo.kreuzer@reactos.org>
2025-11-26 14:34:00 +0200

    [LIBXML2] Update to release 2.12.8 (from Wine-10.0)

---
+++ sdk/lib/3rdparty/libxml2/include/private/regexp.h
@@ -0,0 +1,9 @@
+#ifndef XML_REGEXP_H_PRIVATE__
+#define XML_REGEXP_H_PRIVATE__
+
+#include <libxml/xmlautomata.h>
+
+XML_HIDDEN void
+xmlAutomataSetFlags(xmlAutomataPtr am, int flags);
+
+#endif /* XML_REGEXP_H_PRIVATE__ */
