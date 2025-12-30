fefd7b328bc9492bd7fefdc2145b6ae17594a332
MuerteSeguraZ <anneud.pedemonte.t@gmail.com>
2025-12-30 10:30:41 +0100

    remove non-related changes

--- sdk/cmake/gcc.cmake
+++ sdk/cmake/gcc.cmake
@@ -176,6 +176,7 @@ add_compile_options(-Wall -Wpointer-arith)
 
 # Disable some overzealous warnings
 add_compile_options(
+    -Wno-unknown-warning-option
     -Wno-char-subscripts
     -Wno-multichar
     -Wno-unused-value
