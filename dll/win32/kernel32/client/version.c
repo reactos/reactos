1bb77189590e45bced2c2966272ed665940981be
Justin Miller <justin.miller@reactos.org>
2026-01-02 03:02:33 -0800

    [KERNEL32] Split off some ANSI functions from the bulk of Kernel32

--- dll/win32/kernel32/client/version.c
+++ dll/win32/kernel32/client/version.c
@@ -152,28 +152,3 @@ VerifyVersionInfoW(IN LPOSVERSIONINFOEXW lpVersionInformation,
             return TRUE;
     }
 }
-
-/*
- * @implemented
- */
-BOOL
-WINAPI
-VerifyVersionInfoA(IN LPOSVERSIONINFOEXA lpVersionInformation,
-                   IN DWORD dwTypeMask,
-                   IN DWORDLONG dwlConditionMask)
-{
-    OSVERSIONINFOEXW viex;
-
-    /* NOTE: szCSDVersion is ignored, we don't need to convert it to Unicode */
-    viex.dwOSVersionInfoSize = sizeof(viex);
-    viex.dwMajorVersion = lpVersionInformation->dwMajorVersion;
-    viex.dwMinorVersion = lpVersionInformation->dwMinorVersion;
-    viex.dwBuildNumber = lpVersionInformation->dwBuildNumber;
-    viex.dwPlatformId = lpVersionInformation->dwPlatformId;
-    viex.wServicePackMajor = lpVersionInformation->wServicePackMajor;
-    viex.wServicePackMinor = lpVersionInformation->wServicePackMinor;
-    viex.wSuiteMask = lpVersionInformation->wSuiteMask;
-    viex.wProductType = lpVersionInformation->wProductType;
-    viex.wReserved = lpVersionInformation->wReserved;
-    return VerifyVersionInfoW(&viex, dwTypeMask, dwlConditionMask);
-}
