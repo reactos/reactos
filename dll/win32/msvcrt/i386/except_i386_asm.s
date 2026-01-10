ee5698115552a96887b7306e9c1ac0b9d0fcb943
Timo Kreuzer <timo.kreuzer@reactos.org>
2026-01-10 22:02:33 +0200

    [MSVCRT] Build improvements for asm code
    - Move call_copy_ctor and call_dtor asm functions into exception_ptr.s
    - The code in exception_ptr.c isn't needed, only the asm functions, so remove the file from build and instead compile exception_ptr.s for both GCC and MSVC builds

--- dll/win32/msvcrt/i386/except_i386_asm.s
+++ dll/win32/msvcrt/i386/except_i386_asm.s
@@ -99,26 +99,4 @@ __EH_prolog:
     //.cfi_adjust_cfa_offset 4
     ret
 
-PUBLIC _call_copy_ctor
-_call_copy_ctor:
-    push ebp
-    //.cfi_adjust_cfa_offset 4
-    //.cfi_rel_offset ebp, 0
-    mov ebp, esp
-    //.cfi_def_cfa_register ebp
-    push 1
-    mov ecx, [ebp + 12]
-    push dword ptr [ebp + 16]
-    call dword ptr [ebp + 8]
-    //.cfi_def_cfa esp, 4
-    //.cfi_same_value ebp
-    leave
-    ret
-
-PUBLIC _call_dtor
-_call_dtor:
-    mov ecx, [esp + 8]
-    call dword ptr [esp + 4]
-    ret
-
 END
