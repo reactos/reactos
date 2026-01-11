4af32dfb0b6f1e2dc6d9174fc8dfd4aea78b9907
Ahmed ARIF <arif.ing@outlook.com>
2026-01-11 01:04:41 +0100

    [ROSSYM] Add 64-bit compatible PeSect structure and ULONG_PTR types

--- sdk/lib/rossym_new/pe.h
+++ sdk/lib/rossym_new/pe.h
@@ -4,29 +4,39 @@
 #include "compat.h"
 
 struct DwarfBlock;
-typedef struct _IMAGE_SECTION_HEADER PeSect;
+
+/*
+ * Extended section header structure for 64-bit compatibility.
+ * On 32-bit, ANSI_STRING fits in 8 bytes, but on 64-bit it needs 16 bytes
+ * due to pointer alignment. We use a separate field for the name string.
+ */
+typedef struct _PeSect {
+    struct _IMAGE_SECTION_HEADER hdr;
+    ANSI_STRING name_str;
+} PeSect;
 
 typedef struct _Pe {
 	void *fd;
 	u16int (*e2)(const unsigned char *data);
 	u32int (*e4)(const unsigned char *data);
 	u64int (*e8)(const unsigned char *data);
-	ulong imagebase, imagesize, loadbase;
-    ulong codestart, datastart;
+	ULONG_PTR imagebase, imagesize, loadbase;
+	ULONG_PTR codestart, datastart;
 	int (*loadsection)(struct _Pe *pe, char *name, struct DwarfBlock *b);
 	int nsections;
-	struct _IMAGE_SECTION_HEADER *sect;
+	PeSect *sect;
 } Pe;
 
 Pe *peopen(const char *name);
 int loaddisksection(struct _Pe *pe, char *name, struct DwarfBlock *b);
+int loadmemsection(struct _Pe *pe, char *name, struct DwarfBlock *b);
 u16int peget2(const unsigned char *ptr);
 u32int peget4(const unsigned char *ptr);
 u64int peget8(const unsigned char *ptr);
 void pefree(struct _Pe *pe);
-ulong pefindrva(struct _IMAGE_SECTION_HEADER *SectionHeader, int NumberOfSections, ulong TargetPhysical);
+ulong pefindrva(PeSect *SectionHeaders, int NumberOfSections, ulong TargetPhysical);
 int GetStrnlen(const char *string, int maxlen);
 
-#define ANSI_NAME_STRING(s) ((PANSI_STRING)((s)->Name))
+#define ANSI_NAME_STRING(s) (&(s)->name_str)
 
 #endif/*_LIBMACH_PE_H_*/
