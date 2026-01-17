ba1e474a98f22937ee95da502748eae0897ac614
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-01-17 23:21:19 +0900

    [SHELL32] Translating PIDLs: Check NULL and fix PIDL flags update (#8603)
    #8569 introduced an exception bug.
    JIRA issue: CORE-20441
    - In CreateNotificationParamAndSend
      function, check NULL for PIDLs.
    - In SHELL32_ReparentAsAliasPidl
      function, fix updating PIDL flags.

--- dll/win32/shell32/changenotify.cpp
+++ dll/win32/shell32/changenotify.cpp
@@ -342,8 +342,8 @@ CreateNotificationParamAndSend(LONG wEventId, UINT uFlags, LPCITEMIDLIST pidl1, 
         SHELL32_AliasTranslatePidl(pidl2, &pidl2Alias, ALIAS_ANY);
 
     HANDLE hTicket2 = NULL;
-    if ((pidl1Alias || pidl2Alias) &&
-        (!ILIsEqual(pidl1, pidl1Alias) || !ILIsEqual(pidl2, pidl2Alias)))
+    if (((pidl1 && pidl1Alias && !ILIsEqual(pidl1, pidl1Alias)) ||
+         (pidl2 && pidl2Alias && !ILIsEqual(pidl2, pidl2Alias))))
     {
         hTicket2 = CreateNotificationParam(wEventId, uFlags, pidl1Alias, pidl2Alias,
                                            pid, dwTick);
