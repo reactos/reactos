242651d005f569684091fa4408f7fd5e4b880647
Timo Kreuzer <timo.kreuzer@reactos.org>
2026-01-31 12:24:52 +0200

    [SHELL32] Reduce diff to wine (indentation)
    Fixes GCC 13 warnings about misleading indentation.
    
    C:/ReactOS/reactos/dll/win32/shell32/wine/classes.c: In function 'HCR_GetIconA':
    C:/ReactOS/reactos/dll/win32/shell32/wine/classes.c:357:5: error: this 'else' clause does not guard... [-Werror=misleading-indentation]
      357 |     else
          |     ^~~~
    C:/ReactOS/reactos/dll/win32/shell32/wine/classes.c:360:9: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'else'
      360 |         return ret;
          |         ^~~~~~
    
    C:/ReactOS/reactos/dll/win32/shell32/wine/shellord.c: In function 'SHCreateStdEnumFmtEtc':
    C:/ReactOS/reactos/dll/win32/shell32/wine/shellord.c:2455:5: error: this 'if' clause does not guard... [-Werror=misleading-indentation]
     2455 |     if (FAILED(hRes))
          |     ^~
    In file included from C:/ReactOS/reactos/sdk/include/psdk/objbase.h:252,
                     from C:/ReactOS/reactos/sdk/include/psdk/ole2.h:25,
                     from C:/ReactOS/reactos/sdk/include/psdk/shlobj.h:22,
                     from C:/ReactOS/reactos/dll/win32/shell32/wine/shellord.c:32:
    C:/ReactOS/build-gcc-x86/sdk/include/psdk/objidl.h:9734:37: note: ...this statement, but the latter is misleadingly indented as if it were guarded by the 'if'
     9734 | #define IEnumFORMATETC_AddRef(This) (This)->lpVtbl->AddRef(This)
          |                                     ^
    C:/ReactOS/reactos/dll/win32/shell32/wine/shellord.c:2458:9: note: in expansion of macro 'IEnumFORMATETC_AddRef'
     2458 |         IEnumFORMATETC_AddRef(pef);
          |         ^~~~~~~~~~~~~~~~~~~~~

--- dll/win32/shell32/wine/shellord.c
+++ dll/win32/shell32/wine/shellord.c
@@ -2443,7 +2443,7 @@ HRESULT WINAPI CIDLData_CreateFromIDArray(
  *
  */
 HRESULT WINAPI SHCreateStdEnumFmtEtc(
-    UINT cFormats,
+	UINT cFormats,
 	const FORMATETC *lpFormats,
 	LPENUMFORMATETC *ppenumFormatetc)
 {
@@ -2451,9 +2451,9 @@ HRESULT WINAPI SHCreateStdEnumFmtEtc(
 	HRESULT hRes;
 	TRACE("cf=%d fe=%p pef=%p\n", cFormats, lpFormats, ppenumFormatetc);
 
-    hRes = IEnumFORMATETC_Constructor(cFormats, lpFormats, &pef);
-    if (FAILED(hRes))
-        return hRes;
+	hRes = IEnumFORMATETC_Constructor(cFormats, lpFormats, &pef);
+	if (FAILED(hRes))
+	    return hRes;
 
 	IEnumFORMATETC_AddRef(pef);
 	hRes = IEnumFORMATETC_QueryInterface(pef, &IID_IEnumFORMATETC, (LPVOID*)ppenumFormatetc);
