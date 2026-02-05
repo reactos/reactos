86990c38939a82ee267a8918fe8780c1e0b3405f
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2026-02-05 21:39:39 +0100

    [DDK][VIDEOPRT] VideoPortDebugPrint(): Fix the debug filter being used. Update SAL annotations.
    - Use the `DPFLTR_VIDEO_ID` debug filter ID, which is the same as what
      Windows' videoprt.sys uses, so that tools and people who debug can
      easily interoperate between the two.
      Addendum to commits 11c95f7b5d (r19801) and 5b799176ba (r31477).
    
    - Use SAL2 annotations.

--- win32ss/drivers/videoprt/videoprt.c
+++ win32ss/drivers/videoprt/videoprt.c
@@ -1144,10 +1144,11 @@ VideoPortInitialize(
  * @implemented
  */
 VOID
+__cdecl
 VideoPortDebugPrint(
-    IN VIDEO_DEBUG_LEVEL DebugPrintLevel,
-    IN PCHAR DebugMessage,
-    ...)
+    _In_ VIDEO_DEBUG_LEVEL DebugPrintLevel,
+    _In_ PSTR DebugMessage,
+    _In_ ...)
 {
     va_list ap;
 
@@ -1155,7 +1156,7 @@ VideoPortDebugPrint(
         DebugPrintLevel = Error;
 
     va_start(ap, DebugMessage);
-    vDbgPrintEx(DPFLTR_IHVVIDEO_ID, DebugPrintLevel, DebugMessage, ap);
+    vDbgPrintEx(DPFLTR_VIDEO_ID, DebugPrintLevel, DebugMessage, ap);
     va_end(ap);
 }
 
