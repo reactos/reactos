88d64b2e520ac32293e3da05f6cf28a7ea5efc63
Doug Lyons <douglyons@douglyons.com>
2026-04-13 16:24:46 -0500

    [CHKDSK][VFATLIB] Chkdsk improve code CORE-20546 Avoid second lseek if we do not have "use_read" and need to do read alignment.

--- sdk/lib/fslib/vfatlib/check/io.c
+++ sdk/lib/fslib/vfatlib/check/io.c
@@ -372,10 +372,10 @@ void fs_write(off_t pos, int size, void *data)
 
             /* Patch data in memory */
             memcpy((char *)scratch + seek_delta, data, size);
-        }
 
-        /* Seek back to the beginning of our read/write */ 
-        if (lseek(fd, seekpos_aligned, 0) != seekpos_aligned) pdie("Seek to %lld",seekpos_aligned);
+            /* Seek back to the beginning of our read */ 
+            if (lseek(fd, seekpos_aligned, 0) != seekpos_aligned) pdie("Seek to %lld",seekpos_aligned);
+        }
 
         /* Write it back */
         if ((did = write(fd, scratch, readsize_aligned)) == (int)readsize_aligned)
