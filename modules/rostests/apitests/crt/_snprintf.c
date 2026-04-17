5f51f2469725077308d517ecf84b6633c636b552
Timo Kreuzer <timo.kreuzer@reactos.org>
2026-04-17 18:05:36 +0300

    [CRT_APITEST] Improve some tests
    This is needed to automatically derive the test name from START_TEST.

--- modules/rostests/apitests/crt/_snprintf.c
+++ modules/rostests/apitests/crt/_snprintf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#define func__sntprintf func__snprintf
 typedef int (__cdecl *PFN_sntprintf)(char *_Dest, size_t _Count, const char *_Format, ...);
 #define str_sntprintf "_snprintf"
 #include "_sntprintf.h"
+
+START_TEST(_snprintf)
+{
+    Test__sntprintf();
+}
