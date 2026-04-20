f894e7325b8d7147e71d799d8324a6e4900b1b39
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-04-20 21:31:27 +0900

    s/wine_debugstr_a/wine_dbgstr_a/

--- dll/win32/shell32/wine/shellord.c
+++ dll/win32/shell32/wine/shellord.c
@@ -2512,7 +2512,7 @@ void WINAPI SHUpdateImageW(LPCWSTR pszHashItem, int iIndex, UINT uFlags, int iIm
 VOID WINAPI SHUpdateImageA(LPCSTR pszHashItem, INT iIndex, UINT uFlags, INT iImageIndex)
 {
 #ifdef __REACTOS__
-    TRACE("(%s, %d, 0x%x, %d)\n", wine_debugstr_a(pszHashItem), iIndex, uFlags, iImageIndex);
+    TRACE("(%s, %d, 0x%x, %d)\n", wine_dbgstr_a(pszHashItem), iIndex, uFlags, iImageIndex);
     WCHAR szHashItem[MAX_PATH];
     SHAnsiToUnicode(pszHashItem, szHashItem, _countof(szHashItem));
     SHUpdateImageW(szHashItem, iIndex, uFlags, iImageIndex);
