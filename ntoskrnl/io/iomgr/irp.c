56b3cf4ccaa68411bb164becbf0126241a0caf04
YASAKCI-ZIHNIYET-TURKISH <hasanelk101@gmail.com>
2026-04-22 23:01:47 +0300

    [NTOS:IO] Fix potential use-after-free in IoCancelThreadIo
    "Currently, IoCancelThreadIo traverses the IrpList by fetching the next entry after calling IoCancelIrp. This is unsafe because IoCancelIrp may cause the current IRP to be completed and freed, leading to a use-after-free or list corruption when accessing NextEntry->Flink. This PR fixes it by saving the next pointer before initiating the cancellation."

--- ntoskrnl/io/iomgr/irp.c
+++ ntoskrnl/io/iomgr/irp.c
@@ -1165,18 +1165,21 @@ IoCancelThreadIo(IN PETHREAD Thread)
 
     /* Start by cancelling all the IRPs in the current thread queue. */
     ListHead = &Thread->IrpList;
-    NextEntry = ListHead->Flink;
-    while (ListHead != NextEntry)
-    {
-        /* Get the IRP */
-        Irp = CONTAINING_RECORD(NextEntry, IRP, ThreadListEntry);
+NextEntry = ListHead->Flink;
+while (ListHead != NextEntry)
+{
+    /* Save the next entry before cancelling the current IRP */
+    PLIST_ENTRY TemporaryNext = NextEntry->Flink;
 
-        /* Cancel it */
-        IoCancelIrp(Irp);
+    /* Get the IRP */
+    Irp = CONTAINING_RECORD(NextEntry, IRP, ThreadListEntry);
 
-        /* Move to the next entry */
-        NextEntry = NextEntry->Flink;
-    }
+    /* Cancel it */
+    IoCancelIrp(Irp);
+
+    /* Move to the saved next entry */
+    NextEntry = TemporaryNext;
+}
 
      /* Wait 100 milliseconds */
     Interval.QuadPart = -1000000;
