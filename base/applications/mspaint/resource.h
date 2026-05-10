36884217a3195dd152b9b9283ba48d3aa3eaba1e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-10 18:03:27 +0900

    fix IDS_ values

--- base/applications/mspaint/resource.h
+++ base/applications/mspaint/resource.h
@@ -229,7 +229,7 @@
 #define IDS_SAVEERROR   941
 #define IDS_CANTSENDMAIL 942
 #define IDS_LOSECOLOR   943
-#define IDS_MONOBMP     344
-#define IDS_4BPPBMP     345
-#define IDS_8BPPBMP     346
-#define IDS_24BPPBMP    347
+#define IDS_MONOBMP     944
+#define IDS_4BPPBMP     945
+#define IDS_8BPPBMP     946
+#define IDS_24BPPBMP    947
