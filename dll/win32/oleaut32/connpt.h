459b4b097bea5ea16af2241e77a714dc2411ef91
Amine Khaldi <amine.khaldi@reactos.org>
2026-05-12 20:07:13 +0100

    [OLEAUT32] Sync with Wine 10.0. CORE-20595

--- dll/win32/oleaut32/connpt.h
+++ dll/win32/oleaut32/connpt.h
@@ -19,6 +19,6 @@
 #ifndef _CONNPT_H
 #define _CONNPT_H
 
-HRESULT CreateConnectionPoint(IUnknown *pUnk, REFIID riid, IConnectionPoint **pCP) DECLSPEC_HIDDEN;
+HRESULT CreateConnectionPoint(IUnknown *pUnk, REFIID riid, IConnectionPoint **pCP);
 
 #endif /* _CONNPT_H */
