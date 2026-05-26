cc7609a83610ad35a527e713a1c0a86be8802039
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-26 17:58:10 +0900

    copyright years

--- base/applications/mspaint/palettemodel.cpp
+++ base/applications/mspaint/palettemodel.cpp
@@ -3,7 +3,7 @@
  * LICENSE:    LGPL-2.0-or-later (https://spdx.org/licenses/LGPL-2.0-or-later)
  * PURPOSE:    Keep track of palette data, notify listeners
  * COPYRIGHT:  Copyright 2015 Benedikt Freisen <b.freisen@gmx.net>
- *             Copyright 2021 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
+ *             Copyright 2021-2026 Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
  */
 
 #include "precomp.h"
