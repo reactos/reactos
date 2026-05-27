3f5e3a558b5745b52a3ebe43344ac2bd841eae06
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2026-05-27 14:00:13 +0200

    [WIN32SS:NTUSER] IntGetDesktopObjectHandle(): use a non-zero DesiredAccess in ObOpenObjectByPointer()
    Use `DESKTOP_ALL_ACCESS` for the DesiredAccess, and revert to using
    `IntGetDesktopObjectHandle()` in `DesktopWindowProc()` for the
    `WM_WINDOWPOSCHANGING` case.
    
    Addendum to commit bcde8100ea (r9315) that added the call but without
    specifying any DesiredAccess. As a result the call failed and commit
    60448f83fb switched to using `UserOpenInputDesktop()` in `DesktopWindowProc()`.
    This wasn't necessary.

--- win32ss/user/ntuser/desktop.c
+++ win32ss/user/ntuser/desktop.c
@@ -1336,7 +1336,7 @@ IntGetDesktopObjectHandle(PDESKTOP DesktopObject)
         Status = ObOpenObjectByPointer(DesktopObject,
                                        0,
                                        NULL,
-                                       0,
+                                       DESKTOP_ALL_ACCESS,
                                        ExDesktopObjectType,
                                        UserMode,
                                        (PHANDLE)&hDesk);
@@ -1551,7 +1551,7 @@ DesktopWindowProc(PWND Wnd, UINT Msg, WPARAM wParam, LPARAM lParam, LRESULT *lRe
             PWINDOWPOS pWindowPos = (PWINDOWPOS)lParam;
             if ((pWindowPos->flags & SWP_SHOWWINDOW) != 0)
             {
-                HDESK hdesk = UserOpenInputDesktop(0, FALSE, DESKTOP_ALL_ACCESS);
+                HDESK hdesk = IntGetDesktopObjectHandle(gpdeskInputDesktop);
                 IntSetThreadDesktop(hdesk, FALSE);
             }
             break;
