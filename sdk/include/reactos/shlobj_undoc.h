43951afccb8434d3fc3ea6cd9f0b257baa721b11
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-05-30 13:35:42 +0900

    use ACCESS_MASK

--- sdk/include/reactos/shlobj_undoc.h
+++ sdk/include/reactos/shlobj_undoc.h
@@ -1236,7 +1236,7 @@ DECLARE_INTERFACE_(IObjectWithRegistryKey, IUnknown) // {D960050C-F4E1-4294-AC4B
     STDMETHOD_(ULONG,Release)(THIS) PURE;
     /*** IObjectWithRegistryKey ***/
     STDMETHOD(SetKey)(THIS_ HKEY hKey) PURE;
-    STDMETHOD(GetKey)(THIS_ REGSAM samDesired, HKEY *phKey) PURE;
+    STDMETHOD(GetKey)(THIS_ ACCESS_MASK samDesired, HKEY *phKey) PURE;
 };
 #undef INTERFACE
 
