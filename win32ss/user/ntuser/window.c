44cd0c3284865fd097de247101ccc04caa09f0e9
Mikhail Tyukin <mishakeys20@gmail.com>
2026-06-12 19:30:39 -0400

    [WIN32SS:USER] Fix ScrollBar class creation when using comctl32 v6, mute useless errors
    For some reason, both Windows and Wine define ScrollBar class to be forwarded. Wine does not re-register the ScrollBar class, and Windows does not appear to do so either. Previously, ReactOS relied on a hack to register the ScrollBar class: https://github.com/reactos/reactos/blob/eecfbaeaa0bdef86f0cecff9828871be276ccd6e/dll/win32/comctl32/theming.c#L164. Wine uses a is_builtin_class function to determine if the class is implemented server side rather than user32/comctl32, so use the same function.
    
    Wine switched to registering all of its comctl32 classes in RegisterClassNameW rather than its dllmain. See https://github.com/wine-mirror/wine/commit/05421efde8f3e2dc6ffb1883ac1fa282839224e1.
    Remove errors when the class could not be located in the first pass, which is now the default behavior, which results in user32!VersionRegisterClass being called, which allows CreateWindow to succeed. I also confirmed that Windows's comctl32 registers its classes when RegisterClassNameW is called by user32, not at startup using a debugger.

--- win32ss/user/ntuser/window.c
+++ win32ss/user/ntuser/window.c
@@ -2206,7 +2206,7 @@ co_UserCreateWindowEx(CREATESTRUCTW* Cs,
    if(!Class)
    {
        EngSetLastError(ERROR_CANNOT_FIND_WND_CLASS);
-       ERR("Failed to find class %wZ\n", ClassName);
+       TRACE("Failed to find class %wZ\n", ClassName);
        goto cleanup;
    }
 
