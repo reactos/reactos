86930812f6616e4d094eeb4a1fb5e8140f09baca
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-06-13 20:45:53 +0900

    __REACTOS__

--- dll/win32/msvcrt/string.c
+++ dll/win32/msvcrt/string.c
@@ -38,8 +38,10 @@ WINE_DEFAULT_DEBUG_CHANNEL(msvcrt);
 
 #ifdef _MSC_VER
 #pragma function(_strset,memchr,memcmp,memcpy,memset,strcat,strcmp,strcpy,strlen)
+#ifdef __REACTOS__
 #if defined(_M_ARM) || _MSC_VER >= 1932 // VS2022 version 17.2 and later
 #pragma function(strncpy,strncmp)
+#endif
 #endif
 #endif
 
