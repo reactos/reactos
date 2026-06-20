7cfd651c9e5483ac42bcf7de0c746590fe1f69c9
Stanislav Motylkov <x86corez@gmail.com>
2026-06-20 23:52:06 +0300

    Partial revert
    Co-authored-by: Stanislav Motylkov <x86corez@gmail.com>

--- base/applications/mspaint/main.cpp
+++ base/applications/mspaint/main.cpp
@@ -500,6 +500,9 @@ LRESULT CMainWindow::OnMouseWheel(UINT nMsg, WPARAM wParam, LPARAM lParam, BOOL&
         UINT nCount = 3;
         if (::GetAsyncKeyState(VK_SHIFT) < 0)
         {
+#ifndef SPI_GETWHEELSCROLLCHARS
+    #define SPI_GETWHEELSCROLLCHARS 0x006C  // Needed for pre-NT6 PSDK
+#endif
             SystemParametersInfoW(SPI_GETWHEELSCROLLCHARS, 0, &nCount, 0);
             for (UINT i = 0; i < nCount; ++i)
             {
