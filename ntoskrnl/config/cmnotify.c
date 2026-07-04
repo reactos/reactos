78e9b3882c1744f8934159d3a88aba34c2f7f3b3
Mohammad Amin Mollazadeh <madamin@pm.me>
2026-07-04 18:59:34 +0330

    [NTOS:CM] Implement CmpReportNotify
    This is an internal undocumented function which can be found in
    official debugging symbols released by Microsoft for NTOSKRNL.
    As the name and usage suggests, this function needs to report
    changes made in the registry to the listeners.
    
    A new internal CMP_POST_BLOCK data structure is also added that
    represent a change listeners. The only official mentions of this
    data structure is inside CM_NOTIFY_BLOCK as a list entry for PostList
    field and it doesn't even exist in public debugging symbols.
    Reverse-engineering NtNotifyChangeMultipleKeys implementation from
    original NTOSKRNL suggests Windows uses this data structure to save
    a reference to the KEVENT object used to signal the change listener.
    
    CORE-11865

--- ntoskrnl/config/cmnotify.c
+++ ntoskrnl/config/cmnotify.c
@@ -12,8 +12,144 @@
 #define NDEBUG
 #include "debug.h"
 
+/* PRIVATE FUNCTIONS *********************************************************/
+
+/**
+ * @brief Report a change in registry to a listener represented by the post block
+ * 
+ * This function signals the listener that a change has occurred in the registry,
+ * then releases the resources used by the @p PostBlock as the notification session
+ * has finished.
+ * This is an utility function used by CmpReportNotify to report changes to each
+ * CMP_POST_BLOCK attached to a CM_NOTIFY_BLOCK.
+ * 
+ * @param[in] PostBlock The change notification receiver
+ */
+VOID
+NTAPI
+CmpReportNotifyToPostBlock(_In_ PCMP_POST_BLOCK PostBlock)
+{
+    PCMP_POST_BLOCK SecondaryPostBlock;
+
+    /* Report the operation result using caller-allocated IO_STATUS_BLOCK buffer  */
+    if (PostBlock->IoStatusBlock)
+    {
+        /* Write to an IO_STATUS_BLOCK allocated by an user-mode caller */
+        if (PostBlock->OwnerProcess != NULL)
+        {
+            /* Attach to caller's context if necessary */
+            KAPC_STATE ApcState;
+            BOOLEAN IsSameProcess = PostBlock->OwnerProcess == &PsGetCurrentProcess()->Pcb;
+            if (!IsSameProcess)
+                KeStackAttachProcess(PostBlock->OwnerProcess, &ApcState);
+
+            /* Try to write status to buffer */
+            _SEH2_TRY
+            {
+                PostBlock->IoStatusBlock->Status = STATUS_NOTIFY_ENUM_DIR;
+                PostBlock->IoStatusBlock->Information = 0;
+            }
+            _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER)
+            {
+                NTSTATUS Status = _SEH2_GetExceptionCode();
+                DPRINT1("CmpReportNotifyToPostBlock: Writing to IO_STATUS_BLOCK failed with %lx. Process=0x%lx, IoStatusBlock=0x%lx\n", Status, PostBlock->OwnerProcess, PostBlock->IoStatusBlock);
+            }
+            _SEH2_END;
+
+            if (!IsSameProcess)
+                KeUnstackDetachProcess(&ApcState);
+        }
+        else
+        {
+            /* PostBlock->OwnerProcess should be set to NULL for kernel-mode callers */
+            PostBlock->IoStatusBlock->Status = STATUS_NOTIFY_ENUM_DIR;
+            PostBlock->IoStatusBlock->Information = 0;
+        }
+    }
+
+    /* Signal the event object provided by the user-mode or kernel-mode callers */
+    if (PostBlock->Event)
+    {
+        KeSetEvent(PostBlock->Event, 1, FALSE);
+        ObDereferenceObject(PostBlock->Event);
+        PostBlock->Event = NULL;
+    }
+
+    /* Queue the WorkQueueItem for asynchronous kernel-mode callers */
+    if (PostBlock->WorkQueueItem)
+    {
+        ExQueueWorkItem(PostBlock->WorkQueueItem, PostBlock->WorkQueueType);
+    }
+    else if (PostBlock->UserApc)
+    {
+        /* FIXME: TODO */
+    }
+
+    /* Detach the PostBlock from its NotifyBlock as the wait is complete */
+    RemoveEntryList(&(PostBlock->NotifyList));
+    if (PostBlock->SecondaryBlock)
+    {
+        /* Also detach the PostBlock for the Subordinate key if one was provided */
+        SecondaryPostBlock = CONTAINING_RECORD(PostBlock->SecondaryBlock->PostList.Flink, CMP_POST_BLOCK, NotifyList);
+        RemoveEntryList(&SecondaryPostBlock->NotifyList);
+        /* Free the memory allocated for the SecondaryPostBlock as we don't need it anymore */
+        ExFreePoolWithTag(SecondaryPostBlock, TAG_CM_NOTIFY);
+        ExFreePoolWithTag(PostBlock->SecondaryBlock, TAG_CM_NOTIFY);
+    }
+    ExFreePoolWithTag(PostBlock, TAG_CM_NOTIFY);
+}
+
+/**
+ * @brief Check if @p SubKey is part of @p ParentKey's subtree
+ * 
+ * This is an utility function used by CmpReportNotify to check
+ * if a notification should be send or not when WatchTree flag is set.
+ * 
+ * @param[in] ParentKey The parent key to check against
+ * 
+ * @param[in] SubKey The target key
+ * 
+ * @return TRUE if @p SubKey is a subkey of @p ParentKey, FALSE otherwise
+ */
+BOOLEAN
+NTAPI
+CmpIsKcbSubKey(_In_ PCM_KEY_CONTROL_BLOCK ParentKey,
+               _In_ PCM_KEY_CONTROL_BLOCK SubKey)
+{
+    PCM_KEY_CONTROL_BLOCK SubKeyParent;
+    ULONG Depth = SubKey->TotalLevels - ParentKey->TotalLevels;
+
+    if (Depth <= 0)
+        return FALSE;
+
+    SubKeyParent = SubKey->ParentKcb;
+    while (SubKeyParent != NULL && SubKeyParent != ParentKey && Depth >= 0)
+    {
+        SubKeyParent = SubKeyParent->ParentKcb;
+        Depth--;
+    }
+
+    return SubKeyParent == ParentKey;
+}
+
 /* FUNCTIONS *****************************************************************/
 
+/**
+ * @brief Report a change in a registry key
+ * 
+ * This function is called by CM functions whenever a change occurs in a registry key.
+ * It iterates through all attached CM_NOTIFY_BLOCKs to find one that matches the change
+ * then reports the change to all attached CMP_POST_BLOCKs.
+ * 
+ * @param[in] Kcb The changed registry key
+ * 
+ * @param[in] Hive The registry hive containing the changed key
+ * 
+ * @param[in] Cell Unused
+ * 
+ * @param[in] Filter The type of change, can be REG_NOTIFY_CHANGE_NAME, REG_NOTIFY_CHANGE_ATTRIBUTES, REG_NOTIFY_CHANGE_LAST_SET, or REG_NOTIFY_CHANGE_SECURITY
+ * 
+ */
 VOID
 NTAPI
 CmpReportNotify(IN PCM_KEY_CONTROL_BLOCK Kcb,
@@ -21,8 +157,60 @@ CmpReportNotify(IN PCM_KEY_CONTROL_BLOCK Kcb,
                 IN HCELL_INDEX Cell,
                 IN ULONG Filter)
 {
-    /* FIXME: TODO */
-    return;
+    PCMHIVE KeyHive;
+    PLIST_ENTRY ListHead, NextEntry;
+    PLIST_ENTRY PostListHead, PostNextEntry;
+    PCM_NOTIFY_BLOCK NotifyBlock;
+    PCMP_POST_BLOCK PostBlock;
+
+    KeyHive = CONTAINING_RECORD(Hive, CMHIVE, Hive);
+    ListHead = &(KeyHive->NotifyList);
+    if (IsListEmpty(ListHead)) return;
+
+    NextEntry = ListHead->Flink;
+    while (NextEntry != ListHead)
+    {
+        NotifyBlock = CONTAINING_RECORD(NextEntry, CM_NOTIFY_BLOCK, HiveList);
+        NextEntry = NextEntry->Flink;
+
+        /* Check if this KCB matches to this NotifyBlock */
+        if (NotifyBlock->KeyControlBlock != Kcb && 
+            !(NotifyBlock->WatchTree && CmpIsKcbSubKey(NotifyBlock->KeyControlBlock, Kcb)))
+            continue;
+
+        /* Check the notification filter */
+        if ((NotifyBlock->Filter & Filter) != Filter) 
+            continue;
+
+        PostListHead = &(NotifyBlock->PostList);
+        if (IsListEmpty(PostListHead))
+        {
+            /* A notification block is allocated but no one is watching, so this is a pending notification */
+            NotifyBlock->NotifyPending = TRUE;
+            continue;
+        }
+        PostNextEntry = PostListHead->Flink;
+        while (PostNextEntry != PostListHead)
+        {
+            PostBlock = CONTAINING_RECORD(PostNextEntry, CMP_POST_BLOCK, NotifyList);
+            PostNextEntry = PostNextEntry->Flink;
+
+            if (PostBlock->IsPrimary)
+            {
+                CmpReportNotifyToPostBlock(PostBlock);
+            }
+            else
+            {
+                /* Prevent system from freeing Primary PostBlock's NotifyBlock by locking its KCB */
+                PCM_KEY_CONTROL_BLOCK PrimaryKcb = PostBlock->PrimaryNotifyBlock->KeyControlBlock;
+                CmpAcquireKcbLockShared(PrimaryKcb);
+
+                CmpReportNotifyToPostBlock(PostBlock->PrimaryPostBlock);
+
+                CmpReleaseKcbLock(PrimaryKcb);
+            }
+        }
+    }
 }
 
 VOID
