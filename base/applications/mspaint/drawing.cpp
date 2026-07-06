ad3ea1cb1201c9230736a5981a956ff63588d365
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2026-07-06 23:52:37 +0900

    Update base/applications/mspaint/drawing.cpp
    Co-authored-by: Hermès BÉLUSCA - MAÏTO <hermes.belusca-maito@reactos.org>

--- base/applications/mspaint/drawing.cpp
+++ base/applications/mspaint/drawing.cpp
@@ -470,7 +470,7 @@ Brush(HDC hdc, LONG x1, LONG y1, LONG x2, LONG y2, HBRUSH hBrush, LONG style, IN
         return;
     }
 
-    HGDIOBJ oldPen = SelectObject(hdc, CreatePen(PS_NULL, 0, 0)); // 1px off
+    HGDIOBJ oldPen = SelectObject(hdc, GetStockObject(NULL_PEN)); // 1px off
     HGDIOBJ oldBrush = SelectObject(hdc, hBrush);
     BrushInternal(hdc, x1, y1, x2, y2, style, thickness);
     SelectObject(hdc, oldBrush);
