3900c807931e24acff3d73fa9596d6acf1759dd4
Mohammad Amin Mollazadeh <madamin@pm.me>
2026-07-11 23:49:26 +0330

    [ROSTESTS:CONTROLS] Add listbox page

--- modules/rostests/win32/user32/controls/pages/listbox.c
+++ modules/rostests/win32/user32/controls/pages/listbox.c
@@ -7,33 +7,92 @@
 
 #include "../controls.h"
 
-/*
- * +------------------------------------------------+
- * | Single Selection                              |
- * | +--------------------------------------------+ |
- * | | Item 1                                     | |
- * | | Item 2                                     | |
- * | | Item 3                                     | |
- * | +--------------------------------------------+ |
- * |                                                |
- * | Multiple Selection                            |
- * | +--------------------------------------------+ |
- * | | [ ] Item A                                 | |
- * | | [x] Item B                                 | |
- * | | [x] Item C                                 | |
- * | +--------------------------------------------+ |
- * |                                                |
- * | Extended Selection                            |
- * | +--------------------------------------------+ |
- * | | Item 1                                     | |
- * | | Item 2                                     | |
- * | | Item 3                                     | |
- * | +--------------------------------------------+ |
- * +------------------------------------------------+
- */
+typedef struct _LISTBOX_PAGE_DATA
+{
+    HWND hLblSingle;
+    HWND hSingle;
+
+    HWND hLblMultiple;
+    HWND hMultiple;
+
+    HWND hLblExtended;
+    HWND hExtended;
+} LISTBOX_PAGE_DATA, *PLISTBOX_PAGE_DATA;
+
+static PCWSTR Items[] = {
+    L"Cherry",
+    L"Date",
+    L"Grape",
+    L"Apple",
+    L"Banana",
+    L"Fig",
+    L"Elderberry",
+};
+
+static VOID
+PopulateListBox(HWND hCombo, BOOL bSelectFirst)
+{
+    for (int i = 0; i < _countof(Items); i++)
+    {
+        SendMessageW(hCombo, LB_ADDSTRING, 0, (LPARAM)Items[i]);
+    }
+
+    if (bSelectFirst)
+        SendMessageW(hCombo, LB_SETCURSEL, 0, 0);
+}
+
+
+LRESULT
+InitPage(HWND Parent, PLISTBOX_PAGE_DATA PageData)
+{
+    PageData->hLblSingle =
+        CreateChild(0, L"STATIC", L"Single ListBox", SS_LEFT, 20, 10, 200, 15, Parent);
+    PageData->hSingle =
+        CreateChild(0, L"LISTBOX", NULL, LBS_STANDARD, 20, 30, 200, 100, Parent);
+    PopulateListBox(PageData->hSingle, TRUE);
+
+    PageData->hLblMultiple =
+        CreateChild(0, L"STATIC", L"Multiple Selection ListBox", SS_LEFT, 20, 150, 200, 15, Parent);
+    PageData->hMultiple =
+        CreateChild(0, L"LISTBOX", NULL, LBS_STANDARD | LBS_MULTIPLESEL, 20, 170, 200, 100, Parent);
+    PopulateListBox(PageData->hMultiple, FALSE);
+
+    PageData->hLblExtended =
+        CreateChild(0, L"STATIC", L"Extended ListBox", SS_LEFT, 240, 10, 200, 15, Parent);
+    PageData->hExtended =
+        CreateChild(0, L"LISTBOX", NULL, LBS_STANDARD | LBS_EXTENDEDSEL, 240, 30, 200, 100, Parent);
+    PopulateListBox(PageData->hExtended, FALSE);
+
+    return TRUE;
+}
 
 LRESULT
 ListBoxPageProc(PPAGE_HOST PageHost, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    switch (msg)
+    {
+        case WM_CREATE:
+        {
+            PLISTBOX_PAGE_DATA PageData;
+
+            PageData = HeapAlloc(GetProcessHeap(),
+                                 HEAP_ZERO_MEMORY,
+                                 sizeof(*PageData));
+
+            if (!PageData)
+                return FALSE;
+
+            PageHost->UserData = PageData;
+            return InitPage(PageHost->Wnd, PageData);
+        }
+
+        case WM_DESTROY:
+        {
+            PLISTBOX_PAGE_DATA PageData = (PLISTBOX_PAGE_DATA)PageHost->UserData;
+            HeapFree(GetProcessHeap(), 0, PageData);
+            return TRUE;
+        }
+    }
+
     return FALSE;
 }
