fe6b85883d980c2abe1bc4505209a9da0d3681f3
thiagomucci <thmucc@protonmail.com>
2026-07-17 22:52:53 -0400

    msconfig: disable tree view label editing via window styles

--- base/applications/msconfig/systempage.c
+++ base/applications/msconfig/systempage.c
@@ -91,10 +91,20 @@ SystemPageWndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message) {
         case WM_INITDIALOG:
         {
+            HWND hTree;
+            LONG_PTR style;
             hSystemDialog = hDlg;
             SetWindowPos(hDlg, NULL, 10, 32, 0, 0, SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOSIZE | SWP_NOZORDER);
             InitializeSystemDialog(hDlg);
             DisableAllExcept(hDlg, IDC_SYSTEM_TREE); // FIXME: Implement saving
+            hTree = GetDlgItem(hDlg, IDC_SYSTEM_TREE);
+            if (hTree)
+            {
+                style = GetWindowLongPtrW(hTree, GWL_STYLE);
+                style &= ~TVS_EDITLABELS;
+                SetWindowLongPtrW(hTree, GWL_STYLE, style);
+                SetWindowPos(hTree, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+            }
             return TRUE;
         }
     }
