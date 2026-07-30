502770af5826ae49dc1a805ce89a3cd40ce875a9
Stanislav Motylkov <x86corez@gmail.com>
2026-07-31 02:08:57 +0300

    [SHELL32_APITEST] Extend ShellState API tests
    - Skip registry tests if the key is missing, but keep testing
    - Add tests for different bitmasks and compare amount of set bytes
    - Add inter-process tests (using shell32_apitest_sub) that verify
      shell behavior, especially covering parts that were recently fixed
      in 4fecc908e7d (#9293).
    
    All tests are confirmed to pass on Windows 2003, Vista, 7, 8.1 and 10.
    
    CORE-20585 CORE-12822

--- modules/rostests/apitests/shell32/ShellState.cpp
+++ modules/rostests/apitests/shell32/ShellState.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "shelltest.h"
+#include "shell32_apitest_sub.h"
 
 #define NDEBUG
 #include <debug.h>
@@ -14,6 +15,7 @@
 #include <strsafe.h>
 #include <shlwapi.h>
 #include <shlwapi_undoc.h>
+#include <versionhelpers.h>
 
 /* [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer] */
 /* The contents of RegValue ShellState. */
@@ -23,9 +25,15 @@ typedef struct REGSHELLSTATE
     SHELLSTATE ss;
 } REGSHELLSTATE, *PREGSHELLSTATE;
 
-static void dump(const char *name, const void *ptr, size_t siz)
+static const LPCWSTR s_pszExplorer =
+    L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer";
+static const LPCWSTR s_pszShellState =
+    L"ShellState";
+
+static ULONG dump(const char *name, const void *ptr, size_t siz)
 {
     char buf[256], sz[16];
+    ULONG ret = 0;
 
     StringCbCopyA(buf, sizeof(buf), name);
     StringCbCatA(buf, sizeof(buf), ": ");
@@ -33,11 +41,14 @@ static void dump(const char *name, const void *ptr, size_t siz)
     const BYTE *pb = reinterpret_cast<const BYTE *>(ptr);
     while (siz--)
     {
+        if (*pb)
+            ret++;
         StringCbPrintfA(sz, sizeof(sz), "%02X ", *pb++);
         StringCbCatA(buf, sizeof(buf), sz);
     }
 
     trace("%s\n", buf);
+    return ret;
 }
 
 static int read_key(REGSHELLSTATE *prss)
@@ -45,8 +56,6 @@ static int read_key(REGSHELLSTATE *prss)
     HKEY hKey;
     LONG result;
     DWORD cb;
-    static const LPCWSTR s_pszExplorer =
-        L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer";
 
     memset(prss, 0, sizeof(*prss));
 
@@ -61,10 +70,9 @@ static int read_key(REGSHELLSTATE *prss)
     }
 
     cb = sizeof(*prss);
-    result = RegQueryValueExW(hKey, L"ShellState", NULL, NULL, reinterpret_cast<LPBYTE>(prss), &cb);
+    result = RegQueryValueExW(hKey, s_pszShellState, NULL, NULL, reinterpret_cast<LPBYTE>(prss), &cb);
     RegCloseKey(hKey);
 
-    ok(result == ERROR_SUCCESS, "result was %ld\n", result);
     if (result != ERROR_SUCCESS)
     {
         skip("RegQueryValueEx failed: %ld\n", result);
@@ -152,8 +160,350 @@ static int read_advanced_key(SHELLSTATE* pss)
 
 static int dump_pss(SHELLSTATE *pss)
 {
-    dump("SHELLSTATE", pss, sizeof(*pss));
+    dump("SHELLSTATE KEY", pss, sizeof(*pss));
     return 0;
+}
+
+static HWND start_sub(void)
+{
+    int retries = 50;
+    WCHAR s_szSubProgram[MAX_PATH]; // shell32_apitest_sub.exe
+    if (!FindSubProgram(s_szSubProgram, _countof(s_szSubProgram)))
+    {
+        trace("shell32_apitest_sub.exe not found\n");
+        return 0;
+    }
+
+    // Close the SUB_CLASSNAME windows
+    DoWaitForWindow(SUB_CLASSNAME, SUB_CLASSNAME, TRUE, TRUE);
+
+    // Execute sub program
+    HINSTANCE hinst = ShellExecuteW(NULL, NULL, s_szSubProgram, L"----", NULL, SW_HIDE);
+    if ((INT_PTR)hinst <= 32)
+    {
+        trace("Unable to run shell32_apitest_sub.exe.\n");
+        return 0;
+    }
+
+Retry:
+    HWND s_hSubWnd = DoWaitForWindow(SUB_CLASSNAME, SUB_CLASSNAME, FALSE, FALSE);
+    if (!s_hSubWnd)
+    {
+        if (--retries > 0)
+        {
+            Sleep(100);
+            goto Retry;
+        }
+        trace("Unable to find sub-program window.\n");
+        return 0;
+    }
+
+    return s_hSubWnd;
+}
+
+static void stop_sub(HWND s_hSubWnd)
+{
+    PostMessageW(s_hSubWnd, WM_COMMAND, IDNO, 0); // Finish
+    DoWaitForWindow(SUB_CLASSNAME, SUB_CLASSNAME, TRUE, TRUE); // Close sub-windows
+}
+
+static LONG del_state_key()
+{
+    HKEY hKey;
+    LONG res = RegOpenKeyExW(HKEY_CURRENT_USER, s_pszExplorer, 0, KEY_SET_VALUE, &hKey);
+    if (res != ERROR_SUCCESS || !hKey)
+    {
+        trace("RegOpenKeyEx failed: %ld, skipping default values test\n", res);
+        return res;
+    }
+
+    res = RegDeleteValueW(hKey, s_pszShellState);
+    RegCloseKey(hKey);
+    return res;
+}
+
+static LONG state_key_exists()
+{
+    HKEY hKey;
+    LONG res = RegOpenKeyExW(HKEY_CURRENT_USER, s_pszExplorer, 0, KEY_QUERY_VALUE, &hKey);
+    if (res != ERROR_SUCCESS || !hKey)
+    {
+        trace("RegOpenKeyEx failed: %ld, skipping default values test\n", res);
+        return res;
+    }
+
+    res = RegQueryValueExW(hKey, s_pszShellState, NULL, NULL, NULL, NULL);
+    RegCloseKey(hKey);
+    return res;
+}
+
+static void process_test(SHELLSTATE *pss)
+{
+    SHELLSTATE bak, ss1, ss2;
+    SHELLSTATE_SUB sub;
+    LONG result;
+    COPYDATASTRUCT copyData = { ID_SHSTATE, sizeof(sub), &sub };
+
+    // Backup current state
+    memset(&bak, 0, sizeof(bak));
+    SHGetSetSettings(&bak, MAXDWORD, FALSE);
+
+    memset(&ss1, 0, sizeof(ss1));
+    SHGetSetSettings(&ss1, MAXDWORD, FALSE);
+
+    // Test 1: Read one field, then write another
+    HWND s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping read/write value test 1\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    sub.dwMask = SSF_DOUBLECLICKINWEBVIEW;
+    sub.bSet = FALSE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    sub.ss.fNoConfirmRecycle = !ss1.fNoConfirmRecycle;
+    ss1.fNoConfirmRecycle = sub.ss.fNoConfirmRecycle;
+    sub.dwMask = SSF_NOCONFIRMRECYCLE;
+    sub.bSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    memset(&ss2, 0, sizeof(ss2));
+    SHGetSetSettings(&ss2, MAXDWORD, FALSE);
+
+#define CHECK_BITS(x) ok(ss1.x == ss2.x, "ss2.%s expected %d, was %d\n", #x, (int)ss1.x, (int)ss2.x)
+    CHECK_BITS(fNoConfirmRecycle);
+    CHECK_BITS(fDoubleClickInWebView);
+    CHECK_BITS(fDesktopHTML);
+    CHECK_BITS(fWin95Classic);
+    CHECK_BITS(lParamSort);
+    CHECK_BITS(iSortDirection);
+    CHECK_BITS(fStartPanelOn);
+    CHECK_BITS(fShowCompColor);
+    CHECK_BITS(fShowAttribCol);
+    CHECK_BITS(fShowInfoTip);
+
+    stop_sub(s_hSubWnd);
+
+    // Test 2: Only write a field
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping write value test 2\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    sub.ss.fNoConfirmRecycle = !ss1.fNoConfirmRecycle;
+    ss1.fNoConfirmRecycle = sub.ss.fNoConfirmRecycle;
+    sub.dwMask = SSF_NOCONFIRMRECYCLE;
+    sub.bSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    memset(&ss2, 0, sizeof(ss2));
+    SHGetSetSettings(&ss2, MAXDWORD, FALSE);
+
+    CHECK_BITS(fNoConfirmRecycle);
+    CHECK_BITS(fDoubleClickInWebView);
+    CHECK_BITS(fDesktopHTML);
+    CHECK_BITS(fWin95Classic);
+    CHECK_BITS(lParamSort);
+    CHECK_BITS(iSortDirection);
+    CHECK_BITS(fStartPanelOn);
+    CHECK_BITS(fShowCompColor);
+    CHECK_BITS(fShowAttribCol);
+    CHECK_BITS(fShowInfoTip);
+
+    stop_sub(s_hSubWnd);
+
+    // Test 3: Write another one
+    ss1.fNoConfirmRecycle = 0;
+    SHGetSetSettings(&ss1, SSF_NOCONFIRMRECYCLE, TRUE);
+    ss1.fNoConfirmRecycle = 1;
+    ss1.fDoubleClickInWebView = 1;
+    SHGetSetSettings(&ss1, SSF_NOCONFIRMRECYCLE | SSF_DOUBLECLICKINWEBVIEW, TRUE);
+
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping write value test 3\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    sub.ss.fDoubleClickInWebView = !ss1.fDoubleClickInWebView;
+    ss1.fDoubleClickInWebView = sub.ss.fDoubleClickInWebView;
+    sub.dwMask = SSF_DOUBLECLICKINWEBVIEW;
+    sub.bSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    memset(&ss2, 0, sizeof(ss2));
+    SHGetSetSettings(&ss2, MAXDWORD, FALSE);
+
+    CHECK_BITS(fNoConfirmRecycle);
+    CHECK_BITS(fDoubleClickInWebView);
+    CHECK_BITS(fDesktopHTML);
+    CHECK_BITS(fWin95Classic);
+    CHECK_BITS(lParamSort);
+    CHECK_BITS(iSortDirection);
+    CHECK_BITS(fStartPanelOn);
+    CHECK_BITS(fShowCompColor);
+    CHECK_BITS(fShowAttribCol);
+    CHECK_BITS(fShowInfoTip);
+
+    stop_sub(s_hSubWnd);
+
+    // Test 4: Check default state values
+    ss1.fNoConfirmRecycle = 1;
+    ss1.fDoubleClickInWebView = 0;
+    ss1.fDesktopHTML = 1;
+    ss1.fWin95Classic = 1;
+    ss1.fStartPanelOn = 0;
+    ss1.fShowCompColor = 0;
+    ss1.fShowAttribCol = 1;
+    ss1.fShowInfoTip = 0;
+    SHGetSetSettings(&ss1, MAXDWORD, TRUE);
+
+    // ShellState key is expected to always exist at this point (we have set the state above)
+    result = del_state_key();
+    ok(result == ERROR_SUCCESS, "del_state_key failed: %ld\n", result);
+    if (result != ERROR_SUCCESS)
+    {
+        skip("Skipping default values test\n");
+        goto Cleanup;
+    }
+
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping default values test\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    sub.dwMask = SSF_NOCONFIRMRECYCLE;
+    sub.bGetSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    memset(&ss2, 0, sizeof(ss2));
+    SHGetSetSettings(&ss2, MAXDWORD, FALSE);
+
+#define CHECK_TRUE(x) ok(ss2.x, "ss2.%s expected to be TRUE\n", #x)
+#define CHECK_FALSE(x) ok(!ss2.x, "ss2.%s expected to be FALSE\n", #x)
+    if (GetNTVersion() < _WIN32_WINNT_WIN8)
+        CHECK_FALSE(fNoConfirmRecycle); // Different between Windows versions
+    else
+        CHECK_TRUE(fNoConfirmRecycle);
+    CHECK_TRUE(fDoubleClickInWebView);
+    CHECK_FALSE(fDesktopHTML);
+    CHECK_FALSE(fWin95Classic);
+    if (IsReactOS())
+        CHECK_FALSE(fStartPanelOn); // CORE-12158: Disabled intentionally, as no Modern Start menu yet
+    else
+        CHECK_TRUE(fStartPanelOn);
+    CHECK_BITS(fShowCompColor); // Keeps the state
+    CHECK_FALSE(fShowAttribCol);
+    CHECK_BITS(fShowInfoTip); // Keeps the state
+
+    stop_sub(s_hSubWnd);
+
+    // Test 5: Check when the registry key is created
+    result = del_state_key();
+    ok(result == ERROR_SUCCESS, "del_state_key failed: %ld\n", result);
+    if (result != ERROR_SUCCESS)
+    {
+        skip("Skipping registry key test\n");
+        goto Cleanup;
+    }
+
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping default values test\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    // Just get the field
+    sub.dwMask = SSF_DOUBLECLICKINWEBVIEW;
+    sub.bSet = FALSE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    stop_sub(s_hSubWnd);
+
+    result = state_key_exists();
+    if (GetNTVersion() < _WIN32_WINNT_WIN7)
+    {
+        ok(result == ERROR_FILE_NOT_FOUND, "There should be no registry key: %ld\n", result);
+    }
+    else
+    {
+        trace("state_key_exists(): %ld\n", result); // This may be random
+    }
+    if (result == ERROR_SUCCESS)
+        del_state_key();
+
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping default values test\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    // Set the field to expected default value
+    sub.ss.fDoubleClickInWebView = 1;
+    sub.dwMask = SSF_DOUBLECLICKINWEBVIEW;
+    sub.bSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    stop_sub(s_hSubWnd);
+
+    result = state_key_exists();
+    if (GetNTVersion() < _WIN32_WINNT_WIN7)
+    {
+        ok(result == ERROR_FILE_NOT_FOUND, "There should be no registry key: %ld\n", result);
+    }
+    else
+    {
+        trace("state_key_exists(): %ld\n", result); // This may be random
+    }
+    if (result == ERROR_SUCCESS)
+        del_state_key();
+
+    s_hSubWnd = start_sub();
+    if (!s_hSubWnd)
+    {
+        skip("Skipping default values test\n");
+        goto Cleanup;
+    }
+
+    memset(&sub, 0, sizeof(sub));
+
+    // Set the field to different value
+    sub.ss.fDoubleClickInWebView = 0;
+    sub.dwMask = SSF_DOUBLECLICKINWEBVIEW;
+    sub.bSet = TRUE;
+    SendMessageW(s_hSubWnd, WM_COPYDATA, 0, (LPARAM)&copyData);
+
+    stop_sub(s_hSubWnd);
+
+    result = state_key_exists();
+    ok(result == ERROR_SUCCESS, "Registry state value expected: %ld\n", result);
+
+Cleanup:
+    // Restore current state
+    SHGetSetSettings(&bak, MAXDWORD, TRUE);
+
+    return;
 }
 
 START_TEST(ShellState)
@@ -162,8 +512,10 @@ START_TEST(ShellState)
     REGSHELLSTATE rss;
     SHELLSTATE ss, *pss;
     SHELLFLAGSTATE FlagState;
+    BOOL GotReg = TRUE;
     LPBYTE pb;
     int ret;
+    ULONG c1, c2;
 
     trace("GetVersion(): 0x%08lX\n", GetVersion());
 
@@ -201,7 +553,8 @@ START_TEST(ShellState)
     ret = read_key(&rss);
     if (ret)
     {
-        return;
+        GotReg = FALSE;
+        goto SkipReg;
     }
 
     dump_pss(pss);
@@ -243,6 +596,7 @@ START_TEST(ShellState)
     DUMP_BOOL(pss->fShowStatusBar);
 #endif
 
+SkipReg:
 #define SSF_MASK \
     (SSF_SHOWALLOBJECTS | SSF_SHOWEXTENSIONS | SSF_NOCONFIRMRECYCLE | SSF_SHOWSYSFILES | \
      SSF_SHOWCOMPCOLOR | SSF_DOUBLECLICKINWEBVIEW | SSF_DESKTOPHTML | \
@@ -254,6 +608,8 @@ START_TEST(ShellState)
     /* Get the settings */
     memset(&ss, 0, sizeof(ss));
     SHGetSetSettings(&ss, SSF_MASK, FALSE);
+    if (GotReg)
+    {
 #define CHECK_REG_FLAG(x) ok(pss->x == ss.x, "ss.%s expected %d, was %d\n", #x, (int)pss->x, (int)ss.x)
     CHECK_REG_FLAG(fShowAllObjects);
     CHECK_REG_FLAG(fShowExtensions);
@@ -276,6 +632,14 @@ START_TEST(ShellState)
 #if NTDDI_VERSION >= NTDDI_WIN8     // for future use
     CHECK_REG_FLAG(fShowStatusBar);
 #endif
+    }
+    c1 = dump("SHELLSTATE SSF_MASK", &ss, sizeof(ss));
+
+    memset(&ss, 0, sizeof(ss));
+    SHGetSetSettings(&ss, MAXDWORD, FALSE);
+    c2 = dump("SHELLSTATE MAXDWORD", &ss, sizeof(ss));
+
+    ok(c2 >= c1, "Set bytes in MAXDWORD state expected to be greater than %u, was %u\n", c1, c2);
 
     /* Get the flag settings */
     memset(&FlagState, 0, sizeof(FlagState));
@@ -298,6 +662,16 @@ START_TEST(ShellState)
     CHECK_FLAG(fAutoCheckSelect);
     CHECK_FLAG(fIconsOnly);
 #endif
+    c1 = dump("SHELLFLAGSTATE SSF_MASK", &FlagState, sizeof(FlagState));
+
+    memset(&FlagState, 0, sizeof(FlagState));
+    SHGetSettings(&FlagState, MAXDWORD);
+    c2 = dump("SHELLFLAGSTATE MAXDWORD", &FlagState, sizeof(FlagState));
+
+    ok(c2 >= c1, "Set bytes in MAXDWORD flags expected to be greater than %u, was %u\n", c1, c2);
+
+    /* Inter-process tests */
+    process_test(pss);
 
     /* Structure alignment tests */
 #if 1
