d8850a7415443bf376425e808d9e37c938053acc
Mikhail Tyukin <mishakeys20@gmail.com>
2026-08-28 14:31:19 -0400

    [FAULTREP] Sync to Wine-10.0

--- modules/rostests/winetests/faultrep/faultrep.c
+++ modules/rostests/winetests/faultrep/faultrep.c
@@ -36,63 +36,28 @@ static const char regpath_exclude[] = "ExclusionList";
 
 static BOOL is_process_limited(void)
 {
-    static BOOL (WINAPI *pCheckTokenMembership)(HANDLE,PSID,PBOOL) = NULL;
-    static BOOL (WINAPI *pOpenProcessToken)(HANDLE, DWORD, PHANDLE) = NULL;
-    SID_IDENTIFIER_AUTHORITY NtAuthority = {SECURITY_NT_AUTHORITY};
-    PSID Group;
-    BOOL IsInGroup;
     HANDLE token;
+    TOKEN_ELEVATION_TYPE type = TokenElevationTypeDefault;
+    DWORD size;
 
-    if (!pOpenProcessToken)
-    {
-        HMODULE hadvapi32 = GetModuleHandleA("advapi32.dll");
-        pOpenProcessToken = (void*)GetProcAddress(hadvapi32, "OpenProcessToken");
-        pCheckTokenMembership = (void*)GetProcAddress(hadvapi32, "CheckTokenMembership");
-        if (!pCheckTokenMembership || !pOpenProcessToken)
-        {
-            /* Win9x (power to the masses) or NT4 (no way to know) */
-            trace("missing pOpenProcessToken or CheckTokenMembership\n");
-            return FALSE;
-        }
-    }
+    OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token);
+    GetTokenInformation(token, TokenElevationType, &type, sizeof(type), &size);
+    CloseHandle(token);
 
-    if (!AllocateAndInitializeSid(&NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID,
-                                  DOMAIN_ALIAS_RID_ADMINS,
-                                  0, 0, 0, 0, 0, 0, &Group) ||
-        !pCheckTokenMembership(NULL, Group, &IsInGroup))
-    {
-        trace("Could not check if the current user is an administrator\n");
-        return FALSE;
-    }
-    if (!IsInGroup)
-    {
-        if (!AllocateAndInitializeSid(&NtAuthority, 2,
-                                      SECURITY_BUILTIN_DOMAIN_RID,
-                                      DOMAIN_ALIAS_RID_POWER_USERS,
-                                      0, 0, 0, 0, 0, 0, &Group) ||
-            !pCheckTokenMembership(NULL, Group, &IsInGroup))
-        {
-            trace("Could not check if the current user is a power user\n");
-            return FALSE;
-        }
-        if (!IsInGroup)
-        {
-            /* Only administrators and power users can be powerful */
-            return TRUE;
-        }
-    }
+    return type == TokenElevationTypeLimited;
+}
 
-    if (pOpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
-    {
-        BOOL ret;
-        TOKEN_ELEVATION_TYPE type = TokenElevationTypeDefault;
-        DWORD size;
+static BOOL is_registry_virtualization_enabled(void)
+{
+    HANDLE token;
+    DWORD enabled = FALSE;
+    DWORD size;
 
-        ret = GetTokenInformation(token, TokenElevationType, &type, sizeof(type), &size);
-        CloseHandle(token);
-        return (ret && type == TokenElevationTypeLimited);
-    }
-    return FALSE;
+    OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token);
+    GetTokenInformation(token, TokenVirtualizationEnabled, &enabled, sizeof(enabled), &size);
+    CloseHandle(token);
+
+    return enabled;
 }
 
 
@@ -107,11 +72,6 @@ static void test_AddERExcludedApplicationA(void)
 
     /* clean state */
     lres = RegCreateKeyA(HKEY_LOCAL_MACHINE, regpath_root, &hroot);
-    if (lres == ERROR_ACCESS_DENIED)
-    {
-        skip("Not enough access rights\n");
-        return;
-    }
 
     if (!lres)
         lres = RegOpenKeyA(hroot, regpath_exclude, &hexclude);
@@ -122,11 +82,11 @@ static void test_AddERExcludedApplicationA(void)
 
     SetLastError(0xdeadbeef);
     res = AddERExcludedApplicationA(NULL);
-    ok(!res, "got %d and 0x%x (expected FALSE)\n", res, GetLastError());
+    ok(!res, "got %d and 0x%lx (expected FALSE)\n", res, GetLastError());
 
     SetLastError(0xdeadbeef);
     res = AddERExcludedApplicationA("");
-    ok(!res, "got %d and 0x%x (expected FALSE)\n", res, GetLastError());
+    ok(!res, "got %d and 0x%lx (expected FALSE)\n", res, GetLastError());
 
     SetLastError(0xdeadbeef);
     /* existence of the path doesn't matter this function succeeded */
@@ -134,16 +94,17 @@ static void test_AddERExcludedApplicationA(void)
     if (is_process_limited())
     {
         /* LastError is not set! */
-        ok(!res, "AddERExcludedApplicationA should have failed got %d\n", res);
+        ok(!res || broken(is_registry_virtualization_enabled()) /* win8 */,
+           "AddERExcludedApplicationA should have failed, got %d\n", res);
     }
     else
     {
-        ok(res, "AddERExcludedApplicationA failed (le=0x%x)\n", GetLastError());
+        ok(res, "AddERExcludedApplicationA failed (le=0x%lx)\n", GetLastError());
 
         /* add, when already present */
         SetLastError(0xdeadbeef);
         res = AddERExcludedApplicationA("winetest_faultrep.exe");
-        ok(res, "AddERExcludedApplicationA failed (le=0x%x)\n", GetLastError());
+        ok(res, "AddERExcludedApplicationA failed (le=0x%lx)\n", GetLastError());
     }
 
     /* cleanup */
