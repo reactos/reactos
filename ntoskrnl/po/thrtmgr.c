1835fdaaf2842c7aa3d87e8fd9b0b4b988c41b24
George Bișoc <george.bisoc@reactos.org>
2026-08-29 23:07:53 +0200

    [NTOS:PO] Stub the entire rest of Power Manager
    These files serve as the skeleton of the Power Manager directory structure.
    More stuff will come later as I continue working on Power Manager.

---
+++ ntoskrnl/po/thrtmgr.c
@@ -0,0 +1,16 @@
+/*
+ * PROJECT:     ReactOS Kernel
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Throttle manager infrastructure
+ * COPYRIGHT:   Copyright 2023 George Bișoc <george.bisoc@reactos.org>
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include <ntoskrnl.h>
+#define NDEBUG
+#include <debug.h>
+
+/* PRIVATE FUNCTIONS **********************************************************/
+
+/* EOF */
