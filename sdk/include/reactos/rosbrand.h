cf144a03a96f29a8af0b4e0c1fa18a9caa8161dd
Carl J. Bialorucki <carl.bialorucki@reactos.org>
2026-09-03 18:59:38 -0500

    [ROSBRAND] Add branding library

---
+++ sdk/include/reactos/rosbrand.h
@@ -0,0 +1,9 @@
+/*
+ * PROJECT:     ReactOS Branding
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Header for using dynamic brand resources
+ * COPYRIGHT:   Copyright 2026 Carl Bialorucki <carl.bialorucki@reactos.org>
+ */
+
+#define IDB_BRAND_BANNER      100
+#define IDB_BRAND_BANNERLINE  101
