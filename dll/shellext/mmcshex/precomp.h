37edcadb8653e885b6d42c61c0ba985e5f718d26
Whindmar Saksit <whindsaks@proton.me>
2026-09-04 21:27:47 +0200

    [MMCSHEX] Created MMCSHEX icon extractor for .msc files

---
+++ dll/shellext/mmcshex/precomp.h
@@ -0,0 +1,17 @@
+/*
+ * PROJECT:     ReactOS MSC Shell Extension
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Precompiled header file
+ * COPYRIGHT:   Copyright 2026 Whindmar Saksit <whindsaks@proton.me>
+ */
+
+#pragma once
+#define NTOS_MODE_USER
+#include <windows.h>
+#include <atlbase.h>
+#include <atlcom.h>
+#include <strsafe.h>
+#include <shlobj.h>
+#include <shobjidl.h>
+#include <shlwapi.h>
+#include <shellapi.h>
