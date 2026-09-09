420ca5b9c76b6444ca826ca6d3030fd10fd4db83
Doug Lyons <douglyons@douglyons.com>
2026-09-09 18:45:18 -0500

    Only skip these tests if we are running ReactOS.
    Co-authored-by: Carl J. Bialorucki <carl.bialorucki@reactos.org>

--- modules/rostests/winetests/winhttp/winhttp.c
+++ modules/rostests/winetests/winhttp/winhttp.c
@@ -6297,7 +6297,7 @@ START_TEST (winhttp)
     }
 
 #ifdef __REACTOS__
-    if (!winetest_interactive)
+    if (!winetest_interactive && is_reactos())
     {
         skip("Skipping tests due to hang. See ROSTESTS-422\n");
     }
