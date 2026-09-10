c7ede94758d577d3885a2cb4d05e437cf0bd41da
Hervé Poussineau <hpoussin@reactos.org>
2026-09-10 19:04:09 +0200

    [VIDEOPRT] Remove old ReactOS hack
    I'm not sure of the reason, but /NOVESA and /BASEVIDEO are different options.
    /NOVESA requests vga.sys/vgapnp.sys to not use VESA video modes
    /BASEVIDEO requests win32k to not use primary graphic driver, but VgaSave one.

--- win32ss/drivers/videoprt/videoprt.c
+++ win32ss/drivers/videoprt/videoprt.c
@@ -901,9 +901,6 @@ IntLoadRegistryParameters(VOID)
 
     ExFreePoolWithTag(KeyInfo, TAG_VIDEO_PORT);
 
-    /* FIXME: Old ReactOS-compatibility... */
-    if (VpBaseVideo) VpNoVesa = TRUE;
-
     if (VpNoVesa)
         VideoPortDebugPrint(Info, "VESA mode disabled\n");
     else
