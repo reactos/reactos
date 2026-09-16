0345c201b4f02c63d5c69b7c871fc285e0a1e285
Alex Mendoza <05alex.mendozaa@gmail.com>
2026-09-16 12:14:32 +0200

    [HALX86] Add header file for x2APIC (#9441)
    Create a new HAL module for x2APIC support, starting with its private
    header! x2APIC and xAPIC are sadly not register compatible, so this lives
    as its own module instead of extending the existing APIC.
    
    This adds the following:
    - MSR base/offset defines,
    - the x2APIC `APIC_BASE_ADDRESS_REGISTER` and
      `APIC_INTERRUPT_COMMAND_REGISTER` layouts
    - MSR based register access helpers,
    - and declarations for `X2ApicIsSupported`/`X2ApicEnable`
      (they will be implemented in another PR)

---
+++ hal/halx86/x2apic/x2apicp.h
@@ -0,0 +1,101 @@
+/*
+ * PROJECT:     ReactOS Kernel
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Private header file for x2APIC support in HAL
+ * COPYRIGHT:   Copyright 2026 Alex Mendoza <05alex.mendozaa@gmail.com>
+ */
+
+#pragma once
+
+#define MSR_APIC_BASE 0x0000001B
+#define X2APIC_MSR_BASE 0x00000800
+#define X2APIC_MSR_ICR 0x00000830
+#define X2APIC_MSR_SELF_IPI 0x0000083F
+#define CPUID_X2APIC_FEATURE_BIT 21
+
+#include <pshpack1.h>
+typedef union _X2APIC_BASE_ADDRESS_REGISTER
+{
+    UINT64 LongLong;
+    struct
+    {
+        UINT64 Reserved1:8;
+        UINT64 BootStrapCPUCore:1;
+        UINT64 Reserved2:1;
+        UINT64 EnableX2Apic:1;
+        UINT64 Enable:1;
+        UINT64 BaseAddress:40;
+        UINT64 ReservedMBZ:12;
+    };
+} X2APIC_BASE_ADDRESS_REGISTER;
+#include <poppack.h>
+
+typedef enum _X2APIC_REGISTER
+{
+    X2APIC_ID_REG = 0x0020,
+    X2APIC_EOI_REG = 0x00B0,
+    X2APIC_SIVR_REG = 0x00F0,
+    X2APIC_ICR0_REG = 0x0300,
+} X2APIC_REGISTER;
+
+#include <pshpack1.h>
+typedef union _X2APIC_INTERRUPT_COMMAND_REGISTER
+{
+    UINT64 LongLong;
+    struct
+    {
+        UINT64 Vector:8;
+        UINT64 MessageType:3;
+        UINT64 DestinationMode:1;
+        UINT64 DeliveryStatus:1;
+        UINT64 ReservedMBZ:1;
+        UINT64 Level:1;
+        UINT64 TriggerMode:1;
+        UINT64 ReservedMBZ2:2;
+        UINT64 DestinationShortHand:2;
+        UINT64 ReservedMBZ3:12;
+        UINT64 Destination:32;
+    };
+} X2APIC_INTERRUPT_COMMAND_REGISTER;
+#include <poppack.h>
+
+FORCEINLINE
+X2APIC_REGISTER
+X2ApicMsrFromRegister(
+    _In_ X2APIC_REGISTER Register)
+{
+    return (X2APIC_REGISTER)(X2APIC_MSR_BASE + (Register / 0x10));
+}
+
+FORCEINLINE
+ULONG
+X2ApicRead(
+    _In_ X2APIC_REGISTER Register)
+{
+    return (ULONG)__readmsr(X2ApicMsrFromRegister(Register));
+}
+
+FORCEINLINE
+VOID
+X2ApicWrite(
+    _In_ X2APIC_REGISTER Register,
+    _In_ ULONG Value)
+{
+    __writemsr(X2ApicMsrFromRegister(Register), Value);
+}
+
+FORCEINLINE
+VOID
+X2ApicWriteIcr(
+    _In_ X2APIC_INTERRUPT_COMMAND_REGISTER IcrValue)
+{
+    __writemsr(X2APIC_MSR_ICR, IcrValue.LongLong);
+}
+
+BOOLEAN
+NTAPI
+X2ApicIsSupported(VOID);
+
+VOID
+NTAPI
+X2ApicEnable(VOID);
